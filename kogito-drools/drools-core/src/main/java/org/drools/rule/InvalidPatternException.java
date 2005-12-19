723209ee5a4ff14dc0f17e38a435e405ba595a9d
Mark Proctor <mdproctor@gmail.com>
2005-12-19 00:07:09 +0000

    JBRULES-10 : Rete Builder -Take a given AND Conditional Element and turn it and all its children into a Rete network.
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@1855 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-core/src/main/java/org/drools/rule/InvalidPatternException.java
@@ -0,0 +1,35 @@
+package org.drools.rule;
+
+public class InvalidPatternException extends RuleConstructionException {
+    /**
+     * @see java.lang.Exception#Exception()
+     */
+	InvalidPatternException()
+    {
+        super();
+    }
+
+    /**
+     * @see java.lang.Exception#Exception(String message)
+     */
+	InvalidPatternException( String message )
+    {
+        super( message );
+    }
+
+    /**
+     * @see java.lang.Exception#Exception(String message, Throwable cause)
+     */
+	InvalidPatternException( String message, Throwable cause )
+    {
+        super( message, cause );
+    }
+
+    /**
+     * @see java.lang.Exception#Exception(Throwable cause)
+     */
+	InvalidPatternException( Throwable cause )
+    {
+        super( cause );
+    }
+}
