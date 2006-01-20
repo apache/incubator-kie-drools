3e978583ceb0d5730fed7be5212f2324c9c71286
Michael Neale <michael.neale@gmail.com>
2006-01-20 05:09:58 +0000

    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@2148 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-smf/src/main/java/org/drools/smf/ConditionInvoker.java
+++ drools-smf/src/main/java/org/drools/smf/ConditionInvoker.java
@@ -5,6 +5,10 @@ import java.util.Map;
 import org.drools.rule.Declaration;
 import org.drools.spi.Tuple;
 
+/**
+ * @deprecated Not used in Drools 3.0
+ *
+ */
 public interface ConditionInvoker extends Invoker
 {
     public boolean invoke(Tuple tuple,
