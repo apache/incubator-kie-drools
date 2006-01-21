b12c751a27f2089903fbeabdf77da0efb456c984
Michael Neale <michael.neale@gmail.com>
2006-01-21 03:35:23 +0000

    seperated out from natural-dsl
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@2162 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-ruledoc/README.txt
@@ -0,0 +1,13 @@
+The Drools Rule Documents module aims to provide an alternative means of rule management.
+
+Ruledocuments are human readable documents that have actual rules (nominally in a pseudo natural 
+and domain specific language) embedded in them.
+
+Ideas come from Donald Knuth's "Literate Programming" (www.literateprogramming.com).
+
+The most fundamental valid format these days is HTML, this it will be supported as default.
+
+Other valid and possible formats are OpenDocument (open office) and OfficeXML (Microsoft) from 
+word processors.
+
+The key idea is to keep documentation and the actual rules together.
---
+++ kogito-drools/drools-ruledoc/src/test/java/org/drools/natural/ruledoc/DictionaryHelperTest.java
@@ -0,0 +1,18 @@
+package org.drools.natural.ruledoc;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import junit.framework.TestCase;
+
+public class DictionaryHelperTest extends TestCase
+{
+    public void testLoad() throws IOException {
+        
+        Properties props = new Properties();
+        props.load(getClass().getResourceAsStream("sample.dictionary.properties"));
+        DictionaryHelper dic = new DictionaryHelper(props);        
+        assertEquals("convertToDate(\"${right}\")", dic.getItem("date of"));       
+        assertEquals("System.out.println(${right})", dic.getGrammar().getExpression("Log"));
+    }
+}
