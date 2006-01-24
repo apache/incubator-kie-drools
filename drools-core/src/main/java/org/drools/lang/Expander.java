70cd1a909f480291b9c4e9c572a0fe2fc71e49c8
Michael Neale <michael.neale@gmail.com>
2006-01-24 12:06:36 +0000

    first cut of expanders.
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@2189 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/main/java/org/drools/lang/Expander.java
+++ drools-core/src/main/java/org/drools/lang/Expander.java
@@ -1,7 +1,16 @@
 package org.drools.lang;
 
 /**
- * Expanders provide just in time expansion for expressions in DRL.
+ * Expanders are extension points for expanding 
+ * expressions in DRL at parse time.
+ * This is just-in-time translation, or macro expansion, or
+ * whatever you want.
+ * 
+ * The important thing is that it happens at the last possible moment, 
+ * so any errors in expansion are included in the parsers errors.
+ * 
+ * Just-in-time expansions may include complex pre-compilers, 
+ * or just macros, and everything in between.
  * 
  * Expanders should ideally not make presumptions on any embedded semantic 
  * language. For instance, java aware pre processing should be done in
@@ -13,4 +22,24 @@ package org.drools.lang;
  */
 public interface Expander {
 
+    /**
+     * The parser should call this on an expression/line that potentially needs expanding 
+     * BEFORE it parses that line (as the line may change radically as the result of expansion).
+     * 
+     * Expands the expression Just-In-Time for the parser.
+     * If the expression is not meant to be expanded, or if no
+     * appropriate expander is found, it will echo back the same 
+     * expression.
+     * 
+     * @param expression The "line" or expression to be expanded/pre-compiled.
+     * @param context The context of the current state of parsing. This can help
+     * the expander know if it needs to expand, what to do etc.
+     * 
+     * If <code>isEnabled()</code> is false then it is not required to 
+     * call this method.
+     */
+    public String expand(String pattern, Parser context);
+    
+    
+    
 }
--- drools-core/src/main/java/org/drools/lang/ExpanderContext.java
+++ drools-core/src/main/java/org/drools/lang/ExpanderContext.java
@@ -1,76 +1,78 @@
 package org.drools.lang;
 
+import java.io.InputStream;
 import java.io.Serializable;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.drools.lang.dsl.DefaultExpander;
 
 /**
- * Expanders are extension points for expanding 
- * expressions in DRL at parse time.
- * This is just-in-time translation, or macro expansion, or
- * whatever you want.
+ * Create instances of appropriate expanders based on name, and prior configuration of expanders.
  * 
- * The important thing is that it happens at the last possible moment, 
- * so any errors in expansion are included in the parsers errors.
- * 
- * Just-in-time expansions may include complex pre-compilers, 
- * or just macros, and everything in between.
+ * Expanders can be added to this programmatically, or they can be loaded from the classpath.
  * 
  * @author <a href="mailto:michael.neale@gmail.com"> Michael Neale</a>
- *
  */
 public class ExpanderContext implements Serializable {
 
 	private static final long serialVersionUID = 1806461802987228880L;
-	private boolean enabled = true;
-	
-	private final Set expanders;
-	
-	/**
-	 * This indicates that at least one expander has been configured for 
-	 * this parser configuration.
-	 */
-	public boolean isEnabled() {
-		return enabled;
-	}
-	
-	public void disable() {
-		this.enabled = false;
-	}
-	
-	public ExpanderContext(Collection initialExpanders) {
-		this.expanders = new HashSet(initialExpanders);
-	}
-	
-	public ExpanderContext() {	
-		this.expanders = new HashSet();
-	}
-	
-	public ExpanderContext addExpander(Expander exp) {
-		expanders.add(exp);
-		return this;
-	}
-	
-	/**
-     * The parser should call this on an expression/line that potentially needs expanding 
-     * BEFORE it parses that line (as the line may change radically as the result of expansion).
+	private Map expanders;
+    
+    private static final ExpanderContext INSTANCE = new ExpanderContext();
+    
+	public static ExpanderContext getInstance() {
+	    return INSTANCE;
+    }
+    
+    private ExpanderContext() {
+        expanders = new HashMap();
+    }
+    
+    /** 
+     * This registers an expander against a name for DRL files to use when compiling.
+     * The name appears as "use expander name" in DRL source.
      * 
-	 * Expands the expression Just-In-Time for the parser.
-	 * If the expression is not meant to be expanded, or if no
-	 * appropriate expander is found, it will echo back the same 
-	 * expression.
-	 * 
-	 * @param expression The "line" or expression to be expanded/pre-compiled.
-	 * @param context The context of the current state of parsing. This can help
-	 * the expander know if it needs to expand, what to do etc.
-	 * 
-	 * If <code>isEnabled()</code> is false then it is not required to 
-	 * call this method.
-	 */
-	public CharSequence expand(CharSequence expression, Parser context) {
-		return expression;
-	}
+     * This can be called again to "refresh" an expander.
+     */
+    public void registerExpander(Expander expander, String name) {
+        if (expanders.containsKey(name)) {
+            expanders.remove(name);
+        }
+        expanders.put(name, expander);
+    }
+    
+    /** 
+     * This will load up the appropriate expander.
+     * The default expander is configured via a properties file. 
+     */
+    public Expander getExpander(String expanderName) {
+    
+        if (expanders.containsKey(expanderName)) return (Expander) expanders.get(expanderName);
+        
+        try {
+            InputStream stream = this.getClass().getResourceAsStream(expanderName);
+            if (stream == null) {
+                URL url = new URL(expanderName);
+                stream = url.openStream();
+            }
+            
+            Properties props = new Properties();
+            props.load(stream);
+            stream.close();
+            
+            DefaultExpander expander = new DefaultExpander(props);
+            registerExpander(expander, expanderName);
+            return expander;
+            
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Unable to load expander with name: " + expanderName);
+        }
+    }
+	
+	
 	
 	
 	
