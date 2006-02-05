3fbfdecc9a65c4b780abd7a1f558c96b5f4202c6
Michael Neale <michael.neale@gmail.com>
2006-02-05 05:25:39 +0000

    added property change support
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@2264 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/test/java/org/drools/util/proxy/ProxyGeneratorTest.java
+++ kogito-drools/drools-core/src/test/java/org/drools/util/proxy/ProxyGeneratorTest.java
@@ -1,6 +1,11 @@
 package org.drools.util.proxy;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 import junit.framework.TestCase;
 
@@ -11,7 +16,7 @@ public class ProxyGeneratorTest extends TestCase {
         original.setAge(31);
         original.setName("michael");
         
-        Person p = (Person) ProxyGenerator.generateProxy(original, false);
+        Person p = (Person) ProxyGenerator.generateProxy(original, false, false);
         standardTests( original,
                    p );
                
@@ -27,7 +32,7 @@ public class ProxyGeneratorTest extends TestCase {
         original.setAge(31);
         original.setName("michael");
         
-        Person p = (Person) ProxyGenerator.generateProxy(original, true);
+        Person p = (Person) ProxyGenerator.generateProxy(original, true, false);
         standardTests( original,
                    p );
         
@@ -37,6 +42,58 @@ public class ProxyGeneratorTest extends TestCase {
         assertEquals("jo", original.getName());        
         
     }
+    
+    public void xxtestChangeListener() throws IOException {
+        Person original = new Person();
+        original.setAge(31);
+        original.setName("michael");
+        
+        Person p = (Person) ProxyGenerator.generateProxy(original, false, true);
+        standardTests( original,
+                   p );
+               
+        
+        //check that stuff is passed through (bypasses the proxy).
+        p.changeName("jo");
+        assertEquals("jo", p.getName());
+        assertEquals("jo", original.getName());
+        
+        assertTrue(p instanceof ChangeListener);
+        ChangeListener cl = (ChangeListener) p;
+        
+        TestListener listener = new TestListener();
+        cl.addPropertyChangeListener(listener);
+        
+        p.setAge(42);
+        assertEquals(42, p.getAge()); //just to make sure it actually worked
+        assertEquals(42, original.getAge());
+        assertEquals(1, listener.events.size());
+        PropertyChangeEvent event = (PropertyChangeEvent) listener.events.get(0);
+        assertEquals("age", event.getPropertyName());
+        assertEquals(new Integer(31), event.getOldValue());
+        assertEquals(new Integer(42), event.getNewValue());
+        
+        
+        p.setName("chloe");
+        assertEquals(2, listener.events.size());
+        event = (PropertyChangeEvent) listener.events.get(1);
+        assertEquals("name", event.getPropertyName());
+        assertEquals("jo", event.getOldValue());
+        assertEquals("chloe", event.getNewValue());
+        
+    }
+    
+    public void testOtherObject() throws Exception {
+        Person p = new Person();
+        Object personProxy = ProxyGenerator.generateProxy(p, false, false);
+        
+        Child c = new Child(new BigInteger("321321321"), "Chloe Emma Neale");
+        Child proxyChild = (Child) ProxyGenerator.generateProxy(c, true, false);
+        assertNotSame(proxyChild, c);
+        assertEquals(proxyChild.getName(), c.getName());
+    }
+
+    
 
     private void standardTests(Person original,
                            Person p) {
@@ -60,6 +117,43 @@ public class ProxyGeneratorTest extends TestCase {
         assertTrue(p instanceof TargetAccessor);
         TargetAccessor t = (TargetAccessor) p;
         assertEquals(original, t.getTarget());
+    }
+    
+    public void testPerf() throws Exception {
+        
+        Person o = new Person();
+        o.setName("michael");
+        o.setAge(31);
+        
+        Person p = (Person) ProxyGenerator.generateProxy(o, true, false);
+        
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            String s = p.getName() + i;
+        }
+        System.out.println("Time taken proxy: " + (System.currentTimeMillis() - start));
+        
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            String s = o.getName() + i;
+        }
+        System.out.println("Time taken native: " + (System.currentTimeMillis() - start));        
+        
+        
+        
+        
+        
+        
+    }
+    
+    static class TestListener implements PropertyChangeListener {
+
+        List events = new ArrayList();
+        
+        public void propertyChange(PropertyChangeEvent event) {
+            events.add(event);
+        }
+        
     }
     
 }
