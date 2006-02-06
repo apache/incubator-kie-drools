2f27a444dd5dcf7f412e564202f84455886e1b6b
Mark Proctor <mdproctor@gmail.com>
2006-02-06 05:20:41 +0000

    -Adds new LinkedList based Rete implementation
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@2272 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-core/src/main/java/org/drools/spi/Constraint.java
@@ -0,0 +1,5 @@
+package org.drools.spi;
+
+public interface Constraint {
+
+}
---
+++ drools-core/src/main/java/org/drools/util/AbstractBaseLinkedListNode.java
@@ -0,0 +1,66 @@
+package org.drools.util;
+/*
+ * Copyright 2005 JBoss Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/**
+ * Provides a abstract base implementation that an object can extend so that it can be used in a LinkedList.
+ * 
+ * @see LinkedList
+ * 
+ * @author <a href="mailto:mark.proctor@jboss.com">Mark Proctor</a>
+ * @author <a href="mailto:bob@werken.com">Bob McWhirter</a>
+ */
+public class AbstractBaseLinkedListNode implements LinkedListNode {
+    private LinkedListNode               previous;
+
+    private LinkedListNode               next;  
+    
+    /**
+     * Empty Constructor
+     */
+    public AbstractBaseLinkedListNode() {
+    }        
+    
+
+    /* (non-Javadoc)
+     * @see org.drools.reteoo.LinkedListNode#getNext()
+     */
+    public LinkedListNode getNext() {
+        return this.next;
+    }
+
+    /* (non-Javadoc)
+     * @see org.drools.reteoo.LinkedListNode#setNext(org.drools.reteoo.LinkedListNode)
+     */
+    public void setNext(LinkedListNode next) {
+        this.next = next;
+    }
+
+    /* (non-Javadoc)
+     * @see org.drools.reteoo.LinkedListNode#getPrevious()
+     */
+    public LinkedListNode getPrevious() {
+        return this.previous;
+    }
+
+    /* (non-Javadoc)
+     * @see org.drools.reteoo.LinkedListNode#setPrevious(org.drools.reteoo.LinkedListNode)
+     */
+    public void setPrevious(LinkedListNode previous) {
+        this.previous = previous;
+    }    
+}
