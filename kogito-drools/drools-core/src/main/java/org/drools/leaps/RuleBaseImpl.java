204cfd57843c534e95b717bdfa809d450dc80470
Alexander Bagerman <bagerman@gmail.com>
2006-02-08 02:35:43 +0000

    refactoring
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@2301 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/main/java/org/drools/leaps/RuleBaseImpl.java
+++ kogito-drools/drools-core/src/main/java/org/drools/leaps/RuleBaseImpl.java
@@ -1,6 +1,5 @@
 package org.drools.leaps;
 
-import java.io.Serializable;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -9,205 +8,131 @@ import java.util.Map;
 import java.util.Set;
 import java.util.WeakHashMap;
 
+import org.drools.FactException;
 import org.drools.RuleBase;
+import org.drools.RuleIntegrationException;
+import org.drools.RuleSetIntegrationException;
 import org.drools.WorkingMemory;
-import org.drools.leaps.conflict.DefaultConflictResolver;
-import org.drools.reteoo.DefaultFactHandleFactory;
-import org.drools.rule.DuplicateRuleNameException;
+import org.drools.spi.FactHandleFactory;
 import org.drools.rule.InvalidPatternException;
-import org.drools.rule.InvalidRuleException;
 import org.drools.rule.Rule;
 import org.drools.rule.RuleSet;
-import org.drools.spi.FactHandleFactory;
+import org.drools.spi.ClassObjectTypeResolver;
+import org.drools.spi.ObjectTypeResolver;
 import org.drools.spi.RuleBaseContext;
 
 /**
  * This base class for the engine and analogous to Drool's RuleBase class. It
  * has a similar interface adapted to the Leaps algorithm
- * 
+ *  
  * @author Alexander Bagerman
  * 
  */
-public class RuleBaseImpl implements RuleBase, Serializable {
-	private static final long serialVersionUID = 0L;
-
-	// to store rules added just as addRule rather than as a part of ruleSet
-	private final static String defaultRuleSet = "___default___rule___set___";
-
-	private HashMap ruleSets;
-
-	private Map applicationData;
-
-	private RuleBaseContext ruleBaseContext;
-
-	private ConflictResolver conflictResolver;
-
-	private Builder builder;
+public class RuleBaseImpl implements RuleBase {
 
 	private HashMap leapsRules = new HashMap();
+
+	private final Builder builder;
 
 	/**
 	 * TODO we do not need it here. and it references RETEoo class
 	 * 
 	 * The fact handle factory.
 	 */
+	/** The fact handle factory. */
 	private final FactHandleFactory factHandleFactory;
 
-	/* @todo: replace this with a weak HashSet */
+	private Set ruleSets;
+
+	private Map applicationData;
+
+	private RuleBaseContext ruleBaseContext;
+
+	// @todo: replace this with a weak HashSet
+	/**
+	 * WeakHashMap to keep references of WorkingMemories but allow them to be
+	 * garbage collected
+	 */
 	private final transient Map workingMemories;
 
 	/** Special value when adding to the underlying map. */
 	private static final Object PRESENT = new Object();
 
-	/**
-	 * constractor that supplies default conflict resolution
-	 * 
-	 * @see LeapsDefaultConflictResolver
-	 */
-	public RuleBaseImpl() throws DuplicateRuleNameException,
-			InvalidPatternException, InvalidRuleException {
-		this(DefaultConflictResolver.getInstance(),
-				new DefaultFactHandleFactory(), new HashSet(), new HashMap(),
-				new RuleBaseContext());
+	// ------------------------------------------------------------
+	// Constructors
+	// ------------------------------------------------------------
 
+	/**
+	 * Construct.
+	 * 
+	 * @param rete
+	 *            The rete network.
+	 */
+	public RuleBaseImpl() throws RuleIntegrationException,
+			RuleSetIntegrationException, FactException, InvalidPatternException {
+		this(new HandleFactory(), new HashSet(), new HashMap(),
+				new RuleBaseContext());
 	}
 
-	public RuleBaseImpl(ConflictResolver conflictResolver,
-			FactHandleFactory factHandleFactory, Set ruleSets,
+	/**
+	 * Construct.
+	 * 
+	 * @param rete
+	 *            The rete network.
+	 * @param conflictResolver
+	 *            The conflict resolver.
+	 * @param factHandleFactory
+	 *            The fact handle factory.
+	 * @param ruleSets
+	 * @param applicationData
+	 */
+	public RuleBaseImpl(FactHandleFactory factHandleFactory, Set ruleSets,
 			Map applicationData, RuleBaseContext ruleBaseContext)
-			throws DuplicateRuleNameException, InvalidPatternException,
-			InvalidRuleException {
+			throws RuleIntegrationException, RuleSetIntegrationException,
+			FactException, InvalidPatternException {
+		ObjectTypeResolver resolver = new ClassObjectTypeResolver();
+		this.builder = new Builder(this, resolver);
 		this.factHandleFactory = factHandleFactory;
-		this.conflictResolver = conflictResolver;
+		this.ruleSets = ruleSets;
 		this.applicationData = applicationData;
 		this.ruleBaseContext = ruleBaseContext;
 		this.workingMemories = new WeakHashMap();
-		this.builder = new Builder();
 
-		this.ruleSets = new HashMap();
-		if (ruleSets != null) {
-			int i = 0;
-			RuleSet ruleSet;
-			for (Iterator it = ruleSets.iterator(); it.hasNext(); i++) {
-				ruleSet = (RuleSet) it.next();
-				this.ruleSets.put(new Integer(i), ruleSet);
-				Rule[] rules = ruleSet.getRules();
-				for (int k = 0; k < rules.length; k++) {
-					this.addRule(rules[k]);
-				}
-			}
+		this.ruleSets = new HashSet();
+		for (Iterator it = ruleSets.iterator(); it.hasNext();) {
+			this.addRuleSet((RuleSet) it.next());
 		}
-		// default one to collect standalone rules
-		this.ruleSets.put(defaultRuleSet, new RuleSet(defaultRuleSet,
-				this.ruleBaseContext));
 	}
 
-	/**
-	 * constractor. Takes conflict resolution class that for each fact and rule
-	 * sides must not return 0 if o1 != 02
-	 */
-
-	public RuleBaseImpl(ConflictResolver conflictResolver)
-			throws DuplicateRuleNameException, InvalidPatternException,
-			InvalidRuleException {
-		this(conflictResolver, new DefaultFactHandleFactory(), new HashSet(),
-				new HashMap(), new RuleBaseContext());
-
-	}
+	// ------------------------------------------------------------
+	// Instance methods
+	// ------------------------------------------------------------
 
 	/**
-	 * factory method for new working memory. will keep reference by default.
-	 * <b>Note:</b> references kept in a week hashmap.
-	 * 
-	 * @return new working memory instance
-	 * 
-	 * @see LeapsWorkingMemory
+	 * @see RuleBase
 	 */
 	public WorkingMemory newWorkingMemory() {
-		return this.newWorkingMemory(true);
+		return newWorkingMemory(true);
 	}
 
 	/**
-	 * factory method for new working memory. will keep reference by default.
-	 * <b>Note:</b> references kept in a week hashmap.
-	 * 
-	 * @param keepReference
-	 * @return new working memory instance
-	 * 
-	 * @see LeapsWorkingMemory
+	 * @see RuleBase
 	 */
 	public WorkingMemory newWorkingMemory(boolean keepReference) {
-		WorkingMemory workingMemory = new WorkingMemoryImpl(this);
-		// process existing rules
+		WorkingMemoryImpl workingMemory = new WorkingMemoryImpl(this);
+		// add all rules added so far
 		for (Iterator it = this.leapsRules.values().iterator(); it.hasNext();) {
 			((WorkingMemoryImpl) workingMemory).addLeapsRules((List) it.next());
 		}
+		//
 		if (keepReference) {
-			this.workingMemories.put(workingMemory, PRESENT);
+			this.workingMemories.put(workingMemory, RuleBaseImpl.PRESENT);
 		}
 		return workingMemory;
 	}
 
 	void disposeWorkingMemory(WorkingMemory workingMemory) {
 		this.workingMemories.remove(workingMemory);
-	}
-
-	public Set getWorkingMemories() {
-		return this.workingMemories.keySet();
-	}
-
-	/**
-	 * TODO clash with leaps conflict resolver
-	 * 
-	 * @see RuleBase
-	 */
-	public org.drools.spi.ConflictResolver getConflictResolver() {
-		return (org.drools.spi.ConflictResolver) null;
-	}
-
-	public ConflictResolver getLeapsConflictResolver() {
-		return this.conflictResolver;
-	}
-
-	/**
-	 * @see RuleBase
-	 */
-	public RuleSet[] getRuleSets() {
-		return (RuleSet[]) this.ruleSets.values().toArray(
-				new RuleSet[this.ruleSets.size()]);
-	}
-
-	/**
-	 * Creates leaps rule wrappers and propagate rule to the working memories
-	 * 
-	 * @param rule
-	 * @throws DuplicateRuleNameException
-	 * @throws InvalidRuleException
-	 * @throws InvalidPatternException
-	 */
-	public void addRule(Rule rule) throws DuplicateRuleNameException,
-			InvalidRuleException, InvalidPatternException {
-		List rules = this.builder.processRule(rule);
-
-		this.leapsRules.put(rule, rules);
-
-		for(Iterator it = this.workingMemories.keySet().iterator(); it.hasNext();) {
-			((WorkingMemoryImpl)it.next()).addLeapsRules(rules);
-		}
-	}
-
-	/**
-	 * @see RuleBase
-	 */
-	public Map getApplicationData() {
-		return this.applicationData;
-	}
-
-	/**
-	 * @see RuleBase
-	 */
-	public RuleBaseContext getRuleBaseContext() {
-		return this.ruleBaseContext;
 	}
 
 	/**
@@ -217,5 +142,120 @@ public class RuleBaseImpl implements RuleBase, Serializable {
 	 */
 	public FactHandleFactory getFactHandleFactory() {
 		return this.factHandleFactory;
+	}
+
+	public FactHandleFactory newFactHandleFactory() {
+		return this.factHandleFactory.newInstance();
+	}
+
+	/**
+	 * Assert a fact object.
+	 * 
+	 * @param handle
+	 *            The handle.
+	 * @param object
+	 *            The fact.
+	 * @param workingMemory
+	 *            The working-memory.
+	 * 
+	 * @throws FactException
+	 *             If an error occurs while performing the assertion.
+	 */
+//	void assertObject(FactHandle handle, Object object,
+//			PropagationContext context, WorkingMemoryImpl workingMemory)
+//			throws FactException {
+//		workingMemory.assertObject(object);
+//	}
+
+	/**
+	 * Retract a fact object.
+	 * 
+	 * @param handle
+	 *            The handle.
+	 * @param workingMemory
+	 *            The working-memory.
+	 * 
+	 * @throws FactException
+	 *             If an error occurs while performing the retraction.
+	 */
+//	void retractObject(FactHandle handle, PropagationContext context,
+//			WorkingMemoryImpl workingMemory) throws FactException {
+//		workingMemory.retractObject(handle);
+//	}
+//
+	public RuleSet[] getRuleSets() {
+		return (RuleSet[]) this.ruleSets.toArray(new RuleSet[this.ruleSets
+				.size()]);
+	}
+
+	public Map getApplicationData() {
+		return this.applicationData;
+	}
+
+	public RuleBaseContext getRuleBaseContext() {
+		return this.ruleBaseContext;
+	}
+
+	/**
+	 * Add a <code>RuleSet</code> to the network. Iterates through the
+	 * <code>RuleSet</code> adding Each individual <code>Rule</code> to the
+	 * network.
+	 * 
+	 * @param ruleSet
+	 *            The rule-set to add.
+	 * 
+	 * @throws RuleIntegrationException
+	 *             if an error prevents complete construction of the network for
+	 *             the <code>Rule</code>.
+	 * @throws FactException
+	 * @throws InvalidPatternException
+	 */
+	public void addRuleSet(RuleSet ruleSet) throws RuleIntegrationException,
+			RuleSetIntegrationException, FactException, InvalidPatternException {
+		Map newApplicationData = ruleSet.getApplicationData();
+
+		// Check that the application data is valid, we cannot change the type
+		// of an already declared application data variable
+		for (Iterator it = newApplicationData.keySet().iterator(); it.hasNext();) {
+			String identifier = (String) it.next();
+			Class type = (Class) newApplicationData.get(identifier);
+			if (this.applicationData.containsKey(identifier)
+					&& !this.applicationData.get(identifier).equals(type)) {
+				throw new RuleSetIntegrationException(ruleSet);
+			}
+		}
+		this.applicationData.putAll(newApplicationData);
+
+		this.ruleSets.add(ruleSet);
+
+		Rule[] rules = ruleSet.getRules();
+
+		for (int i = 0; i < rules.length; ++i) {
+			addRule(rules[i]);
+		}
+	}
+
+	/**
+	 * Creates leaps rule wrappers and propagate rule to the working memories
+	 * 
+	 * @param rule
+	 * @throws FactException
+	 * @throws RuleIntegrationException
+	 * @throws InvalidPatternException
+	 */
+	public void addRule(Rule rule) throws FactException,
+			RuleIntegrationException, InvalidPatternException {
+		List rules = this.builder.processRule(rule);
+
+		this.leapsRules.put(rule, rules);
+
+		for (Iterator it = this.workingMemories.keySet().iterator(); it
+				.hasNext();) {
+			((WorkingMemoryImpl) it.next()).addLeapsRules(rules);
+		}
+	}
+
+	public Set getWorkingMemories() {
+		return this.workingMemories.keySet();
 	}
 }
