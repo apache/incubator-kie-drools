0eab4c0e2d0beb6ed147c35f1a3292727c5564ae
Michael Neale <michael.neale@gmail.com>
2006-02-13 01:51:51 +0000

    made it serializable
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@2396 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/main/java/org/drools/util/asm/FieldAccessor.java
+++ drools-core/src/main/java/org/drools/util/asm/FieldAccessor.java
@@ -1,5 +1,7 @@
 package org.drools.util.asm;
 
+import java.io.Serializable;
+
 /**
  * This provides "field" access to getters on a given class.
  * Implementations are generated into byte code (using a switchtable) 
@@ -7,13 +9,15 @@ package org.drools.util.asm;
  * 
  * @author Michael Neale
  */
-public interface FieldAccessor {
+public interface FieldAccessor extends Serializable {
 
     /**
      * Returns the "field" corresponding to the order in which it is in the object (class).
      * 
      * @param obj The object for the field to be extracted from.
-     * @param idx The index of the "field".
+     * @param idx The index of the "field". Refer to FieldAccessorMap to get the mapping
+     * of the names of the "fields" to the index value to use for fast lookup.
+     * 
      * @return Appropriate return type. Primitives are boxed to the corresponding type.
      */
     public Object getFieldByIndex(Object obj, int idx);
