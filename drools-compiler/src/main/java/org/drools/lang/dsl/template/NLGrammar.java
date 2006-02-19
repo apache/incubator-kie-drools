699017e33f57dc27853f86e4b6d475b63b8ef52f
Michael Neale <michael.neale@gmail.com>
2006-02-19 10:09:33 +0000

    added scope for DSL definitions (so it can apply to only parts of a rule). Replaced properties with a better loader.
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@2538 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-compiler/src/main/java/org/drools/lang/dsl/template/NLGrammar.java
+++ drools-compiler/src/main/java/org/drools/lang/dsl/template/NLGrammar.java
@@ -1,14 +1,26 @@
 package org.drools.lang.dsl.template;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-/** This represents a simple grammar mapping. */
+import org.apache.commons.lang.StringUtils;
+
+/** 
+ * This represents a simple grammar mapping.
+ * Order of operations is as stored in the list. 
+ * Global expressions are processed first, followed by condition or consequence scoped ones. */
 public class NLGrammar
     implements
     Serializable {
@@ -16,35 +28,94 @@ public class NLGrammar
     
     private static final long serialVersionUID = 1L;
     private List mappings = new ArrayList();
+    private static final Pattern itemPrefix = Pattern.compile( "\\[\\s*(when|then)\\s*\\].*" );
     
     public NLGrammar() {
     }
     
 
-    /** 
-     * When loading from properties, the order in which they appear in the props file
-     * is the priority. (TODO: order means nothing to properties it seems).
-     * 
-     * Which makes sense intuitively, the order you read them it the order in which they will be applied.
-     */
-    public void loadFromProperties(Properties props) {
-        int i = props.size();
-        for ( Iterator iter = props.keySet().iterator(); iter.hasNext(); ) {
-            String key = (String) iter.next();
-            String property = props.getProperty(key);
-            NLMappingItem item = new NLMappingItem(i, key, property);
-            addNLItem(item);
-            i--;
-        }
-    }
-    
+
     public void addNLItem(NLMappingItem item) {
         this.mappings.add(item);
     }
     
-    public Collection getMappings() {
-        Collections.sort(mappings);
-        return Collections.unmodifiableCollection(mappings);
+    public List getMappings() {        
+        return mappings;
+    }
+
+    /** 
+     * This will load from a reader to an appropriate text DSL config.
+     * It it roughly equivalent to a properties file.
+     * (you can use a properties file actually), 
+     * 
+     * But you can also prefix it with things like:
+     * 
+     * [XXX]Expression=Target
+     * 
+     * Where XXX is either "when" or "then" which indicates which part of the rule the
+     * item relates to.
+     *
+     * If the "XXX" part if left out, then it will apply to the whole rule when looking for a 
+     * match to expand.
+     */
+    public void load(InputStreamReader reader) {
+        BufferedReader buf = new BufferedReader(reader);
+        try {
+            String line = null;
+            while ((line = buf.readLine())  != null) {
+                while (line.endsWith( "\\") ) {
+                    line = line.substring( 0, line.length() - 1 ) + buf.readLine();
+                }
+                line = line.trim();
+                if (line.equals( "" ) || line.startsWith( "#" )) {
+                    //ignore comment
+                } else {                        
+                    this.mappings.add( parseLine( line ) );
+                }
+            }
+            
+        } catch ( IOException e ) {
+            throw new IllegalArgumentException("Unable to read DSL configuration.", e);
+        }
+    }
+    
+    /**
+     * Filter the items for the appropriate scope.
+     * Will include global ones.
+     */
+    public List getMappings(String scope) {
+        List list = new ArrayList();
+        for ( Iterator iter = mappings.iterator(); iter.hasNext(); ) {
+            NLMappingItem item = (NLMappingItem) iter.next();
+            if (item.getScope().equals( "*" ) || item.getScope().equals( scope )) {
+                list.add( item );
+            }
+        }
+        return list;
+    }
+
+    /**
+     * This will parse a line into a NLMapping item.
+     */
+    public NLMappingItem parseLine(String line) {
+        int split = line.indexOf( "=" );
+        String left = line.substring( 0, split ).trim();
+        String right = line.substring( split + 1 ).trim();
+        
+        left = StringUtils.replace( left, "\\", "" );
+        
+        Matcher matcher = itemPrefix.matcher( left );        
+        if (matcher.matches()) {
+            //get out priority, association
+            String type = matcher.group( 1 );
+            left = left.substring( left.indexOf( "]" ) + 1 ).trim();
+            return new NLMappingItem(left, right, type);
+            
+            
+        } else {
+            return new NLMappingItem(left, right, "*");
+            
+        }
     }
     
     
--- drools-compiler/src/test/java/org/drools/lang/dsl/template/NLExpressionCompilerTest.java
+++ drools-compiler/src/test/java/org/drools/lang/dsl/template/NLExpressionCompilerTest.java
@@ -8,54 +8,51 @@ public class NLExpressionCompilerTest extends TestCase {
 
     public void testIntegration() {
         NLGrammar grammar = new NLGrammar();
-        grammar.addNLItem(new NLMappingItem(0, "{0} likes cheese", "likesCheese({0})"));
+        grammar.addNLItem(new NLMappingItem("{0} likes cheese", "likesCheese({0})", "*"));
         
         NLExpressionCompiler compiler = new NLExpressionCompiler(grammar);
-        String result = compiler.compile("bob likes cheese");
+        String result = compiler.compile("bob likes cheese", "when");
         
         assertEquals("likesCheese(bob)", result);
                 
-        //now lets use a properties
-        Properties props = new Properties();
-        props.setProperty("{0} likes cheese", "likesCheese({0})");
-        props.setProperty("the date between {0} and {1}", "dateCompare({0}, {1})");
-        props.setProperty("bind", "=>");
-        
         grammar = new NLGrammar();
-        grammar.loadFromProperties(props);
+        grammar.addNLItem( new NLMappingItem("{0} likes cheese", "likesCheese({0})", "*") );
+        grammar.addNLItem( new NLMappingItem("the date between {0} and {1}", "dateCompare({0}, {1})", "*") );
+        grammar.addNLItem( new NLMappingItem("bind", "=>", "*") );
+        
         compiler = new NLExpressionCompiler(grammar);
-        result = compiler.compile("bob likes cheese");
+        result = compiler.compile("bob likes cheese", "when");
         
         assertEquals("likesCheese(bob)", result);
         
-        result = compiler.compile("the date between bob and michael");
+        result = compiler.compile("the date between bob and michael", "when");
         assertEquals("dateCompare(bob, michael)", result);
         
-        result = compiler.compile("bind");
+        result = compiler.compile("bind", "then");
         assertEquals("=>", result);
         
     }
     
     public void testLargeGrammar() {
-        Properties props = new Properties();
         
+        NLGrammar grammar = new NLGrammar();
         for (int i = 0; i < 1000; i++ ) {
-            props.put("some {0} grammar" + i, "some mapping{0}");
+            grammar.addNLItem(new NLMappingItem("some {0} grammar" + i, "some mapping{0}", "*"));
             if (i == 42) {
-                props.put("{0} likes cheese", "{0}.likesCheese()");
-                props.put("{0} is happy", "{0}.isHappy()");
+                grammar.addNLItem( new NLMappingItem("{0} likes cheese", "{0}.likesCheese()", "*") );
+                grammar.addNLItem( new NLMappingItem("{0} is happy", "{0}.isHappy()", "*") );
             }
         }
         
         
         
-        NLGrammar grammar = new NLGrammar();
-        grammar.loadFromProperties(props);
+        
+        
         
         NLExpressionCompiler compiler = new NLExpressionCompiler(grammar);
         
         long start = System.currentTimeMillis();
-        String result = compiler.compile("michael likes cheese and michael is happy");
+        String result = compiler.compile("michael likes cheese and michael is happy", "then");
         long runtime = System.currentTimeMillis() - start;
         System.out.println("Runtime for compile with dictionary of 1000: " + runtime + "ms");
         assertEquals("michael.likesCheese() and michael.isHappy()", result);
@@ -66,43 +63,63 @@ public class NLExpressionCompilerTest extends TestCase {
     public void testNestingAndOrderOfExpressions() {
         NLGrammar grammar = new NLGrammar();
         
-        grammar.addNLItem(new NLMappingItem(0, "{0} likes cheese", "{0}.likesCheese()" ));
-        grammar.addNLItem(new NLMappingItem(1, "print out cheese fan status {0}", "print({0})" ));
+        grammar.addNLItem(new NLMappingItem("{0} likes cheese", "{0}.likesCheese()", "*" ));
+        grammar.addNLItem(new NLMappingItem("print out cheese fan status {0}", "print({0})", "*" ));
        
         
         NLExpressionCompiler compiler = new NLExpressionCompiler(grammar);
         String nl = "print out cheese fan status bob likes cheese";
         String expected = "print(bob.likesCheese())";
         
-        String result = compiler.compile(nl);
+        String result = compiler.compile(nl, "when");
         
         assertEquals(expected, result);
-
         
         grammar = new NLGrammar();
         
-        grammar.addNLItem(new NLMappingItem(1, "date of '{0}'", "dateOf({0})"));
-        grammar.addNLItem(new NLMappingItem(2, "age of [{0}]", "{0}.getAge()"));
-        grammar.addNLItem(new NLMappingItem(3, "Today", "new java.util.Date()"));
-        grammar.addNLItem(new NLMappingItem(4, "{0} is before {1}", "({0}).compareTo({1}) > 0"));
+        grammar.addNLItem(new NLMappingItem("date of '{0}'", "dateOf({0})", "*"));
+        grammar.addNLItem(new NLMappingItem("age of [{0}]", "{0}.getAge()", "*"));
+        grammar.addNLItem(new NLMappingItem("Today", "new java.util.Date()", "*"));
+        grammar.addNLItem(new NLMappingItem("{0} is before {1}", "({0}).compareTo({1}) > 0", "*"));
         
         nl = "date of '10-jul-2006' is before Today";
         
         compiler = new NLExpressionCompiler(grammar);
         expected = "(dateOf(10-jul-2006)).compareTo(new java.util.Date()) > 0";
-        assertEquals(expected, compiler.compile(nl));
+        assertEquals(expected, compiler.compile(nl, "when"));
         
         //test repeating...
         nl = "age of [bob] < age of [michael]";
         expected = "bob.getAge() < michael.getAge()";
-        assertEquals(expected, compiler.compile(nl));
+        assertEquals(expected, compiler.compile(nl, "when"));
         
         //test no change to output
         nl = "nothing relevant here... move along";
         expected = nl;
         
-        assertEquals(expected, compiler.compile(nl));
+        try {
+            assertEquals(expected, compiler.compile(nl, "when"));
+            fail("should have thrown an exception for non expansion.");
+        } catch (RuntimeException e) {
+            
+        }
         
+    }
+    
+    
+    public void testProcessWhenAndThenSeperately() {
+        NLGrammar g = new NLGrammar();
+        g.addNLItem( new NLMappingItem("something", "blah", "when") );
+        g.addNLItem( new NLMappingItem("boo", "ya", "when") );
+        g.addNLItem( new NLMappingItem("coo", "eee", "then") );
+        g.addNLItem( new NLMappingItem("ska", "fa", "*") );
+        
+        NLExpressionCompiler compiler = new NLExpressionCompiler(g);
+        String result = compiler.compile( "something boo coo ska", "when" );
+        assertEquals("blah ya coo fa", result);
+        
+        result = compiler.compile( "something boo coo ska", "then" );
+        assertEquals( "something boo eee fa", result );
     }
     
 }
