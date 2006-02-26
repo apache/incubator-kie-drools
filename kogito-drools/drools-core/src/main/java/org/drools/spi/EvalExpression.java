1a42946f86dae4cf3e634a07f89ff1e62d319b1f
Mark Proctor <mdproctor@gmail.com>
2006-02-26 11:55:16 +0000

    -Rules now wireup, sets invokeables, during the build process.
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@2650 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/main/java/org/drools/spi/EvalExpression.java
+++ kogito-drools/drools-core/src/main/java/org/drools/spi/EvalExpression.java
@@ -3,7 +3,7 @@ package org.drools.spi;
 import org.drools.WorkingMemory;
 import org.drools.rule.Declaration;
 
-public interface EvalExpression extends Invoker {
+public interface EvalExpression extends Invokeable {
     public boolean evaluate(Tuple tuple,
                             Declaration[] requiredDeclarations, 
                             WorkingMemory workingMemory);
--- kogito-drools/drools-core/src/main/java/org/drools/spi/ReturnValueExpression.java
+++ kogito-drools/drools-core/src/main/java/org/drools/spi/ReturnValueExpression.java
@@ -3,7 +3,7 @@ package org.drools.spi;
 import org.drools.WorkingMemory;
 import org.drools.rule.Declaration;
 
-public interface ReturnValueExpression extends Invoker {
+public interface ReturnValueExpression extends Invokeable {
     public Object evaluate(Tuple tuple,
                            Declaration[] requiredDeclarations,
                            WorkingMemory workingMemory);
