7545058e0c0c2365e011f525e356b76198d20332
Mark Proctor <mdproctor@gmail.com>
2006-03-09 03:33:11 +0000

    -Started to add dynamic rule test -Updated bob's unit test to make sure it handles field constraints correctly
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@2823 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-compiler/src/test/java/org/drools/integrationtests/Cheese.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.drools.integrationtests;
+
+public class Cheese {
+    private String type;
+    private int    price;
+
+    public Cheese(String type,
+                  int price) {
+        super();
+        this.type = type;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
