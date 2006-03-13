81bc6d31c57d6e0ec178e2071b653ea9ca56bdc9
Michael Neale <michael.neale@gmail.com>
2006-03-13 07:27:09 +0000

    some unit test coverage
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@2895 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/test/java/org/drools/base/EvaluatorFactoryTestCase.java
+++ kogito-drools/drools-core/src/test/java/org/drools/base/EvaluatorFactoryTestCase.java
@@ -1,5 +1,8 @@
 package org.drools.base;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.drools.spi.Evaluator;
 
 import junit.framework.TestCase;
@@ -25,7 +28,13 @@ public class EvaluatorFactoryTestCase extends TestCase {
         assertTrue(EvaluatorFactory.getEvaluator( Evaluator.OBJECT_TYPE, "!=" )
                     .evaluate( "foo", "bar" ));
         
-        
+        List list = new ArrayList();
+        list.add( "foo" );
+         
+        assertTrue(EvaluatorFactory.getEvaluator( Evaluator.OBJECT_TYPE, "contains" )
+                   .evaluate( "foo", list ));
+        assertFalse(EvaluatorFactory.getEvaluator( Evaluator.OBJECT_TYPE, "contains" )
+                   .evaluate( "bar", list ));
         
         
     }
