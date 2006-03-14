90430ca60dbfc183aff91147c379ac273fd1aabc
Michael Neale <michael.neale@gmail.com>
2006-03-14 06:50:24 +0000

    JBRULES-63
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@2915 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-core/src/main/java/org/drools/base/evaluators/CharacterFactory.java
@@ -0,0 +1,135 @@
+package org.drools.base.evaluators;
+
+import org.drools.base.BaseEvaluator;
+import org.drools.spi.Evaluator;
+
+public class CharacterFactory {
+    
+    public static Evaluator getCharacterEvaluator(int operator) {
+        switch ( operator ) {
+            case Evaluator.EQUAL :
+                return CharacterEqualEvaluator.INSTANCE;
+            case Evaluator.NOT_EQUAL :
+                return CharacterNotEqualEvaluator.INSTANCE;
+            case Evaluator.LESS :
+                return CharacterLessEvaluator.INSTANCE;
+            case Evaluator.LESS_OR_EQUAL :
+                return CharacterLessOrEqualEvaluator.INSTANCE;
+            case Evaluator.GREATER :
+                return CharacterGreaterEvaluator.INSTANCE;
+            case Evaluator.GREATER_OR_EQUAL :
+                return CharacterGreaterOrEqualEvaluator.INSTANCE;
+            default :
+                throw new RuntimeException( "Operator '" + operator + "' does not exist for CharacterEvaluator" );
+        }
+    }
+
+    static class CharacterEqualEvaluator extends BaseEvaluator {
+        public final static Evaluator INSTANCE = new CharacterEqualEvaluator();
+
+        private CharacterEqualEvaluator() {
+            super( Evaluator.CHAR_TYPE,
+                   Evaluator.EQUAL );
+        }
+
+        public boolean evaluate(Object object1,
+                                Object object2) {
+            return ((Character) object1).charValue() == ((Character) object2).charValue();
+        }
+        
+        public String toString() {
+            return "Character ==";
+        }         
+    }
+
+    static class CharacterNotEqualEvaluator extends BaseEvaluator {
+        public final static Evaluator INSTANCE = new CharacterNotEqualEvaluator();
+
+        private CharacterNotEqualEvaluator() {
+            super( Evaluator.CHAR_TYPE,
+                   Evaluator.NOT_EQUAL );
+        }
+
+        public boolean evaluate(Object object1,
+                                Object object2) {
+            return ((Character) object1).charValue() != ((Character) object2).charValue();
+        }
+        
+        public String toString() {
+            return "Character !=";
+        }                 
+    }
+
+    static class CharacterLessEvaluator extends BaseEvaluator {
+        public final static Evaluator INSTANCE = new CharacterLessEvaluator();
+
+        private CharacterLessEvaluator() {
+            super( Evaluator.CHAR_TYPE,
+                   Evaluator.LESS );
+        }
+
+        public boolean evaluate(Object object1,
+                                Object object2) {
+            return ((Character) object1).charValue() < ((Character) object2).charValue();
+        }
+        
+        public String toString() {
+            return "Character <";
+        }                 
+    }
+
+    static class CharacterLessOrEqualEvaluator extends BaseEvaluator {
+        public final static Evaluator INSTANCE = new CharacterLessOrEqualEvaluator();
+
+        private CharacterLessOrEqualEvaluator() {
+            super( Evaluator.CHAR_TYPE,
+                   Evaluator.LESS_OR_EQUAL );
+        }
+
+        public boolean evaluate(Object object1,
+                                Object object2) {
+            return ((Character) object1).charValue() <= ((Character) object2).charValue();
+        }
+        
+        public String toString() {
+            return "Character <=";
+        }         
+    }
+
+    static class CharacterGreaterEvaluator extends BaseEvaluator {
+        public final static Evaluator INSTANCE = new CharacterGreaterEvaluator();
+
+        private CharacterGreaterEvaluator() {
+            super( Evaluator.CHAR_TYPE,
+                   Evaluator.GREATER );
+        }
+
+        public boolean evaluate(Object object1,
+                                Object object2) {
+            return ((Character) object1).charValue() > ((Character) object2).charValue();
+        }
+        
+        public String toString() {
+            return "Character >";
+        }         
+    }
+
+    static class CharacterGreaterOrEqualEvaluator extends BaseEvaluator {
+        private final static Evaluator INSTANCE = new CharacterGreaterOrEqualEvaluator();
+
+        private CharacterGreaterOrEqualEvaluator() {
+            super( Evaluator.CHAR_TYPE,
+                   Evaluator.GREATER_OR_EQUAL );
+        }
+
+        public boolean evaluate(Object object1,
+                                Object object2) {
+            return ((Character) object1).charValue() >= ((Character) object2).charValue();
+        }
+        
+        public String toString() {
+            return "Character >=";
+        }         
+    }
+
+}
---
+++ drools-core/src/main/java/org/drools/base/evaluators/LongFactory.java
@@ -0,0 +1,134 @@
+package org.drools.base.evaluators;
+
+import org.drools.base.BaseEvaluator;
+import org.drools.spi.Evaluator;
+
+public class LongFactory {
+    public static Evaluator getLongEvaluator(int operator) {
+        switch ( operator ) {
+            case Evaluator.EQUAL :
+                return LongEqualEvaluator.INSTANCE;
+            case Evaluator.NOT_EQUAL :
+                return LongNotEqualEvaluator.INSTANCE;
+            case Evaluator.LESS :
+                return LongLessEvaluator.INSTANCE;
+            case Evaluator.LESS_OR_EQUAL :
+                return LongLessOrEqualEvaluator.INSTANCE;
+            case Evaluator.GREATER :
+                return LongGreaterEvaluator.INSTANCE;
+            case Evaluator.GREATER_OR_EQUAL :
+                return LongGreaterOrEqualEvaluator.INSTANCE;
+            default :
+                throw new RuntimeException( "Operator '" + operator + "' does not exist for LongEvaluator" );
+        }
+    }
+
+    static class LongEqualEvaluator extends BaseEvaluator {
+        public final static Evaluator INSTANCE = new LongEqualEvaluator();
+
+        private LongEqualEvaluator() {
+            super( Evaluator.LONG_TYPE,
+                   Evaluator.EQUAL );
+        }
+
+        public boolean evaluate(Object object1,
+                                Object object2) {
+            return ((Number) object1).longValue() == ((Number) object2).longValue();
+        }
+        
+        public String toString() {
+            return "Long ==";
+        }         
+    }
+
+    static class LongNotEqualEvaluator extends BaseEvaluator {
+        public final static Evaluator INSTANCE = new LongNotEqualEvaluator();
+
+        private LongNotEqualEvaluator() {
+            super( Evaluator.LONG_TYPE,
+                   Evaluator.NOT_EQUAL );
+        }
+
+        public boolean evaluate(Object object1,
+                                Object object2) {
+            return ((Number) object1).longValue() != ((Number) object2).longValue();
+        }
+        
+        public String toString() {
+            return "Long !=";
+        }                 
+    }
+
+    static class LongLessEvaluator extends BaseEvaluator {
+        public final static Evaluator INSTANCE = new LongLessEvaluator();
+
+        private LongLessEvaluator() {
+            super( Evaluator.LONG_TYPE,
+                   Evaluator.LESS );
+        }
+
+        public boolean evaluate(Object object1,
+                                Object object2) {
+            return ((Number) object1).longValue() < ((Number) object2).longValue();
+        }
+        
+        public String toString() {
+            return "Long <";
+        }                 
+    }
+
+    static class LongLessOrEqualEvaluator extends BaseEvaluator {
+        public final static Evaluator INSTANCE = new LongLessOrEqualEvaluator();
+
+        private LongLessOrEqualEvaluator() {
+            super( Evaluator.LONG_TYPE,
+                   Evaluator.LESS_OR_EQUAL );
+        }
+
+        public boolean evaluate(Object object1,
+                                Object object2) {
+            return ((Number) object1).longValue() <= ((Number) object2).longValue();
+        }
+        
+        public String toString() {
+            return "Long <=";
+        }         
+    }
+
+    static class LongGreaterEvaluator extends BaseEvaluator {
+        public final static Evaluator INSTANCE = new LongGreaterEvaluator();
+
+        private LongGreaterEvaluator() {
+            super( Evaluator.LONG_TYPE,
+                   Evaluator.GREATER );
+        }
+
+        public boolean evaluate(Object object1,
+                                Object object2) {
+            return ((Number) object1).longValue() > ((Number) object2).longValue();
+        }
+        
+        public String toString() {
+            return "Long >";
+        }         
+    }
+
+    static class LongGreaterOrEqualEvaluator extends BaseEvaluator {
+        private final static Evaluator INSTANCE = new LongGreaterOrEqualEvaluator();
+
+        private LongGreaterOrEqualEvaluator() {
+            super( Evaluator.LONG_TYPE,
+                   Evaluator.GREATER_OR_EQUAL );
+        }
+
+        public boolean evaluate(Object object1,
+                                Object object2) {
+            return ((Number) object1).longValue() >= ((Number) object2).longValue();
+        }
+        
+        public String toString() {
+            return "Long >=";
+        }         
+    }
+
+}
