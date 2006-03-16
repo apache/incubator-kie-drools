3110dea6880830ac44f8512951e13f1b18080b38
Mark Proctor <mdproctor@gmail.com>
2006-03-16 02:59:05 +0000

    -Fixes for dynamic rules
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@2950 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/test/java/org/drools/integrationtests/DynamicRuleTest.java
+++ kogito-drools/drools-compiler/src/test/java/org/drools/integrationtests/DynamicRuleTest.java
@@ -6,6 +6,8 @@ import java.io.Reader;
 import junit.framework.TestCase;
 
 import org.drools.Cheese;
+import org.drools.Person;
+
 import org.drools.WorkingMemory;
 import org.drools.compiler.DrlParser;
 import org.drools.compiler.PackageBuilder;
@@ -14,20 +16,44 @@ import org.drools.rule.Package;
 
 public class DynamicRuleTest extends TestCase {
         public void testDrl() throws Exception {
-            Reader reader = new InputStreamReader( getClass().getResourceAsStream( "dynamic-rule-test.drl" ) );
-            DrlParser parser = new DrlParser();
-            PackageDescr packageDescr = parser.parse( reader );
-            
+            Reader reader = new InputStreamReader( getClass().getResourceAsStream( "dynamic1.drl" ) );
+                                                
             PackageBuilder builder = new PackageBuilder();
-            builder.addPackage( packageDescr );
-            Package pkg = builder.getPackage();
+            builder.addPackageFromDrl( reader );            
+            Package pkg1 = builder.getPackage();
             
             org.drools.reteoo.RuleBaseImpl ruleBase = new org.drools.reteoo.RuleBaseImpl();
-            ruleBase.addPackage( pkg );
+            ruleBase.addPackage( pkg1 );
             WorkingMemory workingMemory = ruleBase.newWorkingMemory();
             
             Cheese stilton = new Cheese("stilton", 5);
             workingMemory.assertObject( stilton );
+            
+            Cheese cheddar = new Cheese("cheddar", 5);
+            workingMemory.assertObject( cheddar );            
             workingMemory.fireAllRules();
+            
+            reader = new InputStreamReader( getClass().getResourceAsStream( "dynamic2.drl" ) );
+            builder = new PackageBuilder();
+            builder.addPackageFromDrl( reader );
+            Package pkg2 = builder.getPackage();
+            ruleBase.addPackage( pkg2 );
+            
+            reader = new InputStreamReader( getClass().getResourceAsStream( "dynamic3.drl" ) );
+            builder = new PackageBuilder();
+            builder.addPackageFromDrl( reader );
+            Package pkg3 = builder.getPackage();
+            ruleBase.addPackage( pkg3 );      
+            
+            Person bob = new Person("bob", "stilton");
+            workingMemory.assertObject( bob );  
+            
+            workingMemory.fireAllRules();
+            
+            reader = new InputStreamReader( getClass().getResourceAsStream( "dynamic4.drl" ) );
+            builder = new PackageBuilder();
+            builder.addPackageFromDrl( reader );
+            Package pkg4 = builder.getPackage();
+            ruleBase.addPackage( pkg4 );              
         }
 }
