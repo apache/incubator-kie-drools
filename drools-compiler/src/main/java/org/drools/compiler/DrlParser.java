e785a12fbf4273d345279ac7f9d1c3356c67bab2
Michael Neale <michael.neale@gmail.com>
2006-03-19 12:32:03 +0000

    added DSL support (as an option)
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@3035 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-compiler/src/main/java/org/drools/compiler/DrlParser.java
+++ drools-compiler/src/main/java/org/drools/compiler/DrlParser.java
@@ -11,6 +11,7 @@ import org.antlr.runtime.RecognitionException;
 import org.drools.lang.RuleParser;
 import org.drools.lang.RuleParserLexer;
 import org.drools.lang.descr.PackageDescr;
+import org.drools.lang.dsl.DefaultExpanderResolver;
 
 public class DrlParser {
     
@@ -21,20 +22,47 @@ public class DrlParser {
 	}
 	
 	public PackageDescr parse(String text) throws DroolsParserException {
-        RuleParser parser = new RuleParser( new CommonTokenStream( new RuleParserLexer( new ANTLRStringStream( text ) ) ) );
-                
+        RuleParser parser = getParser( text );
+        compile( parser );
+        return parser.getPackageDescr();			
+		
+	}
+
+    private void compile(RuleParser parser) throws DroolsParserException {
         try {
             parser.compilation_unit();
         } catch ( RecognitionException e ) {
             throw new DroolsParserException( e );
         }
+    }
 
-        return parser.getPackageDescr();			
-		
-	}
+    private RuleParser getParser(String text) {
+        return new RuleParser( new CommonTokenStream( new RuleParserLexer( new ANTLRStringStream( text ) ) ) );
+    }
 
+    
+    
+    /** Parse and build a rule package from a DRL source */
 	public PackageDescr parse(Reader reader) throws IOException, DroolsParserException {
-	       StringBuffer text = new StringBuffer();
+	       StringBuffer text = getDRLText( reader );
+	       return parse( text.toString() );
+	}
+    
+    /** 
+     * Parse and build a rule package from a DRL source with a 
+     * domain specific language.
+     */
+    public PackageDescr parse(Reader drl, Reader dsl) throws DroolsParserException, IOException {
+        StringBuffer text = getDRLText( drl );
+        DefaultExpanderResolver resolver = new DefaultExpanderResolver(dsl);
+        RuleParser parser = getParser( text.toString() );
+        parser.setExpanderResolver( resolver );
+        compile( parser );
+        return parser.getPackageDescr();
+    }
+
+    private StringBuffer getDRLText(Reader reader) throws IOException {
+        StringBuffer text = new StringBuffer();
 
 	        char[] buf = new char[1024];
 	        int len = 0;
@@ -44,9 +72,8 @@ public class DrlParser {
 	                         0,
 	                         len );
 	        }
-
-	        return parse( text.toString() );
-	}
+        return text;
+    }
     
     public ParserError[] getErrors() {
         return ( ParserError[] ) this.results.toArray( new ParserError[ this.results.size() ] );
