ae012d7c9c29393a57c51069f8c3dc1d3eb25221
Mark Proctor <mdproctor@gmail.com>
2006-03-19 04:25:30 +0000

    -Rename ReteBuilder to ReteooBuilder -Make eval work in ReteooBuilder -Add integrity checks to ReteooBuilder for correctly bound and required declarations -Add integrity checks to ReteooBuilder for Rule and Query Consequences
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@3025 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/test/java/org/drools/integrationtests/IntegrationTest.java
+++ kogito-drools/drools-compiler/src/test/java/org/drools/integrationtests/IntegrationTest.java
@@ -1,10 +1,27 @@
 package org.drools.integrationtests;
 
+/*
+ * Copyright 2005 JBoss Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.List;
 
 import org.drools.Cheese;
+import org.drools.Person;
 import org.drools.WorkingMemory;
 import org.drools.compiler.DrlParser;
 import org.drools.compiler.PackageBuilder;
@@ -40,8 +57,37 @@ public class IntegrationTest extends TestCase {
         Cheese stilton = new Cheese("stinky", 5);
         workingMemory.assertObject( stilton );
         List results = workingMemory.getQueryResults( "simple" );
-        assertEquals(1, results.size());
-        
+        assertEquals(1, results.size());        
     }
+    
+    public void testEval() throws Exception {
+        PackageBuilder builder = new PackageBuilder();
+        builder.addPackageFromDrl( new InputStreamReader( getClass().getResourceAsStream( "eval_rule.drl" ) ) );
+        Package pkg = builder.getPackage();
+        
+        org.drools.reteoo.RuleBaseImpl ruleBase = new org.drools.reteoo.RuleBaseImpl();
+        ruleBase.addPackage( pkg );
+        WorkingMemory workingMemory = ruleBase.newWorkingMemory();
+        
+        Cheese stilton = new Cheese("stilton", 5);
+        workingMemory.assertObject( stilton );
+        workingMemory.fireAllRules();
+    }      
+    
+    public void testReturnValue() throws Exception {
+        PackageBuilder builder = new PackageBuilder();
+        builder.addPackageFromDrl( new InputStreamReader( getClass().getResourceAsStream( "returnvalue_rule.drl" ) ) );
+        Package pkg = builder.getPackage();
+        
+        org.drools.reteoo.RuleBaseImpl ruleBase = new org.drools.reteoo.RuleBaseImpl();
+        ruleBase.addPackage( pkg );
+        WorkingMemory workingMemory = ruleBase.newWorkingMemory();
+        
+        Person peter = new Person("peter", null, 12);
+        workingMemory.assertObject( peter );
+        Person jane = new Person("jane", null, 10);
+        workingMemory.assertObject( peter );        
+        workingMemory.fireAllRules();
+    }       
     
 }
