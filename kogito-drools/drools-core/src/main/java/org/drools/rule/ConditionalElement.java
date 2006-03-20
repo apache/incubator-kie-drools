18a6b06d1e651f2f3579f9b8e35cf7aa922329dd
Mark Proctor <mdproctor@gmail.com>
2006-03-20 16:48:44 +0000

    -Split ConditionalElement in to ConditionalElement and GroupElement to enabel better eval support -Made eval integration test work
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@3058 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-core/src/main/java/org/drools/rule/ConditionalElement.java
@@ -0,0 +1,12 @@
+package org.drools.rule;
+
+import java.io.Serializable;
+
+public abstract class ConditionalElement
+    implements
+    Serializable,
+    Cloneable  {
+    
+    public abstract Object clone();
+
+}
