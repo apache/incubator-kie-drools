2b49923ec2015967f9c657b4c405975010b03186
Alexander Bagerman <bagerman@gmail.com>
2006-03-22 06:38:51 +0000

    changes to fix problems uncovered during leaps integration tests
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@3135 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/test/java/org/drools/integrationtests/LeapsTest.java
+++ kogito-drools/drools-compiler/src/test/java/org/drools/integrationtests/LeapsTest.java
@@ -9,17 +9,7 @@ public class LeapsTest extends IntegrationCases {
         return new org.drools.leaps.RuleBaseImpl();
     }
 
-    public void testEval() throws Exception {
-
-    }
-
     public void testExists() throws Exception {
-    }
-
-    public void testNot() throws Exception {
-    }
-
-    public void testNotWithBindings() throws Exception {
     }
 
     public void testQuery() throws Exception {
