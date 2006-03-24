422d4517428651c3ddf75a4d03e6bd160ce1ff79
Michael Neale <michael.neale@gmail.com>
2006-03-24 11:58:59 +0000

    JBRULES-89
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@3193 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-compiler/src/main/java/org/drools/compiler/DrlParser.java
+++ drools-compiler/src/main/java/org/drools/compiler/DrlParser.java
@@ -13,6 +13,10 @@ import org.drools.lang.RuleParserLexer;
 import org.drools.lang.descr.PackageDescr;
 import org.drools.lang.dsl.DefaultExpanderResolver;
 
+/**
+ * This is a low level parser API. This will return textual AST representations
+ * of the DRL source, including with DSL expanders if appropriate.
+ */
 public class DrlParser {
     
     private List results = new ArrayList();
@@ -21,13 +25,14 @@ public class DrlParser {
 		
 	}
 	
+    /** Parse a rule from text */
 	public PackageDescr parse(String text) throws DroolsParserException {
         RuleParser parser = getParser( text );
         compile( parser );
         return parser.getPackageDescr();			
 		
 	}
-
+    
     private void compile(RuleParser parser) throws DroolsParserException {
         try {
             parser.compilation_unit();
@@ -54,8 +59,19 @@ public class DrlParser {
      */
     public PackageDescr parse(Reader drl, Reader dsl) throws DroolsParserException, IOException {
         StringBuffer text = getDRLText( drl );
+        return parse( text.toString(), dsl );
+    }
+
+    /**
+     * Parse and build a rule package from a DRL source with a domain specific language.
+     * @param source As Text.
+     * @param dsl 
+     * @return
+     * @throws DroolsParserException
+     */
+    public PackageDescr parse(String source, Reader dsl) throws DroolsParserException {
         DefaultExpanderResolver resolver = new DefaultExpanderResolver(dsl);
-        RuleParser parser = getParser( text.toString() );
+        RuleParser parser = getParser( source );
         parser.setExpanderResolver( resolver );
         compile( parser );
         return parser.getPackageDescr();
