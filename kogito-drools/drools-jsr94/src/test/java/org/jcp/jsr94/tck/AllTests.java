29b29bcb6bf3e5d6d447f5338b76524e3c5129b5
Mark Proctor <mdproctor@gmail.com>
2006-03-27 03:35:08 +0000

    -added latest jsr94 patch from Michael Frandsen
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@3246 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-jsr94/src/test/java/org/jcp/jsr94/tck/AllTests.java
+++ kogito-drools/drools-jsr94/src/test/java/org/jcp/jsr94/tck/AllTests.java
@@ -19,44 +19,80 @@ public class AllTests
 {
    public static void main (String[] args)
    {
+       //use this option in Run -> VM-Arguments
+       //-Djsr94.tck.configuration=drools-jsr94/src/test/resources/org/drools/jsr94/rules
+       System.setProperty(
+                          "jsr94.tck.configuration", "drools-jsr94/src/test/resources/org/drools/jsr94/rules");
+       System.out.println(System.getProperty("jsr94.tck.configuration"));
        junit.textui.TestRunner.run( suite() );
    }
-
    public static Test suite()
    {
-       System.setProperty(
-           "jsr94.tck.configuration", "drools-jsr94/src/test/resources/org/drools/jsr94/tck");
-       
        TestSuite suite = new TestSuite( "JSR 94 Test Compatability Kit" );
-
-	   suite.addTestSuite(ApiSignatureTest.class);
-	   suite.addTestSuite(ClassLoaderTest.class);
-	   suite.addTestSuite(ConfigurationExceptionTest.class);
-	   suite.addTestSuite(HandleTest.class);
-	   suite.addTestSuite(InvalidHandleExceptionTest.class);
-	   suite.addTestSuite(InvalidRuleSessionExceptionTest.class);
-	   suite.addTestSuite(ObjectFilterTest.class);
-	   suite.addTestSuite(RuleExceptionTest.class);
-	   suite.addTestSuite(RuleExecutionExceptionTest.class);
-	   suite.addTestSuite(RuleExecutionSetMetadataTest.class);
-	   suite.addTestSuite(RuleExecutionSetNotFoundExceptionTest.class);
-	   suite.addTestSuite(RuleRuntimeTest.class);
-	   suite.addTestSuite(RuleServiceProviderManagerTest.class);
-	   suite.addTestSuite(RuleServiceProviderTest.class);
-	   suite.addTestSuite(RuleSessionCreateExceptionTest.class);
-	   suite.addTestSuite(RuleSessionTest.class);
-	   suite.addTestSuite(RuleSessionTypeUnsupportedExceptionTest.class);
+       suite.addTestSuite(ApiSignatureTest.class);
+       suite.addTestSuite(ClassLoaderTest.class);
+       suite.addTestSuite(ConfigurationExceptionTest.class);
+       suite.addTestSuite(HandleTest.class);
+       suite.addTestSuite(InvalidHandleExceptionTest.class);
+       suite.addTestSuite(InvalidRuleSessionExceptionTest.class);
+       suite.addTestSuite(ObjectFilterTest.class);
+       suite.addTestSuite(RuleExceptionTest.class);
+       suite.addTestSuite(RuleExecutionExceptionTest.class);
+       suite.addTestSuite(RuleExecutionSetMetadataTest.class);
+       suite.addTestSuite(RuleExecutionSetNotFoundExceptionTest.class);
+       suite.addTestSuite(RuleRuntimeTest.class);
+       suite.addTestSuite(RuleServiceProviderManagerTest.class);
+       suite.addTestSuite(RuleServiceProviderTest.class);
+       suite.addTestSuite(RuleSessionCreateExceptionTest.class);
+       suite.addTestSuite(RuleSessionTest.class);
+       suite.addTestSuite(RuleSessionTypeUnsupportedExceptionTest.class);
+       suite.addTestSuite(StatefulRuleSessionTest.class);
+       suite.addTestSuite(StatelessRuleSessionTest.class);
+       suite.addTestSuite(LocalRuleExecutionSetProviderTest.class);
+       suite.addTestSuite(RuleAdministrationExceptionTest.class);
+       suite.addTestSuite(RuleAdministratorTest.class);
+       suite.addTestSuite(RuleExecutionSetCreateExceptionTest.class);
+//     suite.addTestSuite(RuleExecutionSetProviderTest.class);
+       suite.addTestSuite(RuleExecutionSetRegisterExceptionTest.class);
+       suite.addTestSuite(RuleExecutionSetTest.class);
+       suite.addTestSuite(RuleExecutionSetDeregistrationExceptionTest.class);
+       suite.addTestSuite(RuleTest.class);
+       return suite;
+   }
+//   public static Test suite()
+//   {
+//       
+//
+//       TestSuite suite = new TestSuite( "JSR 94 Test Compatability Kit" );
+//       System.out.println(System.getProperty("jsr94.tck.configuration"));
+//	   suite.addTestSuite(ApiSignatureTest.class);
+//	   suite.addTestSuite(ClassLoaderTest.class);
+//	   suite.addTestSuite(ConfigurationExceptionTest.class);
+//	   suite.addTestSuite(HandleTest.class);
+//	   suite.addTestSuite(InvalidHandleExceptionTest.class);
+//	   suite.addTestSuite(InvalidRuleSessionExceptionTest.class);
+//	   suite.addTestSuite(ObjectFilterTest.class);
+//	   suite.addTestSuite(RuleExceptionTest.class);
+//	   suite.addTestSuite(RuleExecutionExceptionTest.class);
+//	   suite.addTestSuite(RuleExecutionSetMetadataTest.class);
+//	   suite.addTestSuite(RuleExecutionSetNotFoundExceptionTest.class);
+//	   suite.addTestSuite(RuleRuntimeTest.class);
+//	   suite.addTestSuite(RuleServiceProviderManagerTest.class);
+//	   suite.addTestSuite(RuleServiceProviderTest.class);
+//	   suite.addTestSuite(RuleSessionCreateExceptionTest.class);
+//	   suite.addTestSuite(RuleSessionTest.class);
+//	   suite.addTestSuite(RuleSessionTypeUnsupportedExceptionTest.class);
 //	   suite.addTestSuite(StatefulRuleSessionTest.class);
 //	   suite.addTestSuite(StatelessRuleSessionTest.class);
-	   suite.addTestSuite(LocalRuleExecutionSetProviderTest.class);
-	   suite.addTestSuite(RuleAdministrationExceptionTest.class);
-	   suite.addTestSuite(RuleAdministratorTest.class);
-	   suite.addTestSuite(RuleExecutionSetCreateExceptionTest.class);
-//	   suite.addTestSuite(RuleExecutionSetProviderTest.class);
-	   suite.addTestSuite(RuleExecutionSetRegisterExceptionTest.class);
-	   suite.addTestSuite(RuleExecutionSetTest.class);
-	   suite.addTestSuite(RuleExecutionSetDeregistrationExceptionTest.class);
-	   suite.addTestSuite(RuleTest.class);
-	   return suite;
-   }
+//	   suite.addTestSuite(LocalRuleExecutionSetProviderTest.class);
+//	   suite.addTestSuite(RuleAdministrationExceptionTest.class);
+//	   suite.addTestSuite(RuleAdministratorTest.class);
+//	   suite.addTestSuite(RuleExecutionSetCreateExceptionTest.class);
+////	   suite.addTestSuite(RuleExecutionSetProviderTest.class);
+//	   suite.addTestSuite(RuleExecutionSetRegisterExceptionTest.class);
+//	   suite.addTestSuite(RuleExecutionSetTest.class);
+//	   suite.addTestSuite(RuleExecutionSetDeregistrationExceptionTest.class);
+//	   suite.addTestSuite(RuleTest.class);
+//	   return suite;
+//   }
 }
