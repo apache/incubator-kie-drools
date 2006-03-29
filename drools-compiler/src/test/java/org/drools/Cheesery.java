74d46ee2540e60e1263435dfa08f61d7c73f332a
Mark Proctor <mdproctor@gmail.com>
2006-03-29 07:19:53 +0000

    JBRULES-143 Add bound variable contains integration test
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@3326 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-compiler/src/test/java/org/drools/Cheesery.java
@@ -0,0 +1,12 @@
+package org.drools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cheesery {
+    private List cheeses = new ArrayList();    
+    
+    public List getCheeses() {
+        return this.cheeses;
+    }
+}
