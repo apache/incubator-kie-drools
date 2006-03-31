3a9b634b03de4a4ab78f0d5ac805aefccd6336cb
Mark Proctor <mdproctor@gmail.com>
2006-03-31 03:11:20 +0000

    -Refactored FieldImpl out.
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@3354 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/main/java/org/drools/base/FieldFactory.java
+++ drools-core/src/main/java/org/drools/base/FieldFactory.java
@@ -63,34 +63,5 @@ public class FieldFactory {
         return field;
     }
     
-    public static class FieldImpl implements FieldValue {
-        private Object value;
-        
-        public FieldImpl(Object value) {
-            this.value = value;
-        }
-
-        public Object getValue() {
-            return this.value;
-        }
-        
-        public boolean equals(Object other) {
-            if(this == other) {
-                return true;
-            }
-            if(!(other instanceof FieldImpl)) {
-                return false;
-            }
-            FieldImpl field = (FieldImpl) other;
-            
-            return (((this.value == null ) && (field.value == null)) ||
-                    ((this.value != null ) && (this.value.equals(field.value))));
-        }
-        
-        public int hashCode() {
-            return this.value.hashCode();
-        }
-    }
-    
     
 }
