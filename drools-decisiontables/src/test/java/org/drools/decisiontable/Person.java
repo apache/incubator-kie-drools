60e3d777945c7944b4ddce55c1d2b2331bae7cea
Michael Neale <michael.neale@gmail.com>
2006-04-02 02:28:37 +0000

    added test classes to module
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@3378 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-decisiontables/src/test/java/org/drools/decisiontable/Person.java
@@ -0,0 +1,70 @@
+package org.drools.decisiontable;
+
+public class Person {
+    private final String name;
+    private final String likes;
+    private final int age;
+    
+    private char sex;
+    
+    private boolean alive;
+    
+    private String status;
+    
+    public Person( String name  ) {
+        this( name, "", 0 );
+    }
+    
+    
+    public Person( String name, String likes ) {
+        this( name, likes, 0 );
+    }
+    
+    public Person( String name, String likes, int age ) {
+        this.name = name;
+        this.likes = likes;
+        this.age = age;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getLikes() {
+        return likes;
+    }
+
+    public String getName() {
+        return name;
+    }
+        
+    public int getAge() {
+        return this.age;
+    }
+    
+    
+    
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public String toString() {
+        return "[Person name='" + this.name + "']";
+    }
+}
