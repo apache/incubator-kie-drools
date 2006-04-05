016252812ef3fa5e8228dca8d5335984fb8872d3
Edson Tirelli <ed.tirelli@gmail.com>
2006-04-05 02:06:04 +0000

    JBRULES-56: Adding waltz code. Results are not correct yet though.
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@3444 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-compiler/src/test/java/org/drools/integrationtests/waltz/WaltzTest.java
@@ -0,0 +1,71 @@
+package org.drools.integrationtests.waltz;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import junit.framework.TestCase;
+
+import org.drools.PackageIntegrationException;
+import org.drools.RuleBase;
+import org.drools.RuleIntegrationException;
+import org.drools.WorkingMemory;
+import org.drools.compiler.DrlParser;
+import org.drools.compiler.DroolsParserException;
+import org.drools.compiler.PackageBuilder;
+import org.drools.event.DebugAgendaEventListener;
+import org.drools.event.DebugWorkingMemoryEventListener;
+import org.drools.lang.descr.PackageDescr;
+import org.drools.reteoo.RuleBaseImpl;
+import org.drools.rule.InvalidPatternException;
+import org.drools.rule.Package;
+ 
+/**
+ * This is a sample file to launch a rule package from a rule source file.
+ */
+public class WaltzTest extends TestCase {
+
+    public void testWaltz() {
+        try {
+            
+            //load up the rulebase
+            RuleBase ruleBase = readRule();
+            WorkingMemory workingMemory = ruleBase.newWorkingMemory();
+            
+//            DebugWorkingMemoryEventListener wmListener = new DebugWorkingMemoryEventListener();
+//            DebugAgendaEventListener agendaListener = new DebugAgendaEventListener();
+//            workingMemory.addEventListener( wmListener );
+//            workingMemory.addEventListener( agendaListener );
+            
+            //go !            
+            Stage stage = new Stage(Stage.START);
+            workingMemory.assertObject( stage );
+            workingMemory.fireAllRules();               
+        } catch (Throwable t) {
+            t.printStackTrace();
+            fail(t.getMessage());
+        }
+    }
+
+    /**
+     * Please note that this is the "low level" rule assembly API.
+     */
+    private static RuleBase readRule() throws IOException, DroolsParserException, RuleIntegrationException, PackageIntegrationException, InvalidPatternException {
+        //read in the source
+        Reader reader = new InputStreamReader( WaltzTest.class.getResourceAsStream( "waltz.drl" ) );
+        DrlParser parser = new DrlParser();
+        PackageDescr packageDescr = parser.parse( reader );
+        
+        //pre build the package
+        PackageBuilder builder = new PackageBuilder();
+        builder.addPackage( packageDescr );
+        Package pkg = builder.getPackage();
+        
+        //add the package to a rulebase
+        RuleBaseImpl ruleBase = new RuleBaseImpl();
+        ruleBase.addPackage( pkg );
+        return ruleBase;
+    }
+    
+}
+
