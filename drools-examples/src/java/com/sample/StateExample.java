4a72dd67240e38a68359fc4fe302408f35deffff
Mark Proctor <mdproctor@gmail.com>
2006-04-07 00:32:27 +0000

    -Added drools-examples project -Added State example
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@3496 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-examples/src/java/com/sample/StateExample.java
@@ -0,0 +1,48 @@
+package com.sample;
+
+import java.io.InputStreamReader;
+
+import org.drools.RuleBase;
+import org.drools.RuleBaseFactory;
+import org.drools.WorkingMemory;
+import org.drools.compiler.PackageBuilder;
+import org.drools.reteoo.WorkingMemoryFileLogger;
+
+public class StateExample {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws  Exception {
+        
+        PackageBuilder builder = new PackageBuilder();
+        builder.addPackageFromDrl( new InputStreamReader( StateExample.class.getResourceAsStream( "/State.drl" )) );        
+        
+        RuleBase ruleBase = RuleBaseFactory.newRuleBase();
+        ruleBase.addPackage( builder.getPackage() );
+        
+        WorkingMemory workingMemory = ruleBase.newWorkingMemory( );
+        
+        WorkingMemoryFileLogger logger = new WorkingMemoryFileLogger(workingMemory);
+        logger.setFileName("log/state");
+
+        State a = new State( "A" );
+        State b = new State( "B" );
+        State c = new State( "C" );
+        State d = new State( "D" );
+
+        // By setting dynamic to TRUE, Drools will use JavaBean
+        // PropertyChangeListeners so you don't have to call modifyObject().
+        boolean dynamic = true;
+
+        workingMemory.assertObject( a, dynamic );
+        workingMemory.assertObject( b, dynamic );
+        workingMemory.assertObject( c, dynamic );
+        workingMemory.assertObject( d, dynamic );
+
+        workingMemory.fireAllRules( );
+        
+        logger.writeToDisk();
+    }
+
+}
