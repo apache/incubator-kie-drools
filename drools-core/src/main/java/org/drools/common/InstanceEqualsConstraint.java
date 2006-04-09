c4603971c6893e6b802e8e335eec1eeaaad229c6
Mark Proctor <mdproctor@gmail.com>
2006-04-09 00:59:15 +0000

    JBRULES-189 Remove instance equal cross product
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@3527 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-core/src/main/java/org/drools/common/InstanceEqualsConstraint.java
@@ -0,0 +1,28 @@
+package org.drools.common;
+
+import org.drools.FactHandle;
+import org.drools.WorkingMemory;
+import org.drools.rule.Declaration;
+import  org.drools.spi.FieldConstraint;
+import org.drools.spi.Tuple;
+
+public class InstanceEqualsConstraint implements FieldConstraint {        
+    private Declaration[] declarations = new Declaration[0];
+    
+    private int otherColumn;
+
+    public InstanceEqualsConstraint(int otherColumn) {
+        this.otherColumn = otherColumn;
+    }    
+    
+    public Declaration[] getRequiredDeclarations() {
+        return this.declarations;
+    }
+
+    public boolean isAllowed(FactHandle handle,
+                             Tuple tuple,
+                             WorkingMemory workingMemory) {
+        return ! (workingMemory.getObject( tuple.get( this.otherColumn ) )== workingMemory.getObject( handle ) );
+    }
+
+}
---
+++ drools-core/src/test/java/org/drools/reteoo/CrossProductTest.java
@@ -0,0 +1,73 @@
+package org.drools.reteoo;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.drools.RuleBase;
+import org.drools.RuleBaseFactory;
+import org.drools.WorkingMemory;
+import org.drools.base.ClassObjectType;
+import org.drools.rule.Column;
+import org.drools.rule.Declaration;
+import org.drools.rule.Package;
+import org.drools.rule.Rule;
+import org.drools.spi.Consequence;
+import org.drools.spi.KnowledgeHelper;
+import org.drools.spi.ObjectType;
+
+public class CrossProductTest extends TestCase {
+    public void test1() throws Exception {
+        ObjectType list1ObjectType = new ClassObjectType( List.class );
+        ObjectType list2ObjectType = new ClassObjectType( List.class );
+
+        Rule rule = new Rule( "rule-1" );
+
+        Column list1Column = new Column( 0,
+                                         list1ObjectType,
+                                         "list1" );
+        Column list2Column = new Column( 1,
+                                         list2ObjectType,
+                                         "list2" );
+
+        rule.addPattern( list1Column );
+        rule.addPattern( list2Column );
+
+        final Declaration list1Declaration = rule.getDeclaration( "list1" );
+        final Declaration list2Declaration = rule.getDeclaration( "list2" );
+
+        final List values = new ArrayList();
+
+        rule.setConsequence( new Consequence() {
+
+            public void evaluate(KnowledgeHelper knowledgeHelper,
+                                 WorkingMemory workingMemory) throws Exception {
+                List list1 = (List) knowledgeHelper.get( list1Declaration );
+                List list2 = (List) knowledgeHelper.get( list2Declaration );
+                values.add( new List[]{list1, list2} );
+            }
+
+        } );
+
+        Package pkg = new Package( "org.drools" );
+        pkg.addRule( rule );
+
+        RuleBase ruleBase = RuleBaseFactory.newRuleBase();
+        ruleBase.addPackage( pkg );
+
+        WorkingMemory workingMemory = ruleBase.newWorkingMemory();
+        workingMemory.assertObject( new ArrayList() );
+        workingMemory.assertObject( new ArrayList() );
+        workingMemory.assertObject( new LinkedList() );
+        workingMemory.assertObject( new LinkedList() );
+
+        workingMemory.fireAllRules();
+
+        // A full cross product is 16, this is just 12
+        assertEquals( 12,
+                      values.size() );
+    }
+
+}
