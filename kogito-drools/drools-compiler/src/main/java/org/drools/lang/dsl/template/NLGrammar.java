970c885f134db094a52f678d65b33ea322e60e5d
Edson Tirelli <ed.tirelli@gmail.com>
2006-04-12 13:51:36 +0000

    JBRULES-88:
      * added validation method to NLGrammar
      * added MappingError class to represent errors found in mapping
      * updated unit test
    
    
    
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@3595 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/main/java/org/drools/lang/dsl/template/NLGrammar.java
+++ kogito-drools/drools-compiler/src/main/java/org/drools/lang/dsl/template/NLGrammar.java
@@ -1,22 +1,16 @@
 package org.drools.lang.dsl.template;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Serializable;
 import java.io.Writer;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Map.Entry;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -29,14 +23,15 @@ import org.apache.commons.lang.StringUtils;
 public class NLGrammar
     implements
     Serializable {
-
     
     private static final long serialVersionUID = 1L;
     private List mappings = new ArrayList();
     private static final Pattern itemPrefix = Pattern.compile( "\\[\\s*(when|then)\\s*\\].*" );
-    private String description;
+    private static final Pattern tokenPattern = Pattern.compile( "\\{(\\w*)\\}" ); 
+    private static final Pattern invalidPattern1 = Pattern.compile( "\\{\\w*(\\z|[^\\}\\w])" );
+    private static final Pattern invalidPattern2 = Pattern.compile( "[^\\{\\w]\\w*\\}" );
     
-
+    private String description;
 
     public NLGrammar() {
     }
@@ -163,5 +158,108 @@ public class NLGrammar
         }
     }
     
+    /**
+     * Validades the mapping returning a list of errors found 
+     * or an empty list in case of no errors
+     * 
+     * @return a List of MappingError's found or an empty list in case no one was found
+     */
+    public List validateMapping(NLMappingItem item) {
+        List errors = this.validateTokenUsage(item);
+        errors.addAll( this.validateUnmatchingBraces( item ) );
+        return errors;
+    }
+    
+    
+    /**
+     * Checks for tokens declared in the natural expression but not used in the 
+     * mapping and tokens used in the mapping but not declared in the natural 
+     * expression
+     * 
+     * @param item
+     * @return
+     */
+    private List validateTokenUsage(NLMappingItem item) {
+        List result = new ArrayList();
+        Matcher natural = tokenPattern.matcher( item.getNaturalTemplate() );
+        Matcher target = tokenPattern.matcher( item.getTargetTemplate() );
+        Set naturalSet = new HashSet();
+        Set targetSet = new HashSet();
+        while(natural.find()) {
+            naturalSet.add( natural.group() );
+        }
+        while(target.find()) {
+            targetSet.add( target.group() );
+        }
+        if( ! naturalSet.equals( targetSet )) {
+            Set aux = new HashSet(naturalSet);
+            naturalSet.removeAll( targetSet );
+            targetSet.removeAll( aux );
+            
+            for(Iterator i = naturalSet.iterator(); i.hasNext() ; ) {
+                String token = (String) i.next();
+                result.add( new MappingError(MappingError.ERROR_UNUSED_TOKEN,
+                                             MappingError.TEMPLATE_NATURAL,
+                                             item.getNaturalTemplate().indexOf( token ), 
+                                             token) );
+            }
+            for(Iterator i = targetSet.iterator(); i.hasNext() ; ) {
+                String token = (String) i.next();
+                result.add( new MappingError(MappingError.ERROR_UNDECLARED_TOKEN,
+                                             MappingError.TEMPLATE_TARGET,
+                                             item.getTargetTemplate().indexOf( token ),
+                                             token ) );
+            }
+        }
+        return result;
+    }
+    
+    /**
+     * Checks for unmatched brackets and invalid tokens
+     * 
+     * @param item
+     * @return
+     */
+    private List validateUnmatchingBraces(NLMappingItem item) {
+        List result = new ArrayList();
+        Matcher natural1 = invalidPattern1.matcher( item.getNaturalTemplate() );
+        Matcher natural2 = invalidPattern2.matcher( item.getNaturalTemplate() );
+        Matcher target1  = invalidPattern1.matcher( item.getTargetTemplate() );
+        Matcher target2  = invalidPattern2.matcher( item.getTargetTemplate() );
+        
+        while(natural1.find()) {
+            String token = natural1.group();
+            result.add( new MappingError( MappingError.ERROR_INVALID_TOKEN,
+                                          MappingError.TEMPLATE_NATURAL,
+                                          natural1.start(),
+                                          token));
+        }
+        
+        while(natural2.find()) {
+            String token = natural2.group();
+            result.add( new MappingError( MappingError.ERROR_UNMATCHED_BRACES,
+                                          MappingError.TEMPLATE_NATURAL,
+                                          natural2.start(),
+                                          token));
+        }
+        
+        while(target1.find()) {
+            String token = target1.group();
+            result.add( new MappingError( MappingError.ERROR_INVALID_TOKEN,
+                                          MappingError.TEMPLATE_TARGET,
+                                          target1.start(),
+                                          token));
+        }
+        
+        while(target2.find()) {
+            String token = target2.group();
+            result.add( new MappingError( MappingError.ERROR_UNMATCHED_BRACES,
+                                          MappingError.TEMPLATE_TARGET,
+                                          target2.start(),
+                                          token));
+        }
+        
+        return result;
+    }
     
 }
