0b33252e9b6821ae885a91ad13bc884c366901a8
Michael Neale <michael.neale@gmail.com>
2006-04-18 04:48:29 +0000

    JBRULES-215
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@3757 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-compiler/src/main/java/org/drools/semantics/java/parser/JavaParserLexer.java
+++ drools-compiler/src/main/java/org/drools/semantics/java/parser/JavaParserLexer.java
@@ -1,4 +1,21 @@
 // $ANTLR 3.0ea8 /Users/bob/checkouts/jbossrules/drools-compiler/src/main/resources/org/drools/semantics/java/parser/java.g 2006-04-10 01:50:30
+/*
+ * Copyright 2005 JBoss Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 
 	package org.drools.semantics.java.parser;
 
