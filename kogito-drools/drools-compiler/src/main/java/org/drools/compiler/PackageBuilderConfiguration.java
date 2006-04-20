6048ca2701075f31e04903b03c43dd1b46e898e8
Mark Proctor <mdproctor@gmail.com>
2006-04-20 03:54:18 +0000

    -Set back to JDT
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@3831 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/main/java/org/drools/compiler/PackageBuilderConfiguration.java
+++ kogito-drools/drools-compiler/src/main/java/org/drools/compiler/PackageBuilderConfiguration.java
@@ -1,4 +1,5 @@
 package org.drools.compiler;
+
 /*
  * Copyright 2005 JBoss Inc
  * 
@@ -15,51 +16,49 @@ package org.drools.compiler;
  * limitations under the License.
  */
 
-
-
 import org.apache.commons.jci.compilers.JavaCompilerFactory;
 import org.drools.RuntimeDroolsException;
 
-public class PackageBuilderConfiguration {    
-    public static final int ECLIPSE = 0;
-    public static final int JANINO  = 1;    
-    
-    private int compiler = JavaCompilerFactory.ECLIPSE;
-    
-    private ClassLoader          classLoader;
-    
-    public PackageBuilderConfiguration(){
+public class PackageBuilderConfiguration {
+    public static final int ECLIPSE  = 0;
+    public static final int JANINO   = 1;
+
+    private int             compiler = JavaCompilerFactory.ECLIPSE;
+
+    private ClassLoader     classLoader;
+
+    public PackageBuilderConfiguration() {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         if ( classLoader == null ) {
             classLoader = this.getClass().getClassLoader();
         }
-        this.classLoader = classLoader;     
-    }        
-    
+        this.classLoader = classLoader;
+    }
+
     public int getCompiler() {
         return this.compiler;
     }
-    
+
     public void setCompiler(int compiler) {
         switch ( compiler ) {
             case PackageBuilderConfiguration.ECLIPSE :
                 this.compiler = JavaCompilerFactory.ECLIPSE;
                 break;
-            case PackageBuilderConfiguration.JANINO:
+            case PackageBuilderConfiguration.JANINO :
                 this.compiler = JavaCompilerFactory.JANINO;
                 break;
-            default:
+            default :
                 throw new RuntimeDroolsException( "value '" + compiler + "' is not a valid compiler" );
         }
     }
-    
+
     public ClassLoader getClassLoader() {
         return this.classLoader;
     }
-    
-    public void setClassLoader( ClassLoader classLoader ) {
+
+    public void setClassLoader(ClassLoader classLoader) {
         if ( classLoader != null ) {
             this.classLoader = classLoader;
         }
-    }    
+    }
 }
