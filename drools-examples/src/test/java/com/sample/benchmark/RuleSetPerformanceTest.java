0baa325e7bdf8c11feba99212368a64e4de143e1
Peter Lin <woolfel@gmail.com>
2006-04-21 12:04:04 +0000

    updated the rule generator and performance test so I can run tests with 2k rules and 1 to 3 conditions per rule.
    peter
    
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@3884 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-examples/src/test/java/com/sample/benchmark/RuleSetPerformanceTest.java
+++ drools-examples/src/test/java/com/sample/benchmark/RuleSetPerformanceTest.java
@@ -234,4 +234,173 @@ public class RuleSetPerformanceTest extends TestCase {
 			e.printStackTrace();
 		}
 	}
+
+	/**
+	 * 
+	 *
+	 */
+	public void test2KRuleFire() {
+		try {
+			int factCount = 50000;
+			String file = "2000_rules_1condition.drl";
+			int loop = 5;
+			long totalload = 0;
+			long totalassert = 0;
+			long totalfire = 0;
+			for (int c=0; c < loop; c++) {
+				long loadStart = System.currentTimeMillis();
+		        RuleBase ruleBase = readRule(file);
+		        long loadEnd = System.currentTimeMillis();
+		        long loadet = loadEnd - loadStart;
+		        totalload += loadet;
+		        System.out.println("time to load " + file +
+		        		" " + loadet + "ms");
+		        WorkingMemory workingMemory = ruleBase.newWorkingMemory();
+		        ArrayList objects = new ArrayList();
+		        // create the objects
+		        for (int idx=0; idx < factCount; idx++) {
+		        	Account acc = new Account();
+		        	acc.setAccountId("acc"+idx);
+		        	acc.setTitle("mr");
+		        	acc.setStatus("standard");
+		        	objects.add(acc);
+		        }
+		        Iterator itr = objects.iterator();
+		        long assertStart = System.currentTimeMillis();
+		        while( itr.hasNext() ) {
+		        	workingMemory.assertObject(itr.next());
+		        }
+		        long assertEnd = System.currentTimeMillis();
+		        long assertet = assertEnd - assertStart;
+		        totalassert += assertet;
+		        System.out.println("time to assert " + assertet +
+		        		" ms");
+		        long fireStart = System.currentTimeMillis();
+		        workingMemory.fireAllRules();
+		        long fireEnd = System.currentTimeMillis();
+		        long fireet = fireEnd - fireStart;
+		        totalfire += fireet;
+		        System.out.println("time to fireAllRules " + fireet +
+		        		" ms");
+		        workingMemory.dispose();
+			}
+			System.out.println(file);
+			System.out.println("number of objects asserted " + factCount);
+			System.out.println("average load " + (totalload/loop) + " ms");
+			System.out.println("average assert " + (totalassert/loop) + " ms");
+			System.out.println("average fire " + (totalfire/loop) + " ms");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void test2KRuleFire2Conditions() {
+		try {
+			int factCount = 50000;
+			String file = "2000_rules_2condition.drl";
+			int loop = 5;
+			long totalload = 0;
+			long totalassert = 0;
+			long totalfire = 0;
+			for (int c=0; c < loop; c++) {
+				long loadStart = System.currentTimeMillis();
+		        RuleBase ruleBase = readRule(file);
+		        long loadEnd = System.currentTimeMillis();
+		        long loadet = loadEnd - loadStart;
+		        totalload += loadet;
+		        System.out.println("time to load " + file +
+		        		" " + loadet + "ms");
+		        WorkingMemory workingMemory = ruleBase.newWorkingMemory();
+		        ArrayList objects = new ArrayList();
+		        // create the objects
+		        for (int idx=0; idx < factCount; idx++) {
+		        	Account acc = new Account();
+		        	acc.setAccountId("acc"+idx);
+		        	acc.setTitle("mr");
+		        	acc.setStatus("standard");
+		        	objects.add(acc);
+		        }
+		        Iterator itr = objects.iterator();
+		        long assertStart = System.currentTimeMillis();
+		        while( itr.hasNext() ) {
+		        	workingMemory.assertObject(itr.next());
+		        }
+		        long assertEnd = System.currentTimeMillis();
+		        long assertet = assertEnd - assertStart;
+		        totalassert += assertet;
+		        System.out.println("time to assert " + assertet +
+		        		" ms");
+		        long fireStart = System.currentTimeMillis();
+		        workingMemory.fireAllRules();
+		        long fireEnd = System.currentTimeMillis();
+		        long fireet = fireEnd - fireStart;
+		        totalfire += fireet;
+		        System.out.println("time to fireAllRules " + fireet +
+		        		" ms");
+		        workingMemory.dispose();
+			}
+			System.out.println(file);
+			System.out.println("number of objects asserted " + factCount);
+			System.out.println("average load " + (totalload/loop) + " ms");
+			System.out.println("average assert " + (totalassert/loop) + " ms");
+			System.out.println("average fire " + (totalfire/loop) + " ms");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void test2KRuleFire3Conditions() {
+		try {
+			int factCount = 50000;
+			String file = "2000_rules_3condition.drl";
+			int loop = 5;
+			long totalload = 0;
+			long totalassert = 0;
+			long totalfire = 0;
+			for (int c=0; c < loop; c++) {
+				long loadStart = System.currentTimeMillis();
+		        RuleBase ruleBase = readRule(file);
+		        long loadEnd = System.currentTimeMillis();
+		        long loadet = loadEnd - loadStart;
+		        totalload += loadet;
+		        System.out.println("time to load " + file +
+		        		" " + loadet + "ms");
+		        WorkingMemory workingMemory = ruleBase.newWorkingMemory();
+		        ArrayList objects = new ArrayList();
+		        // create the objects
+		        for (int idx=0; idx < factCount; idx++) {
+		        	Account acc = new Account();
+		        	acc.setAccountId("acc"+idx);
+		        	acc.setTitle("mr");
+		        	acc.setStatus("standard");
+		        	objects.add(acc);
+		        }
+		        Iterator itr = objects.iterator();
+		        long assertStart = System.currentTimeMillis();
+		        while( itr.hasNext() ) {
+		        	workingMemory.assertObject(itr.next());
+		        }
+		        long assertEnd = System.currentTimeMillis();
+		        long assertet = assertEnd - assertStart;
+		        totalassert += assertet;
+		        System.out.println("time to assert " + assertet +
+		        		" ms");
+		        long fireStart = System.currentTimeMillis();
+		        workingMemory.fireAllRules();
+		        long fireEnd = System.currentTimeMillis();
+		        long fireet = fireEnd - fireStart;
+		        totalfire += fireet;
+		        System.out.println("time to fireAllRules " + fireet +
+		        		" ms");
+		        workingMemory.dispose();
+			}
+			System.out.println(file);
+			System.out.println("number of objects asserted " + factCount);
+			System.out.println("average load " + (totalload/loop) + " ms");
+			System.out.println("average assert " + (totalassert/loop) + " ms");
+			System.out.println("average fire " + (totalfire/loop) + " ms");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
