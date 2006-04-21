73ef419e8f07a1b9f95805c591f2d485c2584ab9
Edson Tirelli <ed.tirelli@gmail.com>
2006-04-21 14:47:25 +0000

    JBRULES-233:
      * Fixing problems described in the ticket, related to logical assertions
      * Adding unit tests and integration tests
    
    
    
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@3886 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-compiler/src/test/java/org/drools/EmergencyTeam.java
@@ -0,0 +1,5 @@
+package org.drools;
+
+public class EmergencyTeam {
+
+}
--- kogito-drools/drools-compiler/src/test/java/org/drools/Person.java
+++ kogito-drools/drools-compiler/src/test/java/org/drools/Person.java
@@ -84,4 +84,36 @@ public class Person {
     public String toString() {
         return "[Person name='" + this.name + "']";
     }
+
+
+    /**
+     * @inheritDoc
+     */
+    public int hashCode() {
+        final int PRIME = 31;
+        int result = 1;
+        result = PRIME * result + age;
+        result = PRIME * result + (alive ? 1231 : 1237);
+        result = PRIME * result + ((name == null) ? 0 : name.hashCode());
+        result = PRIME * result + sex;
+        return result;
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public boolean equals(Object obj) {
+        if ( this == obj ) return true;
+        if ( obj == null ) return false;
+        if ( getClass() != obj.getClass() ) return false;
+        final Person other = (Person) obj;
+        if ( age != other.age ) return false;
+        if ( alive != other.alive ) return false;
+        if ( name == null ) {
+            if ( other.name != null ) return false;
+        } else if ( !name.equals( other.name ) ) return false;
+        if ( sex != other.sex ) return false;
+        return true;
+    }
 }
