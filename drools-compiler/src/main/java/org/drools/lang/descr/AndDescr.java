39f993b7ebecb1c2a6340e446829e9ac43c5cc2c
Michael Neale <michael.neale@gmail.com>
2006-04-22 03:06:44 +0000

    refactored helloworld into integration tests
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@3896 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-compiler/src/main/java/org/drools/lang/descr/AndDescr.java
+++ drools-compiler/src/main/java/org/drools/lang/descr/AndDescr.java
@@ -1,4 +1,5 @@
 package org.drools.lang.descr;
+
 /*
  * Copyright 2005 JBoss Inc
  * 
@@ -15,26 +16,69 @@ package org.drools.lang.descr;
  * limitations under the License.
  */
 
-
-
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
-public class AndDescr extends PatternDescr 
-    implements ConditionalElementDescr {
-    private List descrs = Collections.EMPTY_LIST;
-    
+public class AndDescr extends PatternDescr
+    implements
+    ConditionalElementDescr {
+    private List descrs       = Collections.EMPTY_LIST;
+    private Map  boundColumns = new HashMap();
+
     public AndDescr() {
     }
-    
+
     public void addDescr(PatternDescr patternDescr) {
         if ( this.descrs == Collections.EMPTY_LIST ) {
-            this.descrs = new ArrayList(1);
+            this.descrs = new ArrayList( 1 );
         }
-        this.descrs.add( patternDescr );
+//MN: No pattern combining just yet        
+//        if ( patternDescr instanceof ColumnDescr ) {
+//            addColumn( (ColumnDescr) patternDescr );
+//        } else {
+            this.descrs.add( patternDescr );
+//        }
     }
-    
+
+    /**
+     * NOTE: to be used possibly in a future version... wire in above...
+     * This will check the column binding, and add the patterns 
+     * to a previously bound column
+     * if one exists. 
+     * If its not a bound column, or it is a unique bound variable column,
+     * it will just add it to the list of children descrs.
+     */
+    private void addColumn(ColumnDescr col) {
+        String identifier = col.getIdentifier();
+        if ( identifier == null || "".equals( identifier ) ) {
+            this.descrs.add( col );
+        } else {
+            if (boundColumns.containsKey( identifier )) {
+                ColumnDescr existingCol = (ColumnDescr) boundColumns.get( identifier );
+                if (existingCol.getObjectType().equals( col.getObjectType() )) {
+                    combinePatterns(existingCol, col.getDescrs());
+                } else {
+                    this.descrs.add( col );
+                }
+            } else {
+                boundColumns.put( identifier, col );
+                this.descrs.add( col );
+            }
+        }
+    }
+
+    private void combinePatterns(ColumnDescr existingCol,
+                                 List newColPatterns) {
+        for ( Iterator iter = newColPatterns.iterator(); iter.hasNext(); ) {
+            existingCol.addDescr( (PatternDescr) iter.next() );
+        }
+        
+    }
+
     public List getDescrs() {
         return this.descrs;
     }
