3558b6298691dc1d442cadd43421543b2e38fffe
Mark Proctor <mdproctor@gmail.com>
2006-05-01 00:57:40 +0000

    -JCI is now fixed, so we dont need to maintain our own MemoryResourceReader
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@4019 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/main/java/org/drools/compiler/PackageBuilder.java
+++ kogito-drools/drools-compiler/src/main/java/org/drools/compiler/PackageBuilder.java
@@ -28,6 +28,7 @@ import org.apache.commons.jci.compilers.CompilationResult;
 import org.apache.commons.jci.compilers.JavaCompiler;
 import org.apache.commons.jci.compilers.JavaCompilerFactory;
 import org.apache.commons.jci.readers.ResourceReader;
+import org.apache.commons.jci.readers.MemoryResourceReader;
 import org.apache.commons.jci.stores.ResourceStore;
 import org.drools.lang.descr.FunctionDescr;
 import org.drools.lang.descr.PackageDescr;
@@ -400,45 +401,4 @@ public class PackageBuilder {
         }
 
     }
-
-    public class MemoryResourceReader implements ResourceReader {
-        
-        private Map resources;
-
-        public boolean isAvailable(final String pResourceName) {
-            if (resources == null) {
-                return false;
-            }
-
-            return resources.containsKey( pResourceName );
-        }
-        
-        public void add(final String pResourceName, final byte[] pContent) {
-            if (resources == null) {
-                resources = new HashMap();
-            }
-            
-            resources.put(pResourceName, pContent);
-        }
-        
-        public void remove(final String pResourceName) {
-            if (resources != null) {
-                resources.remove(pResourceName);
-            }    
-        }    
-        
-
-        public byte[] getBytes(final String pResourceName)
-        {
-            return (byte[]) resources.get(pResourceName);
-        }
-
-        public String[] list() {
-            if (resources == null) {
-                return new String[0];
-            }
-            return (String[]) resources.keySet().toArray(new String[resources.size()]);
-        }
-    }    
-
 }
