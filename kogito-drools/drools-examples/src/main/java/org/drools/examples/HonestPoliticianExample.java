ae71a19958b022863e219a0380711a29987e2a43
Mark Proctor <mdproctor@gmail.com>
2006-05-18 22:05:22 +0000

    JBRULES-270 Honest Politician -Thanks to Peter Lin for this logical assertion and not example
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@4321 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-examples/src/main/java/org/drools/examples/HonestPoliticianExample.java
@@ -0,0 +1,73 @@
+package org.drools.examples;
+
+import java.io.InputStreamReader;
+
+import org.drools.FactHandle;
+import org.drools.RuleBase;
+import org.drools.RuleBaseFactory;
+import org.drools.WorkingMemory;
+import org.drools.audit.WorkingMemoryFileLogger;
+import org.drools.compiler.PackageBuilder;
+
+public class HonestPoliticianExample {
+
+    /**
+     * @param args
+     */
+    public static void main(final String[] args) throws Exception {
+
+        final PackageBuilder builder = new PackageBuilder();
+        builder.addPackageFromDrl( new InputStreamReader( HonestPoliticianExample.class.getResourceAsStream( "HonestPolitician.drl" ) ) );
+
+        final RuleBase ruleBase = RuleBaseFactory.newRuleBase();
+        ruleBase.addPackage( builder.getPackage() );
+
+        final WorkingMemory workingMemory = ruleBase.newWorkingMemory();
+
+        final WorkingMemoryFileLogger logger = new WorkingMemoryFileLogger( workingMemory );
+        logger.setFileName( "log/honest-politician" );
+
+        final Politician blair  = new Politician("blair", true);
+        final Politician bush  = new Politician("bush", true);
+        final Politician chirac  = new Politician("chirac", true);
+        final Politician schroder   = new Politician("schroder", true);
+        
+        workingMemory.assertObject( blair );
+        workingMemory.assertObject( bush );
+        workingMemory.assertObject( chirac );
+        workingMemory.assertObject( schroder );
+
+        workingMemory.fireAllRules();
+        
+        logger.writeToDisk();
+    }
+    
+    public static class Politician {
+    	private String name;
+    	
+    	private boolean honest;
+    	
+		public Politician(String name, boolean honest) {
+			super();
+			this.name = name;
+			this.honest = honest;
+		}
+		
+		public boolean isHonest() {
+			return honest;
+		}
+		
+		public void setHonest(boolean honest) {
+			this.honest = honest;
+		}
+
+		public String getName() {
+			return name;
+		}    			    
+    }
+
+    public static class Hope {
+    	
+    }
+    
+}
