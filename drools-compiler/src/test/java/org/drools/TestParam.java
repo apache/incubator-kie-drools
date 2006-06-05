ca8c06d0322e741b26c5adc3daa588bfc669c430
Michael Neale <michael.neale@gmail.com>
2006-06-05 02:29:02 +0000

    test for eval node sharing
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@4602 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-compiler/src/test/java/org/drools/TestParam.java
@@ -0,0 +1,21 @@
+package org.drools;
+
+public class TestParam {
+
+    private String value1;
+    private String value2;
+    public String getValue1() {
+        return value1;
+    }
+    public void setValue1(String value1) {
+        this.value1 = value1;
+    }
+    public String getValue2() {
+        return value2;
+    }
+    public void setValue2(String value2) {
+        this.value2 = value2;
+    }
+    
+    
+}
