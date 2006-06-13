5381b72ff7d9e9e634e8267ff7a9e3f683ba4e1b
Mark Proctor <mdproctor@gmail.com>
2006-06-13 21:41:51 +0000

    JBRULES-309 Convert over Jess golfing example
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@4757 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-examples/src/main/java/org/drools/examples/GolfingExample.java
@@ -0,0 +1,76 @@
+package org.drools.examples;
+
+import java.io.InputStreamReader;
+
+import org.drools.RuleBase;
+import org.drools.RuleBaseFactory;
+import org.drools.WorkingMemory;
+import org.drools.compiler.PackageBuilder;
+
+public class GolfingExample {
+
+    /**
+     * @param args
+     */
+    public static void main(final String[] args) throws Exception {
+
+        final PackageBuilder builder = new PackageBuilder();
+        builder.addPackageFromDrl( new InputStreamReader( GolfingExample.class.getResourceAsStream( "golf.drl" ) ) );
+
+        final RuleBase ruleBase = RuleBaseFactory.newRuleBase();
+        ruleBase.addPackage( builder.getPackage() );
+
+        final WorkingMemory workingMemory = ruleBase.newWorkingMemory();
+        
+        String[] names = new String[] { "Fred", "Joe", "Bob", "Tom" };
+        String[] colors = new String[] { "red", "blue", "plaid", "orange" };
+        int[] positions = new int[] { 1, 2, 3, 4 };
+        
+        for ( int n = 0; n < names.length; n++ ) {
+            for ( int c = 0; c < colors.length; c++ ) {
+                for ( int p = 0; p < positions.length; p++ ) {
+                    workingMemory.assertObject( new Golfer( names[n], colors[c], positions[p]) );
+                }                
+            }            
+        }
+
+        workingMemory.fireAllRules(); 
+    }
+
+
+    public static class Golfer {
+        private String name;
+        private String color;
+        private int position;
+        
+        public Golfer(String name,
+                      String color,
+                      int position) {
+            super();
+            this.name = name;
+            this.color = color;
+            this.position = position;
+        }
+        /**
+         * @return the color
+         */
+        public String getColor() {
+            return this.color;
+        }
+        /**
+         * @return the name
+         */
+        public String getName() {
+            return this.name;
+        }
+        
+        /**
+         * @return the name
+         */
+        public int getPosition() {
+            return this.position;
+        }        
+        
+    }
+}
+
