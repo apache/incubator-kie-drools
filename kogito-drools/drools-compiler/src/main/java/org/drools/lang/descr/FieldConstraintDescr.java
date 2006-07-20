68dda879209bf90553564c3a1ffa386a25be6937
Mark Proctor <mdproctor@gmail.com>
2006-07-20 15:02:32 +0000

    JBRULES-333 -Added new Restriction paradigm, so each FieldConstraint can have multiple restrictions -Compiler is currently broken, this is being committed so that Mic can fix the parser.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@5193 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-compiler/src/main/java/org/drools/lang/descr/FieldConstraintDescr.java
@@ -0,0 +1,51 @@
+package org.drools.lang.descr;
+
+import java.util.List;
+
+/*
+ * Copyright 2005 JBoss Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * This represents a literal node in the rule language. This is
+ * a constraint on a single field of a column. 
+ * The "text" contains the content, which may also be an enumeration. 
+ */
+public class FieldConstraintDescr extends PatternDescr {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 320;
+    private String  fieldName;
+    private List    restrictions;
+
+    public FieldConstraintDescr(final String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getFieldName() {
+        return this.fieldName;
+    }
+    
+    public void addRestriction(RestrictionDescr restriction) {
+        this.restrictions.add( restriction );
+    }
+    
+    public List getRestrictions() {
+        return this.restrictions;
+    }
+
+    
+}
