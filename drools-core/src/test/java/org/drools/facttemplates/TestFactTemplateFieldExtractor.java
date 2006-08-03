40bf69a997e5c6e35bddd04023fdc9986792fe41
Mark Proctor <mdproctor@gmail.com>
2006-08-03 18:51:20 +0000

    JBRULES-328 FactTemplates -added unit tests for FactTemplate
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@5450 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/test/java/org/drools/facttemplates/TestFactTemplateFieldExtractor.java
+++ drools-core/src/test/java/org/drools/facttemplates/TestFactTemplateFieldExtractor.java
@@ -1,11 +1,13 @@
 package org.drools.facttemplates;
 
+import org.drools.Cheese;
+import org.drools.rule.Declaration;
 import org.drools.spi.Extractor;
 
 import junit.framework.TestCase;
 
 public class TestFactTemplateFieldExtractor extends TestCase {
-    public void test1() {
+    public void testExtractor() {
         FieldTemplate cheeseName = new FieldTemplateImpl( "name",
                                                           0,
                                                           String.class );
@@ -45,5 +47,35 @@ public class TestFactTemplateFieldExtractor extends TestCase {
 
         assertEquals( new Integer( 55 ),
                       extractPrice.getValue( brie ) );
+    }
+    
+    public void testDeclaration() {
+        FieldTemplate cheeseName = new FieldTemplateImpl( "name",
+                                                          0,
+                                                          String.class );
+        FieldTemplate cheesePrice = new FieldTemplateImpl( "price",
+                                                           1,
+                                                           Integer.class );
+        FieldTemplate[] fields = new FieldTemplate[]{cheeseName, cheesePrice};
+        FactTemplate cheese = new FactTemplateImpl( "org.store",
+                                                    "Cheese",
+                                                    fields );
+        
+        Extractor extractName = new FactTemplateFieldExtractor( cheese,
+                                                                0 );        
+        
+        final Declaration declaration = new Declaration( "typeOfCheese",
+                                                         extractName,
+                                                         0 );
+
+        Fact brie = cheese.createFact( 12 );
+        brie.setFieldValue( "name",
+                            "brie" );
+        brie.setFieldValue( "price",
+                            new Integer( 55 ) );
+
+        /* Check we can extract Declarations correctly */
+        assertEquals( "brie",
+                      declaration.getValue( brie ) );        
     }
 }
