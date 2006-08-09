1de0587c9960e19f2ab95c05bb48c5e4dc9853e2
Michael Neale <michael.neale@gmail.com>
2006-08-09 10:54:58 +0000

    More stuff done
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@5631 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-brms/test/org/drools/brms/client/JBRMSEntryPointTest.java
@@ -0,0 +1,20 @@
+package org.drools.brms.client;
+
+
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+import junit.framework.TestCase;
+
+public class JBRMSEntryPointTest extends GWTTestCase {
+
+	public String getModuleName() {
+
+		return "org.drools.brms.JBRMS";
+	}
+	
+	public void testStartup() {
+		//JBRMSEntryPoint entry = new JBRMSEntryPoint();
+	}
+
+}
