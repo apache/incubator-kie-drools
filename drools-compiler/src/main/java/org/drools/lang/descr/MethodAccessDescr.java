3ae261f150666f3456785abb8ed941b06fa94684
Michael Neale <michael.neale@gmail.com>
2006-08-10 11:04:27 +0000

    Initial Descrs for the "from"
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@5702 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-compiler/src/main/java/org/drools/lang/descr/MethodAccessDescr.java
@@ -0,0 +1,55 @@
+package org.drools.lang.descr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * This represents a method call.
+ * As in:
+ * 
+ * variableName.methodName(argument list)
+ *
+ */
+public class MethodAccessDescr extends DeclarativeInvokerDescr {
+
+	private String methodName;
+	private List arguments = new ArrayList();
+	private String variableName;
+	
+	public MethodAccessDescr(String variableName, String methodName) {
+		this.methodName = methodName;
+		this.variableName = variableName;
+	}
+	
+	public void addArgument(String arg) {
+		this.arguments.add(arg);
+	}
+
+	public List getArguments() {
+		return arguments;
+	}
+
+	public void setArguments(List arguments) {
+		this.arguments = arguments;
+	}
+
+	public String getMethodName() {
+		return methodName;
+	}
+
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
+
+	public String getVariableName() {
+		return variableName;
+	}
+
+	public void setVariableName(String variableName) {
+		this.variableName = variableName;
+	}
+	
+	
+	
+}
