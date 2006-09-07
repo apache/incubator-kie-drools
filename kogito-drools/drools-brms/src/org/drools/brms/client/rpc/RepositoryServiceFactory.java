198e0faed472b0d4709903ef14a1585491e50dc0
Michael Neale <michael.neale@gmail.com>
2006-09-07 15:59:58 +0000

    updated to use an honest to God servlet !
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@6108 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-brms/src/org/drools/brms/client/rpc/RepositoryServiceFactory.java
+++ kogito-drools/drools-brms/src/org/drools/brms/client/rpc/RepositoryServiceFactory.java
@@ -13,9 +13,10 @@ public class RepositoryServiceFactory {
 
     /**
      * Change this to switch between debug/mock mode (ie web front end only)
-     * versus full RPC (which requires the back end be running).
+     * versus full RPC (which requires the back end be running in some form).
+     * Can set it to DEBUG if you want to run it client side only.
      */
-    public static boolean DEBUG = true;
+    public static boolean DEBUG = false;
     
     public static RepositoryServiceAsync getService() {
         if (DEBUG)
@@ -33,7 +34,7 @@ public class RepositoryServiceFactory {
         RepositoryServiceAsync svc =
             (RepositoryServiceAsync) GWT.create(RepositoryService.class);
         ServiceDefTarget endpoint = (ServiceDefTarget) svc;
-        endpoint.setServiceEntryPoint("/jbrmsService");
+        endpoint.setServiceEntryPoint(GWT.getModuleBaseURL() + "/jbrmsService");
         return svc;
     }
     
