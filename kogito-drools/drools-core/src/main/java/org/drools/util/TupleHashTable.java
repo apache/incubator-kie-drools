7b07c4f0587862bf1b0fc97810e041b62e4eed96
Mark Proctor <mdproctor@gmail.com>
2006-09-22 14:58:54 +0000

    JBRULES-498 Optimised HashMap impl -HashTable just for ReteTuples
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@6362 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-core/src/main/java/org/drools/util/TupleHashTable.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package org.drools.util;
+
+import org.drools.reteoo.ReteTuple;
+
+public class TupleHashTable extends AbstractHashTable {
+    public TupleHashTable() {
+        this( 16,
+              0.75f );
+    }
+
+    public TupleHashTable(int capacity,
+                         float loadFactor) {
+        super( capacity,
+               loadFactor );
+    }
+
+    public Object add(ReteTuple tuple) {
+        int hashCode = tuple.hashCode();
+        int index = indexOf( hashCode,
+                             table.length );
+
+        this.table[index] = tuple;
+
+        if ( this.size++ >= this.threshold ) {
+            resize( 2 * this.table.length );
+        }
+        return null;
+    }
+
+    public Object get(ReteTuple tuple) {
+        int hashCode = tuple.hashCode();
+        int index = indexOf( hashCode,
+                             table.length );
+
+        ReteTuple current = (ReteTuple) this.table[index];
+        while ( current != null ) {
+            if ( hashCode == current.hashCode() && tuple.equals( current ) ) {
+                return current;
+            }
+            current = (ReteTuple) current.getNext();
+        }
+        return null;
+    }
+
+    public Object remove(ReteTuple tuple) {
+        int hashCode = tuple.hashCode();
+        int index = indexOf( hashCode,
+                             table.length );
+
+        ReteTuple previous = (ReteTuple) this.table[index];
+        ReteTuple current = previous;
+        while ( current != null ) {
+            ReteTuple next = (ReteTuple) current.getNext();
+            if ( hashCode == current.hashCode() && tuple.equals( current ) ) {
+                if ( previous == current ) {
+                    this.table[index] = next;
+                } else {
+                    previous.setNext( next );
+                }
+                current.setNext( null );
+                this.size--;
+                return current;
+            }
+            previous = current;
+            current = next;
+        }
+        return current;
+    }
+    
+    public Entry getBucket(int hashCode) {
+        return this.table[ indexOf( hashCode, table.length ) ];
+    }    
+
+    protected int indexOf(int hashCode,
+                          int dataSize) {
+        return hashCode & (dataSize - 1);
+    }
+}
