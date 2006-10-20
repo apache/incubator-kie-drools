214a875be5f0ce9441d2980db9a557760ffa970d
Edson Tirelli <ed.tirelli@gmail.com>
2006-10-20 16:30:38 +0000

    JBRULES-527: fixing compilation problems
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@6963 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/main/java/org/drools/reteoo/NotNode.java
+++ drools-core/src/main/java/org/drools/reteoo/NotNode.java
@@ -107,7 +107,7 @@ public class NotNode extends BetaNode {
         memory.getTupleMemory().add( leftTuple );
 
         final Iterator it = memory.getObjectMemory().iterator( leftTuple );
-        this.constraints.updateFromTuple( leftTuple );
+        this.constraints.updateFromTuple( workingMemory, leftTuple );
         int matches = 0;
         for ( FactEntry entry = (FactEntry) it.next(); entry != null; entry = (FactEntry) it.next() ) {
             final InternalFactHandle handle = entry.getFactHandle();
@@ -144,7 +144,7 @@ public class NotNode extends BetaNode {
         memory.getObjectMemory().add( handle );
 
         final Iterator it = memory.getTupleMemory().iterator();
-        this.constraints.updateFromFactHandle( handle );
+        this.constraints.updateFromFactHandle( workingMemory, handle );
         for ( ReteTuple tuple = (ReteTuple) it.next(); tuple != null; tuple = (ReteTuple) it.next() ) {
             if ( this.constraints.isAllowedCachedRight( tuple ) ) {
                 final int matches = tuple.getMatches();
@@ -181,7 +181,7 @@ public class NotNode extends BetaNode {
         }
 
         final Iterator it = memory.getTupleMemory().iterator();
-        this.constraints.updateFromFactHandle( handle );
+        this.constraints.updateFromFactHandle( workingMemory, handle );
         for ( ReteTuple tuple = (ReteTuple) it.next(); tuple != null; tuple = (ReteTuple) it.next() ) {
             if ( this.constraints.isAllowedCachedRight( tuple ) ) {
                 tuple.setMatches( tuple.getMatches() - 1 );
