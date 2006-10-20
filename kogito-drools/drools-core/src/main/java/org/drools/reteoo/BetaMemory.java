3101ed7ec691ac416958bb1ffb44f348b4050d23
Edson Tirelli <ed.tirelli@gmail.com>
2006-10-20 22:43:16 +0000

    JBRULES-527: fixing compilation problems
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@6992 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/main/java/org/drools/reteoo/BetaMemory.java
+++ kogito-drools/drools-core/src/main/java/org/drools/reteoo/BetaMemory.java
@@ -1,10 +1,14 @@
 package org.drools.reteoo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.drools.util.TupleHashTable;
 
 public class BetaMemory {
     private TupleHashTable  tupleMemory;
     private ObjectHashTable objectMemory;
+    private Map         createdHandles;
 
     public BetaMemory(final TupleHashTable tupleMemory,
                       final ObjectHashTable objectMemory) {
@@ -18,5 +22,12 @@ public class BetaMemory {
 
     public TupleHashTable getTupleMemory() {
         return this.tupleMemory;
+    }
+    
+    public Map getCreatedHandles() {
+        if(createdHandles == null) {
+            createdHandles = new HashMap();
+        }
+        return createdHandles;
     }
 }
