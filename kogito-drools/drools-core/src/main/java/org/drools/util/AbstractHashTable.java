a1f501ac0e051d5b3232edb28f8becbdbed7eec1
Mark Proctor <mdproctor@gmail.com>
2006-10-23 22:53:33 +0000

    -fixed testNullValuesIndexing
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@7051 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/main/java/org/drools/util/AbstractHashTable.java
+++ kogito-drools/drools-core/src/main/java/org/drools/util/AbstractHashTable.java
@@ -294,6 +294,9 @@ public abstract class AbstractHashTable
 
         public boolean equal(final Object object1,
                              final Object object2) {
+        	if ( object1 == null ) {
+        		return object2 == null;
+        	}
             return object1.equals( object2 );
         }
     }
