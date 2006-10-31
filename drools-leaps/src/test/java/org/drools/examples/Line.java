f9ae550efc9b5429e23186e86ab2ae4f83b6fd92
Edson Tirelli <ed.tirelli@gmail.com>
2006-10-31 21:37:46 +0000

    moving waltz from core to leaps
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@7254 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-leaps/src/test/java/org/drools/examples/Line.java
@@ -0,0 +1,54 @@
+package org.drools.examples.waltz;
+
+/*
+ * Copyright 2005 JBoss Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Alexander Bagerman
+ * 
+ */
+
+public class Line {
+    private int p1;
+
+    private int p2;
+
+    public Line(final int p1,
+                final int p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    protected int getP1() {
+        return this.p1;
+    }
+
+    protected void setP1(final int p1) {
+        this.p1 = p1;
+    }
+
+    protected int getP2() {
+        return this.p2;
+    }
+
+    protected void setP2(final int p2) {
+        this.p2 = p2;
+    }
+
+    public String toString() {
+        return "{Line p1=" + this.p1 + ", p2=" + this.p2 + "}";
+    }
+}
