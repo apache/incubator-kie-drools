701cecf317512fbdb70b34cdd4d42319551fee96
Mark Proctor <mdproctor@gmail.com>
2006-11-02 00:01:21 +0000

    JBRULES-540 Create a Java Interface to allow JavaBeans to work more seamlessly as Facts
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@7304 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-core/src/main/java/org/drools/spi/JavaFact.java
@@ -0,0 +1,18 @@
+package org.drools.spi;
+
+import org.drools.WorkingMemory;
+import org.drools.base.JavaFactRegistryEntry;
+
+public interface JavaFact {
+        public JavaFactRegistryEntry[] listWorkingMemories();
+
+        public boolean register(final WorkingMemory workingMemory);
+
+        public void unregisterAll();
+
+        public boolean unregister(final WorkingMemory workingMemory);
+
+        public boolean isRegistered(final WorkingMemory workingMemory);
+
+        public int[] getChanges();            
+}
---
+++ kogito-drools/drools-core/src/test/java/org/drools/base/DelegateJavaFactHandlerTest.java
@@ -0,0 +1,124 @@
+package org.drools.base;
+
+import java.lang.reflect.Field;
+
+import junit.framework.TestCase;
+
+import org.drools.RuleBaseFactory;
+import org.drools.WorkingMemory;
+
+public class DelegateJavaFactHandlerTest extends TestCase {
+    
+    public void test1Entry() throws Exception {
+        DelegateJavaFactHandler handler = new DelegateJavaFactHandler();
+        
+        Field field = handler.getClass().getDeclaredField( "entries" );
+        field.setAccessible( true );
+        
+        WorkingMemory wm1 = RuleBaseFactory.newRuleBase().newWorkingMemory();        
+        handler.register( wm1 );
+        
+        JavaFactRegistryEntry[] entries = ( JavaFactRegistryEntry[] ) field.get( handler );
+        assertEquals( 1, entries.length );
+        
+        assertTrue( handler.isRegistered( wm1 ));
+        
+        assertEquals(1, handler.listWorkingMemories().length );
+        assertSame( wm1, handler.listWorkingMemories()[0].getWorkingMemory() );
+        
+        WorkingMemory wm2 = RuleBaseFactory.newRuleBase().newWorkingMemory();
+        assertFalse( handler.isRegistered( wm2 ));
+        
+        handler.unregister( wm1 );
+        assertFalse( handler.isRegistered( wm1 ));
+        entries = ( JavaFactRegistryEntry[] ) field.get( handler );
+        assertNull( entries );                                
+    }
+    
+    public void test2Entries() throws Exception {
+        DelegateJavaFactHandler handler = new DelegateJavaFactHandler();
+        
+        Field field = handler.getClass().getDeclaredField( "entries" );
+        field.setAccessible( true );
+        
+        WorkingMemory wm1 = RuleBaseFactory.newRuleBase().newWorkingMemory();
+        WorkingMemory wm2 = RuleBaseFactory.newRuleBase().newWorkingMemory();
+        handler.register( wm1 );
+        handler.register( wm2 );
+        
+        JavaFactRegistryEntry[] entries = ( JavaFactRegistryEntry[] ) field.get( handler );
+        assertEquals( 2, entries.length );
+        
+        assertTrue( handler.isRegistered( wm1 ));
+        assertTrue( handler.isRegistered( wm2 ));
+        
+        assertEquals(2, handler.listWorkingMemories().length );
+        assertSame( wm1, handler.listWorkingMemories()[0].getWorkingMemory() );
+        assertSame( wm2, handler.listWorkingMemories()[1].getWorkingMemory() );                
+        
+        handler.unregister( wm1 );
+        assertFalse( handler.isRegistered( wm1 ));
+        entries = ( JavaFactRegistryEntry[] ) field.get( handler );
+        assertEquals( 1, entries.length );       
+        
+        handler.unregister( wm2 );
+        assertFalse( handler.isRegistered( wm2 ));
+        entries = ( JavaFactRegistryEntry[] ) field.get( handler );
+        assertNull( entries );  
+        
+        // check  revererse
+        handler.register( wm1 );
+        handler.register( wm2 );    
+        
+        handler.unregister( wm2 );
+        assertFalse( handler.isRegistered( wm2 ));
+        entries = ( JavaFactRegistryEntry[] ) field.get( handler );
+        assertEquals( 1, entries.length );               
+        
+        handler.unregister( wm1 );
+        assertFalse( handler.isRegistered( wm1 ));
+        entries = ( JavaFactRegistryEntry[] ) field.get( handler );
+        assertNull( entries );               
+    }    
+    
+    public void test3Entries() throws Exception {
+        DelegateJavaFactHandler handler = new DelegateJavaFactHandler();
+        
+        Field field = handler.getClass().getDeclaredField( "entries" );
+        field.setAccessible( true );
+        
+        WorkingMemory wm1 = RuleBaseFactory.newRuleBase().newWorkingMemory();
+        WorkingMemory wm2 = RuleBaseFactory.newRuleBase().newWorkingMemory();
+        WorkingMemory wm3 = RuleBaseFactory.newRuleBase().newWorkingMemory();
+        handler.register( wm1 );
+        handler.register( wm2 );
+        handler.register( wm3 );
+        
+        JavaFactRegistryEntry[] entries = ( JavaFactRegistryEntry[] ) field.get( handler );
+        assertEquals( 3, entries.length );
+        
+        assertTrue( handler.isRegistered( wm1 ));
+        assertTrue( handler.isRegistered( wm2 ));
+        assertTrue( handler.isRegistered( wm3 ));
+        
+        assertEquals(3, handler.listWorkingMemories().length );
+        assertSame( wm1, handler.listWorkingMemories()[0].getWorkingMemory() );
+        assertSame( wm2, handler.listWorkingMemories()[1].getWorkingMemory() );
+        assertSame( wm3, handler.listWorkingMemories()[2].getWorkingMemory() );
+        
+        handler.unregister( wm2 );
+        assertFalse( handler.isRegistered( wm2 ));
+        entries = ( JavaFactRegistryEntry[] ) field.get( handler );
+        assertEquals( 2, entries.length );   
+        
+        handler.unregister( wm1 );
+        assertFalse( handler.isRegistered( wm1 ));
+        entries = ( JavaFactRegistryEntry[] ) field.get( handler );
+        assertEquals( 1, entries.length );       
+        
+        handler.unregister( wm3 );
+        assertFalse( handler.isRegistered( wm3 ));
+        entries = ( JavaFactRegistryEntry[] ) field.get( handler );
+        assertNull( entries );                
+    }        
+}
