a6abee74f5c552dade89fc96101c4b14af535d5c
Michael Neale <michael.neale@gmail.com>
2006-11-23 21:08:17 +0000

    still experimental, but its a start
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@7802 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-analytics/src/main/java/org/drools/analytics/AnalysisResult.java
@@ -0,0 +1,48 @@
+package org.drools.analytics;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.drools.spi.KnowledgeHelper;
+
+/**
+ * This is a class which contains lists of warnings and errors to be reported to the user via a user interface or static report of
+ * some form. 
+ * 
+ * It uses the ReportItem class to contains rules/warnings as pertains to them. Display them or ignore them as you will.
+ * 
+ * @author Michael Neale
+ *
+ */
+public class AnalysisResult implements Serializable {
+
+    
+    private static final long serialVersionUID = -6207688526236713721L;
+    private List warnings = new ArrayList();
+    private List errors = new ArrayList();
+    
+    public void addWarning(KnowledgeHelper k, String message) {
+        warnings.add( new ReportItem(k.getRule().getName(), message) );
+    }
+    
+    public void addError(KnowledgeHelper k, String message) {
+        errors.add( new ReportItem(k.getRule().getName(), message) );
+    }
+    
+    static class ReportItem implements Serializable {
+        private static final long serialVersionUID = 3674119457586457238L;
+        public String rule;
+        public String message;
+        
+        public ReportItem(String rule, String message) {
+            this.rule = rule;
+            this.message = message;
+        }
+        
+        public String toString() {
+            return rule + ": " + message;
+        }
+    }
+    
+}
---
+++ drools-analytics/src/main/java/org/drools/analytics/Rule.java
@@ -0,0 +1,9 @@
+package org.drools.analytics;
+
+public class Rule {
+
+    
+    public String name;
+    public Pattern[] patterns;
+    public String rhs;
+}
