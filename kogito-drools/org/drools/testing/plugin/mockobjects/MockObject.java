425884b1ecb55f724a5e119a781edbd8332a3476
Matthew Shaw <mshaw@emergency.qld.gov.au>
2007-01-30 13:22:43 +0000

    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@9134 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/org/drools/testing/plugin/mockobjects/MockObject.java
@@ -0,0 +1,28 @@
+package org.drools.testing.plugin.mockobjects;
+
+public class MockObject {
+
+	private String a = "a";
+	
+	private String b = "b";
+	
+	public MockObject () {
+		
+	}
+
+	public String getA() {
+		return a;
+	}
+
+	public void setA(String a) {
+		this.a = a;
+	}
+
+	public String getB() {
+		return b;
+	}
+
+	public void setB(String b) {
+		this.b = b;
+	}
+}
