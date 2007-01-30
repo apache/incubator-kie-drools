d8b63ded1b123900e7dce8373ab77434c0d77a09
Matthew Shaw <mshaw@emergency.qld.gov.au>
2007-01-30 13:30:11 +0000

    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@9140 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-testing-plugin/org/drools/testing/plugin/listeners/TreeViewSelectionListener.java
@@ -0,0 +1,15 @@
+package org.drools.testing.plugin.listeners;
+
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+
+public class TreeViewSelectionListener implements ISelectionChangedListener {
+
+	public TreeViewSelectionListener () {
+		 
+	}
+	
+	public void selectionChanged(SelectionChangedEvent event) {
+		
+	}
+}
