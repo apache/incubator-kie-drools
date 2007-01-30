2f98da16f9b998bda4fc1cead298abef3d40b9d9
Matthew Shaw <mshaw@emergency.qld.gov.au>
2007-01-30 13:30:11 +0000

    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@9140 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-testing-plugin/org/drools/testing/plugin/editors/EditorConstants.java
@@ -0,0 +1,24 @@
+package org.drools.testing.plugin.editors;
+
+public final class EditorConstants {
+
+	public static final String name = "Rtl";
+	
+	public interface MenuTitles {
+		
+		public final static String MENU_HEADER = "Rtl";
+		
+		public final static String GENERATE_RTL = "Generate Rtl";
+	}
+	
+	public interface ToolBar {
+		
+		public final static String GENERATE = "Generate";
+		
+		public final static String GENERATE_TOOLTIP = "Click here to generate an Rtl file";
+		
+		public final static String GENERATE_IMG = "/icons/RTL_small.gif";
+	}
+	
+	
+}
