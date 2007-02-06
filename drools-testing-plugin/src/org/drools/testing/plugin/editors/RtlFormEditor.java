584536ac6168834ba3d5e30a4a58100da3a992f5
Matthew Shaw <mshaw@emergency.qld.gov.au>
2007-02-06 03:48:02 +0000

    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@9360 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-testing-plugin/src/org/drools/testing/plugin/editors/RtlFormEditor.java
@@ -0,0 +1,43 @@
+package org.drools.testing.plugin.editors;
+
+import org.drools.testing.plugin.forms.InputForm;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.forms.editor.FormEditor;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+
+public class RtlFormEditor extends FormEditor {
+
+	public RtlFormEditor() {
+		
+	}
+
+	protected FormToolkit createToolkit(Display display) {
+		// Create a toolkit that shares colors between editors.
+		return new FormToolkit(display);
+	}
+
+	protected void addPages() {
+		try {
+			TextEditor editor = new TextEditor();
+			addPage(editor, getEditorInput());
+			addPage(new InputForm(this));
+		}
+		catch (PartInitException e) {
+			//
+		}
+	}
+
+	public void doSave(IProgressMonitor monitor) {
+	}
+
+	public void doSaveAs() {
+	}
+
+	public boolean isSaveAsAllowed() {
+		return false;
+	}
+	
+}
