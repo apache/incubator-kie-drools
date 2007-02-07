d2794a1c345baa1bf87853925a5ca0cf38ee67a3
Matthew Shaw <mshaw@emergency.qld.gov.au>
2007-02-07 12:31:27 +0000

    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@9390 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-testing-plugin/src/org/drools/testing/plugin/forms/InputForm.java
+++ kogito-drools/drools-testing-plugin/src/org/drools/testing/plugin/forms/InputForm.java
@@ -1,11 +1,22 @@
 package org.drools.testing.plugin.forms;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import org.drools.testing.core.beans.TestSuite;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormEditor;
 import org.eclipse.ui.forms.editor.FormPage;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
+import org.eclipse.ui.part.FileEditorInput;
+import org.exolab.castor.xml.Unmarshaller;
 
 public class InputForm extends FormPage {
 	
@@ -17,6 +28,17 @@ public class InputForm extends FormPage {
 	}
 	
 	protected void createFormContent(IManagedForm managedForm) {
+		
+		TestSuite testSuite;
+		try {
+			FileEditorInput fileEditorInput = ((FileEditorInput)getEditorInput());
+			BufferedReader br = new BufferedReader(new FileReader(fileEditorInput.getFile().getName()));
+			Unmarshaller unmarshaller = new Unmarshaller();
+			testSuite = (TestSuite) unmarshaller.unmarshal(br);
+		}catch (Exception e) {	
+			e.printStackTrace();
+		}
+		
 		ScrolledForm form = managedForm.getForm();
 		FormToolkit toolkit = managedForm.getToolkit();
 		form.setText("Rtl Input Capture"); //$NON-NLS-1$
@@ -27,6 +49,15 @@ public class InputForm extends FormPage {
 		//createTableSection(form, toolkit, Messages.getString("SecondPage.firstSection")); //$NON-NLS-1$
 		//createTableSection(form, toolkit, Messages.getString("SecondPage.secondSection"));		 //$NON-NLS-1$
 	}
+	
+	public void init(IEditorSite site, IEditorInput input) {
+		setSite(site);
+		setInput(input);
+	}
 
-
+	private void throwCoreException(String message) throws CoreException {
+		IStatus status =
+			new Status(IStatus.ERROR, "org.drools.testing.plugin", IStatus.OK, message, null);
+		throw new CoreException(status);
+	}
 }
