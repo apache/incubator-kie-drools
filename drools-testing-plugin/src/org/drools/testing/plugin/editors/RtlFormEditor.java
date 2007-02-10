cba645617fa96fa7430ae2f9581317b0c1be528f
Matthew Shaw <mshaw@emergency.qld.gov.au>
2007-02-10 08:14:26 +0000

    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@9422 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-testing-plugin/src/org/drools/testing/plugin/editors/RtlFormEditor.java
+++ drools-testing-plugin/src/org/drools/testing/plugin/editors/RtlFormEditor.java
@@ -1,16 +1,26 @@
 package org.drools.testing.plugin.editors;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import org.drools.testing.core.beans.TestSuite;
 import org.drools.testing.plugin.forms.InputForm;
-import org.drools.testing.plugin.resources.TestResourcesPlugin;
+import org.drools.testing.plugin.forms.MasterDetailsPage;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.editors.text.TextEditor;
 import org.eclipse.ui.forms.editor.FormEditor;
 import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.part.FileEditorInput;
+import org.exolab.castor.xml.Unmarshaller;
 
 public class RtlFormEditor extends FormEditor {
 
+	private TestSuite testSuite;
+	
 	public RtlFormEditor() {
 		
 	}
@@ -21,10 +31,12 @@ public class RtlFormEditor extends FormEditor {
 	}
 
 	protected void addPages() {
+		initialiseModel();
 		try {
 			TextEditor editor = new TextEditor();
 			int index = addPage(editor, getEditorInput());
 			setPageText(index, EditorConstants.EditorPageTitles.TITLE_FREE_FORM);
+			addPage(new MasterDetailsPage(this));
 			addPage(new InputForm(this));
 		}
 		catch (PartInitException e) {
@@ -33,13 +45,36 @@ public class RtlFormEditor extends FormEditor {
 	}
 
 	public void doSave(IProgressMonitor monitor) {
+		getEditor(0).doSave(monitor);
 	}
 
 	public void doSaveAs() {
+		IEditorPart editor = getEditor(0);
+		editor.doSaveAs();
+		setInput(editor.getEditorInput());
 	}
 
 	public boolean isSaveAsAllowed() {
 		return false;
+	}
+	
+	private void initialiseModel () {
+		try {
+			FileEditorInput fileEditorInput = ((FileEditorInput)getEditorInput());
+			BufferedReader br = new BufferedReader(new FileReader(fileEditorInput.getFile().getName()));
+			Unmarshaller unmarshaller = new Unmarshaller(TestSuite.class);
+			testSuite = (TestSuite) unmarshaller.unmarshal(br);
+		}catch (Exception e) {	
+			MessageDialog.openError(this.getSite().getShell(), "Error", e.getMessage());
+		}
+	}
+
+	public TestSuite getTestSuite() {
+		return testSuite;
+	}
+
+	public void setTestSuite(TestSuite testSuite) {
+		this.testSuite = testSuite;
 	}
 	
 }
--- drools-testing-plugin/src/org/drools/testing/plugin/forms/InputForm.java
+++ drools-testing-plugin/src/org/drools/testing/plugin/forms/InputForm.java
@@ -1,15 +1,16 @@
 package org.drools.testing.plugin.forms;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-
 import org.drools.testing.core.beans.TestSuite;
+import org.drools.testing.plugin.editors.RtlFormEditor;
 import org.drools.testing.plugin.resources.Messages;
 import org.drools.testing.plugin.resources.TestResourcesPlugin;
-import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.forms.IManagedForm;
@@ -17,18 +18,16 @@ import org.eclipse.ui.forms.editor.FormEditor;
 import org.eclipse.ui.forms.editor.FormPage;
 import org.eclipse.ui.forms.events.ExpansionAdapter;
 import org.eclipse.ui.forms.events.ExpansionEvent;
+import org.eclipse.ui.forms.widgets.ColumnLayout;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
 import org.eclipse.ui.forms.widgets.Section;
-import org.eclipse.ui.forms.widgets.TableWrapData;
-import org.eclipse.ui.forms.widgets.TableWrapLayout;
-import org.eclipse.ui.part.FileEditorInput;
-import org.exolab.castor.xml.Unmarshaller;
 
 public class InputForm extends FormPage {
 	
 	private FormToolkit toolkit;
 	private ScrolledForm form;
+	private TestSuite testSuite = null;
 
 	public InputForm(FormEditor editor) {
 		super(editor, "Input Capture","Rtl Input Capture"); //$NON-NLS-1$ //$NON-NLS-2$
@@ -36,61 +35,66 @@ public class InputForm extends FormPage {
 	
 	protected void createFormContent(IManagedForm managedForm) {
 		
-		TestSuite testSuite = null;
-		try {
-			FileEditorInput fileEditorInput = ((FileEditorInput)getEditorInput());
-			BufferedReader br = new BufferedReader(new FileReader(fileEditorInput.getFile().getName()));
-			Unmarshaller unmarshaller = new Unmarshaller(TestSuite.class);
-			testSuite = (TestSuite) unmarshaller.unmarshal(br);
-		}catch (Exception e) {	
-			MessageDialog.openError(this.getSite().getShell(), "Error", e.getMessage());
-		}
+		testSuite = ((RtlFormEditor) getEditor()).getTestSuite();
 	
 		form = managedForm.getForm();
 		toolkit = managedForm.getToolkit();
 		form.setText(Messages.getString("InputForm.title")); //$NON-NLS-1$
 		form.setBackgroundImage(TestResourcesPlugin.getDefault().getImage(TestResourcesPlugin.IMG_FORM_BG));
 		
-		TableWrapLayout layout = new TableWrapLayout();
+		ColumnLayout layout = new ColumnLayout();
+		layout.topMargin = 0;
+		layout.bottomMargin = 5;
+		layout.leftMargin = 10;
+		layout.rightMargin = 10;
+		layout.horizontalSpacing = 10;
+		layout.verticalSpacing = 10;
+		layout.maxNumColumns = 2;
+		layout.minNumColumns = 1;
 		form.getBody().setLayout(layout);
-		layout.numColumns = 2;
 		
-		toolkit.createLabel(form.getBody(), Messages.getString("FormView.testLabel")); //$NON-NLS-1$
-		Text text = toolkit.createText(form.getBody(), "Foo"); //$NON-NLS-1$
-		TableWrapData td = new TableWrapData(TableWrapData.FILL_GRAB);
-		text.setLayoutData(td);
+		createTestSuiteSection(managedForm, Messages.getString("InputForm.testSuiteSection"), Messages.getString("InputForm.testSuiteSection.descr"));
 		
-		Section section = toolkit.createSection(form.getBody(), 
-		Section.DESCRIPTION|Section.TITLE_BAR|
-		Section.TWISTIE|Section.EXPANDED);
-		td = new TableWrapData(TableWrapData.FILL_GRAB);
-		td.colspan = 2;
-
-		section.setLayoutData(td);
+	}
+	
+	private void createTestSuiteSection (IManagedForm mform, String title, String desc) {
+		
+		Composite client = createSection(mform, title, desc, 2);
+		FormToolkit toolkit = mform.getToolkit();
+		toolkit.createLabel(client, Messages.getString("InputForm.testSuiteSection.nameLabel"));
+		toolkit.createText(client, testSuite.getName());
+		/*Button apply = toolkit.createButton(client, Messages.getString("InputForm.apply"), SWT.PUSH); //$NON-NLS-1$
+		apply.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_FILL|GridData.VERTICAL_ALIGN_BEGINNING));*/
+	}
+	
+	private Composite createSection(IManagedForm mform, String title,
+			String desc, int numColumns) {
+		final ScrolledForm form = mform.getForm();
+		FormToolkit toolkit = mform.getToolkit();
+		Section section = toolkit.createSection(form.getBody(), Section.TWISTIE
+				| Section.TITLE_BAR | Section.DESCRIPTION | Section.EXPANDED);
+		section.setText(title);
+		section.setDescription(desc);
+		//toolkit.createCompositeSeparator(section);
+		Composite client = toolkit.createComposite(section);
+		GridLayout layout = new GridLayout();
+		layout.marginWidth = layout.marginHeight = 0;
+		layout.numColumns = numColumns;
+		client.setLayout(layout);
+		section.setClient(client);
 		section.addExpansionListener(new ExpansionAdapter() {
-		public void expansionStateChanged(ExpansionEvent e) {
-			form.reflow(true);
-		  	}
+			public void expansionStateChanged(ExpansionEvent e) {
+				form.reflow(false);
+			}
 		});
-		section.setText(Messages.getString("InputForm.testSuiteSection"));
-		section.setDescription(Messages.getString("InputForm.testSuiteSection.descr"));
-
-		Composite sectionClient = toolkit.createComposite(section);
-		GridLayout gd = new GridLayout();
-		gd.numColumns = 2;
-		sectionClient.setLayout(gd);
-		toolkit.createLabel(sectionClient, Messages.getString("InputForm.testSuiteSection.nameLabel"));
-		toolkit.createText(sectionClient, testSuite.getName());
-		section.setClient(sectionClient);
-		
-
-		 toolkit.paintBordersFor(form.getBody());
-		
+		return client;
 	}
 	
 	public void init(IEditorSite site, IEditorInput input) {
 		setSite(site);
 		setInput(input);
 	}
+	
+	
 
 }
