cd4cf524a9df5ce8806720cbe4ef98406840095e
Edson Tirelli <ed.tirelli@gmail.com>
2007-02-12 20:22:18 +0000

    JBRULES-659: adding test case for class type resolver as contributed by Frederic Burlet
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@9455 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-core/src/test/java/org/codehaus/jfdi/interpreter/ClassTypeResolverTest.java
@@ -0,0 +1,114 @@
+/**
+ * 
+ */
+package org.codehaus.jfdi.interpreter;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+
+import org.drools.Cheese;
+
+/**
+ * @author fburlet
+ *
+ */
+public class ClassTypeResolverTest extends TestCase {
+    
+    public void testResolvePrimtiveTypes() throws Exception {
+        ClassTypeResolver resolver = new ClassTypeResolver();
+        assertEquals(boolean.class, resolver.resolveType("boolean"));
+        assertEquals(double.class, resolver.resolveType("double"));
+        assertEquals(float.class, resolver.resolveType("float"));
+        assertEquals(int.class, resolver.resolveType("int"));
+        assertEquals(char.class, resolver.resolveType("char"));
+        assertEquals(long.class, resolver.resolveType("long"));
+        assertEquals(byte.class, resolver.resolveType("byte"));
+        assertEquals(short.class, resolver.resolveType("short"));
+    }
+    
+    public void testResolveArrayOfPrimitiveTypes() throws Exception {
+        ClassTypeResolver resolver = new ClassTypeResolver();
+        assertEquals(boolean[].class, resolver.resolveType("boolean[]"));
+        assertEquals(double[].class, resolver.resolveType("double[]"));
+        assertEquals(float[].class, resolver.resolveType("float[]"));
+        assertEquals(int[].class, resolver.resolveType("int[]"));
+        assertEquals(char[].class, resolver.resolveType("char[]"));
+        assertEquals(long[].class, resolver.resolveType("long[]"));
+        assertEquals(byte[].class, resolver.resolveType("byte[]"));
+        assertEquals(short[].class, resolver.resolveType("short[]"));
+    }
+    
+    public void testResolveMultidimensionnalArrayOfPrimitiveTypes() throws Exception {
+        ClassTypeResolver resolver = new ClassTypeResolver(); 
+        assertEquals(int[][].class, resolver.resolveType("int[][]"));
+        assertEquals(int[][][].class, resolver.resolveType("int[][][]"));
+        assertEquals(int[][][][].class, resolver.resolveType("int[][][][]"));
+    }
+    
+    public void testResolveObjectNotFromImport() throws Exception {
+        ClassTypeResolver resolver = new ClassTypeResolver();
+        assertEquals(String.class, resolver.resolveType("String"));
+        assertEquals(String.class, resolver.resolveType("java.lang.String"));
+        try {
+            assertEquals(Cheese.class, resolver.resolveType("Cheese"));
+            fail("Should raise a ClassNotFoundException");
+        } catch ( ClassNotFoundException e ) {
+            // success
+        }
+        assertEquals(Cheese.class, resolver.resolveType("org.drools.Cheese"));
+    }
+    
+    public void testResolveObjectFromImport() throws Exception {
+        ClassTypeResolver resolver = new ClassTypeResolver(new ArrayList());
+        resolver.addImport("org.drools.Cheese");
+        assertEquals(String.class, resolver.resolveType("String"));
+        assertEquals(String.class, resolver.resolveType("java.lang.String"));
+        assertEquals(Cheese.class, resolver.resolveType("Cheese"));
+        assertEquals(Cheese.class, resolver.resolveType("org.drools.Cheese"));
+    }
+    
+    public void testResolveArrayOfObjectsNotFromImport() throws Exception {
+        ClassTypeResolver resolver = new ClassTypeResolver();
+        assertEquals(String[].class, resolver.resolveType("String[]"));
+        assertEquals(String[].class, resolver.resolveType("java.lang.String[]"));
+        try {
+            assertEquals(Cheese[].class, resolver.resolveType("Cheese[]"));
+            fail("Should raise a ClassNotFoundException");
+        } catch ( ClassNotFoundException e ) {
+            // success
+        }
+        assertEquals(Cheese[].class, resolver.resolveType("org.drools.Cheese[]"));
+    }
+    
+    public void testResolveArrayOfObjectsFromImport() throws Exception {
+        ClassTypeResolver resolver = new ClassTypeResolver(new ArrayList());
+        resolver.addImport("org.drools.Cheese");
+        assertEquals(String[].class, resolver.resolveType("String[]"));
+        assertEquals(String[].class, resolver.resolveType("java.lang.String[]"));
+        assertEquals(Cheese[].class, resolver.resolveType("Cheese[]"));
+        assertEquals(Cheese[].class, resolver.resolveType("org.drools.Cheese[]"));
+    }
+    
+    public void testResolveMultidimensionnalArrayOfObjectsNotFromImport() throws Exception {
+        ClassTypeResolver resolver = new ClassTypeResolver();
+        assertEquals(String[][].class, resolver.resolveType("String[][]"));
+        assertEquals(String[][].class, resolver.resolveType("java.lang.String[][]"));
+        try {
+            assertEquals(Cheese[][].class, resolver.resolveType("Cheese[][]"));
+            fail("Should raise a ClassNotFoundException");
+        } catch ( ClassNotFoundException e ) {
+            // success
+        }
+        assertEquals(Cheese[][].class, resolver.resolveType("org.drools.Cheese[][]"));
+    }
+    
+    public void testResolveMultidimensionnalArrayOfObjectsFromImport() throws Exception {
+        ClassTypeResolver resolver = new ClassTypeResolver(new ArrayList());
+        resolver.addImport("org.drools.Cheese");
+        assertEquals(String[][].class, resolver.resolveType("String[][]"));
+        assertEquals(String[][].class, resolver.resolveType("java.lang.String[][]"));
+        assertEquals(Cheese[][].class, resolver.resolveType("Cheese[][]"));
+        assertEquals(Cheese[][].class, resolver.resolveType("org.drools.Cheese[][]"));
+    }
+}
