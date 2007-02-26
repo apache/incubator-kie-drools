f76fdaf172ba265aaa2113a111c7f564380ea7dc
Mark Proctor <mdproctor@gmail.com>
2007-02-26 11:54:53 +0000

    JBRULES-708 MVEL Integration -integration for the MVELEvalBuilder
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@9759 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-core/src/main/java/org/drools/base/MVEL/MVELEvalExpression.java
@@ -0,0 +1,33 @@
+package org.drools.base.MVEL;
+
+import java.io.Serializable;
+
+import org.drools.WorkingMemory;
+import org.drools.base.DroolsMVELFactory;
+import org.drools.rule.Declaration;
+import org.drools.spi.EvalExpression;
+import org.drools.spi.Tuple;
+import org.mvel.MVEL;
+
+public class MVELEvalExpression
+    implements
+    EvalExpression {
+    
+    private final Serializable expr;
+    private final DroolsMVELFactory factory;
+    
+    public MVELEvalExpression(final Serializable expr,
+                              final DroolsMVELFactory factory) {
+        this.expr = expr;
+        this.factory = factory;
+    }
+
+    public boolean evaluate(Tuple tuple,
+                            Declaration[] requiredDeclarations,
+                            WorkingMemory workingMemory) throws Exception {                
+        factory.setContext( tuple, workingMemory );   
+        Boolean result = ( Boolean ) MVEL.executeExpression(this.expr, factory);        
+        return result.booleanValue(); 
+    }
+
+}
