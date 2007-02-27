ba5ae5ea3ed073680881042c1d606979822195bf
Mark Proctor <mdproctor@gmail.com>
2007-02-27 00:02:42 +0000

    JBRULES-708 MVEL Integration -More interface/implementation seperation for dialects
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@9778 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-compiler/src/main/java/org/drools/semantics/java/builder/AccumulateBuilder.java
@@ -0,0 +1,16 @@
+package org.drools.semantics.java.builder;
+
+import org.drools.lang.descr.BaseDescr;
+import org.drools.rule.ConditionalElement;
+
+public interface AccumulateBuilder {
+
+    /* (non-Javadoc)
+     * @see org.drools.semantics.java.builder.ConditionalElementBuilder#build(org.drools.semantics.java.builder.BuildContext, org.drools.semantics.java.builder.BuildUtils, org.drools.semantics.java.builder.ColumnBuilder, org.drools.lang.descr.BaseDescr)
+     */
+    public ConditionalElement build(BuildContext context,
+                                    BuildUtils utils,
+                                    ColumnBuilder columnBuilder,
+                                    BaseDescr descr);
+
+}
