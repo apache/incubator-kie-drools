444b70986082f0a85b03a3c29a0ad0d5f847b37f
Mark Proctor <mdproctor@gmail.com>
2007-02-27 00:06:47 +0000

    JBRULES-708 MVEL Integration -More interface/implementation seperation for dialects
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@9780 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-compiler/src/main/java/org/drools/semantics/java/builder/FromBuilder.java
@@ -0,0 +1,16 @@
+package org.drools.semantics.java.builder;
+
+import org.drools.lang.descr.BaseDescr;
+import org.drools.rule.ConditionalElement;
+
+public interface FromBuilder {
+
+    /**
+     * @inheritDoc
+     */
+    public ConditionalElement build(final BuildContext context,
+                                    final BuildUtils utils,
+                                    final ColumnBuilder columnBuilder,
+                                    final BaseDescr descr);
+
+}
