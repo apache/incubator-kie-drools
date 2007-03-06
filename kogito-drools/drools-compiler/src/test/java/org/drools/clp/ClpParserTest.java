894f1133a611c7e395ae6239a456fe86a7d12e54
Mark Proctor <mdproctor@gmail.com>
2007-03-06 01:12:12 +0000

    JBRULES-720 Clips Parser -'not' and 'exists' now work
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@9993 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/test/java/org/drools/clp/ClpParserTest.java
+++ kogito-drools/drools-compiler/src/test/java/org/drools/clp/ClpParserTest.java
@@ -16,8 +16,10 @@ import org.drools.lang.DRLLexer;
 import org.drools.lang.DRLParser;
 import org.drools.lang.descr.AndDescr;
 import org.drools.lang.descr.ColumnDescr;
+import org.drools.lang.descr.ExistsDescr;
 import org.drools.lang.descr.FieldConstraintDescr;
 import org.drools.lang.descr.LiteralRestrictionDescr;
+import org.drools.lang.descr.NotDescr;
 import org.drools.lang.descr.OrDescr;
 import org.drools.lang.descr.PredicateDescr;
 import org.drools.lang.descr.RestrictionConnectiveDescr;
@@ -26,144 +28,251 @@ import org.drools.lang.descr.RuleDescr;
 import org.drools.lang.descr.VariableRestrictionDescr;
 
 public class ClpParserTest extends TestCase {
-    
+
     private CLPParser parser;
-    
+
     public void testRule() throws Exception {
-        RuleDescr rule = parse("(defrule xxx ?b <- (person (name \"yyy\"&?bf|~\"zzz\"|~=(ppp)&:(ooo)) ) ?c <- (hobby (type ?bf2&~iii) (rating fivestar) )").rule();
-        
-        assertEquals( "xxx", rule.getName() );
-        
+        RuleDescr rule = parse( "(defrule xxx ?b <- (person (name \"yyy\"&?bf|~\"zzz\"|~=(ppp)&:(ooo)) ) ?c <- (hobby (type ?bf2&~iii) (rating fivestar) )" ).rule();
+
+        assertEquals( "xxx",
+                      rule.getName() );
+
         AndDescr lhs = rule.getLhs();
         List lhsList = lhs.getDescrs();
-        assertEquals(2, lhsList.size());
-        
-        // Parse the first column
-        ColumnDescr col = ( ColumnDescr ) lhsList.get( 0 );
-        assertEquals("?b", col.getIdentifier() );
-        assertEquals("person", col.getObjectType() );
-        
-        List colList = col.getDescrs();
-        assertEquals(2, colList.size());
-        FieldConstraintDescr fieldConstraintDescr = ( FieldConstraintDescr ) colList.get( 0 );
-        List restrictionList = fieldConstraintDescr.getRestrictions();
-        
-        assertEquals("name", fieldConstraintDescr.getFieldName() );        
-        // @todo the 7th one has no constraint, as its a predicate, have to figure out how to handle this
-        assertEquals(8, restrictionList.size());
-                
-        
-        LiteralRestrictionDescr litDescr = ( LiteralRestrictionDescr ) restrictionList.get( 0 );
-        assertEquals("==", litDescr.getEvaluator() );
-        assertEquals("yyy", litDescr.getText() );
+        assertEquals( 2,
+                      lhsList.size() );
 
-        RestrictionConnectiveDescr connDescr = ( RestrictionConnectiveDescr ) restrictionList.get( 1 );
-        assertEquals(RestrictionConnectiveDescr.AND, connDescr.getConnective() );
-        
-        VariableRestrictionDescr varDescr = ( VariableRestrictionDescr ) restrictionList.get( 2 );
-        assertEquals("==", varDescr.getEvaluator() );
-        assertEquals("?bf", varDescr.getIdentifier() );        
-        
-        connDescr = ( RestrictionConnectiveDescr ) restrictionList.get( 3 );
-        assertEquals(RestrictionConnectiveDescr.OR, connDescr.getConnective() );
-        
-        litDescr = ( LiteralRestrictionDescr ) restrictionList.get( 4 );
-        assertEquals("!=", litDescr.getEvaluator() );
-        assertEquals("zzz", litDescr.getText() );
-        
-        connDescr = ( RestrictionConnectiveDescr ) restrictionList.get( 5 );
-        assertEquals(RestrictionConnectiveDescr.OR, connDescr.getConnective() );
-        
-        ReturnValueRestrictionDescr retDescr = ( ReturnValueRestrictionDescr ) restrictionList.get( 6 );
-        assertEquals("!=", retDescr.getEvaluator() );
-        assertEquals("ppp", retDescr.getText() );
-        
-        PredicateDescr predicateDescr = ( PredicateDescr ) colList.get( 1 );
-        assertEquals( "ooo", predicateDescr.getText() );
-        
-        
+        // Parse the first column
+        ColumnDescr col = (ColumnDescr) lhsList.get( 0 );
+        assertEquals( "?b",
+                      col.getIdentifier() );
+        assertEquals( "person",
+                      col.getObjectType() );
+
+        List colList = col.getDescrs();
+        assertEquals( 2,
+                      colList.size() );
+        FieldConstraintDescr fieldConstraintDescr = (FieldConstraintDescr) colList.get( 0 );
+        List restrictionList = fieldConstraintDescr.getRestrictions();
+
+        assertEquals( "name",
+                      fieldConstraintDescr.getFieldName() );
+        // @todo the 7th one has no constraint, as its a predicate, have to figure out how to handle this
+        assertEquals( 8,
+                      restrictionList.size() );
+
+        LiteralRestrictionDescr litDescr = (LiteralRestrictionDescr) restrictionList.get( 0 );
+        assertEquals( "==",
+                      litDescr.getEvaluator() );
+        assertEquals( "yyy",
+                      litDescr.getText() );
+
+        RestrictionConnectiveDescr connDescr = (RestrictionConnectiveDescr) restrictionList.get( 1 );
+        assertEquals( RestrictionConnectiveDescr.AND,
+                      connDescr.getConnective() );
+
+        VariableRestrictionDescr varDescr = (VariableRestrictionDescr) restrictionList.get( 2 );
+        assertEquals( "==",
+                      varDescr.getEvaluator() );
+        assertEquals( "?bf",
+                      varDescr.getIdentifier() );
+
+        connDescr = (RestrictionConnectiveDescr) restrictionList.get( 3 );
+        assertEquals( RestrictionConnectiveDescr.OR,
+                      connDescr.getConnective() );
+
+        litDescr = (LiteralRestrictionDescr) restrictionList.get( 4 );
+        assertEquals( "!=",
+                      litDescr.getEvaluator() );
+        assertEquals( "zzz",
+                      litDescr.getText() );
+
+        connDescr = (RestrictionConnectiveDescr) restrictionList.get( 5 );
+        assertEquals( RestrictionConnectiveDescr.OR,
+                      connDescr.getConnective() );
+
+        ReturnValueRestrictionDescr retDescr = (ReturnValueRestrictionDescr) restrictionList.get( 6 );
+        assertEquals( "!=",
+                      retDescr.getEvaluator() );
+        assertEquals( "ppp",
+                      retDescr.getText() );
+
+        PredicateDescr predicateDescr = (PredicateDescr) colList.get( 1 );
+        assertEquals( "ooo",
+                      predicateDescr.getText() );
+
         // Parse the second column
-        col = ( ColumnDescr ) lhsList.get( 1 );
-        assertEquals("?c", col.getIdentifier() );
-        assertEquals("hobby", col.getObjectType() );   
+        col = (ColumnDescr) lhsList.get( 1 );
+        assertEquals( "?c",
+                      col.getIdentifier() );
+        assertEquals( "hobby",
+                      col.getObjectType() );
 
         colList = col.getDescrs();
-        assertEquals(2, colList.size());
-        fieldConstraintDescr = ( FieldConstraintDescr ) colList.get( 0 );
+        assertEquals( 2,
+                      colList.size() );
+        fieldConstraintDescr = (FieldConstraintDescr) colList.get( 0 );
         restrictionList = fieldConstraintDescr.getRestrictions();
-        
-        assertEquals("type", fieldConstraintDescr.getFieldName() );        
-        
-        varDescr = ( VariableRestrictionDescr ) restrictionList.get( 0 );
-        assertEquals("==", varDescr.getEvaluator() );
-        assertEquals("?bf2", varDescr.getIdentifier() );  
-        
-        connDescr = ( RestrictionConnectiveDescr ) restrictionList.get( 1 );
-        assertEquals(RestrictionConnectiveDescr.AND, connDescr.getConnective() );
-        
-        litDescr = ( LiteralRestrictionDescr ) restrictionList.get( 2 );
-        assertEquals("!=", litDescr.getEvaluator() );
-        assertEquals("iii", litDescr.getText() );
-        
-        fieldConstraintDescr = ( FieldConstraintDescr ) colList.get( 1 );
-        restrictionList = fieldConstraintDescr.getRestrictions();        
-        
-        assertEquals("rating", fieldConstraintDescr.getFieldName() );
-        
-        litDescr = ( LiteralRestrictionDescr ) restrictionList.get( 0 );
-        assertEquals("==", litDescr.getEvaluator() );
-        assertEquals("fivestar", litDescr.getText() );        
+
+        assertEquals( "type",
+                      fieldConstraintDescr.getFieldName() );
+
+        varDescr = (VariableRestrictionDescr) restrictionList.get( 0 );
+        assertEquals( "==",
+                      varDescr.getEvaluator() );
+        assertEquals( "?bf2",
+                      varDescr.getIdentifier() );
+
+        connDescr = (RestrictionConnectiveDescr) restrictionList.get( 1 );
+        assertEquals( RestrictionConnectiveDescr.AND,
+                      connDescr.getConnective() );
+
+        litDescr = (LiteralRestrictionDescr) restrictionList.get( 2 );
+        assertEquals( "!=",
+                      litDescr.getEvaluator() );
+        assertEquals( "iii",
+                      litDescr.getText() );
+
+        fieldConstraintDescr = (FieldConstraintDescr) colList.get( 1 );
+        restrictionList = fieldConstraintDescr.getRestrictions();
+
+        assertEquals( "rating",
+                      fieldConstraintDescr.getFieldName() );
+
+        litDescr = (LiteralRestrictionDescr) restrictionList.get( 0 );
+        assertEquals( "==",
+                      litDescr.getEvaluator() );
+        assertEquals( "fivestar",
+                      litDescr.getText() );
     }
-    
+
     public void testNestedCERule() throws Exception {
-        RuleDescr rule = parse("(defrule xxx ?b <- (person (name yyy)) (or (and (hobby1 (type qqq1)) (hobby2 (type ~qqq2))) (food (veg ~shroom) ) ) )").rule();
-        
-        assertEquals( "xxx", rule.getName() );
-        
+        RuleDescr rule = parse( "(defrule xxx ?b <- (person (name yyy)) (or (and (hobby1 (type qqq1)) (hobby2 (type ~qqq2))) (food (veg ~shroom) ) ) )" ).rule();
+
+        assertEquals( "xxx",
+                      rule.getName() );
+
         AndDescr lhs = rule.getLhs();
         List lhsList = lhs.getDescrs();
-        assertEquals(2, lhsList.size());
-        
+        assertEquals( 2,
+                      lhsList.size() );
+
         // Parse the first column
-        ColumnDescr col = ( ColumnDescr ) lhsList.get( 0 );
-        assertEquals("?b", col.getIdentifier() );
-        assertEquals("person", col.getObjectType() );        
-        FieldConstraintDescr fieldConstraintDescr = ( FieldConstraintDescr ) col.getDescrs().get( 0 );
-        assertEquals("name", fieldConstraintDescr.getFieldName() );                //         
-        LiteralRestrictionDescr litDescr = ( LiteralRestrictionDescr ) fieldConstraintDescr.getRestrictions().get( 0 );
-        assertEquals("==", litDescr.getEvaluator() );
-        assertEquals("yyy", litDescr.getText() );        
-        
-        OrDescr orDescr = ( OrDescr ) lhsList.get( 1 );
-        assertEquals( 2, orDescr.getDescrs().size() );
-        
-        AndDescr andDescr = ( AndDescr ) orDescr.getDescrs().get( 0 );
-        col = ( ColumnDescr ) andDescr.getDescrs().get( 0 );
-        assertEquals("hobby1", col.getObjectType() );        
-        fieldConstraintDescr = ( FieldConstraintDescr ) col.getDescrs().get( 0 );
-        assertEquals("type", fieldConstraintDescr.getFieldName() );                //         
-        litDescr = ( LiteralRestrictionDescr ) fieldConstraintDescr.getRestrictions().get( 0 );
-        assertEquals("==", litDescr.getEvaluator() );
-        assertEquals("qqq1", litDescr.getText() );
-        
-        col = ( ColumnDescr ) andDescr.getDescrs().get( 1 );
-        assertEquals("hobby2", col.getObjectType() );        
-        fieldConstraintDescr = ( FieldConstraintDescr ) col.getDescrs().get( 0 );
-        assertEquals("type", fieldConstraintDescr.getFieldName() );                //         
-        litDescr = ( LiteralRestrictionDescr ) fieldConstraintDescr.getRestrictions().get( 0 );
-        assertEquals("!=", litDescr.getEvaluator() );
-        assertEquals("qqq2", litDescr.getText() );        
-        
-        
-        col = ( ColumnDescr ) orDescr.getDescrs().get( 1 );
-        assertEquals("food", col.getObjectType() );        
-        fieldConstraintDescr = ( FieldConstraintDescr ) col.getDescrs().get( 0 );
-        assertEquals("veg", fieldConstraintDescr.getFieldName() );                //         
-        litDescr = ( LiteralRestrictionDescr ) fieldConstraintDescr.getRestrictions().get( 0 );
-        assertEquals("!=", litDescr.getEvaluator() );
-        assertEquals("shroom", litDescr.getText() );       
+        ColumnDescr col = (ColumnDescr) lhsList.get( 0 );
+        assertEquals( "?b",
+                      col.getIdentifier() );
+        assertEquals( "person",
+                      col.getObjectType() );
+        FieldConstraintDescr fieldConstraintDescr = (FieldConstraintDescr) col.getDescrs().get( 0 );
+        assertEquals( "name",
+                      fieldConstraintDescr.getFieldName() ); //         
+        LiteralRestrictionDescr litDescr = (LiteralRestrictionDescr) fieldConstraintDescr.getRestrictions().get( 0 );
+        assertEquals( "==",
+                      litDescr.getEvaluator() );
+        assertEquals( "yyy",
+                      litDescr.getText() );
+
+        OrDescr orDescr = (OrDescr) lhsList.get( 1 );
+        assertEquals( 2,
+                      orDescr.getDescrs().size() );
+
+        AndDescr andDescr = (AndDescr) orDescr.getDescrs().get( 0 );
+        col = (ColumnDescr) andDescr.getDescrs().get( 0 );
+        assertEquals( "hobby1",
+                      col.getObjectType() );
+        fieldConstraintDescr = (FieldConstraintDescr) col.getDescrs().get( 0 );
+        assertEquals( "type",
+                      fieldConstraintDescr.getFieldName() ); //         
+        litDescr = (LiteralRestrictionDescr) fieldConstraintDescr.getRestrictions().get( 0 );
+        assertEquals( "==",
+                      litDescr.getEvaluator() );
+        assertEquals( "qqq1",
+                      litDescr.getText() );
+
+        col = (ColumnDescr) andDescr.getDescrs().get( 1 );
+        assertEquals( "hobby2",
+                      col.getObjectType() );
+        fieldConstraintDescr = (FieldConstraintDescr) col.getDescrs().get( 0 );
+        assertEquals( "type",
+                      fieldConstraintDescr.getFieldName() ); //         
+        litDescr = (LiteralRestrictionDescr) fieldConstraintDescr.getRestrictions().get( 0 );
+        assertEquals( "!=",
+                      litDescr.getEvaluator() );
+        assertEquals( "qqq2",
+                      litDescr.getText() );
+
+        col = (ColumnDescr) orDescr.getDescrs().get( 1 );
+        assertEquals( "food",
+                      col.getObjectType() );
+        fieldConstraintDescr = (FieldConstraintDescr) col.getDescrs().get( 0 );
+        assertEquals( "veg",
+                      fieldConstraintDescr.getFieldName() ); //         
+        litDescr = (LiteralRestrictionDescr) fieldConstraintDescr.getRestrictions().get( 0 );
+        assertEquals( "!=",
+                      litDescr.getEvaluator() );
+        assertEquals( "shroom",
+                      litDescr.getText() );
     }
-    
+
+    public void testNotExistsRule() throws Exception {
+        RuleDescr rule = parse( "(defrule (or (hobby1 (type qqq1)) (not (and (exists (person (name ppp))) (person (name yyy))))))" ).rule();
+
+        assertEquals( "xxx",
+                      rule.getName() );
+
+        AndDescr lhs = rule.getLhs();
+        List lhsList = lhs.getDescrs();
+        assertEquals( 1,
+                      lhsList.size() );
+
+        OrDescr orDescr = (OrDescr) lhsList.get( 1 );
+        assertEquals( 2,
+                      orDescr.getDescrs().size() );
+
+        ColumnDescr col = (ColumnDescr) orDescr.getDescrs().get( 0 );
+        assertEquals( "hobby1",
+                      col.getObjectType() );
+        FieldConstraintDescr fieldConstraintDescr = (FieldConstraintDescr) col.getDescrs().get( 0 );
+        assertEquals( "type",
+                      fieldConstraintDescr.getFieldName() ); //         
+        LiteralRestrictionDescr litDescr = (LiteralRestrictionDescr) fieldConstraintDescr.getRestrictions().get( 0 );
+        assertEquals( "==",
+                      litDescr.getEvaluator() );
+        assertEquals( "qqq1",
+                      litDescr.getText() );
+
+        NotDescr notDescr = (NotDescr) orDescr.getDescrs().get( 1 );
+        assertEquals( 1,
+                      notDescr.getDescrs().size() );
+        
+        AndDescr andDescr = (AndDescr) notDescr.getDescrs().get( 0 );
+        assertEquals( 2, andDescr.getDescrs().size() );
+        ExistsDescr existsDescr = (ExistsDescr) andDescr.getDescrs().get( 0 );
+        col = (ColumnDescr) existsDescr.getDescrs().get( 0 );
+        assertEquals( "person",
+                      col.getObjectType() );
+        fieldConstraintDescr = (FieldConstraintDescr) col.getDescrs().get( 0 );
+        assertEquals( "name",
+                      fieldConstraintDescr.getFieldName() ); //         
+        litDescr = (LiteralRestrictionDescr) fieldConstraintDescr.getRestrictions().get( 0 );
+        assertEquals( "==",
+                      litDescr.getEvaluator() );
+        assertEquals( "ppp",
+                      litDescr.getText() );      
+        
+        col = (ColumnDescr) andDescr.getDescrs().get( 0 );
+        assertEquals( "person",
+                      col.getObjectType() );
+        fieldConstraintDescr = (FieldConstraintDescr) col.getDescrs().get( 0 );
+        assertEquals( "name",
+                      fieldConstraintDescr.getFieldName() ); //         
+        litDescr = (LiteralRestrictionDescr) fieldConstraintDescr.getRestrictions().get( 0 );
+        assertEquals( "==",
+                      litDescr.getEvaluator() );
+        assertEquals( "yyy",
+                      litDescr.getText() );  
+    }
+
     private CLPParser parse(final String text) throws Exception {
         this.parser = newParser( newTokenStream( newLexer( newCharStream( text ) ) ) );
         return this.parser;
