e2e5652baf4bb93382abd3fc82a237e5d737eb83
Mark Proctor <mdproctor@gmail.com>
2007-03-22 15:15:45 +0000

    JBRULES-720 Clips Parser -Modify now works, using MVEL as it will handle coercion for us.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@10435 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/main/java/org/drools/clp/functions/ModifyFunction.java
+++ kogito-drools/drools-compiler/src/main/java/org/drools/clp/functions/ModifyFunction.java
@@ -1,49 +1,26 @@
 package org.drools.clp.functions;
 
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
-import java.beans.PropertyDescriptor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-
-import org.drools.clp.BaseFunction;
 import org.drools.clp.ExecutionContext;
 import org.drools.clp.Function;
 import org.drools.clp.SlotNameValuePair;
 import org.drools.clp.ValueHandler;
-import org.drools.clp.VariableValueHandler;
+import org.mvel.PropertyAccessor;
 
 public class ModifyFunction  implements Function {
     private static final String name = "modify"; 
+    
+    //private static final String modifyExpr
     
     public ModifyFunction() {
     }
 
 
     public Object execute(ValueHandler[] args, ExecutionContext context) {
-        // This is very slow, its just to get things working for now
-        Object object = args[0].getValue( context );
-        
-        try {
+        Object object = args[0].getValue( context );        
             for ( int i = 1, length = args.length; i < length; i++ ) {
-                SlotNameValuePair pair = ( SlotNameValuePair ) args[i];
-                Method method = getField( object.getClass(), pair.getName() ); 
-                method.invoke( object, new Object[] { pair.getValueHandler().getValue( context ) } );
-            }
-        } catch( Exception e ) {
-            e.printStackTrace();
-        }
-                
-        return null;
-    }
-    
-    public Method getField(Class clazz, String name) throws IntrospectionException {
-        PropertyDescriptor[] descrs = Introspector.getBeanInfo( clazz ).getPropertyDescriptors();
-        for ( int i = 0, length = descrs.length; i < length; i++ ) {
-            if ( descrs[i].getName().equals( name ) ) {
-                return descrs[i].getWriteMethod();
-            }
-        }
+                SlotNameValuePair pair = ( SlotNameValuePair ) args[i];                
+                PropertyAccessor.set(object, pair.getName(), pair.getValueHandler().getValue( context ) );
+            }                
         return null;
     }
     
