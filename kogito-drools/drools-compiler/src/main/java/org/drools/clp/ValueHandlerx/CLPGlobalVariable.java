14a4b0b8806bbc92c45f4643045e23d68df9f7da
Mark Proctor <mdproctor@gmail.com>
2007-03-24 19:58:29 +0000

    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@10521 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-compiler/src/main/java/org/drools/clp/ValueHandlerx/CLPGlobalVariable.java
@@ -0,0 +1,50 @@
+package org.drools.clp.ValueHandlers;
+
+import org.drools.clp.CLPFactory;
+import org.drools.clp.ExecutionContext;
+import org.drools.clp.VariableValueHandler;
+
+
+public class CLPGlobalVariable extends BaseValueHandler implements VariableValueHandler  {
+    
+    private String identifier;
+    private Class knownType;
+    private int simpleType;
+    private CLPFactory factory;
+       
+    public CLPGlobalVariable(String identifier,
+                                    Class knownType,
+                                    int simpleType,
+                                    CLPFactory factory ) {
+        this.identifier = identifier;
+        this.factory =  factory;
+        this.knownType = knownType;
+    }
+    
+    public String getIdentifier() {
+        return this.identifier;
+    }
+    
+    public int getValueType(ExecutionContext context) {
+        return this.simpleType;
+    }    
+
+    public Class getKnownType() {
+        return this.knownType;
+    }
+
+    public Object getValue(ExecutionContext context) {
+        return this.factory.getValue( this.identifier );
+    }
+
+    public void setValue(ExecutionContext context, Object value) {
+        throw new UnsupportedOperationException( "External Variable identifer='" + getIdentifier() + "' type='" + getKnownType() + "' is final, it cannot be set" );
+    }       
+    
+    public String toString() {
+        String name = getClass().getName();
+        name = name.substring( name.lastIndexOf( "." ) + 1 );
+        return "[" + name + " identifier = '" + getIdentifier()  + "']";
+    }        
+
+}
---
+++ kogito-drools/drools-compiler/src/main/java/org/drools/clp/ValueHandlerx/CLPPreviousDeclarationVariable.java
@@ -0,0 +1,102 @@
+package org.drools.clp.ValueHandlers;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import org.drools.clp.ExecutionContext;
+import org.drools.clp.VariableValueHandler;
+import org.drools.common.InternalFactHandle;
+import org.drools.rule.Declaration;
+
+public class CLPPreviousDeclarationVariable implements VariableValueHandler {
+
+    private Declaration declaration;
+    
+    public CLPPreviousDeclarationVariable(Declaration declaration) {
+        this.declaration = declaration;
+    }
+
+    public String getIdentifier() {
+        return this.declaration.getIdentifier();
+    }
+
+    public Class getKnownType() {
+        return declaration.getExtractor().getExtractToClass();
+    }
+    
+    public int getValueType(ExecutionContext context) {
+        return this.declaration.getValueType().getSimpleType(); 
+    }
+
+    public void setValue(ExecutionContext context,
+                         Object object) {
+        throw new UnsupportedOperationException( "External Variable identifer='" + getIdentifier() + "' type='" + getKnownType() + "' is final, it cannot be set" );
+    }
+    
+    public Object getValue(ExecutionContext context) {
+        InternalFactHandle handle = context.getTuple().get( this.declaration );
+        return declaration.getValue( handle.getObject() );
+    }
+    
+    public BigDecimal getBigDecimalValue(ExecutionContext context) throws NumberFormatException {
+        InternalFactHandle handle = context.getTuple().get( this.declaration );
+        Object object = declaration.getValue(  handle.getObject() );
+        if ( object instanceof BigDecimal ) {
+            return (BigDecimal) object;
+        } else {
+            return new BigDecimal( object.toString() );
+        }
+    }
+
+    public BigInteger getBigIntegerValue(ExecutionContext context) throws NumberFormatException {
+        InternalFactHandle handle = context.getTuple().get( this.declaration );
+        Object object = declaration.getValue(  handle.getObject() );
+        if ( object instanceof BigInteger ) {
+            return (BigInteger) object;
+        } else {
+            return new BigInteger( object.toString() );
+        }
+    }    
+
+    public boolean getBooleanValue(ExecutionContext context) throws ClassCastException {
+        InternalFactHandle handle = context.getTuple().get( this.declaration );
+        return declaration.getBooleanValue( handle.getObject() );
+    }
+
+    public double getDoubleValue(ExecutionContext context) throws NumberFormatException {
+        InternalFactHandle handle = context.getTuple().get( this.declaration );
+        return declaration.getDoubleValue( handle.getObject() );
+    }
+
+    public float getFloatValue(ExecutionContext context) throws NumberFormatException {
+        InternalFactHandle handle = context.getTuple().get( this.declaration );
+        return declaration.getFloatValue( handle.getObject() );
+    }
+
+    public int getIntValue(ExecutionContext context) throws NumberFormatException {
+        InternalFactHandle handle = context.getTuple().get( this.declaration );
+        return declaration.getIntValue( handle.getObject() );
+    }
+
+    public long getLongValue(ExecutionContext context) throws NumberFormatException {
+        InternalFactHandle handle = context.getTuple().get( this.declaration );
+        return declaration.getLongValue( handle.getObject() );
+    }
+
+    public short getShortValue(ExecutionContext context) throws NumberFormatException {
+        InternalFactHandle handle = context.getTuple().get( this.declaration );
+        return declaration.getShortValue( handle.getObject() );
+    }
+
+    public String getStringValue(ExecutionContext context) {
+        InternalFactHandle handle = context.getTuple().get( this.declaration );
+        return (String) declaration.getValue( handle.getObject() );
+    }
+    
+    public String toString() {
+        String name = getClass().getName();
+        name = name.substring( name.lastIndexOf( "." ) + 1 );
+        return "[" + name + " identifier = '" + getIdentifier()  + "']";
+    }    
+        
+}
