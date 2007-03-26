b438a5f38b444517f0048ab4c400733c237ebf2f
Mark Proctor <mdproctor@gmail.com>
2007-03-26 22:26:33 +0000

    JBRULES-720 Clips Parser -Added a SimpleValueType
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@10543 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-core/src/main/java/org/drools/base/SimpleValueType.java
@@ -0,0 +1,16 @@
+package org.drools.base;
+
+public class SimpleValueType {
+    public static final int UNKNOWN  = 0;
+    public static final int NULL     = 1;
+    public static final int BOOLEAN  = 2;
+    public static final int NUMBER   = 3;
+    public static final int INTEGER  = 4;
+    public static final int DECIMAL  = 5;
+    public static final int CHAR     = 6;
+    public static final int STRING   = 7;
+    public static final int DATE     = 8;
+    public static final int LIST     = 9;
+    public static final int OBJECT   = 10;
+    public static final int FUNCTION = 11; //This one is for LISP
+}
