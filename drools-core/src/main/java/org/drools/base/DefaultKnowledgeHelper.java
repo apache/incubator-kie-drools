3ba1f78427542db2d0249fe6a4f0f2cf5872a99b
Mark Proctor <mdproctor@gmail.com>
2007-04-10 02:47:37 +0000

    JBRULES-778 column fact handle used in consequences even for field bindings -now uses eager rather than lazy facthandle retrieval
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@10843 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/main/java/org/drools/base/DefaultKnowledgeHelper.java
+++ drools-core/src/main/java/org/drools/base/DefaultKnowledgeHelper.java
@@ -92,19 +92,11 @@ public class DefaultKnowledgeHelper
 
     public void modifyObject(FactHandle handle,
                              final Object newObject) throws FactException {
-    	if ( handle == null ) {
-    		// no fact handle exists for this object, so it must be a bound field
-    		// check if that bound field is still a fact in the WM
-    		handle = this.workingMemory.getFactHandle( newObject );
-    	}
-    	
-    	if (  handle != null ) {
-    		// only modify if this fact exists in the wm
-	        this.workingMemory.modifyObject( handle,
-	                                         newObject,
-	                                         this.rule,
-	                                         this.activation );
-    	}
+        // only modify if this fact exists in the wm
+        this.workingMemory.modifyObject( handle,
+                                         newObject,
+                                         this.rule,
+                                         this.activation );
     }
 
     public void retractObject(final FactHandle handle) throws FactException {
