7d1a3af73fb4e302c5dca2747fe9da0fa7043788
Michael Neale <michael.neale@gmail.com>
2007-04-30 12:54:51 +0000

    JBRULES-773 BRMS assembler
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@11506 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-compiler/src/test/java/org/drools/brms/server/rules/SuggestionCompletionLoaderTest.java
@@ -0,0 +1,40 @@
+package org.drools.brms.server.rules;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.drools.Person;
+import org.drools.brms.client.modeldriven.SuggestionCompletionEngine;
+
+import junit.framework.TestCase;
+
+public class SuggestionCompletionLoaderTest extends TestCase {
+
+    public void testSuggestionCompLoader() throws Exception {
+        SuggestionCompletionLoader loader = new SuggestionCompletionLoader();
+        SuggestionCompletionEngine eng = loader.getSuggestionEngine( "package foo \n import org.drools.Person", new ArrayList(), new ArrayList() );
+        assertNotNull(eng);
+        
+    }
+    
+    public void testLoaderWithExistingClassloader() throws Exception {
+        MockClassLoader mcl = new MockClassLoader();
+        SuggestionCompletionLoader loader = new SuggestionCompletionLoader(mcl);
+        SuggestionCompletionEngine eng = loader.getSuggestionEngine( "package foo \n import org.foo.Bar", new ArrayList(), new ArrayList() );
+        assertNotNull(eng);
+        assertTrue(mcl.called);
+        
+    }
+    
+    static class MockClassLoader extends ClassLoader {
+        
+        public boolean called = false;
+        
+        public Class loadClass(String name) {
+            called = true;
+            return Object.class;
+        }
+        
+    }
+    
+}
