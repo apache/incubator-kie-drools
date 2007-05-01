8f44ff1d77a9a380ba9eb3987319e09b082ee100
Steven Williams <stevearoonie@gmail.com>
2007-05-01 13:06:39 +0000

    fix up tests to work with MVEL
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@11554 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-decisiontables/src/test/java/org/drools/decisiontable/ExternalSpreadsheetCompilerUnitTest.java
+++ drools-decisiontables/src/test/java/org/drools/decisiontable/ExternalSpreadsheetCompilerUnitTest.java
@@ -29,8 +29,7 @@ import junit.framework.TestCase;
  *         itself is correct).
  */
 public class ExternalSpreadsheetCompilerUnitTest extends TestCase {
-//  @FIXME    
-	public void FIXME_testLoadFromClassPath() {
+	public void testLoadFromClassPath() {
 		final ExternalSpreadsheetCompiler converter = new ExternalSpreadsheetCompiler();
 		final String drl = converter.compile("/data/MultiSheetDST.xls",
 				"/templates/test_template1.drl", 11, 2);
@@ -44,17 +43,15 @@ public class ExternalSpreadsheetCompilerUnitTest extends TestCase {
 		assertTrue(drl.indexOf("import example.model.Car;") > -1);
 	}
 
-//@FIXME    
-	public void FIXME_testLoadSpecificWorksheet() {
+	public void testLoadSpecificWorksheet() {
 		final ExternalSpreadsheetCompiler converter = new ExternalSpreadsheetCompiler();
 		final String drl = converter.compile("/data/MultiSheetDST.xls",
-				"Another Sheet", "/templates/test_template1.drl", 21, 2);
+				"Another Sheet", "/templates/test_template1.drl", 11, 2);
 //		System.out.println(drl);
 		assertNotNull(drl);
 	}
 
- //@FIXME
-	public void FIXME_testLoadCsv() {
+	public void testLoadCsv() {
 		final ExternalSpreadsheetCompiler converter = new ExternalSpreadsheetCompiler();
 		final String drl = converter.compile("/data/ComplexWorkbook.csv",
 				"/templates/test_template2.drl", InputType.CSV, 10, 2);
