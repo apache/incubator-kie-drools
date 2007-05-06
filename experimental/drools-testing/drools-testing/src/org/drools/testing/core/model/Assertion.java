64d7c3ff29e2a497a43f16cd05e06b9fd014b1d2
Matthew Shaw <mshaw@emergency.qld.gov.au>
2007-05-06 01:11:13 +0000

    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@11703 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- experimental/drools-testing/drools-testing/src/org/drools/testing/core/model/Assertion.java
+++ experimental/drools-testing/drools-testing/src/org/drools/testing/core/model/Assertion.java
@@ -2,4 +2,19 @@ package org.drools.testing.core.model;
 
 public class Assertion {
 
+	private String name;
+	private boolean success;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isSuccess() {
+		return success;
+	}
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
 }
--- experimental/drools-testing/drools-testing/src/org/drools/testing/core/model/Scenario.java
+++ experimental/drools-testing/drools-testing/src/org/drools/testing/core/model/Scenario.java
@@ -4,4 +4,17 @@ public class Scenario {
 
 	public Fact[] facts;
 	public Outcome[] outcomes;
+	
+	public Fact[] getFacts() {
+		return facts;
+	}
+	public void setFacts(Fact[] facts) {
+		this.facts = facts;
+	}
+	public Outcome[] getOutcomes() {
+		return outcomes;
+	}
+	public void setOutcomes(Outcome[] outcomes) {
+		this.outcomes = outcomes;
+	}
 }
