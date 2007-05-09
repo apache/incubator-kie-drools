3e4e65b43ef1a267a02e677c8a49de2c98719d6c
Matthew Shaw <mshaw@emergency.qld.gov.au>
2007-05-09 08:14:53 +0000

    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@11780 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/experimental/drools-testing/src/org/drools/testing/core/model/Outcome.java
@@ -0,0 +1,20 @@
+package org.drools.testing.core.model;
+
+public class Outcome {
+
+	public Rule[] rules;
+	public Assertion[] assertions;
+	
+	public Assertion[] getAssertions() {
+		return assertions;
+	}
+	public void setAssertions(Assertion[] assertions) {
+		this.assertions = assertions;
+	}
+	public Rule[] getRules() {
+		return rules;
+	}
+	public void setRules(Rule[] rules) {
+		this.rules = rules;
+	}
+}
