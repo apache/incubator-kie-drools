f1e51e6e87d8eee05a6dec2fb24c8881feaa79a9
Matthew Shaw <mshaw@emergency.qld.gov.au>
2007-05-12 08:04:20 +0000

    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@11837 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ experimental/drools-testing/src/org/drools/testing/core/engine/TestRunner.java
@@ -0,0 +1,13 @@
+package org.drools.testing.core.engine;
+
+/**
+ * 
+ * @author Matt
+ *
+ * This is the core API class which takes a TestSuite object and processes
+ * the scenarios via the Jboss rules engine.
+ */
+public class TestRunner {
+
+	
+}
