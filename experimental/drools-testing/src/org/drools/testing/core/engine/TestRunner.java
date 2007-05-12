eae20dcfce01bce229f29c2b598fba73365f3469
Matthew Shaw <mshaw@emergency.qld.gov.au>
2007-05-12 08:12:41 +0000

    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@11838 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- experimental/drools-testing/src/org/drools/testing/core/engine/TestRunner.java
+++ experimental/drools-testing/src/org/drools/testing/core/engine/TestRunner.java
@@ -1,13 +1,36 @@
 package org.drools.testing.core.engine;
 
+import org.drools.testing.core.beans.TestSuite;
+import org.drools.testing.core.exception.RuleTestLanguageException;
+
 /**
  * 
  * @author Matt
  *
  * This is the core API class which takes a TestSuite object and processes
  * the scenarios via the Jboss rules engine.
+ * 
+ * This class relies on the underlying testing model and will return
+ * results reflected in the model by the test scenarios.
  */
 public class TestRunner {
 
+	private TestSuite testSuite;
 	
+	public TestRunner (TestSuite otherValue) {
+		this.testSuite = otherValue;
+	}
+	
+	/**
+	 * The run method is invoked by the client and returns true if test
+	 * was run successfully and false otherwise.
+	 * 
+	 * @return boolean
+	 * @throws RuleTestLanguageException
+	 */
+	public boolean run () throws RuleTestLanguageException {
+		
+		
+		return true;
+	}
 }
