92adfa78a868f8718b69e7aa0c7dab75f62d3c27
Matthew Shaw <mshaw@emergency.qld.gov.au>
2007-05-16 11:15:54 +0000

    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@11881 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/experimental/drools-testing/src/org/drools/testing/core/engine/TestRunner.java
+++ kogito-drools/experimental/drools-testing/src/org/drools/testing/core/engine/TestRunner.java
@@ -12,6 +12,8 @@ import org.drools.testing.core.exception.RuleTestLanguageException;
  * 
  * This class relies on the underlying testing model and will return
  * results reflected in the model by the test scenarios.
+ * 
+ * (c) Matt Shaw
  */
 public class TestRunner {
 
@@ -23,12 +25,13 @@ public class TestRunner {
 	
 	/**
 	 * The run method is invoked by the client and returns true if test
-	 * was run successfully and false otherwise.
+	 * was successfull and false otherwise.
 	 * 
 	 * @return boolean
 	 * @throws RuleTestLanguageException
 	 */
 	public boolean run () throws RuleTestLanguageException {
+		
 		
 		
 		return true;
