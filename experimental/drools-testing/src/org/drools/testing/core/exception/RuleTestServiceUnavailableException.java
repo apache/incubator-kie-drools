8c22038b47657e0e9cfe69cf5cb3edeabfe226e3
Matthew Shaw <mshaw@emergency.qld.gov.au>
2007-05-16 11:36:26 +0000

    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@11884 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ experimental/drools-testing/src/org/drools/testing/core/exception/RuleTestServiceUnavailableException.java
@@ -0,0 +1,22 @@
+package org.drools.testing.core.exception;
+
+public class RuleTestServiceUnavailableException extends RuleTestLanguageException {
+
+public RuleTestServiceUnavailableException () {
+		
+		super();
+	
+	}
+	
+	public RuleTestServiceUnavailableException (String message) {
+	
+		super(message);
+	
+	}
+	
+	public RuleTestServiceUnavailableException (String message, Throwable cause) {
+	
+		super(message,cause);
+	
+	}
+}
