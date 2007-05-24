34f81f1bb91f127604b27c4e395feb0efcc0d3a3
Mark Proctor <mdproctor@gmail.com>
2007-05-24 17:08:30 +0000

    JBRULES-713 Make Dialects Pluggeable -Fixed failing unit test
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@12125 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/test/java/org/drools/rule/builder/dialect/mvel/InstrumentedDeclarationScopeResolver.java
+++ kogito-drools/drools-compiler/src/test/java/org/drools/rule/builder/dialect/mvel/InstrumentedDeclarationScopeResolver.java
@@ -5,6 +5,7 @@ package org.drools.rule.builder.dialect.mvel;
 
 import java.util.Map;
 
+import org.drools.rule.Declaration;
 import org.drools.spi.DeclarationScopeResolver;
 
 public class InstrumentedDeclarationScopeResolver extends DeclarationScopeResolver {
@@ -20,5 +21,9 @@ public class InstrumentedDeclarationScopeResolver extends DeclarationScopeResolv
 
     public Map getDeclarations() {
         return this.declarations;
+    }
+    
+    public Declaration getDeclaration(final String name) {
+        return ( Declaration ) this.declarations.get( name );
     }
 }
