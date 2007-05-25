c8cf16252aa1421cf2540120a442a43d74146486
Michael Neale <michael.neale@gmail.com>
2007-05-25 00:47:44 +0000

    JBRULES-873 optional ; delimiters
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@12131 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-compiler/src/main/java/org/drools/rule/builder/dialect/mvel/MVELConsequenceBuilder.java
+++ drools-compiler/src/main/java/org/drools/rule/builder/dialect/mvel/MVELConsequenceBuilder.java
@@ -1,15 +1,13 @@
 package org.drools.rule.builder.dialect.mvel;
 
 import java.io.Serializable;
-import java.util.HashMap;
 
 import org.drools.base.mvel.DroolsMVELFactory;
 import org.drools.base.mvel.MVELConsequence;
 import org.drools.lang.descr.RuleDescr;
-import org.drools.rule.builder.RuleBuildContext;
 import org.drools.rule.builder.ConsequenceBuilder;
+import org.drools.rule.builder.RuleBuildContext;
 import org.mvel.MVEL;
-import org.mvel.integration.impl.MapVariableResolverFactory;
 
 public class MVELConsequenceBuilder
     implements
@@ -21,10 +19,66 @@ public class MVELConsequenceBuilder
 
         final DroolsMVELFactory factory = new DroolsMVELFactory(context.getDeclarationResolver().getDeclarations(), null,  context.getPkg().getGlobals() );
 
-        final Serializable expr = MVEL.compileExpression( (String) context.getRuleDescr().getConsequence() );
+        final Serializable expr = MVEL.compileExpression( delimitExpressions( (String) context.getRuleDescr().getConsequence()) );
+        //final Serializable expr = MVEL.compileExpression( (String) ruleDescr.getConsequence() );
 
         context.getRule().setConsequence( new MVELConsequence( expr,
                                                                factory ) );
+    }
+    
+    /**
+     * Allows newlines to demarcate expressions, as per MVEL command line.
+     * If expression spans multiple lines (ie inside an unbalanced bracket) then
+     * it is left alone.
+     * Uses character based iteration which is at least an order of magnitude faster then a single 
+     * simple regex.
+     */
+    public String delimitExpressions(String s) {
+
+        StringBuffer result = new StringBuffer();
+        char[] cs = s.toCharArray();
+        int brace = 0;
+        int sqre = 0;
+        int crly = 0;
+        char lastNonWhite = ' ';
+        for ( int i = 0; i < cs.length; i++ ) {
+            char c = cs[i];
+            switch ( c ) {
+                case '(' :
+                    brace++;
+                    break;
+                case '{' :
+                    crly++;
+                    break;
+                case '[' :
+                    sqre++;
+                    break;
+                case ')' :
+                    brace--;
+                    break;
+                case '}' :
+                    crly--;
+                    break;
+                case ']' :
+                    sqre--;
+                    break;                    
+                default :
+                    break;
+            }   
+            if ((brace == 0 && sqre == 0 && crly == 0) &&
+                    (c == '\n' || c == '\r')) {
+                if (lastNonWhite != ';') {
+                    result.append( ';' );
+                    lastNonWhite = ';';
+                }
+            } else if  (!Character.isWhitespace( c )) {
+                lastNonWhite = c;
+            }
+            result.append( c );
+            
+
+        }
+        return result.toString();
     }
 
 }
