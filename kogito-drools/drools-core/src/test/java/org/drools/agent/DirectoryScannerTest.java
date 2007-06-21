54c998ebdd0303609742b49596403bf09d1d6a06
Michael Neale <michael.neale@gmail.com>
2007-06-21 07:45:43 +0000

    JBRULES-752
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@12735 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-core/src/test/java/org/drools/agent/DirectoryScannerTest.java
@@ -0,0 +1,48 @@
+package org.drools.agent;
+
+import java.io.File;
+import java.util.Properties;
+
+import org.drools.RuleBase;
+import org.drools.RuleBaseFactory;
+import org.drools.rule.Package;
+
+import junit.framework.TestCase;
+
+
+
+public class DirectoryScannerTest extends TestCase {
+
+    public void testScan() throws Exception {
+        File dir = RuleBaseAssemblerTest.getTempDirectory();
+        
+        Package p1 = new Package("p1");
+        Package p2 = new Package("p2");
+        File p1f = new File(dir, "p1.pkg");
+        File p2f = new File(dir, "p2.pkg");
+        
+        RuleBaseAssemblerTest.writePackage( p1, p1f );
+        RuleBaseAssemblerTest.writePackage( p2, p2f);
+        
+        DirectoryScanner scan = new DirectoryScanner();
+        Properties props = new Properties();
+        props.setProperty( RuleBaseAgent.DIRECTORY, dir.getPath() );
+                
+        scan.configure( props );
+        
+        RuleBase rb = RuleBaseFactory.newRuleBase();
+        scan.updateRuleBase( rb, true );
+        assertEquals(2, rb.getPackages().length);
+                
+        Package p3 = new Package("p3");
+        File p3f = new File(dir, "p3.pkg");
+        
+        RuleBaseAssemblerTest.writePackage( p3, p3f );
+        
+        scan.updateRuleBase( rb, true );
+        assertEquals(3, rb.getPackages().length);        
+    }
+    
+    
+    
+}
