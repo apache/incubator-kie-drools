cf054df0c7e1f624ef59eb9e803a0af95b79a5ee
Edson Tirelli <ed.tirelli@gmail.com>
2007-06-22 01:19:45 +0000

    JBRULES-941: adding support to pluggable accumulate functions
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@12765 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-core/src/main/java/org/drools/base/accumulators/AccumulateFunction.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2007 JBoss Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created on Jun 20, 2007
+ */
+package org.drools.base.accumulators;
+
+/**
+ * An interface for accumulate external function implementations
+ * 
+ * @author etirelli
+ *
+ */
+public interface AccumulateFunction {
+
+    /**
+     * Creates and returns a new context object
+     * @return
+     */
+    public Object createContext();
+
+    /**
+     * Initializes the accumulator
+     * @param context
+     * @throws Exception
+     */
+    public void init(Object context) throws Exception;
+
+    /**
+     * Executes the accumulation action
+     * @param context
+     * @param value
+     */
+    public void accumulate(Object context,
+                           Object value);
+
+    /**
+     * Reverses the accumulation action
+     * @param context
+     * @param value
+     * @throws Exception
+     */
+    public void reverse(Object context,
+                        Object value) throws Exception;
+
+    /**
+     * Returns the current value in this accumulation session
+     * 
+     * @param context
+     * @return
+     * @throws Exception
+     */
+    public Object getResult(Object context) throws Exception;
+
+    /**
+     * True if the function supports reverse. False otherwise.
+     * 
+     * @return
+     */
+    public boolean supportsReverse();
+
+}
---
+++ drools-core/src/main/java/org/drools/base/accumulators/JavaAccumulatorFunctionExecutor.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2007 JBoss Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created on Jun 20, 2007
+ */
+package org.drools.base.accumulators;
+
+import org.drools.WorkingMemory;
+import org.drools.common.InternalFactHandle;
+import org.drools.rule.Declaration;
+import org.drools.spi.Accumulator;
+import org.drools.spi.ReturnValueExpression;
+import org.drools.spi.Tuple;
+
+/**
+ * An MVEL accumulator function executor implementation
+ * 
+ * @author etirelli
+ */
+public class JavaAccumulatorFunctionExecutor
+    implements
+    Accumulator {
+
+    private static final long           serialVersionUID = 1081306132058101176L;
+
+    private ReturnValueExpression expression;
+    private final AccumulateFunction   function;
+
+    public JavaAccumulatorFunctionExecutor(final AccumulateFunction function) {
+        super();
+        this.function = function;
+    }
+
+    /* (non-Javadoc)
+     * @see org.drools.spi.Accumulator#createContext()
+     */
+    public Object createContext() {
+        return this.function.createContext();
+    }
+
+    /* (non-Javadoc)
+     * @see org.drools.spi.Accumulator#init(java.lang.Object, org.drools.spi.Tuple, org.drools.rule.Declaration[], org.drools.WorkingMemory)
+     */
+    public void init(Object context,
+                     Tuple leftTuple,
+                     Declaration[] declarations,
+                     WorkingMemory workingMemory) throws Exception {
+        this.function.init( context );
+    }
+
+    /* (non-Javadoc)
+     * @see org.drools.spi.Accumulator#accumulate(java.lang.Object, org.drools.spi.Tuple, org.drools.common.InternalFactHandle, org.drools.rule.Declaration[], org.drools.rule.Declaration[], org.drools.WorkingMemory)
+     */
+    public void accumulate(Object context,
+                           Tuple leftTuple,
+                           InternalFactHandle handle,
+                           Declaration[] declarations,
+                           Declaration[] innerDeclarations,
+                           WorkingMemory workingMemory) throws Exception {
+        final Object value = this.expression.evaluate( handle.getObject(),
+                                                       leftTuple,
+                                                       declarations,
+                                                       innerDeclarations,
+                                                       workingMemory ).getValue();
+        this.function.accumulate( context,
+                                  value );
+    }
+
+    public void reverse(Object context,
+                        Tuple leftTuple,
+                        InternalFactHandle handle,
+                        Declaration[] declarations,
+                        Declaration[] innerDeclarations,
+                        WorkingMemory workingMemory) throws Exception {
+        final Object value = this.expression.evaluate( handle.getObject(),
+                                                       leftTuple,
+                                                       declarations,
+                                                       innerDeclarations,
+                                                       workingMemory ).getValue();
+        this.function.reverse( context,
+                               value );
+    }
+
+    /* (non-Javadoc)
+     * @see org.drools.spi.Accumulator#getResult(java.lang.Object, org.drools.spi.Tuple, org.drools.rule.Declaration[], org.drools.WorkingMemory)
+     */
+    public Object getResult(Object context,
+                            Tuple leftTuple,
+                            Declaration[] declarations,
+                            WorkingMemory workingMemory) throws Exception {
+        return this.function.getResult( context );
+    }
+
+    public boolean supportsReverse() {
+        return this.function.supportsReverse();
+    }
+
+    public ReturnValueExpression getExpression() {
+        return expression;
+    }
+
+    public void setExpression(ReturnValueExpression expression) {
+        this.expression = expression;
+    }
+
+}
