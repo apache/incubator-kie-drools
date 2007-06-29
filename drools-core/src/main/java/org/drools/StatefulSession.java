dfe08b28b781c1e38326c0d4d9b6519bcacf1ee7
Michael Neale <michael.neale@gmail.com>
2007-06-29 00:35:39 +0000

    added some missing javadocs
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@12943 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/main/java/org/drools/StatefulSession.java
+++ drools-core/src/main/java/org/drools/StatefulSession.java
@@ -5,6 +5,12 @@ import java.util.List;
 import org.drools.concurrent.Future;
 import org.drools.spi.AgendaFilter;
 
+/**
+ * A stateful session represents a working memory which keeps state 
+ * between invocations (accumulating facts/knowledge).
+ * 
+ * Caution should be used when using the async methods (take not of the javadocs for specific methods). 
+ */
 public interface StatefulSession extends WorkingMemory {
     
     /**
@@ -12,15 +18,51 @@ public interface StatefulSession extends WorkingMemory {
      * 
      */
     void dispose();    
-    
+
+    /**
+     * Insert/Assert an object asynchronously.
+     * (return immediately, even while the insertion is taking effect).
+     * The returned Future object can be queried to check on the status of the task.
+     * You should only use the async methods if you are sure you require a background 
+     * insertion task to take effect (a new thread may be created). 
+     * If you are not sure, then you probably don't need to use it !
+     */
     Future asyncInsert(Object object);    
     Future asyncRetract(FactHandle factHandle);   
     Future asyncUpdate(FactHandle factHandle, Object object);
 
+    /**
+     * Insert/Assert an array of objects..
+     * (return immediately, even while the insertion is taking effect).
+     * The returned Future object can be queried to check on the status of the task.
+     * You should only use the async methods if you are sure you require a background 
+     * insertion task to take effect (a new thread may be created). 
+     * If you are not sure, then you probably don't need to use it !
+     */    
     Future asyncInsert(Object[] list);
+    
+    /**
+     * Insert/Assert a list of objects..
+     * (return immediately, even while the insertion is taking effect).
+     * The returned Future object can be queried to check on the status of the task.
+     * You should only use the async methods if you are sure you require a background 
+     * insertion task to take effect (a new thread may be created). 
+     * If you are not sure, then you probably don't need to use it !
+     */        
     Future asyncInsert(List list);
     
+    /**
+     * This will initiate the firing phase (in the background). 
+     * And return immediately. The returned Future object can be queried
+     * to check on the status of the task.
+     */
     Future asyncFireAllRules();
+    
+    /**
+     * This will initiate the firing phase (in the background). 
+     * And return immediately. The returned Future object can be queried
+     * to check on the status of the task.
+     */    
     Future asyncFireAllRules(AgendaFilter agendaFilter); 
 
 }
--- drools-core/src/main/java/org/drools/StatelessSession.java
+++ drools-core/src/main/java/org/drools/StatelessSession.java
@@ -6,6 +6,16 @@ import org.drools.event.AgendaEventListener;
 import org.drools.spi.AgendaFilter;
 import org.drools.spi.GlobalResolver;
 
+/**
+ * This represents a working memory session where state is not kept between
+ * invocations.
+ * This is typically used for "decision services" where the rules are
+ * provided all the data in one hit, and a conclusion reached by the engine.
+ * (there is no accumulation of facts/knowledge - each invocation is on a fresh session).
+ * 
+ * Care should be used when using the async versions of the methods, consult the javadoc for 
+ * the specific information.
+ */
 public interface StatelessSession {
     /**
      * Returns all event listeners.
@@ -41,22 +51,59 @@ public interface StatelessSession {
     
     void setGlobal(String identifer, Object value);        
     
-    
+    /**
+     * Insert a single fact, an fire the rules, returning when finished.
+     */
     void execute(Object object);
-   
+
+    /**
+     * Insert an array of facts, an fire the rules, returning when finished.
+     * This will assert the list of facts as SEPARATE facts to the engine
+     * (NOT an array).
+     */    
     void execute(Object[] list);
-    
+
+    /**
+     * Insert a List of facts, an fire the rules, returning when finished.
+     * This will assert the list of facts as SEPARATE facts to the engine
+     * (NOT as a List).
+     */        
     void execute(List list);    
     
+    /**
+     * This will assert the object in the background. This is
+     * "send and forget" execution.
+     */
     void asyncExecute(Object object);
-    
+
+    /**
+     * This will assert the object array (as SEPARATE facts) in the background. This is
+     * "send and forget" execution.
+     */
     void asyncExecute(Object[] list);
-    
+
+    /**
+     * This will assert the object List (as SEPARATE facts) in the background. This is
+     * "send and forget" execution.
+     */    
     void asyncExecute(List list);      
     
-    StatelessSessionResult executeWithResults(Object object);
-   
-    StatelessSessionResult executeWithResults(Object[] list);
     
+    /**
+     * Similar to the normal execute method, but this will return
+     * "results". 
+     */
+    StatelessSessionResult executeWithResults(Object object);
+
+    /**
+     * Similar to the normal execute method, but this will return
+     * "results". 
+     */    
+    StatelessSessionResult executeWithResults(Object[] list);
+
+    /**
+     * Similar to the normal execute method, but this will return
+     * "results". 
+     */   
     StatelessSessionResult executeWithResults(List list);
 }
