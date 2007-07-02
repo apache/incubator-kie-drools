2aa505ccc6e92808259aaae007eefc805ba09121
Michael Neale <michael.neale@gmail.com>
2007-07-02 07:56:44 +0000

    bevvy of small fixes
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@12997 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-compiler/src/test/java/org/drools/Address.java
@@ -0,0 +1,20 @@
+package org.drools;
+
+public class Address {
+
+    private String street;
+    private String suburb;
+    public String getStreet() {
+        return street;
+    }
+    public void setStreet(String street) {
+        this.street = street;
+    }
+    public String getSuburb() {
+        return suburb;
+    }
+    public void setSuburb(String suburb) {
+        this.suburb = suburb;
+    }
+    
+}
