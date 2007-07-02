3e76311d5fb1e3fd03c5e7082f4c9f357e1d9493
Michael Neale <michael.neale@gmail.com>
2007-07-02 02:35:24 +0000

    debugging sequential
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@12988 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-compiler/src/test/java/org/drools/integrationtests/sequential/rule_generator.rb
@@ -0,0 +1,34 @@
+OUTPUT="rules.drl"
+
+if File.exists? OUTPUT then File.delete(OUTPUT) end
+
+f = File.new(OUTPUT, "w")
+
+
+for i in 1..200
+
+	s =  "
+	
+	rule \"Cheese_#{i}\"	
+		when
+			$c : Cheese( price == #{i} )
+		then 
+			list.add( $c.getType() );
+	end
+	
+	rule \"Person and cheese_#{i}\"
+		when 
+			$p : Person(name == \"p#{i}\")
+			$c : Cheese(price == 1)
+		then
+			list.add($p.getName());
+		
+	end
+	"
+	f.write s
+
+
+
+end
+
+f.close
