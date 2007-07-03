b30ae7afa795b3f681ea3838cb41e7630ae7ad80
Michael Neale <michael.neale@gmail.com>
2007-07-03 05:11:18 +0000

    added some more profiling tests around MVEL
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@13030 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/test/java/org/drools/compiler/CompilerPerfProfileTest.java
+++ kogito-drools/drools-compiler/src/test/java/org/drools/compiler/CompilerPerfProfileTest.java
@@ -9,20 +9,24 @@ import junit.framework.TestCase;
 
 public class CompilerPerfProfileTest extends TestCase {
 
-    public void testJDT() throws Exception {
+    public void testProfileRuns() throws Exception {
 
-        //first run
-        //build("JDT", "largeRuleNumber.drl", false);
+        //first run for warm up
+        build("JDT", "largeRuleNumber.drl", false);
         build("MVEL", "largeRuleNumberMVEL.drl", false);
 
-//        System.gc();
-//        Thread.sleep( 100 );
-//        
-//        build("JDT", "largeRuleNumber.drl", true);
-//        System.gc();
-//        Thread.sleep( 100 );
-//        
-//        build("MVEL", "largeRuleNumberMVEL.drl", true);
+        System.gc();
+        Thread.sleep( 100 );
+        
+        build("MVEL", "largeRuleNumberMVEL.drl", true);        
+
+        System.gc();
+        Thread.sleep( 100 );
+
+        
+        build("JDT", "largeRuleNumber.drl", true);
+        
+
         
         
         
