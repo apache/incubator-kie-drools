b88dbbacaa4b183e3e5c47f6dbe77056f3ddf2fd
Mark Proctor <mdproctor@gmail.com>
2007-07-07 23:51:29 +0000

    -added mind map
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@13227 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ documentation/drools.mm
@@ -0,0 +1,71 @@
+<map version="0.9.0_Beta_8">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1183832698515" ID="Freemind_Link_1073969978" MODIFIED="1183832711296" TEXT="Drools">
+<node CREATED="1183832746093" ID="Freemind_Link_669795590" MODIFIED="1183832767093" POSITION="left" TEXT="Language">
+<node CREATED="1183832770687" ID="Freemind_Link_517709211" MODIFIED="1183832804109" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    <style type="text/css">
+      <!--
+        p { margin-top: 0 }
+      -->
+    </style>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Conditional
+    </p>
+    <p style="text-align: center">
+      Elements
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1183832825125" ID="Freemind_Link_760771781" MODIFIED="1183832828093" STYLE="fork" TEXT="Pattern">
+<node CREATED="1183832830140" ID="Freemind_Link_747179825" MODIFIED="1183832834125" TEXT="Field Constraints"/>
+</node>
+<node CREATED="1183832836437" ID="Freemind_Link_1380544593" MODIFIED="1183832839562" STYLE="fork" TEXT="&apos;or&apos;"/>
+<node CREATED="1183832854984" ID="Freemind_Link_133725732" MODIFIED="1183832860531" STYLE="fork" TEXT="&apos;and&apos;"/>
+<node CREATED="1183832996875" ID="Freemind_Link_1676215401" MODIFIED="1183833000875" STYLE="fork" TEXT="&apos;not&apos;"/>
+<node CREATED="1183833016812" ID="Freemind_Link_302051494" MODIFIED="1183833019546" TEXT="&apos;exists&apos;"/>
+<node CREATED="1183833024546" ID="Freemind_Link_255517917" MODIFIED="1183833028265" TEXT="&apos;forall&apos;"/>
+</node>
+</node>
+<node CREATED="1183832933781" HGAP="-22" ID="Freemind_Link_1144464740" MODIFIED="1183833186500" POSITION="right" TEXT="Event Model" VSHIFT="-20">
+<node CREATED="1183833445000" ID="Freemind_Link_828801058" MODIFIED="1183833454015" TEXT="WorkingMemoryEventListener"/>
+<node CREATED="1183833455359" ID="Freemind_Link_1551195634" MODIFIED="1183833459890" TEXT="AgendaEventListener"/>
+<node CREATED="1183833460703" ID="Freemind_Link_823505752" MODIFIED="1183833471062" TEXT="RuleFlowEventListener"/>
+</node>
+<node CREATED="1183832943750" ID="Freemind_Link_33416065" MODIFIED="1183832946453" POSITION="left" TEXT="Rule Base"/>
+<node CREATED="1183832949218" HGAP="126" ID="Freemind_Link_248861333" MODIFIED="1183833182250" POSITION="right" TEXT="Working Memory" VSHIFT="-63">
+<node CREATED="1183832817171" HGAP="36" ID="Freemind_Link_1791382073" MODIFIED="1183832923093" TEXT="Agenda" VSHIFT="84">
+<node CREATED="1183832723656" ID="_" MODIFIED="1183832729062" STYLE="fork" TEXT="Conflict Resolution"/>
+</node>
+<node CREATED="1183836540484" ID="Freemind_Link_531729716" MODIFIED="1183836546562" TEXT="Working Memory Actions">
+<node CREATED="1183836548718" ID="Freemind_Link_596024985" MODIFIED="1183836552546" TEXT="Insert"/>
+<node CREATED="1183836554203" ID="Freemind_Link_338994460" MODIFIED="1183836556812" TEXT="Retract"/>
+<node CREATED="1183836562812" ID="Freemind_Link_35904310" MODIFIED="1183836565546" TEXT="Update"/>
+</node>
+</node>
+<node CREATED="1183833155281" HGAP="274" ID="Freemind_Link_413542286" MODIFIED="1183848352375" POSITION="right" TEXT="Execution Control" VSHIFT="-16">
+<node CREATED="1183833340546" ID="Freemind_Link_1423744179" MODIFIED="1183833344265" TEXT="Salience"/>
+<node CREATED="1183833162687" ID="Freemind_Link_1002286117" MODIFIED="1183833300218" TEXT="Rule Flow Groups"/>
+<node CREATED="1183833194921" ID="Freemind_Link_151362928" MODIFIED="1183833206640" TEXT="Agenda Groups"/>
+<node CREATED="1183833213000" ID="Freemind_Link_1849239187" MODIFIED="1183833218484" TEXT="Activation Groups"/>
+<node CREATED="1183833325312" ID="Freemind_Link_1282976401" MODIFIED="1183833333734" TEXT="Semaphores"/>
+<node CREATED="1183833241593" ID="Freemind_Link_1812636782" MODIFIED="1183833305796" TEXT="No Loop"/>
+<node CREATED="1183833219375" ID="Freemind_Link_1568224009" MODIFIED="1183833314484" TEXT="Lock on Activate"/>
+</node>
+<node CREATED="1183833383546" HGAP="43" ID="Freemind_Link_1689798977" MODIFIED="1183833393453" POSITION="left" TEXT="Execution Behaviour" VSHIFT="59">
+<node CREATED="1183833657359" ID="Freemind_Link_1054362582" MODIFIED="1183833662265" TEXT="Pattern Matching"/>
+<node CREATED="1183833397812" ID="Freemind_Link_581322552" MODIFIED="1183833406921" TEXT="Recursion"/>
+<node CREATED="1183833408312" ID="Freemind_Link_1236929690" MODIFIED="1183833418453" TEXT="Cross Products">
+<node CREATED="1183834675203" ID="Freemind_Link_1613731113" MODIFIED="1183836333843" TEXT="Matching"/>
+</node>
+</node>
+<node CREATED="1183848341562" ID="Freemind_Link_132623447" MODIFIED="1183848347781" POSITION="right" TEXT="Truth Maintenance">
+<node CREATED="1183848355328" HGAP="21" ID="Freemind_Link_459968689" MODIFIED="1183848368562" TEXT="Logical Assertions" VSHIFT="49"/>
+</node>
+</node>
+</map>
