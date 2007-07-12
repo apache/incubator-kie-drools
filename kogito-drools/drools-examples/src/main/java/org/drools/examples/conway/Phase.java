45326003c447e3668dbabf29eca13b2933af025a
Fernando Meyer <fmcamargo@gmail.com>
2007-07-12 19:16:24 +0000

    Moving examples
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@13416 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-examples/src/main/java/org/drools/examples/conway/Phase.java
+++
@@ -1,8 +0,0 @@
-package org.drools.examples.conway;
-
-public class Phase {
-    public static final int EVALUATE = 0;
-    public static final int KILL     = 1;
-    public static final int BIRTH    = 2;
-    public static final int DONE     = 3;
-}
