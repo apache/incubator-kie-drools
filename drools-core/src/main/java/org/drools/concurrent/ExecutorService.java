e6b2ab25438e6434909b0e7ab66fb8cd26eb1b1d
Mark Proctor <mdproctor@gmail.com>
2007-07-14 04:43:47 +0000

    -javadoc tidyups
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@13488 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/main/java/org/drools/concurrent/ExecutorService.java
+++ drools-core/src/main/java/org/drools/concurrent/ExecutorService.java
@@ -6,10 +6,36 @@ package org.drools.concurrent;
 import java.io.Serializable;
 
 
-
+/**
+ * This class instance is configed by the RuleBaseConfiguration and is responsible for thread management
+ * of the async services.
+ *
+ */
 public interface ExecutorService extends Serializable {
+    
+    /**
+     * The CommandExecutor is a producer/consumer style class that handles the queue and execution
+     * of the async actions
+     * @param executor
+     */
     public void setCommandExecutor(CommandExecutor executor);
+    
+    /**
+     * Submit a command for execution, adds it ot the commandExecutor's queue
+     * @param command
+     * @return
+     */
     Future submit(Command command);
+    
+    /**
+     * Shutdown this ExecutorService
+     *
+     */
     void shutDown();
+    
+    /**
+     * Startup this ExecutorService, typically called on first submit for lazy startup.
+     *
+     */
     void startUp();
 }
