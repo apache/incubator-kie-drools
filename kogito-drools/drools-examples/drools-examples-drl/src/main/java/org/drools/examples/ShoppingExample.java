a0ce4281a69aca355a44e8e14169dae65e22e193
Mark Proctor <mdproctor@gmail.com>
2007-07-28 18:49:38 +0000

    -tidied up examples
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@13822 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-examples/drools-examples-drl/src/main/java/org/drools/examples/ShoppingExample.java
+++ kogito-drools/drools-examples/drools-examples-drl/src/main/java/org/drools/examples/ShoppingExample.java
@@ -45,5 +45,89 @@ public class ShoppingExample {
         System.out.println( "Customer mark has returned the hat" );
         session.fireAllRules();
     }
+    
+    public static class Customer {
+        private String name;
+        
+        private int discount;
+            
+        public Customer(String name,
+                        int discount) {
+            this.name = name;
+            this.discount = discount;
+        }
 
+        public String getName() {
+            return name;
+        }
+
+        public int getDiscount() {
+            return discount;
+        }
+
+        public void setDiscount(int discount) {
+            this.discount = discount;
+        }
+                       
+    }
+    
+    public static class Discount {
+        private Customer customer;
+        private int amount;
+
+        public Discount(Customer customer,
+                        int amount) {
+            this.customer = customer;
+            this.amount = amount;
+        }    
+        
+        public Customer getCustomer() {
+            return customer;
+        }
+
+        public int getAmount() {
+            return amount;
+        }
+        
+    }
+
+    public static class Product {
+        private String name;
+        private float price;
+        
+        public Product(String name,
+                       float price) {
+            this.name = name;
+            this.price = price;
+        }
+        
+        public String getName() {
+            return name;
+        }
+        
+        public float getPrice() {
+            return price;
+        }
+        
+        
+    }
+    
+    public static class Purchase {
+        private Customer customer;
+        private Product product;
+        
+        public Purchase(Customer customer,
+                        Product product) {
+            this.customer = customer;
+            this.product = product;
+        }
+        
+        public Customer getCustomer() {
+            return customer;
+        }
+        
+        public Product getProduct() {
+            return product;
+        }            
+    }    
 }
