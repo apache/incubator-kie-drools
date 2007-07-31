23954897b8cd84eb13d8e9ddfeae107bc04f4752
Mark Proctor <mdproctor@gmail.com>
2007-07-31 23:34:31 +0000

    JBRULES-1049 WorkingMemory.iterateFactHandles() generates endless loop with 4.0GA. -Added iteration test, but doesn't make anything fail.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@13922 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/test/java/org/drools/reteoo/ReteooWorkingMemoryTest.java
+++ kogito-drools/drools-core/src/test/java/org/drools/reteoo/ReteooWorkingMemoryTest.java
@@ -1,16 +1,22 @@
 package org.drools.reteoo;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import junit.framework.TestCase;
 
+import org.drools.Cheese;
 import org.drools.FactHandle;
+import org.drools.Person;
+import org.drools.RuleBase;
 import org.drools.RuleBaseFactory;
+import org.drools.StatefulSession;
 import org.drools.base.MapGlobalResolver;
 import org.drools.common.EqualityKey;
 import org.drools.common.InternalWorkingMemory;
 import org.drools.common.TruthMaintenanceSystem;
+import org.drools.examples.manners.Guest;
 import org.drools.spi.GlobalResolver;
 
 public class ReteooWorkingMemoryTest extends TestCase {
@@ -84,5 +90,33 @@ public class ReteooWorkingMemoryTest extends TestCase {
                       workingMemory.getGlobal( "global1" ) );
         assertEquals( "value2",
                       workingMemory.getGlobal( "global2" ) );
+    }
+    
+    public void testObjectIterator() {
+        final RuleBase ruleBase = RuleBaseFactory.newRuleBase();
+        final StatefulSession session = ruleBase.newStatefulSession();
+        
+        session.insert( new Person( "bob", 35) );
+        session.insert( new Cheese( "stilton", 35) );
+        session.insert( new Cheese( "brie", 35) );
+        session.insert( new Person( "steve", 55) );
+        session.insert( new Person( "tom", 100) );
+        
+        int i = 0;
+        for ( Iterator it = session.iterateFactHandles(); it.hasNext(); ) {
+            Object object = it.next();
+            if ( i++ > 5 ) {
+                fail( "should not iterate for than 3 times" );
+            }
+        }
+        
+        i = 0;
+        for ( Iterator it = session.iterateObjects(); it.hasNext(); ) {
+            Object object = it.next();
+            if ( i++ > 5 ) {
+                fail( "should not iterate for than 3 times" );
+            }
+        }        
+        
     }
 }
