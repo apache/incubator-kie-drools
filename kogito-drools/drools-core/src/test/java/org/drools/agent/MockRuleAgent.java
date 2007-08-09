6ad2157c32af1455d208c4f4c4921d0aa3392eaf
Mark Proctor <mdproctor@gmail.com>
2007-08-09 01:52:29 +0000

    JBRULES-1067 Allow RuleAgent to take a RuleBaseConfiguration to configure the RuleBase it creates
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@14108 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/test/java/org/drools/agent/MockRuleAgent.java
+++ kogito-drools/drools-core/src/test/java/org/drools/agent/MockRuleAgent.java
@@ -2,12 +2,16 @@ package org.drools.agent;
 
 import java.util.Properties;
 
+import org.drools.RuleBaseConfiguration;
+
 public class MockRuleAgent extends RuleAgent {
 
     
     public boolean refreshCalled = false;
     
-
+    public MockRuleAgent() {
+        super( new RuleBaseConfiguration() );
+    }
 
     public void refreshRuleBase() {
         refreshCalled = true;
