4a0f26c661e5d2efdb14ed14f163e34b9397c71a
Michael Neale <michael.neale@gmail.com>
2007-08-22 02:42:33 +0000

    JBRULES-1086 Initial fact should not be shadowed. Also patched for funky classloading.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@14430 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/main/java/org/drools/base/DroolsQuery.java
+++ drools-core/src/main/java/org/drools/base/DroolsQuery.java
@@ -2,13 +2,13 @@ package org.drools.base;
 
 /*
  * Copyright 2005 JBoss Inc
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,10 +16,10 @@ package org.drools.base;
  * limitations under the License.
  */
 
-public class DroolsQuery {
+public final class DroolsQuery {
     private final String name;
     private final Object[] args;
-    
+
     private static final Object[] EMPTY_PARAMS = new Object[0];
 
     public DroolsQuery(final String name) {
@@ -27,7 +27,7 @@ public class DroolsQuery {
         this.name = name;
         this.args = EMPTY_PARAMS;
     }
-    
+
     public DroolsQuery(final String name, final Object[] params) {
         super();
         this.name = name;
@@ -36,12 +36,12 @@ public class DroolsQuery {
         } else {
             this.args = EMPTY_PARAMS;
         }
-    }    
+    }
 
     public String getName() {
         return this.name;
     }
-    
+
     public Object[] getArguments() {
         return this.args;
     }
