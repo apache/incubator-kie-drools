e5215002a71c3b214537a562fb5ea0cba7baada3
Ahti Kitsik <ahti@codehoop.com>
2007-08-24 17:36:13 +0000

    JBRULES-1120: Improved MVELDebugHandler to allow debug mode change at runtime. Fixed MVELConsequenceBuilderTest to use this.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@14533 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/main/java/org/drools/base/mvel/MVELDebugHandler.java
+++ kogito-drools/drools-core/src/main/java/org/drools/base/mvel/MVELDebugHandler.java
@@ -84,11 +84,32 @@ public final class MVELDebugHandler {
     	//do nothing
     }
 
+    /**
+     * Returns current debug mode.<br/>
+     * Holds lazy initialized internal reference to improve performance.<br/>
+     * Therefore you can't change System property "mvel.debugger" after isDebugMode is called at least once.<br/>
+     * <br/>
+     * To update debug mode at runtime use {@link MVELDebugHandler#setDebugMode(boolean)}<br/>
+     * @return <code>true</code> if debug mode is enabled.
+     */
     public static boolean isDebugMode() {
         if (debugMode==null) {
-            debugMode = Boolean.valueOf(System.getProperty("mvel.debugger"));
+            debugMode = Boolean.valueOf(System.getProperty(DEBUG_LAUNCH_KEY));
         }
         return debugMode.booleanValue();
+    }
+
+    /**
+     * Sets debug mode on/off.<br/>
+     * Updates local MVELDebugHandler property and System property "mvel.debugger"<br/>
+     * <br/>
+     * There's no need to ever call this method unless you write junit tests!<br/>
+     * 
+     * @param b is Debug enabled?
+     */
+    public static void setDebugMode(boolean b) {
+        debugMode = Boolean.valueOf( b );
+        System.setProperty( DEBUG_LAUNCH_KEY, debugMode.toString());
     }
         
 }
