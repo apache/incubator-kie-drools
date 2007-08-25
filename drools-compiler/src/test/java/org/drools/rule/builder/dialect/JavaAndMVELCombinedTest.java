a8ce9ee77f09beb19c7287a477641f8971e03c1f
Ahti Kitsik <ahti@codehoop.com>
2007-08-25 22:43:39 +0000

    test demonstrating slowdown caused by combining java and mvel into one drl
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@14558 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-compiler/src/test/java/org/drools/rule/builder/dialect/JavaAndMVELCombinedTest.java
@@ -0,0 +1,70 @@
+package org.drools.rule.builder.dialect;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.drools.RuleBase;
+import org.drools.RuleBaseFactory;
+import org.drools.compiler.PackageBuilder;
+import org.drools.rule.Package;
+
+public class JavaAndMVELCombinedTest extends TestCase {
+
+    private final static String FN1 = "mveljavarules.drl";
+    private final static String FN2 = "mvelonly.drl";
+    private final static String FN3 = "javaonly.drl";
+
+    public void testJavaMVELCombination() throws Exception {
+        long time1 = timing( new Runnable() {
+            public void run() {
+                readDRL( FN1 );
+            }
+        } );
+        long time2 = timing( new Runnable() {
+            public void run() {
+                readDRL( FN2 );
+            }
+        } );
+        long time3 = timing( new Runnable() {
+            public void run() {
+                readDRL( FN3 );
+            }
+        } );
+        
+        System.out.println("mveljava: "+time1/1000.);
+        System.out.println("    mvel: "+time2/1000.);
+        System.out.println("    java: "+time3/1000.);
+        
+    }
+
+    private long timing(Runnable runnable) {
+        long start = System.currentTimeMillis();
+        runnable.run();
+        return System.currentTimeMillis()-start;
+    }
+
+    private void readDRL(String fn) {
+        try {
+            Reader source = new InputStreamReader( JavaAndMVELCombinedTest.class.getResourceAsStream( fn ) );
+
+            PackageBuilder builder = new PackageBuilder();
+            builder.addPackageFromDrl( source );
+            Assert.assertEquals( 0,
+                                 builder.getErrors().getErrors().length );
+
+            Package pkg = builder.getPackage();
+            RuleBase ruleBase = RuleBaseFactory.newRuleBase();
+            ruleBase.addPackage( pkg );
+
+            Assert.assertEquals( 2,
+                                 pkg.getRules().length );
+        } catch ( Throwable t ) {
+            throw new RuntimeException(t);
+        }
+
+    }
+
+}
