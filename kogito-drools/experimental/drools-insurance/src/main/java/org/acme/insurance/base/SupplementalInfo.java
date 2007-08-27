0491b4e73073e4b88fad7f31b3c3d247dfb4bcf7
Fernando Meyer <fmcamargo@gmail.com>
2007-08-27 19:39:26 +0000

    Adding drools insurance
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@14662 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/experimental/drools-insurance/src/main/java/org/acme/insurance/base/SupplementalInfo.java
@@ -0,0 +1,52 @@
+package org.acme.insurance.base;
+
+public class SupplementalInfo {
+    
+    private int     driverId;
+
+    private boolean extraCar;
+    private boolean extraAssistence;
+    private boolean glassCoverage;
+    private boolean nonRelatedExpenses;
+
+
+    public int getDriverId() {
+        return driverId;
+    }
+
+    public void setDriverId(int driverId) {
+        this.driverId = driverId;
+    }
+
+    public boolean isExtraAssistence() {
+        return extraAssistence;
+    }
+
+    public void setExtraAssistence(boolean extraAssistence) {
+        this.extraAssistence = extraAssistence;
+    }
+
+    public boolean isExtraCar() {
+        return extraCar;
+    }
+
+    public void setExtraCar(boolean extraCar) {
+        this.extraCar = extraCar;
+    }
+
+    public boolean isGlassCoverage() {
+        return glassCoverage;
+    }
+
+    public void setGlassCoverage(boolean glassCoverage) {
+        this.glassCoverage = glassCoverage;
+    }
+
+    public boolean isNonRelatedExpenses() {
+        return nonRelatedExpenses;
+    }
+
+    public void setNonRelatedExpenses(boolean nonRelatedExpenses) {
+        this.nonRelatedExpenses = nonRelatedExpenses;
+    }
+}
