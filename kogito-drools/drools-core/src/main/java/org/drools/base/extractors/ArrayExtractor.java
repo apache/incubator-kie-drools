1710b09a8d6a4cd49134056b3e0f600570f05263
Edson Tirelli <ed.tirelli@gmail.com>
2007-08-31 13:07:48 +0000

    JBRULES-1154: Fixing CCE when retrieving query parameters
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@14778 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/main/java/org/drools/base/extractors/ArrayExtractor.java
+++ kogito-drools/drools-core/src/main/java/org/drools/base/extractors/ArrayExtractor.java
@@ -32,7 +32,7 @@ public class ArrayExtractor implements Extractor {
     }
     public byte getByteValue(InternalWorkingMemory workingMemory, Object object) {
         Object[] array = ( Object[] ) this.arrayExtractor.getValue( workingMemory, object );
-        return ( (Byte)array[ this.index ]).byteValue();
+        return ( (Number)array[ this.index ]).byteValue();
     }
     public char getCharValue(InternalWorkingMemory workingMemory, Object object) {
         Object[] array = ( Object[] ) this.arrayExtractor.getValue( workingMemory, object );
@@ -40,28 +40,28 @@ public class ArrayExtractor implements Extractor {
     }
     public double getDoubleValue(InternalWorkingMemory workingMemory, Object object) {
         Object[] array = ( Object[] ) this.arrayExtractor.getValue( workingMemory, object );
-        return ( (Double)array[ this.index ]).doubleValue();
+        return ( (Number)array[ this.index ]).doubleValue();
     }
 
     public float getFloatValue(InternalWorkingMemory workingMemory, Object object) {
         Object[] array = ( Object[] ) this.arrayExtractor.getValue( workingMemory, object );
-        return ( (Float)array[ this.index ]).floatValue();
+        return ( (Number)array[ this.index ]).floatValue();
     }
 
     public int getIntValue(InternalWorkingMemory workingMemory, Object object) {
         Object[] array = ( Object[] ) this.arrayExtractor.getValue( workingMemory, object );
-        return ( (Integer)array[ this.index ]).intValue();
+        return ( (Number)array[ this.index ]).intValue();
     }
     public long getLongValue(InternalWorkingMemory workingMemory, Object object) {
         Object[] array = ( Object[] ) this.arrayExtractor.getValue( workingMemory, object );
-        return ( (Long) array[ this.index ]).longValue();
+        return ( (Number) array[ this.index ]).longValue();
     }
     public Method getNativeReadMethod() {
         throw new UnsupportedOperationException("cannot call a method on an array extractor" );
     }
     public short getShortValue(InternalWorkingMemory workingMemory, Object object) {
         Object[] array = ( Object[] ) this.arrayExtractor.getValue( workingMemory, object );
-        return ( (Short)array[ this.index ]).shortValue();
+        return ( (Number)array[ this.index ]).shortValue();
     }
     public Object getValue(InternalWorkingMemory workingMemory, Object object) {
         Object[] array = ( Object[] ) this.arrayExtractor.getValue( workingMemory, object );
