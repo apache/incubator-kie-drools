2cecbe4764ee3406e1276651bb2f8246226dcca5
Mark Proctor <mdproctor@gmail.com>
2007-09-09 15:02:33 +0000

    JBRULES-1165 StatelessSessionResult should have getGlobal -Added an Export capability, with unit tests, for a StatelessSession to export globals.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@14967 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/test/java/org/drools/integrationtests/StatelessSessionTest.java
+++ kogito-drools/drools-compiler/src/test/java/org/drools/integrationtests/StatelessSessionTest.java
@@ -5,19 +5,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.drools.Cheese;
+import org.drools.Cheesery;
 import org.drools.RuleBase;
 import org.drools.RuleBaseConfiguration;
 import org.drools.RuleBaseFactory;
 import org.drools.StatelessSession;
 import org.drools.StatelessSessionResult;
 import org.drools.WorkingMemory;
+import org.drools.base.CopyIdentifiersGlobalExporter;
+import org.drools.base.MapGlobalResolver;
+import org.drools.base.ReferenceOriginalGlobalExporter;
 import org.drools.compiler.PackageBuilder;
 import org.drools.rule.Package;
+import org.drools.spi.GlobalExporter;
+import org.drools.spi.GlobalResolver;
 
 import junit.framework.TestCase;
 
 public class StatelessSessionTest extends TestCase {
     final List list = new ArrayList();
+    final Cheesery cheesery = new Cheesery();
+    final GlobalResolver globalResolver = new MapGlobalResolver();
 
     protected RuleBase getRuleBase() throws Exception {
 
@@ -72,6 +80,9 @@ public class StatelessSessionTest extends TestCase {
     public void testSingleObjectAssertWithResults() throws Exception {
         StatelessSession session = getSession();
 
+        // notice I don't export Cheessery
+        session.setGlobalExporter( new CopyIdentifiersGlobalExporter( new String[]{"list"} ) );
+
         final Cheese stilton = new Cheese( "stilton",
                                            5 );
 
@@ -79,10 +90,19 @@ public class StatelessSessionTest extends TestCase {
 
         assertSame( stilton,
                     result.iterateObjects().next() );
+
+        assertEquals( "stilton",
+                      ((List) result.getGlobal( "list" )).get( 0 ) );
+
+        // cheesery should be null
+        assertNull( result.getGlobal( "cheesery" ) );
     }
 
     public void testArrayObjectAssertWithResults() throws Exception {
         StatelessSession session = getSession();
+
+        // notice I don't export Cheessery
+        session.setGlobalExporter( new CopyIdentifiersGlobalExporter( new String[]{"list"} ) );
 
         final Cheese stilton = new Cheese( "stilton",
                                            5 );
@@ -91,10 +111,19 @@ public class StatelessSessionTest extends TestCase {
 
         assertSame( stilton,
                     result.iterateObjects().next() );
+
+        assertEquals( "stilton",
+                      ((List) result.getGlobal( "list" )).get( 0 ) );
+
+        // cheesery should be null
+        assertNull( result.getGlobal( "cheesery" ) );
     }
 
     public void testCollectionObjectAssertWithResults() throws Exception {
         StatelessSession session = getSession();
+
+        // notice I don't export Cheessery
+        session.setGlobalExporter( new CopyIdentifiersGlobalExporter( new String[]{"list"} ) );
 
         final Cheese stilton = new Cheese( "stilton",
                                            5 );
@@ -105,6 +134,12 @@ public class StatelessSessionTest extends TestCase {
 
         assertSame( stilton,
                     result.iterateObjects().next() );
+
+        assertEquals( "stilton",
+                      ((List) result.getGlobal( "list" )).get( 0 ) );
+
+        // cheesery should be null
+        assertNull( result.getGlobal( "cheesery" ) );
     }
 
     public void testAsynSingleOjbectcAssert() throws Exception {
@@ -150,6 +185,76 @@ public class StatelessSessionTest extends TestCase {
         assertEquals( "stilton",
                       list.get( 0 ) );
     }
+    
+    public void testCopyIdentifierGlobalExporterOneValue() throws Exception {
+        StatelessSession session = getSession();       
+
+        // notice I don't export Cheessery
+        session.setGlobalExporter( new CopyIdentifiersGlobalExporter( new String[]{"list"} ) );
+
+        StatelessSessionResult result = session.executeWithResults( (Object) null );
+
+        assertSame( this.list,
+                    result.getGlobal( "list" ) );
+
+        // cheesery should be null
+        assertNull( result.getGlobal( "cheesery" ) );
+        
+        assertNotSame( this.globalResolver, result.getGlobalResolver() );
+    }
+    
+    public void testCopyIdentifierGlobalExporterTwoValues() throws Exception {
+        StatelessSession session = getSession();
+
+        session.setGlobalExporter( new CopyIdentifiersGlobalExporter( new String[]{"list", "cheesery"} ) );
+
+        StatelessSessionResult result = session.executeWithResults( (Object) null );
+
+        assertSame( this.list,
+                    result.getGlobal( "list" ) );
+
+        // cheesery should be null
+        assertSame( this.cheesery,
+                    result.getGlobal( "cheesery" ) );
+        
+        assertNotSame( this.globalResolver, result.getGlobalResolver() );        
+    }    
+    
+    public void testCopyIdentifierGlobalExporterAllValues() throws Exception {
+        StatelessSession session = getSession();
+
+        // I've not specified any identifiers, so it should do them alll
+        session.setGlobalExporter( new CopyIdentifiersGlobalExporter() );
+
+        StatelessSessionResult result = session.executeWithResults( (Object) null );
+
+        assertSame( this.list,
+                    result.getGlobal( "list" ) );
+
+        // cheesery should be null
+        assertSame( this.cheesery,
+                    result.getGlobal( "cheesery" ) );
+        
+        assertNotSame( this.globalResolver, result.getGlobalResolver() );        
+    }     
+    
+    public void testReferenceOriginalGlobalExporter() throws Exception {
+        StatelessSession session = getSession();
+
+        // I've not specified any identifiers, so it should do them alll
+        session.setGlobalExporter( new ReferenceOriginalGlobalExporter() );
+
+        StatelessSessionResult result = session.executeWithResults( (Object) null );
+
+        assertSame( this.list,
+                    result.getGlobal( "list" ) );
+
+        // cheesery should be null
+        assertSame( this.cheesery,
+                    result.getGlobal( "cheesery" ) );
+        
+        assertSame( this.globalResolver, result.getGlobalResolver() );        
+    }        
 
     private StatelessSession getSession() throws Exception {
         final PackageBuilder builder = new PackageBuilder();
@@ -159,9 +264,13 @@ public class StatelessSessionTest extends TestCase {
         final RuleBase ruleBase = getRuleBase();
         ruleBase.addPackage( pkg );
         final StatelessSession session = ruleBase.newStatelessSession();
+        
+        session.setGlobalResolver( this.globalResolver );
 
         session.setGlobal( "list",
                            this.list );
+        session.setGlobal( "cheesery",
+                           this.cheesery );
         return session;
     }
 }
