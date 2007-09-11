7ee5e00d34542eb134d1ccf1ff0c7c7963b6b7e1
Fernando Meyer <fmcamargo@gmail.com>
2007-09-11 20:26:07 +0000

    changes on drools insurance demo
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@15020 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/experimental/drools-insurance/src/test/java/org/acme/insurance/test/InsuranceTestHelper.java
+++ kogito-drools/experimental/drools-insurance/src/test/java/org/acme/insurance/test/InsuranceTestHelper.java
@@ -5,10 +5,8 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 
 import org.drools.RuleBase;
-import org.drools.RuleBaseFactory;
 import org.drools.StatefulSession;
 import org.drools.agent.RuleAgent;
-import org.drools.compiler.PackageBuilder;
 
 public class InsuranceTestHelper {
     private RuleBase        rulebase;
@@ -16,8 +14,8 @@ public class InsuranceTestHelper {
 
     public StatefulSession getSession() {
         try {
-            //rulebase = loadRuleBaseFromRuleAgent();
-            rulebase = loadRuleBaseFromDRL();
+            rulebase = loadRuleBaseFromRuleAgent();
+            //rulebase = loadRuleBaseFromDRL();
 
             session = rulebase.newStatefulSession();
             session.startProcess( "insuranceProcess" );
@@ -39,22 +37,22 @@ public class InsuranceTestHelper {
     }
 
 
-    private RuleBase loadRuleBaseFromDRL() throws Exception {
-
-        PackageBuilder builder = new PackageBuilder();
-        builder.addPackageFromDrl( getTechnicalRules( "/approval/insurancefactor.drl" ) );
-        builder.addPackageFromDrl( getTechnicalRules( "/approval/approval.drl" ) );
-        builder.addPackageFromDrl( getTechnicalRules( "/approval/calculateInsurance.drl" ) );
-        builder.addPackageFromDrl( getTechnicalRules( "/approval/marginalage.dslr" ),
-                                   getTechnicalRules( "/approval/acme.dsl" ) );
-        builder.addRuleFlow( getTechnicalRules( "/approval/insurance-process.rfm" ) );
-
-        
-
-        RuleBase ruleBase = RuleBaseFactory.newRuleBase();
-        ruleBase.addPackage( builder.getPackage() );
-        return ruleBase;
-    }
+//    private RuleBase loadRuleBaseFromDRL() throws Exception {
+//
+//        PackageBuilder builder = new PackageBuilder();
+//        builder.addPackageFromDrl( getTechnicalRules( "/approval/insurancefactor.drl" ) );
+//        builder.addPackageFromDrl( getTechnicalRules( "/approval/approval.drl" ) );
+//        builder.addPackageFromDrl( getTechnicalRules( "/approval/calculateInsurance.drl" ) );
+//        builder.addPackageFromDrl( getTechnicalRules( "/approval/marginalage.dslr" ),
+//                                   getTechnicalRules( "/approval/acme.dsl" ) );
+//        builder.addRuleFlow( getTechnicalRules( "/approval/insurance-process.rfm" ) );
+//
+//        
+//
+//        RuleBase ruleBase = RuleBaseFactory.newRuleBase();
+//        ruleBase.addPackage( builder.getPackage() );
+//        return ruleBase;
+//    }
 
     private Reader getTechnicalRules(String name) {
 
