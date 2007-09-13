a02dc47384cdc2dd458240e347b0f95d40b93b9a
Michael Neale <michael.neale@gmail.com>
2007-09-13 06:07:14 +0000

    JBRULES-1189
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@15075 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/test/java/org/drools/integrationtests/MVELTest.java
+++ kogito-drools/drools-compiler/src/test/java/org/drools/integrationtests/MVELTest.java
@@ -8,6 +8,7 @@ import java.io.ObjectOutputStream;
 import java.io.Reader;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -18,6 +19,7 @@ import org.drools.Cheese;
 import org.drools.RuleBase;
 import org.drools.RuleBaseFactory;
 import org.drools.WorkingMemory;
+import org.drools.base.evaluators.DateFactory;
 import org.drools.common.DroolsObjectInputStream;
 import org.drools.compiler.DrlParser;
 import org.drools.compiler.DroolsParserException;
@@ -27,48 +29,54 @@ import org.drools.rule.Package;
 import org.mvel.MVEL;
 
 public class MVELTest extends TestCase {
-    public void testHelloWorld() throws Exception {                   
+    public void testHelloWorld() throws Exception {
         // read in the source
         final Reader reader = new InputStreamReader( getClass().getResourceAsStream( "test_mvel.drl" ) );
         RuleBase ruleBase = loadRuleBase( reader );
-                
+
         // Bellow lines are a way to make sure serialization is fine
         // start of serialization block
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         ObjectOutputStream obj = new ObjectOutputStream( out );
         obj.writeObject( ruleBase );
         obj.close();
-        
+
         byte[] buf = out.toByteArray();
-        
+
         DroolsObjectInputStream in = new DroolsObjectInputStream( new ByteArrayInputStream( buf ) );
         ruleBase = (RuleBase) in.readObject();
-        // end of serialization block        
+        // end of serialization block
 
         final WorkingMemory workingMemory = ruleBase.newStatefulSession();
 
         final List list = new ArrayList();
         workingMemory.setGlobal( "list",
                                  list );
-        
+
         final List list2 = new ArrayList();
         workingMemory.setGlobal( "list2",
-                                 list2 );        
+                                 list2 );
 
-        workingMemory.insert( new Cheese("stilton", 10) );
+
+        Cheese c = new Cheese("stilton", 10) ;
+        workingMemory.insert( c);
         workingMemory.fireAllRules();
         assertEquals( 2, list.size() );
         assertEquals( new Integer(30), list.get(0));
         assertEquals( new Integer(22), list.get(1));
-        
-        assertEquals( "hello world", list2.get(0));        
-        
+
+        assertEquals( "hello world", list2.get(0));
+
+        Date dt = DateFactory.parseDate( "10-Jul-1974" );
+        assertEquals(dt, c.getUsedBy());
+
+
     }
-    
+
     public Object compiledExecute(String ex) {
         Serializable compiled = MVEL.compileExpression(ex);
         return MVEL.executeExpression(compiled, new Object(), new HashMap());
-    }    
+    }
 
     private RuleBase loadRuleBase(final Reader reader) throws IOException,
                                                       DroolsParserException,
@@ -81,9 +89,9 @@ public class MVELTest extends TestCase {
         // pre build the package
         final PackageBuilder builder = new PackageBuilder();
         builder.addPackage( packageDescr );
-        
+
         System.out.println( builder.getErrors() );
-        
+
         final Package pkg = builder.getPackage();
 
         // add the package to a rulebase
