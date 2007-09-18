c0a735d652b82a5b7150e572789dd82d53503d1a
Edson Tirelli <ed.tirelli@gmail.com>
2007-09-18 13:08:32 +0000

    improving error messages
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@15201 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/main/java/org/drools/lang/dsl/DefaultExpander.java
+++ kogito-drools/drools-compiler/src/main/java/org/drools/lang/dsl/DefaultExpander.java
@@ -136,7 +136,7 @@ public class DefaultExpander
                 String headerFragment = m.group( 2 );
                 expanded.append( headerFragment ); // adding rule header and attributes
                 String lhsFragment = m.group( 3 );
-                expanded.append( this.expandLHS( lhsFragment, countNewlines( headerFragment ) + 2 ) ); // adding expanded LHS
+                expanded.append( this.expandLHS( lhsFragment, countNewlines( headerFragment ) + 1 ) ); // adding expanded LHS
                 String thenFragment = m.group( 4 ); 
                 
                 expanded.append( thenFragment ); // adding "then" header
@@ -147,7 +147,7 @@ public class DefaultExpander
                 // match query
                 String fragment = m.group( 7 );
                 expanded.append( fragment ); // adding query header and attributes
-                expanded.append( this.expandLHS( m.group( 8 ), countNewlines( fragment ) + 2 ) ); // adding expanded LHS
+                expanded.append( this.expandLHS( m.group( 8 ), countNewlines( fragment ) + 1 ) ); // adding expanded LHS
                 expanded.append( m.group( 9 ) ); // adding query trailer
                 expanded.append( "\n" );
             } else {
