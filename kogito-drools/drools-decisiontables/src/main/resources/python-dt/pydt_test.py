ba25a08d5ef78ee2c2b130aaa7d6cfe050b045a3
Michael Neale <michael.neale@gmail.com>
2007-09-24 06:25:19 +0000

    refactored a little
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@15320 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-decisiontables/src/main/resources/python-dt/pydt_test.py
@@ -0,0 +1,44 @@
+import pydt
+
+test_fact = { "Age" : 42, "Risk" : "'HIGH'", "PolicyType" : "'COMPREHENSIVE'" }
+
+test_table = {
+    "condition_headers" : [ ["A" , "Age"], ["B", "Risk =="], ["C", "PolicyType =="]],
+    "action_headers" : [ ["F","Premium"], ["G","Log"]],
+
+
+    "data" : [
+        {"row" : 2, "A" : "> 2", "B" : "'HIGH'", "C": "'COMPREHENSIVE'",  "F" : "245"},
+        {"row" : 3, "A" : "< 25 ", "B" : "'LOW'", "F" : "390"}
+        ]
+
+}
+
+
+#and now some crude test code
+pydt.process_dt(test_fact, test_table)
+print "RESULT: " + str(test_fact)
+if not test_fact.has_key("Premium") :
+	print("ERROR: no premium was calculated")
+if  test_fact["Premium"] == '245' :
+	print("PASSED STEP 1")
+else :
+	print("FAILED STEP 1: Premium was " + test_fact["Premium"])
+
+#some simple test
+tbl = pydt.load_xls("Example.xls")
+if tbl['condition_headers'][0][1] == "Age" :
+	print "PASSED STEP 2"
+else:
+	print "FAILED STEP 2"
+
+#now test it all, end to end
+test_fact = { "Age" : 42, "Risk" : "'HIGH'", "PolicyType" : "'COMPREHENSIVE'" }
+pydt.process_dt(test_fact, tbl)
+if not test_fact.has_key("Premium") :
+	print("ERROR: no premium was calculated")
+premium = test_fact["Premium"]
+if premium == 245 :
+	print("PASSED STEP 3")
+else :
+	print("FAILED STEP 3: Premium was " + test_fact["Premium"])
