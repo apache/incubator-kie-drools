8c67d29c4819bb7b0591e80f10ad1654e939e255
Kris Verlaenen <kris.verlaenen@gmail.com>
2007-10-03 00:20:35 +0000

    JBRULES-1244: Ruleflows should have a package name  - Process now has a packageName
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@15530 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-examples/drools-examples-drl/src/main/java/org/drools/examples/cdss/Terminology.java
+++ kogito-drools/drools-examples/drools-examples-drl/src/main/java/org/drools/examples/cdss/Terminology.java
@@ -4,6 +4,7 @@ public interface Terminology {
 	
 	String DIAGNOSE_HEALTHY = "Diagnose healthy";
 	
+	String DIAGNOSE_X = "Diagnose disease X";
 	String DIAGNOSE_X_TYPE_UNKNOWN = "Diagnose disease X: Type unknown";
 	String DIAGNOSE_X_TYPE1 = "Diagnose disease X: Type 1";
 	String DIAGNOSE_X_TYPE2 = "Diagnose disease X: Type 2";
