45392a617bf7d9a16adc08ec546b3e583c155e1f
Mark Proctor <mdproctor@gmail.com>
2007-10-06 12:45:31 +0000

    -updated the javadocs for RuleBase
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@15617 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/main/java/org/drools/RuleBase.java
+++ drools-core/src/main/java/org/drools/RuleBase.java
@@ -121,23 +121,63 @@ public interface RuleBase
 
     void addPackage(Package pkg) throws Exception;
     
+    /**
+     * This locks the current RuleBase and all there referenced StatefulSessions. This should be 
+     * used when there is a number of dynamic RuleBase changes you wish to make, but cannot have any normal
+     * WorkingMemory operations occuring inbetween.
+     *
+     */
     void lock();
     
+    /**
+     * Unlocks the RuleBase and all of the referenced StatefulSessions.
+     *
+     */
     void unlock();
-    
+
+    /**
+     * Returns the number of additive operations applied since the last lock() was obtained 
+     * @return
+     */
     int getAdditionsSinceLock();
 
+    /**
+     * Returns the number of removal operations applied since the last lock() was obtained 
+     * @return
+     */
     int getRemovalsSinceLock();
 
+    /**
+     * Remove the package and all it's rules, functions etc
+     * @param packageName
+     */
     void removePackage(String packageName);
 
+    /**
+     * Remove a specific Rule in a Package
+     * @param packageName
+     * @param ruleName
+     */
     void removeRule(String packageName,
                     String ruleName);
 
+    /**
+     * Removes a specific function in a specific package.
+     * @param packageName
+     * @param functionName
+     */
     void removeFunction(String packageName,
                         String functionName);
 
+    /**
+     * Removes a process by the process' id
+     * @param id
+     */
     void removeProcess(String id);
 
+    /**
+     * Returns an array of all the referenced StatefulSessions
+     * @return
+     */
     public StatefulSession[] getStatefulSessions();
 }
