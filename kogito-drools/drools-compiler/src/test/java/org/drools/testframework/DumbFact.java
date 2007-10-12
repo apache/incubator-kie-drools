8e0174a8f381abc3723f417d65a32b2e3ca6af7c
Michael Neale <michael.neale@gmail.com>
2007-10-12 07:17:23 +0000

    JBRULES-1271 Test populator.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@15792 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-compiler/src/test/java/org/drools/testframework/DumbFact.java
@@ -0,0 +1,40 @@
+package org.drools.testframework;
+
+public class DumbFact {
+
+
+	private String name;
+	private int age;
+	private Long number;
+	private boolean enabled;
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getNumber() {
+		return number;
+	}
+	public void setNumber(Long number) {
+		this.number = number;
+	}
+
+	public String toString() {
+		return "Name:" + name + " age:" + age + " number:" + number + " enabled:" + enabled;
+	}
+
+}
---
+++ kogito-drools/drools-compiler/src/test/java/org/drools/testframework/FactPopulatorTest.java
@@ -0,0 +1,59 @@
+package org.drools.testframework;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.mvel.MVEL;
+
+import junit.framework.TestCase;
+
+public class FactPopulatorTest extends TestCase {
+
+	public void testMVELPopulate() throws Exception {
+		Object q = MVEL.eval("new org.drools.testframework.DumbFact()");
+
+		Map m = new HashMap();
+		m.put("obj", q);
+		m.put("val", "mike");
+		MVEL.eval("obj.name = val", m);
+
+		m = new HashMap();
+		m.put("obj", q);
+		m.put("val", "42");
+		MVEL.eval("obj.age = val", m);
+
+		m = new HashMap();
+		m.put("obj", q);
+		m.put("val", "44");
+		MVEL.eval("obj.number = val", m);
+
+		m = new HashMap();
+		m.put("obj", q);
+		m.put("val", "true");
+		MVEL.eval("obj.enabled = val", m);
+
+		DumbFact d = (DumbFact) q;
+
+		assertEquals("mike", d.getName());
+		assertEquals(42, d.getAge());
+		assertEquals(new Long(44), d.getNumber());
+		assertEquals(true, d.isEnabled());
+
+
+	}
+
+	public void testMVELFactChecked() throws Exception {
+		//now we have a bean check it can be verified
+		final DumbFact d = new DumbFact();
+		d.setAge(42);
+		Map m = new HashMap() {{
+				put("d", d);
+				put("val", "42");
+		}};
+
+		assertTrue(MVEL.evalToBoolean("d.age == val", m));
+	}
+
+}
