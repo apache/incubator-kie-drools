14ac9b95aaf1f2dd0f3a5cc66e955a38fe1b831a
Mark Proctor <mdproctor@gmail.com>
2007-10-18 13:00:25 +0000

    JBRULES-1283 Unable to serialize rule base ( NotSerializableException: org.drools.base.FireAllRulesRuleBaseUpdateListener ) -made class serialisable
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@15913 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/main/java/org/drools/base/FireAllRulesRuleBaseUpdateListener.java
+++ kogito-drools/drools-core/src/main/java/org/drools/base/FireAllRulesRuleBaseUpdateListener.java
@@ -3,23 +3,27 @@
  */
 package org.drools.base;
 
+import java.io.Serializable;
+
 import org.drools.StatefulSession;
 import org.drools.event.BeforeRuleBaseUnlockedEvent;
 import org.drools.event.DefaultRuleBaseEventListener;
 import org.drools.spi.RuleBaseUpdateListener;
 
-public class FireAllRulesRuleBaseUpdateListener extends DefaultRuleBaseEventListener 
-implements RuleBaseUpdateListener {
+public class FireAllRulesRuleBaseUpdateListener extends DefaultRuleBaseEventListener
+    implements
+    RuleBaseUpdateListener,
+    Serializable {
     private StatefulSession session;
-    
+
     public FireAllRulesRuleBaseUpdateListener() {
-        
+
     }
-    
+
     public void setSession(StatefulSession session) {
         this.session = session;
     }
-    
+
     public void beforeRuleBaseUnlocked(BeforeRuleBaseUnlockedEvent event) {
         if ( session.getRuleBase().getAdditionsSinceLock() > 0 ) {
             session.fireAllRules();
