cebcae2e1a0024de4fdc1dc76c0eb542440f6211
Michael Neale <michael.neale@gmail.com>
2007-10-29 02:54:33 +0000

    JBRULES-1271 testing framework
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@16125 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-compiler/src/main/java/org/drools/brms/client/modeldriven/testing/Fixture.java
@@ -0,0 +1,10 @@
+package org.drools.brms.client.modeldriven.testing;
+
+import org.drools.brms.client.modeldriven.brl.PortableObject;
+
+/**
+ * All test fixtures implement this.
+ */
+public interface Fixture extends PortableObject {
+
+}
