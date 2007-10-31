6edd7aebdb2daeb4ddfff661823a3be3066b3689
Michael Neale <michael.neale@gmail.com>
2007-10-31 01:02:18 +0000

    JBRULES-1271 testing framework
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@16179 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-compiler/src/test/java/org/drools/testframework/FactDataTest.java
@@ -0,0 +1,17 @@
+package org.drools.testframework;
+
+import org.drools.brms.client.modeldriven.testing.FactData;
+import org.drools.brms.client.modeldriven.testing.FieldData;
+
+import junit.framework.TestCase;
+
+public class FactDataTest extends TestCase {
+	public void testAdd() {
+		FactData fd = new FactData("x", "y", new FieldData[0], false, false );
+		assertEquals(0, fd.fieldData.length);
+		fd.addFieldData(new FieldData("x", "y", false));
+		assertEquals(1, fd.fieldData.length);
+		fd.addFieldData(new FieldData("q", "x", false));
+		assertEquals(2, fd.fieldData.length);
+	}
+}
