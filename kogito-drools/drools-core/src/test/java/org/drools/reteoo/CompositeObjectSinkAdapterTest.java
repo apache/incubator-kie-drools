28d66b43a4b24eaf7961d9d9eaf715b50fe16ddf
Edson Tirelli <ed.tirelli@gmail.com>
2007-11-09 16:08:01 +0000

    JBRULES-1284: fixing mockup for test
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@16470 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/test/java/org/drools/reteoo/CompositeObjectSinkAdapterTest.java
+++ kogito-drools/drools-core/src/test/java/org/drools/reteoo/CompositeObjectSinkAdapterTest.java
@@ -357,7 +357,7 @@ public class CompositeObjectSinkAdapterTest extends TestCase {
 
         public ValueType getValueType() {
             //  Auto-generated method stub
-            return ValueType.OBJECT_TYPE;
+            return ValueType.STRING_TYPE;
         }
 
         public int getHashCode(InternalWorkingMemory workingMemory,
