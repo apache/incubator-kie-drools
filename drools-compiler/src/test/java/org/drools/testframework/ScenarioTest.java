95659baa90c4965694a3310c4f359eeab64382d0
Michael Neale <michael.neale@gmail.com>
2007-11-13 01:30:37 +0000

    JBRULES-1271 testing framework junk
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@16558 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-compiler/src/test/java/org/drools/testframework/ScenarioTest.java
@@ -0,0 +1,56 @@
+package org.drools.testframework;
+
+import org.drools.brms.client.modeldriven.testing.Scenario;
+import org.drools.brms.client.modeldriven.testing.VerifyRuleFired;
+
+import junit.framework.TestCase;
+
+public class ScenarioTest extends TestCase {
+
+	public void testInsertAfter() {
+		Scenario sc = new Scenario();
+		VerifyRuleFired vf = new VerifyRuleFired();
+		sc.insertAfter(null, vf);
+		assertEquals(1, sc.fixtures.size());
+		assertEquals(vf, sc.fixtures.get(0));
+
+		VerifyRuleFired vf2 = new VerifyRuleFired();
+		sc.fixtures.add(vf2);
+
+		VerifyRuleFired vf3 = new VerifyRuleFired();
+		sc.insertAfter(vf, vf3);
+		assertEquals(3, sc.fixtures.size());
+		assertEquals(vf, sc.fixtures.get(0));
+		assertEquals(vf3, sc.fixtures.get(1));
+		assertEquals(vf2, sc.fixtures.get(2));
+
+		VerifyRuleFired vf4 = new VerifyRuleFired();
+		sc.insertAfter(vf2, vf4);
+		assertEquals(4, sc.fixtures.size());
+		assertEquals(vf4, sc.fixtures.get(3));
+		assertEquals(vf2, sc.fixtures.get(2));
+
+		VerifyRuleFired vf5 = new VerifyRuleFired();
+		sc.insertAfter(null, vf5);
+		assertEquals(5, sc.fixtures.size());
+		assertEquals(vf5, sc.fixtures.get(0));
+	}
+
+	public void testRemoveFixture() {
+		Scenario sc = new Scenario();
+
+		VerifyRuleFired vf1 = new VerifyRuleFired();
+		VerifyRuleFired vf2 = new VerifyRuleFired();
+		VerifyRuleFired vf3 = new VerifyRuleFired();
+
+		sc.fixtures.add(vf1);
+		sc.fixtures.add(vf2);
+		sc.fixtures.add(vf3);
+
+		sc.removeFixture(vf2);
+		assertEquals(2, sc.fixtures.size());
+		assertEquals(vf1, sc.fixtures.get(0));
+		assertEquals(vf3, sc.fixtures.get(1));
+	}
+
+}
