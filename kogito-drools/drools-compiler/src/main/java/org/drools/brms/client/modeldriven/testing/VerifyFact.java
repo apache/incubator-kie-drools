ebc4e8e1dd124661724ef7ef8e342fe9091392de
Michael Neale <michael.neale@gmail.com>
2007-11-14 03:36:32 +0000

    JBRULES-1271 test scenario stuff
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@16574 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/main/java/org/drools/brms/client/modeldriven/testing/VerifyFact.java
+++ kogito-drools/drools-compiler/src/main/java/org/drools/brms/client/modeldriven/testing/VerifyFact.java
@@ -1,5 +1,8 @@
 package org.drools.brms.client.modeldriven.testing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This is for making assertions over a specific facts value/state AFTER execution.
  * @author Michael Neale
@@ -7,21 +10,24 @@ package org.drools.brms.client.modeldriven.testing;
  */
 public class VerifyFact implements Expectation {
 
-	public VerifyField[] fieldValues = new VerifyField[0];
+	/**
+	 * @gwt.typeArgs <org.drools.brms.client.modeldriven.testing.VerifyField>
+	 */
+	public List fieldValues = new ArrayList();
 	public String factName;
 	public String description;
 
 
 	public VerifyFact() {}
-	public VerifyFact(String factName, VerifyField[] fieldValues) {
+	public VerifyFact(String factName, List fieldValues) {
 		this.factName = factName;
 		this.fieldValues = fieldValues;
 	}
 
 
 	public boolean wasSuccessful() {
-		for (int i = 0; i < fieldValues.length; i++) {
-			VerifyField vf = fieldValues[i];
+		for (int i = 0; i < fieldValues.size(); i++) {
+			VerifyField vf = (VerifyField) fieldValues.get(i);
 			if (! vf.successResult.booleanValue()) {
 				return false;
 			}
