0738376efd4df53c860bd066ad0c266b5c2ac157
Toni Rikkola <toni@rikkola.net>
2007-11-29 15:29:50 +0000

    Redundancy test rules for rule possibilities.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@16913 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-analytics/src/test/java/org/drools/analytics/redundancy/RedundantPossibilitiesTest.java
@@ -0,0 +1,147 @@
+package org.drools.analytics.redundancy;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+import org.drools.StatelessSession;
+import org.drools.StatelessSessionResult;
+import org.drools.analytics.TestBase;
+import org.drools.analytics.components.AnalyticsRule;
+import org.drools.analytics.components.LiteralRestriction;
+import org.drools.analytics.components.Pattern;
+import org.drools.analytics.components.PatternPossibility;
+import org.drools.analytics.components.RulePossibility;
+import org.drools.analytics.dao.AnalyticsDataFactory;
+import org.drools.analytics.dao.AnalyticsResult;
+import org.drools.analytics.report.components.Redundancy;
+import org.drools.base.RuleNameMatchesAgendaFilter;
+
+public class RedundantPossibilitiesTest extends RedundancyTestBase {
+
+	public void testPatternPossibilityRedundancy() throws Exception {
+		StatelessSession session = getStatelessSession(this.getClass()
+				.getResourceAsStream("Possibilities.drl"));
+
+		AnalyticsDataFactory.clearAnalyticsData();
+		session.setAgendaFilter(new RuleNameMatchesAgendaFilter(
+				"Find pattern possibility redundancy"));
+
+		Collection<Object> data = new ArrayList<Object>();
+
+		AnalyticsDataFactory.clearAnalyticsResult();
+		AnalyticsResult result = AnalyticsDataFactory.getAnalyticsResult();
+		session.setGlobal("result", result);
+
+		String ruleName1 = "Rule 1";
+		String ruleName2 = "Rule 2";
+
+		Pattern p1 = new Pattern();
+		p1.setRuleName(ruleName1);
+		Pattern p2 = new Pattern();
+		p2.setRuleName(ruleName2);
+
+		LiteralRestriction lr1 = new LiteralRestriction();
+		lr1.setRuleName(ruleName1);
+		LiteralRestriction lr2 = new LiteralRestriction();
+		lr2.setRuleName(ruleName2);
+
+		PatternPossibility pp1 = new PatternPossibility();
+		pp1.setPatternId(p1.getId());
+		pp1.setRuleName(ruleName1);
+		pp1.add(lr1);
+
+		PatternPossibility pp2 = new PatternPossibility();
+		pp2.setPatternId(p2.getId());
+		pp2.setRuleName(ruleName2);
+		pp2.add(lr2);
+
+		Redundancy r1 = new Redundancy(lr1, lr2);
+		Redundancy r2 = new Redundancy(p1, p2);
+
+		data.add(p1);
+		data.add(p2);
+		data.add(lr1);
+		data.add(lr2);
+		data.add(pp1);
+		data.add(pp2);
+		data.add(r1);
+		data.add(r2);
+
+		StatelessSessionResult sessionResult = session.executeWithResults(data);
+
+		Map<String, Set<String>> map = createRedundancyMap(sessionResult
+				.iterateObjects());
+
+		assertTrue(TestBase.mapContains(map, ruleName1, ruleName2)
+				^ TestBase.mapContains(map, ruleName2, ruleName1));
+
+		if (!map.isEmpty()) {
+			fail("More redundancies than was expected.");
+		}
+	}
+
+	public void testRulePossibilityRedundancy() throws Exception {
+		StatelessSession session = getStatelessSession(this.getClass()
+				.getResourceAsStream("Possibilities.drl"));
+
+		AnalyticsDataFactory.clearAnalyticsData();
+		session.setAgendaFilter(new RuleNameMatchesAgendaFilter(
+				"Find rule possibility redundancy"));
+
+		Collection<Object> data = new ArrayList<Object>();
+
+		AnalyticsDataFactory.clearAnalyticsResult();
+		AnalyticsResult result = AnalyticsDataFactory.getAnalyticsResult();
+		session.setGlobal("result", result);
+
+		String ruleName1 = "Rule 1";
+		String ruleName2 = "Rule 2";
+
+		AnalyticsRule r1 = new AnalyticsRule();
+		r1.setRuleName(ruleName1);
+		AnalyticsRule r2 = new AnalyticsRule();
+		r2.setRuleName(ruleName2);
+
+		PatternPossibility pp1 = new PatternPossibility();
+		pp1.setRuleName(ruleName1);
+		PatternPossibility pp2 = new PatternPossibility();
+		pp2.setRuleName(ruleName2);
+
+		RulePossibility rp1 = new RulePossibility();
+		rp1.setRuleId(r1.getId());
+		rp1.setRuleName(ruleName1);
+		rp1.add(pp1);
+
+		RulePossibility rp2 = new RulePossibility();
+		rp2.setRuleId(r2.getId());
+		rp2.setRuleName(ruleName2);
+		rp2.add(pp2);
+
+		Redundancy possibilityredundancy = new Redundancy(
+				Redundancy.RedundancyType.STRONG, pp1, pp2);
+		Redundancy ruleRedundancy = new Redundancy(r1, r2);
+
+		data.add(r1);
+		data.add(r2);
+		data.add(pp1);
+		data.add(pp2);
+		data.add(possibilityredundancy);
+		data.add(ruleRedundancy);
+		data.add(rp1);
+		data.add(rp2);
+
+		StatelessSessionResult sessionResult = session.executeWithResults(data);
+
+		Map<String, Set<String>> map = createRedundancyMap(sessionResult
+				.iterateObjects());
+
+		assertTrue(TestBase.mapContains(map, ruleName1, ruleName2)
+				^ TestBase.mapContains(map, ruleName2, ruleName1));
+
+		if (!map.isEmpty()) {
+			fail("More redundancies than was expected.");
+		}
+	}
+}
--- kogito-drools/drools-analytics/src/test/java/org/drools/analytics/redundancy/RedundantRestrictionsTest.java
+++ kogito-drools/drools-analytics/src/test/java/org/drools/analytics/redundancy/RedundantRestrictionsTest.java
@@ -13,27 +13,15 @@ import org.drools.StatelessSessionResult;
 import org.drools.analytics.TestBase;
 import org.drools.analytics.components.AnalyticsComponent;
 import org.drools.analytics.components.AnalyticsRule;
-import org.drools.analytics.components.LiteralRestriction;
-import org.drools.analytics.components.Pattern;
 import org.drools.analytics.components.PatternPossibility;
 import org.drools.analytics.components.RulePossibility;
-import org.drools.analytics.dao.AnalyticsResult;
 import org.drools.analytics.dao.AnalyticsDataFactory;
+import org.drools.analytics.dao.AnalyticsResult;
 import org.drools.analytics.report.components.PartialRedundancy;
 import org.drools.analytics.report.components.Redundancy;
-import org.drools.analytics.report.components.Subsumption;
 import org.drools.base.RuleNameMatchesAgendaFilter;
 
-/**
- * 
- * @author Toni Rikkola
- * 
- */
-public class RedundancyTest extends TestBase {
-
-	public void testFake() {
-		assertTrue(true);
-	}
+public class RedundantRestrictionsTest extends RedundancyTestBase {
 
 	public void fixmetestPartOfRulePossibilityRedundancy() throws Exception {
 		StatelessSession session = getStatelessSession(this.getClass()
@@ -106,151 +94,13 @@ public class RedundancyTest extends TestBase {
 			}
 		}
 
-		assertTrue(RedundancyTest.mapContains(map, ruleName1 + ":" + ruleName2,
-				redundancy1));
+		assertTrue(RedundancyTestBase.mapContains(map, ruleName1 + ":"
+				+ ruleName2, redundancy1));
 
 		if (!map.isEmpty()) {
 			fail("More redundancies than was expected.");
 		}
 	}
-
-	public void fixmetestPossibilityRedundancy() throws Exception {
-		StatelessSession session = getStatelessSession(this.getClass()
-				.getResourceAsStream("Possibilities.drl"));
-
-		session.setAgendaFilter(new RuleNameMatchesAgendaFilter(
-				"Find redundant Possibilities"));
-
-		Collection<Object> data = new ArrayList<Object>();
-
-		AnalyticsResult result = AnalyticsDataFactory.getAnalyticsResult();
-		session.setGlobal("result", result);
-
-		String ruleName1 = "Rule 1";
-		String ruleName2 = "Rule 2";
-
-		PatternPossibility pp1 = new PatternPossibility();
-		pp1.setRuleName(ruleName1);
-
-		PatternPossibility pp2 = new PatternPossibility();
-		pp2.setRuleName(ruleName2);
-
-		Subsumption s1 = new Subsumption(pp1, pp2);
-		Subsumption s2 = new Subsumption(pp2, pp1);
-
-		data.add(pp1);
-		data.add(pp2);
-		data.add(s1);
-		data.add(s2);
-
-		StatelessSessionResult sessionResult = session.executeWithResults(data);
-
-		Iterator<Object> iter = sessionResult.iterateObjects();
-
-		Map<String, Set<String>> map = createRedundancyMap(iter);
-
-		assertTrue(RedundancyTest.mapContains(map, ruleName2, ruleName1));
-
-		if (!map.isEmpty()) {
-			fail("More redundancies than was expected.");
-		}
-	}
-
-	public void testPatternPossibilityRedundancy() throws Exception {
-		StatelessSession session = getStatelessSession(this.getClass()
-				.getResourceAsStream("Possibilities.drl"));
-
-		AnalyticsDataFactory.clearAnalyticsData();
-		session.setAgendaFilter(new RuleNameMatchesAgendaFilter(
-				"Find pattern possibility redundancy"));
-
-		Collection<Object> data = new ArrayList<Object>();
-
-		AnalyticsDataFactory.clearAnalyticsResult();
-		AnalyticsResult result = AnalyticsDataFactory.getAnalyticsResult();
-		session.setGlobal("result", result);
-
-		String ruleName1 = "Rule 1";
-		String ruleName2 = "Rule 2";
-
-		Pattern p1 = new Pattern();
-		p1.setRuleName(ruleName1);
-		Pattern p2 = new Pattern();
-		p2.setRuleName(ruleName2);
-
-		LiteralRestriction lr1 = new LiteralRestriction();
-		lr1.setRuleName(ruleName1);
-		LiteralRestriction lr2 = new LiteralRestriction();
-		lr2.setRuleName(ruleName2);
-
-		PatternPossibility pp1 = new PatternPossibility();
-		pp1.setPatternId(p1.getId());
-		pp1.setRuleName(ruleName1);
-		pp1.add(lr1);
-
-		PatternPossibility pp2 = new PatternPossibility();
-		pp2.setPatternId(p2.getId());
-		pp2.setRuleName(ruleName2);
-		pp2.add(lr2);
-
-		Redundancy r1 = new Redundancy(lr1, lr2);
-		Redundancy r2 = new Redundancy(p1, p2);
-
-		data.add(p1);
-		data.add(p2);
-		data.add(lr1);
-		data.add(lr2);
-		data.add(pp1);
-		data.add(pp2);
-		data.add(r1);
-		data.add(r2);
-
-		StatelessSessionResult sessionResult = session.executeWithResults(data);
-
-		Map<String, Set<String>> map = createRedundancyMap(sessionResult
-				.iterateObjects());
-
-		assertTrue(TestBase.mapContains(map, ruleName1, ruleName2)
-				^ TestBase.mapContains(map, ruleName2, ruleName1));
-
-		if (!map.isEmpty()) {
-			fail("More redundancies than was expected.");
-		}
-	}
-
-	/*
-	 * Not enough data to test this right now.
-	 */
-	/*
-	 * public void testRuleRedundancy() throws Exception { StatelessSession
-	 * session = getStatelessSession(this.getClass()
-	 * .getResourceAsStream("Redundancy.drl"));
-	 * 
-	 * session.setAgendaFilter(new RuleNameMatchesAgendaFilter( "Find redundant
-	 * Rule shells"));
-	 * 
-	 * Collection<Object> data = getTestData(this.getClass()
-	 * .getResourceAsStream("RuleRedundancyTest.drl"));
-	 * 
-	 * AnalysisResultNormal analysisResult = new AnalysisResultNormal();
-	 * session.setGlobal("result", analysisResult);
-	 * 
-	 * StatelessSessionResult sessionResult = session.executeWithResults(data);
-	 * 
-	 * Map<String, Set<String>> map = createRedundancyMap(sessionResult
-	 * .iterateObjects());
-	 * 
-	 * assertTrue(mapContains(map, "Rule redundancy 1a", "Rule redundancy 1b"));
-	 * assertTrue(mapContains(map, "Rule redundancy 1b", "Rule redundancy 1a"));
-	 * assertTrue(mapContains(map, "Rule redundancy 2a", "Rule redundancy 2b"));
-	 * assertTrue(mapContains(map, "Rule redundancy 2b", "Rule redundancy 2a"));
-	 * assertTrue(mapContains(map, "Rule redundancy 3a", "Rule redundancy 3b"));
-	 * assertTrue(mapContains(map, "Rule redundancy 3b", "Rule redundancy 3a"));
-	 * assertTrue(mapContains(map, "Rule redundancy 4a", "Rule redundancy 4b"));
-	 * assertTrue(mapContains(map, "Rule redundancy 4b", "Rule redundancy 4a"));
-	 * 
-	 * if (!map.isEmpty()) { fail("More redundancies than was expected."); } }
-	 */
 
 	public void testPatternRedundancyWithRestrictions() throws Exception {
 		StatelessSession session = getStatelessSession(this.getClass()
@@ -417,59 +267,5 @@ public class RedundancyTest extends TestBase {
 		if (!map.isEmpty()) {
 			fail("More redundancies than was expected.");
 		}
-	}
-
-	/**
-	 * Creates redundancy map from Redundancy objects, one rule may have several
-	 * redundancy dependencies.
-	 * 
-	 * @param iter
-	 * @return
-	 */
-	private Map<String, Set<String>> createRedundancyMap(Iterator<Object> iter) {
-
-		Map<String, Set<String>> map = new HashMap<String, Set<String>>();
-		while (iter.hasNext()) {
-			Object o = (Object) iter.next();
-			if (o instanceof Redundancy) {
-				Redundancy r = (Redundancy) o;
-				AnalyticsComponent left = (AnalyticsComponent) r.getLeft();
-				AnalyticsComponent right = (AnalyticsComponent) r.getRight();
-
-				if (map.containsKey(left.getRuleName())) {
-					Set<String> set = map.get(left.getRuleName());
-					set.add(right.getRuleName());
-				} else {
-					Set<String> set = new HashSet<String>();
-					set.add(right.getRuleName());
-					map.put(left.getRuleName(), set);
-				}
-			}
-		}
-
-		return map;
-	}
-
-	/**
-	 * Returns true if map contains redundancy where key is redundant to value.
-	 * 
-	 * @param map
-	 * @param key
-	 * @param value
-	 * @return True if redundancy exists.
-	 */
-	private static boolean mapContains(Map<String, Set<Redundancy>> map,
-			String key, Object value) {
-		if (map.containsKey(key)) {
-			Set<Redundancy> set = map.get(key);
-			boolean exists = set.remove(value);
-
-			// If set is empty remove key from map.
-			if (set.isEmpty()) {
-				map.remove(key);
-			}
-			return exists;
-		}
-		return false;
 	}
 }
