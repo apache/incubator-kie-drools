42251174c651f668964f773b67a05bb9ead85b71
Toni Rikkola <toni@rikkola.net>
2007-12-27 20:38:09 +0000

    Fix for broken test.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@17418 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-analytics/src/test/java/org/drools/analytics/AnalyzerTest.java
+++ kogito-drools/drools-analytics/src/test/java/org/drools/analytics/AnalyzerTest.java
@@ -12,7 +12,7 @@ import junit.framework.TestCase;
 
 public class AnalyzerTest extends TestCase {
 
-	public void fixmetestAnalyzer() throws Exception {
+	public void testAnalyzer() throws Exception {
 		Analyzer anal = new Analyzer();
 
 		DrlParser p = new DrlParser();
@@ -26,7 +26,7 @@ public class AnalyzerTest extends TestCase {
 		AnalyticsResult result = anal.getResult();
 		assertNotNull(result);
 		assertEquals(0, result.getBySeverity(AnalyticsMessageBase.Severity.ERROR).size());
-		assertEquals(17, result.getBySeverity(AnalyticsMessageBase.Severity.WARNING).size());
+		assertEquals(10, result.getBySeverity(AnalyticsMessageBase.Severity.WARNING).size());
 		assertEquals(1, result.getBySeverity(AnalyticsMessageBase.Severity.NOTE).size());
 
 
@@ -44,8 +44,7 @@ public class AnalyzerTest extends TestCase {
 		result = anal.getResult();
 		assertNotNull(result);
 		assertEquals(0, result.getBySeverity(AnalyticsMessageBase.Severity.ERROR).size());
-		// This row has a problem
-		assertEquals( 17, result.getBySeverity(AnalyticsMessageBase.Severity.WARNING).size());
+		assertEquals(10, result.getBySeverity(AnalyticsMessageBase.Severity.WARNING).size());
 		assertEquals(1, result.getBySeverity(AnalyticsMessageBase.Severity.NOTE).size());
 
 
