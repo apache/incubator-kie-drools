10f07922cd22b05cb315607ccd61fd347696a0ab
Mark Proctor <mdproctor@gmail.com>
2008-01-04 16:46:06 +0000

    JBRULES-1394 RuleFlow Nodes should be pluggable -making unfinished test compile, as build is currently broken.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@17554 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-compiler/src/test/java/org/drools/process/builder/StoreNodeBuilder.java
@@ -0,0 +1,30 @@
+package org.drools.process.builder;
+
+import org.drools.compiler.Dialect;
+import org.drools.lang.descr.ActionDescr;
+import org.drools.lang.descr.ProcessDescr;
+import org.drools.rule.builder.ProcessBuildContext;
+import org.drools.ruleflow.common.core.Process;
+import org.drools.ruleflow.core.Node;
+import org.drools.ruleflow.core.impl.ActionNodeImpl;
+import org.drools.ruleflow.core.impl.DroolsConsequenceAction;
+
+public class StoreNodeBuilder
+    implements
+    ProcessNodeBuilder {
+
+    public void build(Process process,
+                           ProcessDescr processDescr,
+                           ProcessBuildContext context,
+                           Node node) {
+        ActionNodeImpl actionNode = ( ActionNodeImpl ) node;
+        DroolsConsequenceAction action = (DroolsConsequenceAction) actionNode.getAction();
+        ActionDescr actionDescr = new ActionDescr();
+        actionDescr.setText( action.getConsequence() );   
+        
+        Dialect dialect = context.getDialectRegistry().getDialect( action.getDialect() );            
+        
+        dialect.getActionBuilder().build( context, actionNode, actionDescr );
+    }
+
+}
