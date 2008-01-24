7406d753033ffc4708302520c1f3eb01eb43d20a
Mark Proctor <mdproctor@gmail.com>
2008-01-24 21:15:43 +0000

    JBRULES-720 Clips Parser -Initial set of MVEL conversion tests are parsing.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@18087 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-clips/src/test/java/org/drools/clp/ShellTest.java
+++ kogito-drools/drools-clips/src/test/java/org/drools/clp/ShellTest.java
@@ -35,7 +35,35 @@ public class ShellTest extends TestCase {
         WorkingMemory wm = shell.getWorkingMemory();
         wm.insert( new Person("bob", "cheddar", 30) );
         wm.insert( new Person("bob", "stilton", 35) );        
-        wm.fireAllRules();
-        
+        wm.fireAllRules();        
     }
+    
+    public void test2() {
+        Shell shell = new Shell();
+        
+        shell.evalString( "(import org.drools.Person)" );
+        
+        shell.evalString( "(defrule yyy  => (printout t yy \" \" (eq 1 1) \"\n\" ) ) )" );        
+        Package pkg = shell.getWorkingMemory().getRuleBase().getPackage( "MAIN" );
+        
+        Rule rule = pkg.getRule( "yyy" );
+        assertEquals( "yyy", rule.getName() );
+        
+        shell.evalString( "(defrule xxx (Person (name ?name&bob) (age 30) ) (Person  (name ?name) (age 35)) => (printout t ?name \" \" (eq 1 1) \"\n\" ) )" );
+        
+        
+        rule = pkg.getRule( "xxx" );
+        assertEquals( "xxx", rule.getName() );
+        
+
+        
+        assertEquals( 2, pkg.getRules().length );
+        
+        assertTrue( pkg.getImports().containsKey( "org.drools.Person" ) );
+        
+        WorkingMemory wm = shell.getWorkingMemory();
+        wm.insert( new Person("bob", "cheddar", 30) );
+        wm.insert( new Person("bob", "stilton", 35) );        
+        wm.fireAllRules();        
+    }    
 }
