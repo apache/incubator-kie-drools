0dc9e0c22555ce5ce54475c65245493f4743d79a
Edson Tirelli <ed.tirelli@gmail.com>
2008-02-05 19:37:03 +0000

    JBRULES-1392: adding multi-thread test case
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@18323 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/test/java/org/drools/Child.java
+++ kogito-drools/drools-compiler/src/test/java/org/drools/Child.java
@@ -2,11 +2,27 @@ package org.drools;
 
 public class Child extends Parent {
 
+    private Parent parent;
+    
     public Child() {
     }
 
     public Child(final String name) {
         super( name );
+    }
+
+    /**
+     * @return the parent
+     */
+    public Parent getParent() {
+        return parent;
+    }
+
+    /**
+     * @param parent the parent to set
+     */
+    public void setParent(Parent parent) {
+        this.parent = parent;
     }
 
 }
