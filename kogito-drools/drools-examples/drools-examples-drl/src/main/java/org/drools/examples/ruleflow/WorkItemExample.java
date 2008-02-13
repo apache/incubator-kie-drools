63c0dfd9d7fb166c3b54dde18fe48c0c0df4839d
Kris Verlaenen <kris.verlaenen@gmail.com>
2008-02-13 16:15:25 +0000

    Sample ruleflow using work items
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@18477 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-examples/drools-examples-drl/src/main/java/org/drools/examples/ruleflow/WorkItemExample.java
@@ -0,0 +1,49 @@
+package org.drools.examples.ruleflow;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.drools.RuleBase;
+import org.drools.RuleBaseFactory;
+import org.drools.WorkingMemory;
+import org.drools.compiler.PackageBuilder;
+import org.drools.process.instance.impl.demo.SystemOutWorkItemHandler;
+import org.drools.process.instance.impl.demo.UIWorkItemHandler;
+import org.drools.rule.Package;
+
+public class WorkItemExample {
+
+    public static final void main(String[] args) {
+        try {
+            RuleBase ruleBase = readRule();
+            WorkingMemory workingMemory = ruleBase.newStatefulSession();
+            
+            // logging all work items to sysout
+            SystemOutWorkItemHandler handler = new SystemOutWorkItemHandler();
+            workingMemory.getWorkItemManager().registerWorkItemHandler("Email", handler);
+            workingMemory.getWorkItemManager().registerWorkItemHandler("Log", handler);
+            
+            // using a dialog to show all work items
+            UIWorkItemHandler handler2 = new UIWorkItemHandler();
+            //workingMemory.getWorkItemManager().registerWorkItemHandler("Email", handler2);
+            //workingMemory.getWorkItemManager().registerWorkItemHandler("Log", handler2);
+            //handler2.setVisible(true);
+            
+            workingMemory.startProcess("com.sample.ruleflow");
+            workingMemory.fireAllRules();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+	private static RuleBase readRule() throws Exception {
+		PackageBuilder builder = new PackageBuilder();
+		Reader source = new InputStreamReader( WorkItemExample.class.getResourceAsStream( "/org/drools/examples/ruleflow/workitems.rfm" ) );
+		builder.addRuleFlow(source);
+		Package pkg = builder.getPackage();
+		RuleBase ruleBase = RuleBaseFactory.newRuleBase();
+		ruleBase.addPackage( pkg );
+		return ruleBase;
+	}
+	
+}
