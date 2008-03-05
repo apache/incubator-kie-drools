cb9d1491b8c3160010fe37eb3335743cb633ef6f
Michael Neale <michael.neale@gmail.com>
2008-03-05 04:42:13 +0000

    JBRULES-1495 Properties and case sensitivity
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@18682 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-decisiontables/src/test/java/org/drools/decisiontable/parser/PropertiesSheetListenerTest.java
+++ kogito-drools/drools-decisiontables/src/test/java/org/drools/decisiontable/parser/PropertiesSheetListenerTest.java
@@ -2,13 +2,13 @@ package org.drools.decisiontable.parser;
 
 /*
  * Copyright 2005 JBoss Inc
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -60,9 +60,28 @@ public class PropertiesSheetListenerTest extends TestCase {
                           "key3", SheetListener.NON_MERGED );
 
         assertEquals( "value1",
-                      props.getProperty( "key1" ) );
+                      props.getProperty( "Key1" ) );
         assertEquals( "value2",
                       props.getProperty( "key2" ) );
 
     }
+
+    public void testCaseInsensitive() {
+    	Properties map = new PropertiesSheetListener.CaseInsensitiveMap();
+    	map.setProperty("x3", "hey");
+    	map.setProperty("x4", "wHee");
+    	map.setProperty("XXx", "hey2");
+
+    	assertNull(map.getProperty("x"));
+    	assertEquals("hey", map.getProperty("x3"));
+    	assertEquals("hey", map.getProperty("X3"));
+    	assertEquals("wHee", map.getProperty("x4"));
+    	assertEquals("hey2", map.getProperty("xxx"));
+    	assertEquals("hey2", map.getProperty("XXX"));
+    	assertEquals("hey2", map.getProperty("XXx"));
+
+    	assertEquals("Whee2", map.getProperty("x", "Whee2"));
+
+    }
+
 }
