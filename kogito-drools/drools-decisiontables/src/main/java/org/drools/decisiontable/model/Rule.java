6f39b2299457296d1bc72cc36b04ae135bd559c6
Michael Neale <michael.neale@gmail.com>
2008-03-06 00:58:39 +0000

    JBRULES-1496 Duration not rendering correctly
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@18710 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-decisiontables/src/main/java/org/drools/decisiontable/model/Rule.java
+++ kogito-drools/drools-decisiontables/src/main/java/org/drools/decisiontable/model/Rule.java
@@ -2,13 +2,13 @@ package org.drools.decisiontable.model;
 
 /*
  * Copyright 2005 JBoss Inc
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,7 +23,7 @@ import java.util.List;
 
 /**
  * @author <a href="mailto:michael.neale@gmail.com"> Michael Neale </a>
- * 
+ *
  * Represents a rule.
  */
 public class Rule extends DRLElement
@@ -36,7 +36,7 @@ public class Rule extends DRLElement
 
     private String           _name;
 
-    private Duration         _duration;       // RIK: New variable to the Rule class (Defines
+    private String         _duration;       // RIK: New variable to the Rule class (Defines
     // a Duration tag for the rule)
 
     private String           _description;    // RIK: New variable to the Rule class (Set
@@ -63,10 +63,10 @@ public class Rule extends DRLElement
     /**
      * Create a new rule. Note that the rule name should be post-fixed with the row number,
      * as one way of providing tracability for errors back to the originating spreadsheet.
-     * @param name The name of the rule. This may be used to calculate DRL row error 
+     * @param name The name of the rule. This may be used to calculate DRL row error
      * to Spreadsheet row error (just need to keep track of output lines, and map spreadsheetRow to a start
      * and end range in the rendered output).
-     * @param salience 
+     * @param salience
      * @param spreadsheetRow The phyical row number from the spreadsheet.
      */
     public Rule(final String name,
@@ -224,13 +224,13 @@ public class Rule extends DRLElement
         return this._description;
     }
 
-    public void setDuration(final Duration value) // Set the duration of the rule
+    public void setDuration(final String value) // Set the duration of the rule
     {
         this._duration = value;
     }
 
     public String getDuration() {
-        return this._duration.getSnippet();
+        return this._duration;
     }
 
     public void setActivationGroup(final String value) // Set the duration of the rule
@@ -265,7 +265,7 @@ public class Rule extends DRLElement
     }
 
     /**
-     * @return The row in the spreadsheet this represents. 
+     * @return The row in the spreadsheet this represents.
      * This can be handy when mapping a line error from Parser back to the rule row.
      * Will need to have a map of ranges of line numbers that each rule covers.
      * Then can find out the rule that cause it, and this will give the row number to report.
