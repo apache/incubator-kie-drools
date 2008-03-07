db191c975c49e42011ddb42a9d205d683b4d263d
Michael Neale <michael.neale@gmail.com>
2008-03-07 05:20:10 +0000

    JBRULES-1494 Decision table model
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@18750 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-compiler/src/main/java/org/drools/brms/client/modeldriven/dt/ActionRetractFactCol.java
@@ -0,0 +1,12 @@
+package org.drools.brms.client.modeldriven.dt;
+
+public class ActionRetractFactCol extends ActionCol {
+
+	/**
+	 * The name of the fact to be retracted.
+	 */
+	public String boundName;
+
+
+
+}
---
+++ drools-compiler/src/main/java/org/drools/brms/client/modeldriven/dt/AttributeCol.java
@@ -0,0 +1,14 @@
+package org.drools.brms.client.modeldriven.dt;
+
+import org.drools.brms.client.modeldriven.brl.PortableObject;
+
+/**
+ * This is a rule attribute - eg salience, no-loop etc.
+ * @author Michael Neale
+ *
+ */
+public class AttributeCol implements PortableObject {
+
+	public String attr;
+
+}
