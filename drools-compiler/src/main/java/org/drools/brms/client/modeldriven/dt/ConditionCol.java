7ba16dcfa2afa27578c26ba4c759a75e59d0d409
Ming Jin <ming.jin@travelocity.com>
2008-03-15 04:17:02 +0000

    recovered from merging.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@19007 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-compiler/src/main/java/org/drools/brms/client/modeldriven/dt/ConditionCol.java
+++ drools-compiler/src/main/java/org/drools/brms/client/modeldriven/dt/ConditionCol.java
@@ -2,19 +2,50 @@ package org.drools.brms.client.modeldriven.dt;
 
 import org.drools.brms.client.modeldriven.brl.PortableObject;
 
-import java.io.ObjectOutput;
-import java.io.IOException;
-import java.io.ObjectInput;
-
+/**
+ * This is the config for a condition column. Typically many of them have their constraints added.
+ *
+ * @author Michael Neale
+ *
+ */
 public class ConditionCol implements PortableObject {
 
-    public String header;
+	/**
+	 * What is displayed at the top
+	 */
+	public String header;
 
-    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-        header  = (String)in.readObject();
-    }
+	/**
+	 * The type of the fact - class - eg Driver, Person, Cheese etc.
+	 */
+	public String factType;
 
-    public void writeExternal(ObjectOutput out) throws IOException {
-        out.writeObject(header);
-    }
+	/**
+	 * The name that this gets referenced as. Multiple columns with the same name mean their constraints will be combined.
+	 */
+	public String boundName;
+
+	/**
+	 * The type of the value that is in the cell, eg if it is a formula, or literal value etc.
+	 * The valid types are from ISingleFieldConstraint:
+	 *   TYPE_LITERAL
+	 *   TYPE_RET_VALUE
+	 *   TYPE_PREDICATE (in this case, the field and operator are ignored).
+	 */
+	public int constraintValueType;
+
+
+	/**
+	 * The field of the fact that this pertains to (if its a predicate, ignore it).
+	 */
+	public String factField;
+
+
+	/**
+	 * The operator to use to compare the field with the value (unless its a predicate, in which case this is ignored).
+	 */
+	public String operator;
+
+
+
 }
