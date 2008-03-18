e6d4ecf07873f5c0b8e04fd26b7e3811afd7e081
Edson Tirelli <ed.tirelli@gmail.com>
2008-03-18 17:07:45 +0000

    JBRULES-1510: fixing copy&paste bug
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@19053 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/main/java/org/drools/util/ObjenesisFactory.java
+++ kogito-drools/drools-core/src/main/java/org/drools/util/ObjenesisFactory.java
@@ -30,7 +30,7 @@ import org.drools.objenesis.ObjenesisStd;
 public class ObjenesisFactory implements Serializable {
     
     private static final long serialVersionUID = 969174504278340793L;
-    private static final Objenesis OBJENESIS_INSTANCE = null;
+    private static Objenesis OBJENESIS_INSTANCE = null;
 
     private ObjenesisFactory() {}
     
@@ -39,7 +39,7 @@ public class ObjenesisFactory implements Serializable {
      */
     public static Objenesis getStaticObjenesis() {
         if( OBJENESIS_INSTANCE == null ) {
-            new ObjenesisStd(true);
+            OBJENESIS_INSTANCE = new ObjenesisStd(true);
         }
         return OBJENESIS_INSTANCE;
     }
