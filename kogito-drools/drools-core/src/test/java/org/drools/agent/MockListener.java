f2d288d9607634260a95fecc540f659caaea8513
Michael Neale <michael.neale@gmail.com>
2008-03-26 01:47:21 +0000

    JBRULES-1528 URLScanner wasn't reporting IO exception causes to the log when falling back to local cache.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@19228 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/test/java/org/drools/agent/MockListener.java
+++ kogito-drools/drools-core/src/test/java/org/drools/agent/MockListener.java
@@ -1,31 +1,36 @@
 package org.drools.agent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MockListener
     implements
     AgentEventListener {
 
+	public List exceptions = new ArrayList();
+
     public void debug(String message) {
-        
+
 
     }
 
     public void exception(Exception e) {
-        
+        exceptions.add(e);
 
     }
 
     public void info(String message) {
-        
+
 
     }
 
     public void setAgentName(String name) {
-        
+
 
     }
 
     public void warning(String message) {
-        
+
 
     }
 
