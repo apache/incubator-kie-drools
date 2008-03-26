2378a5d9ba462dc485da908d0ad197a83f43177f
Mark Proctor <mdproctor@gmail.com>
2008-03-26 22:59:32 +0000

    JBRULES-1520 RightTuple merge for asymmetrical Rete propagations -Fixed SchedulerTest
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@19260 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/test/java/org/drools/reteoo/SchedulerTest.java
+++ kogito-drools/drools-core/src/test/java/org/drools/reteoo/SchedulerTest.java
@@ -104,7 +104,7 @@ public class SchedulerTest extends DroolsTestCase {
                                                                        null );
 
         final LeftTuple tuple = new LeftTuple( new DefaultFactHandle( 1,
-                                                                      "cheese" ) );
+                                                                      "cheese" ), null );
 
         assertEquals( 0,
                       data.size() );
@@ -166,7 +166,7 @@ public class SchedulerTest extends DroolsTestCase {
                                                                                     rule,
                                                                                     knowledgeHelper.getActivation() );
                     final LeftTuple tuple2 = new LeftTuple( new DefaultFactHandle( 2,
-                                                                                   "cheese" ) );
+                                                                                   "cheese" ), null );
                     node.assertLeftTuple( tuple2,
                                       context2,
                                       (ReteooWorkingMemory) workingMemory );
@@ -188,7 +188,7 @@ public class SchedulerTest extends DroolsTestCase {
                                                                         null );
 
         final LeftTuple tuple1 = new LeftTuple( new DefaultFactHandle( 1,
-                                                                       "cheese" ) );
+                                                                       "cheese" ), null );
 
         node.assertLeftTuple( tuple1,
                           context1,
@@ -254,7 +254,7 @@ public class SchedulerTest extends DroolsTestCase {
                                                                                     rule,
                                                                                     knowledgeHelper.getActivation() );
                     final LeftTuple tuple2 = new LeftTuple( new DefaultFactHandle( 2,
-                                                                                   "cheese" ) );
+                                                                                   "cheese" ), null );
                     node.assertLeftTuple( tuple2,
                                       context2,
                                       (ReteooWorkingMemory) workingMemory );
@@ -276,7 +276,7 @@ public class SchedulerTest extends DroolsTestCase {
                                                                         null );
 
         final LeftTuple tuple1 = new LeftTuple( new DefaultFactHandle( 1,
-                                                                       "cheese" ) );
+                                                                       "cheese" ), null );
         node.assertLeftTuple( tuple1,
                           context1,
                           workingMemory );
