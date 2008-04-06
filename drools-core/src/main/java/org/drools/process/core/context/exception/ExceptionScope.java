0fd1ec4d82ec7c03de7c59ff838f52eebc6dae6d
Kris Verlaenen <kris.verlaenen@gmail.com>
2008-04-06 23:48:18 +0000

    JBRULES-1548: ePDL: XML-based process definition language  - Ruleflow processes now stored using nicer XML format JBRULES-1549: Workflow Context  - Context as the basis for grouping nodes JBRULES-1550: Workflow timer  - Timer node + service JBRULES-1551: Workflow human tasks  - Added initial implementation for integration human tasks
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@19431 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-core/src/main/java/org/drools/process/core/context/exception/ExceptionScope.java
@@ -0,0 +1,41 @@
+package org.drools.process.core.context.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.drools.process.core.Context;
+import org.drools.process.core.context.AbstractContext;
+
+public class ExceptionScope extends AbstractContext {
+
+    private static final long serialVersionUID = 400L;
+
+    public static final String EXCEPTION_SCOPE = "ExceptionScope";
+    
+    private Map<String, ExceptionHandler> exceptionHandlers = new HashMap<String, ExceptionHandler>();
+    
+    public String getType() {
+        return EXCEPTION_SCOPE;
+    }
+
+    public void setExceptionHandler(String exception, ExceptionHandler exceptionHandler) {
+        this.exceptionHandlers.put(exception, exceptionHandler);
+    }
+    
+    public ExceptionHandler getExceptionHandler(String exception) {
+        return this.exceptionHandlers.get(exception);
+    }
+
+    public void removeExceptionHandler(String exception) {
+        this.exceptionHandlers.remove(exception);
+    }
+
+    public Context resolveContext(Object param) {
+        if (param instanceof String) {
+            return getExceptionHandler((String) param) == null ? null : this;
+        }
+        throw new IllegalArgumentException(
+            "ExceptionScopes can only resolve exception names: " + param);
+    }
+
+}
