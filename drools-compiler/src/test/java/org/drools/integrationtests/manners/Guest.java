a20f8be6f9d8f29d476d0f49f58a89a0a5289ef8
Mark Proctor <mdproctor@gmail.com>
2008-04-07 02:38:36 +0000

    JBRULES-1552 AlphaNode Compiler -Added initially illustrative AlphaNodeCompiler -Added Manners to compiler, might as well have it there as we use it enough.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@19436 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-compiler/src/test/java/org/drools/integrationtests/manners/Guest.java
@@ -0,0 +1,54 @@
+package org.drools.integrationtests.manners;
+
+/*
+ * Copyright 2005 JBoss Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.Serializable;
+
+public class Guest implements Serializable {
+	private String name;
+
+	private Sex sex;
+
+	private Hobby hobby;
+	
+	public Guest() {
+		
+	}
+
+	public Guest(String name, Sex sex, Hobby hobby) {
+		this.name = name;
+		this.sex = sex;
+		this.hobby = hobby;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public Hobby getHobby() {
+		return this.hobby;
+	}
+
+	public Sex getSex() {
+		return this.sex;
+	}
+
+	public String toString() {
+		return "[Guest name=" + this.name + ", sex=" + this.sex + ", hobbies="
+				+ this.hobby + "]";
+	}
+}
---
+++ drools-compiler/src/test/java/org/drools/integrationtests/manners/Seating.java
@@ -0,0 +1,86 @@
+package org.drools.integrationtests.manners;
+
+/*
+ * Copyright 2005 JBoss Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.Serializable;
+
+public class Seating implements Serializable {
+	private int id, pid;
+
+	private int leftSeat, rightSeat;
+
+	private String leftGuestName, rightGuestName;
+
+	private boolean pathDone;
+	
+	public Seating() {
+		
+	}
+
+	public Seating( int id,  int pid,  boolean pathDone,
+			 int leftSeat,  String leftGuestName,
+			 int rightSeat,  String rightGuestName) {
+		super();
+		this.id = id;
+		this.pid = pid;
+		this.pathDone = pathDone;
+		this.leftSeat = leftSeat;
+		this.leftGuestName = leftGuestName;
+		this.rightSeat = rightSeat;
+		this.rightGuestName = rightGuestName;
+	}
+
+	public  boolean isPathDone() {
+		return this.pathDone;
+	}
+
+	public  void setPathDone(boolean pathDone) {
+		this.pathDone = pathDone;
+	}
+
+	public  int getId() {
+		return this.id;
+	}
+
+	public  String getLeftGuestName() {
+		return this.leftGuestName;
+	}
+
+	public  int getLeftSeat() {
+		return this.leftSeat;
+	}
+
+	public  int getPid() {
+		return this.pid;
+	}
+
+	public  String getRightGuestName() {
+		return this.rightGuestName;
+	}
+
+	public  int getRightSeat() {
+		return this.rightSeat;
+	}
+
+	public  String toString() {
+		return "[Seating id=" + this.id + " , pid=" + this.pid + " , pathDone="
+				+ this.pathDone + " , leftSeat=" + this.leftSeat
+				+ ", leftGuestName=" + this.leftGuestName + ", rightSeat="
+				+ this.rightSeat + ", rightGuestName=" + this.rightGuestName
+				+ "]";
+	}
+}
