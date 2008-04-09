dc0f56a0ee633250e6d49913a36d0606a1faae93
Michael Neale <michael.neale@gmail.com>
2008-04-09 04:42:48 +0000

    adding checks for nested object construction, date coercion
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@19502 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-compiler/src/test/java/org/drools/OuterFact.java
@@ -0,0 +1,20 @@
+package org.drools;
+
+public class OuterFact {
+
+	private String name;
+	private Cheese innerFact;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Cheese getInnerFact() {
+		return innerFact;
+	}
+	public void setInnerFact(Cheese innerFact) {
+		this.innerFact = innerFact;
+	}
+
+}
