a99fa536c29a60a227bc325e84315a80767386a7
Michael Neale <michael.neale@gmail.com>
2008-04-17 07:06:44 +0000

    fixing adding of test scenarios
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@19604 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-compiler/src/test/java/org/drools/testframework/ScenarioTest.java
+++ drools-compiler/src/test/java/org/drools/testframework/ScenarioTest.java
@@ -16,10 +16,10 @@ import org.drools.brms.client.modeldriven.testing.VerifyRuleFired;
 
 public class ScenarioTest extends TestCase {
 
-	public void testInsertAfter() {
+	public void testInsertBetween() {
 		Scenario sc = new Scenario();
 		VerifyRuleFired vf = new VerifyRuleFired();
-		sc.insertAfter(null, vf);
+		sc.insertBetween(null, vf);
 		assertEquals(1, sc.fixtures.size());
 		assertEquals(vf, sc.fixtures.get(0));
 
@@ -27,23 +27,86 @@ public class ScenarioTest extends TestCase {
 		sc.fixtures.add(vf2);
 
 		VerifyRuleFired vf3 = new VerifyRuleFired();
-		sc.insertAfter(vf, vf3);
+		sc.insertBetween(vf, vf3);
 		assertEquals(3, sc.fixtures.size());
 		assertEquals(vf, sc.fixtures.get(0));
-		assertEquals(vf3, sc.fixtures.get(1));
-		assertEquals(vf2, sc.fixtures.get(2));
+		assertEquals(vf2, sc.fixtures.get(1));
+		assertEquals(vf3, sc.fixtures.get(2));
 
 		VerifyRuleFired vf4 = new VerifyRuleFired();
-		sc.insertAfter(vf2, vf4);
+		sc.insertBetween(vf2, vf4);
 		assertEquals(4, sc.fixtures.size());
-		assertEquals(vf4, sc.fixtures.get(3));
-		assertEquals(vf2, sc.fixtures.get(2));
+		assertEquals(3, sc.fixtures.indexOf(vf4));
+		assertEquals(2, sc.fixtures.indexOf(vf3));
+		//assertEquals(vf4, sc.fixtures.get(3));
+		assertEquals(1, sc.fixtures.indexOf(vf2));
+		//assertEquals(vf2, sc.fixtures.get(2));
+		assertEquals(0, sc.fixtures.indexOf(vf));
+
 
 		VerifyRuleFired vf5 = new VerifyRuleFired();
-		sc.insertAfter(null, vf5);
+		sc.insertBetween(null, vf5);
 		assertEquals(5, sc.fixtures.size());
-		assertEquals(vf5, sc.fixtures.get(0));
+		assertEquals(4, sc.fixtures.indexOf(vf5));
+
+
+		sc = new Scenario();
+
+		sc.fixtures.add(vf);
+		ExecutionTrace ex = new ExecutionTrace();
+		sc.fixtures.add(ex);
+		sc.insertBetween(null, vf2);
+		assertEquals(0, sc.fixtures.indexOf(vf));
+		assertEquals(1, sc.fixtures.indexOf(vf2));
+		assertEquals(2, sc.fixtures.indexOf(ex));
+		assertEquals(3, sc.fixtures.size());
+
+		sc.insertBetween(ex, vf3);
+		assertEquals(4, sc.fixtures.size());
+		assertEquals(0, sc.fixtures.indexOf(vf));
+		assertEquals(1, sc.fixtures.indexOf(vf2));
+		assertEquals(2, sc.fixtures.indexOf(ex));
+		assertEquals(3, sc.fixtures.indexOf(vf3));
+
+
+		ExecutionTrace ex2 = new ExecutionTrace();
+		sc.fixtures.add(ex2);
+		sc.insertBetween(ex, vf4);
+		assertEquals(6, sc.fixtures.size());
+		assertEquals(0, sc.fixtures.indexOf(vf));
+		assertEquals(1, sc.fixtures.indexOf(vf2));
+		assertEquals(2, sc.fixtures.indexOf(ex));
+		assertEquals(3, sc.fixtures.indexOf(vf3));
+		assertEquals(4, sc.fixtures.indexOf(vf4));
+		assertEquals(5, sc.fixtures.indexOf(ex2));
+
+
+		sc.insertBetween(ex2, vf5);
+		assertEquals(7, sc.fixtures.size());
+		assertEquals(0, sc.fixtures.indexOf(vf));
+		assertEquals(1, sc.fixtures.indexOf(vf2));
+		assertEquals(2, sc.fixtures.indexOf(ex));
+		assertEquals(3, sc.fixtures.indexOf(vf3));
+		assertEquals(4, sc.fixtures.indexOf(vf4));
+		assertEquals(5, sc.fixtures.indexOf(ex2));
+		assertEquals(6, sc.fixtures.indexOf(vf5));
+
+
+		sc = new Scenario();
+		sc.fixtures.add(ex);
+
+		sc.insertBetween(null, vf);
+		assertEquals(2, sc.fixtures.size());
+		assertEquals(0, sc.fixtures.indexOf(vf));
+		assertEquals(1, sc.fixtures.indexOf(ex));
+
+
+
+
+
 	}
+
+
 
 	public void testRemoveFixture() {
 		Scenario sc = new Scenario();
