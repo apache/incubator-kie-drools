81f4e2c3c1269e1a3c296a8efe5f2db0a2d3c5a1
Mark Proctor <mdproctor@gmail.com>
2008-05-03 18:09:14 +0000

    JBRULES-1572 sometimes getting ClassFieldExtractor error -sychronized the enqueue and dequeue methods
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@19848 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/main/java/org/drools/util/BinaryHeapQueue.java
+++ kogito-drools/drools-core/src/main/java/org/drools/util/BinaryHeapQueue.java
@@ -129,7 +129,7 @@ public class BinaryHeapQueue
      *
      * @param element the Queueable to be inserted
      */
-    public void enqueue(final Queueable element) {
+    public synchronized void enqueue(final Queueable element) {
         if ( isFull() ) {
             grow();
         }
@@ -143,7 +143,7 @@ public class BinaryHeapQueue
      * @return the Queueable at top of heap
      * @throws NoSuchElementException if <code>isEmpty() == true</code>
      */
-    public Queueable dequeue() throws NoSuchElementException {
+    public synchronized Queueable dequeue() throws NoSuchElementException {
         if ( isEmpty() ) {
             return null;
         }
@@ -168,7 +168,7 @@ public class BinaryHeapQueue
      *
      * @param index
      */
-    public Queueable dequeue(final int index) {
+    public synchronized Queueable dequeue(final int index) {
         if ( index < 1 || index > this.size ) {
             //throw new NoSuchElementException();
             return null;
