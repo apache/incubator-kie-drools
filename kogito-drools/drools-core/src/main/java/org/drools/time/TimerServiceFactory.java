0c6ea1b6badf15b7eb6b7d34a5bd4e77f9f86063
Mark Proctor <mdproctor@gmail.com>
2008-05-29 19:55:38 +0000

    JBRULES-1625 Create Scheduler API with JDK implementation -Basic refactoring for for TimerService, instead of Scheduler
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@20222 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/main/java/org/drools/time/TimerServiceFactory.java
+++ kogito-drools/drools-core/src/main/java/org/drools/time/TimerServiceFactory.java
@@ -2,10 +2,10 @@ package org.drools.time;
 
 import org.drools.time.impl.JDKTimerService;
 
-public class SchedulerFactory {
+public class TimerServiceFactory {
     private static TimerService timerService = new JDKTimerService();
     
-    public static TimerService getScheduler() {
+    public static TimerService getTimerService() {
         return timerService;
     }
 }
