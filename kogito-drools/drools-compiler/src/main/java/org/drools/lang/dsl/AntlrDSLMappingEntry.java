011b90a104350624754a0b13acb1c2bc5ceff970
Edson Tirelli <ed.tirelli@gmail.com>
2008-06-09 18:26:19 +0000

    Fixing jsr94 errors
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@20384 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/main/java/org/drools/lang/dsl/AntlrDSLMappingEntry.java
+++ kogito-drools/drools-compiler/src/main/java/org/drools/lang/dsl/AntlrDSLMappingEntry.java
@@ -26,120 +26,128 @@ import java.util.regex.Pattern;
  * 
  * @author mattgeis
  */
-public class AntlrDSLMappingEntry extends AbstractDSLMappingEntry implements DSLMappingEntry {
-	private boolean headMatchGroupAdded = false;
-	private boolean tailMatchGroupAdded = false;
-	
-	public AntlrDSLMappingEntry() {
-		this(DSLMappingEntry.ANY, DSLMappingEntry.EMPTY_METADATA, null, null);
-	}
+public class AntlrDSLMappingEntry extends AbstractDSLMappingEntry
+    implements
+    DSLMappingEntry {
+    private boolean headMatchGroupAdded = false;
+    private boolean tailMatchGroupAdded = false;
 
-	public AntlrDSLMappingEntry(final Section section, final MetaData metadata,
-			final String key, final String value) {
-		this.section = section;
-		this.metadata = metadata;
-		this.setMappingKey(key);
-		this.setMappingValue(value);
-	}
+    public AntlrDSLMappingEntry() {
+        this( DSLMappingEntry.ANY,
+              DSLMappingEntry.EMPTY_METADATA,
+              null,
+              null );
+    }
 
-	
-	/**
-	 * @param key
-	 *            the key to set
-	 */
-	public void setMappingKey(String key) {
-		//the "key" in this case is already mostly formed into 
-		//a pattern by ANTLR, and just requires a bit of post-processing.
-		if (key != null) {
-			key = key.trim();
-		}
-		this.key = key;
+    public AntlrDSLMappingEntry(final Section section,
+                                final MetaData metadata,
+                                final String key,
+                                final String value) {
+        this.section = section;
+        this.metadata = metadata;
+        this.setMappingKey( key );
+        this.setMappingValue( value );
+    }
 
-		if (key != null) {
-			int substr = 0;
-			// escape '$' to avoid errors
-			//final String escapedKey = key.replaceAll("\\$", "\\\\\\$");
-			// retrieving variables list and creating key pattern
-			final StringBuffer buf = new StringBuffer();
+    /**
+     * @param key
+     *            the key to set
+     */
+    public void setMappingKey(String key) {
+        //the "key" in this case is already mostly formed into 
+        //a pattern by ANTLR, and just requires a bit of post-processing.
+        if ( key != null ) {
+            key = key.trim();
+        }
+        this.key = key;
 
-			if (!key.startsWith("^")) {
-				// making it start with a space char or a line start
-				buf.append("(\\W|^)").append(key);
-				redistributeVariables();
-				headMatchGroupAdded = true;
-			}
+        if ( key != null ) {
+            int substr = 0;
+            // escape '$' to avoid errors
+            //final String escapedKey = key.replaceAll("\\$", "\\\\\\$");
+            // retrieving variables list and creating key pattern
+            final StringBuffer buf = new StringBuffer();
 
+            if ( !key.startsWith( "^" ) ) {
+                // making it start with a space char or a line start
+                buf.append( "(\\W|^)" ).append( key );
+                redistributeVariables();
+                headMatchGroupAdded = true;
+            }
 
-			// if pattern ends with a pure variable whose pattern could create
-			// a greedy match, append a line end to avoid multiple line matching
-			if (buf.toString().endsWith("(.*?)")) {
-				buf.append("$");
-			} else {
-				buf.append("(\\W|$)");
-				tailMatchGroupAdded = true;
-			}
+            // if pattern ends with a pure variable whose pattern could create
+            // a greedy match, append a line end to avoid multiple line matching
+            if ( buf.toString().endsWith( "(.*?)" ) ) {
+                buf.append( "$" );
+            } else {
+                buf.append( "(\\W|$)" );
+                tailMatchGroupAdded = true;
+            }
 
-			// setting the key pattern and making it space insensitive
-			String pat = buf.toString();
-			//first, look to see if it's 
-			if (key.substring(substr).trim().startsWith("-")
-					&& (!key.substring(substr).trim().startsWith("-\\s*"))) {
-				pat = pat.substring(0, pat.indexOf('-') + 1) + "\\s*"
-						+ pat.substring(pat.indexOf('-') + 1).trim();
-			}
-			//may not need to do this at all
-			//pat = pat.replaceAll("\\s+", "\\\\s+");
-			this.keyPattern = Pattern.compile(pat, Pattern.DOTALL
-					| Pattern.MULTILINE);
+            // setting the key pattern and making it space insensitive
+            String pat = buf.toString();
+            //first, look to see if it's 
+            if ( key.substring( substr ).trim().startsWith( "-" ) && (!key.substring( substr ).trim().startsWith( "-\\s*" )) ) {
+                pat = pat.substring( 0,
+                                     pat.indexOf( '-' ) + 1 ) + "\\s*" + pat.substring( pat.indexOf( '-' ) + 1 ).trim();
+            }
+            //may not need to do this at all
+            //pat = pat.replaceAll("\\s+", "\\\\s+");
+            this.keyPattern = Pattern.compile( pat,
+                                               Pattern.DOTALL | Pattern.MULTILINE );
 
-		} else {
-			this.keyPattern = null;
-		}
-		// update value mapping
-		//this.setMappingValue(this.value);
-	}
-	
-	/**
-	 * The keys for this map are integers, starting at 1.  However,
-	 * in certain cases we insert a matching group at the start of the
-	 * pattern, which means that 1 should become 2, 2 become 3, etc.
-	 */
-	private void redistributeVariables(){
-		for (Iterator it = variables.entrySet().iterator(); it.hasNext();) {
-			Map.Entry entry = (Map.Entry) it.next();
-			Integer i = (Integer)entry.getValue();
-			variables.put(entry.getKey(), new Integer(i.intValue() + 1));
-		}
-	}
+        } else {
+            this.keyPattern = null;
+        }
+        // update value mapping
+        //this.setMappingValue(this.value);
+    }
 
-	/**
-	 * @param value
-	 *            the value to set
-	 */
-	public void setMappingValue(final String value) {
-		StringBuffer valuePatternBuffer = new StringBuffer();
-		StringBuffer valueBuffer = new StringBuffer();
-		if(headMatchGroupAdded){
-			valuePatternBuffer.append("$1");
-			valueBuffer.append("$1");
-		}
-		valuePatternBuffer.append(value);
-		valueBuffer.append(value);
-		if(tailMatchGroupAdded){
-			int maxGroupIndex = 0;
-			if(!variables.isEmpty()){
-				Integer tailMatchGroupIndex = (Integer) Collections.max(variables.values());
-				maxGroupIndex = tailMatchGroupIndex.intValue();
-			}else if(headMatchGroupAdded){
-				//if empty, but head group matched, set max group to 1
-				maxGroupIndex++;
-			}
-			maxGroupIndex++;
-			valuePatternBuffer.append("$" + maxGroupIndex);
-			valueBuffer.append("$" + maxGroupIndex);
-		}
-		this.valuePattern = valuePatternBuffer.toString();
-		this.value = valueBuffer.toString();
-		
-	}
+    /**
+     * The keys for this map are integers, starting at 1.  However,
+     * in certain cases we insert a matching group at the start of the
+     * pattern, which means that 1 should become 2, 2 become 3, etc.
+     */
+    private void redistributeVariables() {
+        for ( Iterator it = variables.entrySet().iterator(); it.hasNext(); ) {
+            Map.Entry entry = (Map.Entry) it.next();
+            Integer i = (Integer) entry.getValue();
+            variables.put( entry.getKey(),
+                           new Integer( i.intValue() + 1 ) );
+        }
+    }
+
+    /**
+     * @param value
+     *            the value to set
+     */
+    public void setMappingValue(String value) {
+        if ( value != null ) {
+            StringBuffer valuePatternBuffer = new StringBuffer();
+            StringBuffer valueBuffer = new StringBuffer();
+
+            if ( headMatchGroupAdded ) {
+                valuePatternBuffer.append( "$1" );
+                valueBuffer.append( "$1" );
+            }
+            valuePatternBuffer.append( value );
+            valueBuffer.append( value );
+            if ( tailMatchGroupAdded ) {
+                int maxGroupIndex = 0;
+                if ( !variables.isEmpty() ) {
+                    Integer tailMatchGroupIndex = (Integer) Collections.max( variables.values() );
+                    maxGroupIndex = tailMatchGroupIndex.intValue();
+                } else if ( headMatchGroupAdded ) {
+                    //if empty, but head group matched, set max group to 1
+                    maxGroupIndex++;
+                }
+                maxGroupIndex++;
+                valuePatternBuffer.append( "$" + maxGroupIndex );
+                valueBuffer.append( "$" + maxGroupIndex );
+            }
+            this.valuePattern = valuePatternBuffer.toString();
+            this.value = valueBuffer.toString();
+        }
+
+    }
 }
