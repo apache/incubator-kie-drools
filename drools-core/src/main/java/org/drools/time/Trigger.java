a32492f687d05a58c323c0e7b71e7827af582dd7
Mark Proctor <mdproctor@gmail.com>
2008-06-09 22:41:10 +0000

    JBRULES-1625 Create Scheduler API with JDK implementation -actually decided not to use Externalisable at all now.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@20388 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/main/java/org/drools/time/Trigger.java
+++ drools-core/src/main/java/org/drools/time/Trigger.java
@@ -3,6 +3,6 @@ package org.drools.time;
 import java.io.Externalizable;
 import java.util.Date;
 
-public interface Trigger extends Externalizable {
+public interface Trigger {
     public Date getNextFireTime();
 }
