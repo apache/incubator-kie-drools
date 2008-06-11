d6ae8aee902c9e1251424bb347c64aa51a95add3
Matt Geis <mgeis@yahoo.com>
2008-06-11 00:18:54 +0000

    JBRULES-1633: Parse entire DSL file in one go, rather than line by line.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@20411 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/main/java/org/drools/lang/dsl/DSLTokenizedMappingFile.java
+++ kogito-drools/drools-compiler/src/main/java/org/drools/lang/dsl/DSLTokenizedMappingFile.java
@@ -1,9 +1,7 @@
 package org.drools.lang.dsl;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.io.StringReader;
 import java.util.LinkedList;
 import org.antlr.runtime.ANTLRReaderStream;
 import org.antlr.runtime.CharStream;
@@ -13,50 +11,41 @@ import org.antlr.runtime.tree.CommonTree;
 import org.antlr.runtime.tree.CommonTreeNodeStream;
 
 public class DSLTokenizedMappingFile extends DSLMappingFile {
+	
+	
 	public DSLTokenizedMappingFile() {
         super();
     }
 	
 	public boolean parseAndLoad(final Reader dsl) throws IOException {
-        String line = null;
-        int linecounter = 0;
-        final BufferedReader dslFileReader = new BufferedReader( dsl );
-        this.mapping = new DefaultDSLMapping();
         this.errors = new LinkedList();
-        //Note: Use a string builder for 1.5 targets
-        StringBuffer sb = new StringBuffer();
-        while ( (line = dslFileReader.readLine()) != null ) {
-            linecounter++;
-            String trimmedline = line.trim() + "\n"; //this can be more efficient, get rid of trim(), iterate-- over last chars only.
-            try{
-            	final DSLMappingEntry entry = buildEntry(trimmedline);
-            	if(entry != null) this.mapping.addEntry(entry);//will be null if a comment
-            }catch(Exception re){
-            	final String error = "Error parsing mapping entry: " + line;
-            	final DSLMappingParseException exception = 
-            		new DSLMappingParseException( error, linecounter );
-            	this.errors.add( exception );
-            }
-        }
-        return this.errors.isEmpty();
+		try{
+			this.mapping = buildFileMapping(dsl);		
+		}catch(Exception e){
+			final String error = "Error parsing mapping file: " + e.getMessage();
+        	final DSLMappingParseException exception = 
+        		new DSLMappingParseException( error, -1 );
+        	this.errors.add( exception );
+		}
+		return this.errors.isEmpty();
     }
 	
-	private DSLMappingEntry buildEntry(String line) throws IOException, RecognitionException{
-		StringReader sr = new StringReader(line);
-		ANTLRReaderStream reader = new ANTLRReaderStream(sr);
-		DSLMapWalker walker = buildEntryWalker(reader);
-		DSLMappingEntry entry = walker.valid_entry();
-		return entry;
+	private DSLMapping buildFileMapping(Reader dsl) throws IOException, RecognitionException{
+		ANTLRReaderStream reader = new ANTLRReaderStream(dsl);
+		DSLMapWalker walker = buildFileMappingWalker(reader);
+		DSLMapping mapping = walker.mapping_file();
+		return mapping;
 	}
 	
-	private DSLMapWalker buildEntryWalker(CharStream stream) throws RecognitionException{
+	private DSLMapWalker buildFileMappingWalker(CharStream stream) throws RecognitionException{
 		DSLMapLexer lexer = new DSLMapLexer(stream);
 		CommonTokenStream tokens = new CommonTokenStream();
 		tokens.setTokenSource(lexer);
 		DSLMapParser parser = new DSLMapParser(tokens);
-		DSLMapParser.statement_return example = parser.statement();
+		DSLMapParser.mapping_file_return example = parser.mapping_file();
+		this.errors.addAll(parser.getErrorList());
 		CommonTree tree = (CommonTree) example.getTree();
-		//System.out.println(tree.toStringTree());
+//		System.out.println(tree.toStringTree());
 		
 		CommonTreeNodeStream nodes = new CommonTreeNodeStream(tree);
 		DSLMapWalker walker = new DSLMapWalker(nodes);
