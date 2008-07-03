3c10079e8f8e08df7a74aa2faedffd7c0ed6bcbc
Kris Verlaenen <kris.verlaenen@gmail.com>
2008-07-03 08:26:27 +0000

    - Order example
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@20894 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-examples/drools-examples-drl/src/main/java/org/drools/examples/process/order/ShippingWorkItemHandler.java
@@ -0,0 +1,33 @@
+package org.drools.examples.process.order;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.drools.WorkingMemory;
+import org.drools.process.instance.WorkItem;
+import org.drools.process.instance.WorkItemHandler;
+import org.drools.process.instance.WorkItemManager;
+
+public class ShippingWorkItemHandler implements WorkItemHandler {
+
+	private WorkingMemory workingMemory;
+	private int trackingCounter;
+	
+	public ShippingWorkItemHandler(WorkingMemory workingMemory) {
+		this.workingMemory = workingMemory;
+	}
+	
+	public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+		Order order = (Order) workItem.getParameter("order");
+		System.out.println("Shipping order " + order.getOrderId());
+		order.setTrackingId("Tracking-" + ++trackingCounter);
+		workingMemory.update(workingMemory.getFactHandle(order), order);
+		Map<String, Object> results = new HashMap<String, Object>();
+		results.put("trackingId", order.getTrackingId());
+		manager.completeWorkItem(workItem.getId(), results);
+	}
+
+	public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
+	}
+
+}
