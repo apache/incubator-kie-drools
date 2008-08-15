d8da4a0bb64c508649b4ee3c11ef9dd14fa2664a
Mark Proctor <mdproctor@gmail.com>
2008-08-15 02:39:09 +0000

    JBRULES-1734 Drools-API -added LoadProvider
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@21558 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-api/drools-api/src/main/java/org/drools/KnowledgeBaseFactory.java
+++ kogito-api/drools-api/src/main/java/org/drools/KnowledgeBaseFactory.java
@@ -1,5 +1,7 @@
 package org.drools;
 
+import org.drools.util.ChainedProperties;
+
 public class KnowledgeBaseFactory {
     private static KnowledgeBaseProvider provider;
     
@@ -8,6 +10,28 @@ public class KnowledgeBaseFactory {
     }
     
     public static KnowledgeBase newKnowledgeBase() {
+        if ( provider == null ) {
+            loadProvider();
+        }        
         return provider.newKnowledgeBase();
     }
+    
+    private static void loadProvider() {
+        try {
+            ChainedProperties properties = new ChainedProperties( "drools-providers.conf" );
+            String className = properties.getProperty( "KnowledgeSessionProvider", null );
+            if ( className != null || className.trim().length() > 0 ) {
+                Class<KnowledgeBaseProvider> cls = ( Class<KnowledgeBaseProvider> ) Class.forName( className );
+                setKnowledgeBaseProvider( cls.newInstance() );
+            }
+        } catch ( Exception e1 ) {
+            try {
+                // we didn't find anything in properties so lets try and us reflection
+                Class<KnowledgeBaseProvider> cls = ( Class<KnowledgeBaseProvider> ) Class.forName( "org.drools.KnowledgeBaseProviderImpl" );
+                setKnowledgeBaseProvider( cls.newInstance() );
+            } catch ( Exception e2 ) {
+                throw new ProviderInitializationException( "Provider was not set and the Factory was unable to load a provider from properties, nor could reflection find org.drools.KnowledgeBaseProviderImpl." );
+            }
+        }
+    }    
 }
