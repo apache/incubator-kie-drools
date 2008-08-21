4f21b52e5071f27cfd7c098034e92ba9dd8f0577
Alexandre Porcelli <porcelli@localhost>
2008-08-21 19:43:56 +0000

    JBRULES-1689: renamed file
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@21666 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-compiler/src/main/java/org/drools/lang/DroolsParaphraseTypes.java
@@ -0,0 +1,11 @@
+package org.drools.lang;
+
+/**
+ * Simple enum to identify a paraphrase type. This enum is used to better format
+ * error messages during parsing.
+ * 
+ * @author porcelli
+ */
+public enum DroolsParaphraseTypes {
+	PACKAGE, IMPORT, FUNCTION_IMPORT, GLOBAL, FUNCTION, QUERY, TEMPLATE, RULE, RULE_ATTRIBUTE, PATTERN, TYPE_DECLARE;
+}
