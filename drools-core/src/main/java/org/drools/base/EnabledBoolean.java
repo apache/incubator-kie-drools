c4691583426b92eaee0c64deaa088301acdce4c2
Edson Tirelli <ed.tirelli@gmail.com>
2008-10-17 21:51:44 +0000

    JBRULES-1805: fixing multithread bug
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@23501 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-core/src/main/java/org/drools/base/EnabledBoolean.java
@@ -0,0 +1,52 @@
+package org.drools.base;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+import org.drools.WorkingMemory;
+import org.drools.spi.Enabled;
+import org.drools.spi.Tuple;
+
+public class EnabledBoolean
+    implements
+    Enabled,
+    Externalizable {
+
+    /**
+     *
+     */
+    private static final long   serialVersionUID = 400L;
+
+    public static final Enabled ENABLED_TRUE  = new EnabledBoolean( true );
+    public static final Enabled ENABLED_FALSE  = new EnabledBoolean( false );
+
+    private boolean             value;
+
+    public EnabledBoolean() {
+    }
+
+    public EnabledBoolean(boolean value) {
+        this.value = value;
+    }
+
+    public void readExternal(ObjectInput in) throws IOException,
+                                            ClassNotFoundException {
+        value = in.readBoolean();
+    }
+
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeBoolean( value );
+    }
+
+    public boolean getValue(final Tuple tuple,
+                            final WorkingMemory workingMemory) {
+        return this.value;
+    }
+
+    public String toString() {
+        return String.valueOf( this.value );
+    }
+
+}
