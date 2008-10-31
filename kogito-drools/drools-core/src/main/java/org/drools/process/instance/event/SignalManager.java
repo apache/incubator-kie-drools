83ffeec78db10a07fd6e8f58e02241219795ef41
Kris Verlaenen <kris.verlaenen@gmail.com>
2008-10-31 17:06:12 +0000

    JBRULES-1755: Add default process persistence / transaction support  - timers are now also executed as part of a transaction
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@23658 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/main/java/org/drools/process/instance/event/SignalManager.java
+++ kogito-drools/drools-core/src/main/java/org/drools/process/instance/event/SignalManager.java
@@ -1,10 +1,13 @@
 package org.drools.process.instance.event;
 
 import org.drools.process.instance.EventListener;
+import org.drools.process.instance.ProcessInstance;
 
 public interface SignalManager {
 	
 	void signalEvent(String type, Object event);
+	
+	void signalEvent(ProcessInstance processInstance, String type, Object event);
 	
 	void addEventListener(String type, EventListener eventListener);
 	
