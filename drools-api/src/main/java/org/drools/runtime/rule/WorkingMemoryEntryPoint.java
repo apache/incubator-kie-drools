82a15647e2fbf146b793b319f8f0e30cd1089f8e
Mark Proctor <mdproctor@gmail.com>
2008-11-08 06:57:27 +0000

    JBRULES-1734 Drools API -get/set property is now working for SessionConfiguration
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@23786 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-api/src/main/java/org/drools/runtime/rule/WorkingMemoryEntryPoint.java
+++ drools-api/src/main/java/org/drools/runtime/rule/WorkingMemoryEntryPoint.java
@@ -3,7 +3,7 @@ package org.drools.runtime.rule;
 public interface WorkingMemoryEntryPoint {
 
     FactHandle insert(Object object);
-
+    
     void retract(FactHandle handle);
 
     void update(FactHandle handle,
