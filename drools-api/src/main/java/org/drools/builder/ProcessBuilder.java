1ff672e380aca637b1a27ff3c041d854dc68eef0
Mark Proctor <mdproctor@gmail.com>
2008-11-09 06:58:51 +0000

    JBRULES-1734 Drools API -KnowledgeBuilder.addResource now works
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@23787 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-api/src/main/java/org/drools/builder/ProcessBuilder.java
+++ drools-api/src/main/java/org/drools/builder/ProcessBuilder.java
@@ -4,7 +4,5 @@ import java.io.Reader;
 import java.net.URL;
 
 public interface ProcessBuilder {
-    void addProcessFromXml(URL url);
-    void addProcessFromXml(Reader reader);
-    
+
 }
--- drools-api/src/main/java/org/drools/builder/RuleBuilder.java
+++ drools-api/src/main/java/org/drools/builder/RuleBuilder.java
@@ -4,9 +4,5 @@ import java.io.Reader;
 import java.net.URL;
 
 public interface RuleBuilder {
- 
-    public void addPackageFromDrl(URL url);
-    public void addPackageFromDrl(Reader reader);
-    public void addPackageFromXml(URL url);
-    public void addPackageFromXml(Reader reader);    
+    
 }
