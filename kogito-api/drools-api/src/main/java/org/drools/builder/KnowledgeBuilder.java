c78c26462cb08f07830a97110d30828a8519034d
Mark Proctor <mdproctor@gmail.com>
2008-11-09 07:38:51 +0000

    JBRULES-1734 Drools API -DSLs now work -In the process of adding Error handling -Fixed api issue for Task
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@23789 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-api/drools-api/src/main/java/org/drools/builder/KnowledgeBuilder.java
+++ kogito-api/drools-api/src/main/java/org/drools/builder/KnowledgeBuilder.java
@@ -7,8 +7,22 @@ import java.util.Collection;
 import org.drools.definition.KnowledgePackage;
 
 public interface KnowledgeBuilder extends RuleBuilder, ProcessBuilder {
-    public void addResource(URL url, KnowledgeType type);    
-    public void addResource(Reader reader, KnowledgeType type);
+    void addResource(URL url, KnowledgeType type);    
+    void addResource(Reader reader, KnowledgeType type);
     
+    /**
+     * Returns the built packages.
+     * 
+     * If the KnowledgeBuilder has errors the Collection will be empty. The hasErrors()
+     * method should always be checked first, to make sure you are getting the packages
+     * that you wanted built.
+     * 
+     * @return
+     *     The Collection of KnowledgePackages
+     */
 	Collection<KnowledgePackage> getKnowledgePackages();
+	
+	boolean hasErrors();
+	
+	
 }
--- kogito-api/drools-api/src/main/java/org/drools/builder/KnowledgeType.java
+++ kogito-api/drools-api/src/main/java/org/drools/builder/KnowledgeType.java
@@ -1,5 +1,21 @@
 package org.drools.builder;
 
 public enum KnowledgeType {
-    DRL, DSLR, DSL, XDRL, DRF, XLS;
+    /** Drools Rule Language */
+    DRL, 
+    
+    /** Drools DSL Rule */    
+    DSLR, 
+    
+    /** Drools DSL */    
+    DSL, 
+    
+    /** Drools XML Rule Language */
+    XDRL, 
+    
+    /** Drools Rule Flow Language */
+    DRF, 
+    
+    /** Excel Decision Table */
+    XLS;
 }
