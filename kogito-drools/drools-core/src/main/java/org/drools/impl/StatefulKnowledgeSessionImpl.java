e46f2523c0706d3f128a8648063aefdd7ed8c881
Toni Rikkola <toni@rikkola.net>
2008-11-15 01:04:35 +0000

    JBRULES-1734 Drools API Added a missing dispose() to StatefulKnowledgeSession.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@23880 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/main/java/org/drools/impl/StatefulKnowledgeSessionImpl.java
+++ kogito-drools/drools-core/src/main/java/org/drools/impl/StatefulKnowledgeSessionImpl.java
@@ -167,6 +167,10 @@ public class StatefulKnowledgeSessionImpl
         this.session.setFocus( agendaGroup );
     }
 
+    public void dispose() {
+        this.session.dispose();
+    }
+    
     public FactHandle insert(Object object) {
         return this.session.insert( object );
     }
