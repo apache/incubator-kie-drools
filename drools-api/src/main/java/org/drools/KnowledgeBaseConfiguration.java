78e19c7990f08a0c7fe68301f89c2fecf7d5b926
Mark Proctor <mdproctor@gmail.com>
2008-11-19 10:19:31 +0000

    JBRULES-1734 Drools API -started up to update javadocs
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@23951 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-api/src/main/java/org/drools/KnowledgeBaseConfiguration.java
+++ drools-api/src/main/java/org/drools/KnowledgeBaseConfiguration.java
@@ -1,45 +1,56 @@
 package org.drools;
 
 /**
- * KnowledgeBaseConfiguration
- *
+ *<p>
  * A class to store KnowledgeBase related configuration. It must be used at KnowledgeBase instantiation time
  * or not used at all.
- * 
+ * </p>
+ *
+ * <p>
  * This class will automatically load default values from a number of places, accumulating properties from each location.
  * This list of locations, in given priority is:
  * System properties, home directory, working directory, META-INF/ of optionally provided classLoader
  * META-INF/ of Thread.currentThread().getContextClassLoader() and META-INF/ of  ClassLoader.getSystemClassLoader()
+ * </p>
  * 
+ * <p>
  * So if you want to set a default configuration value for all your new KnowledgeBase, you can simply set the property as
  * a System property.
- *
+ * </p>
+ * 
+ * <p>
  * After the KnowledgeBase is created, it makes the configuration immutable and there is no way to make it
  * mutable again. This is to avoid inconsistent behaviour inside KnowledgeBase.
+ * </p>
  * 
+ * <p>
  * The following properties are supported:
- *
- * drools.maintainTms = <true|false>
- * drools.assertBehaviour = <identity|equality>
- * drools.logicalOverride = <discard|preserve>
- * drools.sequential = <true|false>
- * drools.sequential.agenda = <sequential|dynamic>
- * drools.removeIdentities = <true|false>
- * drools.shareAlphaNodes  = <true|false>
- * drools.shareBetaNodes = <true|false>
- * drools.alphaMemory = <true/false>
- * drools.alphaNodeHashingThreshold = <1...n>
- * drools.compositeKeyDepth  = <1..3>
- * drools.indexLeftBetaMemory = <true/false>
- * drools.indexRightBetaMemory = <true/false>
- * drools.consequenceExceptionHandler = <qualified class name>
+ * <ul>
+ * <li>drools.maintainTms = <true|false></li>
+ * <li>drools.assertBehaviour = <identity|equality></li>
+ * <li>drools.logicalOverride = <discard|preserve></li>
+ * <li>drools.sequential = <true|false></li>
+ * <li>drools.sequential.agenda = <sequential|dynamic></li>
+ * <li>drools.removeIdentities = <true|false></li>
+ * <li>drools.shareAlphaNodes  = <true|false></li>
+ * <li>drools.shareBetaNodes = <true|false></li>
+ * <li>drools.alphaMemory = <true/false></li>
+ * <li>drools.alphaNodeHashingThreshold = <1...n></li>
+ * <li>drools.compositeKeyDepth  = <1..3></li>
+ * <li>drools.indexLeftBetaMemory = <true/false></li>
+ * <li>drools.indexRightBetaMemory = <true/false></li>
+ * <li>drools.consequenceExceptionHandler = <qualified class name></li>
+ * </ul>
+ * </p>
  * 
+ * <p>
  * The follow properties have not yet been migrated from the Drools 4.0 api:
- *
- * drools.executorService = <qualified class name>
- * drools.conflictResolver = <qualified class name>
- * drools.ruleBaseUpdateHandler = <qualified class name>
- *  
+ * <ul>
+ * <li>drools.executorService = <qualified class name></li>
+ * <li>drools.conflictResolver = <qualified class name></li>
+ * <li>drools.ruleBaseUpdateHandler = <qualified class name></li>
+ * </ul>
+ * </p>
  */
 public interface KnowledgeBaseConfiguration
     extends
