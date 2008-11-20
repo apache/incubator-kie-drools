1e5312d0a61704698c45710860c3d4763f21de9b
Mark Proctor <mdproctor@gmail.com>
2008-11-20 15:53:05 +0000

    -more javadocs
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@23998 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-api/src/main/java/org/drools/PropertiesConfiguration.java
+++ drools-api/src/main/java/org/drools/PropertiesConfiguration.java
@@ -1,8 +1,25 @@
 package org.drools;
 
+/**
+ * Base class for other Configuration classes.
+ *
+ */
 public interface PropertiesConfiguration {
+    
+    /**
+     * Sets a property value
+     * 
+     * @param name
+     * @param value
+     */
     public void setProperty(String name,
                             String value);
 
+    /**
+     * Gets a property value
+     * 
+     * @param name
+     * @return
+     */
     public String getProperty(String name);
 }
