17b69d52c6a6ecb39b8503bfe4f486c304c9f8a5
Mark Proctor <mdproctor@gmail.com>
2008-11-21 17:20:18 +0000

    -more javadocs
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@24038 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-api/drools-api/src/main/java/org/drools/runtime/process/StatelessProcessSession.java
+++ kogito-api/drools-api/src/main/java/org/drools/runtime/process/StatelessProcessSession.java
@@ -1,5 +1,11 @@
 package org.drools.runtime.process;
 
+/**
+ * There are currently no process related methods that are used with the external StatelessKnowledgeSession class. So this
+ * interface is just a marker at the moment, but may find more uses in the future as requirements are discovered.
+ *
+ * @see org.drools.runtime.StatelessKnowledgeSession
+ */
 public interface StatelessProcessSession {
 
 }
