3a486365ffef99d9e952b5e465ffde60f835c88e
Mark Proctor <mdproctor@gmail.com>
2008-12-07 05:40:42 +0000

    JBRULES-1874 Create Resource Framework -fixed javadocs from addResource to add
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@24274 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-api/src/main/java/org/drools/ChangeSet.java
+++ drools-api/src/main/java/org/drools/ChangeSet.java
@@ -2,9 +2,15 @@ package org.drools;
 
 import java.util.Collection;
 
-import org.drools.definition.KnowledgeDefinition;
 import org.drools.io.Resource;
 
+/**
+ * This class is used to provide a ChangeSet info to a ResourceChangeNotifier. It should be used when you implement the ResourceChangeMonitor interface.
+ * Each method provides a Collection of removed, added and modified resources, and determined by the monitor. Drools currently only provides the
+ * ResourceChangeScanner, which scans the local disk for changes. 
+ * 
+ * This interface, as well as ResourceChangeMonitor and ResourceChangeNotifier are still considered subject to change.
+ */
 public interface ChangeSet {
     public Collection<Resource> getResourcesRemoved();
 
@@ -12,8 +18,5 @@ public interface ChangeSet {
     public Collection<Resource> getResourcesAdded();
     
     public Collection<Resource> getResourcesModified();
-
-
-    //public Collection<KnowledgeDefinition> getKnowledgeDefinitionsRemoved();
 
 }
--- drools-api/src/main/java/org/drools/io/ResourceChangeMonitor.java
+++ drools-api/src/main/java/org/drools/io/ResourceChangeMonitor.java
@@ -1,6 +1,9 @@
 package org.drools.io;
 
-
+/**
+ * 
+ *  * This interface, as well as ResourceChangeNotifier are still considered subject to change.
+ */
 public interface ResourceChangeMonitor {
     void subscribeNotifier(ResourceChangeNotifier notifier, Resource resource);
     void unsubscribeNotifier(ResourceChangeNotifier notifier, Resource resource);
