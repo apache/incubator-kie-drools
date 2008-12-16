12c7d5e0fa778c62a8f7172a68b2d4aa40619270
Kris Verlaenen <kris.verlaenen@gmail.com>
2008-12-16 00:05:10 +0000

    JBRULES-1895: Rule constraints can use processInstance variable to refer to current process instance  - binding process instance to current process instance when evaluating constraint JBRULES-1896: Support kcontext variable in rules and processes  - adding support in core and compiler for kcontext variable
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@24389 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-api/drools-api/src/main/java/org/drools/runtime/process/ProcessContext.java
@@ -0,0 +1,17 @@
+package org.drools.runtime.process;
+
+import org.drools.runtime.KnowledgeRuntime;
+
+public interface ProcessContext {
+	
+	ProcessInstance getProcessInstance();
+	
+	NodeInstance getNodeInstance();
+	
+	KnowledgeRuntime getKnowledgeRuntime();
+	
+	Object getVariable(String variableName);
+	
+	void setVariable(String variableName, Object value);
+
+}
