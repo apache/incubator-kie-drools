70aa814ed06daa0b6de1252e7631a57050c7da08
Mark Proctor <mdproctor@gmail.com>
2009-01-04 05:23:31 +0000

    JBRULES-1903 Pipeline support for DataLoaders and Feeders -Added MvelAction -Added simple unit tests for core stages
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@24529 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-core/src/main/java/org/drools/runtime/pipeline/impl/MvelAction.java
@@ -0,0 +1,40 @@
+package org.drools.runtime.pipeline.impl;
+
+import java.io.Serializable;
+
+import org.drools.runtime.pipeline.Action;
+import org.drools.runtime.pipeline.PipelineContext;
+import org.drools.runtime.pipeline.Receiver;
+import org.mvel2.MVEL;
+import org.mvel2.ParserContext;
+import org.mvel2.compiler.ExpressionCompiler;
+
+public class MvelAction extends BaseEmitter
+    implements
+    Action,
+    Receiver {
+    private Serializable expr;
+
+    public MvelAction(String text) {
+//        final ParserContext parserContext = new ParserContext();
+//        parserContext.setStrictTypeEnforcement( false );
+
+        ExpressionCompiler compiler = new ExpressionCompiler( text );
+        this.expr = compiler.compile( );
+    }
+
+    public void receive(Object object,
+                        PipelineContext context) {
+        try {
+            MVEL.executeExpression( this.expr,
+                                    object );
+        } catch ( Exception e ) {
+            handleException( this,
+                             object,
+                             e );
+        }
+        emit( object,
+              context );
+    }
+
+}
---
+++ kogito-drools/drools-core/src/test/java/org/drools/runtime/pipeline/impl/ListAdapterTest.java
@@ -0,0 +1,44 @@
+package org.drools.runtime.pipeline.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.drools.runtime.pipeline.Action;
+import org.drools.runtime.pipeline.Callable;
+import org.drools.runtime.pipeline.Expression;
+import org.drools.runtime.pipeline.ListAdapter;
+import org.drools.runtime.pipeline.PipelineFactory;
+
+import junit.framework.TestCase;
+
+public class ListAdapterTest extends TestCase {
+    public void testListAdapter() {
+        MockClass mock = new MockClass();
+        List list = new ArrayList();
+        
+        Action action = PipelineFactory.newMvelAction( "this.set = true" );   
+        
+        ListAdapter listAdapter = PipelineFactory.newListAdapter( list, true );        
+        action.addReceiver( listAdapter );
+        
+        assertFalse( mock.isSet() );
+        action.receive( mock, new BasePipelineContext( Thread.currentThread().getContextClassLoader() ) );
+
+        assertEquals( 1, list.size());
+        assertSame( mock, list.get( 0 ) );
+        assertTrue( mock.isSet() );
+    }
+    
+    public static class MockClass {        
+        private boolean set;
+
+        public boolean isSet() {
+            return set;
+        }
+
+        public void setSet(boolean set) {
+            this.set = set;
+        }
+                
+    }
+}
