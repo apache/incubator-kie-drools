fc59d169dc0900b4b5961592e2de8750f901c68a
Edson Tirelli <ed.tirelli@gmail.com>
2009-01-08 06:05:27 +0000

    Adding fusion example
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@24597 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-examples/drools-examples-fusion/src/main/java/org/drools/examples/broker/model/StockTick.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2008 Red Hat
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.drools.examples.broker.model;
+
+
+/**
+ * A stock tick event informing of a state change due to some operation;
+ * 
+ * @author etirelli
+ */
+public class StockTick {
+    private final String symbol;
+    private final double price;
+    private final long timestamp;
+    
+    public StockTick(String symbol,
+                     double price,
+                     long timestamp) {
+        super();
+        this.symbol = symbol;
+        this.price = price;
+        this.timestamp = timestamp;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+    
+    public String toString() {
+        return symbol+"( "+price+" )";
+    }
+}
