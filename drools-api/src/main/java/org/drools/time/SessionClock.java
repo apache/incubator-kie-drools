2beac6079b8e76b69e8e950159af8dbbbad18066
Edson Tirelli <ed.tirelli@gmail.com>
2009-01-19 21:53:24 +0000

    JBRULES-1920: fixing NOT delay calculation
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@24800 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-api/src/main/java/org/drools/time/SessionClock.java
+++ drools-api/src/main/java/org/drools/time/SessionClock.java
@@ -26,7 +26,7 @@ public interface SessionClock {
     /**
      * Returns the current time. There is no semantics attached
      * to the long return value, so it will depend on the actual
-     * implementation. For isntance, for a real clock it may be
+     * implementation. For instance, for a real time clock it may be
      * milliseconds.
      * 
      * @return The current time. The unit of the time, depends on
