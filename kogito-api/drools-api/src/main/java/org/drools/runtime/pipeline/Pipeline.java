ad6ab3a14597ee14331ef32791b7e02725086a55
Mark Proctor <mdproctor@gmail.com>
2009-01-20 03:16:11 +0000

    -added some javadocs for pipeline
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@24811 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-api/drools-api/src/main/java/org/drools/runtime/pipeline/Pipeline.java
+++ kogito-api/drools-api/src/main/java/org/drools/runtime/pipeline/Pipeline.java
@@ -1,6 +1,6 @@
 package org.drools.runtime.pipeline;
 
 
-public interface Pipeline extends Stage, Emitter{
+public interface Pipeline extends Stage, Emitter {
     void insert(Object object, ResultHandler resultHandler);
 }
