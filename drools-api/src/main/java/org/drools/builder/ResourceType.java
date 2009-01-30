1a13a52b9f0b9ef877b8ad8e5345be485014ae98
Kris Verlaenen <kris.verlaenen@gmail.com>
2009-01-30 13:30:55 +0000

    JBRULES-1915: Improve process persistence usability  - improved command-based implementation of StatefulKnowledgeSession
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@25015 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-api/src/main/java/org/drools/builder/ResourceType.java
+++ drools-api/src/main/java/org/drools/builder/ResourceType.java
@@ -1,27 +1,70 @@
 package org.drools.builder;
 
-public enum ResourceType {
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResourceType {
+	
+	private String name;
+	
+    private static final Map<String, ResourceType> CACHE = Collections.synchronizedMap( new HashMap<String, ResourceType>() );
+
+    public ResourceType(String name) {
+		this.name = name;
+	}
+	
+    public static ResourceType addResourceTypeToRegistry(final String resourceType) {
+		ResourceType resource = new ResourceType(resourceType);
+		CACHE.put(resourceType, resource);
+		return resource;
+	}
+
     /** Drools Rule Language */
-    DRL,
+    public static final ResourceType DRL = addResourceTypeToRegistry("DRL");
 
     /** Drools XML Rule Language */
-    XDRL,
+    public static final ResourceType XDRL = addResourceTypeToRegistry("XDRL");
 
     /** Drools DSL */
-    DSL,
+    public static final ResourceType DSL = addResourceTypeToRegistry("DSL");
 
     /** Drools DSL Rule */
-    DSLR,
+    public static final ResourceType DSLR = addResourceTypeToRegistry("DSLR");
 
     /** Drools Rule Flow Language */
-    DRF,
+    public static final ResourceType DRF = addResourceTypeToRegistry("DRF");
 
     /** Decision Table */
-    DTABLE,
+    public static final ResourceType DTABLE = addResourceTypeToRegistry("DTABLE");
 
     /** Binary Package */
-    PKG,
+    public static final ResourceType PKG = addResourceTypeToRegistry("PKG");
 
     /** ChangeSet */
-    ChangeSet;
+    public static final ResourceType ChangeSet = addResourceTypeToRegistry("ChangeSet");
+    
+    public static ResourceType getResourceType(final String resourceType) {
+    	ResourceType resource = CACHE.get(resourceType);
+        if ( resource == null ) {
+            throw new RuntimeException( "Unable to determine resource type " + resourceType );
+        }
+        return resource;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ( this == obj ) return true;
+        if ( obj == null ) return false;
+        if ( getClass() != obj.getClass() ) return false;
+        final ResourceType other = (ResourceType) obj;
+        if ( !name.equals( other.name ) ) return false;
+        return true;
+    }
+    
 }
