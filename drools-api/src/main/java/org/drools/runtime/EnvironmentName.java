f77bfa35a56e266bc812e03916d68c64217a7c3f
Mark Proctor <mdproctor@gmail.com>
2009-02-14 04:52:53 +0000

    JBRULES-1971 JPA Cleanups and Corrections
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@25261 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-api/src/main/java/org/drools/runtime/EnvironmentName.java
@@ -0,0 +1,7 @@
+package org.drools.runtime;
+
+public class EnvironmentName {
+    public static final String ENTITY_MANAGER_FACTORY = "drools.emf";
+    public static final String ENTITY_MANAGER = "drools.em";
+    public static final String PLACEHOLDER_RESOLVER_STRATEGY_FACTORY = "drools.placeholderResolverStrategyFactory";
+}
