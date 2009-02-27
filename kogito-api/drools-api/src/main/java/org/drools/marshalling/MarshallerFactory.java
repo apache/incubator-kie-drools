28bb8f595f029e8ce54b3bfa267d1ff0c4388744
Mark Proctor <mdproctor@gmail.com>
2009-02-27 03:53:08 +0000

    -updated javadocs
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@25453 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-api/drools-api/src/main/java/org/drools/marshalling/MarshallerFactory.java
+++ kogito-api/drools-api/src/main/java/org/drools/marshalling/MarshallerFactory.java
@@ -1,8 +1,65 @@
 package org.drools.marshalling;
 
+import java.io.ByteArrayOutputStream;
+
 import org.drools.KnowledgeBase;
 import org.drools.ProviderInitializationException;
 
+
+/**
+ * <p>
+ * The MarshallerFactory is used to marshal and unmarshal StatefulKnowledgeSessions. At the simplest it can be used as follows:
+ * </p>
+ * <pre>
+ * // ksession is the StatefulKnowledgeSession
+ * // kbase is the KnowledgeBase
+ * ByteArrayOutputStream baos = new ByteArrayOutputStream();
+ * Marshaller marshaller = MarshallerFactory.newMarshaller( kbase );
+ * marshaller.marshall( baos, ksession );
+ * baos.close();
+ * </pre>
+ * 
+ * <p>
+ * However with marshalling you need more flexibility when dealing with referenced user data. To achieve this we have the 
+ * ObjectMarshallingStrategy interface. Two implementations are provided, but the user can implement their own. The two
+ * supplied are IdentityMarshallingStrategy and SerializeMarshallingStrategy. SerializeMarshallingStrategy is the default, as used
+ * in the example above and it just calls the Serializable or Externalizable methods on a user instance. IdentityMarshallingStrategy
+ * instead creates an int id for each user object and stores them in a Map the id is written to the stream. When unmarshalling
+ * it simply looks to the IdentityMarshallingStrategy map to retrieve the instance. This means that if you use the IdentityMarshallingStrategy
+ * it's stateful for the life of the Marshaller instance and will create ids and keep references to all objects that it attempts to marshal.
+ * Here is he code to use a IdentityMarshallingStrategy.
+ * </p>
+ * <pre>
+ * ByteArrayOutputStream baos = new ByteArrayOutputStream();
+ * Marshaller marshaller = MarshallerFactory.newMarshaller( kbase, new ObjectMarshallingStrategy[] { MarshallerFactory.newIdentityMarshallingStrategy() } );
+ * marshaller.marshall( baos, ksession );
+ * baos.close();
+ * </pre>
+
+ * <p>
+ * For added flexability we can't assume that a single strategy is suitable for this we have added the ObjectMarshallingStrategyAcceptor interface that each 
+ * ObjectMarshallingStrategy has. The Marshaller has a chain of strategies and when it attempts to read or write a user object it iterates the strategies asking
+ * if they accept responsability for marshalling the user object. One one implementation is provided the ClassFilterAcceptor. This allows strings and wild cards
+ * to be used to match class names. The default is "*.*", so in the above the IdentityMarshallingStrategy is used which has a default "*.*" acceptor.
+ * </p>
+ * 
+ * <p>
+ * But lets say we want to serialise all classes except for one given package, where we will use identity lookup, we could do the following:
+ * </p>
+ * <pre>
+ * ByteArrayOutputStream baos = new ByteArrayOutputStream();
+ * ObjectMarshallingStrategyAcceptor identityAceceptor = MarshallerFactory.newClassFilterAcceptor( new String[] { "org.domain.pkg1.*" } );
+ * ObjectMarshallingStrategy identityStratetgy = MarshallerFactory.newIdentityMarshallingStrategy( identityAceceptor );
+ * Marshaller marshaller = MarshallerFactory.newMarshaller( kbase, new ObjectMarshallingStrategy[] { identityStratetgy, MarshallerFactory.newSerializeMarshallingStrategy() } );
+ * marshaller.marshall( baos, ksession );
+ * baos.close();
+ * </pre>
+ * 
+ * <p>
+ * Not that the acceptance checking order is in the natural order of the supplied array.
+ * </p>
+ *
+ */
 public class MarshallerFactory {
     private static volatile MarshallerProvider provider;
 
