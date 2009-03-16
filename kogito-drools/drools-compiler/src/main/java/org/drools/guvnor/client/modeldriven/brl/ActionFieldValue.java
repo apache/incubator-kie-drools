8be9e9d4eac7c3d58436ad4f744ebb4da438c5cf
Toni Rikkola <toni@rikkola.net>
2009-03-16 10:28:05 +0000

    JBRULES-1929 : In the then part (RHS) of guided rule editor, it is not possible to affect a value with variable name of the when part (LHS) and not possible to call a method in the new created instance
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@25659 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/main/java/org/drools/guvnor/client/modeldriven/brl/ActionFieldValue.java
+++ kogito-drools/drools-compiler/src/main/java/org/drools/guvnor/client/modeldriven/brl/ActionFieldValue.java
@@ -12,6 +12,38 @@ public class ActionFieldValue
 
     public String field;
     public String value;
+    public long nature;
+    /**
+     * This is used only when action is first created.
+     * This means that there is no value yet for the constraint.
+     */
+    public static final int TYPE_UNDEFINED = 0;
+
+    /**
+     * This may be string, or number, anything really.
+     */
+    public static final int TYPE_LITERAL   = 1;
+
+    /**
+     * This is when it is set to a valid previously bound variable.
+     */
+    public static final int TYPE_VARIABLE  = 2;
+
+    /**
+     * This is for a "formula" that calculates a value.
+     */
+    public static final int TYPE_FORMULA = 3;
+
+    /**
+     * This is not used yet. ENUMs are not suitable for business rules
+     * until we can get data driven non code enums.
+     */
+    public static final int TYPE_ENUM      = 4;
+
+    /**
+     * The fieldName and fieldBinding is not used in the case of a predicate.
+     */
+    public static final int TYPE_PREDICATE = 5;
 
     /**
      * This is the datatype archectype (eg String, Numeric etc).
