4201ebb69fa33bb3ccb3b281b00400adcaab0e34
Michael Neale <michael.neale@gmail.com>
2009-03-19 05:08:03 +0000

    odd comment
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@25724 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-api/drools-api/src/main/java/org/drools/runtime/pipeline/XStreamTransformerProvider.java
+++ kogito-api/drools-api/src/main/java/org/drools/runtime/pipeline/XStreamTransformerProvider.java
@@ -6,6 +6,8 @@ import com.thoughtworks.xstream.XStream;
 /**
  * 
  * <p>This api is experimental and thus the classes and the interfaces returned are subject to change.</p>
+ * MN: well why make this an interface? Seriously, just use concrete if it is going to change.
+ * This is just making the code harder to reason over. 
  *
  */
 public interface XStreamTransformerProvider {
