376c9f34467553b85f18df6478480a63de41a52b
Mark Proctor <mdproctor@gmail.com>
2009-03-27 05:07:52 +0000

    -changed ChangeSet to CHANGE_SET -Added toStrings() on ResourceType
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@25845 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-api/src/main/java/org/drools/builder/ResourceType.java
+++ drools-api/src/main/java/org/drools/builder/ResourceType.java
@@ -5,54 +5,74 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ResourceType {
-	
-	private String name;
-	
+
+    private String                                 name;
+
+    private String                                 description;
+
     private static final Map<String, ResourceType> CACHE = Collections.synchronizedMap( new HashMap<String, ResourceType>() );
 
-    public ResourceType(String name) {
-		this.name = name;
-	}
-	
-    public static ResourceType addResourceTypeToRegistry(final String resourceType) {
-		ResourceType resource = new ResourceType(resourceType);
-		CACHE.put(resourceType, resource);
-		return resource;
-	}
+    public ResourceType(String name,
+                        String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public static ResourceType addResourceTypeToRegistry(final String resourceType,
+                                                         String description) {
+        ResourceType resource = new ResourceType( resourceType,
+                                                  description );
+        CACHE.put( resourceType,
+                   resource );
+        return resource;
+    }
 
     /** Drools Rule Language */
-    public static final ResourceType DRL = addResourceTypeToRegistry("DRL");
+    public static final ResourceType DRL        = addResourceTypeToRegistry( "DRL",
+                                                                             "Drools Rule Language" );
 
     /** Drools XML Rule Language */
-    public static final ResourceType XDRL = addResourceTypeToRegistry("XDRL");
+    public static final ResourceType XDRL       = addResourceTypeToRegistry( "XDRL",
+                                                                             "Drools XML Rule Language" );
 
     /** Drools DSL */
-    public static final ResourceType DSL = addResourceTypeToRegistry("DSL");
+    public static final ResourceType DSL        = addResourceTypeToRegistry( "DSL",
+                                                                             "Drools DSL" );
 
     /** Drools DSL Rule */
-    public static final ResourceType DSLR = addResourceTypeToRegistry("DSLR");
+    public static final ResourceType DSLR       = addResourceTypeToRegistry( "DSLR",
+                                                                             "Drools DSL Rule" );
 
     /** Drools Rule Flow Language */
-    public static final ResourceType DRF = addResourceTypeToRegistry("DRF");
+    public static final ResourceType DRF        = addResourceTypeToRegistry( "DRF",
+                                                                             "Drools Rule Flow Language" );
 
     /** Decision Table */
-    public static final ResourceType DTABLE = addResourceTypeToRegistry("DTABLE");
+    public static final ResourceType DTABLE     = addResourceTypeToRegistry( "DTABLE",
+                                                                             "Decision Table" );
 
     /** Binary Package */
-    public static final ResourceType PKG = addResourceTypeToRegistry("PKG");
+    public static final ResourceType PKG        = addResourceTypeToRegistry( "PKG",
+                                                                             "Binary Package" );
 
-    /** Drools Rule Language */
-    public static final ResourceType BRL = addResourceTypeToRegistry("BRL");
+    /** Drools Business Rule Language */
+    public static final ResourceType BRL        = addResourceTypeToRegistry( "BRL",
+                                                                             "Drools Business Rule Language" );
 
-    /** ChangeSet */
-    public static final ResourceType ChangeSet = addResourceTypeToRegistry("ChangeSet");
-    
+    /** Change Set */
+    public static final ResourceType CHANGE_SET = addResourceTypeToRegistry( "CHANGE_SET",
+                                                                             "Change Set" );
+
     public static ResourceType getResourceType(final String resourceType) {
-    	ResourceType resource = CACHE.get(resourceType);
+        ResourceType resource = CACHE.get( resourceType );
         if ( resource == null ) {
             throw new RuntimeException( "Unable to determine resource type " + resourceType );
         }
         return resource;
+    }
+
+    public String toString() {
+        return "ResourceType = '" + this.description + "'";
     }
 
     @Override
@@ -69,5 +89,5 @@ public class ResourceType {
         if ( !name.equals( other.name ) ) return false;
         return true;
     }
-    
+
 }
