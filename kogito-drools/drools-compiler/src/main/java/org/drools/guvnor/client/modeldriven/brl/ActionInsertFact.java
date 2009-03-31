57f21e544f6b42e4f65adc2b99565d6cb4e79a42
Toni Rikkola <toni@rikkola.net>
2009-03-31 13:02:58 +0000

    JBRULES-1929 : In the then part (RHS) of guided rule editor, it is not possible to affect a value with variable name of the when part (LHS) and not possible to call a method in the new created instance
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@25895 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/main/java/org/drools/guvnor/client/modeldriven/brl/ActionInsertFact.java
+++ kogito-drools/drools-compiler/src/main/java/org/drools/guvnor/client/modeldriven/brl/ActionInsertFact.java
@@ -1,22 +1,33 @@
 package org.drools.guvnor.client.modeldriven.brl;
 
-
 /**
  * This is used when asserting a new fact.
+ * 
  * @author Michael Neale
- *
+ * 
  */
 public class ActionInsertFact extends ActionFieldList {
 
+	public String factType;
+	private String boundName;
+	private boolean isBound ;
+	
+	public boolean isBound() {
+		return isBound;
+	}
 
-    public String factType;
+	public ActionInsertFact(final String type) {
+		this.factType = type;
+	}
 
-    public ActionInsertFact(final String type) {
-        this.factType = type;
-    }
+	public ActionInsertFact() {
+	}
+	public String getBoundName() {
+		return boundName;
+	}
 
-    public ActionInsertFact() {
-    }
-
-
+	public void setBoundName(String boundName) {
+		this.boundName = boundName;
+		isBound = true;
+	}
 }
