cd63e5d42ad51c97a09d17d7c6b4320390fe2410
Mark Proctor <mdproctor@gmail.com>
2009-04-02 04:32:49 +0000

    -some javadoc layout fixes.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@25909 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-api/drools-api/src/main/java/org/drools/ChangeSet.java
+++ kogito-api/drools-api/src/main/java/org/drools/ChangeSet.java
@@ -5,17 +5,24 @@ import java.util.Collection;
 import org.drools.io.Resource;
 
 /**
+ * <p>
  * This class is used to provide a ChangeSet info to a ResourceChangeNotifier. It should be used when you implement the ResourceChangeMonitor interface.
  * Each method provides a Collection of removed, added and modified resources, and determined by the monitor. Drools currently only provides the
  * ResourceChangeScanner, which scans the local disk for changes. 
+ * </p>
  * 
+ * <p>
  * This interface, as well as ResourceChangeMonitor, ResourceChangeNotifier, ResourceChangeScanner and ResourceChangeListener are still considered subject to change. 
  * Use the XML format change-set, as
  * part of the ResourceType api when adding to KnowledgeBuilder, which is considered stable. KnowledgeBuilder currently ignored Added/Modified xml elements,
  * the KnowledgeAgent will use them, when rebuilding the KnowledgeBase.
+ * </p>
  * 
+ * <p>
  * the xml format has a root level <change-set> element and then it can contain <add>, <modified>, <removed> elements - each one can only be used once.
  * add, modified, removed then contain a list of <resource> elements. Resources may take a configuration, currently only decision table resources use that.
+ * </p>
+ * 
  * <pre>
  * &lt;change-set xmlns='http://drools.org/drools-5.0/change-set'
  *             xmlns:xs='http://www.w3.org/2001/XMLSchema-instance'
