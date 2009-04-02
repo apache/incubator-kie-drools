5293457dd4d6954d702340d401b677c1aa579ec6
Edson Tirelli <ed.tirelli@gmail.com>
2009-04-02 19:06:52 +0000

    Adding docs
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@25924 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-api/src/main/java/org/drools/KnowledgeBaseConfiguration.java
+++ drools-api/src/main/java/org/drools/KnowledgeBaseConfiguration.java
@@ -41,6 +41,8 @@ import org.drools.conf.KnowledgeBaseOptionsConfiguration;
  * <li>drools.indexLeftBetaMemory = &lt;true/false&gt;</li>
  * <li>drools.indexRightBetaMemory = &lt;true/false&gt;</li>
  * <li>drools.consequenceExceptionHandler = &lt;qualified class name&gt;</li>
+ * <li>drools.maxThreads = &lt;-1|1..n&gt;</li>
+ * <li>drools.multithreadEvaluation = &lt;true|false&gt;</li>
  * </ul>
  * </p>
  * 
