bc6cf8fe700eb521cad3a8dabca5b7f02fd3e213
Mark Proctor <mdproctor@gmail.com>
2009-04-15 15:45:02 +0000

    -GetAgendaCommand was returning the wrong instance, now fixed.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@26042 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/main/java/org/drools/process/command/GetAgendaCommand.java
+++ kogito-drools/drools-core/src/main/java/org/drools/process/command/GetAgendaCommand.java
@@ -1,17 +1,19 @@
 package org.drools.process.command;
 
+import org.drools.common.InternalAgenda;
 import org.drools.reteoo.ReteooWorkingMemory;
 import org.drools.runtime.rule.Agenda;
+import org.drools.runtime.rule.impl.AgendaImpl;
 
 public class GetAgendaCommand
     implements
     Command<Agenda> {
 
     public Agenda execute(ReteooWorkingMemory session) {
-        return session.getAgenda();
+        return new AgendaImpl( (InternalAgenda) session.getAgenda() );
     }
 
     public String toString() {
-        return "session.getAgenda();";
+        return "ksession.getAgenda();";
     }
 }
