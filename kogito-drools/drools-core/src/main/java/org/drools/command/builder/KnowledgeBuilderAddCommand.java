2e1bec8c1803a2897b8684426ee215932d8cdc16
Mark Proctor <mdproctor@gmail.com>
2009-07-16 01:27:11 +0000

    JBRULES-2209 Refactor Commands to mirror drools-api
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@28078 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-core/src/main/java/org/drools/command/builder/KnowledgeBuilderAddCommand.java
@@ -0,0 +1,40 @@
+package org.drools.command.builder;
+
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.ResourceConfiguration;
+import org.drools.builder.ResourceType;
+import org.drools.command.Context;
+import org.drools.command.impl.GenericCommand;
+import org.drools.command.impl.KnowledgeCommandContext;
+import org.drools.io.Resource;
+
+public class KnowledgeBuilderAddCommand
+    implements
+    GenericCommand<Void> {
+
+    private Resource              resource;
+    private ResourceType          resourceType;
+    private ResourceConfiguration resourceConfiguration;
+
+    public KnowledgeBuilderAddCommand(Resource resource,
+                                      ResourceType resourceType,
+                                      ResourceConfiguration resourceConfiguration) {
+        this.resource = resource;
+        this.resourceType = resourceType;
+        this.resourceConfiguration = resourceConfiguration;
+    }
+
+    public Void execute(Context context) {
+        KnowledgeBuilder kbuilder = ((KnowledgeCommandContext) context).getKnowledgeBuilder();
+        if ( resourceConfiguration == null ) {
+            kbuilder.add( resource,
+                          resourceType );
+        } else {
+            kbuilder.add( resource,
+                          resourceType,
+                          resourceConfiguration );
+        }
+        return null;
+    }
+
+}
