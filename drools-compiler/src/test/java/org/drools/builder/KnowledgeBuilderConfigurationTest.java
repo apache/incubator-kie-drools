5382ffd9a2b5fd039130e4c99d0f851f599590a6
Jervis Liu <jervisliu@gmail.com>
2009-07-27 03:29:26 +0000

    GUVNOR-358. Make org.drools.runtime.rule.AccumulateFunction extends from Externalizable to aviod java.io.NotSerializableException
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@28476 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-compiler/src/test/java/org/drools/builder/KnowledgeBuilderConfigurationTest.java
+++ drools-compiler/src/test/java/org/drools/builder/KnowledgeBuilderConfigurationTest.java
@@ -17,6 +17,9 @@
 package org.drools.builder;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 import java.io.Serializable;
 
 import junit.framework.TestCase;
@@ -284,6 +287,15 @@ public class KnowledgeBuilderConfigurationTest extends TestCase {
         public boolean supportsReverse() {
             return false;
         }
+		public void readExternal(ObjectInput in) throws IOException,
+				ClassNotFoundException {
+			// TODO Auto-generated method stub
+			
+		}
+		public void writeExternal(ObjectOutput out) throws IOException {
+			// TODO Auto-generated method stub
+			
+		}
     }
 
 }
