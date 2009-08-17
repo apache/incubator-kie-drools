2c662cebaee1cc0990c0cd9d9e7a55229598e884
Edson Tirelli <ed.tirelli@gmail.com>
2009-08-17 01:41:40 +0000

    JBRULES-2247: Adding support to monitoring through MBeans
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@28955 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-api/src/main/java/org/drools/management/ObjectTypeNodeMonitorMBean.java
+++ drools-api/src/main/java/org/drools/management/ObjectTypeNodeMonitorMBean.java
@@ -1,17 +1,54 @@
 package org.drools.management;
 
+/**
+ * An interface for OTN MBean
+ * 
+ * @author etirelli
+ */
 public interface ObjectTypeNodeMonitorMBean {
 
+    /**
+     * The ID of the node
+     * 
+     * @return
+     */
     public int getId();
 
+    /**
+     * The entry point for the node
+     * 
+     * @return
+     */
     public String getEntryPoint();
 
+    /**
+     * The Object Type of the node
+     * 
+     * @return
+     */
     public String getObjectType();
 
+    /**
+     * The partition the node belongs to
+     * 
+     * @return
+     */
     public String getPartitionId();
 
+    /**
+     * True if this node corresponds to an event type
+     * 
+     * @return
+     */
     public boolean isEvent();
 
+    /**
+     * The calculated expiration offset for this node
+     * in case it is an event. -1 means it does not
+     * expires.
+     * 
+     * @return
+     */
     public long getExpirationOffset();
 
 }
