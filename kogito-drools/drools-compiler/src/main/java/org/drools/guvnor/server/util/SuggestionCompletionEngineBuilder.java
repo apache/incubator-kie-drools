a5348de27200f1bea9a4d1b0598601d2ab11859c
Nicolas Héron <nicolas.heron.java@gmail.com>
2009-09-05 09:16:55 +0000

    GUVNOR-263 Guided editor : How to check if a list contains an object
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@29191 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-compiler/src/main/java/org/drools/guvnor/server/util/SuggestionCompletionEngineBuilder.java
+++ kogito-drools/drools-compiler/src/main/java/org/drools/guvnor/server/util/SuggestionCompletionEngineBuilder.java
@@ -17,6 +17,9 @@
 package org.drools.guvnor.server.util;
 
 import java.util.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.lang.reflect.ParameterizedType;
 
 import org.drools.guvnor.client.modeldriven.SuggestionCompletionEngine;
 import org.drools.guvnor.client.modeldriven.brl.DSLSentence;
@@ -35,6 +38,7 @@ public class SuggestionCompletionEngineBuilder {
     private Map                        fieldsForType         = new HashMap();
     private Map                        modifiersForType      = new HashMap();
     private Map                        fieldTypes            = new HashMap();
+    private Map<String,Field>          fieldTypesField       = new HashMap<String,Field>();
     private Map                        globalTypes           = new HashMap();
     private List                       actionDSLSentences    = new ArrayList();
     private List                       conditionDSLSentences = new ArrayList();
@@ -55,6 +59,7 @@ public class SuggestionCompletionEngineBuilder {
         this.fieldsForType = new HashMap();
         this.modifiersForType = new HashMap();
         this.fieldTypes = new HashMap();
+        this.fieldTypesField = new HashMap();
         this.globalTypes = new HashMap();
         this.actionDSLSentences = new ArrayList();
         this.conditionDSLSentences = new ArrayList();
@@ -114,7 +119,17 @@ public class SuggestionCompletionEngineBuilder {
         this.fieldTypes.put( field,
                              type );
     }
-
+      /**
+     * Adds a type declaration for a field
+     *
+     * @param field
+     * @param type
+     */
+    public void addFieldTypeField(final String field,
+                             final Field type) {
+        this.fieldTypesField.put( field,
+                             type );
+    }
     /**
      * Adds a global and its corresponding type to the engine
      *
@@ -160,6 +175,22 @@ public class SuggestionCompletionEngineBuilder {
         this.instance.fieldsForType = this.fieldsForType;
         this.instance.modifiers = this.modifiersForType;
         this.instance.fieldTypes = this.fieldTypes;
+        for (String fieldName  : this.fieldTypesField.keySet()){
+            Field field =  (Field) this.fieldTypesField.get(fieldName);
+            if (field != null){
+                Type type = field.getGenericType();
+                if (type instanceof ParameterizedType) {
+                    ParameterizedType pt = (ParameterizedType) type;
+                    Type goodType=null;
+                    for (Type t : pt.getActualTypeArguments()) {
+                        goodType = t;
+                    }
+                    int index = goodType.toString().lastIndexOf(".");
+                    String className = goodType.toString().substring(index+1);
+                    this.instance.fieldParametersType.put(fieldName,className);
+                  }
+            }
+        }
         this.instance.globalTypes = this.globalTypes;
         this.instance.actionDSLSentences = makeArray(this.actionDSLSentences);
         this.instance.conditionDSLSentences = makeArray(this.conditionDSLSentences);
