6bb4a56c73f48cb70d85e263241e4c87d8d7c209
Mark Proctor <mdproctor@gmail.com>
2009-09-06 04:54:03 +0000

    JBRULES-2269 Virtual Service Manager -Initial Commit of the Virtual Service Manager
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@29194 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-api/drools-api/src/main/java/org/drools/io/Resource.java
+++ kogito-api/drools-api/src/main/java/org/drools/io/Resource.java
@@ -3,13 +3,14 @@ package org.drools.io;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
+import java.io.Serializable;
 
 /**
  * <p>
  * Generic interface to provide a Reader or InputStream for the underlying IO resource.
  * </p>
  */
-public interface Resource {
+public interface Resource extends Serializable {
     /**
      * Open an InputStream to the resource, the user must close this when finished.
      * 
