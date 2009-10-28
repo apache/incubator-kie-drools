e493d4bf4fd06ad90e21a499dbf1270fbdb02a71
Mark Proctor <mdproctor@gmail.com>
2009-10-28 21:33:27 +0000

    JBRULES-2269 Virtual Service Manager -rio now uses GenericIoWriter
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@29862 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-vsm/src/main/java/org/drools/vsm/mina/MinaIoHandler.java
+++ kogito-drools/drools-vsm/src/main/java/org/drools/vsm/mina/MinaIoHandler.java
@@ -1,25 +1,22 @@
 package org.drools.vsm.mina;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
 import org.drools.SystemEventListener;
+import org.drools.vsm.ClientGenericMessageReceiver;
 import org.drools.vsm.GenericMessageHandler;
 import org.drools.vsm.Message;
 import org.drools.vsm.MessageResponseHandler;
 
 public class MinaIoHandler extends IoHandlerAdapter {
-    protected Map<Integer, MessageResponseHandler> responseHandlers;
-
-    private GenericMessageHandler                  handler;
-
     /**
      * Listener used for logging
      */
-    private final SystemEventListener              systemEventListener;
+    private final SystemEventListener systemEventListener;
+
+    private ClientGenericMessageReceiver clientMessageReceiver;
 
     public MinaIoHandler(SystemEventListener systemEventListener) {
         this( systemEventListener,
@@ -30,15 +27,15 @@ public class MinaIoHandler extends IoHandlerAdapter {
     public MinaIoHandler(SystemEventListener systemEventListener,
                          GenericMessageHandler handler) {
         this.systemEventListener = systemEventListener;
-        this.responseHandlers = new ConcurrentHashMap<Integer, MessageResponseHandler>();
-        this.handler = handler;
+        this.clientMessageReceiver = new ClientGenericMessageReceiverImpl( handler,
+                                                                           systemEventListener );
 
     }
 
     public void addResponseHandler(int id,
                                    MessageResponseHandler responseHandler) {
-        this.responseHandlers.put( id,
-                                   responseHandler );
+        this.clientMessageReceiver.addResponseHandler( id,
+                                                       responseHandler );
     }
 
     public void exceptionCaught(IoSession session,
@@ -50,24 +47,8 @@ public class MinaIoHandler extends IoHandlerAdapter {
     public void messageReceived(IoSession session,
                                 Object object) throws Exception {
         Message msg = (Message) object;
-
-        systemEventListener.debug( "Message receieved : " + msg );
-
-        MessageResponseHandler responseHandler = (MessageResponseHandler) responseHandlers.remove( msg.getResponseId() );
-
-        if ( responseHandler != null ) {
-            Object payload = msg.getPayload();
-            if ( payload != null && payload instanceof RuntimeException ) {
-                responseHandler.setError( (RuntimeException) payload );
-            } else {
-                responseHandler.receive( msg );
-            }
-        } else if ( handler != null ) {
-            this.handler.messageReceived( new MinaIoWriter( session ),
-                                          (Message) object );
-        } else {
-            throw new RuntimeException( "Unable to process Message" );
-        }
+        clientMessageReceiver.messageReceived( new MinaIoWriter( session ),
+                                               msg );
     }
 
     @Override
