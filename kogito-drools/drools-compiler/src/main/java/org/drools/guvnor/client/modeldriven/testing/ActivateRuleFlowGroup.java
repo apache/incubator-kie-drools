3067bebe5e642f3436e4f08709431415625b352b
Toni Rikkola <toni@rikkola.net>
2009-11-27 07:54:53 +0000

    GUVNOR-474 : In test scenarios
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@30363 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-compiler/src/main/java/org/drools/guvnor/client/modeldriven/testing/ActivateRuleFlowGroup.java
@@ -0,0 +1,18 @@
+package org.drools.guvnor.client.modeldriven.testing;
+
+
+/**
+ * @author Toni Rikkola 
+ *
+ */
+public class ActivateRuleFlowGroup implements Fixture {
+
+    public ActivateRuleFlowGroup() {}
+    public ActivateRuleFlowGroup(String s) {
+        this.name = s;
+    }
+
+    public String name;
+
+
+}
