7eaa1d4b5eae0d62806608cfef112daf9099280c
Mark Proctor <mdproctor@gmail.com>
2009-12-14 23:20:16 +0000

    JBRULES-2373 Add String converter -Added various points for String based conversion, but not working yet, just basic plumbing.  Currently hard coded to dates.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@30651 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-api/drools-api/src/main/java/org/drools/runtime/EnvironmentName.java
+++ kogito-api/drools-api/src/main/java/org/drools/runtime/EnvironmentName.java
@@ -6,4 +6,6 @@ public class EnvironmentName {
     public static final String TRANSACTION_MANAGER = "drools.persistence.jpa.TransactionManager";
     public static final String OBJECT_MARSHALLING_STRATEGIES = "drools.marshalling.ObjectMarshallingStrategies";
     public static final String GLOBALS = "drools.Globals";
+    public static final String CALENDARS = "org.drools.time.Calendars";
+    public static final String DATE_FORMATS = "org.drools.build.DateFormats";
 }
