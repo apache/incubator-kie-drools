f5c6ad9f39a909325dfa02ebf610440ef82365ae
Pablo Nussembaum <baunax@gmail.com>
2009-12-16 20:35:52 +0000

    JBRULES-2382 allows classes to have @Id annotation on methods
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@30734 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-persistence-jpa/src/main/java/org/drools/persistence/processinstance/persisters/JPAVariablePersister.java
+++ kogito-drools/drools-persistence-jpa/src/main/java/org/drools/persistence/processinstance/persisters/JPAVariablePersister.java
@@ -1,7 +1,9 @@
 package org.drools.persistence.processinstance.persisters;
 
+import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,7 +16,6 @@ import org.drools.runtime.Environment;
 import org.drools.runtime.EnvironmentName;
 
 /**
- * 
  * @author <a href="mailto:kris_verlaenen@hotmail.com">Kris Verlaenen</a>
  * @author salaboy
  */
@@ -22,46 +23,47 @@ public class JPAVariablePersister implements VariablePersister {
 
 	public VariableInstanceInfo persistExternalVariable(String name, Object o,
 			VariableInstanceInfo oldValue, Environment env) {
-                if(o == null || (oldValue != null && oldValue.getPersister().equals(""))){
-                    return null;
-                }
+		if (o == null || (oldValue != null && oldValue.getPersister().equals(""))) {
+			return null;
+		}
 		try {
 			boolean newVariable = false;
-                        EntityManager em = (EntityManager) env.get(EnvironmentName.ENTITY_MANAGER);
+			EntityManager em = (EntityManager) env.get(EnvironmentName.ENTITY_MANAGER);
 			JPAPersistedVariable result = null;
 			if (oldValue instanceof JPAPersistedVariable) {
 				result = (JPAPersistedVariable) oldValue;
 			}
 			if (result == null) {
 				result = new JPAPersistedVariable();
-				
+
 				newVariable = true;
 			}
-			Long idValue = getClassIdValue(o);
+			Serializable idValue = getClassIdValue(o);
 			if (idValue != null) {
-                                System.out.println("Variable "+name +" -> Updating external Entity = "+o);
+				System.out.println("Variable " + name + " -> Updating external Entity = " + o);
 				em.merge(o);
 			} else {
-                                System.out.println("Variable "+name +" -> Persisting external Entity for the first time ="+o);
+				System.out.println("Variable " + name + " -> Persisting external Entity for the first time =" + o);
 				em.persist(o);
 				idValue = getClassIdValue(o);
 			}
 			result.setPersister(this.getClass().getName());
-                        result.setName(name);
-                        // entity might have changed, updating info
+			result.setName(name);
+			// entity might have changed, updating info
 			result.setEntityId(idValue);
 			result.setEntity(o);
 			result.setEntityClass(o.getClass().getCanonicalName());
-                        if(newVariable){
-                            em.persist(result);
-                        }else{
-                            em.merge(result);
-                        }
-			System.out.println("Saving JPAPersistedVariable id=" + result.getId() + " entityId=" + result.getEntityId() + " class=" + result.getEntityClass() + " value=" + result.getEntity());
+			if (newVariable) {
+				em.persist(result);
+			} else {
+				em.merge(result);
+			}
+			System.out.println("Saving JPAPersistedVariable id=" + result.getId() + " entityId=" + result.getEntityId()
+					+ " class=" + result.getEntityClass() + " value=" + result.getEntity());
 			return result;
 		} catch (Throwable t) {
 			Logger.getLogger(JPAVariablePersister.class.getName())
-				.log(Level.SEVERE, null, t);
+					.log(Level.SEVERE, null, t);
 			throw new RuntimeException("Could not persist external variable", t);
 		}
 	}
@@ -69,36 +71,59 @@ public class JPAVariablePersister implements VariablePersister {
 	public Object getExternalPersistedVariable(
 			VariableInstanceInfo variableInstanceInfo, Environment env) {
 		EntityManager em = (EntityManager) env.get(EnvironmentName.ENTITY_MANAGER);
-                if(((JPAPersistedVariable) variableInstanceInfo) == null || ((JPAPersistedVariable) variableInstanceInfo).getEntityId() == null){
-                    return null;
-                }
-		System.out.println("Restoring JPAPersistedVariable id=" + ((JPAPersistedVariable) variableInstanceInfo).getId() + " entityId=" + ((JPAPersistedVariable) variableInstanceInfo).getEntityId() + " class=" + ((JPAPersistedVariable) variableInstanceInfo).getEntityClass() + " value=" + ((JPAPersistedVariable) variableInstanceInfo).getEntity());
+		if (((JPAPersistedVariable) variableInstanceInfo) == null
+				|| ((JPAPersistedVariable) variableInstanceInfo).getEntityId() == null) {
+			return null;
+		}
+		System.out.println("Restoring JPAPersistedVariable id=" + ((JPAPersistedVariable) variableInstanceInfo).getId()
+				+ " entityId=" + ((JPAPersistedVariable) variableInstanceInfo).getEntityId() + " class="
+				+ ((JPAPersistedVariable) variableInstanceInfo).getEntityClass() + " value="
+				+ ((JPAPersistedVariable) variableInstanceInfo).getEntity());
 		try {
 			String varType = ((JPAPersistedVariable) variableInstanceInfo).getEntityClass();
 			return em.find(Class.forName(varType),
-				((JPAPersistedVariable) variableInstanceInfo).getEntityId());
+					((JPAPersistedVariable) variableInstanceInfo).getEntityId());
 		} catch (ClassNotFoundException ex) {
 			Logger.getLogger(JPAVariablePersister.class.getName())
-				.log(Level.SEVERE, null, ex);
+					.log(Level.SEVERE, null, ex);
 			throw new RuntimeException("Could not restore external variable", ex);
 		}
 	}
 
-	private Long getClassIdValue(Object o) throws NoSuchMethodException,
+	private Serializable getClassIdValue(Object o) throws NoSuchMethodException,
 			SecurityException, IllegalAccessException,
 			InvocationTargetException, IllegalArgumentException {
-		Field[] fields = o.getClass().getDeclaredFields();
-		Long idValue = null;
-		for (int i = 0; i < fields.length; i++) {
-			Id id = fields[i].getAnnotation(Id.class);
+		Class<? extends Object> varClass = o.getClass();
+		Field[] fields = varClass.getDeclaredFields();
+		Serializable idValue = null;
+		for (int i = 0; i < fields.length && idValue == null; i++) {
+			Field field = fields[i];
+			Id id = field.getAnnotation(Id.class);
 			if (id != null) {
-				idValue = (Long) o.getClass().getMethod("get"
-					+ Character.toUpperCase(fields[i].getName().charAt(0))
-					+ fields[i].getName().substring(1),
-					new Class<?>[] {}).invoke(o, new Object[] {});
-				break;
+				try {
+					idValue = callIdMethod(o, "get"
+							+ Character.toUpperCase(field.getName().charAt(0))
+							+ field.getName().substring(1));
+				} catch (NoSuchMethodException e) {
+					idValue = (Serializable) field.get(o);
+				}
+			}
+		}
+		if (idValue == null) {
+			Method[] methods = varClass.getMethods();
+			for (int i = 0; i < methods.length && idValue == null; i++) {
+				Method method = methods[i];
+				Id id = method.getAnnotation(Id.class);
+				if (id != null) {
+					idValue = (Serializable) method.invoke(o);
+				}
 			}
 		}
 		return idValue;
+	}
+
+	private Serializable callIdMethod(Object target, String methodName) throws IllegalArgumentException,
+			SecurityException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+		return (Serializable) target.getClass().getMethod(methodName, (Class[]) null).invoke(target, new Object[] {});
 	}
 }
