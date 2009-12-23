2eab708ab891bcb4067ab8887b743f2744ceb0b7
Mark Proctor <mdproctor@gmail.com>
2009-12-23 01:34:56 +0000

    -added javadocs to ExitPoint
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@30802 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-api/drools-api/src/main/java/org/drools/runtime/ExitPoint.java
+++ kogito-api/drools-api/src/main/java/org/drools/runtime/ExitPoint.java
@@ -1,5 +1,31 @@
 package org.drools.runtime;
 
+/**
+ * <p>
+ * An exit point provides a mechanism to send objects from the working memory to some external process 
+ * or function.  For instance, an exit point can be used to inform some piece of code that an object 
+ * matches a rule.  
+ * </p>
+ *
+ * <p>
+ * To create an exit point, implement the interface and register it with the KnowledgeRuntime:
+ * </p>
+ * <pre>
+ * ...
+ * ksession.registerExitPoint("my-exit-point", new MyExitPointImpl());
+ * </pre>
+ * 
+ * <p>
+ * Exit points are invoked from the consequence side of a rule:
+ * </p>
+ * <pre>
+ * when
+ *   ...
+ * then
+ *   exitPoint["my-exit-point"].insert(...);
+ * </pre>
+ */
+
 public interface ExitPoint {
     void insert(Object object);
 }
