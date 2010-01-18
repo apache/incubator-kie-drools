6730344ff8f81e60c64716cbe3be0fbfd148440a
Mark Proctor <mdproctor@gmail.com>
2010-01-18 02:07:59 +0000

    JBRULES-2351 OSGi Ready -First working version, with control over optional services.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@31114 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-api/drools-api/src/main/java/org/drools/osgi/api/Activator.java
+++ kogito-api/drools-api/src/main/java/org/drools/osgi/api/Activator.java
@@ -1,11 +1,10 @@
 package org.drools.osgi.api;
 
+import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.concurrent.Callable;
 
-import org.drools.KnowledgeBaseProvider;
 import org.drools.Service;
-import org.drools.io.ResourceProvider;
 import org.drools.util.ServiceRegistry;
 import org.drools.util.ServiceRegistryImpl;
 import org.osgi.framework.BundleActivator;
@@ -21,24 +20,24 @@ public class Activator
     private ServiceRegistration serviceRegistry;
     private ServiceTracker      registryTracker;
 
-
     public void start(BundleContext bc) throws Exception {
         System.out.println( "registering api services" );
+
         this.serviceRegistry = bc.registerService( ServiceRegistry.class.getName(),
                                                    new ServiceRegistryImpl(),
                                                    new Hashtable() );
 
-        registryTracker = new ServiceTracker( bc,
-                                              Service.class.getName(),
-                                              new DroolsServiceTracker( bc,
-                                                                        this ) );
+        this.registryTracker = new ServiceTracker( bc,
+                                                   Service.class.getName(),
+                                                   new DroolsServiceTracker( bc,
+                                                                             this ) );
         registryTracker.open();
         System.out.println( "api drools services registered" );
     }
 
     public void stop(BundleContext bc) throws Exception {
-        this.registryTracker.close();
         this.serviceRegistry.unregister();
+        this.registryTracker.close();
     }
 
     public static class DroolsServiceTracker
@@ -50,40 +49,55 @@ public class Activator
         public DroolsServiceTracker(BundleContext bc,
                                     Activator activator) {
             this.bc = bc;
+            this.activator = activator;
         }
 
         public Object addingService(ServiceReference ref) {
-            Service service = (Service) bc.getService( ref );
-            System.out.println( "registering" + service );
-            
-            
-            ServiceRegistryImpl.getInstance().registerLocator( service.getClass().getInterfaces()[0], new ReturnInstance( service ) );
+            Service service = (Service) this.bc.getService( ref );
+            System.out.println( "registering : " + service + " : " + service.getClass().getInterfaces()[0] );
+
+            Dictionary dic = new Hashtable();
+            ServiceReference regServiceRef = this.activator.serviceRegistry.getReference();
+            for ( String key : regServiceRef.getPropertyKeys() ) {
+                dic.put( key,
+                         regServiceRef.getProperty( key ) );
+            }
+            dic.put( service.getClass().getInterfaces()[0].getName(),
+                     "true" );
+            activator.serviceRegistry.setProperties( dic );
+
+            ServiceRegistryImpl.getInstance().registerLocator( service.getClass().getInterfaces()[0],
+                                                               new BundleContextInstantiator( this.bc,
+                                                                                              ref ) );
             return service;
         }
 
         public void modifiedService(ServiceReference arg0,
                                     Object arg1) {
-            
+
         }
 
         public void removedService(ServiceReference ref,
                                    Object arg1) {
             Service service = (Service) bc.getService( ref );
-            ServiceRegistryImpl.getInstance().unregisterLocator( service.getClass().getInterfaces()[0]);
+            ServiceRegistryImpl.getInstance().unregisterLocator( service.getClass().getInterfaces()[0] );
         }
     }
 
-    static class ReturnInstance<V>
+    public static class BundleContextInstantiator<V>
         implements
         Callable<V> {
-        private Service service;
+        private BundleContext    bc;
+        private ServiceReference ref;
 
-        public ReturnInstance(Service service) {
-            this.service = service;
+        public BundleContextInstantiator(BundleContext bc,
+                                         ServiceReference ref) {
+            this.bc = bc;
+            this.ref = ref;
         }
 
         public V call() throws Exception {
-            return (V) this.service;
+            return (V) this.bc.getService( this.ref );
         }
     }
 }
