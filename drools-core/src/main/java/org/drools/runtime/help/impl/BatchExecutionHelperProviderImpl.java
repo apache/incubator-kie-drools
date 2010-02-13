786098908c64848589deb8c766f0942a2e19d013
Mark Proctor <mdproctor@gmail.com>
2010-02-13 01:00:43 +0000

    -felx-foundation is now excluded (has jdk stuff gets in the way on the classpath) -committed commented out code by mistake, undoing
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@31655 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/main/java/org/drools/runtime/help/impl/BatchExecutionHelperProviderImpl.java
+++ drools-core/src/main/java/org/drools/runtime/help/impl/BatchExecutionHelperProviderImpl.java
@@ -62,9 +62,6 @@ public class BatchExecutionHelperProviderImpl
 
     public XStream newXStreamMarshaller(XStream xstream) {
         ElementNames names = new XmlElementNames();
-        // ElementNames names = new JsonElementNames();
-
-        // XStream xstream = new XStream( new JettisonMappedXmlDriver() );
         
         // xstream.setMode( XStream.NO_REFERENCES );
         xstream.processAnnotations( BatchExecutionImpl.class );
@@ -109,24 +106,23 @@ public class BatchExecutionHelperProviderImpl
                        NativeQueryResults.class );
         xstream.alias("fact-handle", DefaultFactHandle.class);
 
-
-//        xstream.registerConverter( new InsertConverter( xstream.getMapper() ) );
-//        xstream.registerConverter( new RetractConverter( xstream.getMapper() ) );
-//        xstream.registerConverter( new ModifyConverter( xstream.getMapper() ) );
-//        xstream.registerConverter( new GetObjectConverter( xstream.getMapper() ) );
-//        xstream.registerConverter( new InsertElementsConverter( xstream.getMapper() ) );
-//        xstream.registerConverter( new FireAllRulesConverter( xstream.getMapper() ) );
-//        xstream.registerConverter( new StartProcessConvert( xstream.getMapper() ) );
-//        xstream.registerConverter( new SignalEventConverter( xstream.getMapper() ) );
-//        xstream.registerConverter( new CompleteWorkItemConverter( xstream.getMapper() ) );
-//        xstream.registerConverter( new AbortWorkItemConverter( xstream.getMapper() ) );
-//        xstream.registerConverter( new QueryConverter( xstream.getMapper() ) );
-//        xstream.registerConverter( new SetGlobalConverter( xstream.getMapper() ) );
-//        xstream.registerConverter( new GetGlobalConverter( xstream.getMapper() ) );
-//        xstream.registerConverter( new GetObjectsConverter( xstream.getMapper() ) );
-//        xstream.registerConverter( new BatchExecutionResultConverter( xstream.getMapper() ) );
-//        xstream.registerConverter( new QueryResultsConverter( xstream.getMapper() ) );
-//        xstream.registerConverter( new FactHandleConverter(xstream.getMapper()));
+        xstream.registerConverter( new InsertConverter( xstream.getMapper() ) );
+        xstream.registerConverter( new RetractConverter( xstream.getMapper() ) );
+        xstream.registerConverter( new ModifyConverter( xstream.getMapper() ) );
+        xstream.registerConverter( new GetObjectConverter( xstream.getMapper() ) );
+        xstream.registerConverter( new InsertElementsConverter( xstream.getMapper() ) );
+        xstream.registerConverter( new FireAllRulesConverter( xstream.getMapper() ) );
+        xstream.registerConverter( new StartProcessConvert( xstream.getMapper() ) );
+        xstream.registerConverter( new SignalEventConverter( xstream.getMapper() ) );
+        xstream.registerConverter( new CompleteWorkItemConverter( xstream.getMapper() ) );
+        xstream.registerConverter( new AbortWorkItemConverter( xstream.getMapper() ) );
+        xstream.registerConverter( new QueryConverter( xstream.getMapper() ) );
+        xstream.registerConverter( new SetGlobalConverter( xstream.getMapper() ) );
+        xstream.registerConverter( new GetGlobalConverter( xstream.getMapper() ) );
+        xstream.registerConverter( new GetObjectsConverter( xstream.getMapper() ) );
+        xstream.registerConverter( new BatchExecutionResultConverter( xstream.getMapper() ) );
+        xstream.registerConverter( new QueryResultsConverter( xstream.getMapper() ) );
+        xstream.registerConverter( new FactHandleConverter(xstream.getMapper()));
 
         return xstream;
     }
