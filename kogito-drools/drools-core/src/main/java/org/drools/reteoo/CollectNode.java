99ce88b5c79c02287e43a7c04a116600239d711b
Edson Tirelli <ed.tirelli@gmail.com>
2010-03-30 01:08:28 +0000

    JBRULES-2339: JBRULES-2340: fixing memory leak
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@32292 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/main/java/org/drools/reteoo/CollectNode.java
+++ kogito-drools/drools-core/src/main/java/org/drools/reteoo/CollectNode.java
@@ -375,7 +375,7 @@ public class CollectNode extends BetaNode {
                         } else {
                             // we must re-add this to ensure deterministic iteration
                             LeftTuple temp = childLeftTuple.getLeftParentNext();
-                            childLeftTuple.reAddLeft();
+                            childLeftTuple.reAddRight();
                             childLeftTuple = temp;
                         }
                     } else if ( childLeftTuple != null && childLeftTuple.getRightParent() == rightTuple ) {
@@ -469,7 +469,7 @@ public class CollectNode extends BetaNode {
                         final CollectContext colctx = (CollectContext) memory.betaMemory.getCreatedHandles().get( leftTuple );
                         if ( childLeftTuple == null || childLeftTuple.getLeftParent() == leftTuple ) {
                             // we must re-add this to ensure deterministic iteration
-                            childLeftTuple.reAddRight();
+                            childLeftTuple.reAddLeft();
                             removeMatch( rightTuple,
                                          childLeftTuple,
                                          colctx );
