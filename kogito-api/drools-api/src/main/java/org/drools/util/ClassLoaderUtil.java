ec4d4021a98fa55ff796302b21a86776891bb88f
Edson Tirelli <ed.tirelli@gmail.com>
2010-04-09 03:08:57 +0000

    JBRULES-2067: adding support to class resolution caching on user and system classloaders to improve compilation performance.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@32483 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-api/drools-api/src/main/java/org/drools/util/ClassLoaderUtil.java
+++ kogito-api/drools-api/src/main/java/org/drools/util/ClassLoaderUtil.java
@@ -1,39 +1,37 @@
 package org.drools.util;
 
-import java.util.ArrayList;
-import java.util.IdentityHashMap;
-import java.util.List;
-import java.util.Map.Entry;
-
 public class ClassLoaderUtil {
-    public static ClassLoader getClassLoader(final ClassLoader classLoader, Class cls) {
+    public static CompositeClassLoader getClassLoader(final ClassLoader classLoader,
+                                                      final Class< ? > cls,
+                                                      final boolean enableCache) {
         ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
-        ClassLoader currentClassLoader = ( cls != null ) ? cls.getClassLoader() : ClassLoaderUtil.class.getClassLoader();
-        ClassLoader systemClassLoader = Class.class.getClassLoader().getSystemClassLoader();
-        
-        CompositeClassLoader cl = new CompositeClassLoader( null );        
-        
-        if (classLoader != null ) {
+        ClassLoader currentClassLoader = (cls != null) ? cls.getClassLoader() : ClassLoaderUtil.class.getClassLoader();
+        ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+
+        CompositeClassLoader cl = new CompositeClassLoader( null );
+
+        if ( classLoader != null ) {
             // the user specified classloader
-        	cl.addClassLoader(classLoader); 
+            cl.addClassLoader( classLoader );
         }
-        
-        if (currentClassLoader != null ) {
+
+        if ( currentClassLoader != null ) {
             // the current classloader, typically from a drools-core or drools-compiler class
-            cl.addClassLoader(currentClassLoader); 
-        }         
-        
-        if (contextClassLoader != null ) {
+            cl.addClassLoader( currentClassLoader );
+        }
+
+        if ( contextClassLoader != null ) {
             // context classloader
-        	cl.addClassLoader(contextClassLoader); 
-        }       
-        
-        if (systemClassLoader != null ) {
+            cl.addClassLoader( contextClassLoader );
+        }
+
+        if ( systemClassLoader != null ) {
             // system classloader
-        	cl.addClassLoader(systemClassLoader); 
-        }        
-        
+            cl.addClassLoader( systemClassLoader );
+        }
+
+        cl.setCachingEnabled( enableCache );
+
         return cl;
-                       
-    } 
+    }
 }
