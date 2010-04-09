db8affe2cc52cc7aa3ac2675d0bb678e9ae13493
Edson Tirelli <ed.tirelli@gmail.com>
2010-04-09 03:08:57 +0000

    JBRULES-2067: adding support to class resolution caching on user and system classloaders to improve compilation performance.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@32483 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-api/src/main/java/org/drools/util/CompositeClassLoader.java
+++ drools-api/src/main/java/org/drools/util/CompositeClassLoader.java
@@ -5,17 +5,30 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class CompositeClassLoader extends ClassLoader {
     /* Assumption: modifications are really rare, but iterations are frequent. */
-    private final List<ClassLoader> classLoaders = new CopyOnWriteArrayList<ClassLoader>();
+    private final List<ClassLoader>       classLoaders = new CopyOnWriteArrayList<ClassLoader>();
+    private final AtomicReference<Loader> loader       = new AtomicReference<Loader>();
 
     public CompositeClassLoader(final ClassLoader parentClassLoader) {
         super( null );
+        loader.set( new DefaultLoader() );
+    }
+
+    public synchronized void setCachingEnabled(boolean enabled) {
+        if ( enabled && loader.get() instanceof DefaultLoader ) {
+            loader.set( new CachingLoader() );
+        } else if ( !enabled && loader.get() instanceof CachingLoader ) {
+            loader.set( DefaultLoader.INSTANCE );
+        }
     }
 
     public synchronized void addClassLoader(final ClassLoader classLoader) {
@@ -30,6 +43,7 @@ public class CompositeClassLoader extends ClassLoader {
             }
         }
         this.classLoaders.add( classLoader );
+        this.loader.get().reset();
     }
 
     public synchronized void removeClassLoader(final ClassLoader classLoader) {
@@ -37,6 +51,7 @@ public class CompositeClassLoader extends ClassLoader {
          * addClassLoader(x) and removeClassLoader(x).
          */
         classLoaders.remove( classLoader );
+        this.loader.get().reset();
     }
 
     /**
@@ -45,25 +60,9 @@ public class CompositeClassLoader extends ClassLoader {
      */
     public Class< ? > loadClass(final String name,
                                 final boolean resolve) throws ClassNotFoundException {
-        // search the child ClassLoaders
-        Class< ? > cls = null;
-
-        for ( final ClassLoader classLoader : this.classLoaders ) {
-            try {            	
-                cls = Class.forName( name, true, classLoader );
-            } catch ( ClassNotFoundException e ) {
-                // swallow as we need to check more classLoaders
-            }
-            if ( cls != null ) {
-                break;
-            }
-        }
-
-        if ( resolve ) {
-            resolveClass( cls );
-        }
-
-        return cls;
+        return loader.get().load( this,
+                                  name,
+                                  resolve );
     }
 
     /**
@@ -111,10 +110,119 @@ public class CompositeClassLoader extends ClassLoader {
         }
     }
 
+    public void dumpStats() {
+        System.out.println( loader.toString() );
+    }
+
+    private static interface Loader {
+        public Class< ? > load(final CompositeClassLoader cl,
+                               final String name,
+                               final boolean resolve);
+
+        public void reset();
+    }
+
+    private static class DefaultLoader
+        implements
+        Loader {
+
+        // this class is stateless, so lets make a singleton of it
+        public static final DefaultLoader INSTANCE = new DefaultLoader();
+
+        private DefaultLoader() {
+        }
+
+        public Class< ? > load(final CompositeClassLoader cl,
+                               final String name,
+                               final boolean resolve) {
+            // search the child ClassLoaders
+            Class< ? > cls = null;
+
+            for ( final ClassLoader classLoader : cl.classLoaders ) {
+                try {
+                    cls = Class.forName( name,
+                                         true,
+                                         classLoader );
+                } catch ( ClassNotFoundException e ) {
+                    // swallow as we need to check more classLoaders
+                }
+                if ( cls != null ) {
+                    break;
+                }
+            }
+
+            if ( resolve ) {
+                cl.resolveClass( cls );
+            }
+
+            return cls;
+        }
+
+        public void reset() {
+            // nothing to do
+        }
+    }
+
+    private static class CachingLoader
+        implements
+        Loader {
+
+        private final Map<String, Object> classLoaderResultMap = new HashMap<String, Object>();
+        public long                       successfulCalls      = 0;
+        public long                       failedCalls          = 0;
+        public long                       cacheHits            = 0;
+
+        public Class< ? > load(final CompositeClassLoader cl,
+                               final String name,
+                               final boolean resolve) {
+            if ( classLoaderResultMap.containsKey( name ) ) {
+                cacheHits++;
+                return (Class< ? >) classLoaderResultMap.get( name );
+            }
+            // search the child ClassLoaders
+            Class< ? > cls = null;
+
+            for ( final ClassLoader classLoader : cl.classLoaders ) {
+                try {
+                    cls = Class.forName( name,
+                                         true,
+                                         classLoader );
+                } catch ( ClassNotFoundException e ) {
+                    // swallow as we need to check more classLoaders
+                }
+                if ( cls != null ) {
+                    break;
+                }
+            }
+
+            if ( resolve ) {
+                cl.resolveClass( cls );
+            }
+
+            classLoaderResultMap.put( name,
+                                      cls );
+            if ( cls != null ) {
+                this.successfulCalls++;
+            } else {
+                this.failedCalls++;
+            }
+
+            return cls;
+        }
+
+        public void reset() {
+            this.successfulCalls = this.failedCalls = this.cacheHits = 0;
+        }
+
+        public String toString() {
+            return new StringBuilder().append( "TotalCalls: " ).append( successfulCalls + failedCalls + cacheHits ).append( " CacheHits: " ).append( cacheHits ).append( " successfulCalls: " ).append( successfulCalls ).append( " FailedCalls: " ).append( failedCalls ).toString();
+        }
+    }
+
     private static class CompositeEnumeration<URL>
         implements
         Enumeration<URL> {
-        private List<URL> list;
+        private List<URL>     list;
         private Iterator<URL> it;
 
         public void addEnumeration(Enumeration<URL> enumeration) {
@@ -122,15 +230,15 @@ public class CompositeClassLoader extends ClassLoader {
                 // don't add it, if it's empty
                 return;
             }
-            
+
             if ( this.it != null ) {
                 throw new IllegalStateException( "cannot add more enumerations while iterator" );
             }
-            
+
             if ( this.list == null ) {
                 this.list = new ArrayList<URL>();
             }
-            
+
             while ( enumeration.hasMoreElements() ) {
                 this.list.add( enumeration.nextElement() );
             }
