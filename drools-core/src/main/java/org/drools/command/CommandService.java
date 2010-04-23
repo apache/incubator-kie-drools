938223728a5f9905100c06883c8a83becc0f0284
Pablo Nussembaum <baunax@gmail.com>
2010-04-23 21:11:23 +0000

    removed unused imports
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@32632 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/main/java/org/drools/command/CommandService.java
+++ drools-core/src/main/java/org/drools/command/CommandService.java
@@ -1,7 +1,5 @@
 package org.drools.command;
 
-import org.drools.StatefulSession;
-import org.drools.command.Context;
 import org.drools.command.impl.GenericCommand;
 
 public interface CommandService {
