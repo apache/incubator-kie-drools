d9e612ad28022cd050a55eceed8dbf9591f233ee
Pablo Nussembaum <baunax@gmail.com>
2010-05-03 20:07:20 +0000

    JBRULES-2499 Create a new module to host IDE common behavior 	- moved guvnor's packages from drools-compiler to drools-ide-common
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@32713 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-compiler/src/main/java/org/drools/guvnor/server/util/BRLPersistence.java
+++
@@ -1,9 +0,0 @@
-package org.drools.guvnor.server.util;
-
-import org.drools.guvnor.client.modeldriven.brl.RuleModel;
-
-public interface BRLPersistence {
-
-    public String marshal(final RuleModel model);
-    public RuleModel unmarshal(final String str);
-}
