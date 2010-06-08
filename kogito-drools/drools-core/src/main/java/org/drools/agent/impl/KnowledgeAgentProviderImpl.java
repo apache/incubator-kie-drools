7c8f78e79f71377f697248ea56448e9bd1186feb
Esteban Aliverti <esteban.aliverti@gmail.com>
2010-06-08 18:53:47 +0000

    JBRULES-2541: KnowledgeAgent: Add the possibility to add custom KnowledgeBuilderConfiguration 	- KnowledgeAgentFactory now provides a new method to construct a new KnowledgeAgent using a custom KnowledgeBuilderConfiguration object. 	- KnowledgeAgentImpl now have a new constructor parameter that permits to use a custom KnowledgeBuilderConfiguration object in every insternal builder it uses.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@33409 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/main/java/org/drools/agent/impl/KnowledgeAgentProviderImpl.java
+++ kogito-drools/drools-core/src/main/java/org/drools/agent/impl/KnowledgeAgentProviderImpl.java
@@ -3,10 +3,10 @@ package org.drools.agent.impl;
 import java.util.Properties;
 
 import org.drools.KnowledgeBase;
-import org.drools.SystemEventListener;
 import org.drools.agent.KnowledgeAgent;
 import org.drools.agent.KnowledgeAgentConfiguration;
 import org.drools.agent.KnowledgeAgentProvider;
+import org.drools.builder.KnowledgeBuilderConfiguration;
 
 public class KnowledgeAgentProviderImpl implements KnowledgeAgentProvider {
     
@@ -20,13 +20,18 @@ public class KnowledgeAgentProviderImpl implements KnowledgeAgentProvider {
 
     public KnowledgeAgent newKnowledgeAgent(String name,
                                             KnowledgeBase kbase) {
-        return new KnowledgeAgentImpl(name, kbase, new KnowledgeAgentConfigurationImpl() );
+        return new KnowledgeAgentImpl(name, kbase, new KnowledgeAgentConfigurationImpl(),null );
     }
 
     public KnowledgeAgent newKnowledgeAgent(String name,
                                             KnowledgeBase kbase,
                                             KnowledgeAgentConfiguration configuration) {
-        return new KnowledgeAgentImpl(name, kbase, configuration);
+        return new KnowledgeAgentImpl(name, kbase, configuration, null);
     }
+
+    public KnowledgeAgent newKnowledgeAgent(String name, KnowledgeBase kbase, KnowledgeAgentConfiguration configuration, KnowledgeBuilderConfiguration builderConfiguration) {
+        return new KnowledgeAgentImpl(name, kbase, configuration, builderConfiguration);
+    }
+
 
 }
