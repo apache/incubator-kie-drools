81ed49762a4754c931856d0814d5ef7ed35fa9c0
Mark Proctor <mdproctor@gmail.com>
2010-07-17 05:43:45 +0000

    -deleted vsm, as no longer used -moved grid stuff out of drools-api for now, until ready to merge and sure it's got correct interface/impl separation
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@33987 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-api/src/main/java/org/drools/vsm/HumanTaskService.java
+++
@@ -1,5 +0,0 @@
-package org.drools.vsm;
-
-public interface HumanTaskService {
-
-}
--- drools-api/src/main/java/org/drools/vsm/HumanTaskServiceProvider.java
+++
@@ -1,7 +0,0 @@
-package org.drools.vsm;
-
-public interface HumanTaskServiceProvider {
-
-	HumanTaskService newHumanTaskServiceClient();
-
-}
