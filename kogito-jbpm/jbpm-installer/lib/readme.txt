849015383c8302b4bfcad5e59d826fbc3c5c5c9f
kverlaen <kverlaen@localhost.localdomain>
2010-09-28 15:44:01 +0200

    adding installer

---
+++ kogito-jbpm/jbpm-installer/lib/readme.txt
@@ -0,0 +1,24 @@
+This location will be used to download the necessary installation files.
+You can also drop them here if you don't want ANT to download them for you:
+
+JBoss AS:
+  jboss-4.2.3.GA.zip
+
+Birt runtime engine:
+  birt-runtime-2_3_2_2.zip
+
+Eclipse: 
+  eclipse-SDK-3.5.1-linux-gtk.tar.gz (linux)
+  eclipse-SDK-3.5.1-win32.zip (windows)
+  eclipse-SDK-3.5.1-macosx-carbon.tar.gz (mac)
+
+Eclipse GEF:
+  GEF-runtime-3.5.1.zip
+
+jBPM:
+  jbpm-#{drools.version}-bin.zip
+  jbpm-#{drools.version}-eclipse-all.zip
+  jbpm-#{drools.version}-gwt-console.zip
+
+Drools:
+  drools-#{drools.version}-guvnor.war
---
+++ kogito-jbpm/jbpm-installer/sample/evaluation/src/main/java/com/sample/ProcessTest.java
@@ -0,0 +1,56 @@
+package com.sample;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.logger.KnowledgeRuntimeLogger;
+import org.drools.logger.KnowledgeRuntimeLoggerFactory;
+import org.jbpm.process.workitem.wsht.WSHumanTaskHandler;
+import org.drools.runtime.StatefulKnowledgeSession;
+
+/**
+ * This is a sample file to launch a process.
+ */
+public class ProcessTest {
+
+	public static final void main(String[] args) {
+		try {
+			// load up the knowledge base
+			KnowledgeBase kbase = readKnowledgeBase();
+			StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+			KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newThreadedFileLogger(ksession, "test", 1000);
+			ksession.getWorkItemManager().registerWorkItemHandler("Human Task", new WSHumanTaskHandler());
+			// start a new process instance
+			Map<String, Object> params = new HashMap<String, Object>();
+			params.put("employee", "krisv");
+			ksession.startProcess("com.sample.evaluation", params);
+			logger.close();
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
+	private static KnowledgeBase readKnowledgeBase() throws Exception {
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+		kbuilder.add(ResourceFactory.newClassPathResource("Evaluation.bpmn"), ResourceType.BPMN2);
+		KnowledgeBuilderErrors errors = kbuilder.getErrors();
+		if (errors.size() > 0) {
+			for (KnowledgeBuilderError error: errors) {
+				System.err.println(error);
+			}
+			throw new IllegalArgumentException("Could not parse knowledge.");
+		}
+		KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+		kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+		return kbase;
+	}
+
+}
