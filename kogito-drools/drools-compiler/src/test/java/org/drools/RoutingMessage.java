36c91330519962b5dd34a8c5e261f8f843b62e3c
Tihomir Surdilovic <tsurdilo@redhat.com>
2010-10-25 19:01:05 +0000

    JBRULES-2753 : Add the StrEvaluator to the list of core evaluators
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@35719 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ kogito-drools/drools-compiler/src/test/java/org/drools/RoutingMessage.java
@@ -0,0 +1,14 @@
+package org.drools;
+
+public class RoutingMessage {
+    private String routingValue;
+
+    public String getRoutingValue() {
+            return routingValue;
+    }
+
+    public void setRoutingValue(String routingValue) {
+            this.routingValue = routingValue;
+    }
+}
+
