b9d4185aad11f61541441ad9a3c3774ade97732c
Mauricio Salatino <salaboy@gmail.com>
2010-10-28 20:22:54 +0000

    JBRULES-2746: Drools Grid Impl2             - refactor timerservice into schedulerservice and timerservice
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@35790 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-core/src/main/java/org/drools/time/SchedulerService.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2010 salaboy.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * under the License.
+ */
+package org.drools.time;
+
+/**
+ *
+ * @author salaboy
+ */
+public interface SchedulerService {
+
+    /**
+     * Schedule a job for later execution
+     * 
+     * @param job
+     * @param ctx
+     * @param trigger
+     * 
+     * @return
+     */
+    public JobHandle scheduleJob(Job job, JobContext ctx, Trigger trigger);
+
+    /**
+     * Remove the job identified by the given job handle from the 
+     * scheduled queue
+     * 
+     * @param jobHandle the job identity handle
+     * 
+     * @return
+     */
+    public boolean removeJob(JobHandle jobHandle);
+}
