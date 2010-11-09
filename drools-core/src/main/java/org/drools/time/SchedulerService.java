804798b11138f3016a35bf5a42c30c5cf65b6a81
Mark Proctor <mdproctor@gmail.com>
2010-11-09 13:08:02 +0000

    JBRULES-2746 Drools Grid Impl2 -more cleaning up and removing of redundant code
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@35924 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/main/java/org/drools/time/SchedulerService.java
+++ drools-core/src/main/java/org/drools/time/SchedulerService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 salaboy.
+* Copyright 2010 salaboy.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
