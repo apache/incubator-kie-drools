a595b894a24223e6d926ea30985c29012cb046da
Kris Verlaenen <kris.verlaenen@gmail.com>
2010-11-26 15:26:24 +0000

    - fixing process variable logging
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@36087 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- kogito-drools/drools-core/src/main/java/org/drools/event/ProcessEventSupport.java
+++ kogito-drools/drools-core/src/main/java/org/drools/event/ProcessEventSupport.java
@@ -23,6 +23,7 @@ import org.drools.event.process.ProcessEventListener;
 import org.drools.event.process.ProcessNodeLeftEvent;
 import org.drools.event.process.ProcessNodeTriggeredEvent;
 import org.drools.event.process.ProcessStartedEvent;
+import org.drools.event.process.ProcessVariableChangedEvent;
 import org.drools.runtime.KnowledgeRuntime;
 import org.drools.runtime.process.NodeInstance;
 import org.drools.runtime.process.ProcessInstance;
@@ -125,6 +126,36 @@ public class ProcessEventSupport extends AbstractEventSupport<ProcessEventListen
 
             do{
                 iter.next().afterNodeLeft(event);
+            } while (iter.hasNext());
+        }
+    }
+
+    public void fireBeforeVariableChanged(final String id, final String instanceId, 
+    		final Object oldValue, final Object newValue, 
+    		final ProcessInstance processInstance, KnowledgeRuntime kruntime) {
+        final Iterator<ProcessEventListener> iter = getEventListenersIterator();
+
+        if (iter.hasNext()) {
+            final ProcessVariableChangedEvent event = new ProcessVariableChangedEventImpl(
+        		id, instanceId, oldValue, newValue, processInstance, kruntime);
+
+            do {
+                iter.next().beforeVariableChanged(event);
+            } while (iter.hasNext());
+        }
+    }
+
+    public void fireAfterVariableChanged(final String name, final String id, 
+    		final Object oldValue, final Object newValue, 
+    		final ProcessInstance processInstance, KnowledgeRuntime kruntime) {
+        final Iterator<ProcessEventListener> iter = getEventListenersIterator();
+
+        if (iter.hasNext()) {
+            final ProcessVariableChangedEvent event = new ProcessVariableChangedEventImpl(
+        		name, id, oldValue, newValue, processInstance, kruntime);
+
+            do {
+                iter.next().afterVariableChanged(event);
             } while (iter.hasNext());
         }
     }
---
+++ kogito-drools/drools-core/src/main/java/org/drools/event/ProcessVariableChangedEventImpl.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2005 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.event;
+
+import org.drools.event.process.ProcessVariableChangedEvent;
+import org.drools.runtime.KnowledgeRuntime;
+import org.drools.runtime.process.ProcessInstance;
+
+/**
+ * @author <a href="mailto:kris_verlaenen@hotmail.com">Kris Verlaenen</a>
+ */
+public class ProcessVariableChangedEventImpl extends ProcessEvent implements ProcessVariableChangedEvent {
+
+    private static final long serialVersionUID = 510l;
+    
+    private String id;
+    private String instanceId;
+    private Object oldValue;
+    private Object newValue;
+
+    public ProcessVariableChangedEventImpl(final String id, final String instanceId,
+    		final Object oldValue, final Object newValue, 
+    		final ProcessInstance processInstance, KnowledgeRuntime kruntime) {
+        super( processInstance, kruntime );
+        this.id = id;
+        this.instanceId = instanceId;
+        this.oldValue = oldValue;
+        this.newValue = newValue;
+    }
+    
+    public String getVariableInstanceId() {
+        return instanceId;
+    }
+    
+    public String getVariableId() {
+    	return id;
+    }
+    
+    public Object getOldValue() {
+    	return oldValue;
+    }
+    
+    public Object getNewValue() {
+    	return newValue;
+    }
+
+    public String toString() {
+        return "==>[ProcessVariableChanged(id=" + id + "; instanceId=" + instanceId + "; oldValue=" + oldValue + "; newValue=" + newValue
+            + "; processName=" + getProcessInstance().getProcessName() + "; processId=" + getProcessInstance().getProcessId() + ")]";
+    }
+}
