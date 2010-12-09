05a75d743141463d8d20824cebee00ee924e37df
Kurt T Stam <kstam@redhat.com>
2010-12-09 06:23:31 +0000

    GUVNORSOA-47, merging ModeShape Integration, reuse an active JAAS session. I've put the login in our JCR API since it seems to be different to do this for JR and MS.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@36296 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-repo/drools-repository/src/main/java/org/drools/repository/RulesRepositoryConfigurator.java
+++ drools-repo/drools-repository/src/main/java/org/drools/repository/RulesRepositoryConfigurator.java
@@ -82,75 +82,83 @@ public class RulesRepositoryConfigurator {
 		return rulesRepositoryConfigurator;
 	}
 	
-
-/**
- * Attempts to setup the repository. If the work that it tries to do has already been done, it will return without modifying
- * the repository. This will register any node types, and setup bootstrap nodes as needed. This will not erase any data.
- * 
- * @throws RepositoryException
- */
-public void setupRepository(Session session) throws RepositoryException {
-	log.info("Setting up the repository, registering node types etc.");
-	try {
-		Node root = session.getRootNode();
-		Workspace ws = session.getWorkspace();
-
-		//no need to set it up again, skip it if it has.
-		boolean registered = RulesRepositoryAdministrator.isNamespaceRegistered( session );
- 
-		if (!registered) {
-			ws.getNamespaceRegistry().registerNamespace("drools", RulesRepository.DROOLS_URI);
-
-			//Note, the order in which they are registered actually does matter !
-			jcrRepositoryConfigurator.registerNodeTypesFromCndFile("/node_type_definitions/tag_node_type.cnd", session, ws);
-			jcrRepositoryConfigurator.registerNodeTypesFromCndFile("/node_type_definitions/state_node_type.cnd", session, ws);
-			jcrRepositoryConfigurator.registerNodeTypesFromCndFile("/node_type_definitions/versionable_node_type.cnd", session, ws);
-			jcrRepositoryConfigurator.registerNodeTypesFromCndFile("/node_type_definitions/versionable_asset_folder_node_type.cnd", session, ws);
-
-			jcrRepositoryConfigurator.registerNodeTypesFromCndFile("/node_type_definitions/rule_node_type.cnd", session, ws);
-			jcrRepositoryConfigurator.registerNodeTypesFromCndFile("/node_type_definitions/rulepackage_node_type.cnd", session, ws);
-		}
-
-		// Setup the rule repository node
-		Node repositoryNode = RulesRepository.addNodeIfNew(root, RulesRepository.RULES_REPOSITORY_NAME, "nt:folder");
-
-
-
-		// Setup the RulePackageItem area        
-		Node packageAreaNode = RulesRepository.addNodeIfNew(repositoryNode, RulesRepository.RULE_PACKAGE_AREA, "nt:folder");
-
-		// Setup the global area        
-		if(!packageAreaNode.hasNode(RulesRepository.RULE_GLOBAL_AREA)){
-			Node globalAreaNode = RulesRepository.addNodeIfNew(packageAreaNode, RulesRepository.RULE_GLOBAL_AREA, PackageItem.RULE_PACKAGE_TYPE_NAME);
-			globalAreaNode.addNode( PackageItem.ASSET_FOLDER_NAME,  "drools:versionableAssetFolder" );
-			globalAreaNode.setProperty( PackageItem.TITLE_PROPERTY_NAME,  RulesRepository.RULE_GLOBAL_AREA);
-			globalAreaNode.setProperty( AssetItem.DESCRIPTION_PROPERTY_NAME, "the global area that holds sharable assets");         
-			globalAreaNode.setProperty(AssetItem.FORMAT_PROPERTY_NAME,	PackageItem.PACKAGE_FORMAT);
-			globalAreaNode.setProperty(PackageItem.CREATOR_PROPERTY_NAME, session.getUserID());
-			Calendar lastModified = Calendar.getInstance();
-			globalAreaNode.setProperty(PackageItem.LAST_MODIFIED_PROPERTY_NAME,	lastModified);
-		}
-
-		// Setup the Snapshot area        
-		RulesRepository.addNodeIfNew(repositoryNode, RulesRepository.PACKAGE_SNAPSHOT_AREA, "nt:folder");
-
-		//Setup the Category area                
-		RulesRepository.addNodeIfNew(repositoryNode, RulesRepository.TAG_AREA, "nt:folder");
-
-		//Setup the State area                
-		RulesRepository.addNodeIfNew(repositoryNode, RulesRepository.STATE_AREA, "nt:folder");
-
-		//and we need the "Draft" state
-		RulesRepository.addNodeIfNew( repositoryNode.getNode( RulesRepository.STATE_AREA ), StateItem.DRAFT_STATE_NAME, StateItem.STATE_NODE_TYPE_NAME );
-
-		session.save();                        
+	public Session login(String userName, String password) throws RepositoryException {
+		return jcrRepositoryConfigurator.login(userName, password);
 	}
-	catch(Exception e) {
-		log.error("Caught Exception", e);
-		System.err.println(e.getMessage());
-		throw new RepositoryException(e);
+	
+	public Session login(String userName) throws RepositoryException {
+		return jcrRepositoryConfigurator.login(userName);
 	}
-}
+	
+
+	/**
+	 * Attempts to setup the repository. If the work that it tries to do has already been done, it will return without modifying
+	 * the repository. This will register any node types, and setup bootstrap nodes as needed. This will not erase any data.
+	 * 
+	 * @throws RepositoryException
+	 */
+	public void setupRepository(Session session) throws RepositoryException {
+		log.info("Setting up the repository, registering node types etc.");
+		try {
+			Node root = session.getRootNode();
+			Workspace ws = session.getWorkspace();
+	
+			//no need to set it up again, skip it if it has.
+			boolean registered = RulesRepositoryAdministrator.isNamespaceRegistered( session );
+	 
+			if (!registered) {
+				ws.getNamespaceRegistry().registerNamespace("drools", RulesRepository.DROOLS_URI);
+	
+				//Note, the order in which they are registered actually does matter !
+				jcrRepositoryConfigurator.registerNodeTypesFromCndFile("/node_type_definitions/tag_node_type.cnd", session, ws);
+				jcrRepositoryConfigurator.registerNodeTypesFromCndFile("/node_type_definitions/state_node_type.cnd", session, ws);
+				jcrRepositoryConfigurator.registerNodeTypesFromCndFile("/node_type_definitions/versionable_node_type.cnd", session, ws);
+				jcrRepositoryConfigurator.registerNodeTypesFromCndFile("/node_type_definitions/versionable_asset_folder_node_type.cnd", session, ws);
+	
+				jcrRepositoryConfigurator.registerNodeTypesFromCndFile("/node_type_definitions/rule_node_type.cnd", session, ws);
+				jcrRepositoryConfigurator.registerNodeTypesFromCndFile("/node_type_definitions/rulepackage_node_type.cnd", session, ws);
+			}
+	
+			// Setup the rule repository node
+			Node repositoryNode = RulesRepository.addNodeIfNew(root, RulesRepository.RULES_REPOSITORY_NAME, "nt:folder");
+	
+	
+	
+			// Setup the RulePackageItem area        
+			Node packageAreaNode = RulesRepository.addNodeIfNew(repositoryNode, RulesRepository.RULE_PACKAGE_AREA, "nt:folder");
+	
+			// Setup the global area        
+			if(!packageAreaNode.hasNode(RulesRepository.RULE_GLOBAL_AREA)){
+				Node globalAreaNode = RulesRepository.addNodeIfNew(packageAreaNode, RulesRepository.RULE_GLOBAL_AREA, PackageItem.RULE_PACKAGE_TYPE_NAME);
+				globalAreaNode.addNode( PackageItem.ASSET_FOLDER_NAME,  "drools:versionableAssetFolder" );
+				globalAreaNode.setProperty( PackageItem.TITLE_PROPERTY_NAME,  RulesRepository.RULE_GLOBAL_AREA);
+				globalAreaNode.setProperty( AssetItem.DESCRIPTION_PROPERTY_NAME, "the global area that holds sharable assets");         
+				globalAreaNode.setProperty(AssetItem.FORMAT_PROPERTY_NAME,	PackageItem.PACKAGE_FORMAT);
+				globalAreaNode.setProperty(PackageItem.CREATOR_PROPERTY_NAME, session.getUserID());
+				Calendar lastModified = Calendar.getInstance();
+				globalAreaNode.setProperty(PackageItem.LAST_MODIFIED_PROPERTY_NAME,	lastModified);
+			}
+	
+			// Setup the Snapshot area        
+			RulesRepository.addNodeIfNew(repositoryNode, RulesRepository.PACKAGE_SNAPSHOT_AREA, "nt:folder");
+	
+			//Setup the Category area                
+			RulesRepository.addNodeIfNew(repositoryNode, RulesRepository.TAG_AREA, "nt:folder");
+	
+			//Setup the State area                
+			RulesRepository.addNodeIfNew(repositoryNode, RulesRepository.STATE_AREA, "nt:folder");
+	
+			//and we need the "Draft" state
+			RulesRepository.addNodeIfNew( repositoryNode.getNode( RulesRepository.STATE_AREA ), StateItem.DRAFT_STATE_NAME, StateItem.STATE_NODE_TYPE_NAME );
+	
+			session.save();                        
+		}
+		catch(Exception e) {
+			log.error("Caught Exception", e);
+			System.err.println(e.getMessage());
+			throw new RepositoryException(e);
+		}
+	}
 
 	public void shutdown() {
 		jcrRepositoryConfigurator.shutdown();
