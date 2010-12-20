db46372a2ea9f72a04d912058938f8b5873f94eb
Diego López León <dieguitoll@gmail.com>
2010-12-20 18:38:19 -0300

    package rename

--- kogito-drools/drools-persistence-jpa/src/main/java/org/drools/persistence/jpa/JpaJDKTimerService.java
+++ kogito-drools/drools-persistence-jpa/src/main/java/org/drools/persistence/jpa/JpaJDKTimerService.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.drools.persistence.session;
+package org.drools.persistence.jpa;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
