9c7207807a19509d1f5cdd0a718dc98d051e30df
Diego López León <dieguitoll@gmail.com>
2010-12-28 12:04:40 -0300

    JBRULES-2835: added constructor and equals+hashcode

--- drools-persistence-jpa/src/test/java/org/drools/persistence/map/impl/Buddy.java
+++ drools-persistence-jpa/src/test/java/org/drools/persistence/map/impl/Buddy.java
@@ -6,11 +6,38 @@ public class Buddy implements Serializable{
 
     private String name;
 
+    public Buddy() {
+    }
+
+    public Buddy(String string) {
+        this.name = string;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ( this == obj ) return true;
+        if ( obj == null ) return false;
+        if ( getClass() != obj.getClass() ) return false;
+        Buddy other = (Buddy) obj;
+        if ( name == null ) {
+            if ( other.name != null ) return false;
+        } else if ( !name.equals( other.name ) ) return false;
+        return true;
     }
 }
