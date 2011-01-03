5a513c7fb1998024f913d2840f4e8f1a16cce7aa
Diego López León <dieguitoll@gmail.com>
2011-01-03 16:48:33 -0300

    JBRULES-2835: renaming interface to make them more specific

--- drools-persistence-jpa/src/main/java/org/drools/persistence/map/KnowledgeSessionStorageEnvironmentBuilder.java
+++ drools-persistence-jpa/src/main/java/org/drools/persistence/map/KnowledgeSessionStorageEnvironmentBuilder.java
@@ -3,20 +3,26 @@ package org.drools.persistence.map;
 import org.drools.persistence.PersistenceContextManager;
 import org.drools.persistence.TransactionManager;
 
-public class AbstractStorageEnvironmentBuilder {
+public class KnowledgeSessionStorageEnvironmentBuilder implements EnvironmentBuilder {
 
     private MapBasedPersistenceContext persistenceContext;
-    private AbstractStorage storage;
+    private KnowledgeSessionStorage storage;
 
-    public AbstractStorageEnvironmentBuilder(AbstractStorage storage) {
+    public KnowledgeSessionStorageEnvironmentBuilder(KnowledgeSessionStorage storage) {
         this.storage = storage;
         this.persistenceContext = new MapBasedPersistenceContext( storage );
     }
     
+    /* (non-Javadoc)
+     * @see org.drools.persistence.map.EnvironmentBuilder#getPersistenceContextManager()
+     */
     public PersistenceContextManager getPersistenceContextManager(){
         return new MapPersistenceContextManager( persistenceContext );
     }
     
+    /* (non-Javadoc)
+     * @see org.drools.persistence.map.EnvironmentBuilder#getTransactionManager()
+     */
     public TransactionManager getTransactionManager(){
         return new ManualTransactionManager( persistenceContext, storage );
     }
