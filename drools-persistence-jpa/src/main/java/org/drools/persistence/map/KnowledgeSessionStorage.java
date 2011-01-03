ce5a4c1efcd6ee898678812e947559ef8f0f6363
Diego López León <dieguitoll@gmail.com>
2011-01-03 16:48:33 -0300

    JBRULES-2835: renaming interface to make them more specific

--- drools-persistence-jpa/src/main/java/org/drools/persistence/map/KnowledgeSessionStorage.java
+++ drools-persistence-jpa/src/main/java/org/drools/persistence/map/KnowledgeSessionStorage.java
@@ -2,7 +2,7 @@ package org.drools.persistence.map;
 
 import org.drools.persistence.info.SessionInfo;
 
-public interface AbstractStorage {
+public interface KnowledgeSessionStorage {
 
     SessionInfo findSessionInfo(Long id);
 
