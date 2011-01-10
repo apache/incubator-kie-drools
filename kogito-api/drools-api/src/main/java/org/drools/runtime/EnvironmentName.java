11b881c0c8fc6fe7ff024fa9f04cc88d544521b9
Diego López León <dieguitoll@gmail.com>
2011-01-10 20:19:17 -0300

    JBRULES-2835: added backward compatible configuration

--- kogito-api/drools-api/src/main/java/org/drools/runtime/EnvironmentName.java
+++ kogito-api/drools-api/src/main/java/org/drools/runtime/EnvironmentName.java
@@ -24,6 +24,7 @@ public class EnvironmentName {
     public static final String ENTITY_MANAGER_FACTORY               = "drools.persistence.jpa.EntityManagerFactory";
     public static final String CMD_SCOPED_ENTITY_MANAGER            = "drools.persistence.jpa.CmdScopedEntityManager";
     public static final String APP_SCOPED_ENTITY_MANAGER            = "drools.persistence.jpa.AppScopedEntityManager";
+    public static final String PERSISTENCE_CONTEXT_MANAGER          = "drools.persistence.PersistenceContextManager";
 
     public static final String OBJECT_MARSHALLING_STRATEGIES        = "drools.marshalling.ObjectMarshallingStrategies";
     public static final String GLOBALS                              = "drools.Globals";
