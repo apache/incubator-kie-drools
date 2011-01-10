246bfddfa71b172318f78d427ccbd6e7dc345f0a
Diego López León <dieguitoll@gmail.com>
2011-01-10 20:22:59 -0300

    JBRULES-2835: add test refactor with different environments

--- drools-persistence-jpa/src/test/java/org/drools/persistence/map/impl/MapPersistenceTest.java
+++ drools-persistence-jpa/src/test/java/org/drools/persistence/map/impl/MapPersistenceTest.java
@@ -1,8 +1,5 @@
 package org.drools.persistence.map.impl;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.drools.KnowledgeBase;
 import org.drools.KnowledgeBaseFactory;
 import org.drools.builder.KnowledgeBuilder;
@@ -11,61 +8,24 @@ import org.drools.builder.KnowledgeBuilderErrors;
 import org.drools.builder.KnowledgeBuilderFactory;
 import org.drools.builder.ResourceType;
 import org.drools.io.impl.ByteArrayResource;
-import org.drools.persistence.info.SessionInfo;
-import org.drools.persistence.info.WorkItemInfo;
-import org.drools.persistence.jpa.JPAKnowledgeService;
-import org.drools.persistence.map.EnvironmentBuilder;
-import org.drools.persistence.map.KnowledgeSessionStorage;
-import org.drools.persistence.map.KnowledgeSessionStorageEnvironmentBuilder;
-import org.drools.runtime.Environment;
-import org.drools.runtime.EnvironmentName;
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.rule.FactHandle;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MapPersistenceTest {
+public abstract class MapPersistenceTest {
 
     @Test
     public void createPersistentSession() {
         KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
-        KnowledgeSessionStorage storage = new KnowledgeSessionStorage() {
-
-            public void saveOrUpdate(SessionInfo storedObject) {
-                System.out.println( "saving sessionInfo" );
-            }
-
-            public SessionInfo findSessionInfo(Long id) {
-                System.out.println( "finding sessionInfo" );
-                return null;
-            }
-
-            public void saveOrUpdate(WorkItemInfo workItemInfo) {
-                System.out.println( "saving workItemInfo" );
-            }
-
-            public Long getNextWorkItemId() {
-                return null;
-            }
-
-            public WorkItemInfo findWorkItemInfo(Long id) {
-                System.out.println( "finding workItemInfo" );
-                return null;
-            }
-
-            public void remove(WorkItemInfo workItemInfo) {
-                System.out.println( "removing workItemInfo" );
-            }
-        };
         
-        StatefulKnowledgeSession crmPersistentSession = createSession( kbase,
-                                                                       storage );
+        StatefulKnowledgeSession crmPersistentSession = createSession( kbase );
         crmPersistentSession.fireAllRules();
 
-        crmPersistentSession = createSession( kbase,
-                                              storage );
+        crmPersistentSession = createSession( kbase );
         Assert.assertNotNull( crmPersistentSession );
     }
+
 
     @Test
     public void createPersistentSessionWithRules() {
@@ -94,10 +54,7 @@ public class MapPersistenceTest {
         KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
         kbase.addKnowledgePackages( kbuilder.getKnowledgePackages() );
 
-        KnowledgeSessionStorage storage = getSimpleStorage();
-        
-        StatefulKnowledgeSession ksession = createSession( kbase,
-                                                           storage );
+        StatefulKnowledgeSession ksession = createSession( kbase );
 
         FactHandle buddyFactHandle = ksession.insert( new Buddy() );
         ksession.fireAllRules();
@@ -106,8 +63,7 @@ public class MapPersistenceTest {
                              ksession.getObjects().size() );
 
         ksession = disposeAndReloadSession( ksession,
-                                            kbase,
-                                            storage );
+                                            kbase );
 
         Assert.assertNotNull( ksession );
 
@@ -122,68 +78,37 @@ public class MapPersistenceTest {
     @Test
     public void dontCreateMoreSessionsThanNecessary() {
         KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
-        final Map<Long, SessionInfo> savedEntities = new HashMap<Long, SessionInfo>();
 
-        KnowledgeSessionStorage storage = new KnowledgeSessionStorage() {
-
-            private Map<Long, WorkItemInfo> workItems = new HashMap<Long, WorkItemInfo>();
-            
-            public void saveOrUpdate(SessionInfo storedObject) {
-                storedObject.update();
-                savedEntities.put( storedObject.getId(), storedObject );
-            }
-
-            public SessionInfo findSessionInfo(Long id) {
-                return savedEntities.get( id );
-            }
-
-            public void saveOrUpdate(WorkItemInfo workItemInfo) {
-                workItems.put( workItemInfo.getId(), workItemInfo );
-            }
-
-            public Long getNextWorkItemId() {
-                return new Long(workItems.size() + 1);
-            }
-
-            public WorkItemInfo findWorkItemInfo(Long id) {
-                return workItems.get( id );
-            }
-
-            public void remove(WorkItemInfo workItemInfo) {
-                workItems.remove( workItemInfo.getId() );
-            }
-        };
-
-        StatefulKnowledgeSession crmPersistentSession = createSession(kbase, storage);
+        StatefulKnowledgeSession crmPersistentSession = createSession(kbase);
 
         long ksessionId = crmPersistentSession.getId();
         crmPersistentSession.fireAllRules();
 
-        crmPersistentSession = disposeAndReloadSession(crmPersistentSession, kbase, storage);
+        crmPersistentSession = disposeAndReloadSession(crmPersistentSession, kbase);
 
         Assert.assertEquals(ksessionId, crmPersistentSession.getId());
 
         ksessionId = crmPersistentSession.getId();
         crmPersistentSession.fireAllRules();
 
-        crmPersistentSession = disposeAndReloadSession(crmPersistentSession, kbase, storage);
+        crmPersistentSession = disposeAndReloadSession(crmPersistentSession, kbase);
 
         crmPersistentSession.fireAllRules();
 
-        Assert.assertEquals(1, savedEntities.size());
+        Assert.assertEquals(1, getSavedSessionsCount());
         crmPersistentSession.dispose();
     }
+
 
     @Test
     public void insertObjectIntoKsessionAndRetrieve() {
         KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
-        KnowledgeSessionStorage storage = getSimpleStorage();
         
-        StatefulKnowledgeSession crmPersistentSession = createSession(kbase, storage);
+        StatefulKnowledgeSession crmPersistentSession = createSession(kbase);
         Buddy bestBuddy = new Buddy("john");
         crmPersistentSession.insert(bestBuddy);
 
-        crmPersistentSession = disposeAndReloadSession(crmPersistentSession, kbase, storage);
+        crmPersistentSession = disposeAndReloadSession(crmPersistentSession, kbase);
         Object obtainedBuddy = crmPersistentSession
                 .getObjects().iterator().next();
         Assert.assertNotSame( bestBuddy, obtainedBuddy );
@@ -192,69 +117,10 @@ public class MapPersistenceTest {
         crmPersistentSession.dispose();
     }
 
-    private KnowledgeSessionStorage getSimpleStorage() {
-        return new KnowledgeSessionStorage() {
+    protected abstract StatefulKnowledgeSession createSession(KnowledgeBase kbase);
+    
+    protected abstract StatefulKnowledgeSession disposeAndReloadSession(StatefulKnowledgeSession crmPersistentSession,
+                                                                        KnowledgeBase kbase);
 
-            private Map<Long, SessionInfo> ksessions = new HashMap<Long, SessionInfo>();
-            private Map<Long, WorkItemInfo> workItems = new HashMap<Long, WorkItemInfo>();
-            
-            public SessionInfo findSessionInfo(Long id) {
-                return ksessions.get( id );
-            }
-
-            public void saveOrUpdate(SessionInfo storedObject) {
-                storedObject.update();
-                ksessions.put( storedObject.getId(), storedObject );
-            }
-
-            public void saveOrUpdate(WorkItemInfo workItemInfo) {
-                workItems.put( workItemInfo.getId(), workItemInfo );
-            }
-
-            public Long getNextWorkItemId() {
-                return new Long(workItems.size() + 1);
-            }
-
-            public WorkItemInfo findWorkItemInfo(Long id) {
-                return workItems.get( id );
-            }
-
-            public void remove(WorkItemInfo workItemInfo) {
-                workItems.remove( workItemInfo.getId() );
-            }
-        };
-    }
-
-    private StatefulKnowledgeSession createSession(KnowledgeBase kbase,
-                                                   KnowledgeSessionStorage storage) {
-        
-        EnvironmentBuilder envBuilder = new KnowledgeSessionStorageEnvironmentBuilder( storage );
-        Environment env = KnowledgeBaseFactory.newEnvironment();
-        //FIXME temporary usage of this constants
-        env.set( EnvironmentName.TRANSACTION_MANAGER,
-                 envBuilder.getTransactionManager() );
-        env.set( EnvironmentName.ENTITY_MANAGER_FACTORY,
-                 envBuilder.getPersistenceContextManager() );
-
-        return JPAKnowledgeService.newStatefulKnowledgeSession( kbase,
-                                                                null,
-                                                                env );
-    }
-
-    private StatefulKnowledgeSession disposeAndReloadSession(StatefulKnowledgeSession ksession,
-                                                             KnowledgeBase kbase,
-                                                             KnowledgeSessionStorage storage) {
-        long sessionId = ksession.getId();
-        ksession.dispose();
-        EnvironmentBuilder envBuilder = new KnowledgeSessionStorageEnvironmentBuilder( storage );
-        Environment env = KnowledgeBaseFactory.newEnvironment();
-        //FIXME temporary usage of this constants
-        env.set( EnvironmentName.TRANSACTION_MANAGER,
-                 envBuilder.getTransactionManager() );
-        env.set( EnvironmentName.ENTITY_MANAGER_FACTORY,
-                 envBuilder.getPersistenceContextManager() );
-        
-        return JPAKnowledgeService.loadStatefulKnowledgeSession( sessionId, kbase, null, env );
-    }
-
+    protected abstract long getSavedSessionsCount();
 }
