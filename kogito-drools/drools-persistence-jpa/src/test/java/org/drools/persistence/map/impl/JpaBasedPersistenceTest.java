9d5fc39b582fb5080d38fd3a65b3b7b6ae653b2c
Diego López León <dieguitoll@gmail.com>
2011-01-10 20:22:59 -0300

    JBRULES-2835: add test refactor with different environments

---
+++ kogito-drools/drools-persistence-jpa/src/test/java/org/drools/persistence/map/impl/JpaBasedPersistenceTest.java
@@ -0,0 +1,75 @@
+package org.drools.persistence.map.impl;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.base.MapGlobalResolver;
+import org.drools.persistence.jpa.JPAKnowledgeService;
+import org.drools.runtime.Environment;
+import org.drools.runtime.EnvironmentName;
+import org.drools.runtime.StatefulKnowledgeSession;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import bitronix.tm.TransactionManagerServices;
+import bitronix.tm.resource.jdbc.PoolingDataSource;
+
+public class JpaBasedPersistenceTest extends MapPersistenceTest {
+
+    private static PoolingDataSource ds1;
+    
+    @BeforeClass
+    public static void setUp() throws Exception {
+        ds1 = new PoolingDataSource();
+        ds1.setUniqueName( "jdbc/testDS1" );
+        ds1.setClassName( "org.h2.jdbcx.JdbcDataSource" );
+        ds1.setMaxPoolSize( 3 );
+        ds1.setAllowLocalTransactions( true );
+        ds1.getDriverProperties().put( "user",
+                                       "sa" );
+        ds1.getDriverProperties().put( "password",
+                                       "sasa" );
+        ds1.getDriverProperties().put( "URL",
+                                       "jdbc:h2:mem:mydb" );
+        ds1.init();
+
+    }
+    
+    @AfterClass
+    public static void tearDown() throws Exception {
+        ds1.close();
+    }
+    
+    @Override
+    protected StatefulKnowledgeSession createSession(KnowledgeBase kbase) {
+        return JPAKnowledgeService.newStatefulKnowledgeSession( kbase, null, createEnvironment() );
+    }
+
+    @Override
+    protected StatefulKnowledgeSession disposeAndReloadSession(StatefulKnowledgeSession ksession,
+                                                               KnowledgeBase kbase) {
+        long ksessionId = ksession.getId();
+        ksession.dispose();
+        return JPAKnowledgeService.loadStatefulKnowledgeSession( ksessionId, kbase, null, createEnvironment() );
+    }
+
+    @Override
+    protected long getSavedSessionsCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    private Environment createEnvironment(){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory( "org.drools.persistence.jpa" );
+        Environment env = KnowledgeBaseFactory.newEnvironment();
+        env.set( EnvironmentName.ENTITY_MANAGER_FACTORY,
+                 emf );
+        env.set( EnvironmentName.TRANSACTION_MANAGER,
+                 TransactionManagerServices.getTransactionManager() );
+        env.set( EnvironmentName.GLOBALS, new MapGlobalResolver() );
+        
+        return env;
+    }
+}
