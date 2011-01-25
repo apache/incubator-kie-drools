68774548e92a9dd04b62809cf3c896a0e7796e28
Wolfgang Laun <wolfgang.laun@gmail.com>
2011-01-25 09:44:35 +0100

    JBRULES-2867  update Main.java, was missing from 1st batch

--- drools-examples/drools-examples-drl/src/main/java/org/drools/examples/sudoku/Main.java
+++ drools-examples/drools-examples-drl/src/main/java/org/drools/examples/sudoku/Main.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2010 JBoss Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,13 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-
-/*
- * JBoss, the OpenSource J2EE webOS
- * 
- * Distributable under LGPL license.
- * See terms of license at gnu.org.
  */
 package org.drools.examples.sudoku;
 
@@ -36,9 +29,11 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
-import org.drools.examples.sudoku.rules.DroolsSudokuGridModel;
+import org.drools.KnowledgeBase;
+import org.drools.examples.sudoku.rules.DroolsUtil;
 import org.drools.examples.sudoku.swing.SudokuGridSamples;
 import org.drools.examples.sudoku.swing.SudokuGridView;
+
 
 /**
  * This example shows how Drools can be used to solve a 9x9 Sudoku Grid.
@@ -50,109 +45,91 @@ import org.drools.examples.sudoku.swing.SudokuGridView;
  * @version $Revision: 1.1 $
  */
 public class Main
-   implements ActionListener
+implements ActionListener
 {
-   private JFrame mainFrame;
-   private SudokuGridView sudokuGridView;
-   private DroolsSudokuGridModel droolsSudokuGridModel;
-   private JMenuBar menuBar = new JMenuBar();
-   private JMenu fileMenu = new JMenu("File");
-   private JMenu samplesMenu = new JMenu("Samples");
-   private JMenuItem openMenuItem = new JMenuItem("Open...");
-   private JMenuItem exitMenuItem = new JMenuItem("Exit");
-   private BorderLayout borderLayout = new BorderLayout();
-   private FlowLayout flowLayout = new FlowLayout(FlowLayout.RIGHT);
-   private JPanel buttonPanel = new JPanel(flowLayout);
-   private JButton solveButton = new JButton("Solve");
-   private JFileChooser fileChooser;
-   
-   public static void main(String[] args)
-   {
-      @SuppressWarnings("unused")
-      Main main = new Main();
-   }
-   
-   public Main()
-   {
-      mainFrame = new JFrame("Drools Sudoku Example");
-      for (String sampleName : SudokuGridSamples.getInstance().getSampleNames())
-      {
-         JMenuItem menuItem = new JMenuItem(sampleName);
-         menuItem.addActionListener(this);
-         samplesMenu.add(menuItem);
-      }
-      fileMenu.add(samplesMenu);
-      openMenuItem.addActionListener(this);
-      // fileMenu.add(openMenuItem);
-      exitMenuItem.addActionListener(this);
-      fileMenu.add(exitMenuItem);
-      menuBar.add(fileMenu);
-      mainFrame.setJMenuBar(menuBar);
-      sudokuGridView = new SudokuGridView();
-      droolsSudokuGridModel = new DroolsSudokuGridModel(SudokuGridSamples.getInstance().getSample("Simple"));
-      mainFrame.setLayout(borderLayout);
-      mainFrame.add(BorderLayout.CENTER, sudokuGridView);
-      // buttonPanel.add(fireOneRuleButton);
-      buttonPanel.add(solveButton);
-      solveButton.addActionListener(this);
-      mainFrame.add(BorderLayout.SOUTH, buttonPanel);
-      mainFrame.setSize(400,400);
-      mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      mainFrame.setVisible(true);
-      sudokuGridView.setModel(droolsSudokuGridModel);
-   }
+    private JFrame mainFrame;
+    private SudokuGridView sudokuGridView;
+    private Sudoku sudoku;
+    private JMenuBar menuBar = new JMenuBar();
+    private JMenu fileMenu = new JMenu("File");
+    private JMenu samplesMenu = new JMenu("Samples");
+    private JMenuItem openMenuItem = new JMenuItem("Open...");
+    private JMenuItem exitMenuItem = new JMenuItem("Exit");
+    private BorderLayout borderLayout = new BorderLayout();
+    private FlowLayout flowLayout = new FlowLayout(FlowLayout.RIGHT);
+    private JPanel buttonPanel = new JPanel(flowLayout);
+    private JButton solveButton = new JButton("Solve");
+    private JButton stepButton  = new JButton("Step");
+    private JFileChooser fileChooser;
 
-   public void actionPerformed(ActionEvent ev)
-   {
-      if (ev.getSource().equals(solveButton))
-      {
-         long startTime = System.currentTimeMillis();
+    public static void main(String[] args) throws Exception{
+        @SuppressWarnings("unused")
+        Main main = new Main();
+    }
 
-         if (droolsSudokuGridModel.solve())
-         {
-            solveButton.setText("Solved ("+(System.currentTimeMillis()-startTime)+" ms)");
-            solveButton.setEnabled(false);
-         }
-         else
-         {
-            solveButton.setText("Unsolved ("+(System.currentTimeMillis()-startTime)+" ms)");
-            solveButton.setEnabled(false);            
-         }
-      }
-      else if (ev.getSource().equals(openMenuItem))
-      {
-         if (fileChooser == null)
-         {
-            fileChooser = new JFileChooser();
-         }
-         
-         try
-         {
-            if (fileChooser.showOpenDialog(mainFrame) == JFileChooser.APPROVE_OPTION)
-            {
-               System.out.println(fileChooser.getSelectedFile().getCanonicalPath());
+    public Main() throws Exception{
+        mainFrame = new JFrame("Drools Sudoku Example");
+        for (String sampleName : SudokuGridSamples.getInstance().getSampleNames()){
+            JMenuItem menuItem = new JMenuItem(sampleName);
+            menuItem.addActionListener(this);
+            samplesMenu.add(menuItem);
+        }
+        fileMenu.add(samplesMenu);
+        openMenuItem.addActionListener(this);
+        // fileMenu.add(openMenuItem);
+        exitMenuItem.addActionListener(this);
+        fileMenu.add(exitMenuItem);
+        menuBar.add(fileMenu);
+        mainFrame.setJMenuBar(menuBar);
+        sudokuGridView = new SudokuGridView();
+
+        KnowledgeBase kBase = DroolsUtil.readKnowledgeBase( "/org/drools/examples/sudoku/sudoku.drl" );
+        sudoku = new Sudoku( kBase );
+
+        mainFrame.setLayout(borderLayout);
+        mainFrame.add(BorderLayout.CENTER, sudokuGridView);
+        buttonPanel.add(solveButton);
+        solveButton.addActionListener(this);
+        buttonPanel.add(stepButton);
+        stepButton.addActionListener(this);
+        mainFrame.add(BorderLayout.SOUTH, buttonPanel);
+        mainFrame.setSize(400,400);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+        sudokuGridView.setModel( sudoku );
+
+        sudoku.setCellValues( SudokuGridSamples.getInstance().getSample("Simple") );
+    }
+
+    public void actionPerformed(ActionEvent ev){
+        if( ev.getSource().equals(solveButton) ){
+            long startTime = System.currentTimeMillis();
+            sudoku.solve();
+            
+        } else  if( ev.getSource().equals(stepButton) ){
+            sudoku.step();
+           
+        } else if (ev.getSource().equals(openMenuItem)) {
+            if( fileChooser == null ){
+                fileChooser = new JFileChooser();
             }
-         }
-         catch (IOException ex)
-         {
-            ex.printStackTrace();
-         }
-      }
-      else if (ev.getSource().equals(exitMenuItem))
-      {
-         System.exit(0);
-      }
-      else if (ev.getSource() instanceof JMenuItem)
-      {
-         JMenuItem menuItem = (JMenuItem) ev.getSource();
-         droolsSudokuGridModel = new DroolsSudokuGridModel(SudokuGridSamples.getInstance().getSample(menuItem.getText()));
-         sudokuGridView.setModel(droolsSudokuGridModel);
-         solveButton.setText("Solve");
-         solveButton.setEnabled(true);
-      }
-      else
-      {
-         //
-      }
-   }
+
+            try {
+                if( fileChooser.showOpenDialog(mainFrame) == JFileChooser.APPROVE_OPTION ){
+                    System.out.println(fileChooser.getSelectedFile().getCanonicalPath());
+                }
+            } catch( IOException ex ){
+                ex.printStackTrace();
+            }
+            
+        } else if( ev.getSource().equals( exitMenuItem ) ){
+            System.exit(0);
+            
+        } else if( ev.getSource() instanceof JMenuItem ){
+            JMenuItem menuItem = (JMenuItem) ev.getSource();
+            sudoku.setCellValues( SudokuGridSamples.getInstance().getSample(menuItem.getText()) );
+        } else {
+            //
+        }
+    }
 }
