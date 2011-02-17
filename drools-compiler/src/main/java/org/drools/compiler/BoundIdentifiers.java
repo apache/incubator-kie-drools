0557efbb908da41bdb605fd4bd53d6a20d97bd5a
mdproctor <mdproctor@gmail.com>
2011-02-17 11:50:06 +0000

    JBRULES-2906

---
+++ drools-compiler/src/main/java/org/drools/compiler/BoundIdentifiers.java
@@ -0,0 +1,36 @@
+package org.drools.compiler;
+
+import java.util.Map;
+
+public class BoundIdentifiers {
+    private Map<String, Class> declarations;
+    private Map<String, Class> globals;
+    private Class thisClass;
+    
+    public BoundIdentifiers(Map<String, Class> declarations,
+                            Map<String, Class> globals) {
+        this.declarations = declarations;
+        this.globals = globals;
+    }
+    
+    public BoundIdentifiers(Map<String, Class> declarations,
+                            Map<String, Class> globals,
+                            Class thisClass) {
+        this.declarations = declarations;
+        this.globals = globals;
+        this.thisClass  = thisClass;
+    }    
+    
+    public Map<String, Class> getDeclarations() {
+        return declarations;
+    }
+    public Map<String, Class> getGlobals() {
+        return globals;
+    }
+    
+    public Class getThisClass() {
+        return thisClass;
+    }
+    
+    
+}
---
+++ drools-compiler/src/main/java/org/drools/rule/builder/dialect/java/parser/JavaUpdateBlockDescr.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package org.drools.rule.builder.dialect.java.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A helper class used during java code parsing to identify
+ * and handle update() blocks
+ * 
+ * @author etirelli
+ */
+public class JavaUpdateBlockDescr implements JavaBlockDescr {
+    private int start;
+    private int end;
+    private String targetExpression;
+    
+    public JavaUpdateBlockDescr( String targetExpression ) {
+        this.targetExpression = targetExpression;
+    }
+    
+    public int getStart() {
+        return start;
+    }
+    public void setStart(int start) {
+        this.start = start;
+    }
+    public int getEnd() {
+        return end;
+    }
+    public void setEnd(int end) {
+        this.end = end;
+    }
+    public String getTargetExpression() {
+        return targetExpression;
+    }
+    
+    public void setTargetExpression(String targetExpression) {
+        this.targetExpression = targetExpression;
+    }
+    
+    public String toString() {
+        return "UpdateBlock( start="+start+" end="+end+" expression="+targetExpression+" )";
+    }
+
+    public BlockType getType() {
+        return BlockType.MODIFY;
+    }
+
+}
