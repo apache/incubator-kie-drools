d7deb38c5ab3be3d1d4fb0db8b969ede4a7b126e
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2011-02-18 16:52:50 +0100

    JBRULES-2577 License headers in java files are not valid javadocs and should not start with /**: java files

--- drools-core/src/main/java/org/drools/process/core/impl/ParameterDefinitionImpl.java
+++ drools-core/src/main/java/org/drools/process/core/impl/ParameterDefinitionImpl.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2010 JBoss Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,9 +27,42 @@ import org.drools.process.core.datatype.DataType;
  */
 public class ParameterDefinitionImpl implements ParameterDefinition, Serializable {
    
-    private static final long serialVersionUID = 510l;       private String name;    private DataType type;
+    private static final long serialVersionUID = 510l;
+   
+    private String name;
+    private DataType type;
     
     public ParameterDefinitionImpl() {
     }
     
-    public ParameterDefinitionImpl(String name, DataType type) {        setName(name);        setType(type);    }        public String getName() {        return name;    }        public void setName(String name) {        if (name == null) {            throw new IllegalArgumentException("Name cannot be null");        }        this.name = name;    }        public DataType getType() {        return type;    }        public void setType(DataType type) {        if (type == null) {            throw new IllegalArgumentException("Data type cannot be null");        }        this.type = type;    }        public String toString() {        return name;    }}
+    public ParameterDefinitionImpl(String name, DataType type) {
+        setName(name);
+        setType(type);
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("Name cannot be null");
+        }
+        this.name = name;
+    }
+    
+    public DataType getType() {
+        return type;
+    }
+    
+    public void setType(DataType type) {
+        if (type == null) {
+            throw new IllegalArgumentException("Data type cannot be null");
+        }
+        this.type = type;
+    }
+    
+    public String toString() {
+        return name;
+    }
+}
--- drools-core/src/main/java/org/drools/reteoo/EntryPointNode.java
+++ drools-core/src/main/java/org/drools/reteoo/EntryPointNode.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2007 JBoss Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
