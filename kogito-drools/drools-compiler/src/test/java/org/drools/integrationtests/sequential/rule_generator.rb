7604bff1e647c5b430a37e43187108f76ccd89d8
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2011-02-18 18:40:50 +0100

    JBRULES-2855 Do not use tabs (\t) for indentation, use spaces instead: 4 for rb files

--- kogito-drools/drools-compiler/src/test/java/org/drools/integrationtests/sequential/rule_generator.rb
+++ kogito-drools/drools-compiler/src/test/java/org/drools/integrationtests/sequential/rule_generator.rb
@@ -7,25 +7,25 @@ f = File.new(OUTPUT, "w")
 
 for i in 1..200
 
-	s =  "
-	
-	rule \"Cheese_#{i}\"	
-		when
-			$c : Cheese( price == #{i} )
-		then 
-			list.add( $c.getType() );
-	end
-	
-	rule \"Person and cheese_#{i}\"
-		when 
-			$p : Person(name == \"p#{i}\")
-			$c : Cheese(price == 1)
-		then
-			list.add($p.getName());
-		
-	end
-	"
-	f.write s
+    s =  "
+
+    rule \"Cheese_#{i}\"
+        when
+            $c : Cheese( price == #{i} )
+        then
+            list.add( $c.getType() );
+    end
+
+    rule \"Person and cheese_#{i}\"
+        when
+            $p : Person(name == \"p#{i}\")
+            $c : Cheese(price == 1)
+        then
+            list.add($p.getName());
+
+    end
+    "
+    f.write s
 
 
 
