0cb715657fb44f444a06d94f9e6c0f5dc6f47bc3
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2011-02-26 12:49:42 +0100

    javadoc typo

--- drools-planner-core/src/main/java/org/drools/planner/core/localsearch/decider/acceptor/Acceptor.java
+++ drools-planner-core/src/main/java/org/drools/planner/core/localsearch/decider/acceptor/Acceptor.java
@@ -30,7 +30,7 @@ public interface Acceptor extends LocalSearchSolverLifecycleListener {
 
     /**
      * @param moveScope not null
-     * @return never negative; if rejected 0.0; if accepted higher than 0.0 (ussually 1.0)
+     * @return never negative; if rejected 0.0; if accepted higher than 0.0 (usually 1.0)
      */
     double calculateAcceptChance(MoveScope moveScope);
 
