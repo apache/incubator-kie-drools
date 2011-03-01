86e9ddf8e87196f4b86871c339f39bdc8543ad88
marianbuenosayres <marianbuenosayres@gmail.com>
2011-03-01 15:33:53 -0300

    Adding JMS Human Task Server

---
+++ kogito-jbpm/jbpm-human-task/src/main/java/org/jbpm/task/service/jms/JMSTaskClientHandler.java
@@ -0,0 +1,50 @@
+package org.jbpm.task.service.jms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.jms.Destination;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.Topic;
+
+import org.drools.SystemEventListener;
+import org.jbpm.task.service.TaskClient;
+import org.jbpm.task.service.TaskClientHandler;
+
+public class JMSTaskClientHandler extends BaseJMSHandler {
+	private TaskClientHandler handler;
+	private Map<String, MessageProducer> producers;
+
+	public JMSTaskClientHandler(SystemEventListener systemEventListener) {
+		this.handler = new TaskClientHandler(this.responseHandlers, systemEventListener);
+		this.producers = new HashMap<String, MessageProducer>();
+	}
+
+	public TaskClient getClient() {
+		return this.handler.getClient();
+	}
+
+	public void setClient(TaskClient client) {
+		this.handler.setClient(client);
+	}
+
+	public void exceptionCaught(Session session, Throwable cause) throws Exception {
+	}
+
+	public void messageReceived(Session session, Object message, Destination destination, String selector) throws Exception {
+		String name = "";
+		if (destination instanceof Queue) {
+			name = ((Queue) destination).getQueueName();
+		} else if (destination instanceof Topic) {
+			name = ((Topic) destination).getTopicName();
+		}
+		MessageProducer producer = (MessageProducer) this.producers.get(name);
+		if (producer == null) {
+			producer = session.createProducer(destination);
+			this.producers.put(name, producer);
+		}
+		this.handler.messageReceived(new JMSSessionWriter(session, producer, selector), message);
+	}
+}
---
+++ kogito-jbpm/jbpm-human-task/src/test/java/org/jbpm/task/service/jms/TaskServiceDeadlinesJMSTest.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright 2010 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jbpm.task.service.jms;
+
+import java.util.Properties;
+
+import javax.naming.Context;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.drools.SystemEventListenerFactory;
+import org.easymock.EasyMock;
+import org.jbpm.task.service.TaskClient;
+import org.jbpm.task.service.TaskServiceDeadlinesBaseTest;
+import org.subethamail.wiser.Wiser;
+
+public class TaskServiceDeadlinesJMSTest extends TaskServiceDeadlinesBaseTest {
+
+	private Context context;
+	
+	@Override
+	protected void setUp() throws Exception {        
+		super.setUp();
+
+		setConf(new Properties());
+		getConf().setProperty("mail.smtp.host", "localhost");
+		getConf().setProperty("mail.smtp.port", "2345");
+		getConf().setProperty("from", "from@domain.com");
+		getConf().setProperty("replyTo", "replyTo@domain.com");
+		getConf().setProperty("defaultLanguage", "en-UK");
+
+		ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
+		
+		this.context = EasyMock.createMock(Context.class);
+		EasyMock.expect(context.lookup("ConnectionFactory")).andReturn(factory).anyTimes();
+		EasyMock.replay(context);
+		
+		Properties serverProperties = new Properties();
+		serverProperties.setProperty("JMSTaskServer.connectionFactory", "ConnectionFactory");
+		serverProperties.setProperty("JMSTaskServer.transacted", "true");
+		serverProperties.setProperty("JMSTaskServer.acknowledgeMode", "AUTO_ACKNOWLEDGE");
+		serverProperties.setProperty("JMSTaskServer.queueName", "tasksQueue");
+		serverProperties.setProperty("JMSTaskServer.responseQueueName", "tasksResponseQueue");
+		
+		server = new JMSTaskServer(taskService, serverProperties, context);
+		Thread thread = new Thread(server);
+		thread.start();
+		System.out.println("Waiting for the JMS Task Server to come up");
+        while (!server.isRunning()) {
+        	System.out.print(".");
+        	Thread.sleep( 50 );
+        }
+
+        Properties clientProperties = new Properties();
+		clientProperties.setProperty("JMSTaskClient.connectionFactory", "ConnectionFactory");
+		clientProperties.setProperty("JMSTaskClient.transactedQueue", "true");
+		clientProperties.setProperty("JMSTaskClient.acknowledgeMode", "AUTO_ACKNOWLEDGE");
+		clientProperties.setProperty("JMSTaskClient.queueName", "tasksQueue");
+		clientProperties.setProperty("JMSTaskClient.responseQueueName", "tasksResponseQueue");
+        
+        client = new TaskClient(new JMSTaskClientConnector("client 1",
+				new JMSTaskClientHandler(SystemEventListenerFactory.getSystemEventListener()),
+				clientProperties, context));
+        client.connect();
+
+		setWiser(new Wiser());
+		getWiser().setHostname(getConf().getProperty("mail.smtp.host"));
+		getWiser().setPort(Integer.parseInt(getConf().getProperty("mail.smtp.port")));        
+		getWiser().start();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		client.disconnect();
+		server.stop();
+		getWiser().stop();
+	}
+
+}
