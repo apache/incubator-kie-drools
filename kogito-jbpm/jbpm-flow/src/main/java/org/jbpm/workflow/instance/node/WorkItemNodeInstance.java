d28786100cfb91ed9ae5ab676f0dc8dc29c13009
Antoine Toulme <antoine@lunar-ocean.com>
2011-03-02 16:35:09 -0800

    comment out the code starting with com.sun.* left from experiments

--- kogito-jbpm/jbpm-flow/src/main/java/org/jbpm/workflow/instance/node/WorkItemNodeInstance.java
+++ kogito-jbpm/jbpm-flow/src/main/java/org/jbpm/workflow/instance/node/WorkItemNodeInstance.java
@@ -16,8 +16,6 @@
 
 package org.jbpm.workflow.instance.node;
 
-import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -44,10 +42,6 @@ import org.jbpm.workflow.core.node.WorkItemNode;
 import org.jbpm.workflow.instance.impl.NodeInstanceResolverFactory;
 import org.jbpm.workflow.instance.impl.WorkItemResolverFactory;
 import org.mvel2.MVEL;
-
-import com.sun.org.apache.xml.internal.serialize.OutputFormat;
-import com.sun.org.apache.xml.internal.serialize.XML11Serializer;
-import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 
 /**
  * Runtime counterpart of a work item node.
