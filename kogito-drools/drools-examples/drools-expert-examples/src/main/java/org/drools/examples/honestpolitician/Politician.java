81cc73eacf0749aad78e59520142b8985d94f52b
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2011-03-11 16:10:59 +0100

    move honest politician into it's own package for clarity

---
+++ kogito-drools/drools-examples/drools-expert-examples/src/main/java/org/drools/examples/honestpolitician/Politician.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2011 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.examples.honestpolitician;
+
+public class Politician {
+    private String  name;
+
+    private boolean honest;
+
+    public Politician() {
+
+    }
+
+    public Politician(String name,
+            boolean honest) {
+        super();
+        this.name = name;
+        this.honest = honest;
+    }
+
+    public boolean isHonest() {
+        return honest;
+    }
+
+    public void setHonest(boolean honest) {
+        this.honest = honest;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
