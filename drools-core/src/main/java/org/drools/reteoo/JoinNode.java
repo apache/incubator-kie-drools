f2d4eba4b8439a9b4845c15442eb76e464e91297
Leo Gomes <leonardo.f.gomes@gmail.com>
2011-04-05 02:40:58 +0200

    JBRULES-2776 Left and Right Unlinking.

--- drools-core/src/main/java/org/drools/reteoo/JoinNode.java
+++ drools-core/src/main/java/org/drools/reteoo/JoinNode.java
@@ -56,12 +56,22 @@ public class JoinNode extends BetaNode {
                binder,
                behaviors );
         this.tupleMemoryEnabled = context.isTupleMemoryEnabled();
+        this.lrUnlinkingEnabled = context.getRuleBase().getConfiguration().isLRUnlinkingEnabled();
     }
 
     public void assertLeftTuple(final LeftTuple leftTuple,
                                 final PropagationContext context,
                                 final InternalWorkingMemory workingMemory) {
         final BetaMemory memory = (BetaMemory) workingMemory.getNodeMemory( this );
+
+        
+        if(lrUnlinkingEnabled &&
+           leftUnlinked(context,
+                        workingMemory,
+                        memory)) {
+            return;
+        }
+        
         memory.setOpen( true );
         
         RightTupleMemory rightMemory = memory.getRightTupleMemory();
@@ -116,6 +126,15 @@ public class JoinNode extends BetaNode {
                              final PropagationContext context,
                              final InternalWorkingMemory workingMemory) {
         final BetaMemory memory = (BetaMemory) workingMemory.getNodeMemory( this );
+        
+        if (lrUnlinkingEnabled &&
+            rightUnlinked(context,
+                         workingMemory,
+                          memory)) {
+            context.getPropagationAttemptsMemory().add( this );
+            return;
+        }
+        
 
         RightTuple rightTuple = createRightTuple( factHandle,
                                                   this );
@@ -159,6 +178,13 @@ public class JoinNode extends BetaNode {
                                   final PropagationContext context,
                                   final InternalWorkingMemory workingMemory) {
         final BetaMemory memory = (BetaMemory) workingMemory.getNodeMemory( this );
+        
+        if (lrUnlinkingEnabled &&
+            memory.isRightUnlinked()) {
+            return;
+        }
+        
+        
         behavior.retractRightTuple( memory.getBehaviorContext(),
                                     rightTuple,
                                     workingMemory );
@@ -175,6 +201,12 @@ public class JoinNode extends BetaNode {
                                  final PropagationContext context,
                                  final InternalWorkingMemory workingMemory) {
         final BetaMemory memory = (BetaMemory) workingMemory.getNodeMemory( this );
+        
+        if (lrUnlinkingEnabled &&
+            memory.isLeftUnlinked()) {
+            return;
+        }        
+        
         memory.getLeftTupleMemory().remove( leftTuple );
         if ( leftTuple.firstChild != null ) {
             this.sink.propagateRetractLeftTuple( leftTuple,
@@ -398,6 +430,36 @@ public class JoinNode extends BetaNode {
             this.constraints.resetTuple( memory.getContext() );
         }
     }
+    
+    @Override
+    public void modifyLeftTuple(InternalFactHandle factHandle,
+            ModifyPreviousTuples modifyPreviousTuples,
+            PropagationContext context, InternalWorkingMemory workingMemory) {
+
+        BetaMemory memory = (BetaMemory) workingMemory.getNodeMemory(this);
+        
+        if (lrUnlinkingEnabled &&
+            memory.isLeftUnlinked())
+            return;
+        
+        super.modifyLeftTuple(factHandle, modifyPreviousTuples, context, workingMemory);
+    }
+
+    @Override
+    public void modifyObject(InternalFactHandle factHandle,
+            ModifyPreviousTuples modifyPreviousTuples,
+            PropagationContext context, InternalWorkingMemory workingMemory) {
+        
+
+        BetaMemory memory = (BetaMemory) workingMemory.getNodeMemory(this);
+        
+        if (lrUnlinkingEnabled &&
+            memory.isRightUnlinked())
+            return;
+        
+        super.modifyObject(factHandle, modifyPreviousTuples, context, workingMemory);
+    }
+    
 
     public short getType() {
         return NodeTypeEnums.JoinNode;
