6bf7c1a81148998d3f01e4feda41a118e7d8beef
Toni Rikkola <toni@rikkola.net>
2011-04-08 21:04:00 +0300

    fixing verifier tests, still few tests in ignore

--- drools-verifier/src/test/java/org/drools/verifier/visitor/PackageDescrVisitorTest.java
+++ drools-verifier/src/test/java/org/drools/verifier/visitor/PackageDescrVisitorTest.java
@@ -55,7 +55,7 @@ public class PackageDescrVisitorTest {
 
 
     @Test
-    @Ignore("08-APR-2011 temporally ignoring -Rikkola-")
+//    @Ignore("08-APR-2011 temporally ignoring -Rikkola-")
     public void testVisit() throws Exception {
 
         PackageDescr packageDescr = getPackageDescr(Verifier.class.getResourceAsStream("Misc3.drl"));
