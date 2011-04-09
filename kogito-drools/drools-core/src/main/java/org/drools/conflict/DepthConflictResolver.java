64f2041d5a837b8bded7cd55e51ab41e7cfe5c65
Mark Proctor <mdproctor@gmail.com>
2011-04-09 04:54:14 +0100

    JBRULES-2642 Getting tests working for new updated parser. -Small tweak to conflict resolution

--- kogito-drools/drools-core/src/main/java/org/drools/conflict/DepthConflictResolver.java
+++ kogito-drools/drools-core/src/main/java/org/drools/conflict/DepthConflictResolver.java
@@ -54,10 +54,15 @@ public class DepthConflictResolver
         final int s1 = existing.getSalience();
         final int s2 = adding.getSalience();
         
-        if ( s1 != s2 ) {
-            return s1 - s2;
+        if ( s1 > s2 ) {
+            return 1;
+        } else if ( s1 < s2 ) {
+            return -1;
         }
-
+        
+//        else {
+//            return 0;
+//        }
 
         // we know that no two activations will have the same number
         return (int) ( existing.getActivationNumber() - adding.getActivationNumber() );
