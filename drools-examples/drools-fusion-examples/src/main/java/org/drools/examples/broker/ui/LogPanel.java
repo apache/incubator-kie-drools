e7ad0dc6a0acf3edb4f8993fb5c903789c3e4543
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2011-04-12 15:11:30 +0200

    remove jgoodies dependency from drools-fusion-examples so there are no extra dependencies needed to run the example + improve layout stretching

--- drools-examples/drools-fusion-examples/src/main/java/org/drools/examples/broker/ui/LogPanel.java
+++ drools-examples/drools-fusion-examples/src/main/java/org/drools/examples/broker/ui/LogPanel.java
@@ -17,7 +17,9 @@
 package org.drools.examples.broker.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -26,14 +28,12 @@ import javax.swing.SwingUtilities;
 /**
  * A panel to log information
  */
-public class LogPanel {
+public class LogPanel extends JPanel {
 
-    private final JPanel    panel;
     private final JTextArea log;
 
     public LogPanel() {
-        panel = new JPanel();
-        panel.setLayout( new BorderLayout() );
+        setLayout(new BorderLayout());
         log = new JTextArea();
         log.setEditable( false );
         
@@ -41,8 +41,10 @@ public class LogPanel {
         areaScrollPane.setVerticalScrollBarPolicy(
                 JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         
-        panel.add( areaScrollPane,
-                   BorderLayout.CENTER );
+        add(areaScrollPane,
+                BorderLayout.CENTER);
+        setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        setPreferredSize(new Dimension(400, 50));
     }
 
     public void log(final String text) {
@@ -51,10 +53,6 @@ public class LogPanel {
                 log.append( text + "\n" );
             }
         } );
-    }
-
-    public JPanel getPanel() {
-        return panel;
     }
 
 }
