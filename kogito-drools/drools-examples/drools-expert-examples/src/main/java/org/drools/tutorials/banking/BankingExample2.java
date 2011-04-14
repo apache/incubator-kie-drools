8c3367ab7573bc3c3237a92ce240bf1d84878de9
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2011-04-14 13:44:35 +0200

    namespace banking examples + create aggregate ExamplesApp

--- kogito-drools/drools-examples/drools-expert-examples/src/main/java/org/drools/tutorials/banking/BankingExample2.java
+++ kogito-drools/drools-examples/drools-expert-examples/src/main/java/org/drools/tutorials/banking/BankingExample2.java
@@ -16,9 +16,14 @@
 
 package org.drools.tutorials.banking;
 
-public class Example1 {
-    public static void main(String[] args) throws Exception {
-        new RuleRunner().runRules( new String[] { "Example1.drl" },
-                                   new Object[0] );
+public class BankingExample2 {
+    public static void main(String[] args) {
+        Number[] numbers = new Number[] {wrap(3), wrap(1), wrap(4), wrap(1), wrap(5)};
+        new RuleRunner().runRules( new String[] { "Example2.drl" },
+                                   numbers );
+    }
+    
+    private static Integer wrap(int i) {
+        return new Integer(i);
     }
 }
