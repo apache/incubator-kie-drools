cfa5718a943270393af8600d0fe6494599077849
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2011-04-15 10:06:35 +0200

    namespace shopping

--- kogito-drools/drools-examples/drools-expert-examples/src/main/java/org/drools/examples/shopping/ShoppingExample.java
+++ kogito-drools/drools-examples/drools-expert-examples/src/main/java/org/drools/examples/shopping/ShoppingExample.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.drools.examples;
+package org.drools.examples.shopping;
 
 import org.drools.KnowledgeBase;
 import org.drools.KnowledgeBaseFactory;
