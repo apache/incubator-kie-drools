c1e7bf2afe73511fd421ac213711b7c8afcc136c
Michael Anstis <michael.anstis@gmail.com>
2011-04-27 09:44:31 +0100

    GUVNOR-1310: Attempt to verify rule source fails with DroolsParserException - corrected method name.

--- drools-compiler/src/main/java/org/drools/compiler/BusinessRuleProviderFactory.java
+++ drools-compiler/src/main/java/org/drools/compiler/BusinessRuleProviderFactory.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright 2011 JBoss Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.drools.compiler;
 
 import org.drools.CheckedDroolsException;
 import org.drools.util.ServiceRegistryImpl;
 
 public class BusinessRuleProviderFactory {
+
     private static BusinessRuleProviderFactory instance = new BusinessRuleProviderFactory();
     private static BusinessRuleProvider        provider;
 
@@ -19,14 +36,14 @@ public class BusinessRuleProviderFactory {
         return provider;
     }
 
-    public static synchronized void setDecisionTableProvider(BusinessRuleProvider provider) {
+    public static synchronized void setBusinessRuleProvider(BusinessRuleProvider provider) {
         BusinessRuleProviderFactory.provider = provider;
     }
 
     private void loadProvider() throws CheckedDroolsException {
         ServiceRegistryImpl.getInstance().addDefault( BusinessRuleProvider.class,
                                                       "org.drools.ide.common.BusinessRuleProviderDefaultImpl" );
-        setDecisionTableProvider( ServiceRegistryImpl.getInstance().get( BusinessRuleProvider.class ) );
+        setBusinessRuleProvider( ServiceRegistryImpl.getInstance().get( BusinessRuleProvider.class ) );
     }
 
 }
