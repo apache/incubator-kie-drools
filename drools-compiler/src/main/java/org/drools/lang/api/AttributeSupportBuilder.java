0c996f336634ceb9eff8865d79c1c165e4a8c498
Edson Tirelli <ed.tirelli@gmail.com>
2011-05-17 10:59:45 -0400

    Adding javadocs to the rule authoring API

--- drools-compiler/src/main/java/org/drools/lang/api/AttributeSupportBuilder.java
+++ drools-compiler/src/main/java/org/drools/lang/api/AttributeSupportBuilder.java
@@ -2,8 +2,17 @@ package org.drools.lang.api;
 
 import org.drools.lang.descr.BaseDescr;
 
+/**
+ * An interface for all builders of statements that support attributes
+ */
 public interface AttributeSupportBuilder<T extends BaseDescr> extends DescrBuilder<T> {
 
+    /**
+     * Adds a new attribute to the statement
+     * 
+     * @param name the attribute name
+     * @return the AttributeDescrBuilder to set the attribute value
+     */
     public AttributeDescrBuilder attribute( String name );
 
 }
--- drools-compiler/src/main/java/org/drools/lang/api/ImportDescrBuilder.java
+++ drools-compiler/src/main/java/org/drools/lang/api/ImportDescrBuilder.java
@@ -18,10 +18,19 @@ package org.drools.lang.api;
 
 import org.drools.lang.descr.ImportDescr;
 
+/**
+ * An interface for the import builder
+ */
 public interface ImportDescrBuilder
     extends
     DescrBuilder<ImportDescr> {
 
-    public ImportDescrBuilder target( String target );
+    /**
+     * Sets the import target
+     * 
+     * @param target the class or package being imported
+     * @return the package descr builder
+     */
+    public PackageDescrBuilder target( String target );
 
 }
