ccf948d4e166a2e598b543c258ea81c23e74dc96
krisv <kverlaen@redhat.com>
2011-05-18 02:22:09 +0200

    JBPM-3197: Signal start event doesn't work when process is dynamically added to knowledge base  - adding knowledge base process listeners

--- knowledge-api/src/main/java/org/drools/event/knowledgebase/KnowledgeBaseEventListener.java
+++ knowledge-api/src/main/java/org/drools/event/knowledgebase/KnowledgeBaseEventListener.java
@@ -22,79 +22,79 @@ public interface KnowledgeBaseEventListener
     extends
     EventListener {
     /**
-     * Method called before a new package is added to the rule base
+     * Method called before a new package is added to the knowledge base
      * @param event
      */
     void beforeKnowledgePackageAdded(BeforeKnowledgePackageAddedEvent event);
 
     /**
-     * Method called after a new package is added to the rule base
+     * Method called after a new package is added to the knowledge base
      * @param event
      */
     void afterKnowledgePackageAdded(AfterKnowledgePackageAddedEvent event);
 
     /**
-     * Method called before a package is removed from the rule base
+     * Method called before a package is removed from the knowledge base
      * @param event
      */
     void beforeKnowledgePackageRemoved(BeforeKnowledgePackageRemovedEvent event);
 
     /**
-     * Method called after a package is removed from the rule base
+     * Method called after a package is removed from the knowledge base
      * @param event
      */
     void afterKnowledgePackageRemoved(AfterKnowledgePackageRemovedEvent event);
 
     /**
-     * Method called before a rule base is locked
+     * Method called before a knowledge base is locked
      * @param event
      */
     void beforeKnowledgeBaseLocked(BeforeKnowledgeBaseLockedEvent event);
 
     /**
-     * Method called after a rule base is locked
+     * Method called after a knowledge base is locked
      * @param event
      */
     void afterKnowledgeBaseLocked(AfterKnowledgeBaseLockedEvent event);
 
     /**
-     * Method called before a rule base is unlocked
+     * Method called before a knowledge base is unlocked
      * @param event
      */
     void beforeKnowledgeBaseUnlocked(BeforeKnowledgeBaseUnlockedEvent event);
 
     /**
-     * Method called after a rule base is unlocked
+     * Method called after a knowledge base is unlocked
      * @param event
      */
     void afterKnowledgeBaseUnlocked(AfterKnowledgeBaseUnlockedEvent event);
 
     /**
-     * Method called before a new rule is added to the rule base
+     * Method called before a new rule is added to the knowledge base
      * @param event
      */
     void beforeRuleAdded(BeforeRuleAddedEvent event);
 
     /**
-     * Method called after a new rule is added to the rule base
+     * Method called after a new rule is added to the knowledge base
      * @param event
      */
     void afterRuleAdded(AfterRuleAddedEvent event);
 
     /**
-     * Method called before a rule is removed from the rule base
+     * Method called before a rule is removed from the knowledge base
      * @param event
      */
     void beforeRuleRemoved(BeforeRuleRemovedEvent event);
 
     /**
-     * Method called after a rule is removed from the rule base
+     * Method called after a rule is removed from the knowledge base
      * @param event
      */
     void afterRuleRemoved(AfterRuleRemovedEvent event);
 
     /**
-     * Method called before a function is removed from the rule base
+     * Method called before a function is removed from the knowledge base
      * @param event
      */
     void beforeFunctionRemoved(BeforeFunctionRemovedEvent event);
@@ -104,4 +104,28 @@ public interface KnowledgeBaseEventListener
      * @param event
      */
     void afterFunctionRemoved(AfterFunctionRemovedEvent event);
+
+    /**
+     * Method called before a process is removed from the knowledge base
+     * @param event
+     */
+    void beforeProcessAdded(BeforeProcessAddedEvent event);
+
+    /**
+     * Method called after a function is removed from the knowledge base
+     * @param event
+     */
+    void afterProcessAdded(AfterProcessAddedEvent event);
+
+    /**
+     * Method called before a function is removed from the knowledge base
+     * @param event
+     */
+    void beforeProcessRemoved(BeforeProcessRemovedEvent event);
+
+    /**
+     * Method called after a function is removed from the knowledge base
+     * @param event
+     */
+    void afterProcessRemoved(AfterProcessRemovedEvent event);
 }
