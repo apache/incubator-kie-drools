7b569e025d7b881eb8ad8d5c97b9f363fead9585
Edson Tirelli <ed.tirelli@gmail.com>
2011-06-06 22:31:25 -0400

    Adding test cases

--- drools-compiler/src/main/java/org/drools/lang/api/AttributeDescrBuilder.java
+++ drools-compiler/src/main/java/org/drools/lang/api/AttributeDescrBuilder.java
@@ -25,8 +25,21 @@ public interface AttributeDescrBuilder<P extends DescrBuilder<?,?>>
     extends
     DescrBuilder<P, AttributeDescr> {
 
+    /**
+     * Sets the attribute value
+     * 
+     * @param value
+     * @return itself
+     */
     public AttributeDescrBuilder<P> value( String value );
     
+    /**
+     * Sets the attribute value type
+     * 
+     * @param type see {@link AttributeDescr.Type}
+     * 
+     * @return itself
+     */
     public AttributeDescrBuilder<P> type( AttributeDescr.Type type );
 
 }
--- drools-compiler/src/test/java/org/drools/lang/api/DescrBuilderTest.java
+++ drools-compiler/src/test/java/org/drools/lang/api/DescrBuilderTest.java
@@ -16,6 +16,7 @@
 
 package org.drools.lang.api;
 
+import org.drools.lang.descr.AttributeDescr;
 import org.drools.lang.descr.PackageDescr;
 import org.junit.Test;
 
@@ -29,16 +30,82 @@ public class DescrBuilderTest {
     @Test
     public void testPackage() {
         PackageDescr pkg = DescrFactory.newPackage()
-                              .name( "org.drools" )
-                              .attribute( "dialect" ).value( "mvel" ).end()
-                              .getDescr();
+                .name( "org.drools" )
+                .attribute( "dialect" ).value( "mvel" ).end()
+                .getDescr();
 
         assertEquals( "org.drools",
                       pkg.getName() );
         assertEquals( "mvel",
                       pkg.getAttribute( "dialect" ).getValue() );
         assertNull( pkg.getAttribute( "salience" ) );
-
     }
+
+    @Test
+    public void testPackageAttributes() {
+        PackageDescr pkg = DescrFactory.newPackage()
+                .name( "org.drools" )
+                // first syntax
+                .attribute( "dialect" ).value( "mvel" ).end()
+                // second syntax
+                .attribute( "salience",
+                            "10" )
+                // third syntax
+                .attribute( "lock-on-active",
+                            "true",
+                            AttributeDescr.Type.BOOLEAN )
+                .getDescr();
+
+        assertEquals( "org.drools",
+                      pkg.getName() );
+        assertEquals( 3,
+                      pkg.getAttributes().size() );
+        assertEquals( "mvel",
+                      pkg.getAttribute( "dialect" ).getValue() );
+        assertEquals( "10",
+                      pkg.getAttribute( "salience" ).getValue() );
+        assertEquals( "true",
+                      pkg.getAttribute( "lock-on-active" ).getValue() );
+        assertEquals( AttributeDescr.Type.BOOLEAN,
+                      pkg.getAttribute( "lock-on-active" ).getType() );
+        assertNull( pkg.getAttribute( "no-loop" ) );
+    }
+
+    @Test
+    public void testPackageImports() {
+        PackageDescr pkg = DescrFactory.newPackage()
+                .name( "org.drools" )
+                .newImport().target( "java.util.List" ).end()
+                .newImport().target( "org.drools.examples.*" ).end()
+                .getDescr();
+
+        assertEquals( 2,
+                      pkg.getImports().size() );
+        assertEquals( "java.util.List",
+                      pkg.getImports().get( 0 ).getTarget() );
+        assertEquals( "org.drools.examples.*",
+                      pkg.getImports().get( 1 ).getTarget() );
+    }
+    
+    @Test
+    public void testGlobals() {
+        PackageDescr pkg = DescrFactory.newPackage()
+                .name( "org.drools" )
+                .newGlobal().type( "java.util.List" ).identifier( "list" ).end()
+                .newGlobal().type( "Person" ).identifier( "bob" ).end()
+                .getDescr();
+
+        assertEquals( 2,
+                      pkg.getGlobals().size() );
+        assertEquals( "java.util.List",
+                      pkg.getGlobals().get( 0 ).getType() );
+        assertEquals( "list",
+                      pkg.getGlobals().get( 0 ).getIdentifier() );
+        assertEquals( "Person",
+                      pkg.getGlobals().get( 1 ).getType() );
+        assertEquals( "bob",
+                      pkg.getGlobals().get( 1 ).getIdentifier() );
+    }
+    
 
 }
