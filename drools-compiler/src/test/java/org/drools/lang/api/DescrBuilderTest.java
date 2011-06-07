dbb9c09e73f0ca5947aeaa9c67acbee05adf9212
Edson Tirelli <ed.tirelli@gmail.com>
2011-06-06 22:16:16 -0400

    Adding test case

---
+++ drools-compiler/src/test/java/org/drools/lang/api/DescrBuilderTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2011 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.lang.api;
+
+import org.drools.lang.descr.PackageDescr;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * DescrBuilderTest
+ */
+public class DescrBuilderTest {
+
+    @Test
+    public void testPackage() {
+        PackageDescr pkg = DescrFactory.newPackage()
+                              .name( "org.drools" )
+                              .attribute( "dialect" ).value( "mvel" ).end()
+                              .getDescr();
+
+        assertEquals( "org.drools",
+                      pkg.getName() );
+        assertEquals( "mvel",
+                      pkg.getAttribute( "dialect" ).getValue() );
+        assertNull( pkg.getAttribute( "salience" ) );
+
+    }
+
+}
