c31c743283be37f5334198f359d02f24af05d3c3
Edson Tirelli <ed.tirelli@gmail.com>
2011-06-16 10:18:05 -0400

    Fixing tests, adding api documentation

--- drools-compiler/src/main/java/org/drools/lang/api/FunctionDescrBuilder.java
+++ drools-compiler/src/main/java/org/drools/lang/api/FunctionDescrBuilder.java
@@ -26,13 +26,48 @@ public interface FunctionDescrBuilder
     DescrBuilder<PackageDescrBuilder, FunctionDescr>,
     ParameterSupportBuilder<FunctionDescrBuilder> {
 
+    /**
+     * Sets an alternate namespace for the function
+     * 
+     * @param namespace
+     * 
+     * @return itself
+     */
     public FunctionDescrBuilder namespace( String namespace );
 
+    /**
+     * Sets function name
+     * 
+     * @param name
+     * 
+     * @return itself
+     */
     public FunctionDescrBuilder name( String name );
 
+    /**
+     * Sets function return type
+     * 
+     * @param type
+     * 
+     * @return itself
+     */
     public FunctionDescrBuilder returnType( String type );
 
+    /**
+     * Sets the function body
+     * 
+     * @param body
+     * 
+     * @return itself
+     */
     public FunctionDescrBuilder body( String body );
 
+    /**
+     * Sets the function dialect. Default is java.
+     * 
+     * @param dialect
+     * 
+     * @return itself
+     */
     public FunctionDescrBuilder dialect( String dialect );
 }
