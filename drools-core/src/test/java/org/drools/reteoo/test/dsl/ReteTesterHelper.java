be149538839c3665660cbb9739414422a5e8b067
Mark Proctor <mdproctor@gmail.com>
2011-06-27 04:52:38 +0100

    JBRULES-3101 Right Input triggering doesn't work for Queries (cherry picked from commit 279c9df98434d668d6adc837982a40ae6b9c97d3)

--- drools-core/src/test/java/org/drools/reteoo/test/dsl/ReteTesterHelper.java
+++ drools-core/src/test/java/org/drools/reteoo/test/dsl/ReteTesterHelper.java
@@ -32,12 +32,15 @@ import org.drools.rule.Declaration;
 import org.drools.rule.LiteralConstraint;
 import org.drools.rule.Package;
 import org.drools.rule.Pattern;
+import org.drools.rule.UnificationRestriction;
 import org.drools.rule.VariableConstraint;
+import org.drools.rule.VariableRestriction;
 import org.drools.spi.AlphaNodeFieldConstraint;
 import org.drools.spi.BetaNodeFieldConstraint;
 import org.drools.spi.Evaluator;
 import org.drools.spi.FieldValue;
 import org.drools.spi.InternalReadAccessor;
+import org.drools.spi.Restriction;
 
 public class ReteTesterHelper {
 
@@ -71,22 +74,28 @@ public class ReteTesterHelper {
         return typeResolver;
     }
 
-    public BetaNodeFieldConstraint getBoundVariableConstraint(final Pattern pattern,
+    public BetaNodeFieldConstraint getBoundVariableConstraint(final Class clazz,
                                                               final String fieldName,
                                                               final Declaration declaration,
                                                               final String evaluatorString) throws IntrospectionException {
-        final Class< ? > clazz = ((ClassObjectType) pattern.getObjectType()).getClassType();
 
         final InternalReadAccessor extractor = store.getReader( clazz,
                                                                 fieldName,
                                                                 getClass().getClassLoader() );
 
         Evaluator evaluator = getEvaluator( clazz,
-                                            evaluatorString );
+                                            ":=".equals( evaluatorString ) ? "==" : evaluatorString );
+        
+        Restriction vr = new VariableRestriction( extractor, declaration, evaluator );                
 
-        return new VariableConstraint( extractor,
-                                       declaration,
-                                       evaluator );
+        if (  ":=".equals( evaluatorString ) ) {
+            vr = new UnificationRestriction( (VariableRestriction)  vr );
+        }
+
+        VariableConstraint vc = new VariableConstraint( extractor,
+                                                        vr );
+        
+        return vc;
     }
 
     public AlphaNodeFieldConstraint getLiteralConstraint(final Pattern pattern,
