5d44e6da147bcec6a9950b47704cd96418803061
krisv <kverlaen@redhat.com>
2011-08-24 16:02:15 +0200

    Bug 731702 - jBPM workitems module has dependency on snapshot artifact  - removing finder dependency

--- kogito-jbpm/jbpm-workitems/src/main/java/org/jbpm/process/workitem/finder/FinderWorkItemHandler.java
+++ kogito-jbpm/jbpm-workitems/src/main/java/org/jbpm/process/workitem/finder/FinderWorkItemHandler.java
@@ -22,30 +22,30 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.finder.FileFinder;
-import org.apache.commons.finder.Finder;
+//import org.apache.commons.finder.FileFinder;
+//import org.apache.commons.finder.Finder;
 import org.drools.process.instance.WorkItemHandler;
 import org.drools.runtime.process.WorkItem;
 import org.drools.runtime.process.WorkItemManager;
 
 public class FinderWorkItemHandler implements WorkItemHandler {
 
-	private FileFinder finder;
+//	private FileFinder finder;
 	
 	public FinderWorkItemHandler() {
-		finder = new FileFinder();
+//		finder = new FileFinder();
 	}
 	
 	public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
 		String path = (String) workItem.getParameter("Path");
 		String regex = (String) workItem.getParameter("Regex");
 		Map<String, Object> options = new HashMap<String, Object>();
-		options.put(Finder.REGEX, regex);
-		File[] files = finder.find(new File(path), options);
+//		options.put(Finder.REGEX, regex);
+//		File[] files = finder.find(new File(path), options);
 		List<File> fileList = new ArrayList<File>();
-		for (File file: files) {
-			fileList.add(file);
-		}
+//		for (File file: files) {
+//			fileList.add(file);
+//		}
 		Map<String, Object> results = new HashMap<String, Object>();
 		results.put("Files", fileList);
 		manager.completeWorkItem(workItem.getId(), results);
