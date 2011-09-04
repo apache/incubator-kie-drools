196d0be5a41f24704fb8b83c3c9ca597a0053cd8
Mario Fusco <mario.fusco@gmail.com>
2011-09-04 11:44:23 +0200

    [JBRULES-3194] fix types size

--- drools-compiler/src/test/java/org/drools/rule/builder/dialect/asm/ClassGeneratorTest.java
+++ drools-compiler/src/test/java/org/drools/rule/builder/dialect/asm/ClassGeneratorTest.java
@@ -3,7 +3,6 @@ package org.drools.rule.builder.dialect.asm;
 import org.junit.*;
 import org.mvel2.asm.*;
 
-import static org.drools.rule.builder.dialect.asm.ASMUtil.mDescr;
 import static org.junit.Assert.assertEquals;
 import static org.mvel2.asm.Opcodes.*;
 import static org.mvel2.asm.Type.*;
@@ -18,13 +17,13 @@ public class ClassGeneratorTest {
         generator.addField(ACC_PRIVATE + ACC_FINAL, "name", String.class);
 
         generator.addDefaultConstructor(new ClassGenerator.MethodBody() {
-            public void body(ClassGenerator cg, MethodVisitor mv) {
+            public void body(ClassGenerator cg, MethodVisitor mv, ClassGenerator.MethodHelper mh) {
                 mv.visitVarInsn(ALOAD, 0);// read local variable 0 (initialized to this) and push it on the stack
                 mv.visitLdcInsn(MY_NAME); // push the String MY_NAME on the stack
                 mv.visitFieldInsn(PUTFIELD, cg.getClassDescriptor(), "name", getDescriptor(String.class));
             }
-        }).addMethod(ACC_PUBLIC, "toString", mDescr(String.class), new ClassGenerator.MethodBody() {
-            public void body(ClassGenerator cg, MethodVisitor mv) {
+        }).addMethod(ACC_PUBLIC, "toString", generator.toMethodDescriptor(String.class), new ClassGenerator.MethodBody() {
+            public void body(ClassGenerator cg, MethodVisitor mv, ClassGenerator.MethodHelper mh) {
                 mv.visitVarInsn(ALOAD, 0); // read local variable 0 (initialized to this) and push it on the stack
                 mv.visitFieldInsn(GETFIELD, cg.getClassDescriptor(), "name", getDescriptor(String.class));
                 mv.visitInsn(ARETURN); // return the first object on the stack
