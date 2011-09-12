8f5ff051c742faa8357de1976abdd8fedddef266
Marco Rietveld <mrietvel@redhat.com>
2011-09-12 10:37:47 +0200

    Modified PersistenceUtils to use default values when developer does not use maven plugin -- and added extra util methods

--- kogito-drools/drools-persistence-jpa/src/test/java/org/drools/persistence/util/PersistenceUtil.java
+++ kogito-drools/drools-persistence-jpa/src/test/java/org/drools/persistence/util/PersistenceUtil.java
@@ -1,36 +1,92 @@
+/*
+ * Copyright 2011 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.drools.persistence.util;
+
+import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import org.drools.persistence.jta.JtaTransactionManager;
+import org.drools.runtime.Environment;
+import org.drools.runtime.EnvironmentName;
 import org.h2.tools.DeleteDbFiles;
 import org.h2.tools.Server;
 import org.junit.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import bitronix.tm.resource.jdbc.PoolingDataSource;
 
 public class PersistenceUtil {
 
-    public static final String PERSISTENCE_UNIT_NAME = "org.drools.persistence.jpa";
+    private static Logger logger = LoggerFactory.getLogger( PersistenceUtil.class );
+
+    public static final String DROOLS_PERSISTENCE_UNIT_NAME = "org.drools.persistence.jpa";
+    public static final String JBPM_PERSISTENCE_UNIT_NAME = "org.jbpm.persistence.jpa";
         
     protected static final String DATASOURCE_PROPERTIES = "/datasource.properties";
     private static TestH2Server h2Server = new TestH2Server();
+    
+    private static Properties defaultProperties = null;
+   
+    private static Properties getDefaultProperties() { 
+        if( defaultProperties == null ) { 
+            String [] keyArr = { "serverName", "portNumber", "databaseName", "url", 
+                    "user", "password", "driverClassName", "className", 
+                    "maxPoolSize", "allowLocalTransactions" };
+            String [] defaultPropArr= { "", "", "", "jdbc:h2:tcp://localhost/JPADroolsFlow", 
+                    "sa", "", "org.h2.Driver", "bitronix.tm.resource.jdbc.lrc.LrcXADataSource", 
+                    "16", "true" };
+            Assert.assertTrue("Unequal number of keys for default properties", keyArr.length == defaultPropArr.length);
+            defaultProperties = new Properties();
+            for( int i = 0; i < keyArr.length; ++i ) { 
+                defaultProperties.put(keyArr[i], defaultPropArr[i]);
+            }
+        }
+        
+        return defaultProperties;
+    }
 
-    private static Properties getDatasourceProperties() {
-        String propertiesNotFound = "Unable to load datasource properties ["+ DATASOURCE_PROPERTIES + "]";
+    public static Properties getDatasourceProperties() {
+        boolean propertiesNotFound = false;
 
         InputStream propsInputStream = PersistenceUtil.class.getResourceAsStream(DATASOURCE_PROPERTIES);
-        Assert.assertNotNull(propertiesNotFound, propsInputStream);
         Properties props = new Properties();
-        try {
-            props.load(propsInputStream);
-        } catch (IOException ioe) {
-            Assert.fail(propertiesNotFound + ": " + ioe.getMessage());
-            ioe.printStackTrace();
+        if( propsInputStream != null ) { 
+            try {
+                props.load(propsInputStream);
+            } catch (IOException ioe) {
+                propertiesNotFound = true;
+                logger.warn("Unable to find properties, using default H2 properties: " + ioe.getMessage());
+                ioe.printStackTrace();
+            }
+        }
+        else { 
+            propertiesNotFound = true;
         }
 
+        String password = props.getProperty("password");
+        if( "${maven.jdbc.password}".equals(password) || propertiesNotFound )  { 
+           props = getDefaultProperties();
+        }
+            
         return props;
     }
 
@@ -103,6 +159,18 @@ public class PersistenceUtil {
 
         return pds;
     }
+    
+    public static boolean useTransactions() { 
+        boolean useTransactions = false;
+        String databaseDriverClassName =  getDatasourceProperties().getProperty("driverClassName");
+        
+        // Postgresql has a "Large Object" api which REQUIRES the use of transactions, since 
+        //  @Lob/byte array is actually stored in multiple tables. 
+        if( databaseDriverClassName.startsWith("org.postgresql") ) { 
+            useTransactions = true;
+        }
+        return useTransactions;
+    }
 
     private static class TestH2Server {
         private Server realH2Server;
@@ -129,4 +197,30 @@ public class PersistenceUtil {
         }
 
     }
+    
+    public static Object getValueOfField(String fieldname, Object source) {
+        String sourceClassName = source.getClass().getSimpleName();
+    
+        Field field = null;
+        try {
+            field = source.getClass().getDeclaredField(fieldname);
+            field.setAccessible(true);
+        } catch (SecurityException e) {
+            fail("Unable to retrieve " + fieldname + " field from " + sourceClassName + ": " + e.getCause());
+        } catch (NoSuchFieldException e) {
+            fail("Unable to retrieve " + fieldname + " field from " + sourceClassName + ": " + e.getCause());
+        }
+    
+        assertNotNull("." + fieldname + " field is null!?!", field);
+        Object fieldValue = null;
+        try {
+            fieldValue = field.get(source);
+        } catch (IllegalArgumentException e) {
+            fail("Unable to retrieve value of " + fieldname + " from " + sourceClassName + ": " + e.getCause());
+        } catch (IllegalAccessException e) {
+            fail("Unable to retrieve value of " + fieldname + " from " + sourceClassName + ": " + e.getCause());
+        }
+        return fieldValue;
+    }
+
 }
