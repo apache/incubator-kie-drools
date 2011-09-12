8c4e9b9d56883c1508f30d760650c7129d28661b
Marco Rietveld <mrietvel@redhat.com>
2011-09-12 10:00:07 +0200

    Added Copyright and fixed typo (buildComman_d_Service)

--- drools-persistence-jpa/src/main/java/org/drools/persistence/jpa/KnowledgeStoreServiceImpl.java
+++ drools-persistence-jpa/src/main/java/org/drools/persistence/jpa/KnowledgeStoreServiceImpl.java
@@ -1,4 +1,19 @@
-package org.drools.persistence.jpa;
+/*
+ * Copyright 2010 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ package org.drools.persistence.jpa;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -51,7 +66,7 @@ public class KnowledgeStoreServiceImpl
             throw new IllegalArgumentException( "Environment cannot be null" );
         }
 
-        return new CommandBasedStatefulKnowledgeSession( (CommandService) buildCommanService( kbase,
+        return new CommandBasedStatefulKnowledgeSession( (CommandService) buildCommandService( kbase,
                                                                              mergeConfig( configuration ),
                                                                              environment ) );
     }
@@ -68,13 +83,13 @@ public class KnowledgeStoreServiceImpl
             throw new IllegalArgumentException( "Environment cannot be null" );
         }
 
-        return new CommandBasedStatefulKnowledgeSession( (CommandService) buildCommanService( id,
+        return new CommandBasedStatefulKnowledgeSession( (CommandService) buildCommandService( id,
                                                                                               kbase,
                                                                                               mergeConfig( configuration ),
                                                                                               environment ) );
     }
 
-    private CommandExecutor buildCommanService(Integer sessionId,
+    private CommandExecutor buildCommandService(Integer sessionId,
                                               KnowledgeBase kbase,
                                               KnowledgeSessionConfiguration conf,
                                               Environment env) {
@@ -104,7 +119,7 @@ public class KnowledgeStoreServiceImpl
         }
     }
 
-    private CommandExecutor buildCommanService(KnowledgeBase kbase,
+    private CommandExecutor buildCommandService(KnowledgeBase kbase,
                                               KnowledgeSessionConfiguration conf,
                                               Environment env) {
 
