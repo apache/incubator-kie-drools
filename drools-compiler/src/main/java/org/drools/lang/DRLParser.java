05179653e59f611964925b1ac41dee09cf596d47
Mark Proctor <mdproctor@gmail.com>
2011-09-15 05:48:32 +0100

    JBRULES-2993: fixing DRLParser to not unescape strings, as this is being done by the compiler when MVEL executes the expression.

--- drools-compiler/src/main/java/org/drools/lang/DRLParser.java
+++ drools-compiler/src/main/java/org/drools/lang/DRLParser.java
@@ -3195,9 +3195,6 @@ public class DRLParser {
                                               -1 ).trim();
                         if ( state.failed ) return;
                         if ( state.backtracking == 0 ) {
-                            if ( value.startsWith( "\"" ) && value.endsWith( "\"" ) ) {
-                                value = StringUtils.unescapeJava( value );
-                            }
                             annotation.value( value );
                         }
                     }
@@ -3299,9 +3296,6 @@ public class DRLParser {
             if ( state.failed ) return;
 
             String value = elementValue();
-            if ( value.startsWith( "\"" ) && value.endsWith( "\"" ) ) {
-                value = StringUtils.unescapeJava( value );
-            }
             if ( state.failed ) return;
 
             if ( state.backtracking == 0 ) {
