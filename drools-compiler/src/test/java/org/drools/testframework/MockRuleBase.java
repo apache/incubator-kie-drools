74f6b55f1ac78e5bbf0d1a88eb3278a0a0b3f604
Mark Proctor <mdproctor@gmail.com>
2011-09-16 07:15:42 +0100

    JBRULES-3146 rules with timers do not persist (cherry picked from commit ba5ecf7277cb37f0df635557e44b95c668b94556)

--- drools-compiler/src/test/java/org/drools/testframework/MockRuleBase.java
+++ drools-compiler/src/test/java/org/drools/testframework/MockRuleBase.java
@@ -20,99 +20,77 @@ import org.drools.runtime.Environment;
 public class MockRuleBase implements RuleBase {
 
     public int getAdditionsSinceLock() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     public Package getPackage(String name) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public Package[] getPackages() {
-
         return new Package[0];
     }
 
     public int getRemovalsSinceLock() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     public StatefulSession[] getStatefulSessions() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public void lock() {
-        // TODO Auto-generated method stub
-
     }
 
     public StatefulSession newStatefulSession() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public StatefulSession newStatefulSession(SessionConfiguration config, Environment environment ) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public StatefulSession readStatefulSession(InputStream stream,
-            boolean keepReference) throws IOException, ClassNotFoundException {
-        // TODO Auto-generated method stub
+                                               boolean keepReference) throws IOException, ClassNotFoundException {
         return null;
     }
+    
+    public StatefulSession readStatefulSession(InputStream stream,
+                                               boolean keepReference,
+                                               SessionConfiguration config) throws IOException, ClassNotFoundException {
+        return null;
+    }    
 
     public StatelessSession newStatelessSession() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public void removeFunction(String packageName, String functionName) {
-        // TODO Auto-generated method stub
-
     }
 
     public void removePackage(String packageName) {
-        // TODO Auto-generated method stub
-
     }
 
     public void removeProcess(String id) {
-        // TODO Auto-generated method stub
-
     }
 
     public void removeRule(String packageName, String ruleName) {
-        // TODO Auto-generated method stub
-
     }
 
     public void unlock() {
-        // TODO Auto-generated method stub
-
     }
 
     public void addEventListener(RuleBaseEventListener listener) {
-        // TODO Auto-generated method stub
-
     }
 
     public List getRuleBaseEventListeners() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public void removeEventListener(RuleBaseEventListener listener) {
-        // TODO Auto-generated method stub
-
     }
 
     public void addPackage(Package pkg) {
-        // TODO Auto-generated method stub
-
     }
 
     public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
@@ -125,14 +103,11 @@ public class MockRuleBase implements RuleBase {
 
     public void writeStatefulSession(StatefulSession session,
                                      OutputStream stream) throws IOException {
-        // TODO Auto-generated method stub
-        
     }
 
     public StatefulSession readStatefulSession(InputStream stream,
                                                Marshaller marshaller) throws IOException,
                                                                      ClassNotFoundException {
-        // TODO Auto-generated method stub
         return null;
     }
 
@@ -142,46 +117,41 @@ public class MockRuleBase implements RuleBase {
                                                SessionConfiguration config,
                                                Environment environment) throws IOException,
                                                                      ClassNotFoundException {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public void writeStatefulSession(StatefulSession session,
                                      OutputStream stream,
                                      Marshaller marshaller) throws IOException {
-        // TODO Auto-generated method stub
-        
     }
 
     public FactType getFactType(String string) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public void addPackages(Package[] pkgs) {
-        // TODO Auto-generated method stub
-        
     }
 
     public StatefulSession newStatefulSession(boolean keepReference) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public StatefulSession newStatefulSession(InputStream stream,
                                               boolean keepReference) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public StatefulSession newStatefulSession(InputStream stream) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public void removeQuery(String packageName,
                             String queryName) {
-        // TODO Auto-generated method stub
-        
+    }
+
+    public StatefulSession newStatefulSession(InputStream stream,
+                                              boolean keepReference,
+                                              SessionConfiguration conf) {
+        return null;
     }
 }
