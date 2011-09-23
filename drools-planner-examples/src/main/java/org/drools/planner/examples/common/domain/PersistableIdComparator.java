eff1eb13328f56e3028034491be681d6a1f27c65
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2011-09-23 11:13:39 +0200

    findbugs implements Comparator but not Serializable

--- drools-planner-examples/src/main/java/org/drools/planner/examples/common/domain/PersistableIdComparator.java
+++ drools-planner-examples/src/main/java/org/drools/planner/examples/common/domain/PersistableIdComparator.java
@@ -16,11 +16,12 @@
 
 package org.drools.planner.examples.common.domain;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
 import org.apache.commons.lang.builder.CompareToBuilder;
 
-public class PersistableIdComparator implements Comparator<AbstractPersistable> {
+public class PersistableIdComparator implements Comparator<AbstractPersistable>, Serializable {
 
     public int compare(AbstractPersistable a, AbstractPersistable b) {
         return new CompareToBuilder().append(a.getId(), b.getId()).toComparison();
