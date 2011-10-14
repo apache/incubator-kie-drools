f344b9bb40347a24c59d6fa402d65ba3c5b2eb7d
Marco Rietveld <mrietvel@redhat.com>
2011-10-14 20:32:42 +0200

    Cherry-picking 0dc2ef74 in order to fix 'conditional' process unmarshalling bug

--- drools-core/src/main/java/org/drools/marshalling/impl/OutputMarshaller.java
+++ drools-core/src/main/java/org/drools/marshalling/impl/OutputMarshaller.java
@@ -124,15 +124,14 @@ public class OutputMarshaller {
         context.writeLong( wm.getFactHandleFactory().getRecency() );
         ////context.out.println( "FactHandleFactory int:" + wm.getFactHandleFactory().getId() + " long:" + wm.getFactHandleFactory().getRecency() );
 
-
+        context.writeLong( wm.getPropagationIdCounter() );
+        //context.out.println( "PropagationCounter long:" + wm.getPropagationIdCounter() );        
         
         InternalFactHandle handle = context.wm.getInitialFactHandle();
         context.writeInt( handle.getId() );
         context.writeLong( handle.getRecency() );
         //context.out.println( "InitialFact int:" + handle.getId() + " long:" + handle.getRecency() );
 
-        context.writeLong( wm.getPropagationIdCounter() );
-        //context.out.println( "PropagationCounter long:" + wm.getPropagationIdCounter() );        
         
         writeAgenda( context );        
         
@@ -158,10 +157,16 @@ public class OutputMarshaller {
         if ( context.marshalProcessInstances && processMarshaller != null ) {
             processMarshaller.writeProcessInstances( context );
         }
+        else { 
+            context.stream.writeShort( PersisterEnums.END );
+        }
 
         if ( context.marshalWorkItems && processMarshaller != null ) {
             processMarshaller.writeWorkItems( context );
         }     
+        else { 
+            context.stream.writeShort( PersisterEnums.END );
+        }
         
         if ( processMarshaller != null ) {
             // this now just assigns the writer, it will not write out any timer information
