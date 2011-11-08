ede116be225bb969774f1e798543383d1a3cabcf
mariofusco <mario.fusco@gmail.com>
2011-11-08 10:00:47 +0100

    [JBRULES-3263] custom coercion strategy for booleans

---
+++ kogito-drools/drools-core/src/main/java/org/drools/rule/constraint/BooleanConversionHandler.java
@@ -0,0 +1,24 @@
+package org.drools.rule.constraint;
+
+import org.mvel2.ConversionHandler;
+
+public class BooleanConversionHandler implements ConversionHandler {
+
+    public static final BooleanConversionHandler INSTANCE = new BooleanConversionHandler();
+
+    private BooleanConversionHandler() { }
+
+    public Object convertFrom(Object in) {
+        if (in.getClass() == Boolean.class || in.getClass() == boolean.class) return in;
+        if (in instanceof String) {
+            if (((String)in).equalsIgnoreCase("true")) return true;
+            if (((String)in).equalsIgnoreCase("false")) return false;
+        }
+        throw new ClassCastException("Cannot convert " + in + " into a Boolean");
+    }
+
+    public boolean canConvertFrom(Class cls) {
+        // Apparently unused ???
+        return false;
+    }
+}
