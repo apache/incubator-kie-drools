63be4b6a7ec37ef29a95c7a2a7e435b50c4f1333
krisv <kris.verlaenen@gmail.com>
2011-12-06 21:52:47 +0100

    Adding process factory test example

---
+++ kogito-jbpm/jbpm-bpmn2/src/test/java/org/jbpm/bpmn2/ProcessFactoryTest.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2010 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jbpm.bpmn2;
+
+import org.drools.KnowledgeBase;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+import org.drools.runtime.process.ProcessInstance;
+import org.jbpm.JbpmTestCase;
+import org.jbpm.bpmn2.xml.XmlBPMNProcessDumper;
+import org.jbpm.ruleflow.core.RuleFlowProcess;
+import org.jbpm.ruleflow.core.RuleFlowProcessFactory;
+
+public class ProcessFactoryTest extends JbpmTestCase {
+	
+	public void testProcessFactory() {
+		RuleFlowProcessFactory factory = RuleFlowProcessFactory.createProcess("org.jbpm.process");
+		factory
+			// header
+			.name("My process").packageName("org.jbpm")
+			// nodes
+			.startNode(1).name("Start").done()
+			.actionNode(2).name("Action")
+				.action("java", "System.out.println(\"Action\");").done()
+			.endNode(3).name("End").done()
+			// connections
+			.connection(1, 2)
+			.connection(2, 3);
+		RuleFlowProcess process = factory.validate().getProcess();
+		System.out.println(XmlBPMNProcessDumper.INSTANCE.dump(process));
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+		kbuilder.add(ResourceFactory.newByteArrayResource(XmlBPMNProcessDumper.INSTANCE.dump(process).getBytes()), ResourceType.BPMN2);
+		KnowledgeBase kbase = kbuilder.newKnowledgeBase();
+		StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+		ProcessInstance processInstance = ksession.startProcess("org.jbpm.process");
+		assertEquals(ProcessInstance.STATE_COMPLETED, processInstance.getState());
+	}
+
+}
