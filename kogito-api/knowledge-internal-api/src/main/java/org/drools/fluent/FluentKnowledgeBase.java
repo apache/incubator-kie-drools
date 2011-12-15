186be5a3dcf572bc362960b07f477d6a54bfa13a
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2011-12-15 17:34:59 +0100

    move simulation interfaces to internal-api (only those that can be moved, some have hamcrest dependencies)

---
+++ kogito-api/knowledge-internal-api/src/main/java/org/drools/fluent/FluentKnowledgeBase.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2011 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.fluent;
+
+import org.drools.KnowledgeBaseConfiguration;
+import org.drools.builder.KnowledgeBuilderConfiguration;
+import org.drools.builder.ResourceConfiguration;
+import org.drools.builder.ResourceType;
+import org.drools.io.Resource;
+
+public interface FluentKnowledgeBase<T> extends FluentBase {    
+    
+    T addKnowledgePackages();
+    
+    T addKnowledgePackages(Resource resource,
+                           ResourceType type);
+    
+    T addKnowledgePackages(Resource resource,
+                           ResourceType type,
+                           ResourceConfiguration configuration);   
+    
+}
