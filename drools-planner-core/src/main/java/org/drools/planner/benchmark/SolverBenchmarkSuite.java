d5fc0e2819e070d72238faf5cbe768889ccd9afe
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2011-12-18 12:34:07 +0100

    revert extract method initSolverNames

--- drools-planner-core/src/main/java/org/drools/planner/benchmark/SolverBenchmarkSuite.java
+++ drools-planner-core/src/main/java/org/drools/planner/benchmark/SolverBenchmarkSuite.java
@@ -204,15 +204,6 @@ public class SolverBenchmarkSuite {
             solverBenchmark.validate();
             solverBenchmark.resetSolverBenchmarkResultList();
         }
-        initSolverNames(nameSet, noNameBenchmarkSet);
-        initBenchmarkDirectoryAndSubdirs();
-        if (solverBenchmarkComparator == null) {
-            solverBenchmarkComparator = new TotalScoreSolverBenchmarkComparator();
-        }
-//        resetSolverBenchmarkSuiteResultList();
-    }
-
-    private void initSolverNames(Set<String> nameSet, Set<SolverBenchmark> noNameBenchmarkSet) {
         int generatedNameIndex = 0;
         for (SolverBenchmark solverBenchmark : noNameBenchmarkSet) {
             String generatedName = "Config_" + generatedNameIndex;
@@ -223,6 +214,11 @@ public class SolverBenchmarkSuite {
             solverBenchmark.setName(generatedName);
             generatedNameIndex++;
         }
+        initBenchmarkDirectoryAndSubdirs();
+        if (solverBenchmarkComparator == null) {
+            solverBenchmarkComparator = new TotalScoreSolverBenchmarkComparator();
+        }
+//        resetSolverBenchmarkSuiteResultList();
     }
 
     private void initBenchmarkDirectoryAndSubdirs() {
