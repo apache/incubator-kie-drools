b1e2517792c7ac55edca3abb67c6c119e5389aa7
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2012-01-12 17:20:04 +0100

    Remove FluentBase interface

--- kogito-api/knowledge-internal-api/src/main/java/org/drools/fluent/FluentKnowledgeBuilder.java
+++ kogito-api/knowledge-internal-api/src/main/java/org/drools/fluent/FluentKnowledgeBuilder.java
@@ -20,7 +20,7 @@ import org.drools.builder.ResourceConfiguration;
 import org.drools.builder.ResourceType;
 import org.drools.io.Resource;
 
-public interface FluentKnowledgeBuilder<T> extends FluentBase {
+public interface FluentKnowledgeBuilder<T> {
     
     T add(Resource resource,
           ResourceType type);
