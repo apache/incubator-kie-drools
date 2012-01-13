4648424fea989cf8e439543cccd640592843a4c3
Davide Sottara <dsotty@gmail.com>
2012-01-14 00:54:55 +0100

    JBRULES-3153 ChangeSet loading will take 5 minutes without an internet connection

--- drools-core/src/main/java/org/drools/agent/impl/KnowledgeAgentConfigurationImpl.java
+++ drools-core/src/main/java/org/drools/agent/impl/KnowledgeAgentConfigurationImpl.java
@@ -19,7 +19,16 @@ package org.drools.agent.impl;
 import java.util.Properties;
 
 import org.drools.agent.KnowledgeAgentConfiguration;
+import org.drools.agent.conf.MonitorChangesetEventsOption;
+import org.drools.agent.conf.NewInstanceOption;
+import org.drools.agent.conf.ScanDirectoriesOption;
+import org.drools.agent.conf.ScanResourcesOption;
+import org.drools.agent.conf.UseKnowledgeBaseClassloaderOption;
+import org.drools.agent.conf.ValidationTimeoutOption;
 import org.drools.core.util.StringUtils;
+import org.drools.util.ChainedProperties;
+import org.drools.util.ClassLoaderUtil;
+import org.drools.util.CompositeClassLoader;
 
 /**
  * drools.agent.scanResources = <true|false>
@@ -31,22 +40,107 @@ public class KnowledgeAgentConfigurationImpl
     implements
     KnowledgeAgentConfiguration {
 
+
+    private static final KnowledgeAgentConfiguration defaultConf = new KnowledgeAgentConfigurationImpl();
+
+    public static KnowledgeAgentConfiguration getDefaultInstance() {
+        return defaultConf;
+    }
+
+
+    private ChainedProperties       chainedProperties;
+    private CompositeClassLoader    classLoader;
+    private boolean                 immutable              = false;
+    private boolean                 classLoaderCache       = true;
+
+
     private boolean scanResources          = true;
     private boolean scanDirectories        = true;
     private boolean monitorChangeSetEvents = true;
     private boolean newInstance            = true;
     private boolean useKBaseClassLoaderForCompiling = false;
+    private int     validationTimeout      = 0;
 
-    public KnowledgeAgentConfigurationImpl() {
 
-    }
 
     public KnowledgeAgentConfigurationImpl(Properties properties) {
-        for ( Object key : properties.keySet() ) {
-            setProperty( (String) key,
-                         (String) properties.get( key ) );
-        }
+        init( properties,
+              null );
     }
+
+    public KnowledgeAgentConfigurationImpl() {
+        init( null,
+              null );
+    }
+
+    /**
+     * A constructor that sets the parent classloader to be used
+     * while dealing with this knowledge agent
+     *
+     * @param classLoaders
+     */
+    public KnowledgeAgentConfigurationImpl(ClassLoader... classLoaders) {
+        init( null,
+              classLoaders );
+    }
+
+    /**
+     * A constructor that sets the classloader to be used as the parent classloader
+     * of this knowledge agent classloaders, and the properties to be used
+     * as base configuration options
+     *
+     * @param classLoaders
+     * @param properties
+     */
+    public KnowledgeAgentConfigurationImpl(Properties properties,
+                                           ClassLoader... classLoaders) {
+        init( properties,
+              classLoaders );
+    }
+
+
+
+    private void init(Properties properties,
+                      ClassLoader... classLoaders) {
+        this.immutable = false;
+
+        setClassLoader( classLoaders );
+
+        this.chainedProperties = new ChainedProperties( "knowledgeagent.conf",
+                                                        this.classLoader,
+                                                        true );
+
+        if ( properties != null ) {
+            this.chainedProperties.addProperties( properties );
+        }
+
+        setProperty( MonitorChangesetEventsOption.PROPERTY_NAME,
+                     this.chainedProperties.getProperty( MonitorChangesetEventsOption.PROPERTY_NAME,
+                                                         MonitorChangesetEventsOption.NO.toString() ) );
+
+        setProperty( ScanDirectoriesOption.PROPERTY_NAME,
+                     this.chainedProperties.getProperty( ScanDirectoriesOption.PROPERTY_NAME,
+                                                         ScanDirectoriesOption.NO.toString() ) );
+
+        setProperty( ScanResourcesOption.PROPERTY_NAME,
+                     this.chainedProperties.getProperty( ScanResourcesOption.PROPERTY_NAME,
+                                                         ScanResourcesOption.NO.toString() ) );
+
+        setProperty( NewInstanceOption.PROPERTY_NAME,
+                     this.chainedProperties.getProperty( NewInstanceOption.PROPERTY_NAME,
+                                                         NewInstanceOption.NO.toString() ) );
+
+        setProperty( UseKnowledgeBaseClassloaderOption.PROPERTY_NAME,
+                     this.chainedProperties.getProperty( UseKnowledgeBaseClassloaderOption.PROPERTY_NAME,
+                                                         UseKnowledgeBaseClassloaderOption.NO.toString() ) );
+
+        setProperty( ValidationTimeoutOption.PROPERTY_NAME,
+                     this.chainedProperties.getProperty( ValidationTimeoutOption.PROPERTY_NAME,
+                                                         "0" ) );
+
+    }
+
+
 
     public void setProperty(String name,
                             String value) {
@@ -55,25 +149,32 @@ public class KnowledgeAgentConfigurationImpl
             return;
         }
 
-        if ( name.equals( "drools.agent.monitorChangeSetEvents" ) ) {
+        if ( name.equals( MonitorChangesetEventsOption.PROPERTY_NAME ) ) {
             setMonitorChangeSetEvents( StringUtils.isEmpty( value ) ? true : Boolean.parseBoolean( value ) );
-        } else if ( name.equals( "drools.agent.scanDirectories" ) ) {
+        } else if ( name.equals( ScanDirectoriesOption.PROPERTY_NAME ) ) {
             boolean bool = StringUtils.isEmpty( value ) ? true : Boolean.parseBoolean( value );
             setScanDirectories( bool );
             if ( bool ) {
                 setScanResources( true );
                 setMonitorChangeSetEvents( true );
             }
-        } else if ( name.equals( "drools.agent.scanResources" ) ) {
+        } else if ( name.equals( ScanResourcesOption.PROPERTY_NAME ) ) {
             boolean bool = StringUtils.isEmpty( value ) ? true : Boolean.parseBoolean( value );
             setScanResources( bool );
             if ( bool ) {
                 setMonitorChangeSetEvents( true );
             }
-        } else if ( name.equals( "drools.agent.newInstance" ) ) {
+        } else if ( name.equals( NewInstanceOption.PROPERTY_NAME ) ) {
             setNewInstance( StringUtils.isEmpty( value ) ? true : Boolean.parseBoolean( value ) );
-        } else if ( name.equals( "drools.agent.useKBaseClassLoaderForCompiling" ) ) {
+        } else if ( name.equals( UseKnowledgeBaseClassloaderOption.PROPERTY_NAME ) ) {
             setUseKBaseClassLoaderForCompiling( StringUtils.isEmpty( value ) ? true : Boolean.parseBoolean( value ) );
+        } else if ( name.equals( ValidationTimeoutOption.PROPERTY_NAME ) ) {
+            try {
+                int millisec =  StringUtils.isEmpty( value ) ? 0 : Integer.parseInt( value );
+                setValidationTimeout( millisec );
+            } catch ( NumberFormatException nfe ) {
+                setValidationTimeout( 0 );
+            }
         }
     }
 
@@ -83,20 +184,67 @@ public class KnowledgeAgentConfigurationImpl
             return null;
         }
 
-        if ( name.equals( "drools.agent.scanResources" ) ) {
+        if ( name.equals( ScanResourcesOption.PROPERTY_NAME ) ) {
             return Boolean.toString( this.scanResources );
-        } else if ( name.equals( "drools.agent.scanDirectories" ) ) {
+        } else if ( name.equals( ScanDirectoriesOption.PROPERTY_NAME ) ) {
             return Boolean.toString( this.scanDirectories );
-        } else if ( name.equals( "drools.agent.monitorChangeSetEvents" ) ) {
+        } else if ( name.equals( MonitorChangesetEventsOption.PROPERTY_NAME ) ) {
             return Boolean.toString( this.monitorChangeSetEvents );
-        } else if ( name.equals( "drools.agent.newInstance" ) ) {
+        } else if ( name.equals( NewInstanceOption.PROPERTY_NAME ) ) {
             return Boolean.toString( this.newInstance );
-        } else if ( name.equals( "drools.agent.useKBaseClassLoaderForCompiling" ) ) {
+        } else if ( name.equals( UseKnowledgeBaseClassloaderOption.PROPERTY_NAME ) ) {
             return Boolean.toString( this.useKBaseClassLoaderForCompiling );
+        } else if ( name.equals( ValidationTimeoutOption.PROPERTY_NAME ) ) {
+            return Integer.toString( this.validationTimeout );
         }
 
         return null;
     }
+
+
+    /**
+     * Makes the configuration object immutable. Once it becomes immutable,
+     * there is no way to make it mutable again.
+     * This is done to keep consistency.
+     */
+    public void makeImmutable() {
+        this.immutable = true;
+    }
+
+    /**
+     * Returns true if this configuration object is immutable or false otherwise.
+     * @return
+     */
+    public boolean isImmutable() {
+        return this.immutable;
+    }
+
+    private void checkCanChange() {
+        if ( this.immutable ) {
+            throw new UnsupportedOperationException( "Can't set a property after configuration becomes immutable" );
+        }
+    }
+
+
+    public CompositeClassLoader getClassLoader() {
+        return this.classLoader.clone();
+    }
+
+    public void setClassLoader(ClassLoader... classLoaders) {
+        this.classLoader = ClassLoaderUtil.getClassLoader(classLoaders,
+                getClass(),
+                isClassLoaderCacheEnabled());
+    }
+
+    public boolean isClassLoaderCacheEnabled() {
+        return classLoaderCache;
+    }
+
+    public void setClassLoaderCacheEnabled(boolean classLoaderCacheEnabled) {
+        this.classLoaderCache = classLoaderCacheEnabled;
+        this.classLoader.setCachingEnabled( this.classLoaderCache );
+    }
+
 
     public boolean isScanResources() {
         return scanResources;
@@ -138,6 +286,12 @@ public class KnowledgeAgentConfigurationImpl
         this.useKBaseClassLoaderForCompiling = useKBaseClassLoaderForCompiling;
     }
 
-    
+    public int getValidationTimeout() {
+        return validationTimeout;
+    }
+
+    public void setValidationTimeout(int validationTimeout) {
+        this.validationTimeout = validationTimeout;
+    }
 
 }
