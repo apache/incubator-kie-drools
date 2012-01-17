53edb2d2186cc3141a6f5451bc45092cf1ba532c
Edson Tirelli <ed.tirelli@gmail.com>
2012-01-16 22:47:29 -0500

    JBRULES-3348: fixing compilation error due to missing import

--- drools-core/src/main/java/org/drools/common/AbstractWorkingMemory.java
+++ drools-core/src/main/java/org/drools/common/AbstractWorkingMemory.java
@@ -25,6 +25,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Executors;
