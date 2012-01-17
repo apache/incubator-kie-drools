ba47bd692d079dbf1dc8ece1c7ea8bf37ff88626
Davide Sottara <dsotty@gmail.com>
2012-01-17 16:43:37 +0100

    JBRULES-3009 - Provide for declarative Java 1.5 enum definiton in DRL

--- drools-compiler/src/main/java/org/drools/lang/api/EnumDeclarationDescrBuilder.java
+++ drools-compiler/src/main/java/org/drools/lang/api/EnumDeclarationDescrBuilder.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,15 +16,12 @@
 
 package org.drools.lang.api;
 
-import org.drools.lang.descr.TypeDeclarationDescr;
 
-/**
- *  A descriptor builder for declared types
- */
-public interface TypeDeclarationDescrBuilder
-    extends
-    AnnotatedDescrBuilder<TypeDeclarationDescrBuilder>,
-    AbstractClassTypeDeclarationBuilder<TypeDeclarationDescr> {
+import org.drools.lang.descr.EnumDeclarationDescr;
+
+public interface EnumDeclarationDescrBuilder extends
+    AnnotatedDescrBuilder<EnumDeclarationDescrBuilder>,
+    AbstractClassTypeDeclarationBuilder<EnumDeclarationDescr> {
 
 
     /**
@@ -34,18 +31,13 @@ public interface TypeDeclarationDescrBuilder
      *
      * @return itself
      */
-    public TypeDeclarationDescrBuilder name( String type );
+    public EnumDeclarationDescrBuilder name( String type );
 
     /**
-     * Defines the super type of this type. For POJOs, this is the
-     * super class it will extend
-     * 
-     * @param type the super type for this type
-     * 
-     * @return itself
+     * Adds an enum literal
+     *
+     * @param lit
+     * @return
      */
-    public TypeDeclarationDescrBuilder superType( String type );
-    
-
-
+    public EnumLiteralDescrBuilder newEnumLiteral( String lit );
 }
