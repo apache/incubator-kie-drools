ea4bbd1df6e50e41d04094d9758f14dc2d89b0e7
Davide Sottara <dsotty@gmail.com>
2012-01-25 14:51:50 +0100

    JBRULES-3153 Fixing default property values

--- kogito-drools/drools-core/src/main/java/org/drools/agent/impl/KnowledgeAgentConfigurationImpl.java
+++ kogito-drools/drools-core/src/main/java/org/drools/agent/impl/KnowledgeAgentConfigurationImpl.java
@@ -116,23 +116,23 @@ public class KnowledgeAgentConfigurationImpl
 
         setProperty( MonitorChangesetEventsOption.PROPERTY_NAME,
                      this.chainedProperties.getProperty( MonitorChangesetEventsOption.PROPERTY_NAME,
-                                                         MonitorChangesetEventsOption.NO.toString() ) );
+                                                         "true" ) );
 
         setProperty( ScanDirectoriesOption.PROPERTY_NAME,
                      this.chainedProperties.getProperty( ScanDirectoriesOption.PROPERTY_NAME,
-                                                         ScanDirectoriesOption.NO.toString() ) );
+                                                         "true" ) );
 
         setProperty( ScanResourcesOption.PROPERTY_NAME,
                      this.chainedProperties.getProperty( ScanResourcesOption.PROPERTY_NAME,
-                                                         ScanResourcesOption.NO.toString() ) );
+                                                         "true" ) );
 
         setProperty( NewInstanceOption.PROPERTY_NAME,
                      this.chainedProperties.getProperty( NewInstanceOption.PROPERTY_NAME,
-                                                         NewInstanceOption.NO.toString() ) );
+                                                         "true" ) );
 
         setProperty( UseKnowledgeBaseClassloaderOption.PROPERTY_NAME,
                      this.chainedProperties.getProperty( UseKnowledgeBaseClassloaderOption.PROPERTY_NAME,
-                                                         UseKnowledgeBaseClassloaderOption.NO.toString() ) );
+                                                         "false" ) );
 
         setProperty( ValidationTimeoutOption.PROPERTY_NAME,
                      this.chainedProperties.getProperty( ValidationTimeoutOption.PROPERTY_NAME,
@@ -167,7 +167,7 @@ public class KnowledgeAgentConfigurationImpl
         } else if ( name.equals( NewInstanceOption.PROPERTY_NAME ) ) {
             setNewInstance( StringUtils.isEmpty( value ) ? true : Boolean.parseBoolean( value ) );
         } else if ( name.equals( UseKnowledgeBaseClassloaderOption.PROPERTY_NAME ) ) {
-            setUseKBaseClassLoaderForCompiling( StringUtils.isEmpty( value ) ? true : Boolean.parseBoolean( value ) );
+            setUseKBaseClassLoaderForCompiling( StringUtils.isEmpty( value ) ? false : Boolean.parseBoolean( value ) );
         } else if ( name.equals( ValidationTimeoutOption.PROPERTY_NAME ) ) {
             try {
                 int millisec =  StringUtils.isEmpty( value ) ? 0 : Integer.parseInt( value );
