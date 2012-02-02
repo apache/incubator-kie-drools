673fb2ebd6bcf1c8d860c65fea40234b450cffa4
Edson Tirelli <ed.tirelli@gmail.com>
2012-02-02 10:07:14 -0500

    BZ-728168: updating the documentation to state that the use of # for comments is deprecated and not supported by the rule builder API (cherry picked from commit 0df9873f42c295301cbdbb8bbf20be1e48c61ab8)

--- kogito-drools/drools-compiler/src/main/java/org/drools/lang/api/RuleDescrBuilder.java
+++ kogito-drools/drools-compiler/src/main/java/org/drools/lang/api/RuleDescrBuilder.java
@@ -28,12 +28,43 @@ public interface RuleDescrBuilder
     AttributeSupportBuilder<RuleDescrBuilder>,
     DescrBuilder<PackageDescrBuilder, RuleDescr> {
 
+    /**
+     * The name of the rule. Best practice is to keep rule names relatively short,
+     * i.e. under 60 characters.
+     * 
+     * @param name name of the rule
+     * 
+     * @return itself
+     */
     public RuleDescrBuilder name( String name );
 
+    /**
+     * Defines the name of the rule this rule extends. It will cause the rule
+     * to inherit the LHS from the parent rule.
+     * 
+     * @param name name of the parent rule
+     * 
+     * @return itself
+     */
     public RuleDescrBuilder extendsRule( String name );
 
+    /**
+     * The right hand side (consequence) of the rule. This is a code block
+     * that must be valid according to the used dialect (java or MVEL). In particular,
+     * the deprecated '#' character, that was used for one line comments is not supported.
+     * For one line comments, please use standard '//'.
+     * 
+     * @param rhs the code block 
+     * 
+     * @return itself
+     */
     public RuleDescrBuilder rhs( String rhs );
 
+    /**
+     * Defines the LHS (condition) of the rule.
+     * 
+     * @return a Conditional Element descriptor builder with the AND CE semantic.
+     */
     public CEDescrBuilder<RuleDescrBuilder, AndDescr> lhs();
 
 }
