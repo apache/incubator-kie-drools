ba402067becee8a0e90128052f92b0067a83f252
Mark Proctor <mproctor@codehaus.org>
2012-02-07 02:38:37 +0000

    JBRULES-3360 Wumpus World

--- drools-examples/src/main/java/org/drools/examples/wumpus/Reset.java
+++ drools-examples/src/main/java/org/drools/examples/wumpus/Reset.java
@@ -1,5 +1,15 @@
 package org.drools.examples.wumpus;
 
 public class Reset {
+    private boolean resetScore;
 
+    public boolean isResetScore() {
+        return resetScore;
+    }
+
+    public void setResetScore(boolean resetScore) {
+        this.resetScore = resetScore;
+    }
+    
+    
 }
