3d6e5ed93554d022a76dfeafc9b5160624c4e12e
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2012-02-26 17:28:41 +0100

    vehicle routing: domain

---
+++ drools-planner-examples/src/main/java/org/drools/planner/examples/vehiclerouting/domain/VrpCustomer.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2012 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.planner.examples.vehiclerouting.domain;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.drools.planner.api.domain.entity.PlanningEntity;
+import org.drools.planner.api.domain.variable.PlanningVariable;
+import org.drools.planner.api.domain.variable.ValueRange;
+import org.drools.planner.api.domain.variable.ValueRangeType;
+import org.drools.planner.api.domain.variable.ValueRanges;
+import org.drools.planner.examples.common.domain.AbstractPersistable;
+
+@PlanningEntity
+@XStreamAlias("VrpCustomer")
+public class VrpCustomer extends AbstractPersistable implements VrpAppearance {
+
+    private VrpLocation location; // the destinationCity
+    
+    // Planning variables: changes during planning, between score calculations.
+    private VrpAppearance previousAppearance;
+
+    public VrpLocation getLocation() {
+        return location;
+    }
+
+    public void setLocation(VrpLocation location) {
+        this.location = location;
+    }
+
+    @PlanningVariable(chained = true)
+    @ValueRanges({
+            @ValueRange(type = ValueRangeType.FROM_SOLUTION_PROPERTY, solutionProperty = "vehicleList"),
+            @ValueRange(type = ValueRangeType.FROM_SOLUTION_PROPERTY, solutionProperty = "customerList",
+                    excludeUninitializedPlanningEntity = true)})
+    public VrpAppearance getPreviousAppearance() {
+        return previousAppearance;
+    }
+
+    public void setPreviousAppearance(VrpAppearance previousAppearance) {
+        this.previousAppearance = previousAppearance;
+    }
+
+    // ************************************************************************
+    // Complex methods
+    // ************************************************************************
+
+    public VrpVehicle getVehicle() {
+        // HACK TODO Invent a system like DependentPlanningVariable or PlanningVariableListener to cope with this
+        VrpAppearance firstAppearance = getPreviousAppearance();
+        while (firstAppearance instanceof VrpCustomer) {
+            firstAppearance = ((VrpCustomer) firstAppearance).getPreviousAppearance();
+        }
+        return (VrpVehicle) firstAppearance;
+    }
+
+    public int getDistanceToPreviousAppearance() {
+        if (previousAppearance == null) {
+            return 0;
+        }
+        return getDistanceTo(previousAppearance);
+    }
+
+    public int getDistanceTo(VrpAppearance appearance) {
+        return location.getDistance(appearance.getLocation());
+    }
+
+    /**
+     * Warning: previous and next do not point to new clones.
+     * @return never null
+     */
+    public VrpCustomer clone() {
+        VrpCustomer clone = new VrpCustomer();
+        clone.id = id;
+        clone.location = location;
+        clone.previousAppearance = previousAppearance;
+        return clone;
+    }
+
+    /**
+     * The normal methods {@link #equals(Object)} and {@link #hashCode()} cannot be used because the rule engine already
+     * requires them (for performance in their original state).
+     * @see #solutionHashCode()
+     */
+    public boolean solutionEquals(Object o) {
+        if (this == o) {
+            return true;
+        } else if (o instanceof VrpCustomer) {
+            VrpCustomer other = (VrpCustomer) o;
+            return new EqualsBuilder()
+                    .append(id, other.id)
+                    .append(location, other.location) // TODO performance leak: not needed?
+                    .append(previousAppearance, other.previousAppearance) // TODO performance leak: not needed?
+                    .isEquals();
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * The normal methods {@link #equals(Object)} and {@link #hashCode()} cannot be used because the rule engine already
+     * requires them (for performance in their original state).
+     * @see #solutionEquals(Object)
+     */
+    public int solutionHashCode() {
+        return new HashCodeBuilder()
+                .append(id)
+                .append(location) // TODO performance leak: not needed?
+                .append(previousAppearance) // TODO performance leak: not needed?
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return location + "(after " + (previousAppearance == null ? "null" : previousAppearance.getLocation()) + ")";
+    }
+
+}
---
+++ drools-planner-examples/src/main/java/org/drools/planner/examples/vehiclerouting/domain/VrpLocation.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2012 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.planner.examples.vehiclerouting.domain;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import org.drools.planner.examples.common.domain.AbstractPersistable;
+
+@XStreamAlias("VrpLocation")
+public class VrpLocation extends AbstractPersistable {
+
+    private String name = null;
+    private double latitude;
+    private double longitude;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    // ************************************************************************
+    // Complex methods
+    // ************************************************************************
+
+    /**
+     * The distance is not in miles or km, but in the TSPLIB's unit of measurement.
+     * @param location never null
+     * @return a positive number
+     */
+    public int getDistance(VrpLocation location) {
+        // Implementation specified by TSPLIB http://www2.iwr.uni-heidelberg.de/groups/comopt/software/TSPLIB95/
+        // Euclidean distance (Pythagorean theorem) - not correct when the surface is a sphere
+        double latitudeDifference = Math.abs(location.latitude - latitude);
+        double longitudeDifference = Math.abs(location.longitude - longitude);
+        double distance = Math.sqrt(
+                (latitudeDifference * latitudeDifference) + (longitudeDifference * longitudeDifference));
+        return (int) (distance + 0.5);
+    }
+
+    @Override
+    public String toString() {
+        if (name == null) {
+            return id.toString();
+        }
+        return id.toString() + "-" + name;
+    }
+
+    public String getSafeName() {
+        if (name == null) {
+            return id.toString();
+        }
+        return name;
+    }
+
+}
