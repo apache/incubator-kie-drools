40043a78df3e9d5357a99433a3e7d0295d4a3fe7
esteban-aliverti <esteban.aliverti@gmail.com>
2012-02-28 22:28:46 +0100

    - Added implementations for ResourceDescriptor and CompositeResourceDescriptor

---
+++ drools-core/src/main/java/org/drools/io/impl/ResourceDescriptorImpl.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2012 JBoss by Red Hat.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.drools.io.impl;
+
+import java.io.Serializable;
+import java.net.URL;
+import java.util.Date;
+import java.util.Set;
+import org.drools.io.ResourceDescriptor;
+
+/**
+ * Agnostic description of a Resource
+ */
+public class ResourceDescriptorImpl implements ResourceDescriptor, Serializable{
+    private String type;
+    private URL resourceURL;
+    private String version;
+    private String status;
+    private String name;
+    private String description;
+    private String id;
+    private URL documentation;
+    private Set<String> categories;
+    private String author;
+    private Date creationTime;
+    private Date lastModificationTime;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Set<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<String> categories) {
+        this.categories = categories;
+    }
+
+    public Date getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(Date creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public URL getDocumentation() {
+        return documentation;
+    }
+
+    public void setDocumentation(URL documentation) {
+        this.documentation = documentation;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getLastModificationTime() {
+        return lastModificationTime;
+    }
+
+    public void setLastModificationTime(Date lastModificationTime) {
+        this.lastModificationTime = lastModificationTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public URL getResourceURL() {
+        return resourceURL;
+    }
+
+    public void setResourceURL(URL resourceURL) {
+        this.resourceURL = resourceURL;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+    
+}
