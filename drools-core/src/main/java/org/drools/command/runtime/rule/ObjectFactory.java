a16078f3435bebe090f2010599fa9540a8f235ab
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2012-03-01 13:55:19 +0100

    register all jaxb'ed commands

--- drools-core/src/main/java/org/drools/command/runtime/rule/ObjectFactory.java
+++ drools-core/src/main/java/org/drools/command/runtime/rule/ObjectFactory.java
@@ -29,10 +29,15 @@ import org.drools.command.runtime.rule.ModifyCommand.SetterImpl;
 
 @XmlRegistry
 public class ObjectFactory {
+
     public FireAllRulesCommand createFireAllRulesCommand() {
         return new FireAllRulesCommand();
     }
     
+    public GetObjectCommand createGetObjectCommand() {
+        return new GetObjectCommand();
+    }
+
     public GetObjectsCommand createGetObjectsCommand() {
         return new GetObjectsCommand();
     }
@@ -44,9 +49,17 @@ public class ObjectFactory {
     public InsertObjectCommand createInsertObjectCommand() {
         return new InsertObjectCommand();
     }
+
+    public InsertObjectInEntryPointCommand createInsertObjectInEntryPointCommand() {
+        return new InsertObjectInEntryPointCommand();
+    }
     
     public ModifyCommand createModifyCommand() {
         return new ModifyCommand();
+    }
+
+    public SetterImpl createModifyCommand$SetterImpl() {
+        return new SetterImpl();
     }
     
     public QueryCommand createQueryCommand() {
@@ -56,8 +69,5 @@ public class ObjectFactory {
     public RetractCommand createRetractCommand() {
         return new RetractCommand();
     }
-    
-    public SetterImpl createModifyCommand$SetterImpl() {
-        return new SetterImpl();
-    }
+
 }
