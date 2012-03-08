369ec1510d11f18de4f85a9d5d7f31e312577d04
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2012-03-08 12:05:11 +0100

    GenericChainedChangeMoveTest.oldAndNewTrailing

--- drools-planner-core/src/test/java/org/drools/planner/core/move/generic/GenericChainedChangeMoveTest.java
+++ drools-planner-core/src/test/java/org/drools/planner/core/move/generic/GenericChainedChangeMoveTest.java
@@ -23,25 +23,65 @@ public class GenericChainedChangeMoveTest {
         a2.setChainedObject(a1);
         TestdataChainedEntity a3 = new TestdataChainedEntity("a3");
         a3.setChainedObject(a2);
-        FactHandle a3FactHandle = mock(FactHandle.class);
 
         TestdataChainedAnchor b0 = new TestdataChainedAnchor("b0");
         TestdataChainedEntity b1 = new TestdataChainedEntity("b1");
         b1.setChainedObject(b0);
 
         PlanningEntityDescriptor entityDescriptor = TestdataChainedEntity.buildEntityDescriptor();
-        GenericChainedChangeMove move = new GenericChainedChangeMove(a3,
-                a3FactHandle, entityDescriptor.getPlanningVariableDescriptor("chainedObject"), b1, null, null, null, null);
-
+        PlanningVariableDescriptor variableDescriptor = entityDescriptor.getPlanningVariableDescriptor("chainedObject");
         WorkingMemory workingMemory = mock(WorkingMemory.class);
+        FactHandle a3FactHandle = mock(FactHandle.class);
+
+        GenericChainedChangeMove move = new GenericChainedChangeMove(a3, a3FactHandle,
+                variableDescriptor, b1, null, null, null, null);
         move.doMove(workingMemory);
 
         assertEquals(a0, a1.getChainedObject());
         assertEquals(a1, a2.getChainedObject());
+
         assertEquals(b0, b1.getChainedObject());
         assertEquals(b1, a3.getChainedObject());
 
         verify(workingMemory).update(a3FactHandle, a3);
+        verifyNoMoreInteractions(workingMemory);
+    }
+
+    @Test
+    public void oldAndNewTrailing() {
+        TestdataChainedAnchor a0 = new TestdataChainedAnchor("a0");
+        TestdataChainedEntity a1 = new TestdataChainedEntity("a1");
+        a1.setChainedObject(a0);
+        TestdataChainedEntity a2 = new TestdataChainedEntity("a2");
+        a2.setChainedObject(a1);
+        TestdataChainedEntity a3 = new TestdataChainedEntity("a3");
+        a3.setChainedObject(a2);
+
+        TestdataChainedAnchor b0 = new TestdataChainedAnchor("b0");
+        TestdataChainedEntity b1 = new TestdataChainedEntity("b1");
+        b1.setChainedObject(b0);
+
+        PlanningEntityDescriptor entityDescriptor = TestdataChainedEntity.buildEntityDescriptor();
+        PlanningVariableDescriptor variableDescriptor = entityDescriptor.getPlanningVariableDescriptor("chainedObject");
+        WorkingMemory workingMemory = mock(WorkingMemory.class);
+        FactHandle a2FactHandle = mock(FactHandle.class);
+        FactHandle a3FactHandle = mock(FactHandle.class);
+        FactHandle b1FactHandle = mock(FactHandle.class);
+
+        GenericChainedChangeMove move = new GenericChainedChangeMove(a2, a2FactHandle,
+                variableDescriptor, b0, a3, a3FactHandle, b1, b1FactHandle);
+        move.doMove(workingMemory);
+
+        assertEquals(a0, a1.getChainedObject());
+        assertEquals(a1, a3.getChainedObject());
+
+        assertEquals(b0, a2.getChainedObject());
+        assertEquals(a2, b1.getChainedObject());
+
+        verify(workingMemory).update(a2FactHandle, a2);
+        verify(workingMemory).update(a3FactHandle, a3);
+        verify(workingMemory).update(b1FactHandle, b1);
+        verifyNoMoreInteractions(workingMemory);
     }
 
 }
