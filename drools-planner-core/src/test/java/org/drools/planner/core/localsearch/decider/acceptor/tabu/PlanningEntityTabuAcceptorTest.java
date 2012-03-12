203c30e4c3bfa28e8e2a3d91ac2f070730fabf49
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2012-03-12 19:59:06 +0100

    JBRULES-3007 testcase (ignored for now)

--- drools-planner-core/src/test/java/org/drools/planner/core/localsearch/decider/acceptor/tabu/PlanningEntityTabuAcceptorTest.java
+++ drools-planner-core/src/test/java/org/drools/planner/core/localsearch/decider/acceptor/tabu/PlanningEntityTabuAcceptorTest.java
@@ -5,12 +5,18 @@ import org.drools.planner.core.domain.variable.PlanningVariableDescriptor;
 import org.drools.planner.core.localsearch.LocalSearchSolverPhaseScope;
 import org.drools.planner.core.localsearch.LocalSearchStepScope;
 import org.drools.planner.core.localsearch.decider.MoveScope;
+import org.drools.planner.core.move.Move;
 import org.drools.planner.core.move.generic.GenericChangeMove;
 import org.drools.planner.core.score.buildin.simple.DefaultSimpleScore;
 import org.drools.planner.core.solver.DefaultSolverScope;
 import org.drools.planner.core.testdata.domain.TestdataEntity;
 import org.drools.planner.core.testdata.domain.TestdataValue;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -98,14 +104,116 @@ public class PlanningEntityTabuAcceptorTest {
         tabuAcceptor.phaseEnded(solverPhaseScope);
     }
 
-    private MoveScope buildMoveScope(LocalSearchStepScope stepScope, TestdataEntity entity) {
-        return buildMoveScope(stepScope, entity, 0);
+    @Test @Ignore("FIXME JBRULES-3007")
+    public void tabuSizeMultipleEntitiesPerStep() {
+        PlanningEntityTabuAcceptor tabuAcceptor = new PlanningEntityTabuAcceptor();
+        tabuAcceptor.setTabuSize(2);
+        tabuAcceptor.setAspirationEnabled(true);
+
+        TestdataEntity e0 = new TestdataEntity("e0");
+        TestdataEntity e1 = new TestdataEntity("e1");
+        TestdataEntity e2 = new TestdataEntity("e2");
+        TestdataEntity e3 = new TestdataEntity("e3");
+        TestdataEntity e4 = new TestdataEntity("e4");
+
+        DefaultSolverScope solverScope = new DefaultSolverScope();
+        solverScope.setBestScore(new DefaultSimpleScore(0));
+        LocalSearchSolverPhaseScope solverPhaseScope = new LocalSearchSolverPhaseScope(solverScope);
+        tabuAcceptor.phaseStarted(solverPhaseScope);
+
+        // Nothing tabu
+        LocalSearchStepScope stepScope0 = new LocalSearchStepScope(solverPhaseScope);
+        stepScope0.setStepIndex(0);
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope0, e0)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope0, e1)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope0, e2)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope0, e3)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope0, e4)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope0, e0, e1)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope0, e0, e2)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope0, e0, e3)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope0, e0, e4)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope0, e1, e2)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope0, e1, e3)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope0, e1, e4)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope0, e2, e3)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope0, e2, e4)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope0, e3, e4)));
+        stepScope0.setStep(buildMoveScope(stepScope0, e0, e2).getMove());
+        tabuAcceptor.stepTaken(stepScope0);
+
+        LocalSearchStepScope stepScope1 = new LocalSearchStepScope(solverPhaseScope);
+        stepScope1.setStepIndex(1);
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope1, e0)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope1, e1)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope1, e2)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope1, e3)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope1, e4)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope1, e0, e1)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope1, e0, e2)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope1, e0, e3)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope1, e0, e4)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope1, e1, e2)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope1, e1, e3)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope1, e1, e4)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope1, e2, e3)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope1, e2, e4)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope1, e3, e4)));
+        stepScope1.setStep(buildMoveScope(stepScope1, e1).getMove());
+        tabuAcceptor.stepTaken(stepScope1);
+
+        LocalSearchStepScope stepScope2 = new LocalSearchStepScope(solverPhaseScope);
+        stepScope2.setStepIndex(2);
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope2, e0)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope2, e1)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope2, e2)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope2, e3)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope2, e4)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope2, e0, e1)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope2, e0, e2)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope2, e0, e3)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope2, e0, e4)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope2, e1, e2)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope2, e1, e3)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope2, e1, e4)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope2, e2, e3)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope2, e2, e4)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope2, e3, e4)));
+        stepScope2.setStep(buildMoveScope(stepScope2, e3, e4).getMove());
+        tabuAcceptor.stepTaken(stepScope2);
+
+        LocalSearchStepScope stepScope3 = new LocalSearchStepScope(solverPhaseScope);
+        stepScope3.setStepIndex(2);
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope3, e0)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope3, e1)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope3, e2)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope3, e3)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope3, e4)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope3, e0, e1)));
+        assertEquals(true, tabuAcceptor.isAccepted(buildMoveScope(stepScope3, e0, e2)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope3, e0, e3)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope3, e0, e4)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope3, e1, e2)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope3, e1, e3)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope3, e1, e4)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope3, e2, e3)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope3, e2, e4)));
+        assertEquals(false, tabuAcceptor.isAccepted(buildMoveScope(stepScope3, e3, e4)));
+        stepScope3.setStep(buildMoveScope(stepScope3, e0).getMove());
+        tabuAcceptor.stepTaken(stepScope3);
+
+        tabuAcceptor.phaseEnded(solverPhaseScope);
     }
 
-    private MoveScope buildMoveScope(LocalSearchStepScope stepScope, TestdataEntity entity, int score) {
+    private MoveScope buildMoveScope(LocalSearchStepScope stepScope, TestdataEntity... entities) {
+        return buildMoveScope(stepScope, 0, entities);
+    }
+
+    private MoveScope buildMoveScope(LocalSearchStepScope stepScope, int score, TestdataEntity... entities) {
         MoveScope moveScope = new MoveScope(stepScope);
-        moveScope.setMove(new GenericChangeMove(entity, mock(FactHandle.class), mock(PlanningVariableDescriptor.class),
-                new TestdataValue("mock")));
+        Move move = mock(Move.class);
+        when(move.getPlanningEntities()).thenReturn((Collection) Arrays.asList(entities));
+        moveScope.setMove(move);
         moveScope.setScore(new DefaultSimpleScore(score));
         return moveScope;
     }
