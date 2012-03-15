ed0bf88fc9af0994fb44ba844190670e000ab351
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2012-03-15 16:16:56 +0100

    typo

--- knowledge-api/src/main/java/org/drools/runtime/ExecutionResults.java
+++ knowledge-api/src/main/java/org/drools/runtime/ExecutionResults.java
@@ -20,7 +20,7 @@ import java.util.Collection;
 
 /**
  * <p>
- * Contains the results for the BatchExecution Command. If the identifier is reference the results of a query, you'll need to cast the vlaue to
+ * Contains the results for the BatchExecution Command. If the identifier is reference the results of a query, you'll need to cast the value to
  * QueryResults.
  * </p>
  */
