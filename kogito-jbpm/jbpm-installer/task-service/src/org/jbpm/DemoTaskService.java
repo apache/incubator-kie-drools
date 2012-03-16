5f3fa2212c9c41724f45779c4d6d6e3782b8a0ba
Marco Rietveld <mrietvel@redhat.com>
2012-03-16 14:12:11 +0100

    Cleaning up DemoTaskService (use of TaskService.eval())

--- kogito-jbpm/jbpm-installer/task-service/src/org/jbpm/DemoTaskService.java
+++ kogito-jbpm/jbpm-installer/task-service/src/org/jbpm/DemoTaskService.java
@@ -11,34 +11,13 @@ import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.jbpm.task.AccessType;
-import org.jbpm.task.AllowedToDelegate;
-import org.jbpm.task.Attachment;
-import org.jbpm.task.BooleanExpression;
-import org.jbpm.task.Comment;
-import org.jbpm.task.Deadline;
-import org.jbpm.task.Deadlines;
-import org.jbpm.task.Delegation;
-import org.jbpm.task.Escalation;
+import org.drools.SystemEventListenerFactory;
 import org.jbpm.task.Group;
-import org.jbpm.task.I18NText;
-import org.jbpm.task.Notification;
-import org.jbpm.task.OrganizationalEntity;
-import org.jbpm.task.PeopleAssignments;
-import org.jbpm.task.Reassignment;
-import org.jbpm.task.Status;
-import org.jbpm.task.Task;
-import org.jbpm.task.TaskData;
 import org.jbpm.task.User;
-import org.jbpm.task.query.TaskSummary;
 import org.jbpm.task.service.TaskService;
 import org.jbpm.task.service.TaskServiceSession;
 import org.jbpm.task.service.UserGroupCallbackManager;
 import org.jbpm.task.service.mina.MinaTaskServer;
-import org.drools.SystemEventListenerFactory;
-import org.mvel2.MVEL;
-import org.mvel2.ParserContext;
-import org.mvel2.compiler.ExpressionCompiler;
 
 public class DemoTaskService {
 	
@@ -52,7 +31,7 @@ public class DemoTaskService {
         if(usersin != null) {
         	Reader reader = new InputStreamReader( usersin );   
         	@SuppressWarnings("unchecked")
-        	Map<String, User> users = ( Map<String, User> ) eval( reader, vars );   
+        	Map<String, User> users = ( Map<String, User> ) TaskService.eval( reader, vars );   
         	for ( User user : users.values() ) {
         		taskSession.addUser( user );
         	}           
@@ -61,7 +40,7 @@ public class DemoTaskService {
         if(groupsin != null) {
         	Reader reader = new InputStreamReader( groupsin );   
         	@SuppressWarnings("unchecked")
-        	Map<String, Group> groups = ( Map<String, Group> ) eval( reader, vars );     
+        	Map<String, Group> groups = ( Map<String, Group> ) TaskService.eval( reader, vars );     
         	for ( Group group : groups.values() ) {
         		taskSession.addGroup( group );
         	}
@@ -69,10 +48,10 @@ public class DemoTaskService {
         // try to get the usergroup callback properties
         InputStream usergroupsin = DemoTaskService.class.getResourceAsStream(  "jbpm.usergroup.callback.properties" );
         if(usergroupsin != null) {
-        	Reader reader = new InputStreamReader( usergroupsin );
         	Properties callbackproperties = new Properties();
         	try {
-        		callbackproperties.load(reader);
+        	    // Properties.load(Reader) is a JDK 6 method
+        		callbackproperties.load(usergroupsin);
         		UserGroupCallbackManager.getInstance().setCallbackFromProperties(callbackproperties);
         		System.out.println("Task service registered usergroup callback ...");
         	} catch (Exception e) {
@@ -88,14 +67,6 @@ public class DemoTaskService {
         System.out.println("Task service running ...");
     }
 
-    public static Object eval(Reader reader, Map vars) {
-        try {
-            return eval( readerToString( reader ), vars );
-        } catch ( IOException e ) {
-            throw new RuntimeException( "Exception Thrown", e );
-        }
-    }
-    
     public static String readerToString(Reader reader) throws IOException {
         int charValue = 0;
         StringBuffer sb = new StringBuffer( 1024 );
@@ -104,37 +75,6 @@ public class DemoTaskService {
             sb.append( (char) charValue );
         }
         return sb.toString();
-    }
-
-    public static Object eval(String str, Map vars) {
-        ExpressionCompiler compiler = new ExpressionCompiler( str.trim() );
-
-        ParserContext context = new ParserContext();
-        context.addPackageImport( "org.jbpm.task" );
-        context.addPackageImport( "java.util" );
-        
-        context.addImport( "AccessType", AccessType.class );
-        context.addImport( "AllowedToDelegate", AllowedToDelegate.class );
-        context.addImport( "Attachment", Attachment.class );
-        context.addImport( "BooleanExpression", BooleanExpression.class );
-        context.addImport( "Comment", Comment.class );
-        context.addImport( "Deadline", Deadline.class );
-        context.addImport( "Deadlines", Deadlines.class );
-        context.addImport( "Delegation", Delegation.class );
-        context.addImport( "Escalation", Escalation.class );
-        context.addImport( "Group", Group.class );
-        context.addImport( "I18NText", I18NText.class );
-        context.addImport( "Notification", Notification.class );
-        context.addImport( "OrganizationalEntity", OrganizationalEntity.class );
-        context.addImport( "PeopleAssignments", PeopleAssignments.class );
-        context.addImport( "Reassignment", Reassignment.class );
-        context.addImport( "Status", Status.class );
-        context.addImport( "Task", Task.class );
-        context.addImport( "TaskData", TaskData.class );
-        context.addImport( "TaskSummary", TaskSummary.class );
-        context.addImport( "User", User.class );
-
-        return MVEL.executeExpression( compiler.compile( context ), vars );
     }
 
 }
