2eafee0b309b722b04540e5d0cd99eafd3655e4e
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2012-04-01 21:35:47 +0200

    IncrementalScoreDirector => prototype in cloud balance

---
+++ drools-planner-examples/src/main/java/org/drools/planner/examples/cloudbalancing/solver/score/CloudBalancingIncrementalScoreCalculator.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2012 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.planner.examples.cloudbalancing.solver.score;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.drools.planner.core.score.Score;
+import org.drools.planner.core.score.buildin.hardandsoft.DefaultHardAndSoftScore;
+import org.drools.planner.core.score.director.incremental.AbstractIncrementalScoreCalculator;
+import org.drools.planner.core.score.director.simple.SimpleScoreCalculator;
+import org.drools.planner.core.solution.Solution;
+import org.drools.planner.examples.cloudbalancing.domain.CloudBalance;
+import org.drools.planner.examples.cloudbalancing.domain.CloudComputer;
+import org.drools.planner.examples.cloudbalancing.domain.CloudProcess;
+
+public class CloudBalancingIncrementalScoreCalculator extends AbstractIncrementalScoreCalculator<CloudBalance> {
+
+    private Map<CloudComputer, Integer> cpuPowerUsageMap;
+    private Map<CloudComputer, Integer> memoryUsageMap;
+    private Map<CloudComputer, Integer> networkBandwidthUsageMap;
+    private Map<CloudComputer, Integer> processCountMap;
+    
+    private int hardScore;
+    private int softScore;
+
+    public void resetWorkingSolution(CloudBalance cloudBalance) {
+        int computerListSize = cloudBalance.getComputerList().size();
+        cpuPowerUsageMap = new HashMap<CloudComputer, Integer>(computerListSize);
+        memoryUsageMap = new HashMap<CloudComputer, Integer>(computerListSize);
+        networkBandwidthUsageMap = new HashMap<CloudComputer, Integer>(computerListSize);
+        processCountMap = new HashMap<CloudComputer, Integer>(computerListSize);
+        for (CloudComputer computer : cloudBalance.getComputerList()) {
+            cpuPowerUsageMap.put(computer, 0);
+            memoryUsageMap.put(computer, 0);
+            networkBandwidthUsageMap.put(computer, 0);
+            processCountMap.put(computer, 0);
+        }
+        hardScore = 0;
+        softScore = 0;
+        for (CloudProcess process : cloudBalance.getProcessList()) {
+            insert(process);
+        }
+    }
+
+    public void beforeEntityAdded(Object entity) {
+        // Do nothing
+    }
+
+    public void afterEntityAdded(Object entity) {
+        insert((CloudProcess) entity);
+    }
+
+    public void beforeAllVariablesChanged(Object entity) {
+        retract((CloudProcess) entity);
+    }
+
+    public void afterAllVariablesChanged(Object entity) {
+        insert((CloudProcess) entity);
+    }
+
+    public void beforeVariableChanged(Object entity, String variableName) {
+        retract((CloudProcess) entity);
+    }
+
+    public void afterVariableChanged(Object entity, String variableName) {
+        insert((CloudProcess) entity);
+    }
+
+    public void beforeEntityRemoved(Object entity) {
+        retract((CloudProcess) entity);
+    }
+
+    public void afterEntityRemoved(Object entity) {
+        // Do nothing
+    }
+
+    private void insert(CloudProcess process) {
+        CloudComputer computer = process.getComputer();
+        if (computer != null) {
+            int cpuPower = computer.getCpuPower();
+            int oldCpuPowerUsage = cpuPowerUsageMap.get(computer);
+            int oldCpuPowerAvailable = cpuPower - oldCpuPowerUsage;
+            int newCpuPowerUsage = oldCpuPowerUsage + process.getRequiredCpuPower();
+            int newCpuPowerAvailable = cpuPower - newCpuPowerUsage;
+            hardScore += Math.min(newCpuPowerAvailable, 0) - Math.min(oldCpuPowerAvailable, 0);
+            cpuPowerUsageMap.put(computer, newCpuPowerUsage);
+            
+            int memory = computer.getMemory();
+            int oldMemoryUsage = memoryUsageMap.get(computer);
+            int oldMemoryAvailable = memory - oldMemoryUsage;
+            int newMemoryUsage = oldMemoryUsage + process.getRequiredMemory();
+            int newMemoryAvailable = memory - newMemoryUsage;
+            hardScore += Math.min(newMemoryAvailable, 0) - Math.min(oldMemoryAvailable, 0);
+            memoryUsageMap.put(computer, newMemoryUsage);
+            
+            int networkBandwidth = computer.getNetworkBandwidth();
+            int oldNetworkBandwidthUsage = networkBandwidthUsageMap.get(computer);
+            int oldNetworkBandwidthAvailable = networkBandwidth - oldNetworkBandwidthUsage;
+            int newNetworkBandwidthUsage = oldNetworkBandwidthUsage + process.getRequiredNetworkBandwidth();
+            int newNetworkBandwidthAvailable = networkBandwidth - newNetworkBandwidthUsage;
+            hardScore += Math.min(newNetworkBandwidthAvailable, 0) - Math.min(oldNetworkBandwidthAvailable, 0);
+            networkBandwidthUsageMap.put(computer, newNetworkBandwidthUsage);
+
+            int oldProcessCount = processCountMap.get(computer);
+            if (oldProcessCount == 0) {
+                softScore -= computer.getCost();
+            }
+            int newProcessCount = oldProcessCount + 1;
+            processCountMap.put(computer, newProcessCount);
+        }
+    }
+
+    private void retract(CloudProcess process) {
+        CloudComputer computer = process.getComputer();
+        if (computer != null) {
+            int cpuPower = computer.getCpuPower();
+            int oldCpuPowerUsage = cpuPowerUsageMap.get(computer);
+            int oldCpuPowerAvailable = cpuPower - oldCpuPowerUsage;
+            int newCpuPowerUsage = oldCpuPowerUsage - process.getRequiredCpuPower();
+            int newCpuPowerAvailable = cpuPower - newCpuPowerUsage;
+            hardScore += Math.min(newCpuPowerAvailable, 0) - Math.min(oldCpuPowerAvailable, 0);
+            cpuPowerUsageMap.put(computer, newCpuPowerUsage);
+
+            int memory = computer.getMemory();
+            int oldMemoryUsage = memoryUsageMap.get(computer);
+            int oldMemoryAvailable = memory - oldMemoryUsage;
+            int newMemoryUsage = oldMemoryUsage - process.getRequiredMemory();
+            int newMemoryAvailable = memory - newMemoryUsage;
+            hardScore += Math.min(newMemoryAvailable, 0) - Math.min(oldMemoryAvailable, 0);
+            memoryUsageMap.put(computer, newMemoryUsage);
+
+            int networkBandwidth = computer.getNetworkBandwidth();
+            int oldNetworkBandwidthUsage = networkBandwidthUsageMap.get(computer);
+            int oldNetworkBandwidthAvailable = networkBandwidth - oldNetworkBandwidthUsage;
+            int newNetworkBandwidthUsage = oldNetworkBandwidthUsage - process.getRequiredNetworkBandwidth();
+            int newNetworkBandwidthAvailable = networkBandwidth - newNetworkBandwidthUsage;
+            hardScore += Math.min(newNetworkBandwidthAvailable, 0) - Math.min(oldNetworkBandwidthAvailable, 0);
+            networkBandwidthUsageMap.put(computer, newNetworkBandwidthUsage);
+
+            int oldProcessCount = processCountMap.get(computer);
+            int newProcessCount = oldProcessCount - 1;
+            if (newProcessCount == 0) {
+                softScore += computer.getCost();
+            }
+            processCountMap.put(computer, newProcessCount);
+if (hardScore > 0 || softScore > 0) {
+    throw new IllegalArgumentException();
+}
+        }
+    }
+
+    public Score calculateScore(CloudBalance cloudBalance) {
+        return DefaultHardAndSoftScore.valueOf(hardScore, softScore);
+    }
+
+}
