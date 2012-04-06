763a509ec26d9a8035ac9442fabf02f8d2a7ac77
mariofusco <mario.fusco@gmail.com>
2012-04-06 10:50:07 +0200

    [JBRULES-3436] fix

--- kogito-drools/drools-compiler/src/test/java/org/drools/integrationtests/MiscTest.java
+++ kogito-drools/drools-compiler/src/test/java/org/drools/integrationtests/MiscTest.java
@@ -10753,4 +10753,22 @@ public class MiscTest extends CommonTestMethodBase {
 
         assertTrue(kbuilder.hasErrors());
     }
+
+    @Test
+    public void testMissingClosingBraceOnModify() throws Exception {
+        // JBRULES-3436
+        String str = "package org.drools.test;\n" +
+                "import org.drools.*\n" +
+                "rule R1 when\n" +
+                "   $p : Person( )" +
+                "   $c : Cheese( )" +
+                "then\n" +
+                "   modify($p) { setCheese($c) ;\n" +
+                "end\n";
+
+        KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+        kbuilder.add( ResourceFactory.newByteArrayResource(str.getBytes()), ResourceType.DRL );
+
+        assertTrue( kbuilder.hasErrors() );
+    }
 }
