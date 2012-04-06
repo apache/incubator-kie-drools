f8c593f588a7ab6a795478f1741943fa1165670d
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2012-04-06 18:00:56 +0200

    Use FIRST_FIT_DECREASING in VehicleRouting (much much better!)

--- drools-planner-examples/src/main/java/org/drools/planner/examples/vehiclerouting/domain/VrpCustomer.java
+++ drools-planner-examples/src/main/java/org/drools/planner/examples/vehiclerouting/domain/VrpCustomer.java
@@ -25,8 +25,9 @@ import org.drools.planner.api.domain.variable.ValueRange;
 import org.drools.planner.api.domain.variable.ValueRangeType;
 import org.drools.planner.api.domain.variable.ValueRanges;
 import org.drools.planner.examples.common.domain.AbstractPersistable;
+import org.drools.planner.examples.vehiclerouting.domain.solver.VrpCustomerDifficultyComparator;
 
-@PlanningEntity
+@PlanningEntity(difficultyComparatorClass = VrpCustomerDifficultyComparator.class)
 @XStreamAlias("VrpCustomer")
 public class VrpCustomer extends AbstractPersistable implements VrpAppearance {
 
---
+++ drools-planner-examples/src/main/java/org/drools/planner/examples/vehiclerouting/domain/solver/VrpCustomerDifficultyComparator.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2012 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.planner.examples.vehiclerouting.domain.solver;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+import org.apache.commons.lang.builder.CompareToBuilder;
+import org.drools.planner.examples.cloudbalancing.domain.CloudProcess;
+import org.drools.planner.examples.vehiclerouting.domain.VrpCustomer;
+
+public class VrpCustomerDifficultyComparator implements Comparator<VrpCustomer>, Serializable {
+
+    public int compare(VrpCustomer a, VrpCustomer b) {
+        return new CompareToBuilder()
+                // TODO experiment with (aLatitude - bLatitude) % 10
+                .append(a.getLocation().getLatitude(), b.getLocation().getLatitude())
+                .append(a.getLocation().getLongitude(), b.getLocation().getLongitude())
+                .append(a.getDemand(), b.getDemand())
+                .append(a.getId(), b.getId())
+                .toComparison();
+    }
+
+}
