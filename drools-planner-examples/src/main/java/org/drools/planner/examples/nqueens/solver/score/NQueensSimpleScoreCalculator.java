97a2a2f1a02e07bf987f17afb49a33460196a119
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2012-04-07 13:36:40 +0200

    nqueens: simpleScoreCalculatorClass

---
+++ drools-planner-examples/src/main/java/org/drools/planner/examples/nqueens/solver/score/NQueensSimpleScoreCalculator.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2012 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.planner.examples.nqueens.solver.score;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.drools.planner.core.score.Score;
+import org.drools.planner.core.score.buildin.hardandsoft.DefaultHardAndSoftScore;
+import org.drools.planner.core.score.buildin.simple.DefaultSimpleScore;
+import org.drools.planner.core.score.director.simple.SimpleScoreCalculator;
+import org.drools.planner.examples.nqueens.domain.NQueens;
+import org.drools.planner.examples.nqueens.domain.Queen;
+
+public class NQueensSimpleScoreCalculator implements SimpleScoreCalculator<NQueens> {
+
+    public Score calculateScore(NQueens nQueens) {
+        int n = nQueens.getN();
+        List<Queen> queenList = nQueens.getQueenList();
+        
+        int score = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                Queen leftQueen = queenList.get(i);
+                Queen rightQueen = queenList.get(j);
+                if (leftQueen.getRow() != null && rightQueen.getRow() != null) {
+                    if (leftQueen.getRowIndex() == rightQueen.getRowIndex()) {
+                        score--;
+                    }
+                    if (leftQueen.getAscendingDiagonalIndex() == rightQueen.getAscendingDiagonalIndex()) {
+                        score--;
+                    }
+                    if (leftQueen.getDescendingDiagonalIndex() == rightQueen.getDescendingDiagonalIndex()) {
+                        score--;
+                    }
+                }
+            }
+        }
+        return DefaultSimpleScore.valueOf(score);
+    }
+
+}
