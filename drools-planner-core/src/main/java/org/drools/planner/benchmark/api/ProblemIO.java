466916d97c277ab5f089abd69363fe167b4617eb
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2012-04-14 12:20:22 +0200

    Improve javadoc to deal with file extension confusion

--- drools-planner-core/src/main/java/org/drools/planner/benchmark/api/ProblemIO.java
+++ drools-planner-core/src/main/java/org/drools/planner/benchmark/api/ProblemIO.java
@@ -20,9 +20,24 @@ import java.io.File;
 
 import org.drools.planner.core.solution.Solution;
 
+/**
+ * Reads or writes a {@link Solution} from or to a {@link File}.
+ */
 public interface ProblemIO {
 
     /**
+     * Every {@link Solution} type potentially has its own file extension.
+     * If no specific file extension is defined by the use case, the following are recommended:
+     * <ul>
+     *     <li>If this {@link ProblemIO} implementation serializes to XML, use file extension "xml".</li>
+     *     <li>If this {@link ProblemIO} implementation serializes to text, use file extension "txt".</li>
+     *     <li>If this {@link ProblemIO} implementation serializes to binary, use file extension "dat".</li>
+     * </ul>
+     * <p/>
+     * It's good practice that both the input and the output file have the same file extension,
+     * because an output file should be able to function as an input file.
+     * If that isn't the case, this method should return the output file extension.
+     * <p/>
      * The file extension does not include the dot that separates it from the base name.
      * @return never null, for example "xml"
      */
