0a425ed26ae925dbf194b0ff00c69f71acae836f
Marco Rietveld <mrietvel@redhat.com>
2012-04-19 04:26:11 +0200

    Typo fixed which kept TransactionSynchronizationRegistry from being used. (cherry picked from commit d6b2da6f88226b2bc9c68146ad71651f82b13cc5)

--- kogito-drools/drools-persistence-jpa/src/main/java/org/drools/persistence/jta/JtaTransactionManager.java
+++ kogito-drools/drools-persistence-jpa/src/main/java/org/drools/persistence/jta/JtaTransactionManager.java
@@ -138,10 +138,10 @@ public class JtaTransactionManager
         String jndiName = DEFAULT_TRANSACTION_SYNCHRONIZATION_REGISTRY_NAME;
         try {
             InitialContext context = new InitialContext();
-            context.lookup( jndiName );
+            Object tsrObject = context.lookup( jndiName );
             logger.debug( "JTA TransactionSynchronizationRegistry found at default JNDI location [{}]",
                           jndiName );
-            return tsr;
+            return tsrObject;
         } catch ( NamingException ex ) {
             logger.debug( "No JTA TransactionSynchronizationRegistry found at default JNDI location [{}]",
                           jndiName,
