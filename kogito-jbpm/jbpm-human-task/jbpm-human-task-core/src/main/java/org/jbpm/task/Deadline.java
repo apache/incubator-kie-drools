5a7214de11b7cf3af51078ff64371a01738a62e5
salaboy <salaboy@gmail.com>
2012-05-05 22:15:57 +0100

    - JBPM-3625: Named query UnescalatedDeadlines fails while running on Oracle  - fixed by changing the boolean values to Short in classes Deadline and Task

--- kogito-jbpm/jbpm-human-task/jbpm-human-task-core/src/main/java/org/jbpm/task/Deadline.java
+++ kogito-jbpm/jbpm-human-task/jbpm-human-task-core/src/main/java/org/jbpm/task/Deadline.java
@@ -1,19 +1,18 @@
 /**
  * Copyright 2010 JBoss Inc
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
-
 package org.jbpm.task;
 
 import java.io.Externalizable;
@@ -24,62 +23,70 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 import org.jbpm.task.utils.CollectionUtils;
 
 @Entity
 public class Deadline implements Externalizable {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)    
-    private long             id;
-
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
     @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "Deadline_Documentation_Id", nullable = true)     
-    private List<I18NText>   documentation = Collections.emptyList();
-
+    @JoinColumn(name = "Deadline_Documentation_Id", nullable = true)
+    private List<I18NText> documentation = Collections.emptyList();
     @Column(name = "deadline_date")
-    private Date             date;
-
+    private Date date;
     @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "Deadline_Escalation_Id", nullable = true)    
+    @JoinColumn(name = "Deadline_Escalation_Id", nullable = true)
     private List<Escalation> escalations = Collections.emptyList();
     
-    private boolean escalated;
-    
-    public void writeExternal(ObjectOutput out) throws IOException {
-        out.writeLong( id );
-        
-        if ( date != null ) {
-            out.writeBoolean( true );
-            out.writeLong( date.getTime() );
-        } else {
-            out.writeBoolean( false );
+    @Basic
+    private Short escalated = 0;
+
+    public Boolean isEscalated() {
+        if (escalated == null) {
+            return null;
         }
-        CollectionUtils.writeI18NTextList( documentation, out );
-        CollectionUtils.writeEscalationList( escalations, out );
-        
-        out.writeBoolean( escalated );
+        return (escalated == 1) ? Boolean.TRUE : Boolean.FALSE;
     }
-    
-    public void readExternal(ObjectInput in) throws IOException,
-                                            ClassNotFoundException {
-        id = in.readLong();
-        
-        if ( in.readBoolean()) {
-            date = new Date( in.readLong() );
+
+    public void setEscalated(Boolean escalated) {
+        if (escalated == null) {
+            this.escalated = null;
+        } else {
+            this.escalated = (escalated == true) ? new Short("1") : new Short("0");
         }
-        documentation = CollectionUtils.readI18NTextList( in );
-        escalations = CollectionUtils.readEscalationList( in );   
+    }
+
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeLong(id);
+
+        if (date != null) {
+            out.writeBoolean(true);
+            out.writeLong(date.getTime());
+        } else {
+            out.writeBoolean(false);
+        }
+        CollectionUtils.writeI18NTextList(documentation, out);
+        CollectionUtils.writeEscalationList(escalations, out);
+
+        out.writeShort(escalated);
         
-        escalated = in.readBoolean();
+    }
+
+    public void readExternal(ObjectInput in) throws IOException,
+            ClassNotFoundException {
+        id = in.readLong();
+
+        if (in.readBoolean()) {
+            date = new Date(in.readLong());
+        }
+        documentation = CollectionUtils.readI18NTextList(in);
+        escalations = CollectionUtils.readEscalationList(in);
+
+        escalated = in.readShort();
     }
 
     public long getId() {
@@ -114,43 +121,44 @@ public class Deadline implements Externalizable {
         this.escalations = escalations;
     }
 
-    public boolean isEscalated() {
-        return escalated;
-    }
-
-    public void setEscalated(boolean escalated) {
-        this.escalated = escalated;
-    }
-
+   
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((date == null) ? 0 : date.hashCode());
-        result = prime * result + CollectionUtils.hashCode( documentation );
-        result = prime * result + CollectionUtils.hashCode( escalations );
-        result = prime * result + (escalated ? 1231 : 1237);
+        result = prime * result + CollectionUtils.hashCode(documentation);
+        result = prime * result + CollectionUtils.hashCode(escalations);
+        result = prime * result + (isEscalated() ? 1231 : 1237);
         result = prime * result + (int) (id ^ (id >>> 32));
         return result;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if ( this == obj ) return true;
-        if ( obj == null ) return false;
-        if ( !(obj instanceof Deadline) ) return false;
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Deadline)) {
+            return false;
+        }
         Deadline other = (Deadline) obj;
-        
-        if ( date == null ) {
-            if ( other.date != null ) return false;
-        } else if ( date.getTime() != other.date.getTime() ) return false;
-        
-        if ( escalated != other.escalated ) return false;                        
-    
-        return CollectionUtils.equals( documentation, other.documentation ) && CollectionUtils.equals( escalations, other.escalations );        
+
+        if (date == null) {
+            if (other.date != null) {
+                return false;
+            }
+        } else if (date.getTime() != other.date.getTime()) {
+            return false;
+        }
+
+        if (isEscalated() != other.isEscalated()) {
+            return false;
+        }
+
+        return CollectionUtils.equals(documentation, other.documentation) && CollectionUtils.equals(escalations, other.escalations);
     }
-
-
-    
-    
 }
