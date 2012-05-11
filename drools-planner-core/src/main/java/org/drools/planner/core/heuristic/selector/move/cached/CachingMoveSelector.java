91b13ef9191c4b0b7bace6374c80073aece9f4fa
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2012-05-11 14:27:19 +0200

    Unified selectors: Selector and MoveSelector interface draft + CachingMoveSelector

---
+++ drools-planner-core/src/main/java/org/drools/planner/core/heuristic/selector/move/cached/CachingMoveSelector.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2012 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.planner.core.heuristic.selector.move.cached;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.drools.planner.core.heuristic.selector.cached.SelectorCacheType;
+import org.drools.planner.core.heuristic.selector.move.AbstractMoveSelector;
+import org.drools.planner.core.heuristic.selector.move.MoveSelector;
+import org.drools.planner.core.move.Move;
+import org.drools.planner.core.phase.AbstractSolverPhaseScope;
+import org.drools.planner.core.phase.step.AbstractStepScope;
+import org.drools.planner.core.solver.DefaultSolverScope;
+
+/**
+ * A {@link MoveSelector} that caches the result of its child {@link MoveSelector}.
+ */
+public class CachingMoveSelector extends AbstractMoveSelector {
+
+    protected final SelectorCacheType cacheType;
+    protected MoveSelector childMoveSelector;
+
+    protected long cachedSize = -1L;
+    protected List<Move> cachedMoveList = null;
+    protected long cachedRandomProbabilityWeight = -1L;
+
+    public CachingMoveSelector(SelectorCacheType cacheType) {
+        this.cacheType = cacheType;
+        if (cacheType != SelectorCacheType.SOLVER && cacheType != SelectorCacheType.PHASE
+                && cacheType != SelectorCacheType.STEP) {
+            throw new IllegalArgumentException("The cacheType (" + cacheType
+                    + ") is not supported on the class (" + getClass().getName() + ").");
+        }
+    }
+
+    public MoveSelector getChildMoveSelector() {
+        return childMoveSelector;
+    }
+
+    public void setChildMoveSelector(MoveSelector childMoveSelector) {
+        this.childMoveSelector = childMoveSelector;
+        if (childMoveSelector.isNeverEnding()) {
+            throw new IllegalStateException("The childMoveSelector (" + childMoveSelector + ") has sizeInfinite ("
+                    + childMoveSelector.isNeverEnding() + ") on a class (" + getClass().getName() + ") instance.");
+        }
+    }
+
+    // ************************************************************************
+    // Cache lifecycle methods
+    // ************************************************************************
+
+    @Override
+    public void solvingStarted(DefaultSolverScope solverScope) {
+        super.solvingStarted(solverScope);
+        if (cacheType == SelectorCacheType.SOLVER) {
+            constructCache(solverScope);
+        }
+    }
+
+    @Override
+    public void phaseStarted(AbstractSolverPhaseScope solverPhaseScope) {
+        super.phaseStarted(solverPhaseScope);
+        if (cacheType == SelectorCacheType.PHASE) {
+            constructCache(solverPhaseScope.getSolverScope());
+        }
+    }
+
+    @Override
+    public void stepStarted(AbstractStepScope stepScope) {
+        super.stepStarted(stepScope);
+        if (cacheType == SelectorCacheType.STEP) {
+            constructCache(stepScope.getSolverPhaseScope().getSolverScope());
+        }
+    }
+
+    @Override
+    public void stepEnded(AbstractStepScope stepScope) {
+        super.stepEnded(stepScope);
+        if (cacheType == SelectorCacheType.STEP) {
+            disposeCache(stepScope.getSolverPhaseScope().getSolverScope());
+        }
+    }
+
+    @Override
+    public void phaseEnded(AbstractSolverPhaseScope solverPhaseScope) {
+        super.phaseEnded(solverPhaseScope);
+        if (cacheType == SelectorCacheType.PHASE) {
+            disposeCache(solverPhaseScope.getSolverScope());
+        }
+    }
+
+    @Override
+    public void solvingEnded(DefaultSolverScope solverScope) {
+        super.solvingEnded(solverScope);
+        if (cacheType == SelectorCacheType.SOLVER) {
+            disposeCache(solverScope);
+        }
+    }
+
+    protected void constructCache(DefaultSolverScope solverScope) {
+        cachedSize = childMoveSelector.getSize();
+        if (cachedSize > (long) Integer.MAX_VALUE) {
+            throw new IllegalStateException("The moveSelector (" + this + ") has a childMoveSelector ("
+                    + childMoveSelector + ") with cachedSize (" + cachedSize
+                    + ") which is higher then Integer.MAX_VALUE.");
+        }
+        cachedMoveList = new ArrayList<Move>((int)cachedSize);
+        CollectionUtils.addAll(cachedMoveList, childMoveSelector.iterator());
+        cachedRandomProbabilityWeight = childMoveSelector.getRandomProbabilityWeight();
+        orderCache(solverScope);
+    }
+
+    protected void orderCache(DefaultSolverScope solverScope) {
+        // Hook method
+    }
+
+    protected void disposeCache(DefaultSolverScope solverScope) {
+        cachedSize = -1L;
+        cachedMoveList = null;
+        cachedRandomProbabilityWeight = -1L;
+    }
+
+    // ************************************************************************
+    // Worker methods
+    // ************************************************************************
+
+    public Iterator<Move> iterator() {
+        return cachedMoveList.iterator();
+    }
+
+    public boolean isContinuous() {
+        return false;
+    }
+
+    public boolean isNeverEnding() {
+        return false;
+    }
+
+    public long getSize() {
+        return cachedSize;
+    }
+
+    public long getRandomProbabilityWeight() {
+        return cachedRandomProbabilityWeight;
+    }
+
+    @Override
+    public String toString() {
+        return "Caching(" + childMoveSelector + ")";
+    }
+
+}
---
+++ drools-planner-core/src/test/java/org/drools/planner/core/heuristic/selector/move/cached/CachingMoveSelectorTest.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2012 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.planner.core.heuristic.selector.move.cached;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.drools.planner.core.heuristic.selector.cached.SelectorCacheType;
+import org.drools.planner.core.heuristic.selector.move.MoveSelector;
+import org.drools.planner.core.move.DummyMove;
+import org.drools.planner.core.move.Move;
+import org.drools.planner.core.phase.AbstractSolverPhaseScope;
+import org.drools.planner.core.phase.step.AbstractStepScope;
+import org.drools.planner.core.solver.DefaultSolverScope;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class CachingMoveSelectorTest {
+
+    @Test
+    public void cacheTypeSolver() {
+        runCacheType(SelectorCacheType.SOLVER, 1);
+    }
+
+    @Test
+    public void cacheTypePhase() {
+        runCacheType(SelectorCacheType.PHASE, 2);
+    }
+
+    @Test
+    public void cacheTypeStep() {
+        runCacheType(SelectorCacheType.STEP, 5);
+    }
+
+    public void runCacheType(SelectorCacheType cacheType, int timesCalled) {
+        CachingMoveSelector moveSelector = new CachingMoveSelector(cacheType);
+        MoveSelector childMoveSelector = mock(MoveSelector.class);
+        final List<Move> moveList = Arrays.<Move>asList(new DummyMove(), new DummyMove());
+        when(childMoveSelector.iterator()).thenAnswer(new Answer<Object>() {
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                return moveList.iterator();
+            }
+        });
+        when(childMoveSelector.isContinuous()).thenReturn(false);
+        when(childMoveSelector.isNeverEnding()).thenReturn(false);
+        when(childMoveSelector.getSize()).thenReturn((long) moveList.size());
+        when(childMoveSelector.getRandomProbabilityWeight()).thenReturn(7L);
+        moveSelector.setChildMoveSelector(childMoveSelector);
+        verify(childMoveSelector, times(1)).isNeverEnding();
+
+        DefaultSolverScope solverScope = mock(DefaultSolverScope.class);
+        moveSelector.solvingStarted(solverScope);
+
+        AbstractSolverPhaseScope phaseScopeA = mock(AbstractSolverPhaseScope.class);
+        when(phaseScopeA.getSolverScope()).thenReturn(solverScope);
+        moveSelector.phaseStarted(phaseScopeA);
+
+        AbstractStepScope stepScopeA1 = mock(AbstractStepScope.class);
+        when(stepScopeA1.getSolverPhaseScope()).thenReturn(phaseScopeA);
+        moveSelector.stepStarted(stepScopeA1);
+        runAsserts(moveSelector);
+        moveSelector.stepEnded(stepScopeA1);
+
+        AbstractStepScope stepScopeA2 = mock(AbstractStepScope.class);
+        when(stepScopeA2.getSolverPhaseScope()).thenReturn(phaseScopeA);
+        moveSelector.stepStarted(stepScopeA2);
+        runAsserts(moveSelector);
+        moveSelector.stepEnded(stepScopeA2);
+
+        moveSelector.phaseEnded(phaseScopeA);
+
+        AbstractSolverPhaseScope phaseScopeB = mock(AbstractSolverPhaseScope.class);
+        when(phaseScopeB.getSolverScope()).thenReturn(solverScope);
+        moveSelector.phaseStarted(phaseScopeB);
+
+        AbstractStepScope stepScopeB1 = mock(AbstractStepScope.class);
+        when(stepScopeB1.getSolverPhaseScope()).thenReturn(phaseScopeB);
+        moveSelector.stepStarted(stepScopeB1);
+        runAsserts(moveSelector);
+        moveSelector.stepEnded(stepScopeB1);
+
+        AbstractStepScope stepScopeB2 = mock(AbstractStepScope.class);
+        when(stepScopeB2.getSolverPhaseScope()).thenReturn(phaseScopeB);
+        moveSelector.stepStarted(stepScopeB2);
+        runAsserts(moveSelector);
+        moveSelector.stepEnded(stepScopeB2);
+
+        AbstractStepScope stepScopeB3 = mock(AbstractStepScope.class);
+        when(stepScopeB3.getSolverPhaseScope()).thenReturn(phaseScopeB);
+        moveSelector.stepStarted(stepScopeB3);
+        runAsserts(moveSelector);
+        moveSelector.stepEnded(stepScopeB3);
+
+        moveSelector.phaseEnded(phaseScopeB);
+
+        moveSelector.solvingEnded(solverScope);
+
+        verify(childMoveSelector, times(timesCalled)).iterator();
+        verify(childMoveSelector, times(timesCalled)).getSize();
+        verify(childMoveSelector, times(timesCalled)).getRandomProbabilityWeight();
+    }
+
+    private void runAsserts(CachingMoveSelector moveSelector) {
+        Iterator<Move> iterator = moveSelector.iterator();
+        assertNotNull(iterator);
+        assertTrue(iterator.hasNext());
+        assertNotNull(iterator.next());
+        assertTrue(iterator.hasNext());
+        assertNotNull(iterator.next());
+        assertFalse(iterator.hasNext());
+        assertEquals(false, moveSelector.isContinuous());
+        assertEquals(false, moveSelector.isNeverEnding());
+        assertEquals(2L, moveSelector.getSize());
+        assertEquals(7L, moveSelector.getRandomProbabilityWeight());
+    }
+
+}
