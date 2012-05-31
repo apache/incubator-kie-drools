96588fa4c4b25871f2e13eb20ae21e7b41bc07f8
mariofusco <mario.fusco@gmail.com>
2012-05-31 19:39:15 +0200

    [JBRULES-3522] consequence metadata for update and insert

--- kogito-drools/drools-compiler/src/main/java/org/drools/lang/dsl/DSLMapWalker.java
+++ kogito-drools/drools-compiler/src/main/java/org/drools/lang/dsl/DSLMapWalker.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.3 Nov 30, 2010 12:46:29 src/main/resources/org/drools/lang/dsl/DSLMapWalker.g 2012-06-01 16:13:26
+// $ANTLR 3.3 Nov 30, 2010 12:46:29 src/main/resources/org/drools/lang/dsl/DSLMapWalker.g 2012-06-01 16:16:17
 
     package org.drools.lang.dsl;
 
