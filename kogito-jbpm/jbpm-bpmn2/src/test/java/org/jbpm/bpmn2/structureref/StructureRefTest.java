2afa34f2d5144d8a77477ad672d60a02120ad761
Maciej Swiderski <mswiders@redhat.com>
2012-09-24 13:06:22 +0200

    JBPM-3779 - added test cases for all supported types

--- kogito-jbpm/jbpm-bpmn2/src/test/java/org/jbpm/bpmn2/structureref/StructureRefTest.java
+++ kogito-jbpm/jbpm-bpmn2/src/test/java/org/jbpm/bpmn2/structureref/StructureRefTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jbpm.bpmn2.structureref;
 
 import java.util.HashMap;
@@ -14,7 +29,7 @@ public class StructureRefTest extends JbpmBpmn2TestCase {
     }
 
     public void testStringStructureRef() {
-        KnowledgeBase kbase = createKnowledgeBase("BPMN2-StructureRef.bpmn2");
+        KnowledgeBase kbase = createKnowledgeBase("BPMN2-StringStructureRef.bpmn2");
         StatefulKnowledgeSession ksession = createKnowledgeSession(kbase);
         TestWorkItemHandler workItemHandler = new TestWorkItemHandler();
         ksession.getWorkItemManager().registerWorkItemHandler("Human Task",
@@ -26,6 +41,78 @@ public class StructureRefTest extends JbpmBpmn2TestCase {
         res.put("testHT", "test value");
         ksession.getWorkItemManager().completeWorkItem(
                 workItemHandler.getWorkItem().getId(), res);
+        
+        assertProcessInstanceCompleted(processInstance.getId(), ksession);
+    }
+    
+    public void testBooleanStructureRef() {
+        KnowledgeBase kbase = createKnowledgeBase("BPMN2-BooleanStructureRef.bpmn2");
+        StatefulKnowledgeSession ksession = createKnowledgeSession(kbase);
+        TestWorkItemHandler workItemHandler = new TestWorkItemHandler();
+        ksession.getWorkItemManager().registerWorkItemHandler("Human Task",
+                workItemHandler);
+        ProcessInstance processInstance = ksession.startProcess("StructureRef");
+        assertTrue(processInstance.getState() == ProcessInstance.STATE_ACTIVE);
+
+        Map<String, Object> res = new HashMap<String, Object>();
+        res.put("testHT", "true");
+        ksession.getWorkItemManager().completeWorkItem(
+                workItemHandler.getWorkItem().getId(), res);
+        
+        assertProcessInstanceCompleted(processInstance.getId(), ksession);
     }
 
+    public void testIntegerStructureRef() {
+        KnowledgeBase kbase = createKnowledgeBase("BPMN2-IntegerStructureRef.bpmn2");
+        StatefulKnowledgeSession ksession = createKnowledgeSession(kbase);
+        TestWorkItemHandler workItemHandler = new TestWorkItemHandler();
+        ksession.getWorkItemManager().registerWorkItemHandler("Human Task",
+                workItemHandler);
+        ProcessInstance processInstance = ksession.startProcess("StructureRef");
+        assertTrue(processInstance.getState() == ProcessInstance.STATE_ACTIVE);
+
+        Map<String, Object> res = new HashMap<String, Object>();
+        res.put("testHT", "25");
+        ksession.getWorkItemManager().completeWorkItem(
+                workItemHandler.getWorkItem().getId(), res);
+        
+        assertProcessInstanceCompleted(processInstance.getId(), ksession);
+    }
+    
+    public void testFloatStructureRef() {
+        KnowledgeBase kbase = createKnowledgeBase("BPMN2-FloatStructureRef.bpmn2");
+        StatefulKnowledgeSession ksession = createKnowledgeSession(kbase);
+        TestWorkItemHandler workItemHandler = new TestWorkItemHandler();
+        ksession.getWorkItemManager().registerWorkItemHandler("Human Task",
+                workItemHandler);
+        ProcessInstance processInstance = ksession.startProcess("StructureRef");
+        assertTrue(processInstance.getState() == ProcessInstance.STATE_ACTIVE);
+
+        Map<String, Object> res = new HashMap<String, Object>();
+        res.put("testHT", "5.5");
+        ksession.getWorkItemManager().completeWorkItem(
+                workItemHandler.getWorkItem().getId(), res);
+        
+        assertProcessInstanceCompleted(processInstance.getId(), ksession);
+    }
+    
+    public void testObjectStructureRef() {
+        
+        String personAsXml = "<org.jbpm.bpmn2.objects.Person><id>1</id><name>john</name></org.jbpm.bpmn2.objects.Person>";
+        
+        KnowledgeBase kbase = createKnowledgeBase("BPMN2-ObjectStructureRef.bpmn2");
+        StatefulKnowledgeSession ksession = createKnowledgeSession(kbase);
+        TestWorkItemHandler workItemHandler = new TestWorkItemHandler();
+        ksession.getWorkItemManager().registerWorkItemHandler("Human Task",
+                workItemHandler);
+        ProcessInstance processInstance = ksession.startProcess("StructureRef");
+        assertTrue(processInstance.getState() == ProcessInstance.STATE_ACTIVE);
+
+        Map<String, Object> res = new HashMap<String, Object>();
+        res.put("testHT", personAsXml);
+        ksession.getWorkItemManager().completeWorkItem(
+                workItemHandler.getWorkItem().getId(), res);
+        
+        assertProcessInstanceCompleted(processInstance.getId(), ksession);
+    }
 }
