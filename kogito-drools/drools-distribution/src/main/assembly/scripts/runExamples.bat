368116bbe575ec79812817b395754274f6c802f9
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2012-09-27 16:04:46 +0200

    runExamples.bat: Prevent the terminal window to disappear before the user has seen the error message

--- kogito-drools/drools-distribution/src/main/assembly/scripts/runExamples.bat
+++ kogito-drools/drools-distribution/src/main/assembly/scripts/runExamples.bat
@@ -21,3 +21,10 @@ if exist %JAVA_HOME%\bin\java.exe (
 ) else (
     java -Xms256m -Xmx512m -cp %mainClasspath% %mainClass%
 )
+
+if %ERRORLEVEL% neq 0 (
+    echo
+    echo "Error occurred. Check if JAVA_HOME (%JAVA_HOME%) is correct."
+    rem Prevent the terminal window to disappear before the user has seen the error message
+    sleep 20
+)
