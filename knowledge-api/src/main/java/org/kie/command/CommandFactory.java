11e2096dbf35d1211c4a2634f7d96e531e7173b5
mariofusco <mario.fusco@gmail.com>
2012-11-13 15:23:39 +0100

    fix kie refactor

--- knowledge-api/src/main/java/org/kie/command/CommandFactory.java
+++ knowledge-api/src/main/java/org/kie/command/CommandFactory.java
@@ -368,10 +368,10 @@ public class CommandFactory {
 
     private static void loadProvider() {
         try {
-            Class<CommandFactoryService> cls = (Class<CommandFactoryService>) Class.forName( "org.kie.command.impl.CommandFactoryServiceImpl" );
+            Class<CommandFactoryService> cls = (Class<CommandFactoryService>) Class.forName( "org.drools.command.impl.CommandFactoryServiceImpl" );
             setCommandFactoryProvider( cls.newInstance() );
         } catch ( Exception e2 ) {
-            throw new RuntimeException( "Provider org.kie.command.impl.CommandFactoryProviderImpl could not be set.",
+            throw new RuntimeException( "Provider org.drools.command.impl.CommandFactoryProviderImpl could not be set.",
                                                        e2 );
         }
     }
--- knowledge-api/src/main/java/org/kie/persistence/jpa/JPAKnowledgeService.java
+++ knowledge-api/src/main/java/org/kie/persistence/jpa/JPAKnowledgeService.java
@@ -149,10 +149,10 @@ public class JPAKnowledgeService {
     private static void loadProvider() {
         try {
             // we didn't find anything in properties so lets try and us reflection
-            Class<KnowledgeStoreService> cls = (Class<KnowledgeStoreService>) Class.forName( "org.kie.persistence.jpa.KnowledgeStoreServiceImpl" );
+            Class<KnowledgeStoreService> cls = (Class<KnowledgeStoreService>) Class.forName( "org.drools.persistence.jpa.KnowledgeStoreServiceImpl" );
             setJPAKnowledgeServiceProvider( cls.newInstance() );
         } catch ( Exception e ) {
-            throw new RuntimeException( "Provider org.kie.persistence.jpa.KnowledgeStoreServiceImpl could not be set.",
+            throw new RuntimeException( "Provider org.drools.persistence.jpa.KnowledgeStoreServiceImpl could not be set.",
                                         e );
         }
     }
