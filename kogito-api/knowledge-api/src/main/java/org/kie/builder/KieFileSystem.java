ad3088a50db33a0c1aed656b3aa9337f51485d4d
Mark Proctor <mproctor@codehaus.org>
2012-12-01 19:04:36 +0000

    Implementing the API for Drools 6

--- kogito-api/knowledge-api/src/main/java/org/kie/builder/KieFileSystem.java
+++ kogito-api/knowledge-api/src/main/java/org/kie/builder/KieFileSystem.java
@@ -4,8 +4,16 @@ import org.kie.io.Resource;
 
 public interface KieFileSystem {
 
+    KieFileSystem generateAndWritePomXML(GAV gav);
+    
+    KieFileSystem writePomXML(byte[] content);
+    KieFileSystem writePomXML(String content);
+
+    KieFileSystem writeProjectXML(byte[] content);
+    KieFileSystem writeProjectXML(String content);
+    
     KieFileSystem write(String path, byte[] content);
-    KieFileSystem write(String path, String text);
+    KieFileSystem write(String path, String content);
     KieFileSystem write(String path, Resource resource);
 
     void delete(String... paths);
