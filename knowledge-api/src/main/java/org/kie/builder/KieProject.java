b38829dbf8cf6b7ade4a8fe44ca52bb9e6ff0b54
Mark Proctor <mproctor@codehaus.org>
2012-12-01 19:04:36 +0000

    Implementing the API for Drools 6

--- knowledge-api/src/main/java/org/kie/builder/KieProject.java
+++ knowledge-api/src/main/java/org/kie/builder/KieProject.java
@@ -5,17 +5,6 @@ import java.util.Map;
 
 public interface KieProject {
 
-    String KPROJECT_JAR_PATH = "META-INF/kproject.xml";
-    String KPROJECT_RELATIVE_PATH = "src/main/resources/" + KPROJECT_JAR_PATH;
-
-    GAV getGroupArtifactVersion();
-
-    KieProject setGroupArtifactVersion(GAV gav);
-
-    String getKBasesPath();
-
-    KieProject setKBasesPath(String kprojectPath);
-
     KieBaseModel newKieBaseModel(String name);
     
     void removeKieBaseModel(String qName);
