c4a710bdb3fcec5fa6d228c100960615babd4407
mariofusco <mario.fusco@gmail.com>
2012-12-03 19:58:20 +0100

    Implementing the API for Drools 6

--- kogito-api/kie-api/src/main/java/org/kie/builder/KieSessionModel.java
+++ kogito-api/kie-api/src/main/java/org/kie/builder/KieSessionModel.java
@@ -18,7 +18,7 @@ public interface KieSessionModel {
 
     KieSessionModel setClockType(ClockTypeOption clockType);
 
-    ListenerModel newListenerModel(String type);
+    ListenerModel newListenerModel(String type, ListenerModel.Kind kind);
 
     List<ListenerModel> getListenerModels();
 
--- kogito-api/kie-api/src/main/java/org/kie/builder/ListenerModel.java
+++ kogito-api/kie-api/src/main/java/org/kie/builder/ListenerModel.java
@@ -1,8 +1,38 @@
 package org.kie.builder;
 
+import static java.util.EnumSet.allOf;
+
 public interface ListenerModel {
 
+    public enum Kind {
+        AGENDA_EVENT_LISTENER("agendaEventListener"),
+        WORKING_MEMORY_EVENT_LISTENER("workingMemoryEventListener"),
+        PROCESS_EVENT_LISTENER("processEventListener");
+
+        private final String name;
+
+        private Kind(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+        public static Kind fromString(String name) {
+            for (Kind kind : allOf(Kind.class)) {
+                if (kind.toString().equals(name)) {
+                    return kind;
+                }
+            }
+            return null;
+        }
+    }
+
     String getType();
+
+    ListenerModel.Kind getKind();
 
     QualifierModel getQualifierModel();
 
