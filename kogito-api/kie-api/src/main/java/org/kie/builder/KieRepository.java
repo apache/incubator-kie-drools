72097d20c76ce04f0fedeae52b5aa21e4b5e3751
Mark Proctor <mproctor@codehaus.org>
2012-12-04 10:42:23 +0000

    kie-api/src/main

--- kogito-api/kie-api/src/main/java/org/kie/builder/KieRepository.java
+++ kogito-api/kie-api/src/main/java/org/kie/builder/KieRepository.java
@@ -1,12 +1,16 @@
 package org.kie.builder;
 
+import org.kie.io.Resource;
+
 public interface KieRepository {
     
     public GAV getDefaultGAV();
     
     void addKieModule(KieModule kjar);
+    
+    KieModule addKieModule(Resource resource);    
 
     Results verfyKieModule(GAV gav);
     
-    KieModule getKieModule(GAV gav);
+    KieModule getKieModule(GAV gav);     
 }
