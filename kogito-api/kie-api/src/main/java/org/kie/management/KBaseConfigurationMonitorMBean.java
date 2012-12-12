6bc81d4064be2af4337689e994ded6cfddd9b426
Edson Tirelli <ed.tirelli@gmail.com>
2012-12-11 21:09:06 -0500

    Deprecating methods

--- kogito-api/kie-api/src/main/java/org/kie/management/KBaseConfigurationMonitorMBean.java
+++ kogito-api/kie-api/src/main/java/org/kie/management/KBaseConfigurationMonitorMBean.java
@@ -18,36 +18,47 @@ package org.kie.management;
 
 public interface KBaseConfigurationMonitorMBean {
 
+    @Deprecated
     public int getAlphaNodeHashingThreshold();
 
     public String getAssertBehaviour();
 
+    @Deprecated
     public int getCompositeKeyDepth();
 
     public String getEventProcessingMode();
 
+    @Deprecated
     public int getMaxThreads();
 
+    @Deprecated
     public String getSequentialAgenda();
 
     public boolean isAdvancedProcessRuleIntegration();
 
+    @Deprecated
     public boolean isIndexLeftBetaMemory();
 
+    @Deprecated
     public boolean isIndexRightBetaMemory();
 
+    @Deprecated
     public boolean isMaintainTms();
 
     public boolean isMBeansEnabled();
 
+    @Deprecated
     public boolean isMultithreadEvaluation();
 
     public boolean isRemoveIdentities();
 
+    @Deprecated
     public boolean isSequential();
 
+    @Deprecated
     public boolean isShareAlphaNodes();
 
+    @Deprecated
     public boolean isShareBetaNodes();
 
 }
