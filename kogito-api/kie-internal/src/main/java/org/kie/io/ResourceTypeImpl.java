0bceb98716cc97e427fbbba933d9f536460740de
Edson Tirelli <ed.tirelli@gmail.com>
2012-12-11 20:57:52 -0500

    Cleaning up ResourceType

---
+++ kogito-api/kie-internal/src/main/java/org/kie/io/ResourceTypeImpl.java
@@ -0,0 +1,35 @@
+package org.kie.io;
+
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ResourceTypeImpl {
+    private static final String                    KIE_RESOURCE_CONF_CLASS = "kie.resource.conf.class";
+    
+    private static final Logger                    logger                  = LoggerFactory.getLogger( ResourceTypeImpl.class );
+
+    public static Properties toProperties(ResourceConfiguration conf) {
+        Properties prop = conf.toProperties();
+        prop.setProperty( KIE_RESOURCE_CONF_CLASS, conf.getClass().getName() );
+        return prop;
+    }
+
+    public static ResourceConfiguration fromProperties(Properties prop) {
+        String className = prop.getProperty( KIE_RESOURCE_CONF_CLASS );
+        try {
+            // not sure how to get the proper classloader here, but the resource configurations
+            // should be accessible from the current classloader
+            ResourceConfiguration conf = (ResourceConfiguration) Class.forName( className ).newInstance();
+            conf.fromProperties( prop );
+            return conf;
+        } catch ( Exception e ) {
+            logger.error( "Error loading resource configuration from properties", e );
+        }
+        return null;
+    }
+
+    
+
+}
