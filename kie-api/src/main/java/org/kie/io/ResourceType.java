566cb1773fa0ae0258e0c7598febe7792fa5e895
Edson Tirelli <ed.tirelli@gmail.com>
2012-12-11 20:57:52 -0500

    Cleaning up ResourceType

--- kie-api/src/main/java/org/kie/io/ResourceType.java
+++ kie-api/src/main/java/org/kie/io/ResourceType.java
@@ -31,10 +31,6 @@ public class ResourceType
 
     private static final long                      serialVersionUID        = 1613735834228581906L;
 
-    private static final String                    KIE_RESOURCE_CONF_CLASS = "kie.resource.conf.class";
-
-    private static final Logger                    logger                  = LoggerFactory.getLogger( ResourceType.class );
-
     private String                                 name;
 
     private String                                 description;
@@ -177,26 +173,6 @@ public class ResourceType
             if ( type.matchesExtension( resourceName ) ) {
                 return type;
             }
-        }
-        return null;
-    }
-
-    public static Properties toProperties(ResourceConfiguration conf) {
-        Properties prop = conf.toProperties();
-        prop.setProperty( KIE_RESOURCE_CONF_CLASS, conf.getClass().getName() );
-        return prop;
-    }
-
-    public static ResourceConfiguration fromProperties(Properties prop) {
-        String className = prop.getProperty( KIE_RESOURCE_CONF_CLASS );
-        try {
-            // not sure how to get the proper classloader here, but the resource configurations
-            // should be accessible from the current classloader
-            ResourceConfiguration conf = (ResourceConfiguration) Class.forName( className ).newInstance();
-            conf.fromProperties( prop );
-            return conf;
-        } catch ( Exception e ) {
-            logger.error( "Error loading resource configuration from properties", e );
         }
         return null;
     }
