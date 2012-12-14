eaede78adcbd0e7786ea6e84a71bcf0c9c3c5835
Edson Tirelli <ed.tirelli@gmail.com>
2012-12-14 10:44:23 -0500

    Restoring correct command files

--- drools-core/src/main/java/org/drools/command/runtime/rule/GetEntryPointCommand.java
+++ drools-core/src/main/java/org/drools/command/runtime/rule/GetEntryPointCommand.java
@@ -1,14 +1,43 @@
+/*
+ * Copyright 2010 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.drools.command.runtime.rule;
 
-import org.kie.command.Command;
+import org.drools.command.impl.GenericCommand;
+import org.drools.command.impl.KnowledgeCommandContext;
+import org.kie.command.Context;
+import org.kie.runtime.StatefulKnowledgeSession;
 import org.kie.runtime.rule.SessionEntryPoint;
 
 public class GetEntryPointCommand
     implements
-    Command<SessionEntryPoint> {
+    GenericCommand<SessionEntryPoint> {
+
+    private String name;
 
     public GetEntryPointCommand(String name) {
-        // TODO Auto-generated constructor stub
+        this.name = name;
     }
 
+    public SessionEntryPoint execute(Context context) {
+        StatefulKnowledgeSession ksession = ((KnowledgeCommandContext) context).getStatefulKnowledgesession();
+        return ksession.getEntryPoint( name );
+    }
+
+    public String toString() {
+        return "session.getEntryPoint( " + name + " );";
+    }
 }
