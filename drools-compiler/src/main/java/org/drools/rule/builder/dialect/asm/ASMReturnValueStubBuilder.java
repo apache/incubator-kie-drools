0385cce146e7df1927c930a5161943f48652dfec
mariofusco <mario.fusco@gmail.com>
2013-01-25 17:27:20 +0100

    [JBRULES-3675] synchronize lazy creation of evals and predicates (cherry picked from commit 8408e54cea1d974bc835a174b31698ba0a06fbcc)

--- drools-compiler/src/main/java/org/drools/rule/builder/dialect/asm/ASMReturnValueStubBuilder.java
+++ drools-compiler/src/main/java/org/drools/rule/builder/dialect/asm/ASMReturnValueStubBuilder.java
@@ -17,7 +17,13 @@ import static org.mvel2.asm.Opcodes.ACC_PUBLIC;
 import static org.mvel2.asm.Opcodes.ACONST_NULL;
 import static org.mvel2.asm.Opcodes.ALOAD;
 import static org.mvel2.asm.Opcodes.ARETURN;
+import static org.mvel2.asm.Opcodes.ASTORE;
+import static org.mvel2.asm.Opcodes.ATHROW;
+import static org.mvel2.asm.Opcodes.DUP;
+import static org.mvel2.asm.Opcodes.GOTO;
 import static org.mvel2.asm.Opcodes.IFNONNULL;
+import static org.mvel2.asm.Opcodes.MONITORENTER;
+import static org.mvel2.asm.Opcodes.MONITOREXIT;
 import static org.mvel2.asm.Opcodes.RETURN;
 
 public class ASMReturnValueStubBuilder extends AbstractASMReturnValueBuilder {
@@ -41,16 +47,43 @@ public class ASMReturnValueStubBuilder extends AbstractASMReturnValueBuilder {
         }).addMethod(ACC_PUBLIC, "replaceDeclaration", generator.methodDescr(null, Declaration.class, Declaration.class)
         ).addMethod(ACC_PUBLIC, "evaluate", generator.methodDescr(FieldValue.class, Object.class, Tuple.class, Declaration[].class, Declaration[].class, WorkingMemory.class, Object.class), new String[]{"java/lang/Exception"}, new ClassGenerator.MethodBody() {
             public void body(MethodVisitor mv) {
+                Label syncStart = new Label();
+                Label syncEnd = new Label();
                 Label l1 = new Label();
+                Label l2 = new Label();
+                mv.visitTryCatchBlock(syncStart, l1, l2, null);
+                Label l3 = new Label();
+                mv.visitTryCatchBlock(l2, l3, l2, null);
+                mv.visitVarInsn(ALOAD, 0);
+                mv.visitInsn(DUP);
+                mv.visitVarInsn(ASTORE, 7);
+                // synchronized(this) {
+                mv.visitInsn(MONITORENTER);
+                mv.visitLabel(syncStart);
                 getFieldFromThis("returnValue", ReturnValueExpression.class);
-                mv.visitJumpInsn(IFNONNULL, l1);
+                // if (returnValue == null) ...
+                Label ifNotInitialized = new Label();
+                mv.visitJumpInsn(IFNONNULL, ifNotInitialized);
                 mv.visitVarInsn(ALOAD, 0);
                 mv.visitVarInsn(ALOAD, 2);
                 mv.visitVarInsn(ALOAD, 3);
                 mv.visitVarInsn(ALOAD, 4);
                 mv.visitVarInsn(ALOAD, 5);
                 invokeStatic(ReturnValueGenerator.class, "generate", null, ReturnValueStub.class, Tuple.class, Declaration[].class, Declaration[].class, WorkingMemory.class);
+                mv.visitLabel(ifNotInitialized);
+                mv.visitVarInsn(ALOAD, 7);
+                mv.visitInsn(MONITOREXIT);
                 mv.visitLabel(l1);
+                mv.visitJumpInsn(GOTO, syncEnd);
+                mv.visitLabel(l2);
+                mv.visitVarInsn(ASTORE, 8);
+                mv.visitVarInsn(ALOAD, 7);
+                mv.visitInsn(MONITOREXIT);
+                mv.visitLabel(l3);
+                mv.visitVarInsn(ALOAD, 8);
+                mv.visitInsn(ATHROW);
+                mv.visitLabel(syncEnd);
+                // } end of synchronized
                 getFieldFromThis("returnValue", ReturnValueExpression.class);
                 mv.visitVarInsn(ALOAD, 1);
                 mv.visitVarInsn(ALOAD, 2);
