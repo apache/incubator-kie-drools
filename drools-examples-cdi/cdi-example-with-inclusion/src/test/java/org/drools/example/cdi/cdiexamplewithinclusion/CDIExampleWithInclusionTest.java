79b28cccbc70794fd3b4e0e2e372f838ae0f9916
mariofusco <mario.fusco@gmail.com>
2013-02-06 20:50:15 +0100

    add kie cdi examples

---
+++ drools-examples-cdi/cdi-example-with-inclusion/src/test/java/org/drools/example/cdi/cdiexamplewithinclusion/CDIExampleWithInclusionTest.java
@@ -0,0 +1,37 @@
+package org.drools.example.cdi.cdiexamplewithinclusion;
+
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class CDIExampleWithInclusionTest {
+    
+    @Test
+    public void testGo() {
+        Weld w = new Weld();        
+        WeldContainer wc = w.initialize();
+        
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream( baos );
+        
+        CDIExampleWithInclusion bean = wc.instance().select(CDIExampleWithInclusion.class).get();
+        bean.go( ps );
+        
+        ps.close();
+        
+        String actual = new String( baos.toByteArray() );
+        String expected = "" +
+                "Dave: Hello, HAL. Do you read me, HAL?\n" +
+                "HAL: Dave. I read you.\n" +
+                "Dave: Open the pod bay doors, HAL.\n" +
+                "HAL: I'm sorry, Dave. I'm afraid I can't do that.\n";
+        assertEquals(expected, actual );
+        
+        w.shutdown();        
+    }
+}
---
+++ drools-examples-cdi/cdi-example/src/main/java/org/drools/example/cdi/cdiexample/CDIExample.java
@@ -0,0 +1,32 @@
+package org.drools.example.cdi.cdiexample;
+
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+import org.kie.cdi.KSession;
+import org.kie.runtime.KieSession;
+
+import javax.inject.Inject;
+import java.io.PrintStream;
+
+public class CDIExample {
+
+    @Inject @KSession("ksession1")
+    KieSession kSession;
+    
+    public void go(PrintStream out) {
+        kSession.setGlobal( "out", out );
+        kSession.insert( new Message("Dave", "Hello, HAL. Do you read me, HAL?") );
+        kSession.fireAllRules();
+    }    
+    
+    public static void main( String[] args ) {
+        Weld w = new Weld();
+        
+        WeldContainer wc = w.initialize();
+        CDIExample bean = wc.instance().select(CDIExample.class).get();
+        bean.go( System.out );
+        
+        w.shutdown();
+    }
+
+}
