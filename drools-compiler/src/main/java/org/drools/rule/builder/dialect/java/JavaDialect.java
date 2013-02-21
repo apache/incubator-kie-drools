7532bf3bff228772ffc43c64b9656b6a0def1393
Edson Tirelli <ed.tirelli@gmail.com>
2013-02-20 21:01:29 -0500

    DROOLS-46: fixing kbuilder attach to an existing kbase (cherry picked from commit 8d50302f22543a847c2cb248a8fd624f732df7bf)
    Conflicts:
    
    	drools-compiler/src/test/java/org/drools/integrationtests/DynamicRulesTest.java
    	drools-core/src/main/java/org/drools/common/AbstractRuleBase.java
    (cherry picked from commit ce10282eb0001d72e61e5d420db8b2ec433a56dc)
    
    Conflicts:
    
    	drools-compiler/src/main/java/org/drools/rule/builder/dialect/java/JavaDialect.java

--- drools-compiler/src/main/java/org/drools/rule/builder/dialect/java/JavaDialect.java
+++ drools-compiler/src/main/java/org/drools/rule/builder/dialect/java/JavaDialect.java
@@ -171,10 +171,10 @@ public class JavaDialect
 
         this.generatedClassList = new ArrayList<String>();
 
-        JavaDialectRuntimeData data;
+        JavaDialectRuntimeData data = (JavaDialectRuntimeData) pkg.getDialectRuntimeRegistry().getDialectData( ID );
 
-        // initialie the dialect runtime data if it doesn't already exist
-        if ( pkg.getDialectRuntimeRegistry().getDialectData( ID ) == null ) {
+        // initialise the dialect runtime data if it doesn't already exist
+        if ( data == null ) {
             data = new JavaDialectRuntimeData();
             this.pkg.getDialectRuntimeRegistry().setDialectData( ID,
                                                                  data );
