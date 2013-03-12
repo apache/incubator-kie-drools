80903c83b777249f9837d43ce74473094a8d31d8
Michael Anstis <michael.anstis@gmail.com>
2013-03-12 15:07:21 +0000

    Incremental Build test for XLS files

---
+++ kogito-drools/drools-decisiontables/src/test/java/org/drools/decisiontable/integrationtests/IncrementalCompilationTest.java
@@ -0,0 +1,99 @@
+package org.drools.decisiontable.integrationtests;
+
+import java.io.InputStream;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.kie.KieServices;
+import org.kie.builder.IncrementalResults;
+import org.kie.builder.InternalKieBuilder;
+import org.kie.builder.KieBuilder;
+import org.kie.builder.KieFileSystem;
+
+import static junit.framework.Assert.*;
+
+public class IncrementalCompilationTest {
+
+    @Test
+    public void testDuplicateXLSResources() throws Exception {
+
+        KieServices ks = KieServices.Factory.get();
+
+        KieFileSystem kfs = ks.newKieFileSystem();
+
+        InputStream in1 = null;
+        InputStream in2 = null;
+
+        try {
+
+            //Add XLS decision table
+            in1 = this.getClass().getResourceAsStream( "incrementalBuild.dtable.xls" );
+            kfs.write( "src/main/resources/incrementalBuild1.dtable.xls",
+                       KieServices.Factory.get().getResources().newInputStreamResource( in1 ) );
+
+            //Add the same XLS decision table again as a different resource
+            in2 = this.getClass().getResourceAsStream( "incrementalBuild.dtable.xls" );
+            kfs.write( "src/main/resources/incrementalBuild2.dtable.xls",
+                       KieServices.Factory.get().getResources().newInputStreamResource( in2 ) );
+
+            //Check errors on a full build
+            assertEquals( 1, ks.newKieBuilder( kfs ).buildAll().getResults().getMessages().size() );
+
+        } finally {
+            if ( in1 != null ) {
+                in1.close();
+            }
+            if ( in2 != null ) {
+                in2.close();
+            }
+        }
+
+    }
+
+    @Test
+    @Ignore("This does not pass at the moment. Incremental Build gives different results to a full build")
+    public void testIncrementalCompilationDuplicateXLSResources() throws Exception {
+
+        KieServices ks = KieServices.Factory.get();
+
+        KieFileSystem kfs = ks.newKieFileSystem();
+
+        InputStream in1 = null;
+        InputStream in2 = null;
+
+        try {
+
+            //Add XLS decision table
+            in1 = this.getClass().getResourceAsStream( "incrementalBuild.dtable.xls" );
+            kfs.write( "src/main/resources/incrementalBuild1.dtable.xls",
+                       KieServices.Factory.get().getResources().newInputStreamResource( in1 ) );
+
+            //Expect no errors
+            KieBuilder kieBuilder = ks.newKieBuilder( kfs ).buildAll();
+            assertEquals( 0, kieBuilder.getResults().getMessages( org.kie.builder.Message.Level.ERROR ).size() );
+
+            //Add the same XLS decision table again as a different resource
+            in2 = this.getClass().getResourceAsStream( "incrementalBuild.dtable.xls" );
+            kfs.write( "src/main/resources/incrementalBuild2.dtable.xls",
+                       KieServices.Factory.get().getResources().newInputStreamResource( in2 ) );
+            IncrementalResults addResults = ( (InternalKieBuilder) kieBuilder ).createFileSet( "src/main/resources/incrementalBuild2.dtable.xls" ).build();
+
+            //Expect duplicate rule errors
+            assertEquals( 1, addResults.getAddedMessages().size() );
+            assertEquals( 0, addResults.getRemovedMessages().size() );
+
+            //Check errors on a full build
+            assertEquals( 1, ks.newKieBuilder( kfs ).buildAll().getResults().getMessages().size() );
+
+        } finally {
+            if ( in1 != null ) {
+                in1.close();
+            }
+            if ( in2 != null ) {
+                in2.close();
+            }
+        }
+
+    }
+
+}
