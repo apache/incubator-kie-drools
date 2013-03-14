81537b1d580e2ea3e58195c6f3130957d16728bb
Maciej Swiderski <mswiders@redhat.com>
2013-03-14 18:59:55 +0100

    fixed invalid merge that reverted change

--- kogito-jbpm/jbpm-flow/src/main/java/org/jbpm/marshalling/impl/AbstractProtobufProcessInstanceMarshaller.java
+++ kogito-jbpm/jbpm-flow/src/main/java/org/jbpm/marshalling/impl/AbstractProtobufProcessInstanceMarshaller.java
@@ -383,7 +383,7 @@ public abstract class AbstractProtobufProcessInstanceMarshaller
             Header _header;
             try {
 
-                _header = PersisterHelper.readFromStreamWithHeader(context, registry);
+                _header = PersisterHelper.readFromStreamWithHeaderPreloaded(context, registry);
             } catch ( ClassNotFoundException e ) {
                 // Java 5 does not accept [new IOException(String, Throwable)]
                 IOException ioe =  new IOException( "Error deserializing process instance." );
