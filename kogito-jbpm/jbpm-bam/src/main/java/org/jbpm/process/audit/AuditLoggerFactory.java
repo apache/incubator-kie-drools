1eab222a589fb3da19caf05ea044934bbfd941a1
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2013-03-18 14:14:10 +0100

    Resolve split-packages: move everything from kie-internal under org.kie.internal: move org.kie.event part 1

--- kogito-jbpm/jbpm-bam/src/main/java/org/jbpm/process/audit/AuditLoggerFactory.java
+++ kogito-jbpm/jbpm-bam/src/main/java/org/jbpm/process/audit/AuditLoggerFactory.java
@@ -8,7 +8,6 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import org.jbpm.process.audit.jms.AsyncAuditLogProducer;
-import org.kie.event.KnowledgeRuntimeEventManager;
 import org.kie.runtime.KieSession;
 
 /**
