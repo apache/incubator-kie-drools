7da207876668b796d3c7fc435a26b011f648ad65
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2013-03-18 15:09:09 +0100

    Resolve split-packages: move everything from kie-api under org.kie.api: move classes directly under org.kie.marshalling

--- kogito-api/kie-api/src/main/java/org/kie/api/KieServices.java
+++ kogito-api/kie-api/src/main/java/org/kie/api/KieServices.java
@@ -9,7 +9,7 @@ import org.kie.api.builder.ReleaseId;
 import org.kie.api.command.KieCommands;
 import org.kie.io.KieResources;
 import org.kie.api.logger.KieLoggers;
-import org.kie.marshalling.KieMarshallers;
+import org.kie.api.marshalling.KieMarshallers;
 import org.kie.api.persistence.jpa.KieStoreServices;
 import org.kie.runtime.KieContainer;
 
