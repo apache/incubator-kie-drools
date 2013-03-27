c5c64d045a8ac3183fb63e12eaa0f26d195cc42f
mariofusco <mario.fusco@gmail.com>
2013-03-27 19:18:37 +0100

    allow to get the KieModule from the InternalKieBuilder even if there are errors

--- kogito-api/kie-internal/src/main/java/org/kie/internal/builder/InternalKieBuilder.java
+++ kogito-api/kie-internal/src/main/java/org/kie/internal/builder/InternalKieBuilder.java
@@ -1,7 +1,11 @@
 package org.kie.internal.builder;
 
 import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieModule;
 
 public interface InternalKieBuilder extends KieBuilder {
+
     KieBuilderSet createFileSet(String... files);
+
+    KieModule getKieModuleIgnoringErrors();
 }
