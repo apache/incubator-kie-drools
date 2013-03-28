1307c923eca9fec7ef5af750fab11bbee8e0c72e
mariofusco <mario.fusco@gmail.com>
2013-03-28 14:38:30 +0100

    [DROOLS-79] fix jitting of constraints with primitive cast

--- drools-compiler/src/test/java/org/drools/compiler/integrationtests/MiscTest2.java
+++ drools-compiler/src/test/java/org/drools/compiler/integrationtests/MiscTest2.java
@@ -1583,4 +1583,20 @@ public class MiscTest2 extends CommonTestMethodBase {
         assertTrue(list.contains("React"));
         assertTrue(list.contains("React2"));
     }
+
+    @Test
+    public void testJitCastOfPrimitiveType() {
+        // DROOLS-79
+        String str =
+                "rule R when\n" +
+                        " Number(longValue < (Long)7)\n" +
+                        "then\n" +
+                        "end\n";
+
+        KnowledgeBase kbase = loadKnowledgeBaseFromString(str);
+        StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+
+        ksession.insert(new Long(6));
+        assertEquals(1, ksession.fireAllRules());
+    }
 }
