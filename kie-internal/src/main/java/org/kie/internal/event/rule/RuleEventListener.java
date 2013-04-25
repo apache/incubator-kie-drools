84dddfe84ba92077ab680db7409c85bcec62a2f0
Mark Proctor <mproctor@codehaus.org>
2013-04-25 05:50:37 +0100

    DROOLS-7 Phreak -Dynamic Rule Additions

---
+++ kie-internal/src/main/java/org/kie/internal/event/rule/RuleEventListener.java
@@ -0,0 +1,11 @@
+package org.kie.internal.event.rule;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mdproctor
+ * Date: 06/04/2013
+ * Time: 19:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class RuleEventListener {
+}
