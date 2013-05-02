1df403b057567518c023cb2330f9f0121e9caf25
Maciej Swiderski <mswiders@redhat.com>
2013-05-02 17:28:19 +0200

    added mvel based bean creator to allow building more advanced listeners and work item handlers

--- kogito-drools/drools-compiler/src/main/java/org/drools/compiler/kie/util/CDIHelper.java
+++ kogito-drools/drools-compiler/src/main/java/org/drools/compiler/kie/util/CDIHelper.java
@@ -1,6 +1,7 @@
 package org.drools.compiler.kie.util;
 
 import java.lang.annotation.Annotation;
+import java.util.Map;
 import java.util.Set;
 
 import javax.enterprise.inject.spi.Bean;
@@ -17,6 +18,7 @@ import org.kie.api.event.rule.AgendaEventListener;
 import org.kie.api.event.rule.WorkingMemoryEventListener;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.process.WorkItemHandler;
+import org.mvel2.MVEL;
 
 public class CDIHelper {
 
@@ -26,6 +28,10 @@ public class CDIHelper {
 
     public static void wireListnersAndWIHs(BeanManager beanManager, KieSessionModel model, KieSession kSession) {
         wireListnersAndWIHs(new CDIBeanCreator(beanManager), model, kSession);
+    }
+
+    public static void wireListnersAndWIHs(KieSessionModel model, KieSession kSession, Map<String, Object> parameters) {
+        wireListnersAndWIHs(new MVELBeanCreator(parameters), model, kSession);
     }
 
     private static void wireListnersAndWIHs(BeanCreator beanCreator, KieSessionModel model, KieSession kSession) {
@@ -128,6 +134,21 @@ public class CDIHelper {
                 throw new IllegalArgumentException("Cannot use a qualifier without a CDI container");
             }
             return (T)Class.forName(type).newInstance();
+        }
+    }
+
+    private static class MVELBeanCreator implements BeanCreator {
+
+        private Map<String, Object> parameters;
+
+        private MVELBeanCreator(Map<String, Object> parameters) {
+            this.parameters = parameters;
+        }
+        public <T> T createBean(String type, QualifierModel qualifier) throws Exception {
+            if (qualifier != null) {
+                throw new IllegalArgumentException("Cannot use a qualifier without a CDI container");
+            }
+            return (T)MVEL.eval( type, parameters );
         }
     }
 }
