685b54132000fa338e26312ad1f938c8c7d88d58
mariofusco <mario.fusco@gmail.com>
2013-05-02 11:12:53 +0200

    [JBRULES-3705] Same criteria across multiple rules cancels activation with Property Reactive Facts

--- drools-compiler/src/test/java/org/drools/compiler/integrationtests/PropertyReactivityTest.java
+++ drools-compiler/src/test/java/org/drools/compiler/integrationtests/PropertyReactivityTest.java
@@ -8,6 +8,7 @@ import org.kie.api.definition.type.PropertyReactive;
 import org.kie.internal.KnowledgeBase;
 import org.kie.internal.runtime.StatefulKnowledgeSession;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -433,4 +434,59 @@ public class PropertyReactivityTest extends CommonTestMethodBase {
 
         ksession.fireAllRules();
     }
+
+    @Test
+    public void testRepeatedPatternWithPR() {
+        // JBRULES-3705
+        String str1 =
+                "package org.test;\n" +
+                "global java.util.List list; \n" +
+                "" +
+                "declare SampleBean \n" +
+                "@propertyReactive \n" +
+                "x : java.math.BigDecimal \n" +
+                "y : java.math.BigDecimal \n" +
+                "id : Long @key\n" +
+                "end \n" +
+                "" +
+                " rule \"calculate y\"\n" +
+                " dialect \"mvel\"\n" +
+                " when\n" +
+                " $bean : SampleBean(id == 1L);\n" +
+                " then\n" +
+                " modify($bean){\n" +
+                " y =5B;\n" +
+                " }\n" +
+                " list.add( $bean.y ); \n" +
+                " end\n" +
+                "\n" +
+                " rule \"calculate x\"\n" +
+                " dialect \"mvel\"\n" +
+                " when\n" +
+                " $bean : SampleBean(id == 1L);\n" +
+                " then\n" +
+                " modify($bean){\n" +
+                " x =10B;\n" +
+                " }\n" +
+                " list.add( $bean.x ); \n" +
+                " end\n" +
+                "" +
+                " rule Init \n" +
+                " when\n" +
+                " then\n" +
+                " insert( new SampleBean( 1L ) ); \n" +
+                " end";
+
+
+        KnowledgeBase kbase = loadKnowledgeBaseFromString( str1 );
+        StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+        ArrayList list = new ArrayList();
+        ksession.setGlobal( "list", list );
+        ksession.fireAllRules();
+
+        assertEquals( 2, list.size() );
+        assertTrue( list.contains( new BigDecimal( 10 ) ) );
+        assertTrue(list.contains(new BigDecimal(5)));
+    }
+
 }
