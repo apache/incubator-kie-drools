1d14486accf8a5567f86751c1dbd43a471205218
mariofusco <mario.fusco@gmail.com>
2013-05-08 12:58:53 +0200

    [DROOLS-128] provide an accessor for all globals keys

--- kie-api/src/main/java/org/kie/api/runtime/Globals.java
+++ kie-api/src/main/java/org/kie/api/runtime/Globals.java
@@ -16,6 +16,8 @@
 
 package org.kie.api.runtime;
 
+import java.util.Collection;
+
 public interface Globals {
     Object get(String identifier);
 
@@ -23,4 +25,6 @@ public interface Globals {
              Object value);
     
     void setDelegate(Globals delegate);
+
+    Collection<String> getGlobalKeys();
 }
