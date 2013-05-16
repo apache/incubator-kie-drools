2fec861179f86395da6f88914de73a68aae94f57
Marco Rietveld <mrietvel@redhat.com>
2013-05-16 23:55:33 +0200

    JBPM-4007 Removing unneccessary code from kie-* modules that sould be in the jbpm-human-task-core module

--- kie-api/src/main/java/org/kie/api/task/model/Status.java
+++ kie-api/src/main/java/org/kie/api/task/model/Status.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2010 JBoss Inc
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,31 +16,7 @@
 
 package org.kie.api.task.model;
 
-import javax.xml.bind.annotation.XmlEnum;
 
-@XmlEnum
 public enum Status {
-    Created, Ready, Reserved, InProgress, Suspended, Completed, Failed, Error, Exited, Obsolete;
-
-    private final String value;
-
-    private Status() {
-        value = this.name();
-    }
-
-    public String value() {
-        return value;
-    }
-
-    public static Status fromValue(String newValue) {
-        if (newValue == null) {
-            throw new IllegalArgumentException(newValue);
-        }
-        for (Status value : Status.values()) {
-            if (value.value.toLowerCase().equals(newValue.toLowerCase())) {
-                return value;
-            }
-        }
-        throw new IllegalArgumentException(newValue);
-    }
+    Created, Ready, Reserved, InProgress, Suspended, Completed, Failed, Error, Exited, Obsolete
 }
