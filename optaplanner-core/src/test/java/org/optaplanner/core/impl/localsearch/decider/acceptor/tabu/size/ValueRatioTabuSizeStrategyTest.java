754c309251e28fc6dec6ed80dfb014c956d3d650
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2013-05-17 16:42:00 +0200

    ValueRatioTabuSizeStrategyTest

---
+++ optaplanner-core/src/test/java/org/optaplanner/core/impl/localsearch/decider/acceptor/tabu/size/ValueRatioTabuSizeStrategyTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2013 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaplanner.core.impl.localsearch.decider.acceptor.tabu.size;
+
+import org.junit.Test;
+import org.optaplanner.core.impl.localsearch.scope.LocalSearchSolverPhaseScope;
+import org.optaplanner.core.impl.localsearch.scope.LocalSearchStepScope;
+import org.optaplanner.core.impl.solver.scope.DefaultSolverScope;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class ValueRatioTabuSizeStrategyTest {
+
+    @Test
+    public void tabuSize() {
+        LocalSearchSolverPhaseScope phaseScope = new LocalSearchSolverPhaseScope(mock(DefaultSolverScope.class));
+        when(phaseScope.getWorkingValueCount()).thenReturn(20);
+        LocalSearchStepScope stepScope = new LocalSearchStepScope(phaseScope);
+        assertEquals(2, new ValueRatioTabuSizeStrategy(0.1).determineTabuSize(stepScope));
+        assertEquals(10, new ValueRatioTabuSizeStrategy(0.5).determineTabuSize(stepScope));
+    }
+
+}
