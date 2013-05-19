d740ced5ac223e7ece7fb2040d2b0b189417ad05
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2013-05-19 10:37:49 +0200

    TspSimpleScoreCalculator

---
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/tsp/solver/score/TspSimpleScoreCalculator.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2013 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaplanner.examples.tsp.solver.score;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.optaplanner.core.api.score.buildin.simple.SimpleScore;
+import org.optaplanner.core.impl.score.director.simple.SimpleScoreCalculator;
+import org.optaplanner.examples.tsp.domain.Appearance;
+import org.optaplanner.examples.tsp.domain.Domicile;
+import org.optaplanner.examples.tsp.domain.TravelingSalesmanTour;
+import org.optaplanner.examples.tsp.domain.Visit;
+
+public class TspSimpleScoreCalculator implements SimpleScoreCalculator<TravelingSalesmanTour> {
+
+    public SimpleScore calculateScore(TravelingSalesmanTour tour) {
+        List<Visit> visitList = tour.getVisitList();
+        Set<Visit> tailVisitSet = new HashSet<Visit>(visitList);
+        int score = 0;
+        for (Visit visit : visitList) {
+            Appearance previousAppearance = visit.getPreviousAppearance();
+            if (previousAppearance != null) {
+                score -= visit.getDistanceToPreviousAppearance();
+                if (previousAppearance instanceof Visit) {
+                    tailVisitSet.remove(previousAppearance);
+                }
+            }
+        }
+        // TODO support more than 1 domicile
+        if (tour.getDomicileList().size() != 1) {
+            throw new UnsupportedOperationException(
+                    "The domicileList (" + tour.getDomicileList() + ") should be a singleton.");
+        }
+        Domicile domicile = tour.getDomicileList().get(0);
+        for (Visit tailVisit : tailVisitSet) {
+            if (tailVisit.getPreviousAppearance() != null) {
+                score -= domicile.getCity().getDistance(tailVisit.getCity());
+            }
+        }
+        return SimpleScore.valueOf(score);
+    }
+
+}
