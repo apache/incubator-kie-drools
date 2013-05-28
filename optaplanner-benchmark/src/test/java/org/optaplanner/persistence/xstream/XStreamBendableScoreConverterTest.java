3b1694bb465504e1bab782e3bed239e4d5dc1f79
Radovan Synek <rsynek@redhat.com>
2013-05-28 17:39:21 +0200

    XStreamBendableScoreConverterTest and XStreamScoreConverterTest (based on pull request thanks to rsynek)

--- optaplanner-benchmark/src/test/java/org/optaplanner/persistence/xstream/XStreamBendableScoreConverterTest.java
+++ optaplanner-benchmark/src/test/java/org/optaplanner/persistence/xstream/XStreamBendableScoreConverterTest.java
@@ -1,5 +1,63 @@
 package org.optaplanner.persistence.xstream;
 
+import java.io.Serializable;
+
+import com.thoughtworks.xstream.annotations.XStreamConverter;
+import org.junit.Test;
+import org.optaplanner.core.api.score.buildin.bendable.BendableScore;
+import org.optaplanner.core.impl.util.SerializationTestUtils;
+
+import static org.junit.Assert.assertEquals;
+
 public class XStreamBendableScoreConverterTest {
+
+    @Test
+    public void serializeAndDeserializeWithNullField() {
+        XStreamBendableScoreConverterTestObject input = new XStreamBendableScoreConverterTestObject(null);
+        SerializationTestUtils.serializeAndDeserializeWithAll(input,
+                new SerializationTestUtils.OutputAsserter<XStreamBendableScoreConverterTestObject>() {
+                    public void assertOutput(XStreamBendableScoreConverterTestObject output) {
+                        assertEquals(null, output.getScore());
+                    }
+                }
+        );
+    }
+
+    @Test
+    public void serializeAndDeserialize() {
+        XStreamBendableScoreConverterTestObject input = new XStreamBendableScoreConverterTestObject(
+                BendableScore.valueOf(new int[]{-5}, new int[]{-300, -4000}));
+        SerializationTestUtils.serializeAndDeserializeWithAll(input,
+                new SerializationTestUtils.OutputAsserter<XStreamBendableScoreConverterTestObject>() {
+                    public void assertOutput(XStreamBendableScoreConverterTestObject output) {
+                        BendableScore score = output.getScore();
+                        assertEquals(1, score.getHardLevelCount());
+                        assertEquals(-5, score.getHardScore(0));
+                        assertEquals(2, score.getSoftLevelCount());
+                        assertEquals(-300, score.getSoftScore(0));
+                        assertEquals(-4000, score.getSoftScore(1));
+                    }
+                }
+        );
+    }
+
+    public static class XStreamBendableScoreConverterTestObject implements Serializable {
+
+        @XStreamConverter(value = XStreamBendableScoreConverter.class, ints = {1, 2})
+        private BendableScore score;
+
+        public XStreamBendableScoreConverterTestObject(BendableScore score) {
+            this.score = score;
+        }
+
+        public BendableScore getScore() {
+            return score;
+        }
+
+        public void setScore(BendableScore score) {
+            this.score = score;
+        }
+    }
+
 
 }
--- optaplanner-benchmark/src/test/java/org/optaplanner/persistence/xstream/XStreamScoreConverterTest.java
+++ optaplanner-benchmark/src/test/java/org/optaplanner/persistence/xstream/XStreamScoreConverterTest.java
@@ -1,5 +1,57 @@
 package org.optaplanner.persistence.xstream;
 
+import java.io.Serializable;
+
+import com.thoughtworks.xstream.annotations.XStreamConverter;
+import org.junit.Test;
+import org.optaplanner.core.api.score.buildin.simple.SimpleScore;
+import org.optaplanner.core.impl.score.buildin.simple.SimpleScoreDefinition;
+import org.optaplanner.core.impl.util.SerializationTestUtils;
+
+import static org.junit.Assert.assertEquals;
+
 public class XStreamScoreConverterTest {
+
+    @Test
+    public void serializeAndDeserializeWithNullField() {
+        XStreamScoreConverterTestObject input = new XStreamScoreConverterTestObject(null);
+        SerializationTestUtils.serializeAndDeserializeWithAll(input,
+                new SerializationTestUtils.OutputAsserter<XStreamScoreConverterTestObject>() {
+                    public void assertOutput(XStreamScoreConverterTestObject output) {
+                        assertEquals(null, output.getScore());
+                    }
+                }
+        );
+    }
+
+    @Test
+    public void serializeAndDeserialize() {
+        XStreamScoreConverterTestObject input = new XStreamScoreConverterTestObject(SimpleScore.valueOf(123));
+        SerializationTestUtils.serializeAndDeserializeWithAll(input,
+                new SerializationTestUtils.OutputAsserter<XStreamScoreConverterTestObject>() {
+                    public void assertOutput(XStreamScoreConverterTestObject output) {
+                        assertEquals(123, output.getScore().getScore());
+                    }
+                }
+        );
+    }
+
+    public static class XStreamScoreConverterTestObject implements Serializable {
+
+        @XStreamConverter(value = XStreamScoreConverter.class, types = {SimpleScoreDefinition.class})
+        private SimpleScore score;
+
+        public XStreamScoreConverterTestObject(SimpleScore score) {
+            this.score = score;
+        }
+
+        public SimpleScore getScore() {
+            return score;
+        }
+
+        public void setScore(SimpleScore score) {
+            this.score = score;
+        }
+    }
 
 }
