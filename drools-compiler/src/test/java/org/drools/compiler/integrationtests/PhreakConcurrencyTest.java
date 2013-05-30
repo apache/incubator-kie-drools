9870b5cb3072542266f9829a40beabc9c7ef5fd0
mariofusco <mario.fusco@gmail.com>
2013-05-30 18:14:32 +0200

    [DROOLS-7] add concurrency test for path and segment masks

--- drools-compiler/src/test/java/org/drools/compiler/integrationtests/PhreakConcurrencyTest.java
+++ drools-compiler/src/test/java/org/drools/compiler/integrationtests/PhreakConcurrencyTest.java
@@ -2,6 +2,16 @@ package org.drools.compiler.integrationtests;
 
 import org.drools.compiler.CommonTestMethodBase;
 import org.drools.compiler.StockTick;
+import org.drools.core.common.InternalWorkingMemory;
+import org.drools.core.common.NamedEntryPoint;
+import org.drools.core.reteoo.AlphaNode;
+import org.drools.core.reteoo.BetaMemory;
+import org.drools.core.reteoo.BetaNode;
+import org.drools.core.reteoo.ObjectSink;
+import org.drools.core.reteoo.ObjectTypeNode;
+import org.drools.core.reteoo.PathMemory;
+import org.drools.core.reteoo.RuleTerminalNode;
+import org.drools.core.reteoo.SegmentMemory;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -17,12 +27,14 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletionService;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 
 import static java.util.Arrays.asList;
+import static org.drools.core.reteoo.ReteDumper.dumpRete;
 
 public class PhreakConcurrencyTest extends CommonTestMethodBase {
 
@@ -238,8 +250,7 @@ public class PhreakConcurrencyTest extends CommonTestMethodBase {
         }
     }
 
-    @Test
-    public void testMultipleConcurrentEPs3() {
+    private KieSession getKieSessionWith3Segments() {
         String str = "global java.util.List results\n" +
                      "rule R1 when\n" +
                      "   String( this == \"1\") from entry-point EP1\n" +
@@ -287,7 +298,12 @@ public class PhreakConcurrencyTest extends CommonTestMethodBase {
                      "end\n";
 
         KnowledgeBase kbase = loadKnowledgeBaseFromString(str);
-        final KieSession ksession = kbase.newStatefulKnowledgeSession();
+        return kbase.newStatefulKnowledgeSession();
+    }
+
+    @Test
+    public void testMultipleConcurrentEPs3() {
+        final KieSession ksession = getKieSessionWith3Segments();
 
         List<String> results = new ArrayList<String>();
         ksession.setGlobal("results", results);
@@ -360,6 +376,13 @@ public class PhreakConcurrencyTest extends CommonTestMethodBase {
         public Boolean call() throws Exception {
             SessionEntryPoint ep = ksession.getEntryPoint("EP" + index);
 
+            InternalWorkingMemory wm = ((NamedEntryPoint)ep).getInternalWorkingMemory();
+            ObjectTypeNode otn = ((NamedEntryPoint)ep).getEntryPointNode().getObjectTypeNodes().values().iterator().next();
+            AlphaNode alpha = (AlphaNode)otn.getSinkPropagator().getSinks()[0];
+            BetaNode beta = (BetaNode)alpha.getSinkPropagator().getSinks()[0];
+            BetaMemory memory = (BetaMemory) wm.getNodeMemory(beta);
+            memory.getSegmentMemory();
+
             for (int i = 0; i < 100; i++) {
                 Thread.sleep(RANDOM.nextInt(100));
                 if (fh == null) {
@@ -389,6 +412,270 @@ public class PhreakConcurrencyTest extends CommonTestMethodBase {
         public EPManipulator3 setDeleteIndex(int deleteIndex) {
             this.deleteIndex = deleteIndex;
             return this;
+        }
+    }
+
+    @Test
+    public void testMultipleConcurrentEPs4() {
+        final KieSession ksession = getKieSessionWith3Segments();
+
+        List<String> results = new ArrayList<String>();
+        ksession.setGlobal("results", results);
+
+        EPManipulator4[] epManipulators = new EPManipulator4[9];
+        CyclicBarrier barrier = new CyclicBarrier(9, new SegmentChecker(epManipulators));
+        for (int i = 0; i < 9; i++) {
+            epManipulators[i] = new EPManipulator4(ksession, i+1, barrier);
+        }
+
+        new Thread () {
+            public void run () {
+                ksession.fireUntilHalt();
+            }
+        }.start();
+
+        for (int deleteIndex = 0; deleteIndex < 11; deleteIndex++) {
+            boolean success = true;
+            CompletionService<Boolean> ecs = new ExecutorCompletionService<Boolean>(executor);
+            for (int i = 0; i < 9; i++) {
+                ecs.submit(epManipulators[i].setDeleteIndex(deleteIndex % 10));
+            }
+
+            for (int i = 0; i < 9; i++) {
+                try {
+                    success = ecs.take().get() && success;
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            assertTrue(success);
+        }
+
+        ksession.halt();
+    }
+
+    public static class EPManipulator4 implements Callable<Boolean> {
+
+        private static final Random RANDOM = new Random();
+
+        private final KieSession ksession;
+        private final int index;
+        private final CyclicBarrier barrier;
+
+        private int deleteIndex;
+
+        private FactHandle fh = null;
+
+        private final SessionEntryPoint ep;
+
+        public EPManipulator4(KieSession ksession, int index, CyclicBarrier barrier) {
+            this.ksession = ksession;
+            this.index = index;
+            this.barrier = barrier;
+            this.ep = ksession.getEntryPoint("EP" + index);
+        }
+
+        public Boolean call() throws Exception {
+            for (int i = 0; i < 100; i++) {
+                Thread.sleep(RANDOM.nextInt(100));
+                if (fh == null) {
+                    fh = ep.insert("" + index);
+                } else {
+                    if ( RANDOM.nextInt(100) < 70 ) {
+                        ep.delete(fh);
+                        fh = null;
+                    } else {
+                        ep.update(fh, "" + index);
+                    }
+                }
+
+                if (i % 10 == 9) {
+                    if (!barrier.isBroken()) {
+                        barrier.await();
+                    } else {
+                        fail("This is not a bug in phreak synchronization, but a for some reason the barrier is broken, run the test again");
+                        return false;
+                    }
+                }
+            }
+
+            if (index == deleteIndex) {
+                if (fh != null) {
+                    ep.delete(fh);
+                    fh = null;
+                }
+            } else if (fh == null) {
+                fh = ep.insert("" + index);
+            }
+
+            return true;
+        }
+
+        public EPManipulator4 setDeleteIndex(int deleteIndex) {
+            this.deleteIndex = deleteIndex;
+            return this;
+        }
+
+        public boolean isInserted() {
+            return fh != null;
+        }
+
+        public CyclicBarrier getBarrier() {
+            return barrier;
+        }
+
+        public SessionEntryPoint getEntryPoiny() {
+            return ep;
+        }
+    }
+
+    public static class SegmentChecker implements Runnable {
+
+        private final EPManipulator4[] epManipulators;
+
+        private PathMemory[] pathMemories;
+
+        public SegmentChecker(EPManipulator4[] epManipulators) {
+            this.epManipulators = epManipulators;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Sync point");
+            if (pathMemories == null) {
+                initPathMemories();
+            }
+
+            String s = "";
+            for (int i = 0; i < epManipulators.length; i++) {
+                if (epManipulators[i].isInserted()) {
+                    s = "1" + s;
+                } else {
+                    s = "0" + s;
+                }
+            }
+            System.out.println("Inserted facts mask = " + s);
+
+            // P0 = s0 + s1
+            SegmentMemory s0 = pathMemories[0].getSegmentMemories()[0];
+            SegmentMemory s1 = pathMemories[0].getSegmentMemories()[1];
+
+            // P1 = s0 + s2 + s3
+            SegmentMemory s2 = pathMemories[1].getSegmentMemories()[1];
+            SegmentMemory s3 = pathMemories[1].getSegmentMemories()[2];
+
+            // P2 = s0 + s2 + s4
+            SegmentMemory s4 = pathMemories[2].getSegmentMemories()[2];
+
+            long s0Mask = 1; // InitialFact is always present
+            long s1Mask = 0;
+            long s2Mask = 0;
+            long s3Mask = 0;
+            long s4Mask = 0;
+
+            if (epManipulators[0].isInserted()) {
+                s0Mask |= 2;
+            }
+            if (epManipulators[1].isInserted()) {
+                s0Mask |= 4;
+            }
+            if (epManipulators[2].isInserted()) {
+                s0Mask |= 8;
+            }
+            if (epManipulators[3].isInserted()) {
+                s1Mask |= 1;
+                s2Mask |= 2;
+            }
+            if (epManipulators[4].isInserted()) {
+                s1Mask |= 2;
+                s2Mask |= 4;
+            }
+            if (epManipulators[5].isInserted()) {
+                s1Mask |= 4;
+                s2Mask |= 8;
+            }
+            if (epManipulators[6].isInserted()) {
+                s1Mask |= 8;
+                s3Mask |= 1;
+                s4Mask |= 2;
+            }
+            if (epManipulators[7].isInserted()) {
+                s1Mask |= 16;
+                s3Mask |= 2;
+                s4Mask |= 4;
+            }
+            if (epManipulators[8].isInserted()) {
+                s1Mask |= 32;
+                s3Mask |= 4;
+                s4Mask |= 8;
+            }
+
+            assertEquals(s0Mask, s0.getLinkedNodeMask());
+            assertEquals(s1Mask, s1.getLinkedNodeMask());
+            assertEquals(s2Mask, s2.getLinkedNodeMask());
+            assertEquals(s3Mask, s3.getLinkedNodeMask());
+            assertEquals(s4Mask, s4.getLinkedNodeMask());
+
+            long p0Mask = 0;
+            long p1Mask = 0;
+            long p2Mask = 0;
+
+            if ((s0Mask & 15) == 15) {
+                assertTrue(s0.isSegmentLinked());
+                p0Mask |= 1;
+                p1Mask |= 1;
+                p2Mask |= 1;
+            } else {
+                assertFalse(s0.isSegmentLinked());
+            }
+
+            if ((s1Mask & 63) == 63) {
+                assertTrue(s1.isSegmentLinked());
+                p0Mask |= 2;
+            } else {
+                assertFalse(s1.isSegmentLinked());
+            }
+
+            if ((s2Mask & 14) == 14) {
+                assertTrue(s2.isSegmentLinked());
+                p1Mask |= 2;
+                p2Mask |= 2;
+            } else {
+                assertFalse(s2.isSegmentLinked());
+            }
+
+            if ((s3Mask & 7) == 7) {
+                assertTrue(s3.isSegmentLinked());
+                p1Mask |= 4;
+            } else {
+                assertFalse(s3.isSegmentLinked());
+            }
+
+            if ((s4Mask & 14) == 14) {
+                assertTrue(s4.isSegmentLinked());
+                p2Mask |= 4;
+            } else {
+                assertFalse(s4.isSegmentLinked());
+            }
+
+            assertEquals(p0Mask == 3, pathMemories[0].isRuleLinked());
+            assertEquals(p1Mask == 7, pathMemories[1].isRuleLinked());
+            assertEquals(p2Mask == 7, pathMemories[2].isRuleLinked());
+        }
+
+        private void initPathMemories() {
+            pathMemories = new PathMemory[3];
+            NamedEntryPoint ep = (NamedEntryPoint)epManipulators[8].getEntryPoiny();
+            InternalWorkingMemory wm = ((NamedEntryPoint)ep).getInternalWorkingMemory();
+            ObjectTypeNode otn = ((NamedEntryPoint)ep).getEntryPointNode().getObjectTypeNodes().values().iterator().next();
+            AlphaNode alpha = (AlphaNode)otn.getSinkPropagator().getSinks()[0];
+            ObjectSink[] sinks = alpha.getSinkPropagator().getSinks();
+            for (int i = 0; i < sinks.length; i++) {
+                BetaNode beta = (BetaNode)sinks[i];
+                RuleTerminalNode rtn = (RuleTerminalNode)beta.getSinkPropagator().getSinks()[0];
+                pathMemories[i] =  ( PathMemory ) wm.getNodeMemory(rtn);
+            }
         }
     }
 }
