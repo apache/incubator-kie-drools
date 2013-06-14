5c1c1ee9fdf3a17e7d669390cdcff4627d6ad658
mariofusco <mario.fusco@gmail.com>
2013-06-14 18:18:06 +0200

    fix drools-maven-plugin example

--- drools-maven-plugin-example/src/test/java/org/drools/kproject/KProjectTest.java
+++ drools-maven-plugin-example/src/test/java/org/drools/kproject/KProjectTest.java
@@ -2,7 +2,7 @@ package org.drools.kproject;
 
 import org.junit.Ignore;
 import org.junit.Test;
-import org.kie.KieServices;
+import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
