5161453bec00b79c43d4289906dacd6caac982ba
Mark Proctor <mproctor@codehaus.org>
2013-06-24 19:27:24 +0100

    DROOLS-7 Phreak -More fixes to AddRemoveRule, so now handles recursing through nodes without memory. Such as evals.

--- kogito-drools/drools-compiler/src/test/java/org/drools/compiler/phreak/RemoveRuleTest.java
+++ kogito-drools/drools-compiler/src/test/java/org/drools/compiler/phreak/RemoveRuleTest.java
@@ -6,6 +6,7 @@ import org.drools.core.common.NodeMemories;
 import org.drools.core.impl.KnowledgeBaseImpl;
 import org.drools.core.impl.StatefulKnowledgeSessionImpl;
 import org.drools.core.reteoo.BetaMemory;
+import org.drools.core.reteoo.EvalConditionNode;
 import org.drools.core.reteoo.JoinNode;
 import org.drools.core.reteoo.LeftInputAdapterNode;
 import org.drools.core.reteoo.LeftInputAdapterNode.LiaNodeMemory;
@@ -211,7 +212,72 @@ public class RemoveRuleTest {
 
         assertEquals( "r1", ((Match)list.get(6)).getRule().getName() );
         assertEquals(7, list.size() ); // only one more added, as second rule as removed
+    }
 
+    @Test
+    public void testPopulatedRuleWithEvals() throws Exception {
+        KnowledgeBase kbase1 = buildKnowledgeBase("r1", "   a:A() B() eval(1==1) eval(1==1) C(1;) \n");
+
+        ReteooWorkingMemoryInterface wm = ((StatefulKnowledgeSessionImpl)kbase1.newStatefulKnowledgeSession()).session;
+        List list = new ArrayList();
+        wm.setGlobal("list", list);
+
+        wm.insert(new A(1));
+        wm.insert(new A(2));
+        wm.insert(new A(3));
+        wm.insert(new B(1));
+        wm.insert(new C(1));
+        wm.insert(new C(2));
+        wm.insert(new D(1));
+        wm.insert(new E(1));
+        wm.fireAllRules();
+
+        assertEquals( 13, countNodeMemories(wm.getNodeMemories()));
+
+        kbase1.addKnowledgePackages( buildKnowledgePackage("r2", "   a:A() B() eval(1==1) eval(1==1) C(2;) \n") );
+        wm.fireAllRules();
+
+        ObjectTypeNode aotn = getObjectTypeNode(kbase1, A.class );
+        LeftInputAdapterNode liaNode = (LeftInputAdapterNode) aotn.getSinkPropagator().getSinks()[0];
+        JoinNode bNode = (JoinNode) liaNode.getSinkPropagator().getFirstLeftTupleSink();
+
+        EvalConditionNode e1 = (EvalConditionNode) bNode.getSinkPropagator().getFirstLeftTupleSink();
+        EvalConditionNode e2 = (EvalConditionNode) e1.getSinkPropagator().getFirstLeftTupleSink();
+
+        JoinNode c1Node = (JoinNode) e2.getSinkPropagator().getFirstLeftTupleSink();
+        JoinNode c2Node = (JoinNode) e2.getSinkPropagator().getLastLeftTupleSink();
+
+        LiaNodeMemory lm = ( LiaNodeMemory ) wm.getNodeMemory(liaNode);
+        SegmentMemory sm = lm.getSegmentMemory();
+
+        BetaMemory c1Mem = ( BetaMemory ) wm.getNodeMemory(c1Node);
+        assertSame( sm.getFirst(), c1Mem.getSegmentMemory());
+        assertEquals( 3, c1Mem.getLeftTupleMemory().size() );
+        assertEquals( 1, c1Mem.getRightTupleMemory().size() );
+
+        BetaMemory c2Mem = ( BetaMemory ) wm.getNodeMemory(c2Node);
+        SegmentMemory c2Smem =  sm.getFirst().getNext();
+        assertSame( c2Smem, c2Mem.getSegmentMemory());
+        assertEquals( 3, c2Mem.getLeftTupleMemory().size() );
+        assertEquals( 1, c2Mem.getRightTupleMemory().size() );
+        assertEquals(6, list.size() );
+
+
+        kbase1.removeRule("org.kie", "r2");
+        assertEquals( 13, countNodeMemories(wm.getNodeMemories()));
+
+        assertNull( sm.getFirst());
+
+        assertEquals( 0, c2Mem.getLeftTupleMemory().size() );
+        assertEquals( 0, c2Mem.getRightTupleMemory().size() );
+
+        assertSame( sm, c1Mem.getSegmentMemory()); // c1SMem repoints back to original Smem
+
+        wm.insert(new A(1));
+        wm.fireAllRules();
+
+        assertEquals( "r1", ((Match)list.get(6)).getRule().getName() );
+        assertEquals(7, list.size() ); // only one more added, as second rule as removed
     }
 
     @Test
