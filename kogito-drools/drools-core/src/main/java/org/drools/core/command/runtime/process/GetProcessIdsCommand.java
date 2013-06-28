14a8c2b2595a3635858def0363f72238028381a2
krisv <kris.verlaenen@gmail.com>
2013-06-28 04:25:40 +0200

    - adding two more process commands

---
+++ kogito-drools/drools-core/src/main/java/org/drools/core/command/runtime/process/GetProcessIdsCommand.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2010 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.core.command.runtime.process;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+import org.drools.core.command.impl.GenericCommand;
+import org.drools.core.command.impl.KnowledgeCommandContext;
+import org.kie.api.definition.process.Process;
+import org.kie.internal.command.Context;
+
+@XmlAccessorType(XmlAccessType.NONE)
+public class GetProcessIdsCommand
+    implements
+    GenericCommand<List<String>> {
+
+    public List<String> execute(Context context) {
+    	List<String> result = new ArrayList<String>();
+        for (Process p: ((KnowledgeCommandContext) context).getKieSession().getKieBase().getProcesses()) {
+        	result.add(p.getId());
+        }
+        return result;
+    }
+
+    public String toString() {
+        return "session.getKieBase().getProcesses();";
+    }
+
+}
---
+++ kogito-drools/drools-core/src/main/java/org/drools/core/command/runtime/process/SetProcessInstanceVariablesCommand.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2010 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.core.command.runtime.process;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import org.drools.core.command.impl.GenericCommand;
+import org.drools.core.command.impl.KnowledgeCommandContext;
+import org.drools.core.xml.jaxb.util.JaxbMapAdapter;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.process.ProcessInstance;
+import org.kie.api.runtime.process.WorkflowProcessInstance;
+import org.kie.internal.command.Context;
+
+@XmlAccessorType(XmlAccessType.NONE)
+public class SetProcessInstanceVariablesCommand implements GenericCommand<Void> {
+
+	@XmlAttribute(required = true)
+    private Long processInstanceId;
+
+    @XmlJavaTypeAdapter(JaxbMapAdapter.class)
+    @XmlElement(name="variables")
+    private Map<String, Object> variables = new HashMap<String, Object>();
+
+    public SetProcessInstanceVariablesCommand() {
+    }
+
+    public SetProcessInstanceVariablesCommand(long processInstanceId, 
+    		Map<String, Object> variables) {
+        this.processInstanceId = processInstanceId;
+        this.variables = variables;
+    }
+
+    public long getProcessInstanceId() {
+        return processInstanceId;
+    }
+
+    public void setProcessInstanceId(long processInstanceId) {
+        this.processInstanceId = processInstanceId;
+    }
+
+    public Map<String, Object> getVariables() {
+        return variables;
+    }
+
+    public void setVariables(Map<String, Object> variables) {
+        this.variables = variables;
+    }
+
+    public Void execute(Context context) {
+        KieSession ksession = ((KnowledgeCommandContext) context).getKieSession();
+        ProcessInstance processInstance = ksession.getProcessInstance(processInstanceId);
+        if (processInstance != null) {
+        	if (variables != null) {
+	        	for (Map.Entry<String, Object> entry: variables.entrySet()) {
+	        		((WorkflowProcessInstance) processInstance).setVariable(entry.getKey(), entry.getValue());
+	        	}
+        	}
+        }
+        return null;
+    }
+
+    public String toString() {
+        String result = "WorkflowProcessInstance processInstance = (WorkflowProcessInstance) session.getProcessInstance(" + processInstanceId + ");";
+        if (variables != null) {
+            for (Map.Entry<String, Object> entry: variables.entrySet()) {
+                result += "\nprocessInstance.setVariable(\"" + entry.getKey() + "\", " 
+            		+ (entry.getValue() == null ? "null" : entry.getValue().toString()) + ");";
+            }
+        }
+        return result;
+    }
+
+}
