12beaea6d35eef3d36c87ef486284bad17817cd2
mariofusco <mario.fusco@gmail.com>
2013-07-03 20:00:23 +0200

    [DROOLS-183] add test case creating a KieBuilder from a file system folder

---
+++ kogito-drools/drools-examples-api/default-kiesession/src/test/java/org/drools/example/api/defaultkiesession/DefaultKieSessionFromFSExampleTest.java
@@ -0,0 +1,56 @@
+package org.drools.example.api.defaultkiesession;
+
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.runtime.KieSession;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class DefaultKieSessionFromFSExampleTest {
+
+    @Test
+    public void testGo() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        go(ps);
+        ps.close();
+
+        String actual = new String(baos.toByteArray());
+        String expected = "" +
+                          "Dave: Hello, HAL. Do you read me, HAL?\n" +
+                          "HAL: Dave. I read you.\n";
+        assertEquals(expected, actual);
+    }
+
+    public void go(PrintStream out) {
+        String currentFolder = null;
+        try {
+            currentFolder = new File(".").getCanonicalPath();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        File rootFolder = null;
+        if (currentFolder.endsWith("drools")) {
+            rootFolder = new File("drools-examples-api/default-kiesession");
+        } else if (currentFolder.endsWith("drools-examples-api")) {
+            rootFolder = new File("default-kiesession");
+        } else {
+            rootFolder = new File(".");
+        }
+
+        KieServices ks = KieServices.Factory.get();
+        KieBuilder kieBuilder = ks.newKieBuilder(rootFolder).buildAll();
+
+        KieSession kSession = ks.newKieContainer(kieBuilder.getKieModule().getReleaseId()).newKieSession();
+        kSession.setGlobal("out", out);
+        kSession.insert(new Message("Dave", "Hello, HAL. Do you read me, HAL?"));
+        kSession.fireAllRules();
+    }
+}
