048ea3237dbecdbe75cce3a0a821e9ffb0fadd1e
mariofusco <mario.fusco@gmail.com>
2013-07-04 18:21:34 +0200

    [DROOLS-7] put trace logs in conditioned blocks

--- drools-core/src/main/java/org/drools/core/reteoo/NotNode.java
+++ drools-core/src/main/java/org/drools/core/reteoo/NotNode.java
@@ -229,7 +229,9 @@ public class NotNode extends BetaNode {
         if ( isStreamMode() ) {
             stagedDeleteWasEmpty = memory.getSegmentMemory().getTupleQueue().isEmpty();
             memory.getSegmentMemory().getTupleQueue().add(new RightTupleEntry(rightTuple, rightTuple.getPropagationContext(), memory ));
-            log.trace( "JoinNode delete queue={} size={} pctx={} lt={}", System.identityHashCode( memory.getSegmentMemory().getTupleQueue() ), memory.getSegmentMemory().getTupleQueue().size(), PropagationContextImpl.intEnumToString(rightTuple.getPropagationContext()), rightTuple );
+            if ( log.isTraceEnabled() ) {
+                log.trace( "JoinNode delete queue={} size={} pctx={} lt={}", System.identityHashCode( memory.getSegmentMemory().getTupleQueue() ), memory.getSegmentMemory().getTupleQueue().size(), PropagationContextImpl.intEnumToString(rightTuple.getPropagationContext()), rightTuple );
+            }
         } else {
             stagedDeleteWasEmpty = stagedRightTuples.addDelete( rightTuple );
         }
