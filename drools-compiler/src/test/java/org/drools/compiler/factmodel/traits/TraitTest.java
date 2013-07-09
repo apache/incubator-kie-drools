41a921ab076af61fde731c92ce51cd88c654fc10
mariofusco <mario.fusco@gmail.com>
2013-07-09 14:33:09 +0200

    [DROOLS-64] Primitive trait field initialization

--- drools-compiler/src/test/java/org/drools/compiler/factmodel/traits/TraitTest.java
+++ drools-compiler/src/test/java/org/drools/compiler/factmodel/traits/TraitTest.java
@@ -3409,10 +3409,12 @@ public class TraitTest extends CommonTestMethodBase {
                         "global java.util.List list; \n" +
                         "\n" +
                         "declare trait Foo\n" +
-                        " hardList : List = new ArrayList() " +
-                        " softList : List = new ArrayList() " +
-                        " moreList : List = new ArrayList() " +
-                        " otraList : List = new ArrayList() " +
+                        " hardList : List = new ArrayList() \n" +
+                        " softList : List = new ArrayList() \n" +
+                        " moreList : List = new ArrayList() \n" +
+                        " otraList : List = new ArrayList() \n" +
+                        " primFld : int = 3 \n" +
+                        " primDbl : double = 0.421 \n" +
                         "\n" +
                         "end\n" +
                         "" +
@@ -3433,12 +3435,14 @@ public class TraitTest extends CommonTestMethodBase {
                         "end\n" +
                         "" +
                         "rule Don when\n" +
-                        " $x : Foo( $h : hardList, $s : softList, $o : otraList, $m : moreList ) \n" +
+                        " $x : Foo( $h : hardList, $s : softList, $o : otraList, $m : moreList, $i : primFld, $d : primDbl ) \n" +
                         "then \n" +
                         " list.add( $h ); \n" +
                         " list.add( $s ); \n" +
                         " list.add( $o ); \n" +
                         " list.add( $m ); \n" +
+                        " list.add( $i ); \n" +
+                        " list.add( $d ); \n" +
                         " System.out.println( $x ); \n" +
                         "end\n" +
                         ""
@@ -3447,23 +3451,25 @@ public class TraitTest extends CommonTestMethodBase {
         StatefulKnowledgeSession ks = getSessionFromString( source );
         TraitFactory.setMode( mode, ks.getKieBase() );
 
-        List<List> list = new ArrayList<List>();
+        List list = new ArrayList();
         ks.setGlobal( "list", list );
         ks.fireAllRules();
 
-        assertEquals( 4, list.size() );
+        assertEquals( 6, list.size() );
         assertFalse( list.contains( null ) );
 
-        List hard = list.get( 0 );
-        List soft = list.get( 1 );
-        List otra = list.get( 2 );
-        List more = list.get( 3 );
+        List hard = (List) list.get( 0 );
+        List soft = (List) list.get( 1 );
+        List otra = (List) list.get( 2 );
+        List more = (List) list.get( 3 );
 
         assertTrue( hard.isEmpty() );
         assertTrue( soft.isEmpty() );
         assertEquals( more, Arrays.asList( 1, 2, 3 ) );
         assertEquals( otra, Arrays.asList( 42 ) );
-    }
 
+        assertTrue( list.contains( 3 ) );
+        assertTrue( list.contains( 0.421 ) );
+    }
 
 }
