578eb3d575b9562553b369e5cc00b152064b3248
Mark Proctor <mproctor@codehaus.org>
2013-07-23 05:12:04 +0100

    DROOLS-198 Separate Rete and Phreak -Fixed test, that broke one proper KieContainer isolation was done -temporarily removed drools-reteoo from pom

--- drools-compiler/src/test/java/org/drools/compiler/integrationtests/MiscTest2.java
+++ drools-compiler/src/test/java/org/drools/compiler/integrationtests/MiscTest2.java
@@ -1924,7 +1924,7 @@ public class MiscTest2 extends CommonTestMethodBase {
 
         FactType messageType2 = ksession2.getKieBase().getFactType("org.drools.test", "Message");
         Object message2 = messageType2.newInstance();
-        messageType.set(message2, "message", "Hello World");
+        messageType2.set(message2, "message", "Hello World");
 
         ksession2.insert(message2);
         assertEquals( 1, ksession2.fireAllRules() );
