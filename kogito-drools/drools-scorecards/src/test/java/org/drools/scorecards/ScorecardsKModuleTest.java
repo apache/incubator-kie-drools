51941319671a781224d24f03635c4185256c87ca
vinodkiran <vinodkiran@usa.net>
2013-07-24 16:56:40 +0530

    drools-scorecards: addition of a test for scorecard resource referenced by kmodule.xml

---
+++ kogito-drools/drools-scorecards/src/test/java/org/drools/scorecards/ScorecardsKModuleTest.java
@@ -0,0 +1,44 @@
+package org.drools.scorecards;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.definition.type.FactType;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+public class ScorecardsKModuleTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testScorecardFromKModule2() throws Exception {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieBase kBase = kContainer.getKieBase("namedkiesession");
+        assertNotNull(kBase);
+
+        KieSession kSession = kContainer.newKieSession("ksession1");
+        assertNotNull(kSession);
+
+        FactType scorecardType = kBase.getFactType( "org.drools.scorecards.example","SampleScore" );
+        assertNotNull(scorecardType);
+
+        DroolsScorecard scorecard = (DroolsScorecard) scorecardType.newInstance();
+        assertNotNull(scorecard);
+
+        scorecardType.set(scorecard, "age", 10);
+        kSession.insert( scorecard );
+        kSession.fireAllRules();
+        kSession.dispose();
+        //occupation = 5, age = 25, validLicence -1
+        assertEquals(29.0,scorecard.getCalculatedScore());
+    }
+
+}
