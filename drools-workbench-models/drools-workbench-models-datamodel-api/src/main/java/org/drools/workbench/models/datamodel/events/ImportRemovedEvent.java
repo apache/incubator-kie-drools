14b8398794037d2f3f37969e93af0263ee049ea6
Toni Rikkola <toni@rikkola.net>
2013-09-10 14:12:07 +0300

    BZ-986000 - DRL-to-RuleModel marshalling improvements - DataModelOracle moved
    (cherry picked from commit 206acb12c6f6638bb2625a4ea5618ac7874b7313)

---
+++ drools-workbench-models/drools-workbench-models-datamodel-api/src/main/java/org/drools/workbench/models/datamodel/events/ImportRemovedEvent.java
@@ -0,0 +1,31 @@
+package org.drools.workbench.models.datamodel.events;
+
+import org.drools.workbench.models.commons.shared.imports.Import;
+import org.kie.commons.validation.PortablePreconditions;
+import org.drools.workbench.models.datamodel.oracle.PackageDataModelOracle;
+
+/**
+ * An event signalling removal of an import
+ */
+public class ImportRemovedEvent {
+
+    private final Import item;
+    private final PackageDataModelOracle oracle;
+
+    public ImportRemovedEvent( final PackageDataModelOracle oracle,
+                               final Import item ) {
+        this.oracle = PortablePreconditions.checkNotNull( "oracle",
+                                                          oracle );
+        this.item = PortablePreconditions.checkNotNull( "item",
+                                                        item );
+    }
+
+    public Import getImport() {
+        return this.item;
+    }
+
+    public PackageDataModelOracle getDataModelOracle() {
+        return this.oracle;
+    }
+
+}
