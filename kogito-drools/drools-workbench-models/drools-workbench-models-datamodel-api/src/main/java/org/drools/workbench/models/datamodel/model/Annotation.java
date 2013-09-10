398b79c7d7e983ed0cac9d67dd39cabf1378e9ca
Toni Rikkola <toni@rikkola.net>
2013-09-10 14:12:07 +0300

    BZ-986000 - DRL-to-RuleModel marshalling improvements - DataModelOracle moved

---
+++ kogito-drools/drools-workbench-models/drools-workbench-models-datamodel-api/src/main/java/org/drools/workbench/models/datamodel/model/Annotation.java
@@ -0,0 +1,42 @@
+package org.drools.workbench.models.datamodel.model;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.kie.commons.validation.PortablePreconditions;
+
+/**
+ * Portable representation of an annotation
+ */
+public class Annotation {
+
+    private String qualifiedTypeName;
+    private Map<String, String> attributes = new HashMap<String, String>();
+
+    public Annotation() {
+        //Needed for Errai marshalling
+    }
+
+    public Annotation( final String qualifiedTypeName ) {
+        PortablePreconditions.checkNotNull( "qualifiedTypeName",
+                                            qualifiedTypeName );
+        this.qualifiedTypeName = qualifiedTypeName;
+    }
+
+    public String getQualifiedTypeName() {
+        return qualifiedTypeName;
+    }
+
+    public Map<String, String> getAttributes() {
+        return Collections.unmodifiableMap( attributes );
+    }
+
+    public void addAttribute( final String name,
+                              final String value ) {
+        PortablePreconditions.checkNotNull( "name",
+                                            name );
+        this.attributes.put( name,
+                             value );
+    }
+}
