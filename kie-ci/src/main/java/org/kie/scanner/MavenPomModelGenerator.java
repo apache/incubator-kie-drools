bcbfd88b374480d460ed5090604eeef4b51baf49
mariofusco <mario.fusco@gmail.com>
2013-10-01 18:34:01 +0200

    [DROOLS-266][BZ-1014157] use native mavel pom parser when kie-ci is on the classpath

---
+++ kie-ci/src/main/java/org/kie/scanner/MavenPomModelGenerator.java
@@ -0,0 +1,45 @@
+package org.kie.scanner;
+
+import org.apache.maven.model.Dependency;
+import org.apache.maven.project.MavenProject;
+import org.drools.compiler.kproject.ReleaseIdImpl;
+import org.drools.compiler.kproject.xml.PomModel;
+import org.drools.compiler.kproject.xml.PomModelGenerator;
+
+import java.io.InputStream;
+
+import static org.kie.scanner.embedder.MavenProjectLoader.parseMavenPom;
+
+public class MavenPomModelGenerator implements PomModelGenerator {
+
+    @Override
+    public PomModel parse(String path, InputStream pomStream) {
+        PomModel pomModel = new PomModel();
+        MavenProject mavenProject = parseMavenPom(pomStream);
+
+        pomModel.setReleaseId(new ReleaseIdImpl(mavenProject.getGroupId(),
+                                                mavenProject.getArtifactId(),
+                                                mavenProject.getVersion()));
+        try {
+            MavenProject parentProject = mavenProject.getParent();
+            if (parentProject != null) {
+                pomModel.setParentReleaseId(new ReleaseIdImpl(parentProject.getGroupId(),
+                                                              parentProject.getArtifactId(),
+                                                              parentProject.getVersion()));
+            }
+        } catch (Exception e) {
+            // ignore
+        }
+
+        for (Dependency dep : mavenProject.getDependencies()) {
+            String scope = dep.getScope();
+            if ("provided".equals(scope) || "test".equals(scope)) {
+                continue;
+            }
+            pomModel.addDependency(new ReleaseIdImpl(dep.getGroupId(),
+                                                     dep.getArtifactId(),
+                                                     dep.getVersion()));
+        }
+        return pomModel;
+    }
+}
