8853e398561e62fb76d0ec9e3b400f06fa905a47
sotty <dsotty@gmail.com>
2013-10-23 16:39:04 -0700

    [DROOLS-223] Serializing and Deserializing a PackageDescr causes imports to change to import null (cherry picked from commit eadcf4c95dea135973c1e496b39a0979ef860f05)

--- drools-compiler/src/test/java/org/drools/compiler/lang/descr/PackageDescrTest.java
+++ drools-compiler/src/test/java/org/drools/compiler/lang/descr/PackageDescrTest.java
@@ -1,8 +1,18 @@
 package org.drools.compiler.lang.descr;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.util.List;
 import java.util.Map;
 
+import org.drools.compiler.Person;
+import org.drools.compiler.lang.api.DescrFactory;
+import org.drools.compiler.lang.api.PackageDescrBuilder;
 import org.drools.compiler.lang.descr.AttributeDescr;
 import org.drools.compiler.lang.descr.PackageDescr;
 import org.drools.compiler.lang.descr.RuleDescr;
@@ -36,5 +46,37 @@ public class PackageDescrTest {
         assertEquals("default", ((AttributeDescr)ruleAts.get( "foo2" )).getValue());
         
     }
-    
+
+    @Test
+    public void testSerialization() {
+        PackageDescrBuilder builder = DescrFactory.newPackage().name( "foo" );
+        String className = Person.class.getName();
+        builder.newImport().target(className).end();
+        PackageDescr descr = builder.getDescr();
+
+        ImportDescr importDescr = new ImportDescr(className);
+        ImportDescr badImportDescr = new ImportDescr(null);
+
+        assertTrue(descr.getImports().contains(importDescr));
+
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutput out = new ObjectOutputStream(baos);
+            descr.writeExternal(out);
+
+            ObjectInput in = new ObjectInputStream(new ByteArrayInputStream(baos.toByteArray()));
+            PackageDescr newDescr = new PackageDescr();
+            newDescr.readExternal(in);
+
+            assertFalse(newDescr.getImports().contains(badImportDescr));
+            assertTrue(newDescr.getImports().contains(importDescr));
+
+        } catch ( IOException ioe ) {
+            fail( ioe.getMessage() );
+        } catch ( ClassNotFoundException cnfe ) {
+            fail( cnfe.getMessage() );
+        }
+
+    }
+
 }
