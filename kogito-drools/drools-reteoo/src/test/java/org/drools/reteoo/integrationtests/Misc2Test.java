653f3b696395310381db918246ae340dc910d187
sotty <dsotty@gmail.com>
2013-10-23 15:54:00 -0700

    [DROOLS-131] Test cases

--- kogito-drools/drools-reteoo/src/test/java/org/drools/reteoo/integrationtests/Misc2Test.java
+++ kogito-drools/drools-reteoo/src/test/java/org/drools/reteoo/integrationtests/Misc2Test.java
@@ -2572,4 +2572,39 @@ public class Misc2Test extends CommonTestMethodBase {
         ksession.fireAllRules();
         assertEquals(1, students.size());
     }
+
+    @Test
+    public void testFactLeak() throws InterruptedException {
+        //DROOLS-131
+        String drl = "package org.drools.test; \n" +
+                     "global java.util.List list; \n" +
+                     "" +
+                     "" +
+                     "rule Intx when\n" +
+                     "  $x : Integer() from entry-point \"x\" \n" +
+                     "then\n" +
+                     "  list.add( $x ); \n" +
+                     "end";
+        int N = 1100;
+
+        KnowledgeBase kb = loadKnowledgeBaseFromString( drl );
+        final StatefulKnowledgeSession ks = kb.newStatefulKnowledgeSession();
+        ArrayList list = new ArrayList();
+        ks.setGlobal( "list", list );
+
+        new Thread () {
+            public void run () {
+                ks.fireUntilHalt();
+            }
+        }.start ();
+
+        for ( int j = 0; j < N; j++ ) {
+            ks.getEntryPoint( "x" ).insert( new Integer( j ) );
+        }
+
+        Thread.sleep( 1000 );
+        ks.halt();
+
+        assertEquals( N, list.size() );
+    }
 }
