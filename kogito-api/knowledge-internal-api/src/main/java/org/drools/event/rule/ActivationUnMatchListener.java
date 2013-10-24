4a246008b31bdc6286d5d866b63a0692edbc3f6e
mariofusco <mario.fusco@gmail.com>
2013-10-24 10:00:35 +0200

    remove old and no longer useful knowledge-api

--- kogito-api/knowledge-internal-api/src/main/java/org/drools/event/rule/ActivationUnMatchListener.java
+++
@@ -1,8 +0,0 @@
-package org.drools.core.event.rule;
-
-import org.drools.core.runtime.rule.Activation;
-import org.drools.core.runtime.rule.WorkingMemory;
-
-public interface ActivationUnMatchListener {
-    public void unMatch(WorkingMemory wm, Activation activation);
-}
