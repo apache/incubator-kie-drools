d4b3061556de2716307580c384c275040f18190e
mariofusco <mario.fusco@gmail.com>
2013-10-24 17:06:59 +0200

    make legacy drools 5 api to always run with rete + remove no longer implemented KnowledgeAgent

--- kogito-drools/knowledge-api-legacy5-adapter/src/main/java/org/drools/impl/KnowledgeBaseFactoryServiceImpl.java
+++ kogito-drools/knowledge-api-legacy5-adapter/src/main/java/org/drools/impl/KnowledgeBaseFactoryServiceImpl.java
@@ -2,13 +2,17 @@ package org.drools.impl;
 
 import org.drools.KnowledgeBase;
 import org.drools.KnowledgeBaseConfiguration;
+import org.drools.KnowledgeBaseFactory;
 import org.drools.KnowledgeBaseFactoryService;
+import org.drools.conf.KnowledgeBaseOption;
 import org.drools.impl.adapters.EnvironmentAdapter;
 import org.drools.impl.adapters.KnowledgeBaseAdapter;
 import org.drools.impl.adapters.KnowledgeBaseConfigurationAdapter;
 import org.drools.impl.adapters.KnowledgeSessionConfigurationAdapter;
 import org.drools.runtime.Environment;
 import org.drools.runtime.KnowledgeSessionConfiguration;
+import org.kie.api.KieBaseConfiguration;
+import org.kie.internal.builder.conf.RuleEngineOption;
 
 import java.util.Properties;
 
@@ -33,19 +37,23 @@ public class KnowledgeBaseFactoryServiceImpl implements KnowledgeBaseFactoryServ
     }
 
     public KnowledgeBase newKnowledgeBase() {
-        return new KnowledgeBaseAdapter(delegate.newKnowledgeBase());
+        return newKnowledgeBase(KnowledgeBaseFactory.newKnowledgeBaseConfiguration());
     }
 
     public KnowledgeBase newKnowledgeBase(String kbaseId) {
-        return new KnowledgeBaseAdapter(delegate.newKnowledgeBase(kbaseId));
+        return newKnowledgeBase(kbaseId, KnowledgeBaseFactory.newKnowledgeBaseConfiguration());
     }
 
     public KnowledgeBase newKnowledgeBase(KnowledgeBaseConfiguration conf) {
-        return new KnowledgeBaseAdapter(delegate.newKnowledgeBase(((KnowledgeBaseConfigurationAdapter)conf).getDelegate()));
+        KieBaseConfiguration kieBaseConf = ((KnowledgeBaseConfigurationAdapter) conf).getDelegate();
+        kieBaseConf.setOption(RuleEngineOption.RETEOO);
+        return new KnowledgeBaseAdapter(delegate.newKnowledgeBase(kieBaseConf));
     }
 
     public KnowledgeBase newKnowledgeBase(String kbaseId, KnowledgeBaseConfiguration conf) {
-        return new KnowledgeBaseAdapter(delegate.newKnowledgeBase(kbaseId, ((KnowledgeBaseConfigurationAdapter)conf).getDelegate()));
+        KieBaseConfiguration kieBaseConf = ((KnowledgeBaseConfigurationAdapter) conf).getDelegate();
+        kieBaseConf.setOption(RuleEngineOption.RETEOO);
+        return new KnowledgeBaseAdapter(delegate.newKnowledgeBase(kbaseId, kieBaseConf));
     }
 
     public Environment newEnvironment() {
