1f2a3d549f1a08850894dd4c5e05d4c54d79e75f
mariofusco <mario.fusco@gmail.com>
2013-11-15 17:05:15 +0100

    [DROOLS-341] refresh KieModuleMetaData after an incremental build

--- kogito-drools/kie-ci/src/test/java/org/kie/scanner/KieModuleIncrementalCompilationTest.java
+++ kogito-drools/kie-ci/src/test/java/org/kie/scanner/KieModuleIncrementalCompilationTest.java
@@ -17,8 +17,7 @@ import static junit.framework.Assert.*;
 public class KieModuleIncrementalCompilationTest extends AbstractKieCiTest {
 
     @Test
-    @Ignore()
-    public void testDeletedFileToni() throws Exception {
+    public void testCheckMetaDataAfterIncrementalDelete() throws Exception {
         String drl1 = "package org.kie.scanner\n" +
                 "rule R1 when\n" +
                 "   String()\n" +
@@ -39,13 +38,11 @@ public class KieModuleIncrementalCompilationTest extends AbstractKieCiTest {
 
         KieBuilder kieBuilder = ks.newKieBuilder( kfs ).buildAll();
 
-        KieModuleMetaData.Factory.newKieModuleMetaData( ( (InternalKieBuilder) kieBuilder ).getKieModuleIgnoringErrors() );
-
         assertEquals(2, getRuleNames(kieBuilder).get("org.kie.scanner").size());
 
-        kfs.delete("src/main/resources/KBase1/r2.drl");
+        kfs.delete("src/main/resources/r2.drl");
 
-        IncrementalResults addResults = ((InternalKieBuilder) kieBuilder).createFileSet("src/main/resources/KBase1/r2.drl").build();
+        IncrementalResults addResults = ((InternalKieBuilder) kieBuilder).createFileSet("src/main/resources/r2.drl").build();
 
         assertEquals(1, getRuleNames(kieBuilder).get("org.kie.scanner").size());
     }
@@ -54,14 +51,9 @@ public class KieModuleIncrementalCompilationTest extends AbstractKieCiTest {
         KieModuleMetaData kieModuleMetaData = getKieModuleMetaData(kieBuilder);
         HashMap<String, Collection<String>> ruleNames = new HashMap<String, Collection<String>>();
 
-
-        // BUG Nro. 1. getPackages() only returns the packages that have classes in them, should also return the packages that only have rules in them.
         for (String packageName : kieModuleMetaData.getPackages()) {
             ruleNames.put(packageName, kieModuleMetaData.getRuleNamesInPackage(packageName));
         }
-
-        // Temporal fix for the missing package names.
-        ruleNames.put("org.kie.scanner", kieModuleMetaData.getRuleNamesInPackage("org.kie.scanner"));
 
         return ruleNames;
     }
