6262fedac4732533d980c9209cb0eb089c84dbd6
mariofusco <mario.fusco@gmail.com>
2013-11-21 18:08:30 +0100

    [BZ-1029911][DROOLS-302] add test cases reproducing minor mvel issues

---
+++ kogito-drools/drools-compiler/src/test/java/org/drools/compiler/ScenarioType.java
@@ -0,0 +1,8 @@
+package org.drools.compiler;
+
+public class ScenarioType {
+    public static class Set {
+        public static final ScenarioType ADD = new ScenarioType();
+        public static final ScenarioType EDIT = new ScenarioType();
+    }
+}
