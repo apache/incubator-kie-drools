aa7b303a58a79abb5fb135136c0fba80da908bfa
mariofusco <mario.fusco@gmail.com>
2013-11-28 14:56:14 +0100

    [BZ-1024876] fix NPE when resource is added to kfs with unspecified path and rule in xls contains timer and calendar

---
+++ kogito-drools/drools-decisiontables/src/test/java/org/drools/decisiontable/CalendarTimerResourcesTest.java
@@ -0,0 +1,79 @@
+package org.drools.decisiontable;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+import org.drools.core.time.SessionPseudoClock;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.model.KieBaseModel;
+import org.kie.api.builder.model.KieModuleModel;
+import org.kie.api.conf.EventProcessingOption;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.conf.ClockTypeOption;
+
+public class CalendarTimerResourcesTest {
+
+    private KieSession ksession;
+
+    private SessionPseudoClock clock;
+
+    @Before
+    public void init() {
+
+        final KieServices ks = KieServices.Factory.get();
+        KieFileSystem kfs = ks.newKieFileSystem();
+        KieModuleModel kmodule = ks.newKieModuleModel();
+
+        KieBaseModel baseModel = kmodule.newKieBaseModel("defaultKBase")
+                                        .setDefault(true)
+                                        .setEventProcessingMode(EventProcessingOption.STREAM);
+        baseModel.newKieSessionModel("defaultKSession")
+                 .setDefault(true)
+                 .setClockType(ClockTypeOption.get("pseudo"));
+
+        kfs.writeKModuleXML(kmodule.toXML());
+        kfs.write(ks.getResources().newClassPathResource("calendar_timer.xls", this.getClass())); // README when path is set then test works
+        KieBuilder kieBuilder = ks.newKieBuilder( kfs ).buildAll();
+        assertEquals( 0, kieBuilder.getResults().getMessages( org.kie.api.builder.Message.Level.ERROR ).size() );
+
+        ksession = ks.newKieContainer(ks.getRepository().getDefaultReleaseId()).newKieSession();
+
+        clock = ksession.getSessionClock();
+    }
+
+    @Test
+    public void test() {
+
+        ksession.getCalendars().set("tuesday", TUESDAY);
+        clock.advanceTime(4, TimeUnit.DAYS); // README now it is set to monday (test fails with NPE), when is set to tuesday (rule should fire) then test works
+        ksession.fireAllRules();
+    }
+
+    @After
+    public void clear() {
+
+        if (ksession != null) {
+            ksession.dispose();
+        }
+    }
+
+    private static final org.kie.api.time.Calendar TUESDAY = new org.kie.api.time.Calendar() {
+
+        @Override
+        public boolean isTimeIncluded(long timestamp) {
+            Calendar c = Calendar.getInstance();
+            c.setTimeInMillis(timestamp);
+
+            int day = c.get(Calendar.DAY_OF_WEEK);
+            return day == Calendar.TUESDAY;
+        }
+    };
+}
