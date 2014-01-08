c64c16e129b977cd0c6c107c11ce24313e22e627
mariofusco <mario.fusco@gmail.com>
2014-01-08 09:04:50 +0100

    [BZ-1042867] fix ProjectClassLoader when running in osgi

--- kogito-drools/drools-compiler/src/main/java/org/drools/compiler/compiler/PackageBuilderConfiguration.java
+++ kogito-drools/drools-compiler/src/main/java/org/drools/compiler/compiler/PackageBuilderConfiguration.java
@@ -394,7 +394,7 @@ public class PackageBuilderConfiguration
     /** Use this to override the classLoader that will be used for the rules. */
     private void setClassLoader(ClassLoader classLoader) {
         this.classLoader = ProjectClassLoader.getClassLoader( classLoader,
-                                                              null,
+                                                              getClass(),
                                                               isClassLoaderCacheEnabled() );
     }
 
