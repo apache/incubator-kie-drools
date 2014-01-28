7f2645c6dd0d8eaee3f2f9aedfafacf8fb0082a6
mariofusco <mario.fusco@gmail.com>
2014-01-28 12:04:11 +0100

    [BZ-1058322] add adapters of kbuilder, ksession and kbase options for legacy 5 API

---
+++ knowledge-api-legacy5-adapter/src/main/java/org/drools/impl/adapters/AccumulateFunctionAdapter.java
@@ -0,0 +1,63 @@
+package org.drools.impl.adapters;
+
+import org.drools.compiler.lang.dsl.DSLMapParser;
+import org.kie.api.runtime.rule.AccumulateFunction;
+
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.io.Serializable;
+
+public class AccumulateFunctionAdapter implements AccumulateFunction {
+
+    private final org.drools.runtime.rule.AccumulateFunction delegate;
+
+    public AccumulateFunctionAdapter(org.drools.runtime.rule.AccumulateFunction delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public Serializable createContext() {
+        return delegate.createContext();
+    }
+
+    @Override
+    public void init(Serializable context) throws Exception {
+        delegate.init(context);
+    }
+
+    @Override
+    public void accumulate(Serializable context, Object value) {
+        delegate.accumulate(context, value);
+    }
+
+    @Override
+    public void reverse(Serializable context, Object value) throws Exception {
+        delegate.reverse(context, value);
+    }
+
+    @Override
+    public Object getResult(Serializable context) throws Exception {
+        return delegate.getResult(context);
+    }
+
+    @Override
+    public boolean supportsReverse() {
+        return delegate.supportsReverse();
+    }
+
+    @Override
+    public Class<?> getResultType() {
+        return null;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        delegate.writeExternal(out);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        delegate.readExternal(in);
+    }
+}
