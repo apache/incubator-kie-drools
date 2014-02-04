82f30e5f691949459896ef4d65d08ff70c6fa0b8
Michael Anstis <michael.anstis@gmail.com>
2014-02-04 20:05:40 +0000

    Remove test erroneously merged into 6.0.x

--- drools-workbench-models/drools-workbench-models-commons/src/test/java/org/drools/workbench/models/commons/backend/rule/RuleModelDRLPersistenceUnmarshallingTest.java
+++ drools-workbench-models/drools-workbench-models-commons/src/test/java/org/drools/workbench/models/commons/backend/rule/RuleModelDRLPersistenceUnmarshallingTest.java
@@ -21,7 +21,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import junit.framework.Assert;
 import org.drools.workbench.models.datamodel.oracle.DataType;
 import org.drools.workbench.models.datamodel.oracle.FieldAccessorsAndMutators;
 import org.drools.workbench.models.datamodel.oracle.MethodInfo;
@@ -38,7 +37,6 @@ import org.drools.workbench.models.datamodel.rule.CompositeFactPattern;
 import org.drools.workbench.models.datamodel.rule.CompositeFieldConstraint;
 import org.drools.workbench.models.datamodel.rule.DSLComplexVariableValue;
 import org.drools.workbench.models.datamodel.rule.DSLSentence;
-import org.drools.workbench.models.datamodel.rule.DSLVariableValue;
 import org.drools.workbench.models.datamodel.rule.ExpressionField;
 import org.drools.workbench.models.datamodel.rule.ExpressionFormLine;
 import org.drools.workbench.models.datamodel.rule.ExpressionPart;
@@ -2789,42 +2787,6 @@ public class RuleModelDRLPersistenceUnmarshallingTest {
         RuleAttribute attribute = m.attributes[ 0 ];
         assertEquals( "calendars", attribute.getAttributeName() );
         assertEquals( "a, b", attribute.getValue() );
-    }
-
-    @Test
-    public void testFromRestrictions() {
-        String drl = "package org.mortgages;\n" +
-                "\n" +
-                "import java.lang.Number;\n" +
-                "rule \"Test\"\n" +
-                "  dialect \"mvel\"\n" +
-                "  when\n" +
-                "    reserva : Reserva( )\n" +
-                "    itinerario : Itinerario( destino == \"USA\" ) from reserva.itinerarios\n" +
-                "  then\n" +
-                "end\n";
-
-        addModelField( "org.mortgages.Reserva",
-                       "itinerarios",
-                       "java.lang.List",
-                       DataType.TYPE_COLLECTION );
-        addModelField( "org.mortgages.Itinerario",
-                       "destino",
-                       "String",
-                       DataType.TYPE_STRING );
-
-        RuleModel m = RuleModelDRLPersistenceImpl.getInstance().unmarshal( drl,
-                                                                           dmo );
-        assertTrue( m.lhs[ 1 ] instanceof FromCompositeFactPattern );
-        FromCompositeFactPattern factPattern = (FromCompositeFactPattern) m.lhs[ 1 ];
-        assertNotNull( factPattern.getFactPattern().getConstraintList() );
-        assertEquals( 1, factPattern.getFactPattern().getConstraintList().getNumberOfConstraints() );
-        SingleFieldConstraint constraint = (SingleFieldConstraint) factPattern.getFactPattern().getFieldConstraints()[ 0 ];
-        assertEquals( "Itinerario", constraint.getFactType() );
-        assertEquals( "destino", constraint.getFieldName() );
-        assertEquals( DataType.TYPE_STRING, constraint.getFieldType() );
-        assertEquals( "USA", constraint.getValue() );
-        assertEquals( "==", constraint.getOperator() );
     }
 
     @Test
