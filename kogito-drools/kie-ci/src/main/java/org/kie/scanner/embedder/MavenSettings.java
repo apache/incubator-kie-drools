07d6c58dbb84b76cb3000c4932444bdec39ab221
mariofusco <mario.fusco@gmail.com>
2014-02-19 11:08:17 +0100

    [BZ-1065984] make MavenEmbedder to read user-provided Maven settings

--- kogito-drools/kie-ci/src/main/java/org/kie/scanner/embedder/MavenSettings.java
+++ kogito-drools/kie-ci/src/main/java/org/kie/scanner/embedder/MavenSettings.java
@@ -17,40 +17,24 @@ public class MavenSettings {
     private static final String CUSTOM_SETTINGS_PROPERTY = "kie.maven.settings.custom";
 
     private static class SettingsHolder {
-        private static final Settings settings = initSettings();
+        private static final File userSettingsFile = initUserSettingsFile();
+        private static final Settings settings = initSettings(userSettingsFile);
+    }
+
+    public static File getUserSettingsFile() {
+        return SettingsHolder.userSettingsFile;
     }
 
     public static Settings getSettings() {
         return SettingsHolder.settings;
     }
 
-    private static Settings initSettings() {
+    private static Settings initSettings(File userSettingsFile) {
         SettingsBuilder settingsBuilder = new DefaultSettingsBuilderFactory().newInstance();
-
         DefaultSettingsBuildingRequest request = new DefaultSettingsBuildingRequest();
 
-        boolean useCustomSettingsLocation = false;
-        String customSettings = System.getProperty( CUSTOM_SETTINGS_PROPERTY );
-        if (customSettings != null) {
-            File customSettingsFile = new File( customSettings );
-            if (customSettingsFile.exists()) {
-                request.setUserSettingsFile( customSettingsFile );
-                useCustomSettingsLocation = true;
-            } else {
-                log.warn("Cannot find custom maven settings file: " + customSettings);
-            }
-        }
-
-        String userHome = System.getProperty( "user.home" );
-        if ( !useCustomSettingsLocation ) {
-            if (userHome != null) {
-                File userSettingsFile = new File( userHome + "/.m2/settings.xml" );
-                if (userSettingsFile.exists()) {
-                    request.setUserSettingsFile( userSettingsFile );
-                }
-            } else {
-                log.warn("User home is not set");
-            }
+        if (userSettingsFile != null) {
+            request.setUserSettingsFile( userSettingsFile );
         }
 
         String mavenHome = System.getenv( "M2_HOME" );
@@ -63,7 +47,6 @@ public class MavenSettings {
             log.warn("Environment variable M2_HOME is not set");
         }
 
-
         request.setSystemProperties( System.getProperties() );
 
         Settings settings = null;
@@ -74,6 +57,7 @@ public class MavenSettings {
         }
 
         if (settings.getLocalRepository() == null) {
+            String userHome = System.getProperty( "user.home" );
             if (userHome != null) {
                 settings.setLocalRepository( userHome + "/.m2/repository" );
             } else {
@@ -84,4 +68,27 @@ public class MavenSettings {
         return settings;
     }
 
+    private static File initUserSettingsFile() {
+        String customSettings = System.getProperty( CUSTOM_SETTINGS_PROPERTY );
+        if (customSettings != null) {
+            File customSettingsFile = new File( customSettings );
+            if (customSettingsFile.exists()) {
+                return customSettingsFile;
+            } else {
+                log.warn("Cannot find custom maven settings file: " + customSettings);
+            }
+        }
+
+        String userHome = System.getProperty( "user.home" );
+        if (userHome != null) {
+            File userSettingsFile = new File( userHome + "/.m2/settings.xml" );
+            if (userSettingsFile.exists()) {
+                return userSettingsFile;
+            }
+        } else {
+            log.warn("User home is not set");
+        }
+
+        return null;
+    }
 }
