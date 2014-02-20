b5cdfde71e9d746ae016f65d2bb518810ba081d2
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2014-02-20 17:42:10 -0300

    BZ-1060747: adapted existing code in order to make it work in different cdi implementations

--- kogito-jbpm/jbpm-services/jbpm-kie-services/src/main/java/org/jbpm/kie/services/cdi/producer/HumanTaskServiceProducer.java
+++ kogito-jbpm/jbpm-services/jbpm-kie-services/src/main/java/org/jbpm/kie/services/cdi/producer/HumanTaskServiceProducer.java
@@ -1,7 +1,6 @@
 package org.jbpm.kie.services.cdi.producer;
 
 import java.util.HashSet;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.AmbiguousResolutionException;
 import javax.enterprise.inject.Any;
@@ -22,99 +21,101 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * CDI producer for <code>TaskService</code> instances. By default it runs in new mode, 
+ * CDI producer for <code>TaskService</code> instances. By default it runs in new mode,
  * meaning new <code>TaskService</code> instance for every injection point.
- * This behavior can be altered by setting <code>org.jbpm.cdi.taskservice.mode</code> system 
+ * This behavior can be altered by setting <code>org.jbpm.cdi.taskservice.mode</code> system
  * property to one of the values.
  * <ul>
- * 	<li>none - disables producer to not return TaskService instances</li>
- * 	<li>singleton - produces only one instance of TaskService that will be shared</li>
- * 	<li>new - produces new instance for every injection point</li>
+ * <li>none - disables producer to not return TaskService instances</li>
+ * <li>singleton - produces only one instance of TaskService that will be shared</li>
+ * <li>new - produces new instance for every injection point</li>
  * </ul>
  * This bean accept following injections:
  * <ul>
- * 	<li>UserGroupCallback</li>
- * 	<li>UserInfo</li>
- * 	<li>TaskLifeCycleEventListener</li>
+ * <li>UserGroupCallback</li>
+ * <li>UserInfo</li>
+ * <li>TaskLifeCycleEventListener</li>
  * </ul>
  * all of these are optional injections and if not available defaults will be used. Underneath it uses
  * <code>HumanTaskConfigurator</code> for <code>TaskService</code> instances creations.
- * 
  * @see HumanTaskConfigurator
  */
 @ApplicationScoped
 public class HumanTaskServiceProducer {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HumanTaskServiceProducer.class);
-	final String mode = System.getProperty("org.jbpm.cdi.taskservice.mode", "new");
-	
-	@Inject
-	private Instance<UserGroupCallback> userGroupCallback;
-	
-	@Inject
-	private Instance<UserInfo> userInfo;
-	
-	@Inject
-	@Any
-	private Instance<TaskLifeCycleEventListener> taskListeners;
 
-	// internal member to ensure only single instance of task service is produced
-	private InternalTaskService taskService;
-	
-	@Produces
-	@PersistenceUnit(unitName = "org.jbpm.domain")
-	public CommandBasedTaskService produceTaskService(EntityManagerFactory emf) {
-		if (mode.equalsIgnoreCase("none")) {
-			return null;
-		}
-		if (taskService == null) {
-			HumanTaskConfigurator configurator = HumanTaskServiceFactory.newTaskServiceConfigurator()
-					.entityManagerFactory(emf)
-					.userGroupCallback(safeGet(userGroupCallback))
-					.userInfo(safeGet(userInfo));
-					
-			try {
-				for (TaskLifeCycleEventListener listener : taskListeners) {
-					configurator.listener(listener);
-					logger.debug("Registering listener {}", listener);
-				}
-			} catch (Exception e) {
-				logger.warn("Cannot add listeners to task service due to {}", e.getMessage());
-			}
-			if (mode.equalsIgnoreCase("singleton")) {
-				this.taskService = (CommandBasedTaskService) configurator.getTaskService();
-			} else {
-				return (CommandBasedTaskService) configurator.getTaskService();
-			}
-		}
-		
-		return (CommandBasedTaskService)taskService;
-	}
-	
-	protected <T> T safeGet(Instance<T> instance) {
-		try {
-			T object = instance.get();
-			logger.debug("About to set object {} on task service", object);
-			return object;
-		} catch(AmbiguousResolutionException e) {
-			// special handling in case cdi discovered multiple injections
-			// that are actually same instances - e.g. weld on tomcat
-			HashSet<T> available = new HashSet<T>(); 
-			
-			for (T object : instance) {
-				available.add(object);
-			}
-			
-			if (available.size() == 1) {
-				return available.iterator().next();
-			} else {
-				throw e;
-			}
-		} catch (Throwable e) {
-			logger.warn("Cannot get value of of instance {} due to {}", instance, e.getMessage());
-		}
-		
-		return null;
-	}
+    private static final Logger logger = LoggerFactory.getLogger( HumanTaskServiceProducer.class );
+    final String mode = System.getProperty( "org.jbpm.cdi.taskservice.mode", "new" );
+
+    @Inject
+    private Instance<UserGroupCallback> userGroupCallback;
+
+    @Inject
+    private Instance<UserInfo> userInfo;
+
+    @Inject
+    @Any
+    private Instance<TaskLifeCycleEventListener> taskListeners;
+
+    @Inject
+    @PersistenceUnit(unitName = "org.jbpm.domain")
+    private EntityManagerFactory emf;
+
+    // internal member to ensure only single instance of task service is produced
+    private InternalTaskService taskService;
+
+    @Produces
+    public CommandBasedTaskService produceTaskService() {
+        if ( mode.equalsIgnoreCase( "none" ) ) {
+            return null;
+        }
+        if ( taskService == null ) {
+            HumanTaskConfigurator configurator = HumanTaskServiceFactory.newTaskServiceConfigurator()
+                    .entityManagerFactory( emf )
+                    .userGroupCallback( safeGet( userGroupCallback ) )
+                    .userInfo( safeGet( userInfo ) );
+
+            try {
+                for ( TaskLifeCycleEventListener listener : taskListeners ) {
+                    configurator.listener( listener );
+                    logger.debug( "Registering listener {}", listener );
+                }
+            } catch ( Exception e ) {
+                logger.warn( "Cannot add listeners to task service due to {}", e.getMessage() );
+            }
+            if ( mode.equalsIgnoreCase( "singleton" ) ) {
+                this.taskService = (CommandBasedTaskService) configurator.getTaskService();
+            } else {
+                return (CommandBasedTaskService) configurator.getTaskService();
+            }
+        }
+
+        return (CommandBasedTaskService) taskService;
+    }
+
+    protected <T> T safeGet( Instance<T> instance ) {
+        try {
+            T object = instance.get();
+            logger.debug( "About to set object {} on task service", object );
+            return object;
+        } catch ( AmbiguousResolutionException e ) {
+            // special handling in case cdi discovered multiple injections
+            // that are actually same instances - e.g. weld on tomcat
+            HashSet<T> available = new HashSet<T>();
+
+            for ( T object : instance ) {
+                available.add( object );
+            }
+
+            if ( available.size() == 1 ) {
+                return available.iterator().next();
+            } else {
+                throw e;
+            }
+        } catch ( Throwable e ) {
+            logger.warn( "Cannot get value of of instance {} due to {}", instance, e.getMessage() );
+        }
+
+        return null;
+    }
 
 }
--- kogito-jbpm/jbpm-services/jbpm-kie-services/src/main/java/org/jbpm/kie/services/cdi/producer/TransactionalCommandServiceProducer.java
+++ kogito-jbpm/jbpm-services/jbpm-kie-services/src/main/java/org/jbpm/kie/services/cdi/producer/TransactionalCommandServiceProducer.java
@@ -1,16 +1,20 @@
 package org.jbpm.kie.services.cdi.producer;
 
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 
 import org.jbpm.shared.services.impl.TransactionalCommandService;
 
 public class TransactionalCommandServiceProducer {
-	
-    @Produces
+
+    @Inject
     @PersistenceUnit(unitName = "org.jbpm.domain")
-    public TransactionalCommandService produceCommandService(EntityManagerFactory emf) {
-    	return new TransactionalCommandService(emf);
+    private EntityManagerFactory emf;
+
+    @Produces
+    public TransactionalCommandService produceCommandService() {
+        return new TransactionalCommandService( emf );
     }
 }
