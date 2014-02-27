f29b115a571d023e6a4ed7fb6221dd6dbfd81db2
Jay Guidos <jay.guidos@bidstrading.com>
2014-02-27 13:01:53 -0500

    JBPM-4261 - Work Item Handler that extends AbstractWorkItemHandler throws NPE

--- kogito-jbpm/jbpm-workitems/src/main/java/org/jbpm/process/workitem/AbstractWorkItemHandler.java
+++ kogito-jbpm/jbpm-workitems/src/main/java/org/jbpm/process/workitem/AbstractWorkItemHandler.java
@@ -4,8 +4,13 @@ import java.util.Collection;
 
 import org.drools.core.process.instance.impl.WorkItemImpl;
 import org.jbpm.workflow.instance.node.WorkItemNodeInstance;
+import org.kie.api.runtime.process.NodeInstance;
+import org.kie.api.runtime.process.NodeInstanceContainer;
+import org.kie.api.runtime.process.ProcessInstance;
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkflowProcessInstance;
 import org.kie.internal.runtime.StatefulKnowledgeSession;
-import org.kie.api.runtime.process.*;
 
 public abstract class AbstractWorkItemHandler implements WorkItemHandler {
 	
@@ -43,7 +48,8 @@ public abstract class AbstractWorkItemHandler implements WorkItemHandler {
 		for (NodeInstance nodeInstance: nodeInstances) {
 			if (nodeInstance instanceof WorkItemNodeInstance) {
 				WorkItemNodeInstance workItemNodeInstance = (WorkItemNodeInstance) nodeInstance;
-				if (workItemId == workItemNodeInstance.getWorkItem().getId()) {
+                WorkItem workItem = workItemNodeInstance.getWorkItem();
+                if (workItem != null && workItemId == workItem.getId()) {
 					return workItemNodeInstance;
 				}
 			}
---
+++ kogito-jbpm/jbpm-workitems/src/test/java/org/jbpm/process/workitem/AbstractWorkItemHandlerTest.java
@@ -0,0 +1,88 @@
+package org.jbpm.process.workitem;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.drools.compiler.compiler.BPMN2ProcessFactory;
+import org.drools.compiler.compiler.ProcessBuilderFactory;
+import org.drools.core.impl.EnvironmentFactory;
+import org.drools.core.impl.KnowledgeBaseFactoryServiceImpl;
+import org.drools.core.marshalling.impl.ProcessMarshallerFactory;
+import org.drools.core.runtime.process.ProcessRuntimeFactory;
+import org.jbpm.bpmn2.BPMN2ProcessProviderImpl;
+import org.jbpm.marshalling.impl.ProcessMarshallerFactoryServiceImpl;
+import org.jbpm.process.builder.ProcessBuilderFactoryServiceImpl;
+import org.jbpm.process.instance.ProcessRuntimeFactoryServiceImpl;
+import org.junit.Test;
+import org.kie.api.io.ResourceType;
+import org.kie.api.runtime.KieSessionConfiguration;
+import org.kie.api.runtime.process.NodeInstance;
+import org.kie.api.runtime.process.ProcessInstance;
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemManager;
+import org.kie.api.runtime.process.WorkflowProcessInstance;
+import org.kie.internal.KnowledgeBase;
+import org.kie.internal.KnowledgeBaseFactory;
+import org.kie.internal.builder.KnowledgeBuilder;
+import org.kie.internal.builder.KnowledgeBuilderFactory;
+import org.kie.internal.io.ResourceFactory;
+import org.kie.internal.runtime.StatefulKnowledgeSession;
+
+public class AbstractWorkItemHandlerTest {
+
+	
+    @Test
+    public void testServiceInvocationWithMultipleIntParams() throws Exception {
+        KnowledgeBaseFactory.setKnowledgeBaseServiceFactory(new KnowledgeBaseFactoryServiceImpl());
+        KnowledgeBase kbase = readKnowledgeBase();
+        StatefulKnowledgeSession ksession = createSession(kbase);
+        ksession.getWorkItemManager().registerWorkItemHandler("Human Task", new CustomWorkItemHandler(ksession));
+        Map<String, Object> params = new HashMap<String, Object>();
+
+        
+        WorkflowProcessInstance processInstance = (WorkflowProcessInstance) ksession.startProcess("HR.test", params);
+
+        assertEquals(ProcessInstance.STATE_COMPLETED, processInstance.getState());
+    }
+	
+   private static KnowledgeBase readKnowledgeBase() throws Exception {
+        ProcessBuilderFactory.setProcessBuilderFactoryService(new ProcessBuilderFactoryServiceImpl());
+        ProcessMarshallerFactory.setProcessMarshallerFactoryService(new ProcessMarshallerFactoryServiceImpl());
+        ProcessRuntimeFactory.setProcessRuntimeFactoryService(new ProcessRuntimeFactoryServiceImpl());
+        BPMN2ProcessFactory.setBPMN2ProcessProvider(new BPMN2ProcessProviderImpl());
+        KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+        kbuilder.add(ResourceFactory.newClassPathResource("BPMN2-TwoUserTasks.bpmn2"), ResourceType.BPMN2);
+        return kbuilder.newKnowledgeBase();
+    }
+    
+    private static StatefulKnowledgeSession createSession(KnowledgeBase kbase) {
+        Properties properties = new Properties();
+        properties.put("drools.processInstanceManagerFactory", "org.jbpm.process.instance.impl.DefaultProcessInstanceManagerFactory");
+        properties.put("drools.processSignalManagerFactory", "org.jbpm.process.instance.event.DefaultSignalManagerFactory");
+        KieSessionConfiguration config = KnowledgeBaseFactory.newKnowledgeSessionConfiguration(properties);
+        return kbase.newStatefulKnowledgeSession(config, EnvironmentFactory.newEnvironment());
+    }
+    
+    private class CustomWorkItemHandler extends AbstractWorkItemHandler {
+
+		public CustomWorkItemHandler(StatefulKnowledgeSession ksession) {
+			super(ksession);
+		}
+
+		@Override
+		public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+			NodeInstance nodeInstance = getNodeInstance(workItem);
+			System.out.println("Node instance " + nodeInstance + " for work item " + workItem);
+			manager.completeWorkItem(workItem.getId(), null);
+		}
+
+		@Override
+		public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
+			
+		}
+    	
+    }
+}
