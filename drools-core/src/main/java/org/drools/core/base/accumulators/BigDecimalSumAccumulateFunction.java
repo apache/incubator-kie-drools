db07c74fa243074f73bdce19dcfcda73615c7913
sotty <dsotty@gmail.com>
2014-03-04 12:21:30 +0100

    [DROOLS-315] Support overloading of accumulate functions (temporary patch) (cherry picked from commit 8c64709cbe8ef11961e2d30f02237b4eb908b315)

---
+++ drools-core/src/main/java/org/drools/core/base/accumulators/BigDecimalSumAccumulateFunction.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2010 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.core.base.accumulators;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import org.kie.api.runtime.rule.AccumulateFunction;
+
+/**
+ * An implementation of an accumulator capable of calculating sum of values
+ */
+public class BigDecimalSumAccumulateFunction implements AccumulateFunction {
+
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+
+    }
+
+    public void writeExternal(ObjectOutput out) throws IOException {
+
+    }
+
+    protected static class SumData implements Externalizable {
+        public BigDecimal total = BigDecimal.ZERO;
+
+        public SumData() {}
+
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+            total   = (BigDecimal) in.readObject();
+        }
+
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeObject( total );
+        }
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.kie.base.accumulators.AccumulateFunction#createContext()
+     */
+    public Serializable createContext() {
+        return new SumData();
+    }
+
+    /* (non-Javadoc)
+     * @see org.kie.base.accumulators.AccumulateFunction#init(java.lang.Object)
+     */
+    public void init(Serializable context) throws Exception {
+        SumData data = (SumData) context;
+        data.total = BigDecimal.ZERO;
+    }
+
+    /* (non-Javadoc)
+     * @see org.kie.base.accumulators.AccumulateFunction#accumulate(java.lang.Object, java.lang.Object)
+     */
+    public void accumulate(Serializable context,
+                           Object value) {
+        SumData data = (SumData) context;
+        data.total = data.total.add( BigDecimal.valueOf( ( (Number) value ).doubleValue() ) );
+    }
+
+    /* (non-Javadoc)
+     * @see org.kie.base.accumulators.AccumulateFunction#reverse(java.lang.Object, java.lang.Object)
+     */
+    public void reverse(Serializable context,
+                        Object value) throws Exception {
+        SumData data = (SumData) context;
+        data.total = data.total.subtract( BigDecimal.valueOf( ( (Number) value ).doubleValue() ) );
+    }
+
+    /* (non-Javadoc)
+     * @see org.kie.base.accumulators.AccumulateFunction#getResult(java.lang.Object)
+     */
+    public Object getResult(Serializable context) throws Exception {
+        SumData data = (SumData) context;
+        return data.total;
+    }
+
+    /* (non-Javadoc)
+     * @see org.kie.base.accumulators.AccumulateFunction#supportsReverse()
+     */
+    public boolean supportsReverse() {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Class< ? > getResultType() {
+        return Number.class;
+    }
+
+}
