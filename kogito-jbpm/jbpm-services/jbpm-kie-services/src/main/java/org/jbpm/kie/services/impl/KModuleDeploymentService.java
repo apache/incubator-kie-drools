04b58758676df8f3efb2a87930aee8f67341ea45
Maciej Swiderski <mswiders@redhat.com>
2014-03-14 12:08:53 +0100

    BZ-1070324 - Build problems not reported in the 'Problems' panel.

--- kogito-jbpm/jbpm-services/jbpm-kie-services/src/main/java/org/jbpm/kie/services/impl/KModuleDeploymentService.java
+++ kogito-jbpm/jbpm-services/jbpm-kie-services/src/main/java/org/jbpm/kie/services/impl/KModuleDeploymentService.java
@@ -125,7 +125,7 @@ public class KModuleDeploymentService extends AbstractDeploymentService {
                     process.setForms(formsData);
                     deployedUnit.addAssetLocation(process.getId(), process);
                 } catch (UnsupportedEncodingException e) {
-                    logger.warn("Unable to load content for file '{}' : {}", fileName, e);
+                	throw new IllegalArgumentException("Unsupported encoding while processing process " + fileName);
                 }
             } else if (fileName.matches(".+ftl$")) {
                 try {
@@ -138,7 +138,7 @@ public class KModuleDeploymentService extends AbstractDeploymentService {
                     }
                     formsData.put(key, formContent);
                 } catch (UnsupportedEncodingException e) {
-                    logger.warn("Unable to load content for form '{}' : {}", fileName, e);
+                	throw new IllegalArgumentException("Unsupported encoding while processing form " + fileName);
                 }
             } else if (fileName.matches(".+form$")) {
                 try {
@@ -151,7 +151,7 @@ public class KModuleDeploymentService extends AbstractDeploymentService {
                     }
                     formsData.put(key+".form", formContent);
                 } catch (UnsupportedEncodingException e) {
-                    logger.warn("Unable to load content for form '{}' : {}", fileName, e);
+                	throw new IllegalArgumentException("Unsupported encoding while processing form " + fileName);
                 }
             } else if( fileName.matches(".+class$")) { 
                 String className = fileName.replaceAll("/", ".");
@@ -160,7 +160,7 @@ public class KModuleDeploymentService extends AbstractDeploymentService {
                     deployedUnit.addClass(kieContainer.getClassLoader().loadClass(className));
                     logger.debug( "Loaded {} into the classpath from deployment {}", className, releaseId.toExternalForm());
                 } catch (ClassNotFoundException cnfe) {
-                    logger.error("Unable to load {} when deploying {}", className, releaseId.toExternalForm(), cnfe);
+                    throw new IllegalArgumentException("Class " + className + " not found in the project");
                 }
             }
         }
