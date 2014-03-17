ac653e7b2963b76ea6be24f5c618023e0cbc7349
Michael Anstis <michael.anstis@gmail.com>
2014-03-17 17:55:46 +0000

    BZ1077212 - Guided rule editor: setting fact field to a bound variable in RHS produces invalid DRL (cherry picked from commit c034ed32cae9cb8251c2b2c6eb74dd40f192e765)

--- drools-workbench-models/drools-workbench-models-commons/src/main/java/org/drools/workbench/models/commons/backend/rule/ActionCallMethodBuilder.java
+++ drools-workbench-models/drools-workbench-models-commons/src/main/java/org/drools/workbench/models/commons/backend/rule/ActionCallMethodBuilder.java
@@ -22,33 +22,31 @@ public class ActionCallMethodBuilder {
     private String variable;
     private String[] parameters;
 
-    public ActionCallMethodBuilder(
-            RuleModel model,
-            PackageDataModelOracle dmo,
-            boolean isJavaDialect,
-            Map<String, String> boundParams) {
+    public ActionCallMethodBuilder( RuleModel model,
+                                    PackageDataModelOracle dmo,
+                                    boolean isJavaDialect,
+                                    Map<String, String> boundParams ) {
         this.model = model;
         this.dmo = dmo;
         this.isJavaDialect = isJavaDialect;
         this.boundParams = boundParams;
     }
 
-    public ActionCallMethod get(
-            String variable,
-            String methodName,
-            String[] parameters) {
+    public ActionCallMethod get( String variable,
+                                 String methodName,
+                                 String[] parameters ) {
 
         this.variable = variable;
         this.methodName = methodName;
         this.parameters = parameters;
 
         ActionCallMethod actionCallMethod = new ActionCallMethod();
-        actionCallMethod.setMethodName(methodName);
-        actionCallMethod.setVariable(variable);
-        actionCallMethod.setState(1);
+        actionCallMethod.setMethodName( methodName );
+        actionCallMethod.setVariable( variable );
+        actionCallMethod.setState( 1 );
 
-        for (ActionFieldFunction parameter : getActionFieldFunctions()) {
-            actionCallMethod.addFieldValue(parameter);
+        for ( ActionFieldFunction parameter : getActionFieldFunctions() ) {
+            actionCallMethod.addFieldValue( parameter );
         }
 
         return actionCallMethod;
@@ -59,63 +57,73 @@ public class ActionCallMethodBuilder {
         List<ActionFieldFunction> actionFieldFunctions = new ArrayList<ActionFieldFunction>();
 
         int index = 0;
-        for (String param : parameters) {
+        for ( String param : parameters ) {
             param = param.trim();
-            if (param.length() == 0) {
+            if ( param.length() == 0 ) {
                 continue;
             }
 
-            actionFieldFunctions.add(
-                    getActionFieldFunction(
-                            param,
-                            getDataType(param, index)));
+            actionFieldFunctions.add( getActionFieldFunction( param,
+                                                              getDataType( param,
+                                                                           index ) ) );
         }
         return actionFieldFunctions;
     }
 
-    private String getAdjustedParameter(String param, String dataType) {
+    private String getAdjustedParameter( String param,
+                                         String dataType ) {
         String adjustedParam;
-        if (boundParams.containsKey(param)) {
+        if ( boundParams.containsKey( param ) ) {
             adjustedParam = param;
         } else {
-            adjustedParam = adjustParam(dataType, param, isJavaDialect);
+            adjustedParam = adjustParam( dataType,
+                                         param,
+                                         boundParams,
+                                         isJavaDialect );
         }
         return adjustedParam;
     }
 
-    private ActionFieldFunction getActionFieldFunction(
-            String param,
-            String dataType) {
-        ActionFieldFunction actionFiled = new ActionFieldFunction(null, getAdjustedParameter(param, dataType), dataType);
-        actionFiled.setNature(inferFieldNature(param, boundParams));
-        actionFiled.setField(methodName);
+    private ActionFieldFunction getActionFieldFunction( String param,
+                                                        String dataType ) {
+        ActionFieldFunction actionFiled = new ActionFieldFunction( null,
+                                                                   getAdjustedParameter( param,
+                                                                                         dataType ),
+                                                                   dataType );
+        actionFiled.setNature( inferFieldNature( param,
+                                                 boundParams ) );
+        actionFiled.setField( methodName );
         return actionFiled;
     }
 
-    private String getDataType(String param, int index) {
+    private String getDataType( String param,
+                                int index ) {
         String dataType;
 
         MethodInfo methodInfo = getMethodInfo();
 
-        if (methodInfo != null) {
-            dataType = methodInfo.getParams().get(index++);
+        if ( methodInfo != null ) {
+            dataType = methodInfo.getParams().get( index++ );
         } else {
-            dataType = boundParams.get(param);
+            dataType = boundParams.get( param );
         }
-        if (dataType == null) {
-            return inferDataType(param, isJavaDialect);
-        } else {
-            return dataType;
+        if ( dataType == null ) {
+            dataType = inferDataType( param,
+                                      boundParams,
+                                      isJavaDialect );
         }
+        return dataType;
     }
 
     private MethodInfo getMethodInfo() {
-        String variableType = boundParams.get(variable);
-        if (variableType != null) {
-            List<MethodInfo> methods = getMethodInfosForType(model, dmo, variableType);
-            if (methods != null) {
-                for (MethodInfo method : methods) {
-                    if (method.getName().equals(methodName) && compareParameters(method.getParams())) {
+        String variableType = boundParams.get( variable );
+        if ( variableType != null ) {
+            List<MethodInfo> methods = getMethodInfosForType( model,
+                                                              dmo,
+                                                              variableType );
+            if ( methods != null ) {
+                for ( MethodInfo method : methods ) {
+                    if ( method.getName().equals( methodName ) ) {
                         return method;
                     }
                 }
@@ -125,17 +133,4 @@ public class ActionCallMethodBuilder {
         return null;
     }
 
-    private boolean compareParameters(List<String> methodParams) {
-        if (methodParams.size() != parameters.length) {
-            return false;
-        } else {
-            for (int index = 0; index < methodParams.size(); index++) {
-                if (!methodParams.get(index).equals(boundParams.get(parameters[index]))) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-    }
 }
--- drools-workbench-models/drools-workbench-models-commons/src/main/java/org/drools/workbench/models/commons/backend/rule/RuleModelPersistenceHelper.java
+++ drools-workbench-models/drools-workbench-models-commons/src/main/java/org/drools/workbench/models/commons/backend/rule/RuleModelPersistenceHelper.java
@@ -3,52 +3,108 @@ package org.drools.workbench.models.commons.backend.rule;
 import java.util.List;
 import java.util.Map;
 
+import org.drools.workbench.models.datamodel.imports.Import;
+import org.drools.workbench.models.datamodel.imports.Imports;
 import org.drools.workbench.models.datamodel.oracle.DataType;
 import org.drools.workbench.models.datamodel.oracle.MethodInfo;
+import org.drools.workbench.models.datamodel.oracle.ModelField;
 import org.drools.workbench.models.datamodel.oracle.PackageDataModelOracle;
+import org.drools.workbench.models.datamodel.rule.ActionFieldList;
+import org.drools.workbench.models.datamodel.rule.ActionInsertFact;
+import org.drools.workbench.models.datamodel.rule.ActionSetField;
 import org.drools.workbench.models.datamodel.rule.FieldNatureType;
 import org.drools.workbench.models.datamodel.rule.RuleModel;
 
 class RuleModelPersistenceHelper {
 
-    static String unwrapParenthesis(String s) {
-        int start = s.indexOf('(');
-        int end = s.lastIndexOf(')');
-        return s.substring(start + 1, end).trim();
+    static String unwrapParenthesis( String s ) {
+        int start = s.indexOf( '(' );
+        int end = s.lastIndexOf( ')' );
+        return s.substring( start + 1, end ).trim();
     }
 
-    static int inferFieldNature(String param,
-            Map<String, String> boundParams) {
-        if (param.startsWith("sdf.parse")) {
+    static int inferFieldNature( String param,
+                                 Map<String, String> boundParams ) {
+        if ( param.startsWith( "sdf.parse" ) ) {
             return FieldNatureType.TYPE_UNDEFINED;
         }
-        if (boundParams.keySet().contains(param)) {
+        if ( boundParams.keySet().contains( param ) ) {
             return FieldNatureType.TYPE_VARIABLE;
         }
-        if (param.contains("+") || param.contains("-") || param.contains("*") || param.contains("/")) {
+        if ( param.contains( "+" ) || param.contains( "-" ) || param.contains( "*" ) || param.contains( "/" ) ) {
             return FieldNatureType.TYPE_FORMULA;
         }
-        if (param.startsWith("\"") || Character.isDigit(param.charAt(0))) {
+        if ( param.startsWith( "\"" ) || Character.isDigit( param.charAt( 0 ) ) ) {
             return FieldNatureType.TYPE_LITERAL;
         }
         return FieldNatureType.TYPE_UNDEFINED;
     }
 
-    static String inferDataType(
-            String param,
-            boolean isJavaDialect) {
-        if (param.startsWith("sdf.parse(\"")) {
+    static String inferDataType( ActionFieldList action,
+                                 String field,
+                                 Map<String, String> boundParams,
+                                 PackageDataModelOracle dmo,
+                                 Imports imports ) {
+        String factType = null;
+        if ( action instanceof ActionInsertFact ) {
+            factType = ( (ActionInsertFact) action ).getFactType();
+
+        } else if ( action instanceof ActionSetField ) {
+            String boundParam = ( (ActionSetField) action ).getVariable();
+            factType = boundParams.get( boundParam );
+        }
+        if ( factType == null ) {
+            return null;
+        }
+        //Lookup without package prefix or imports
+        ModelField[] modelFields = dmo.getProjectModelFields().get( factType );
+
+        //Lookup with package prefix
+        if ( modelFields == null ) {
+            String fqcn = dmo.getPackageName() + "." + factType;
+            modelFields = dmo.getProjectModelFields().get( fqcn );
+        }
+
+        //Lookup from imports
+        if ( modelFields == null ) {
+            for ( Import item : imports.getImports() ) {
+                if ( item.getType().endsWith( factType ) ) {
+                    modelFields = dmo.getProjectModelFields().get( item.getType() );
+                    if ( modelFields != null ) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        if ( modelFields == null ) {
+            return null;
+        }
+        for ( ModelField modelField : modelFields ) {
+            if ( modelField.getName().equals( field ) ) {
+                return modelField.getType();
+            }
+        }
+        return null;
+    }
+
+    static String inferDataType( String param,
+                                 Map<String, String> boundParams,
+                                 boolean isJavaDialect ) {
+        if ( param.startsWith( "sdf.parse(\"" ) ) {
             return DataType.TYPE_DATE;
-        } else if (param.startsWith("\"")) {
+        } else if ( param.startsWith( "\"" ) ) {
             return DataType.TYPE_STRING;
-        } else if (param.equals("true") || param.equals("false")) {
+        } else if ( param.equals( "true" ) || param.equals( "false" ) ) {
             return DataType.TYPE_BOOLEAN;
-        } else if (param.endsWith("B") || (isJavaDialect && param.startsWith("new java.math.BigDecimal"))) {
+        } else if ( param.endsWith( "B" ) || ( isJavaDialect && param.startsWith( "new java.math.BigDecimal" ) ) ) {
             return DataType.TYPE_NUMERIC_BIGDECIMAL;
-        } else if (param.endsWith("I") || (isJavaDialect && param.startsWith("new java.math.BigInteger"))) {
+        } else if ( param.endsWith( "I" ) || ( isJavaDialect && param.startsWith( "new java.math.BigInteger" ) ) ) {
             return DataType.TYPE_NUMERIC_BIGINTEGER;
-        } else if (param.startsWith("[") && param.endsWith("]")) {
+        } else if ( param.startsWith( "[" ) && param.endsWith( "]" ) ) {
             return DataType.TYPE_COLLECTION;
+        } else if ( boundParams.containsKey( param ) ) {
+            return DataType.TYPE_OBJECT;
         }
         return DataType.TYPE_NUMERIC;
     }
@@ -56,31 +112,33 @@ class RuleModelPersistenceHelper {
     static String adjustParam(
             String dataType,
             String param,
-            boolean isJavaDialect) {
-        if (dataType == DataType.TYPE_DATE) {
-            return param.substring("sdf.parse(\"".length(), param.length() - 2);
-        } else if (dataType == DataType.TYPE_STRING) {
-            return param.substring(1, param.length() - 1);
-        } else if (dataType == DataType.TYPE_NUMERIC_BIGDECIMAL || dataType == DataType.TYPE_NUMERIC_BIGINTEGER) {
-            if (isJavaDialect) {
-                return param.substring("new java.math.BigDecimal(\"".length(), param.length() - 2);
+            Map<String, String> boundParams,
+            boolean isJavaDialect ) {
+        if ( dataType == DataType.TYPE_DATE ) {
+            return param.substring( "sdf.parse(\"".length(), param.length() - 2 );
+        } else if ( dataType == DataType.TYPE_STRING ) {
+            return param.substring( 1, param.length() - 1 );
+        } else if ( dataType == DataType.TYPE_NUMERIC_BIGDECIMAL || dataType == DataType.TYPE_NUMERIC_BIGINTEGER ) {
+            if ( isJavaDialect ) {
+                return param.substring( "new java.math.BigDecimal(\"".length(), param.length() - 2 );
             } else {
-                return param.substring(0, param.length() - 1);
+                return param.substring( 0, param.length() - 1 );
             }
+        } else if ( boundParams.containsKey( param ) ) {
+            return "=" + param;
         }
         return param;
     }
 
-    static List<MethodInfo> getMethodInfosForType(RuleModel
-            model,
-            PackageDataModelOracle dmo,
-            String variableType) {
-        List<MethodInfo> methods = dmo.getProjectMethodInformation().get(variableType);
-        if (methods == null) {
-            for (String imp : model.getImports().getImportStrings()) {
-                if (imp.endsWith("." + variableType)) {
-                    methods = dmo.getProjectMethodInformation().get(imp);
-                    if (methods != null) {
+    static List<MethodInfo> getMethodInfosForType( RuleModel model,
+                                                   PackageDataModelOracle dmo,
+                                                   String variableType ) {
+        List<MethodInfo> methods = dmo.getProjectMethodInformation().get( variableType );
+        if ( methods == null ) {
+            for ( String imp : model.getImports().getImportStrings() ) {
+                if ( imp.endsWith( "." + variableType ) ) {
+                    methods = dmo.getProjectMethodInformation().get( imp );
+                    if ( methods != null ) {
                         break;
                     }
                 }
