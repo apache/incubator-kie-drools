5d82073b5d6d9f378acd031e5e4948559e2175b3
Michael Anstis <michael.anstis@gmail.com>
2014-03-25 09:30:52 +0000

    BZ1079988 - Guided Decision Table: 'Apply changes' not working for an action column

--- kogito-drools/drools-workbench-models/drools-workbench-models-guided-dtable/src/main/java/org/drools/workbench/models/guided/dtable/shared/model/ActionWorkItemInsertFactCol52.java
+++ kogito-drools/drools-workbench-models/drools-workbench-models-guided-dtable/src/main/java/org/drools/workbench/models/guided/dtable/shared/model/ActionWorkItemInsertFactCol52.java
@@ -41,32 +41,35 @@ public class ActionWorkItemInsertFactCol52 extends ActionInsertFactCol52 {
     public static final String FIELD_PARAMETER_CLASSNAME = "parameterClassName";
 
     @Override
-    public List<BaseColumnFieldDiff> diff(BaseColumn otherColumn) {
-        if (otherColumn == null) return null;
+    public List<BaseColumnFieldDiff> diff( BaseColumn otherColumn ) {
+        if ( otherColumn == null ) {
+            return null;
+        }
 
-        List<BaseColumnFieldDiff> result = super.diff(otherColumn);
-        ActionWorkItemSetFieldCol52 other = (ActionWorkItemSetFieldCol52) otherColumn;
+        List<BaseColumnFieldDiff> result = super.diff( otherColumn );
+        ActionWorkItemInsertFactCol52 other = (ActionWorkItemInsertFactCol52) otherColumn;
 
         // Field: workItemName.
         if ( !isEqualOrNull( this.getWorkItemName(),
-                other.getWorkItemName() ) ) {
-            result.add(new BaseColumnFieldDiffImpl(FIELD_WORK_ITEM_NAME, this.getWorkItemName(), other.getWorkItemName()));
+                             other.getWorkItemName() ) ) {
+            result.add( new BaseColumnFieldDiffImpl( FIELD_WORK_ITEM_NAME, this.getWorkItemName(), other.getWorkItemName() ) );
         }
 
         // Field: workItemResultParameterName.
         if ( !isEqualOrNull( this.getWorkItemResultParameterName(),
-                other.getWorkItemResultParameterName() ) ) {
-            result.add(new BaseColumnFieldDiffImpl(FIELD_WORK_ITEM_RESULT_PARAM_NAME, this.getWorkItemResultParameterName(), other.getWorkItemResultParameterName()));
+                             other.getWorkItemResultParameterName() ) ) {
+            result.add( new BaseColumnFieldDiffImpl( FIELD_WORK_ITEM_RESULT_PARAM_NAME, this.getWorkItemResultParameterName(), other.getWorkItemResultParameterName() ) );
         }
 
         // Field: parameterClassName.
         if ( !isEqualOrNull( this.getParameterClassName(),
-                other.getParameterClassName() ) ) {
-            result.add(new BaseColumnFieldDiffImpl(FIELD_PARAMETER_CLASSNAME, this.getParameterClassName(), other.getParameterClassName()));
+                             other.getParameterClassName() ) ) {
+            result.add( new BaseColumnFieldDiffImpl( FIELD_PARAMETER_CLASSNAME, this.getParameterClassName(), other.getParameterClassName() ) );
         }
 
         return result;
     }
+
     public String getWorkItemName() {
         return workItemName;
     }
