6bce16976562e92dc694815e61d34e778653cb85
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2014-04-03 13:58:34 +0200

    TestdataEntityProvidingEntity

---
+++ optaplanner-core/src/test/java/org/optaplanner/core/impl/testdata/domain/entityproviding/TestdataEntityProvidingSolution.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2014 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaplanner.core.impl.testdata.domain.entityproviding;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.optaplanner.core.api.domain.solution.PlanningEntityCollectionProperty;
+import org.optaplanner.core.api.domain.solution.PlanningSolution;
+import org.optaplanner.core.api.domain.valuerange.ValueRangeProvider;
+import org.optaplanner.core.api.score.buildin.simple.SimpleScore;
+import org.optaplanner.core.impl.domain.solution.descriptor.SolutionDescriptor;
+import org.optaplanner.core.impl.solution.Solution;
+import org.optaplanner.core.impl.testdata.domain.TestdataObject;
+import org.optaplanner.core.impl.testdata.domain.TestdataUtils;
+import org.optaplanner.core.impl.testdata.domain.TestdataValue;
+import org.optaplanner.core.impl.testdata.domain.nullable.TestdataNullableEntity;
+
+@PlanningSolution
+public class TestdataEntityProvidingSolution extends TestdataObject implements Solution<SimpleScore> {
+
+    public static SolutionDescriptor buildSolutionDescriptor() {
+        return TestdataUtils.buildSolutionDescriptor(TestdataEntityProvidingSolution.class, TestdataEntityProvidingEntity.class);
+    }
+
+    private List<TestdataEntityProvidingEntity> entityList;
+
+    private SimpleScore score;
+
+    public TestdataEntityProvidingSolution() {
+    }
+
+    public TestdataEntityProvidingSolution(String code) {
+        super(code);
+    }
+
+    @PlanningEntityCollectionProperty
+    public List<TestdataEntityProvidingEntity> getEntityList() {
+        return entityList;
+    }
+
+    public void setEntityList(List<TestdataEntityProvidingEntity> entityList) {
+        this.entityList = entityList;
+    }
+
+    public SimpleScore getScore() {
+        return score;
+    }
+
+    public void setScore(SimpleScore score) {
+        this.score = score;
+    }
+
+    // ************************************************************************
+    // Complex methods
+    // ************************************************************************
+
+    public Collection<? extends Object> getProblemFacts() {
+        throw new UnsupportedOperationException();
+    }
+
+}
