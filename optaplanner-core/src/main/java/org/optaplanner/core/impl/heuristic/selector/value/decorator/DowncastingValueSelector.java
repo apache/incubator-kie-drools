127c94db14822aaa662da9e36659f5121aa51d94
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2014-04-09 09:42:29 +0200

    ValueSelector: downcastEntityClass

---
+++ optaplanner-core/src/main/java/org/optaplanner/core/impl/heuristic/selector/value/decorator/DowncastingValueSelector.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2014 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaplanner.core.impl.heuristic.selector.value.decorator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import com.google.common.collect.Iterators;
+import org.optaplanner.core.impl.domain.variable.descriptor.GenuineVariableDescriptor;
+import org.optaplanner.core.impl.heuristic.selector.common.iterator.SelectionIterator;
+import org.optaplanner.core.impl.heuristic.selector.value.AbstractValueSelector;
+import org.optaplanner.core.impl.heuristic.selector.value.ValueSelector;
+
+public class DowncastingValueSelector extends AbstractValueSelector {
+
+    protected final ValueSelector childValueSelector;
+    protected final Class<?> downcastEntityClass;
+
+    public DowncastingValueSelector(ValueSelector childValueSelector, Class<?> downcastEntityClass) {
+        this.childValueSelector = childValueSelector;
+        this.downcastEntityClass = downcastEntityClass;
+        solverPhaseLifecycleSupport.addEventListener(childValueSelector);
+    }
+
+    public ValueSelector getChildValueSelector() {
+        return childValueSelector;
+    }
+
+    // ************************************************************************
+    // Worker methods
+    // ************************************************************************
+
+    public GenuineVariableDescriptor getVariableDescriptor() {
+        return childValueSelector.getVariableDescriptor();
+    }
+
+    public boolean isCountable() {
+        return childValueSelector.isCountable();
+    }
+
+    public boolean isNeverEnding() {
+        return childValueSelector.isNeverEnding();
+    }
+
+    public long getSize(Object entity) {
+        if (!downcastEntityClass.isInstance(entity)) {
+            return 0L;
+        }
+        return childValueSelector.getSize(entity);
+    }
+
+    public Iterator<Object> iterator(Object entity) {
+        if (!downcastEntityClass.isInstance(entity)) {
+            return Iterators.emptyIterator();
+        }
+        return childValueSelector.iterator(entity);
+    }
+
+    @Override
+    public String toString() {
+        return "Downcasting(" + childValueSelector + ")";
+    }
+
+}
