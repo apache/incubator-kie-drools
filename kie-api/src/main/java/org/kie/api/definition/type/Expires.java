a04709868793a66c617e3cf351af50d4f4a28443
Walter Medvedeo <wmedvedeo@gmail.com>
2014-04-12 18:24:59 +0200

    Duration, Expires, Timestamp and TypeSafe annotations added.

--- kie-api/src/main/java/org/kie/api/definition/type/Expires.java
+++ kie-api/src/main/java/org/kie/api/definition/type/Expires.java
@@ -23,7 +23,7 @@ import java.lang.annotation.Target;
 
 @Retention(value = RetentionPolicy.RUNTIME)
 @Target(value = ElementType.TYPE)
-public @interface TypeSafe {
+public @interface Expires {
 
-    boolean value() default true;
+    String value();
 }
