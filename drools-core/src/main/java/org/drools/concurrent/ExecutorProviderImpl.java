2135fb939335e0404a3db2a2a9956a9a7c9e80e3
Frank Pavageau <fpavageau@ekino.com>
2014-04-29 13:48:01 +0200

    DROOLS-473: bound the number of JIT threads
    An MvelConstraint submits a JIT task to this executor when it has been
    evaluated enough times, which can create a lot of threads when using
    an unbounded thread pool and firing lots of rules in a short period.
    
    This commit adds a configurable upper bound on the size of the pool.

--- drools-core/src/main/java/org/drools/concurrent/ExecutorProviderImpl.java
+++ drools-core/src/main/java/org/drools/concurrent/ExecutorProviderImpl.java
@@ -3,18 +3,45 @@ package org.drools.concurrent;
 import java.util.concurrent.CompletionService;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorCompletionService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ExecutorProviderImpl implements ExecutorProvider {
 
-    private static final java.util.concurrent.ExecutorService executor = Executors.newCachedThreadPool(new ThreadFactory() {
-        public Thread newThread(Runnable r) {
-            Thread t = new Thread(r);
-            t.setDaemon(true);
-            return t;
+    private static final Logger logger = LoggerFactory.getLogger(ExecutorProviderImpl.class);
+    private static final java.util.concurrent.ExecutorService executor;
+
+    static {
+        // Don't saturate the CPUs with the JIT
+        int poolSize = Math.max(Runtime.getRuntime().availableProcessors() / 2, 1);
+        poolSize = getExecutorParameter("drools.executor.poolSize", poolSize);
+        executor = new ThreadPoolExecutor(0, poolSize, 60L, TimeUnit.SECONDS,
+                new SynchronousQueue<Runnable>(),
+                new ThreadFactory() {
+                    public Thread newThread(Runnable r) {
+                        Thread t = new Thread(r);
+                        t.setDaemon(true);
+                        return t;
+                    }
+                });
+    }
+
+    private static int getExecutorParameter(String key, int defaultValue) {
+        String property = System.getProperty(key);
+        if (property != null && property.length() > 0) {
+            try {
+                return Integer.parseInt(property);
+            } catch (NumberFormatException e) {
+                logger.error("The value of {} cannot be parsed as an integer", key, e);
+            }
         }
-    });;
+        return defaultValue;
+    }
 
     public Executor getExecutor() {
         return executor;
