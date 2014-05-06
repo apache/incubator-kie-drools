f386ceb09aaf9ef1c87a45a4962980f3bea42867
mariofusco <mario.fusco@gmail.com>
2014-05-06 20:02:43 +0200

    [DROOLS-473] allow to configure the size of the executor thread pool

--- drools-core/src/main/java/org/drools/core/concurrent/ExecutorProviderImpl.java
+++ drools-core/src/main/java/org/drools/core/concurrent/ExecutorProviderImpl.java
@@ -1,19 +1,32 @@
 package org.drools.core.concurrent;
 
+import org.kie.api.concurrent.KieExecutors;
+
 import java.util.concurrent.CompletionService;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadFactory;
-
-import org.kie.api.concurrent.KieExecutors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ExecutorProviderImpl implements KieExecutors {
 
-    private static final java.util.concurrent.ExecutorService executor = Executors.newCachedThreadPool(new DaemonThreadFactory());
+    private static class ExecutorHolder {
+        private static final java.util.concurrent.ExecutorService executor;
+
+        static {
+            executor = new ThreadPoolExecutor(0, Pool.SIZE,
+                                              60L, TimeUnit.SECONDS,
+                                              new SynchronousQueue<Runnable>(),
+                                              new DaemonThreadFactory());
+        }
+    }
 
     public Executor getExecutor() {
-        return executor;
+        return ExecutorHolder.executor;
     }
 
     public Executor newSingleThreadExecutor() {
@@ -25,8 +38,11 @@ public class ExecutorProviderImpl implements KieExecutors {
     }
 
     private static class DaemonThreadFactory implements ThreadFactory {
+        private static final AtomicInteger threadCount = new AtomicInteger();
+
         public Thread newThread(Runnable r) {
             Thread t = new Thread(r);
+            t.setName("drools-worker-" + threadCount.incrementAndGet());
             t.setDaemon(true);
             return t;
         }
