0c9d2536d0d05e40ce404759f47c6ae8e2648437
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2014-05-23 16:28:17 +0200

    CurriculumCourseConstructionHeuristicTest + CurriculumCourseExhaustiveSearchTest

---
+++ optaplanner-examples/src/test/java/org/optaplanner/examples/curriculumcourse/app/CurriculumCourseExhaustiveSearchTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2014 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaplanner.examples.curriculumcourse.app;
+
+import java.io.File;
+import java.util.Collection;
+
+import org.junit.runners.Parameterized;
+import org.optaplanner.core.config.exhaustivesearch.ExhaustiveSearchPhaseConfig;
+import org.optaplanner.examples.cloudbalancing.persistence.CloudBalancingDao;
+import org.optaplanner.examples.common.app.ExhaustiveSearchTest;
+import org.optaplanner.examples.common.persistence.SolutionDao;
+import org.optaplanner.examples.curriculumcourse.persistence.CurriculumCourseDao;
+
+public class CurriculumCourseExhaustiveSearchTest extends ExhaustiveSearchTest {
+
+    @Parameterized.Parameters(name = "{index}: {0} - {1}")
+    public static Collection<Object[]> getSolutionFilesAsParameters() {
+        return buildParameters(new CurriculumCourseDao(),
+                "toy01.xml");
+    }
+
+    public CurriculumCourseExhaustiveSearchTest(File unsolvedDataFile,
+            ExhaustiveSearchPhaseConfig.ExhaustiveSearchType exhaustiveSearchType) {
+        super(unsolvedDataFile, exhaustiveSearchType);
+    }
+
+    @Override
+    protected String createSolverConfigResource() {
+        return "org/optaplanner/examples/curriculumcourse/solver/curriculumCourseSolverConfig.xml";
+    }
+
+    @Override
+    protected SolutionDao createSolutionDao() {
+        return new CurriculumCourseDao();
+    }
+
+}
