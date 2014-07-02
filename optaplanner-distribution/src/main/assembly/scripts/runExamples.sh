e6e5fdba90b6125edc939e6af1eefa975b054a58
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2014-07-02 15:33:07 +0200

    Fix bat script so it works on Windows with JRE's that don't install java in the PATH

--- optaplanner-distribution/src/main/assembly/scripts/runExamples.sh
+++ optaplanner-distribution/src/main/assembly/scripts/runExamples.sh
@@ -3,32 +3,35 @@
 # Change directory to the directory of the script
 cd `dirname $0`
 
-mainClasspath=
-for i in binaries/*.jar; do mainClasspath=${mainClasspath}:$i; done
+# You can use -Xmx128m or less too for most examples, but it might be slower
+jvmOptions="-Xms256m -Xmx1024m"
 mainClass=org.optaplanner.examples.app.OptaPlannerExamplesApp
+mainClasspath=
+for i in binaries/*.jar; do
+  mainClasspath=${mainClasspath}:$i
+done
 
 echo "Usage: ./runExamples.sh"
-echo "For example: ./runExamples.sh"
-echo "Some notes:"
+echo "Notes:"
 echo "- Java must be installed. Get the JRE (http://www.java.com) or the JDK."
 echo "- For optimal performance, Java is recommended to be OpenJDK 7 or higher."
 echo "- For JDK, the environment variable JAVA_HOME should be set to the JDK installation directory"
 echo "  For example (linux): export JAVA_HOME=/usr/lib/jvm/java-6-sun"
 echo "  For example (mac): export JAVA_HOME=/Library/Java/Home"
-echo "- Working dir should be the directory of this script."
+echo "- The working dir should be the directory of this script."
 echo
-echo "Starting examples app..."
 
-# You can use -Xmx128m or less too, but it might be slower
 if [ -f $JAVA_HOME/bin/java ]; then
-    $JAVA_HOME/bin/java -Xms256m -Xmx1024m -server -cp ${mainClasspath} ${mainClass} $*
+    echo "Starting examples app with JDK from environment variable JAVA_HOME ($JAVA_HOME)..."
+    $JAVA_HOME/bin/java ${jvmOptions} -server -cp ${mainClasspath} ${mainClass} $*
 else
-    java -Xms256m -Xmx1024m -cp ${mainClasspath} ${mainClass} $*
+    echo "Starting examples app with java from environment variable PATH..."
+    java ${jvmOptions} -cp ${mainClasspath} ${mainClass} $*
 fi
 
 if [ $? != 0 ] ; then
     echo
-    echo "Error occurred. Check if java is installed and \$JAVA_HOME ($JAVA_HOME) is correct."
+    echo "ERROR: Check if Java is installed and environment variable JAVA_HOME ($JAVA_HOME) is correct."
     # Prevent the terminal window to disappear before the user has seen the error message
-    sleep 20
+    read -p "Press [Enter] key to close this window."
 fi
