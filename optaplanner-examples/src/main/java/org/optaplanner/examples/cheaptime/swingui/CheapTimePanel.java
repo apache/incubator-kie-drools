5df6890cb11f41dad29821122e44cfb00042827a
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2014-07-14 21:21:20 +0200

    CheapTimePanel

--- optaplanner-examples/src/main/java/org/optaplanner/examples/cheaptime/swingui/CheapTimePanel.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/cheaptime/swingui/CheapTimePanel.java
@@ -16,8 +16,11 @@
 
 package org.optaplanner.examples.cheaptime.swingui;
 
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -36,10 +39,20 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import org.apache.commons.lang.ObjectUtils;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.YIntervalRenderer;
+import org.jfree.data.xy.YIntervalSeries;
+import org.jfree.data.xy.YIntervalSeriesCollection;
 import org.optaplanner.core.api.domain.solution.Solution;
 import org.optaplanner.core.impl.score.director.ScoreDirector;
 import org.optaplanner.core.impl.solver.ProblemFactChange;
 import org.optaplanner.examples.cheaptime.domain.CheapTimeSolution;
+import org.optaplanner.examples.cheaptime.domain.Machine;
+import org.optaplanner.examples.cheaptime.domain.TaskAssignment;
 import org.optaplanner.examples.common.swingui.SolutionPanel;
 
 public class CheapTimePanel extends SolutionPanel {
@@ -47,6 +60,7 @@ public class CheapTimePanel extends SolutionPanel {
 //    public static final String LOGO_PATH = "/org/optaplanner/examples/cheapTime/swingui/cheapTimeLogo.png";
 
     public CheapTimePanel() {
+        setLayout(new BorderLayout());
     }
 
     @Override
@@ -59,12 +73,52 @@ public class CheapTimePanel extends SolutionPanel {
     }
 
     public void resetPanel(Solution solution) {
-        updatePanel(solution);
+        removeAll();
+        ChartPanel chartPanel = new ChartPanel(createChart((CheapTimeSolution) solution));
+        add(chartPanel, BorderLayout.CENTER);
     }
 
-    @Override
-    public void updatePanel(Solution solution) {
-        CheapTimeSolution cheapTimeSolution = (CheapTimeSolution) solution;
+    private JFreeChart createChart(CheapTimeSolution solution) {
+        YIntervalSeriesCollection seriesCollection = new YIntervalSeriesCollection();
+        Map<Machine, YIntervalSeries> machineSeriesMap = new LinkedHashMap<Machine, YIntervalSeries>(
+                solution.getMachineList().size());
+        YIntervalRenderer renderer = new YIntervalRenderer();
+        int seriesIndex = 0;
+        for (Machine machine : solution.getMachineList()) {
+            YIntervalSeries projectSeries = new YIntervalSeries(machine.getLabel());
+            seriesCollection.addSeries(projectSeries);
+            machineSeriesMap.put(machine, projectSeries);
+            renderer.setSeriesShape(seriesIndex, new Rectangle());
+            renderer.setSeriesStroke(seriesIndex, new BasicStroke(3.0f));
+            seriesIndex++;
+        }
+        YIntervalSeries unassignedProjectSeries = new YIntervalSeries("Unassigned");
+        seriesCollection.addSeries(unassignedProjectSeries);
+        machineSeriesMap.put(null, unassignedProjectSeries);
+        renderer.setSeriesShape(seriesIndex, new Rectangle());
+        renderer.setSeriesStroke(seriesIndex, new BasicStroke(3.0f));
+        for (TaskAssignment taskAssignment : solution.getTaskAssignmentList()) {
+            Integer startPeriod = taskAssignment.getStartPeriod();
+            Integer endPeriod = taskAssignment.getEndPeriod();
+            if (startPeriod == null) {
+                startPeriod = 0;
+                endPeriod = 0;
+            }
+            YIntervalSeries machineSeries = machineSeriesMap.get(taskAssignment.getMachine());
+            machineSeries.add(taskAssignment.getId(), (startPeriod + endPeriod) / 2.0,
+                    startPeriod, endPeriod);
+        }
+        NumberAxis domainAxis = new NumberAxis("Task");
+        domainAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        domainAxis.setRange(-0.5, solution.getTaskAssignmentList().size() - 0.5);
+        domainAxis.setInverted(true);
+        NumberAxis rangeAxis = new NumberAxis("Period");
+        rangeAxis.setRange(-0.5, solution.getGlobalPeriodRangeTo() + 0.5);
+        XYPlot plot = new XYPlot(seriesCollection, domainAxis, rangeAxis, renderer);
+        plot.setOrientation(PlotOrientation.HORIZONTAL);
+        return new JFreeChart("Project Job Scheduling", JFreeChart.DEFAULT_TITLE_FONT,
+                plot, true);
     }
+
 
 }
