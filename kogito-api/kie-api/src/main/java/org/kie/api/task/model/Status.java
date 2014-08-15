2feae9cbb58a4383fb68f6471145f84bdaaa1dbe
Marco Rietveld <mrietvel@redhat.com>
2014-08-15 14:57:30 +0200

    BZ-1085958 / BPMSPL-25 - "Flat" command generation and use

--- kogito-api/kie-api/src/main/java/org/kie/api/task/model/Status.java
+++ kogito-api/kie-api/src/main/java/org/kie/api/task/model/Status.java
@@ -16,7 +16,10 @@
 
 package org.kie.api.task.model;
 
+import javax.xml.bind.annotation.XmlType;
 
+
+@XmlType
 public enum Status {
     Created, Ready, Reserved, InProgress, Suspended, Completed, Failed, Error, Exited, Obsolete
 }
