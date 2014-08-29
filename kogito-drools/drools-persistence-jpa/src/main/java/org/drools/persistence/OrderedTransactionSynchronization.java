36624dbee3b2f660f25c2f434749c5d60f33978f
Maciej Swiderski <mswiders@redhat.com>
2014-08-29 19:55:47 +0200

    JBPM-4439 - Multiple RuntimeEngines not getting disposed

--- kogito-drools/drools-persistence-jpa/src/main/java/org/drools/persistence/OrderedTransactionSynchronization.java
+++ kogito-drools/drools-persistence-jpa/src/main/java/org/drools/persistence/OrderedTransactionSynchronization.java
@@ -3,9 +3,11 @@ package org.drools.persistence;
 public abstract class OrderedTransactionSynchronization implements TransactionSynchronization, Comparable<OrderedTransactionSynchronization> {
 
     private Integer order;
+    private String identifier;
 
-    public OrderedTransactionSynchronization(Integer order) {
+    public OrderedTransactionSynchronization(Integer order, String identifier) {
         this.order = order;
+        this.identifier = identifier;
     }
 
     public Integer getOrder() {
@@ -16,11 +18,24 @@ public abstract class OrderedTransactionSynchronization implements TransactionSy
         this.order = order;
     }
 
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
     @Override
     public int compareTo(OrderedTransactionSynchronization o) {
         if (this.getClass() != o.getClass()) {
             return this.getOrder().compareTo(o.getOrder()+1);
         }
-        return this.getOrder().compareTo(o.getOrder());
+        int result = this.getOrder().compareTo(o.getOrder());
+        if (result == 0) {
+            return this.getIdentifier().compareTo(o.getIdentifier());
+        }
+
+        return result;
     }
 }
