b59594616a9aea1abd5478f53ef12bd704c9c3ed
Marco Rietveld <mrietvel@redhat.com>
2014-09-01 21:56:36 +0200

    BPMSPL-84 - moving JaxbTaskSummary to kie-remote-client to facilitate query operations

--- kie-internal/src/main/java/org/kie/internal/task/api/model/SubTasksStrategy.java
+++ kie-internal/src/main/java/org/kie/internal/task/api/model/SubTasksStrategy.java
@@ -16,7 +16,9 @@
 
 package org.kie.internal.task.api.model;
 
+import javax.xml.bind.annotation.XmlType;
 
+@XmlType
 public enum SubTasksStrategy{
     NoAction, EndParentOnAllSubTasksEnd, SkipAllSubTasksOnParentSkip
 }
