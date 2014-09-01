b85ea9273ca77024d6b9f1728679ee51e8479d97
Marco Rietveld <mrietvel@redhat.com>
2014-09-01 12:16:36 +0200

    JBPM-4296 - The ProcessInstanceIdCommand class to simplify serialization

--- kogito-api/kie-internal/src/main/java/org/kie/internal/command/ProcessInstanceIdCommand.java
+++ kogito-api/kie-internal/src/main/java/org/kie/internal/command/ProcessInstanceIdCommand.java
@@ -1,9 +1,24 @@
 package org.kie.internal.command;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+
 import org.kie.api.command.Command;
 
-public interface ProcessInstanceIdCommand<T> extends Command<T> {
+public abstract class ProcessInstanceIdCommand<T> implements Command<T> {
 
-    public Long getProcessInstanceId();
+    private static final long serialVersionUID = -7958811586021197629L;
+    
+    @XmlElement(name="process-instance-id")
+    @XmlSchemaType(name="long")
+    protected Long processInstanceId;
+    
+    public void setProcessInstanceId(Long procInstId) { 
+       this.processInstanceId = procInstId;
+    }
+    
+    public Long getProcessInstanceId() { 
+       return processInstanceId; 
+    }
     
 }
