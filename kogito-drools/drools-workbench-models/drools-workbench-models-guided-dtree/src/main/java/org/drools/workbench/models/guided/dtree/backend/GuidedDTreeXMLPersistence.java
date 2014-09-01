acfcc08ce58c5054a64009f04569495b2317b6b3
Michael Anstis <manstis@gmail.com>
2014-09-01 11:47:17 +0100

    GUVNOR-2122: Add support for Guided Decision Trees to the Workbench

---
+++ kogito-drools/drools-workbench-models/drools-workbench-models-guided-dtree/src/main/java/org/drools/workbench/models/guided/dtree/backend/GuidedDTreeXMLPersistence.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2014 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.workbench.models.guided.dtree.backend;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import org.drools.workbench.models.guided.dtree.shared.model.GuidedDecisionTree;
+
+public class GuidedDTreeXMLPersistence {
+
+    private static final GuidedDTreeXMLPersistence INSTANCE = new GuidedDTreeXMLPersistence();
+
+    private XStream xt;
+
+    private GuidedDTreeXMLPersistence() {
+        xt = new XStream( new DomDriver() );
+    }
+
+    public static GuidedDTreeXMLPersistence getInstance() {
+        return INSTANCE;
+    }
+
+    public String marshal( final GuidedDecisionTree dtree ) {
+        return xt.toXML( dtree );
+    }
+
+    public GuidedDecisionTree unmarshal( final String xml ) {
+        if ( xml == null || xml.trim().equals( "" ) ) {
+            return new GuidedDecisionTree();
+        }
+
+        GuidedDecisionTree model = (GuidedDecisionTree) xt.fromXML( xml );
+        return model;
+    }
+
+}
