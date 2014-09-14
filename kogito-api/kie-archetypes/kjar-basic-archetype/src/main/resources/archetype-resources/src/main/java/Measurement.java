a90ac83b23d8d8a3ed4452cadf459f6dc652b6d5
tarilabs <matteo.mortari@gmail.com>
2014-09-14 12:25:53 +0200

    [DROOLS-599] kjar archertype
    My contribution, as per JIRA's reference.

---
+++ kogito-api/kie-archetypes/kjar-basic-archetype/src/main/resources/archetype-resources/src/main/java/Measurement.java
@@ -0,0 +1,37 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+public class Measurement {
+	private String id;
+	private String val;
+	
+	public Measurement(String id, String val) {
+		super();
+		this.id = id;
+		this.val = val;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getVal() {
+		return val;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Measurement [");
+		if (id != null)
+			builder.append("id=").append(id).append(", ");
+		if (val != null)
+			builder.append("val=").append(val);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+}
