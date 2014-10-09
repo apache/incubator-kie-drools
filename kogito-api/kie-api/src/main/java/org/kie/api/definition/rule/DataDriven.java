f9939b4caf5cae635d554feafc3f8284ad7e94c1
mariofusco <mario.fusco@gmail.com>
2014-10-09 17:27:31 +0200

    [DROOLS-630] implemnt @DataDriven annotation

---
+++ kogito-api/kie-api/src/main/java/org/kie/api/definition/rule/DataDriven.java
@@ -0,0 +1,11 @@
+package org.kie.api.definition.rule;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target(value = ElementType.TYPE)
+public @interface DataDriven {
+}
