be68fc779533f0f553dd9964da0f52f742f80d7f
Mario Fusco <mario.fusco@gmail.com>
2014-10-10 15:16:05 +0200

    [BZ-1097148] obey maven offline mode in kie-ci

--- kie-ci/src/main/java/org/kie/scanner/embedder/MavenProjectLoader.java
+++ kie-ci/src/main/java/org/kie/scanner/embedder/MavenProjectLoader.java
@@ -14,10 +14,14 @@ public class MavenProjectLoader {
     private static MavenProject mavenProject;
 
     public static MavenProject parseMavenPom(File pomFile) {
+        return parseMavenPom(pomFile, false);
+    }
+
+    public static MavenProject parseMavenPom(File pomFile, boolean offline) {
         if (!pomFile.exists()) {
             return null;
         }
-        MavenRequest mavenRequest = createMavenRequest();
+        MavenRequest mavenRequest = createMavenRequest(offline);
         mavenRequest.setPom( pomFile.getAbsolutePath() );
         try {
             MavenEmbedder mavenEmbedder = new MavenEmbedder( Thread.currentThread().getContextClassLoader(), mavenRequest );
@@ -28,7 +32,11 @@ public class MavenProjectLoader {
     }
 
     public static MavenProject parseMavenPom(InputStream pomStream) {
-        MavenRequest mavenRequest = createMavenRequest();
+        return parseMavenPom(pomStream, false);
+    }
+
+    public static MavenProject parseMavenPom(InputStream pomStream, boolean offline) {
+        MavenRequest mavenRequest = createMavenRequest(offline);
         try {
             MavenEmbedder mavenEmbedder = new MavenEmbedder( Thread.currentThread().getContextClassLoader(), mavenRequest );
             return mavenEmbedder.readProject( pomStream );
@@ -37,21 +45,26 @@ public class MavenProjectLoader {
         }
     }
 
-    private static MavenRequest createMavenRequest() {
+    private static MavenRequest createMavenRequest(boolean offline) {
         MavenRequest mavenRequest = new MavenRequest();
         mavenRequest.setLocalRepositoryPath( MavenSettings.getSettings().getLocalRepository() );
         // BZ-1007894: If dependency is not resolvable and maven project builder does not complain about it,
         // then a <code>java.lang.NullPointerException</code> is thrown to the client.
         // So, the user will se an exception message "null", not descriptive about the real error.
         mavenRequest.setResolveDependencies(true);
+        mavenRequest.setOffline(offline);
         return mavenRequest;
     }
 
     public static synchronized MavenProject loadMavenProject() {
+        return loadMavenProject(false);
+    }
+
+    public static synchronized MavenProject loadMavenProject(boolean offline) {
         if (mavenProject == null) {
             File pomFile = new File( "pom.xml" );
             try {
-                mavenProject = parseMavenPom(pomFile);
+                mavenProject = parseMavenPom(pomFile, offline);
             } catch (Exception e) {
                 log.warn("Unable to parse pom.xml file of the running project: " + e.getMessage());
             }
