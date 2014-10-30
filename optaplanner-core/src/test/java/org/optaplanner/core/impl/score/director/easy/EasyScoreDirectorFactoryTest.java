480b619c2d5cb1b5c9db2f86402a87bb4efb07f6
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2014-10-30 11:38:40 +0100

    PR 58 review: refactor EasyScoreDirectorFactoryTest to use mocks + to use the Testdata* classes + to use assertEquals instead of assertTrue + to not assert that easy calc is too weak to fulfill the matchPreference

--- optaplanner-core/src/test/java/org/optaplanner/core/impl/score/director/easy/EasyScoreDirectorFactoryTest.java
+++ optaplanner-core/src/test/java/org/optaplanner/core/impl/score/director/easy/EasyScoreDirectorFactoryTest.java
@@ -1,65 +1,39 @@
 package org.optaplanner.core.impl.score.director.easy;
 
 import org.junit.Test;
+import org.mockito.Matchers;
 import org.optaplanner.core.api.domain.solution.Solution;
 import org.optaplanner.core.api.score.Score;
 import org.optaplanner.core.api.score.buildin.simple.SimpleScore;
 import org.optaplanner.core.impl.domain.solution.descriptor.SolutionDescriptor;
+import org.optaplanner.core.impl.testdata.domain.TestdataSolution;
+import org.optaplanner.core.impl.testdata.domain.chained.mappedby.TestdataMappedByChainedSolution;
 
 import java.util.Collection;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class EasyScoreDirectorFactoryTest {
 
-    private class TestSolution implements Solution<SimpleScore> {
-
-        private SimpleScore score;
-
-        @Override
-        public SimpleScore getScore() {
-            return score;
-        }
-
-        @Override
-        public void setScore(SimpleScore score) {
-            this.score = score;
-        }
-
-        @Override
-        public Collection<? extends Object> getProblemFacts() {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-    }
-
-    private static class EasyScoreCalcul implements EasyScoreCalculator<TestSolution> {
-
-        public static final Integer TESTING_SCORE = -42;
-
-        @Override
-        public Score calculateScore(TestSolution solution) {
-            return SimpleScore.parseScore(TESTING_SCORE.toString());
-        }
-    }
-
     @Test
     public void getEasyScoreCalculator() {
-        EasyScoreDirectorFactory factory = new EasyScoreDirectorFactory(new EasyScoreCalcul());
-        assertTrue(factory.getEasyScoreCalculator() instanceof EasyScoreCalcul);
-        assertTrue(((SimpleScore) ((EasyScoreCalcul) factory.getEasyScoreCalculator()).calculateScore(null)).getScore()
-                == EasyScoreCalcul.TESTING_SCORE);
+        EasyScoreCalculator scoreCalculator = mock(EasyScoreCalculator.class);
+        EasyScoreDirectorFactory directorFactory = new EasyScoreDirectorFactory(scoreCalculator);
+        assertSame(scoreCalculator, directorFactory.getEasyScoreCalculator());
     }
 
     @Test
     public void buildScoreDirector() {
-        EasyScoreDirectorFactory factory = new EasyScoreDirectorFactory(new EasyScoreCalcul());
-        factory.setSolutionDescriptor(new SolutionDescriptor(Solution.class));
-        EasyScoreDirector director = factory.buildScoreDirector(true);
-        director.setWorkingSolution(new TestSolution());
-        assertTrue(((SimpleScore) director.calculateScore()).getScore() == EasyScoreCalcul.TESTING_SCORE);
-        assertTrue(!director.isConstraintMatchEnabled());
-        EasyScoreDirector director2 = factory.buildScoreDirector(false);
-        assertTrue(!director2.isConstraintMatchEnabled());
+        SolutionDescriptor solutionDescriptor = TestdataSolution.buildSolutionDescriptor();
+        EasyScoreCalculator scoreCalculator = mock(EasyScoreCalculator.class);
+        when(scoreCalculator.calculateScore(any(TestdataSolution.class))).thenReturn(SimpleScore.valueOf(-10));
+        EasyScoreDirectorFactory directorFactory = new EasyScoreDirectorFactory(scoreCalculator);
+        directorFactory.setSolutionDescriptor(solutionDescriptor);
+
+        EasyScoreDirector director = directorFactory.buildScoreDirector(false);
+        director.setWorkingSolution(new TestdataSolution());
+        assertEquals(SimpleScore.valueOf(-10), director.calculateScore());
     }
 
 }
