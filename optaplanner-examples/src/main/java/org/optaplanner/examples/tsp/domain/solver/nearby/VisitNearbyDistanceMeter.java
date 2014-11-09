3eb7e91bf38306c19781808c40350e608b8d2c54
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2014-11-09 21:01:58 +0100

    VisitNearbyDistanceMeter

---
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/tsp/domain/solver/nearby/VisitNearbyDistanceMeter.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2014 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaplanner.examples.tsp.domain.solver.nearby;
+
+import org.optaplanner.core.impl.heuristic.selector.common.nearby.NearbyDistanceMeter;
+import org.optaplanner.examples.tsp.domain.Standstill;
+import org.optaplanner.examples.tsp.domain.Visit;
+
+public class VisitNearbyDistanceMeter implements NearbyDistanceMeter<Visit, Standstill> {
+
+    @Override
+    public double getNearbyDistance(Visit origin, Standstill destination) {
+        long distance = origin.getDistanceTo(destination);
+        // If arriving early also inflicts a cost (more than just not using the vehicle more), such as the driver's wage, use this:
+//        if (origin instanceof TimeWindowedCustomer && destination instanceof TimeWindowedCustomer) {
+//            distance += ((TimeWindowedCustomer) origin).getTimeWindowGapTo((TimeWindowedCustomer) destination);
+//        }
+        return distance;
+    }
+
+}
