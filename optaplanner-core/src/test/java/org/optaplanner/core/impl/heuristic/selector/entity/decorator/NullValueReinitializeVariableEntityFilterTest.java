c736738a909a4eeca25b823177faa369618e0da2
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2014-11-28 17:04:27 +0100

    Peer review PR-59: refactor NullValueReinitializeVariableEntityFilterTest to use actual EntityDescriptors etc

--- optaplanner-core/src/test/java/org/optaplanner/core/impl/heuristic/selector/entity/decorator/NullValueReinitializeVariableEntityFilterTest.java
+++ optaplanner-core/src/test/java/org/optaplanner/core/impl/heuristic/selector/entity/decorator/NullValueReinitializeVariableEntityFilterTest.java
@@ -3,6 +3,7 @@ package org.optaplanner.core.impl.heuristic.selector.entity.decorator;
 import org.junit.Test;
 import org.optaplanner.core.impl.domain.entity.descriptor.EntityDescriptor;
 import org.optaplanner.core.impl.domain.variable.descriptor.GenuineVariableDescriptor;
+import org.optaplanner.core.impl.domain.variable.descriptor.VariableDescriptor;
 import org.optaplanner.core.impl.testdata.domain.TestdataEntity;
 import org.optaplanner.core.impl.testdata.domain.TestdataValue;
 import org.optaplanner.core.impl.testdata.domain.nullable.TestdataNullableEntity;
@@ -11,40 +12,26 @@ import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class NullValueReinitializeVariableEntityFilterTest {
 
-    /*
-    this method is written too dummy! ask lpetrovi or geoffrey about mocking it somehow
-     */
     @Test
-    public void accept() throws IntrospectionException {
-        EntityDescriptor entityDescriptor = new EntityDescriptor(null, TestdataNullableEntity.class);
-        PropertyDescriptor[] propertyDescriptors = Introspector.getBeanInfo(TestdataNullableEntity.class).getPropertyDescriptors();
-
-        for (int i = 0; i < propertyDescriptors.length; i++) {
-            if(propertyDescriptors[i].getName().equals("value")) {
-                GenuineVariableDescriptor descriptor = new GenuineVariableDescriptor(entityDescriptor, propertyDescriptors[i]);
-                NullValueReinitializeVariableEntityFilter filter = new NullValueReinitializeVariableEntityFilter(descriptor);
-                assertTrue(filter.accept(null, new TestdataNullableEntity()));
-                break;
-            }
-        }
-
-        entityDescriptor = new EntityDescriptor(null, TestdataEntity.class);
-        propertyDescriptors = Introspector.getBeanInfo(TestdataEntity.class).getPropertyDescriptors();
-
-        for (int i = 0; i < propertyDescriptors.length; i++) {
-            if(propertyDescriptors[i].getName().equals("value")) {
-                GenuineVariableDescriptor descriptor = new GenuineVariableDescriptor(entityDescriptor, propertyDescriptors[i]);
-                NullValueReinitializeVariableEntityFilter filter = new NullValueReinitializeVariableEntityFilter(descriptor);
-                TestdataEntity object = new TestdataEntity();
-                object.setValue(new TestdataValue());
-                assertFalse(filter.accept(null, object));
-                break;
-            }
-        }
+    public void accept() {
+        EntityDescriptor entityDescriptor = TestdataEntity.buildEntityDescriptor();
+        GenuineVariableDescriptor variableDescriptor = entityDescriptor.getGenuineVariableDescriptor("value");
+        NullValueReinitializeVariableEntityFilter filter = new NullValueReinitializeVariableEntityFilter(variableDescriptor);
+        assertEquals(false, filter.accept(null, new TestdataEntity("a", new TestdataValue())));
+        assertEquals(true, filter.accept(null, new TestdataEntity("b", null)));
     }
+
+    @Test
+    public void acceptWithNullableEntity() {
+        EntityDescriptor entityDescriptor = TestdataNullableEntity.buildEntityDescriptor();
+        GenuineVariableDescriptor variableDescriptor = entityDescriptor.getGenuineVariableDescriptor("value");
+        NullValueReinitializeVariableEntityFilter filter = new NullValueReinitializeVariableEntityFilter(variableDescriptor);
+        assertEquals(false, filter.accept(null, new TestdataNullableEntity("a", new TestdataValue())));
+        assertEquals(true, filter.accept(null, new TestdataNullableEntity("b", null)));
+    }
+
 }
