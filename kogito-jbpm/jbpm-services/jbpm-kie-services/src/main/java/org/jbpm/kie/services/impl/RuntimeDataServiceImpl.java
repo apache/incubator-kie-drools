118a63810eddd4cfb5263233e96def7238833ee7
Maciej Swiderski <mswiders@redhat.com>
2014-12-02 18:26:08 +0100

    BZ-1169629 - Process Def list: add column options

--- kogito-jbpm/jbpm-services/jbpm-kie-services/src/main/java/org/jbpm/kie/services/impl/RuntimeDataServiceImpl.java
+++ kogito-jbpm/jbpm-services/jbpm-kie-services/src/main/java/org/jbpm/kie/services/impl/RuntimeDataServiceImpl.java
@@ -144,6 +144,8 @@ public class RuntimeDataServiceImpl implements RuntimeDataService, DeploymentEve
 						return o1.getName().compareTo(o2.getName());
 					} else if ("ProcessVersion".equals(queryContext.getOrderBy())) {
 						return o1.getVersion().compareTo(o2.getVersion());
+					} else if ("Project".equals(queryContext.getOrderBy())) {
+						return o1.getDeploymentId().compareTo(o2.getDeploymentId());
 					}
 					return 0;
 				}
