176e1ecf1439edc3abcba473bd7e408e4b791305
Maciej Swiderski <mswiders@redhat.com>
2014-12-12 16:49:42 +0100

    BZ-1171859 - History data cleanup feature for process / human task

---
+++ kogito-api/kie-internal/src/main/java/org/kie/internal/runtime/manager/audit/query/RequestInfoLogDeleteBuilder.java
@@ -0,0 +1,58 @@
+package org.kie.internal.runtime.manager.audit.query;
+
+import java.util.Date;
+
+import org.kie.internal.executor.api.STATUS;
+import org.kie.internal.query.ParametrizedUpdate;
+
+public interface RequestInfoLogDeleteBuilder extends AuditDeleteBuilder<RequestInfoLogDeleteBuilder>{
+
+    /**
+     * Specify one or more dates as criteria in the query
+     * @param date one or more dates
+     * @return The current query builder instance
+     */
+    public RequestInfoLogDeleteBuilder date(Date... date);
+   
+    /**
+     * Specify the begin of a date range to be used as a criteria on the date field.
+     * The date range includes the date specified.
+     * @param date the start (early end) of the date range
+     * @return The current query builder instance
+     */
+    public RequestInfoLogDeleteBuilder dateRangeStart(Date rangeStart);
+    
+    /**
+     * Specify the end of a date range to be used as a criteria on the date field.
+     * The date range includes this date. 
+     * @param date the end (later end) of the date range
+     * @return The current query builder instance
+     */
+    public RequestInfoLogDeleteBuilder dateRangeEnd(Date rangeStart);
+
+    /**
+     * Specify one or more deployment ids to use as a criteria.
+     * @param deploymentId one or more string deployment ids
+     * @return The current query builder instance
+     */
+    public RequestInfoLogDeleteBuilder deploymentId(String... deploymentId);
+    
+    /**
+     * Specify one more statuses (in the form of an int) as criteria.
+     * @param status one or more int statuses
+     * @return The current instance of this query builder
+     */
+    public RequestInfoLogDeleteBuilder status(STATUS... status);
+    
+    /**
+     * Create the {@link ParametrizedUpdate} instance that can be used
+     * to execute update/delete of {@link AuditTask} instances.
+     * </p>
+     * Further modifications to the {@link ProcessInstanceLogDeleteBuilder} instance
+     * will <em>not</em> affect the query criteria used in the {@link ParametrizedUpdate} 
+     * produced by this method.
+     * @return The results of the update/delete
+     */
+    public ParametrizedUpdate build();
+
+}
