1ba9de5c191489ea936140ca7132bd9846c016e1
Michael Anstis <michael.anstis@gmail.com>
2015-01-08 12:32:23 +0000

    BZ1179623: No alert when closing editors with unsaved changes (Guided Score Card)

--- kogito-drools/drools-workbench-models/drools-workbench-models-guided-scorecard/src/main/java/org/drools/workbench/models/guided/scorecard/shared/Characteristic.java
+++ kogito-drools/drools-workbench-models/drools-workbench-models-guided-scorecard/src/main/java/org/drools/workbench/models/guided/scorecard/shared/Characteristic.java
@@ -87,4 +87,56 @@ public class Characteristic {
     public void setDataType( final String dataType ) {
         this.dataType = dataType;
     }
+
+    @Override
+    public boolean equals( Object o ) {
+        if ( this == o ) {
+            return true;
+        }
+        if ( !( o instanceof Characteristic ) ) {
+            return false;
+        }
+
+        Characteristic that = (Characteristic) o;
+
+        if ( Double.compare( that.baselineScore, baselineScore ) != 0 ) {
+            return false;
+        }
+        if ( attributes != null ? !attributes.equals( that.attributes ) : that.attributes != null ) {
+            return false;
+        }
+        if ( dataType != null ? !dataType.equals( that.dataType ) : that.dataType != null ) {
+            return false;
+        }
+        if ( fact != null ? !fact.equals( that.fact ) : that.fact != null ) {
+            return false;
+        }
+        if ( field != null ? !field.equals( that.field ) : that.field != null ) {
+            return false;
+        }
+        if ( name != null ? !name.equals( that.name ) : that.name != null ) {
+            return false;
+        }
+        if ( reasonCode != null ? !reasonCode.equals( that.reasonCode ) : that.reasonCode != null ) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = fact != null ? fact.hashCode() : 0;
+        result = 31 * result + ( field != null ? field.hashCode() : 0 );
+        temp = Double.doubleToLongBits( baselineScore );
+        result = 31 * result + (int) ( temp ^ ( temp >>> 32 ) );
+        result = 31 * result + ( reasonCode != null ? reasonCode.hashCode() : 0 );
+        result = 31 * result + ( attributes != null ? attributes.hashCode() : 0 );
+        result = 31 * result + ( name != null ? name.hashCode() : 0 );
+        result = 31 * result + ( dataType != null ? dataType.hashCode() : 0 );
+        return result;
+    }
+
 }
