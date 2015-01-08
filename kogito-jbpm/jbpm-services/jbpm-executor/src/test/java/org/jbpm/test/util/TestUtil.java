cf6085abedc5a51a433fc175cf3680803c7c35ea
Maciej Swiderski <mswiders@redhat.com>
2015-01-08 13:32:03 +0100

    BZ-1178142 - org.jbpm.executor.ReconfiguredExecutorTest.simpleExcecutionTest fails with MySQL 5.5 on SQL syntax error on 'create table ErrorInfo'

--- kogito-jbpm/jbpm-services/jbpm-executor/src/test/java/org/jbpm/test/util/TestUtil.java
+++ kogito-jbpm/jbpm-services/jbpm-executor/src/test/java/org/jbpm/test/util/TestUtil.java
@@ -18,7 +18,12 @@ package org.jbpm.test.util;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
+import org.jbpm.persistence.util.PersistenceUtil;
+import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,18 +33,89 @@ public class TestUtil {
     
     private static final Logger logger = LoggerFactory.getLogger(TestUtil.class);
 
+    protected static final String DATASOURCE_PROPERTIES = "/datasource.properties";
+    
+    protected static final String MAX_POOL_SIZE = "maxPoolSize";
+    protected static final String ALLOW_LOCAL_TXS = "allowLocalTransactions";
+    
+    protected static final String DATASOURCE_CLASS_NAME = "className";
+    protected static final String DRIVER_CLASS_NAME = "driverClassName";
+    protected static final String USER = "user";
+    protected static final String PASSWORD = "password";
+    protected static final String JDBC_URL = "url";
+    
     public static PoolingDataSource setupPoolingDataSource() {
-        PoolingDataSource pds = new PoolingDataSource();
-        pds.setUniqueName("jdbc/jbpm-ds");
-        pds.setClassName("bitronix.tm.resource.jdbc.lrc.LrcXADataSource");
-        pds.setMaxPoolSize(50);
-        pds.setAllowLocalTransactions(true);
-        pds.getDriverProperties().put("user", "sa");
-        pds.getDriverProperties().put("password", "sasa");
-        pds.getDriverProperties().put("url", "jdbc:h2:mem:test;MVCC=true");
-        pds.getDriverProperties().put("driverClassName", "org.h2.Driver");
+        Properties dsProps = getDatasourceProperties();
+        PoolingDataSource pds = PersistenceUtil.setupPoolingDataSource(dsProps, "jdbc/jbpm-ds", false);
         pds.init();
+        
         return pds;
+    }
+    
+    
+    /**
+     * This reads in the (maven filtered) datasource properties from the test
+     * resource directory.
+     * 
+     * @return Properties containing the datasource properties.
+     */
+    private static Properties getDatasourceProperties() { 
+        boolean propertiesNotFound = false;
+        
+        // Central place to set additional H2 properties
+        System.setProperty("h2.lobInDatabase", "true");
+        
+        InputStream propsInputStream = TestUtil.class.getResourceAsStream(DATASOURCE_PROPERTIES);
+        Properties props = new Properties();
+        if (propsInputStream != null) {
+            try {
+                props.load(propsInputStream);
+            } catch (IOException ioe) {
+                propertiesNotFound = true;
+                logger.warn("Unable to find properties, using default H2 properties: " + ioe.getMessage());
+                ioe.printStackTrace();
+            }
+        } else {
+            propertiesNotFound = true;
+        }
+
+        String password = props.getProperty("password");
+        if ("${maven.jdbc.password}".equals(password) || propertiesNotFound) {
+           logger.warn( "Unable to load datasource properties [" + DATASOURCE_PROPERTIES + "]" );
+        }
+        
+        // If maven filtering somehow doesn't work the way it should.. 
+        setDefaultProperties(props);
+
+        return props;
+    }
+
+    /**
+     * Return the default database/datasource properties - These properties use
+     * an in-memory H2 database
+     * 
+     * This is used when the developer is somehow running the tests but
+     * bypassing the maven filtering that's been turned on in the pom.
+     * 
+     * @return Properties containing the default properties
+     */
+    private static void setDefaultProperties(Properties props) {
+        String[] keyArr = { 
+                "serverName", "portNumber", "databaseName", JDBC_URL,
+                USER, PASSWORD,
+                DRIVER_CLASS_NAME, DATASOURCE_CLASS_NAME,
+                MAX_POOL_SIZE, ALLOW_LOCAL_TXS };
+        String[] defaultPropArr = { 
+                "", "", "", "jdbc:h2:mem:jbpm-db;MVCC=true",
+                "sa", "", 
+                "org.h2.Driver", "bitronix.tm.resource.jdbc.lrc.LrcXADataSource", 
+                "5", "true" };
+        Assert.assertTrue("Unequal number of keys for default properties", keyArr.length == defaultPropArr.length);
+        for (int i = 0; i < keyArr.length; ++i) {
+            if( ! props.containsKey(keyArr[i]) ) {
+                props.put(keyArr[i], defaultPropArr[i]);
+            }
+        }
     }
     
     public static void cleanupSingletonSessionId() {
