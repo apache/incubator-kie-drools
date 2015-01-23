2e7b2166b49dabf1dbd1f3bf0328b0745153e506
Marco Rietveld <mrietvel@redhat.com>
2015-01-23 01:33:15 +0100

    BZ-1175899,BZ-1184009 - SessionInfo and (Errai) User problems from web logins + REST calls
    (cherry picked from commit 8f95063604f9267ccd02e387c243185200157ad2)

--- kogito-api/kie-internal/src/main/java/org/kie/internal/identity/IdentityProvider.java
+++ kogito-api/kie-internal/src/main/java/org/kie/internal/identity/IdentityProvider.java
@@ -19,6 +19,8 @@ package org.kie.internal.identity;
 import java.util.List;
 
 public interface IdentityProvider {
+    
+    public static final String UNKNOWN_USER_IDENTITY = "unknown";
 
     String getName();
     
