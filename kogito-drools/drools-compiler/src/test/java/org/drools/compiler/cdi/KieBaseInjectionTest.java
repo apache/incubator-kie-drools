b300513c8934fbac5068f978ead0befe3cc83142
Mario Fusco <mario.fusco@gmail.com>
2015-05-14 15:46:29 +0200

    [DROOLS-755] allow default value on @KSession annotation

--- kogito-drools/drools-compiler/src/test/java/org/drools/compiler/cdi/KieBaseInjectionTest.java
+++ kogito-drools/drools-compiler/src/test/java/org/drools/compiler/cdi/KieBaseInjectionTest.java
@@ -1,18 +1,5 @@
 package org.drools.compiler.cdi;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.inject.Inject;
-
 import org.drools.compiler.kproject.AbstractKnowledgeTest;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -23,17 +10,31 @@ import org.kie.api.cdi.KBase;
 import org.kie.api.cdi.KReleaseId;
 import org.kie.api.runtime.KieSession;
 
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 @RunWith(CDITestRunner.class)
 public class KieBaseInjectionTest {
     public static AbstractKnowledgeTest helper;  
     
     @Inject
-    @KBase("jar1.KBase1") 
+    @KBase
     @KReleaseId( groupId    = "jar1",
                  artifactId = "art1", 
                  version    = "1.0")
+    private KieBase defaultKBase;
+
+    @Inject
+    @KBase("jar1.KBase1")
+    @KReleaseId( groupId    = "jar1",
+                 artifactId = "art1",
+                 version    = "1.0")
     private KieBase jar1KBase1v10;
-    
+
     @Inject
     @KBase("jar1.KBase1") 
     @KReleaseId(groupId    = "jar1",
@@ -99,29 +100,24 @@ public class KieBaseInjectionTest {
     
     @Test
     public void testDynamicKieBaseReleaseId() throws IOException, ClassNotFoundException, InterruptedException {
-        assertNotNull( jar1KBase1v10 );
-        assertNotNull( jar1KBase1v11 );
-        
-        KieSession kSession = jar1KBase1v10.newKieSession();
+        checkKBase( defaultKBase, "1.0" );
+        checkKBase( jar1KBase1v10, "1.0" );
+        checkKBase( jar1KBase1v11, "1.1" );
+    }
+
+    private void checkKBase(KieBase kBase, String check) {
+        assertNotNull( kBase );
+
+        KieSession kSession = kBase.newKieSession();
         List<String> list = new ArrayList<String>();
         kSession.setGlobal( "list", list );
         kSession.fireAllRules();
-        
-        assertEquals( 2, list.size() );
-        assertTrue( list.get(0).endsWith( "1.0" ) );
-        assertTrue( list.get(1).endsWith( "1.0" ) );
-
-        
-        kSession = jar1KBase1v11.newKieSession();
-        list = new ArrayList<String>();
-        kSession.setGlobal( "list", list );
-        kSession.fireAllRules();
 
         assertEquals( 2, list.size() );
-        assertTrue( list.get(0).endsWith( "1.1" ) );
-        assertTrue( list.get(1).endsWith( "1.1" ) );        
-    }    
-    
+        assertTrue( list.get( 0 ).endsWith( check ) );
+        assertTrue( list.get(1).endsWith( check ) );
+    }
+
     @Test
     public void testNamedKieBases() throws IOException, ClassNotFoundException, InterruptedException {
         assertNotNull(jar1KBase1kb1);
