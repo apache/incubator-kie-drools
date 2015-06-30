891fa71b7dede770978023dc6ca09b3d9bfd3b7d
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2015-06-30 12:23:42 +0200

    PLANNER-343 Auto scan for annoted classes with scannotation

--- optaplanner-core/src/main/java/org/optaplanner/core/config/domain/ScanAnnotatedClassesConfig.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/config/domain/ScanAnnotatedClassesConfig.java
@@ -16,14 +16,22 @@
 
 package org.optaplanner.core.config.domain;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamImplicit;
+import org.optaplanner.core.api.domain.entity.PlanningEntity;
+import org.optaplanner.core.api.domain.solution.PlanningSolution;
 import org.optaplanner.core.api.domain.solution.Solution;
 import org.optaplanner.core.config.util.ConfigUtils;
 import org.optaplanner.core.impl.domain.solution.descriptor.SolutionDescriptor;
+import org.scannotation.AnnotationDB;
+import org.scannotation.ClasspathUrlFinder;
 
 @XStreamAlias("scanAnnotatedClasses")
 public class ScanAnnotatedClassesConfig {
@@ -44,12 +52,70 @@ public class ScanAnnotatedClassesConfig {
     // ************************************************************************
 
     public SolutionDescriptor buildSolutionDescriptor() {
-        Class<? extends Solution> solutionClass = null;
-        List<Class<?>> entityClassList = null;
-        // TODO
-
-
+        AnnotationDB annotationDB = new AnnotationDB();
+        annotationDB.setScanClassAnnotations(true);
+        annotationDB.setScanFieldAnnotations(false);
+        annotationDB.setScanMethodAnnotations(false);
+        annotationDB.setScanParameterAnnotations(false);
+        if (!ConfigUtils.isEmptyCollection(packageIncludeList)) {
+            annotationDB.setScanPackages(packageIncludeList.toArray(new String[packageIncludeList.size()]));
+        }
+        URL[] urls = ClasspathUrlFinder.findClassPaths();
+        try {
+            annotationDB.scanArchives(urls);
+        } catch (IOException e) {
+            throw new IllegalStateException("The scanAnnotatedClasses (" + this
+                    + ") could not scan for annotated classes using urls (" + urls + ").", e);
+        }
+        Map<String, Set<String>> annotationIndex = annotationDB.getAnnotationIndex();
+        Class<? extends Solution> solutionClass = loadSolutionClass(annotationIndex);
+        List<Class<?>> entityClassList = loadEntityClassList(annotationIndex);
         return SolutionDescriptor.buildSolutionDescriptor(solutionClass, entityClassList);
+    }
+
+    protected Class<? extends Solution> loadSolutionClass(Map<String, Set<String>> annotationIndex) {
+        Set<String> solutionClassNameSet = annotationIndex.get(PlanningSolution.class.getName());
+        if (ConfigUtils.isEmptyCollection(solutionClassNameSet)) {
+            throw new IllegalStateException("The scanAnnotatedClasses (" + this
+                    + ") did not find any classes with a " + PlanningSolution.class.getSimpleName()
+                    + " annotation.");
+        } else if (solutionClassNameSet.size() > 1) {
+            throw new IllegalStateException("The scanAnnotatedClasses (" + this
+                    + ") found multiple classes (" + solutionClassNameSet
+                    + ") with a " + PlanningSolution.class.getSimpleName() + " annotation.");
+        }
+        Class<? extends Solution> solutionClass;
+        String solutionClassName = solutionClassNameSet.iterator().next();
+        try {
+            solutionClass = (Class<? extends Solution>) Class.forName(solutionClassName);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("The solution class (" + solutionClassName
+                    + ") with a " + PlanningSolution.class.getSimpleName()
+                    + " annotation could not be loaded by scanAnnotatedClasses (" + this + ").", e);
+        }
+        return solutionClass;
+    }
+
+    protected List<Class<?>> loadEntityClassList(Map<String, Set<String>> annotationIndex) {
+        Set<String> entityClassNameSet = annotationIndex.get(PlanningEntity.class.getName());
+        if (ConfigUtils.isEmptyCollection(entityClassNameSet)) {
+            throw new IllegalStateException("The scanAnnotatedClasses (" + this
+                    + ") did not find any classes with a " + PlanningEntity.class.getSimpleName()
+                    + " annotation.");
+        }
+        List<Class<?>> entityClassList = new ArrayList<Class<?>>(entityClassNameSet.size());
+        for (String entityClassName : entityClassNameSet) {
+            Class<?> entityClass;
+            try {
+                entityClass = (Class<? extends Solution>) Class.forName(entityClassName);
+            } catch (ClassNotFoundException e) {
+                throw new IllegalStateException("The entity class (" + entityClassName
+                        + ") with a " + PlanningEntity.class.getSimpleName()
+                        + " annotation could not be loaded by scanAnnotatedClasses (" + this + ").", e);
+            }
+            entityClassList.add(entityClass);
+        }
+        return entityClassList;
     }
 
     public void inherit(ScanAnnotatedClassesConfig inheritedConfig) {
