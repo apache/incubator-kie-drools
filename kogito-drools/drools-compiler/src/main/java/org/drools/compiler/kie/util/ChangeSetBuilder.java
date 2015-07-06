88fbe27c4e17ef66a7a2255e98344ba8df17c4a6
Mario Fusco <mario.fusco@gmail.com>
2015-07-06 18:23:45 +0200

    [BZ-1227866] fix RuleHierarchyComparator

--- kogito-drools/drools-compiler/src/main/java/org/drools/compiler/kie/util/ChangeSetBuilder.java
+++ kogito-drools/drools-compiler/src/main/java/org/drools/compiler/kie/util/ChangeSetBuilder.java
@@ -197,7 +197,7 @@ public class ChangeSetBuilder {
     private static class RuleHierarchyComparator implements Comparator<RuleDescr> {
         @Override
         public int compare( RuleDescr r1, RuleDescr r2 ) {
-            return r1.getName().equals( r2.getParentName() ) ? -1 : r1.getName().equals( r2.getParentName() ) ? 1 : 0;
+            return r1.getName().equals( r2.getParentName() ) ? -1 : r2.getName().equals( r1.getParentName() ) ? 1 : 0;
         }
     }
 
