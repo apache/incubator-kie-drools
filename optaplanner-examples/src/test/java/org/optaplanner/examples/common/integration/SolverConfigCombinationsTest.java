cfc0d9110018b7b76cf1002c89d9dfcc3e5d166e
Jiri Vahala <jvahala@redhat.com>
2015-07-13 10:47:48 +0200

    Stronger noPhaseConfig test

--- optaplanner-examples/src/test/java/org/optaplanner/examples/common/integration/SolverConfigCombinationsTest.java
+++ optaplanner-examples/src/test/java/org/optaplanner/examples/common/integration/SolverConfigCombinationsTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.optaplanner.core.api.domain.solution.Solution;
 import org.optaplanner.core.api.solver.Solver;
 import org.optaplanner.core.api.solver.SolverFactory;
+import org.optaplanner.core.api.solver.event.SolverEventListener;
 import org.optaplanner.core.config.heuristic.selector.move.generic.ChangeMoveSelectorConfig;
 import org.optaplanner.core.config.localsearch.LocalSearchPhaseConfig;
 import org.optaplanner.core.config.localsearch.decider.acceptor.AcceptorConfig;
@@ -13,6 +14,12 @@ import org.optaplanner.core.config.localsearch.decider.forager.LocalSearchForage
 import org.optaplanner.core.config.phase.PhaseConfig;
 import org.optaplanner.core.config.solver.SolverConfig;
 import org.optaplanner.core.config.solver.termination.TerminationConfig;
+import org.optaplanner.core.impl.phase.event.PhaseLifecycleListener;
+import org.optaplanner.core.impl.phase.scope.AbstractPhaseScope;
+import org.optaplanner.core.impl.phase.scope.AbstractStepScope;
+import org.optaplanner.core.impl.solver.DefaultSolver;
+import org.optaplanner.core.impl.solver.scope.DefaultSolverScope;
+import org.optaplanner.core.impl.testdata.util.listeners.PhaseTestListener;
 import org.optaplanner.examples.cheaptime.app.CheapTimeApp;
 import org.optaplanner.examples.cheaptime.persistence.CheapTimeDao;
 import org.optaplanner.examples.cloudbalancing.persistence.CloudBalancingDao;
@@ -20,16 +27,20 @@ import org.optaplanner.examples.cloudbalancing.persistence.CloudBalancingDao;
 import java.io.File;
 import java.util.Arrays;
 
+import static org.junit.Assert.assertEquals;
+
 public class SolverConfigCombinationsTest {
 
     @Test
     public void noPhaseConfiguration() {
         SolverConfig config = SolverFactory.createFromXmlResource("org/optaplanner/examples/cloudbalancing/solver/cloudBalancingSolverConfig.xml").getSolverConfig();
-        config.getTerminationConfig().setSecondsSpentLimit(null);
-        config.getTerminationConfig().setSecondsSpentLimit(2L);
-        Solver solver = config.buildSolver();
-        Solution planningProblem = new CloudBalancingDao().readSolution(new File("data/cloudbalancing/unsolved/2computers-6processes.xml"));
+        config.getTerminationConfig().setBestScoreLimit("0hard/-5460soft");
+        DefaultSolver solver = (DefaultSolver) config.buildSolver();
+        PhaseTestListener listener = new PhaseTestListener();
+        solver.addPhaseLifecycleListener(listener);
+        Solution planningProblem = new CloudBalancingDao().readSolution(new File("data/cloudbalancing/unsolved/3computers-9processes.xml"));
         solver.solve(planningProblem);
+        assertEquals(2, listener.getPhaseList().size());
     }
 
     @Test
