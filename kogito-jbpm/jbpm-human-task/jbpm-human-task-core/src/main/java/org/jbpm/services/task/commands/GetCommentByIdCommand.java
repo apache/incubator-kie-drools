f72d254bd9f4398543fc1be142a5994d83bcb716
salaboy <salaboy@gmail.com>
2015-07-17 17:30:30 +0100

    BPMSPL-114: adding new command comments by id

---
+++ kogito-jbpm/jbpm-human-task/jbpm-human-task-core/src/main/java/org/jbpm/services/task/commands/GetCommentByIdCommand.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2015 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.jbpm.services.task.commands;
+
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.kie.api.task.model.Comment;
+import org.kie.internal.command.Context;
+
+@XmlRootElement(name="get-comment-by-id-command")
+@XmlAccessorType(XmlAccessType.NONE)
+public class GetCommentByIdCommand extends TaskCommand<Comment> {
+
+	private static final long serialVersionUID = 5911387213149078240L;
+	private Long commentId;
+	public GetCommentByIdCommand() {
+	}
+	
+	public GetCommentByIdCommand(Long commentId) {
+		this.commentId = commentId;
+	}
+
+	public Comment execute(Context cntxt) {
+        TaskContext context = (TaskContext) cntxt;
+        return context.getTaskCommentService().getCommentById(commentId);
+    }
+
+}
