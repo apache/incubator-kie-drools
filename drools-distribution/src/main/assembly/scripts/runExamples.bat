3afa45e84e17474a1655aa2ef71d9a695be8c68e
Petr Siroky <psiroky@redhat.com>
2015-07-24 16:31:37 +0200

    [DROOLS-539] make runExamples.bat work OOTB
     * the "java" command is not on PATH when users
       have only JRE installed. The script now looks
       in other places as well before failing
    
     * thanks @geoffrey for the solution

--- drools-distribution/src/main/assembly/scripts/runExamples.bat
+++ drools-distribution/src/main/assembly/scripts/runExamples.bat
@@ -1,23 +1,62 @@
 @ECHO OFF
+setLocal enableExtensions enableDelayedExpansion
 
-setLocal EnableDelayedExpansion
-set mainClasspath=
-for %%i in (binaries\*.jar) do (set mainClasspath=!mainClasspath!;%%i)
-set mainClass=org.drools.examples.DroolsExamplesApp
+rem You can use -Xmx128m or less too for most examples, but it might be slower
+set "jvmOptions=-Xms256m -Xmx1024m"
+set "mainClass=org.drools.examples.DroolsExamplesApp"
+set "mainClasspath="
+for %%i in (binaries\*.jar) do (set "mainClasspath=!mainClasspath!;%%i")
 
-echo "Usage: runExamples.bat"
-echo "For example: runExamples.bat"
-echo "Some notes:"
-echo "- Working dir should be the directory of this script."
-echo "- Java is recommended to be JDK and java 6 for optimal performance"
-echo "- The environment variable JAVA_HOME should be set to the JDK installation directory"
-echo "  For example: set JAVA_HOME="C:\Program Files\Java\jdk1.6.0"
-echo
-echo "Starting examples app..."
+echo Usage: runExamples.bat
+echo Notes:
+echo - Java must be installed. Get the JRE ^(http://www.java.com^) or the JDK.
+echo - For optimal performance, Java is recommended to be OpenJDK 7 or higher.
+echo - For JDK, the environment variable JAVA_HOME should be set to the JDK installation directory
+echo   For example: set "JAVA_HOME=C:\Program Files\Java\jdk1.6.0"
+echo - The working dir should be the directory of this script.
+echo.
 
-rem You can use -Xmx128m or less too, but it might be slower
-if exist %JAVA_HOME%\bin\java.exe (
-    %JAVA_HOME%\bin\java -Xms256m -Xmx512m -server -cp %mainClasspath% %mainClass%
+if exist "%JAVA_HOME%\bin\java.exe" (
+    echo Starting examples app with JDK from environment variable JAVA_HOME ^(%JAVA_HOME%^)...
+    rem JDK supports -server mode
+    "%JAVA_HOME%\bin\java" !jvmOptions! -server -cp !mainClasspath! !mainClass!
+    goto endProcess
 ) else (
-    java -Xms256m -Xmx512m -cp %mainClasspath% %mainClass%
+    rem Find JRE home in Windows Registry
+    set "jreRegKey=HKLM\SOFTWARE\JavaSoft\Java Runtime Environment"
+    set "jreVersion="
+    for /f "tokens=3" %%v in ('reg query "!jreRegKey!" /v "CurrentVersion" 2^>nul') do set "jreVersion=%%v"
+    if not defined jreVersion (
+        set "jreRegKey=HKLM\SOFTWARE\Wow6432Node\JavaSoft\Java Runtime Environment"
+        for /f "tokens=3" %%v in ('reg query "!jreRegKey!" /v "CurrentVersion" 2^>nul') do set "jreVersion=%%v"
+        if not defined jreVersion (
+            echo ERROR: The JRE is not installed on this system.
+            goto failure
+        )
+    )
+    set "jreHome="
+    for /f "tokens=2,*" %%d in ('reg query "!jreRegKey!\!jreVersion!" /v "JavaHome" 2^>nul') do set "jreHome=%%e"
+    if not defined jreHome (
+        echo ERROR: The JRE is not properly installed on this system, although jreVersion ^(!jreVersion!^) exists.
+        goto failure
+    )
+
+    if not exist "!jreHome!\bin\java.exe" (
+        echo ERROR: The JRE home ^(!jreHome!^) does not contain java ^(bin\java.exe^).
+        goto failure
+    )
+    echo Starting examples app with JRE from JRE home ^(!jreHome!^)...
+    "!jreHome!\bin\java" !jvmOptions! -cp !mainClasspath! !mainClass!
+    goto endProcess
 )
+
+:failure
+    echo.
+    echo Please install Java from http://www.java.com first.
+    rem Prevent the terminal window to disappear before the user has seen the error message
+    echo Press any key to close this window.
+    pause
+    goto endProcess
+
+:endProcess
+    endLocal
