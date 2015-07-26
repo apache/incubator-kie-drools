9b83f2de28b1d02359a604aaa65896d0b736abfc
Marco Rietveld <mrietvel@redhat.com>
2015-07-26 22:34:05 +0200

    BZ-1245616 - REST API is using wrong context for Correlation Key related commands

---
+++ kie-internal/src/main/java/org/kie/internal/command/CorrelationKeyCommand.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2015 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.kie.internal.command;
+
+import org.kie.internal.process.CorrelationKey;
+
+public interface CorrelationKeyCommand {
+
+    public void setCorrelationKey(CorrelationKey key); 
+    
+    public CorrelationKey getCorrelationKey();
+    
+}
