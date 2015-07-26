9c70d3ed5c891f1751d9c501c46fe1821b5ea9bf
Marco Rietveld <mrietvel@redhat.com>
2015-07-26 22:31:50 +0200

    JBPM-4296 - Make commands that contain a "process instance id" field implement the ProcessInstanceIdCommand interface

--- kie-internal/src/main/java/org/kie/internal/command/ProcessInstanceIdCommand.java
+++ kie-internal/src/main/java/org/kie/internal/command/ProcessInstanceIdCommand.java
@@ -15,25 +15,11 @@
 
 package org.kie.internal.command;
 
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlSchemaType;
 
-import org.kie.api.command.Command;
+public interface ProcessInstanceIdCommand {
 
-public abstract class ProcessInstanceIdCommand<T> implements Command<T> {
-
-    private static final long serialVersionUID = -7958811586021197629L;
+    public void setProcessInstanceId(Long procInstId);
     
-    @XmlElement(name="process-instance-id")
-    @XmlSchemaType(name="long")
-    protected Long processInstanceId;
-    
-    public void setProcessInstanceId(Long procInstId) { 
-       this.processInstanceId = procInstId;
-    }
-    
-    public Long getProcessInstanceId() { 
-       return processInstanceId; 
-    }
+    public Long getProcessInstanceId();
     
 }
