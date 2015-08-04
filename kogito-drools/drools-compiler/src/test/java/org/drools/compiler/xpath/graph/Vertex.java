27703b36cebe4f5f0b560858c2082f3ac6d60dcc
Mario Fusco <mario.fusco@gmail.com>
2015-08-04 09:12:24 +0200

    [DROOLS-664] add oopath test case using graph-like domain

---
+++ kogito-drools/drools-compiler/src/test/java/org/drools/compiler/xpath/graph/Vertex.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2015 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.drools.compiler.xpath.graph;
+
+import org.drools.core.phreak.ReactiveList;
+import org.drools.core.phreak.ReactiveObject;
+
+import java.util.List;
+
+public class Vertex<T> extends ReactiveObject {
+
+    private static int ID_GENERATOR = 1;
+
+    private final int id;
+    private final T it;
+
+    private final List<Edge> inEs = new ReactiveList<Edge>();
+    private final List<Edge> outEs = new ReactiveList<Edge>();
+
+    private final List<Vertex<?>> inVs = new ReactiveList<Vertex<?>>();
+    private final List<Vertex<?>> outVs = new ReactiveList<Vertex<?>>();
+
+    public Vertex( T content ) {
+        this(ID_GENERATOR++, content);
+    }
+
+    public Vertex( int id, T it ) {
+        this.id = id;
+        this.it = it;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public T getIt() {
+        return it;
+    }
+
+    public List<Edge> getInEs() {
+        return inEs;
+    }
+
+    public List<Vertex<?>> getInVs() {
+        return inVs;
+    }
+
+    public List<Edge> getOutEs() {
+        return outEs;
+    }
+
+    public List<Vertex<?>> getOutVs() {
+        return outVs;
+    }
+
+    public void addInEdge(Edge edge) {
+        inEs.add( edge );
+        edge.setOutV( this );
+    }
+
+    public void addOutEdge(Edge edge) {
+        outEs.add( edge );
+        edge.setInV( this );
+    }
+
+    public Edge connectTo(Vertex<?> other) {
+        Edge edge = new Edge();
+        this.addOutEdge( edge );
+        other.addInEdge( edge );
+
+        outVs.add(other);
+        other.inVs.add(this);
+
+        return edge;
+    }
+}
---
+++ kogito-drools/drools-compiler/src/test/java/org/drools/compiler/xpath/graph/XpathOnGraphTest.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2005 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.compiler.xpath.graph;
+
+import org.junit.Test;
+import org.kie.api.io.ResourceType;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.utils.KieHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class XpathOnGraphTest {
+
+    @Test
+    public void testXpathOnGraph() {
+        String drl =
+                "import org.drools.compiler.xpath.graph.*;\n" +
+                "import " + Library.class.getCanonicalName() + ";\n" +
+                "import " + Book.class.getCanonicalName() + ";\n" +
+                "import " + Person.class.getCanonicalName() + ";\n" +
+                "global java.util.List list\n" +
+                "\n" +
+                "rule R when\n" +
+                "  Vertex( it instanceof Library, $a : /outVs/outVs/it{ #Person, age > 40 } )\n" +
+                "then\n" +
+                "  list.add( $a.getName() );\n" +
+                "end\n";
+
+        KieSession ksession = new KieHelper().addContent( drl, ResourceType.DRL )
+                                             .build()
+                                             .newKieSession();
+
+        List<String> list = new ArrayList<String>();
+        ksession.setGlobal( "list", list );
+
+        Vertex<Library> library = getGraph();
+        ksession.insert( library );
+        ksession.fireAllRules();
+
+        assertEquals( 1, list.size() );
+        assertTrue( list.contains( "Mario" ) );
+        list.clear();
+
+        Vertex<?> book = library.getOutVs().get(0);
+        Vertex<Person> alan = new Vertex<Person>( new Person( "Alan", 53 ) );
+        book.connectTo( alan );
+
+        ksession.fireAllRules();
+        assertEquals( 1, list.size() );
+        assertTrue( list.contains( "Alan" ) );
+    }
+
+    private Vertex<Library> getGraph() {
+        Vertex<Library> library = new Vertex<Library>( new Library() );
+
+        Vertex<Book> java8inAction = new Vertex<Book>( new Book( "Java 8 in Action" ) );
+        library.connectTo( java8inAction );
+
+        Vertex<Person> raoul = new Vertex<Person>( new Person( "Raoul", 25 ) );
+        java8inAction.connectTo( raoul );
+
+        Vertex<Person> mario = new Vertex<Person>( new Person( "Mario", 41 ) );
+        java8inAction.connectTo( mario );
+
+        return library;
+    }
+
+    public static class Library {
+    }
+
+    public static class Book {
+        private final String title;
+
+        public Book( String title ) {
+            this.title = title;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+    }
+
+    public static class Person {
+        private final String name;
+        private final int age;
+
+        public Person( String name, int age ) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
+}
