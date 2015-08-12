c72d894f4b7f9d7fd0735b5c5be390c9e0d477be
Petr Siroky <psiroky@redhat.com>
2015-08-12 18:33:08 +0200

    Fix formatting in ByteArrayResourceToStringTest

--- drools-core/src/test/java/org/drools/core/io/impl/ByteArrayResourceToStringTest.java
+++ drools-core/src/test/java/org/drools/core/io/impl/ByteArrayResourceToStringTest.java
@@ -32,31 +32,29 @@ public class ByteArrayResourceToStringTest {
 
     @Parameterized.Parameters(name = "{index}: bytes={0}, encoding={1}")
     public static Collection<Object[]> data() {
-        return new ArrayList<Object[]>(Arrays.asList(new Object[][]
-                        {
-                                {
-                                        Arrays.asList(new Byte[]{10, 20, 30, 40}),
-                                        null,
-                                        "ByteArrayResource[bytes=[10, 20, 30, 40], encoding=null]"
-                                },
-                                {
-                                        Arrays.asList(new Byte[]{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}),
-                                        null,
-                                        "ByteArrayResource[bytes=[10, 20, 30, 40, 50, 60, 70, 80, 90, 100], encoding=null]"
-                                },
-                                {
-                                        Arrays.asList(new Byte[]{10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120}),
-                                        null,
-                                        "ByteArrayResource[bytes=[10, 20, 30, 40, 50, 60, 70, 80, 90, 100, ...], encoding=null]"
-                                },
-                                // non-null encoding
-                                {
-                                        Arrays.asList(new Byte[]{10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120}),
-                                        "UTF-8",
-                                        "ByteArrayResource[bytes=[10, 20, 30, 40, 50, 60, 70, 80, 90, 100, ...], encoding=UTF-8]"
-                                },
-                        }
-        ));
+        return new ArrayList<Object[]>(Arrays.asList(new Object[][]{
+                {
+                        Arrays.asList(new Byte[]{10, 20, 30, 40}),
+                        null,
+                        "ByteArrayResource[bytes=[10, 20, 30, 40], encoding=null]"
+                },
+                {
+                        Arrays.asList(new Byte[]{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}),
+                        null,
+                        "ByteArrayResource[bytes=[10, 20, 30, 40, 50, 60, 70, 80, 90, 100], encoding=null]"
+                },
+                {
+                        Arrays.asList(new Byte[]{10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120}),
+                        null,
+                        "ByteArrayResource[bytes=[10, 20, 30, 40, 50, 60, 70, 80, 90, 100, ...], encoding=null]"
+                },
+                // non-null encoding
+                {
+                        Arrays.asList(new Byte[]{10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120}),
+                        "UTF-8",
+                        "ByteArrayResource[bytes=[10, 20, 30, 40, 50, 60, 70, 80, 90, 100, ...], encoding=UTF-8]"
+                },
+        }));
     }
 
     // using List<Byte> instead of directly byte[] to make sure the bytes are printed as part of the test name
