ae12bedd14d126bd1cebad698aa4b93baf4e938b
Marco Rietveld <mrietvel@redhat.com>
2015-08-24 03:34:14 +0200

    JBPM-4741 - Adding roles to to control access to the REST API

---
+++ kie-internal/src/main/java/org/kie/internal/remote/PermissionConstants.java
@@ -0,0 +1,15 @@
+package org.kie.internal.remote;
+
+public interface PermissionConstants {
+
+    public static final String REST_ROLE            = "rest-all";
+    public static final String REST_PROJECT_ROLE    = "rest-project";
+    public static final String REST_DEPLOYMENT_ROLE = "rest-deployment";
+    public static final String REST_PROCESS_ROLE    = "rest-process";
+    public static final String REST_PROCESS_RO_ROLE = "rest-process-read-only";
+    public static final String REST_TASK_ROLE       = "rest-task";
+    public static final String REST_TASK_RO_ROLE    = "rest-task-read-only";
+    public static final String REST_QUERY_ROLE      = "rest-query";
+    public static final String REST_CLIENT_ROLE          = "rest-client";
+    
+}
