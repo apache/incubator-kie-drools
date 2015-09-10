355821d1f9923c9ef49577c20f1275033a32a3ba
Marco Rietveld <mrietvel@redhat.com>
2015-09-10 17:16:10 +0200

    JBPM-4763 - Add a REST operation to retrieve work item ids
    (cherry picked from commit 7037709fcf16705b88ec7f29a1834d9b1eb21dc8)

---
+++ drools-core/src/main/java/org/drools/core/command/runtime/process/GetWorkItemIdsCommand.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2010 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.core.command.runtime.process;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.drools.core.command.impl.GenericCommand;
+import org.drools.core.command.impl.KnowledgeCommandContext;
+import org.drools.core.process.instance.WorkItem;
+import org.drools.core.process.instance.WorkItemManager;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.command.Context;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+public class GetWorkItemIdsCommand implements GenericCommand<List<Long>> {
+
+    /** generated serial version UID */
+    private static final long serialVersionUID = 1471981530823361925L;
+
+    public List<Long> execute(Context context) {
+        KieSession ksession = ((KnowledgeCommandContext) context).getKieSession();
+        Set<WorkItem> workItems = ((WorkItemManager) ksession.getWorkItemManager()).getWorkItems();
+        List<Long> workItemIds = new ArrayList<Long>(workItems.size());
+        for(WorkItem workItem : workItems ) { 
+            workItemIds.add(workItem.getId());
+        }
+        return workItemIds;
+    }
+
+}
