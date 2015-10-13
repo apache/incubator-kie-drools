eccda54afc00a226a2cde2defe4b7e7d46125b2a
Mario Fusco <mario.fusco@gmail.com>
2015-10-13 11:20:50 +0200

    add test utility classes to check ksession consistency

---
+++ kogito-drools/drools-core/src/test/java/org/drools/core/reteoo/ReteComparator.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2015 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.drools.core.reteoo;
+
+import org.drools.core.common.BaseNode;
+import org.drools.core.impl.InternalKnowledgeBase;
+import org.kie.api.KieBase;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.KnowledgeBase;
+import org.kie.internal.runtime.KnowledgeRuntime;
+
+import static org.drools.core.reteoo.ReteDumper.getSinks;
+
+public class ReteComparator {
+
+    private ReteComparator() { }
+
+    public static void compare(KieBase kbase1, KieBase kbase2) {
+        compare( (InternalKnowledgeBase) kbase1, (InternalKnowledgeBase) kbase2 );
+    }
+
+    public static void compare(KnowledgeBase kbase1, KnowledgeBase kbase2) {
+        compare( (InternalKnowledgeBase) kbase1, (InternalKnowledgeBase) kbase2 );
+    }
+
+    public static void compare(KnowledgeRuntime session1, KnowledgeRuntime session2) {
+        compare( (InternalKnowledgeBase) session1.getKieBase(), (InternalKnowledgeBase) session2.getKieBase() );
+    }
+
+    public static void compare(KieSession session1, KieSession session2) {
+        compare( (InternalKnowledgeBase) session1.getKieBase(), (InternalKnowledgeBase) session2.getKieBase() );
+    }
+
+    public static void compare(InternalKnowledgeBase kBase1, InternalKnowledgeBase kBase2) {
+        compare( kBase1.getRete(), kBase2.getRete() );
+    }
+
+    public static void compare(Rete rete1, Rete rete2) {
+        for (EntryPointNode epn1 : rete1.getEntryPointNodes().values()) {
+            EntryPointNode epn2 = rete2.getEntryPointNode( epn1.getEntryPoint() );
+            compareNodes( epn1, epn2 );
+        }
+    }
+
+    private static void compareNodes(BaseNode node1, BaseNode node2) {
+        if (!node1.equals( node2 )) {
+            throw new RuntimeException( node1 + " and " + node2 + " are not equal as expected" );
+        }
+
+        Sink[] sinks1 = getSinks( node1 );
+        Sink[] sinks2 = getSinks( node2 );
+
+        if (sinks1 == null) {
+            if (sinks2 == null) {
+                return;
+            } else {
+                throw new RuntimeException( node1 + " has no sinks while " + node2 + " has " + sinks2.length + " sinks" );
+            }
+        } else if (sinks2 == null) {
+            throw new RuntimeException( node1 + " has " + sinks1.length + " sinks while " + node2 + " has " + sinks2.length + " sinks" );
+        }
+
+        if (sinks1.length != sinks2.length) {
+            throw new RuntimeException( node1 + " has " + sinks1.length + " sinks while " + node2 + " has no sinks" );
+        }
+
+        for (int i = 0; i < sinks1.length; i++) {
+            if (sinks1[i] instanceof BaseNode) {
+                compareNodes( (BaseNode) sinks1[i], (BaseNode) sinks2[i] );
+            }
+        }
+    }
+}
---
+++ kogito-drools/drools-core/src/test/java/org/drools/core/reteoo/ReteMemoryChecker.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2005 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.core.reteoo;
+
+import org.drools.core.common.BaseNode;
+import org.drools.core.common.InternalWorkingMemory;
+import org.drools.core.common.Memory;
+import org.drools.core.common.MemoryFactory;
+import org.drools.core.impl.InternalKnowledgeBase;
+import org.kie.api.runtime.KieSession;
+
+import static org.drools.core.reteoo.ReteDumper.getSinks;
+
+public class ReteMemoryChecker {
+
+    public static void checkNodeMemories(KieSession session) {
+        InternalKnowledgeBase kbase = (InternalKnowledgeBase)session.getKieBase();
+        for (EntryPointNode entryPointNode : kbase.getRete().getEntryPointNodes().values()) {
+            checkNodeMemory( (InternalWorkingMemory) session, entryPointNode );
+        }
+    }
+
+    private static void checkNodeMemory(InternalWorkingMemory wm, BaseNode node) {
+        if (node instanceof MemoryFactory) {
+            Memory memory = wm.getNodeMemory( (MemoryFactory) node );
+            if ( NodeTypeEnums.ObjectTypeNode == node.getType() ) {
+                if ( !( memory instanceof ObjectTypeNode.ObjectTypeNodeMemory ) ) {
+                    throw new RuntimeException( "Invalid memory type. Node: " + node + " has memory " + memory );
+                }
+            } else if ( NodeTypeEnums.LeftInputAdapterNode == node.getType() ) {
+                if ( !( memory instanceof LeftInputAdapterNode.LiaNodeMemory ) ) {
+                    throw new RuntimeException( "Invalid memory type. Node: " + node + " has memory " + memory );
+                }
+            } else if ( NodeTypeEnums.AlphaNode == node.getType() ) {
+                if ( !( memory instanceof AlphaNode.AlphaMemory ) ) {
+                    throw new RuntimeException( "Invalid memory type. Node: " + node + " has memory " + memory );
+                }
+            } else if ( NodeTypeEnums.isBetaNode( node ) ) {
+                if ( NodeTypeEnums.AccumulateNode == node.getType() ) {
+                    if ( !( memory instanceof AccumulateNode.AccumulateMemory ) ) {
+                        throw new RuntimeException( "Invalid memory type. Node: " + node + " has memory " + memory );
+                    }
+                } else if ( !( memory instanceof BetaMemory ) ) {
+                    throw new RuntimeException( "Invalid memory type. Node: " + node + " has memory " + memory );
+                }
+            } else if ( NodeTypeEnums.FromNode == node.getType() ) {
+                if ( !( memory instanceof FromNode.FromMemory ) ) {
+                    throw new RuntimeException( "Invalid memory type. Node: " + node + " has memory " + memory );
+                }
+            } else if ( NodeTypeEnums.WindowNode == node.getType() ) {
+                if ( !( memory instanceof WindowNode.WindowMemory ) ) {
+                    throw new RuntimeException( "Invalid memory type. Node: " + node + " has memory " + memory );
+                }
+            } else if ( NodeTypeEnums.isTerminalNode( node ) ) {
+                if ( !( memory instanceof PathMemory ) ) {
+                    throw new RuntimeException( "Invalid memory type. Node: " + node + " has memory " + memory );
+                }
+                checkPathMemory((PathMemory)memory);
+            }
+        }
+
+        Sink[] sinks = getSinks( node );
+        if (sinks != null) {
+            for (Sink sink : sinks) {
+                if (sink instanceof BaseNode) {
+                    checkNodeMemory( wm, (BaseNode) sink );
+                }
+            }
+        }
+    }
+
+    private static void checkPathMemory( PathMemory pathMemory ) {
+        SegmentMemory[] smems = pathMemory.getSegmentMemories();
+        if ( !NodeTypeEnums.isLeftTupleSource( smems[0].getRootNode() ) ) {
+            throw new RuntimeException( "The root node for path " + pathMemory + " has to be a LeftTupleSource but is a " + smems[0].getRootNode() );
+        }
+        if ( !NodeTypeEnums.isTerminalNode( smems[smems.length-1].getTipNode() ) ) {
+            throw new RuntimeException( "The tip node for path " + pathMemory + " has to be a TerminalNode but is a " + smems[smems.length-1].getTipNode() );
+        }
+        for (int i = 0; i < smems.length; i++) {
+            if (smems[i] == null) {
+                throw new RuntimeException( "Missing segment in position " + i + " for " + pathMemory );
+            }
+            if (i != smems[i].getPos()) {
+                throw new RuntimeException( "Segment " + smems[i] + " is expected to be in position " + i + " but it is in position " + smems[i].getPos() );
+            }
+        }
+    }
+}
