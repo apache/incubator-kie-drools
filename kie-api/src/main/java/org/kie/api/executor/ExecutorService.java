7a2dc873c017d5e6ee34dafdd7d5e177463a3756
Maciej Swiderski <mswiders@redhat.com>
2015-10-19 15:48:28 +0200

    BZ-1272512 - Business Central deployed together with custom EJB application fails to clone repository via REST

--- kie-api/src/main/java/org/kie/api/executor/ExecutorService.java
+++ kie-api/src/main/java/org/kie/api/executor/ExecutorService.java
@@ -16,11 +16,16 @@
 
 package org.kie.api.executor;
 
+import java.io.InputStream;
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.kie.api.runtime.manager.RuntimeManagerFactory.Factory;
 import org.kie.api.runtime.query.QueryContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -38,10 +43,10 @@ public interface ExecutorService {
 	
 	/**
 	 * Allow to use custom identifiers for the executor instance where default is to rely on local id of clustering of kie
-	 * if present, otherwise use simple 'default-executor'
+	 * if present, otherwise use simple 'default-executor'. Alternatively an jbpm-executor.id file can be dropped on root 
+	 * of the classpath to provide application scoped id instead of JVM scoped (system property)
 	 */
-	public static final String EXECUTOR_ID = System.getProperty("org.kie.executor.id", 
-			System.getProperty("org.uberfire.cluster.local.id", "default-executor"));
+	public static final String EXECUTOR_ID = IdProvider.get();
 
     public List<RequestInfo> getQueuedRequests(QueryContext queryContext);
 
@@ -104,5 +109,38 @@ public interface ExecutorService {
     public RequestInfo getRequestById(Long requestId);
 
     public List<ErrorInfo> getErrorsByRequestId(Long requestId);
+    
+    public static class IdProvider {
+        private static boolean initialized = false;   
+        private static String EXECUTOR_ID;
+        private static Logger logger = LoggerFactory.getLogger(Factory.class);
+
+
+
+        public static String get() {
+            if (!initialized) {
+                EXECUTOR_ID = create();
+            }
+            return EXECUTOR_ID;
+        }
+
+        private static synchronized String create() {
+            initialized = true;
+            String idSystemProperty = System.getProperty("org.kie.executor.id", 
+                    System.getProperty("org.uberfire.cluster.local.id", "default-executor"));
+            try {
+                InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("/jbpm-executor.properties");
+                if (is != null) {
+                    Properties executorProps = new Properties();
+                    executorProps.load(is);
+                    
+                    return idSystemProperty+ "-" + executorProps.getProperty("executor.id");
+                }
+            } catch (Exception e) {
+                logger.warn("Unable to find executor id due to '{}', using default...", e.getMessage());
+            }
+            return idSystemProperty;
+        }
+    }
     
 }
