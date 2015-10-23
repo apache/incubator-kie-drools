1c1204cad208deef7c07ecbe2f062ef397931b90
Maciej Swiderski <mswiders@redhat.com>
2015-10-23 13:25:49 +0200

    BZ-1208056 - Concurrent process execution will fail if it references a new user/group - remove systout

--- kogito-drools/drools-persistence-jpa/src/main/java/org/drools/persistence/jpa/OptimisticLockRetryInterceptor.java
+++ kogito-drools/drools-persistence-jpa/src/main/java/org/drools/persistence/jpa/OptimisticLockRetryInterceptor.java
@@ -84,7 +84,6 @@ public class OptimisticLockRetryInterceptor extends AbstractInterceptor {
             } catch (RuntimeException ex) {
                 // in case there is another interceptor of this type in the stack don't handle it here
                 if (hasInterceptorInStack()) {
-                    System.out.print("###################");
                     throw ex;
                 }
 
