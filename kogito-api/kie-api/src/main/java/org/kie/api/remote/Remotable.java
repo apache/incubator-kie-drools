f637891e8c4d74794e6937c866ae0bd2464f0351
Marco Rietveld <mrietvel@redhat.com>
2015-11-10 14:17:37 +0100

    BZ-1125077 - Limiting serialization classes in a deployment closes #109

--- kogito-api/kie-api/src/main/java/org/kie/api/remote/Remotable.java
+++ kogito-api/kie-api/src/main/java/org/kie/api/remote/Remotable.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -19,27 +19,27 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
- * This annotation should be used by users to specify (user-defined) classes that 
- * should be available to remote services (REST, JMS, WS) when using a workbench. 
- * <br> 
+ * This annotation should be used by users to specify (user-defined) classes that
+ * should be available to remote services (REST, JMS, WS) when using a workbench.
+ * <br>
  * A user will use this annotation on a class that's either part of a kjar or deployment
  * or on a class that is a dependency of the aforementioned kjar or deployment. When this deployment
  * is deployed, the workbench will scan the deployment and the dependency tree of the deployment
- * for classes marked with this annotation. 
+ * for classes marked with this annotation.
  * <br>
- * While classes at the top level in the deployment itself marked with the appropriate JAXB annotations
+ * Classes at the top level in the deployment itself marked with the appropriate JAXB annotations
  * are always available for use with the remote services (regardless of whether the classes
- * are annotated with the {@code @Remotable} annotation, in the dependency tree of the deployment, 
- * only classes marked with this annotation (and with the appropriate JAXB annotations) 
- * will be able to be used with the workbench's remote services.
+ * are annotated with the {@code @Remotable} annotation or not).
+ * However, in the dependency tree of the deployment, only classes marked with this annotation
+ * (and with the appropriate JAXB annotations) will be usable with the workbench's remote services.
+ * <br>
+ * The @Remotable annotation is <em>not</em> inherited.
  */
 
-@Inherited
 @Documented
 @Retention(RUNTIME)
 @Target({ElementType.TYPE})
--- kogito-api/kie-internal/src/main/java/org/kie/internal/runtime/conf/DeploymentDescriptorBuilder.java
+++ kogito-api/kie-internal/src/main/java/org/kie/internal/runtime/conf/DeploymentDescriptorBuilder.java
@@ -24,54 +24,56 @@ import java.util.List;
  *
  */
 public interface DeploymentDescriptorBuilder {
-	
+
 	DeploymentDescriptor get();
-	
+
 	DeploymentDescriptorBuilder persistenceUnit(String pu);
-	
+
 	DeploymentDescriptorBuilder auditPersistenceUnit(String pu);
-	
+
 	DeploymentDescriptorBuilder auditMode(AuditMode mode);
-	
+
 	DeploymentDescriptorBuilder persistenceMode(PersistenceMode mode);
-	
+
 	DeploymentDescriptorBuilder addConfiguration(NamedObjectModel model);
-	
+
 	DeploymentDescriptorBuilder addEnvironmentEntry(NamedObjectModel model);
-	
+
 	DeploymentDescriptorBuilder addWorkItemHandler(NamedObjectModel model);
-	
+
 	DeploymentDescriptorBuilder addGlobal(NamedObjectModel model);
-	
+
 	DeploymentDescriptorBuilder addEventListener(ObjectModel model);
-	
+
 	DeploymentDescriptorBuilder addTaskEventListener(ObjectModel model);
-	
+
 	DeploymentDescriptorBuilder addMarshalingStrategy(ObjectModel model);
-	
+
 	DeploymentDescriptorBuilder addRequiredRole(String role);
-	
+
 	DeploymentDescriptorBuilder addClass(String clazz);
-	
+
 	DeploymentDescriptorBuilder runtimeStrategy(RuntimeStrategy strategy);
-	
+
 	DeploymentDescriptorBuilder setConfiguration(List<NamedObjectModel> models);
-	
+
 	DeploymentDescriptorBuilder setEnvironmentEntries(List<NamedObjectModel> models);
-	
+
 	DeploymentDescriptorBuilder setWorkItemHandlers(List<NamedObjectModel> models);
-	
+
 	DeploymentDescriptorBuilder setGlobals(List<NamedObjectModel> models);
-	
+
 	DeploymentDescriptorBuilder setEventListeners(List<ObjectModel> models);
-	
+
 	DeploymentDescriptorBuilder setTaskEventListeners(List<ObjectModel> models);
-	
+
 	DeploymentDescriptorBuilder setMarshalingStrategies(List<ObjectModel> models);
-	
+
 	DeploymentDescriptorBuilder setRequiredRoles(List<String> roles);
-	
+
 	DeploymentDescriptorBuilder setClasses(List<String> classes);
-	
+
+	DeploymentDescriptorBuilder setLimitSerializationClasses(Boolean limit);
+
 	void setBuildHandler(BuilderHandler handler);
 }
