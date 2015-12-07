4cffc57c75fc0c5b6e7ca75ba287f307d0431e0b
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2015-12-07 10:19:22 +0100

    javadoc nearby distance meter

--- optaplanner-core/src/main/java/org/optaplanner/core/impl/heuristic/selector/common/nearby/NearbyDistanceMeter.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/impl/heuristic/selector/common/nearby/NearbyDistanceMeter.java
@@ -19,6 +19,12 @@ package org.optaplanner.core.impl.heuristic.selector.common.nearby;
 public interface NearbyDistanceMeter<O, D> {
 
     /**
+     * Measures the distance from the origin to the destination.
+     * The distance can be in any unit, such a meters, foot, seconds or milliseconds.
+     * For example, vehicle routing often uses driving time in seconds.
+     * <p/>
+     * Distances can be asymmetrical: the distance from an origin to a destination
+     * often differs from the distance from that destination to that origin.
      * @param origin never null
      * @param destination never null
      * @return Preferably always {@code >= 0.0}. If origin == destination, it usually returns 0.0.
