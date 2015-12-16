8379cbadd183117427fdf39b895e935c0cc4035d
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2015-12-16 12:20:22 +0100

    changes in license header closes #114

--- kie-internal/src/main/java/org/kie/internal/runtime/manager/cdi/qualifier/Singleton.java
+++ kie-internal/src/main/java/org/kie/internal/runtime/manager/cdi/qualifier/Singleton.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 JBoss Inc
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
