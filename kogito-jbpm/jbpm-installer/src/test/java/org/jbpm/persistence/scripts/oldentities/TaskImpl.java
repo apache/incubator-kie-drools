5f61ec0c80273a580da54b716fb5b5e83738a7c0
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2015-12-16 12:20:24 +0100

    changes in license header closes #367

--- kogito-jbpm/jbpm-installer/src/test/java/org/jbpm/persistence/scripts/oldentities/TaskImpl.java
+++ kogito-jbpm/jbpm-installer/src/test/java/org/jbpm/persistence/scripts/oldentities/TaskImpl.java
@@ -1,7 +1,7 @@
 package org.jbpm.persistence.scripts.oldentities;
 
 /**
- * Copyright 2010 JBoss Inc
+ * Copyright 2010 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
