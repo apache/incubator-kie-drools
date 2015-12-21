2d58dff9cec0fdfd1eb50c93ddcf786486d152ba
Mario Fusco <mario.fusco@gmail.com>
2015-12-21 14:46:03 +0100

    [DROOLS-1012] register the ClassLoaderResolver on the ServiceRegistry in kie-ci-osgi Activator
    (cherry picked from commit e48ee17304c6af5568fd9a7af567a87f01e29040)

--- kie-ci-osgi/src/main/java/org/kie/scanner/Activator.java
+++ kie-ci-osgi/src/main/java/org/kie/scanner/Activator.java
@@ -1,8 +1,8 @@
 package org.kie.scanner;
 
 import org.kie.api.Service;
-import org.kie.api.builder.KieScanner;
 import org.kie.api.builder.KieScannerFactoryService;
+import org.kie.internal.utils.ClassLoaderResolver;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
@@ -10,13 +10,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Hashtable;
-import java.util.concurrent.Callable;
 
 public class Activator implements BundleActivator {
 
     protected static final transient Logger logger = LoggerFactory.getLogger(Activator.class);
 
     private ServiceRegistration scannerReg;
+    private ServiceRegistration classResolverReg;
 
     @Override
     public void start(BundleContext context) throws Exception {
@@ -24,11 +24,15 @@ public class Activator implements BundleActivator {
         this.scannerReg = context.registerService( new String[]{ KieScannerFactoryService.class.getName(), Service.class.getName() },
                                                    new KieScannerFactoryServiceImpl(),
                                                    new Hashtable() );
+        this.classResolverReg = context.registerService( new String[]{ ClassLoaderResolver.class.getName(), Service.class.getName() },
+                                                         new MavenClassLoaderResolver(),
+                                                         new Hashtable() );
         logger.info( "kiescanner services registered" );
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
         this.scannerReg.unregister();
+        this.classResolverReg.unregister();
     }
 }
