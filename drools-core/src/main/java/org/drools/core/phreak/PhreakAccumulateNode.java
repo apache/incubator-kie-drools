1e36e4c6365d59660b43ea8d32762be8e775f113
Mario Fusco <mario.fusco@gmail.com>
2016-01-25 19:20:14 +0100

    [DROOLS-998] normalize staged tuples in accumulate
    (cherry picked from commit db9def520d995c640caf6ed30d9b2b1cd67d91ac)

--- drools-core/src/main/java/org/drools/core/phreak/PhreakAccumulateNode.java
+++ drools-core/src/main/java/org/drools/core/phreak/PhreakAccumulateNode.java
@@ -63,7 +63,7 @@ public class PhreakAccumulateNode {
 
         if (srcLeftTuples.getDeleteFirst() != null) {
             // use the real target here, as dealing direct with left tuples
-            doLeftDeletes(accNode, am, wm, srcLeftTuples, trgLeftTuples);
+            doLeftDeletes(accNode, am, wm, srcLeftTuples, trgLeftTuples, stagedLeftTuples);
         }
 
         if (srcRightTuples.getDeleteFirst() != null) {
@@ -570,7 +570,8 @@ public class PhreakAccumulateNode {
                               AccumulateMemory am,
                               InternalWorkingMemory wm,
                               TupleSets<LeftTuple> srcLeftTuples,
-                              TupleSets<LeftTuple> trgLeftTuples) {
+                              TupleSets<LeftTuple> trgLeftTuples,
+                              TupleSets<LeftTuple> stagedLeftTuples) {
         BetaMemory bm = am.getBetaMemory();
         TupleMemory ltm = bm.getLeftTupleMemory();
         Accumulate accumulate = accNode.getAccumulate();
@@ -593,6 +594,16 @@ public class PhreakAccumulateNode {
                                                   false);
 
                 if (accctx.propagated) {
+                    switch (accctx.resultLeftTuple.getStagedType()) {
+                        // handle clash with already staged entries
+                        case LeftTuple.INSERT:
+                            stagedLeftTuples.removeInsert(accctx.resultLeftTuple);
+                            break;
+                        case LeftTuple.UPDATE:
+                            stagedLeftTuples.removeUpdate(accctx.resultLeftTuple);
+                            break;
+                    }
+
                     trgLeftTuples.addDelete(accctx.resultLeftTuple);
                 }
             }
