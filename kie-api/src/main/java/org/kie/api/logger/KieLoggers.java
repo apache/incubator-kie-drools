5b3f7672b3af9f25af7c77fe83decccc83f029fe
Mario Fusco <mario.fusco@gmail.com>
2016-01-27 12:50:27 +0100

    [DROOLS-991] allow to configure maxEventsInMemory in FileLogger

--- kie-api/src/main/java/org/kie/api/logger/KieLoggers.java
+++ kie-api/src/main/java/org/kie/api/logger/KieLoggers.java
@@ -23,12 +23,29 @@ import org.kie.api.event.KieRuntimeEventManager;
  */
 public interface KieLoggers {
 
+    /**
+     * Creates a new FileLogger with the given name for the given session.
+     * The maximum number of log events that are allowed in memory by default is 1000.
+     * If this number is reached, all events are written to the file.
+     */
     KieRuntimeLogger newFileLogger(KieRuntimeEventManager session,
-                                         String fileName);
+                                   String fileName);
+
+    /**
+     * Creates a new FileLogger with the given name for the given session.
+     * also setting the maximum number of log events that are allowed in memory.
+     * If this number is reached, all events are written to the file.
+     *
+     * By setting maxEventsInMemory to 0 makes all events to be immediately flushed to the file.
+     * This option is slow and then not suggested in production but can be useful while debugging.
+     */
+    KieRuntimeLogger newFileLogger(KieRuntimeEventManager session,
+                                   String fileName,
+                                   int maxEventsInMemory);
 
     KieRuntimeLogger newThreadedFileLogger(KieRuntimeEventManager session,
-                                                 String fileName,
-                                                 int interval);
+                                           String fileName,
+                                           int interval);
 
     KieRuntimeLogger newConsoleLogger(KieRuntimeEventManager session);
 
