7975377844a63a76519f302767e72711b7e26a9c
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2016-04-06 14:39:49 +0200

    PLANNER-560 Make bestUninitializedVariableCount and bestScore volatile because they can be fetched from the thread-safe methods of the Solver interface (this doesn't give atomic guarantees, but that's 405's job)

--- optaplanner-core/src/main/java/org/optaplanner/core/impl/solver/scope/DefaultSolverScope.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/impl/solver/scope/DefaultSolverScope.java
@@ -45,8 +45,8 @@ public class DefaultSolverScope<Solution_> {
     protected Score startingInitializedScore; // TODO after initialization => ambiguous with solve()'s planningProblem
 
     protected volatile Solution_ bestSolution;
-    protected int bestUninitializedVariableCount; // TODO remove me by folding me into bestSolution.getScore(): https://issues.jboss.org/browse/PLANNER-405
-    protected Score bestScore; // TODO remove me by folding me into bestSolution.getScore(): https://issues.jboss.org/browse/PLANNER-405
+    protected volatile int bestUninitializedVariableCount; // TODO remove me by folding me into bestSolution.getScore(): https://issues.jboss.org/browse/PLANNER-405
+    protected volatile Score bestScore; // TODO remove me by folding me into bestSolution.getScore(): https://issues.jboss.org/browse/PLANNER-405
     protected Long bestSolutionTimeMillis;
 
 
