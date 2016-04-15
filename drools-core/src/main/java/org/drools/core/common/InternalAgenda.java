392ae23432393be499e0e42a254f8678f220f8c6
Mario Fusco <mario.fusco@gmail.com>
2016-04-15 19:42:01 +0200

    [BZ-1327202] submit queries on the propagation queue

--- drools-core/src/main/java/org/drools/core/common/InternalAgenda.java
+++ drools-core/src/main/java/org/drools/core/common/InternalAgenda.java
@@ -36,7 +36,6 @@ import org.kie.api.runtime.rule.AgendaFilter;
 
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.concurrent.Callable;
 
 public interface InternalAgenda
     extends
@@ -282,7 +281,6 @@ public interface InternalAgenda
 
     boolean isFiring();
     void executeTask( ExecutableEntry executable );
-    <T> T executeCallable( Callable<T> callable );
 
     void activate();
     void deactivate();
