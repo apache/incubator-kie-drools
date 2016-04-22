6aa777aab18c4296e61fa2348c432151f2923f80
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2016-04-22 17:51:22 +0200

    task assigning: add consume and producer threads in UI

--- optaplanner-examples/src/main/java/org/optaplanner/examples/taskassigning/swingui/TaskAssigningPanel.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/taskassigning/swingui/TaskAssigningPanel.java
@@ -17,16 +17,21 @@
 package org.optaplanner.examples.taskassigning.swingui;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
 
+import javax.swing.AbstractAction;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
+import javax.swing.JToggleButton;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.Timer;
 
 import org.optaplanner.examples.common.swingui.SolutionPanel;
-import org.optaplanner.examples.taskassigning.domain.Task;
 import org.optaplanner.examples.taskassigning.domain.TaskAssigningSolution;
-import org.optaplanner.swing.impl.TangoColorFactory;
-
-import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
 
 public class TaskAssigningPanel extends SolutionPanel<TaskAssigningSolution> {
 
@@ -34,10 +39,73 @@ public class TaskAssigningPanel extends SolutionPanel<TaskAssigningSolution> {
 
     private TaskOverviewPanel taskOverviewPanel;
 
+    private JSpinner consumeRateField;
+    private AbstractAction consumeAction;
+    private Timer consumeTimer;
+    private JSpinner produceRateField;
+    private AbstractAction produceAction;
+    private Timer produceTimer;
+
+    private int consumedDurationInSeconds = 0;
+    private int producedDurationInSeconds = 0;
+
     public TaskAssigningPanel() {
         setLayout(new BorderLayout());
+        JPanel headerPanel = createHeaderPanel();
+        add(headerPanel, BorderLayout.NORTH);
         taskOverviewPanel = new TaskOverviewPanel();
         add(new JScrollPane(taskOverviewPanel), BorderLayout.CENTER);
+    }
+
+    private JPanel createHeaderPanel() {
+        JPanel headerPanel = new JPanel(new GridLayout(1, 0));
+        JPanel consumePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        consumePanel.add(new JLabel("Consume rate:"));
+        consumeRateField = new JSpinner(new SpinnerNumberModel(60, 10, 3600, 10));
+        consumePanel.add(consumeRateField);
+        consumeTimer = new Timer(1000, e -> {
+            consumedDurationInSeconds += (Integer) consumeRateField.getValue();
+            repaint();
+System.out.println("c " + consumedDurationInSeconds);
+        });
+        consumeAction = new AbstractAction("Consume") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!consumeTimer.isRunning()) {
+                    consumeRateField.setEnabled(false);
+                    consumeTimer.start();
+                } else {
+                    consumeRateField.setEnabled(true);
+                    consumeTimer.stop();
+                }
+            }
+        };
+        consumePanel.add(new JToggleButton(consumeAction));
+        headerPanel.add(consumePanel);
+        JPanel producePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        producePanel.add(new JLabel("Produce rate:"));
+        produceRateField = new JSpinner(new SpinnerNumberModel(60, 10, 3600, 10));
+        producePanel.add(produceRateField);
+        produceTimer = new Timer(1000, e -> {
+            producedDurationInSeconds += (Integer) produceRateField.getValue();
+            repaint();
+System.out.println("p " + producedDurationInSeconds);
+        });
+        produceAction = new AbstractAction("Produce") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!produceTimer.isRunning()) {
+                    produceRateField.setEnabled(false);
+                    produceTimer.start();
+                } else {
+                    produceRateField.setEnabled(true);
+                    produceTimer.stop();
+                }
+            }
+        };
+        producePanel.add(new JToggleButton(produceAction));
+        headerPanel.add(producePanel);
+        return headerPanel;
     }
 
     @Override
