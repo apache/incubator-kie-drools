4f91484b6225d0aafff7307139735762da31933c
Mario Fusco <mario.fusco@gmail.com>
2016-05-17 16:43:19 +0200

    [DROOLS-1174] rehash instead of remove deleted RightTuples from the RightTupleMemory before processing right updates

--- drools-core/src/main/java/org/drools/core/phreak/RuleNetworkEvaluator.java
+++ drools-core/src/main/java/org/drools/core/phreak/RuleNetworkEvaluator.java
@@ -814,7 +814,7 @@ public class RuleNetworkEvaluator {
         // and so it is the first entry in the wrong bucket
 
         for (RightTuple rightTuple = srcRightTuples.getDeleteFirst(); rightTuple != null; rightTuple = rightTuple.getStagedNext()) {
-            rtm.remove(rightTuple);
+            rtm.removeAdd(rightTuple);
         }
 
         for (RightTuple rightTuple = srcRightTuples.getUpdateFirst(); rightTuple != null; rightTuple = rightTuple.getStagedNext()) {
