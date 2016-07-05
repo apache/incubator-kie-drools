59b0de3030ff2768bab1e8261f2bc46ca04adb80
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2016-07-05 17:11:12 +0200

    PLANNER-611 ignored test that proves that CompositeMove doesn't work in all cases

--- optaplanner-core/src/test/java/org/optaplanner/core/impl/heuristic/move/CompositeMoveTest.java
+++ optaplanner-core/src/test/java/org/optaplanner/core/impl/heuristic/move/CompositeMoveTest.java
@@ -20,11 +20,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.optaplanner.core.impl.domain.variable.descriptor.GenuineVariableDescriptor;
+import org.optaplanner.core.impl.heuristic.selector.move.generic.ChangeMove;
+import org.optaplanner.core.impl.heuristic.selector.move.generic.SwapMove;
 import org.optaplanner.core.impl.score.director.ScoreDirector;
+import org.optaplanner.core.impl.testdata.domain.TestdataEntity;
+import org.optaplanner.core.impl.testdata.domain.TestdataSolution;
+import org.optaplanner.core.impl.testdata.domain.TestdataValue;
 
 import static org.mockito.Mockito.*;
 import static org.optaplanner.core.impl.testdata.util.PlannerAssert.*;
+import static org.optaplanner.core.impl.testdata.util.PlannerTestUtils.mockScoreDirector;
 
 public class CompositeMoveTest {
 
@@ -99,14 +107,46 @@ public class CompositeMoveTest {
         Move first = new DummyMove();
         Move second = new NoChangeMove();
         Move move = CompositeMove.buildMove(Arrays.asList(first, second));
-        Move other = CompositeMove.buildMove(Arrays.asList(first, second));
+        Move other = CompositeMove.buildMove(first, second);
         assertTrue(move.equals(other));
 
-        move = CompositeMove.buildMove(Arrays.asList(first, second));
-        other = CompositeMove.buildMove(Arrays.asList(second, first));
+        move = CompositeMove.buildMove(first, second);
+        other = CompositeMove.buildMove(second, first);
         assertFalse(move.equals(other));
         assertFalse(move.equals(new DummyMove()));
         assertTrue(move.equals(move));
+    }
+
+    @Test @Ignore("PLANNER-611") // TODO https://issues.jboss.org/browse/PLANNER-611
+    public void interconnectedChildMoves() {
+        TestdataSolution solution = new TestdataSolution("s1");
+        TestdataValue v1 = new TestdataValue("v1");
+        TestdataValue v2 = new TestdataValue("v2");
+        TestdataValue v3 = new TestdataValue("v3");
+        solution.setValueList(Arrays.asList(v1, v2, v3));
+        TestdataEntity e1 = new TestdataEntity("e1", v1);
+        TestdataEntity e2 = new TestdataEntity("e2", v2);
+        solution.setEntityList(Arrays.asList(e1, e2));
+
+        GenuineVariableDescriptor<TestdataSolution> variableDescriptor = TestdataEntity.buildVariableDescriptorForValue();
+        Move first = new SwapMove<>(Collections.singletonList(variableDescriptor), e1, e2);
+        Move second = new ChangeMove(e1, variableDescriptor, v3);
+        Move move = CompositeMove.buildMove(first, second);
+
+        assertSame(v1, e1.getValue());
+        assertSame(v2, e2.getValue());
+
+        ScoreDirector scoreDirector = mockScoreDirector(variableDescriptor.getEntityDescriptor().getSolutionDescriptor());
+        Move undoMove = move.createUndoMove(scoreDirector);
+        move.doMove(scoreDirector);
+
+        assertSame(v3, e1.getValue());
+        assertSame(v1, e2.getValue());
+
+        undoMove.doMove(scoreDirector);
+
+        assertSame(v1, e1.getValue());
+        assertSame(v2, e2.getValue());
     }
 
 }
