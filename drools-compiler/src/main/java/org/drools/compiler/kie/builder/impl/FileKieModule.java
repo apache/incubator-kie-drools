0e56464dfcb60969a1f35eacaef44e8d49b53dd8
sivy <sisivy@users.noreply.github.com>
2016-08-05 22:46:00 +0200

    Update FileKieModule.java
    fixed code indentation #2

--- drools-compiler/src/main/java/org/drools/compiler/kie/builder/impl/FileKieModule.java
+++ drools-compiler/src/main/java/org/drools/compiler/kie/builder/impl/FileKieModule.java
@@ -54,27 +54,27 @@ public class FileKieModule extends AbstractKieModule implements InternalKieModul
 
 
     @Override
-    public byte[] getBytes(String pResourceName) {
-    	FileInputStream input = null;
-    	try {
+    public byte[] getBytes(String pResourceName ) {
+        FileInputStream input = null;
+        try {
             File resource = new File( file, pResourceName);
             if( resource.exists() ) {
-            	input = new FileInputStream( resource );
-				return IoUtils.readBytesFromInputStream( input );
+                input = new FileInputStream( resource );
+                return IoUtils.readBytesFromInputStream( input );
             } else {
                 return null;
             }
         } catch ( IOException e ) {
-            throw new RuntimeException("Unable to get bytes for: " + new File( file, pResourceName) +" "+ e.getMessage());
+            throw new RuntimeException("Unable to get bytes for: " + new File( file, pResourceName) + " " +e.getMessage());
         } finally {
-			if( input != null ) {
-				try {
-					input.close();
-				} catch ( IOException e ) {
-					e.printStackTrace();
-				}
-			}
-		}
+            if( input != null ){
+                try {
+                    input.close();
+                } catch ( IOException e ) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
