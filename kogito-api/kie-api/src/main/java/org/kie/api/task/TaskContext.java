bdc2ba879abcdaca4cb6d6f02c816764ab7efe98
Maciej Swiderski <swiderski.maciej@gmail.com>
2016-08-12 11:23:39 +0200

    JBPM-5240 - Provide easy way to access task variables via TaskEvent for process instance (#151)

--- kogito-api/kie-api/src/main/java/org/kie/api/task/TaskContext.java
+++ kogito-api/kie-api/src/main/java/org/kie/api/task/TaskContext.java
@@ -15,9 +15,21 @@
  */
 package org.kie.api.task;
 
+import org.kie.api.task.model.Task;
 
 public interface TaskContext {
 
-	UserGroupCallback getUserGroupCallback();
+    /**
+     * Returns currently configured UserGroupCallback.
+     * @return returns user group callback
+     */
+    UserGroupCallback getUserGroupCallback();
 
+    /**
+     * Loads task (given as argument) variables - both input and output if exists.
+     * In case variables are already set they are not reread from data store.
+     * @param task task which should have variables (both input and output) set
+     * @return returns task with variables set
+     */
+    Task loadTaskVariables(Task task);
 }
