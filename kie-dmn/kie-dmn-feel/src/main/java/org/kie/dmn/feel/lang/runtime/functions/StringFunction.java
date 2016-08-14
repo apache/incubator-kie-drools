16beb4ce7b07c2064866a514235c2f6905849778
Edson Tirelli <ed.tirelli@gmail.com>
2016-08-14 18:48:19 -0400

    Implementing FEEL runtime

---
+++ kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/lang/runtime/functions/StringFunction.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.dmn.feel.lang.runtime.functions;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.util.Arrays;
+import java.util.List;
+
+public class StringFunction
+        extends BaseFEELFunction {
+
+    public StringFunction() {
+        super( "string" );
+    }
+
+    @Override
+    public List<List<String>> getParameterNames() {
+        return Arrays.asList(
+                Arrays.asList( "from" )
+        );
+    }
+
+    public String apply(Object val) {
+        if ( val == null ) {
+            return null;
+        } else {
+            return val.toString();
+        }
+    }
+}
