tarilabs/drools

DROOLS-1235 MBean naming rework
* give KieContainer an optional user defined name
* KieContainer MBean and various refactorings.
* jargon: containerId 
* jargon: configuredReleaseId, resolvedReleaseId
* unit testing
* assuming the containerId can be "recycled" on dispose.
* introducing test for KieServices API with containerId.
* fixing other tests.
* avoid using lock and leverage ConcurrentMap without java8
* fixing objectname conventions.

drools-compiler/src/main/java/org/drools/compiler/management/KieContainerMonitor.java
@@ -1,0 +1,39 @@
+package org.drools.compiler.management;
+
+import org.drools.compiler.kie.builder.impl.InternalKieContainer;
+import org.kie.api.builder.ReleaseId;
+import org.kie.api.management.KieContainerMonitorMXBean;
+import org.kie.api.management.ReleaseIdInfo;
+
+public class KieContainerMonitor implements KieContainerMonitorMXBean {
+	private InternalKieContainer kieContainer;
+
+	public KieContainerMonitor(InternalKieContainer kieContainer) {
+		this.kieContainer = kieContainer;
+	}
+
+	@Override
+	public String getContainerId() {
+		return kieContainer.getContainerId();
+	}
+
+	@Override
+	public String getConfiguredReleaseIdStr() {
+		return kieContainer.getConfiguredReleaseId().toString();
+	}
+
+	@Override
+	public String getResolvedReleaseIdStr() {
+		return kieContainer.getResolvedReleaseId().toString();
+	}
+
+    @Override
+    public ReleaseIdInfo getConfiguredReleaseId() {
+        return ReleaseIdInfo.from(kieContainer.getConfiguredReleaseId());
+    }
+
+    @Override
+    public ReleaseIdInfo getResolvedReleaseId() {
+        return ReleaseIdInfo.from(kieContainer.getResolvedReleaseId());
+    }
+}

apache/incubator-kie-drools

drools-compiler/src/main/java/org/drools/compiler/management/KieContainerMonitor.java
@@ -1,0 +1,39 @@
+package org.drools.compiler.management;
+
+import org.drools.compiler.kie.builder.impl.InternalKieContainer;
+import org.kie.api.builder.ReleaseId;
+import org.kie.api.management.KieContainerMonitorMXBean;
+import org.kie.api.management.GAV;
+
+public class KieContainerMonitor implements KieContainerMonitorMXBean {
+	private InternalKieContainer kieContainer;
+
+	public KieContainerMonitor(InternalKieContainer kieContainer) {
+		this.kieContainer = kieContainer;
+	}
+
+	@Override
+	public String getContainerId() {
+		return kieContainer.getContainerId();
+	}
+
+	@Override
+	public String getConfiguredReleaseIdStr() {
+		return kieContainer.getConfiguredReleaseId().toString();
+	}
+
+	@Override
+	public String getResolvedReleaseIdStr() {
+		return kieContainer.getResolvedReleaseId().toString();
+	}
+
+    @Override
+    public GAV getConfiguredReleaseId() {
+        return GAV.from(kieContainer.getConfiguredReleaseId());
+    }
+
+    @Override
+    public GAV getResolvedReleaseId() {
+        return GAV.from(kieContainer.getResolvedReleaseId());
+    }
+}