6796c51da95b493e2c82a98a7cbc0092f86e6c48
tarilabs <matteo.mortari@gmail.com>
2016-09-01 11:12:11 +0200

    DROOLS-1236 JMX objectnaming separation for Stateful Vs Stateless
    (and for alignment with RHQ/JON plug-in).

--- drools-core/src/main/java/org/drools/core/management/KieSessionMonitoringImpl.java
+++ drools-core/src/main/java/org/drools/core/management/KieSessionMonitoringImpl.java
@@ -1,15 +1,27 @@
 package org.drools.core.management;
 
+import javax.management.ObjectName;
+
 import org.drools.core.common.InternalWorkingMemoryActions;
+import org.kie.api.builder.model.KieSessionModel.KieSessionType;
 import org.kie.api.event.KieRuntimeEventManager;
 import org.kie.api.management.KieSessionMonitoringMBean;
 
 public class KieSessionMonitoringImpl extends GenericKieSessionMonitoringImpl implements KieSessionMonitoringMBean {
     
+    private ObjectName name;
+
     public KieSessionMonitoringImpl(String containerId, String kbaseId, String ksessionName) {
         super(containerId, kbaseId, ksessionName);
+        
+        this.name = DroolsManagementAgent.createObjectNameBy(containerId, kbaseId, KieSessionType.STATEFUL, ksessionName);
     }
     
+    @Override
+    public ObjectName getName() {
+        return this.name;
+    }
+
     @Override
     public long getTotalSessions() {
         return ksessions.size();
