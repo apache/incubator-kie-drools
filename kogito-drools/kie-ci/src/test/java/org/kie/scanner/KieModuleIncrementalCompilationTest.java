79a132b201ef92b47e9a6fda7d58c46435c774dd
Michael Anstis <manstis@users.noreply.github.com>
2016-09-21 11:32:54 +0100

    More Unit Tests involving Incremental Compilation and Functions (#923)

--- kogito-drools/kie-ci/src/test/java/org/kie/scanner/KieModuleIncrementalCompilationTest.java
+++ kogito-drools/kie-ci/src/test/java/org/kie/scanner/KieModuleIncrementalCompilationTest.java
@@ -18,7 +18,6 @@ package org.kie.scanner;
 import java.util.Collection;
 import java.util.HashMap;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
@@ -48,32 +47,32 @@ public class KieModuleIncrementalCompilationTest extends AbstractKieCiTest {
         KieServices ks = KieServices.Factory.get();
 
         KieFileSystem kfs = ks.newKieFileSystem()
-                .write("src/main/resources/r1.drl", drl1)
-                .write("src/main/resources/r2.drl", drl2);
+                .write( "src/main/resources/r1.drl", drl1 )
+                .write( "src/main/resources/r2.drl", drl2 );
 
         KieBuilder kieBuilder = ks.newKieBuilder( kfs ).buildAll();
 
-        assertEquals(2, getRuleNames(kieBuilder).get("org.kie.scanner").size());
+        assertEquals( 2, getRuleNames( kieBuilder ).get( "org.kie.scanner" ).size() );
 
-        kfs.delete("src/main/resources/r2.drl");
+        kfs.delete( "src/main/resources/r2.drl" );
 
-        IncrementalResults addResults = ((InternalKieBuilder) kieBuilder).createFileSet("src/main/resources/r2.drl").build();
+        IncrementalResults addResults = ( (InternalKieBuilder) kieBuilder ).createFileSet( "src/main/resources/r2.drl" ).build();
 
-        assertEquals(1, getRuleNames(kieBuilder).get("org.kie.scanner").size());
+        assertEquals( 1, getRuleNames( kieBuilder ).get( "org.kie.scanner" ).size() );
     }
 
-    private HashMap<String, Collection<String>> getRuleNames(KieBuilder kieBuilder) {
-        KieModuleMetaData kieModuleMetaData = getKieModuleMetaData(kieBuilder);
+    private HashMap<String, Collection<String>> getRuleNames( KieBuilder kieBuilder ) {
+        KieModuleMetaData kieModuleMetaData = getKieModuleMetaData( kieBuilder );
         HashMap<String, Collection<String>> ruleNames = new HashMap<String, Collection<String>>();
 
-        for (String packageName : kieModuleMetaData.getPackages()) {
-            ruleNames.put(packageName, kieModuleMetaData.getRuleNamesInPackage(packageName));
+        for ( String packageName : kieModuleMetaData.getPackages() ) {
+            ruleNames.put( packageName, kieModuleMetaData.getRuleNamesInPackage( packageName ) );
         }
 
         return ruleNames;
     }
 
-    private KieModuleMetaData getKieModuleMetaData(KieBuilder kieBuilder) {
+    private KieModuleMetaData getKieModuleMetaData( KieBuilder kieBuilder ) {
         return KieModuleMetaData.Factory.newKieModuleMetaData( ( (InternalKieBuilder) kieBuilder ).getKieModuleIgnoringErrors() );
     }
 
@@ -105,13 +104,111 @@ public class KieModuleIncrementalCompilationTest extends AbstractKieCiTest {
         kfs.write( "src/main/resources/KBase1/r1.drl", drl1 );
         KieBuilder kieBuilder = ks.newKieBuilder( kfs ).buildAll();
         assertEquals( 1,
-                kieBuilder.getResults().getMessages( org.kie.api.builder.Message.Level.ERROR ).size() );
+                      kieBuilder.getResults().getMessages( org.kie.api.builder.Message.Level.ERROR ).size() );
 
         //Add file with error - expect 1 "added" error message
         kfs.write( "src/main/resources/KBase1/r2.drl", drl2 );
         IncrementalResults addResults = ( (InternalKieBuilder) kieBuilder ).createFileSet( "src/main/resources/KBase1/r2.drl" ).build();
 
         assertEquals( 1, addResults.getAddedMessages().size() );
+        assertEquals( 0, addResults.getRemovedMessages().size() );
+    }
+
+    @Test
+    public void checkIncrementalCompilationWithRuleFunctionRule() throws Exception {
+        String rule_1 = "package org.kie.scanner\n" +
+                "rule R1 when\n" +
+                "   String()\n" +
+                "then\n" +
+                "end\n";
+
+        String rule_2 = "package org.kie.scanner\n" +
+                "rule R1 when\n" +
+                "   String()\n" +
+                "then\n" +
+                "   System.out.println(MyFunction());\n" +
+                "end\n";
+
+        String function = "package org.kie.scanner\n" +
+                "function int MyFunction() {\n" +
+                "   return 1;\n" +
+                "}\n";
+
+        KieServices ks = KieServices.Factory.get();
+
+        KieFileSystem kfs = ks.newKieFileSystem();
+
+        kfs.write( "src/main/resources/org/kie/scanner/rule.drl", rule_1 );
+        KieBuilder kieBuilder = ks.newKieBuilder( kfs ).buildAll();
+        assertEquals( 0,
+                      kieBuilder.getResults().getMessages( org.kie.api.builder.Message.Level.ERROR ).size() );
+
+        kfs.write( "src/main/resources/org/kie/scanner/function.drl", function );
+        IncrementalResults addResults1 = ( (InternalKieBuilder) kieBuilder ).createFileSet( "src/main/resources/org/kie/scanner/function.drl" ).build();
+        assertEquals( 0, addResults1.getAddedMessages().size() );
+        assertEquals( 0, addResults1.getRemovedMessages().size() );
+
+        kfs.write( "src/main/resources/org/kie/scanner/rule.drl", rule_2 );
+        IncrementalResults addResults2 = ( (InternalKieBuilder) kieBuilder ).createFileSet( "src/main/resources/org/kie/scanner/rule.drl" ).build();
+        assertEquals( 0, addResults2.getAddedMessages().size() );
+        assertEquals( 0, addResults2.getRemovedMessages().size() );
+    }
+
+    @Test
+    public void checkIncrementalCompilationWithRuleThenFunction() throws Exception {
+        String rule = "package org.kie.scanner\n" +
+                "rule R1 when\n" +
+                "   String()\n" +
+                "then\n" +
+                "   System.out.println(MyFunction());\n" +
+                "end\n";
+
+        String function = "package org.kie.scanner\n" +
+                "function int MyFunction() {\n" +
+                "   return 1;\n" +
+                "}\n";
+
+        KieServices ks = KieServices.Factory.get();
+
+        KieFileSystem kfs = ks.newKieFileSystem();
+
+        kfs.write( "src/main/resources/org/kie/scanner/rule.drl", rule );
+        KieBuilder kieBuilder = ks.newKieBuilder( kfs ).buildAll();
+        assertEquals( 1,
+                      kieBuilder.getResults().getMessages( org.kie.api.builder.Message.Level.ERROR ).size() );
+
+        kfs.write( "src/main/resources/org/kie/scanner/function.drl", function );
+        IncrementalResults addResults1 = ( (InternalKieBuilder) kieBuilder ).createFileSet( "src/main/resources/org/kie/scanner/function.drl" ).build();
+        assertEquals( 0, addResults1.getAddedMessages().size() );
+        assertEquals( 1, addResults1.getRemovedMessages().size() );
+    }
+
+    @Test
+    public void checkIncrementalCompilationWithFunctionThenRule() throws Exception {
+        String rule = "package org.kie.scanner\n" +
+                "rule R1 when\n" +
+                "   String()\n" +
+                "then\n" +
+                "   System.out.println(MyFunction());\n" +
+                "end\n";
+
+        String function = "package org.kie.scanner\n" +
+                "function int MyFunction() {\n" +
+                "   return 1;\n" +
+                "}\n";
+
+        KieServices ks = KieServices.Factory.get();
+
+        KieFileSystem kfs = ks.newKieFileSystem();
+
+        kfs.write( "src/main/resources/org/kie/scanner/function.drl", function );
+        KieBuilder kieBuilder = ks.newKieBuilder( kfs ).buildAll();
+        assertEquals( 0,
+                      kieBuilder.getResults().getMessages( org.kie.api.builder.Message.Level.ERROR ).size() );
+
+        kfs.write( "src/main/resources/org/kie/scanner/rule.drl", rule );
+        IncrementalResults addResults = ( (InternalKieBuilder) kieBuilder ).createFileSet( "src/main/resources/org/kie/scanner/rule.drl" ).build();
+        assertEquals( 0, addResults.getAddedMessages().size() );
         assertEquals( 0, addResults.getRemovedMessages().size() );
     }
 
