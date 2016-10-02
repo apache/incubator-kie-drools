340c0030732c7fe444059e0cd8f1ac996cd09e67
Edson Tirelli <ed.tirelli@gmail.com>
2016-10-02 10:20:21 -0400

    Initial DMN core API implementation

--- kie-dmn-core/src/main/java/org/kie/dmn/core/impl/DMNModelImpl.java
+++ kie-dmn-core/src/main/java/org/kie/dmn/core/impl/DMNModelImpl.java
@@ -19,7 +19,11 @@ package org.kie.dmn.core.impl;
 import org.kie.dmn.core.runtime.DMNContext;
 import org.kie.dmn.core.runtime.DMNModel;
 import org.kie.dmn.core.runtime.DMNResult;
-import org.kie.dmn.feel.model.v1_1.Definitions;
+import org.kie.dmn.feel.FEEL;
+import org.kie.dmn.feel.model.v1_1.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DMNModelImpl implements DMNModel {
 
@@ -46,6 +50,40 @@ public class DMNModelImpl implements DMNModel {
 
     @Override
     public DMNResult evaluateAll(DMNContext context) {
+        List<DRGElement> drgElements = definitions.getDrgElement();
+        DMNResultImpl result = new DMNResultImpl();
+        result.setContext( context.clone() );
+        for( DRGElement e : drgElements ) {
+            if( e instanceof Decision ) {
+                Decision decision = (Decision) e;
+                List<InformationRequirement> missingInput = new ArrayList<>(  );
+                for( InformationRequirement ir : decision.getInformationRequirement() ) {
+                    if( ir.getRequiredInput() != null ) {
+                        InputData input = findElementById( ir.getRequiredInput().getHref() );
+                        String name = input.getName();
+                        if( ! context.isDefined( name ) ) {
+                            missingInput.add( ir );
+                        }
+                    }
+                }
+                if( ! missingInput.isEmpty() ) {
+                    System.out.println("Missing inputs: "+missingInput );
+                    return null;
+                }
+                Object val = FEEL.newInstance().evaluate( ((LiteralExpression) decision.getExpression()).getText(), result.getContext().getAll() );
+                result.getContext().set( decision.getVariable().getName(), val );
+            }
+        }
+        return result;
+    }
+
+    private InputData findElementById(String href) {
+        String id = href.contains( "#" ) ? href.substring( href.indexOf( '#' ) + 1 ) : href;
+        for( DRGElement e : definitions.getDrgElement() ) {
+            if( e instanceof InputData && id.equals( e.getId() ) ) {
+                return (InputData) e;
+            }
+        }
         return null;
     }
 
