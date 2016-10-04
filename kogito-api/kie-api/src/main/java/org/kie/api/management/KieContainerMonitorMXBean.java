3d42b6b043cbf55ad3546d824517421d94130b89
tarilabs <matteo.mortari@gmail.com>
2016-10-04 09:25:02 +0200

    DROOLS-1316 Missing MBean for Classpath KieContainer causes RHQ/JON plug-in failure to display the whole hierarchy (#174)

--- kogito-api/kie-api/src/main/java/org/kie/api/management/KieContainerMonitorMXBean.java
+++ kogito-api/kie-api/src/main/java/org/kie/api/management/KieContainerMonitorMXBean.java
@@ -1,29 +1,38 @@
 package org.kie.api.management;
 
 public interface KieContainerMonitorMXBean {
+    public static final GAV CLASSPATH_KIECONTAINER_RELEASEID = new GAV("classpath", "classpath", "0.0.0");
 
     String getContainerId();
 
     /**
-     * The RelaseId configured while creating the Kiecontainer.
+     * The RelaseId configured while creating the KieContainer.
+     * <p />
+     * If the KieContainer has been created from Classpath instead, the hardcoded value {@link #CLASSPATH_KIECONTAINER_RELEASEID} will be returned. 
      * @return
      */
     GAV getConfiguredReleaseId();
 
     /**
-     * The RelaseId configured while creating the Kiecontainer.
+     * The RelaseId configured while creating the KieContainer.
+     * <p />
+     * If the KieContainer has been created from Classpath instead, the hardcoded value {@link #CLASSPATH_KIECONTAINER_RELEASEID} will be returned. 
      * @return
      */
     String getConfiguredReleaseIdStr();
 
     /**
      * The actual resolved ReleaseId.
+     * <p />
+     * If the KieContainer has been created from Classpath instead, the hardcoded value {@link #CLASSPATH_KIECONTAINER_RELEASEID} will be returned. 
      * @return
      */
     GAV getResolvedReleaseId();
 
     /**
      * The actual resolved ReleaseId.
+     * <p />
+     * If the KieContainer has been created from Classpath instead, the hardcoded value {@link #CLASSPATH_KIECONTAINER_RELEASEID} will be returned. 
      * @return
      */
     String getResolvedReleaseIdStr();
