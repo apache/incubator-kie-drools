751d88d8e169004f8ecfae6d9d06ecf0f774e637
tarilabs <matteo.mortari@gmail.com>
2016-10-13 16:46:00 +0200

    Custom XMLUnit similarity definition for default attributes. (#4)
    * Custom XMLUnit similarity definition for default attributes.
    
    * itemComponent is an alias for itemDefinition, having a opt.default attr.

--- kie-dmn/kie-dmn-backend/src/test/java/org/kie/dmn/backend/unmarshalling/v1_1/UnmarshalMarshalTest.java
+++ kie-dmn/kie-dmn-backend/src/test/java/org/kie/dmn/backend/unmarshalling/v1_1/UnmarshalMarshalTest.java
@@ -11,7 +11,11 @@ import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.xml.namespace.QName;
 import javax.xml.transform.Source;
 
 import org.junit.Ignore;
@@ -23,38 +27,35 @@ import org.xmlunit.builder.Input;
 import org.xmlunit.diff.Comparison;
 import org.xmlunit.diff.ComparisonListener;
 import org.xmlunit.diff.ComparisonResult;
+import org.xmlunit.diff.ComparisonType;
 import org.xmlunit.diff.DOMDifferenceEngine;
 import org.xmlunit.diff.Diff;
 import org.xmlunit.diff.DifferenceEngine;
+import org.xmlunit.diff.DifferenceEvaluators;
 
 
 public class UnmarshalMarshalTest {
     
-    @Ignore("vaguely working, missing xmlnamespaces....")
     @Test 
     public void test0001() throws Exception {
         testRoundTrip("org/kie/dmn/backend/unmarshalling/v1_1/", "0001-input-data-string.dmn");
     }
-    
-    @Ignore("vaguely working, missing xmlnamespaces....")
+
     @Test 
     public void test0002() throws Exception {
         testRoundTrip("org/kie/dmn/backend/unmarshalling/v1_1/", "0002-input-data-number.dmn");
     }
     
-    @Ignore("vaguely working, missing xmlnamespaces....")
     @Test 
     public void test0003() throws Exception {
         testRoundTrip("org/kie/dmn/backend/unmarshalling/v1_1/", "0003-input-data-string-allowed-values.dmn");
     }
     
-    @Ignore("vaguely working, missing xmlnamespaces...., and CDATA is not marshalled - is it really required?")
     @Test
     public void testDish() throws Exception {
         testRoundTrip("", "dish-decision.xml");
     }
     
-    @Ignore("still converter issues.")
     @Test
     public void testCh11() throws Exception {
         testRoundTrip("", "ch11example.xml");
@@ -94,21 +95,74 @@ public class UnmarshalMarshalTest {
         targetFos.flush();
         targetFos.close();
         
-        
+        System.out.println("Default XMLUnit comparison:");
         Source control = Input.fromFile( inputXMLFile ).build();
         Source test = Input.fromFile( new File(baseOutputDir, subdir + "b." + xmlfile) ).build();
-        Diff myDiff = DiffBuilder
+        Diff allDiffsSimilarAndDifferent = DiffBuilder
                 .compare( control )
                 .withTest( test )
-                .withDifferenceListeners(
-                        new ComparisonListener() {
-                            public void comparisonPerformed(Comparison comparison, ComparisonResult outcome) {
-                                System.out.println("found a difference: " + comparison);
-                            }
-                        }    
-                        )
-                .ignoreWhitespace()
                 .build();
-        assertFalse("XML similar " + myDiff.toString(), myDiff.hasDifferences());
+        allDiffsSimilarAndDifferent.getDifferences().forEach(System.out::println);
+
+        System.out.println("XMLUnit comparison with customized similarity for defaults:");
+        // in the following a manual DifferenceEvaluator is needed until XMLUnit is configured for properly parsing the XSD linked inside the XML,
+        // in order to detect the optional+defaultvalue attributes of xml element which might be implicit in source-test, and explicit in test-serialized.
+        /*
+         * $ grep -Eo "<xsd:attribute name=\\\"([^\\\"]*)\\\" type=\\\"([^\\\"]*)\\\" use=\\\"optional\\\" default=\\\"([^\\\"])*\\\"" dmn.xsd 
+<xsd:attribute name="expressionLanguage" type="xsd:anyURI" use="optional" default="http://www.omg.org/spec/FEEL/20140401"
+<xsd:attribute name="typeLanguage" type="xsd:anyURI" use="optional" default="http://www.omg.org/spec/FEEL/20140401"
+<xsd:attribute name="isCollection" type="xsd:boolean" use="optional" default="false"
+<xsd:attribute name="hitPolicy" type="tHitPolicy" use="optional" default="UNIQUE"
+<xsd:attribute name="preferredOrientation" type="tDecisionTableOrientation" use="optional" default="Rule-as-Row"
+
+         */
+        Set<QName> attrWhichCanDefault = new HashSet<QName>();
+        attrWhichCanDefault.addAll(Arrays.asList(new QName[] {
+                new QName("expressionLanguage"), 
+                new QName("typeLanguage"), 
+                new QName("isCollection"), 
+                new QName("hitPolicy"), 
+                new QName("preferredOrientation")
+                }));
+        Set<String> nodeHavingDefaultableAttr = new HashSet<>();
+        nodeHavingDefaultableAttr.addAll(Arrays.asList(new String[]{"definitions", "decisionTable", "itemDefinition", "itemComponent"}));
+        Diff checkSimilar = DiffBuilder
+                .compare( control )
+                .withTest( test )
+                .withDifferenceEvaluator(
+                        DifferenceEvaluators.chain(DifferenceEvaluators.Default,
+                        ((comparison, outcome) -> {
+                            if (outcome == ComparisonResult.DIFFERENT && comparison.getType() == ComparisonType.ELEMENT_NUM_ATTRIBUTES) {
+                                if (comparison.getControlDetails().getTarget().getNodeName().equals( comparison.getTestDetails().getTarget().getNodeName() )
+                                        && nodeHavingDefaultableAttr.contains( comparison.getControlDetails().getTarget().getNodeName() )) {
+                                    return ComparisonResult.SIMILAR;
+                                }
+                            }
+                            if (outcome == ComparisonResult.DIFFERENT && comparison.getType() == ComparisonType.ATTR_NAME_LOOKUP) {
+                                boolean testIsDefaulableAttribute = false;
+                                QName whichDefaultableAttr = null;
+                                if (comparison.getControlDetails().getValue() == null && attrWhichCanDefault.contains(comparison.getTestDetails().getValue())) {
+                                    for (QName a : attrWhichCanDefault) {
+                                        boolean check = comparison.getTestDetails().getXPath().endsWith("@"+a);
+                                        if (check) {
+                                            testIsDefaulableAttribute = true;
+                                            whichDefaultableAttr = a;
+                                            continue;
+                                        }
+                                    }
+                                }
+                                if ( testIsDefaulableAttribute ) {
+                                    if (comparison.getTestDetails().getXPath().equals(comparison.getControlDetails().getXPath() + "/@" + whichDefaultableAttr )) {
+                                        return ComparisonResult.SIMILAR;
+                                    }
+                                }
+                            }
+                        return outcome;
+                    })))
+                .ignoreWhitespace()
+                .checkForSimilar()
+                .build();
+        checkSimilar.getDifferences().forEach(System.err::println);
+        assertFalse("XML are NOT similar: " + checkSimilar.toString(), checkSimilar.hasDifferences());
     }
 }
