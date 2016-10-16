5198c4382a975395df2f4e9280c3b03279fe9425
Edson Tirelli <ed.tirelli@gmail.com>
2016-10-16 12:33:43 -0400

    Refactoring package names and marshaller interface

---
+++ kie-dmn/kie-dmn-backend/src/main/java/org/kie/dmn/backend/marshalling/v1_1/xstream/ExtensionElementsConverter.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.dmn.backend.marshalling.v1_1.xstream;
+
+import org.kie.dmn.feel.model.v1_1.DMNElement;
+import org.kie.dmn.feel.model.v1_1.DMNElement.ExtensionElements;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+
+public class ExtensionElementsConverter extends DMNModelInstrumentedBaseConverter {
+
+    @Override
+    protected void assignAttributes(HierarchicalStreamReader reader, Object parent) {
+        super.assignAttributes(reader, parent);
+        // no attributes.
+    }
+
+    @Override
+    public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+        Object obj = createModelObject();
+        assignAttributes( reader, obj );
+        while ( reader.hasMoreChildren() ) {
+            reader.moveDown();
+            String nodeName = reader.getNodeName();
+            // skipping nodeName
+            reader.moveUp();
+        }
+        return obj;
+    }
+
+    @Override
+    protected void writeAttributes(HierarchicalStreamWriter writer, Object parent) {
+        super.writeAttributes(writer, parent);
+        // no attributes.
+    }
+
+    public ExtensionElementsConverter(XStream xstream) {
+        super(xstream);
+    }
+
+    @Override
+    protected Object createModelObject() {
+        return new DMNElement.ExtensionElements();
+    }
+
+    @Override
+    public boolean canConvert(Class clazz) {
+        return clazz.equals( ExtensionElements.class );
+    }
+
+}
