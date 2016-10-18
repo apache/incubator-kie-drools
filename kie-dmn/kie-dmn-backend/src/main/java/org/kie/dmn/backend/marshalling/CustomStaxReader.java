d8ac9bad6c1a8befee72e8519056cbbcb81a1b9b
tarilabs <matteo.mortari@gmail.com>
2016-10-18 15:12:00 +0200

    Unmarshalling Namespace context at the level of each element, draft.

---
+++ kie-dmn/kie-dmn-backend/src/main/java/org/kie/dmn/backend/marshalling/CustomStaxReader.java
@@ -0,0 +1,35 @@
+package org.kie.dmn.backend.marshalling;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.XMLConstants;
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.stream.XMLStreamReader;
+
+import com.thoughtworks.xstream.io.xml.QNameMap;
+import com.thoughtworks.xstream.io.xml.StaxReader;
+
+
+public class CustomStaxReader extends StaxReader {
+
+    /** 
+     * ATTENTION use read-only methods do not mutate
+     */
+    private XMLStreamReader in;
+
+    public CustomStaxReader(QNameMap qnameMap, XMLStreamReader in) {
+        super(qnameMap, in);
+        this.in = in;
+    }
+    
+    public Map<String, String> getNSContext() {
+        Map<String, String> nsContext = new HashMap<>();
+        for (int nsIndex = 0; nsIndex < in.getNamespaceCount(); nsIndex++) {
+            String nsPrefix = in.getNamespacePrefix(nsIndex);
+            String nsId = in.getNamespaceURI(nsIndex);
+            nsContext.put(in.getNamespacePrefix(nsIndex), in.getNamespaceURI(nsIndex) );
+          }
+        return nsContext;
+    }
+}
