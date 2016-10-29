cf9db2ca4233e74f516d85015d7f6858f154183a
tarilabs <matteo.mortari@gmail.com>
2016-10-29 20:40:36 +0200

    Basics FEEL Decision Table support (#8)
    * Very basics working but still missing a lot of tests and cases.
    
    * Decision Table, Named parameter support.

--- kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/DecisionTableFunction.java
+++ kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/DecisionTableFunction.java
@@ -16,11 +16,123 @@
 
 package org.kie.dmn.feel.runtime.functions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.kie.dmn.feel.lang.EvaluationContext;
+import org.kie.dmn.feel.runtime.Range;
+import org.kie.dmn.feel.runtime.UnaryTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DecisionTableFunction
         extends BaseFEELFunction {
-
+    
+    private static final Logger LOG = LoggerFactory.getLogger( DecisionTableFunction.class );
+    
     public DecisionTableFunction() {
         super( "decision table" );
     }
 
+    public Object apply( @ParameterName("outputs") Object outputs, @ParameterName("input expression list") List inputExpressionList,
+                           @ParameterName("rule list") List<List> ruleList, @ParameterName("hit policy") Object hitPolicy) {
+        
+        List<DecisionRule> decisionRules = ruleList.stream()
+                .map(o->DecisionTableFunction.toDecisionRule(o, inputExpressionList.size()))
+                .collect(Collectors.toList());
+        
+        // TODO shoud the name be the information item?
+        // TODO what if inputExpressionList does not contain only strings?
+        return new ConcreteDTFunction(UUID.randomUUID().toString(), inputExpressionList, decisionRules);
+    }
+    
+    public static DecisionRule toDecisionRule(List<?> rule, int inputSize) {
+        // TODO should be check indeed block of inputSize n inputs, followed by block of outputs.
+        DecisionRule dr = new DecisionRule();
+        for ( int i=0 ; i<rule.size() ; i++ ) {
+            Object o = rule.get(i);
+            if ( i<inputSize ) {
+                if ( o instanceof UnaryTest ) {
+                    dr.getInputEntry().add((UnaryTest) o);
+                } else if ( o instanceof Range ) {
+                    dr.getInputEntry().add(x -> ((Range)o).includes((Comparable<?>)x));
+                } else {
+                    dr.getInputEntry().add(x -> x.equals(o));
+                }
+            } else {
+                dr.getOutputEntry().add(o);
+            }
+        }
+        return dr;
+    }
+    
+    public static class DecisionRule {
+        private List<UnaryTest> inputEntry;
+        private List<Object> outputEntry;
+
+        public List<UnaryTest> getInputEntry() {
+            if ( inputEntry == null ) {
+                inputEntry = new ArrayList<>();
+            }
+            return this.inputEntry;
+        }
+
+        public List<Object> getOutputEntry() {
+            if ( outputEntry == null ) {
+                outputEntry = new ArrayList<>();
+            }
+            return this.outputEntry;
+        }
+    }
+    
+    public static class ConcreteDTFunction extends BaseFEELFunction {
+        private List<DecisionRule> decisionRules;
+        private List<String> inputs;
+
+        public ConcreteDTFunction(String name, List<String> inputs, List<DecisionRule> decisionRules) {
+            super(name);
+            this.decisionRules = decisionRules;
+            this.inputs = inputs;
+        }
+        
+        public Object apply(EvaluationContext ctx, Object[] params) {            
+            if (decisionRules.isEmpty()) {
+                return null;
+            }
+            
+            if (params.length != decisionRules.get(0).getInputEntry().size()) {
+                LOG.error("The parameters supplied does not match input expression list");
+                return null;
+            }
+
+            for ( DecisionRule decisionRule : decisionRules ) {
+                Boolean ruleMatches = IntStream.range(0, params.length)                         // TODO could short-circuit by using for/continue
+                        .mapToObj(i -> decisionRule.getInputEntry().get(i).apply(params[i]))
+                        .reduce((a, b) -> a && b)
+                        .orElse(false);
+                if (ruleMatches) {
+                    if (decisionRule.getOutputEntry().size() == 1) {
+                        return decisionRule.getOutputEntry().get(0);
+                    } else {
+                        return decisionRule.getOutputEntry();
+                    }
+                }
+            }
+            
+            return null;
+        }
+
+        @Override
+        protected boolean isCustomFunction() {
+            return true;
+        }
+        
+        public List<List<String>> getParameterNames() {
+            return Arrays.asList( inputs );
+        }
+    }
 }
