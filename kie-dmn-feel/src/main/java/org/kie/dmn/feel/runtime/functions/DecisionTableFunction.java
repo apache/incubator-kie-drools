a74b0a392c1a4c8f47688c2dff6f6189f619a77b
Edson Tirelli <ed.tirelli@gmail.com>
2016-10-29 15:33:12 -0400

    Minor fixes on the FEEL decision table tests

--- kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/DecisionTableFunction.java
+++ kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/DecisionTableFunction.java
@@ -16,63 +16,61 @@
 
 package org.kie.dmn.feel.runtime.functions;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 import org.kie.dmn.feel.lang.EvaluationContext;
 import org.kie.dmn.feel.runtime.Range;
 import org.kie.dmn.feel.runtime.UnaryTest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class DecisionTableFunction
         extends BaseFEELFunction {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger( DecisionTableFunction.class );
-    
+
     public DecisionTableFunction() {
         super( "decision table" );
     }
 
-    public Object apply( @ParameterName("outputs") Object outputs, @ParameterName("input expression list") List inputExpressionList,
-                           @ParameterName("rule list") List<List> ruleList, @ParameterName("hit policy") Object hitPolicy) {
-        
+    public Object apply(
+            @ParameterName("outputs") Object outputs, @ParameterName("input expression list") Object inputExpressionList,
+            @ParameterName("rule list") List<List> ruleList, @ParameterName("hit policy") String hitPolicy) {
+        // input expression list can have a single element or be a list
+        List<String> input = inputExpressionList instanceof List ? (List) inputExpressionList : Collections.singletonList( (String) inputExpressionList );
+
         List<DecisionRule> decisionRules = ruleList.stream()
-                .map(o->DecisionTableFunction.toDecisionRule(o, inputExpressionList.size()))
-                .collect(Collectors.toList());
-        
-        // TODO shoud the name be the information item?
-        // TODO what if inputExpressionList does not contain only strings?
-        return new ConcreteDTFunction(UUID.randomUUID().toString(), inputExpressionList, decisionRules);
+                .map( o -> DecisionTableFunction.toDecisionRule( o, input.size() ) )
+                .collect( Collectors.toList() );
+
+        return new ConcreteDTFunction( UUID.randomUUID().toString(), input, decisionRules, HitPolicy.fromString( hitPolicy ) );
     }
-    
+
     public static DecisionRule toDecisionRule(List<?> rule, int inputSize) {
         // TODO should be check indeed block of inputSize n inputs, followed by block of outputs.
         DecisionRule dr = new DecisionRule();
-        for ( int i=0 ; i<rule.size() ; i++ ) {
-            Object o = rule.get(i);
-            if ( i<inputSize ) {
+        for ( int i = 0; i < rule.size(); i++ ) {
+            Object o = rule.get( i );
+            if ( i < inputSize ) {
                 if ( o instanceof UnaryTest ) {
-                    dr.getInputEntry().add((UnaryTest) o);
+                    dr.getInputEntry().add( (UnaryTest) o );
                 } else if ( o instanceof Range ) {
-                    dr.getInputEntry().add(x -> ((Range)o).includes((Comparable<?>)x));
+                    dr.getInputEntry().add( x -> ((Range) o).includes( (Comparable<?>) x ) );
                 } else {
-                    dr.getInputEntry().add(x -> x.equals(o));
+                    dr.getInputEntry().add( x -> x.equals( o ) );
                 }
             } else {
-                dr.getOutputEntry().add(o);
+                dr.getOutputEntry().add( o );
             }
         }
         return dr;
     }
-    
+
     public static class DecisionRule {
         private List<UnaryTest> inputEntry;
-        private List<Object> outputEntry;
+        private List<Object>    outputEntry;
 
         public List<UnaryTest> getInputEntry() {
             if ( inputEntry == null ) {
@@ -88,41 +86,77 @@ public class DecisionTableFunction
             return this.outputEntry;
         }
     }
-    
-    public static class ConcreteDTFunction extends BaseFEELFunction {
-        private List<DecisionRule> decisionRules;
-        private List<String> inputs;
 
-        public ConcreteDTFunction(String name, List<String> inputs, List<DecisionRule> decisionRules) {
-            super(name);
+    public static enum HitPolicy {
+        UNIQUE( "U" ),
+        FIRST( "F" ),
+        PRIORITY( "P" ),
+        ANY( "A" ),
+        COLLECT( "C" ),
+        COLLECT_SUM( "C+" ),
+        COLLECT_COUNT( "C#" ),
+        COLLECT_MIN( "C<" ),
+        COLLECT_MAX( "C>" ),
+        RULE_ORDER( "R" ),
+        OUTPUT_ORDER( "O" );
+
+        private final String value;
+
+        HitPolicy(final String v) {
+            value = v;
+        }
+
+        public String value() {
+            return value;
+        }
+
+        public static HitPolicy fromString(final String policy) {
+            for ( HitPolicy c : HitPolicy.values() ) {
+                if ( c.value.equals( policy ) ) {
+                    return c;
+                }
+            }
+            throw new IllegalArgumentException( "Unknown hit policy: " + policy );
+        }
+    }
+
+    public static class ConcreteDTFunction
+            extends BaseFEELFunction {
+        private       List<DecisionRule> decisionRules;
+        private       List<String>       inputs;
+        private final HitPolicy          hitPolicy;
+
+        public ConcreteDTFunction(String name, List<String> inputs, List<DecisionRule> decisionRules, HitPolicy hitPolicy) {
+            super( name );
             this.decisionRules = decisionRules;
             this.inputs = inputs;
+            this.hitPolicy = hitPolicy;
         }
-        
-        public Object apply(EvaluationContext ctx, Object[] params) {            
-            if (decisionRules.isEmpty()) {
+
+        public Object apply(EvaluationContext ctx, Object[] params) {
+            if ( decisionRules.isEmpty() ) {
                 return null;
             }
-            
-            if (params.length != decisionRules.get(0).getInputEntry().size()) {
-                LOG.error("The parameters supplied does not match input expression list");
+
+            if ( params.length != decisionRules.get( 0 ).getInputEntry().size() ) {
+                LOG.error( "The parameters supplied does not match input expression list" );
                 return null;
             }
 
             for ( DecisionRule decisionRule : decisionRules ) {
-                Boolean ruleMatches = IntStream.range(0, params.length)                         // TODO could short-circuit by using for/continue
-                        .mapToObj(i -> decisionRule.getInputEntry().get(i).apply(params[i]))
-                        .reduce((a, b) -> a && b)
-                        .orElse(false);
-                if (ruleMatches) {
-                    if (decisionRule.getOutputEntry().size() == 1) {
-                        return decisionRule.getOutputEntry().get(0);
+                Boolean ruleMatches = IntStream.range( 0, params.length )                         // TODO could short-circuit by using for/continue
+                        .mapToObj( i -> decisionRule.getInputEntry().get( i ).apply( params[i] ) )
+                        .reduce( (a, b) -> a && b )
+                        .orElse( false );
+                if ( ruleMatches ) {
+                    if ( decisionRule.getOutputEntry().size() == 1 ) {
+                        return decisionRule.getOutputEntry().get( 0 );
                     } else {
                         return decisionRule.getOutputEntry();
                     }
                 }
             }
-            
+
             return null;
         }
 
@@ -130,9 +164,13 @@ public class DecisionTableFunction
         protected boolean isCustomFunction() {
             return true;
         }
-        
+
         public List<List<String>> getParameterNames() {
             return Arrays.asList( inputs );
+        }
+
+        public HitPolicy getHitPolicy() {
+            return hitPolicy;
         }
     }
 }
