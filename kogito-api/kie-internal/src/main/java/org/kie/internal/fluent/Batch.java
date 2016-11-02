dc70fa574b1cc5f28218bdf71bb2e1d8ca55c79e
Mario Fusco <mario.fusco@gmail.com>
2016-11-02 18:38:59 +0100

    [DROOLS-1353] contextual conversation

--- kogito-api/kie-internal/src/main/java/org/kie/internal/fluent/Batch.java
+++ kogito-api/kie-internal/src/main/java/org/kie/internal/fluent/Batch.java
@@ -1,14 +1,15 @@
 package org.kie.internal.fluent;
 
+import org.kie.api.command.BatchExecutionCommand;
 import org.kie.api.command.Command;
 
 import java.util.List;
 
-public interface Batch {
+public interface Batch extends BatchExecutionCommand {
 
-    public long getDistance();
+    long getDistance();
 
-    public void addCommand(Command cmd);
+    Batch addCommand(Command cmd);
 
     List<Command> getCommands();
 }
