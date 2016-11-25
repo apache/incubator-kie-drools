0a760e3e95b93309d0e70058a59b0dfdc310ae72
tarilabs <matteo.mortari@gmail.com>
2016-11-25 16:18:00 +0100

    JavaBackedType cache. (#17)
    Now, to create a JavaBackedType, the method `JavaBackedType.of` must
    be used which will memoize if necessary the result.
    
    The method `JavaBackedType.of` returns a BuiltInType.UNKNOWN if it is
    not possible to represent Class<> as a JavaBackedType.
    
    The criteria if a Class<> can be represented as a JavaBackedType is 
    currently implemented in `JavaBackedType.createIfAnnotated`
    
    Added test to demonstrate currently not necessary to express properties
    which are standard JavaBean compliant.
    Only non-standard JavaBean compliant properties are currently needed
    to be captured by JavaBackedType.

--- kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/lang/impl/JavaBackedType.java
+++ kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/lang/impl/JavaBackedType.java
@@ -1,26 +1,50 @@
 package org.kie.dmn.feel.lang.impl;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.kie.dmn.feel.lang.CustomType;
 import org.kie.dmn.feel.lang.FEELProperty;
 import org.kie.dmn.feel.lang.Property;
+import org.kie.dmn.feel.lang.Type;
+import org.kie.dmn.feel.lang.types.BuiltInType;
 import org.kie.dmn.feel.parser.feel11.ParserHelper;
 
 public class JavaBackedType implements CustomType {
+    private static Map<Class<?>, JavaBackedType> cache = new HashMap<>();
+    
     private Class<?> wrapped;
     private Map<String, Property> properties = new HashMap<>();
 
-    public JavaBackedType(Class<?> class1) {
+    private JavaBackedType(Class<?> class1) {
         this.wrapped = class1;
         Stream.of( class1.getMethods() )
             .filter( m -> m.getAnnotation(FEELProperty.class) != null )
             .forEach( m -> properties.put( m.getAnnotation(FEELProperty.class).value() , new PropertyImpl( m.getAnnotation(FEELProperty.class).value() , ParserHelper.determineTypeFromClass(m.getReturnType()) ) ) );
             ;
+    }
+    
+    /**
+     * If clazz can be represented as a JavaBackedType, returns a JavaBackedType for representing clazz.
+     * If clazz can not be represented as a JavaBackedType, returns BuiltInType.UNKNOWN.
+     * This method performs memoization when necessary.
+     * @param clazz the class to be represented as JavaBackedType
+     * @return JavaBackedType representing clazz or BuiltInType.UNKNOWN
+     */
+    public static Type of(Class<?> clazz) {
+        return Optional.ofNullable( (Type) cache.computeIfAbsent( clazz, JavaBackedType::createIfAnnotated ) ).orElse( BuiltInType.UNKNOWN );
+    }
+    
+    /**
+     * For internal use, returns a new JavaBackedType if clazz can be represented as such, returns null otherwise.
+     */
+    private static JavaBackedType createIfAnnotated(Class<?> clazz) {
+        if (Stream.of(clazz.getMethods()).anyMatch(m->m.getAnnotation(FEELProperty.class)!=null)) {
+            return new JavaBackedType(clazz) ;
+        }
+        return null;
     }
 
     @Override
