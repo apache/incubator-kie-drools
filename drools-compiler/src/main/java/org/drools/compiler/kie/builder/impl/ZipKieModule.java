42481b47aa447085a362eb2fe19b962a36159889
Mario Fusco <mario.fusco@gmail.com>
2016-12-09 13:51:10 +0100

    [RHBPMS-4526] fix build of kjar with resource type suffix in the artifactId

--- drools-compiler/src/main/java/org/drools/compiler/kie/builder/impl/ZipKieModule.java
+++ drools-compiler/src/main/java/org/drools/compiler/kie/builder/impl/ZipKieModule.java
@@ -15,24 +15,34 @@
 
 package org.drools.compiler.kie.builder.impl;
 
-import java.io.File;
-import java.util.Collection;
-import java.util.Map;
-
-import org.drools.core.util.IoUtils;
 import org.kie.api.builder.ReleaseId;
 import org.kie.api.builder.model.KieModuleModel;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import static org.drools.core.util.IoUtils.readBytesFromInputStream;
+
 public class ZipKieModule extends AbstractKieModule implements InternalKieModule {
-    private final File             file;    
+    private final File file;
     private Map<String, byte[]> zipEntries;
+    private List<String> fileNames;
 
     public ZipKieModule(ReleaseId releaseId,
                         KieModuleModel kieProject,
                         File file) {
         super(releaseId, kieProject );
         this.file = file;
-        this.zipEntries = IoUtils.indexZipFile( file );
+        indexZipFile( file );
     }
 
     @Override
@@ -52,7 +62,7 @@ public class ZipKieModule extends AbstractKieModule implements InternalKieModule
 
     @Override
     public Collection<String> getFileNames() {
-        return this.zipEntries.keySet();
+        return fileNames;
     }
 
     @Override
@@ -66,5 +76,59 @@ public class ZipKieModule extends AbstractKieModule implements InternalKieModule
 
     public String toString() {
         return "ZipKieModule[releaseId=" + getReleaseId() + ",file=" + file + "]";
+    }
+
+    private void indexZipFile(java.io.File jarFile) {
+        Map<String, List<String>> folders = new HashMap<String, List<String>>();
+        zipEntries = new HashMap<String, byte[]>();
+        fileNames = new ArrayList<String>();
+
+        ZipFile zipFile = null;
+        try {
+            zipFile = new ZipFile( jarFile );
+            Enumeration< ? extends ZipEntry> entries = zipFile.entries();
+            while ( entries.hasMoreElements() ) {
+                ZipEntry entry = entries.nextElement();
+                if (entry.getName().endsWith(".dex")) {
+                    continue; //avoid out of memory error, it is useless anyway
+                }
+                String entryName = entry.getName();
+                if (entry.isDirectory()) {
+                    if (entryName.endsWith( "/" )) {
+                        entryName = entryName.substring( 0, entryName.length()-1 );
+                    }
+                } else {
+                    byte[] bytes = readBytesFromInputStream( zipFile.getInputStream( entry ) );
+                    zipEntries.put( entryName, bytes );
+                    fileNames.add( entryName );
+                }
+                int lastSlashPos = entryName.lastIndexOf( '/' );
+                String folderName = lastSlashPos < 0 ? "" : entryName.substring( 0, lastSlashPos );
+                List<String> folder = folders.get(folderName);
+                if (folder == null) {
+                    folder = new ArrayList<String>();
+                    folders.put( folderName, folder );
+                }
+                folder.add(lastSlashPos < 0 ? entryName : entryName.substring( lastSlashPos+1 ));
+            }
+        } catch ( IOException e ) {
+            throw new RuntimeException( "Unable to get all ZipFile entries: " + jarFile, e );
+        } finally {
+            if ( zipFile != null ) {
+                try {
+                    zipFile.close();
+                } catch ( IOException e ) {
+                    throw new RuntimeException( "Unable to get all ZipFile entries: " + jarFile, e );
+                }
+            }
+        }
+
+        for (Map.Entry<String, List<String>> folder : folders.entrySet()) {
+            StringBuilder sb = new StringBuilder();
+            for (String child : folder.getValue()) {
+                sb.append( child ).append( "\n" );
+            }
+            zipEntries.put( folder.getKey(), sb.toString().getBytes( StandardCharsets.UTF_8 ) );
+        }
     }
 }
