cffccb78d6c137d750edda7b4fa51e7d59e33669
Edson Tirelli <ed.tirelli@gmail.com>
2016-12-21 16:51:57 -0500

    Adding support for default values on decision tables

--- kogito-drools/kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/decisiontables/DTOutputClause.java
+++ kogito-drools/kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/decisiontables/DTOutputClause.java
@@ -7,19 +7,21 @@ import java.util.List;
 public class DTOutputClause {
     private final String name;
     private final String id;
+    private final String defaultValue;
     /**
 [...] output values is a list of values for the one output. Each value is a string.
 TODO ^ this might be conflict in specs if comparing FEEL scope Vs broader DMN scope.
      */
     private final List<String> outputValues;
 
-    public DTOutputClause(String name, List<String> outputValues ) {
-        this( name, null, outputValues );
+    public DTOutputClause(String name, List<String> outputValues) {
+        this( name, null, outputValues, null );
     }
     
-    public DTOutputClause(String name, String id, List<String> outputValues) {
+    public DTOutputClause(String name, String id, List<String> outputValues, String defaultValue) {
         this.name = name;
         this.id = id;
+        this.defaultValue = defaultValue;
 
         if (outputValues != null) {
             this.outputValues = Collections.unmodifiableList(new ArrayList<String>(outputValues));
@@ -39,5 +41,8 @@ TODO ^ this might be conflict in specs if comparing FEEL scope Vs broader DMN sc
     public List<String> getOutputValues() {
         return outputValues;
     }
-    
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
 }
