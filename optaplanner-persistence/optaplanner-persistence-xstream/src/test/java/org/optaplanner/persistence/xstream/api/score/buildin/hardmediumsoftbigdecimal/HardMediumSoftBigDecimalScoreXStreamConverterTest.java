0979d83e456a60a6c2c22dc1e9adf79db8c9b47d
Brad Hards <bradh@frogmouth.net>
2017-01-08 10:53:57 +1100

    PLANNER-635: Implement XStream persistence for HardMediumSoftBigDecimalScore.
    Includes tests.

---
+++ optaplanner-persistence/optaplanner-persistence-xstream/src/test/java/org/optaplanner/persistence/xstream/api/score/buildin/hardmediumsoftbigdecimal/HardMediumSoftBigDecimalScoreXStreamConverterTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.optaplanner.persistence.xstream.api.score.buildin.hardmediumsoftbigdecimal;
+
+import java.math.BigDecimal;
+
+import com.thoughtworks.xstream.annotations.XStreamConverter;
+import org.junit.Test;
+import org.optaplanner.core.api.score.buildin.hardmediumsoftbigdecimal.HardMediumSoftBigDecimalScore;
+import org.optaplanner.persistence.xstream.api.score.AbstractScoreXStreamConverterTest;
+
+public class HardMediumSoftBigDecimalScoreXStreamConverterTest extends AbstractScoreXStreamConverterTest {
+
+    @Test
+    public void serializeAndDeserialize() {
+        assertSerializeAndDeserialize(null, new TestHardMediumSoftBigDecimalScoreWrapper(null));
+        HardMediumSoftBigDecimalScore score = HardMediumSoftBigDecimalScore.valueOfInitialized(new BigDecimal("1200.0021"), new BigDecimal("-3.1415"), new BigDecimal("34.4300"));
+        assertSerializeAndDeserialize(score, new TestHardMediumSoftBigDecimalScoreWrapper(score));
+        score = HardMediumSoftBigDecimalScore.valueOf(-7, new BigDecimal("1200.0021"), new BigDecimal("-3.1415"), new BigDecimal("34.4300"));
+        assertSerializeAndDeserialize(score, new TestHardMediumSoftBigDecimalScoreWrapper(score));
+    }
+
+    public static class TestHardMediumSoftBigDecimalScoreWrapper extends TestScoreWrapper<HardMediumSoftBigDecimalScore> {
+
+        @XStreamConverter(HardMediumSoftBigDecimalScoreXStreamConverter.class)
+        private final HardMediumSoftBigDecimalScore score;
+
+        public TestHardMediumSoftBigDecimalScoreWrapper(HardMediumSoftBigDecimalScore score) {
+            this.score = score;
+        }
+
+        @Override
+        public HardMediumSoftBigDecimalScore getScore() {
+            return score;
+        }
+
+    }
+
+}
