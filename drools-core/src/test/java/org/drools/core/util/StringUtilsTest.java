85bac1718b37feb24aed1bef518c12810c77b82d
tarilabs <matteo.mortari@gmail.com>
2017-01-10 14:53:14 +0100

    RHBRMS-2733 DROOLS-1399 Comparison of DRL changes should not consider spaces duri… (#1053)
    RHBRMS-2733 backport of DROOLS-1399 to 6.5-community branch.

--- drools-core/src/test/java/org/drools/core/util/StringUtilsTest.java
+++ drools-core/src/test/java/org/drools/core/util/StringUtilsTest.java
@@ -60,4 +60,178 @@ public class StringUtilsTest {
         Assert.assertEquals("Expected and actual end of method args index for expr '" + strExpr + "' are not equal!",
                 expectedIndex, actualIndex);
     }
+    
+    @Test
+    public void test_codeAwareEqualsIgnoreSpaces() {
+        Assert.assertTrue( StringUtils.codeAwareEqualsIgnoreSpaces( null, null ) );
+        Assert.assertTrue( StringUtils.codeAwareEqualsIgnoreSpaces( "", "") );
+        Assert.assertFalse( StringUtils.codeAwareEqualsIgnoreSpaces( "", null ) );
+        Assert.assertFalse( StringUtils.codeAwareEqualsIgnoreSpaces( null, "" ) );
+        
+        Assert.assertTrue( StringUtils.codeAwareEqualsIgnoreSpaces( " ", "" ) );
+        Assert.assertTrue( StringUtils.codeAwareEqualsIgnoreSpaces( "", " " ) );
+        
+        Assert.assertTrue( StringUtils.codeAwareEqualsIgnoreSpaces( " ", "  " ) );
+        
+        Assert.assertTrue(
+                StringUtils.codeAwareEqualsIgnoreSpaces(
+                        "rule Rx when then end",
+                        " rule Rx  when then end " // <<- DIFF 3x 
+                )
+            );
+        Assert.assertTrue(
+                StringUtils.codeAwareEqualsIgnoreSpaces(
+                        "rule Rx when then end\n",
+                        " rule Rx  when then end\n " // <<- DIFF, both terminate with whitespace but different types
+                )
+            );
+        
+        Assert.assertFalse(
+                StringUtils.codeAwareEqualsIgnoreSpaces(
+                        "package org.drools.compiler\n",
+                        
+                        "package org.drools.compiler\n " +
+                        "rule Rx when\n" +
+                        "   $m : Message( message == \"Hello World\" )\n" +
+                        "then\n" +
+                        "end\n"
+                )
+            );
+        
+        Assert.assertTrue(
+                StringUtils.codeAwareEqualsIgnoreSpaces(
+                        "package org.drools.compiler\n" +
+                        "rule Rx when\n" +
+                        "   $m : Message( message == \"Hello World\" )\n" +
+                        "then\n" +
+                        "end\n",
+                        
+                        "package org.drools.compiler\n " +  // <<- DIFF
+                        "rule Rx when\n" +
+                        "   $m : Message( message == \"Hello World\" )\n" +
+                        "then\n" +
+                        "end\n"
+                )
+            );
+        Assert.assertTrue(
+                StringUtils.codeAwareEqualsIgnoreSpaces(
+                        "package org.drools.compiler\n" +
+                        "rule Rx when\n" +
+                        "   $m : Message( message == \"Hello World\" )\n" +
+                        "then\n" +
+                        "end\n",
+                        
+                        " package org.drools.compiler\n" +  // <<- DIFF (at beginning of this line)
+                        "rule Rx when\n" +
+                        "   $m : Message( message == \"Hello World\" )\n" +
+                        "then\n" +
+                        "end\n"
+                )
+            );
+        Assert.assertTrue(
+                StringUtils.codeAwareEqualsIgnoreSpaces(
+                        "package org.drools.compiler\n" +
+                        "rule Rx when\n" +
+                        "   $m : Message( message == \"Hello World\" )\n" +
+                        "then\n" +
+                        "end\n",
+                        
+                        " package org.drools.compiler\n " +  // <<- DIFF 2x
+                        "rule Rx when\n" +
+                        "   $m : Message( message == \"Hello World\" )\n" +
+                        "then\n" +
+                        "end\n " // <<- DIFF 
+                )
+            );
+        Assert.assertTrue(
+                StringUtils.codeAwareEqualsIgnoreSpaces(
+                        "package org.drools.compiler\n" +
+                        "rule Rx when\n" +
+                        "   $m : Message( message == \"Hello World\" )\n" +
+                        "then\n" +
+                        "end\n",
+                        
+                        "package org.drools.compiler\n" +  
+                        "rule Rx when\n" +
+                        "   $m : Message( message == \"Hello World\"  )\n" + // <<- DIFF
+                        "then\n" +
+                        "end\n"
+                )
+            );
+        Assert.assertFalse(
+                StringUtils.codeAwareEqualsIgnoreSpaces(
+                        "package org.drools.compiler\n" +
+                        "rule Rx when\n" +
+                        "   $m : Message( message == \"Hello World\" )\n" +
+                        "then\n" +
+                        "end\n",
+                        
+                        "package org.drools.compiler\n" +  
+                        "rule Rx when\n" +
+                        "   $m : Message( message == \"Hello    World\" )\n" + // <<- DIFF
+                        "then\n" +
+                        "end\n"
+                )
+            );
+        Assert.assertFalse(
+                StringUtils.codeAwareEqualsIgnoreSpaces(
+                        "package org.drools.compiler\n" +
+                        "rule Rx when\n" +
+                        "   $m : Message( message == \"Hello' World\" )\n" +
+                        "then\n" +
+                        "end\n",
+                        
+                        "package org.drools.compiler\n" +  
+                        "rule Rx when\n" +
+                        "   $m : Message( message == \"Hello'    World\" )\n" + // <<- DIFF
+                        "then\n" +
+                        "end\n"
+                )
+            );
+        Assert.assertFalse(
+                StringUtils.codeAwareEqualsIgnoreSpaces(
+                        "package org.drools.compiler\n" +
+                        "rule Rx when\n" +
+                        "   $m : Message( message == 'Hello World' )\n" +
+                        "then\n" +
+                        "end\n",
+                        
+                        "package org.drools.compiler\n" +  
+                        "rule Rx when\n" +
+                        "   $m : Message( message == 'Hello    World' )\n" + // <<- DIFF
+                        "then\n" +
+                        "end\n"
+                )
+            );
+        Assert.assertFalse(
+                StringUtils.codeAwareEqualsIgnoreSpaces(
+                        "package org.drools.compiler\n" +
+                        "rule Rx when\n" +
+                        "   $m : Message( message == 'Hello\" World' )\n" +
+                        "then\n" +
+                        "end\n",
+                        
+                        "package org.drools.compiler\n" +  
+                        "rule Rx when\n" +
+                        "   $m : Message( message == 'Hello\"    World' )\n" + // <<- DIFF
+                        "then\n" +
+                        "end\n"
+                )
+            );
+        Assert.assertFalse(
+                StringUtils.codeAwareEqualsIgnoreSpaces(
+                        "package org.drools.compiler\n" +
+                        "rule Rx when\n" +
+                        "   $m : Message( message == 'Hello\\' World' )\n" +
+                        "then\n" +
+                        "end\n",
+                        
+                        "package org.drools.compiler\n" +  
+                        "rule Rx when\n" +
+                        "   $m : Message( message == 'Hello\\'    World' )\n" + // <<- DIFF
+                        "then\n" +
+                        "end\n"
+                )
+            );
+    }
 }
