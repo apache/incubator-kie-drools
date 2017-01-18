0e12258c12b010be2693294fbc169d27dc40a5fa
Edson Tirelli <ed.tirelli@gmail.com>
2017-01-17 19:10:33 -0500

    Caching built in function on the RootExecutionFrame

--- kie-dmn-feel/src/main/java/org/kie/dmn/feel/lang/impl/ExecutionFrame.java
+++ kie-dmn-feel/src/main/java/org/kie/dmn/feel/lang/impl/ExecutionFrame.java
@@ -1,79 +1,18 @@
-/*
- * Copyright 2016 Red Hat, Inc. and/or its affiliates.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.kie.dmn.feel.lang.impl;
 
-import org.kie.dmn.feel.util.EvalHelper;
-
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
-public class ExecutionFrame {
-    private ExecutionFrame parentFrame;
+/**
+ * Execution Frame interface represents a
+ * stack frame on the FEEL runtime
+ */
+public interface ExecutionFrame {
 
-    private Map<String, Object> variables = new HashMap<>();
+    Object getValue(String symbol);
 
-    public ExecutionFrame(ExecutionFrame parentFrame) {
-        this.parentFrame = parentFrame;
-    }
+    boolean isDefined(String symbol);
 
-    public ExecutionFrame getParentFrame() {
-        return parentFrame;
-    }
+    void setValue(String symbol, Object value);
 
-    public void setParentFrame(ExecutionFrame parentFrame) {
-        this.parentFrame = parentFrame;
-    }
-
-    public Object getValue(String symbol) {
-        symbol = EvalHelper.normalizeVariableName( symbol );
-        if ( variables.containsKey( symbol ) ) {
-            return variables.get( symbol );
-        }
-        if ( parentFrame != null ) {
-            return parentFrame.getValue( symbol );
-        }
-        return null;
-    }
-
-    public Object getValue(String[] symbol) {
-        throw new UnsupportedOperationException( "needs implementation?" );
-    }
-
-    public boolean isDefined( String symbol ) {
-        symbol = EvalHelper.normalizeVariableName( symbol );
-        if ( variables.containsKey( symbol ) ) {
-            return true;
-        }
-        if ( parentFrame != null ) {
-            return parentFrame.isDefined( symbol );
-        }
-        return false;
-    }
-
-    public boolean isDefined( String[] name ) {
-        throw new UnsupportedOperationException( "needs implementation?" );
-    }
-
-    public void setValue(String symbol, Object value) {
-        this.variables.put( EvalHelper.normalizeVariableName( symbol ), value );
-    }
-
-    public Map<String, Object> getAllValues() {
-        return this.variables;
-    }
+    Map<String, Object> getAllValues();
 }
