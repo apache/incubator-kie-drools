b3cbac738d29898fa10de3009248419b121a5894
Maciej Swiderski <swiderski.maciej@gmail.com>
2017-01-23 12:24:39 +0100

    JBPM-5591 - Pluggable task assignment (#201)

---
+++ kogito-api/kie-internal/src/main/java/org/kie/internal/task/api/assignment/Assignment.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.kie.internal.task.api.assignment;
+
+import java.io.Serializable;
+
+public class Assignment implements Serializable, Comparable<Assignment>{
+
+    private static final long serialVersionUID = -6569072011166557126L;
+
+    private String user;
+
+    public Assignment(String user) {
+        super();
+        this.user = user;
+    }
+
+    public String getUser(){
+        return this.user;
+    }
+
+    @Override
+    public String toString() {
+        return "Assignment [user=" + user + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((user == null) ? 0 : user.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Assignment other = (Assignment) obj;
+        if (user == null) {
+            if (other.user != null)
+                return false;
+        } else if (!user.equals(other.user))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int compareTo(Assignment o) {
+        if (this.user == null) {
+            return -1;
+        }
+        return this.user.compareTo(o.getUser());
+    };
+}
