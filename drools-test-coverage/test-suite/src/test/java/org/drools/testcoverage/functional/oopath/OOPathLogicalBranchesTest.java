acd93d4161f27e19a486bb9b829f4336cc59d077
Marek Winkler <mwinkler@redhat.com>
2017-02-23 10:36:30 +0100

    [DROOLS-1379] Add more OOPath tests for logical operations

---
+++ drools-test-coverage/test-suite/src/test/java/org/drools/testcoverage/functional/oopath/OOPathLogicalBranchesTest.java
@@ -0,0 +1,409 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.testcoverage.functional.oopath;
+
+import org.assertj.core.api.Assertions;
+import org.drools.testcoverage.common.model.Address;
+import org.drools.testcoverage.common.model.Employee;
+import org.drools.testcoverage.common.util.KieBaseUtil;
+import org.junit.After;
+import org.junit.Test;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Tests usage of OOPath expressions resulting in multiple conditional branches (e.g. OR operator).
+ */
+public class OOPathLogicalBranchesTest {
+
+    private static final KieServices KIE_SERVICES = KieServices.Factory.get();
+
+    private KieSession kieSession;
+
+    @After
+    public void disposeKieSession() {
+        if (this.kieSession != null) {
+            this.kieSession.dispose();
+            this.kieSession = null;
+        }
+    }
+
+    @Test
+    public void testBasicOrCondition() {
+        final String drl =
+                "import org.drools.testcoverage.common.model.Employee;\n" +
+                "import org.drools.testcoverage.common.model.Address;\n" +
+                "global java.util.List list\n" +
+                "\n" +
+                "rule R when\n" +
+                "  Employee( $address: /address{ street == 'Elm' || city == 'Big City' } )\n" +
+                "then\n" +
+                "  list.add( $address.getCity() );\n" +
+                "end\n";
+
+        final KieBase kieBase = KieBaseUtil.getKieBaseFromDRLResources(true, KIE_SERVICES.getResources().newByteArrayResource(drl.getBytes()));
+        this.kieSession =  kieBase.newKieSession();
+
+        final List<String> list = new ArrayList<String>();
+        this.kieSession.setGlobal("list", list);
+
+        final Address addressOfBruno = new Address("Elm", 10, "Small City");
+        final Employee bruno = new Employee("Bruno");
+        bruno.setAddress(addressOfBruno);
+
+        final Address addressOfAlice = new Address("Elm", 10, "Big City");
+        final Employee alice = new Employee("Alice");
+        alice.setAddress(addressOfAlice);
+
+        this.kieSession.insert(bruno);
+        this.kieSession.insert(alice);
+        this.kieSession.fireAllRules();
+
+        Assertions.assertThat(list).containsExactlyInAnyOrder("Big City", "Small City");
+    }
+
+    @Test
+    public void testConstraintOr() {
+        final String drl =
+                "import org.drools.testcoverage.common.model.Employee;\n" +
+                "import org.drools.testcoverage.common.model.Address;\n" +
+                "global java.util.List list\n" +
+                "\n" +
+                "rule R when\n" +
+                "  $emp: Employee( $address: /address{ street == 'Elm' || city == 'Big City' } )\n" +
+                "        Employee( this != $emp, /address{ street == 'Elm' || city == 'Big City' } )\n" +
+                "then\n" +
+                "  list.add( $address.getCity() );\n" +
+                "end\n";
+
+        final KieBase kieBase = KieBaseUtil.getKieBaseFromDRLResources(true, KIE_SERVICES.getResources().newByteArrayResource(drl.getBytes()));
+        this.kieSession =  kieBase.newKieSession();
+
+        final List<String> list = new ArrayList<String>();
+        this.kieSession.setGlobal("list", list);
+
+        final Address addressOfBruno = new Address("Elm", 10, "Small City");
+        final Employee bruno = new Employee("Bruno");
+        bruno.setAddress(addressOfBruno);
+
+        final Address addressOfAlice = new Address("Elm", 10, "Big City");
+        final Employee alice = new Employee("Alice");
+        alice.setAddress(addressOfAlice);
+
+        this.kieSession.insert(bruno);
+        this.kieSession.insert(alice);
+        this.kieSession.fireAllRules();
+
+        Assertions.assertThat(list).containsExactlyInAnyOrder("Big City", "Small City");
+    }
+
+    @Test
+    public void testConstraintOrNoBinding() {
+        final String drl =
+                "import org.drools.testcoverage.common.model.Employee;\n" +
+                        "import org.drools.testcoverage.common.model.Address;\n" +
+                        "global java.util.List list\n" +
+                        "\n" +
+                        "rule R when\n" +
+                        "  $emp: Employee( /address{ street == 'Elm' || city == 'Big City' } )\n" +
+                        "        Employee( this != $emp, /address{ street == 'Elm' || city == 'Big City' } )\n" +
+                        "then\n" +
+                        "  list.add( $emp.getName() );\n" +
+                        "end\n";
+
+        final KieBase kieBase = KieBaseUtil.getKieBaseFromDRLResources(true, KIE_SERVICES.getResources().newByteArrayResource(drl.getBytes()));
+        this.kieSession =  kieBase.newKieSession();
+
+        final List<String> list = new ArrayList<String>();
+        this.kieSession.setGlobal("list", list);
+
+        final Address addressOfBruno = new Address("Elm", 10, "Small City");
+        final Employee bruno = new Employee("Bruno");
+        bruno.setAddress(addressOfBruno);
+
+        final Address addressOfAlice = new Address("Elm", 10, "Big City");
+        final Employee alice = new Employee("Alice");
+        alice.setAddress(addressOfAlice);
+
+        this.kieSession.insert(bruno);
+        this.kieSession.insert(alice);
+        this.kieSession.fireAllRules();
+
+        Assertions.assertThat(list).containsExactlyInAnyOrder("Bruno", "Alice");
+    }
+
+    @Test
+    public void testCEOr() {
+        final String drl =
+                "import org.drools.testcoverage.common.model.Employee;\n" +
+                "import org.drools.testcoverage.common.model.Address;\n" +
+                "global java.util.List list\n" +
+                "\n" +
+                "rule R when\n" +
+                "  Employee( $address: /address{ street == 'Elm', city == 'Big City' } )\n" +
+                " or " +
+                "  Employee( $address: /address{ street == 'Elm', city == 'Small City' } )\n" +
+                "then\n" +
+                "  list.add( $address.getCity() );\n" +
+                "end\n";
+
+        final KieBase kieBase = KieBaseUtil.getKieBaseFromDRLResources(true, KIE_SERVICES.getResources().newByteArrayResource(drl.getBytes()));
+        this.kieSession =  kieBase.newKieSession();
+
+        final List<String> list = new ArrayList<String>();
+        this.kieSession.setGlobal("list", list);
+
+        final Address addressOfBruno = new Address("Elm", 10, "Small City");
+        final Employee bruno = new Employee("Bruno");
+        bruno.setAddress(addressOfBruno);
+
+        final Address addressOfAlice = new Address("Elm", 10, "Big City");
+        final Employee alice = new Employee("Alice");
+        alice.setAddress(addressOfAlice);
+
+        this.kieSession.insert(bruno);
+        this.kieSession.insert(alice);
+        this.kieSession.fireAllRules();
+
+        Assertions.assertThat(list).containsExactlyInAnyOrder("Big City", "Small City");
+    }
+
+    @Test
+    public void testCEOrNoBinding() {
+        final String drl =
+                "import org.drools.testcoverage.common.model.Employee;\n" +
+                        "import org.drools.testcoverage.common.model.Address;\n" +
+                        "global java.util.List list\n" +
+                        "\n" +
+                        "rule R when\n" +
+                        " $employee: (\n" +
+                        "  Employee( /address{ street == 'Elm', city == 'Big City' } )\n" +
+                        " or " +
+                        "  Employee( /address{ street == 'Elm', city == 'Small City' } )\n" +
+                        " )\n" +
+                        "then\n" +
+                        "  list.add( $employee.getName() );\n" +
+                        "end\n";
+
+        final KieBase kieBase = KieBaseUtil.getKieBaseFromDRLResources(true, KIE_SERVICES.getResources().newByteArrayResource(drl.getBytes()));
+        this.kieSession =  kieBase.newKieSession();
+
+        final List<String> list = new ArrayList<String>();
+        this.kieSession.setGlobal("list", list);
+
+        final Address addressOfBruno = new Address("Elm", 10, "Small City");
+        final Employee bruno = new Employee("Bruno");
+        bruno.setAddress(addressOfBruno);
+
+        final Address addressOfAlice = new Address("Elm", 10, "Big City");
+        final Employee alice = new Employee("Alice");
+        alice.setAddress(addressOfAlice);
+
+        this.kieSession.insert(bruno);
+        this.kieSession.insert(alice);
+        this.kieSession.fireAllRules();
+
+        Assertions.assertThat(list).containsExactlyInAnyOrder("Bruno", "Alice");
+    }
+
+    @Test
+    public void testBasicAndCondition() {
+        final String drl =
+                "import org.drools.testcoverage.common.model.Employee;\n" +
+                        "import org.drools.testcoverage.common.model.Address;\n" +
+                        "global java.util.List list\n" +
+                        "\n" +
+                        "rule R when\n" +
+                        "  Employee( $address: /address{ street == 'Elm' && city == 'Big City' } )\n" +
+                        "then\n" +
+                        "  list.add( $address.getCity() );\n" +
+                        "end\n";
+
+        final KieBase kieBase = KieBaseUtil.getKieBaseFromDRLResources(true, KIE_SERVICES.getResources().newByteArrayResource(drl.getBytes()));
+        this.kieSession =  kieBase.newKieSession();
+
+        final List<String> list = new ArrayList<String>();
+        this.kieSession.setGlobal("list", list);
+
+        final Address addressOfBruno = new Address("Elm", 10, "Small City");
+        final Employee bruno = new Employee("Bruno");
+        bruno.setAddress(addressOfBruno);
+
+        final Address addressOfAlice = new Address("Elm", 10, "Big City");
+        final Employee alice = new Employee("Alice");
+        alice.setAddress(addressOfAlice);
+
+        this.kieSession.insert(bruno);
+        this.kieSession.insert(alice);
+        this.kieSession.fireAllRules();
+
+        Assertions.assertThat(list).containsExactly("Big City");
+    }
+
+    @Test
+    public void testConstraintAnd() {
+        final String drl =
+                "import org.drools.testcoverage.common.model.Employee;\n" +
+                        "import org.drools.testcoverage.common.model.Address;\n" +
+                        "global java.util.List list\n" +
+                        "\n" +
+                        "rule R when\n" +
+                        "  $emp: Employee( $address: /address{ street == 'Elm' && city == 'Big City' } )\n" +
+                        "        Employee( this != $emp, /address{ street == 'Elm' && city == 'Small City' } )\n" +
+                        "then\n" +
+                        "  list.add( $address.getCity() );\n" +
+                        "end\n";
+
+        final KieBase kieBase = KieBaseUtil.getKieBaseFromDRLResources(true, KIE_SERVICES.getResources().newByteArrayResource(drl.getBytes()));
+        this.kieSession =  kieBase.newKieSession();
+
+        final List<String> list = new ArrayList<String>();
+        this.kieSession.setGlobal("list", list);
+
+        final Address addressOfBruno = new Address("Elm", 10, "Small City");
+        final Employee bruno = new Employee("Bruno");
+        bruno.setAddress(addressOfBruno);
+
+        final Address addressOfAlice = new Address("Elm", 10, "Big City");
+        final Employee alice = new Employee("Alice");
+        alice.setAddress(addressOfAlice);
+
+        this.kieSession.insert(bruno);
+        this.kieSession.insert(alice);
+        this.kieSession.fireAllRules();
+
+        Assertions.assertThat(list).containsExactlyInAnyOrder("Big City");
+    }
+
+    @Test
+    public void testConstraintAndNoBinding() {
+        final String drl =
+                "import org.drools.testcoverage.common.model.Employee;\n" +
+                        "import org.drools.testcoverage.common.model.Address;\n" +
+                        "global java.util.List list\n" +
+                        "\n" +
+                        "rule R when\n" +
+                        "  $emp: Employee( /address{ street == 'Elm' && city == 'Big City' } )\n" +
+                        "        Employee( this != $emp, /address{ street == 'Elm' && city == 'Small City' } )\n" +
+                        "then\n" +
+                        "  list.add( $emp.getName() );\n" +
+                        "end\n";
+
+        final KieBase kieBase = KieBaseUtil.getKieBaseFromDRLResources(true, KIE_SERVICES.getResources().newByteArrayResource(drl.getBytes()));
+        this.kieSession =  kieBase.newKieSession();
+
+        final List<String> list = new ArrayList<String>();
+        this.kieSession.setGlobal("list", list);
+
+        final Address addressOfBruno = new Address("Elm", 10, "Small City");
+        final Employee bruno = new Employee("Bruno");
+        bruno.setAddress(addressOfBruno);
+
+        final Address addressOfAlice = new Address("Elm", 10, "Big City");
+        final Employee alice = new Employee("Alice");
+        alice.setAddress(addressOfAlice);
+
+        this.kieSession.insert(bruno);
+        this.kieSession.insert(alice);
+        this.kieSession.fireAllRules();
+
+        Assertions.assertThat(list).containsExactlyInAnyOrder("Alice");
+    }
+
+    @Test
+    public void testCEAnd() {
+        final String drl =
+                "import org.drools.testcoverage.common.model.Employee;\n" +
+                        "import org.drools.testcoverage.common.model.Address;\n" +
+                        "global java.util.List list\n" +
+                        "\n" +
+                        "rule R when\n" +
+                        "  Employee( $address: /address{ street == 'Elm', city == 'Big City' } )\n" +
+                        " and " +
+                        "  Employee( /address{ street == 'Elm', city == 'Small City' } )\n" +
+                        "then\n" +
+                        "  list.add( $address.getCity() );\n" +
+                        "end\n";
+
+        final KieBase kieBase = KieBaseUtil.getKieBaseFromDRLResources(true, KIE_SERVICES.getResources().newByteArrayResource(drl.getBytes()));
+        this.kieSession =  kieBase.newKieSession();
+
+        final List<String> list = new ArrayList<String>();
+        this.kieSession.setGlobal("list", list);
+
+        final Address addressOfBruno = new Address("Elm", 10, "Small City");
+        final Employee bruno = new Employee("Bruno");
+        bruno.setAddress(addressOfBruno);
+
+        final Address addressOfAlice = new Address("Elm", 10, "Big City");
+        final Employee alice = new Employee("Alice");
+        alice.setAddress(addressOfAlice);
+
+        this.kieSession.insert(bruno);
+        this.kieSession.insert(alice);
+        this.kieSession.fireAllRules();
+
+        Assertions.assertThat(list).containsExactlyInAnyOrder("Big City");
+    }
+
+    @Test
+    public void testCEAndWithNot() {
+        final String drl =
+                "import org.drools.testcoverage.common.model.Employee;\n" +
+                        "import org.drools.testcoverage.common.model.Address;\n" +
+                        "global java.util.List list\n" +
+                        "\n" +
+                        "rule R when\n" +
+                        "  $employee: Employee( /address{ street == 'Elm', city == 'Big City' } )\n" +
+                        " and " +
+                        "  not Employee( /address{ street == 'Elm', city == 'Small City' } )\n" +
+                        "then\n" +
+                        "  list.add( $employee.getName() );\n" +
+                        "end\n";
+
+        final KieBase kieBase = KieBaseUtil.getKieBaseFromDRLResources(true, KIE_SERVICES.getResources().newByteArrayResource(drl.getBytes()));
+        this.kieSession =  kieBase.newKieSession();
+
+        final List<String> list = new ArrayList<String>();
+        this.kieSession.setGlobal("list", list);
+
+        final Address addressOfBruno = new Address("Elm", 10, "Small City");
+        final Employee bruno = new Employee("Bruno");
+        bruno.setAddress(addressOfBruno);
+
+        final Address addressOfAlice = new Address("Elm", 10, "Big City");
+        final Employee alice = new Employee("Alice");
+        alice.setAddress(addressOfAlice);
+
+        final FactHandle brunoFactHandle = this.kieSession.insert(bruno);
+        this.kieSession.insert(alice);
+        this.kieSession.fireAllRules();
+
+        Assertions.assertThat(list).isEmpty();
+
+        this.kieSession.delete(brunoFactHandle);
+        this.kieSession.fireAllRules();
+
+        Assertions.assertThat(list).containsExactlyInAnyOrder("Alice");
+    }
+
+}
