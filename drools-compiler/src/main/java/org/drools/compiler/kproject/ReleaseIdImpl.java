8f976316a3abfcd6c231e0a2421e2dbd0ec660d5
Mario Fusco <mario.fusco@gmail.com>
2017-03-08 18:32:59 +0100

    [RHBPMS-4634] handle pom type dependencies in kie-ci (#1135)
    [RHBPMS-4634] handle pom type dependencies in kie-ci

--- drools-compiler/src/main/java/org/drools/compiler/kproject/ReleaseIdImpl.java
+++ drools-compiler/src/main/java/org/drools/compiler/kproject/ReleaseIdImpl.java
@@ -15,9 +15,6 @@
 
 package org.drools.compiler.kproject;
 
-import org.drools.core.util.StringUtils;
-import org.kie.api.builder.ReleaseId;
-
 import java.io.Externalizable;
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,11 +23,15 @@ import java.io.ObjectOutput;
 import java.io.StringReader;
 import java.util.Properties;
 
+import org.drools.core.util.StringUtils;
+import org.kie.api.builder.ReleaseId;
+
 public class ReleaseIdImpl implements ReleaseId, Externalizable {
 
     private String groupId;
     private String artifactId;
     private String version;
+    private String type;
 
     private String snapshotVersion;
 
@@ -44,12 +45,15 @@ public class ReleaseIdImpl implements ReleaseId, Externalizable {
         this.version = split[2];
     }
 
-    public ReleaseIdImpl(String groupId,
-            String artifactId,
-            String version) {
+    public ReleaseIdImpl(String groupId, String artifactId, String version) {
+        this(groupId, artifactId, version, "jar");
+    }
+
+    public ReleaseIdImpl(String groupId, String artifactId, String version, String type) {
         this.groupId = groupId;
         this.artifactId = artifactId;
         this.version = version;
+        this.type = type;
     }
 
     public String getGroupId() {
@@ -78,7 +82,9 @@ public class ReleaseIdImpl implements ReleaseId, Externalizable {
 
     @Override
     public String toString() {
-        return groupId + ":" + artifactId + ":" + version;
+        return type == null || type.equals( "jar" ) ?
+               groupId + ":" + artifactId + ":" + version :
+               groupId + ":" + artifactId + ":" + type + ":" + version;
     }
 
     public String toExternalForm() {
@@ -142,28 +148,28 @@ public class ReleaseIdImpl implements ReleaseId, Externalizable {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o)
+        if (this == o) {
             return true;
-        if (o == null || getClass() != o.getClass())
+        }
+        if (o == null || getClass() != o.getClass()) {
             return false;
+        }
 
         ReleaseIdImpl that = (ReleaseIdImpl) o;
+        return equalsIgnoreNull(artifactId, that.artifactId) && equalsIgnoreNull(groupId, that.groupId) &&
+               equalsIgnoreNull(version, that.version) && equalsIgnoreNull(type, that.type);
+    }
 
-        if (artifactId != null ? !artifactId.equals(that.artifactId) : that.artifactId != null)
-            return false;
-        if (groupId != null ? !groupId.equals(that.groupId) : that.groupId != null)
-            return false;
-        if (version != null ? !version.equals(that.version) : that.version != null)
-            return false;
-
-        return true;
+    private boolean equalsIgnoreNull(Object o1, Object o2) {
+        return o1 != null ? o1.equals(o2) : o2 == null;
     }
 
     @Override
     public int hashCode() {
         int result = groupId != null ? groupId.hashCode() : 0;
-        result = 31 * result + (artifactId != null ? artifactId.hashCode() : 0);
+        result = 29 * result + (artifactId != null ? artifactId.hashCode() : 0);
         result = 31 * result + (version != null ? version.hashCode() : 0);
+        result = 37 * result + (type != null ? type.hashCode() : 0);
         return result;
     }
 
@@ -180,6 +186,7 @@ public class ReleaseIdImpl implements ReleaseId, Externalizable {
         out.writeObject(groupId);
         out.writeObject(artifactId);
         out.writeObject(version);
+        out.writeObject(type);
     }
 
     @Override
@@ -187,5 +194,6 @@ public class ReleaseIdImpl implements ReleaseId, Externalizable {
         groupId = (String) in.readObject();
         artifactId = (String) in.readObject();
         version = (String) in.readObject();
+        type = (String) in.readObject();
     }
 }
