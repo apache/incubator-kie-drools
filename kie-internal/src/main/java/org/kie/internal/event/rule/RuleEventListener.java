0975914d4aea83b336e13a291bc54daea03e7dcc
Mario Fusco <mario.fusco@gmail.com>
2017-03-30 15:17:43 +0200

    [DROOLS-1497] implement RuleEventListener (#219)

--- kie-internal/src/main/java/org/kie/internal/event/rule/RuleEventListener.java
+++ kie-internal/src/main/java/org/kie/internal/event/rule/RuleEventListener.java
@@ -18,16 +18,15 @@ package org.kie.internal.event.rule;
 
 import java.util.EventListener;
 
-public interface RuleEventListener extends
-                                   EventListener {
-//
-//
-//    void onMatch(Match match);
-//    void onReMatch(Match match);
-//    void onUnMatch(Match match);
-//
-//    void onFiring(Match match);
-//    void onFired(Match match);
+import org.kie.api.runtime.rule.Match;
+
+public interface RuleEventListener extends EventListener {
+
+    default void onBeforeMatchFire(Match match) {}
+    default void onAfterMatchFire(Match match) {}
+
+    default void onDeleteMatch(Match match) {}
+    default void onUpdateMatch(Match match) {}
 
 //    to add later
 //    void onAllFiring(Rule rule);
