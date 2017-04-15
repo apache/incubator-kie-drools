913ed2e8a1425c35ba41729b9dd88bb4f622cfeb
tarilabs <matteo.mortari@gmail.com>
2017-04-15 15:06:53 +0200

    DROOLS-1528 : Complements support for FEEL Parser isDynamicResolution (#1208)
    * Demonstrate isDynamicResolution limitation
    * Resolve isDynamicResolution limitation for variable name tokens check
    * Solution 2 to resolve isDynamicResolution limitation for variable name tokens check

---
+++ kie-dmn/kie-dmn-feel/src/test/java/org/kie/dmn/feel/lang/examples/CompileEvaluateTest.java
@@ -0,0 +1,95 @@
+package org.kie.dmn.feel.lang.examples;
+
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+import org.kie.dmn.api.feel.runtime.events.FEELEvent;
+import org.kie.dmn.api.feel.runtime.events.FEELEvent.Severity;
+import org.kie.dmn.api.feel.runtime.events.FEELEventListener;
+import org.kie.dmn.feel.FEEL;
+import org.kie.dmn.feel.lang.CompiledExpression;
+import org.kie.dmn.feel.lang.CompilerContext;
+import org.kie.dmn.feel.lang.FEELProperty;
+import org.kie.dmn.feel.lang.FEELType;
+import org.kie.dmn.feel.lang.types.BuiltInType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CompileEvaluateTest {
+    private static final Logger LOG = LoggerFactory.getLogger(CompileEvaluateTest.class);
+    private static final FEEL feel = FEEL.newInstance();
+    static {
+        feel.addListener(evt -> { 
+            if (evt.getSeverity() == FEELEvent.Severity.ERROR) { 
+                LOG.error("{}", evt);
+                if ( evt.getSourceException().getCause() != null ) {
+                    Throwable c = evt.getSourceException().getCause();
+                    while (c != null) {
+                        LOG.error(" caused by: {} {}", c.getClass(), c.getMessage() != null ? c.getMessage() : "");
+                        c = c.getCause();
+                    }
+                    LOG.error(" [stacktraces omitted.]");
+                }
+            } else if (evt.getSeverity() == FEELEvent.Severity.WARN) { 
+                LOG.warn("{}", evt);
+            }
+        } );
+    }
+    
+    public static <K, V> Map.Entry<K, V> entry(K key, V value) {
+        return new AbstractMap.SimpleEntry<>(key, value);
+    }
+
+    public static <K, U> Collector<Map.Entry<K, U>, ?, Map<K, U>> toMap() {
+        return Collectors.toMap(x -> x.getKey(), x -> x.getValue());
+    }
+    
+    @SafeVarargs
+    public static <K, V> Map<K, V> prototype(Map.Entry<K, V>... attributes) {
+        return Stream.of(attributes).collect(toMap());
+    }
+    
+    @Test
+    public void test_isDynamicResolution() {
+        CompilerContext ctx = feel.newCompilerContext();
+        ctx.addInputVariableType( "Person List", BuiltInType.LIST);
+        
+        final List<FEELEvent> errors = new ArrayList<>();
+        FEELEventListener  errorsCountingListener = evt -> { if ( evt.getSeverity() == Severity.ERROR ) { errors.add(evt); } };
+        feel.addListener( errorsCountingListener );
+        
+        CompiledExpression compiledExpression = feel.compile( "Person List[My Variable 1 = \"A\"]", ctx );
+        
+        assertThat(errors.toString(), errors.size(), is(0) );
+
+        Map<String, Object> inputs = new HashMap<>();
+        List<Map<String, ?>> pList = new ArrayList<>();
+        inputs.put("Person List", pList);
+        pList.add(prototype(entry("Full Name",    "Edson Tirelli"),
+                            entry("My Variable 1","A"))
+                );
+        pList.add(prototype(entry("Full Name",    "Matteo Mortari"),
+                            entry("My Variable 1","B"))
+                );
+        
+        Object result = feel.evaluate(compiledExpression, inputs);
+        
+        assertThat( result, instanceOf( List.class ) );
+        assertThat( (List<?>) result, hasSize(1) );
+        assertThat( ((Map<?, ?>) ((List<?>) result).get(0)).get("Full Name"), is("Edson Tirelli") );
+        
+        feel.removeListener( errorsCountingListener );
+    }
+}
