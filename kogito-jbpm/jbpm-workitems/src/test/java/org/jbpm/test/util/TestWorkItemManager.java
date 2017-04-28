da76b0a6e7f000ec8cb85ed40f1b948a957c8ec9
Maciej Swiderski <mswiders@redhat.com>
2017-04-28 18:57:44 +0200

    JBPM-5923 - Business rule work item handler

---
+++ kogito-jbpm/jbpm-workitems/src/test/java/org/jbpm/test/util/TestWorkItemManager.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.jbpm.test.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+
+
+public class TestWorkItemManager implements WorkItemManager {
+
+    private Map<Long, Map<String, Object>> completedWorkItems = new HashMap<>();
+    
+    @Override
+    public void completeWorkItem(long id, Map<String, Object> results) {
+        completedWorkItems.put(id, results);
+    }
+
+    @Override
+    public void abortWorkItem(long id) {        
+    }
+
+    @Override
+    public void registerWorkItemHandler(String workItemName, WorkItemHandler handler) {
+
+    }
+
+    public Map<String, Object> getResults(Long id) {
+        return completedWorkItems.get(id);
+    }
+}
