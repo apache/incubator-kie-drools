fe552cc446520effe121a99a0f7538259e640f4a
Edson Tirelli <etirelli@Edsons-MacBook-Pro.local>
2017-05-05 11:51:15 -0400

    DROOLS-1545: fixing date functions support

--- kie-dmn/kie-dmn-feel/src/test/java/org/kie/dmn/feel/runtime/BaseFEELTest.java
+++ kie-dmn/kie-dmn-feel/src/test/java/org/kie/dmn/feel/runtime/BaseFEELTest.java
@@ -21,16 +21,20 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.kie.dmn.api.feel.runtime.events.FEELEvent;
 import org.kie.dmn.api.feel.runtime.events.FEELEvent.Severity;
+import org.kie.dmn.api.feel.runtime.events.FEELEventListener;
 import org.kie.dmn.feel.FEEL;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(Parameterized.class)
 public abstract class BaseFEELTest {
-
-    private final FEEL feel = FEEL.newInstance();
 
     @Parameterized.Parameter(0)
     public String expression;
@@ -40,17 +44,28 @@ public abstract class BaseFEELTest {
 
     @Test
     public void testExpression() {
-        feel.addListener( (evt) -> { if (evt.getSeverity() == Severity.ERROR) System.err.println(evt); } );
         assertResult( expression, result );
     }
 
     protected void assertResult( String expression, Object result ) {
+        FEEL feel = FEEL.newInstance();
+        ErrorListener el = new ErrorListener();
+        feel.addListener( el );
         if( result == null ) {
             assertThat( "Evaluating: '" + expression + "'", feel.evaluate( expression ), is( nullValue() ) );
         } else if( result instanceof Class<?> ) {
             assertThat( "Evaluating: '" + expression + "'", feel.evaluate( expression ), is( instanceOf( (Class<?>) result ) ) );
         } else {
             assertThat( "Evaluating: '"+expression+"'", feel.evaluate( expression ), is( result ) );
+        }
+        assertThat( el.events.toString(), el.events.isEmpty(), is( true ) );
+    }
+
+    public static class ErrorListener implements FEELEventListener {
+        public final List<FEELEvent> events = new ArrayList<>(  );
+        @Override
+        public void onEvent(FEELEvent event) {
+            events.add( event );
         }
     }
 }
