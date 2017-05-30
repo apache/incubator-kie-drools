cd8fb2602c28217306d1d71cb5feb6a626d81f14
Mario Fusco <mario.fusco@gmail.com>
2017-05-30 16:33:24 +0200

    [DROOLS-1379] Add tests for OOPath in queries (#1292)

---
+++ kogito-drools/drools-compiler/src/test/java/org/drools/compiler/oopath/model/SensorEvent.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.drools.compiler.oopath.model;
+
+public class SensorEvent {
+    private Sensor sensor;
+    private double value;
+
+    public SensorEvent(Sensor sensor, double value) {
+        this.sensor = sensor;
+        this.value = value;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
