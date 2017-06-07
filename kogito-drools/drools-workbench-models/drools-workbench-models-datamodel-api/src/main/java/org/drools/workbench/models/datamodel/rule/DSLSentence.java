329f30392fa0078605d9cb026ae0d8571c2c11fc
Guilherme Carreiro <karreiro@gmail.com>
2017-06-07 06:33:18 -0300

    GUVNOR-2864: [Guided Decision Table] Values are reset during column edit (#1304)

--- kogito-drools/drools-workbench-models/drools-workbench-models-datamodel-api/src/main/java/org/drools/workbench/models/datamodel/rule/DSLSentence.java
+++ kogito-drools/drools-workbench-models/drools-workbench-models-datamodel-api/src/main/java/org/drools/workbench/models/datamodel/rule/DSLSentence.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * This represents a DSL sentence.
@@ -44,28 +45,28 @@ public class DSLSentence
      */
     public String toString() {
         getDefinition();
-        StringBuilder result = new StringBuilder( definition );
-        int variableStart = definition.indexOf( "{" );
-        while ( variableStart >= 0 ) {
-            int variableEnd = getIndexForEndOfVariable( result.toString(),
-                                                        variableStart );
-            String variable = result.substring( variableStart + 1,
-                                                variableEnd );
-            int variableNameEnd = variable.indexOf( ":" );
-            if ( variableNameEnd == -1 ) {
+        StringBuilder result = new StringBuilder(definition);
+        int variableStart = definition.indexOf("{");
+        while (variableStart >= 0) {
+            int variableEnd = getIndexForEndOfVariable(result.toString(),
+                                                       variableStart);
+            String variable = result.substring(variableStart + 1,
+                                               variableEnd);
+            int variableNameEnd = variable.indexOf(":");
+            if (variableNameEnd == -1) {
                 variableNameEnd = variable.length();
             }
-            String variableName = variable.substring( 0,
-                                                      variableNameEnd );
-            result.replace( variableStart + 1,
-                            variableEnd,
-                            variableName );
+            String variableName = variable.substring(0,
+                                                     variableNameEnd);
+            result.replace(variableStart + 1,
+                           variableEnd,
+                           variableName);
             variableEnd = variableEnd - variable.length() + variableName.length();
-            variableStart = result.indexOf( "{",
-                                            variableEnd );
+            variableStart = result.indexOf("{",
+                                           variableEnd);
         }
-        return result.toString().replace( "\\n",
-                                          "\n" );
+        return result.toString().replace("\\n",
+                                         "\n");
     }
 
     /**
@@ -73,29 +74,29 @@ public class DSLSentence
      */
     public String interpolate() {
         getValues();
-        if ( definition == null ) {
+        if (definition == null) {
             return "";
         }
 
-        int variableStart = definition.indexOf( "{" );
-        if ( variableStart < 0 ) {
+        int variableStart = definition.indexOf("{");
+        if (variableStart < 0) {
             return definition;
         }
 
         int index = 0;
         int variableEnd = 0;
         StringBuilder sb = new StringBuilder();
-        while ( variableStart >= 0 ) {
-            sb.append( definition.substring( variableEnd,
-                                             variableStart ) );
-            variableEnd = getIndexForEndOfVariable( definition,
-                                                    variableStart ) + 1;
-            variableStart = definition.indexOf( "{",
-                                                variableEnd );
-            sb.append( values.get( index++ ).getValue() );
+        while (variableStart >= 0) {
+            sb.append(definition.substring(variableEnd,
+                                           variableStart));
+            variableEnd = getIndexForEndOfVariable(definition,
+                                                   variableStart) + 1;
+            variableStart = definition.indexOf("{",
+                                               variableEnd);
+            sb.append(values.get(index++).getValue());
         }
-        if ( variableEnd < definition.length() ) {
-            sb.append( definition.substring( variableEnd ) );
+        if (variableEnd < definition.length()) {
+            sb.append(definition.substring(variableEnd));
         }
         return sb.toString();
     }
@@ -105,75 +106,79 @@ public class DSLSentence
      * @return
      */
     public DSLSentence copy() {
-        final DSLSentence newOne = new DSLSentence();
-        newOne.drl = getDrl();
-        newOne.definition = getDefinition();
-        List<DSLVariableValue> variableValues = getValues();
-        if ( variableValues != null ) {
-            for ( DSLVariableValue value : getValues() ) {
-                newOne.getValues().add( value );
-            }
-        }
-        return newOne;
+        final DSLSentence copy = new DSLSentence();
+
+        copy.drl = getDrl();
+        copy.definition = getDefinition();
+        copy.values = mapCopy(getValues());
+
+        return copy;
+    }
+
+    private List<DSLVariableValue> mapCopy(final List<DSLVariableValue> values) {
+        return values
+                .stream()
+                .map(DSLVariableValue::copy)
+                .collect(Collectors.toList());
     }
 
     public String getDrl() {
         return this.drl;
     }
 
+    public void setDrl(String drl) {
+        this.drl = drl;
+    }
+
     public String getDefinition() {
-        if ( definition == null ) {
+        if (definition == null) {
             parseSentence();
         }
         return definition;
     }
 
-    public void setDrl( String drl ) {
-        this.drl = drl;
-    }
-
-    public void setDefinition( String definition ) {
+    public void setDefinition(String definition) {
         this.definition = definition;
     }
 
     public List<DSLVariableValue> getValues() {
-        if ( this.values == null ) {
+        if (this.values == null) {
             parseDefinition();
         }
         return values;
     }
 
     public Map<String, String> getEnumFieldValueMap() {
-        if ( this.values == null ) {
+        if (this.values == null) {
             parseDefinition();
         }
         Map<String, String> fieldValueMap = new HashMap<String, String>();
-        if ( getValues().isEmpty() ) {
+        if (getValues().isEmpty()) {
             return fieldValueMap;
         }
 
-        int variableStart = definition.indexOf( "{" );
+        int variableStart = definition.indexOf("{");
         int iVariable = 0;
-        while ( variableStart >= 0 ) {
-            int variableEnd = getIndexForEndOfVariable( definition,
-                                                        variableStart );
-            String variable = definition.substring( variableStart + 1,
-                                                    variableEnd );
+        while (variableStart >= 0) {
+            int variableEnd = getIndexForEndOfVariable(definition,
+                                                       variableStart);
+            String variable = definition.substring(variableStart + 1,
+                                                   variableEnd);
 
             //Extract field name for enumerations
-            if ( variable.contains( ENUM_TAG ) ) {
-                int lastIndex = variable.lastIndexOf( ":" );
-                String factAndField = variable.substring( lastIndex + 1,
-                                                          variable.length() );
-                int dotIndex = factAndField.indexOf( "." );
-                String field = factAndField.substring( dotIndex + 1,
-                                                       factAndField.length() );
-                fieldValueMap.put( field,
-                                   values.get( iVariable ).getValue() );
+            if (variable.contains(ENUM_TAG)) {
+                int lastIndex = variable.lastIndexOf(":");
+                String factAndField = variable.substring(lastIndex + 1,
+                                                         variable.length());
+                int dotIndex = factAndField.indexOf(".");
+                String field = factAndField.substring(dotIndex + 1,
+                                                      factAndField.length());
+                fieldValueMap.put(field,
+                                  values.get(iVariable).getValue());
             }
             iVariable++;
-            variableStart = definition.indexOf( "{",
-                                                variableEnd );
+            variableStart = definition.indexOf("{",
+                                               variableEnd);
         }
 
         return fieldValueMap;
@@ -184,59 +189,59 @@ public class DSLSentence
     //to set the value of a DSL parameter to text including the special escaping used 
     //to differentiate value, from data-type, from restriction
     private void parseSentence() {
-        if ( sentence == null ) {
+        if (sentence == null) {
             return;
         }
         definition = sentence;
         values = new ArrayList<DSLVariableValue>();
         sentence = null;
 
-        int variableStart = definition.indexOf( "{" );
-        while ( variableStart >= 0 ) {
-            int variableEnd = getIndexForEndOfVariable( definition,
-                                                        variableStart );
-            String variable = definition.substring( variableStart + 1,
-                                                    variableEnd );
-            values.add( parseValue( variable ) );
-            variableStart = definition.indexOf( "{",
-                                                variableEnd );
+        int variableStart = definition.indexOf("{");
+        while (variableStart >= 0) {
+            int variableEnd = getIndexForEndOfVariable(definition,
+                                                       variableStart);
+            String variable = definition.substring(variableStart + 1,
+                                                   variableEnd);
+            values.add(parseValue(variable));
+            variableStart = definition.indexOf("{",
+                                               variableEnd);
         }
     }
 
     //Build the Values from the Definition.
     private void parseDefinition() {
         values = new ArrayList<DSLVariableValue>();
-        if ( getDefinition() == null ) {
+        if (getDefinition() == null) {
             return;
         }
 
-        int variableStart = definition.indexOf( "{" );
-        while ( variableStart >= 0 ) {
-            int variableEnd = getIndexForEndOfVariable( definition,
-                                                        variableStart );
-            String variable = definition.substring( variableStart + 1,
-                                                    variableEnd );
-            values.add( parseValue( variable ) );
-            variableStart = definition.indexOf( "{",
-                                                variableEnd );
+        int variableStart = definition.indexOf("{");
+        while (variableStart >= 0) {
+            int variableEnd = getIndexForEndOfVariable(definition,
+                                                       variableStart);
+            String variable = definition.substring(variableStart + 1,
+                                                   variableEnd);
+            values.add(parseValue(variable));
+            variableStart = definition.indexOf("{",
+                                               variableEnd);
         }
     }
 
-    private int getIndexForEndOfVariable( String dsl,
-                                          int start ) {
+    private int getIndexForEndOfVariable(String dsl,
+                                         int start) {
         int end = -1;
         int bracketCount = 0;
-        if ( start > dsl.length() ) {
+        if (start > dsl.length()) {
             return end;
         }
-        for ( int i = start; i < dsl.length(); i++ ) {
-            char c = dsl.charAt( i );
-            if ( c == '{' ) {
+        for (int i = start; i < dsl.length(); i++) {
+            char c = dsl.charAt(i);
+            if (c == '{') {
                 bracketCount++;
             }
-            if ( c == '}' ) {
+            if (c == '}') {
                 bracketCount--;
-                if ( bracketCount == 0 ) {
+                if (bracketCount == 0) {
                     end = i;
                     return end;
                 }
@@ -245,41 +250,42 @@ public class DSLSentence
         return -1;
     }
 
-    private DSLVariableValue parseValue( String variable ) {
+    private DSLVariableValue parseValue(String variable) {
         //if the variable doesn't have a ':', then it is considered as a 
         //simple value
-        if ( !variable.contains( ":" ) ) {
-            return new DSLVariableValue( variable );
+        if (!variable.contains(":")) {
+            return new DSLVariableValue(variable);
         }
 
         //if it does containt a ':', then the part before it is considered
         //as the real value (used to create the final drl) and the part
         //after it is considered as an id
-        String value = variable.substring( 0,
-                                           variable.indexOf( ":" ) );
-        String id = variable.substring( variable.indexOf( ":" ) + 1 );
+        String value = variable.substring(0,
+                                          variable.indexOf(":"));
+        String id = variable.substring(variable.indexOf(":") + 1);
 
-        return new DSLComplexVariableValue( id, value );
+        return new DSLComplexVariableValue(id,
+                                           value);
     }
 
     @Override
-    public boolean equals( Object o ) {
-        if ( this == o ) {
+    public boolean equals(Object o) {
+        if (this == o) {
             return true;
         }
-        if ( o == null || getClass() != o.getClass() ) {
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
 
         DSLSentence that = (DSLSentence) o;
 
-        if ( definition != null ? !definition.equals( that.definition ) : that.definition != null ) {
+        if (definition != null ? !definition.equals(that.definition) : that.definition != null) {
             return false;
         }
-        if ( sentence != null ? !sentence.equals( that.sentence ) : that.sentence != null ) {
+        if (sentence != null ? !sentence.equals(that.sentence) : that.sentence != null) {
             return false;
         }
-        if ( values != null ? !values.equals( that.values ) : that.values != null ) {
+        if (values != null ? !values.equals(that.values) : that.values != null) {
             return false;
         }
 
@@ -290,9 +296,9 @@ public class DSLSentence
     public int hashCode() {
         int result = sentence != null ? sentence.hashCode() : 0;
         result = ~~result;
-        result = 31 * result + ( definition != null ? definition.hashCode() : 0 );
+        result = 31 * result + (definition != null ? definition.hashCode() : 0);
         result = ~~result;
-        result = 31 * result + ( values != null ? values.hashCode() : 0 );
+        result = 31 * result + (values != null ? values.hashCode() : 0);
         result = ~~result;
         return result;
     }
