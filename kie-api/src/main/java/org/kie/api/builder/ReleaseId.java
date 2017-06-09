0f69aeeb285b43f38ca598e70c4274ed52f75900
Mario Fusco <mario.fusco@gmail.com>
2017-06-09 19:47:37 +0200

    [DROOLS-1582] split kie-ci (#238)
    * [DROOLS-1582] split kie-ci
    
    * [DROOLS-1582] split kie-ci

--- kie-api/src/main/java/org/kie/api/builder/ReleaseId.java
+++ kie-api/src/main/java/org/kie/api/builder/ReleaseId.java
@@ -20,11 +20,6 @@ package org.kie.api.builder;
  * ReleaseId is a full identifier far a given version of an artifact.
  * Following the Maven conventions it is composed of 3 parts: a groupId, an artifactId and a version
  */
-public interface ReleaseId {
+public interface ReleaseId extends org.appformer.maven.support.ReleaseId {
 
-    String getGroupId();
-    String getArtifactId();
-    String getVersion();
-    String toExternalForm();
-    boolean isSnapshot();
 }
