5d055fd877a5837d058e4d1d6ef3b81a0936f5b0
tarilabs <matteo.mortari@gmail.com>
2017-06-26 16:42:07 +0200

    Avoid problem with Woodstox trying to insert trailing newline before EOF

--- kie-dmn/kie-dmn-backend/src/main/java/org/kie/dmn/backend/marshalling/CustomStaxWriter.java
+++ kie-dmn/kie-dmn-backend/src/main/java/org/kie/dmn/backend/marshalling/CustomStaxWriter.java
@@ -50,11 +50,22 @@ public class CustomStaxWriter extends StaxWriter {
         this.lastOp = Op.END_NODE;
         
         if ( this.tagDepth == 0 ) {
-            // closed last element before EOF
-            try {
-                out.writeCharacters( System.lineSeparator() );
-            } catch (XMLStreamException e) {
-                throw new StreamException(e);
+            if ( out.getClass().getPackage().getName().startsWith("com.ctc.wstx") ) {
+                // DROOLS-1630: problem with Woodstox trying to insert trailing newline before EOF
+                // java.lang.NullPointerException
+                //   at java.lang.System.arraycopy(Native Method)
+                //   at java.lang.String.getChars(String.java:826)
+                //   at com.ctc.wstx.sw.BufferingXmlWriter.writeRaw(BufferingXmlWriter.java:314)
+                //   at com.ctc.wstx.sw.BaseStreamWriter.writeCharacters(BaseStreamWriter.java:436)
+                //   at org.kie.dmn.backend.marshalling.CustomStaxWriter.endNode(CustomStaxWriter.java:55)
+                // do not insert trailing newline before EOF.
+            } else {
+                // closed last element before EOF
+                try {
+                    out.writeCharacters( System.lineSeparator() );
+                } catch (XMLStreamException e) {
+                    throw new StreamException(e);
+                }
             }
         }
     }
