fb200bc0a0847406a6f6baa148f1a14f16d19116
Mario Fusco <mario.fusco@gmail.com>
2017-07-03 19:23:43 +0200

    [DROOLS-1646] make Iterables, arrays and single objects bindable to a rule unit oopath

--- drools-core/src/main/java/org/drools/core/datasources/BindableDataProvider.java
+++ drools-core/src/main/java/org/drools/core/datasources/BindableDataProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,15 +16,11 @@
 
 package org.drools.core.datasources;
 
-import org.drools.core.common.InternalWorkingMemory;
-import org.drools.core.spi.Activation;
-import org.drools.core.util.bitmask.BitMask;
-import org.kie.api.runtime.rule.DataSource;
-import org.kie.api.runtime.rule.FactHandle;
+import org.kie.api.runtime.rule.EntryPoint;
+import org.kie.api.runtime.rule.RuleUnit;
 
-public interface InternalDataSource<T> extends DataSource<T>, BindableDataProvider {
+public interface BindableDataProvider {
+    void bind( RuleUnit unit, EntryPoint ep );
 
-    void update( FactHandle fh, Object obj, BitMask mask, Class<?> modifiedClass, Activation activation );
-
-    void setWorkingMemory( InternalWorkingMemory workingMemory );
+    default void unbind( RuleUnit unit ) { }
 }
