3af2abd6b27a114ed8efe660d738c69d703b3e89
tarilabs <matteo.mortari@gmail.com>
2017-07-07 11:40:21 +0200

    DROOLS-1622 Fix NPE when compiling DMN file on IBM JDK (#1357)
    Workaroung IBM JDK limitation
    which XML impl. classes is not compliant with the Java JDK javadoc specs

--- kie-dmn/kie-dmn-backend/src/main/java/org/kie/dmn/backend/marshalling/CustomStaxReader.java
+++ kie-dmn/kie-dmn-backend/src/main/java/org/kie/dmn/backend/marshalling/CustomStaxReader.java
@@ -19,6 +19,9 @@ public class CustomStaxReader extends StaxReader {
     private XMLStreamReader in;
 
     public CustomStaxReader(QNameMap qnameMap, XMLStreamReader in) {
+        // Please note that the super() internally calls moveDown(). If one day this need to be extended in this class,
+        // remind to defer inside the overrided moveDown() in this class with a simple return if this.in is null
+        // and make an explicit moveDown() call as part of THIS constructor.
         super(qnameMap, in);
         this.in = in;
     }
@@ -36,4 +39,34 @@ public class CustomStaxReader extends StaxReader {
     public Location getLocation() {
         return in.getLocation();
     }
+    
+    @Override
+    public String getAttribute(String name) {
+        // REFEDINES default XStream behavior, by expliciting the namespaceURI to use, instead of generic `null` 
+        // which is problematic in case of:
+        //  - multiple attribute with the same name and different namespace, not supported by XStream
+        //  - if using IBM JDK, because the XML infra is not respecting the JDK API javadoc contract.
+        // ref: DROOLS-1622
+
+        // Also note.
+        // To avoid semantic ambiguities as per example in W3C https://www.w3.org/TR/REC-xml-names/#uniqAttrs
+        // <!-- http://www.w3.org is bound to n1 and is the default -->
+        // <x xmlns:n1="http://www.w3.org" 
+        //    xmlns="http://www.w3.org" >
+        //   <good a="1"     b="2" />
+        //   <good a="1"     n1:a="2" />     <<-- this has semantic ambiguity in our case because if `a` was `id` which value to bind?
+        // </x>
+        // hence we do not support for attributes an "explicit" prefix for the namespace.
+        // In other words, a similar example:
+        // <dmn:inputData dmn:id="_3d560678-a126-4654-a686-bc6d941fe40b" dmn:name="MyInput">
+        // is not supported, and is expected as standard XML:
+        // <dmn:inputData id="_3d560678-a126-4654-a686-bc6d941fe40b" name="MyInput">
+                
+        return getAttribute( XMLConstants.DEFAULT_NS_PREFIX, this.encodeAttribute(name) );
+    }
+    
+    public String getAttribute(String namespaceURI, String name) {
+        return this.in.getAttributeValue( namespaceURI, this.encodeAttribute(name) );
+    }
+
 }
