0d76e7c41226127da66a32fafbacee2d79d7b40b
Tibor Zimanyi <tibor.zimanyi@gmail.com>
2017-08-08 16:17:32 +0200

    DO NOT MERGE Add reproducer for DROOLS-1696 (#1410)
    [DROOLS-1696] immediately propagate event's expiration when equal to current timestamp

--- drools-compiler/src/test/java/org/drools/compiler/integrationtests/CepFireUntilHaltTimerTest.java
+++ drools-compiler/src/test/java/org/drools/compiler/integrationtests/CepFireUntilHaltTimerTest.java
@@ -15,6 +15,7 @@
 
 package org.drools.compiler.integrationtests;
 
+import org.drools.compiler.util.TimerUtils;
 import org.kie.api.time.SessionPseudoClock;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -208,51 +209,4 @@ public class CepFireUntilHaltTimerTest {
             return String.format("MetadataEvent[name='%s' timestamp='%s', duration='%s']", name, metadataTimestamp, metadataDuration);
         }
     }
-
-    /**
-     * Utility class providing methods for coping with timing issues, such as
-     * {@link java.lang.Thread#sleep(long, int)} inaccuracy, on certain OS.
-     * <p/>
-     * Inspired by http://stackoverflow.com/questions/824110/accurate-sleep-for-java-on-windows
-     * and http://andy-malakov.blogspot.cz/2010/06/alternative-to-threadsleep.html.
-     */
-    public static class TimerUtils {
-
-        private static final long SLEEP_PRECISION = Long.valueOf(System.getProperty("TIMER_SLEEP_PRECISION", "50000"));
-
-        private static final long SPIN_YIELD_PRECISION = Long.valueOf(System.getProperty("TIMER_YIELD_PRECISION", "30000"));
-
-        private TimerUtils() {
-        }
-
-        /**
-         * Sleeps for specified amount of time in milliseconds.
-         *
-         * @param duration the amount of milliseconds to wait
-         * @throws InterruptedException if the current thread gets interrupted
-         */
-        public static void sleepMillis(final long duration) throws InterruptedException {
-            sleepNanos(TimeUnit.MILLISECONDS.toNanos(duration));
-        }
-
-        /**
-         * Sleeps for specified amount of time in nanoseconds.
-         *
-         * @param nanoDuration the amount of nanoseconds to wait
-         * @throws InterruptedException if the current thread gets interrupted
-         */
-        public static void sleepNanos(final long nanoDuration) throws InterruptedException {
-            final long end = System.nanoTime() + nanoDuration;
-            long timeLeft = nanoDuration;
-            do {
-                if (timeLeft > SLEEP_PRECISION) {
-                    Thread.sleep(1);
-                } else if (timeLeft > SPIN_YIELD_PRECISION) {
-                    Thread.yield();
-                }
-                timeLeft = end - System.nanoTime();
-            } while (timeLeft > 0);
-        }
-    }
-
 }
