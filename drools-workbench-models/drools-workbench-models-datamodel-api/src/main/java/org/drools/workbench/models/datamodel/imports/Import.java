ea9403839a120f7c3fbe548465043d78c9c02b1f
porcelli <alexandre.porcelli@gmail.com>
2017-08-21 08:55:13 -0400

    AF-540: update models and restored old type (Import) deprecating it (just for compat)

---
+++ drools-workbench-models/drools-workbench-models-datamodel-api/src/main/java/org/drools/workbench/models/datamodel/imports/Import.java
@@ -0,0 +1,13 @@
+package org.drools.workbench.models.datamodel.imports;
+
+@Deprecated
+public class Import extends org.appformer.project.datamodel.imports.Import {
+
+    public Import() {
+        super();
+    }
+
+    public Import(final String t) {
+        super(t);
+    }
+}
