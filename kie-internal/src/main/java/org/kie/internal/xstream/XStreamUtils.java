14438e9ad37ea5fa6badf8598f12c3cae2783c14
Mario Fusco <mario.fusco@gmail.com>
2017-08-22 13:04:12 +0300

    [RHBRMS-2784] centralize xstream initialization (#259)
    [RHBRMS-2784] centralize xstream initialization

---
+++ kie-internal/src/main/java/org/kie/internal/xstream/XStreamUtils.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.internal.xstream;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.HierarchicalStreamDriver;
+
+public class XStreamUtils {
+    private static final String[] VOID_TYPES = {"void.class", "Void.class"};
+
+    public static XStream createXStream() {
+        return internalCreateXStream( new XStream() );
+    }
+
+    public static XStream createXStream(HierarchicalStreamDriver hierarchicalStreamDriver ) {
+        return internalCreateXStream( new XStream(hierarchicalStreamDriver) );
+    }
+
+    private static XStream internalCreateXStream( XStream xstream ) {
+        xstream.denyTypes(VOID_TYPES);
+        return xstream;
+    }
+}
