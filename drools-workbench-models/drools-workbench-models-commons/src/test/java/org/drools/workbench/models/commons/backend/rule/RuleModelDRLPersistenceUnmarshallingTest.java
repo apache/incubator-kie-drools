7a893a1f048dd92aae240956ccb503f57ed4f21f
Toni Rikkola <toni@rikkola.net>
2017-09-04 11:03:45 +0300

    GUVNOR-3451 : GRE DSL: Multiline code blocks break the editor (#1439)

--- drools-workbench-models/drools-workbench-models-commons/src/test/java/org/drools/workbench/models/commons/backend/rule/RuleModelDRLPersistenceUnmarshallingTest.java
+++ drools-workbench-models/drools-workbench-models-commons/src/test/java/org/drools/workbench/models/commons/backend/rule/RuleModelDRLPersistenceUnmarshallingTest.java
@@ -3671,6 +3671,32 @@ public class RuleModelDRLPersistenceUnmarshallingTest extends BaseRuleModelTest 
                      dslComplexVariableValue.getId());
     }
 
+    /*
+     * https://issues.jboss.org/browse/GUVNOR-3451
+     */
+    @Test
+    public void testDSLWithMultilineBrackets() {
+        String drl = "rule \"rule1\"\n"
+                + "when\n"
+                + "> Applicant(\n"
+                + "> )\n"
+                + "then\n"
+                + "end\n";
+
+        final RuleModel m = RuleModelDRLPersistenceImpl.getInstance().unmarshalUsingDSL(drl,
+                                                                                        Collections.emptyList(),
+                                                                                        dmo,
+                                                                                        "");
+
+        assertNotNull(m);
+
+        assertTrue(m.lhs[0] instanceof FactPattern);
+        FactPattern pattern = (FactPattern) m.lhs[0];
+        assertEquals("Applicant",
+                     pattern.getFactType());
+
+    }
+
     @Test
     public void testFunctionCalls() {
         String drl =
