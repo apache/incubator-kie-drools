be292f90fc1708403e89aa7d53b5fccc66c3e435
Luca Molteni <volothamp@gmail.com>
2017-09-15 18:02:54 +0200

    Fix test compilation

--- kogito-drools/drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/CompilerTest.java
+++ kogito-drools/drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/CompilerTest.java
@@ -90,6 +90,10 @@ public class CompilerTest {
             return value;
         }
 
+        public void setValue(Object value) {
+            this.value = value;
+        }
+
         @Override
         public boolean equals(Object o) {
             if (this == o) return true;
