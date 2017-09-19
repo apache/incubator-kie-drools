e44ceb59185ea390b8367e0a4ec8d89266924c98
Luca Molteni <volothamp@gmail.com>
2017-09-19 17:32:23 +0200

    Fix Unit tests by removing resources model

--- drools-model/drools-model-compiler/src/test/resources/model/Person.java
+++ drools-model/drools-model-compiler/src/test/resources/model/Person.java
@@ -3,18 +3,10 @@ package org.model;
 public class Person {
     private final String name;
     private final int myAge;
-    private final Address address;
 
     public Person(String name, int age) {
         this.name = name;
         this.myAge = age;
-        this.address = null;
-    }
-
-    public Person(String name, int myAge, Address address) {
-        this.name = name;
-        this.myAge = myAge;
-        this.address = address;
     }
 
     public String getName() {
@@ -23,9 +15,5 @@ public class Person {
 
     public int getAge() {
         return myAge;
-    }
-
-    public Optional<Address> getAddress() {
-        return address;
     }
 }
