a700e8fedfc4ec06cd71aeba4d0fccdce1eaf54b
Jiří Locker <jiri.locker@gmail.com>
2017-09-22 11:30:50 +0200

    Use fixed thread pool
    There was no reason for using a cached thread pool. The number of tasks
    (partion solvers, partCount) is fixed. They all need to be executed in
    parallel to prevent partition starving, therefore the number of threads
    needed is exactly same as parrtCount. No additional threads are needed.

--- optaplanner-core/src/main/java/org/optaplanner/core/impl/partitionedsearch/DefaultPartitionedSearchPhase.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/impl/partitionedsearch/DefaultPartitionedSearchPhase.java
@@ -20,8 +20,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -174,11 +174,8 @@ public class DefaultPartitionedSearchPhase<Solution_> extends AbstractPhase<Solu
     }
 
     private ExecutorService createThreadPoolExecutor(int partCount) {
-        // Based on Executors.newCachedThreadPool(...)
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(0, Integer.MAX_VALUE,
-                60L, TimeUnit.SECONDS,
-                new SynchronousQueue<>(),
-                threadFactory);
+        ThreadPoolExecutor threadPoolExecutor
+                = (ThreadPoolExecutor) Executors.newFixedThreadPool(partCount, threadFactory);
 
         if (threadPoolExecutor.getMaximumPoolSize() < partCount) {
             throw new IllegalStateException(
