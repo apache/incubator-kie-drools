460f3799cf2b4e339d8818ff433f83a0da0c7df6
lucamolteni <volothamp@gmail.com>
2017-09-26 12:17:35 +0200

    First test for global in consequence

--- drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/CompilerTest.java
+++ drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/CompilerTest.java
@@ -106,7 +106,7 @@ public class CompilerTest {
 
 
     @Test
-    public void testGlobal() {
+    public void testGlobalInConsequence() {
         String str =
                 "package org.mypkg;" +
                 "import " + Person.class.getCanonicalName() + ";" +
@@ -122,6 +122,37 @@ public class CompilerTest {
 
         Result result = new Result();
         ksession.setGlobal("globalResult", result);
+
+        ksession.insert(new Person("Mark", 37));
+        ksession.insert(new Person("Edson", 35));
+        ksession.insert(new Person("Mario", 40));
+
+        ksession.fireAllRules();
+
+        assertEquals( "Mark is 37", result.getValue() );
+
+    }
+
+    @Test
+    public void testGlobalInConstraint() {
+        String str =
+                "package org.mypkg;" +
+                "import " + Person.class.getCanonicalName() + ";" +
+                "import " + Result.class.getCanonicalName() + ";" +
+                "global java.lang.String nameG;" +
+                "global org.drools.modelcompiler.Result resultG;" +
+                "rule X when\n" +
+                "  $p1 : Person(name == nameG)\n" +
+                "then\n" +
+                " resultG.setValue($p1.getName() + \" is \" + $p1.getAge());\n" +
+                "end";
+
+        KieSession ksession = getKieSession( str );
+
+        ksession.setGlobal("nameG", "Mark");
+
+        Result result = new Result();
+        ksession.setGlobal("resultG", result);
 
         ksession.insert(new Person("Mark", 37));
         ksession.insert(new Person("Edson", 35));
