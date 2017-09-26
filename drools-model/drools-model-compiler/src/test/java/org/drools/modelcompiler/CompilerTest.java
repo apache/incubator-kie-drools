cd98cde22e12fbb50c2b05ee653a949e45848299
Mario Fusco <mario.fusco@gmail.com>
2017-09-26 18:54:58 +0200

    fix globals resolution

--- drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/CompilerTest.java
+++ drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/CompilerTest.java
@@ -830,6 +830,38 @@ public class CompilerTest {
         assertEquals( "Mario", results.iterator().next().getValue() );
     }
 
+
+    @Test
+    public void testQueryInvokedWithGlobal() {
+        String str =
+                "import " + Result.class.getCanonicalName() + ";" +
+                        "import " + Person.class.getCanonicalName() + ";" +
+                        "global Integer ageG;" +
+                        "query olderThan( Person $p, int $age )\n" +
+                        "    $p := Person(age > $age)\n" +
+                        "end\n" +
+                        "rule R when\n" +
+                        "    $p : Person( name.startsWith(\"M\") )\n" +
+                        "    olderThan( $p, ageG; )\n" +
+                        "then\n" +
+                        "    insert(new Result($p.getName()));\n" +
+                        "end";
+
+        KieSession ksession = getKieSession( str );
+
+        ksession.setGlobal("ageG", 40);
+
+        ksession.insert( new Person( "Mark", 39 ) );
+        ksession.insert( new Person( "Mario", 41 ) );
+        ksession.insert( new Person( "Edson", 41 ) );
+
+        ksession.fireAllRules();
+
+        Collection<Result> results = getObjects( ksession, Result.class );
+        assertEquals( 1, results.size() );
+        assertEquals( "Mario", results.iterator().next().getValue() );
+    }
+
     @Test
     public void testAccumulate1() {
         String str =
