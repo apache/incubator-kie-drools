c5a3e8bd629097f48f0c1f50985a7c07c2fff84a
Luca Molteni <volothamp@gmail.com>
2017-10-05 16:34:23 +0200

    From in pattern generation (#1512)

--- drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/DeclarationSpec.java
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/DeclarationSpec.java
@@ -7,19 +7,23 @@ import java.util.Optional;
 import org.drools.compiler.lang.descr.BehaviorDescr;
 import org.drools.compiler.lang.descr.EntryPointDescr;
 import org.drools.compiler.lang.descr.PatternDescr;
+import org.drools.compiler.lang.descr.PatternSourceDescr;
 
 public class DeclarationSpec {
     final Class<?> declarationClass;
     final Optional<PatternDescr> optPattern;
+    final Optional<PatternSourceDescr> optSource;
 
-    public DeclarationSpec( Class<?> declarationClass, PatternDescr pattern ) {
+    public DeclarationSpec(Class<?> declarationClass, PatternDescr pattern) {
         this.declarationClass = declarationClass;
         this.optPattern = Optional.of(pattern);
+        this.optSource = Optional.ofNullable(pattern.getSource());
     }
 
     public DeclarationSpec( Class<?> declarationClass ) {
         this.declarationClass = declarationClass;
         this.optPattern = Optional.empty();
+        this.optSource = Optional.empty();
     }
 
     Optional<String> getEntryPoint() {
