abd6c93a13017ae81d8ea0ba9787cffa0ae0289e
Luca Molteni <volothamp@gmail.com>
2017-10-20 16:04:10 +0200

    Last oopath tests (#1549)
    * testReactiveOOPath
    
    * Test backreferences
    
    Use another generator instead of expr generator
    
    Use a different list for OOPath declarations
    
    Removed ignore

--- kogito-drools/drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/OOPathExprVisitor.java
+++ kogito-drools/drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/OOPathExprVisitor.java
@@ -1,5 +1,10 @@
 package org.drools.modelcompiler.builder.generator;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+
 import org.drools.javaparser.ast.drlx.OOPathChunk;
 import org.drools.javaparser.ast.drlx.OOPathExpr;
 import org.drools.javaparser.ast.expr.Expression;
@@ -7,10 +12,10 @@ import org.drools.javaparser.ast.expr.MethodCallExpr;
 import org.drools.javaparser.ast.expr.NameExpr;
 import org.drools.modelcompiler.builder.PackageModel;
 
-import java.util.HashSet;
-
 import static org.drools.core.util.ClassUtils.extractGenericType;
-import static org.drools.modelcompiler.builder.generator.DrlxParseUtil.*;
+import static org.drools.modelcompiler.builder.generator.DrlxParseUtil.generateLambdaWithoutParameters;
+import static org.drools.modelcompiler.builder.generator.DrlxParseUtil.prepend;
+import static org.drools.modelcompiler.builder.generator.DrlxParseUtil.toVar;
 import static org.drools.modelcompiler.builder.generator.ModelGenerator.buildExpressionWithIndexing;
 
 public class OOPathExprVisitor {
@@ -28,6 +33,8 @@ public class OOPathExprVisitor {
         Class<?> previousClass = originalClass;
         String previousBind = originalBind;
 
+        List<ModelGenerator.DrlxParseResult> ooPathConditionExpressions = new ArrayList<>();
+
         for (OOPathChunk chunk : ooPathExpr.getChunks()) {
 
             final String fieldName = chunk.getField().toString();
@@ -38,7 +45,7 @@ public class OOPathExprVisitor {
                 fieldType = extractGenericType(previousClass, ((MethodCallExpr) callExpr.getExpression()).getName().toString());
             }
 
-            final String bindingId = context.getExprId(fieldType, fieldName);
+            final String bindingId = context.getOOPathId(fieldType, fieldName);
             final Expression accessorLambda = generateLambdaWithoutParameters(new HashSet<>(),
                                                                     prepend(new NameExpr("_this"), callExpr.getExpression()));
 
@@ -46,16 +53,45 @@ public class OOPathExprVisitor {
             reactiveFrom.addArgument(new NameExpr(toVar(previousBind)));
             reactiveFrom.addArgument(accessorLambda);
 
-            context.addDeclaration(new DeclarationSpec(bindingId, fieldType, reactiveFrom));
+            DeclarationSpec newDeclaration = new DeclarationSpec(bindingId, fieldType, reactiveFrom);
+            context.addDeclaration(newDeclaration);
+            context.addOOPathDeclaration(newDeclaration);
 
             final Expression condition = chunk.getCondition();
             if (condition != null) {
                 final ModelGenerator.DrlxParseResult drlxParseResult = ModelGenerator.drlxParse(context, packageModel, fieldType, bindingId, condition.toString());
-                context.addExpression(buildExpressionWithIndexing(drlxParseResult));
+                ooPathConditionExpressions.add(drlxParseResult);
             }
 
             previousBind = bindingId;
             previousClass = fieldType;
         }
+
+        // If the OOPath has an inner binding, it will be in the context's declarations without its type (as it's inferred from the last OOPath chunk).
+        // We remove the original expression without type and use its name in the last expression
+        List<DeclarationSpec> declarations = context.getDeclarations();
+        final Optional<DeclarationSpec> missingClassDeclarationFound = declarations.stream().filter(d -> d.declarationClass == null).findFirst();
+
+        missingClassDeclarationFound.ifPresent(missingClassDeclaration -> {
+            final String innerBindingId = missingClassDeclaration.getBindingId();
+
+            final int lastIndex = declarations.size() - 1;
+            final DeclarationSpec last = declarations.get(lastIndex);
+            declarations.set(lastIndex, new DeclarationSpec(innerBindingId, last.declarationClass, last.optPattern, last.declarationSource));
+            declarations.remove(declarations.indexOf(missingClassDeclaration));
+
+            // In the meanwhile some condition could have used that binding, we need to rename that also
+            for(ModelGenerator.DrlxParseResult r : ooPathConditionExpressions) {
+                if(r.getExprId().equals(last.getBindingId())) {
+                    r.setExprId(innerBindingId);
+                } else if(r.getPatternBinding().equals(last.getBindingId())) {
+                    r.setPatternBinding(innerBindingId);
+                }
+            }
+        });
+
+        ooPathConditionExpressions.forEach(d -> context.addExpression(buildExpressionWithIndexing(d)));
+
+
     }
 }
