5fe56d9038b45e3e464d8c9f4901497a86d0b6df
Mario Fusco <mario.fusco@gmail.com>
2017-10-23 17:38:36 +0200

    add support for pattern's watch annotation

--- drools-model/drools-canonical-model/src/main/java/org/drools/model/patterns/PatternImpl.java
+++ drools-model/drools-canonical-model/src/main/java/org/drools/model/patterns/PatternImpl.java
@@ -1,5 +1,12 @@
 package org.drools.model.patterns;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import org.drools.model.Binding;
 import org.drools.model.Constraint;
 import org.drools.model.DataSourceDefinition;
@@ -9,19 +16,14 @@ import org.drools.model.Variable;
 import org.drools.model.constraints.AbstractConstraint;
 import org.drools.model.impl.DataSourceDefinitionImpl;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 public class PatternImpl<T> extends AbstractSinglePattern implements Pattern<T> {
 
     private final Variable<T> variable;
     private Variable[] inputVariables;
     private final DataSourceDefinition dataSourceDefinition;
     private Constraint constraint;
-    private List<Binding> bindings;
+    private Collection<Binding> bindings;
+    private Collection<String> watchedProps;
 
     public PatternImpl(Variable<T> variable) {
         this(variable, SingleConstraint.EMPTY, DataSourceDefinitionImpl.DEFAULT);
@@ -72,8 +74,24 @@ public class PatternImpl<T> extends AbstractSinglePattern implements Pattern<T> 
         bindings.add(binding);
     }
 
-    public List<Binding> getBindings() {
+    public Collection<Binding> getBindings() {
         return bindings != null ? bindings : Collections.emptyList();
+    }
+
+    public void addWatchedProps(String[] props) {
+        if (props == null || props.length == 0) {
+            return;
+        }
+        if (watchedProps == null) {
+            watchedProps = new LinkedHashSet<>();
+        }
+        for (String prop : props) {
+            watchedProps.add(prop);
+        }
+    }
+
+    public String[] getWatchedProps() {
+        return watchedProps != null ? watchedProps.toArray( new String[watchedProps.size()] ) : new String[0];
     }
 
     private Variable[] collectInputVariables() {
