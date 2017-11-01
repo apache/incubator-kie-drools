4f2153c0239c15b04da5dc8852518d7ac9513d13
rikkola <toni@rikkola.net>
2017-11-01 18:34:55 +0200

    DROOLS-1903 : GRE DSL: Multiline code blocks break the editor
    (cherry picked from commit b253722ba30d700cc067bda284c4033da5c52673)

--- drools-workbench-models/drools-workbench-models-commons/src/main/java/org/drools/workbench/models/commons/backend/rule/RuleModelDRLPersistenceImpl.java
+++ drools-workbench-models/drools-workbench-models-commons/src/main/java/org/drools/workbench/models/commons/backend/rule/RuleModelDRLPersistenceImpl.java
@@ -676,7 +676,7 @@ public class RuleModelDRLPersistenceImpl
                 buf.append(" from ");
                 renderExpression(pattern.getExpression());
                 if (!isSubPattern) {
-                    buf.append(")");
+                   buf.append(")");
                 }
                 buf.append("\n");
             }
@@ -702,6 +702,9 @@ public class RuleModelDRLPersistenceImpl
                                     gctx);
 
                 buf.append(" from collect ( ");
+
+                buf.append("\n");
+
                 if (pattern.getRightPattern() != null) {
                     if (pattern.getRightPattern() instanceof FactPattern) {
                         generateFactPattern((FactPattern) pattern.getRightPattern(),
@@ -723,6 +726,11 @@ public class RuleModelDRLPersistenceImpl
                     } else {
                         throw new IllegalArgumentException("Unsupported pattern " + pattern.getRightPattern() + " for FROM COLLECT");
                     }
+                }
+                if (isDSLEnhanced) {
+                    buf.append("\n"); // Just in case we add a row. Not sure what the methods above append.
+                    buf.append(indentation);
+                    buf.append(">");
                 }
                 buf.append(") \n");
             }
