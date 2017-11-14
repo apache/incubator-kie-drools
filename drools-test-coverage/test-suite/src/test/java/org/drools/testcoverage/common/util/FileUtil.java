2c861e6c75146b18c4a3fdb0ecd3ff15bdd6ec9d
Tibor Zimanyi <tibor.zimanyi@gmail.com>
2017-11-14 12:34:50 +0100

    Fix canonical model usage in tests (#1603)

---
+++ drools-test-coverage/test-suite/src/test/java/org/drools/testcoverage/common/util/FileUtil.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.testcoverage.common.util;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.kie.api.builder.ReleaseId;
+
+public final class FileUtil {
+
+    public static File bytesToTempKJARFile(ReleaseId releaseId, byte[] bytes, String extension ) {
+        File file = new File( System.getProperty( "java.io.tmpdir" ), releaseId.getArtifactId() + "-" + releaseId.getVersion() + extension );
+        try {
+            new PrintWriter(file).close();
+            FileOutputStream fos = new FileOutputStream(file, false );
+            fos.write( bytes );
+            fos.flush();
+            fos.close();
+        } catch ( IOException e ) {
+            throw new RuntimeException( e );
+        }
+        return file;
+    }
+
+    private FileUtil() {
+        // Creating instances of this class is not allowed.
+    }
+}
