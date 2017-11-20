a6310210c09356aedbfd108046edaff2c427a4c9
Edson Tirelli <ed.tirelli@gmail.com>
2017-11-20 17:19:45 -0500

    DROOLS-2119: fixing append to support singleton element instead of list (#1595)
    * DROOLS-2119: fixing append to support singleton element instead of list
    
    * DROOLS-2119: improving code to remove duplication

--- kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/AppendFunction.java
+++ kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/AppendFunction.java
@@ -18,6 +18,7 @@ package org.kie.dmn.feel.runtime.functions;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import org.kie.dmn.api.feel.runtime.events.FEELEvent.Severity;
 import org.kie.dmn.feel.runtime.events.InvalidParametersEvent;
@@ -30,15 +31,25 @@ public class AppendFunction
     }
 
     public FEELFnResult<List<Object>> invoke( @ParameterName( "list" ) List list, @ParameterName( "item" ) Object[] items ) {
-        if ( list == null ) {
+        return invoke((Object) list, items);
+    }
+
+    public FEELFnResult<List<Object>> invoke( @ParameterName( "list" ) Object appendTo, @ParameterName( "item" ) Object[] items ) {
+        if (appendTo == null) {
             return FEELFnResult.ofError(new InvalidParametersEvent(Severity.ERROR, "list", "cannot be null"));
         }
-        if ( items == null ) {
+        if (items == null) {
             return FEELFnResult.ofError(new InvalidParametersEvent(Severity.ERROR, "item", "cannot be null"));
         }
         // spec requires us to return a new list
-        final List<Object> result = new ArrayList<Object>( list );
+        final List<Object> result = new ArrayList<>();
+        if (appendTo instanceof Collection) {
+            result.addAll((Collection) appendTo);
+        } else {
+            result.add(appendTo);
+        }
         result.addAll(Arrays.asList(items));
-        return FEELFnResult.ofResult( result );
+        return FEELFnResult.ofResult(result);
     }
+
 }
