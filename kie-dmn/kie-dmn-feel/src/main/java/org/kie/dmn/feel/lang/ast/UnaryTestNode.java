1a65ad8866c95b9fb3b89ef9fa94b3bcc262a8b1
tarilabs <matteo.mortari@gmail.com>
2017-11-20 20:08:32 +0100

    DROOLS-2125 Lists as unary tests should be checked for containment, not equality

--- kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/lang/ast/UnaryTestNode.java
+++ kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/lang/ast/UnaryTestNode.java
@@ -16,6 +16,10 @@
 
 package org.kie.dmn.feel.lang.ast;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.function.BiPredicate;
+
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.kie.dmn.api.feel.runtime.events.FEELEvent.Severity;
 import org.kie.dmn.feel.lang.EvaluationContext;
@@ -24,9 +28,6 @@ import org.kie.dmn.feel.runtime.UnaryTest;
 import org.kie.dmn.feel.runtime.UnaryTestImpl;
 import org.kie.dmn.feel.util.EvalHelper;
 import org.kie.dmn.feel.util.Msg;
-
-import java.util.List;
-import java.util.function.BiPredicate;
 
 public class UnaryTestNode
         extends BaseNode {
@@ -120,29 +121,52 @@ public class UnaryTestNode
         };
     }
 
+    /**
+     * For a Unary Test an = (equal) semantic depends on the RIGHT value.
+     * If the RIGHT is NOT a list, then standard equals semantic applies
+     * If the RIGHT is a LIST, then the semantic is "right contains left"
+     */
+    private Boolean utEqualSemantic(Object left, Object right, EvaluationContext context) {
+        if (right instanceof Collection) {
+            return ((Collection) right).contains(left);
+        } else {
+            // evaluate single entity
+            return EvalHelper.isEqual(left, right, context);
+        }
+    }
+
     private UnaryTest createIsEqualUnaryTest( ) {
         return (context, left) -> {
             Object right = value.evaluate( context );
-            return EvalHelper.isEqual( left, right, context );
+            return utEqualSemantic(left, right, context);
         };
     }
 
     private UnaryTest createIsNotEqualUnaryTest( ) {
         return (context, left) -> {
             Object right = value.evaluate( context );
-            Boolean result = EvalHelper.isEqual( left, right, context );
+            Boolean result = utEqualSemantic(left, right, context);
             return result != null ? ! result : null;
         };
     }
 
     private UnaryTest createInUnaryTest() {
         return (c, o) -> {
+            if (o == null) {
+                return false;
+            }
             Object val = value.evaluate( c );
-            try {
-                return o != null && ((Range) val).includes( o );
-            } catch ( Exception e ) {
-                c.notifyEvt( astEvent(Severity.ERROR, Msg.createMessage(Msg.EXPRESSION_IS_RANGE_BUT_VALUE_IS_NOT_COMPARABLE, o.toString(), val.toString() ) ) );
-                throw e;
+            if (val instanceof Range) {
+                try {
+                    return ((Range) val).includes(o);
+                } catch (Exception e) {
+                    c.notifyEvt(astEvent(Severity.ERROR, Msg.createMessage(Msg.EXPRESSION_IS_RANGE_BUT_VALUE_IS_NOT_COMPARABLE, o.toString(), val.toString())));
+                    throw e;
+                }
+            } else if (val instanceof List) {
+                return ((List) val).contains(o);
+            } else {
+                return false; // make consistent with #createNotUnaryTest()
             }
         };
     }
@@ -176,7 +200,8 @@ public class UnaryTestNode
                         c.notifyEvt( astEvent(Severity.ERROR, Msg.createMessage(Msg.EXPRESSION_IS_RANGE_BUT_VALUE_IS_NOT_COMPARABLE, o.toString(), test.toString() ) ) );
                         throw e;
                     }
-
+                } else if (test instanceof List) {
+                    return !((List) val).contains(o);
                 } else {
                     // test is a constant, so return false if it is equal to "o"
                     if( test.equals( o ) ) {
