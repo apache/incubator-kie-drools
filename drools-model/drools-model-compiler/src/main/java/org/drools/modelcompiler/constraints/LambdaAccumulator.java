5dcb6ef6ce152630f2068f36f5ed055489d61496
Luca Molteni <volothamp@gmail.com>
2017-11-23 17:58:18 +0100

    Resources test.test drl (#1624)
    * Visit the pattern among the AccumulateDescr
    
    * Removed an hardcoded pattern binding in accumulate
    
    * New tests for accumulate
    
    * Correct test for accumulate3
    
    * Source and mapper as optional parameters
    
    Generic variable for source only sum
    
    Correct type signature for the DSL
    
    * Correct type signature for the DSL
    
    * toString in Function1,2,3
    
    * Lazy initialization of lambda fingerprint
    
    * Lazy initialization of accumulate declaration
    
    Find correct declaration in LambdaAccumulator.java
    
    * Always add the sum declaration even, with the optional source
    
    Support accumulate function generation of binded expressions
    
    Min and max
    
    Correct variables in FlowResourcesTest.java
    
    Correct variable declarations
    
    Find bind using toString
    
    Correct declaration class
    
    * Correct variable
    
    * Support multiple accumulate functions
    
    Refactor
    
    Source in superclass
    
    * Removed useless tests

--- drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/constraints/LambdaAccumulator.java
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/constraints/LambdaAccumulator.java
@@ -1,13 +1,17 @@
 package org.drools.modelcompiler.constraints;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Optional;
 
 import org.drools.core.WorkingMemory;
 import org.drools.core.common.InternalFactHandle;
+import org.drools.core.common.InternalWorkingMemory;
 import org.drools.core.rule.Declaration;
 import org.drools.core.spi.Accumulator;
 import org.drools.core.spi.Tuple;
 import org.drools.model.AccumulateFunction;
+import org.drools.model.Variable;
 
 public class LambdaAccumulator implements Accumulator {
 
@@ -34,7 +38,16 @@ public class LambdaAccumulator implements Accumulator {
 
     @Override
     public void accumulate(Object workingMemoryContext, Object context, Tuple leftTuple, InternalFactHandle handle, Declaration[] declarations, Declaration[] innerDeclarations, WorkingMemory workingMemory) throws Exception {
-        accumulateFunction.action((Serializable)context, handle.getObject());
+        final Optional<Variable<?>> optSource = accumulateFunction.getOptSource();
+        final Object object  = optSource.map((Variable<?> source) -> {
+            final Declaration decl = Arrays.stream(innerDeclarations)
+                    .filter(d -> d.getIdentifier().equals(source.getName()))
+                    .findFirst()
+                    .orElseThrow(() -> new RuntimeException("Cannot find declaration with name" + source.getName()));
+
+            return decl.getValue((InternalWorkingMemory) workingMemory, handle.getObject());
+        }).orElseGet(handle::getObject);
+        accumulateFunction.action((Serializable)context, object);
     }
 
     @Override
