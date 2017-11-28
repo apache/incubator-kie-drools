a8b617903261efb6f42a717744b88866ac6ae63d
Mario Fusco <mario.fusco@gmail.com>
2017-11-28 10:14:11 +0100

    add accumulate test with and

--- kogito-drools/drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/domain/Child.java
+++ kogito-drools/drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/domain/Child.java
@@ -16,16 +16,23 @@
 
 package org.drools.modelcompiler.domain;
 
-import org.drools.core.phreak.ReactiveList;
-
 import java.util.List;
 
+import org.drools.core.phreak.ReactiveList;
+
 public class Child extends Person {
+
+    private final String parent;
 
     private final List<Toy> toys = new ReactiveList<Toy>();
 
     public Child(String name, int age) {
+        this(name, age, null);
+    }
+
+    public Child(String name, int age, String parent) {
         super(name, age);
+        this.parent = parent;
     }
 
     public List<Toy> getToys() {
@@ -34,5 +41,9 @@ public class Child extends Person {
 
     public void addToy(Toy toy) {
         toys.add(toy);
+    }
+
+    public String getParent() {
+        return parent;
     }
 }
