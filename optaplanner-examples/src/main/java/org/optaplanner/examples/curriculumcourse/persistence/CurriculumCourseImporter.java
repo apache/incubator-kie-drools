f713904592c0536967d2f391040bffa19a8a5610
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2017-12-04 13:30:32 +0100

    remove unused field

--- optaplanner-examples/src/main/java/org/optaplanner/examples/curriculumcourse/persistence/CurriculumCourseImporter.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/curriculumcourse/persistence/CurriculumCourseImporter.java
@@ -39,7 +39,6 @@ import org.optaplanner.examples.curriculumcourse.domain.UnavailablePeriodPenalty
 public class CurriculumCourseImporter extends AbstractTxtSolutionImporter<CourseSchedule> {
 
     private static final String INPUT_FILE_SUFFIX = "ctt";
-    private static final String SPLIT_REGEX = "[\\ \\t]+";
 
     public static void main(String[] args) {
         new CurriculumCourseImporter().convertAll();
