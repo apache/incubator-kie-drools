48b692567d2e80d5def7a4a789a540817754c1d1
Tibor Zimanyi <tibor.zimanyi@gmail.com>
2017-12-12 20:50:36 +0100

    Fix test failing on IBM JDK (#1647)

--- drools-core/src/test/java/org/drools/core/util/KeyStoreHelperTest.java
+++ drools-core/src/test/java/org/drools/core/util/KeyStoreHelperTest.java
@@ -15,13 +15,22 @@
 
 package org.drools.core.util;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.security.InvalidKeyException;
+import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SignatureException;
 import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
 
 import org.junit.Test;
 
@@ -31,6 +40,16 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class KeyStoreHelperTest {
+
+    private static final String KEYSTORE_SERVER_RESOURCE_NAME = "droolsServer.keystore";
+    private static final String KEYSTORE_CLIENT_RESOURCE_NAME = "droolsClient.keystore";
+    private static final String KEYSTORE_JCEKS_RESOURCE_NAME = "droolsServer.jceks";
+    private static final String KEYSTORE_JCEKS_FILENAME = "target/test-classes/org/drools/core/util/droolsServer.jceks";
+    private static final String KEYSTORE_SERVER_PASSWORD = "serverpwd";
+    private static final String KEYSTORE_CLIENT_PASSWORD = "clientpwd";
+    private static final String KEY_ALIAS = "droolsKey";
+    private static final String KEY_PASSWORD = "keypwd";
+    private static final String KEY_PHRASE = "secretkey";
 
     @Test
     public void testSignDataWithPrivateKey() throws UnsupportedEncodingException,
@@ -42,69 +61,86 @@ public class KeyStoreHelperTest {
         // The server signs the data with the private key
         
         // Set properties to simulate the server
-        URL serverKeyStoreURL = getClass().getResource( "droolsServer.keystore" );
-        System.setProperty( KeyStoreConstants.PROP_SIGN, "true" );
+        final URL serverKeyStoreURL = getClass().getResource(KEYSTORE_SERVER_RESOURCE_NAME);
+        System.setProperty( KeyStoreConstants.PROP_SIGN, Boolean.TRUE.toString() );
         System.setProperty( KeyStoreConstants.PROP_PVT_KS_URL, serverKeyStoreURL.toExternalForm() );
-        System.setProperty( KeyStoreConstants.PROP_PVT_KS_PWD, "serverpwd" );
-        System.setProperty( KeyStoreConstants.PROP_PVT_ALIAS, "droolsKey" );
-        System.setProperty( KeyStoreConstants.PROP_PVT_PWD, "keypwd" );
-        KeyStoreHelper serverHelper = new KeyStoreHelper();
+        System.setProperty(KeyStoreConstants.PROP_PVT_KS_PWD, KEYSTORE_SERVER_PASSWORD);
+        System.setProperty( KeyStoreConstants.PROP_PVT_ALIAS, KEY_ALIAS );
+        System.setProperty( KeyStoreConstants.PROP_PVT_PWD, KEY_PASSWORD );
+        final KeyStoreHelper serverHelper = new KeyStoreHelper();
 
         // get some data to sign
-        byte[] data = "Hello World".getBytes( "UTF8" );
+        final byte[] data = "Hello World".getBytes("UTF8" );
 
         // sign the data
-        byte[] signature = serverHelper.signDataWithPrivateKey( data );
+        final byte[] signature = serverHelper.signDataWithPrivateKey(data );
 
         // now, initialise the client helper
         
         // Set properties to simulate the client
-        URL clientKeyStoreURL = getClass().getResource( "droolsClient.keystore" );
-        System.setProperty( KeyStoreConstants.PROP_SIGN, "true" );
+        final URL clientKeyStoreURL = getClass().getResource(KEYSTORE_CLIENT_RESOURCE_NAME );
+        System.setProperty( KeyStoreConstants.PROP_SIGN, Boolean.TRUE.toString() );
         System.setProperty( KeyStoreConstants.PROP_PUB_KS_URL, clientKeyStoreURL.toExternalForm() );
-        System.setProperty( KeyStoreConstants.PROP_PUB_KS_PWD, "clientpwd" );
+        System.setProperty( KeyStoreConstants.PROP_PUB_KS_PWD, KEYSTORE_CLIENT_PASSWORD );
         // client needs no password to access the certificate and public key
-        KeyStoreHelper clientHelper = new KeyStoreHelper( );
+        final KeyStoreHelper clientHelper = new KeyStoreHelper( );
 
         // check the signature against the data
-        assertTrue( clientHelper.checkDataWithPublicKey( "droolsKey",
+        assertTrue( clientHelper.checkDataWithPublicKey( KEY_ALIAS,
                                                          data,
                                                          signature ) );
 
         // check some fake data
-        assertFalse( clientHelper.checkDataWithPublicKey( "droolsKey",
+        assertFalse( clientHelper.checkDataWithPublicKey( KEY_ALIAS,
                                                           "fake".getBytes( "UTF8" ), 
                                                           signature ) );
     }
 
     @Test
-    public void testLoadPassword() {
-        // $ keytool -importpassword -keystore droolsServer.jceks -keypass keypwd -alias droolsKey -storepass serverpwd -storetype JCEKS
-        // password is passwd
-
-        // test no key store
-        KeyStoreHelper serverHelper = new KeyStoreHelper();
+    public void testLoadPasswordNoKeystore() {
+        final KeyStoreHelper serverHelper = new KeyStoreHelper();
         try {
             serverHelper.getPasswordKey(null, null);
             fail();
-        } catch (RuntimeException re) {
+        } catch (final RuntimeException re) {
             assertTrue(true);
         }
+    }
+
+    @Test
+    public void testLoadPassword() throws CertificateException, NoSuchAlgorithmException, IOException, KeyStoreException, InvalidKeyException, InvalidKeySpecException {
+        final SecretKey storedSecretKey = storeKeyIntoKeyStoreFile(KEY_PHRASE);
 
         // Set properties to simulate the server
-        URL serverKeyStoreURL = getClass().getResource("droolsServer.jceks");
+        final URL serverKeyStoreURL = getClass().getResource(KEYSTORE_JCEKS_RESOURCE_NAME);
         System.setProperty(KeyStoreConstants.PROP_PWD_KS_URL, serverKeyStoreURL.toExternalForm());
-        System.setProperty(KeyStoreConstants.PROP_PWD_KS_PWD, "serverpwd");
-        String PROP_PWD_ALIAS = "droolsKey";
-        char[] PROP_PWD_PWD = "keypwd".toCharArray();
+        System.setProperty(KeyStoreConstants.PROP_PWD_KS_PWD, KEYSTORE_SERVER_PASSWORD);
 
         try {
-            serverHelper = new KeyStoreHelper();
+            final KeyStoreHelper serverHelper = new KeyStoreHelper();
 
-            String passwordKey = serverHelper.getPasswordKey(PROP_PWD_ALIAS, PROP_PWD_PWD);
-            assertEquals("passwd", passwordKey);
-        } catch (RuntimeException re) {
-            fail();
+            final String passwordKey = serverHelper.getPasswordKey(KEY_ALIAS, KEY_PASSWORD.toCharArray());
+            assertEquals(new String(storedSecretKey.getEncoded()), passwordKey);
+        } catch (final RuntimeException re) {
+            re.printStackTrace();
+            fail(re.getMessage());
         }
+    }
+
+    private SecretKey storeKeyIntoKeyStoreFile(final String keyPhrase)
+            throws CertificateException, NoSuchAlgorithmException, IOException, KeyStoreException,
+            InvalidKeyException, InvalidKeySpecException {
+        final KeyStore keyStore = KeyStore.getInstance("JCEKS");
+        keyStore.load(null, KEYSTORE_SERVER_PASSWORD.toCharArray());
+
+        final SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DES");
+        final SecretKey mySecretKey = secretKeyFactory.generateSecret(new DESKeySpec(keyPhrase.getBytes()));
+        final KeyStore.SecretKeyEntry skEntry = new KeyStore.SecretKeyEntry(mySecretKey);
+        keyStore.setEntry(KEY_ALIAS, skEntry, new KeyStore.PasswordProtection(KEY_PASSWORD.toCharArray()));
+
+        try (FileOutputStream fos = new java.io.FileOutputStream(KEYSTORE_JCEKS_FILENAME, false)) {
+            keyStore.store(fos, KEYSTORE_SERVER_PASSWORD.toCharArray());
+        }
+        return mySecretKey;
     }
 }
