1f3882dea993b311d291968a3be321803a13e59b
tarilabs <matteo.mortari@gmail.com>
2017-12-13 10:54:33 +0100

    Executable model Pojo generation should inherit all DRL imports (#1649)
    ... and fix DSL for not() and exists() for allowing single and() as 
    the only parameter.

--- drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/GeneratedClassWithPackage.java
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/GeneratedClassWithPackage.java
@@ -1,14 +1,23 @@
 package org.drools.modelcompiler.builder;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.drools.javaparser.ast.body.ClassOrInterfaceDeclaration;
 
 public class GeneratedClassWithPackage {
     private final ClassOrInterfaceDeclaration generatedClass;
     private final String packageName;
+    private final Collection<String> imports = new ArrayList<>();
 
     public GeneratedClassWithPackage(ClassOrInterfaceDeclaration generatedClass, String packageName) {
         this.generatedClass = generatedClass;
         this.packageName = packageName;
+    }
+
+    public GeneratedClassWithPackage(ClassOrInterfaceDeclaration generatedClass, String packageName, Collection<String> imports) {
+        this(generatedClass, packageName);
+        this.imports.addAll(imports);
     }
 
     public ClassOrInterfaceDeclaration getGeneratedClass() {
@@ -17,6 +26,14 @@ public class GeneratedClassWithPackage {
 
     public String getPackageName() {
         return packageName;
+    }
+
+    public void addImport(String importClass) {
+        imports.add(importClass);
+    }
+
+    public Collection<String> getImports() {
+        return imports;
     }
 
     @Override
