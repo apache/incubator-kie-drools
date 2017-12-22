33beef21ed7c53965cadfd2ab5c6d66decd35f9e
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2017-12-22 09:37:03 +0100

    javadoc: some variable listeners are configured multiple shadow variables

--- optaplanner-core/src/main/java/org/optaplanner/core/impl/domain/variable/listener/VariableListener.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/impl/domain/variable/listener/VariableListener.java
@@ -22,9 +22,9 @@ import org.optaplanner.core.impl.score.director.ScoreDirector;
 /**
  * Changes shadow variables when a genuine planning variable changes.
  * <p>
- * Important: it must only change the shadow variable for which it's configured!
+ * Important: it must only change the shadow variable(s) for which it's configured!
  * It should never change a genuine variable or a problem fact.
- * It can change its shadow variable on multiple entity instances
+ * It can change its shadow variable(s) on multiple entity instances
  * (for example: an arrivalTime change affects all trailing entities too).
  * <p>
  * Each {@link ScoreDirector} has a different {@link VariableListener} instance, so it can be stateful.
