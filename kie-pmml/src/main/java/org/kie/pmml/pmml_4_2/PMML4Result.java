b64c6d39efc5e51a564b87a6c2595dc229e67e14
Lance Leverich <lance.leverich@gmail.com>
2018-01-10 09:26:21 -0800

    RHDM-243 Rule engine freezes when evaluating multiple PMML scorecards (master) * Added new unit test for scorecards - This also provides an example of how to use PMML with rule units * Updated test_scorecard.pmml so that it provides reason codes for occupations other than SKYDIVER and ASTRONAUT * Removed some System.out.println() calls

--- kie-pmml/src/main/java/org/kie/pmml/pmml_4_2/PMML4Result.java
+++ kie-pmml/src/main/java/org/kie/pmml/pmml_4_2/PMML4Result.java
@@ -26,219 +26,224 @@ import org.kie.pmml.pmml_4_2.model.mining.SegmentExecution;
 
 @PropertyReactive
 public class PMML4Result {
-	private String correlationId;
-	private String segmentationId;
-	private String segmentId;
-	private int segmentIndex;
-	private String resultCode;
-	private Map<String, Object> resultVariables;
-	
-	public PMML4Result() {
-		resultVariables = new HashMap<>();
-	}
-	
-	public PMML4Result(SegmentExecution segEx) {
-		this.correlationId = segEx.getCorrelationId();
-		this.segmentationId = segEx.getSegmentationId();
-		this.segmentId = segEx.getSegmentId();
-		this.segmentIndex = segEx.getSegmentIndex();
-		this.resultVariables = new HashMap<>();
-	}
+    private String correlationId;
+    private String segmentationId;
+    private String segmentId;
+    private int segmentIndex;
+    private String resultCode;
+    private Map<String, Object> resultVariables;
+    
+    public PMML4Result() {
+        resultVariables = new HashMap<>();
+    }
+    
+    public PMML4Result(String correlationId) {
+        this.correlationId = correlationId;
+        this.resultVariables = new HashMap<>();
+    }
+    
+    public PMML4Result(SegmentExecution segEx) {
+        this.correlationId = segEx.getCorrelationId();
+        this.segmentationId = segEx.getSegmentationId();
+        this.segmentId = segEx.getSegmentId();
+        this.segmentIndex = segEx.getSegmentIndex();
+        this.resultVariables = new HashMap<>();
+    }
 
-	public String getResultCode() {
-		return resultCode;
-	}
+    public String getResultCode() {
+        return resultCode;
+    }
 
-	public void setResultCode(String resultCode) {
-		this.resultCode = resultCode;
-	}
+    public void setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+    }
 
-	public String getSegmentationId() {
-		return segmentationId;
-	}
+    public String getSegmentationId() {
+        return segmentationId;
+    }
 
-	public void setSegmentationId(String segmentationId) {
-		this.segmentationId = segmentationId;
-	}
+    public void setSegmentationId(String segmentationId) {
+        this.segmentationId = segmentationId;
+    }
 
-	public String getSegmentId() {
-		return segmentId;
-	}
+    public String getSegmentId() {
+        return segmentId;
+    }
 
-	public void setSegmentId(String segmentId) {
-		this.segmentId = segmentId;
-	}
+    public void setSegmentId(String segmentId) {
+        this.segmentId = segmentId;
+    }
 
-	public int getSegmentIndex() {
-		return segmentIndex;
-	}
+    public int getSegmentIndex() {
+        return segmentIndex;
+    }
 
-	public void setSegmentIndex(int segmentIndex) {
-		this.segmentIndex = segmentIndex;
-	}
+    public void setSegmentIndex(int segmentIndex) {
+        this.segmentIndex = segmentIndex;
+    }
 
-	public Map<String, Object> getResultVariables() {
-		if (resultVariables == null) {
-			resultVariables = new HashMap<>();
-		}
-		return resultVariables;
-	}
-	
-	public void updateResultVariable(String objName, Object obj) {
-		if (this.resultVariables == null) {
-			this.resultVariables = new HashMap<>();
-		}
-		resultVariables.put(objName,obj);
-	}
+    public Map<String, Object> getResultVariables() {
+        if (resultVariables == null) {
+            resultVariables = new HashMap<>();
+        }
+        return resultVariables;
+    }
+    
+    public void updateResultVariable(String objName, Object obj) {
+        if (this.resultVariables == null) {
+            this.resultVariables = new HashMap<>();
+        }
+        resultVariables.put(objName,obj);
+    }
 
-	public void setResultVariables(Map<String, Object> resultVariables) {
-		this.resultVariables = resultVariables;
-	}
-	
-	public void addResultVariable(String objName, Object object) {
-		if (this.resultVariables == null) {
-			this.resultVariables = new HashMap<>();
-		}
-		this.resultVariables.put(objName, object);
-	}
-	
-	private String getGetterMethodName(Object wrapper, String fieldName, String prefix) {
-		String capFieldName = fieldName.substring(0,1).toUpperCase() + fieldName.substring(1);
-		return prefix + capFieldName;
-	}
-	
-	public <T> Optional<T> getResultValue(String objName, String objField, Class<T> clazz, Object...params) {
-		T value = null;
-		Object obj = getResultValue(objName, objField, params);
-		if (clazz.isAssignableFrom(obj.getClass())) {
-			value = (T)obj;
-		}
-		return value != null ? Optional.of(value) : Optional.empty();
-	}
-	
-	public Object getResultValue(String objName, String objField, Object...params) {
-		Object value = null;
-		Object holder = getResultVariables().get(objName);
-		if (holder != null) {
-			if (objField != null && !objField.trim().isEmpty()) {
-				String defFldRetriever = getGetterMethodName(holder,objField,"get");
-				try {
-					Class[] paramTypes = null;
-					Method m = null;
-					boolean retry = true;
-					if (params != null && params.length > 0) {
-						paramTypes = new Class[params.length];
-						for (int x = 0; x < params.length;x++) {
-							paramTypes[x] = params[x].getClass();
-						}
-						do {
-							try {
-								m = holder.getClass().getMethod(defFldRetriever, paramTypes);
-							} catch (NoSuchMethodException nsmx) {
-								if (m == null && defFldRetriever.startsWith("get")) {
-									defFldRetriever = getGetterMethodName(holder,objField,"is");
-								} else {
-									retry = false;
-								}
-							}
-						} while (m == null && retry);
-					} else {
-						do {
-							try {
-								m = holder.getClass().getMethod(defFldRetriever);
-							} catch (NoSuchMethodException nsmx) {
-								if (m == null && defFldRetriever.startsWith("get")) {
-									defFldRetriever = getGetterMethodName(holder,objField,"is");
-								} else {
-									retry = false;
-								}
-							}
-						} while (m == null && retry);
-					}
-					if (m != null) {
-						if (params != null && params.length > 0) {
-							value = m.invoke(holder, params);
-						} else {
-							value = m.invoke(holder);
-						}
-					}
-				} catch (SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e1) {
-					e1.printStackTrace();
-				}
-			} else {
-				value = holder;
-			}
-			
-		}
-		return value;
-	}
-	
+    public void setResultVariables(Map<String, Object> resultVariables) {
+        this.resultVariables = resultVariables;
+    }
+    
+    public void addResultVariable(String objName, Object object) {
+        if (this.resultVariables == null) {
+            this.resultVariables = new HashMap<>();
+        }
+        this.resultVariables.put(objName, object);
+    }
+    
+    private String getGetterMethodName(Object wrapper, String fieldName, String prefix) {
+        String capFieldName = fieldName.substring(0,1).toUpperCase() + fieldName.substring(1);
+        return prefix + capFieldName;
+    }
+    
+    public <T> Optional<T> getResultValue(String objName, String objField, Class<T> clazz, Object...params) {
+        T value = null;
+        Object obj = getResultValue(objName, objField, params);
+        if (clazz.isAssignableFrom(obj.getClass())) {
+            value = (T)obj;
+        }
+        return value != null ? Optional.of(value) : Optional.empty();
+    }
+    
+    public Object getResultValue(String objName, String objField, Object...params) {
+        Object value = null;
+        Object holder = getResultVariables().get(objName);
+        if (holder != null) {
+            if (objField != null && !objField.trim().isEmpty()) {
+                String defFldRetriever = getGetterMethodName(holder,objField,"get");
+                try {
+                    Class[] paramTypes = null;
+                    Method m = null;
+                    boolean retry = true;
+                    if (params != null && params.length > 0) {
+                        paramTypes = new Class[params.length];
+                        for (int x = 0; x < params.length;x++) {
+                            paramTypes[x] = params[x].getClass();
+                        }
+                        do {
+                            try {
+                                m = holder.getClass().getMethod(defFldRetriever, paramTypes);
+                            } catch (NoSuchMethodException nsmx) {
+                                if (m == null && defFldRetriever.startsWith("get")) {
+                                    defFldRetriever = getGetterMethodName(holder,objField,"is");
+                                } else {
+                                    retry = false;
+                                }
+                            }
+                        } while (m == null && retry);
+                    } else {
+                        do {
+                            try {
+                                m = holder.getClass().getMethod(defFldRetriever);
+                            } catch (NoSuchMethodException nsmx) {
+                                if (m == null && defFldRetriever.startsWith("get")) {
+                                    defFldRetriever = getGetterMethodName(holder,objField,"is");
+                                } else {
+                                    retry = false;
+                                }
+                            }
+                        } while (m == null && retry);
+                    }
+                    if (m != null) {
+                        if (params != null && params.length > 0) {
+                            value = m.invoke(holder, params);
+                        } else {
+                            value = m.invoke(holder);
+                        }
+                    }
+                } catch (SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e1) {
+                    e1.printStackTrace();
+                }
+            } else {
+                value = holder;
+            }
+            
+        }
+        return value;
+    }
+    
 
-	
-	public String getCorrelationId() {
-		return correlationId;
-	}
+    
+    public String getCorrelationId() {
+        return correlationId;
+    }
 
-	public void setCorrelationId(String correlationId) {
-		this.correlationId = correlationId;
-	}
+    public void setCorrelationId(String correlationId) {
+        this.correlationId = correlationId;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((correlationId == null) ? 0 : correlationId.hashCode());
-		result = prime * result + ((segmentId == null) ? 0 : segmentId.hashCode());
-		result = prime * result + segmentIndex;
-		result = prime * result + ((segmentationId == null) ? 0 : segmentationId.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((correlationId == null) ? 0 : correlationId.hashCode());
+        result = prime * result + ((segmentId == null) ? 0 : segmentId.hashCode());
+        result = prime * result + segmentIndex;
+        result = prime * result + ((segmentationId == null) ? 0 : segmentationId.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		PMML4Result other = (PMML4Result) obj;
-		if (correlationId == null) {
-			if (other.correlationId != null) {
-				return false;
-			}
-		} else if (!correlationId.equals(other.correlationId)) {
-			return false;
-		}
-		if (segmentId == null) {
-			if (other.segmentId != null) {
-				return false;
-			}
-		} else if (!segmentId.equals(other.segmentId)) {
-			return false;
-		}
-		if (segmentIndex != other.segmentIndex) {
-			return false;
-		}
-		if (segmentationId == null) {
-			if (other.segmentationId != null) {
-				return false;
-			}
-		} else if (!segmentationId.equals(other.segmentationId)) {
-			return false;
-		}
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        PMML4Result other = (PMML4Result) obj;
+        if (correlationId == null) {
+            if (other.correlationId != null) {
+                return false;
+            }
+        } else if (!correlationId.equals(other.correlationId)) {
+            return false;
+        }
+        if (segmentId == null) {
+            if (other.segmentId != null) {
+                return false;
+            }
+        } else if (!segmentId.equals(other.segmentId)) {
+            return false;
+        }
+        if (segmentIndex != other.segmentIndex) {
+            return false;
+        }
+        if (segmentationId == null) {
+            if (other.segmentationId != null) {
+                return false;
+            }
+        } else if (!segmentationId.equals(other.segmentationId)) {
+            return false;
+        }
+        return true;
+    }
 
-	@Override
-	public String toString() {
-		return "PMML4Result [correlationId=" + correlationId + ", segmentationId=" + segmentationId + ", segmentId="
-				+ segmentId + ", segmentIndex=" + segmentIndex + ", resultCode=" + resultCode + ", resultVariables="
-				+ resultVariables + "]";
-	}
+    @Override
+    public String toString() {
+        return "PMML4Result [correlationId=" + correlationId + ", segmentationId=" + segmentationId + ", segmentId="
+                + segmentId + ", segmentIndex=" + segmentIndex + ", resultCode=" + resultCode + ", resultVariables="
+                + resultVariables + "]";
+    }
 
-	
+    
 }
