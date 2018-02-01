cd7d8454c663d91317974ad13c35a2aecfae8785
Luca Molteni <volothamp@gmail.com>
2018-02-01 12:36:26 +0100

    DROOLS-2284 Aggregate pattern based on inner binding (#1738)
    * OOPath with inner binding without infering and substituting the type of
    the declarations
    
    Aggregate pattern based on inner binding
    
    * Added three regression tests
    
    * Regressions are not related to model generation

--- kogito-drools/drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/visitor/PatternVisitor.java
+++ kogito-drools/drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/visitor/PatternVisitor.java
@@ -1,8 +1,10 @@
 package org.drools.modelcompiler.builder.generator.visitor;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.drools.compiler.lang.descr.AccumulateDescr;
@@ -40,27 +42,39 @@ public class PatternVisitor {
         this.packageModel = packageModel;
     }
 
-    public void visit(PatternDescr pattern ) {
+    public void visit(PatternDescr pattern) {
         String className = pattern.getObjectType();
         List<? extends BaseDescr> constraintDescrs = pattern.getConstraint().getDescrs();
 
         // Expression is a query, get bindings from query parameter type
-        if ( QueryGenerator.bindQuery(context, packageModel, pattern, constraintDescrs ) ) {
+        if (QueryGenerator.bindQuery(context, packageModel, pattern, constraintDescrs)) {
             return;
         }
 
-        if ( QueryGenerator.createQueryCall(packageModel, context, pattern) ) {
+        if (QueryGenerator.createQueryCall(packageModel, context, pattern)) {
             return;
         }
 
         Class<?> patternType = getClassFromContext(context.getPkg().getTypeResolver(), className);
+
+        final boolean allConstraintsPositional = areAllConstraintsPositional(constraintDescrs);
 
         if (pattern.getIdentifier() == null) {
             if (pattern.getObjectType().equals( "Object" ) && pattern.getSource() instanceof AccumulateDescr) {
                 buildConstraintsForAccumulate(pattern, (( AccumulateDescr ) pattern.getSource()), constraintDescrs);
                 return;
             }
-            pattern.setIdentifier( generateName("pattern_" + patternType.getSimpleName()) );
+
+            final List<ConstraintParseResult> firstParsedConstraints = findAllConstraint(pattern, constraintDescrs, patternType);
+            final Optional<String> innerBindingName = findInnerBindingName(firstParsedConstraints);
+
+            final String generatedName = generateName("pattern_" + patternType.getSimpleName());
+
+            final String patternNameAggregated = innerBindingName
+                    .map(ib -> context.getAggregatePatternMap().putIfAbsent(ib, generatedName))
+                    .orElse(generatedName);
+
+            pattern.setIdentifier(patternNameAggregated);
         }
 
         final Optional<Expression> declarationSource = buildFromDeclaration(pattern);
@@ -69,7 +83,8 @@ public class PatternVisitor {
         if (constraintDescrs.isEmpty() && pattern.getSource() == null) {
             context.addExpression(createInputExpression(pattern));
         } else {
-            buildConstraints(pattern, constraintDescrs, patternType);
+            final List<ConstraintParseResult> constraintParseResults = findAllConstraint(pattern, constraintDescrs, patternType);
+            buildConstraints(pattern, patternType, constraintParseResults, allConstraintsPositional);
         }
     }
 
@@ -81,45 +96,68 @@ public class PatternVisitor {
         }
     }
 
-    private void buildConstraints(PatternDescr pattern, List<? extends BaseDescr> constraintDescrs, Class<?> patternType) {
-        final boolean allConstraintsPositional = areAllConstraintsPositional(constraintDescrs);
-        if(allConstraintsPositional) {
+    private Optional<String> findInnerBindingName(List<ConstraintParseResult> firstParsedConstraints) {
+        return firstParsedConstraints.stream()
+                .map(c -> c.drlxParseResult)
+                .filter(Objects::nonNull)
+                .map(DrlxParseResult::getExprBinding)
+                .filter(Objects::nonNull)
+                .findFirst();
+    }
+
+    private List<ConstraintParseResult> findAllConstraint(PatternDescr pattern, List<? extends BaseDescr> constraintDescrs, Class<?> patternType) {
+        final List<ConstraintParseResult> constraintParseResults = new ArrayList<>();
+        for (BaseDescr constraint : constraintDescrs) {
+            final ConstraintParseResult constraintParseResult = parseConstraint(pattern, patternType, constraint);
+            constraintParseResults.add(constraintParseResult);
+        }
+        return constraintParseResults;
+    }
+
+    private ConstraintParseResult parseConstraint(PatternDescr pattern, Class<?> patternType, BaseDescr constraint) {
+        final boolean isPositional = isPositional(constraint);
+        final String patternIdentifier = pattern.getIdentifier();
+
+        String expression = getConstraintExpression(patternType, constraint, isPositional);
+        if (expression.contains(":=")) {
+            expression = expression.replace(":=", "==");
+        }
+
+        final DrlxParseResult drlxParseResult = ModelGenerator.drlxParse(context, packageModel, patternType, patternIdentifier, expression, isPositional);
+        return new ConstraintParseResult(expression, patternIdentifier, drlxParseResult);
+    }
+
+    private void buildConstraints(PatternDescr pattern, Class<?> patternType, List<ConstraintParseResult> constraintParseResults, boolean allConstraintsPositional) {
+        if (allConstraintsPositional) {
             final MethodCallExpr andDSL = new MethodCallExpr(null, "and");
             context.addExpression(andDSL);
             context.pushExprPointer(andDSL::addArgument);
         }
 
-        for (BaseDescr constraint : constraintDescrs) {
-            buildConstraint(pattern, patternType, constraint);
+        for (ConstraintParseResult constraintParseResult : constraintParseResults) {
+            buildConstraint(pattern, patternType, constraintParseResult);
         }
-        if(allConstraintsPositional) {
+        if (allConstraintsPositional) {
             context.popExprPointer();
         }
     }
 
-    private void buildConstraint(PatternDescr pattern, Class<?> patternType, BaseDescr constraint) {
-        boolean isPositional = isPositional( constraint );
-        String expression = getConstraintExpression(patternType, constraint, isPositional);
-        String patternIdentifier = pattern.getIdentifier();
-        if(expression.contains(":=")) {
-            expression = expression.replace(":=", "==");
-        }
-
-        DrlxParseResult drlxParseResult = ModelGenerator.drlxParse(context, packageModel, patternType, patternIdentifier, expression, isPositional);
+    private void buildConstraint(PatternDescr pattern, Class<?> patternType, ConstraintParseResult constraintParseResult) {
+        DrlxParseResult drlxParseResult = constraintParseResult.getDrlxParseResult();
+        String patternIdentifier = constraintParseResult.getPatternIdentifier();
+        String expression = constraintParseResult.getExpression();
         if (drlxParseResult != null) {
-            if(drlxParseResult.getExpr() instanceof OOPathExpr) {
+            if (drlxParseResult.getExpr() instanceof OOPathExpr) {
 
                 // If the  outer pattern does not have a binding we generate it
-                if(patternIdentifier == null) {
+                if (patternIdentifier == null) {
                     patternIdentifier = context.getExprId(patternType, expression);
                     context.addDeclaration(new DeclarationSpec(patternIdentifier, patternType, Optional.of(pattern), Optional.empty()));
                 }
 
                 context.addExpression(createInputExpression(pattern));
 
-                new OOPathExprGenerator(context, packageModel).visit(patternType, patternIdentifier, (OOPathExpr)drlxParseResult.getExpr());
-
-
+                new OOPathExprGenerator(context, packageModel).visit(patternType, patternIdentifier, drlxParseResult);
             } else {
                 // need to augment the reactOn inside drlxParseResult with the look-ahead properties.
                 Collection<String> lookAheadFieldsOfIdentifier = context.getRuleDescr().lookAheadFieldsOfIdentifier(pattern);
@@ -130,7 +168,7 @@ public class PatternVisitor {
                     drlxParseResult.setPatternBindingUnification(true);
                 }
 
-                ModelGenerator.processExpression( context, drlxParseResult );
+                ModelGenerator.processExpression(context, drlxParseResult);
             }
         }
     }
@@ -155,29 +193,53 @@ public class PatternVisitor {
     }
 
     private static String getConstraintExpression(Class<?> patternType, BaseDescr constraint, boolean isPositional) {
-        if ( isPositional ) {
-            int position = (( ExprConstraintDescr ) constraint).getPosition();
+        if (isPositional) {
+            int position = ((ExprConstraintDescr) constraint).getPosition();
             return getFieldAtPosition(patternType, position) + " == " + constraint.toString();
         }
         return constraint.toString();
     }
 
-    private static boolean isPositional( BaseDescr constraint ) {
-        return constraint instanceof ExprConstraintDescr && (( ExprConstraintDescr ) constraint).getType() == ExprConstraintDescr.Type.POSITIONAL;
+    private static boolean isPositional(BaseDescr constraint) {
+        return constraint instanceof ExprConstraintDescr && ((ExprConstraintDescr) constraint).getType() == ExprConstraintDescr.Type.POSITIONAL;
     }
 
     private static String getFieldAtPosition(Class<?> patternType, int position) {
         for (Field field : patternType.getDeclaredFields()) {
-            Position p = field.getAnnotation(Position.class );
+            Position p = field.getAnnotation(Position.class);
             if (p != null && p.value() == position) {
                 return field.getName();
             }
         }
-        throw new RuntimeException( "Cannot find field in position " + position + " for " + patternType );
+        throw new RuntimeException("Cannot find field in position " + position + " for " + patternType);
     }
 
     private static String[] getPatternListenedProperties(PatternDescr pattern) {
-        AnnotationDescr watchAnn = pattern != null ? pattern.getAnnotation("watch" ) : null;
+        AnnotationDescr watchAnn = pattern != null ? pattern.getAnnotation("watch") : null;
         return watchAnn == null ? new String[0] : watchAnn.getValue().toString().split(",");
+    }
+
+    private class ConstraintParseResult {
+        private String expression;
+        private String patternIdentifier;
+        private DrlxParseResult drlxParseResult;
+
+        public ConstraintParseResult(String expression, String patternIdentifier, DrlxParseResult drlxParseResult) {
+            this.expression = expression;
+            this.patternIdentifier = patternIdentifier;
+            this.drlxParseResult = drlxParseResult;
+        }
+
+        public String getExpression() {
+            return expression;
+        }
+
+        public String getPatternIdentifier() {
+            return patternIdentifier;
+        }
+
+        public DrlxParseResult getDrlxParseResult() {
+            return drlxParseResult;
+        }
     }
 }
