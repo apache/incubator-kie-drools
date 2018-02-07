4d6fa859dda3bc24f7f9605c7f6071b37e6fc5ef
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2018-02-07 18:13:24 +0100

    PLANNER-1000 Error message of empty <secondaryEntitySelector> with multiple entity classes must be clearer

--- optaplanner-core/src/main/java/org/optaplanner/core/config/heuristic/selector/move/generic/SwapMoveSelectorConfig.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/config/heuristic/selector/move/generic/SwapMoveSelectorConfig.java
@@ -107,10 +107,10 @@ public class SwapMoveSelectorConfig extends MoveSelectorConfig<SwapMoveSelectorC
             EntityDescriptor onlySecondaryEntityDescriptor = secondaryEntitySelectorConfig.extractEntityDescriptor(configPolicy);
             if (onlyEntityDescriptor != onlySecondaryEntityDescriptor) {
                 throw new IllegalArgumentException("The entitySelector (" + entitySelectorConfig
-                        + ")'s entityDescriptor (" + onlyEntityDescriptor
+                        + ")'s entityClass (" + (onlyEntityDescriptor == null ? null : onlyEntityDescriptor.getEntityClass())
                         + ") and secondaryEntitySelectorConfig (" + secondaryEntitySelectorConfig
-                        + ")'s entityDescriptor (" + onlySecondaryEntityDescriptor
-                        + ") must have the same entity class.");
+                        + ")'s entityClass (" + (onlySecondaryEntityDescriptor == null ? null : onlySecondaryEntityDescriptor.getEntityClass())
+                        + ") must be the same entity class.");
             }
         }
         if (onlyEntityDescriptor != null) {
