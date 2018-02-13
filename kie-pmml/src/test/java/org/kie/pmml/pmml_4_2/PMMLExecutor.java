e954eb372deeb82c09164cbee6d3d6dbdbe09dc0
Lance Leverich <lance.leverich@gmail.com>
2018-02-13 08:55:24 -0800

    Updates to bring KIE-PMML changes into line with latest version

---
+++ kie-pmml/src/test/java/org/kie/pmml/pmml_4_2/PMMLExecutor.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2011 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.pmml.pmml_4_2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.drools.core.definitions.InternalKnowledgePackage;
+import org.drools.core.definitions.rule.impl.RuleImpl;
+import org.drools.core.impl.InternalKnowledgeBase;
+import org.drools.core.impl.InternalRuleUnitExecutor;
+import org.drools.core.ruleunit.RuleUnitDescr;
+import org.drools.core.ruleunit.RuleUnitRegistry;
+import org.kie.api.KieBase;
+import org.kie.api.logger.KieRuntimeLogger;
+import org.kie.api.runtime.rule.DataSource;
+import org.kie.api.runtime.rule.RuleUnit;
+import org.kie.api.runtime.rule.RuleUnitExecutor;
+import org.kie.pmml.pmml_4_2.model.PMML4UnitImpl;
+import org.kie.api.pmml.PMMLRequestData;
+import org.kie.api.pmml.PMML4Data;
+import org.kie.api.pmml.PMML4Result;
+
+public class PMMLExecutor {
+    private KieBase kieBase;
+    private String loggingFileName;
+    private boolean runWithLogging;
+
+    public PMMLExecutor(KieBase kieBase) {
+        this.kieBase = kieBase;
+    }
+
+    public PMMLExecutor(KieBase kieBase, String loggingFileName) {
+        this.kieBase = kieBase;
+        this.loggingFileName = loggingFileName;
+    }
+
+    public KieBase getKieBase() {
+        return kieBase;
+    }
+
+    public String getLoggingFileName() {
+        return loggingFileName;
+    }
+
+    public void setLoggingFileName(String loggingFileName) {
+        this.loggingFileName = loggingFileName;
+    }
+
+    public boolean isRunWithLogging() {
+        return runWithLogging;
+    }
+
+    public void setRunWithLogging( boolean runWithLogging ) {
+        this.runWithLogging = runWithLogging;
+    }
+
+    public PMML4Result run(PMMLRequestData requestData) {
+        return run(requestData, Optional.empty());
+    }
+
+    public PMML4Result run(PMMLRequestData requestData, PMML4Data pmml4Data) {
+        return run(requestData, Optional.of(pmml4Data));
+    }
+
+    private PMML4Result run(PMMLRequestData requestData,
+            Optional<PMML4Data> data) {
+        KieRuntimeLogger logger = null;
+        RuleUnitExecutor ruleUnitExecutor = RuleUnitExecutor.create().bind(kieBase);
+        if (runWithLogging) {
+            if (loggingFileName != null) {
+                logger = ((InternalRuleUnitExecutor)ruleUnitExecutor).addFileLogger(loggingFileName);
+            } else {
+                logger = ((InternalRuleUnitExecutor)ruleUnitExecutor).addConsoleLogger();
+            }
+        }
+        DataSource<PMMLRequestData> requestDataSource = ruleUnitExecutor.newDataSource("request");;
+        DataSource<PMML4Result> resultDataSource = ruleUnitExecutor.newDataSource("results");
+        DataSource<PMML4Data> pmmlDataSource = ruleUnitExecutor.newDataSource("pmmlData");
+
+        requestDataSource.insert(requestData);
+        PMML4Result resultHandler = new PMML4Result();
+        resultDataSource.insert(resultHandler);
+        if (data.isPresent()) {
+            pmmlDataSource.insert(data.get());
+        }
+
+        try {
+            ruleUnitExecutor.run(startingRuleUnit("RuleUnitIndicator", requestData.getModelName(),
+                    "org.drools.scorecards.example"));
+        } finally {
+            if (logger != null) {
+                logger.close();
+            }
+        }
+        return resultHandler;
+    }
+
+    private List<String> calculatePossiblePackageNames(String modelId, String...knownPackageNames) {
+        List<String> packageNames = new ArrayList<>();
+        String javaModelId = modelId.replaceAll("\\s","");
+        if (knownPackageNames != null && knownPackageNames.length > 0) {
+            for (String knownPkgName: knownPackageNames) {
+                packageNames.add(knownPkgName + "." + javaModelId);
+            }
+        }
+        String basePkgName = PMML4UnitImpl.DEFAULT_ROOT_PACKAGE+"."+javaModelId;
+        packageNames.add(basePkgName);
+        return packageNames;
+    }
+
+    protected Class<? extends RuleUnit> startingRuleUnit(String startingRule, String modelId, String...knownPackageNames) {
+        List<String> possiblePackages = calculatePossiblePackageNames(modelId, knownPackageNames);
+        InternalKnowledgeBase internalKnowledgeBase = (InternalKnowledgeBase) kieBase;
+
+        RuleUnitRegistry unitRegistry = internalKnowledgeBase.getRuleUnitRegistry();
+        Map<String,InternalKnowledgePackage> pkgs = internalKnowledgeBase.getPackagesMap();
+        RuleImpl ruleImpl = null;
+        for (String pkgName: possiblePackages) {
+            if (pkgs.containsKey(pkgName)) {
+                InternalKnowledgePackage pkg = pkgs.get(pkgName);
+                ruleImpl = pkg.getRule(startingRule);
+                if (ruleImpl != null) {
+                    RuleUnitDescr descr = unitRegistry.getRuleUnitFor(ruleImpl).orElse(null);
+                    if (descr != null) {
+                        return descr.getRuleUnitClass();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
--- kie-pmml/src/test/java/org/kie/pmml/pmml_4_2/predictive/models/DecisionTreeTest.java
+++ kie-pmml/src/test/java/org/kie/pmml/pmml_4_2/predictive/models/DecisionTreeTest.java
@@ -21,9 +21,11 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
+import org.assertj.core.api.Assertions;
 import org.drools.core.impl.InternalKnowledgeBase;
 import org.drools.core.impl.InternalRuleUnitExecutor;
 import org.junit.After;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.kie.api.KieBase;
 import org.kie.api.io.Resource;
@@ -37,6 +39,8 @@ import org.kie.api.runtime.rule.RuleUnitExecutor;
 import org.kie.internal.io.ResourceFactory;
 import org.kie.internal.utils.KieHelper;
 import org.kie.pmml.pmml_4_2.DroolsAbstractPMMLTest;
+import org.kie.pmml.pmml_4_2.PMMLExecutor;
+import org.kie.pmml.pmml_4_2.PMMLKieBaseUtil;
 import org.kie.api.pmml.PMML4Result;
 import org.kie.pmml.pmml_4_2.model.AbstractModel;
 import org.kie.api.pmml.PMMLRequestData;
@@ -50,14 +54,26 @@ import static org.junit.Assert.assertTrue;
 
 public class DecisionTreeTest extends DroolsAbstractPMMLTest {
 
+    private static final String DECISION_TREES_FOLDER = "org/kie/pmml/pmml_4_2/";
 
     private static final boolean VERBOSE = false;
-    private static final String source1 = "org/kie/pmml/pmml_4_2/test_tree_simple.pmml";
-    private static final String source2 = "org/kie/pmml/pmml_4_2/test_tree_missing.pmml";
-    private static final String source3 = "org/kie/pmml/pmml_4_2/test_tree_handwritten.pmml";
+    private static final String source1 = DECISION_TREES_FOLDER + "test_tree_simple.pmml";
+    private static final String source2 = DECISION_TREES_FOLDER + "test_tree_missing.pmml";
+    private static final String source3 = DECISION_TREES_FOLDER + "test_tree_handwritten.pmml";
     private static final String packageName = "org.kie.pmml.pmml_4_2.test";
 
-
+    private static final String TREE_RETURN_NULL_NOTRUECHILD_STRATEGY = DECISION_TREES_FOLDER +
+            "test_tree_return_null_notruechild_strategy.pmml";
+    private static final String TREE_RETURN_LAST_NOTRUE_CHILD_STRATEGY = DECISION_TREES_FOLDER +
+            "test_tree_return_last_notruechild_strategy.pmml";
+    private static final String TREE_DEFAULT_CHILD_MISSING_STRATEGY =
+            DECISION_TREES_FOLDER + "test_tree_default_child_missing_value_strategy.pmml";
+    private static final String TREE_LAST_CHILD_MISSING_STRATEGY = DECISION_TREES_FOLDER +
+            "test_tree_last_missing_value_strategy.pmml";
+    private static final String TREE_RETURN_NULL_MISSING_STRATEGY = DECISION_TREES_FOLDER +
+            "test_tree_return_null_missing_value_strategy.pmml";
+    private static final String TREE_WEIGHTED_CONFIDENCE_MISSING_STRATEGY = DECISION_TREES_FOLDER +
+            "test_tree_weightedconfidence_missing_value_strategy.pmml";
 
     @After
     public void tearDown() {
@@ -125,7 +141,123 @@ public class DecisionTreeTest extends DroolsAbstractPMMLTest {
         assertEquals("tgtY",targetValue);
     }
     
-    
+    @Test
+    public void testReturnNullNoTrueChildPredictionStrategy() {
+        KieBase kieBase = PMMLKieBaseUtil.createKieBaseWithPMML(TREE_RETURN_NULL_NOTRUECHILD_STRATEGY);
+        PMMLExecutor executor = new PMMLExecutor(kieBase);
+
+        PMMLRequestData request = new PMMLRequestData("123","TreeTest");
+        request.addRequestParam("fld1", 30.0);
+        PMML4Result resultHolder = executor.run(request);
+        Assertions.assertThat(resultHolder).isNotNull();
+        String targetValue = resultHolder.getResultValue("Fld2", "value", String.class).orElse(null);
+        Assertions.assertThat(targetValue).isEqualTo("tgtY");
+
+        request = new PMMLRequestData("123","TreeTest");
+        request.addRequestParam("fld1", 50.0);
+        resultHolder = executor.run(request);
+        Assertions.assertThat(resultHolder).isNotNull();
+        Assertions.assertThat(resultHolder.getResultValue("Fld2", "value", String.class)).isEmpty();
+    }
+
+    @Test
+    public void testReturnLastNoTrueChildPredictionStrategy() {
+        KieBase kieBase = PMMLKieBaseUtil.createKieBaseWithPMML(TREE_RETURN_LAST_NOTRUE_CHILD_STRATEGY);
+        PMMLExecutor executor = new PMMLExecutor(kieBase);
+
+        PMMLRequestData request = new PMMLRequestData("123","TreeTest");
+        request.addRequestParam("fld1", 30.0);
+        PMML4Result resultHolder = executor.run(request);
+        Assertions.assertThat(resultHolder).isNotNull();
+        String targetValue = resultHolder.getResultValue("Fld2", "value", String.class).orElse(null);
+        Assertions.assertThat(targetValue).isEqualTo("tgtY");
+
+        request = new PMMLRequestData("123","TreeTest");
+        request.addRequestParam("fld1", 50.0);
+        resultHolder = executor.run(request);
+        Assertions.assertThat(resultHolder).isNotNull();
+        targetValue = resultHolder.getResultValue("Fld2", "value", String.class).orElse(null);
+        Assertions.assertThat(targetValue).isEqualTo("tgtX");
+    }
+
+    @Test
+    @Ignore
+    public void testLastPredictionMissingValueStrategy() {
+        KieBase kieBase = PMMLKieBaseUtil.createKieBaseWithPMML(TREE_LAST_CHILD_MISSING_STRATEGY);
+        PMMLExecutor executor = new PMMLExecutor(kieBase);
+
+        PMMLRequestData request = new PMMLRequestData("123","TreeTest");
+        request.addRequestParam("fld1", 30.0);
+        PMML4Result resultHolder = executor.run(request);
+        Assertions.assertThat(resultHolder).isNotNull();
+        String targetValue = resultHolder.getResultValue("Fld3", "value", String.class).orElse(null);
+        Assertions.assertThat(targetValue).isEqualTo("tgtY");
+
+        executor.setRunWithLogging(true);
+        request = new PMMLRequestData("123","TreeTest");
+        request.addRequestParam("fld1", 100.0);
+        resultHolder = executor.run(request);
+        Assertions.assertThat(resultHolder).isNotNull();
+        targetValue = resultHolder.getResultValue("Fld3", "value", String.class).orElse(null);
+        Assertions.assertThat(targetValue).isEqualTo("tgtA");
+    }
+
+    @Test
+    public void testNullPredictionMissingValueStrategy() {
+        KieBase kieBase = PMMLKieBaseUtil.createKieBaseWithPMML(TREE_RETURN_NULL_MISSING_STRATEGY);
+        PMMLExecutor executor = new PMMLExecutor(kieBase);
+
+        PMMLRequestData request = new PMMLRequestData("123","TreeTest");
+        request.addRequestParam("fld1", 30.0);
+        PMML4Result resultHolder = executor.run(request);
+        Assertions.assertThat(resultHolder).isNotNull();
+        String targetValue = resultHolder.getResultValue("Fld3", "value", String.class).orElse(null);
+        Assertions.assertThat(targetValue).isEqualTo("tgtY");
+
+        request = new PMMLRequestData("123","TreeTest");
+        request.addRequestParam("fld1", 100.0);
+        resultHolder = executor.run(request);
+        Assertions.assertThat(resultHolder).isNotNull();
+        targetValue = resultHolder.getResultValue("Fld3", "value", String.class).orElse(null);
+        Assertions.assertThat(targetValue).isNull();
+    }
+
+    @Test
+    public void testDefaultChildMissingValueStrategy() {
+        KieBase kieBase = PMMLKieBaseUtil.createKieBaseWithPMML(TREE_DEFAULT_CHILD_MISSING_STRATEGY);
+        PMMLExecutor executor = new PMMLExecutor(kieBase);
+
+        PMMLRequestData request = new PMMLRequestData("123","TreeTest");
+        request.addRequestParam("fld1", 30.0);
+        PMML4Result resultHolder = executor.run(request);
+        Assertions.assertThat(resultHolder).isNotNull();
+        Assertions.assertThat(resultHolder.getResultValue("Fld3", "value", String.class).get()).isEqualTo("tgtY");
+
+        request = new PMMLRequestData("123","TreeTest");
+        request.addRequestParam("fld1", 100.0);
+        resultHolder = executor.run(request);
+        Assertions.assertThat(resultHolder).isNotNull();
+        Assertions.assertThat(resultHolder.getResultValue("Fld3", "value", String.class).get()).isEqualTo("tgtZ");
+    }
+
+    @Test
+    @Ignore
+    public void testWeightedConfidenceMissingValueStrategy() {
+        KieBase kieBase = PMMLKieBaseUtil.createKieBaseWithPMML(TREE_WEIGHTED_CONFIDENCE_MISSING_STRATEGY);
+        PMMLExecutor executor = new PMMLExecutor(kieBase);
+
+        PMMLRequestData request = new PMMLRequestData("123","TreeTest");
+        request.addRequestParam("fld1", 30.0);
+        PMML4Result resultHolder = executor.run(request);
+        Assertions.assertThat(resultHolder).isNotNull();
+        Assertions.assertThat(resultHolder.getResultValue("Fld3", "value", String.class).get()).isEqualTo("tgtY");
+
+        request = new PMMLRequestData("123","TreeTest");
+        request.addRequestParam("fld1", 50.0);
+        resultHolder = executor.run(request);
+        Assertions.assertThat(resultHolder).isNotNull();
+        Assertions.assertThat(resultHolder.getResultValue("Fld3", "value", String.class).get()).isEqualTo("tgtX");
+    }
     
     protected Object getToken( KieSession kSession, String treeModelName ) {
         String className = AbstractModel.PMML_JAVA_PACKAGE_NAME + "." + treeModelName + "TreeToken";
