afbde18f7a2e034e03573f6a3b6ccbe3f7a22a7d
Luca Molteni <volothamp@gmail.com>
2018-02-21 12:36:47 +0100

    [DROOLS-2284] Fix Accumulate with PatternDSL (#1783)
    Add bind as last chain call
    
    Also add bind as last chained call in BinaryExpr

--- kogito-drools/drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/visitor/accumulate/AccumulateVisitorPatternDSL.java
+++ kogito-drools/drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/visitor/accumulate/AccumulateVisitorPatternDSL.java
@@ -1,8 +1,10 @@
 package org.drools.modelcompiler.builder.generator.visitor.accumulate;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
+import org.drools.javaparser.ast.Node;
 import org.drools.javaparser.ast.expr.Expression;
 import org.drools.javaparser.ast.expr.MethodCallExpr;
 import org.drools.javaparser.ast.expr.NameExpr;
@@ -35,23 +37,32 @@ public class AccumulateVisitorPatternDSL extends AccumulateVisitor {
     protected void processNewBinding(Optional<NewBinding> optNewBinding) {
         optNewBinding.ifPresent(newBinding -> {
             final Optional<String> patterBinding = newBinding.patternBinding;
+            final List<Expression> allExpressions = context.getExpressions();
+            final MethodCallExpr newBindingExpression = newBinding.bindExpression;
             if (patterBinding.isPresent()) {
-                final Optional<MethodCallExpr> optPattern = DrlxParseUtil.findPatternWithBinding(patterBinding.get(), context.getExpressions());
-                optPattern.ifPresent(p -> p.addArgument(newBinding.bindExpression));
+                final Optional<MethodCallExpr> optPattern = DrlxParseUtil.findPatternWithBinding(patterBinding.get(), allExpressions);
+                optPattern.ifPresent(pattern -> addBindAsLastChainCall(newBindingExpression, pattern));
             } else {
-                final MethodCallExpr lastPattern = DrlxParseUtil.findLastPattern(context.getExpressions())
+                final MethodCallExpr lastPattern = DrlxParseUtil.findLastPattern(allExpressions)
                         .orElseThrow(() -> new RuntimeException("Need the last pattern to add the binding"));
-                final Expression replaceBinding = replaceBindingWithPatternBinding(newBinding.bindExpression, lastPattern);
-                lastPattern.addArgument(replaceBinding);
+                final MethodCallExpr replacedBinding = replaceBindingWithPatternBinding(newBindingExpression, lastPattern);
+                addBindAsLastChainCall(replacedBinding, lastPattern);
             }
         });
     }
 
-    private Expression replaceBindingWithPatternBinding(Expression bindExpression, MethodCallExpr lastPattern) {
+    private void addBindAsLastChainCall(MethodCallExpr newBindingExpression, MethodCallExpr pattern) {
+        final Expression newScope = (Expression) pattern.getParentNode().orElse(pattern);
+        final Optional<Node> optParent = newScope.getParentNode();
+        newBindingExpression.setScope(newScope);
+        optParent.ifPresent(parent -> parent.replace(newScope, newBindingExpression));
+    }
+
+    private MethodCallExpr replaceBindingWithPatternBinding(Expression bindExpression, MethodCallExpr lastPattern) {
         final Expression bindingId = lastPattern.getArgument(0);
         final Optional<NameExpr> first = bindExpression.findFirst(NameExpr.class, e -> e.equals(bindingId));
         first.ifPresent(bindExpression::remove);
-        return bindExpression;
+        return (MethodCallExpr) bindExpression;
     }
 
     @Override
