a0c83d1deb591107a3d0e04c62204638258bb51d
Luca Molteni <volothamp@gmail.com>
2018-02-28 15:39:55 +0100

    LegacyAccumulateInvoker

--- drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/accumulateLegacy/LegacyAccumulateInvoker.java
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/accumulateLegacy/LegacyAccumulateInvoker.java
@@ -1,44 +1,22 @@
 package org.drools.modelcompiler.builder.generator.accumulateLegacy;
 
-import java.io.Serializable;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import org.drools.core.base.ClassFieldAccessorCache;
-import org.drools.core.base.ClassFieldAccessorStore;
-import org.drools.core.base.ClassObjectType;
-import org.drools.core.rule.Declaration;
-import org.drools.core.spi.PatternExtractor;
-import org.drools.core.util.StringUtils;
 import org.drools.javaparser.JavaParser;
 import org.drools.javaparser.ast.CompilationUnit;
-import org.drools.modelcompiler.domain.Person;
-import org.junit.Before;
-import org.junit.Test;
+import org.drools.javaparser.ast.body.ClassOrInterfaceDeclaration;
 
-import static org.junit.Assert.*;
+public class LegacyAccumulateInvoker {
 
-public class LegacyAccumulateTest {
+    public final Map<String, String> map;
 
-    @Before
-    public void setUp() throws Exception {
-        store.setClassFieldAccessorCache( new ClassFieldAccessorCache(Thread.currentThread().getContextClassLoader() ) );
-        store.setEagerWire( true );
-    }
-
-    ClassFieldAccessorStore store = new ClassFieldAccessorStore();
-
-    public String multiPatternResult =
-            "package org.drools;\n" +
+    public String fixedPart = "package org.drools;\n" +
             "\n" +
             "import org.mvel2.asm.ClassReader;\n" +
             "import org.mvel2.asm.util.TraceMethodVisitor;\n" +
             "import org.drools.core.util.asm.MethodComparator.Tracer;\n" +
             "import java.util.Collections;\n" +
-            "public class Rule0Accumulate0Invoker implements org.drools.core.spi.Accumulator, org.drools.core.spi.CompiledInvoker\n" +
+            "public class _CLASS_NAME implements org.drools.core.spi.Accumulator, org.drools.core.spi.CompiledInvoker\n" +
             "{\n" +
             "    private static final long serialVersionUID  = 510l;\n" +
             "\n" +
@@ -190,231 +168,23 @@ public class LegacyAccumulateTest {
             "    }\n" +
             "}";
 
-
-    final CompilationUnit multiPatternResultParsed = JavaParser.parse(multiPatternResult);
-
-
-    @Test
-    public void testInvokerGenerationMultiPattern() throws Exception {
-
-        final String className = "accumulate0";
-
-        final String[] declarationTypes = new String[]{"String", "int"};
-        final Declaration[] declarations = new Declaration[]{new Declaration("name",
-                                                                             store.getReader( Person.class,
-                                                                                              "name" ),
-                                                                             null ), new Declaration( "age",
-                                                                                                      store.getReader( Person.class,
-                                                                                                                       "age" ),
-                                                                                                      null )};
-        final Declaration[] inner = new Declaration[]{new Declaration( "$cheese",
-                                                                       new PatternExtractor(new ClassObjectType(Cheese.class ) ),
-                                                                       null ), new Declaration( "$person",
-                                                                                                new PatternExtractor( new ClassObjectType( Person.class ) ),
-                                                                                                null )};
-        final String[] globals = new String[]{"aGlobal", "anotherGlobal"};
-        final List globalTypes = Arrays.asList(new String[]{"String", "String"} );
-
-        final Map map = new HashMap();
-
-        map.put( "className",
-                 StringUtils.ucFirst( className ) );
-
-        map.put( "instanceName",
-                 className );
-
-        map.put( "package",
-                 "org.drools" );
-
-        map.put( "ruleClassName",
-                 "Rule0" );
-
-        map.put( "invokerClassName",
-                 "Rule0" + StringUtils.ucFirst(className ) + "Invoker" );
-
-        map.put( "declarations",
-                 declarations );
-
-        map.put( "declarationTypes",
-                 declarationTypes );
-
-        map.put( "globals",
-                 globals );
-
-        map.put( "globalTypes",
-                 globalTypes );
-
-        map.put( "innerDeclarations",
-                 inner );
-
-        map.put( "attributes",
-                 new Attribute[]{new Attribute("int",
-                                                    "x" )} );
-        map.put( "initCode",
-                 "x = 0;" );
-        map.put( "actionCode",
-                 "x += 1;" );
-        map.put( "reverseCode",
-                 "" );
-        map.put( "resultCode",
-                 "x + 10" );
-
-        map.put( "supportsReverse",
-                 "false" );
-
-        map.put( "resultType",
-                 Integer.class );
-
-        map.put( "hashCode",
-                 new Integer( 10 ) );
-        map.put( "isMultiPattern",
-                 Boolean.TRUE );
-
-
-
-        LegacyAccumulate la = new LegacyAccumulate();
-
-        final CompilationUnit build = la.build(map);
-
-        assertEquals(multiPatternResultParsed, build);
+    public LegacyAccumulateInvoker(Map<String, String> map) {
+        this.map = map;
     }
 
+    public CompilationUnit build() {
 
-    public static class Attribute {
-        private String type;
-        private String name;
+        final CompilationUnit cu = JavaParser.parse(fixedPart);
 
-        public Attribute(String type,
-                         String name) {
-            this.type = type;
-            this.name = name;
-        }
+        setInvokerClassName(cu);
 
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-        public void setType(String type) {
-            this.type = type;
-        }
+        return cu;
     }
 
-    public class Cheese
-            implements
-            Serializable {
-
-        public static final String STILTON = "stilton";
-
-        public static final int BASE_PRICE = 10;
-
-        private static final long serialVersionUID = 510l;
-        private String            type;
-        private int               price;
-        private int               oldPrice;
-        private Date usedBy;
-        private double            doublePrice;
-
-        public Cheese() {
-
-        }
-
-        public Cheese(final String type) {
-            super();
-            this.type = type;
-            this.price = 0;
-        }
-
-        public Cheese(final String type,
-                      final int price) {
-            super();
-            this.type = type;
-            this.price = price;
-        }
-
-        public Cheese(final String type,
-                      final int price,
-                      final int oldPrice ) {
-            super();
-            this.type = type;
-            this.price = price;
-            this.oldPrice = oldPrice;
-        }
-
-        public int getPrice() {
-            return this.price;
-        }
-
-        public String getType() {
-            return this.type;
-        }
-
-        public void setType(final String type) {
-            this.type = type;
-        }
-
-        public void setPrice(final int price) {
-            this.price = price;
-        }
-
-        public String toString() {
-            return "Cheese( type='" + this.type + "', price=" + this.price + " )";
-        }
-
-        public int hashCode() {
-            final int PRIME = 31;
-            int result = 1;
-            result = PRIME * result + price;
-            result = PRIME * result + ((type == null) ? 0 : type.hashCode());
-            return result;
-        }
-
-        public boolean equals(Object obj) {
-            if ( this == obj ) return true;
-            if ( obj == null ) return false;
-            if ( getClass() != obj.getClass() ) return false;
-            final Cheese other = (Cheese) obj;
-            if ( price != other.price ) return false;
-            if ( type == null ) {
-                if ( other.type != null ) return false;
-            } else if ( !type.equals( other.type ) ) return false;
-            return true;
-        }
-
-        public int getOldPrice() {
-            return oldPrice;
-        }
-
-        public void setOldPrice(int oldPrice) {
-            this.oldPrice = oldPrice;
-        }
-
-        public Date getUsedBy() {
-            return usedBy;
-        }
-
-        public void setUsedBy(Date usedBy) {
-            this.usedBy = usedBy;
-        }
-
-        public synchronized double getDoublePrice() {
-            return doublePrice;
-        }
-
-        public synchronized void setDoublePrice(double doublePrice) {
-            this.doublePrice = doublePrice;
-        }
-
-
-
+    private void setInvokerClassName(CompilationUnit cu) {
+        final String className = map.get("invokerClassName");
+        cu.findAll(ClassOrInterfaceDeclaration.class)
+                .stream()
+                .forEach(n -> n.setName(className));
     }
-
-
 }
--- drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/builder/generator/accumulateLegacy/LegacyAccumulateInvokerTest.java
+++ drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/builder/generator/accumulateLegacy/LegacyAccumulateInvokerTest.java
@@ -1,14 +1,38 @@
 package org.drools.modelcompiler.builder.generator.accumulateLegacy;
 
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.drools.core.base.ClassFieldAccessorCache;
+import org.drools.core.base.ClassFieldAccessorStore;
+import org.drools.core.base.ClassObjectType;
+import org.drools.core.rule.Declaration;
+import org.drools.core.spi.PatternExtractor;
+import org.drools.core.util.StringUtils;
 import org.drools.javaparser.JavaParser;
 import org.drools.javaparser.ast.CompilationUnit;
-import org.drools.javaparser.ast.stmt.BlockStmt;
+import org.drools.modelcompiler.domain.Person;
+import org.junit.Before;
+import org.junit.Test;
 
-public class LegacyAccumulate {
+import static org.junit.Assert.*;
 
-    public String fixedPart =  "package org.drools;\n" +
+public class LegacyAccumulateInvokerTest {
+
+    @Before
+    public void setUp() throws Exception {
+        store.setClassFieldAccessorCache( new ClassFieldAccessorCache(Thread.currentThread().getContextClassLoader() ) );
+        store.setEagerWire( true );
+    }
+
+    ClassFieldAccessorStore store = new ClassFieldAccessorStore();
+
+    public String multiPatternResult =
+            "package org.drools;\n" +
             "\n" +
             "import org.mvel2.asm.ClassReader;\n" +
             "import org.mvel2.asm.util.TraceMethodVisitor;\n" +
@@ -167,11 +191,230 @@ public class LegacyAccumulate {
             "}";
 
 
+    final CompilationUnit multiPatternResultParsed = JavaParser.parse(multiPatternResult);
 
-    public CompilationUnit build(Map map) {
 
-        final CompilationUnit blockStmt = JavaParser.parse(fixedPart);
+    @Test
+    public void testInvokerGenerationMultiPattern() throws Exception {
 
-        return blockStmt;
+        final String className = "accumulate0";
+
+        final String[] declarationTypes = new String[]{"String", "int"};
+        final Declaration[] declarations = new Declaration[]{new Declaration("name",
+                                                                             store.getReader( Person.class,
+                                                                                              "name" ),
+                                                                             null ), new Declaration( "age",
+                                                                                                      store.getReader( Person.class,
+                                                                                                                       "age" ),
+                                                                                                      null )};
+        final Declaration[] inner = new Declaration[]{new Declaration( "$cheese",
+                                                                       new PatternExtractor(new ClassObjectType(Cheese.class ) ),
+                                                                       null ), new Declaration( "$person",
+                                                                                                new PatternExtractor( new ClassObjectType( Person.class ) ),
+                                                                                                null )};
+        final String[] globals = new String[]{"aGlobal", "anotherGlobal"};
+        final List globalTypes = Arrays.asList(new String[]{"String", "String"} );
+
+        final Map map = new HashMap();
+
+        map.put( "className",
+                 StringUtils.ucFirst( className ) );
+
+        map.put( "instanceName",
+                 className );
+
+        map.put( "package",
+                 "org.drools" );
+
+        map.put( "ruleClassName",
+                 "Rule0" );
+
+        map.put( "invokerClassName",
+                 "Rule0" + StringUtils.ucFirst(className ) + "Invoker" );
+
+        map.put( "declarations",
+                 declarations );
+
+        map.put( "declarationTypes",
+                 declarationTypes );
+
+        map.put( "globals",
+                 globals );
+
+        map.put( "globalTypes",
+                 globalTypes );
+
+        map.put( "innerDeclarations",
+                 inner );
+
+        map.put( "attributes",
+                 new Attribute[]{new Attribute("int",
+                                                    "x" )} );
+        map.put( "initCode",
+                 "x = 0;" );
+        map.put( "actionCode",
+                 "x += 1;" );
+        map.put( "reverseCode",
+                 "" );
+        map.put( "resultCode",
+                 "x + 10" );
+
+        map.put( "supportsReverse",
+                 "false" );
+
+        map.put( "resultType",
+                 Integer.class );
+
+        map.put( "hashCode",
+                 new Integer( 10 ) );
+        map.put( "isMultiPattern",
+                 Boolean.TRUE );
+
+
+
+        LegacyAccumulateInvoker la = new LegacyAccumulateInvoker(map);
+
+        final CompilationUnit build = la.build();
+
+        assertEquals(multiPatternResultParsed.toString(), build.toString());
     }
+
+
+    public static class Attribute {
+        private String type;
+        private String name;
+
+        public Attribute(String type,
+                         String name) {
+            this.type = type;
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+    }
+
+    public class Cheese
+            implements
+            Serializable {
+
+        public static final String STILTON = "stilton";
+
+        public static final int BASE_PRICE = 10;
+
+        private static final long serialVersionUID = 510l;
+        private String            type;
+        private int               price;
+        private int               oldPrice;
+        private Date usedBy;
+        private double            doublePrice;
+
+        public Cheese() {
+
+        }
+
+        public Cheese(final String type) {
+            super();
+            this.type = type;
+            this.price = 0;
+        }
+
+        public Cheese(final String type,
+                      final int price) {
+            super();
+            this.type = type;
+            this.price = price;
+        }
+
+        public Cheese(final String type,
+                      final int price,
+                      final int oldPrice ) {
+            super();
+            this.type = type;
+            this.price = price;
+            this.oldPrice = oldPrice;
+        }
+
+        public int getPrice() {
+            return this.price;
+        }
+
+        public String getType() {
+            return this.type;
+        }
+
+        public void setType(final String type) {
+            this.type = type;
+        }
+
+        public void setPrice(final int price) {
+            this.price = price;
+        }
+
+        public String toString() {
+            return "Cheese( type='" + this.type + "', price=" + this.price + " )";
+        }
+
+        public int hashCode() {
+            final int PRIME = 31;
+            int result = 1;
+            result = PRIME * result + price;
+            result = PRIME * result + ((type == null) ? 0 : type.hashCode());
+            return result;
+        }
+
+        public boolean equals(Object obj) {
+            if ( this == obj ) return true;
+            if ( obj == null ) return false;
+            if ( getClass() != obj.getClass() ) return false;
+            final Cheese other = (Cheese) obj;
+            if ( price != other.price ) return false;
+            if ( type == null ) {
+                if ( other.type != null ) return false;
+            } else if ( !type.equals( other.type ) ) return false;
+            return true;
+        }
+
+        public int getOldPrice() {
+            return oldPrice;
+        }
+
+        public void setOldPrice(int oldPrice) {
+            this.oldPrice = oldPrice;
+        }
+
+        public Date getUsedBy() {
+            return usedBy;
+        }
+
+        public void setUsedBy(Date usedBy) {
+            this.usedBy = usedBy;
+        }
+
+        public synchronized double getDoublePrice() {
+            return doublePrice;
+        }
+
+        public synchronized void setDoublePrice(double doublePrice) {
+            this.doublePrice = doublePrice;
+        }
+
+
+
+    }
+
+
 }
