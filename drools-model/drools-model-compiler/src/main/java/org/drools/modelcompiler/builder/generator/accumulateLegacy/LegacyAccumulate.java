2d8d00a26b7e28bd86304193ab78d6d5dcde6851
Luca Molteni <volothamp@gmail.com>
2018-02-28 15:13:51 +0100

    New test for generation of template

---
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/accumulateLegacy/LegacyAccumulate.java
@@ -0,0 +1,10 @@
+package org.drools.modelcompiler.builder.generator.accumulateLegacy;
+
+import java.util.Map;
+
+public class LegacyAccumulate {
+
+    public String build(Map map) {
+        return "";
+    }
+}
