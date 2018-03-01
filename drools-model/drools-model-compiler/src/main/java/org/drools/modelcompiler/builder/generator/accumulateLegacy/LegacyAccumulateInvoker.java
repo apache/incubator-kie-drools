630ab2c0fc71f5938ac64895c7f5ec1f9bfe7b27
Luca Molteni <volothamp@gmail.com>
2018-03-01 10:01:06 +0100

    Some more class generation

--- drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/accumulateLegacy/LegacyAccumulateInvoker.java
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/accumulateLegacy/LegacyAccumulateInvoker.java
@@ -1,14 +1,17 @@
 package org.drools.modelcompiler.builder.generator.accumulateLegacy;
 
+import java.lang.reflect.Method;
 import java.util.Map;
 
+import org.drools.core.rule.Declaration;
 import org.drools.javaparser.JavaParser;
 import org.drools.javaparser.ast.CompilationUnit;
 import org.drools.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import org.drools.javaparser.ast.body.MethodDeclaration;
 
 public class LegacyAccumulateInvoker {
 
-    public final Map<String, String> map;
+    public final Map<String, Object> map;
 
     public String fixedPart = "package __mypackage;\n" +
             "\n" +
@@ -29,10 +32,6 @@ public class LegacyAccumulateInvoker {
             "                     org.drools.core.spi.Tuple tuple,\n" +
             "                     org.drools.core.rule.Declaration[] declarations,\n" +
             "                     org.drools.core.WorkingMemory workingMemory) throws Exception {\n" +
-            "         java.lang.String name = ( java.lang.String ) declarations[0].getValue( (org.drools.core.common.InternalWorkingMemory) workingMemory, tuple.getObject( declarations[0] ) );\n" +
-            "            \n" +
-            "         int age = ( java.lang.Integer ) declarations[1].getIntValue( (org.drools.core.common.InternalWorkingMemory) workingMemory, tuple.getObject( declarations[1] ) );\n" +
-            "            \n" +
             "        \n" +
             "         String aGlobal = ( String ) workingMemory.getGlobal( \"aGlobal\" );\n" +
             "         String anotherGlobal = ( String ) workingMemory.getGlobal( \"anotherGlobal\" );\n" +
@@ -179,15 +178,24 @@ public class LegacyAccumulateInvoker {
         setPackage(cu);
         setInvokerClassName(cu);
 
+
+        MethodDeclaration initMethod = cu.findAll(MethodDeclaration.class, m -> m.getName().asString().equals("init")).iterator().next();
+
+        for(Declaration d : (Declaration[]) map.get("declarations")) {
+             String res = "@{declr.typeName} @{declr.identifier} = ( @{declr.boxedTypeName} ) previousDeclarations[@{i0}].@{declr.nativeReadMethod.name}( (org.drools.core.common.InternalWorkingMemory) workingMemory, tuple.getObject( previousDeclarations[@{i0}] ) );";
+
+
+        }
+
         return cu;
     }
 
     private CompilationUnit setPackage(CompilationUnit cu) {
-        return cu.setPackageDeclaration(map.get("package"));
+        return cu.setPackageDeclaration((String) map.get("package"));
     }
 
     private void setInvokerClassName(CompilationUnit cu) {
-        final String className = map.get("invokerClassName");
+        final String className = (String) map.get("invokerClassName");
         cu.findAll(ClassOrInterfaceDeclaration.class)
                 .stream()
                 .forEach(n -> n.setName(className));
