8cb521917c103de8a57f478ed544216217adc6ef
Maciej Swiderski <swiderski.maciej@gmail.com>
2018-03-05 15:21:34 +0100

    JBPM-6894 - User should be able to define case, process, activity level SLA (#1795)

---
+++ kogito-drools/drools-core/src/main/java/org/drools/core/event/SLAViolatedEventImpl.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.core.event;
+
+import org.kie.api.event.process.SLAViolatedEvent;
+import org.kie.api.runtime.KieRuntime;
+import org.kie.api.runtime.process.NodeInstance;
+import org.kie.api.runtime.process.ProcessInstance;
+
+public class SLAViolatedEventImpl extends ProcessEvent implements SLAViolatedEvent {
+
+    private static final long serialVersionUID = 510l;
+    private NodeInstance nodeInstance;
+
+    public SLAViolatedEventImpl(final ProcessInstance instance, KieRuntime kruntime ) {
+        super( instance, kruntime );
+    }
+    
+    public SLAViolatedEventImpl(final ProcessInstance instance, NodeInstance nodeInstance, KieRuntime kruntime ) {
+        super( instance, kruntime );
+        this.nodeInstance = nodeInstance;
+    }
+
+    public String toString() {
+        return "==>[SLAViolatedEvent(name=" + getProcessInstance().getProcessName() + "; id=" + getProcessInstance().getProcessId() + ")]";
+    }
+
+    @Override
+    public NodeInstance getNodeInstance() {
+        return nodeInstance;
+    }
+    
+}
