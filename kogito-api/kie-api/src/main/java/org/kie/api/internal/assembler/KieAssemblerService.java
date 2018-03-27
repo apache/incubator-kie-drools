adcc9f2d0462eb4a1b8ace651419f7d6c26b12c3
tarilabs <matteo.mortari@gmail.com>
2018-03-27 08:59:24 +0200

    DROOLS-1663 Kie DMN doesn't support IMPORT decisions between DMN files (#300)
    * WIP
    
    * .
    
    * Dependency sorter
    
    * Externalize ResourceWithConfiguration interface
    
    * Use wide type Collection for collection of resources
    
    * Correct typo

--- kogito-api/kie-api/src/main/java/org/kie/api/internal/assembler/KieAssemblerService.java
+++ kogito-api/kie-api/src/main/java/org/kie/api/internal/assembler/KieAssemblerService.java
@@ -15,13 +15,24 @@
 
 package org.kie.api.internal.assembler;
 
+import java.util.Collection;
+
 import org.kie.api.internal.utils.KieService;
 import org.kie.api.io.Resource;
 import org.kie.api.io.ResourceConfiguration;
 import org.kie.api.io.ResourceType;
+import org.kie.api.io.ResourceWithConfiguration;
 
 public interface KieAssemblerService extends KieService {
     ResourceType getResourceType();
+
+    default void addResources(Object kbuilder, Collection<ResourceWithConfiguration> resources, ResourceType type) throws Exception {
+        for (ResourceWithConfiguration rd : resources) {
+            rd.getBeforeAdd().accept(kbuilder);
+            addResource(kbuilder, rd.getResource(), type, rd.getResourceConfiguration());
+            rd.getAfterAdd().accept(kbuilder);
+        }
+    }
 
     void addResource(Object kbuilder, Resource resource, ResourceType type, ResourceConfiguration configuration) throws Exception;
 }
