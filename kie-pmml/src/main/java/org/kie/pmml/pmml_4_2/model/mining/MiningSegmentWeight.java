dcd303538db60f9b3c34c9bcdab14364ea04e37a
Lance Leverich <lance.leverich@gmail.com>
2018-04-03 08:45:53 -0700

    RHDM-515 Add weighted average mode to MiningModel * Added functionality to retrieve the name of a segment's target field * Added a new class MiningSegmentWeight that is used during the application of a model (as opposed to being used during rule generation) * Updated the MiningSegmentation to generate rules for weighted average mode * Added template for weighted average mode rules * Added test to MiningmodelTest for weighted average mode * Added a test PMML model

---
+++ kie-pmml/src/main/java/org/kie/pmml/pmml_4_2/model/mining/MiningSegmentWeight.java
@@ -0,0 +1,111 @@
+package org.kie.pmml.pmml_4_2.model.mining;
+
+import java.util.List;
+
+public class MiningSegmentWeight {
+	private String segmentationId;
+	private String segmentId;
+	private String targetName;
+	private Double weight;
+	private Number segmentValue;
+	
+	public MiningSegmentWeight(MiningSegment segment) {
+		super();
+		this.segmentationId = segment.getOwner().getSegmentationId();
+		this.segmentId = segment.getSegmentId();
+		List<String> targets = segment.getTargetsForWeighting();
+		if (targets != null && !targets.isEmpty()) {
+		   this.targetName = targets.get(0);
+		}
+		this.weight = segment.getWeight();
+	}
+	public MiningSegmentWeight(String segmentationId, String segmentId, String targetName, Double weight) {
+		super();
+		this.segmentationId = segmentationId;
+		this.segmentId = segmentId;
+		this.targetName = targetName;
+		this.weight = weight;
+	}
+
+	public String getSegmentationId() {
+		return segmentationId;
+	}
+
+	public void setSegmentationId(String segmentationId) {
+		this.segmentationId = segmentationId;
+	}
+
+	public String getSegmentId() {
+		return segmentId;
+	}
+
+	public void setSegmentId(String segmentId) {
+		this.segmentId = segmentId;
+	}
+
+	public String getTargetName() {
+		return targetName;
+	}
+
+	public void setTargetName(String targetName) {
+		this.targetName = targetName;
+	}
+
+	public Double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Double weight) {
+		this.weight = weight;
+	}
+
+	public Number getSegmentValue() {
+		return segmentValue;
+	}
+
+	public void setSegmentValue(Number segmentValue) {
+		this.segmentValue = segmentValue;
+	}
+	
+	public Number getWeightedSegmentValue() {
+		return this.segmentValue.doubleValue() * this.weight;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((segmentId == null) ? 0 : segmentId.hashCode());
+		result = prime * result + ((segmentationId == null) ? 0 : segmentationId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MiningSegmentWeight other = (MiningSegmentWeight) obj;
+		if (segmentId == null) {
+			if (other.segmentId != null)
+				return false;
+		} else if (!segmentId.equals(other.segmentId))
+			return false;
+		if (segmentationId == null) {
+			if (other.segmentationId != null)
+				return false;
+		} else if (!segmentationId.equals(other.segmentationId))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "MiningSegmentWeight [segmentationId=" + segmentationId + ", segmentId=" + segmentId + ", targetName="
+				+ targetName + ", weight=" + weight + ", segmentValue=" + segmentValue + "]";
+	}
+
+	
+}
