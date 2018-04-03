06b689733cfe476c12b25607723b81971f4cbd51
Luca Molteni <volothamp@gmail.com>
2018-04-03 15:49:12 +0200

    ObjectTypeNodeCompilerTest.java with executable model

--- drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/alphaNetworkCompiler/ObjectTypeNodeCompilerTest.java
+++ drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/alphaNetworkCompiler/ObjectTypeNodeCompilerTest.java
@@ -1,19 +1,12 @@
 package org.drools.compiler.reteoo.compiled;
 
-import org.drools.core.ClockType;
 import org.drools.modelcompiler.BaseModelTest;
 import org.drools.modelcompiler.domain.Person;
 import org.drools.modelcompiler.domain.Result;
 import org.junit.Test;
-import org.kie.api.KieBaseConfiguration;
 import org.kie.api.KieServices;
 import org.kie.api.builder.model.KieModuleModel;
-import org.kie.api.conf.EventProcessingOption;
-import org.kie.api.io.ResourceType;
 import org.kie.api.runtime.KieSession;
-import org.kie.api.runtime.conf.ClockTypeOption;
-import org.kie.internal.conf.AlphaNetworkCompilerOption;
-import org.kie.internal.utils.KieHelper;
 
 import static org.junit.Assert.*;
 
@@ -23,62 +16,50 @@ public class ObjectTypeNodeCompilerTest extends BaseModelTest {
         super(testRunType);
     }
 
-//    @Test
-//    public void testAlphaConstraint() {
-//        String str =
-//                "rule \"Bind\"\n" +
-//                        "when\n" +
-//                        "  $s : String( length > 4, length < 10)\n" +
-//                        "then\n" +
-//                        "end";
+    @Test
+    public void testAlphaConstraint() {
+        String str =
+                "rule \"Bind\"\n" +
+                        "when\n" +
+                        "  $s : String( length > 4, length < 10)\n" +
+                        "then\n" +
+                        "end";
+
+        KieModuleModel kproj = getKieModuleModelWithAlphaNetworkCompiler();
+        KieSession ksession = getKieSession(kproj, str );
+
+        ksession.insert("Luca");
+        ksession.insert("Asdrubale");
+
+        assertEquals(1, ksession.fireAllRules());
+    }
 //
-//        KieBaseConfiguration configuration = KieServices.Factory.get().newKieBaseConfiguration();
-//
-//        configuration.setProperty(AlphaNetworkCompilerOption.PROPERTY_NAME, String.valueOf(Boolean.TRUE));
-//
-//        KieSession ksession = new KieHelper()
-//                .addContent(str, ResourceType.DRL)
-//                .build(configuration)
-//                .newKieSession();
-//
-//        ksession.insert("Luca");
-//        ksession.insert("Asdrubale");
-//
-//        assertEquals(1, ksession.fireAllRules());
-//    }
-//
-//    @Test
-//    public void testAlphaConstraintWithModification() {
-//        String str =
-//                "import " + Result.class.getCanonicalName() + ";" +
-//                        "rule \"Bind\"\n" +
-//                        "when\n" +
-//                        "  $r : Result()\n" +
-//                        "  $s : String( length > 4, length < 10)\n" +
-//                        "then\n" +
-//                        "  $r.setValue($s + \" is greater than 4 and smaller than 10\");\n" +
-//                        "end";
-//
-//        KieBaseConfiguration configuration = KieServices.Factory.get().newKieBaseConfiguration();
-//
-//        configuration.setProperty(AlphaNetworkCompilerOption.PROPERTY_NAME, String.valueOf(Boolean.TRUE));
-//
-//        KieSession ksession = new KieHelper()
-//                .addContent(str, ResourceType.DRL)
-//                .build(configuration)
-//                .newKieSession();
-//
-//        ksession.insert("Luca");
-//        ksession.insert("Asdrubale");
-//
-//        Result result = new Result();
-//        ksession.insert(result);
-//
-//        assertEquals(1, ksession.fireAllRules());
-//
-//        ksession.fireAllRules();
-//        assertEquals("Asdrubale is greater than 4 and smaller than 10", result.getValue());
-//    }
+    @Test
+    public void testAlphaConstraintWithModification() {
+        String str =
+                "import " + Result.class.getCanonicalName() + ";" +
+                        "rule \"Bind\"\n" +
+                        "when\n" +
+                        "  $r : Result()\n" +
+                        "  $s : String( length > 4, length < 10)\n" +
+                        "then\n" +
+                        "  $r.setValue($s + \" is greater than 4 and smaller than 10\");\n" +
+                        "end";
+
+        KieModuleModel kproj = getKieModuleModelWithAlphaNetworkCompiler();
+        KieSession ksession = getKieSession(kproj, str );
+
+        ksession.insert("Luca");
+        ksession.insert("Asdrubale");
+
+        Result result = new Result();
+        ksession.insert(result);
+
+        assertEquals(1, ksession.fireAllRules());
+
+        ksession.fireAllRules();
+        assertEquals("Asdrubale is greater than 4 and smaller than 10", result.getValue());
+    }
 
 
 
@@ -106,37 +87,31 @@ public class ObjectTypeNodeCompilerTest extends BaseModelTest {
     }
 
 
-//    @Test
-//    public void testModify2() {
-//        String str =
-//                "import " + Person.class.getCanonicalName() + ";" +
-//                        "rule \"Modify\"\n" +
-//                        "when\n" +
-//                        "  $p : Person( age < 40 )\n" +
-//                        "then\n" +
-//                        "   modify($p) { setAge($p.getAge() + 1); }" +
-//                        "end";
-//
-//        KieBaseConfiguration configuration = KieServices.Factory.get().newKieBaseConfiguration();
-//
-//        configuration.setProperty(AlphaNetworkCompilerOption.PROPERTY_NAME, String.valueOf(Boolean.TRUE));
-//
-//        KieSession ksession = new KieHelper()
-//                .addContent(str, ResourceType.DRL)
-//                .build(configuration)
-//                .newKieSession();
-//
-//        final Person luca = new Person("Luca", 30, false);
-//        ksession.insert(luca);
-//
-//        Result result = new Result();
-//        ksession.insert(result);
-//
-//        assertEquals(10, ksession.fireAllRules());
-//
-//        ksession.fireAllRules();
-//        assertTrue(luca.getAge() == 40);
-//    }
+    @Test
+    public void testModify2() {
+        String str =
+                "import " + Person.class.getCanonicalName() + ";" +
+                        "rule \"Modify\"\n" +
+                        "when\n" +
+                        "  $p : Person( age < 40 )\n" +
+                        "then\n" +
+                        "   modify($p) { setAge($p.getAge() + 1); }" +
+                        "end";
+
+        KieModuleModel kproj = getKieModuleModelWithAlphaNetworkCompiler();
+        KieSession ksession = getKieSession(kproj, str );
+
+        final Person luca = new Person("Luca", 30);
+        ksession.insert(luca);
+
+        Result result = new Result();
+        ksession.insert(result);
+
+        assertEquals(10, ksession.fireAllRules());
+
+        ksession.fireAllRules();
+        assertTrue(luca.getAge() == 40);
+    }
 
 
     private KieModuleModel getKieModuleModelWithAlphaNetworkCompiler() {
