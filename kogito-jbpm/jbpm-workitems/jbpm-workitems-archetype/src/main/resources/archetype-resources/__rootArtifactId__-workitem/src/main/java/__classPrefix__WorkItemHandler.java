0c9d2a000d63e7f6ed419b410b1f5e29dbf07400
Tihomir Surdilovic <tsurdilo@redhat.com>
2018-04-15 20:50:02 -0400

    workitem archetype alignment with workitem repository (#1185)

--- kogito-jbpm/jbpm-workitems/jbpm-workitems-archetype/src/main/resources/archetype-resources/__rootArtifactId__-workitem/src/main/java/__classPrefix__WorkItemHandler.java
+++ kogito-jbpm/jbpm-workitems/jbpm-workitems-archetype/src/main/resources/archetype-resources/__rootArtifactId__-workitem/src/main/java/__classPrefix__WorkItemHandler.java
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ${package};
 
 import java.util.HashMap;
 import java.util.Map;
 import org.jbpm.process.workitem.core.AbstractLogOrThrowWorkItemHandler;
+import org.jbpm.process.workitem.core.util.RequiredParameterValidator;
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemManager;
 import org.jbpm.process.workitem.core.util.Wid;
@@ -14,8 +30,8 @@ import org.jbpm.process.workitem.core.util.WidMavenDepends;
         displayName="${classPrefix}Definitions", icon="${classPrefix}Icon.png",
         defaultHandler="mvel: new ${package}.${classPrefix}WorkItemHandler()",
         parameters={
-            @WidParameter(name="SampleParam"),
-            @WidParameter(name="SampleParamTwo")
+            @WidParameter(name="SampleParam", required = true),
+            @WidParameter(name="SampleParamTwo", required = true)
         },
         results={
             @WidResult(name="SampleResult")
@@ -28,13 +44,14 @@ public class ${classPrefix}WorkItemHandler extends AbstractLogOrThrowWorkItemHan
     public void executeWorkItem(WorkItem workItem,
                                 WorkItemManager manager) {
         try {
+            RequiredParameterValidator.validate(this.getClass(),
+               workItem);
 
             // sample parameters
             String sampleParam = (String) workItem.getParameter("SampleParam");
             String sampleParamTwo = (String) workItem.getParameter("SampleParamTwo");
 
             // complete workitem impl...
-
 
             // return results
             String sampleResult = "sample result";
--- kogito-jbpm/jbpm-workitems/jbpm-workitems-archetype/src/main/resources/archetype-resources/__rootArtifactId__-workitem/src/test/java/__classPrefix__WorkItemHandlerTest.java
+++ kogito-jbpm/jbpm-workitems/jbpm-workitems-archetype/src/main/resources/archetype-resources/__rootArtifactId__-workitem/src/test/java/__classPrefix__WorkItemHandlerTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ${package};
 
 import org.drools.core.process.instance.impl.WorkItemImpl;
@@ -11,6 +26,9 @@ public class ${classPrefix}WorkItemHandlerTest extends AbstractBaseTest {
     @Test
     public void testHandler() throws Exception {
         WorkItemImpl workItem = new WorkItemImpl();
+        workItem.setParameter("SampleParam", "testParamValue");
+        workItem.setParameter("SampleParamTwo", "testParamValue");
+
         TestWorkItemManager manager = new TestWorkItemManager();
 
         ${classPrefix}WorkItemHandler handler = new ${classPrefix}WorkItemHandler();
