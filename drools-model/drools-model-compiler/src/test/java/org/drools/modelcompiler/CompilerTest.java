4241925c6879e128cef4d0b33ecef39464cb30b8
Luca Molteni <volothamp@gmail.com>
2018-04-26 17:14:24 +0200

    Test also for boolean

--- drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/CompilerTest.java
+++ drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/CompilerTest.java
@@ -18,9 +18,7 @@ package org.drools.modelcompiler;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.assertj.core.api.Assertions;
@@ -1322,45 +1320,74 @@ public class CompilerTest extends BaseModelTest {
     }
 
 
-    @Test
-    public void testAlphaNull() {
-        String str =
-                "import " + Person.class.getCanonicalName() + ";" +
-                "import " + Result.class.getCanonicalName() + ";" +
-                "rule R1 when\n" +
-                "  $p : Person( name == null)\n" +
-                "then\n" +
-                "  insert(new Result($p.getName()));\n" +
+        @Test
+        public void testAlphaNull() {
+            String str =
+                    "import " + Person.class.getCanonicalName() + ";" +
+                    "import " + Result.class.getCanonicalName() + ";" +
+                    "rule R1 when\n" +
+                    "  $p : Person( name == null)\n" +
+                    "then\n" +
+                    "  insert(new Result($p.getName()));\n" +
 
-                "end\n" +
-                "rule R2 when\n" +
-                "  $p : Person(  name == \"Luca\")\n" +
-                "then\n" +
-                "  insert(new Result($p.getName()));\n" +
-                "end\n" +
-                "rule R3 when\n" +
-                "  $p : Person(  name == \"Pippo\")\n" +
-                "then\n" +
-                "  insert(new Result($p.getName()));\n" +
-                "end";
+                    "end\n" +
+                    "rule R2 when\n" +
+                    "  $p : Person(  name == \"Luca\")\n" +
+                    "then\n" +
+                    "  insert(new Result($p.getName()));\n" +
+                    "end\n" +
+                    "rule R3 when\n" +
+                    "  $p : Person(  name == \"Pippo\")\n" +
+                    "then\n" +
+                    "  insert(new Result($p.getName()));\n" +
+                    "end";
 
-        KieSession ksession = getKieSession( str );
+            KieSession ksession = getKieSession( str );
 
-        Person first = new Person( null, 40 );
-        Person second = new Person( "Luca", 40 );
-        Person third = new Person( "Mario", 40 );
-        ksession.insert( first );
-        ksession.insert( second );
-        ksession.insert( third );
-        ksession.fireAllRules();
+            Person first = new Person( null, 40 );
+            Person second = new Person( "Luca", 40 );
+            Person third = new Person( "Mario", 40 );
+            ksession.insert( first );
+            ksession.insert( second );
+            ksession.insert( third );
+            ksession.fireAllRules();
 
-        List<Object> results = getObjectsIntoList(ksession, Result.class)
-                .stream().map(Result::getValue).collect(Collectors.toList());
-        assertEquals(2, results.size());
+            List<Object> results = getObjectsIntoList(ksession, Result.class)
+                    .stream().map(Result::getValue).collect(Collectors.toList());
+            assertEquals(2, results.size());
 
-        Assertions.assertThat(results).containsExactlyInAnyOrder("Luca", null);
+            Assertions.assertThat(results).containsExactlyInAnyOrder("Luca", null);
 
-    }
+        }
+
+        @Test
+        public void testAlphaNullBoolean() {
+            String str =
+                    "import " + Person.class.getCanonicalName() + ";" +
+                    "import " + Result.class.getCanonicalName() + ";" +
+                    "rule R1 when\n" +
+                    "  $p : Person( employed == null)\n" +
+                    "then\n" +
+                    "  insert(new Result($p.getName()));\n" +
+                    "end\n";
+
+            KieSession ksession = getKieSession( str );
+
+            Person first = new Person( "First", 40 );
+            first.setEmployed(null);
+            Person second = new Person( "Second", 40 );
+            second.setEmployed(true);
+            ksession.insert( first );
+            ksession.insert( second );
+            ksession.fireAllRules();
+
+            List<Object> results = getObjectsIntoList(ksession, Result.class)
+                    .stream().map(Result::getValue).collect(Collectors.toList());
+            assertEquals(1, results.size());
+
+            Assertions.assertThat(results).containsExactlyInAnyOrder("First");
+
+        }
 
     @Test
     public void testStringValueOf() {
