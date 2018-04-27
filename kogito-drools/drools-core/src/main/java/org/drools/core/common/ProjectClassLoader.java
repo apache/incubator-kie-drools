0913c2d3371ba8d631d4cc15afae54ea628b182e
Tibor Zimanyi <tibor.zimanyi@gmail.com>
2018-04-27 08:47:40 +0200

    RHPAM-795 RHPAM-55 Register project classloaders as parallel capable (#1874)
    * Add test for a classloader deadlock
    
    * RHPAM-795 Register project classloaders as parallel capable

--- kogito-drools/drools-core/src/main/java/org/drools/core/common/ProjectClassLoader.java
+++ kogito-drools/drools-core/src/main/java/org/drools/core/common/ProjectClassLoader.java
@@ -44,6 +44,10 @@ public class ProjectClassLoader extends ClassLoader implements KieTypeResolver {
 
     private static boolean isIBM_JVM = System.getProperty("java.vendor").toLowerCase().contains("ibm");
 
+    static {
+        registerAsParallelCapable();
+    }
+
     private Map<String, byte[]> store;
 
     private Map<String, ClassBytecode> definedTypes;
@@ -348,6 +352,13 @@ public class ProjectClassLoader extends ClassLoader implements KieTypeResolver {
         }
     }
 
+    // WARNING: This is and should be used just for testing purposes.
+    // If not, dragons will come to the Earth, eat all cookies and
+    // hijack all kittens and puppies.
+    void setInternalClassLoader(InternalTypesClassLoader classLoader) {
+        typesClassLoader = classLoader;
+    }
+
     public void setResourceProvider(ResourceProvider resourceProvider) {
         this.resourceProvider = resourceProvider;
     }
@@ -362,7 +373,7 @@ public class ProjectClassLoader extends ClassLoader implements KieTypeResolver {
         nonExistingClasses.addAll(other.nonExistingClasses);
     }
 
-    private InternalTypesClassLoader makeClassLoader() {
+    InternalTypesClassLoader makeClassLoader() {
         return ClassUtils.isAndroid() ?
                 (InternalTypesClassLoader) ClassUtils.instantiateObject(
                         "org.drools.android.DexInternalTypesClassLoader", null, this) :
@@ -405,6 +416,10 @@ public class ProjectClassLoader extends ClassLoader implements KieTypeResolver {
     }
 
     private static class DefaultInternalTypesClassLoader extends ClassLoader implements InternalTypesClassLoader {
+
+        static {
+            registerAsParallelCapable();
+        }
 
         private final ProjectClassLoader projectClassLoader;
 
