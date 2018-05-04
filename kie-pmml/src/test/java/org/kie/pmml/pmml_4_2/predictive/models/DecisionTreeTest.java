28f7e2a3c61e6e2b4b1b271755c893fe9d48750f
Lance Leverich <lance.leverich@gmail.com>
2018-05-04 08:34:20 -0700

    RHDM-341_7.7.x *Removed @Ignore from DecisionTreeTest methods that were no longer in error

--- kie-pmml/src/test/java/org/kie/pmml/pmml_4_2/predictive/models/DecisionTreeTest.java
+++ kie-pmml/src/test/java/org/kie/pmml/pmml_4_2/predictive/models/DecisionTreeTest.java
@@ -209,7 +209,6 @@ public class DecisionTreeTest extends DroolsAbstractPMMLTest {
     }
 
     @Test
-    @Ignore
     public void testLastPredictionMissingValueStrategy() {
         KieBase kieBase = PMMLKieBaseUtil.createKieBaseWithPMML(TREE_LAST_CHILD_MISSING_STRATEGY);
         PMMLExecutor executor = new PMMLExecutor(kieBase);
@@ -269,7 +268,6 @@ public class DecisionTreeTest extends DroolsAbstractPMMLTest {
     }
 
     @Test
-    @Ignore
     public void testWeightedConfidenceMissingValueStrategy() {
         KieBase kieBase = PMMLKieBaseUtil.createKieBaseWithPMML(TREE_WEIGHTED_CONFIDENCE_MISSING_STRATEGY);
         PMMLExecutor executor = new PMMLExecutor(kieBase);
