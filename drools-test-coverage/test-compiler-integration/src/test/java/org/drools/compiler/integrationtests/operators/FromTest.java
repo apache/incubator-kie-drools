87c00c98fcc406cf2056f93aeb69319280a947f8
Luca Molteni <volothamp@gmail.com>
2018-05-17 16:46:24 +0200

    Enable all failing tests

--- drools-test-coverage/test-compiler-integration/src/test/java/org/drools/compiler/integrationtests/operators/FromTest.java
+++ drools-test-coverage/test-compiler-integration/src/test/java/org/drools/compiler/integrationtests/operators/FromTest.java
@@ -78,7 +78,7 @@ public class FromTest {
 
     @Parameterized.Parameters(name = "KieBase type={0}")
     public static Collection<Object[]> getParameters() {
-        return TestParametersUtil.getKieBaseCloudConfigurations(false);
+        return TestParametersUtil.getKieBaseCloudConfigurations(true);
     }
 
     public static class ListsContainer {
