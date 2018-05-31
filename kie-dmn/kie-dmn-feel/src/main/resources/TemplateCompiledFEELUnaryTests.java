308ade18e5cd34fe4b3f3f1e6d600a8bd3971095
tarilabs <matteo.mortari@gmail.com>
2018-05-31 14:44:07 +0200

    DROOLS-1701 Support for FEEL fn definition (non-external, FEEL defined)

--- kie-dmn/kie-dmn-feel/src/main/resources/TemplateCompiledFEELUnaryTests.java
+++ kie-dmn/kie-dmn-feel/src/main/resources/TemplateCompiledFEELUnaryTests.java
@@ -2,8 +2,10 @@ package org.kie.dmn.feel.codegen.feel11;
 
 import static org.kie.dmn.feel.codegen.feel11.CompiledFEELSemanticMappings.*;
 
+import org.kie.dmn.feel.codegen.feel11.CompiledCustomFEELFunction;
 import org.kie.dmn.feel.codegen.feel11.CompiledFEELExpression;
 import org.kie.dmn.feel.codegen.feel11.CompiledFEELUnaryTests;
+import org.kie.dmn.feel.codegen.feel11.CompiledFEELSupport;
 import org.kie.dmn.feel.codegen.feel11.CompiledFEELSupport;
 import org.kie.dmn.feel.lang.EvaluationContext;
 
