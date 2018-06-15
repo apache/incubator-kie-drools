773b00c0d6757adb7e0eef138a6e9af6c2a1dbe2
Tibor Zimanyi <tibor.zimanyi@gmail.com>
2018-06-15 08:32:12 +0200

    DROOLS-2634 - Migrate test classes starting with C to a new test module (#1947)
    * DROOLS-2390 Move test classes starting with C from drools-compiler
    
    * DROOLS-2390 Enable exec. model in integ. test classes starting with C

--- drools-test-coverage/test-suite/src/test/java/org/drools/testcoverage/common/model/Attribute.java
+++ drools-test-coverage/test-suite/src/test/java/org/drools/testcoverage/common/model/Attribute.java
@@ -16,27 +16,11 @@
 
 package org.drools.testcoverage.common.model;
 
-import org.kie.api.definition.type.Role;
+import java.io.Serializable;
 
-@Role(Role.Type.EVENT)
-public class StockTickEvent extends StockTick {
+public class Attribute implements Serializable {
 
-    public StockTickEvent() {
-        super();
-    }
-
-    public StockTickEvent(final long seq,
-                          final String company,
-                          final double price,
-                          final long time) {
-        super(seq, company, price, time);
-    }
-
-    public StockTickEvent(final long seq,
-                          final String company,
-                          final double price,
-                          final long time,
-                          final long duration) {
-        super(seq, company, price, time, duration);
+    public Integer getValue() {
+        return null;
     }
 }
