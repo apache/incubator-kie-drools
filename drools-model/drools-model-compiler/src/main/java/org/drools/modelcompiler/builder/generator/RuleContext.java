01168e8e8c3c9195eed616f6556e45ebc101a0d8
mariofusco <mario.fusco@gmail.com>
2018-06-16 17:36:59 +0200

    [DROOLS-2637] avoid overwriting accumulate pattern type with accumulate function type in executable model generation + add support for predicates with 5 and 6 arguments
    (cherry picked from commit 025e5546a19966f7491acd0403b67259a06799cd)

--- drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/RuleContext.java
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/RuleContext.java
@@ -161,7 +161,7 @@ public class RuleContext {
     public void addDeclaration(DeclarationSpec d) {
         // It would be probably be better to avoid putting the same declaration multiple times
         // instead of using Set semantic here
-        if(!getDeclarationById(d.getBindingId()).isPresent()) {
+        if (!getDeclarationById(d.getBindingId()).isPresent()) {
             this.declarations.add(d);
         }
     }
@@ -171,7 +171,10 @@ public class RuleContext {
         // instead of using Set semantic here
         final String bindingId = d.getBindingId();
         final Optional<DeclarationSpec> declarationById = getDeclarationById(bindingId);
-        if(declarationById.isPresent()) {
+        if (declarationById.isPresent()) {
+            if ( d.getDeclarationClass().isAssignableFrom( declarationById.get().getDeclarationClass() )) {
+                return;
+            }
             removeDeclarationById(bindingId);
         }
         this.declarations.add(d);
