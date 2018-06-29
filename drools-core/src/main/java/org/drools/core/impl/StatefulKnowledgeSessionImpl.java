4ebadf571d2b1abf0378bfa315f66152d88a9cd8
tzimanyi <tibor.zimanyi@gmail.com>
2018-06-29 10:37:11 +0200

    [findbugs] Possible double check of field (double-checked locking)

--- drools-core/src/main/java/org/drools/core/impl/StatefulKnowledgeSessionImpl.java
+++ drools-core/src/main/java/org/drools/core/impl/StatefulKnowledgeSessionImpl.java
@@ -239,7 +239,7 @@ public class StatefulKnowledgeSessionImpl extends AbstractRuntime
     // or -1 if there are operation being executed at this moment
     private AtomicLong lastIdleTimestamp;
 
-    private InternalProcessRuntime processRuntime;
+    private volatile InternalProcessRuntime processRuntime;
 
     private Map<String, Object> runtimeServices;
 
