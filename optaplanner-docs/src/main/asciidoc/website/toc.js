060d3a72fb6c96a4181e790dc8bb7ce9ce583439
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2018-07-04 09:05:01 +0200

    toc script: clean up variable names

--- optaplanner-docs/src/main/asciidoc/website/toc.js
+++ optaplanner-docs/src/main/asciidoc/website/toc.js
@@ -1,23 +1,8 @@
-/*
- * Copyright 2018 Red Hat, Inc. and/or its affiliates.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 $(document).ready(function() {
-    $('ul.sectlevel1').wrap('<div id="toctree"></div>');
+    $('ul.sectlevel1').wrap('<div id="tocTree"></div>');
 
-    $('#toctree').jstree({
+    var $tocTree = $('#tocTree');
+    $tocTree.jstree({
          "core" : {
              "themes" : {"variant" : "small", "icons" : false}
          },
@@ -25,18 +10,21 @@ $(document).ready(function() {
     })
     .on("activate_node.jstree", function (e, data) { location.href = data.node.a_attr.href; });
 
-    $('#toctree').before('<input placeholder="Search TOC" id="tocsearch" type="text">');
+    $tocTree.before('<input placeholder="Search TOC" id="tocSearch" type="text">');
 
+    var $tocSearch = $('#tocSearch');
     var searchTimeout = false;
-    $('#tocsearch').keyup(function () {
-        if(searchTimeout) { clearTimeout(searchTimeout); }
+    $tocSearch.keyup(function () {
+        if (searchTimeout) {
+            clearTimeout(searchTimeout);
+        }
         searchTimeout = setTimeout(function () {
-            var v = $('#tocsearch').val();
-            $('#toctree').jstree(true).search(v);
+            var v = $('#tocSearch').val();
+            $('#tocTree').jstree(true).search(v);
         }, 250);
     });
 
-    $('#tocsearch').after('<a href="#" id="toctreeexpand" title="Expand All"><i class="fa fa-plus-square" aria-hidden="true"></i></a><a href="#" id="toctreecollapse" title="Collapse All"><i class="fa fa-minus-square" aria-hidden="true"></i></a>');
-    $('#toctreeexpand').click(function() { $('#toctree').jstree('open_all'); });
-    $('#toctreecollapse').click(function() { $('#toctree').jstree('close_all'); });
+    $tocSearch.after('<a href="#" id="tocTreeExpandAll" title="Expand All"><i class="fa fa-plus-square" aria-hidden="true"></i></a><a href="#" id="tocTreeCollapseAll" title="Collapse All"><i class="fa fa-minus-square" aria-hidden="true"></i></a>');
+    $('#tocTreeExpandAll').click(function() { $('#tocTree').jstree('open_all'); });
+    $('#tocTreeCollapseAll').click(function() { $('#tocTree').jstree('close_all'); });
 });
