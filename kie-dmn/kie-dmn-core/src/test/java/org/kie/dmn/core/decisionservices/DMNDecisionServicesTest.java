5527327db10b0b57a5ecf23826c96703ac524e14
tarilabs <matteo.mortari@gmail.com>
2018-07-16 17:47:55 +0200

    Additional tests

--- kie-dmn/kie-dmn-core/src/test/java/org/kie/dmn/core/decisionservices/DMNDecisionServicesTest.java
+++ kie-dmn/kie-dmn-core/src/test/java/org/kie/dmn/core/decisionservices/DMNDecisionServicesTest.java
@@ -16,6 +16,9 @@
 
 package org.kie.dmn.core.decisionservices;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
 import org.junit.Test;
 import org.kie.dmn.api.core.DMNContext;
 import org.kie.dmn.api.core.DMNModel;
@@ -30,6 +33,9 @@ import org.slf4j.LoggerFactory;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 public class DMNDecisionServicesTest {
@@ -171,5 +177,98 @@ public class DMNDecisionServicesTest {
 
         DMNContext result = dmnResult.getContext();
         assertThat(result.get("Decide based on A and DS"), is("demn"));
+    }
+
+    @Test
+    public void testMixtypeDS() {
+        DMNRuntime runtime = DMNRuntimeUtil.createRuntime("mixtype-DS.dmn", this.getClass());
+        DMNModel dmnModel = runtime.getModel("http://www.trisotech.com/dmn/definitions/_c9885563-aa54-4c7b-ae8a-738cfd29b544", "mixtype DS");
+        assertThat(dmnModel, notNullValue());
+        assertThat(DMNRuntimeUtil.formatMessages(dmnModel.getMessages()), dmnModel.hasErrors(), is(false));
+
+        DMNContext context = DMNFactory.newContext();
+        context.set("Person name", "John");
+        context.set("Person year of birth", BigDecimal.valueOf(1980));
+
+        DMNResult dmnResult = runtime.evaluateAll(dmnModel, context);
+        LOG.debug("{}", dmnResult);
+        dmnResult.getDecisionResults().forEach(x -> LOG.debug("{}", x));
+        assertThat(DMNRuntimeUtil.formatMessages(dmnResult.getMessages()), dmnResult.hasErrors(), is(false));
+
+        DMNContext result = dmnResult.getContext();
+        assertThat(result.get("Greet the Person"), is("Hello, John"));
+        assertThat(result.get("Person age"), is(BigDecimal.valueOf(38)));
+        assertThat(result.get("is Person an adult"), is(true));
+
+        assertThat((Map<String, Object>) result.get("eval DS all"), hasEntry(is("Greet the Person"), is("Hello, ds all")));
+        assertThat((Map<String, Object>) result.get("eval DS all"), hasEntry(is("Person age"), is(BigDecimal.valueOf(18))));
+        assertThat((Map<String, Object>) result.get("eval DS all"), hasEntry(is("is Person an adult"), is(true)));
+        assertThat((Map<String, Object>) result.get("eval DS all"), not(hasEntry(is("hardcoded now"), anything())));
+
+        assertThat((Map<String, Object>) result.get("eval DS encapsulate"), hasEntry(is("Greet the Person"), is("Hello, DS encapsulate")));
+        assertThat((Map<String, Object>) result.get("eval DS encapsulate"), not(hasEntry(is("Person age"), anything())));
+        assertThat((Map<String, Object>) result.get("eval DS encapsulate"), hasEntry(is("is Person an adult"), is(true)));
+        assertThat((Map<String, Object>) result.get("eval DS encapsulate"), not(hasEntry(is("hardcoded now"), anything())));
+
+        assertThat((Map<String, Object>) result.get("eval DS greet adult"), hasEntry(is("Greet the Person"), is("Hello, DS greet adult")));
+        assertThat((Map<String, Object>) result.get("eval DS greet adult"), not(hasEntry(is("Person age"), anything())));
+        assertThat((Map<String, Object>) result.get("eval DS greet adult"), hasEntry(is("is Person an adult"), is(true)));
+        assertThat((Map<String, Object>) result.get("eval DS greet adult"), not(hasEntry(is("hardcoded now"), anything())));
+
+        // additionally check DS one-by-one
+        testMixtypeDS_checkDSall(runtime, dmnModel);
+        testMixtypeDS_checkDSencapsulate(runtime, dmnModel);
+        testMixtypeDS_checkDSgreetadult(runtime, dmnModel);
+    }
+
+    private void testMixtypeDS_checkDSall(DMNRuntime runtime, DMNModel dmnModel) {
+        DMNContext context = DMNFactory.newContext();
+        context.set("Person name", "John");
+        context.set("Person year of birth", BigDecimal.valueOf(2008));
+
+        DMNResult dmnResult = ((DMNRuntimeImpl) runtime).evaluateDecisionService(dmnModel, context, "DS all");
+        LOG.debug("{}", dmnResult);
+        dmnResult.getDecisionResults().forEach(x -> LOG.debug("{}", x));
+        assertThat(DMNRuntimeUtil.formatMessages(dmnResult.getMessages()), dmnResult.hasErrors(), is(false));
+
+        DMNContext result = dmnResult.getContext();
+        assertThat(result.getAll(), hasEntry(is("Greet the Person"), is("Hello, John")));
+        assertThat(result.getAll(), hasEntry(is("Person age"), is(BigDecimal.valueOf(10))));
+        assertThat(result.getAll(), hasEntry(is("is Person an adult"), is(false)));
+        assertThat(result.getAll(), not(hasEntry(is("hardcoded now"), anything())));
+    }
+
+    private void testMixtypeDS_checkDSencapsulate(DMNRuntime runtime, DMNModel dmnModel) {
+        DMNContext context = DMNFactory.newContext();
+        context.set("Person name", "John");
+        context.set("Person year of birth", BigDecimal.valueOf(2008));
+
+        DMNResult dmnResult = ((DMNRuntimeImpl) runtime).evaluateDecisionService(dmnModel, context, "DS encapsulate");
+        LOG.debug("{}", dmnResult);
+        dmnResult.getDecisionResults().forEach(x -> LOG.debug("{}", x));
+        assertThat(DMNRuntimeUtil.formatMessages(dmnResult.getMessages()), dmnResult.hasErrors(), is(false));
+
+        DMNContext result = dmnResult.getContext();
+        assertThat(result.getAll(), hasEntry(is("Greet the Person"), is("Hello, John")));
+        assertThat(result.getAll(), not(hasEntry(is("Person age"), anything())));
+        assertThat(result.getAll(), hasEntry(is("is Person an adult"), is(false)));
+        assertThat(result.getAll(), not(hasEntry(is("hardcoded now"), anything())));
+    }
+
+    private void testMixtypeDS_checkDSgreetadult(DMNRuntime runtime, DMNModel dmnModel) {
+        DMNContext context = DMNFactory.newContext();
+        context.set("Person name", "John");
+        context.set("Person age", BigDecimal.valueOf(10));
+
+        DMNResult dmnResult = ((DMNRuntimeImpl) runtime).evaluateDecisionService(dmnModel, context, "DS greet adult");
+        LOG.debug("{}", dmnResult);
+        dmnResult.getDecisionResults().forEach(x -> LOG.debug("{}", x));
+        assertThat(DMNRuntimeUtil.formatMessages(dmnResult.getMessages()), dmnResult.hasErrors(), is(false));
+
+        DMNContext result = dmnResult.getContext();
+        assertThat(result.getAll(), hasEntry(is("Greet the Person"), is("Hello, John")));
+        assertThat(dmnResult.getDecisionResultByName("Person age"), nullValue());
+        assertThat(result.getAll(), hasEntry(is("is Person an adult"), is(false)));
+        assertThat(result.getAll(), not(hasEntry(is("hardcoded now"), anything())));
     }
 }
