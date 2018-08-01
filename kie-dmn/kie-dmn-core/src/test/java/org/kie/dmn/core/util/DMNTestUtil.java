68aa62cff10cf721d79fc7bdb91b2952fef775eb
Matteo Mortari <matteo.mortari@gmail.com>
2018-08-01 09:54:52 +0200

    DROOLS-2791 Test DMN Import with 3 layers of antipattern of importing .. (#1995)
    * DROOLS-2791 Test DMN Import with 3 layers of antipattern of importing ..
    
    Decision and InputData.
    
    Test DMN Import transitive 3 level diamond
    
    * Implementing code review comment

---
+++ kie-dmn/kie-dmn-core/src/test/java/org/kie/dmn/core/util/DMNTestUtil.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.dmn.core.util;
+
+import org.kie.dmn.api.core.DMNModel;
+import org.kie.dmn.api.core.DMNRuntime;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class DMNTestUtil {
+
+    private DMNTestUtil() {
+        // No constructor for util class.
+    }
+
+    public static DMNModel getAndAssertModelNoErrors(final DMNRuntime runtime, final String namespace, final String modelName) {
+        DMNModel dmnModel = runtime.getModel(namespace, modelName);
+        assertThat(dmnModel, notNullValue());
+        assertThat(DMNRuntimeUtil.formatMessages(dmnModel.getMessages()), dmnModel.hasErrors(), is(false));
+        return dmnModel;
+    }
+}
