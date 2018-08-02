74a6163b135cfb8d0efd111a975c5e92fd248828
Mario Fusco <mario.fusco@gmail.com>
2018-08-02 17:45:26 +0200

    [DROOLS-2634] add support for custom operators in executable model

---
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/operatorspec/NativeOperatorSpec.java
@@ -0,0 +1,63 @@
+package org.drools.modelcompiler.builder.generator.operatorspec;
+
+import java.util.Optional;
+
+import org.drools.javaparser.ast.drlx.expr.PointFreeExpr;
+import org.drools.javaparser.ast.expr.Expression;
+import org.drools.javaparser.ast.expr.MethodCallExpr;
+import org.drools.javaparser.ast.expr.StringLiteralExpr;
+import org.drools.javaparser.ast.expr.UnaryExpr;
+import org.drools.model.functions.Operator;
+import org.drools.modelcompiler.builder.generator.RuleContext;
+import org.drools.modelcompiler.builder.generator.TypedExpression;
+import org.drools.modelcompiler.builder.generator.drlxparse.CoercedExpression;
+import org.drools.modelcompiler.builder.generator.expressiontyper.ExpressionTyper;
+
+import static org.drools.modelcompiler.builder.generator.DslMethodNames.EVAL_CALL;
+
+public class NativeOperatorSpec implements OperatorSpec {
+    public static final NativeOperatorSpec INSTANCE = new NativeOperatorSpec();
+
+    public Expression getExpression(RuleContext context, PointFreeExpr pointFreeExpr, TypedExpression left, ExpressionTyper expressionTyper) {
+        MethodCallExpr methodCallExpr = new MethodCallExpr( null, EVAL_CALL );
+
+        String opName = pointFreeExpr.getOperator().asString();
+        Operator operator = addOperatorArgument( context, methodCallExpr, opName );
+
+        methodCallExpr.addArgument( left.getExpression() );
+        for (Expression rightExpr : pointFreeExpr.getRight()) {
+            final Optional<TypedExpression> optionalRight = expressionTyper.toTypedExpression(rightExpr).getTypedExpression();
+            optionalRight.ifPresent( right -> {
+                final TypedExpression coercedRight;
+                if (operator != null && operator.requiresCoercion()) {
+                    final CoercedExpression.CoercedExpressionResult coerce = new CoercedExpression(left, right).coerce();
+                    coercedRight = coerce.getCoercedRight();
+                } else {
+                    coercedRight = right;
+                }
+                methodCallExpr.addArgument(coercedRight.getExpression() );
+            });
+        }
+
+        return pointFreeExpr.isNegated() ?
+                new UnaryExpr( methodCallExpr, UnaryExpr.Operator.LOGICAL_COMPLEMENT ) :
+                methodCallExpr;
+    }
+
+    protected Operator addOperatorArgument( RuleContext context, MethodCallExpr methodCallExpr, String opName ) {
+        Operator operator = Operator.Register.getOperator( opName );
+        try {
+            // if the operator has an INSTANCE field avoid the operator lookup at runtime
+            operator.getClass().getField( "INSTANCE" );
+            methodCallExpr.addArgument( operator.getClass().getCanonicalName() + ".INSTANCE" );
+        } catch (NoSuchFieldException e) {
+            methodCallExpr.addArgument( new StringLiteralExpr( opName ) );
+        }
+        return operator;
+    }
+
+    @Override
+    public boolean isStatic() {
+        return false;
+    }
+}
