2bb3eb28b491a1aee24d99714e9c01619da0ce2a
tarilabs <matteo.mortari@gmail.com>
2018-08-03 13:45:29 +0200

    (working OK) Finalizations

--- kie-dmn/kie-dmn-validation/src/test/java/org/kie/dmn/validation/ValidatorImportTest.java
+++ kie-dmn/kie-dmn-validation/src/test/java/org/kie/dmn/validation/ValidatorImportTest.java
@@ -25,11 +25,13 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.kie.dmn.api.core.DMNContext;
 import org.kie.dmn.api.core.DMNMessage;
+import org.kie.dmn.api.core.DMNMessageType;
 import org.kie.dmn.api.core.DMNModel;
 import org.kie.dmn.api.core.DMNResult;
 import org.kie.dmn.api.core.DMNRuntime;
 import org.kie.dmn.core.api.DMNFactory;
 import org.kie.dmn.core.util.DMNRuntimeUtil;
+import org.kie.dmn.model.v1_1.DMNElementReference;
 import org.kie.dmn.validation.DMNValidator.Validation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,6 +39,7 @@ import org.slf4j.LoggerFactory;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.kie.dmn.core.util.DynamicTypeUtils.entry;
 import static org.kie.dmn.core.util.DynamicTypeUtils.mapOf;
 
@@ -63,35 +66,99 @@ public class ValidatorImportTest extends AbstractValidatorTest {
     public void testBaseModel_OK__ReaderInput() throws IOException {
         try (final Reader reader0 = getReader("import/Base-model.dmn");
                 final Reader reader1 = getReader("import/Import-base-model.dmn");) {
-            final List<DMNMessage> messages = validator.validateUsing( // VALIDATE_SCHEMA, disabled, due to QName use not compliant. 
-                                                                       Validation.VALIDATE_MODEL,
-                                                                       Validation.VALIDATE_COMPILATION)
+            final List<DMNMessage> messages = validator.validateUsing(// VALIDATE_SCHEMA, disabled, due to QName use not compliant. 
+                                                                      Validation.VALIDATE_MODEL,
+                                                                      Validation.VALIDATE_COMPILATION)
                                                        .theseModels(reader0, reader1);
-            assertThat( messages.toString(), messages.size(), is( 0 ) );
+            assertThat(ValidatorUtil.formatMessages(messages), messages.size(), is(0));
         }
     }
 
     @Test
     public void testBaseModel_OK__FileInput() throws IOException {
-        final List<DMNMessage> messages = validator.validateUsing( // VALIDATE_SCHEMA, disabled, due to QName use not compliant. 
-                                                                   Validation.VALIDATE_MODEL,
-                                                                   Validation.VALIDATE_COMPILATION)
+        final List<DMNMessage> messages = validator.validateUsing(// VALIDATE_SCHEMA, disabled, due to QName use not compliant. 
+                                                                  Validation.VALIDATE_MODEL,
+                                                                  Validation.VALIDATE_COMPILATION)
                                                    .theseModels(getFile("import/Base-model.dmn"),
                                                                 getFile("import/Import-base-model.dmn"));
-        assertThat(messages.toString(), messages.size(), is(0));
+        assertThat(ValidatorUtil.formatMessages(messages), messages.size(), is(0));
     }
 
     @Test
     public void testBaseModel_OK__DefinitionsInput() throws IOException {
-        final List<DMNMessage> messages = validator.validateUsing( // VALIDATE_SCHEMA, disabled, due to QName use not compliant. 
-                                                                   Validation.VALIDATE_MODEL,
-                                                                   Validation.VALIDATE_COMPILATION)
+        final List<DMNMessage> messages = validator.validateUsing(// VALIDATE_SCHEMA, disabled, due to QName use not compliant. 
+                                                                  Validation.VALIDATE_MODEL,
+                                                                  Validation.VALIDATE_COMPILATION)
                                                    .theseModels(getDefinitions(Arrays.asList("import/Base-model.dmn", "import/Import-base-model.dmn"),
                                                                                "http://www.trisotech.com/definitions/_70df1ad5-2a33-4ede-b8b2-869988ac1d30",
                                                                                "Base model"),
                                                                 getDefinitions(Arrays.asList("import/Base-model.dmn", "import/Import-base-model.dmn"),
                                                                                "http://www.trisotech.com/dmn/definitions/_b33fa7d9-f501-423b-afa8-15ded7e7f493",
                                                                                "Import base model"));
-        assertThat(messages.toString(), messages.size(), is(0));
+        assertThat(ValidatorUtil.formatMessages(messages), messages.size(), is(0));
+    }
+
+    @Test
+    public void testWrongImportBaseModel__ReaderInput() throws IOException {
+        try (final Reader reader0 = getReader("import/Base-model.dmn");
+                final Reader reader1 = getReader("import/Wrong-Import-base-model.dmn");) {
+            final List<DMNMessage> messages = validator.validateUsing(Validation.VALIDATE_MODEL)
+                                                       .theseModels(reader0, reader1);
+            assertThat(ValidatorUtil.formatMessages(messages), messages.size(), is(1));
+            assertTrue(messages.stream().anyMatch(p -> p.getMessageType().equals(DMNMessageType.REQ_NOT_FOUND) &&
+                                                       p.getSourceReference() instanceof DMNElementReference &&
+                                                       ((DMNElementReference) p.getSourceReference()).getHref()
+                                                                                                     .equals("http://www.trisotech.com/definitions/_70df1ad5-2a33-4ede-b8b2-869988ac1d30#_1d52934e-aa4e-47c9-a011-fc989d795664")));
+        }
+    }
+    
+    @Test
+    public void testWrongImportBaseModel__FileInput() throws IOException {
+        final List<DMNMessage> messages = validator.validateUsing(Validation.VALIDATE_MODEL)
+                                                   .theseModels(getFile("import/Base-model.dmn"),
+                                                                getFile("import/Wrong-Import-base-model.dmn"));
+        assertThat(ValidatorUtil.formatMessages(messages), messages.size(), is(1));
+        assertTrue(messages.stream().anyMatch(p -> p.getMessageType().equals(DMNMessageType.REQ_NOT_FOUND) &&
+                                                   p.getSourceReference() instanceof DMNElementReference &&
+                                                   ((DMNElementReference) p.getSourceReference()).getHref()
+                                                                                                 .equals("http://www.trisotech.com/definitions/_70df1ad5-2a33-4ede-b8b2-869988ac1d30#_1d52934e-aa4e-47c9-a011-fc989d795664")));
+    }
+
+    @Test
+    public void testWrongImportBaseModel__DefinitionsInput() throws IOException {
+        final List<DMNMessage> messages = validator.validateUsing(Validation.VALIDATE_MODEL)
+                                                   .theseModels(getDefinitions(Arrays.asList("import/Base-model.dmn", "import/Wrong-Import-base-model.dmn"),
+                                                                               "http://www.trisotech.com/definitions/_70df1ad5-2a33-4ede-b8b2-869988ac1d30",
+                                                                               "Base model"),
+                                                                getDefinitions(Arrays.asList("import/Base-model.dmn", "import/Wrong-Import-base-model.dmn"),
+                                                                               "http://www.trisotech.com/dmn/definitions/_719a2325-5cac-47ea-8a99-665c01d570a5",
+                                                                               "Wrong Import base model"));
+        assertTrue(messages.stream().anyMatch(p -> p.getMessageType().equals(DMNMessageType.REQ_NOT_FOUND) &&
+                                                   p.getSourceReference() instanceof DMNElementReference &&
+                                                   ((DMNElementReference) p.getSourceReference()).getHref()
+                                                                                                 .equals("http://www.trisotech.com/definitions/_70df1ad5-2a33-4ede-b8b2-869988ac1d30#_1d52934e-aa4e-47c9-a011-fc989d795664")));
+    }
+
+    @Test
+    public void testOnlyImportBaseModel_ReaderInput() throws IOException {
+        try (final Reader reader1 = getReader("import/Only-Import-base-model.dmn");) {
+            final List<DMNMessage> messages = validator.validate(reader1, Validation.VALIDATE_MODEL);
+            assertTrue(messages.stream().anyMatch(p -> p.getMessageType().equals(DMNMessageType.IMPORT_NOT_FOUND)));
+        }
+    }
+
+    @Test
+    public void testOnlyImportBaseModel_FileInput() throws IOException {
+        final List<DMNMessage> messages = validator.validate(getFile("import/Only-Import-base-model.dmn"), Validation.VALIDATE_MODEL);
+        assertTrue(messages.stream().anyMatch(p -> p.getMessageType().equals(DMNMessageType.IMPORT_NOT_FOUND)));
+    }
+
+    @Test
+    public void testOnlyImportBaseModel_DefinitionsInput() throws IOException {
+        final List<DMNMessage> messages = validator.validate(getDefinitions("import/Only-Import-base-model.dmn",
+                                                                            "http://www.trisotech.com/dmn/definitions/_a9bfa4de-cf5c-4b2f-9011-ab576e00b162",
+                                                                            "Only Import base model"),
+                                                             Validation.VALIDATE_MODEL);
+        assertTrue(messages.stream().anyMatch(p -> p.getMessageType().equals(DMNMessageType.IMPORT_NOT_FOUND)));
     }
 }
