5af788f20c9f3ae2b5ead16d074496457958f71e
Mario Fusco <mario.fusco@gmail.com>
2018-08-09 10:59:32 +0200

    [DROOLS-2871] remove manners example

--- drools-examples/src/main/java/org/drools/benchmark/DroolsBenchmarkExamplesApp.java
+++ drools-examples/src/main/java/org/drools/benchmark/DroolsBenchmarkExamplesApp.java
@@ -16,19 +16,13 @@
 
 package org.drools.benchmark;
 
-import org.drools.benchmark.manners.MannersBenchmark;
-import org.drools.benchmark.waltz.WaltzBenchmark;
-import org.drools.benchmark.waltzdb.WaltzDbBenchmark;
-
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
-/**
- * Warning: Manners benchmark is working (producing the correct result), but it performs a
- * breadth first search instead of a depth first one as it could be expected to do.
- * At the moment it is not possible to specify the type of tree search in drools.
- */
+import javax.swing.*;
+import org.drools.benchmark.waltz.WaltzBenchmark;
+import org.drools.benchmark.waltzdb.WaltzDbBenchmark;
+
 public class DroolsBenchmarkExamplesApp extends JFrame {
 
     public static void main(String[] args) {
@@ -46,11 +40,6 @@ public class DroolsBenchmarkExamplesApp extends JFrame {
     private Container createContentPane() {
         JPanel contentPane = new JPanel(new GridLayout(0, 1));
         contentPane.add(new JLabel("Which benchmark do you want to see in the output?"));
-        contentPane.add(new JButton(new AbstractAction("MannersBenchmark") {
-            public void actionPerformed(ActionEvent e) {
-                MannersBenchmark.main(new String[0]);
-            }
-        }));
         contentPane.add(new JButton(new AbstractAction("WaltzBenchmark") {
             public void actionPerformed(ActionEvent e) {
                 WaltzBenchmark.main(new String[0]);
