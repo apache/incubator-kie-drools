f72108707c5c422cabbd9618840912e9c2e23142
tarilabs <matteo.mortari@gmail.com>
2018-08-22 13:02:29 +0200

    .

--- kie-dmn/kie-dmn-backend/src/main/java/org/kie/dmn/backend/marshalling/v1_2/xstream/FormatUtils.java
+++ kie-dmn/kie-dmn-backend/src/main/java/org/kie/dmn/backend/marshalling/v1_2/xstream/FormatUtils.java
@@ -23,7 +23,7 @@ public class FormatUtils {
     public static String manageDouble(Double d) {
         Objects.requireNonNull(d);
         long longValue = d.longValue();
-        if (d != null && d == longValue) {
+        if (d == longValue) {
             return String.format("%d", longValue);
         } else {
             return String.format("%s", d);
