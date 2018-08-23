d9d27f6a859176e356ac6af9264929caaf338341
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2018-08-23 16:18:57 +0200

    Move drools load balance functions to common package so others can reuse them

--- optaplanner-examples/src/main/java/org/optaplanner/examples/common/solver/drools/functions/LoadBalanceByCountAccumulateFunction.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/common/solver/drools/functions/LoadBalanceByCountAccumulateFunction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.optaplanner.examples.tennis.solver.drools.functions;
+package org.optaplanner.examples.common.solver.drools.functions;
 
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
