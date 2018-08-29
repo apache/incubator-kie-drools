1d42a25c8738d66f4fd6711c6257229dd4f34d83
Luca Molteni <volothamp@gmail.com>
2018-08-29 16:37:27 +0200

    Refactor test

--- drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/builder/generator/expressiontyper/FlattenScopeTest.java
+++ drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/builder/generator/expressiontyper/FlattenScopeTest.java
@@ -1,14 +1,15 @@
 package org.drools.modelcompiler.builder.generator.expressiontyper;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.drools.javaparser.ast.Node;
 import org.drools.javaparser.ast.expr.Expression;
+import org.drools.javaparser.ast.expr.NameExpr;
 import org.drools.javaparser.ast.expr.SimpleName;
 import org.drools.modelcompiler.builder.generator.DrlxParseUtil;
 import org.junit.Test;
 
+import static java.util.Arrays.asList;
 import static org.drools.modelcompiler.builder.generator.expressiontyper.FlattenScope.flattenScope;
 import static org.junit.Assert.*;
 
@@ -18,13 +19,12 @@ public class FlattenScopeTest {
     @Test
     public void flattenFields() {
         List<Node> actual = flattenScope(expr("Field.INT"));
-        List<Node> expected = Arrays.asList(expr("Field"), new SimpleName("INT"));
-
+        List<Node> expected = asList(new NameExpr("Field"), new SimpleName("INT"));
 
         assertArrayEquals(expected.toArray(), actual.toArray());
     }
 
-    private Expression expr(String s) {
-        return DrlxParseUtil.parseExpression(s).getExpr();
+    private Expression expr(String inputExpr) {
+        return DrlxParseUtil.parseExpression(inputExpr).getExpr();
     }
 }
