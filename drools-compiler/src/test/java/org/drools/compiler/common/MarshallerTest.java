42832a5e64daabbce7c0d6b67b9bebacb1c6fac0
Luca Molteni <volothamp@gmail.com>
2018-09-12 12:32:25 +0200

    Added test for deserialization

---
+++ drools-compiler/src/test/java/org/drools/compiler/common/MarshallerTest.java
@@ -0,0 +1,31 @@
+package org.drools.compiler.common;
+
+import org.drools.compiler.integrationtests.SerializationHelper;
+import org.junit.Test;
+import org.kie.api.KieBase;
+import org.kie.api.io.ResourceType;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.utils.KieHelper;
+
+import static org.junit.Assert.assertEquals;
+
+public class MarshallerTest {
+
+    @Test
+    public void testAgendaReconciliation() throws Exception {
+        String str =
+                "import java.util.Collection\n" +
+                        "rule R1 when\n" +
+                        "    String() from [ \"x\", \"y\", \"z\" ]\n" +
+                        "then\n" +
+                        "end\n";
+
+        KieBase kbase = new KieHelper().addContent(str, ResourceType.DRL ).build();
+        KieSession ksession = kbase.newKieSession();
+        assertEquals( 3, ksession.fireAllRules() );
+
+        ksession = SerializationHelper.getSerialisedStatefulKnowledgeSession(ksession, true );
+
+        assertEquals( 0, ksession.fireAllRules() );
+    }
+}
