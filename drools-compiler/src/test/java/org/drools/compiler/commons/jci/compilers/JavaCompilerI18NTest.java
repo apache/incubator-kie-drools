760e4e9dbdc454b9f20ef88c394c0f0371f8227e
Enrique Gonzalez <egonzale@redhat.com>
2018-09-19 09:27:19 +0200

    [RHPAM-1184] URLDecoder: Illegal hex characters in escape (%) pattern" when a rule has a name with "%" (#2072)
    moving decoding the file name to the EJC compilation unit adapter so memory file system
    is not inconsistent anymore regarding the names

---
+++ drools-compiler/src/test/java/org/drools/compiler/commons/jci/compilers/JavaCompilerI18NTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.drools.compiler.commons.jci.compilers;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.drools.compiler.compiler.io.memory.MemoryFile;
+import org.drools.compiler.compiler.io.memory.MemoryFileSystem;
+import org.junit.Test;
+
+public class JavaCompilerI18NTest {
+
+    @Test
+    public void testi18NFile () throws Exception {
+        String fileStr = "com/myspace/test/" + URLEncoder.encode("あ", "UTF-8") + ".java";
+        List<String> classes = new ArrayList<>();
+        classes.add(fileStr);
+
+        MemoryFileSystem fs = new MemoryFileSystem();
+        MemoryFile file = (MemoryFile) fs.getFile(fileStr);
+
+        String fileContents = "package com.myspace.test; public class あ { }";
+        fs.setFileContents(file, fileContents.getBytes());
+
+
+        EclipseJavaCompilerSettings settings = new EclipseJavaCompilerSettings();
+        settings.setSourceVersion( "1.5" );
+        settings.setTargetVersion( "1.5" );
+        EclipseJavaCompiler compiler = new EclipseJavaCompiler( settings, "" );
+        CompilationResult res = compiler.compile( classes.toArray( new String[classes.size()] ), fs, fs );
+        assertEquals(res.getErrors().length, 0);
+	}
+}
