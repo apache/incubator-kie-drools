67407e9746f793872064f5d410a906ee4e2f691c
Luca Molteni <volothamp@gmail.com>
2018-09-20 14:51:57 +0200

    New test for unfired tuples

--- drools-compiler/src/test/java/org/drools/compiler/common/MarshallerTest.java
+++ drools-compiler/src/test/java/org/drools/compiler/common/MarshallerTest.java
@@ -30,22 +30,19 @@ public class MarshallerTest {
     }
 
     @Test
-    public void testAgendaReconciliation2() throws Exception {
+    public void testAgendaReconciliation3() throws Exception {
         String str =
                 "import java.util.Collection\n" +
                         "rule R1 when\n" +
-                        "    String(this == \"ciao\") \n" +
+                        "    String() from [ \"x\", \"y\", \"z\" ]\n" +
                         "then\n" +
                         "end\n";
 
         KieBase kbase = new KieHelper().addContent(str, ResourceType.DRL ).build();
         KieSession ksession = kbase.newKieSession();
-        ksession.insert("prova");
-        ksession.insert("ciao");
-        assertEquals( 1, ksession.fireAllRules() );
 
         ksession = SerializationHelper.getSerialisedStatefulKnowledgeSession(ksession, true );
 
-        assertEquals( 0, ksession.fireAllRules() );
+        assertEquals( 3, ksession.fireAllRules() );
     }
 }
