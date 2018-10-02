5ed82489d6d7a82216080efb6d5a10d1f11b7cd3
Luca Molteni <volothamp@gmail.com>
2018-10-02 17:11:48 +0200

    JavaSerializableResolverStrategy

---
+++ drools-core/src/main/java/org/drools/core/marshalling/impl/JavaSerializableResolverStrategy.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2010 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.core.marshalling.impl;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.kie.api.marshalling.ObjectMarshallingStrategy;
+import org.kie.api.marshalling.ObjectMarshallingStrategyAcceptor;
+
+public class JavaSerializableResolverStrategy
+        implements
+        ObjectMarshallingStrategy {
+
+    private ObjectMarshallingStrategyAcceptor acceptor;
+
+    public JavaSerializableResolverStrategy(ObjectMarshallingStrategyAcceptor acceptor) {
+        this.acceptor = acceptor;
+    }
+
+    public Object read(ObjectInputStream os) throws IOException,
+            ClassNotFoundException {
+        return os.readObject();
+    }
+
+    public void write(ObjectOutputStream os,
+                      Object object) throws IOException {
+        os.writeObject(object);
+    }
+
+    public boolean accept(Object object) {
+        return this.acceptor.accept(object);
+    }
+
+    public byte[] marshal(Context context,
+                          ObjectOutputStream os,
+                          Object object) {
+        try (ByteArrayOutputStream bs = new ByteArrayOutputStream()) {
+            os = new ObjectOutputStream(bs);
+            write(os, object);
+            return bs.toByteArray();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Object unmarshal(Context context,
+                            ObjectInputStream is,
+                            byte[] object,
+                            ClassLoader classloader) {
+        try (ByteArrayInputStream bs = new ByteArrayInputStream(object)) {
+            is = new ObjectInputStream(bs);
+            return read(is);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Context createContext() {
+        // no need for context
+        return null;
+    }
+}
