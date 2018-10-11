19ad8351d19b147d1fc14225fe1fb3e857f03a7b
mariofusco <mario.fusco@gmail.com>
2018-10-11 16:05:21 +0200

    [DROOLS-3076] add javadoc for KieSession pool

--- kie-api/src/main/java/org/kie/api/runtime/KieSessionsPool.java
+++ kie-api/src/main/java/org/kie/api/runtime/KieSessionsPool.java
@@ -16,17 +16,49 @@
 
 package org.kie.api.runtime;
 
+import org.kie.api.command.Command;
+
 /**
  * A pool of session created from a KieContainer
  */
 public interface KieSessionsPool {
 
+    /**
+     * Obtain a {@link KieSession} from this pool using the default session configuration.
+     * Calling {@link KieSession#dispose()} on this session when you are done will push it back into the pool.
+     *
+     * @return created {@link KieSession}
+     */
     KieSession newKieSession();
 
+    /**
+     * Obtain a {@link KieSession} from this pool using using the given session configuration.
+     * Calling {@link KieSession#dispose()} on this session when you are done will push it back into the pool.
+     *
+     * @return created {@link KieSession}
+     */
     KieSession newKieSession(KieSessionConfiguration conf);
 
+    /**
+     * Obtain a {@link StatelessKieSession} from this pool using the default session configuration.
+     * You do not need to call @{link #dispose()} on this.
+     * Note that, what is pooled here is not {@link StatelessKieSession} but the {@link KieSession} that it internally
+     * wraps, so calling multiple times {@link KieSession#execute(Command)} ()} (or one of its overload) will
+     * make this {@link StatelessKieSession} to get a {@link KieSession} from the pool instead of creating a new one.
+     *
+     * @return created {@link StatelessKieSession}
+     */
     StatelessKieSession newStatelessKieSession();
 
+    /**
+     * Obtain a {@link StatelessKieSession} from this pool using using the given session configuration.
+     * You do not need to call @{link #dispose()} on this.
+     * Note that, what is pooled here is not {@link StatelessKieSession} but the {@link KieSession} that it internally
+     * wraps, so calling multiple times {@link KieSession#execute(Command)} ()} (or one of its overload) will
+     * make this {@link StatelessKieSession} to get a {@link KieSession} from the pool instead of creating a new one.
+     *
+     * @return created {@link StatelessKieSession}
+     */
     StatelessKieSession newStatelessKieSession( KieSessionConfiguration conf );
 
     /**
