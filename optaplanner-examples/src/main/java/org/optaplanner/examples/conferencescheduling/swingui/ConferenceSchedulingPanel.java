91eb28a2c48880ec46f1d936898f1bbf91d10cb3
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2018-11-20 09:59:41 +0100

    While importing CFP, show a waiting dialog

--- optaplanner-examples/src/main/java/org/optaplanner/examples/conferencescheduling/swingui/ConferenceSchedulingPanel.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/conferencescheduling/swingui/ConferenceSchedulingPanel.java
@@ -17,20 +17,29 @@
 package org.optaplanner.examples.conferencescheduling.swingui;
 
 import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.Desktop;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.SwingWorker;
+import javax.swing.WindowConstants;
 
 import org.optaplanner.examples.common.swingui.SolutionPanel;
 import org.optaplanner.examples.conferencescheduling.domain.ConferenceSolution;
@@ -56,8 +65,7 @@ public class ConferenceSchedulingPanel extends SolutionPanel<ConferenceSolution>
             int option = JOptionPane.showConfirmDialog(this, dialogue, "Import", JOptionPane.OK_CANCEL_OPTION);
             if (option == JOptionPane.OK_OPTION) {
                 String conferenceBaseUrl = cfpRestUrlTextField.getText();
-                new ImporterWorker(conferenceBaseUrl).execute();
-                JOptionPane.showMessageDialog(this, "Importing CFP data in progress ...");
+                new ImporterWorker(conferenceBaseUrl).executeAndShowDialog();
             }
         });
 
@@ -111,29 +119,60 @@ public class ConferenceSchedulingPanel extends SolutionPanel<ConferenceSolution>
 
     private class ImporterWorker extends SwingWorker<ConferenceSolution, Void> {
 
-        private String conferenceBaseUrl;
+        private final String conferenceBaseUrl;
+        private final JDialog dialog;
 
         public ImporterWorker(String conferenceBaseUrl) {
             this.conferenceBaseUrl = conferenceBaseUrl;
+            dialog = new JDialog(ConferenceSchedulingPanel.this.getSolverAndPersistenceFrame(), true);
+            JPanel contentPane = new JPanel(new BorderLayout(10, 10));
+            contentPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+            contentPane.add(new JLabel("Importing CFP data in progress..."), BorderLayout.NORTH);
+            JProgressBar progressBar = new JProgressBar(SwingConstants.HORIZONTAL);
+            progressBar.setIndeterminate(true);
+            contentPane.add(progressBar, BorderLayout.CENTER);
+            JButton button = new JButton("Cancel");
+            button.addActionListener(e -> cancel(false));
+            contentPane.add(button, BorderLayout.SOUTH);
+            dialog.setContentPane(contentPane);
+            dialog.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+            dialog.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    cancel(false);
+                }
+            });
+            dialog.pack();
+            dialog.setLocationRelativeTo(ConferenceSchedulingPanel.this.getSolverAndPersistenceFrame());
+        }
+
+        public void executeAndShowDialog() {
+            execute();
+            dialog.setVisible(true);
         }
 
         @Override
-        protected ConferenceSolution doInBackground() throws Exception {
+        protected ConferenceSolution doInBackground() {
             return new ConferenceSchedulingCfpDevoxxImporter(conferenceBaseUrl).importSolution();
         }
 
         @Override
         protected void done() {
+            dialog.dispose();
+            if (isCancelled()) {
+                return;
+            }
             try {
                 ConferenceSolution cfpProblem = get();
                 solutionBusiness.setSolution(cfpProblem);
+                JOptionPane.showMessageDialog(ConferenceSchedulingPanel.this, "CFP data imported successfully.");
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 throw new IllegalStateException("Importing was interrupted.", e);
             } catch (ExecutionException e) {
+                JOptionPane.showMessageDialog(ConferenceSchedulingPanel.this,
+                        "CFP data imported failed.\nThe next dialog will explain the cause.");
                 throw new IllegalStateException("Importing failed.", e.getCause());
-            } finally {
-                JOptionPane.showMessageDialog(ConferenceSchedulingPanel.this, "CFP data imported successfully.");
             }
         }
 
