6cc04c2f7f3434e772a3be8b6f94e8af88c00724
tarilabs <matteo.mortari@gmail.com>
2018-11-26 10:17:48 +0100

    cleanup test

--- kie-dmn/kie-dmn-core/src/test/java/org/kie/dmn/core/DMNMessagesAPITest.java
+++ kie-dmn/kie-dmn-core/src/test/java/org/kie/dmn/core/DMNMessagesAPITest.java
@@ -4,20 +4,14 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import org.drools.compiler.kie.builder.impl.InternalKieModule;
 import org.junit.Test;
 import org.kie.api.KieServices;
-import org.kie.api.builder.KieBuilder;
-import org.kie.api.builder.KieFileSystem;
-import org.kie.api.builder.KieModule;
 import org.kie.api.builder.Message;
-import org.kie.api.builder.ReleaseId;
 import org.kie.api.builder.Results;
-import org.kie.api.io.Resource;
 import org.kie.api.runtime.KieContainer;
 import org.kie.dmn.api.core.DMNMessage;
 import org.kie.dmn.api.core.DMNMessageType;
-import org.kie.internal.builder.InternalKieBuilder;
+import org.kie.dmn.core.util.DMNRuntimeUtil;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -26,16 +20,17 @@ public class DMNMessagesAPITest {
 
     @Test
     public void testAPIUsage() {
+        // DROOLS-3335 Broken DMN resource should inhibit KJAR and report KieBuilder message
         final KieServices ks = KieServices.Factory.get();
-        final KieContainer kieContainer = getKieContainer(ks.newReleaseId("org.kie", "dmn-test-" + UUID.randomUUID(), "1.0"),
-                                                          ks.getResources().newClassPathResource("incomplete_expression.dmn", this.getClass()),
-                                                          ks.getResources().newClassPathResource("duff.drl", this.getClass())
-                                                          );
+        final KieContainer kieContainer = DMNRuntimeUtil.getKieContainerIgnoringErrors(ks.newReleaseId("org.kie", "dmn-test-" + UUID.randomUUID(), "1.0"),
+                                                                                       ks.getResources().newClassPathResource("incomplete_expression.dmn", this.getClass()),
+                                                                                       ks.getResources().newClassPathResource("duff.drl", this.getClass()));
 
         Results verify = kieContainer.verify();
         List<Message> kie_messages = verify.getMessages();
         kie_messages.forEach(System.out::println);
         assertThat(kie_messages.size(), is(3));
+        assertThat(kie_messages.stream().filter(m -> m.getPath().equals("duff.drl")).count(), is(2L));
 
         List<DMNMessage> dmnMessages = kie_messages.stream()
                                                    .filter(DMNMessage.class::isInstance)
@@ -46,43 +41,6 @@ public class DMNMessagesAPITest {
         DMNMessage dmnMessage = dmnMessages.get(0);
         assertThat(dmnMessage.getSourceId(), is("_c990c3b2-e322-4ef9-931d-79bcdac99686"));
         assertThat(dmnMessage.getMessageType(), is(DMNMessageType.ERR_COMPILING_FEEL));
-    }
-
-    public static KieContainer getKieContainer(ReleaseId releaseId,
-                                               Resource... resources) {
-        KieServices ks = KieServices.Factory.get();
-        createAndDeployJar(ks, releaseId, resources);
-        return ks.newKieContainer(releaseId);
-    }
-
-    public static KieModule createAndDeployJar(KieServices ks,
-                                               ReleaseId releaseId,
-                                               Resource... resources) {
-        byte[] jar = createJar(ks, releaseId, resources);
-
-        KieModule km = deployJarIntoRepository(ks, jar);
-        return km;
-    }
-
-    public static byte[] createJar(KieServices ks, ReleaseId releaseId, Resource... resources) {
-        KieFileSystem kfs = ks.newKieFileSystem().generateAndWritePomXML(releaseId);
-        for (int i = 0; i < resources.length; i++) {
-            if (resources[i] != null) {
-                kfs.write(resources[i]);
-            }
-        }
-        KieBuilder kieBuilder = ks.newKieBuilder(kfs);
-        ((InternalKieBuilder) kieBuilder).buildAll(o -> true);
-
-        InternalKieModule kieModule = (InternalKieModule) ((InternalKieBuilder) kieBuilder).getKieModuleIgnoringErrors();
-
-        byte[] jar = kieModule.getBytes();
-        return jar;
-    }
-
-    private static KieModule deployJarIntoRepository(KieServices ks, byte[] jar) {
-        Resource jarRes = ks.getResources().newByteArrayResource(jar);
-        KieModule km = ks.getRepository().addKieModule(jarRes);
-        return km;
+        assertThat(dmnMessage.getPath(), is("incomplete_expression.dmn"));
     }
 }
