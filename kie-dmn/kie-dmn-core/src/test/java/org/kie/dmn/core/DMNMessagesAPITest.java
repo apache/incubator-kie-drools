63cd03480c869b1f969c047da621b4e088d47e7b
tarilabs <matteo.mortari@gmail.com>
2018-11-26 10:39:05 +0100

    Kie API usage snippet for custom build for documentation

--- kie-dmn/kie-dmn-core/src/test/java/org/kie/dmn/core/DMNMessagesAPITest.java
+++ kie-dmn/kie-dmn-core/src/test/java/org/kie/dmn/core/DMNMessagesAPITest.java
@@ -6,8 +6,12 @@ import java.util.stream.Collectors;
 
 import org.junit.Test;
 import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
 import org.kie.api.builder.Message;
+import org.kie.api.builder.ReleaseId;
 import org.kie.api.builder.Results;
+import org.kie.api.io.Resource;
 import org.kie.api.runtime.KieContainer;
 import org.kie.dmn.api.core.DMNMessage;
 import org.kie.dmn.api.core.DMNMessageType;
@@ -42,5 +46,23 @@ public class DMNMessagesAPITest {
         assertThat(dmnMessage.getSourceId(), is("_c990c3b2-e322-4ef9-931d-79bcdac99686"));
         assertThat(dmnMessage.getMessageType(), is(DMNMessageType.ERR_COMPILING_FEEL));
         assertThat(dmnMessage.getPath(), is("incomplete_expression.dmn"));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testAPIUsageSnippetForDocumentation() {
+        KieServices ks = KieServices.Factory.get();
+        
+        ReleaseId releaseId = ks.newReleaseId("org.kie", "dmn-test-" + UUID.randomUUID(), "1.0");
+        Resource dmnResource = ks.getResources().newClassPathResource("incomplete_expression.dmn", this.getClass());
+        
+        KieFileSystem kfs = ks.newKieFileSystem()
+                              .generateAndWritePomXML(releaseId)
+                              .write(dmnResource);
+        KieBuilder kieBuilder = ks.newKieBuilder(kfs)
+                                  .buildAll();
+        Results results = kieBuilder.getResults();
+        if (results.hasMessages(Message.Level.ERROR)) {
+            throw new IllegalStateException(results.getMessages(Message.Level.ERROR).toString());
+        }
     }
 }
