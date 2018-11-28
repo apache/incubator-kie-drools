2cd5169d9843ea97de0b2f90ed899309a6cc1cbf
Luca Molteni <volothamp@gmail.com>
2018-11-28 15:46:37 +0100

    Refactor

--- kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/compiler/execmodelbased/ExecModelDTableModel.java
+++ kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/compiler/execmodelbased/ExecModelDTableModel.java
@@ -2,7 +2,6 @@ package org.kie.dmn.core.compiler.execmodelbased;
 
 import java.lang.reflect.Field;
 import java.util.Map;
-import java.util.Optional;
 
 import org.kie.dmn.core.compiler.DMNFEELHelper;
 import org.kie.dmn.core.impl.DMNModelImpl;
@@ -10,7 +9,6 @@ import org.kie.dmn.core.util.Msg;
 import org.kie.dmn.feel.codegen.feel11.CompiledFEELExpression;
 import org.kie.dmn.feel.lang.CompilerContext;
 import org.kie.dmn.feel.lang.EvaluationContext;
-import org.kie.dmn.feel.lang.impl.CompiledExpressionImpl;
 import org.kie.dmn.model.api.DMNElement;
 import org.kie.dmn.model.api.DecisionTable;
 import org.slf4j.Logger;
@@ -18,16 +16,14 @@ import org.slf4j.LoggerFactory;
 
 public class ExecModelDTableModel extends DTableModel {
 
-    private final ClassLoader rootClassLoader;
-    private final DMNRuleClassFile dmnRuleClassFile;
-
     static final Logger logger = LoggerFactory.getLogger(ExecModelDTableModel.class);
+    private final String className;
+    private final Class<?> clazz;
 
-
-    public ExecModelDTableModel(DMNFEELHelper feel, DMNModelImpl model, String dtName, String tableName, DecisionTable dt, ClassLoader rootClassLoader, DMNRuleClassFile dmnRuleClassFile) {
+    public ExecModelDTableModel(DMNFEELHelper feel, DMNModelImpl model, String dtName, String tableName, DecisionTable dt, Class<?> feelExpressionClass) {
         super(feel, model, dtName, tableName, dt);
-        this.rootClassLoader = rootClassLoader;
-        this.dmnRuleClassFile = dmnRuleClassFile;
+        className = getGeneratedClassName(ExecModelDMNEvaluatorCompiler.GeneratorsEnum.FEEL_EXPRESSION);
+        clazz = feelExpressionClass;
     }
 
     @Override
@@ -38,11 +34,8 @@ public class ExecModelDTableModel extends DTableModel {
 
     @Override
     protected void initInputClauses(CompilerContext feelctx, Map<String, CompiledFEELExpression> compilationCache) {
-        // read init input clauses from here
-        final String className = getGeneratedClassName(ExecModelDMNEvaluatorCompiler.GeneratorsEnum.FEEL_EXPRESSION);
 
         try {
-            Class<?> clazz = rootClassLoader.loadClass(className);
 
             logger.debug("Read compiled input clause from class loader: " + className);
 
@@ -53,38 +46,22 @@ public class ExecModelDTableModel extends DTableModel {
                 column.compiledInputClause = (CompiledFEELExpression) feel_expression_input_clauses.get(clazz);
                 index++;
             }
-
-        } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException e) {
+        } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
     }
 
     @Override
     protected CompiledFEELExpression compileFeelExpression(DMNElement element, DMNFEELHelper feel, CompilerContext feelctx, Msg.Message msg, Map<String, CompiledFEELExpression> compilationCache, String expr, int index) {
-        // load from class loader
-        final String className = getGeneratedClassName(ExecModelDMNEvaluatorCompiler.GeneratorsEnum.FEEL_EXPRESSION);
-        Optional<String> generatedClass = dmnRuleClassFile.getCompiledClass(className);
+        logger.debug("Read compiled Feel Expression from class loader: " + className);
 
-        return generatedClass.map(gc -> {
-            try {
-                Class<?> clazz = rootClassLoader.loadClass(gc);
-
-                logger.debug("Read compiled Feel Expression from class loader: " + className);
-
-                CompiledFEELExpression expression1 = new CompiledFEELExpression() {
-                    @Override
-                    public Object apply(EvaluationContext evaluationContext) {
-                        return null;
-                    }
-                };
-
-                return expression1;
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
+        CompiledFEELExpression expression1 = new CompiledFEELExpression() {
+            @Override
+            public Object apply(EvaluationContext evaluationContext) {
+                return null;
             }
+        };
 
-        }).orElseThrow(() -> new RuntimeException("Cannot instantiate evaluator"));
-
-
+        return expression1;
     }
 }
