2c2d31da623152ab5a74a97a2bf419e246dcd823
Toni Rikkola <toni@rikkola.net>
2018-12-18 11:23:05 +0200

    DROOLS-3396 : CRLF in ACTION cell of xlsx spreadsheet is not treated as is (#2184)

---
+++ drools-decisiontables/src/test/java/com/sample/FactData.java
@@ -0,0 +1,26 @@
+package com.sample;
+
+/**
+ * ファクト
+ */
+public class FactData {
+
+	private int 値;
+	private String エラーメッセージ;
+
+	public int get値() {
+		return this.値;
+	}
+
+	public void set値(int 値) {
+		this.値 = 値;
+	}
+
+	public String getエラーメッセージ() {
+		return this.エラーメッセージ;
+	}
+
+	public void setエラーメッセージ(String エラーメッセージ) {
+		this.エラーメッセージ = エラーメッセージ;
+	}
+}
