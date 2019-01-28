209520b0e24221ee791d38fb5cc3b8d9f79174a4
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2019-01-28 15:12:13 +0900

    [DROOLS-3558] "NoSuchElementException: No value present" with executable-model, Map and binding - Unit test

--- drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/CompilerTest.java
+++ drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/CompilerTest.java
@@ -1441,4 +1441,31 @@ public class CompilerTest extends BaseModelTest {
         assertEquals(john.getAge(), 1);
         assertEquals(john.getLikes(), "bread");
     }
+
+    @Test
+    public void testMapWithBinding() {
+        // DROOLS-3558
+        final String drl1 = "package org.drools.compiler\n" +
+                "import " + Person.class.getCanonicalName() + ";\n" +
+                "import " + Address.class.getCanonicalName() + ";\n" +
+                "rule R1\n" +
+                "    when\n" +
+                "        $p : Person()\n" +
+                "        $a : Address(number == $p.items[1])\n" +
+                "    then\n" +
+                "end\n";
+
+        KieSession ksession = getKieSession( drl1 );
+
+        final Person john = new Person("John");
+        HashMap<Integer, Integer> items = new HashMap<Integer, Integer>();
+        items.put(1, 20);
+        john.setItems(items);
+        ksession.insert(john);
+
+        final Address address = new Address("Tasman", 20, "Nelson");
+        ksession.insert(address);
+
+        assertEquals( 1, ksession.fireAllRules() );
+    }
 }
