0fc09cc1ebb1200c5ba9e067f5ccbec11a86ae0b
Mario Fusco <mario.fusco@gmail.com>
2019-02-04 12:19:02 +0100

    [DROOLS-3556] implement property reactivity for update statements in mvel consequences (#2239)

--- drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/consequence/MVELConsequence.java
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/consequence/MVELConsequence.java
@@ -38,6 +38,7 @@ import org.drools.core.rule.MVELDialectRuntimeData;
 import org.drools.core.spi.Consequence;
 import org.drools.core.spi.KnowledgeHelper;
 import org.drools.core.spi.Tuple;
+import org.drools.core.util.ClassUtils;
 import org.drools.model.Variable;
 import org.drools.model.functions.ScriptBlock;
 import org.drools.modelcompiler.RuleContext;
@@ -124,6 +125,8 @@ public class MVELConsequence implements Consequence {
         Declaration[] previousDeclarations = new Declaration[] {};
         Declaration[] localDeclarations = new Declaration[] {};
         String[] otherIdentifiers = new String[] {};
+
+        expression = MVELConsequenceBuilder.rewriteUpdates( context::getDeclarationClass, ClassUtils::getAccessibleProperties, expression );
 
         cu = new MVELCompilationUnit(name,
                 expression,
