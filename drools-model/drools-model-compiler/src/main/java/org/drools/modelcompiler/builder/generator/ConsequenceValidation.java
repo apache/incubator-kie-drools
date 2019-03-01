7a93aec03cc0a46ae0597cdff68f6f2fcbd29082
Luca Molteni <volothamp@gmail.com>
2019-03-01 16:12:39 +0100

    All MvelDialectTest passing

--- drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/ConsequenceValidation.java
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/ConsequenceValidation.java
@@ -3,14 +3,8 @@ package org.drools.modelcompiler.builder.generator;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.drools.compiler.compiler.DroolsError;
-import org.drools.compiler.compiler.RuleBuildError;
-import org.drools.compiler.compiler.io.memory.MemoryFile;
-import org.drools.compiler.compiler.io.memory.MemoryFileSystem;
 import org.drools.compiler.kie.builder.impl.ResultsImpl;
 import org.drools.compiler.lang.descr.RuleDescr;
 import org.drools.core.definitions.InternalKnowledgePackage;
@@ -49,13 +43,6 @@ public class ConsequenceValidation {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        List<DroolsError> droolsErrors = findErrors(ruleClass);
-        for (DroolsError error : droolsErrors) {
-            messages.addMessage(Message.Level.ERROR, className, "Mvel expression in error" + error.getMessage());
-        }
-    }
-
-    private List<DroolsError> findErrors(Class<?> ruleClass) {
         KnowledgePackageImpl pkg = new KnowledgePackageImpl(packageName);
         ScriptBlock block = new ScriptBlock(ruleClass, consequenceString);
 
@@ -69,18 +56,16 @@ public class ConsequenceValidation {
 
             @Override
             public ClassLoader getClassLoader() {
-                return this.getClass().getClassLoader();
+                return classLoader;
             }
         };
 
-        final List<DroolsError> errors = new ArrayList<>();
         try {
             MVELConsequence mvelConsequence = new MVELConsequence(consequence, context);
             mvelConsequence.init();
         } catch (Exception e) {
-            errors.add(new RuleBuildError(null, ruleDescr, null, e.getMessage()));
+            messages.addMessage(Message.Level.ERROR, className, "Mvel expression in error" + e.getMessage());
         }
-        return errors;
     }
 
     public void setClassName(String className) {
