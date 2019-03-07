9fa17b543593e39d24b399bd4f486737a16b8ed2
Luca Molteni <volothamp@gmail.com>
2019-03-07 15:57:02 +0100

    New test

--- drools-model/drools-mvel-compiler/src/test/java/org/drools/MvelCompilerTest.java
+++ drools-model/drools-mvel-compiler/src/test/java/org/drools/MvelCompilerTest.java
@@ -15,4 +15,11 @@ public class MvelCompilerTest {
         ParsingResult compiled = new MvelCompiler(mvelCompilerContext).compile("$p.parent.name");
         assertEquals("$p.getParent().getName()", compiled.resultAsString());
     }
+
+    @Test
+    public void testStringLength() {
+        MvelCompilerContext mvelCompilerContext = new MvelCompilerContext().addDeclaration("$p", Person.class);
+        ParsingResult compiled = new MvelCompiler(mvelCompilerContext).compile("$p.name.length");
+        assertEquals("$p.getName().length()", compiled.resultAsString());
+    }
 }
