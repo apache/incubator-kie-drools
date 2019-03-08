ba81f347bc440f6e41442f2896e59a93c51ce7ec
Luca Molteni <volothamp@gmail.com>
2019-03-08 10:49:22 +0100

    Process first node using the tree

--- drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/phase3/FirstChildProcessPhase.java
+++ drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/phase3/FirstChildProcessPhase.java
@@ -2,8 +2,10 @@ package org.drools.mvelcompiler.phase3;
 
 import java.util.Optional;
 
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.FieldAccessExpr;
 import org.drools.constraint.parser.ast.expr.DrlNameExpr;
-import org.drools.constraint.parser.ast.expr.NullSafeFieldAccessExpr;
 import org.drools.constraint.parser.ast.visitor.DrlGenericVisitor;
 import org.drools.mvelcompiler.context.Declaration;
 import org.drools.mvelcompiler.context.MvelCompilerContext;
@@ -21,14 +23,20 @@ public class FirstChildProcessPhase implements DrlGenericVisitor<TypedExpression
 
     public FirstChildProcessResult invoke(FlattenedExpressionResult flattenedExpressionResult) {
 
-        TypedExpression accept = flattenedExpressionResult.getFirstNode().accept(this, null);
-        return new FirstChildProcessResult(accept, flattenedExpressionResult.getOtherNodes());
+        Expression expression = flattenedExpressionResult.getExpression();
+        TypedExpression accept1 = expression.accept(this, null);
+
+        return new FirstChildProcessResult(accept1, flattenedExpressionResult.getOtherNodes());
     }
 
     @Override
-    public TypedExpression visit(NullSafeFieldAccessExpr nullSafeFieldAccessExpr, Void arg) {
+    public TypedExpression visit(FieldAccessExpr n, Void arg) {
+        for(Node children : n.getChildNodes()) {
+            return children.accept(this, null);
+        }
         return null;
     }
+
 
     @Override
     public TypedExpression visit(DrlNameExpr firstNode, Void arg) {
