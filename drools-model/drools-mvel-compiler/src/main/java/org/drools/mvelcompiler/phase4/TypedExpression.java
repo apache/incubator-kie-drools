0ac02bea8ffde6ab7f78c8deb89d8f0bfcc2e711
Luca Molteni <volothamp@gmail.com>
2019-03-08 11:32:54 +0100

    Navigating tree?

--- drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/phase4/TypedExpression.java
+++ drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/phase4/TypedExpression.java
@@ -1,7 +1,9 @@
 package org.drools.mvelcompiler.phase4;
 
 import java.lang.reflect.Type;
+import java.util.stream.Stream;
 
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.expr.Expression;
 
 public interface TypedExpression {
@@ -11,5 +13,7 @@ public interface TypedExpression {
     Expression toJavaExpression();
 
 //    TypedExpressions childNodes();
+
+    public Stream<Node> stream();
 }
 
