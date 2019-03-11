56949ecb2a032d04ca2503b0b5fc94e46cf278bb
Luca Molteni <volothamp@gmail.com>
2019-03-11 12:14:46 +0100

    Only one phase, visiting the tree

---
+++ drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/ast/TypedExpression.java
@@ -0,0 +1,35 @@
+package org.drools.mvelcompiler.ast;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.Expression;
+
+public abstract class TypedExpression {
+
+    Node originalExpression;
+    List<TypedExpression> children = new ArrayList<>();
+
+    TypedExpression(Node originalExpression) {
+        this.originalExpression = originalExpression;
+    }
+
+    public TypedExpression addChildren(TypedExpression te) {
+        children.add(te);
+        return this;
+    }
+
+    public abstract Type getType();
+
+    public Expression toJavaExpression() {
+        if (!children.isEmpty()) {
+            TypedExpression last = children.get(children.size() - 1);
+            return last.toJavaExpression();
+        } else {
+            return toJavaExpression();
+        }
+    }
+}
+
