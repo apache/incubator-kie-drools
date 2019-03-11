4cefd19bfa37fcc304669079712532da10928e21
Luca Molteni <volothamp@gmail.com>
2019-03-11 12:18:04 +0100

    Moved error message

---
+++ drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/MvelCompilerException.java
@@ -0,0 +1,8 @@
+package org.drools.mvelcompiler;
+
+public class MvelCompilerException extends RuntimeException {
+
+    public MvelCompilerException(String message) {
+        super(message);
+    }
+}
--- drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/context/MvelCompilerContext.java
+++ drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/context/MvelCompilerContext.java
@@ -2,6 +2,8 @@ package org.drools.mvelcompiler.context;
 
 import java.util.Optional;
 
+import org.drools.mvelcompiler.MvelCompilerException;
+
 public class MvelCompilerContext {
     public Declarations declarations = new Declarations();
 
@@ -12,6 +14,13 @@ public class MvelCompilerContext {
 
     public Optional<Declaration> findDeclarations(String name) {
         return declarations.findDeclaration(name);
+    }
+
+
+    public Declaration getDeclarations(String name) {
+        return declarations
+                .findDeclaration(name)
+                .orElseThrow(() -> new MvelCompilerException("No declaration found with name: " + name));
     }
 
     public void addCompilationError(String message) {
