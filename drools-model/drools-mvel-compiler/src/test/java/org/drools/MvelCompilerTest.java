85f20def776530d423ba3112d9ba4424ebf34c19
Luca Molteni <volothamp@gmail.com>
2019-03-12 10:48:25 +0100

    Refactor test

--- drools-model/drools-mvel-compiler/src/test/java/org/drools/MvelCompilerTest.java
+++ drools-model/drools-mvel-compiler/src/test/java/org/drools/MvelCompilerTest.java
@@ -1,5 +1,7 @@
 package org.drools;
 
+import java.util.function.Function;
+
 import org.drools.mvelcompiler.MvelCompiler;
 import org.drools.mvelcompiler.ParsingResult;
 import org.drools.mvelcompiler.context.MvelCompilerContext;
@@ -12,32 +14,36 @@ public class MvelCompilerTest {
 
     @Test
     public void testConvertPropertyToAccessor() {
-        MvelCompilerContext mvelCompilerContext = new MvelCompilerContext().addDeclaration("$p", Person.class);
-        ParsingResult compiled = new MvelCompiler(mvelCompilerContext).compile("{ $p.parent.name; } ");
-        assertThat(compiled.resultAsString(), equalToIgnoringWhiteSpace("{ $p.getParent().getName(); }"));
-
+        test(ctx -> ctx.addDeclaration("$p", Person.class),
+             "{ $p.parent.name; } ",
+             "{ $p.getParent().getName(); }");
     }
 
     @Test
     public void testStringLength() {
-        MvelCompilerContext mvelCompilerContext = new MvelCompilerContext().addDeclaration("$p", Person.class);
-        ParsingResult compiled = new MvelCompiler(mvelCompilerContext).compile("{ $p.name.length; }");
-        assertThat(compiled.resultAsString(), equalToIgnoringWhiteSpace("{ $p.getName().length(); }"));
-
+        test(ctx -> ctx.addDeclaration("$p", Person.class),
+             "{ $p.name.length; }",
+             "{ $p.getName().length(); }");
     }
 
     @Test
     public void testAssignment() {
-        MvelCompilerContext mvelCompilerContext = new MvelCompilerContext().addDeclaration("$p", Person.class);;
-        ParsingResult compiled = new MvelCompiler(mvelCompilerContext).compile("{ Person np = $p; }");
-        assertThat(compiled.resultAsString(), equalToIgnoringWhiteSpace("{ org.drools.Person np = $p; }"));
+        test(ctx -> ctx.addDeclaration("$p", Person.class),
+             "{ Person np = $p; }",
+             "{ org.drools.Person np = $p; }");
     }
 
     @Test
-    public void testAssignment2() {
-        MvelCompilerContext mvelCompilerContext = new MvelCompilerContext().addDeclaration("$p", Person.class);
-        ParsingResult compiled = new MvelCompiler(mvelCompilerContext).compile("{ Person np = $p; np = $p; }");
+    public void testMultiLineAssignment() {
+        test(ctx -> ctx.addDeclaration("$p", Person.class),
+             "{ Person np = $p; np = $p; }",
+             "{ org.drools.Person np = $p; np = $p; }");
+    }
 
-        assertThat(compiled.resultAsString(), equalToIgnoringWhiteSpace("{ org.drools.Person np = $p; np = $p; }"));
+    private void test(Function<MvelCompilerContext, MvelCompilerContext> testFunction, String actualExpression, String expectedResult) {
+        MvelCompilerContext mvelCompilerContext = new MvelCompilerContext();
+        testFunction.apply(mvelCompilerContext);
+        ParsingResult compiled = new MvelCompiler(mvelCompilerContext).compile(actualExpression);
+        assertThat(compiled.resultAsString(), equalToIgnoringWhiteSpace(expectedResult));
     }
 }
