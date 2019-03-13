c232ae8260af9d382200101d35c469547d180182
Matteo Mortari <matteo.mortari@gmail.com>
2019-03-13 09:49:39 +0100

    DROOLS-3738 DMN DT Analysis to cover Boolean data type (#2278)

---
+++ kie-dmn/kie-dmn-validation/src/test/java/org/kie/dmn/validation/dtanalysis/GapsOverlapsBoolean_Test.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.dmn.validation.dtanalysis;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.kie.dmn.api.core.DMNMessage;
+import org.kie.dmn.feel.runtime.Range.RangeBoundary;
+import org.kie.dmn.validation.dtanalysis.model.Bound;
+import org.kie.dmn.validation.dtanalysis.model.DTAnalysis;
+import org.kie.dmn.validation.dtanalysis.model.Hyperrectangle;
+import org.kie.dmn.validation.dtanalysis.model.Interval;
+import org.kie.dmn.validation.dtanalysis.model.Overlap;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+import static org.kie.dmn.validation.DMNValidator.Validation.ANALYZE_DECISION_TABLE;
+import static org.kie.dmn.validation.DMNValidator.Validation.VALIDATE_COMPILATION;
+
+public class GapsOverlapsBoolean_Test extends AbstractDTAnalysisTest {
+
+    @Test
+    public void test() {
+        List<DMNMessage> validate = validator.validate(getReader("GapsOverlapsBoolean.dmn"), VALIDATE_COMPILATION, ANALYZE_DECISION_TABLE);
+        DTAnalysis analysis = getAnalysis(validate, "_EE34FD37-00D1-47A7-B2F6-CC9BCEF30005");
+
+        assertThat(analysis.getGaps(), hasSize(1));
+
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        List<Hyperrectangle> gaps = Arrays.asList(new Hyperrectangle(2,
+                                                                     Arrays.asList(Interval.newFromBounds(new Bound(true,
+                                                                                                                    RangeBoundary.CLOSED,
+                                                                                                                    null),
+                                                                                                          new Bound(true,
+                                                                                                                    RangeBoundary.CLOSED,
+                                                                                                                    null)),
+                                                                                   Interval.newFromBounds(new Bound(true,
+                                                                                                                    RangeBoundary.CLOSED,
+                                                                                                                    null),
+                                                                                                          new Bound(true,
+                                                                                                                    RangeBoundary.CLOSED,
+                                                                                                                    null)))));
+        assertThat(gaps, hasSize(1));
+
+        // Assert GAPS
+        assertThat(analysis.getGaps(), contains(gaps.toArray()));
+
+        // assert OVERLAPs count.
+        assertThat(analysis.getOverlaps(), hasSize(1));
+
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        List<Overlap> overlaps = Arrays.asList(new Overlap(Arrays.asList(Long.valueOf(1),
+                                                                         Long.valueOf(2)),
+                                                           new Hyperrectangle(2,
+                                                                              Arrays.asList(Interval.newFromBounds(new Bound(false,
+                                                                                                                             RangeBoundary.CLOSED,
+                                                                                                                             null),
+                                                                                                                   new Bound(true,
+                                                                                                                             RangeBoundary.OPEN,
+                                                                                                                             null)),
+                                                                                            Interval.newFromBounds(new Bound(false,
+                                                                                                                             RangeBoundary.CLOSED,
+                                                                                                                             null),
+                                                                                                                   new Bound(true,
+                                                                                                                             RangeBoundary.OPEN,
+                                                                                                                             null))))));
+        assertThat(overlaps, hasSize(1));
+
+        // Assert OVERLAPs same values
+        assertThat(analysis.getOverlaps(), contains(overlaps.toArray()));
+    }
+}
