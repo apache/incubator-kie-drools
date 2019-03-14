7130f5787a144f1046ebbea6825b1b5500cd2082
Luca Molteni <volothamp@gmail.com>
2019-03-14 14:59:13 +0100

    setter test passing, every other test failing

--- drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/LHSPhase.java
+++ drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/LHSPhase.java
@@ -1,5 +1,6 @@
 package org.drools.mvelcompiler;
 
+import java.lang.reflect.Method;
 import java.util.Optional;
 import java.util.Stack;
 
@@ -8,18 +9,27 @@ import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.expr.AssignExpr;
 import com.github.javaparser.ast.expr.FieldAccessExpr;
 import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.SimpleName;
 import com.github.javaparser.ast.expr.VariableDeclarationExpr;
 import com.github.javaparser.ast.stmt.ExpressionStmt;
 import com.github.javaparser.ast.stmt.Statement;
+import org.drools.constraint.parser.ast.expr.DrlNameExpr;
 import org.drools.constraint.parser.ast.visitor.DrlGenericVisitor;
 import org.drools.mvelcompiler.ast.AssignExprT;
 import org.drools.mvelcompiler.ast.ExpressionStmtT;
+import org.drools.mvelcompiler.ast.MethodCallTExpr;
+import org.drools.mvelcompiler.ast.NameTExpr;
+import org.drools.mvelcompiler.ast.SimpleNameTExpr;
 import org.drools.mvelcompiler.ast.TypedExpression;
 import org.drools.mvelcompiler.ast.VariableDeclarationTExpr;
 import org.drools.mvelcompiler.ast.VariableDeclaratorTExpr;
+import org.drools.mvelcompiler.context.Declaration;
 import org.drools.mvelcompiler.context.MvelCompilerContext;
 
+import static java.util.Collections.singletonList;
 import static org.drools.constraint.parser.printer.PrintUtil.printConstraint;
+import static org.drools.core.util.ClassUtils.getSetter;
+import static org.drools.modelcompiler.util.ClassUtil.toRawClass;
 
 public class LHSPhase implements DrlGenericVisitor<TypedExpression, LHSPhase.Context> {
 
@@ -47,8 +57,34 @@ public class LHSPhase implements DrlGenericVisitor<TypedExpression, LHSPhase.Con
     }
 
     @Override
+    public TypedExpression visit(SimpleName n, Context arg) {
+        Declaration typeFromDeclarations = mvelCompilerContext.getDeclarations(n.asString());
+        Class<?> clazz = typeFromDeclarations.getClazz();
+        SimpleNameTExpr simpleNameTExpr = new SimpleNameTExpr(n, clazz);
+        arg.lastTypedExpression.push(simpleNameTExpr);
+        return simpleNameTExpr;
+    }
+
+    @Override
+    public TypedExpression visit(DrlNameExpr n, Context arg) {
+        NameTExpr nameTExpr = new NameTExpr(n, null);
+        for (Node children : n.getChildNodes()) {
+            nameTExpr.addChildren(children.accept(this, arg));
+        }
+        return nameTExpr;
+    }
+
+    @Override
     public TypedExpression visit(FieldAccessExpr n, Context arg) {
-        return rhs;
+        TypedExpression scope = n.getScope().accept(this, arg);
+        TypedExpression lastTypedExpression = arg.lastTypedExpression.peek();
+
+        Class<?> setterArgumentType = (Class<?>) rhs.getType();
+        Class<?> objectClass = toRawClass(lastTypedExpression.getType());
+        String setterName = printConstraint(n.getName());
+        Method accessor = getSetter(objectClass, setterName, setterArgumentType);
+
+        return new MethodCallTExpr(n, scope, accessor, singletonList(rhs));
     }
 
     @Override
@@ -81,9 +117,12 @@ public class LHSPhase implements DrlGenericVisitor<TypedExpression, LHSPhase.Con
 
     @Override
     public TypedExpression visit(AssignExpr n, Context arg) {
-        AssignExprT assignExprT = new AssignExprT(n);
-        assignExprT.addChildren(rhs);
-        return assignExprT;
+        TypedExpression target = n.getTarget().accept(this, arg);
+        if(target instanceof MethodCallTExpr) {
+            return target;
+        } else {
+            return new AssignExprT(n, target, rhs);
+        }
     }
 }
 
