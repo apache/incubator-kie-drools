03bd978d7f320a939376a2ad917f837726ce8f21
tarilabs <matteo.mortari@gmail.com>
2019-03-26 15:44:26 +0100

    SUBMARINE removing no longer necessary /history utility scripts

--- history/deletefromhistory.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf $1
-git add .
-git commit -m "SUBMARINE deleted $1 from /history"
-
--- history/move.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mv $1 history
-git add .
-git commit -m "SUBMARINE moved $1 into /history"
