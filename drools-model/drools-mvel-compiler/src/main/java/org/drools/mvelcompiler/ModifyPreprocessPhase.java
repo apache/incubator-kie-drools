affd5ba5b7d11b0a86a20c5a7ef637b906bacbd7
Luca Molteni <volothamp@gmail.com>
2019-04-08 17:37:45 +0200

    Fix first modify rewrite code, removed custom modify parsing

--- drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/ModifyPreprocessPhase.java
+++ drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/ModifyPreprocessPhase.java
@@ -98,6 +98,7 @@ public class ModifyPreprocessPhase {
 
         List<Statement> statements = modifyStatement.getExpressions()
                 .stream()
+                .filter(Statement::isExpressionStmt)
                 .map(s -> s.asExpressionStmt().getExpression())
                 .map(ExpressionStmt::new).collect(Collectors.toList());
         return result.addStatements(statements);
