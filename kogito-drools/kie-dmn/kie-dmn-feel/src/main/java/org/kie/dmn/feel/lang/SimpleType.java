d5c072e754c89d6ee8f7454b8b43238feb2741ca
Matteo Mortari <matteo.mortari@gmail.com>
2019-04-16 12:36:25 +0200

    Removing kie-dmn module which was copy-pasted without history (#24)

--- kogito-drools/kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/lang/SimpleType.java
+++
@@ -1,9 +0,0 @@
-package org.kie.dmn.feel.lang;
-
-/**
- * A simple type definition interface, i.e., a type that does not contain fields
- */
-public interface SimpleType
-        extends Type {
-
-}
