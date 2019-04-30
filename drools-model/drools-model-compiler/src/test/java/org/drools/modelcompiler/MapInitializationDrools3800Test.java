aa986a57724e2f16cabea9c084b8e168d309e46f
mariofusco <mario.fusco@gmail.com>
2019-04-30 18:41:02 +0200

    [DROOLS-3849] prevent property reactivity to filter anything when constraints invokes an external function

--- drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/MapInitializationDrools3800Test.java
+++ drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/MapInitializationDrools3800Test.java
@@ -3,12 +3,11 @@ package org.drools.modelcompiler;
 import java.util.Map;
 import java.util.Objects;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.kie.api.runtime.KieSession;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class MapInitializationDrools3800Test extends BaseModelTest {
 
@@ -73,29 +72,29 @@ public class MapInitializationDrools3800Test extends BaseModelTest {
     }
 
     @Test
-    @Ignore
-    // https://issues.jboss.org/browse/DROOLS-3849
-    // This is hanging using executable model because of reactivity default
     public void testPropertyReactivityHanging() {
-        StringBuilder r = new StringBuilder();
-        r.append("package ").append(getClass().getPackage().getName()).append("\n");
-        r.append("\n");
-        r.append("import ").append(Fact.class.getCanonicalName()).append("\n");
-        r.append("import static ").append(getClass().getName()).append(".calc").append("\n");
-        r.append("\n");
-        r.append("rule rule1").append("\n");
-        r.append("  when").append("\n");
-        r.append("    $fact: Fact(").append("\n");
-        r.append("      calc([\"src\":name, \"target\":\"TEST\"])").append("\n");
-        r.append("    )").append("\n");
-        r.append("  then").append("\n");
-        r.append("    modify ($fact) {").append("\n");
-        r.append("      setResult(\"OK\")").append("\n");
-        r.append("    }").append("\n");
-        r.append("end").append("\n");
+        // DROOLS-3849
+        String rule =
+                "package " + getClass().getPackage().getName() + "\n" +
+                "import " + Fact.class.getCanonicalName() + "\n" +
+                "import static " + getClass().getName() + ".calc\n" +
+                "\n" +
+                "rule rule1\n" +
+                "  when\n" +
+                "    $fact: Fact(\n" +
+                "      calc([\"src\":name, \"target\":\"TEST\"])\n" +
+                "    )\n" +
+                "  then\n" +
+                "    if ($fact.getResult() != null) {\n" +
+                "        $fact.setResult(\"OK\");\n" +
+                "    } else {\n" +
+                "        modify ($fact) {\n" +
+                "            setResult(\"FIRST\")\n" +
+                "        }\n" +
+                "    }\n" +
+                "end";
 
-        KieSession ksession = getKieSession(r.toString() );
-
+        KieSession ksession = getKieSession(rule);
 
         Fact fact = new Fact();
         fact.setName("TEST");
