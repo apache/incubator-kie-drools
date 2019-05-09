a5c3e4746bde220a10b172911402293f8a387e88
Matteo Mortari <matteo.mortari@gmail.com>
2019-05-09 10:01:58 +0200

    DROOLS-3981 DMN DT Analysis coverage for not() (#2329)

--- kie-dmn/kie-dmn-validation/src/main/java/org/kie/dmn/validation/dtanalysis/DMNDTAnalyser.java
+++ kie-dmn/kie-dmn-validation/src/main/java/org/kie/dmn/validation/dtanalysis/DMNDTAnalyser.java
@@ -45,7 +45,6 @@ import org.kie.dmn.feel.lang.ast.UnaryTestNode;
 import org.kie.dmn.feel.lang.ast.UnaryTestNode.UnaryOperator;
 import org.kie.dmn.feel.lang.impl.InterpretedExecutableExpression;
 import org.kie.dmn.feel.lang.impl.UnaryTestInterpretedExecutableExpression;
-import org.kie.dmn.feel.runtime.Range;
 import org.kie.dmn.feel.runtime.Range.RangeBoundary;
 import org.kie.dmn.model.api.DecisionRule;
 import org.kie.dmn.model.api.DecisionTable;
@@ -154,7 +153,7 @@ public class DMNDTAnalyser {
 
                 DDTAInputClause ddtaInputClause = ddtaTable.getInputs().get(jColIdx);
 
-                DDTAInputEntry ddtaInputEntry = new DDTAInputEntry(utln.getElements(), toIntervals(utln.getElements(), ddtaInputClause.getDomainMinMax(), ddtaInputClause.getDiscreteValues(), jRowIdx + 1, jColIdx + 1));
+                DDTAInputEntry ddtaInputEntry = new DDTAInputEntry(utln.getElements(), toIntervals(utln.getElements(), utln.isNegated(), ddtaInputClause.getDomainMinMax(), ddtaInputClause.getDiscreteValues(), jRowIdx + 1, jColIdx + 1));
                 for (Interval interval : ddtaInputEntry.getIntervals()) {
                     Interval domainMinMax = ddtaTable.getInputs().get(jColIdx).getDomainMinMax();
                     if (!domainMinMax.includes(interval)) {
@@ -295,10 +294,10 @@ public class DMNDTAnalyser {
             for (Bound<?> currentBound : bounds) {
                 LOG.debug("lastBound {} currentBound {}      activeIntervals {} == rules {}", lastBound, currentBound, activeIntervals, activeIntervalsToRules(activeIntervals));
                 if (activeIntervals.size() > 1 && canBeNewCurrIntervalForOverlaps(lastBound, currentBound)) {
-                    Interval analysisInterval = new Interval(lastBound.isUpperBound() ? invertBoundary(lastBound.getBoundaryType()) : lastBound.getBoundaryType(),
+                    Interval analysisInterval = new Interval(lastBound.isUpperBound() ? Interval.invertBoundary(lastBound.getBoundaryType()) : lastBound.getBoundaryType(),
                                                              lastBound.getValue(),
                                                              currentBound.getValue(),
-                                                             currentBound.isLowerBound() ? invertBoundary(currentBound.getBoundaryType()) : currentBound.getBoundaryType(),
+                                                             currentBound.isLowerBound() ? Interval.invertBoundary(currentBound.getBoundaryType()) : currentBound.getBoundaryType(),
                                                              0, 0);
                     currentIntervals[jColIdx] = analysisInterval;
                     findOverlaps(analysis, ddtaTable, jColIdx + 1, currentIntervals, activeIntervalsToRules(activeIntervals));
@@ -362,10 +361,10 @@ public class DMNDTAnalyser {
                     analysis.addGap(gap);
                 }
                 if (!activeIntervals.isEmpty() && canBeNewCurrInterval(lastBound, currentBound)) {
-                    Interval missingInterval = new Interval(lastBound.isUpperBound() ? invertBoundary(lastBound.getBoundaryType()) : lastBound.getBoundaryType(),
+                    Interval missingInterval = new Interval(lastBound.isUpperBound() ? Interval.invertBoundary(lastBound.getBoundaryType()) : lastBound.getBoundaryType(),
                                                             lastBound.getValue(),
                                                             currentBound.getValue(),
-                                                            currentBound.isLowerBound() ? invertBoundary(currentBound.getBoundaryType()) : currentBound.getBoundaryType(),
+                                                            currentBound.isLowerBound() ? Interval.invertBoundary(currentBound.getBoundaryType()) : currentBound.getBoundaryType(),
                                                             0, 0);
                     currentIntervals[jColIdx] = missingInterval;
                     findGaps(analysis, ddtaTable, jColIdx + 1, currentIntervals, activeIntervalsToRules(activeIntervals));
@@ -435,27 +434,18 @@ public class DMNDTAnalyser {
     private static Interval lastDimensionUncoveredInterval(Bound<?> l, Bound<?> r, Interval domain) {
         boolean isLmin = l.isLowerBound() && l.equals(domain.getLowerBound());
         boolean isRmax = r.isUpperBound() && r.equals(domain.getUpperBound());
-        return new Interval(isLmin ? domain.getLowerBound().getBoundaryType() : invertBoundary(l.getBoundaryType()),
+        return new Interval(isLmin ? domain.getLowerBound().getBoundaryType() : Interval.invertBoundary(l.getBoundaryType()),
                             l.getValue(),
                             r.getValue(),
-                            isRmax ? domain.getUpperBound().getBoundaryType() : invertBoundary(r.getBoundaryType()),
+                            isRmax ? domain.getUpperBound().getBoundaryType() : Interval.invertBoundary(r.getBoundaryType()),
                             0, 0);
     }
 
-    private static Range.RangeBoundary invertBoundary(Range.RangeBoundary b) {
-        if (b == RangeBoundary.OPEN) {
-            return RangeBoundary.CLOSED;
-        } else if (b == RangeBoundary.CLOSED) {
-            return RangeBoundary.OPEN;
-        } else {
-            throw new IllegalStateException("invertBoundary for: " + b);
-        }
-    }
-
-    private List<Interval> toIntervals(List<BaseNode> elements, Interval minMax, List discreteValues, int rule, int col) {
+    private List<Interval> toIntervals(List<BaseNode> elements, boolean isNegated, Interval minMax, List discreteValues, int rule, int col) {
         List<Interval> results = new ArrayList<>();
         if (discreteValues != null && !discreteValues.isEmpty() && areAllEQUnaryTest(elements) && elements.size() > 1) {
-            BitSet hitValues = new BitSet(discreteValues.size());
+            int bitsetLogicalSize = discreteValues.size(); // JDK BitSet size will always be larger.
+            BitSet hitValues = new BitSet(bitsetLogicalSize);
             for (BaseNode n : elements) {
                 Comparable<?> thisValue = valueFromNode(((UnaryTestNode) n).getValue());
                 int indexOf = discreteValues.indexOf(thisValue);
@@ -463,6 +453,9 @@ public class DMNDTAnalyser {
                     throw new IllegalStateException("Unable to determine discreteValue index for: " + n);
                 }
                 hitValues.set(indexOf);
+            }
+            if (isNegated) {
+                hitValues.flip(0, bitsetLogicalSize);
             }
             int lowerBoundIdx = -1;
             int upperBoundIdx = -1;
@@ -478,6 +471,8 @@ public class DMNDTAnalyser {
                 } else {
                     if (lowerBoundIdx >= 0 && upperBoundIdx >=0) {
                         results.add(createIntervalOfRule(discreteValues, rule, col, lowerBoundIdx, upperBoundIdx));
+                        lowerBoundIdx = -1;
+                        upperBoundIdx = -1;
                     }
                 }
             }
@@ -491,7 +486,12 @@ public class DMNDTAnalyser {
                     continue;
                 }
                 UnaryTestNode ut = (UnaryTestNode) n;
-                results.add(utnToInterval(ut, minMax, discreteValues, rule, col));
+                Interval interval = utnToInterval(ut, minMax, discreteValues, rule, col);
+                if (isNegated) {
+                    results.addAll(Interval.invertOverDomain(interval, minMax));
+                } else {
+                    results.add(interval);
+                }
             }
         }
         return results;
---
+++ kie-dmn/kie-dmn-validation/src/test/java/org/kie/dmn/validation/dtanalysis/NotTest.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.dmn.validation.dtanalysis;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.kie.dmn.api.core.DMNMessage;
+import org.kie.dmn.feel.runtime.Range.RangeBoundary;
+import org.kie.dmn.validation.dtanalysis.model.Bound;
+import org.kie.dmn.validation.dtanalysis.model.DTAnalysis;
+import org.kie.dmn.validation.dtanalysis.model.Hyperrectangle;
+import org.kie.dmn.validation.dtanalysis.model.Interval;
+import org.kie.dmn.validation.dtanalysis.model.Overlap;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+import static org.kie.dmn.validation.DMNValidator.Validation.ANALYZE_DECISION_TABLE;
+
+public class NotTest extends AbstractDTAnalysisTest {
+
+    @Test
+    public void testNOTString() {
+        List<DMNMessage> validate = validator.validate(getReader("DTusingNOT.dmn"), ANALYZE_DECISION_TABLE);
+        DTAnalysis analysis = getAnalysis(validate, "_b53fac34-fb12-4601-8605-c226e68292f9");
+
+        assertThat(analysis.getGaps(), hasSize(1));
+
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        List<Hyperrectangle> gaps = Arrays.asList(new Hyperrectangle(1,
+                                                                     Arrays.asList(Interval.newFromBounds(new Bound("i",
+                                                                                                                    RangeBoundary.CLOSED,
+                                                                                                                    null),
+                                                                                                          new Bound("o",
+                                                                                                                    RangeBoundary.OPEN,
+                                                                                                                    null)))));
+        assertThat(gaps, hasSize(1));
+
+        // Assert GAPS
+        assertThat(analysis.getGaps(), contains(gaps.toArray()));
+
+        // assert OVERLAPs count.
+        assertThat(analysis.getOverlaps(), hasSize(1));
+
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        List<Overlap> overlaps = Arrays.asList(new Overlap(Arrays.asList(1,
+                                                                         2),
+                                                           new Hyperrectangle(1,
+                                                                              Arrays.asList(Interval.newFromBounds(new Bound("o",
+                                                                                                                             RangeBoundary.CLOSED,
+                                                                                                                             null),
+                                                                                                                   new Bound("u",
+                                                                                                                             RangeBoundary.OPEN,
+                                                                                                                             null))))));
+        assertThat(overlaps, hasSize(1));
+
+        // Assert OVERLAPs same values
+        assertThat(analysis.getOverlaps(), contains(overlaps.toArray()));
+    }
+
+    @Test
+    public void testNOTString2() {
+        List<DMNMessage> validate = validator.validate(getReader("DTusingNOT2.dmn"), ANALYZE_DECISION_TABLE);
+        DTAnalysis analysis = getAnalysis(validate, "_b53fac34-fb12-4601-8605-c226e68292f9");
+
+        assertThat(analysis.getGaps(), hasSize(1));
+
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        List<Hyperrectangle> gaps = Arrays.asList(new Hyperrectangle(1,
+                                                                     Arrays.asList(Interval.newFromBounds(new Bound("i",
+                                                                                                                    RangeBoundary.CLOSED,
+                                                                                                                    null),
+                                                                                                          new Bound("o",
+                                                                                                                    RangeBoundary.OPEN,
+                                                                                                                    null)))));
+        assertThat(gaps, hasSize(1));
+
+        // Assert GAPS
+        assertThat(analysis.getGaps(), contains(gaps.toArray()));
+
+        // assert OVERLAPs count.
+        assertThat(analysis.getOverlaps(), hasSize(0));
+
+    }
+
+    @Test
+    public void testNOTnumber() {
+        List<DMNMessage> validate = validator.validate(getReader("DTusingNOTnumber.dmn"), ANALYZE_DECISION_TABLE);
+        DTAnalysis analysis = getAnalysis(validate, "_d0cbacca-55d4-47dd-acc6-131add2a8a53");
+
+        assertThat(analysis.getGaps(), hasSize(1));
+
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        List<Hyperrectangle> gaps = Arrays.asList(new Hyperrectangle(1,
+                                                                     Arrays.asList(Interval.newFromBounds(new Bound(new BigDecimal("0"),
+                                                                                                                    RangeBoundary.CLOSED,
+                                                                                                                    null),
+                                                                                                          new Bound(new BigDecimal("0"),
+                                                                                                                    RangeBoundary.CLOSED,
+                                                                                                                    null)))));
+        assertThat(gaps, hasSize(1));
+
+        // Assert GAPS
+        assertThat(analysis.getGaps(), contains(gaps.toArray()));
+
+        // assert OVERLAPs count.
+        assertThat(analysis.getOverlaps(), hasSize(1));
+
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        List<Overlap> overlaps = Arrays.asList(new Overlap(Arrays.asList(1,
+                                                                         2),
+                                                           new Hyperrectangle(1,
+                                                                              Arrays.asList(Interval.newFromBounds(new Bound(new BigDecimal("0"),
+                                                                                                                             RangeBoundary.OPEN,
+                                                                                                                             null),
+                                                                                                                   new Bound(Interval.POS_INF,
+                                                                                                                             RangeBoundary.CLOSED,
+                                                                                                                             null))))));
+        assertThat(overlaps, hasSize(1));
+
+        // Assert OVERLAPs same values
+        assertThat(analysis.getOverlaps(), contains(overlaps.toArray()));
+    }
+
+}
