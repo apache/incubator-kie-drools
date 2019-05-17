4ce9cc8ea1f24f0dba474849dce576af8bb9f8e4
Luca Molteni <volothamp@gmail.com>
2019-05-17 15:14:17 +0200

    Recurse over ModifyProcessor

--- drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/ModifyCompiler.java
+++ drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/ModifyCompiler.java
@@ -5,11 +5,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.EmptyStmt;
 import com.github.javaparser.ast.stmt.Statement;
 import org.drools.constraint.parser.DrlConstraintParser;
+import org.drools.constraint.parser.ast.expr.ModifyStatement;
 import org.drools.mvelcompiler.context.MvelCompilerContext;
 
 // A special case of compiler in which only the modify statements are processed
@@ -26,24 +29,24 @@ public class ModifyCompiler {
 
         BlockStmt mvelExpression = DrlConstraintParser.parseBlock(mvelBlock);
 
-
-        // TODO: remove duplication in MvelCompiler and ModifyCompiler
-        List<Statement> withoutEmptyStatements =
-                mvelExpression
-                        .getStatements()
-                        .stream()
-                        .filter(statement -> !statement.isEmptyStmt())
-                        .collect(Collectors.toList());
+        // TODO: remove duplication in MvelCompiler and ModifyCompiler in removing empty stmt
+        mvelExpression
+                .findAll(EmptyStmt.class)
+                .forEach(Node::remove);
 
         List<Statement> preProcessedStatements = new ArrayList<>();
-        // TODO: This preprocessing will change the order of the modify statments
-        // Write a test for that
+        // TODO: This preprocessing will change the order of the modify statments Write a test for that
+
+        // TODO: Preprocessor does not recurse see MapInitializationDrools3800Test.testPropertyReactivityHanging
+        // or put a modify into a in if branch
         Map<String, Set<String>> modifiedProperties = new HashMap<>();
-        for(Statement t : withoutEmptyStatements) {
-            PreprocessPhase.PreprocessPhaseResult invoke = preprocessPhase.invoke(t);
-            modifiedProperties.putAll(invoke.getModifyProperties());
-            preProcessedStatements.addAll(invoke.getStatements());
-        }
+
+        mvelExpression.findAll(ModifyStatement.class)
+                .forEach(s -> {
+                    PreprocessPhase.PreprocessPhaseResult invoke = preprocessPhase.invoke(s);
+                    modifiedProperties.putAll(invoke.getModifyProperties());
+                    s.replace(new BlockStmt(NodeList.nodeList(invoke.getStatements())));
+                });
 
         return new ParsingResult(preProcessedStatements).setModifyProperties(modifiedProperties);
     }
