5a24e734348fb95b2eb0d226716bd209afec7fe9
tzimanyi <tibor.zimanyi@gmail.com>
2019-05-21 14:14:20 +0200

    DROOLS-4047 Provide InputType for spreadsheet compiler

--- drools-decisiontables/src/main/java/org/drools/decisiontable/InputType.java
+++ drools-decisiontables/src/main/java/org/drools/decisiontable/InputType.java
@@ -23,6 +23,7 @@ import org.drools.decisiontable.parser.csv.CsvLineParser;
 import org.drools.decisiontable.parser.csv.CsvParser;
 import org.drools.decisiontable.parser.xls.ExcelParser;
 import org.drools.template.parser.DataListener;
+import org.kie.internal.builder.DecisionTableInputType;
 
 /**
  * Provides valid input types for decision tables.
@@ -43,6 +44,22 @@ public abstract class InputType {
     public abstract DecisionTableParser createParser(DataListener listener);
     public abstract DecisionTableParser createParser(List<DataListener> listeners);
 
+    /**
+     * Converts DecisionTableInputType to InputType.
+     *
+     * @param decisionTableInputType DecisionTableInputType to convert
+     * @return the appropriate InputType based on the specified DecisionTableInputType
+     */
+    public static InputType getInputTypeFromDecisionTableInputType(final DecisionTableInputType decisionTableInputType) {
+        switch (decisionTableInputType) {
+            case CSV:
+                return InputType.CSV;
+            case XLS:
+            case XLSX:
+                return InputType.XLS;
+            default: throw new IllegalArgumentException("Unsupported DecisionTableInputType: " + decisionTableInputType + "!");
+        }
+    }
 }
 
 class XlsInput extends InputType {
