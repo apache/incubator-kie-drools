f19a3c1dd6bbaa55f1ac419a606929a2ba7b2bb2
Enrique Gonzalez <egonzale@redhat.com>
2019-05-23 21:22:22 +0200

    [JBPM-8430] RETRY handlingStrategy should handle exception multiple times while handling exception via subprocess (#381)

--- kie-api/src/main/java/org/kie/api/runtime/process/ProcessWorkItemHandlerException.java
+++ kie-api/src/main/java/org/kie/api/runtime/process/ProcessWorkItemHandlerException.java
@@ -18,25 +18,39 @@ package org.kie.api.runtime.process;
 
 public class ProcessWorkItemHandlerException extends RuntimeException {
 
-    private static final long serialVersionUID = -5953387125605633663L;
+    private static final long serialVersionUID = 328927887939759492L;
 
+    private static final int MAX_NUMBER_OF_RETRIES = 3;
     public enum HandlingStrategy {
         RETRY, COMPLETE, ABORT, RETHROW
     }
 
     private String processId;
     private HandlingStrategy strategy;
-    
+    private int retries;
+
     public ProcessWorkItemHandlerException(String processId, String strategy, Throwable cause) {        
-        this(processId, HandlingStrategy.valueOf(strategy), cause);
+        this(processId, HandlingStrategy.valueOf(strategy), cause, MAX_NUMBER_OF_RETRIES);
+    }
+
+    public ProcessWorkItemHandlerException(String processId, String strategy, Throwable cause, int retries) {
+        this(processId, HandlingStrategy.valueOf(strategy), cause, retries);
     }
 
     public ProcessWorkItemHandlerException(String processId, HandlingStrategy strategy, Throwable cause) {
+        this(processId, strategy, cause, MAX_NUMBER_OF_RETRIES);
+    }
+
+    public ProcessWorkItemHandlerException(String processId, HandlingStrategy strategy, Throwable cause, int retries) {
         super(cause);
         this.processId = processId;
         this.strategy = strategy;
+        this.retries = retries;
         if (processId == null || strategy == null) {
             throw new IllegalArgumentException("Process id and strategy are required");
+        }
+        if (retries < 0) {
+            throw new IllegalArgumentException("Retries cannot be negative");
         }
     }
 
@@ -46,6 +60,10 @@ public class ProcessWorkItemHandlerException extends RuntimeException {
 
     public HandlingStrategy getStrategy() {
         return strategy;
+    }
+
+    public int getRetries() {
+        return retries;
     }
 
 }
