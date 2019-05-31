d1d0176c93314786bc70d899a12d4b5560397a43
tarilabs <matteo.mortari@gmail.com>
2019-05-31 14:55:10 +0200

    .

--- kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/lang/ast/FormalParameterNode.java
+++ kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/lang/ast/FormalParameterNode.java
@@ -18,6 +18,7 @@ package org.kie.dmn.feel.lang.ast;
 
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.kie.dmn.feel.lang.EvaluationContext;
+import org.kie.dmn.feel.lang.SimpleType;
 import org.kie.dmn.feel.lang.Type;
 import org.kie.dmn.feel.lang.types.BuiltInType;
 import org.kie.dmn.feel.runtime.functions.BaseFEELFunction;
@@ -36,7 +37,7 @@ public class FormalParameterNode extends BaseNode {
         } else {
             TypeNode synthetic = new TypeNode();
             synthetic.copyLocationAttributesFrom(name);
-            synthetic.setText("Any");
+            synthetic.setText(SimpleType.ANY);
             this.type = synthetic;
         }
     }
