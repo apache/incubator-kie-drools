1a0cf799174b96b72b6a5195e8f562a99fd348d7
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2019-07-12 12:54:46 +0200

    Remove SA from the blueprints

--- optaplanner-core/src/main/java/org/optaplanner/core/config/localsearch/LocalSearchType.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/config/localsearch/LocalSearchType.java
@@ -16,6 +16,8 @@
 
 package org.optaplanner.core.config.localsearch;
 
+import java.util.Arrays;
+
 public enum LocalSearchType {
     HILL_CLIMBING,
     TABU_SEARCH,
@@ -28,7 +30,10 @@ public enum LocalSearchType {
      * @return {@link #values()} without duplicates (abstract types that end up behaving as one of the other types).
      */
     public static LocalSearchType[] getBluePrintTypes() {
-        return values();
+        return Arrays.stream(values())
+                // Workaround for https://issues.jboss.org/browse/PLANNER-1294
+                .filter(localSearchType -> localSearchType != SIMULATED_ANNEALING)
+                .toArray(LocalSearchType[]::new);
     }
 
 }
