57d5e9b4fa7821a2d0160b5ca796361163ee8c3a
Luca Molteni <volothamp@gmail.com>
2019-07-15 15:58:55 +0200

    Separated construction generation

--- drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/declaredtype/GeneratedClassDeclaration.java
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/declaredtype/GeneratedClassDeclaration.java
@@ -10,20 +10,15 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 import com.github.javaparser.ast.Modifier;
-import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.comments.JavadocComment;
 import com.github.javaparser.ast.expr.FieldAccessExpr;
 import com.github.javaparser.ast.expr.LongLiteralExpr;
-import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.ast.expr.NormalAnnotationExpr;
 import com.github.javaparser.ast.nodeTypes.NodeWithAnnotations;
-import com.github.javaparser.ast.stmt.BlockStmt;
-import com.github.javaparser.ast.stmt.ExpressionStmt;
 import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.type.PrimitiveType;
 import com.github.javaparser.ast.type.Type;
@@ -43,7 +38,6 @@ import org.kie.api.definition.type.Timestamp;
 import org.kie.soup.project.datamodel.commons.types.TypeResolver;
 
 import static com.github.javaparser.StaticJavaParser.parseExpression;
-import static com.github.javaparser.StaticJavaParser.parseStatement;
 import static com.github.javaparser.StaticJavaParser.parseType;
 import static com.github.javaparser.ast.NodeList.nodeList;
 import static java.text.MessageFormat.format;
@@ -155,57 +149,7 @@ class GeneratedClassDeclaration {
             }
         }
 
-        boolean createFullArgsConstructor = typeFields.length < 65;
-        ConstructorDeclaration fullArgumentsCtor = null;
-        NodeList<Statement> ctorFieldStatement = null;
-
-        if (createFullArgsConstructor) {
-            fullArgumentsCtor = generatedClass.addConstructor(Modifier.publicModifier().getKeyword());
-            ctorFieldStatement = nodeList();
-
-            MethodCallExpr superCall = new MethodCallExpr(null, "super");
-            for (TypeFieldDescr typeFieldDescr : inheritedFields) {
-                String fieldName = typeFieldDescr.getFieldName();
-                addCtorArg(fullArgumentsCtor, typeFieldDescr.getPattern().getObjectType(), fieldName);
-                superCall.addArgument(fieldName);
-                if (typeFieldDescr.getAnnotation("key") != null) {
-                    keyFields.add(typeFieldDescr);
-                }
-            }
-            ctorFieldStatement.add(new ExpressionStmt(superCall));
-        }
-
-        for (TypeFieldDescr typeFieldDescr : typeFields) {
-            String fieldName = typeFieldDescr.getFieldName();
-            Type returnType = parseType(typeFieldDescr.getPattern().getObjectType());
-            if (createFullArgsConstructor) {
-                addCtorArg(fullArgumentsCtor, returnType, fieldName);
-                ctorFieldStatement.add(replaceFieldName(parseStatement("this.__fieldName = __fieldName;"), fieldName));
-            }
-
-            if (createFullArgsConstructor) {
-                fullArgumentsCtor.setBody(new BlockStmt(ctorFieldStatement));
-            }
-        }
-
-        if (!keyFields.isEmpty() && keyFields.size() != inheritedFields.size() + typeFields.length) {
-            ConstructorDeclaration keyArgumentsCtor = generatedClass.addConstructor(Modifier.publicModifier().getKeyword());
-            NodeList<Statement> ctorKeyFieldStatement = nodeList();
-            MethodCallExpr keySuperCall = new MethodCallExpr(null, "super");
-            ctorKeyFieldStatement.add(new ExpressionStmt(keySuperCall));
-
-            for (TypeFieldDescr typeFieldDescr : keyFields) {
-                String fieldName = typeFieldDescr.getFieldName();
-                addCtorArg(keyArgumentsCtor, typeFieldDescr.getPattern().getObjectType(), fieldName);
-                if (typeDeclaration.getFields().get(fieldName) != null) {
-                    ctorKeyFieldStatement.add(replaceFieldName(parseStatement("this.__fieldName = __fieldName;"), fieldName));
-                } else {
-                    keySuperCall.addArgument(fieldName);
-                }
-            }
-
-            keyArgumentsCtor.setBody(new BlockStmt(ctorKeyFieldStatement));
-        }
+        new FullArgumentConstructor(typeDeclaration).generateConstructor(generatedClass, inheritedFields, typeFields, keyFields);
 
         if (!keyFields.isEmpty()) {
             generatedClass.addMember(generatedEqualsMethod.method());
@@ -288,14 +232,6 @@ class GeneratedClassDeclaration {
         }
 
         return sortedTypes;
-    }
-
-    private static void addCtorArg(ConstructorDeclaration fullArgumentsCtor, String typeName, String fieldName) {
-        addCtorArg(fullArgumentsCtor, parseType(typeName), fieldName);
-    }
-
-    private static void addCtorArg(ConstructorDeclaration fullArgumentsCtor, Type fieldType, String fieldName) {
-        fullArgumentsCtor.addParameter(fieldType, fieldName);
     }
 
     private List<TypeFieldDescr> findInheritedDeclaredFields() {
