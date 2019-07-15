fcda46ccb8d45717ecae931a07f6432836231343
Luca Molteni <volothamp@gmail.com>
2019-07-15 14:02:49 +0200

    Generated constructor

---
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/declaredtype/GeneratedConstructor.java
@@ -0,0 +1,11 @@
+package org.drools.modelcompiler.builder.generator.declaredtype;
+
+interface GeneratedConstructor {
+
+}
+
+class FullArgumentConstrucotr implements GeneratedConstructor {
+
+}
+
+
