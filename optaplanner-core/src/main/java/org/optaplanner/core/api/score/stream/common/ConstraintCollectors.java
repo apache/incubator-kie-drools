0d7a8626269f1f5c4524d27fc26707b1001e7d1b
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2019-07-15 14:19:09 +0200

    sumLong() instead of sum() to avoid ambigous method call

--- optaplanner-core/src/main/java/org/optaplanner/core/api/score/stream/common/ConstraintCollectors.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/api/score/stream/common/ConstraintCollectors.java
@@ -166,7 +166,7 @@ public final class ConstraintCollectors {
                 resultContainer -> resultContainer[0]);
     }
 
-    public static <A, B> BiConstraintCollector<A, B, ?, Integer> sum(ToIntBiFunction<? super A, ? super B> groupValueMapping) {
+    public static <A, B> BiConstraintCollector<A, B, ?, Integer> sumLong(ToIntBiFunction<? super A, ? super B> groupValueMapping) {
         return new BiConstraintCollector<>(
                 () -> new int[1],
                 (resultContainer, a, b) -> {
@@ -177,7 +177,7 @@ public final class ConstraintCollectors {
                 resultContainer -> resultContainer[0]);
     }
 
-    public static <A, B> BiConstraintCollector<A, B, ?, Long> sum(ToLongBiFunction<? super A, ? super B> groupValueMapping) {
+    public static <A, B> BiConstraintCollector<A, B, ?, Long> sumLong(ToLongBiFunction<? super A, ? super B> groupValueMapping) {
         return new BiConstraintCollector<>(
                 () -> new long[1],
                 (resultContainer, a, b) -> {
@@ -187,7 +187,6 @@ public final class ConstraintCollectors {
                 },
                 resultContainer -> resultContainer[0]);
     }
-
 
     private ConstraintCollectors() {
     }
