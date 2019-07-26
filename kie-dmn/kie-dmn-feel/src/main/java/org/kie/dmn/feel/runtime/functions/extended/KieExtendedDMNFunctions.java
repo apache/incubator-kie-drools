9e09b6a963df4ecb9b647110211ed45c7815bc87
Edson Tirelli <ed.tirelli@gmail.com>
2019-07-26 10:17:04 -0400

    DROOLS-3594: adding meets() and met by() functions (#2465)
    * DROOLS-3594: implementing meets() function
    
    * DROOLS-3594: adding met by() function
    
    * DROOLS-3594: adding met by() function

--- kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/extended/KieExtendedDMNFunctions.java
+++ kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/extended/KieExtendedDMNFunctions.java
@@ -19,17 +19,7 @@ package org.kie.dmn.feel.runtime.functions.extended;
 import java.util.stream.Stream;
 
 import org.kie.dmn.feel.runtime.FEELFunction;
-import org.kie.dmn.feel.runtime.functions.interval.AfterFunction;
-import org.kie.dmn.feel.runtime.functions.interval.BeforeFunction;
-import org.kie.dmn.feel.runtime.functions.interval.CoincidesFunction;
-import org.kie.dmn.feel.runtime.functions.interval.DuringFunction;
-import org.kie.dmn.feel.runtime.functions.interval.FinishedByFunction;
-import org.kie.dmn.feel.runtime.functions.interval.FinishesFunction;
-import org.kie.dmn.feel.runtime.functions.interval.IncludesFunction;
-import org.kie.dmn.feel.runtime.functions.interval.OverlappedByFunction;
-import org.kie.dmn.feel.runtime.functions.interval.OverlapsFunction;
-import org.kie.dmn.feel.runtime.functions.interval.StartedByFunction;
-import org.kie.dmn.feel.runtime.functions.interval.StartsFunction;
+import org.kie.dmn.feel.runtime.functions.interval.*;
 import org.kie.dmn.feel.runtime.functions.twovaluelogic.NNAllFunction;
 import org.kie.dmn.feel.runtime.functions.twovaluelogic.NNAnyFunction;
 import org.kie.dmn.feel.runtime.functions.twovaluelogic.NNCountFunction;
@@ -81,6 +71,8 @@ public class KieExtendedDMNFunctions {
                                                                         IncludesFunction.INSTANCE,
                                                                         OverlapsFunction.INSTANCE,
                                                                         OverlappedByFunction.INSTANCE,
+                                                                        MeetsFunction.INSTANCE,
+                                                                        MetByFunction.INSTANCE
 
     };
 
