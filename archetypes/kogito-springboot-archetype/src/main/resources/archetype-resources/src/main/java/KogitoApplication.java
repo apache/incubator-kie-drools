905ab2e400748fbb41f1706a29bcd944e6c23e3b
Maciej Swiderski <swiderski.maciej@gmail.com>
2019-08-21 16:52:51 +0200

    KOGITO-166 - SpringBoot archetype does not work with custom groupId (#123)

--- archetypes/kogito-springboot-archetype/src/main/resources/archetype-resources/src/main/java/KogitoApplication.java
+++ archetypes/kogito-springboot-archetype/src/main/resources/archetype-resources/src/main/java/KogitoApplication.java
@@ -3,7 +3,7 @@ package ${package};
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={"${package}.**"})
+@SpringBootApplication(scanBasePackages={"${package}.**", "${groupId}.**"})
 public class KogitoApplication {
 
     public static void main(String[] args) {
