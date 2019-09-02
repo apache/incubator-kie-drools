6244f5a59d427cde1e0bcccfcf2d3b8a134b3a37
tarilabs <matteo.mortari@gmail.com>
2019-09-02 15:30:47 +0200

    DROOLS-4488 DMN runtime listener from kmodule (followup for Kogito)

--- kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/impl/DMNRuntimeImpl.java
+++ kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/impl/DMNRuntimeImpl.java
@@ -91,19 +91,21 @@ public class DMNRuntimeImpl
             if (knowledgeBase instanceof KnowledgeBaseImpl) {
                 KnowledgeBaseImpl knowledgeBaseImpl = (KnowledgeBaseImpl) knowledgeBase;
                 KieContainerImpl kieContainer = (KieContainerImpl) knowledgeBaseImpl.getKieContainer();
-                KieBaseModelImpl kieBaseModel = (KieBaseModelImpl) kieContainer.getKieProject().getKieBaseModel(knowledgeBase.getId());
-                for (Entry<String, String> kv : kieBaseModel.getKModule().getConfigurationProperties().entrySet()) {
-                    String k = kv.getKey();
-                    if (k != null && k.startsWith(DMNAssemblerService.DMN_RUNTIME_LISTENER_PREFIX)) {
-                        if (ClassLoaderUtil.CAN_PLATFORM_CLASSLOAD) {
-                            try {
-                                DMNRuntimeEventListener runtimeListenerInstance = (DMNRuntimeEventListener) knowledgeBase.getRootClassLoader().loadClass(kv.getValue()).newInstance();
-                                this.addListener(runtimeListenerInstance);
-                            } catch (Exception e) {
-                                logger.error("Cannot perform classloading of runtime listener: {}", kv, e);
+                if (kieContainer != null) {
+                    KieBaseModelImpl kieBaseModel = (KieBaseModelImpl) kieContainer.getKieProject().getKieBaseModel(knowledgeBase.getId());
+                    for (Entry<String, String> kv : kieBaseModel.getKModule().getConfigurationProperties().entrySet()) {
+                        String k = kv.getKey();
+                        if (k != null && k.startsWith(DMNAssemblerService.DMN_RUNTIME_LISTENER_PREFIX)) {
+                            if (ClassLoaderUtil.CAN_PLATFORM_CLASSLOAD) {
+                                try {
+                                    DMNRuntimeEventListener runtimeListenerInstance = (DMNRuntimeEventListener) knowledgeBase.getRootClassLoader().loadClass(kv.getValue()).newInstance();
+                                    this.addListener(runtimeListenerInstance);
+                                } catch (Exception e) {
+                                    logger.error("Cannot perform classloading of runtime listener: {}", kv, e);
+                                }
+                            } else {
+                                logger.error("This platform does not support classloading of runtime listener: {}", kv);
                             }
-                        } else {
-                            logger.error("This platform does not support classloading of runtime listener: {}", kv);
                         }
                     }
                 }
