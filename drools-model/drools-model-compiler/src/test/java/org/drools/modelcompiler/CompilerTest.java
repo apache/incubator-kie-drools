1c9f4a62fb3fd56c110c5e38ff5dd08c00983f1b
Luca Molteni <volothamp@gmail.com>
2019-09-18 13:19:05 +0200

    Moved test to AccumulateTest.java

--- drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/CompilerTest.java
+++ drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/CompilerTest.java
@@ -2032,12 +2032,3 @@ public class CompilerTest extends BaseModelTest {
                         "    max($time.getTime()))\n" +
                         "then\n" +
                         "end\n";
-
-        KieSession ksession = getKieSession(str);
-
-        StockTick st = new StockTick("RHT");
-        st.setTimeField(new Date().getTime());
-        ksession.insert(st);
-        Assertions.assertThat(ksession.fireAllRules()).isEqualTo(1);;
-    }
-}
