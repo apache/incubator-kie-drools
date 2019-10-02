ad87d36b99f338e258cc9bfbc0c7b9cb9beb0b93
Jozef Marko <jomarko@redhat.com>
2019-10-02 14:36:34 +0200

    Remove unsused import

--- drools-workbench-models/drools-workbench-models-guided-dtable/src/main/java/org/drools/workbench/models/guided/dtable/shared/validation/DecisionTableValidator.java
+++ drools-workbench-models/drools-workbench-models-guided-dtable/src/main/java/org/drools/workbench/models/guided/dtable/shared/validation/DecisionTableValidator.java
@@ -17,7 +17,6 @@ package org.drools.workbench.models.guided.dtable.shared.validation;
 
 import java.util.Set;
 
-import org.drools.workbench.models.datamodel.rule.Attribute;
 import org.drools.workbench.models.guided.dtable.shared.model.AttributeCol52;
 import org.drools.workbench.models.guided.dtable.shared.model.GuidedDecisionTable52;
 
