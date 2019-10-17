e28ab5bc27ea920c9543b9565d1c2a2e735f7ae9
Jozef Marko <jomarko@redhat.com>
2019-10-17 09:47:18 +0200

    DROOLS-4448: Increase coverage

--- drools-workbench-models/drools-workbench-models-commons/src/test/java/org/drools/workbench/models/commons/backend/rule/RuleModelDRLPersistenceTest.java
+++ drools-workbench-models/drools-workbench-models-commons/src/test/java/org/drools/workbench/models/commons/backend/rule/RuleModelDRLPersistenceTest.java
@@ -1371,6 +1371,7 @@ public class RuleModelDRLPersistenceTest extends BaseRuleModelTest {
         RuleModel model = new RuleModel();
         model.name = "test expressions LocalDate";
         FactPattern personPattern = new FactPattern("Person");
+
         SingleFieldConstraintEBLeftSide fieldIsEqualConstraint = new SingleFieldConstraintEBLeftSide();
         fieldIsEqualConstraint.getExpressionLeftSide().appendPart(new ExpressionUnboundFact(personPattern.getFactType()));
         fieldIsEqualConstraint.getExpressionLeftSide().appendPart(new ExpressionField("field1",
@@ -1379,14 +1380,25 @@ public class RuleModelDRLPersistenceTest extends BaseRuleModelTest {
         fieldIsEqualConstraint.setOperator("==");
         fieldIsEqualConstraint.setValue("27-Jun-2011");
         fieldIsEqualConstraint.setConstraintValueType(SingleFieldConstraint.TYPE_LITERAL);
+
+        SingleFieldConstraintEBLeftSide fieldIsNowConstraint = new SingleFieldConstraintEBLeftSide();
+        fieldIsNowConstraint.getExpressionLeftSide().appendPart(new ExpressionUnboundFact(personPattern.getFactType()));
+        fieldIsNowConstraint.getExpressionLeftSide().appendPart(new ExpressionField("field2",
+                                                                                    "java.time.LocalDate",
+                                                                                    DataType.TYPE_LOCAL_DATE));
+        fieldIsNowConstraint.setOperator("==");
+        fieldIsNowConstraint.setValue("java.time.LocalDate.now()");
+        fieldIsNowConstraint.setConstraintValueType(SingleFieldConstraint.TYPE_RET_VALUE);
+
         personPattern.addConstraint(fieldIsEqualConstraint);
+        personPattern.addConstraint(fieldIsNowConstraint);
 
         model.addLhsItem(personPattern);
 
         String expected = "rule \"test expressions LocalDate\""
                 + "dialect \"mvel\""
                 + "when "
-                + "Person( field1 == \"27-Jun-2011\" )"
+                + "Person( field1 == \"27-Jun-2011\", field2 == ( java.time.LocalDate.now() ) )"
                 + "then "
                 + "end";
 
@@ -2057,15 +2069,22 @@ public class RuleModelDRLPersistenceTest extends BaseRuleModelTest {
                                                             DataType.TYPE_LOCAL_DATE));
             model.addRhsItem(celebrateOn1);
 
+            ActionInsertFact celebrateNow = new ActionInsertFact("Birthday");
+            celebrateNow.addFieldValue(new ActionFieldValue("dobPreciseNow",
+                                                            "java.time.LocalDate.now()",
+                                                            DataType.TYPE_LOCAL_DATE) {{
+                setNature(FieldNatureType.TYPE_FORMULA);
+            }});
+            model.addRhsItem(celebrateNow);
+
             String result = RuleModelDRLPersistenceImpl.getInstance().marshal(model);
 
             assertTrue(result.indexOf("java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat(\"dd-MMM-yyyy\");") != -1);
             assertTrue(result.indexOf("fact0.setDob( sdf.parse(\"31-Jan-2000\"") != -1);
             assertTrue(result.indexOf("java.time.format.DateTimeFormatter dtf = java.time.format.DateTimeFormatter.ofPattern(\"dd-MMM-yyyy\");") != -1);
             assertTrue(result.indexOf("fact1.setDobPrecise( java.time.LocalDate.parse(\"31-Jan-2000\", dtf") != -1);
-
-            checkMarshalling(null,
-                             model);
+            assertTrue("Demanded LocalDate formula not found in: " + result,
+                       result.contains("fact2.setDobPreciseNow( java.time.LocalDate.now() )"));
         } finally {
             if (oldValue == null) {
                 System.clearProperty("drools.dateformat");
