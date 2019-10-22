14d9de5bb275534791073c30713d67ec44325153
Luca Molteni <volothamp@gmail.com>
2019-10-22 13:52:40 +0200

    Fix porting

--- drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/ast/MapPutExprT.java
+++ drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/ast/MapPutExprT.java
@@ -8,16 +8,21 @@ import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 
+import static org.drools.mvelcompiler.util.CoercionUtils.PUT_CALL;
+import static org.drools.mvelcompiler.util.CoercionUtils.coerceMapValueToString;
+
 public class MapPutExprT implements TypedExpression {
 
     private final TypedExpression name;
     private final Expression key;
     private final TypedExpression value;
+    private final Optional<Type> type;
 
-    public MapPutExprT(TypedExpression name, Expression key, TypedExpression value) {
+    public MapPutExprT(TypedExpression name, Expression key, TypedExpression value, Optional<Type> type) {
         this.name = name;
         this.key = key;
         this.value = value;
+        this.type = type;
     }
 
     @Override
@@ -27,8 +32,11 @@ public class MapPutExprT implements TypedExpression {
 
     @Override
     public Node toJavaExpression() {
+        final Expression originalValue = (Expression) this.value.toJavaExpression();
+        final Expression coercedValue = coerceMapValueToString(type, originalValue);
+
         return new MethodCallExpr((Expression) name.toJavaExpression(),
-                                  "put",
-                                  NodeList.nodeList(key, (Expression) value.toJavaExpression()));
+                                  PUT_CALL,
+                                  NodeList.nodeList(key, coercedValue));
     }
 }
