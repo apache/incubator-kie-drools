e48ab29ba5c7135bef92559d41656c150cce58ed
yesamer <yamer@redhat.com>
2019-11-05 11:44:56 +0100

    DROOLS-4522: Handling of Expression FactMapping type.

--- drools-scenario-simulation/drools-scenario-simulation-api/src/main/java/org/drools/scenariosimulation/api/model/FactMappingValueType.java
+++ drools-scenario-simulation/drools-scenario-simulation-api/src/main/java/org/drools/scenariosimulation/api/model/FactMappingValueType.java
@@ -18,9 +18,8 @@ package org.drools.scenariosimulation.api.model;
 /**
     It assigns a Type to the <code>FactMapping</code> value. Used <b>ONLY</b> in FrontEnd side, to determine if a
     column value should be managed as an Expression.
-    - EXPRESSION: The value associated with the Fact is of Expression type, to be evaluated in the class defined in
-                  <code>FactMapping.className</code>
-    - NOT_EXPRESSION: The specific value-type is defined by its content defined in <code>FactMapping.className</code>
+    - EXPRESSION: The value associated with the Fact is of Expression type.
+    - NOT_EXPRESSION: The specific value-type is defined by its content.
  */
 public enum FactMappingValueType {
     EXPRESSION,
