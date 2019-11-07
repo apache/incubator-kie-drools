43786a48e1f7c97aeeda3ece5c926af4b37e297d
Michael Anstis <manstis@users.noreply.github.com>
2019-11-07 09:21:47 +0000

    DROOLS-4725: Cannot validate list in legacy Test Scenario (#2635)
    * DROOLS-4725: Cannot validate list in legacy Test Scenario
    
    * Updates following peer review.
    
    * DROOLS-4725: Increase coverage

--- drools-workbench-models/drools-workbench-models-test-scenarios/src/test/java/org/drools/workbench/models/testscenarios/backend/verifiers/FactFieldValueVerifierTest.java
+++ drools-workbench-models/drools-workbench-models-test-scenarios/src/test/java/org/drools/workbench/models/testscenarios/backend/verifiers/FactFieldValueVerifierTest.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -11,11 +11,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 package org.drools.workbench.models.testscenarios.backend.verifiers;
-
-import static org.mockito.Mockito.mock;
 
 import java.sql.Date;
 import java.util.ArrayList;
@@ -23,13 +21,17 @@ import java.util.HashMap;
 import java.util.List;
 
 import junit.framework.TestCase;
-
 import org.drools.workbench.models.testscenarios.backend.SqlDateWrapper;
+import org.drools.workbench.models.testscenarios.backend.StringCollection;
 import org.drools.workbench.models.testscenarios.shared.VerifyField;
+import org.junit.Test;
 import org.kie.soup.project.datamodel.commons.types.TypeResolver;
+
+import static org.mockito.Mockito.mock;
 
 public class FactFieldValueVerifierTest extends TestCase {
 
+    @Test
     public void testSQLDate() throws Exception {
 
         SqlDateWrapper sqlDateWrapper = new SqlDateWrapper();
@@ -48,6 +50,107 @@ public class FactFieldValueVerifierTest extends TestCase {
 
         List<VerifyField> fieldValues = new ArrayList<VerifyField>();
         VerifyField verifyField = new VerifyField("sqlDate", "12-DEC-2012", "==");
+        fieldValues.add(verifyField);
+
+        verifier.checkFields(fieldValues);
+
+        assertTrue(verifyField.getSuccessResult());
+    }
+
+    @Test
+    public void testExpectedList() throws Exception {
+
+        final StringCollection collection = new StringCollection();
+        collection.getList().add("Edam");
+        collection.getList().add("Cheddar");
+
+        final HashMap<String, Object> populatedData = new HashMap<>();
+        populatedData.put("collection", collection);
+
+        final TypeResolver typeResolver = mock(TypeResolver.class);
+
+        final FactFieldValueVerifier verifier = new FactFieldValueVerifier(populatedData,
+                                                                           "collection",
+                                                                           collection,
+                                                                           typeResolver);
+
+        final List<VerifyField> fieldValues = new ArrayList<>();
+        final VerifyField verifyField = new VerifyField("list", "=[\"Edam\", \"Cheddar\"]", "==");
+        fieldValues.add(verifyField);
+
+        verifier.checkFields(fieldValues);
+
+        assertTrue(verifyField.getSuccessResult());
+    }
+
+    @Test
+    public void testExpectedListComplexValues() throws Exception {
+
+        final StringCollection collection = new StringCollection();
+        collection.getList().add("Edam, Mature");
+        collection.getList().add("(Cheddar, Premature)");
+
+        final HashMap<String, Object> populatedData = new HashMap<>();
+        populatedData.put("collection", collection);
+
+        final TypeResolver typeResolver = mock(TypeResolver.class);
+
+        final FactFieldValueVerifier verifier = new FactFieldValueVerifier(populatedData,
+                                                                           "collection",
+                                                                           collection,
+                                                                           typeResolver);
+
+        final List<VerifyField> fieldValues = new ArrayList<>();
+        final VerifyField verifyField = new VerifyField("list", "=[\"Edam, Mature\", \"(Cheddar, Premature)\"]", "==");
+        fieldValues.add(verifyField);
+
+        verifier.checkFields(fieldValues);
+
+        assertTrue(verifyField.getSuccessResult());
+    }
+
+    @Test
+    public void testExpectedListIsEmpty() throws Exception {
+
+        final StringCollection collection = new StringCollection();
+
+        final HashMap<String, Object> populatedData = new HashMap<>();
+        populatedData.put("collection", collection);
+
+        final TypeResolver typeResolver = mock(TypeResolver.class);
+
+        final FactFieldValueVerifier verifier = new FactFieldValueVerifier(populatedData,
+                                                                           "collection",
+                                                                           collection,
+                                                                           typeResolver);
+
+        final List<VerifyField> fieldValues = new ArrayList<>();
+        final VerifyField verifyField = new VerifyField("list", "=[]", "==");
+        fieldValues.add(verifyField);
+
+        verifier.checkFields(fieldValues);
+
+        assertTrue(verifyField.getSuccessResult());
+    }
+
+    @Test
+    public void testExpectedListHasDifferentContent() throws Exception {
+
+        final StringCollection collection = new StringCollection();
+        collection.getList().add("cheddar");
+
+        final HashMap<String, Object> populatedData = new HashMap<>();
+        populatedData.put("collection", collection);
+
+        final TypeResolver typeResolver = mock(TypeResolver.class);
+
+        final FactFieldValueVerifier verifier = new FactFieldValueVerifier(populatedData,
+                                                                           "collection",
+                                                                           collection,
+                                                                           typeResolver);
+
+        final List<VerifyField> fieldValues = new ArrayList<>();
+        final VerifyField verifyField = new VerifyField("list", "=[\"CHEDDAR\"]", "!=");
         fieldValues.add(verifyField);
 
         verifier.checkFields(fieldValues);
