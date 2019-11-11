d231192e3fde68383e2b5e1a44395f5334b9159c
Sara4994 <40579476+Sara4994@users.noreply.github.com>
2019-11-11 09:51:54 +0530

    KOGITO-548 - fixed favicon (#21)

--- packages/management-console/webpack.common.js
+++ packages/management-console/webpack.common.js
@@ -10,7 +10,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html')
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      favicon: 'src/favicon.ico'
     })
   ],
   module: {
