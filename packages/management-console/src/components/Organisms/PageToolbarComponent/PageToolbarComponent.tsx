97819d79cee13a4f61dd0f5332c4295385f21c15
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2019-11-14 23:22:50 +1000

    KOGITO-597 - Remove unused toolbar icons (#26)

--- packages/management-console/src/components/Organisms/PageToolbarComponent/PageToolbarComponent.tsx
+++ packages/management-console/src/components/Organisms/PageToolbarComponent/PageToolbarComponent.tsx
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
 import {
-  Button,
-  ButtonVariant,
   Dropdown,
   DropdownToggle,
-  KebabToggle,
   Toolbar,
   ToolbarGroup,
   ToolbarItem,
@@ -13,18 +10,10 @@ import {
 } from '@patternfly/react-core';
 import accessibleStyles from '@patternfly/react-styles/css/utilities/Accessibility/accessibility';
 import { css } from '@patternfly/react-styles';
-import { BellIcon, CogIcon } from '@patternfly/react-icons';
 import AboutModalBox from '../../Molecules/AboutModalComponent/AboutModal';
-import spacingStyles from '@patternfly/react-styles/css/utilities/Spacing/spacing';
 export interface IOwnProps {}
-export interface IStateProps {
-  isKebabDropdownOpen: boolean;
-  isDropdownOpen: boolean;
-  isModalOpen: boolean;
-}
 
 const PageToolbarComponent: React.FunctionComponent<IOwnProps> = () => {
-  const [isKebabDropdownOpen, setKebabDropdownOpen] = useState(false);
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const [modalToggle, setmodalToggle] = useState(false);
 
@@ -39,20 +28,6 @@ const PageToolbarComponent: React.FunctionComponent<IOwnProps> = () => {
   const onDropdownSelect = () => {
     setDropdownOpen(!isDropdownOpen);
   };
-  const onKebabDropdownSelect = () => {
-    setKebabDropdownOpen(!isKebabDropdownOpen);
-  };
-  const onKebabDropdownToggle = _isKebabDropdownOpen => {
-    setKebabDropdownOpen(_isKebabDropdownOpen);
-  };
-  const kebabDropdownItems = [
-    <DropdownItem key={1}>
-      <BellIcon /> Notifications
-    </DropdownItem>,
-    <DropdownItem key={2}>
-      <CogIcon /> Settings
-    </DropdownItem>
-  ];
   const userDropdownItems = [
     <DropdownItem key={1} onClick={handleModalToggle}>
       About
@@ -66,29 +41,7 @@ const PageToolbarComponent: React.FunctionComponent<IOwnProps> = () => {
     <React.Fragment>
       <AboutModalBox isOpenProp={modalToggle} handleModalToggleProp={handleModalToggle} />
       <Toolbar>
-        <ToolbarGroup className={css(accessibleStyles.screenReader, accessibleStyles.visibleOnLg)}>
-          <ToolbarItem>
-            <Button id="default-example-uid-01" aria-label="Notifications actions" variant={ButtonVariant.plain}>
-              <BellIcon />
-            </Button>
-          </ToolbarItem>
-          <ToolbarItem>
-            <Button id="default-example-uid-02" aria-label="Settings actions" variant={ButtonVariant.plain}>
-              <CogIcon />
-            </Button>
-          </ToolbarItem>
-        </ToolbarGroup>
         <ToolbarGroup>
-          <ToolbarItem className={css(accessibleStyles.hiddenOnLg, spacingStyles.mr_0)}>
-            <Dropdown
-              isPlain
-              position="right"
-              onSelect={onKebabDropdownSelect}
-              toggle={<KebabToggle onToggle={onKebabDropdownToggle} />}
-              isOpen={isKebabDropdownOpen}
-              dropdownItems={kebabDropdownItems}
-            />
-          </ToolbarItem>
           <ToolbarItem className={css(accessibleStyles.screenReader, accessibleStyles.visibleOnMd)}>
             <Dropdown
               isPlain
