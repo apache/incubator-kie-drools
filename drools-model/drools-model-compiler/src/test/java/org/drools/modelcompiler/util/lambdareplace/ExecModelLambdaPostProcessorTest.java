31edacea0e79d594888bfd4211df297ef4969fca
Luca Molteni <volothamp@gmail.com>
2019-11-27 12:20:16 +0100

    Do not convert consequence test

--- drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/util/lambdareplace/ExecModelLambdaPostProcessorTest.java
+++ drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/util/lambdareplace/ExecModelLambdaPostProcessorTest.java
@@ -72,25 +72,28 @@ public class ExecModelLambdaPostProcessorTest {
     }
 
 
-//    @Test
-//    public void doNotConvertConsequence() {
-//        String dslInput = "        org.drools.model.Rule rule = D.rule(\"R\").build(expr,\n" +
-//                "                                                       D.on(var_$p).execute((org.drools.model.Drools drools, Person $p) -> {\n" +
-//                "                                                           $p.setAge($p.getAge() + 1);\n" +
-//                "                                                           drools.update($p, mask_$p);\n" +
-//                "                                                       }));";
-//
-//        Statement expression = StaticJavaParser.parseStatement(dslInput);
-//
-//        PostProcessedExecModel postProcessedExecModel = new ExecModelLambdaPostProcessor(new HashMap<>(), "mypackage", "rulename", new ArrayList<>(), new ArrayList(), expression).convertLambdas();
-//
-//        String expectedResult = "        org.drools.model.Rule rule = D.rule(\"R\").build(expr,\n" +
-//                                                                        "D.on(var_$p).execute((org.drools.model.Drools drools, Person $p) -> {" +
-//                "                                                           $p.setAge($p.getAge() + 1);\n" +
-//                "                                                           drools.update($p, mask_$p);\n" +
-//                "                                                       }));";
-//
-//        assertEquals(StaticJavaParser.parseStatement(expectedResult), StaticJavaParser.parseStatement(postProcessedExecModel.getConvertedBlockAsString()));
-//
-//    }
+    @Test
+    public void convertFlowLambdaDoNotConvertConsequence() throws Exception {
+
+        CompilationUnit inputCU = StaticJavaParser.parseResource("org/drools/modelcompiler/util/lambdareplace/FlowDoNotConvertConsequenceTestHarness.java");
+
+        CompilationUnit clone = inputCU.clone();
+
+        new ExecModelLambdaPostProcessor(new HashMap<>(), "mypackage", "rulename", new ArrayList<>(), new ArrayList<>(), clone).convertLambdas();
+
+        MethodDeclaration expectedResult = inputCU.getClassByName("FlowDoNotConvertConsequenceTestHarness")
+                .map(c -> c.getMethodsByName("expectedOutput"))
+                .flatMap(methods -> methods.stream().findFirst())
+                .map(m -> m.setName("testMethod"))
+                .orElseThrow(RuntimeException::new);
+
+        MethodDeclaration actual = clone.getClassByName("FlowDoNotConvertConsequenceTestHarness")
+                .map(c -> c.getMethodsByName("inputMethod"))
+                .flatMap(methods -> methods.stream().findFirst())
+                .map(m -> m.setName("testMethod"))
+                .orElseThrow(RuntimeException::new);
+
+        assertThat(actual.toString(), equalToIgnoringWhiteSpace(expectedResult.toString()));
+
+    }
 }
