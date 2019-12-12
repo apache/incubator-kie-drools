21dd91cf3ff703f1f4c85a3b0f6962ec8b5a193c
mariofusco <mario.fusco@gmail.com>
2019-12-12 11:09:19 +0100

    [DROOLS-4864] avoid NPE when removing a rule a with an accumulate that didn't produce any result

--- drools-core/src/main/java/org/drools/core/phreak/AddRemoveRule.java
+++ drools-core/src/main/java/org/drools/core/phreak/AddRemoveRule.java
@@ -1044,7 +1044,10 @@ public class AddRemoveRule {
             }
         } else {
             if (lt.getContextObject() instanceof AccumulateContext) {
-                iterateLeftTuple( (( AccumulateContext ) lt.getContextObject()).getResultLeftTuple(), wm );
+                LeftTuple resultLt = (( AccumulateContext ) lt.getContextObject()).getResultLeftTuple();
+                if (resultLt != null) {
+                    iterateLeftTuple( resultLt, wm );
+                }
             }
             for (LeftTuple child = lt.getFirstChild(); child != null; child = child.getHandleNext()) {
                 for (LeftTuple peer = child; peer != null; peer = peer.getPeer()) {
