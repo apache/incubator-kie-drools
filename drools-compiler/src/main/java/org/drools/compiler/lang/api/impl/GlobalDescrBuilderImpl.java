f91c38c53dbc3f06d1e29dcdee91c2be7eb60bd3
Mario Fusco <mario.fusco@gmail.com>
2020-01-16 10:31:01 +0100

    Revert "[KOGITO-432] allow to declare RuleUnit data sources in drl (#2716)" (#2724)
    This reverts commit b8c6b4ebea092a9ba458e7468b8674daca352d43.

--- drools-compiler/src/main/java/org/drools/compiler/lang/api/impl/GlobalDescrBuilderImpl.java
+++ drools-compiler/src/main/java/org/drools/compiler/lang/api/impl/GlobalDescrBuilderImpl.java
@@ -15,7 +15,6 @@
 
 package org.drools.compiler.lang.api.impl;
 
-import org.drools.compiler.lang.api.AnnotationDescrBuilder;
 import org.drools.compiler.lang.api.GlobalDescrBuilder;
 import org.drools.compiler.lang.api.PackageDescrBuilder;
 import org.drools.compiler.lang.descr.GlobalDescr;
@@ -26,12 +25,6 @@ public class GlobalDescrBuilderImpl extends BaseDescrBuilderImpl<PackageDescrBui
 
     protected GlobalDescrBuilderImpl( PackageDescrBuilder parent ) {
         super( parent, new GlobalDescr() );
-    }
-
-    public AnnotationDescrBuilder<GlobalDescrBuilder> newAnnotation( String name ) {
-        AnnotationDescrBuilder<GlobalDescrBuilder> annotation = new AnnotationDescrBuilderImpl<>( this, name );
-        descr.addAnnotation( annotation.getDescr() );
-        return annotation;
     }
 
     public GlobalDescrBuilder type( String type ) {
