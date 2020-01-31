4c47c3f367473464803ed1d568c5067763442c8a
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2020-01-31 16:28:33 +0100

    [DROOLS-4933] Created pmml-runtime archetype

--- kie-pmml/kie-pmml-runtime/kie-pmml-runtime-archetype/Readme.md
+++ kie-pmml/kie-pmml-runtime/kie-pmml-runtime-archetype/Readme.md
@@ -3,14 +3,14 @@ Models Archetype
 
 Usage:
 
-    cd kie-pmml-models
+    cd kie-pmml-runtime
 
     mvn archetype:generate -B \ (not interactive)
     -DarchetypeGroupId=org.drools \
-    -DarchetypeArtifactId=kie-pmml-models-archetype \
+    -DarchetypeArtifactId=kie-pmml-runtime-archetype \
     -DarchetypeVersion=(current_kie_version) \
     -DmodelName=(model name, first letter capital) \ e.g. Testing
-    -DartifactId=kie-pmml-models-(model name lowercase) \ kie-pmml-models-testing
+    -DartifactId=kie-pmml-runtime-(model name lowercase) \ kie-pmml-runtime-testing
 
 N.B.
 
--- kie-pmml/kie-pmml-runtime/kie-pmml-runtime-archetype/src/main/resources/archetype-resources/src/main/java/__artifactModelName__/executor/PMML__modelName__ModelExecutor.java
+++ kie-pmml/kie-pmml-runtime/kie-pmml-runtime-archetype/src/main/resources/archetype-resources/src/main/java/__artifactModelName__/executor/PMML__modelName__ModelExecutor.java
@@ -1,5 +1,8 @@
+#set( $str = "" )
+#set( $dt = $str.getClass().forName("java.util.Date").newInstance() )
+#set( $year = $dt.getYear() + 1900 )
 /*
- * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ * Copyright ${year} Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,35 +16,40 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kie.pmml.runtime.regression.executor;
+package org.kie.pmml.runtime.${artifactModelName}.executor;
+
+import java.util.logging.Logger;
 
 import org.kie.api.pmml.PMML4Result;
 import org.kie.pmml.api.exceptions.KiePMMLException;
 import org.kie.pmml.api.model.KiePMMLModel;
 import org.kie.pmml.api.model.enums.PMML_MODEL;
-import org.kie.pmml.api.model.regression.KiePMMLRegressionModel;
+import org.kie.pmml.api.model.${artifactModelName}.KiePMML${modelName}Model;
 import org.kie.pmml.runtime.api.exceptions.KiePMMLModelException;
 import org.kie.pmml.runtime.api.executor.PMMLContext;
 import org.kie.pmml.runtime.core.executor.PMMLModelExecutor;
 
-import static org.kie.pmml.runtime.regression.executor.PMMLIsNotRegresssionModelExecutor.evaluateNotRegression;
-import static org.kie.pmml.runtime.regression.executor.PMMLIsRegresssionModelExecutor.evaluateRegression;
 
-public class PMMLRegressionModelExecutor implements PMMLModelExecutor {
+/**
+ * Default <code>PMMLModelExecutor</code> for <b>${modelName}</b>
+ */
+public class PMML${modelName}ModelExecutor implements PMMLModelExecutor {
 
-    @Override
-    public PMML_MODEL getPMMLModelType() {
-        return PMML_MODEL.REGRESSION_MODEL;
-    }
+    private static final Logger log = Logger.getLogger(PMML${modelName}ModelExecutor.class.getName());
+
+     @Override
+     public PMML_MODEL getPMMLModelType() {
+        return PMML_MODEL.${modelNameUppercase}_MODEL;
+     }
 
     @Override
     public PMML4Result evaluate(KiePMMLModel model, PMMLContext context) throws KiePMMLException {
-        if (!(model instanceof KiePMMLRegressionModel)) {
-            throw new KiePMMLModelException("Expected a KiePMMLRegressionModel, received a " + model.getClass().getName());
+        log.info("evaluate " + model + " " + context);
+         if (!(model instanceof KiePMML${modelName}Model)) {
+             throw new KiePMMLModelException("Expected a KiePMML${modelName}Model, received a " + model.getClass().getName());
         }
-        final KiePMMLRegressionModel regressionModel = (KiePMMLRegressionModel) model;
-        return (regressionModel).isRegression() ? evaluateRegression(regressionModel, context) : evaluateNotRegression(regressionModel, context);
-    }
-
+        // TODO
+        return null;
+     }
 
 }
