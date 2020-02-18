ff56833d80422e46da63d8619bde3361725b35c1
Mario Fusco <mario.fusco@gmail.com>
2020-02-18 17:10:04 +0100

    [KOGITO-1022] move quarkus extension back into kogito repo (#313)
    [KOGITO-1022] move quarkus extension back into kogito repo

---
+++ kogito-quarkus-extension/integration-test/src/test/java/io/quarkus/it/kogito/drools/DroolsTestIT.java
@@ -0,0 +1,7 @@
+package io.quarkus.it.kogito.drools;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class DroolsTestIT extends DroolsTest {
+}
---
+++ kogito-quarkus-extension/integration-test/src/test/java/io/quarkus/it/kogito/jbpm/OrdersProcessTestIT.java
@@ -0,0 +1,7 @@
+package io.quarkus.it.kogito.jbpm;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class OrdersProcessTestIT extends OrdersProcessTest {
+}
