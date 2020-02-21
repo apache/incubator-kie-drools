4da5d5c64321ae7c793abfaee91f4e737b9d8c96
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2020-02-21 16:10:38 +0100

    [DROOLS-3339] Refactored RegressionModelEvaluator; add tests

--- kie-pmml-new/kie-pmml-models/kie-pmml-models-regression/kie-pmml-models-regression-evaluator/src/main/java/org/kie/pmml/models/regression/evaluator/PMMLRegresssionModelEvaluator.java
+++ kie-pmml-new/kie-pmml-models/kie-pmml-models-regression/kie-pmml-models-regression-evaluator/src/main/java/org/kie/pmml/models/regression/evaluator/PMMLRegresssionModelEvaluator.java
@@ -23,11 +23,15 @@ import java.util.concurrent.atomic.AtomicReference;
 import org.apache.commons.math3.distribution.NormalDistribution;
 import org.kie.api.pmml.PMML4Result;
 import org.kie.api.pmml.PMMLRequestData;
+import org.kie.pmml.commons.model.enums.OP_TYPE;
+import org.kie.pmml.evaluator.api.exceptions.KiePMMLModelException;
 import org.kie.pmml.evaluator.api.executor.PMMLContext;
 import org.kie.pmml.models.regression.model.KiePMMLRegressionModel;
 import org.kie.pmml.models.regression.model.KiePMMLRegressionTable;
 import org.kie.pmml.models.regression.model.enums.REGRESSION_NORMALIZATION_METHOD;
 
+import static org.kie.pmml.commons.Constants.UNEXPECTED_NORMALIZATION_METHOD;
+import static org.kie.pmml.commons.Constants.UNEXPECTED_OP_TYPE;
 import static org.kie.pmml.commons.enums.StatusCode.OK;
 
 public class PMMLRegresssionModelEvaluator {
@@ -38,10 +42,10 @@ public class PMMLRegresssionModelEvaluator {
 
     public static PMML4Result evaluateRegression(KiePMMLRegressionModel regressionModel, PMMLContext context) {
         final List<KiePMMLRegressionTable> regressionTables = regressionModel.getRegressionTables();
-        return evaluateRegression(regressionModel.getTargetField(), regressionModel.getRegressionNormalizationMethod(), regressionTables.get(0), context.getRequestData());
+        return evaluateRegression(regressionModel.getTargetField(), regressionModel.getRegressionNormalizationMethod(), regressionModel.getTargetOpType(), regressionTables.get(0), context.getRequestData());
     }
 
-    public static PMML4Result evaluateRegression(String targetFieldName, REGRESSION_NORMALIZATION_METHOD regressionNormalizationMethod, KiePMMLRegressionTable regressionTable, PMMLRequestData requestData) {
+    public static PMML4Result evaluateRegression(String targetFieldName, REGRESSION_NORMALIZATION_METHOD regressionNormalizationMethod, OP_TYPE opType, KiePMMLRegressionTable regressionTable, PMMLRequestData requestData) {
         final AtomicReference<Double> result = new AtomicReference<>(regressionTable.getIntercept().doubleValue());
         Map<String, Double> resultMap = new HashMap<>();
         requestData.getRequestParams().forEach(parameterInfo -> {
@@ -50,33 +54,66 @@ public class PMMLRegresssionModelEvaluator {
         });
         PMMLRegresssionModelUtils.evaluatePredictorTerms(regressionTable, requestData.getRequestParams(), resultMap);
         resultMap.values().forEach(value -> result.accumulateAndGet(value, Double::sum));
-        if (regressionNormalizationMethod != null) {
-            switch (regressionNormalizationMethod) {
-                case SOFTMAX:
-                case LOGIT:
-                    result.updateAndGet(y -> 1.0 / (1.0 + Math.exp(-y)));
-                    break;
-                case EXP:
-                    result.updateAndGet(Math::exp);
-                    break;
-                case PROBIT:
-                    result.updateAndGet(y -> new NormalDistribution().cumulativeProbability(y));
-                    break;
-                case CLOGLOG:
-                    result.updateAndGet(y -> 1.0 - Math.exp(-Math.exp(y)));
-                    break;
-                case CAUCHIT:
-                    result.updateAndGet(y -> 0.5 + (1 / Math.PI) * Math.atan(y));
-                    break;
-                case NONE:
-                default:
-                    // no op
-            }
-        }
+        updateResult(regressionNormalizationMethod, opType, result);
         PMML4Result toReturn = new PMML4Result();
         toReturn.addResultVariable(targetFieldName, result.get());
         toReturn.setResultObjectName(targetFieldName);
         toReturn.setResultCode(OK.getName());
         return toReturn;
+    }
+
+    protected static void updateResult(REGRESSION_NORMALIZATION_METHOD regressionNormalizationMethod, OP_TYPE opType, final AtomicReference<Double> toUpdate) {
+        switch (regressionNormalizationMethod) {
+            case SOFTMAX:
+            case LOGIT:
+                switch (opType) {
+                    case CATEGORICAL:
+                        throw new KiePMMLModelException(String.format(UNEXPECTED_OP_TYPE, opType));
+                    default:
+                        toUpdate.updateAndGet(y -> 1.0 / (1.0 + Math.exp(-y)));
+                        return;
+                }
+            case EXP:
+                switch (opType) {
+                    case CATEGORICAL:
+                        throw new KiePMMLModelException(String.format(UNEXPECTED_OP_TYPE, opType));
+                    default:
+                        toUpdate.updateAndGet(Math::exp);
+                        return;
+                }
+            case PROBIT:
+                switch (opType) {
+                    case CATEGORICAL:
+                        throw new KiePMMLModelException(String.format(UNEXPECTED_OP_TYPE, opType));
+                    default:
+                        toUpdate.updateAndGet(y -> new NormalDistribution().cumulativeProbability(y));
+                        return;
+                }
+            case CLOGLOG:
+                switch (opType) {
+                    case CATEGORICAL:
+                        throw new KiePMMLModelException(String.format(UNEXPECTED_OP_TYPE, opType));
+                    default:
+                        toUpdate.updateAndGet(y -> 1.0 - Math.exp(-Math.exp(y)));
+                        return;
+                }
+            case CAUCHIT:
+                switch (opType) {
+                    case CATEGORICAL:
+                        throw new KiePMMLModelException(String.format(UNEXPECTED_OP_TYPE, opType));
+                    default:
+                        toUpdate.updateAndGet(y -> 0.5 + (1 / Math.PI) * Math.atan(y));
+                        return;
+                }
+            case NONE:
+                switch (opType) {
+                    case CATEGORICAL:
+                        throw new KiePMMLModelException(String.format(UNEXPECTED_OP_TYPE, opType));
+                    default:
+                        return;
+                }
+            default:
+                throw new KiePMMLModelException(String.format(UNEXPECTED_NORMALIZATION_METHOD, regressionNormalizationMethod));
+        }
     }
 }
--- kie-pmml-new/kie-pmml-models/kie-pmml-models-regression/kie-pmml-models-regression-evaluator/src/test/java/org/kie/pmml/models/regression/evaluator/PMMLRegresssionModelEvaluatorTest.java
+++ kie-pmml-new/kie-pmml-models/kie-pmml-models-regression/kie-pmml-models-regression-evaluator/src/test/java/org/kie/pmml/models/regression/evaluator/PMMLRegresssionModelEvaluatorTest.java
@@ -16,103 +16,208 @@
 
 package org.kie.pmml.models.regression.evaluator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.kie.api.pmml.PMML4Result;
 import org.kie.api.pmml.PMMLRequestData;
+import org.kie.pmml.commons.enums.StatusCode;
+import org.kie.pmml.commons.model.enums.MINING_FUNCTION;
+import org.kie.pmml.commons.model.enums.OP_TYPE;
+import org.kie.pmml.evaluator.api.exceptions.KiePMMLModelException;
+import org.kie.pmml.evaluator.api.executor.PMMLContext;
 import org.kie.pmml.evaluator.core.PMMLContextImpl;
 import org.kie.pmml.models.regression.model.KiePMMLRegressionModel;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.kie.pmml.models.regression.model.KiePMMLRegressionTable;
+import org.kie.pmml.models.regression.model.enums.REGRESSION_NORMALIZATION_METHOD;
+import org.kie.pmml.models.regression.model.predictors.KiePMMLCategoricalPredictor;
+import org.kie.pmml.models.regression.model.predictors.KiePMMLNumericPredictor;
+import org.kie.pmml.models.regression.model.predictors.KiePMMLPredictorTerm;
+import org.kie.pmml.models.regression.model.predictors.KiePMMLRegressionTablePredictor;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.kie.pmml.models.regression.evaluator.PMMLRegresssionModelEvaluator.evaluateRegression;
 
 public class PMMLRegresssionModelEvaluatorTest {
 
-    private static final Logger logger = LoggerFactory.getLogger(PMMLRegresssionModelEvaluatorTest.class);
-
-    private KiePMMLRegressionModel kiePMMLRegressionModel;
-
-    @Before
-    public void setUp() throws Exception {
-        kiePMMLRegressionModel = TestUtils.getKiePMMLRegressionModel();
-    }
-
-//    @Test(expected = KiePMMLInternalException.class)
-//    public void evaluateRegressionWithModelNoTargetField() {
-//        KiePMMLRegressionModel toTest = KiePMMLRegressionModel.builder(MODEL_NAME, _MINING_FUNCTION, Collections.singletonList(getKiePMMLRegressionTable()), _OP_TYPE)
-//                .withAlgorithmName(ALGORITHM_NAME)
-//                .withModelType(_MODEL_TYPE)
-//                .withRegressionNormalizationMethod(_REGRESSION_NORMALIZATION_METHOD)
-//                .withScorable(SCORABLE)
-//                .build();
-//        PMMLRequestData pmmlRequestData = getPMMLRequestData(MODEL_NAME, new HashMap<>());
-//        evaluateRegression(toTest, new PMMLContextImpl(pmmlRequestData));
-//    }
+    private static final String MODEL_NAME = "LinReg";
+    private static final String TARGET_FIELD_NAME = "fld4";
 
     @Test
-    public void evaluateRegressionWithModel() {
-        KiePMMLRegressionModel toTest = KiePMMLRegressionModel.builder(TestUtils.MODEL_NAME, TestUtils._MINING_FUNCTION, Collections.singletonList(TestUtils.getKiePMMLRegressionTable()), TestUtils._OP_TYPE)
-                .withAlgorithmName(TestUtils.ALGORITHM_NAME)
-                .withModelType(TestUtils._MODEL_TYPE)
-                .withRegressionNormalizationMethod(TestUtils._REGRESSION_NORMALIZATION_METHOD)
-                .withScorable(TestUtils.SCORABLE)
-                .withTargetField(TestUtils.TARGETFIELD_NAME)
+    public void evaluateClassification() {
+        final PMML4Result retrieved = PMMLRegresssionModelEvaluator.evaluateRegression(getModel(), getContext());
+        commonVerifyPMM4Result(retrieved);
+    }
+
+    @Test
+    public void testEvaluateClassification() {
+        final PMML4Result retrieved = PMMLRegresssionModelEvaluator.evaluateRegression(TARGET_FIELD_NAME, getModel().getRegressionNormalizationMethod(), OP_TYPE.ORDINAL, getTable(), getRequestData());
+        commonVerifyPMM4Result(retrieved);
+    }
+
+    @Test(expected = KiePMMLModelException.class)
+    public void updateResultSOFTMAXCATEGORICAL() {
+        PMMLRegresssionModelEvaluator.updateResult(REGRESSION_NORMALIZATION_METHOD.SOFTMAX, OP_TYPE.CATEGORICAL, new AtomicReference<>());
+    }
+
+    @Test(expected = KiePMMLModelException.class)
+    public void updateResultLOGITCATEGORICAL() {
+        PMMLRegresssionModelEvaluator.updateResult(REGRESSION_NORMALIZATION_METHOD.LOGIT, OP_TYPE.CATEGORICAL, new AtomicReference<>());
+    }
+
+    @Test(expected = KiePMMLModelException.class)
+    public void updateResultEXPCATEGORICAL() {
+        PMMLRegresssionModelEvaluator.updateResult(REGRESSION_NORMALIZATION_METHOD.EXP, OP_TYPE.CATEGORICAL, new AtomicReference<>());
+    }
+
+    @Test(expected = KiePMMLModelException.class)
+    public void updateResultPROBITCATEGORICAL() {
+        PMMLRegresssionModelEvaluator.updateResult(REGRESSION_NORMALIZATION_METHOD.PROBIT, OP_TYPE.CATEGORICAL, new AtomicReference<>());
+    }
+
+    @Test(expected = KiePMMLModelException.class)
+    public void updateResultCLOGLOGCATEGORICAL() {
+        PMMLRegresssionModelEvaluator.updateResult(REGRESSION_NORMALIZATION_METHOD.CLOGLOG, OP_TYPE.CATEGORICAL, new AtomicReference<>());
+    }
+
+    @Test(expected = KiePMMLModelException.class)
+    public void updateResultCAUCHITCATEGORICAL() {
+        PMMLRegresssionModelEvaluator.updateResult(REGRESSION_NORMALIZATION_METHOD.CAUCHIT, OP_TYPE.CATEGORICAL, new AtomicReference<>());
+    }
+
+    @Test(expected = KiePMMLModelException.class)
+    public void updateResultNONECATEGORICAL() {
+        PMMLRegresssionModelEvaluator.updateResult(REGRESSION_NORMALIZATION_METHOD.NONE, OP_TYPE.CATEGORICAL, new AtomicReference<>());
+    }
+
+    @Test
+    public void updateSOFTMAX() {
+        /*
+        predictedValue = 1/(1+exp(-y1))
+         */
+        AtomicReference<Double> toUpdate = new AtomicReference<>(2.2580000000000005);
+        double expected = 0.905338368331313;
+        PMMLRegresssionModelEvaluator.updateResult(REGRESSION_NORMALIZATION_METHOD.SOFTMAX, OP_TYPE.CONTINUOUS, toUpdate);
+        assertEquals(expected, toUpdate.get(), 0.0);
+    }
+
+    @Test
+    public void updateLOGIT() {
+        /*
+        predictedValue = 1/(1+exp(-y1))
+         */
+        AtomicReference<Double> toUpdate = new AtomicReference<>(2.2580000000000005);
+        double expected = 0.905338368331313;
+        PMMLRegresssionModelEvaluator.updateResult(REGRESSION_NORMALIZATION_METHOD.LOGIT, OP_TYPE.ORDINAL, toUpdate);
+        assertEquals(expected, toUpdate.get(), 0.0);
+    }
+
+    @Test
+    public void updateEXP() {
+        /*
+        predictedValue = exp(y1)
+         */
+        AtomicReference<Double> toUpdate = new AtomicReference<>(2.2580000000000005);
+        double expected = 9.56394214183812;
+        PMMLRegresssionModelEvaluator.updateResult(REGRESSION_NORMALIZATION_METHOD.EXP, OP_TYPE.ORDINAL, toUpdate);
+        assertEquals(expected, toUpdate.get(), 0.0);
+    }
+
+    @Test
+    public void updatePROBIT() {
+        /*
+        predictedValue = CDF(y1)
+         */
+        AtomicReference<Double> toUpdate = new AtomicReference<>(2.2580000000000005);
+        double expected = 0.9880271702826351;
+        PMMLRegresssionModelEvaluator.updateResult(REGRESSION_NORMALIZATION_METHOD.PROBIT, OP_TYPE.CONTINUOUS, toUpdate);
+        assertEquals(expected, toUpdate.get(), 0.0);
+    }
+
+    @Test
+    public void updateCLOGLOG() {
+        /*
+        predictedValue = 1 - exp( -exp( y1))
+         */
+        AtomicReference<Double> toUpdate = new AtomicReference<>(2.2580000000000005);
+        double expected = 0.9999297845469218;
+        PMMLRegresssionModelEvaluator.updateResult(REGRESSION_NORMALIZATION_METHOD.CLOGLOG, OP_TYPE.CONTINUOUS, toUpdate);
+        assertEquals(expected, toUpdate.get(), 0.0);
+    }
+
+    @Test
+    public void updateCAUCHIT() {
+        /*
+        predictedValue = 0.5 + (1/π) arctan(y1)
+         */
+        AtomicReference<Double> toUpdate = new AtomicReference<>(2.2580000000000005);
+        double expected = 0.8672938553679819;
+        PMMLRegresssionModelEvaluator.updateResult(REGRESSION_NORMALIZATION_METHOD.CAUCHIT, OP_TYPE.CONTINUOUS, toUpdate);
+        assertEquals(expected, toUpdate.get(), 0.0);
+    }
+
+    @Test
+    public void updateNONE() {
+        /*
+        predictedValue = y1
+         */
+        AtomicReference<Double> toUpdate = new AtomicReference<>(2.2580000000000005);
+        double expected = toUpdate.get();
+        PMMLRegresssionModelEvaluator.updateResult(REGRESSION_NORMALIZATION_METHOD.NONE, OP_TYPE.CONTINUOUS, toUpdate);
+        assertEquals(expected, toUpdate.get(), 0.0);
+    }
+
+    private void commonVerifyPMM4Result(PMML4Result toVerify) {
+        assertNotNull(toVerify);
+        assertEquals(StatusCode.OK.getName(), toVerify.getResultCode());
+        assertEquals(TARGET_FIELD_NAME, toVerify.getResultObjectName());
+        assertEquals(0.9999297845469218, toVerify.getResultVariables().get(TARGET_FIELD_NAME));
+    }
+
+    private PMMLContext getContext() {
+        return new PMMLContextImpl(getRequestData());
+    }
+
+    private PMMLRequestData getRequestData() {
+        Map<String, Object> inputMap = new HashMap<>();
+        inputMap.put("fld1", 0.9);
+        inputMap.put("fld2", 0.3);
+        inputMap.put("fld3", "x");
+        return TestUtils.getPMMLRequestData(MODEL_NAME, inputMap);
+    }
+
+    private KiePMMLRegressionModel getModel() {
+        return KiePMMLRegressionModel.builder(MODEL_NAME, MINING_FUNCTION.REGRESSION, Collections.singletonList(getTable()), OP_TYPE.CONTINUOUS)
+                .withRegressionNormalizationMethod(REGRESSION_NORMALIZATION_METHOD.CLOGLOG)
+                .withTargetField(TARGET_FIELD_NAME)
                 .build();
-        commonEvaluateWithModel(20, 1950, TestUtils.STREET, toTest);
-        commonEvaluateWithModel(20, 1950, TestUtils.CARPARK, toTest);
-        commonEvaluateWithModel(59, 3750, TestUtils.STREET, toTest);
-        commonEvaluateWithModel(35, 1800, TestUtils.CARPARK, toTest);
     }
 
-    @Test
-    public void evaluateRegressionWithTable() {
-        commonEvaluateWithTable(20, 1950, TestUtils.STREET);
-        commonEvaluateWithTable(20, 1950, TestUtils.CARPARK);
-        commonEvaluateWithTable(59, 3750, TestUtils.STREET);
-        commonEvaluateWithTable(35, 1800, TestUtils.CARPARK);
-    }
+    private KiePMMLRegressionTable getTable() {
+        Set<KiePMMLNumericPredictor> numericPredictors = new HashSet<>(Arrays.asList(
+                new KiePMMLNumericPredictor("fld1", 2, 5, Collections.emptyList()),
+                new KiePMMLNumericPredictor("fld2", 1, 2, Collections.emptyList())
+        ));
+        Set<KiePMMLCategoricalPredictor> categoricalPredictors = new HashSet<>(Arrays.asList(
+                new KiePMMLCategoricalPredictor("fld3", "x", -3, Collections.emptyList()),
+                new KiePMMLCategoricalPredictor("fld3", "y", 3, Collections.emptyList())
+        ));
 
-    private void commonEvaluateWithModel(int age, double salary, String carLocation, KiePMMLRegressionModel toTest) {
-        Map<String, Object> inputData = new HashMap<>();
-        inputData.put("age", age);
-        inputData.put("salary", salary);
-        inputData.put("car_location", carLocation);
-        final PMMLRequestData pmmlRequestData = TestUtils.getPMMLRequestData(TestUtils.MODEL_NAME, inputData);
-        PMML4Result retrieved = evaluateRegression(toTest, new PMMLContextImpl(pmmlRequestData));
-        commonEvaluate(retrieved, age, salary, carLocation);
-    }
-
-    private void commonEvaluateWithTable(int age, double salary, String carLocation) {
-        Map<String, Object> inputData = new HashMap<>();
-        inputData.put("age", age);
-        inputData.put("salary", salary);
-        inputData.put("car_location", carLocation);
-        final PMMLRequestData pmmlRequestData = TestUtils.getPMMLRequestData(TestUtils.MODEL_NAME, inputData);
-        PMML4Result retrieved = evaluateRegression(kiePMMLRegressionModel.getTargetField(), kiePMMLRegressionModel.getRegressionNormalizationMethod(), TestUtils.getKiePMMLRegressionTable(), pmmlRequestData);
-        commonEvaluate(retrieved, age, salary, carLocation);
-    }
-
-    private void commonEvaluate(PMML4Result toEvaluate, int age, double salary, String carLocation) {
-        assertNotNull(toEvaluate);
-        logger.debug(toEvaluate.toString());
-        assertNotNull(toEvaluate.getResultVariables());
-        assertTrue(toEvaluate.getResultVariables().containsKey(TestUtils.TARGETFIELD_NAME));
-        double expected = TestUtils.INTERCEPT + TestUtils.AGE_COEFF * age + TestUtils.SALARY_COEFF * salary;
-        if (TestUtils.CARPARK.equals(carLocation)) {
-            expected += TestUtils.CARPARK_COEFF;
-        } else if (TestUtils.STREET.equals(carLocation)) {
-            expected += TestUtils.STREET_COEFF;
-        }
-        double retrievedDouble = (double) toEvaluate.getResultVariables().get(TestUtils.TARGETFIELD_NAME);
-        assertEquals(expected, retrievedDouble, 0.00001);
-        logger.debug("Expected {} retrieved {}", expected, retrievedDouble);
+        List<KiePMMLRegressionTablePredictor> predictors = new ArrayList<>(numericPredictors);
+        KiePMMLPredictorTerm predictorTerm = new KiePMMLPredictorTerm("predTerm", predictors, 0.4, Collections.emptyList());
+        return KiePMMLRegressionTable.builder(0.5)
+                .withTargetCategory("clerical")
+                .withNumericPredictors(numericPredictors)
+                .withCategoricalPredictors(categoricalPredictors)
+                .withPredictorTerms(Collections.singleton(predictorTerm))
+                .build();
     }
 }
