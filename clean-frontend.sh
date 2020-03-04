34d9c2084b90b8eefa5def1d422356cfcba04af7
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2020-03-05 02:09:20 +1000

    KOGITO-1276 - Frontend build enhancements (#87)
    - Review NPM and Yarn versions
    - Use env var for NPM registry

--- clean-frontend.sh
+++ clean-frontend.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 find . -type d -name 'node_modules' -prune -exec rm -rf {} \;
+find . -type d -name 'node' -prune -exec rm -rf {} \;
 find . -type d -name 'dist' -prune -exec rm -rf {} \;
 find . -type f -name 'package-lock.json' -prune -exec rm -rf {} \;
 find . -type f -name 'yarn.lock' -prune -exec rm -rf {} \;
