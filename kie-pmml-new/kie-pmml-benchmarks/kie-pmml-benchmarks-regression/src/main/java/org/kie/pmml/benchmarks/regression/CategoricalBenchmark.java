b8ec487c4445c6bd0f52a071dc56819132fd20ba
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2020-03-09 16:56:57 +0100

    [DROOLS-3339] Fix import

--- kie-pmml-new/kie-pmml-benchmarks/kie-pmml-benchmarks-regression/src/main/java/org/kie/pmml/benchmarks/regression/CategoricalBenchmark.java
+++ kie-pmml-new/kie-pmml-benchmarks/kie-pmml-benchmarks-regression/src/main/java/org/kie/pmml/benchmarks/regression/CategoricalBenchmark.java
@@ -22,7 +22,6 @@ import org.kie.pmml.evaluator.core.PMMLContextImpl;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
