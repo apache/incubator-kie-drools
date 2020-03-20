62b88efec9b32129ccd8a09abb3f308d9e40e286
Julian Cui <jucui@redhat.com>
2020-03-20 10:39:52 -0400

    Add JSON-B integration docs

--- optaplanner-persistence/optaplanner-persistence-jsonb/src/main/java/org/optaplanner/persistence/jsonb/api/score/AbstractScoreJsonbAdapter.java
+++ optaplanner-persistence/optaplanner-persistence-jsonb/src/main/java/org/optaplanner/persistence/jsonb/api/score/AbstractScoreJsonbAdapter.java
@@ -19,12 +19,14 @@ package org.optaplanner.persistence.jsonb.api.score;
 import javax.json.bind.adapter.JsonbAdapter;
 
 import org.optaplanner.core.api.score.Score;
+import org.optaplanner.persistence.jsonb.api.OptaPlannerJsonbConfig;
 
 /**
  * JSON-B binding support for a {@link Score} type.
  * <p>
  * For example: use {@code @JsonbTypeAdapter(HardSoftScoreJsonbAdapter.class)}
  * on a {@code HardSoftScore score} field and it will be serialized to JSON as {@code "score":"-999hard/-999soft"}.
+ * Or better yet, use {@link OptaPlannerJsonbConfig} instead.
  * @see Score
  * @param <Score_> the actual score type
  */
