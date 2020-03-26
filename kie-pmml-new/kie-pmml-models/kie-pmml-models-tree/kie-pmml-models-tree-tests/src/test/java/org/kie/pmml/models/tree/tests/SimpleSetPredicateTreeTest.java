6a29b4acd58d61b8308ab9bc6e526590e013d897
Jiri Petrlik <jiripetrlik@gmail.com>
2020-03-26 15:48:27 +0100

    [DROOLS-5166] Test for simple set predicate in trees

---
+++ kie-pmml-new/kie-pmml-models/kie-pmml-models-tree/kie-pmml-models-tree-tests/src/test/java/org/kie/pmml/models/tree/tests/SimpleSetPredicateTreeTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.pmml.models.tree.tests;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.kie.api.pmml.PMML4Result;
+import org.kie.api.pmml.PMMLRequestData;
+import org.kie.pmml.commons.model.KiePMMLModel;
+import org.kie.pmml.evaluator.core.PMMLContextImpl;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(Parameterized.class)
+public class SimpleSetPredicateTreeTest extends AbstractPMMLTreeTest {
+
+    private static final String MODEL_NAME = "SimpleSetPredicateTreeModel";
+    private static final String PMML_SOURCE = "SimpleSetPredicateTree.pmml";
+    private static final String TARGET_FIELD = "Predicted_result";
+
+    private double input1;
+    private double input2;
+    private double input3;
+    private String expectedResult;
+
+    public SimpleSetPredicateTreeTest(double input1, double input2, double input3, String expectedResult) {
+        this.input1 = input1;
+        this.input2 = input2;
+        this.input3 = input3;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {0, 3, 0, "classA"},
+                {0, 8.5, 0, "classA"},
+                {-5, 0, 0, "classB"},
+                {0.5, 0, 0, "classB"},
+                {1, 0, 0, "classB"},
+                {10, 0, 0, "classB"},
+                {-10, 15, 15, "classC"},
+        });
+    }
+
+    @Test
+    public void testSetPredicateTree() {
+        final KiePMMLModel pmmlModel = loadPMMLModel(PMML_SOURCE);
+
+        final Map<String, Object> inputData = new HashMap<>();
+        inputData.put("input1", input1);
+        inputData.put("input2", input2);
+        inputData.put("input3", input3);
+
+        final PMMLRequestData pmmlRequestData = getPMMLRequestData(MODEL_NAME, inputData);
+        PMML4Result pmml4Result = EXECUTOR.evaluate(pmmlModel, new PMMLContextImpl(pmmlRequestData), RELEASE_ID);
+        Assertions.assertThat(pmml4Result.getResultVariables().get(TARGET_FIELD)).isNotNull();
+        Assertions.assertThat(pmml4Result.getResultVariables().get(TARGET_FIELD)).isEqualTo(expectedResult);
+    }
+}
