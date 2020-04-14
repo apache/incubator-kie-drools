547b7bb013c895de0f7652983097a5af6d38577a
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2020-04-14 23:07:02 +1000

    KOGITO-1732 - Ensure service url is always present in endpoint (#425) (#441)

--- kogito-api/kogito-services/src/main/java/org/kie/kogito/services/event/impl/ProcessInstanceEventBatch.java
+++ kogito-api/kogito-services/src/main/java/org/kie/kogito/services/event/impl/ProcessInstanceEventBatch.java
@@ -42,18 +42,17 @@ import org.kie.kogito.services.event.ProcessInstanceDataEvent;
 import org.kie.kogito.services.event.UserTaskInstanceDataEvent;
 import org.kie.kogito.services.event.VariableInstanceDataEvent;
 
-
 public class ProcessInstanceEventBatch implements EventBatch {
-        
+
     public static final String TRACKED = "tracked";
-    
+
     private final String service;
     private Addons addons;
     private List<ProcessEvent> rawEvents = new ArrayList<>();
 
     public ProcessInstanceEventBatch(String service, Addons addons) {
         this.service = service;
-        this.addons = addons;                
+        this.addons = addons;
     }
 
     @Override
@@ -68,72 +67,68 @@ public class ProcessInstanceEventBatch implements EventBatch {
         Map<String, ProcessInstanceEventBody> processInstances = new LinkedHashMap<>();
         Map<String, UserTaskInstanceEventBody> userTaskInstances = new LinkedHashMap<>();
         Set<VariableInstanceEventBody> variables = new LinkedHashSet<>();
-        
+
         for (ProcessEvent event : rawEvents) {
             ProcessInstanceEventBody body = processInstances.computeIfAbsent(event.getProcessInstance().getId(), key -> create(event));
-            
+
             if (event instanceof ProcessNodeTriggeredEvent) {
-                
+
                 handleProcessNodeTriggeredEvent((ProcessNodeTriggeredEvent) event, body);
             } else if (event instanceof ProcessNodeLeftEvent) {
-                
+
                 handleProcessNodeLeftEvent((ProcessNodeLeftEvent) event, body);
             } else if (event instanceof ProcessCompletedEvent) {
-                
+
                 handleProcessCompletedEvent((ProcessCompletedEvent) event, body);
             } else if (event instanceof ProcessWorkItemTransitionEvent) {
-                
+
                 handleProcessWorkItemTransitionEvent((ProcessWorkItemTransitionEvent) event, userTaskInstances);
             } else if (event instanceof ProcessVariableChangedEvent) {
-                
+
                 handleProcessVariableChangedEvent((ProcessVariableChangedEvent) event, variables);
             }
-            
         }
-        
+
         Collection<DataEvent<?>> processedEvents = new ArrayList<>();
-                
-        processInstances.values().stream().map(pi -> new ProcessInstanceDataEvent(extractProcessId(pi.metaData()), addons.toString(), pi.metaData(), pi)).forEach(processedEvents::add);
-        userTaskInstances.values().stream().map(pi -> new UserTaskInstanceDataEvent(extractProcessId(pi.metaData()), addons.toString(), pi.metaData(), pi)).forEach(processedEvents::add);
-        variables.stream().map(pi -> new VariableInstanceDataEvent(extractProcessId(pi.metaData()), addons.toString(), pi.metaData(), pi)).forEach(processedEvents::add);
-        
+
+        processInstances.values().stream().map(pi -> new ProcessInstanceDataEvent(extractRuntimeSource(pi.metaData()), addons.toString(), pi.metaData(), pi)).forEach(processedEvents::add);
+        userTaskInstances.values().stream().map(pi -> new UserTaskInstanceDataEvent(extractRuntimeSource(pi.metaData()), addons.toString(), pi.metaData(), pi)).forEach(processedEvents::add);
+        variables.stream().map(pi -> new VariableInstanceDataEvent(extractRuntimeSource(pi.metaData()), addons.toString(), pi.metaData(), pi)).forEach(processedEvents::add);
+
         return processedEvents;
     }
-    
-    protected void handleProcessCompletedEvent(ProcessCompletedEvent event, ProcessInstanceEventBody body) {
-     // in case this is a process complete event always updated and date and state 
-        body.update()
-        .endDate(((WorkflowProcessInstance)event.getProcessInstance()).getEndDate())
-        .state(event.getProcessInstance().getState());
-    }
-    
-    protected void handleProcessNodeTriggeredEvent(ProcessNodeTriggeredEvent event, ProcessInstanceEventBody body) {
 
-        NodeInstanceEventBody nodeInstanceBody = create((ProcessNodeEvent)event);
+    protected void handleProcessCompletedEvent(ProcessCompletedEvent event, ProcessInstanceEventBody body) {
+        // in case this is a process complete event always updated and date and state 
+        body.update()
+                .endDate(((WorkflowProcessInstance) event.getProcessInstance()).getEndDate())
+                .state(event.getProcessInstance().getState());
+    }
+
+    protected void handleProcessNodeTriggeredEvent(ProcessNodeTriggeredEvent event, ProcessInstanceEventBody body) {
+        NodeInstanceEventBody nodeInstanceBody = create((ProcessNodeEvent) event);
         if (!body.getNodeInstances().contains(nodeInstanceBody)) {
             // add it only if it does not exist
             body.update().nodeInstance(nodeInstanceBody);
         }
     }
-    
+
     protected void handleProcessNodeLeftEvent(ProcessNodeLeftEvent event, ProcessInstanceEventBody body) {
-        NodeInstanceEventBody nodeInstanceBody = create((ProcessNodeEvent)event);
+        NodeInstanceEventBody nodeInstanceBody = create((ProcessNodeEvent) event);
         // if it's already there, remove it
         body.getNodeInstances().remove(nodeInstanceBody);
         // and add it back as the node left event has latest information
-        body.update().nodeInstance(nodeInstanceBody); 
+        body.update().nodeInstance(nodeInstanceBody);
     }
-    
+
     protected void handleProcessWorkItemTransitionEvent(ProcessWorkItemTransitionEvent workItemTransitionEvent, Map<String, UserTaskInstanceEventBody> userTaskInstances) {
-        
         WorkItem workItem = workItemTransitionEvent.getWorkItem();
         if (workItem instanceof HumanTaskWorkItem && workItemTransitionEvent.isTransitioned()) {
-            userTaskInstances.putIfAbsent(workItem.getId(), createUserTask(workItemTransitionEvent));                    
+            userTaskInstances.putIfAbsent(workItem.getId(), createUserTask(workItemTransitionEvent));
         }
     }
-    
-    protected void handleProcessVariableChangedEvent(ProcessVariableChangedEvent variableChangedEvent, Set<VariableInstanceEventBody> variables) {        
-        
+
+    protected void handleProcessVariableChangedEvent(ProcessVariableChangedEvent variableChangedEvent, Set<VariableInstanceEventBody> variables) {
         if (variableChangedEvent.hasTag(TRACKED)) {
             variables.add(create(variableChangedEvent));
         }
@@ -164,7 +159,6 @@ public class ProcessInstanceEventBatch implements EventBatch {
                 .inputs(workItem.getParameters())
                 .outputs(workItem.getResults())
                 .build();
-  
     }
 
     protected ProcessInstanceEventBody create(ProcessEvent event) {
@@ -181,25 +175,25 @@ public class ProcessInstanceEventBatch implements EventBatch {
                 .state(pi.getState())
                 .businessKey(pi.getCorrelationKey())
                 .variables(pi.getVariables());
-        
+
         if (pi.getState() == ProcessInstance.STATE_ERROR) {
             eventBuilder.error(ProcessErrorEventBody.create()
-                               .nodeDefinitionId(pi.getNodeIdInError())
-                               .errorMessage(pi.getErrorMessage())
-                               .build());
+                                       .nodeDefinitionId(pi.getNodeIdInError())
+                                       .errorMessage(pi.getErrorMessage())
+                                       .build());
         }
-        
+
         String securityRoles = (String) pi.getProcess().getMetaData().get("securityRoles");
         if (securityRoles != null) {
             eventBuilder.roles(securityRoles.split(","));
         }
-        
+
         return eventBuilder.build();
     }
-    
+
     protected NodeInstanceEventBody create(ProcessNodeEvent event) {
         NodeInstance ni = event.getNodeInstance();
-        
+
         return NodeInstanceEventBody.create()
                 .id(ni.getId())
                 .nodeId(String.valueOf(ni.getNodeId()))
@@ -210,7 +204,7 @@ public class ProcessInstanceEventBatch implements EventBatch {
                 .leaveTime(ni.getLeaveTime())
                 .build();
     }
-    
+
     protected VariableInstanceEventBody create(ProcessVariableChangedEvent event) {
         VariableInstanceEventBody.Builder eventBuilder = VariableInstanceEventBody.create()
                 .changeDate(event.getEventDate())
@@ -221,23 +215,23 @@ public class ProcessInstanceEventBatch implements EventBatch {
                 .variableName(event.getVariableId())
                 .variableValue(event.getNewValue())
                 .variablePreviousValue(event.getOldValue());
-        
+
         if (event.getNodeInstance() != null) {
             eventBuilder
-                .changedByNodeId(event.getNodeInstance().getNodeDefinitionId())
-                .changedByNodeName(event.getNodeInstance().getNodeName())
-                .changedByNodeType(event.getNodeInstance().getNode().getClass().getSimpleName());
-        }
-        
-        return eventBuilder.build();
-    }
-    
-    protected String extractProcessId(Map<String, String> metadata) {
-        String processId = metadata.get(ProcessInstanceEventBody.PROCESS_ID_META_DATA);
-        if (processId.contains(".")) {
-            return processId.substring(processId.lastIndexOf('.') + 1);
+                    .changedByNodeId(event.getNodeInstance().getNodeDefinitionId())
+                    .changedByNodeName(event.getNodeInstance().getNodeName())
+                    .changedByNodeType(event.getNodeInstance().getNode().getClass().getSimpleName());
         }
 
-        return service + "/" + processId;
+        return eventBuilder.build();
+    }
+
+    protected String extractRuntimeSource(Map<String, String> metadata) {
+        String processId = metadata.get(ProcessInstanceEventBody.PROCESS_ID_META_DATA);
+        if(processId == null){
+            return null;
+        } else {
+            return service + "/" + (processId.contains(".") ? processId.substring(processId.lastIndexOf('.') + 1) : processId);
+        }
     }
 }
