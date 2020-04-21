f55767758dc38600ae299b32785b07ae920781f0
Matteo Mortari <matteo.mortari@gmail.com>
2020-04-21 12:09:57 +0200

    DROOLS-5256 DMN DT Analysis improvement to Overlap human friendly render (#2862)

---
+++ kie-dmn/kie-dmn-validation/src/test/java/org/kie/dmn/validation/dtanalysis/OverlapsMsgTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.dmn.validation.dtanalysis;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.kie.dmn.api.core.DMNMessage;
+import org.kie.dmn.feel.runtime.Range.RangeBoundary;
+import org.kie.dmn.validation.dtanalysis.model.Bound;
+import org.kie.dmn.validation.dtanalysis.model.DTAnalysis;
+import org.kie.dmn.validation.dtanalysis.model.Hyperrectangle;
+import org.kie.dmn.validation.dtanalysis.model.Interval;
+import org.kie.dmn.validation.dtanalysis.model.Overlap;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.kie.dmn.validation.DMNValidator.Validation.ANALYZE_DECISION_TABLE;
+import static org.kie.dmn.validation.DMNValidator.Validation.VALIDATE_COMPILATION;
+
+public class OverlapsMsgTest extends AbstractDTAnalysisTest {
+
+    @Test
+    public void test() {
+        List<DMNMessage> validate = validator.validate(getReader("improveHF.dmn"), VALIDATE_COMPILATION, ANALYZE_DECISION_TABLE);
+        DTAnalysis analysis = getAnalysis(validate, "_CA4B40F8-2354-48B6-B323-5C4E4B2CE467");
+
+        // Assert GAPs
+        assertThat(analysis.getGaps(), hasSize(0));
+
+        // assert OVERLAPs
+        assertThat(analysis.getOverlaps(), hasSize(2));
+
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        List<Overlap> overlaps = Arrays.asList(new Overlap(Arrays.asList(3,
+                                                                         1),
+                                                           new Hyperrectangle(2,
+                                                                              Arrays.asList(Interval.newFromBounds(new Bound(Interval.NEG_INF,
+                                                                                                                             RangeBoundary.CLOSED,
+                                                                                                                             null),
+                                                                                                                   new Bound(Interval.POS_INF,
+                                                                                                                             RangeBoundary.CLOSED,
+                                                                                                                             null)),
+                                                                                            Interval.newFromBounds(new Bound(new BigDecimal("0"),
+                                                                                                                             RangeBoundary.OPEN,
+                                                                                                                             null),
+                                                                                                                   new Bound(Interval.POS_INF,
+                                                                                                                             RangeBoundary.CLOSED,
+                                                                                                                             null))))),
+                                               new Overlap(Arrays.asList(2,
+                                                                         3),
+                                                           new Hyperrectangle(2,
+                                                                              Arrays.asList(Interval.newFromBounds(new Bound(new BigDecimal("47"),
+                                                                                                                             RangeBoundary.OPEN,
+                                                                                                                             null),
+                                                                                                                   new Bound(Interval.POS_INF,
+                                                                                                                             RangeBoundary.CLOSED,
+                                                                                                                             null)),
+                                                                                            Interval.newFromBounds(new Bound(Interval.NEG_INF,
+                                                                                                                             RangeBoundary.CLOSED,
+                                                                                                                             null),
+                                                                                                                   new Bound(new BigDecimal("0"),
+                                                                                                                             RangeBoundary.CLOSED,
+                                                                                                                             null))))));
+        assertThat(overlaps, hasSize(2));
+
+        assertThat(analysis.getOverlaps(), contains(overlaps.toArray()));
+
+        assertThat(analysis.getOverlaps().get(0).getOverlap().asHumanFriendly(analysis.getDdtaTable()), is("[ -, >0 ]"));
+        assertThat(analysis.getOverlaps().get(1).getOverlap().asHumanFriendly(analysis.getDdtaTable()), is("[ >47, <=0 ]"));
+    }
+}
