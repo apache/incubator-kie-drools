2dc39fbbce1400666793c381fa446a1a15c05b0b
Jiri Petrlik <jiripetrlik@gmail.com>
2020-04-29 16:30:44 +0200

    [DROOLS-5239] Use target fields for tests

--- kie-pmml-new/kie-pmml-models/kie-pmml-models-drools/kie-pmml-models-drools-scorecard/kie-pmml-models-drools-scorecard-tests/src/test/java/org/kie/pmml/models/drools/scorecard/tests/SimpleScorecardTest.java
+++ kie-pmml-new/kie-pmml-models/kie-pmml-models-drools/kie-pmml-models-drools-scorecard/kie-pmml-models-drools-scorecard-tests/src/test/java/org/kie/pmml/models/drools/scorecard/tests/SimpleScorecardTest.java
@@ -36,7 +36,7 @@ public class SimpleScorecardTest extends AbstractPMMLScorecardTest {
 
     private static final String MODEL_NAME = "SimpleScorecard";
     private static final String PMML_SOURCE = "SimpleScorecard.pmml";
-    private static final String TARGET_FIELD = "score"; // TODO: Change to "Score" when output fields are implemented
+    private static final String TARGET_FIELD = "Score";
     private static final String REASON_CODE1_FIELD = "Reason Code 1";
     private static final String REASON_CODE2_FIELD = "Reason Code 2";
 
