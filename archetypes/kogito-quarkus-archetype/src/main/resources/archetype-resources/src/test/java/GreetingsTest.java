ee4d351c58e693ef708940c747486bb3bcc1136d
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-05-13 18:40:55 +0200

    KOGITO-2136 Add integration tests to archetypes (#497)

---
+++ archetypes/kogito-quarkus-archetype/src/main/resources/archetype-resources/src/test/java/GreetingsTest.java
@@ -0,0 +1,28 @@
+package ${package};
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
+
+@QuarkusTest
+public class GreetingsTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .body("{}")
+        .when()
+            .post("/greetings")
+        .then()
+            .statusCode(200)
+            .body("id", notNullValue());    
+    }
+
+}
---
+++ archetypes/kogito-springboot-archetype/src/main/resources/archetype-resources/src/test/java/GreetingsTest.java
@@ -0,0 +1,38 @@
+package ${package};
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = KogitoApplication.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) // reset spring context after each test method
+public class GreetingsTest {
+
+    @LocalServerPort
+    int randomServerPort;
+
+    @Test
+    public void testOrderProcess() throws Exception {
+        RestAssured.port = randomServerPort;
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{}")
+            .when()
+                .post("/greetings")
+            .then()
+                .statusCode(200)
+                .body("id", notNullValue());
+    }
+
+}
