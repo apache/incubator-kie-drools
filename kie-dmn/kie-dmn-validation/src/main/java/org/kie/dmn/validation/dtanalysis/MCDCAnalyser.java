2b0e7d7bb1bf1c909ee601155089d139f58e7742
tarilabs <matteo.mortari@gmail.com>
2020-06-03 18:34:17 +0200

    adding output to convenience XLSX while debug

--- kie-dmn/kie-dmn-validation/src/main/java/org/kie/dmn/validation/dtanalysis/MCDCAnalyser.java
+++ kie-dmn/kie-dmn-validation/src/main/java/org/kie/dmn/validation/dtanalysis/MCDCAnalyser.java
@@ -1,6 +1,11 @@
 package org.kie.dmn.validation.dtanalysis;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.math.BigDecimal;
+import java.nio.file.Files;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,9 +24,20 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.kie.dmn.feel.runtime.Range.RangeBoundary;
 import org.kie.dmn.model.api.DecisionTable;
 import org.kie.dmn.model.api.HitPolicy;
+import org.kie.dmn.model.api.InputClause;
+import org.kie.dmn.model.api.OutputClause;
 import org.kie.dmn.validation.dtanalysis.model.Bound;
 import org.kie.dmn.validation.dtanalysis.model.DDTAInputEntry;
 import org.kie.dmn.validation.dtanalysis.model.DDTARule;
@@ -94,6 +110,119 @@ public class MCDCAnalyser {
             LOG.info(" ");
         }
         LOG.info("total of cases: {}", mcdcRecords.size());
+        if (LOG.isDebugEnabled()) {
+            outputOpenXLSX();
+        }
+    }
+
+    private void outputOpenXLSX() {
+        XSSFWorkbook wb = new XSSFWorkbook();
+        CreationHelper ch = wb.getCreationHelper();
+        CellStyle styleColumn = wb.createCellStyle();
+        styleColumn.setFillForegroundColor(IndexedColors.LIGHT_YELLOW.getIndex());
+        styleColumn.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        Font font = wb.createFont();
+        font.setColor(IndexedColors.RED.getIndex());
+        CellStyle styleRepeated = wb.createCellStyle();
+        styleRepeated.setFont(font);
+
+        Sheet sheet = wb.createSheet("MC DC Analysis");
+        int r = 0, c = 0;
+        Row row = sheet.createRow(r++);
+        row.createCell(c++).setCellValue(ch.createRichTextString("Rule:"));
+        for (InputClause input : dt.getInput()) {
+            row.createCell(c++).setCellValue(input.getInputExpression().getText());
+        }
+        for (OutputClause output : dt.getOutput()) {
+            row.createCell(c++).setCellValue(dt.getOutputLabel());
+        }
+        row.createCell(c++).setCellValue(ch.createRichTextString("Rule:"));
+        for (InputClause input : dt.getInput()) {
+            row.createCell(c++).setCellValue(input.getInputExpression().getText());
+        }
+        for (OutputClause output : dt.getOutput()) {
+            row.createCell(c++).setCellValue(dt.getOutputLabel());
+        }
+        Set<Record> mcdcRecords = new LinkedHashSet<>();
+        for (PosNegBlock b : selectedBlocks) {
+            row = sheet.createRow(r++);
+            c = 0;
+            boolean add = mcdcRecords.add(b.posRecord);
+            Cell ruleIdxCell = row.createCell(c++);
+            ruleIdxCell.setCellValue(b.posRecord.ruleIdx + 1);
+            if (!add) {
+                ruleIdxCell.setCellStyle(styleRepeated);
+            }
+            for (int i = 0; i < b.posRecord.enums.length; i++) {
+                Object en = b.posRecord.enums[i];
+                Cell enCell = row.createCell(c++);
+                enCell.setCellValue(en.toString());
+                if (!add) {
+                    enCell.setCellStyle(styleRepeated);
+                }
+                if (b.cMarker == i) {
+                    enCell.setCellStyle(styleColumn);
+                }
+            }
+            for (Object out : b.posRecord.output) {
+                Cell outCell = row.createCell(c++);
+                outCell.setCellValue(out.toString());
+                if (!add) {
+                    outCell.setCellStyle(styleRepeated);
+                }
+            }
+            for (int i = 0; i < b.negRecords.size(); i++) {
+                Record negRecord = b.negRecords.get(i);
+                if (i != 0) {
+                    row = sheet.createRow(r++);
+                    c = 0 + 1 + b.posRecord.enums.length + b.posRecord.output.size();
+                }
+                add = mcdcRecords.add(negRecord);
+                ruleIdxCell = row.createCell(c++);
+                ruleIdxCell.setCellValue(negRecord.ruleIdx + 1);
+                if (!add) {
+                    ruleIdxCell.setCellStyle(styleRepeated);
+                }
+                for (Object en : negRecord.enums) {
+                    Cell enCell = row.createCell(c++);
+                    enCell.setCellValue(en.toString());
+                    if (!add) {
+                        enCell.setCellStyle(styleRepeated);
+                    }
+                }
+                for (Object out : negRecord.output) {
+                    Cell outCell = row.createCell(c++);
+                    outCell.setCellValue(out.toString());
+                    if (!add) {
+                        outCell.setCellStyle(styleRepeated);
+                    }
+                }
+            }
+        }
+        File file;
+        try {
+            file = Files.createTempFile("mcdc", ".xlsx").toFile();
+            OutputStream fileOut = new FileOutputStream(file);
+            wb.write(fileOut);
+            wb.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // terminate early;
+            return;
+        }
+        try {
+            boolean isWinPlatform = System.getProperty("os.name").toLowerCase().startsWith("windows");
+            Process process;
+            if (isWinPlatform) {
+                process = Runtime.getRuntime().exec("open " + file.getAbsolutePath());
+            } else {
+                process = Runtime.getRuntime().exec("open " + file.getAbsolutePath());
+            }
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        LOG.trace(System.getProperty("java.io.tmpdir"));
     }
 
     private void step4() {
