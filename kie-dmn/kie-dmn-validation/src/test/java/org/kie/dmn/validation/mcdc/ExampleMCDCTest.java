1db4d596381f58b0a0139a0843f156dc936913ad
tarilabs <matteo.mortari@gmail.com>
2020-06-05 09:31:34 +0200

    move XLSX generation as a test facility

--- kie-dmn/kie-dmn-validation/src/test/java/org/kie/dmn/validation/mcdc/ExampleMCDCTest.java
+++ kie-dmn/kie-dmn-validation/src/test/java/org/kie/dmn/validation/mcdc/ExampleMCDCTest.java
@@ -16,12 +16,27 @@
 
 package org.kie.dmn.validation.mcdc;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 import org.kie.dmn.api.core.DMNContext;
 import org.kie.dmn.api.core.DMNMessage;
@@ -32,6 +47,8 @@ import org.kie.dmn.api.core.event.AfterEvaluateDecisionTableEvent;
 import org.kie.dmn.api.core.event.DMNRuntimeEventListener;
 import org.kie.dmn.core.util.DMNRuntimeUtil;
 import org.kie.dmn.model.api.DecisionTable;
+import org.kie.dmn.model.api.InputClause;
+import org.kie.dmn.model.api.OutputClause;
 import org.kie.dmn.validation.dtanalysis.AbstractDTAnalysisTest;
 import org.kie.dmn.validation.dtanalysis.MCDCAnalyser.PosNegBlock;
 import org.kie.dmn.validation.dtanalysis.MCDCAnalyser.Record;
@@ -56,6 +73,7 @@ public class ExampleMCDCTest extends AbstractDTAnalysisTest {
         assertThat(mcdcCases, hasSize(16));
 
         assertMCDCCases(resourceFileName, analysis.getSource(), mcdcCases);
+        debugOutputAndOpenXLSX(analysis.getSource(), analysis.getMCDCSelectedBlocks());
     }
 
     @Test
@@ -68,6 +86,7 @@ public class ExampleMCDCTest extends AbstractDTAnalysisTest {
         assertThat(mcdcCases, hasSize(14));
 
         assertMCDCCases(resourceFileName, analysis.getSource(), mcdcCases);
+        debugOutputAndOpenXLSX(analysis.getSource(), analysis.getMCDCSelectedBlocks());
     }
 
     public static class MCDCListener implements DMNRuntimeEventListener {
@@ -110,5 +129,112 @@ public class ExampleMCDCTest extends AbstractDTAnalysisTest {
             }
         }
         return mcdcRecords;
+    }
+
+    public static void debugOutputAndOpenXLSX(DecisionTable dt, List<PosNegBlock> selectedBlocks) {
+        XSSFWorkbook wb = new XSSFWorkbook();
+        CreationHelper ch = wb.getCreationHelper();
+        CellStyle styleColumn = wb.createCellStyle();
+        styleColumn.setFillForegroundColor(IndexedColors.LIGHT_YELLOW.getIndex());
+        styleColumn.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        Font font = wb.createFont();
+        font.setColor(IndexedColors.RED.getIndex());
+        CellStyle styleRepeated = wb.createCellStyle();
+        styleRepeated.setFont(font);
+
+        Sheet sheet = wb.createSheet("MC DC Analysis");
+        int r = 0, c = 0;
+        Row row = sheet.createRow(r++);
+        row.createCell(c++).setCellValue(ch.createRichTextString("Rule:"));
+        for (InputClause input : dt.getInput()) {
+            row.createCell(c++).setCellValue(input.getInputExpression().getText());
+        }
+        for (OutputClause output : dt.getOutput()) {
+            row.createCell(c++).setCellValue(dt.getOutputLabel());
+        }
+        row.createCell(c++).setCellValue(ch.createRichTextString("Rule:"));
+        for (InputClause input : dt.getInput()) {
+            row.createCell(c++).setCellValue(input.getInputExpression().getText());
+        }
+        for (OutputClause output : dt.getOutput()) {
+            row.createCell(c++).setCellValue(dt.getOutputLabel());
+        }
+        Set<Record> mcdcRecords = new LinkedHashSet<>();
+        for (PosNegBlock b : selectedBlocks) {
+            row = sheet.createRow(r++);
+            c = 0;
+            boolean add = mcdcRecords.add(b.posRecord);
+            Cell ruleIdxCell = row.createCell(c++);
+            ruleIdxCell.setCellValue(b.posRecord.ruleIdx + 1);
+            if (!add) {
+                ruleIdxCell.setCellStyle(styleRepeated);
+            }
+            for (int i = 0; i < b.posRecord.enums.length; i++) {
+                Object en = b.posRecord.enums[i];
+                Cell enCell = row.createCell(c++);
+                enCell.setCellValue(en.toString());
+                if (!add) {
+                    enCell.setCellStyle(styleRepeated);
+                }
+                if (b.cMarker == i) {
+                    enCell.setCellStyle(styleColumn);
+                }
+            }
+            for (Object out : b.posRecord.output) {
+                Cell outCell = row.createCell(c++);
+                outCell.setCellValue(out.toString());
+                if (!add) {
+                    outCell.setCellStyle(styleRepeated);
+                }
+            }
+            for (int i = 0; i < b.negRecords.size(); i++) {
+                Record negRecord = b.negRecords.get(i);
+                if (i != 0) {
+                    row = sheet.createRow(r++);
+                    c = 0 + 1 + b.posRecord.enums.length + b.posRecord.output.size();
+                }
+                add = mcdcRecords.add(negRecord);
+                ruleIdxCell = row.createCell(c++);
+                ruleIdxCell.setCellValue(negRecord.ruleIdx + 1);
+                if (!add) {
+                    ruleIdxCell.setCellStyle(styleRepeated);
+                }
+                for (Object en : negRecord.enums) {
+                    Cell enCell = row.createCell(c++);
+                    enCell.setCellValue(en.toString());
+                    if (!add) {
+                        enCell.setCellStyle(styleRepeated);
+                    }
+                }
+                for (Object out : negRecord.output) {
+                    Cell outCell = row.createCell(c++);
+                    outCell.setCellValue(out.toString());
+                    if (!add) {
+                        outCell.setCellStyle(styleRepeated);
+                    }
+                }
+            }
+        }
+        File file;
+        try {
+            file = Files.createTempFile("mcdc", ".xlsx").toFile();
+            OutputStream fileOut = new FileOutputStream(file);
+            wb.write(fileOut);
+            wb.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // terminate early;
+            return;
+        }
+        if (Desktop.isDesktopSupported()) {
+            try {
+                Desktop.getDesktop().open(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            throw new UnsupportedOperationException();
+        }
+        LOG.trace(System.getProperty("java.io.tmpdir"));
     }
 }
