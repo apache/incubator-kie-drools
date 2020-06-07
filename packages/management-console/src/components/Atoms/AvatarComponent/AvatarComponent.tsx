a73a8e46d097e75a48ba60dd367acf6ad9b7d044
Sara4994 <40579476+Sara4994@users.noreply.github.com>
2020-06-07 09:27:24 +0530

    KOGITO-2299: Removed unused components from management-console (#252)

--- packages/management-console/src/components/Atoms/AvatarComponent/AvatarComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Avatar } from '@patternfly/react-core';
-import userImage from '../../../static/avatar.svg';
-
-const AvatarComponent: React.FC = () => {
-  return <Avatar src={userImage} alt="Kogito Logo" />;
-};
-
-export default AvatarComponent;
