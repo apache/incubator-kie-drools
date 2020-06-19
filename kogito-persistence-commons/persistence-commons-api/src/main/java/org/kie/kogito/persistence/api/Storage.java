41a6325062665cb840237f2c92ee11562433955d
Jacopo Rota <jacopo.r00ta@gmail.com>
2020-06-19 03:11:49 +0200

    add javadoc to the class StorageService (#277)

--- kogito-persistence-commons/persistence-commons-api/src/main/java/org/kie/kogito/persistence/api/Storage.java
+++ kogito-persistence-commons/persistence-commons-api/src/main/java/org/kie/kogito/persistence/api/Storage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Red Hat, Inc. and/or its affiliates. 
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,25 +24,83 @@ import org.kie.kogito.persistence.api.query.Query;
 
 public interface Storage<K, V> {
 
+    /**
+     * Adds a listener on the create events.
+     *
+     * @param consumer The listener.
+     */
     void addObjectCreatedListener(Consumer<V> consumer);
 
+    /**
+     * Adds a listener on the update events.
+     *
+     * @param consumer The listener.
+     */
     void addObjectUpdatedListener(Consumer<V> consumer);
 
+    /**
+     * Adds a listener on the remove events.
+     *
+     * @param consumer The listener.
+     */
     void addObjectRemovedListener(Consumer<K> consumer);
 
+    /**
+     * Gets the `Query` object to query the storage.
+     *
+     * @return The `Query` instance.
+     */
     Query<V> query();
 
+    /**
+     * Gets an element by key. If the element is not present in the storage, then `null` is returned.
+     *
+     * @param key The key.
+     * @return The element.
+     */
     V get(K key);
 
+    /**
+     * Puts an element with a key. If an element with the same key is already present in the storage, then it is replaced.
+     *
+     * @param key   The key.
+     * @param value The value.
+     * @return The value.
+     */
     V put(K key, V value);
 
+    /**
+     * Removes an element by key. If the element is not present in the storage, then `null` is returned.
+     *
+     * @param key The key.
+     * @return The removed object.
+     */
     V remove(K key);
 
+    /**
+     * Checks whether the storage contains a key.
+     *
+     * @param key The key.
+     * @return `true` if the key is present in the storage, `false` otherwise.
+     */
     boolean containsKey(K key);
 
+    /**
+     * Gets the pair key-value entry set of the elements in the storage.
+     *
+     * @return The key-value pair set of the elements in the storage.
+     */
     Set<Map.Entry<K, V>> entrySet();
 
+    /**
+     * Erase all the elements in the storage.
+     */
     void clear();
 
+    /**
+     * Gets the root type for the storage.
+     *
+     * @return The root type for the storage.
+     */
     String getRootType();
 }
