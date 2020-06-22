50d010ecdd96e80846fd5fb00d07f7e14936809e
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2020-06-22 17:30:19 +0200

    [DROOLS-5247] Removed unused import

--- kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/pmml/AbstractPMMLInvocationEvaluator.java
+++ kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/pmml/AbstractPMMLInvocationEvaluator.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.kie.api.io.Resource;
