1abad863e0dfdf4c53529f3530039a4304a28f5c
Jiri Petrlik <jiripetrlik@gmail.com>
2020-08-06 15:08:45 +0200

    [DROOLS-5529] Implement tests for selectFirst in segmentation classification

---
+++ kie-pmml-trusty/kie-pmml-models/kie-pmml-models-mining/kie-pmml-models-mining-tests/src/test/java/org/kie/pmml/regression/tests/SegmentationClassificationSelectFirstTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.pmml.regression.tests;
+
+import org.assertj.core.api.Assertions;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.kie.api.pmml.PMML4Result;
+import org.kie.pmml.evaluator.api.executor.PMMLRuntime;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(Parameterized.class)
+public class SegmentationClassificationSelectFirstTest extends AbstractPMMLMiningTest {
+    private static final String MODEL_NAME = "SegmentationClassificationSelectFirst";
+    private static final String TARGET_FIELD = "result";
+    private static PMMLRuntime pmmlRuntime;
+
+    private double input1;
+    private double input2;
+    private double input3;
+    private String result;
+
+    public SegmentationClassificationSelectFirstTest(double input1, double input2, double input3, String result) {
+        this.input1 = input1;
+        this.input2 = input2;
+        this.input3 = input3;
+        this.result = result;
+    }
+
+    @BeforeClass
+    public static void setupClass() {
+        pmmlRuntime = getPMMLRuntime(MODEL_NAME);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {0, -1, 0, "classB"},
+                {2, 20, 90, "classC"},
+                {4, 20, 0, "classA"},
+                {15, -1, 0, "classC"},
+                {17, 20, 90, "classA"},
+                {18, 20, 0, "classB"},
+                {55, -1, 0, "classA"},
+                {57, 20, 90, "classB"},
+                {58, 20, 0, "classC"},
+                {58, 20, -1, "classC"},
+        });
+    }
+
+    @Test
+    public void testSegmentationClassificationSelectFirstTest() {
+        final Map<String, Object> inputData = new HashMap<>();
+        inputData.put("input1", input1);
+        inputData.put("input2", input2);
+        inputData.put("input3", input3);
+        PMML4Result pmml4Result = evaluate(pmmlRuntime, inputData, MODEL_NAME);
+
+        Assertions.assertThat(pmml4Result.getResultVariables().get(TARGET_FIELD)).isNotNull();
+        Assertions.assertThat(pmml4Result.getResultVariables().get(TARGET_FIELD)).isEqualTo(result);
+    }
+}
