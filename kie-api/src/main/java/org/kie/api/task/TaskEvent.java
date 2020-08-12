9b038beb36ce0b45d1f7217c7ed4c4a469126f42
Enrique Gonzalez Martinez <egonzale@egonzale.remote.csb>
2020-08-12 12:29:51 +0200

    [JBPM-9277] JMS Audit Data Replication in spring boot

--- kie-api/src/main/java/org/kie/api/task/TaskEvent.java
+++ kie-api/src/main/java/org/kie/api/task/TaskEvent.java
@@ -16,10 +16,13 @@
 package org.kie.api.task;
 
 import java.util.Date;
+import java.util.Map;
 
 import org.kie.api.task.model.Task;
 
 public interface TaskEvent {
+
+    Map<String, Object> getMetadata();
 
     Task getTask();
 
