2bf012e7c28ab9492bf45225af5f5274a8a295f1
porcelli <alexandre.porcelli@gmail.com>
2020-08-20 11:25:38 -0400

    adjusting for code review

--- drools-verifier/drools-verifier-core/src/main/java/org/drools/verifier/core/checks/SingleRangeCheck.java
+++ drools-verifier/drools-verifier-core/src/main/java/org/drools/verifier/core/checks/SingleRangeCheck.java
@@ -124,6 +124,8 @@ public class SingleRangeCheck extends CheckBase {
         if (errors.isEmpty() && dimensions.size() >= 2) {
             for (int i = 0; i < dimensions.size() - 1; i++) {
                 for (int j = i + 1; j < dimensions.size(); j++) {
+                    //AF-2542: the new version of JDT used by GWT has a hard time to resolve some generics.
+                    //         the unnecessary cast is required because of that.
                     if (!checkBidimensionalRanges((List)dimensions.get(i), (List)dimensions.get(j))) {
                         errors.add(new RangeError(partition.getValue(), partition.getKey(), null));
                     }
