de85aa61e03c05d97162324092c5ffdab2ca3e5a
Matteo Mortari <matteo.mortari@gmail.com>
2020-09-11 11:00:05 +0200

    DROOLS-5630 DMN Survey valid.rules for missing element references part 2 (#3089)
    * DROOLS-5630 DMN Survey valid.rules for missing element references part 2
    
    * Cover the case of missing the outputDecision from DS

--- kie-dmn/kie-dmn-validation/src/test/java/org/kie/dmn/validation/ValidatorDecisionServiceTest.java
+++ kie-dmn/kie-dmn-validation/src/test/java/org/kie/dmn/validation/ValidatorDecisionServiceTest.java
@@ -21,6 +21,7 @@ import java.io.Reader;
 import java.util.List;
 
 import org.junit.Test;
+import org.kie.dmn.api.core.DMNContext;
 import org.kie.dmn.api.core.DMNMessage;
 import org.kie.dmn.api.core.DMNMessageType;
 import org.kie.dmn.api.core.DMNModel;
@@ -33,6 +34,8 @@ import org.slf4j.LoggerFactory;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.junit.Assert.assertThat;
 import static org.kie.dmn.validation.DMNValidator.Validation.VALIDATE_COMPILATION;
 import static org.kie.dmn.validation.DMNValidator.Validation.VALIDATE_MODEL;
@@ -90,5 +93,149 @@ public class ValidatorDecisionServiceTest extends AbstractValidatorTest {
         LOG.debug("{}", evaluateAll);
 
         assertThat(evaluateAll.getDecisionResultByName("Decision-1").getResult(), is("Hello World"));
+    }
+
+    @Test
+    public void testDS1ofEach() {
+        DMNRuntime runtime = DMNRuntimeUtil.createRuntime("decisionservice/DS1ofEach_OK.dmn", this.getClass());
+        DMNModel dmnModel = runtime.getModel("https://kiegroup.org/dmn/_40B3D02F-868C-4925-A1F2-5710DFEEF51E",
+                                             "DS1ofEach");
+        assertThat(dmnModel, notNullValue());
+
+        Definitions definitions = dmnModel.getDefinitions();
+        assertThat(definitions, notNullValue());
+
+        List<DMNMessage> messages = DMNValidatorFactory.newValidator().validate(definitions, VALIDATE_MODEL, VALIDATE_COMPILATION);
+        assertThat(messages.toString(), messages.size(), is(0));
+
+        DMNContext dmnContext = runtime.newContext();
+        dmnContext.set("InputData-1", "id1");
+        dmnContext.set("Decision-1", "od1");
+        DMNResult evaluateDS1 = runtime.evaluateDecisionService(dmnModel, dmnContext, "DecisionService-1");
+        LOG.debug("{}", evaluateDS1);
+
+        assertThat(evaluateDS1.getDecisionResultByName("Decision-2"), nullValue());
+        assertThat(evaluateDS1.getDecisionResultByName("Decision-3").getResult(), is("d3:d2:id1od1"));
+    }
+
+    @Test
+    public void testENCAPSULATED_NOT_FOUND_FOR_DS_ReaderInput() throws IOException {
+        try (final Reader reader = getReader("decisionservice/DS1ofEach_missingEncapsulated.dmn")) {
+            final List<DMNMessage> validate = validator.validate(reader,
+                                                                 VALIDATE_SCHEMA, VALIDATE_MODEL);
+            assertThat(ValidatorUtil.formatMessages(validate), validate.size(), is(2)); // DS-1 and Decision-3 are missing their reference now.
+            assertThat(validate.get(0).toString(), validate.get(0).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+            assertThat(validate.get(1).toString(), validate.get(1).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+        }
+    }
+
+    @Test
+    public void testENCAPSULATED_NOT_FOUND_FOR_DS_FileInput() {
+        final List<DMNMessage> validate = validator.validate(getFile("decisionservice/DS1ofEach_missingEncapsulated.dmn"),
+                                                             VALIDATE_SCHEMA, VALIDATE_MODEL);
+        assertThat(ValidatorUtil.formatMessages(validate), validate.size(), is(2));
+        assertThat(validate.get(0).toString(), validate.get(0).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+        assertThat(validate.get(1).toString(), validate.get(1).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+    }
+
+    @Test
+    public void testENCAPSULATED_NOT_FOUND_FOR_DS_DefinitionsInput() {
+        final List<DMNMessage> validate = validator.validate(getDefinitions("decisionservice/DS1ofEach_missingEncapsulated.dmn",
+                                                                            "https://kiegroup.org/dmn/_40B3D02F-868C-4925-A1F2-5710DFEEF51E",
+                                                                            "DS1ofEach"),
+                                                             VALIDATE_MODEL, VALIDATE_COMPILATION);
+        assertThat(ValidatorUtil.formatMessages(validate), validate.size(), greaterThanOrEqualTo(2));
+        assertThat(validate.get(0).toString(), validate.get(0).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+        assertThat(validate.get(1).toString(), validate.get(1).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+    }
+
+    @Test
+    public void testDECISIONINPUT_NOT_FOUND_FOR_DS_ReaderInput() throws IOException {
+        try (final Reader reader = getReader("decisionservice/DS1ofEach_missingDecisionInput.dmn")) {
+            final List<DMNMessage> validate = validator.validate(reader,
+                                                                 VALIDATE_SCHEMA, VALIDATE_MODEL);
+            assertThat(ValidatorUtil.formatMessages(validate), validate.size(), is(2)); // DS-1 and Decision-2 are missing their reference now.
+            assertThat(validate.get(0).toString(), validate.get(0).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+            assertThat(validate.get(1).toString(), validate.get(1).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+        }
+    }
+
+    @Test
+    public void testDECISIONINPUT_NOT_FOUND_FOR_DS_FileInput() {
+        final List<DMNMessage> validate = validator.validate(getFile("decisionservice/DS1ofEach_missingDecisionInput.dmn"),
+                                                             VALIDATE_SCHEMA, VALIDATE_MODEL);
+        assertThat(ValidatorUtil.formatMessages(validate), validate.size(), is(2));
+        assertThat(validate.get(0).toString(), validate.get(0).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+        assertThat(validate.get(1).toString(), validate.get(1).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+    }
+
+    @Test
+    public void testDECISIONINPUT_NOT_FOUND_FOR_DS_DefinitionsInput() {
+        final List<DMNMessage> validate = validator.validate(getDefinitions("decisionservice/DS1ofEach_missingDecisionInput.dmn",
+                                                                            "https://kiegroup.org/dmn/_40B3D02F-868C-4925-A1F2-5710DFEEF51E",
+                                                                            "DS1ofEach"),
+                                                             VALIDATE_MODEL, VALIDATE_COMPILATION);
+        assertThat(ValidatorUtil.formatMessages(validate), validate.size(), greaterThanOrEqualTo(2));
+        assertThat(validate.get(0).toString(), validate.get(0).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+        assertThat(validate.get(1).toString(), validate.get(1).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+    }
+
+    @Test
+    public void testINPUTDATA_NOT_FOUND_FOR_DS_ReaderInput() throws IOException {
+        try (final Reader reader = getReader("decisionservice/DS1ofEach_missingInputData.dmn")) {
+            final List<DMNMessage> validate = validator.validate(reader,
+                                                                 VALIDATE_SCHEMA, VALIDATE_MODEL);
+            assertThat(ValidatorUtil.formatMessages(validate), validate.size(), is(2)); // DS-1 and Decision-2 are missing their reference now.
+            assertThat(validate.get(0).toString(), validate.get(0).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+            assertThat(validate.get(1).toString(), validate.get(1).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+        }
+    }
+
+    @Test
+    public void testINPUTDATA_NOT_FOUND_FOR_DS_FileInput() {
+        final List<DMNMessage> validate = validator.validate(getFile("decisionservice/DS1ofEach_missingInputData.dmn"),
+                                                             VALIDATE_SCHEMA, VALIDATE_MODEL);
+        assertThat(ValidatorUtil.formatMessages(validate), validate.size(), is(2));
+        assertThat(validate.get(0).toString(), validate.get(0).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+        assertThat(validate.get(1).toString(), validate.get(1).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+    }
+
+    @Test
+    public void testINPUTDATA_NOT_FOUND_FOR_DS_DefinitionsInput() {
+        final List<DMNMessage> validate = validator.validate(getDefinitions("decisionservice/DS1ofEach_missingInputData.dmn",
+                                                                            "https://kiegroup.org/dmn/_40B3D02F-868C-4925-A1F2-5710DFEEF51E",
+                                                                            "DS1ofEach"),
+                                                             VALIDATE_MODEL, VALIDATE_COMPILATION);
+        assertThat(ValidatorUtil.formatMessages(validate), validate.size(), greaterThanOrEqualTo(2));
+        assertThat(validate.get(0).toString(), validate.get(0).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+        assertThat(validate.get(1).toString(), validate.get(1).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+    }
+
+    @Test
+    public void testOUTPUTELEMENT_NOT_FOUND_FOR_DS_ReaderInput() throws IOException {
+        try (final Reader reader = getReader("decisionservice/DS1ofEach_missingOutput.dmn")) {
+            final List<DMNMessage> validate = validator.validate(reader,
+                                                                 VALIDATE_SCHEMA, VALIDATE_MODEL);
+            assertThat(ValidatorUtil.formatMessages(validate), validate.size(), is(1)); // DS-1 missing its reference now.
+            assertThat(validate.get(0).toString(), validate.get(0).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+        }
+    }
+
+    @Test
+    public void testOUTPUTELEMENT_NOT_FOUND_FOR_DS_FileInput() {
+        final List<DMNMessage> validate = validator.validate(getFile("decisionservice/DS1ofEach_missingOutput.dmn"),
+                                                             VALIDATE_SCHEMA, VALIDATE_MODEL);
+        assertThat(ValidatorUtil.formatMessages(validate), validate.size(), is(1));
+        assertThat(validate.get(0).toString(), validate.get(0).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
+    }
+
+    @Test
+    public void testOUTPUTELEMENT_NOT_FOUND_FOR_DS_DefinitionsInput() {
+        final List<DMNMessage> validate = validator.validate(getDefinitions("decisionservice/DS1ofEach_missingOutput.dmn",
+                                                                            "https://kiegroup.org/dmn/_40B3D02F-868C-4925-A1F2-5710DFEEF51E",
+                                                                            "DS1ofEach"),
+                                                             VALIDATE_MODEL, VALIDATE_COMPILATION);
+        assertThat(ValidatorUtil.formatMessages(validate), validate.size(), greaterThanOrEqualTo(1));
+        assertThat(validate.get(0).toString(), validate.get(0).getMessageType(), is(DMNMessageType.REQ_NOT_FOUND));
     }
 }
