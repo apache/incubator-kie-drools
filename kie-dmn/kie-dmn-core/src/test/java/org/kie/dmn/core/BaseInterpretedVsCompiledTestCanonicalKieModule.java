8381c33813debfedb5950f4c29290d9df0bbee86
tarilabs <matteo.mortari@gmail.com>
2020-09-20 10:42:43 +0200

    document abstract base test class

--- kie-dmn/kie-dmn-core/src/test/java/org/kie/dmn/core/BaseInterpretedVsCompiledTestCanonicalKieModule.java
+++ kie-dmn/kie-dmn-core/src/test/java/org/kie/dmn/core/BaseInterpretedVsCompiledTestCanonicalKieModule.java
@@ -30,6 +30,13 @@ import org.kie.api.builder.ReleaseId;
 import org.kie.api.io.Resource;
 import org.kie.dmn.core.compiler.ExecModelCompilerOption;
 
+/**
+ * DROOLS-3238
+ * This base test class purpose is to test DMN model in KJAR containing Drools Model file.<br/>
+ * Specifically, as long as the base method {@link #wrapWithDroolsModelResource(KieServices, ReleaseId, Resource...)} is used, this will enable testing a DMN resource
+ * running alongside the CanonicalKieModule, which is needed while using the executable model for DRL file.<br/><br/>
+ * <i>Please note that these tests don't actually verify the correct behaviour of the compiled DRL files, but they only verify the DMN model.</i> Ref: https://github.com/kiegroup/drools/pull/2460#issue-298982811
+ */
 @RunWith(Parameterized.class)
 public abstract class BaseInterpretedVsCompiledTestCanonicalKieModule {
 
