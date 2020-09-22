d1c34f5ef0338867aa5722bd8a11b0592935d705
Jose Carvajal <josecarvajalhilario@gmail.com>
2020-09-22 11:55:19 +0200

    KOGITO-3371: Explainability Service > OIDC Quarkus Extension is not enabled (#460)

---
+++ kogito-explainability/explainability-service-rest/src/test/java/org/kie/kogito/explainability/rest/KeycloakExplainabilityServiceIT.java
@@ -0,0 +1,61 @@
+/*
+ *  Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.kie.kogito.explainability.rest;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.apache.http.HttpStatus;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.junit.jupiter.api.Test;
+import org.keycloak.representations.AccessTokenResponse;
+import org.kie.kogito.testcontainers.KogitoKeycloakContainer;
+import org.kie.kogito.testcontainers.quarkus.KeycloakQuarkusTestResource;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+@QuarkusTestResource(KeycloakQuarkusTestResource.Conditional.class)
+class KeycloakExplainabilityServiceIT {
+
+    private static final String VALID_USER = "jdoe";
+    private static final String SERVICE_ENDPOINT = "/health/live";
+
+    @ConfigProperty(name = KeycloakQuarkusTestResource.KOGITO_KEYCLOAK_PROPERTY)
+    String keycloakURL;
+
+    @Test
+    void shouldReturnUnauthorized() {
+        given().get(SERVICE_ENDPOINT)
+               .then().statusCode(HttpStatus.SC_UNAUTHORIZED);
+    }
+
+    @Test
+    void shouldReturnOkWhenValidUser() {
+        given().auth().oauth2(getAccessToken(VALID_USER)).get(SERVICE_ENDPOINT)
+               .then().statusCode(HttpStatus.SC_OK);
+    }
+
+    private String getAccessToken(String userName) {
+        return given().param("grant_type", "password")
+                      .param("username", userName)
+                      .param("password", userName)
+                      .param("client_id", KogitoKeycloakContainer.CLIENT_ID)
+                      .param("client_secret", KogitoKeycloakContainer.CLIENT_SECRET)
+                      .when()
+                      .post(keycloakURL + "/protocol/openid-connect/token")
+                      .as(AccessTokenResponse.class).getToken();
+    }
+}
