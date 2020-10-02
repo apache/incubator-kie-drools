b08bdb5f1fdf0e7aaaf9afbfbe23155d65e04a81
Jacopo Rota <jacopo.r00ta@gmail.com>
2020-10-02 14:04:50 +0200

    fix logger (#796)

--- kogito-quarkus-extension/deployment/src/main/java/org/kie/kogito/quarkus/deployment/InMemoryCompiler.java
+++ kogito-quarkus-extension/deployment/src/main/java/org/kie/kogito/quarkus/deployment/InMemoryCompiler.java
@@ -38,7 +38,7 @@ import org.slf4j.LoggerFactory;
  */
 public class InMemoryCompiler {
 
-    private static final Logger logger = LoggerFactory.getLogger(KogitoAssetsProcessor.class);
+    private static final Logger logger = LoggerFactory.getLogger(InMemoryCompiler.class);
 
 
     private final JavaCompiler javaCompiler;
