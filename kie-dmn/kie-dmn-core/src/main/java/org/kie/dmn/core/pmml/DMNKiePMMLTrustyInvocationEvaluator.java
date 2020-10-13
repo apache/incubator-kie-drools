2cd62767a0eee684a06b1ea7e0005ef99b733dc6
mariofusco <mario.fusco@gmail.com>
2020-10-13 19:17:36 +0200

    wip

--- kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/pmml/DMNKiePMMLTrustyInvocationEvaluator.java
+++ kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/pmml/DMNKiePMMLTrustyInvocationEvaluator.java
@@ -16,9 +16,6 @@
 
 package org.kie.dmn.core.pmml;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -26,18 +23,19 @@ import java.util.UUID;
 
 import org.kie.api.io.Resource;
 import org.kie.api.pmml.PMML4Result;
+import org.kie.api.runtime.KieRuntimeFactory;
 import org.kie.dmn.api.core.DMNMessage;
 import org.kie.dmn.api.core.DMNResult;
 import org.kie.dmn.api.core.event.DMNRuntimeEventManager;
 import org.kie.dmn.core.ast.DMNFunctionDefinitionEvaluator.FormalParameter;
 import org.kie.dmn.core.impl.DMNResultImpl;
+import org.kie.dmn.core.impl.DMNRuntimeImpl;
 import org.kie.dmn.core.util.Msg;
 import org.kie.dmn.core.util.MsgUtil;
 import org.kie.dmn.feel.util.EvalHelper;
 import org.kie.dmn.model.api.DMNElement;
 import org.kie.pmml.evaluator.api.executor.PMMLContext;
 import org.kie.pmml.evaluator.api.executor.PMMLRuntime;
-import org.kie.pmml.evaluator.assembler.factories.PMMLRuntimeFactory;
 import org.kie.pmml.evaluator.core.PMMLContextImpl;
 import org.kie.pmml.evaluator.core.utils.PMMLRequestDataBuilder;
 import org.slf4j.Logger;
@@ -110,8 +108,8 @@ public class DMNKiePMMLTrustyInvocationEvaluator extends AbstractDMNKiePMMLInvoc
      * @return
      */
     private PMMLRuntime getPMMLRuntime(DMNRuntimeEventManager eventManager) {
-        final File pmmlFile = getPMMLFile();
-        return PMMLRuntimeFactory.getPMMLRuntime(model, pmmlFile);
+        KieRuntimeFactory kieFactory = ((DMNRuntimeImpl) eventManager.getRuntime()).getKieRuntimeFactory(model);
+        return kieFactory.get(PMMLRuntime.class);
     }
 
     private PMMLContext getPMMLPMMLContext(String correlationId, String modelName, DMNResult dmnr) {
@@ -122,45 +120,5 @@ public class DMNKiePMMLTrustyInvocationEvaluator extends AbstractDMNKiePMMLInvoc
             pmmlRequestDataBuilder.addParameter(p.name, pValue, class1);
         }
         return new PMMLContextImpl(pmmlRequestDataBuilder.build());
-    }
-
-    private File getPMMLFile() {
-        try(InputStream inputStream = documentResource.getInputStream()) {
-            return getPMMLFile(model, inputStream);
-        } catch(Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Load a <code>File</code> with the given <b>fullFileName</b> from the given
-     * <code>InputStream</code>
-     *
-     * @param fileName <b>full path</b> of file to load
-     * @param inputStream
-     * @return
-     */
-    private File getPMMLFile(String fileName, InputStream inputStream) {
-        FileOutputStream outputStream = null;
-        try  {
-            File toReturn = File.createTempFile(fileName, null);
-            outputStream = new FileOutputStream(toReturn);
-            byte[] byteArray = new byte[1024];
-            int i;
-            while ((i = inputStream.read(byteArray)) > 0) {
-                outputStream.write(byteArray, 0, i);
-            }
-            return toReturn;
-        } catch(Exception e) {
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (outputStream != null) {
-                    outputStream.close();
-                }
-            } catch (Exception e) {
-                LOG.warn("Failed to close outputStream", e);
-            }
-        }
     }
 }
