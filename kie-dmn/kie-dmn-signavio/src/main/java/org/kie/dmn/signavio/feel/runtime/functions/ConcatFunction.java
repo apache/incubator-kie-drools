b48b9a8f19790be1e0c71bac937bc0d6d6baa19f
Jonas Beyer <jonas.beyer@signavio.com>
2020-10-13 16:41:26 +0200

    DROOLS-5723 Implement Signavio DMN concat() function

---
+++ kie-dmn/kie-dmn-signavio/src/main/java/org/kie/dmn/signavio/feel/runtime/functions/ConcatFunction.java
@@ -0,0 +1,48 @@
+package org.kie.dmn.signavio.feel.runtime.functions;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.kie.dmn.api.feel.runtime.events.FEELEvent;
+import org.kie.dmn.feel.runtime.events.InvalidParametersEvent;
+import org.kie.dmn.feel.runtime.functions.BaseFEELFunction;
+import org.kie.dmn.feel.runtime.functions.FEELFnResult;
+import org.kie.dmn.feel.runtime.functions.ParameterName;
+
+public class ConcatFunction
+		extends BaseFEELFunction {
+	
+	public ConcatFunction() {
+		super("concat");
+	}
+	
+	
+	public FEELFnResult<String> invoke(@ParameterName("values") List<?> list) {
+		if (list == null) {
+			return FEELFnResult.ofError(new InvalidParametersEvent(FEELEvent.Severity.ERROR, "list", "cannot be null"));
+		}
+		if (list.contains(null)) {
+			return FEELFnResult.ofError(new InvalidParametersEvent(FEELEvent.Severity.ERROR, "list", "cannot contain null values"));
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (Object element : list) {
+			if (!(element instanceof String)) {
+				return FEELFnResult.ofError(new InvalidParametersEvent(FEELEvent.Severity.ERROR, "list", "contains an element that is not a string"));
+			}
+			sb.append(element);
+		}
+		return FEELFnResult.ofResult(sb.toString());
+	}
+	
+	
+	public FEELFnResult<String> invoke(@ParameterName("values") Object[] list) {
+		return invoke(Arrays.asList(list));
+	}
+	
+	
+	public FEELFnResult<String> invoke() {
+		return FEELFnResult
+				.ofError(new InvalidParametersEvent(FEELEvent.Severity.ERROR, "list", "is missing"));
+	}
+}
