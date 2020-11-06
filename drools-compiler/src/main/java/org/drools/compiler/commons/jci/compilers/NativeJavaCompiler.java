1c0e16ddb8e40276d0957251e27e4d597c09644d
Jiří Locker <jiri.locker@gmail.com>
2020-11-06 10:23:48 +0100

    KOGITO-3778: Fix loading of classes from Spring Boot fat jars (#3220)

--- drools-compiler/src/main/java/org/drools/compiler/commons/jci/compilers/NativeJavaCompiler.java
+++ drools-compiler/src/main/java/org/drools/compiler/commons/jci/compilers/NativeJavaCompiler.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -355,7 +355,7 @@ public class NativeJavaCompiler extends AbstractJavaCompiler {
 
         private List<JavaFileObject> processJar(URL packageFolderURL) throws IOException {
             String jarUri = packageFolderURL.toExternalForm();
-            int separator = jarUri.indexOf('!');
+            int separator = jarUri.lastIndexOf('!');
             if (separator >= 0) {
                 jarUri = jarUri.substring(0, separator);
             }
