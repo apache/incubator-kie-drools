8cd158860e00b888616b36ed03db5df55064aa84
Tiago Dolphine <tiagodolphine@gmail.com>
2020-12-04 10:13:02 -0300

    KOGITO-3700 migration to spring web api fixes (#538)

--- kogito-apps-integration-tests/integration-tests-trusty-service/integration-tests-trusty-service-springboot/src/main/java/org/kie/kogito/KogitoApplication.java
+++ kogito-apps-integration-tests/integration-tests-trusty-service/integration-tests-trusty-service-springboot/src/main/java/org/kie/kogito/KogitoApplication.java
@@ -18,7 +18,7 @@ package org.kie.kogito;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"org.kie.kogito.**"})
+@SpringBootApplication(scanBasePackages = {"org.kie.kogito.**", "http*"})
 public class KogitoApplication {
 
     public static void main(String[] args) {
