1db03335885819883f8c1cff0a355a3e0885c57f
Karel Suta <ksuta@redhat.com>
2020-12-09 15:41:25 +0100

    KOGITO-3984 SpringBoot application generated by archetype missing http package scan (#924)
    Signed-off-by: Karel Suta <ksuta@redhat.com>

--- archetypes/kogito-springboot-archetype/src/main/resources/archetype-resources/src/main/java/KogitoApplication.java
+++ archetypes/kogito-springboot-archetype/src/main/resources/archetype-resources/src/main/java/KogitoApplication.java
@@ -3,7 +3,7 @@ package ${package};
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={"${package}.**", "${groupId}.**"})
+@SpringBootApplication(scanBasePackages={"${package}.**", "${groupId}.**", "http**"})
 public class KogitoApplication {
 
     public static void main(String[] args) {
