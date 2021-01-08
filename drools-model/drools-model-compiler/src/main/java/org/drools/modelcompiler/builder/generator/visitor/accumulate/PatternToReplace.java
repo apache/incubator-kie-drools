73ab9e2b50184abfcec146697753e30972280e79
mariofusco <mario.fusco@gmail.com>
2021-01-08 12:31:37 +0100

    [DROOLS-5938] fix resolution of accumulating variable in executable model

--- drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/visitor/accumulate/PatternToReplace.java
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/visitor/accumulate/PatternToReplace.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.MethodCallExpr;
@@ -17,19 +16,16 @@ import static org.drools.modelcompiler.builder.generator.DslMethodNames.PATTERN_
 public class PatternToReplace {
 
     final RuleContext context;
-    final Collection<String> patternBindings;
+    final List<Expression> bindingExprsVars;
 
     public PatternToReplace(RuleContext context, Collection<String> patternBindings) {
         this.context = context;
-        this.patternBindings = patternBindings;
-    }
-
-    private Stream<MethodCallExpr> allMethodCallExpressions() {
-        return context.getExpressions().stream().flatMap(e -> e.findAll(MethodCallExpr.class).stream());
+        this.bindingExprsVars = patternBindings.stream().map(context::getVarExpr).collect(toList());
     }
 
     public Optional<MethodCallExpr> findFromPattern() {
-        return allMethodCallExpressions()
+        return context.getExpressions().stream()
+                .flatMap(e -> e.findAll(MethodCallExpr.class).stream())
                 .filter(expr -> expr.getName().asString().equals(PATTERN_CALL))
                 .filter(this::hasBindingExprVar)
                 .map(Expression::asMethodCallExpr)
@@ -37,7 +33,6 @@ public class PatternToReplace {
     }
 
     private boolean hasBindingExprVar(MethodCallExpr expr) {
-        List<Expression> bindingExprsVars = patternBindings.stream().map(context::getVarExpr).collect(toList());
         return !Collections.disjoint(bindingExprsVars, expr.getArguments());
     }
 }
