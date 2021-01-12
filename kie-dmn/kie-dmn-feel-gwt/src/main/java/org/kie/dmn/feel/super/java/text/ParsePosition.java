9842054aba3fcd107bcbb3d12248c22c0212ee11
Guilherme Carreiro <karreiro@gmail.com>
2021-01-13 00:24:57 +0100

    .

---
+++ kie-dmn/kie-dmn-feel-gwt/src/main/java/org/kie/dmn/feel/super/java/text/ParsePosition.java
@@ -0,0 +1,47 @@
+
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package java.text;
+
+public class ParsePosition {
+
+    public int getIndex() {
+        return 0;
+    }
+
+    public void setIndex(final int index) {
+    }
+
+    public void setErrorIndex(final int ei) {
+    }
+
+    public int getErrorIndex() {
+        return 0;
+    }
+
+    public boolean equals(final Object obj) {
+        return true;
+    }
+
+    public int hashCode() {
+        return 0;
+    }
+
+    public String toString() {
+        return null;
+    }
+}
