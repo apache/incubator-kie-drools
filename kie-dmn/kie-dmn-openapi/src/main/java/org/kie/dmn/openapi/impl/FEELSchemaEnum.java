d7b2b2de87cfd17f3bb465b013ff5a9a50e2ab33
tarilabs <matteo.mortari@gmail.com>
2021-01-12 11:25:36 +0100

    DROOLS-5943 DMN OAS enhance number handling

--- kie-dmn/kie-dmn-openapi/src/main/java/org/kie/dmn/openapi/impl/FEELSchemaEnum.java
+++ kie-dmn/kie-dmn-openapi/src/main/java/org/kie/dmn/openapi/impl/FEELSchemaEnum.java
@@ -16,32 +16,80 @@
 
 package org.kie.dmn.openapi.impl;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.eclipse.microprofile.openapi.models.media.Schema;
 import org.kie.dmn.api.core.DMNUnaryTest;
 import org.kie.dmn.feel.FEEL;
+import org.kie.dmn.feel.runtime.Range;
+import org.kie.dmn.feel.runtime.Range.RangeBoundary;
 import org.kie.dmn.feel.runtime.UnaryTestImpl;
+import org.kie.dmn.feel.runtime.impl.RangeImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FEELSchemaEnum {
 
+    private static final Logger LOG = LoggerFactory.getLogger(FEELSchemaEnum.class);
+
     public static void parseAllowedValuesIntoSchema(Schema schema, List<DMNUnaryTest> list) {
-        try {
-            FEEL SimpleFEEL = FEEL.newInstance();
-            List<Object> expectLiterals = list.stream().map(UnaryTestImpl.class::cast)
-                                                       .map(UnaryTestImpl::toString)
-                                                       .map(SimpleFEEL::evaluate)
-                                                       .collect(Collectors.toList());
-            expectLiterals.forEach(o -> {
-                if (!(o instanceof String || o instanceof Number || o instanceof Boolean)) {
-                    throw new UnsupportedOperationException("OAS enumeration only checked for specifc types.");
-                }
-            });
+        List<Object> expectLiterals = evaluateUnaryTests(list);
+        boolean allLiterals = expectLiterals.stream().allMatch(o -> o instanceof String || o instanceof Number || o instanceof Boolean);
+        if (allLiterals) {
             schema.enumeration(expectLiterals);
-        } catch (Exception e) {
-            schema.addExtension(DMNOASConstants.X_DMN_TYPE, schema.getExtensions().get(DMNOASConstants.X_DMN_TYPE) + "\n" + list);
+        } else {
+            LOG.warn("Unable to parse generic allowed value into the JSON Schema for enumeration");
         }
+    }
+    
+    public static void parseNumberAllowedValuesIntoSchema(Schema schema, List<DMNUnaryTest> list) {
+        List<Object> uts = evaluateUnaryTests(list); // we leverage the property of the *base* FEEL grammar(non visited by ASTVisitor, only the ParseTree->AST Visitor) that `>x` is a Range
+        if (uts.size() <= 2 && uts.stream().allMatch(o -> o instanceof Range)) {
+            Range range = consolidateRanges((List) uts); // cast intentional.
+            if (range != null) {
+                schema.minimum((BigDecimal) range.getLowEndPoint());
+                schema.exclusiveMinimum(range.getLowBoundary() == RangeBoundary.OPEN);
+                schema.maximum((BigDecimal) range.getHighEndPoint());
+                schema.exclusiveMaximum(range.getHighBoundary() == RangeBoundary.OPEN);
+            }
+        } else if (uts.stream().allMatch(o -> o instanceof Number)) {
+            schema.enumeration(uts);
+        } else {
+            LOG.warn("Unable to parse generic allowed value into the JSON Schema for enumeration");
+        }
+    }
+
+    public static Range consolidateRanges(List<Range> ranges) {
+        boolean consistent = true;
+        Range result = new RangeImpl();
+        for (Range r : ranges) {
+            if (r.getLowEndPoint() != null) {
+                if (result.getLowEndPoint() == null) {
+                    result = new RangeImpl(r.getLowBoundary(), r.getLowEndPoint(), result.getHighEndPoint(), result.getHighBoundary());
+                } else {
+                    consistent = false;
+                }
+            }
+            if (r.getHighEndPoint() != null) {
+                if (result.getHighEndPoint() == null) {
+                    result = new RangeImpl(result.getLowBoundary(), result.getLowEndPoint(), r.getHighEndPoint(), r.getHighBoundary());
+                } else {
+                    consistent = false;
+                }
+            }
+        }
+        return consistent ? result : null;
+    }
+
+    private static List<Object> evaluateUnaryTests(List<DMNUnaryTest> list) {
+    	FEEL SimpleFEEL = FEEL.newInstance();
+        List<Object> utEvaluated = list.stream().map(UnaryTestImpl.class::cast)
+                                       .map(UnaryTestImpl::toString)
+                                       .map(SimpleFEEL::evaluate)
+                                       .collect(Collectors.toList());
+        return utEvaluated;
     }
 
     private FEELSchemaEnum() {
