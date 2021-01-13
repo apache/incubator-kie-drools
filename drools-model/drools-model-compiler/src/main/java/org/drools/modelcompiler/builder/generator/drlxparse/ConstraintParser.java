2c7b58e74fef8709e2952dc7dc392719f55feb98
mariofusco <mario.fusco@gmail.com>
2021-01-13 18:09:39 +0100

    [DROOLS-5945] fix type coercion for a string bound variable in executable model

--- drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/drlxparse/ConstraintParser.java
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/drlxparse/ConstraintParser.java
@@ -449,18 +449,32 @@ public class ConstraintParser {
     }
 
     private static SpecialComparisonResult getEqualityExpression(TypedExpression left, TypedExpression right, BinaryExpr.Operator operator ) {
-        if((isAnyOperandBigDecimal(left, right) || isAnyOperandBigInteger(left, right)) && !isAnyOperandNullLiteral( left, right )) {
+        if ((isAnyOperandBigDecimal(left, right) || isAnyOperandBigInteger(left, right)) && !isAnyOperandNullLiteral( left, right )) {
             return compareBigDecimal(operator, left, right);
         }
 
-        String equalsMethod = isNumber(left) && isNumber(right) ?
+        boolean isLeftNumber = isNumber(left);
+        boolean isRightNumber = isNumber(right);
+
+        String equalsMethod = isLeftNumber && isRightNumber ?
                 "org.drools.modelcompiler.util.EvaluationUtil.areNumbersNullSafeEquals" :
                 "org.drools.modelcompiler.util.EvaluationUtil.areNullSafeEquals";
 
+        Expression leftExpr = left.uncastExpression();
+        Expression rightExpr = right.uncastExpression();
+
+        if (isLeftNumber) {
+            if ( right.getBoxedType().map( String.class::isAssignableFrom ).orElse( false ) ) {
+                leftExpr = new BinaryExpr(new StringLiteralExpr(""), leftExpr, BinaryExpr.Operator.PLUS);
+            }
+        } else if ( isRightNumber && left.getBoxedType().map( String.class::isAssignableFrom ).orElse( false ) ) {
+            rightExpr = new BinaryExpr(new StringLiteralExpr(""), rightExpr, BinaryExpr.Operator.PLUS);
+        }
+
         MethodCallExpr methodCallExpr = new MethodCallExpr( null, equalsMethod );
         // Avoid casts, by using an helper method we leverage autoboxing and equals
-        methodCallExpr.addArgument(left.uncastExpression());
-        methodCallExpr.addArgument(right.uncastExpression());
+        methodCallExpr.addArgument(leftExpr);
+        methodCallExpr.addArgument(rightExpr);
         Expression expression = operator == BinaryExpr.Operator.EQUALS ? methodCallExpr : new UnaryExpr(methodCallExpr, UnaryExpr.Operator.LOGICAL_COMPLEMENT);
         return new SpecialComparisonResult(expression, left, right);
     }
