43bffd22591c9d0d6956683a6a97394249b479be
rikkola <toni@rikkola.net>
2021-01-13 15:48:16 +0200

    clean up

--- kie-dmn/kie-dmn-feel-gwt/src/main/java/org/kie/dmn/feel/super/java/time/ZoneOffset.java
+++ kie-dmn/kie-dmn-feel-gwt/src/main/java/org/kie/dmn/feel/super/java/time/ZoneOffset.java
@@ -1,6 +1,22 @@
-
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package java.time;
 
+import java.io.DataOutput;
+import java.io.IOException;
 import java.io.Serializable;
 import java.time.format.TextStyle;
 import java.time.temporal.Temporal;
@@ -11,6 +27,8 @@ import java.time.temporal.TemporalQuery;
 import java.time.temporal.ValueRange;
 import java.time.zone.ZoneRules;
 import java.util.Locale;
+
+import org.kie.dmn.model.api.GwtIncompatible;
 
 public final class ZoneOffset
         extends ZoneId
@@ -73,6 +91,11 @@ public final class ZoneOffset
 
     public String toString() {
         return null;
+    }
+
+    @GwtIncompatible
+    void write(final DataOutput out) throws IOException {
+
     }
 
     public String getDisplayName(final TextStyle style, final Locale locale) {
