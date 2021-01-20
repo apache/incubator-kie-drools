ab0dac067a03788e3481e3f18ae5e57b42b138b4
Matteo Mortari <matteo.mortari@gmail.com>
2021-01-20 16:13:15 +0100

    DROOLS-5944 DMN OAS investigate InputData with undefined typeRef (#3329)

---
+++ kie-dmn/kie-dmn-openapi/src/test/java/org/kie/dmn/openapi/UndefinedTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.dmn.openapi;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.networknt.schema.JsonSchema;
+import org.junit.Test;
+import org.kie.dmn.api.core.DMNModel;
+import org.kie.dmn.api.core.DMNRuntime;
+import org.kie.dmn.openapi.model.DMNOASResult;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UndefinedTest extends BaseDMNOASTest {
+
+    @Test
+    public void testUndefinedIO() throws Exception {
+        final DMNRuntime runtime = createRuntime("undefinedIO.dmn", this.getClass());
+        DMNOASResult result = DMNOASGeneratorFactory.generator(runtime.getModels()).build();
+
+        DMNModel modelUnderTest = runtime.getModel("ns1", "undefinedIO");
+        ObjectNode syntheticJSONSchema = synthesizeSchema(result, modelUnderTest);
+        JsonSchema validator = getJSONSchema(syntheticJSONSchema);
+
+        assertThat(validateUsing(validator, "{ \"asd\": 0}")).isNotEmpty();
+        assertThat(validateUsing(validator, "{ \"in1\": 123}")).isEmpty();
+        assertThat(validateUsing(validator, "{ \"in1\": \"John Doe\"}")).isEmpty();
+    }
+}
