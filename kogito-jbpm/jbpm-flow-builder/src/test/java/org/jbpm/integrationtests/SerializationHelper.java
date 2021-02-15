0dc87689ea797550b54001420f2b07e65f6406d9
Cristiano Nicolai <570894+cristianonicolai@users.noreply.github.com>
2021-02-15 15:47:59 +1000

    KOGITO-4376 Copyright reformat to match expected format (#1065)

--- kogito-jbpm/jbpm-flow-builder/src/test/java/org/jbpm/integrationtests/SerializationHelper.java
+++ kogito-jbpm/jbpm-flow-builder/src/test/java/org/jbpm/integrationtests/SerializationHelper.java
@@ -1,7 +1,9 @@
 /*
- * Copyright (c) 2020. Red Hat, Inc. and/or its affiliates.
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *       http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -11,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.jbpm.integrationtests;
 
 import java.io.ByteArrayInputStream;
