82e2e0adf4aa9d6e62669a8c49b9450150015fde
Mario Fusco <mario.fusco@gmail.com>
2021-02-19 14:11:00 +0100

    [RHDM-1552] make setted properties to be recognized for property reactivity both inside and outside a modify block (#3393)
    (cherry picked from commit cc18a491b9b278a9e8872dd385772ce347eb8ef4)

--- drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/PropertyReactivityTest.java
+++ drools-model/drools-model-compiler/src/test/java/org/drools/modelcompiler/PropertyReactivityTest.java
@@ -1171,4 +1171,26 @@ public class PropertyReactivityTest extends BaseModelTest {
 
         assertEquals(43, p.getAge());
     }
+
+    @Test
+    public void testSettersInAndOutModifyBlock() {
+        // RHDM-1552
+        final String str =
+                "import " + Person.class.getCanonicalName() + ";\n" +
+                "\n" +
+                "rule R when\n" +
+                "    $p : Person( age < 50 )\n" +
+                "then\n" +
+                "    $p.setAge( $p.getAge() + 1 );\n" +
+                "    modify($p) { setName( \"Mario\" ) };\n" +
+                "end\n";
+
+        KieSession ksession = getKieSession( str );
+
+        Person p = new Person("Mario", 40);
+        ksession.insert( p );
+        ksession.fireAllRules(3);
+
+        assertEquals(43, p.getAge());
+    }
 }
