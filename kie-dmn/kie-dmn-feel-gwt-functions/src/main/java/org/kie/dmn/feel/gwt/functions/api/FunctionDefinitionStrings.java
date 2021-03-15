547ed93a7a43be417c994ebb21a51174f3273119
Guilherme Carreiro <karreiro@gmail.com>
2021-03-15 14:47:01 +0100

    Include missing headers on 'kie-dmn-feel-gwt-functions'

--- kie-dmn/kie-dmn-feel-gwt-functions/src/main/java/org/kie/dmn/feel/gwt/functions/api/FunctionDefinitionStrings.java
+++ kie-dmn/kie-dmn-feel-gwt-functions/src/main/java/org/kie/dmn/feel/gwt/functions/api/FunctionDefinitionStrings.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.kie.dmn.feel.gwt.functions.api;
 
 public class FunctionDefinitionStrings {
