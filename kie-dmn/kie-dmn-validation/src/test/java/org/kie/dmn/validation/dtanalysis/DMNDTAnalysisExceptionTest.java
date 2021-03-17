01bf1bb645a0eba7665629aed44ade2ecf913ce1
tarilabs <matteo.mortari@gmail.com>
2021-03-17 10:32:23 +0100

    smoke test DMNDTAnalysisException

---
+++ kie-dmn/kie-dmn-validation/src/test/java/org/kie/dmn/validation/dtanalysis/DMNDTAnalysisExceptionTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.dmn.validation.dtanalysis;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.kie.dmn.model.api.DecisionTable;
+import org.kie.dmn.model.v1_3.TDecisionTable;
+
+public class DMNDTAnalysisExceptionTest {
+
+    @Test
+    public void smokeTest() {
+        DecisionTable dtRef = new TDecisionTable();
+        DMNDTAnalysisException ut = new DMNDTAnalysisException("smoke test", dtRef);
+        Assertions.assertThat(ut.getDt()).isEqualTo(dtRef);
+    }
+}
