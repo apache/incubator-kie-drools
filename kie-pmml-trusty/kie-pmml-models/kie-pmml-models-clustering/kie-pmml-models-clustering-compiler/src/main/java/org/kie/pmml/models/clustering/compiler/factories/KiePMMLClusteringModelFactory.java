89f0c5e075c2f0ccba0f684c6a6ea6f5ad87a4fe
Alessandro Costa <alecosta.me@gmail.com>
2021-03-24 12:24:39 +0100

    DROOLS-6100: bare kie-pmml-models-clustering structure generated from archetype

---
+++ kie-pmml-trusty/kie-pmml-models/kie-pmml-models-clustering/kie-pmml-models-clustering-compiler/src/main/java/org/kie/pmml/models/clustering/compiler/factories/KiePMMLClusteringModelFactory.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package  org.kie.pmml.models.clustering.compiler.factories;
+
+import java.util.Map;
+
+import org.dmg.pmml.DataDictionary;
+import org.dmg.pmml.TransformationDictionary;
+import org.dmg.pmml.clustering.ClusteringModel;
+import org.kie.pmml.commons.model.HasClassLoader;
+import org.kie.pmml.models.clustering.model.KiePMMLClusteringModel;
+
+public class KiePMMLClusteringModelFactory {
+
+    private KiePMMLClusteringModelFactory(){
+        // Avoid instantiation
+    }
+
+    public static KiePMMLClusteringModel getKiePMMLClusteringModel(final DataDictionary dataDictionary,
+                                                                       final TransformationDictionary transformationDictionary,
+                                                                       final ClusteringModel model,
+                                                                       final String packageName,
+                                                                       final HasClassLoader hasClassLoader) {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    public static Map<String, String> getKiePMMLClusteringModelSourcesMap(final DataDictionary dataDictionary,
+                                                                                 final TransformationDictionary transformationDictionary,
+                                                                                 final ClusteringModel model,
+                                                                                 final String packageName) {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+
+}
---
+++ kie-pmml-trusty/kie-pmml-models/kie-pmml-models-clustering/kie-pmml-models-clustering-compiler/src/test/java/org/kie/pmml/models/clustering/compiler/executor/ClusteringModelImplementationProviderTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.pmml.models.clustering.compiler.executor;
+
+import org.kie.pmml.api.enums.PMML_MODEL;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class ClusteringModelImplementationProviderTest {
+
+    private static final ClusteringModelImplementationProvider PROVIDER= new ClusteringModelImplementationProvider();
+
+    @Test
+    public void getPMMLModelType(){
+        assertEquals(PMML_MODEL.CLUSTERING_MODEL,PROVIDER.getPMMLModelType());
+    }
+
+    @Test
+    public void getKiePMMLModel() throws Exception {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void getKiePMMLModelWithSources() throws Exception {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+}
