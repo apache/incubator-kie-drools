5d0d3c4cb29dcccc7db4f573b522b480d2b226d5
Luca Molteni <volothamp@gmail.com>
2021-04-19 10:53:51 +0200

    [DROOLS-5989] DMN Alpha Network - Support multiple Unary Tests for each Decision Table cell

--- kie-dmn/kie-dmn-core/src/main/resources/org/kie/dmn/core/alphasupport/AlphaNodeCreationTemplate.java
+++ kie-dmn/kie-dmn-core/src/main/resources/org/kie/dmn/core/alphasupport/AlphaNodeCreationTemplate.java
@@ -1,15 +1,23 @@
 package org.kie.dmn.core.alphasupport;
 
 import org.kie.dmn.core.compiler.alphanetbased.AlphaNetworkCreation;
-import org.kie.dmn.core.compiler.alphanetbased.AlphaNetworkCreation;
 import org.kie.dmn.core.compiler.alphanetbased.TableContext;
 
 // All implementations are used only for templating purposes and should never be called
-public class AlphaNodeCreationTemplate  {
+public class AlphaNodeCreationTemplate {
 
     private AlphaNetworkCreation alphaNetworkCreation;
 
     public AlphaNodeCreationTemplate(org.kie.dmn.core.compiler.alphanetbased.NetworkBuilderContext ctx) {
         alphaNetworkCreation = new AlphaNetworkCreation(ctx);
+    }
+
+    boolean testRxCx(TableContext x) {
+        return UnaryTestRXCX.getInstance().getUnaryTests()
+                .stream()
+                .anyMatch(t -> {
+                    Boolean result = t.apply(x.getEvalCtx(), x.getValue(99999));
+                    return result != null && result;
+                });
     }
 }
