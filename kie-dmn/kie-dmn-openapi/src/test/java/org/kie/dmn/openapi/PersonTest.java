ad4d946e40f10bddc69b9b82db33951300461f8a
Matteo Mortari <matteo.mortari@gmail.com>
2021-04-23 10:41:55 +0200

    DROOLS-6294 DMN OAS for composite field a ref of collections of composites (#3555)

---
+++ kie-dmn/kie-dmn-openapi/src/test/java/org/kie/dmn/openapi/PersonTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.dmn.openapi;
+
+import org.junit.Test;
+import org.kie.dmn.api.core.DMNModel;
+import org.kie.dmn.api.core.DMNRuntime;
+import org.kie.dmn.openapi.model.DMNOASResult;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.networknt.schema.JsonSchema;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PersonTest extends BaseDMNOASTest {
+
+    @Test
+    public void test() throws Exception {
+        final DMNRuntime runtime = createRuntime("personF.dmn", this.getClass());
+        DMNOASResult result = DMNOASGeneratorFactory.generator(runtime.getModels()).build();
+
+        DMNModel modelUnderTest = runtime.getModel("https://kiegroup.org/dmn/_4564F2BC-888D-472A-A38C-A861DA49A780", "personF");
+        ObjectNode syntheticJSONSchema = synthesizeSchema(result, modelUnderTest);
+        JsonSchema validator = getJSONSchema(syntheticJSONSchema);
+
+        assertThat(validateUsing(validator, "{ \"an order\":123 }")).isNotEmpty();
+        assertThat(validateUsing(validator, "{ \"p\": {\"name\":\"John Doe\", \"favourites\":[{\"name\":\"red\", \"hex\": \"#FF0000\"}] }}")).isEmpty();
+    }
+}
