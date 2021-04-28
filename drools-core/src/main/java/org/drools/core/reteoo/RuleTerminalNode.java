4db25431ab9815deb7080b363f925bf8d6db3623
Mario Fusco <mario.fusco@gmail.com>
2021-04-28 09:50:49 +0200

    [RHDM-1711] remove need for a default method in Consequence interface (#3567)
    (cherry picked from commit 7df100cf45e0e6c030d11bf8f393cf8a80726789)

--- drools-core/src/main/java/org/drools/core/reteoo/RuleTerminalNode.java
+++ drools-core/src/main/java/org/drools/core/reteoo/RuleTerminalNode.java
@@ -32,7 +32,6 @@ import org.drools.core.phreak.RuleExecutor;
 import org.drools.core.reteoo.builder.BuildContext;
 import org.drools.core.rule.Declaration;
 import org.drools.core.rule.GroupElement;
-import org.drools.core.spi.Consequence;
 import org.drools.core.spi.PropagationContext;
 import org.drools.core.spi.Tuple;
 
@@ -92,14 +91,6 @@ public class RuleTerminalNode extends AbstractTerminalNode {
         this.subrule = subrule;
         this.consequenceName = context.getConsequenceName();
         initDeclarations();
-
-        Consequence consequence = consequenceName == null || consequenceName.equals( RuleImpl.DEFAULT_CONSEQUENCE_NAME ) ?
-                rule.getConsequence() :
-                rule.getNamedConsequence(consequenceName);
-        if ( consequence != null) {
-            // This can only be null certain mock unit tests
-            consequence.initDeclarations(requiredDeclarations);
-        }
 
         this.subruleIndex = subruleIndex;
 
