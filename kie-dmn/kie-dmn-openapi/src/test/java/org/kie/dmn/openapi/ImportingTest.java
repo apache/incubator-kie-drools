0f7db067400eb72dcc0f935650e0b4dbd6d9bfed
tarilabs <matteo.mortari@gmail.com>
2021-05-10 11:14:37 +0200

    extended test

--- kie-dmn/kie-dmn-openapi/src/test/java/org/kie/dmn/openapi/ImportingTest.java
+++ kie-dmn/kie-dmn-openapi/src/test/java/org/kie/dmn/openapi/ImportingTest.java
@@ -22,6 +22,7 @@ import org.kie.dmn.api.core.DMNRuntime;
 import org.kie.dmn.core.imports.ImportsTest;
 import org.kie.dmn.openapi.model.DMNOASResult;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.networknt.schema.JsonSchema;
 
@@ -36,12 +37,22 @@ public class ImportingTest extends BaseDMNOASTest {
                                                                         "importingSum.dmn");
         DMNOASResult result = DMNOASGeneratorFactory.generator(runtime.getModels()).build();
 
-        DMNModel modelUnderTest = runtime.getModel("https://kiegroup.org/dmn/_1D35A3BF-1DBD-4CD0-882A-CA068C6F2A67",
+        final DMNModel modelUnderTest = runtime.getModel("https://kiegroup.org/dmn/_1D35A3BF-1DBD-4CD0-882A-CA068C6F2A67",
                                                    "importingSum");
         ObjectNode syntheticJSONSchema = synthesizeSchema(result, modelUnderTest);
         JsonSchema validator = getJSONSchema(syntheticJSONSchema);
 
         assertThat(validateUsing(validator, "{ \"x\": 1, \"y\": 2 }")).isEmpty();
+
+        String mutOutputSetName = result.getNamingPolicy().getName(result.lookupIOSetsByModel(modelUnderTest).getOutputSet());
+        JsonNode mutOutputSet = syntheticJSONSchema.get("definitions").get(mutOutputSetName);
+        assertThat(() -> mutOutputSet.get("properties").fieldNames()).doesNotContain("baseSum Decision");
+
+        final DMNModel importedModel = runtime.getModel("https://kiegroup.org/dmn/_FCC62740-4998-47A2-B5F2-CB3E15C98419",
+                                                        "baseSum");
+        String importedOutputSetName = result.getNamingPolicy().getName(result.lookupIOSetsByModel(importedModel).getOutputSet());
+        JsonNode importedOutputSet = syntheticJSONSchema.get("definitions").get(importedOutputSetName);
+        assertThat(() -> importedOutputSet.get("properties").fieldNames()).contains("baseSum Decision");
     }
 
 }
