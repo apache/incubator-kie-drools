6c4b0c30c3e649532a0da0033eb409cae9120ab3
Tiago Dolphine <tiagodolphine@gmail.com>
2021-05-13 20:42:58 -0300

    [KOGITO-4563] Process runtime integration tests with diferent modules for persistence (#1226)
    * KOGITO-4563 refactor integration tests for processes to handle the different persistences
    
    * Fix spring integration tests issues with dependencies
    
    * Fix spring integration tests issues with dependencies
    
    * Adding kafka persistence IT
    
    * Removing unneeded bpmn in IT
    
    * Gitignore bpmn files in persistence IT modules
    
    * Fix spring boot tests
    
    * Fix pom.xml for integration-tests
    
    * removing unecessary application.properties in persistence integration tests
    
    * Inserting Native tests for persistece IT
    
    * integration-tests-springboot/pom.xml revert
    
    * Moving process IT module to parent IT and align persistence IT modules name

---
+++ integration-tests/integration-tests-quarkus-processes-persistence/integration-tests-processes-persistence-common/src/test/java/org/kie/kogito/it/PersistenceTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.kogito.it;
+
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.emptyOrNullString;
+import static org.junit.Assert.assertEquals;
+
+public abstract class PersistenceTest {
+
+    static {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    public static final String PROCESS_ID = "hello";
+
+    @Test
+    void testPersistence() {
+        final String pid = given().contentType(ContentType.JSON)
+                .when()
+                .body(Collections.singletonMap("var1", "Tiago"))
+                .post("/{processId}", PROCESS_ID)
+                .then()
+                .statusCode(201)
+                .header("Location", not(emptyOrNullString()))
+                .body("id", not(emptyOrNullString()))
+                .body("var1", equalTo("Tiago"))
+                .body("var2", equalTo("Hello Tiago! Script"))
+                .extract()
+                .path("id");
+
+        final String createdPid = given().contentType(ContentType.JSON)
+                .when()
+                .get("/{processId}/{id}", PROCESS_ID, pid)
+                .then()
+                .statusCode(200)
+                .body("id", not(emptyOrNullString()))
+                .body("var1", equalTo("Tiago"))
+                .body("var2", equalTo("Hello Tiago! Script"))
+                .extract()
+                .path("id");
+
+        assertEquals(createdPid, pid);
+
+        given().contentType(ContentType.JSON)
+                .when()
+                .delete("/management/processes/{processId}/instances/{processInstanceId}", PROCESS_ID, pid)
+                .then()
+                .statusCode(200);
+
+        given().contentType(ContentType.JSON)
+                .when()
+                .get("/greetings/{id}", pid)
+                .then()
+                .statusCode(404);
+    }
+}
---
+++ integration-tests/integration-tests-quarkus-processes-persistence/integration-tests-quarkus-processes-kafka-persistence/src/test/java/org/kie/kogito/it/KafkaPersistenceIT.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.kogito.it;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.junit.jupiter.api.BeforeEach;
+import org.kie.kogito.testcontainers.quarkus.KafkaQuarkusTestResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+@QuarkusTestResource(KafkaQuarkusTestResource.class)
+public class KafkaPersistenceIT extends PersistenceTest {
+
+    public static final String PROCESS_TOPIC = "kogito.process." + PersistenceTest.PROCESS_ID;
+    private Logger LOGGER = LoggerFactory.getLogger(KafkaPersistenceIT.class);
+
+    @Inject
+    @Named("default-kafka-broker")
+    Map<String, Object> kafkaConfig;
+
+    @BeforeEach
+    public void init() {
+        AdminClient client = AdminClient.create(kafkaConfig);
+        client.listTopics().names()
+                .thenApply(topics -> topics.stream()
+                        .filter(PROCESS_ID::equals)
+                        .findFirst()
+                        .orElseGet(() -> {
+                            try {
+                                client.createTopics(Arrays.asList(new NewTopic(PROCESS_TOPIC, 1, (short) 1))).all().get(10, TimeUnit.SECONDS);
+                                LOGGER.info("Created kogito.process.hello");
+                            } catch (Exception e) {
+                                LOGGER.error("Error creating {}", PROCESS_TOPIC, e);
+                            }
+                            return PROCESS_TOPIC;
+                        }));
+    }
+}
