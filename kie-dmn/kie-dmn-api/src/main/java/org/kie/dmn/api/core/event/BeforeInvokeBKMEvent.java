e504f23414fd0db623e39eef604ec35a4380364c
Matteo Mortari <matteo.mortari@gmail.com>
2021-05-31 15:50:55 +0200

    DROOLS-6375 DMN Listener BKM invocation params and result (#3644)
    * DROOLS-6375 DMN Listener BKM invocation params and result
    
    * Implement feedback to extract param name-value relation
    
    * naming

--- kie-dmn/kie-dmn-api/src/main/java/org/kie/dmn/api/core/event/BeforeInvokeBKMEvent.java
+++ kie-dmn/kie-dmn-api/src/main/java/org/kie/dmn/api/core/event/BeforeInvokeBKMEvent.java
@@ -16,11 +16,17 @@
 
 package org.kie.dmn.api.core.event;
 
+import java.util.List;
+
 import org.kie.dmn.api.core.ast.BusinessKnowledgeModelNode;
 
 public interface BeforeInvokeBKMEvent
         extends DMNEvent {
 
     BusinessKnowledgeModelNode getBusinessKnowledgeModel();
+
+    default List<Object> getInvocationParameters() {
+        return null;
+    }
 
 }
