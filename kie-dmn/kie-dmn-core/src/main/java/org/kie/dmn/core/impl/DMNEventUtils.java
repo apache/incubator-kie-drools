d1e632f2a35591817f496b783f4731f7b0dbce0b
tarilabs <matteo.mortari@gmail.com>
2021-06-03 12:14:51 +0200

    DROOLS-6391 DMN Listener DS invocation params and outputDecisions

--- kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/impl/DMNEventUtils.java
+++ kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/impl/DMNEventUtils.java
@@ -19,10 +19,17 @@ package org.kie.dmn.core.impl;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
+import org.kie.dmn.api.core.ast.DMNNode;
+import org.kie.dmn.api.core.event.AfterEvaluateDecisionServiceEvent;
+import org.kie.dmn.api.core.event.BeforeEvaluateDecisionServiceEvent;
 import org.kie.dmn.api.core.event.BeforeInvokeBKMEvent;
 import org.kie.dmn.core.ast.BusinessKnowledgeModelNodeImpl;
 import org.kie.dmn.core.ast.DMNFunctionDefinitionEvaluator;
+import org.kie.dmn.core.ast.DecisionServiceNodeImpl;
+import org.kie.dmn.core.compiler.DMNCompilerImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,6 +45,29 @@ public final class DMNEventUtils {
             String name = names.get(i);
             Object value = event.getInvocationParameters().get(i);
             results.put(name, value);
+        }
+        return results;
+    }
+
+    public static Map<String, Object> extractDSParameters(BeforeEvaluateDecisionServiceEvent event) {
+        Map<String, Object> results = new LinkedHashMap<String, Object>();
+        DecisionServiceNodeImpl dsi = (DecisionServiceNodeImpl) event.getDecisionService();
+        Map<String, DMNNode> params = dsi.getInputParameters();
+        for (Entry<String, DMNNode> entry : params.entrySet()) {
+            String name = entry.getKey();
+            Object value = event.getResult().getContext().get(name);
+            results.put(name, value);
+        }
+        return results;
+    }
+
+    public static Map<String, Object> extractDSOutputDecisionsValues(AfterEvaluateDecisionServiceEvent event) {
+        Map<String, Object> results = new LinkedHashMap<String, Object>();
+        List<String> decisionIDs = event.getDecisionService().getDecisionService().getOutputDecision().stream().map(er -> DMNCompilerImpl.getId(er)).collect(Collectors.toList());
+        for (String id : decisionIDs) {
+            String decisionName = ((DMNResultImpl) event.getResult()).getModel().getDecisionById(id).getName();
+            Object decisionResult = event.getResult().getContext().get(decisionName);
+            results.put(decisionName, decisionResult);
         }
         return results;
     }
