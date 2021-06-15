d17e4757298588da1c03c24f897bbb1bcd23e41a
Matteo Mortari <matteo.mortari@gmail.com>
2021-06-15 11:33:41 +0200

    DROOLS-6413 DMN align BKM return typeRef from encapsulatedLogic body expression (#3673)

--- kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/compiler/BusinessKnowledgeModelCompiler.java
+++ kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/compiler/BusinessKnowledgeModelCompiler.java
@@ -45,8 +45,8 @@ public class BusinessKnowledgeModelCompiler implements DRGElementCompiler {
         DMNCompilerHelper.checkVariableName( model, bkm, bkm.getName() );
         if (bkm.getVariable().getTypeRef() != null) { // variable must be present, otherwise error was already reported above.
             type = compiler.resolveTypeRef(model, bkm, bkm.getVariable(), bkm.getVariable().getTypeRef());
-        } else if (bkm.getVariable().getTypeRef() == null && bkm.getEncapsulatedLogic() != null && bkm.getEncapsulatedLogic().getTypeRef() != null) {
-            type = compiler.resolveTypeRef(model, bkm, bkm.getEncapsulatedLogic(), bkm.getEncapsulatedLogic().getTypeRef());
+        } else if (bkm.getVariable().getTypeRef() == null && bkm.getEncapsulatedLogic().getExpression() != null && bkm.getEncapsulatedLogic().getExpression().getTypeRef() != null) {
+            type = compiler.resolveTypeRef(model, bkm, bkm.getEncapsulatedLogic().getExpression(), bkm.getEncapsulatedLogic().getExpression().getTypeRef());
         } else {
             // for now the call bellow will return type UNKNOWN
             type = compiler.resolveTypeRef(model, bkm, bkm, null);
