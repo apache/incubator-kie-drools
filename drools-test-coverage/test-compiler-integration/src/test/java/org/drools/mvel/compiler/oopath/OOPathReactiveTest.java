1557f6417103fc239b903dfe1fdb1764196d02d2
Mario Fusco <mario.fusco@gmail.com>
2021-06-15 15:26:34 +0200

    [DROOLS-6133] support passive oopath in executable model (#3679)

--- drools-test-coverage/test-compiler-integration/src/test/java/org/drools/mvel/compiler/oopath/OOPathReactiveTest.java
+++ drools-test-coverage/test-compiler-integration/src/test/java/org/drools/mvel/compiler/oopath/OOPathReactiveTest.java
@@ -70,8 +70,7 @@ public class OOPathReactiveTest {
 
     @Parameterized.Parameters(name = "KieBase type={0}")
     public static Collection<Object[]> getParameters() {
-     // TODO: EM failed with some tests. File JIRAs
-        return TestParametersUtil.getKieBaseCloudConfigurations(false);
+        return TestParametersUtil.getKieBaseCloudConfigurations(true);
     }
 
     @Test
