e53fa5902e099951907cdff07e6701998ae2ae25
tarilabs <matteo.mortari@gmail.com>
2021-06-16 12:26:21 +0200

    DROOLS-6419 DMN OAS when range endpoint unbound do not emit exclusive

--- kie-dmn/kie-dmn-openapi/src/test/java/org/kie/dmn/openapi/NumberAllowedValuesTest.java
+++ kie-dmn/kie-dmn-openapi/src/test/java/org/kie/dmn/openapi/NumberAllowedValuesTest.java
@@ -16,12 +16,14 @@
 
 package org.kie.dmn.openapi;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.networknt.schema.JsonSchema;
 import org.junit.Test;
 import org.kie.dmn.api.core.DMNModel;
 import org.kie.dmn.api.core.DMNRuntime;
 import org.kie.dmn.openapi.model.DMNOASResult;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.networknt.schema.JsonSchema;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -50,5 +52,22 @@ public class NumberAllowedValuesTest extends BaseDMNOASTest {
 
         assertThat(validateUsing(validator, "{ \"in1\": 47 }")).isNotEmpty();
         assertThat(validateUsing(validator, "{ \"in1\": 0 }")).isEmpty();
+    }
+
+    @Test
+    public void testGT() throws Exception {
+        final DMNRuntime runtime = createRuntime("numberGT.dmn", this.getClass());
+        DMNOASResult result = DMNOASGeneratorFactory.generator(runtime.getModels()).build();
+
+        DMNModel modelUnderTest = runtime.getModel("ns1", "numberGT");
+        ObjectNode syntheticJSONSchema = synthesizeSchema(result, modelUnderTest);
+        JsonSchema validator = getJSONSchema(syntheticJSONSchema);
+
+        assertThat(validateUsing(validator, "{ \"age\": 47, \"negative\" : -47 }")).isEmpty();
+
+        JsonNode tAge = syntheticJSONSchema.get("definitions").get("tAge");
+        assertThat(tAge.fieldNames()).toIterable().doesNotContain("exclusiveMaximum", "maximum");
+        JsonNode tNegative = syntheticJSONSchema.get("definitions").get("tNegative");
+        assertThat(tNegative.fieldNames()).toIterable().doesNotContain("exclusiveMinumum", "minumum");
     }
 }
