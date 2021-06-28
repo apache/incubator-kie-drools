11799cf5873081845a4605b6bf0e4f00d7e4d500
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2021-06-28 18:01:20 +0200

    [DROOLS-6455] Fixing common drools code

--- kie-pmml-trusty/kie-pmml-models/kie-pmml-models-drools/kie-pmml-models-drools-common/src/test/java/org/kie/pmml/models/drools/utils/KiePMMLASTTestUtils.java
+++ kie-pmml-trusty/kie-pmml-models/kie-pmml-models-drools/kie-pmml-models-drools-common/src/test/java/org/kie/pmml/models/drools/utils/KiePMMLASTTestUtils.java
@@ -25,9 +25,9 @@ import org.dmg.pmml.DataType;
 import org.dmg.pmml.FieldName;
 import org.dmg.pmml.LocalTransformations;
 import org.dmg.pmml.OpType;
+import org.dmg.pmml.OutputField;
 import org.dmg.pmml.Predicate;
 import org.dmg.pmml.TransformationDictionary;
-import org.kie.pmml.commons.model.KiePMMLOutputField;
 import org.kie.pmml.models.drools.ast.KiePMMLDroolsRule;
 import org.kie.pmml.models.drools.ast.factories.KiePMMLDataDictionaryASTFactory;
 import org.kie.pmml.models.drools.ast.factories.KiePMMLDerivedFieldASTFactory;
@@ -40,7 +40,7 @@ import org.kie.pmml.models.drools.tuples.KiePMMLOriginalTypeGeneratedType;
 public class KiePMMLASTTestUtils {
 
     public static PredicateASTFactoryData getPredicateASTFactoryData(Predicate predicate,
-                                                                     List<KiePMMLOutputField> outputFields,
+                                                                     List<OutputField> outputFields,
                                                                      List<KiePMMLDroolsRule> rules,
                                                                      String parentPath,
                                                                      String currentRule,
