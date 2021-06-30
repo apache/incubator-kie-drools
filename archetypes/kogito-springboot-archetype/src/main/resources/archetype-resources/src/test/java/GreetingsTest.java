847c9675decd41245630e4a11be7efbbd7d44801
Ricardo Zanini <1538000+ricardozanini@users.noreply.github.com>
2021-06-30 14:04:02 -0300

    [KOGITO-5251] - Upgrade to SpringBoot 2.4.3 (#1407)
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>

--- archetypes/kogito-springboot-archetype/src/main/resources/archetype-resources/src/test/java/GreetingsTest.java
+++ archetypes/kogito-springboot-archetype/src/main/resources/archetype-resources/src/test/java/GreetingsTest.java
@@ -17,17 +17,18 @@ package ${package};
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.notNullValue;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = KogitoApplication.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) // reset spring context after each test method
 public class GreetingsTest {
--- archetypes/kogito-springboot-archetype/src/main/resources/archetype-resources/src/test/java/TrafficViolationTest.java
+++ archetypes/kogito-springboot-archetype/src/main/resources/archetype-resources/src/test/java/TrafficViolationTest.java
@@ -20,14 +20,15 @@ import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = KogitoApplication.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) // reset spring context after each test method
 public class TrafficViolationTest {
