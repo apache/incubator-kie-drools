22db851c46aa927523e8d814919bf9ca70e67345
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2021-07-01 10:42:55 +0200

    [DROOLS-6454] Adding NormDiscrete usage inside mining model.

--- kie-pmml-trusty/kie-pmml-models/kie-pmml-models-mining/kie-pmml-models-mining-tests/src/test/java/org/kie/pmml/mining/tests/MixedMiningTest.java
+++ kie-pmml-trusty/kie-pmml-models/kie-pmml-models-mining/kie-pmml-models-mining-tests/src/test/java/org/kie/pmml/mining/tests/MixedMiningTest.java
@@ -41,6 +41,8 @@ public class MixedMiningTest extends AbstractPMMLTest {
     private static final String OUT_RESIDENCESTATE = "out_residenceState";
     private static final String OUT_FUN_OCCUPATION_REFERRED = "out_fun_occupation_referred";
     private static final String CONSTANT_OCCUPATION = "CONSTANT_OCCUPATION";
+    private static final String OUT_NORMDISCRETE_FIELD = "out_normdiscrete_field";
+
     private static PMMLRuntime pmmlRuntime;
 
     private String categoricalX;
@@ -106,5 +108,11 @@ public class MixedMiningTest extends AbstractPMMLTest {
         Assertions.assertThat(pmml4Result.getResultVariables().get(OUT_RESIDENCESTATE)).isEqualTo(residenceState);
         Assertions.assertThat(pmml4Result.getResultVariables().get(OUT_FUN_OCCUPATION_REFERRED)).isNotNull();
         Assertions.assertThat(pmml4Result.getResultVariables().get(OUT_FUN_OCCUPATION_REFERRED)).isEqualTo(CONSTANT_OCCUPATION);
+        Assertions.assertThat(pmml4Result.getResultVariables().get(OUT_NORMDISCRETE_FIELD)).isNotNull();
+        if (occupation.equals("SKYDIVER")) {
+            Assertions.assertThat(pmml4Result.getResultVariables().get(OUT_NORMDISCRETE_FIELD)).isEqualTo(1.0);
+        } else {
+            Assertions.assertThat(pmml4Result.getResultVariables().get(OUT_NORMDISCRETE_FIELD)).isEqualTo(0.0);
+        }
     }
 }
