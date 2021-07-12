0894b8a774d85289762c495bde68fb09b06939a6
Sona Mala <sjamboro@redhat.com>
2021-07-12 16:54:37 +0200

    FAI-507: Document how to start Trusty Console manually (on master) (#868)
    * FAI-507: Config of docker-compose & changed README
    
    The README file contains guide how to build and run Trusty Console from git
    repository. All necessary configuration is stored in repository.
    
    * FAI-507: fixed link in README
    
    * FAI-507: move docker-compose to it-tests
    
    * FAI-507: prepare yarn scripts to run e2e tests
    
    and document steps to perform e2e in README
    
    * FAI-507: Make build script more safe
    
    * FAI-507: typo correction
    
    * FAI-507: add args (-c,-h), clear function, docs
    
    -c means clear docker images which was build previosly
    -h means write help on stdout
    
    without args, the script build whole kogito-apps repository
    
    * FAI-507: env variable for version of docker images
    
    Solution is based on this article: https://docs.docker.com/compose/environment-variables/
    
    * FAI-507: Fix name of scripts in package.json
    
    * FAI-507: Doc the VERSION property in README
    
    * FAI-507: README update
    
    * FAI-507: yarn script build:services generates .env
    
    build:services get version of maven project and inserts it to the .env
    file
    
    This commit should prevent to use wrong version when the version of this
    maven project is changed.
    
    Co-authored-by: Sona Mala <smala@redhat.com>

---
+++ ui-packages/packages/trusty/it-tests/docker-compose/start_docker.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+trap finish SIGTERM EXIT
+
+set -o errexit
+set -o nounset
+#set -o xtrace
+
+
+#######################################
+# Clear popd stack.
+#
+# Globals:
+#   None
+# Arguments:
+#   None
+#######################################
+finish() {
+    popd
+}
+
+#######################################
+# Run docker images. Based on docker-compose.yml
+#
+# Globals:
+#   None
+# Arguments:
+#   None
+#######################################
+
+pushd .
+
+if [[ "${PWD}" == */docker-compose ]]
+then 
+    echo 'Application starts from the docker-compose folder - probably manual start of this script'
+elif [[ "${PWD}" == */packages/trusty ]]
+then 
+    echo 'Application starts from the trusty folder - probably run some yarn script'
+    echo 'Move to the docker-compose folder'
+    cd it-tests/docker-compose
+else
+    >&2 echo "error: script starts from unexpected location: ${PWD}"
+    >&2 echo "error: script expects /ui-packages/packages/trusty or /ui-packages/packages/trusty/it-tests/docker-compose folders"
+    exit
+fi
+
+docker-compose up
+
