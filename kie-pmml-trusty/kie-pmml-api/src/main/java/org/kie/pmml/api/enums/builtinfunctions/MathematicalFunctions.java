53cd501f453505d9f0ed92556b3aa40dec5c3251
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2021-07-28 16:42:33 +0200

    [DROOLS-6518] Implemented MathematicalFunctionsTest.

--- kie-pmml-trusty/kie-pmml-api/src/main/java/org/kie/pmml/api/enums/builtinfunctions/MathematicalFunctions.java
+++ kie-pmml-trusty/kie-pmml-api/src/main/java/org/kie/pmml/api/enums/builtinfunctions/MathematicalFunctions.java
@@ -20,6 +20,8 @@ import java.util.Arrays;
 import org.kie.pmml.api.exceptions.KieEnumException;
 import org.kie.pmml.api.exceptions.KiePMMLException;
 
+import static org.kie.pmml.api.enums.BUILTIN_FUNCTIONS.checkNumbers;
+
 /**
  * @see <a http://dmg.org/pmml/v4-4-1/BuiltinFunctions.html>Built-in functions</a>
  */
@@ -63,9 +65,114 @@ public enum MathematicalFunctions {
 
     public Object getValue(final Object[] inputData) {
         switch (this) {
+            case EXPM1:
+                return expm1(inputData);
+            case HYPOT:
+                return hypot(inputData);
+            case LN1P:
+                return ln1p(inputData);
+            case RINT:
+                return rint(inputData);
+            case SIN:
+                return sin(inputData);
+            case ASIN:
+                return asin(inputData);
+            case SINH:
+                return sinh(inputData);
+            case COS:
+                return cos(inputData);
+            case ACOS:
+                return acos(inputData);
+            case COSH:
+                return cosh(inputData);
+            case TAN:
+                return tan(inputData);
+            case ATAN:
+                return atan(inputData);
+            case TANH:
+                return tanh(inputData);
             default:
                 throw new KiePMMLException("Unmanaged MathematicalFunctions " + this);
         }
+    }
+
+    private double expm1(final Object[] inputData) {
+        checkNumbers(inputData, 1);
+        double x = ((Number) inputData[0]).doubleValue();
+        return Math.expm1(x);
+    }
+
+    private double hypot(final Object[] inputData) {
+        checkNumbers(inputData, 2);
+        double x = ((Number) inputData[0]).doubleValue();
+        double y = ((Number) inputData[1]).doubleValue();
+        return Math.hypot(x, y);
+    }
+
+    private double ln1p(final Object[] inputData) {
+        checkNumbers(inputData, 1);
+        double x = ((Number) inputData[0]).doubleValue();
+        return Math.log1p(x);
+    }
+
+    private double rint(final Object[] inputData) {
+        checkNumbers(inputData, 1);
+        double x = ((Number) inputData[0]).doubleValue();
+        return Math.rint(x);
+    }
+
+    private double sin(final Object[] inputData) {
+        checkNumbers(inputData, 1);
+        double x = ((Number) inputData[0]).doubleValue();
+        return Math.sin(x);
+    }
+
+    private double asin(final Object[] inputData) {
+        checkNumbers(inputData, 1);
+        double x = ((Number) inputData[0]).doubleValue();
+        return Math.asin(x);
+    }
+
+    private double sinh(final Object[] inputData) {
+        checkNumbers(inputData, 1);
+        double x = ((Number) inputData[0]).doubleValue();
+        return Math.sinh(x);
+    }
+
+    private double cos(final Object[] inputData) {
+        checkNumbers(inputData, 1);
+        double x = ((Number) inputData[0]).doubleValue();
+        return Math.cos(x);
+    }
+
+    private double acos(final Object[] inputData) {
+        checkNumbers(inputData, 1);
+        double x = ((Number) inputData[0]).doubleValue();
+        return Math.acos(x);
+    }
+
+    private double cosh(final Object[] inputData) {
+        checkNumbers(inputData, 1);
+        double x = ((Number) inputData[0]).doubleValue();
+        return Math.cosh(x);
+    }
+
+    private double tan(final Object[] inputData) {
+        checkNumbers(inputData, 1);
+        double x = ((Number) inputData[0]).doubleValue();
+        return Math.tan(x);
+    }
+
+    private double atan(final Object[] inputData) {
+        checkNumbers(inputData, 1);
+        double x = ((Number) inputData[0]).doubleValue();
+        return Math.atan(x);
+    }
+
+    private double tanh(final Object[] inputData) {
+        checkNumbers(inputData, 1);
+        double x = ((Number) inputData[0]).doubleValue();
+        return Math.tanh(x);
     }
 
 }
