1de595d4afbe2cddca74c178fbe70a541a48517c
Tristan Radisson <tristan.radisson@gmail.com>
2021-08-02 11:04:21 +0200

    KOGITO-5597 Separate native checks (#1472)
    * KOGITO-5597 Separate native checks
    
    * added unstable to allow pipeline to continue

--- .ci/jenkins/dsl/jobs.groovy
+++ .ci/jenkins/dsl/jobs.groovy
@@ -76,6 +76,7 @@ setupMultijobPrNativeChecks()
 setupMultijobPrLTSChecks()
 
 // Nightly jobs
+setupNativeJob(nightlyBranchFolder)
 setupDeployJob(nightlyBranchFolder, KogitoJobType.NIGHTLY)
 setupPromoteJob(nightlyBranchFolder, KogitoJobType.NIGHTLY)
 
@@ -139,6 +140,20 @@ void setupMultijobPrNativeChecks() {
 
 void setupMultijobPrLTSChecks() {
     KogitoJobTemplate.createMultijobLTSPRJobs(this, getMultijobPRConfig()) { return getDefaultJobParams() }
+}
+
+void setupNativeJob(String jobFolder) {
+    def jobParams = getJobParams('optaplanner-native', jobFolder, 'Jenkinsfile.native', 'Optaplanner Native Testing')
+    jobParams.triggers = [ cron : 'H 6 * * *' ]
+    KogitoJobTemplate.createPipelineJob(this, jobParams).with {
+        parameters {
+            stringParam('BUILD_BRANCH_NAME', "${GIT_BRANCH}", 'Set the Git branch to checkout')
+            stringParam('GIT_AUTHOR', "${GIT_AUTHOR_NAME}", 'Set the Git author to checkout')
+        }
+        environmentVariables {
+            env('JENKINS_EMAIL_CREDS_ID', "${JENKINS_EMAIL_CREDS_ID}")
+        }
+    }
 }
 
 void setupDeployJob(String jobFolder, KogitoJobType jobType) {
