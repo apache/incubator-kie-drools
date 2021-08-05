60fcaa502fe9a7c088332e5a10d7f2959b37c5e6
Tristan Radisson <tristan.radisson@gmail.com>
2021-08-05 10:21:02 +0200

    KOGITO-5635 Add native LTS check (#973)
    * KOGITO-5635 Add native LTS check
    
    * Apply suggestions from code review

--- .jenkins/dsl/jobs.groovy
+++ .jenkins/dsl/jobs.groovy
@@ -69,6 +69,10 @@ if (!Utils.isMainBranch(this)) {
     setupPromoteJob(releaseBranchFolder, KogitoJobType.RELEASE)
 }
 
+if (Utils.isLTSBranch(this)) {
+    setupNativeLTSJob(nightlyBranchFolder)
+}
+
 /////////////////////////////////////////////////////////////////
 // Methods
 /////////////////////////////////////////////////////////////////
@@ -130,6 +134,24 @@ void setupNativeJob(String jobFolder) {
         }
         environmentVariables {
             env('JENKINS_EMAIL_CREDS_ID', "${JENKINS_EMAIL_CREDS_ID}")
+            env('NOTIFICATION_JOB_NAME', 'Native check')
+        }
+    }
+}
+
+void setupNativeLTSJob(String jobFolder) {
+    def jobParams = getJobParams('kogito-apps-native-tls', jobFolder, 'Jenkinsfile.native', 'Kogito Apps Native LTS Testing')
+    jobParams.triggers = [ cron : 'H 8 * * *' ]
+    KogitoJobTemplate.createPipelineJob(this, jobParams).with {
+        parameters {
+            stringParam('BUILD_BRANCH_NAME', "${GIT_BRANCH}", 'Set the Git branch to checkout')
+            stringParam('GIT_AUTHOR', "${GIT_AUTHOR_NAME}", 'Set the Git author to checkout')
+
+            stringParam('NATIVE_BUILDER_IMAGE', Utils.getLTSNativeBuilderImage(this), 'Which native builder image to use ?')
+        }
+        environmentVariables {
+            env('JENKINS_EMAIL_CREDS_ID', "${JENKINS_EMAIL_CREDS_ID}")
+            env('NOTIFICATION_JOB_NAME', 'Native LTS check')
         }
     }
 }
