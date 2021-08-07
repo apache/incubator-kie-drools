bd0c79e3f19c90a8fa97abe89372bfcea8fef792
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2021-08-07 14:24:52 +0200

    Bring Quarkus guide home + update Quarkus + Spring Boot guides

--- optaplanner-quarkus-integration/optaplanner-quarkus-jackson/integration-test/src/main/java/org/optaplanner/quarkus/jackson/it/OptaPlannerTestResource.java
+++ optaplanner-quarkus-integration/optaplanner-quarkus-jackson/integration-test/src/main/java/org/optaplanner/quarkus/jackson/it/OptaPlannerTestResource.java
@@ -19,11 +19,8 @@ package org.optaplanner.quarkus.jackson.it;
 import java.util.concurrent.ExecutionException;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import org.optaplanner.core.api.solver.SolverJob;
 import org.optaplanner.core.api.solver.SolverManager;
@@ -37,8 +34,6 @@ public class OptaPlannerTestResource {
 
     @POST
     @Path("/solver-factory")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
     public ITestdataPlanningSolution solveWithSolverFactory(ITestdataPlanningSolution problem) {
         SolverJob<ITestdataPlanningSolution, Long> solverJob = solverManager.solve(1L, problem);
         try {
