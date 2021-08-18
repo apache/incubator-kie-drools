91165bb5ef68e5c60a4aaae652ad682a104632ad
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2021-08-18 19:12:42 +0900

    [DROOLS-6547] NullSafeDereferencing misses null check in LambdaExtractor for index with exec-model

--- drools-test-coverage/test-compiler-integration/src/test/java/org/drools/mvel/integrationtests/NullSafeDereferencingTest.java
+++ drools-test-coverage/test-compiler-integration/src/test/java/org/drools/mvel/integrationtests/NullSafeDereferencingTest.java
@@ -44,8 +44,7 @@ public class NullSafeDereferencingTest {
 
     @Parameterized.Parameters(name = "KieBase type={0}")
     public static Collection<Object[]> getParameters() {
-        // TODO: EM failed with testMixedNullSafes, testNullSafeInnerConstraint, testNullSafeNullComparison2, testNullSafeWithMethod. File JIRAs
-        return TestParametersUtil.getKieBaseCloudConfigurations(false);
+        return TestParametersUtil.getKieBaseCloudConfigurations(true);
     }
 
     @Test
