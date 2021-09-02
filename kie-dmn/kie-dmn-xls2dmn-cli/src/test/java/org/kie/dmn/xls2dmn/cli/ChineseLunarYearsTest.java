fb5159fbe4cb3b34052a01b11275b714f1b976be
Matteo Mortari <matteo.mortari@gmail.com>
2021-09-02 17:19:24 +0200

    DROOLS-6587 DMN converter from Excel improvements (#3816)
    * DROOLS-6587 DMN converter from Excel improvements
    
    * refactoring per code review

--- kie-dmn/kie-dmn-xls2dmn-cli/src/test/java/org/kie/dmn/xls2dmn/cli/ChineseLunarYearsTest.java
+++ kie-dmn/kie-dmn-xls2dmn-cli/src/test/java/org/kie/dmn/xls2dmn/cli/ChineseLunarYearsTest.java
@@ -18,22 +18,19 @@ package org.kie.dmn.xls2dmn.cli;
 
 import java.io.File;
 import java.time.LocalDate;
-import java.util.Arrays;
 
 import org.junit.Test;
 import org.kie.dmn.api.core.DMNContext;
 import org.kie.dmn.api.core.DMNModel;
 import org.kie.dmn.api.core.DMNResult;
 import org.kie.dmn.api.core.DMNRuntime;
-import org.kie.dmn.core.internal.utils.DMNRuntimeBuilder;
 import org.kie.dmn.core.util.DMNRuntimeUtil;
-import org.kie.dmn.feel.util.Either;
-import org.kie.internal.io.ResourceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.kie.dmn.xls2dmn.cli.TestUtils.getRuntime;
 
 import picocli.CommandLine;
 
@@ -41,23 +38,10 @@ public class ChineseLunarYearsTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(ChineseLunarYearsTest.class);
 
-    private DMNRuntime getDMNRuntimeWithCLI() throws Exception {
-        File tempFile = File.createTempFile("xls2dmn", ".dmn");
-        new CommandLine(new App()).execute(new String[]{"src/test/resources/ChineseLunarYears.xlsx", tempFile.toString()});
-
-        Either<Exception, DMNRuntime> fromResources = DMNRuntimeBuilder.fromDefaults()
-                         .buildConfiguration()
-                         .fromResources(Arrays.asList(ResourceFactory.newFileResource(tempFile)));
-
-        LOG.info("{}", System.getProperty("java.io.tmpdir"));
-        LOG.info("{}", tempFile);
-        DMNRuntime dmnRuntime = fromResources.getOrElseThrow(RuntimeException::new);
-        return dmnRuntime;
-    }
-
     @Test
     public void testCLI() throws Exception {
-        final DMNRuntime dmnRuntime = getDMNRuntimeWithCLI();
+        File tempFile = File.createTempFile("xls2dmn", ".dmn");
+        final DMNRuntime dmnRuntime = getRuntime(new CommandLine(new App())::execute, tempFile, new String[]{"src/test/resources/ChineseLunarYears.xlsx", tempFile.toString()});
         DMNModel dmnModel = dmnRuntime.getModels().get(0);
 
         DMNContext dmnContext = dmnRuntime.newContext();
