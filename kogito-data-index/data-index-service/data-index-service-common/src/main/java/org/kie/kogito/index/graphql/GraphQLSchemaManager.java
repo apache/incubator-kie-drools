209a7ffbef51c7af0276fb131b69de2de4296c91
Cristiano Nicolai <570894+cristianonicolai@users.noreply.github.com>
2021-09-13 11:42:46 +1000

    KOGITO-5856 Make default cache config persistent to file (#1013)

--- kogito-data-index/data-index-service/data-index-service-common/src/main/java/org/kie/kogito/index/graphql/GraphQLSchemaManager.java
+++ kogito-data-index/data-index-service/data-index-service-common/src/main/java/org/kie/kogito/index/graphql/GraphQLSchemaManager.java
@@ -22,6 +22,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
@@ -40,6 +41,7 @@ import org.kie.kogito.index.model.Node;
 import org.kie.kogito.index.model.ProcessInstance;
 import org.kie.kogito.index.model.ProcessInstanceState;
 import org.kie.kogito.index.model.UserTaskInstance;
+import org.kie.kogito.index.service.DataIndexServiceException;
 import org.kie.kogito.persistence.api.Storage;
 import org.kie.kogito.persistence.api.query.Query;
 import org.reactivestreams.Publisher;
@@ -60,6 +62,7 @@ import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
 
+import static java.lang.String.format;
 import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
 import static org.kie.kogito.index.json.JsonUtils.getObjectMapper;
@@ -341,17 +344,21 @@ public class GraphQLSchemaManager {
         return env -> cache.get().objectUpdatedListener();
     }
 
+    private Supplier<DataIndexServiceException> cacheNotFoundException(String processId) {
+        return () -> new DataIndexServiceException(format("Cache for process %s not found", processId));
+    };
+
     protected DataFetcher<Publisher<ObjectNode>> getDomainModelUpdatedDataFetcher(String processId) {
-        return env -> cacheService.getDomainModelCache(processId).objectUpdatedListener();
+        return env -> Optional.ofNullable(cacheService.getDomainModelCache(processId)).orElseThrow(cacheNotFoundException(processId)).objectUpdatedListener();
     }
 
     protected DataFetcher<Publisher<ObjectNode>> getDomainModelAddedDataFetcher(String processId) {
-        return env -> cacheService.getDomainModelCache(processId).objectCreatedListener();
+        return env -> Optional.ofNullable(cacheService.getDomainModelCache(processId)).orElseThrow(cacheNotFoundException(processId)).objectCreatedListener();
     }
 
     protected DataFetcher<Collection<ObjectNode>> getDomainModelDataFetcher(String processId) {
         return env -> {
-            List result = executeAdvancedQueryForCache(cacheService.getDomainModelCache(processId), env);
+            List result = executeAdvancedQueryForCache(Optional.ofNullable(cacheService.getDomainModelCache(processId)).orElseThrow(cacheNotFoundException(processId)), env);
             return (Collection<ObjectNode>) result.stream().map(json -> {
                 try {
                     return (ObjectNode) getObjectMapper().readTree(json.toString());
