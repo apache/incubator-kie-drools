3f4005474a62a2b02d0178861cad452f56dcaaad
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2021-10-01 12:22:19 +0200

    [KOGITO-5965] Fix migration to Trusty PMML 7.60 (#3862)
    * [fix_KiePMMLModel] Add tests to verify null management inside stream
    
    * [fix_KiePMMLModel] Fix as per PR suggestion
    
    * [KOGITO-5965] Fix NPE inside PMMLCompilerImpl
    
    * [KOGITO-5965] Fixed as per PR request
    
    * [KOGITO-5965] Fixed as per PR request

--- kie-pmml-trusty/kie-pmml-compiler/kie-pmml-compiler-core/src/main/java/org/kie/pmml/compiler/executor/PMMLCompilerImpl.java
+++ kie-pmml-trusty/kie-pmml-compiler/kie-pmml-compiler-core/src/main/java/org/kie/pmml/compiler/executor/PMMLCompilerImpl.java
@@ -146,14 +146,8 @@ public class PMMLCompilerImpl implements PMMLCompiler {
      */
     private List<KiePMMLModel> getModelsWithSources(final String packageName, final PMML pmml, final HasClassLoader hasClassloader) {
         logger.trace("getModels {}", pmml);
-        final List<Field<?>> fields = new ArrayList<>();
-        pmml.getDataDictionary().getDataFields().stream().map(Field.class::cast)
-                        .forEach(fields::add);
         TransformationDictionary transformationDictionary = pmml.getTransformationDictionary();
-        if (transformationDictionary.hasDerivedFields()) {
-            transformationDictionary.getDerivedFields().stream().map(Field.class::cast)
-                    .forEach(fields::add);
-        }
+        final List<Field<?>> fields = getFieldsFromDataDictionaryAndTransformationDictionary(pmml.getDataDictionary(), transformationDictionary);
         return pmml
                 .getModels()
                 .stream()
@@ -163,16 +157,24 @@ public class PMMLCompilerImpl implements PMMLCompiler {
                 .collect(Collectors.toList());
     }
 
-    private List<Field<?>> getFieldsFromDataDictionaryTransformationDictionaryAndModel(final DataDictionary dataDictionary,
-                                                                                       final TransformationDictionary transformationDictionary,
-                                                                                       final Model model) {
+    private List<Field<?>> getFieldsFromDataDictionaryAndTransformationDictionary(final DataDictionary dataDictionary,
+                                                                                  final TransformationDictionary transformationDictionary) {
         final List<Field<?>> toReturn = new ArrayList<>();
-        dataDictionary.getDataFields().stream().map(Field.class::cast)
-                .forEach(toReturn::add);
+        if (dataDictionary != null) {
+            dataDictionary.getDataFields().stream().map(Field.class::cast)
+                    .forEach(toReturn::add);
+        }
         if (transformationDictionary != null && transformationDictionary.hasDerivedFields()) {
             transformationDictionary.getDerivedFields().stream().map(Field.class::cast)
                     .forEach(toReturn::add);
         }
+        return toReturn;
+    }
+
+    private List<Field<?>> getFieldsFromDataDictionaryTransformationDictionaryAndModel(final DataDictionary dataDictionary,
+                                                                                       final TransformationDictionary transformationDictionary,
+                                                                                       final Model model) {
+        final List<Field<?>> toReturn = getFieldsFromDataDictionaryAndTransformationDictionary(dataDictionary, transformationDictionary);
         LocalTransformations localTransformations = model.getLocalTransformations();
         if (localTransformations != null && localTransformations.hasDerivedFields()) {
             localTransformations.getDerivedFields().stream().map(Field.class::cast)
