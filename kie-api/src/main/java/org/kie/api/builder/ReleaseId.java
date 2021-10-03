4588e5396f9800c4836912afb428ccd56c593f84
Mario Fusco <mario.fusco@gmail.com>
2021-10-03 16:57:43 +0200

    [DROOLS-6626] bump to 8.0.0-SNAPSHOT (#553)
    * [DROOLS-6626] bump to 8.0.0-SNAPSHOT
    
    * move all boms to -knowledge
    
    * bump to 8.13.0-SNAPSHOT
    
    * boms cleanup
    
    * [DROOLS-6602] get rid of kie-soup dependency in drools 8
    
    * wip

--- kie-api/src/main/java/org/kie/api/builder/ReleaseId.java
+++ kie-api/src/main/java/org/kie/api/builder/ReleaseId.java
@@ -20,7 +20,13 @@ package org.kie.api.builder;
  * ReleaseId is a full identifier far a given version of an artifact.
  * Following the Maven conventions it is composed of 3 parts: a groupId, an artifactId and a version
  */
-public interface ReleaseId extends org.appformer.maven.support.AFReleaseId, Comparable<ReleaseId> {
+public interface ReleaseId extends Comparable<ReleaseId> {
+
+    String getGroupId();
+    String getArtifactId();
+    String getVersion();
+    String toExternalForm();
+    boolean isSnapshot();
 
     @Override
     default int compareTo(ReleaseId that) {
