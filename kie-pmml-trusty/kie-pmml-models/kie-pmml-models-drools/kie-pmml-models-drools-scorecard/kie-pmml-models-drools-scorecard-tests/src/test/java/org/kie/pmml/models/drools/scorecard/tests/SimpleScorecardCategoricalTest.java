f2776802e0f96bbf8a83a4c23d16aaf5185ad648
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2021-10-06 13:51:12 +0200

    [DROOLS-6627] Verify Scorecard reasoncode when result is null (#3875)
    * [DROOLS-6625] Managing missing "required" input data
    
    * [DROOLS-6625] Managing best-effort conversion of input data
    
    * [DROOLS-6625] Managing invalid values - TODO: integration tests
    
    * [DROOLS-6625] Managing invalid values
    
    * [DROOLS-6625] Managing missing values
    
    * [DROOLS-6625] Validate input data
    
    * [DROOLS-6635] Move testing sources in specific files
    
    * [DROOLS-6625] Fix merge with base branch
    
    * [DROOLS-6625] Fix as per PR suggestion
    
    * [DROOLS-6635] Fix merge
    
    * [DROOLS-6627] Verify Scorecard reasoncode when result is null
    
    * [DROOLS-6635] Fix merge with 7.x
    
    * [DROOLS-6627] Fix merge
    
    * [DROOLS-6627] Fixed as per PR request

--- kie-pmml-trusty/kie-pmml-models/kie-pmml-models-drools/kie-pmml-models-drools-scorecard/kie-pmml-models-drools-scorecard-tests/src/test/java/org/kie/pmml/models/drools/scorecard/tests/SimpleScorecardCategoricalTest.java
+++ kie-pmml-trusty/kie-pmml-models/kie-pmml-models-drools/kie-pmml-models-drools-scorecard/kie-pmml-models-drools-scorecard-tests/src/test/java/org/kie/pmml/models/drools/scorecard/tests/SimpleScorecardCategoricalTest.java
@@ -16,7 +16,6 @@
 
 package org.kie.pmml.models.drools.scorecard.tests;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
@@ -35,6 +34,8 @@ import org.kie.api.pmml.PMML4Result;
 import org.kie.pmml.api.runtime.PMMLRuntime;
 import org.kie.pmml.models.tests.AbstractPMMLTest;
 
+import static org.junit.Assert.assertFalse;
+
 @RunWith(Parameterized.class)
 public class SimpleScorecardCategoricalTest extends AbstractPMMLTest {
 
@@ -43,7 +44,7 @@ public class SimpleScorecardCategoricalTest extends AbstractPMMLTest {
     private static final String TARGET_FIELD = "Score";
     private static final String REASON_CODE1_FIELD = "Reason Code 1";
     private static final String REASON_CODE2_FIELD = "Reason Code 2";
-    private static final String[] CATEGORY = new String[] { "classA", "classB", "classC", "classD", "classE", "NA" };
+    private static final String[] CATEGORY = new String[]{"classA", "classB", "classC", "classD", "classE", "NA"};
     private static PMMLRuntime pmmlRuntime;
 
     private String input1;
@@ -52,7 +53,8 @@ public class SimpleScorecardCategoricalTest extends AbstractPMMLTest {
     private String reasonCode1;
     private String reasonCode2;
 
-    public SimpleScorecardCategoricalTest(String input1, String input2, double score, String reasonCode1, String reasonCode2) {
+    public SimpleScorecardCategoricalTest(String input1, String input2, double score, String reasonCode1,
+                                          String reasonCode2) {
         this.input1 = input1;
         this.input2 = input2;
         this.score = score;
@@ -60,7 +62,7 @@ public class SimpleScorecardCategoricalTest extends AbstractPMMLTest {
         this.reasonCode2 = reasonCode2;
     }
 
-  @BeforeClass
+    @BeforeClass
     public static void setupClass() {
         pmmlRuntime = getPMMLRuntime(FILE_NAME);
     }
@@ -91,6 +93,16 @@ public class SimpleScorecardCategoricalTest extends AbstractPMMLTest {
     @Test
     public void testSimpleScorecardCategoricalVerifyNoException() {
         getSamples().stream().map(sample -> evaluate(pmmlRuntime, sample, MODEL_NAME)).forEach(Assert::assertNotNull);
+    }
+
+    @Test
+    public void testSimpleScorecardCategoricalVerifyNoReasonCodeWithoutScore() {
+        getSamples().stream().map(sample -> evaluate(pmmlRuntime, sample, MODEL_NAME))
+                .filter(pmml4Result -> pmml4Result.getResultVariables().get(TARGET_FIELD) == null)
+                .forEach(pmml4Result -> {
+                    assertFalse(pmml4Result.getResultVariables().containsKey(REASON_CODE1_FIELD));
+                    assertFalse(pmml4Result.getResultVariables().containsKey(REASON_CODE2_FIELD));
+                });
     }
 
     private List<Map<String, Object>> getSamples() {
