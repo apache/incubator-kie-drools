fb1adca2ddcd7f49757cb11a511786993cf355e1
Tristan Radisson <tristan.radisson@gmail.com>
2021-10-19 14:34:59 +0200

    [KOGITO-6091] Setup Drools repos for Kogito (#1660)
    * [KOGITO-6091] Setup Drools repos for Kogito
    
    * update
    
    * updated GHA
    
    * updated deploy job

--- .ci/jenkins/dsl/jobs.groovy
+++ .ci/jenkins/dsl/jobs.groovy
@@ -80,8 +80,6 @@ setupMultijobPrLTSChecks()
 
 // Nightly jobs
 if (Utils.isMainBranch(this)) {
-    setupDroolsJob()
-
     setupQuarkusJob('main')
 }
 setupNativeJob()
@@ -124,23 +122,6 @@ void setupMultijobPrNativeChecks() {
 
 void setupMultijobPrLTSChecks() {
     KogitoJobTemplate.createMultijobLTSPRJobs(this, getMultijobPRConfig()) { return getDefaultJobParams() }
-}
-
-void setupDroolsJob() {
-    def jobParams = getJobParams('kogito-drools-snapshot', FolderUtils.getNightlyFolder(this), "${JENKINSFILE_PATH}/Jenkinsfile.drools", 'Kogito Runtimes Drools Snapshot')
-    jobParams.triggers = [ cron : 'H 2 * * *' ]
-    KogitoJobTemplate.createPipelineJob(this, jobParams).with {
-        parameters {
-            stringParam('BUILD_BRANCH_NAME', "${GIT_BRANCH}", 'Set the Git branch to checkout')
-            stringParam('GIT_AUTHOR', "${GIT_AUTHOR_NAME}", 'Set the Git author to checkout')
-
-            stringParam('DROOLS_VERSION', '', '(optional) If not set, then it will be guessed from drools repository')
-            stringParam('DROOLS_REPOSITORY', '', '(optional) In case Drools given version is in a specific repository')
-        }
-        environmentVariables {
-            env('JENKINS_EMAIL_CREDS_ID', "${JENKINS_EMAIL_CREDS_ID}")
-        }
-    }
 }
 
 void setupQuarkusJob(String quarkusBranch) {
@@ -227,6 +208,7 @@ void setupDeployJob(String jobFolder, KogitoJobType jobType) {
             // Release information
             booleanParam('CREATE_PR', false, 'Should we create a PR with the changes ?')
             stringParam('PROJECT_VERSION', '', 'Set the project version')
+            stringParam('DROOLS_VERSION', '', 'Drools version to set')
 
             booleanParam('SEND_NOTIFICATION', false, 'In case you want the pipeline to send a notification on CI channel for this run.')
         }
@@ -275,6 +257,7 @@ void setupPromoteJob(String jobFolder, KogitoJobType jobType) {
 
             // Release information which can override `deployment.properties`
             stringParam('PROJECT_VERSION', '', 'Override `deployment.properties`. Give the project version.')
+            stringParam('DROOLS_VERSION', '', 'Override `deployment.properties`. Drools version to set')
 
             stringParam('GIT_TAG', '', 'Git tag to set, if different from PROJECT_VERSION')
 
