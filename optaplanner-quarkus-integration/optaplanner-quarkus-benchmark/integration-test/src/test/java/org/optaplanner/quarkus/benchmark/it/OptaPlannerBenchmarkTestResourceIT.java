6aa6db8e8f25ed07a6eb380bd98c7dee69210d68
Christopher Chianelli <cchianel@redhat.com>
2021-12-08 12:27:38 -0500

    Remove native test for benchmarker (#1714)

--- optaplanner-quarkus-integration/optaplanner-quarkus-benchmark/integration-test/src/test/java/org/optaplanner/quarkus/benchmark/it/OptaPlannerBenchmarkTestResourceIT.java
+++ optaplanner-quarkus-integration/optaplanner-quarkus-benchmark/integration-test/src/test/java/org/optaplanner/quarkus/benchmark/it/OptaPlannerBenchmarkTestResourceIT.java
@@ -16,12 +16,15 @@
 
 package org.optaplanner.quarkus.benchmark.it;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.test.junit.NativeImageTest;
 
 /**
  * Test various OptaPlanner operations running in native mode
  */
 @NativeImageTest
+@Disabled("optaplanner-quarkus-benchmark cannot compile to native")
 public class OptaPlannerBenchmarkTestResourceIT extends OptaPlannerBenchmarkTestResourceTest {
 
 }
