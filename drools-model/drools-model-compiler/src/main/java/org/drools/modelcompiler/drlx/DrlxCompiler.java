12fea5ed98878c38fb9dfa93546f61b7cb1d2923
Edoardo Vacchi <evacchi@users.noreply.github.com>
2021-12-29 11:18:10 +0100

    restore drlx visitor, test cases

--- drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/drlx/DrlxCompiler.java
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/drlx/DrlxCompiler.java
@@ -1,14 +1,6 @@
 package org.drools.modelcompiler.drlx;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.github.javaparser.ParseResult;
-import com.github.javaparser.ParserConfiguration;
-import com.github.javaparser.Problem;
-import com.github.javaparser.Range;
-import com.github.javaparser.TokenRange;
+import com.github.javaparser.*;
 import com.github.javaparser.ast.CompilationUnit;
 import org.drools.compiler.compiler.ParserError;
 import org.drools.drl.ast.descr.PackageDescr;
@@ -16,6 +8,10 @@ import org.drools.mvel.parser.MvelParser;
 import org.drools.mvel.parser.ParseStart;
 import org.kie.api.io.Resource;
 import org.kie.internal.builder.KnowledgeBuilderResult;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.drools.mvel.parser.Providers.provider;
 
--- drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/drlx/DrlxVisitor.java
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/drlx/DrlxVisitor.java
@@ -6,19 +6,15 @@ import com.github.javaparser.ast.PackageDeclaration;
 import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.modules.ModuleDeclaration;
 import org.drools.compiler.lang.ParseException;
-import org.drools.drl.ast.dsl.CEDescrBuilder;
-import org.drools.drl.ast.dsl.DescrFactory;
-import org.drools.drl.ast.dsl.ImportDescrBuilder;
-import org.drools.drl.ast.dsl.PackageDescrBuilder;
-import org.drools.drl.ast.dsl.PatternDescrBuilder;
-import org.drools.drl.ast.dsl.RuleDescrBuilder;
 import org.drools.drl.ast.descr.AndDescr;
 import org.drools.drl.ast.descr.PackageDescr;
+import org.drools.drl.ast.dsl.*;
 import org.drools.mvel.parser.ast.expr.RuleConsequence;
 import org.drools.mvel.parser.ast.expr.RuleDeclaration;
 import org.drools.mvel.parser.ast.expr.RuleItem;
 import org.drools.mvel.parser.ast.expr.RulePattern;
 import org.drools.mvel.parser.ast.visitor.DrlVoidVisitor;
+import org.drools.mvel.parser.printer.PrintUtil;
 
 public class DrlxVisitor implements DrlVoidVisitor<Void> {
 
@@ -62,13 +58,13 @@ public class DrlxVisitor implements DrlVoidVisitor<Void> {
                 PatternDescrBuilder<? extends CEDescrBuilder<?, AndDescr>> pat = lhs.pattern();
                 RulePattern p = (RulePattern) item;
                 if (p.getBind() == null) {
-                    pat.constraint(p.getExpr().toString());
+                    pat.constraint(PrintUtil.printNode(p.getExpr()));
                 } else {
-                    pat.id(p.getBind().toString(), false).constraint(p.getExpr().toString());
+                    pat.id(PrintUtil.printNode(p.getBind()), false).constraint(PrintUtil.printNode(p.getExpr()));
                 }
             } else if (item instanceof RuleConsequence) {
                 RuleConsequence c = (RuleConsequence) item;
-                ruleDescrBuilder.rhs(c.getStatement().toString());
+                ruleDescrBuilder.rhs(PrintUtil.printNode(c.getStatement()));
             } else {
                 throw new IllegalArgumentException(item.getClass().getCanonicalName());
             }
