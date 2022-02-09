a484b2bf3729f9d04115cee09bf6f29cbf00bc12
Edoardo Vacchi <evacchi@users.noreply.github.com>
2022-02-09 19:05:44 +0100

    cleanup process interface

--- drools-builder-extensions/src/main/java/org/drools/compiler/compiler/ProcessBuilder.java
+++ drools-builder-extensions/src/main/java/org/drools/compiler/compiler/ProcessBuilder.java
@@ -15,15 +15,16 @@
 
 package org.drools.compiler.compiler;
 
-import java.io.IOException;
-import java.util.List;
-
 import org.kie.api.definition.process.Process;
 import org.kie.api.io.Resource;
+import org.kie.internal.builder.KnowledgeBuilderResult;
+
+import java.io.IOException;
+import java.util.List;
 
 public interface ProcessBuilder {
 
     List<Process> addProcessFromXml(Resource resource) throws IOException;
 
-    List<BaseKnowledgeBuilderResultImpl> getErrors();
+    List<KnowledgeBuilderResult> getErrors();
 }
