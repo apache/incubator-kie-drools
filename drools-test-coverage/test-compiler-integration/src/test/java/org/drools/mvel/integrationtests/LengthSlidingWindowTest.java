0c2c563d85ae8cc320a19cb5e69a77c0bdf4f9d5
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2022-03-17 18:46:31 +0900

    [DROOLS-6138] executable-model test failure in test-compiler-integration LengthSlidingWindowTest (#4259)

--- drools-test-coverage/test-compiler-integration/src/test/java/org/drools/mvel/integrationtests/LengthSlidingWindowTest.java
+++ drools-test-coverage/test-compiler-integration/src/test/java/org/drools/mvel/integrationtests/LengthSlidingWindowTest.java
@@ -51,8 +51,7 @@ public class LengthSlidingWindowTest {
 
     @Parameterized.Parameters(name = "KieBase type={0}")
     public static Collection<Object[]> getParameters() {
-     // TODO: EM failed with some tests. File JIRAs
-        return TestParametersUtil.getKieBaseStreamConfigurations(false);
+        return TestParametersUtil.getKieBaseStreamConfigurations(true);
     }
 
     @Test
