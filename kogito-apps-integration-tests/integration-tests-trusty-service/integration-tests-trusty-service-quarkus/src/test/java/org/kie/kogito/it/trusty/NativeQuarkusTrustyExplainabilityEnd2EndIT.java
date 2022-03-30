ed5eff2453bf11d1ea2e1ccb0952b2210751c2b6
Cristiano Nicolai <570894+cristianonicolai@users.noreply.github.com>
2022-03-30 17:22:20 +1000

    FAI-748 - Disable native test (#1284)

--- kogito-apps-integration-tests/integration-tests-trusty-service/integration-tests-trusty-service-quarkus/src/test/java/org/kie/kogito/it/trusty/NativeQuarkusTrustyExplainabilityEnd2EndIT.java
+++ kogito-apps-integration-tests/integration-tests-trusty-service/integration-tests-trusty-service-quarkus/src/test/java/org/kie/kogito/it/trusty/NativeQuarkusTrustyExplainabilityEnd2EndIT.java
@@ -15,11 +15,13 @@
  */
 package org.kie.kogito.it.trusty;
 
+import org.junit.jupiter.api.Disabled;
 import org.kie.kogito.testcontainers.QuarkusKogitoServiceContainer;
 
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
+@Disabled("https://issues.redhat.com/browse/FAI-748")
 public class NativeQuarkusTrustyExplainabilityEnd2EndIT extends AbstractTrustyExplainabilityEnd2EndIT {
 
     public NativeQuarkusTrustyExplainabilityEnd2EndIT() {
