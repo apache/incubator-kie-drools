c936ed76beae0a6a6914c700443d75165b8a393f
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2022-04-27 16:40:16 +0900

    - Added DRLParserWrapper to store errors using DRLErrorListener (#5)
    - Added MiscDRLParserTest which is being ported from RuleParserTest to enhance coverage.

---
+++ drools-drl/drools-drl10-parser/src/main/java/org/drools/parser/DRLErrorListener.java
@@ -0,0 +1,28 @@
+package org.drools.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+
+public class DRLErrorListener extends BaseErrorListener {
+
+    private final List<DRLParserError> errors = new ArrayList<>();
+
+    public List<DRLParserError> getErrors() {
+        return errors;
+    }
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer,
+                            Object offendingSymbol,
+                            int line,
+                            int charPositionInLine,
+                            String msg,
+                            RecognitionException e) {
+
+        errors.add(new DRLParserError(line, charPositionInLine, msg));
+    }
+}
