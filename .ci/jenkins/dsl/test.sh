97c3f0209d90fad2ea8490bbc47720db28b9cdec
radtriste <tradisso@redhat.com>
2022-05-05 13:55:11 +0200

    correction

--- .ci/jenkins/dsl/test.sh
+++ .ci/jenkins/dsl/test.sh
@@ -2,6 +2,6 @@
 
 file=$(mktemp)
 # For more usage of the script, use ./test.sh -h
-curl -o ${file} https://raw.githubusercontent.com/radtriste/kogito-pipelines/main/dsl/seed/scripts/seed_test.sh
+curl -o ${file} https://raw.githubusercontent.com/kiegroup/kogito-pipelines/main/dsl/seed/scripts/seed_test.sh
 chmod u+x ${file}
 ${file} $@
