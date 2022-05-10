f5392e1caf5fc857dda62b8875226a104c464864
Lukáš Petrovický <lukas@petrovicky.net>
2022-05-10 12:44:40 +0200

    Deprecate score holders

--- optaplanner-core/optaplanner-core-impl/src/main/java/org/optaplanner/core/api/score/buildin/hardmediumsoftlong/HardMediumSoftLongScoreHolder.java
+++ optaplanner-core/optaplanner-core-impl/src/main/java/org/optaplanner/core/api/score/buildin/hardmediumsoftlong/HardMediumSoftLongScoreHolder.java
@@ -1,17 +1,19 @@
 /*
- * Copyright 2020 Red Hat, Inc. and/or its affiliates.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  * Copyright 2022 Red Hat, Inc. and/or its affiliates.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package org.optaplanner.core.api.score.buildin.hardmediumsoftlong;
@@ -22,7 +24,10 @@ import org.optaplanner.core.api.score.holder.ScoreHolder;
 
 /**
  * @see HardMediumSoftLongScore
+ * @deprecated Score DRL is deprecated and will be removed in a future major version of OptaPlanner.
+ * See <a href="https://www.optaplanner.org/learn/drl-to-constraint-streams-migration.html">DRL to Constraint Streams migration recipe</a>.
  */
+@Deprecated(forRemoval = true)
 public interface HardMediumSoftLongScoreHolder extends ScoreHolder<HardMediumSoftLongScore> {
 
     /**
--- optaplanner-core/optaplanner-core-impl/src/main/java/org/optaplanner/core/api/score/holder/ScoreHolder.java
+++ optaplanner-core/optaplanner-core-impl/src/main/java/org/optaplanner/core/api/score/holder/ScoreHolder.java
@@ -1,17 +1,19 @@
 /*
- * Copyright 2021 Red Hat, Inc. and/or its affiliates.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  * Copyright 2022 Red Hat, Inc. and/or its affiliates.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package org.optaplanner.core.api.score.holder;
@@ -23,8 +25,11 @@ import org.optaplanner.core.api.score.Score;
 /**
  * This is the base interface for all score holder implementations.
  *
+ * @deprecated Score DRL is deprecated and will be removed in a future major version of OptaPlanner.
+ * See <a href="https://www.optaplanner.org/learn/drl-to-constraint-streams-migration.html">DRL to Constraint Streams migration recipe</a>.
  * @param <Score_> the {@link Score} type
  */
+@Deprecated(forRemoval = true)
 public interface ScoreHolder<Score_ extends Score<Score_>> {
 
     /**
