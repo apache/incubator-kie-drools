ac86d0810b84f2d1f13a7ae7444671c84fc52d28
Lukáš Petrovický <lukas@petrovicky.net>
2022-05-10 12:49:00 +0200

    Deprecate score DRL config

--- optaplanner-core/optaplanner-core-impl/src/main/java/org/optaplanner/core/config/score/definition/ScoreDefinitionType.java
+++ optaplanner-core/optaplanner-core-impl/src/main/java/org/optaplanner/core/config/score/definition/ScoreDefinitionType.java
@@ -1,17 +1,19 @@
 /*
- * Copyright 2014 Red Hat, Inc. and/or its affiliates.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  * Copyright 2022 Red Hat, Inc. and/or its affiliates.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package org.optaplanner.core.config.score.definition;
@@ -26,6 +28,7 @@ public enum ScoreDefinitionType {
      * WARNING: NOT RECOMMENDED TO USE DUE TO ROUNDING ERRORS THAT CAUSE SCORE CORRUPTION.
      * Use {@link #SIMPLE_BIG_DECIMAL} instead.
      */
+    @Deprecated(forRemoval = true)
     SIMPLE_DOUBLE,
     SIMPLE_BIG_DECIMAL,
     HARD_SOFT,
@@ -34,6 +37,7 @@ public enum ScoreDefinitionType {
      * WARNING: NOT RECOMMENDED TO USE DUE TO ROUNDING ERRORS THAT CAUSE SCORE CORRUPTION.
      * Use {@link #HARD_SOFT_BIG_DECIMAL} instead.
      */
+    @Deprecated(forRemoval = true)
     HARD_SOFT_DOUBLE,
     HARD_SOFT_BIG_DECIMAL,
     HARD_MEDIUM_SOFT,
