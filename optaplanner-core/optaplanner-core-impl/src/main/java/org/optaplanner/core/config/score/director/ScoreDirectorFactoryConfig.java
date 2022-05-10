3e086239cb8f68046b86a74aee292f8accc5a623
Lukáš Petrovický <lukas@petrovicky.net>
2022-05-10 13:29:39 +0200

    Fix copyright even more

--- optaplanner-core/optaplanner-core-impl/src/main/java/org/optaplanner/core/config/score/director/ScoreDirectorFactoryConfig.java
+++ optaplanner-core/optaplanner-core-impl/src/main/java/org/optaplanner/core/config/score/director/ScoreDirectorFactoryConfig.java
@@ -12,7 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */age org.optaplanner.core.config.score.director;
+ */
+package org.optaplanner.core.config.score.director;
 
 import java.io.File;
 import java.util.Arrays;
