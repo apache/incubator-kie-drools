5ee14817ee82e58c03f9b282b7be69fed541bb1b
Francisco Javier Tirado Sarti <65240126+fjtirado@users.noreply.github.com>
2022-06-03 13:41:42 +0200

    [KOGITO-7218] Adding integration test for gRPC (#2232)
    * [KOGITO-7218] Adding integration test for gRPC
    
    * [KOGITO-7218] Disable gRPC server on dev mode

---
+++ kogito-quarkus/extensions/kogito-quarkus-serverless-workflow-extension/kogito-quarkus-serverless-workflow-integration-test/src/test/java/org/kie/kogito/quarkus/workflows/RPCGreetIT.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.kogito.quarkus.workflows;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+
+@QuarkusIntegrationTest
+class RPCGreetRestIT {
+    @Test
+    void testEnglish() {
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\"workflowdata\" : {\"name\" : \"John\", \"language\":\"English\"}}").when()
+                .post("/rpcgreet")
+                .then()
+                .statusCode(201)
+                .body("workflowdata.message", containsString("Hello"));
+    }
+
+    @Test
+    void testSpanish() {
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\"workflowdata\" : {\"name\" : \"Javierito\", \"language\":\"Spanish\"}}").when()
+                .post("/rpcgreet")
+                .then()
+                .statusCode(201)
+                .body("workflowdata.message", containsString("Saludos"));
+    }
+
+    @Test
+    void testDefaultLanguage() {
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\"workflowdata\" : {\"name\" : \"John\"}}").when()
+                .post("/rpcgreet")
+                .then()
+                .statusCode(201)
+                .body("workflowdata.message", containsString("Hello"));
+    }
+
+    @Test
+    void testUnsupportedLanguage() {
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\"workflowdata\" : {\"name\" : \"Jan\", \"language\":\"Czech\"}}").when()
+                .post("/rpcgreet")
+                .then()
+                .statusCode(201)
+                .body("workflowdata.message", containsString("Hello"));
+    }
+}
