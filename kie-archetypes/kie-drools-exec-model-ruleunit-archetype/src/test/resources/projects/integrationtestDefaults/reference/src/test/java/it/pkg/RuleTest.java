2c00b37b9ab685fa4cf9bddb49c758eb2fd25c45
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2022-06-09 17:04:19 +0900

    [DROOLS-7009] Create Drools 8 archetype for exec-model and RuleUnit (#4447)

---
+++ kie-archetypes/kie-drools-exec-model-ruleunit-archetype/src/test/resources/projects/integrationtestDefaults/reference/src/test/java/it/pkg/RuleTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2022 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package it.pkg;
+
+import java.util.List;
+
+import org.drools.ruleunits.api.RuleUnitInstance;
+import org.drools.ruleunits.api.RuleUnitInstanceFactory;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class RuleTest {
+
+    static final Logger LOG = LoggerFactory.getLogger(RuleTest.class);
+
+    @Test
+    public void test() {
+        LOG.info("Creating RuleUnit");
+        MeasurementUnit measurementUnit = new MeasurementUnit();
+
+        RuleUnitInstance<MeasurementUnit> instance = RuleUnitInstanceFactory.instance(measurementUnit);
+
+        LOG.info("Insert data");
+        measurementUnit.getMeasurements().add(new Measurement("color", "red"));
+        measurementUnit.getMeasurements().add(new Measurement("color", "green"));
+        measurementUnit.getMeasurements().add(new Measurement("color", "blue"));
+
+        LOG.info("Run query. Rules are also fired");
+        List<Measurement> queryResult = instance.executeQuery("FindColor").stream().map(tuple -> (Measurement) tuple.get("$m")).collect(toList());
+
+        assertEquals(3, queryResult.size());
+        assertTrue("contains red", measurementUnit.getControlSet().contains("red"));
+        assertTrue("contains green", measurementUnit.getControlSet().contains("green"));
+        assertTrue("contains blue", measurementUnit.getControlSet().contains("blue"));
+    }
+}
