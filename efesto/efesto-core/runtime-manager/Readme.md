f834a18d0dfe67f33a7e16814bced3c0b304c061
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2022-06-28 18:43:43 +0200

    [DROOLS-7036] Fix as per PR suggestions

--- efesto/efesto-core/runtime-manager/Readme.md
+++
@@ -1,8 +0,0 @@
-Runtime Manager
-===================
-
-The code in these modules will be responsible for actual execution of models; most of the time this is represented by
-class-loading and method invocation, but exceptions should be considered as well.
-
-The code in `runtime-manager-api` should be the only one visible outside the `core` of the system, while the code
-inside `runtime-manager-common` should be considered **private** and hidden from outside.
--- kie-drl/kie-drl-runtime-common/src/test/java/org/kie/drl/engine/runtime/utils/EfestoKieSessionUtilTest.java
+++ kie-drl/kie-drl-runtime-common/src/test/java/org/kie/drl/engine/runtime/utils/EfestoKieSessionUtilTest.java
@@ -1,4 +1,4 @@
-package org.kie.drl.engine.runtime.utils;/*
+/*
  * Copyright 2022 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +13,7 @@ package org.kie.drl.engine.runtime.utils;/*
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.kie.drl.engine.runtime.utils;
 
 import org.drools.model.Model;
 import org.junit.jupiter.api.BeforeAll;
