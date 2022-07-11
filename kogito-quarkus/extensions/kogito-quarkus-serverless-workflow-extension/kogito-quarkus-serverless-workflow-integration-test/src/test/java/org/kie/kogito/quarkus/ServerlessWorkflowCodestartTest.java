60ab12227a993e3d54b887276ccafb8b8fe9d458
Cristiano Nicolai <570894+cristianonicolai@users.noreply.github.com>
2022-07-11 17:27:52 +1000

    KOGITO-7520 - Quarkus SW codestart (#2323)

---
+++ kogito-quarkus/extensions/kogito-quarkus-serverless-workflow-extension/kogito-quarkus-serverless-workflow-integration-test/src/test/java/org/kie/kogito/quarkus/ServerlessWorkflowCodestartTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2022 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.kogito.quarkus;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.devtools.testing.codestarts.QuarkusCodestartTest;
+
+import static io.quarkus.devtools.codestarts.quarkus.QuarkusCodestartCatalog.Language.JAVA;
+
+public class ServerlessWorkflowCodestartTest {
+
+    @RegisterExtension
+    public static QuarkusCodestartTest codestartTest = QuarkusCodestartTest.builder()
+            .setupStandaloneExtensionTest("org.kie.kogito:kogito-quarkus-serverless-workflow")
+            .languages(JAVA)
+            .build();
+
+    @Test
+    void testContent() throws Throwable {
+        codestartTest.checkGeneratedTestSource("org.acme.GreetTest");
+        codestartTest.assertThatGeneratedFileMatchSnapshot(JAVA, "src/main/resources/greet.sw.json");
+    }
+
+    @Test
+    void buildAllProjectsForLocalUse() throws Throwable {
+        codestartTest.buildAllProjects();
+    }
+
+}
---
+++ kogito-quarkus/extensions/kogito-quarkus-serverless-workflow-extension/kogito-quarkus-serverless-workflow-integration-test/src/test/resources/__snapshots__/ServerlessWorkflowCodestartTest/testContent/src_test_java_ilove_quark_us_GreetTest.java
@@ -0,0 +1,39 @@
+package ilove.quark.us;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+
+@QuarkusTest
+public class GreetTest {
+
+    @Test
+    public void testEnglish() {
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\"workflowdata\" : {\"name\" : \"Yoda\", \"language\":\"English\"}}")
+                .when()
+                .post("/greet")
+                .then()
+                .statusCode(201)
+                .body("workflowdata.greeting", containsString("Hello"));
+    }
+
+    @Test
+    public void testSpanish() {
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\"workflowdata\" : {\"name\" : \"Yoda\", \"language\":\"Spanish\"}}")
+                .when()
+                .post("/greet")
+                .then()
+                .statusCode(201)
+                .body("workflowdata.greeting", containsString("Saludos"));
+    }
+}
