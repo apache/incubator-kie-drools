8ca8770cb19506c2a0fb922fbda36a14ee0bcc4c
Steve Davidson <steve@j2eeguys.com>
2022-07-11 18:03:58 -0500

    Remove unused import.
    Signed-off-by: Steve Davidson <steve@j2eeguys.com>

--- kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/compiler/DMNCompilerContext.java
+++ kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/compiler/DMNCompilerContext.java
@@ -20,7 +20,6 @@ import java.io.Reader;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.function.Function;
 
