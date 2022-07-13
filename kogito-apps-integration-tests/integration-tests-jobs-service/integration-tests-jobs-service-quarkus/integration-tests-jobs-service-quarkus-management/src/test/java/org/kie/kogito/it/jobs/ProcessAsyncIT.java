f45e0cc69c44cb42c4d4e8f7184144a28e477a70
Tiago Dolphine <tiagodolphine@gmail.com>
2022-07-13 02:49:19 -0300

    KOGITO-7489 fix ProcessAsyncIT (#1406)
    * KOGITO-7489 fix ProcessAsyncIT
    
    * KOGITO-7489 fix ProcessAsyncIT
    
    * KOGITO-7489 fix ProcessAsyncIT
    
    * KOGITO-7489 fix ProcessAsyncIT
    
    * KOGITO-7489 fix ProcessAsyncIT
    
    * KOGITO-7489 fix ProcessAsyncIT

--- kogito-apps-integration-tests/integration-tests-jobs-service/integration-tests-jobs-service-quarkus/integration-tests-jobs-service-quarkus-management/src/test/java/org/kie/kogito/it/jobs/ProcessAsyncIT.java
+++ kogito-apps-integration-tests/integration-tests-jobs-service/integration-tests-jobs-service-quarkus/integration-tests-jobs-service-quarkus-management/src/test/java/org/kie/kogito/it/jobs/ProcessAsyncIT.java
@@ -15,14 +15,36 @@
  */
 package org.kie.kogito.it.jobs;
 
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.kie.kogito.test.resources.JobServiceQuarkusTestResource;
 import org.kie.kogito.test.resources.KogitoServiceRandomPortQuarkusTestResource;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static org.awaitility.Awaitility.await;
 
 @QuarkusIntegrationTest
 @QuarkusTestResource(KogitoServiceRandomPortQuarkusTestResource.class)
 @QuarkusTestResource(JobServiceQuarkusTestResource.class)
 class ProcessAsyncIT extends BaseProcessAsyncIT {
+
+    @BeforeEach
+    private void setup() {
+        //health check - wait to be ready
+        await()
+                .atMost(TIMEOUT)
+                .pollDelay(2, TimeUnit.SECONDS)
+                .pollInterval(1, TimeUnit.SECONDS)
+                .untilAsserted(() -> given()
+                        .contentType(ContentType.JSON)
+                        .accept(ContentType.JSON)
+                        .get("/q/health")
+                        .then()
+                        .statusCode(200));
+    }
 }
