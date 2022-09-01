2063912b52aee6c8f4fc1a123095739ed5f9d9ae
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2022-09-01 18:00:28 +0900

    - add EfestoCompilationContext.createIndexFiles overwriting test

--- efesto/efesto-core/efesto-compilation-manager/efesto-compilation-manager-core/src/test/java/org/kie/efesto/compilationmanager/core/service/TestCompilationManagerImpl.java
+++ efesto/efesto-core/efesto-compilation-manager/efesto-compilation-manager-core/src/test/java/org/kie/efesto/compilationmanager/core/service/TestCompilationManagerImpl.java
@@ -21,21 +21,29 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.kie.efesto.common.api.io.IndexFile;
+import org.kie.efesto.common.api.model.FRI;
+import org.kie.efesto.common.api.model.GeneratedExecutableResource;
+import org.kie.efesto.common.api.model.GeneratedResources;
 import org.kie.efesto.compilationmanager.api.model.EfestoCompilationContext;
 import org.kie.efesto.compilationmanager.api.model.EfestoResource;
 import org.kie.efesto.compilationmanager.api.service.CompilationManager;
 import org.kie.efesto.compilationmanager.core.mocks.AbstractMockOutput;
+import org.kie.efesto.compilationmanager.core.mocks.MockEfestoCallableOutput;
+import org.kie.efesto.compilationmanager.core.mocks.MockEfestoCallableOutputE;
 import org.kie.efesto.compilationmanager.core.mocks.MockEfestoRedirectOutputA;
 import org.kie.efesto.compilationmanager.core.mocks.MockEfestoRedirectOutputB;
 import org.kie.efesto.compilationmanager.core.mocks.MockEfestoRedirectOutputC;
 import org.kie.efesto.compilationmanager.core.mocks.MockEfestoRedirectOutputD;
+import org.kie.efesto.compilationmanager.core.mocks.MockEfestoRedirectOutputE;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.kie.efesto.common.api.utils.JSONUtils.getGeneratedResourcesObject;
 
 class TestCompilationManagerImpl {
 
@@ -103,6 +111,60 @@ class TestCompilationManagerImpl {
             assertThat(indexFiles).hasSize(1);
             indexFile = indexFiles.get("mock");
             assertThat(indexFile).exists();
+        } finally {
+            if (indexFile != null) {
+                indexFile.delete();
+            }
+        }
+    }
+
+    @Test
+    void overwritingIndexFile() {
+        IndexFile indexFile = null;
+
+        // 1st round
+        try {
+            EfestoCompilationContext context = EfestoCompilationContext.buildWithParentClassLoader(CompilationManager.class.getClassLoader());
+            List<AbstractMockOutput> toProcess = new ArrayList<>();
+            toProcess.add(new MockEfestoRedirectOutputA());
+            compilationManager.processResource(context, toProcess.toArray(new EfestoResource[0]));
+            assertThat(context.getGeneratedResourcesMap()).hasSize(1);
+
+            Map<String, IndexFile> indexFiles = context.createIndexFiles(TARGET_TEST_CLASSES_DIRECTORY);
+            assertThat(indexFiles).hasSize(1);
+            indexFile = indexFiles.get("mock");
+            assertThat(indexFile).exists();
+        } catch (Exception e) {
+            fail("failed during 1st round", e);
+        }
+
+        // 2nd round
+        try {
+            EfestoCompilationContext context = EfestoCompilationContext.buildWithParentClassLoader(CompilationManager.class.getClassLoader());
+            List<AbstractMockOutput> toProcess = new ArrayList<>();
+            toProcess.add(new MockEfestoRedirectOutputE());
+            compilationManager.processResource(context, toProcess.toArray(new EfestoResource[0]));
+            assertThat(context.getGeneratedResourcesMap()).hasSize(1);
+
+            Map<String, IndexFile> indexFiles = context.createIndexFiles(TARGET_TEST_CLASSES_DIRECTORY);
+            assertThat(indexFiles).hasSize(1);
+            indexFile = indexFiles.get("mock");
+            assertThat(indexFile).exists();
+
+            GeneratedResources finalContent = getGeneratedResourcesObject(indexFile);
+            assertThat(finalContent).hasSize(2);
+
+            List<FRI> friList = Arrays.asList(new MockEfestoCallableOutput().getFri(), new MockEfestoCallableOutputE().getFri());
+
+            List<GeneratedExecutableResource> resourceList = finalContent.stream().filter(GeneratedExecutableResource.class::isInstance)
+                    .map(GeneratedExecutableResource.class::cast)
+                    .filter(resource -> friList.contains(resource.getFri()))
+                    .collect(Collectors.toList());
+
+            // contains "/mock/mock/efesto/output/module" and "/mock/mock/efesto/output/moduleE"
+            assertThat(resourceList).hasSize(2);
+        } catch (Exception e) {
+            fail("failed during 2nd round", e);
         } finally {
             if (indexFile != null) {
                 indexFile.delete();
