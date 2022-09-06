41557ff9228a20c1be3fd573444b43dab2b2d96e
vpiyanov <vladimir.piyanov@gmail.com>
2022-09-06 17:50:51 +0300

    DROOLS-7139 - FEEL context functions `get value` and `get entries` should support Java POJO as argument (#4655)
    * DROOLS-7139 - FEEL context functions `get value` and `get entries` should support Java POJO as argument
    
    * implement test code review simplification
    
    * implement code review simplification
    
    Co-authored-by: Vladimir Piyanov <vpiyanov@natera.com>
    Co-authored-by: tarilabs <matteo.mortari@gmail.com>

--- kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/GetValueFunction.java
+++ kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/GetValueFunction.java
@@ -19,6 +19,8 @@ package org.kie.dmn.feel.runtime.functions;
 import java.util.Map;
 
 import org.kie.dmn.api.feel.runtime.events.FEELEvent.Severity;
+import org.kie.dmn.feel.lang.types.BuiltInType;
+import org.kie.dmn.feel.lang.types.impl.ImmutableFPAWrappingPOJO;
 import org.kie.dmn.feel.runtime.events.InvalidParametersEvent;
 
 public class GetValueFunction extends BaseFEELFunction {
@@ -30,10 +32,12 @@ public class GetValueFunction extends BaseFEELFunction {
     public FEELFnResult<Object> invoke(@ParameterName("m") Object m, @ParameterName("key") String key) {
         if (m == null) {
             return FEELFnResult.ofError(new InvalidParametersEvent(Severity.ERROR, "m", "cannot be null"));
-        } else if (!(m instanceof Map)) {
-            return FEELFnResult.ofError(new InvalidParametersEvent(Severity.ERROR, "m", "is not a context"));
-        } else {
+        } else if (m instanceof Map) {
             return FEELFnResult.ofResult(((Map<?, ?>) m).get(key));
+        } else if (BuiltInType.determineTypeFromInstance(m) == BuiltInType.UNKNOWN) {
+            return FEELFnResult.ofResult(new ImmutableFPAWrappingPOJO(m).getFEELProperty(key).toOptional().orElse(null));
+        } else {
+            return FEELFnResult.ofError(new InvalidParametersEvent(Severity.ERROR, "m", "is not a context"));
         }
     }
 }
