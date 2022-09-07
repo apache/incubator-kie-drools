a518563bcde4223b5a7621893fe02dd9b013e566
Matteo Mortari <matteo.mortari@gmail.com>
2022-09-07 07:15:32 +0200

    DROOLS-7139 - FEEL context functions `get value` and `get entries` should support Java POJO as argument (#4655) (#4666)
    * DROOLS-7139 - FEEL context functions `get value` and `get entries` should support Java POJO as argument
    
    * implement test code review simplification
    
    * implement code review simplification
    
    Co-authored-by: Vladimir Piyanov <vpiyanov@natera.com>
    Co-authored-by: tarilabs <matteo.mortari@gmail.com>
    
    Co-authored-by: vpiyanov <vladimir.piyanov@gmail.com>
    Co-authored-by: Vladimir Piyanov <vpiyanov@natera.com>

--- kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/GetEntriesFunction.java
+++ kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/GetEntriesFunction.java
@@ -22,7 +22,10 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.kie.dmn.api.feel.runtime.events.FEELEvent.Severity;
+import org.kie.dmn.feel.lang.types.BuiltInType;
+import org.kie.dmn.feel.lang.types.impl.ImmutableFPAWrappingPOJO;
 import org.kie.dmn.feel.runtime.events.InvalidParametersEvent;
+import org.kie.dmn.feel.runtime.functions.extended.ContextPutFunction;
 
 public class GetEntriesFunction extends BaseFEELFunction {
 
@@ -33,18 +36,19 @@ public class GetEntriesFunction extends BaseFEELFunction {
     public FEELFnResult<List<Object>> invoke(@ParameterName("m") Object m) {
         if (m == null) {
             return FEELFnResult.ofError(new InvalidParametersEvent(Severity.ERROR, "m", "cannot be null"));
-        } else if (!(m instanceof Map)) {
-            return FEELFnResult.ofError(new InvalidParametersEvent(Severity.ERROR, "m", "is not a context"));
-        } else {
-            List<Object> result = ((Map<?, ?>) m).entrySet().stream()
-                                                 .map(kv -> {
-                                                     Map<Object, Object> entry = new HashMap<>();
-                                                     entry.put("key", kv.getKey());
-                                                     entry.put("value", kv.getValue());
-                                                     return entry;
-                                                 })
-                                                 .collect(Collectors.toList());
-            return FEELFnResult.ofResult(result);
         }
+        return ContextPutFunction.toMap(m).map(GetEntriesFunction::toEntries);
+    }
+
+    private static List<Object> toEntries(Map<?, ?> m) {
+        List<Object> result = m.entrySet().stream()
+                                             .map(kv -> {
+                                                 Map<Object, Object> entry = new HashMap<>();
+                                                 entry.put("key", kv.getKey());
+                                                 entry.put("value", kv.getValue());
+                                                 return entry;
+                                             })
+                                             .collect(Collectors.toList());
+        return result;
     }
 }
