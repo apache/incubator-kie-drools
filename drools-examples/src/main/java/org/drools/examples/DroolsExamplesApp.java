4d71b7586a9fada0c216eaaa71116ba4c6e65309
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2022-10-04 20:31:13 +0900

    [DROOLS-7189] Hide non-working examples (#4730)

--- drools-examples/src/main/java/org/drools/examples/DroolsExamplesApp.java
+++ drools-examples/src/main/java/org/drools/examples/DroolsExamplesApp.java
@@ -20,9 +20,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 
 import javax.swing.*;
-import org.drools.examples.datadriventemplate.DataDrivenTemplateExample;
 import org.drools.examples.decisiontable.PricingRuleDTExample;
-import org.drools.examples.decisiontable.PricingRuleTemplateExample;
 import org.drools.examples.fibonacci.FibonacciExample;
 import org.drools.examples.golfing.GolfingExample;
 import org.drools.examples.helloworld.HelloWorldExample;
@@ -31,14 +29,7 @@ import org.drools.examples.petstore.PetStoreExample;
 import org.drools.examples.shopping.ShoppingExample;
 import org.drools.examples.state.StateExampleUsingAgendaGroup;
 import org.drools.examples.state.StateExampleUsingSalience;
-import org.drools.examples.sudoku.SudokuExample;
-import org.drools.examples.templates.SimpleRuleTemplateExample;
 import org.drools.examples.troubleticket.TroubleTicketExample;
-import org.drools.examples.troubleticket.TroubleTicketExampleWithDSL;
-import org.drools.examples.troubleticket.TroubleTicketExampleWithDT;
-import org.drools.examples.workitemconsequence.WorkItemConsequenceExample1;
-import org.drools.examples.workitemconsequence.WorkItemConsequenceExample2;
-import org.drools.games.adventures.TextAdventure;
 import org.drools.games.pong.PongMain;
 import org.drools.games.wumpus.WumpusWorldMain;
 import org.kie.api.KieServices;
@@ -61,7 +52,7 @@ public class DroolsExamplesApp extends JFrame {
     private final KieContainer kieContainer;
 
     public DroolsExamplesApp() {
-        super("JBoss BRMS examples");
+        super("Drools examples");
         setContentPane(createContentPane());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         logger.info("DroolsExamplesApp started.");
@@ -77,22 +68,23 @@ public class DroolsExamplesApp extends JFrame {
     private Container createContentPane() {
         JPanel contentPane = new JPanel(new GridLayout(0, 1));
         contentPane.add(new JLabel("Which GUI example do you want to see?"));
-
-        contentPane.add(new JButton(new AbstractAction("SudokuExample") {
-            public void actionPerformed(ActionEvent e) {
-                new SudokuExample().init(kieContainer, false);
-            }
-        }));
+// DROOLS-7189
+//        contentPane.add(new JButton(new AbstractAction("SudokuExample") {
+//            public void actionPerformed(ActionEvent e) {
+//                new SudokuExample().init(kieContainer, false);
+//            }
+//        }));
         contentPane.add(new JButton(new AbstractAction("PetStoreExample") {
             public void actionPerformed(ActionEvent e) {
                 new PetStoreExample().init(kieContainer, false);
             }
         }));
-        contentPane.add(new JButton(new AbstractAction("TextAdventure") {
-            public void actionPerformed(ActionEvent e) {
-                new TextAdventure().init(kieContainer, false);
-            }
-        }));
+// DROOLS-7189
+//        contentPane.add(new JButton(new AbstractAction("TextAdventure") {
+//            public void actionPerformed(ActionEvent e) {
+//                new TextAdventure().init(kieContainer, false);
+//            }
+//        }));
         contentPane.add(new JButton(new AbstractAction("Pong") {
             public void actionPerformed(ActionEvent e) {
                 new PongMain().init(kieContainer, false);
@@ -131,26 +123,28 @@ public class DroolsExamplesApp extends JFrame {
                 GolfingExample.execute( kieContainer );
             }
         }));
-        contentPane.add(new JButton(new AbstractAction("SimpleRuleTemplateExample") {
-            public void actionPerformed(ActionEvent e) {
-                SimpleRuleTemplateExample.execute( kieContainer );
-            }
-        }));
+// DROOLS-7189
+//        contentPane.add(new JButton(new AbstractAction("SimpleRuleTemplateExample") {
+//            public void actionPerformed(ActionEvent e) {
+//                SimpleRuleTemplateExample.execute( kieContainer );
+//            }
+//        }));
         contentPane.add(new JButton(new AbstractAction("TroubleTicketExample") {
             public void actionPerformed(ActionEvent e) {
                 TroubleTicketExample.execute( kieContainer );
             }
         }));
-        contentPane.add(new JButton(new AbstractAction("TroubleTicketExampleWithDT") {
-            public void actionPerformed(ActionEvent e) {
-                TroubleTicketExampleWithDT.execute( kieContainer );
-            }
-        }));
-        contentPane.add(new JButton(new AbstractAction("TroubleTicketExampleWithDSL") {
-            public void actionPerformed(ActionEvent e) {
-                TroubleTicketExampleWithDSL.execute( kieContainer );
-            }
-        }));
+// DROOLS-7189
+//        contentPane.add(new JButton(new AbstractAction("TroubleTicketExampleWithDT") {
+//            public void actionPerformed(ActionEvent e) {
+//                TroubleTicketExampleWithDT.execute( kieContainer );
+//            }
+//        }));
+//        contentPane.add(new JButton(new AbstractAction("TroubleTicketExampleWithDSL") {
+//            public void actionPerformed(ActionEvent e) {
+//                TroubleTicketExampleWithDSL.execute( kieContainer );
+//            }
+//        }));
         contentPane.add(new JButton(new AbstractAction("StateExampleUsingSalience") {
             public void actionPerformed(ActionEvent e) {
                 StateExampleUsingSalience.execute( kieContainer );
@@ -161,31 +155,33 @@ public class DroolsExamplesApp extends JFrame {
                 StateExampleUsingAgendaGroup.execute( kieContainer );
             }
         }));
-        contentPane.add(new JButton(new AbstractAction("PricingRuleTemplateExample") {
-            public void actionPerformed(ActionEvent e) {
-                PricingRuleTemplateExample.execute( kieContainer );
-            }
-        }));
+// DROOLS-7189
+//        contentPane.add(new JButton(new AbstractAction("PricingRuleTemplateExample") {
+//            public void actionPerformed(ActionEvent e) {
+//                PricingRuleTemplateExample.execute( kieContainer );
+//            }
+//        }));
         contentPane.add(new JButton(new AbstractAction("PricingRuleDTExample") {
             public void actionPerformed(ActionEvent e) {
                 PricingRuleDTExample.execute( kieContainer );
             }
         }));
-        contentPane.add(new JButton(new AbstractAction("DataDrivenTemplateExample") {
-            public void actionPerformed(ActionEvent e) {
-                DataDrivenTemplateExample.main(new String[0]);
-            }
-        }));
-        contentPane.add(new JButton(new AbstractAction("WorkItemConsequenceExample1") {
-            public void actionPerformed(ActionEvent e) {
-                WorkItemConsequenceExample1.main(new String[0]);
-            }
-        }));
-        contentPane.add(new JButton(new AbstractAction("WorkItemConsequenceExample2") {
-            public void actionPerformed(ActionEvent e) {
-                WorkItemConsequenceExample2.main(new String[0]);
-            }
-        }));
+// DROOLS-7189
+//        contentPane.add(new JButton(new AbstractAction("DataDrivenTemplateExample") {
+//            public void actionPerformed(ActionEvent e) {
+//                DataDrivenTemplateExample.main(new String[0]);
+//            }
+//        }));
+//        contentPane.add(new JButton(new AbstractAction("WorkItemConsequenceExample1") {
+//            public void actionPerformed(ActionEvent e) {
+//                WorkItemConsequenceExample1.main(new String[0]);
+//            }
+//        }));
+//        contentPane.add(new JButton(new AbstractAction("WorkItemConsequenceExample2") {
+//            public void actionPerformed(ActionEvent e) {
+//                WorkItemConsequenceExample2.main(new String[0]);
+//            }
+//        }));
         return contentPane;
     }
 
