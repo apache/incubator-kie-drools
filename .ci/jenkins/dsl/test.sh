1aa97097fe5021207197d59b2e4602eeb3d66280
Tristan Radisson <tradisso@redhat.com>
2022-10-14 17:09:34 +0200

    Correct GHA DSL checks (#1489)

--- .ci/jenkins/dsl/test.sh
+++ .ci/jenkins/dsl/test.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -e
-
-export DSL_DEFAULT_MAIN_CONFIG_FILE_REPO=kiegroup/kogito-pipelines
-export DSL_DEFAULT_MAIN_CONFIG_FILE_REF=main
-export DSL_DEFAULT_MAIN_CONFIG_FILE_PATH=dsl/config/main.yaml
-
 file=$(mktemp)
 # For more usage of the script, use ./test.sh -h
 curl -o ${file} https://raw.githubusercontent.com/kiegroup/kogito-pipelines/main/dsl/seed/scripts/seed_test.sh
