1723bc582f604c448f19f58b7e0096b8820eb202
Tristan Radisson <tradisso@redhat.com>
2022-10-18 10:17:48 +0200

    KOGITO-8110 Correct GHA DSL checks (#2242)
    * Correct GHA DSL checks
    
    * Update .ci/jenkins/dsl/test.sh
    
    * correction

--- .ci/jenkins/dsl/test.sh
+++ .ci/jenkins/dsl/test.sh
@@ -1,10 +1,31 @@
 #!/bin/bash -e
 
-script_dir_path=$(cd `dirname "${BASH_SOURCE[0]}"`; pwd -P)
+# Used to retrieve current git author, to set correctly the main config file repo
+GIT_SERVER='github.com'
 
-export DSL_DEFAULT_MAIN_CONFIG_FILE_REPO=kiegroup/optaplanner
-export DSL_DEFAULT_MAIN_CONFIG_FILE_REF=main
+git_url=$(git remote -v | grep origin | awk -F' ' '{print $2}' | head -n 1)
+if [ -z "${git_url}" ]; then
+  echo "Script must be executed in a Git repository for this script to run correctly"
+  exit 1
+fi
+echo "git_url = ${git_url}"
+
+git_server_url=
+if [[ "${git_url}" = https://* ]]; then
+  git_server_url="https://${GIT_SERVER}/"
+elif [[ "${git_url}" = git@* ]]; then 
+  git_server_url="git@${GIT_SERVER}:"
+else
+  echo "Unknown protocol for url ${git_url}"
+  exit 1
+fi
+
+git_author="$(echo ${git_url} | awk -F"${git_server_url}" '{print $2}' | awk -F. '{print $1}'  | awk -F/ '{print $1}')"
+
+export DSL_DEFAULT_MAIN_CONFIG_FILE_REPO="${git_author}"/optaplanner
+export DSL_DEFAULT_FALLBACK_MAIN_CONFIG_FILE_REPO=kiegroup/optaplanner
 export DSL_DEFAULT_MAIN_CONFIG_FILE_PATH=.ci/jenkins/config/main.yaml
+export DSL_DEFAULT_BRANCH_CONFIG_FILE_REPO="${git_author}"/optaplanner
 
 file=$(mktemp)
 # For more usage of the script, use ./test.sh -h
