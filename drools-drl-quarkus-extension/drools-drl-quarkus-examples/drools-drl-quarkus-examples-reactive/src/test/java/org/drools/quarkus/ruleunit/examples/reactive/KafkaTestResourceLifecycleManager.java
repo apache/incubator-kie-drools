dd902a3be5ea9789bebe13b4f2f3e8f0bba0736a
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2022-11-09 17:56:11 +0900

    [DROOLS-7180] Add Reactive Messaging with DataSource and  RuleUnit example

---
+++ drools-drl-quarkus-extension/drools-drl-quarkus-examples/drools-drl-quarkus-examples-reactive/src/test/java/org/drools/quarkus/ruleunit/examples/reactive/KafkaTestResourceLifecycleManager.java
@@ -0,0 +1,25 @@
+package org.drools.quarkus.ruleunit.examples.reactive;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import io.smallrye.reactive.messaging.providers.connectors.InMemoryConnector;
+
+public class KafkaTestResourceLifecycleManager implements QuarkusTestResourceLifecycleManager {
+
+    @Override
+    public Map<String, String> start() {
+        Map<String, String> env = new HashMap<>();
+        Map<String, String> props1 = InMemoryConnector.switchIncomingChannelsToInMemory("events");
+        Map<String, String> props2 = InMemoryConnector.switchOutgoingChannelsToInMemory("alerts");
+        env.putAll(props1);
+        env.putAll(props2);
+        return env;
+    }
+
+    @Override
+    public void stop() {
+        InMemoryConnector.clear();
+    }
+}
