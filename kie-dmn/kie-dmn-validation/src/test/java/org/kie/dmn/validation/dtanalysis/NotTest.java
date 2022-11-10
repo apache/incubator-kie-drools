d6f0e314116e6dc764e0602f98928d0d7b059cc5
Daniel J dos Santos <danielzhe@users.noreply.github.com>
2022-11-10 17:19:44 -0300

    [RHDM-1895] Saving dmn model is not working when using not() function (#4804)
    * DROOLS-6913 DMN DTAnalysis negation of non-compliant DMN missing enum (#4327)
    
    * rebuild
    
    Co-authored-by: Matteo Mortari <matteo.mortari@gmail.com>

--- kie-dmn/kie-dmn-validation/src/test/java/org/kie/dmn/validation/dtanalysis/NotTest.java
+++ kie-dmn/kie-dmn-validation/src/test/java/org/kie/dmn/validation/dtanalysis/NotTest.java
@@ -29,9 +29,10 @@ import org.kie.dmn.validation.dtanalysis.model.Hyperrectangle;
 import org.kie.dmn.validation.dtanalysis.model.Interval;
 import org.kie.dmn.validation.dtanalysis.model.Overlap;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.kie.dmn.validation.DMNValidator.Validation.ANALYZE_DECISION_TABLE;
 
 public class NotTest extends AbstractDTAnalysisTest {
@@ -98,6 +99,21 @@ public class NotTest extends AbstractDTAnalysisTest {
         // assert OVERLAPs count.
         assertThat(analysis.getOverlaps(), hasSize(0));
 
+    }
+
+    @Test
+    public void testNotStringVowel2() {
+        List<DMNMessage> validate = validator.validate(getReader("NotStringVowel2.dmn"), ANALYZE_DECISION_TABLE);
+        DTAnalysis analysis = getAnalysis(validate, "_406133D7-96FE-4237-8726-44D839F400D6");
+
+        // assert GAPS count
+        assertThat(analysis.getGaps()).hasSize(0);
+
+        // assert OVERLAPs count.
+        assertThat(analysis.getOverlaps()).hasSize(0);
+
+        assertThat(validate).hasSize(1);
+        assertThat(validate).anyMatch(m -> m.getText().contains("string values which can be enumerated for the inputs; Gap analysis skipped"));
     }
 
     @Test
