fb8a58cda928ba604efe7a61f0a6725f3418ddfe
Cristiano Nicolai <570894+cristianonicolai@users.noreply.github.com>
2022-12-20 22:22:24 +1000

    Fix for ProcessAsyncIT adding delay (#1570)

--- kogito-apps-integration-tests/integration-tests-jobs-service/integration-tests-jobs-service-common/src/main/java/org/kie/kogito/AbstractHelloService.java
+++ kogito-apps-integration-tests/integration-tests-jobs-service/integration-tests-jobs-service-common/src/main/java/org/kie/kogito/AbstractHelloService.java
@@ -15,6 +15,8 @@
  */
 package org.kie.kogito;
 
+import java.util.concurrent.TimeUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,7 +24,9 @@ public abstract class AbstractHelloService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractHelloService.class);
 
-    public String hello(String input) {
+    public String hello(String input) throws InterruptedException {
+        //Add artificial delay to avoid knative http completing the callback too fast
+        TimeUnit.SECONDS.sleep(1l);
         LOGGER.info("Executing {}.hello(\"{}\")", getClass().getName(), input);
         return "Hello " + input;
     }
