e24e37c69e941e16bba2ce7f2d7b2f84ce296d4e
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2022-12-22 18:29:49 +0900

    [DROOLS-6992] Enable GenerateModel when drools-engine is a dependency even without explicit drools-model-compiler

--- kie-maven-plugin/src/main/java/org/kie/maven/plugin/helpers/ExecModelModeHelper.java
+++ kie-maven-plugin/src/main/java/org/kie/maven/plugin/helpers/ExecModelModeHelper.java
@@ -20,10 +20,14 @@ import java.util.List;
 import org.apache.maven.model.Dependency;
 
 import static java.util.Arrays.asList;
-import static org.kie.maven.plugin.enums.ExecModelMode.*;
+import static org.kie.maven.plugin.enums.ExecModelMode.WITHANC;
+import static org.kie.maven.plugin.enums.ExecModelMode.WITHDRL_MVEL;
+import static org.kie.maven.plugin.enums.ExecModelMode.WITHMVEL;
+import static org.kie.maven.plugin.enums.ExecModelMode.YES;
+import static org.kie.maven.plugin.enums.ExecModelMode.YES_WITHDRL;
+import static org.kie.maven.plugin.enums.ExecModelMode.valueOf;
 
 public class ExecModelModeHelper {
-
 
     public static boolean execModelParameterEnabled(String s) {
         return asList(YES, YES_WITHDRL, WITHMVEL, WITHDRL_MVEL, WITHANC).contains(valueOf(s.toUpperCase()));
@@ -34,9 +38,11 @@ public class ExecModelModeHelper {
     }
 
     public static boolean isModelCompilerInClassPath(List<Dependency> dependencies) {
-        return dependencies.stream().anyMatch(
-                    d -> d.getArtifactId().equals("drools-model-compiler") &&
-                            d.getGroupId().equals("org.drools"));
+        return dependencies.stream()
+                           .anyMatch(d -> d.getGroupId().equals("org.drools") &&
+                                          (d.getArtifactId().equals("drools-model-compiler") ||
+                                           d.getArtifactId().equals("drools-engine") ||
+                                           d.getArtifactId().equals("drools-ruleunits-engine")));
     }
 
     public static boolean shouldValidateMVEL(String s) {
@@ -47,4 +53,3 @@ public class ExecModelModeHelper {
         return asList(YES, WITHMVEL).contains(valueOf(s.toUpperCase()));
     }
 }
-
