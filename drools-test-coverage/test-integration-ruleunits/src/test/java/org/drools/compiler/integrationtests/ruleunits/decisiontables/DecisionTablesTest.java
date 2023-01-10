6a3b81fae927798a8496343a937efc81ff1cf9be
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2023-01-10 12:28:05 +0900

    - Removed unused query - Split to 2 tests: Basic test and all-rules-coverage test - Add generated DRL as comment - Removed duplicated LoanUnit.drl.xls

--- drools-test-coverage/test-integration-ruleunits/src/test/java/org/drools/compiler/integrationtests/ruleunits/decisiontables/DecisionTablesTest.java
+++ drools-test-coverage/test-integration-ruleunits/src/test/java/org/drools/compiler/integrationtests/ruleunits/decisiontables/DecisionTablesTest.java
@@ -25,8 +25,69 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DecisionTablesTest {
 
+    // Below are rules generated from LoanUnit.drl.xls
+    /*
+    package org.drools.compiler.integrationtests.ruleunits.decisiontables;
+    unit LoanUnit;
+    import org.drools.compiler.integrationtests.ruleunits.decisiontables.LoanApplication;
+    
+    query FindApproved $l: /loanApplications[ approved ] end
+    
+    rule "LoanRule_13"
+    when
+        $l: /loanApplications[applicant.age >= 20, deposit < 1000, amount <= 2000]
+    then
+        modify($l) { setApproved(true) };
+    end
+    
+    rule "LoanRule_14"
+    when
+        $l: /loanApplications[applicant.age >= 20, deposit < 1000, amount > 2000]
+    then
+        modify($l) { setApproved(false) };
+    end
+    
+    rule "LoanRule_15"
+    when
+        $l: /loanApplications[applicant.age >= 20, deposit >= 1000, amount <= maxAmount]
+    then
+        modify($l) { setApproved(true) };
+    end
+    
+    rule "LoanRule_16"
+    when
+        $l: /loanApplications[applicant.age >= 20, deposit >= 1000, amount > maxAmount]
+    then
+        modify($l) { setApproved(false) };
+    end
+    
+    rule "LoanRule_17"
+    when
+        $l: /loanApplications[applicant.age < 20]
+    then
+        modify($l) { setApproved(false) };
+    end
+     */
+
     @Test
     public void decisionTable_basic() throws Exception {
+        LoanUnit loanUnit = new LoanUnit();
+        loanUnit.setMaxAmount(5000);
+
+        try (RuleUnitInstance<LoanUnit> instance = RuleUnitProvider.get().createRuleUnitInstance(loanUnit);) {
+            Applicant applicant = new Applicant("John", 30);
+            LoanApplication loanApplication = new LoanApplication("A001", applicant, 2000, 100);
+            loanUnit.getLoanApplications().add(loanApplication);
+
+            List<LoanApplication> queryResult = instance.executeQuery("FindApproved").toList("$l");
+
+            assertThat(queryResult).containsExactlyInAnyOrder(loanApplication);
+            assertThat(loanApplication.isApproved()).isTrue();
+        }
+    }
+
+    @Test
+    public void decisionTable_allRulesCoverage() throws Exception {
         LoanUnit loanUnit = new LoanUnit();
         loanUnit.setMaxAmount(5000);
 
@@ -34,12 +95,18 @@ public class DecisionTablesTest {
             Applicant applicant1 = new Applicant("John", 30);
             LoanApplication loanApplication1 = new LoanApplication("A001", applicant1, 2000, 100);
             loanUnit.getLoanApplications().add(loanApplication1);
-            Applicant applicant2 = new Applicant("Paul", 29);
+            Applicant applicant2 = new Applicant("Paul", 28);
             LoanApplication loanApplication2 = new LoanApplication("A002", applicant2, 5000, 100);
             loanUnit.getLoanApplications().add(loanApplication2);
             Applicant applicant3 = new Applicant("George", 27);
             LoanApplication loanApplication3 = new LoanApplication("A003", applicant3, 5000, 1000);
             loanUnit.getLoanApplications().add(loanApplication3);
+            Applicant applicant4 = new Applicant("Ringo", 30);
+            LoanApplication loanApplication4 = new LoanApplication("A004", applicant4, 8000, 1000);
+            loanUnit.getLoanApplications().add(loanApplication4);
+            Applicant applicant5 = new Applicant("Zak", 5);
+            LoanApplication loanApplication5 = new LoanApplication("A005", applicant5, 2000, 100);
+            loanUnit.getLoanApplications().add(loanApplication5);
 
             List<LoanApplication> queryResult = instance.executeQuery("FindApproved").toList("$l");
 
@@ -47,6 +114,8 @@ public class DecisionTablesTest {
             assertThat(loanApplication1.isApproved()).isTrue();
             assertThat(loanApplication2.isApproved()).isFalse();
             assertThat(loanApplication3.isApproved()).isTrue();
+            assertThat(loanApplication4.isApproved()).isFalse();
+            assertThat(loanApplication5.isApproved()).isFalse();
         }
     }
 }
