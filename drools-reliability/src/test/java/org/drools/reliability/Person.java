c38779f4d589acb87dbe2bf7f09d3bea3646a118
Mario Fusco <mario.fusco@gmail.com>
2023-01-25 10:33:44 +0100

    refactor reliability module (#4940)
    * refactor reliability module
    
    * remove no longer needed exclusion
    
    * fix CI for DROOLS-7247 branch

---
+++ drools-reliability/src/test/java/org/drools/reliability/Person.java
@@ -0,0 +1,27 @@
+package org.drools.reliability;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
