9c1c265abd458260be8b8f0839166c334078d20d
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2023-02-09 10:06:14 +0100

    [KOGITO-8377]: Merged native and mandrel checks (#1613)

--- .ci/jenkins/dsl/jobs.groovy
+++ .ci/jenkins/dsl/jobs.groovy
@@ -83,13 +83,12 @@ KogitoJobUtils.createNightlyBuildChainBuildAndDeployJobForCurrentRepo(this, '', 
 
 // Environment nightlies
 setupSpecificBuildChainNightlyJob('native')
-setupSpecificBuildChainNightlyJob('mandrel')
 
 // Jobs with integration branch
 setupQuarkusIntegrationJob('quarkus-main')
 setupQuarkusIntegrationJob('quarkus-branch')
 setupQuarkusIntegrationJob('quarkus-lts')
-setupQuarkusIntegrationJob('mandrel-lts')
+setupQuarkusIntegrationJob('native-lts')
 
 // Release jobs
 setupDeployJob(JobType.RELEASE)
