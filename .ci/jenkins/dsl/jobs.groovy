radtriste/drools

Setup Quarkus 3 migration

.ci/jenkins/dsl/jobs.groovy
@@ -81,6 +81,7 @@ setupSpecificBuildChainNightlyJob('quarkus-branch')
 setupSpecificBuildChainNightlyJob('mandrel')
 setupSpecificBuildChainNightlyJob('mandrel-lts')
 setupSpecificBuildChainNightlyJob('quarkus-lts')
+setupSpecificBuildChainNightlyJob('quarkus-3')
 
 // Jobs with integration branch
 setupSpecificNightlyJob('quarkus-main', true)

apache/incubator-kie-drools

Co-authored-by: Gabriele Cardosi <gabriele.cardosi@gmail.com>

.ci/jenkins/dsl/jobs.groovy
@@ -160,6 +160,7 @@ setupQuarkusIntegrationJob('quarkus-main')
 setupQuarkusIntegrationJob('quarkus-branch')
 setupQuarkusIntegrationJob('quarkus-lts')
 setupQuarkusIntegrationJob('mandrel-lts')
+setupQuarkusIntegrationJob('quarkus-3')
 
 // Release jobs
 setupDeployJob(JobType.RELEASE)