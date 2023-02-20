848524656f69017e355bf6948ebd0e29a37ea23d
nprentza <90831697+nprentza@users.noreply.github.com>
2023-02-20 09:50:09 +0200

    Person: hashCode, equals. ReliabilityTest: more tests (#5021)
    * Person: hashCode, equals. ReliabilityTest: more tests
    
    * testSessionFromCacheWithUpdates: update(); one call of secondSession.fireAllRules(), try/finally{dispose}

--- drools-reliability/src/test/java/org/drools/reliability/Person.java
+++ drools-reliability/src/test/java/org/drools/reliability/Person.java
@@ -46,4 +46,32 @@ public class Person implements Serializable {
     public String toString() {
         return "Person [name=" + name + ", age=" + age + "]@" + Integer.toHexString(System.identityHashCode(this));
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + age;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (age != other.age)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
 }
