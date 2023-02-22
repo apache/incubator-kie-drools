711c28129a8fdaf2c1efb65236ddce08ad6a8274
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2023-02-22 14:11:52 +0900

    - license and format

--- drools-test-coverage/test-integration-noxml/src/test/java/org/drools/compiler/integrationtests/noxml/CommandTest.java
+++ drools-test-coverage/test-integration-noxml/src/test/java/org/drools/compiler/integrationtests/noxml/CommandTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 package org.drools.compiler.integrationtests.noxml;
 
 import java.util.ArrayList;
@@ -22,10 +37,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CommandTest {
 
     @Test
-    public void kieCommands_notNull() throws Exception {
-        final String drl =
-                "rule R when\n" +
-                           "    String()\n" +
+    public void kieCommands_notNull() {
+        final String drl = "rule R\n" +
+                           "when\n" +
+                           "  String()\n" +
                            "then\n" +
                            "end\n";
 
@@ -33,8 +48,7 @@ public class CommandTest {
 
         KieHelper kHelper = new KieHelper();
 
-        KieBase kieBase = kHelper
-                                 .setKieModuleModel(kModuleModel)
+        KieBase kieBase = kHelper.setKieModuleModel(kModuleModel)
                                  .addContent(drl, ResourceType.DRL)
                                  .build(ExecutableModelProject.class);
 
@@ -48,7 +62,7 @@ public class CommandTest {
         commands.add(kieCommands.newFireAllRules("fired"));
         BatchExecutionCommand batchExecutionCommand = CommandFactory.newBatchExecution(commands);
 
-        final ExecutionResults batchResult = (ExecutionResults) kieSession.execute(batchExecutionCommand);
+        final ExecutionResults batchResult = kieSession.execute(batchExecutionCommand);
 
         assertThat(batchResult.getValue("fired")).isEqualTo(1);
     }
