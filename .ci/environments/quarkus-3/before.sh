1ec84bbd316f92d537b73b582fb1b3b75409841f
radtriste <tradisso@redhat.com>
2023-03-03 14:13:02 +0100

    Correct quarkus-3 script for downstream builds

--- .ci/environments/quarkus-3/before.sh
+++ .ci/environments/quarkus-3/before.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 script_dir_path=$(cd `dirname "${BASH_SOURCE[0]}"`; pwd -P)
 mvn_cmd="mvn ${BUILD_MVN_OPTS:-} ${BUILD_MVN_OPTS_QUARKUS_UPDATE:-}"
+ci="${CI:-false}
 
 quarkus_version=${QUARKUS_VERSION:-3.0.0.Alpha3}
 quarkus_file="${script_dir_path}/quarkus3.yml"
@@ -13,6 +14,11 @@ echo "Update project with Quarkus version ${quarkus_version}"
 set -x
 
 sed -i "s|{QUARKUS_VERSION}|${quarkus_version}|g" "${quarkus_file}"
+
+if [ "${ci}" = "true" ]; then
+    # In CI we need the main branch snapshot artifacts deployed locally
+    ${mvn_cmd} clean install -Dquickly
+fi
 
 # Change version
 ${mvn_cmd} -e -N -Dfull -DnewVersion=${project_version} -DallowSnapshots=true -DgenerateBackupPoms=false versions:set
