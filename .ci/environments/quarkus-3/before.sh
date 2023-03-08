82fa40222d1dafc5f0b0134ff93f9c7c2ec0cd2c
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2023-03-08 12:00:23 +0100

    Fix hardcoded javax strings (#5063)
    * [quarkus_patch_javax_string] Update maven-rewrite-plugin. Patching hardcoded strings
    
    * [quarkus_patch_javax_string] Fixing sed for mac / restoring quarkus3.yml after migration
    
    * [quarkus_patch_javax_string] Fixing pom for jaxb-runtime. Disabling test
    
    * [quarkus_patch_javax_string] Restoring pom
    
    * [quarkus_patch_javax_string] WIP
    
    * [quarkus_patch_javax_string] WIP
    
    * [quarkus_patch_javax_string] Restore pom. Add patch

--- .ci/environments/quarkus-3/before.sh
+++ .ci/environments/quarkus-3/before.sh
@@ -13,7 +13,7 @@ echo "Update project with Quarkus version ${quarkus_version}"
 
 set -x
 
-sed -i "s|{QUARKUS_VERSION}|${quarkus_version}|g" "${quarkus_file}"
+sed -i.bak "s|{QUARKUS_VERSION}|${quarkus_version}|g" "${quarkus_file}"
 
 if [ "${ci}" = "true" ]; then
     # In CI we need the main branch snapshot artifacts deployed locally
@@ -27,7 +27,7 @@ ${mvn_cmd} -e -N -Dfull -DnewVersion=${project_version} -DallowSnapshots=true -D
 ${mvn_cmd} clean install -Dquickly
 
 # Launch Quarkus 3 Openrewrite
-${mvn_cmd} org.openrewrite.maven:rewrite-maven-plugin:4.36.0:run \
+${mvn_cmd} org.openrewrite.maven:rewrite-maven-plugin:4.41.1:run \
     -Drewrite.configLocation="${quarkus_file}" \
     -DactiveRecipes=io.quarkus.openrewrite.Quarkus3 \
     -Drewrite.recipeArtifactCoordinates=org.kie:jpmml-migration-recipe:"${project_version}" \
@@ -38,3 +38,8 @@ ${mvn_cmd} org.openrewrite.maven:rewrite-maven-plugin:4.36.0:run \
 
 # Update dependencies with Quarkus 3 bom
 ${mvn_cmd} versions:compare-dependencies -pl :drools-build-parent -DremotePom=io.quarkus:quarkus-bom:${quarkus_version} -DupdatePropertyVersions=true -DupdateDependencies=true -DgenerateBackupPoms=false
+
+# Restore origin quarkus file
+echo "Restoring ${quarkus_file}"
+
+mv ${quarkus_file}.bak ${quarkus_file}
