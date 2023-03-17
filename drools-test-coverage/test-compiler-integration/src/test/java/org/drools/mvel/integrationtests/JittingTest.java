ab622d3726860e09bf4ff44789964a4f7a5abbda
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2023-03-17 17:08:39 +0900

    [7.67.x] [DROOLS-7366] str operator with bind variable fails after mvel jitting (#5100) (#5102)
    * [DROOLS-6983] fix EvaluatorWrapper when this is used on the right sid… (#4590)
    
    * [DROOLS-6983] fix EvaluatorWrapper when this is used on the right side of a custom operator
    
    * Update CustomOperatorTest.java
    
    * [DROOLS-7366] str operator with bind variable fails after mvel jitting
    - Unit test only
    - The issue is resolved by backporting DROOLS-6983
    
    ---------
    
    Co-authored-by: Mario Fusco <mario.fusco@gmail.com>

--- drools-test-coverage/test-compiler-integration/src/test/java/org/drools/mvel/integrationtests/JittingTest.java
+++ drools-test-coverage/test-compiler-integration/src/test/java/org/drools/mvel/integrationtests/JittingTest.java
@@ -351,4 +351,24 @@ public class JittingTest {
         kieSession.insert("CX");
         assertThat(kieSession.fireAllRules()).isEqualTo(1);
     }
+
+    @Test
+    public void strOperator() {
+        // DROOLS-7366
+        String drl =
+                "package test\n" +
+                "import " + Person.class.getCanonicalName() + "\n" +
+                "\n" +
+                "dialect \"mvel\"\n" +
+                "rule R1 when \n" +
+                "    Person( $name : name, $name str[startsWith] \"M\" )\n" +
+                "then \n" +
+                "end";
+
+        final KieModule kieModule = KieUtil.getKieModuleFromDrls("test", kieBaseTestConfiguration, drl);
+        final KieBase kieBase = KieBaseUtil.newKieBaseFromKieModuleWithAdditionalOptions(kieModule, kieBaseTestConfiguration, ConstraintJittingThresholdOption.get(0));
+        KieSession ksession = kieBase.newKieSession();
+        ksession.insert(new Person("Mario"));
+        assertThat(ksession.fireAllRules()).isEqualTo(1);
+    }
 }
