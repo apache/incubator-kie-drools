40a15cadcfad86de983597aa0648a9a98136a036
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2023-03-28 14:47:43 +0900

    Minor changes: - No longer need equals and hashCode in Person class - Enable ReliabilityTestUpdateInDrl

--- drools-reliability/src/test/java/org/drools/reliability/Person.java
+++ drools-reliability/src/test/java/org/drools/reliability/Person.java
@@ -47,31 +47,6 @@ public class Person implements Serializable {
         return "Person [name=" + name + ", age=" + age + "]@" + Integer.toHexString(System.identityHashCode(this));
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + age;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Person other = (Person) obj;
-        if (age != other.age)
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        return true;
-    }
+    // This class is a test fact without overriding equals and hashCode
+    // If we want to test a Fact with equals and hashCode, create another class
 }
