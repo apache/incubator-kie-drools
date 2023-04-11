7860c1def1abddacdbdb26da11f81b380c38fff8
Tristan Radisson <tradisso@redhat.com>
2023-04-11 11:18:54 +0200

    KOGITO-8347 Setup install quarkus script (#5081)

--- .ci/environments/common/update_quarkus.sh
+++ .ci/environments/common/update_quarkus.sh
@@ -2,12 +2,13 @@
 set -euo pipefail
 
 mvn_cmd="mvn ${BUILD_MVN_OPTS:-} ${BUILD_MVN_OPTS_QUARKUS_UPDATE:-}"
-quarkus_version="${QUARKUS_VERSION}"
+
+source <(curl -s https://raw.githubusercontent.com/kiegroup/kogito-pipelines/main/dsl/seed/scripts/install_quarkus.sh)
 
 echo "Update project with Quarkus version ${QUARKUS_VERSION}"
 
 set -x
 
 # Update with Quarkus version and commit
-${mvn_cmd} versions:compare-dependencies -pl :drools-build-parent -DremotePom=io.quarkus:quarkus-bom:${quarkus_version} -DupdatePropertyVersions=true -DupdateDependencies=true -DgenerateBackupPoms=false
-${mvn_cmd} versions:set-property -pl :drools-build-parent -Dproperty=version.io.quarkus -DnewVersion=${quarkus_version} -DgenerateBackupPoms=false -Dmaven.wagon.http.ssl.insecure=true
+${mvn_cmd} versions:compare-dependencies -pl :drools-build-parent -DremotePom=io.quarkus:quarkus-bom:${QUARKUS_VERSION} -DupdatePropertyVersions=true -DupdateDependencies=true -DgenerateBackupPoms=false
+${mvn_cmd} versions:set-property -pl :drools-build-parent -Dproperty=version.io.quarkus -DnewVersion=${QUARKUS_VERSION} -DgenerateBackupPoms=false -Dmaven.wagon.http.ssl.insecure=true
