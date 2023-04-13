2fef9d4b252d260ecede897cfbb234c7f4bfc7ce
Helber Belmiro <helber.belmiro@gmail.com>
2023-04-13 10:43:07 -0300

    KOGITO-8983 Registered ServiceDiscoveryConfigBuilder for reflection (#2927)
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- kogito-quarkus/addons/kubernetes/runtime/src/main/java/org/kie/kogito/addons/quarkus/k8s/config/ServiceDiscoveryConfigBuilder.java
+++ kogito-quarkus/addons/kubernetes/runtime/src/main/java/org/kie/kogito/addons/quarkus/k8s/config/ServiceDiscoveryConfigBuilder.java
@@ -15,9 +15,11 @@
  */
 package org.kie.kogito.addons.quarkus.k8s.config;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import io.quarkus.runtime.configuration.ConfigBuilder;
 import io.smallrye.config.SmallRyeConfigBuilder;
 
+@RegisterForReflection
 public class ServiceDiscoveryConfigBuilder implements ConfigBuilder {
 
     @Override
