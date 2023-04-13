1c0d45fbcf0ffcacddb064abe09303ae8cd52e12
Helber Belmiro <helber.belmiro@gmail.com>
2023-04-13 14:01:41 -0300

    KOGITO-8443 Added tests for Knative to property expansion (#2924)
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

---
+++ kogito-quarkus/addons/kubernetes/runtime/src/test/java/org/kie/kogito/addons/quarkus/k8s/config/KubeDiscoveryConfigCacheUpdaterTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.kogito.addons.quarkus.k8s.config;
+
+import java.net.URI;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.kie.kogito.addons.quarkus.k8s.discovery.KnativeServiceDiscovery;
+import org.kie.kogito.addons.quarkus.k8s.discovery.OpenShiftResourceDiscovery;
+import org.kie.kogito.addons.quarkus.k8s.discovery.VanillaKubernetesResourceDiscovery;
+
+import io.fabric8.kubernetes.client.server.mock.KubernetesServer;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.kubernetes.client.KubernetesTestServer;
+import io.quarkus.test.kubernetes.client.WithKubernetesTestServer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.kie.kogito.addons.quarkus.k8s.test.utils.KnativeResourceDiscoveryTestUtil.createServiceIfNotExists;
+
+@QuarkusTest
+@WithKubernetesTestServer
+class KubeDiscoveryConfigCacheUpdaterTest {
+
+    @KubernetesTestServer
+    static KubernetesServer mockServer;
+
+    KubeDiscoveryConfigCacheUpdater kubeDiscoveryConfigCacheUpdater;
+
+    final String remoteServiceUrl = "http://serverless-workflow-greeting-quarkus.test.10.99.154.147.sslip.io";
+
+    @Inject
+    VanillaKubernetesResourceDiscovery vanillaDiscovery;
+
+    @Inject
+    OpenShiftResourceDiscovery openShiftDiscovery;
+
+    @Inject
+    KnativeServiceDiscovery knativeDiscovery;
+
+    @BeforeEach
+    void beforeEach() {
+        createServiceIfNotExists(mockServer, remoteServiceUrl, "knative/quarkus-greeting.yaml", "test", "serverless-workflow-greeting-quarkus");
+        kubeDiscoveryConfigCacheUpdater = new KubeDiscoveryConfigCacheUpdater(vanillaDiscovery, openShiftDiscovery, knativeDiscovery);
+    }
+
+    @Test
+    void knativeService() {
+        assertThat(kubeDiscoveryConfigCacheUpdater.update("knative:test/serverless-workflow-greeting-quarkus"))
+                .hasValue(URI.create(remoteServiceUrl));
+    }
+
+    @Test
+    void knativeResource() {
+        assertThat(kubeDiscoveryConfigCacheUpdater.update("knative:serving.knative.dev/v1/Service/test/serverless-workflow-greeting-quarkus"))
+                .hasValue(URI.create(remoteServiceUrl));
+    }
+}
