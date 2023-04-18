064ca4a788408a16205106c74807a4706c45f9b6
Mauro Vannucci <vannux@gmail.com>
2023-04-18 20:36:16 +0200

    #2745 spring configure consumer for SSL with Kafka (#2746)
    * fix #2745 spring: consumer SSL with Kafka
    
    * KOGITO-2745 fix for property value not set
    
    * Used bean KafkaProperties to produce the ConsumerFactory
    
    * Add missing imports
    
    * Update SpringKafkaConsumerConfig.java
    Update SpringKafkaConsumerConfig.java
    Update SpringKafkaConsumerConfig.java
    Update SpringKafkaConsumerConfig.java
    Update SpringKafkaConsumerConfig.java
    Update SpringKafkaConsumerConfig.java
    
    ---------
    
    Co-authored-by: Mauro Vannucci <m.vannucci@liguriadigitale.it>
    Co-authored-by: Federico Turbino <f.turbino@liguriadigitale.it>
    Co-authored-by: Francisco Javier Tirado Sarti <65240126+fjtirado@users.noreply.github.com>

--- kogito-springboot/addons/messaging/implementation/src/main/java/org/kie/kogito/addon/cloudevents/spring/SpringKafkaConsumerConfig.java
+++ kogito-springboot/addons/messaging/implementation/src/main/java/org/kie/kogito/addon/cloudevents/spring/SpringKafkaConsumerConfig.java
@@ -15,14 +15,9 @@
  */
 package org.kie.kogito.addon.cloudevents.spring;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.kafka.DefaultKafkaConsumerFactoryCustomizer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -34,31 +29,18 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 @Configuration
 public class SpringKafkaConsumerConfig {
 
-    @Value(value = "${spring.kafka.bootstrap-servers}")
-    String kafkaBootstrapAddress;
-    @Value(value = "${spring.kafka.consumer.group-id}")
-    String groupId;
-    @Value(value = "${spring.kafka.consumer.auto-offset-reset}")
-    String offset;
-
-    private static final Logger logger = LoggerFactory.getLogger(SpringKafkaConsumerConfig.class);
-
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
-        logger.info("Creating consumer factory with bootstrap {} and groupid {}", kafkaBootstrapAddress, groupId);
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapAddress);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, offset);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props);
+    public ConsumerFactory<String, String> consumerFactory(KafkaProperties properties, ObjectProvider<DefaultKafkaConsumerFactoryCustomizer> customizers) {
+        DefaultKafkaConsumerFactory<String, String> factory = new DefaultKafkaConsumerFactory<>(
+                properties.buildConsumerProperties());
+        customizers.orderedStream().forEach(customizer -> customizer.customize(factory));
+        return factory;
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(ConsumerFactory<String, String> consumerFactory) {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
+        factory.setConsumerFactory(consumerFactory);
         factory.setBatchListener(true);
         return factory;
     }
