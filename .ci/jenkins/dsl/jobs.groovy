5688344a774f44062a261fd91e640fe35a47b77b
Tristan Radisson <tradisso@redhat.com>
2023-04-20 09:13:29 +0200

    Quarkus3: Remove PR checks until stable (#1703)

--- .ci/jenkins/dsl/jobs.groovy
+++ .ci/jenkins/dsl/jobs.groovy
@@ -62,7 +62,8 @@ Map getMultijobPRConfig(JenkinsFolder jobFolder) {
 
     // For Quarkus 3, run only runtimes PR check... for now
     if (EnvUtils.hasEnvironmentId(this, jobFolder.getEnvironmentName(), 'quarkus3')) {
-        jobConfig.jobs.retainAll { it.id == 'kogito-apps' }
+        // jobConfig.jobs.retainAll { it.id == 'kogito-apps' } // TODO uncomment once quarkus-3 env is stable
+        jobConfig.jobs.clear()
     }
 
     return jobConfig
