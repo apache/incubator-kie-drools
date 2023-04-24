426f4f52fc03ced634f7b2c04c02ea78d96d3201
Paulo Martins <paulovmr@gmail.com>
2023-04-24 17:42:51 -0300

    KOGITO-8909: [SWF Dev UI] Minimize resources on prod profile (#1679)
    * KOGITO-8909: [SWF Dev UI] Minimize resources on prod profile
    
    * KOGITO-8909: [SWF Dev UI] Minimize resources on prod profile

--- ui-packages/packages/runtime-tools-dev-ui-webapp/webpack.prod.js
+++ ui-packages/packages/runtime-tools-dev-ui-webapp/webpack.prod.js
@@ -2,7 +2,6 @@ const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
@@ -13,7 +12,7 @@ module.exports = merge(common, {
     publicPath: "./webapp/"
   },
   optimization: {
-    minimizer: [new OptimizeCSSAssetsPlugin({})]
+    minimize: true
   },
   plugins: [
     new MiniCssExtractPlugin({
