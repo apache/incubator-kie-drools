5dc870c8b5b3ada97963c80f2befe55d1db1e36e
radtriste <tradisso@redhat.com>
2023-05-04 20:32:24 +0200

    updated after script

--- .ci/environments/quarkus-3/after.sh
+++ .ci/environments/quarkus-3/after.sh
@@ -4,7 +4,9 @@ set -euo pipefail
 script_dir_path=$(cd `dirname "${BASH_SOURCE[0]}"`; pwd -P)
 mvn_cmd="mvn ${BUILD_MVN_OPTS:-} ${BUILD_MVN_OPTS_QUARKUS_UPDATE:-}"
 
-project_version=$(${mvn_cmd} -q -Dexpression=project.version -DforceStdout help:evaluate)
+# Retrieve current Maven project version
+project_version=$(mvn -q -Dexpression=project.version -DforceStdout help:evaluate)
+# New version is based on current project version and increment the Major => (M+1).m.y
 new_version=$(echo ${project_version} | awk -F. -v OFS=. '{$1 += 1 ; print}')
 
 # Change version
