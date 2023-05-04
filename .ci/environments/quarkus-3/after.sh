95fb05c24a88a24161aea8317d153f92161f27dc
radtriste <tradisso@redhat.com>
2023-05-04 10:49:01 +0200

    update set version

--- .ci/environments/quarkus-3/after.sh
+++ .ci/environments/quarkus-3/after.sh
@@ -3,7 +3,9 @@ set -euo pipefail
 
 script_dir_path=$(cd `dirname "${BASH_SOURCE[0]}"`; pwd -P)
 mvn_cmd="mvn ${BUILD_MVN_OPTS:-} ${BUILD_MVN_OPTS_QUARKUS_UPDATE:-}"
-project_version='quarkus-3-SNAPSHOT'
+
+project_version=$(${mvn_cmd} -q -Dexpression=project.version -DforceStdout help:evaluate)
+new_version=$(echo ${project_version} | awk -F. -v OFS=. '{$1 += 1 ; print}')
 
 # Change version
-${mvn_cmd} -e -N -Dfull -DnewVersion=${project_version} -DallowSnapshots=true -DgenerateBackupPoms=false versions:set
+${mvn_cmd} -e -N -Dfull -DnewVersion=${new_version} -DallowSnapshots=true -DgenerateBackupPoms=false versions:set
