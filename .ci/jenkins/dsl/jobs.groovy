237c5bdd8fae2423b26cb84e92ab85309ccabec2
Tristan Radisson <tristan.radisson@gmail.com>
2023-05-04 20:04:08 +0200

    Quarkus 3: Update script to 3.0.0.Final (#2969)
    * Quarkus 3: Update script to 3.0.0.Final
    
    * correct jenkins jobs
    
    * [Quarkus 3 migration] Update Openrewrite recipe
    
    * [Quarkus 3 migration] Updated Openrewrite patch
    
    * update patch
    
    ---------
    
    Co-authored-by: radtriste <radtriste@jenkins.redhat>
    Co-authored-by: Jenkins CI <noreply@redhat.com>

--- .ci/jenkins/dsl/jobs.groovy
+++ .ci/jenkins/dsl/jobs.groovy
@@ -271,6 +271,7 @@ void setupPrQuarkus3RewriteJob() {
     ])
     jobParams.env.putAll([
         AUTHOR_CREDS_ID: "${GIT_AUTHOR_CREDENTIALS_ID}",
+        MAVEN_SETTINGS_CONFIG_FILE_ID: "${MAVEN_SETTINGS_FILE_ID}",
     ])
     KogitoJobTemplate.createPRJob(this, jobParams)
 }
@@ -283,6 +284,7 @@ void setupStandaloneQuarkus3RewriteJob() {
         JENKINS_EMAIL_CREDS_ID: "${JENKINS_EMAIL_CREDS_ID}",
         BASE_BRANCH: Utils.getGitBranch(this),
         BASE_AUTHOR: Utils.getGitAuthor(this),
+        MAVEN_SETTINGS_CONFIG_FILE_ID: "${MAVEN_SETTINGS_FILE_ID}",
     ])
     KogitoJobTemplate.createPipelineJob(this, jobParams)?.with {
         parameters {
