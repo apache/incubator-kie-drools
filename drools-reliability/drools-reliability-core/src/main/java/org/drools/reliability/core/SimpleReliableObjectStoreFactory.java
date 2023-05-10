75358e034623b1360e01f533d0cc6a1a2c09fdee
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2023-05-10 16:57:34 +0900

    - Add headers

--- drools-reliability/drools-reliability-core/src/main/java/org/drools/reliability/core/SimpleReliableObjectStoreFactory.java
+++ drools-reliability/drools-reliability-core/src/main/java/org/drools/reliability/core/SimpleReliableObjectStoreFactory.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.drools.reliability.core;
 
 import org.drools.core.common.Storage;
@@ -8,10 +23,11 @@ public interface SimpleReliableObjectStoreFactory extends KieService {
     SimpleReliableObjectStore createSimpleReliableObjectStore(Storage<Long, StoredObject> storage);
 
     class Holder {
+
         private static final SimpleReliableObjectStoreFactory INSTANCE = createInstance();
 
         static SimpleReliableObjectStoreFactory createInstance() {
-            SimpleReliableObjectStoreFactory factory = KieService.load(SimpleReliableObjectStoreFactory.class );
+            SimpleReliableObjectStoreFactory factory = KieService.load(SimpleReliableObjectStoreFactory.class);
             if (factory == null) {
                 return new SimpleSerializationReliableObjectStoreFactory();
             }
@@ -22,5 +38,4 @@ public interface SimpleReliableObjectStoreFactory extends KieService {
     static SimpleReliableObjectStoreFactory get() {
         return SimpleReliableObjectStoreFactory.Holder.INSTANCE;
     }
-
 }
