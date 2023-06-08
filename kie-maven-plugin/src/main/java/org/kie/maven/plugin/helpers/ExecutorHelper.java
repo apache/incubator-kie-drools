eaa5af6ab417556107874e9721e4358aaecbc2b3
Matteo Mortari <matteo.mortari@gmail.com>
2023-06-08 08:59:23 +0200

    DROOLS-7463 revising `drools.dialect.java.compiler.lnglevel` in the kie-maven-plugin (#5283)
    * DROOLS-7463 revising `drools.dialect.java.compiler.lnglevel` in the kie-maven-plugin
    
    * Update log warn statement
    
    Co-authored-by: dupliaka <adupliak@redhat.com>
    
    ---------
    
    Co-authored-by: dupliaka <adupliak@redhat.com>

--- kie-maven-plugin/src/main/java/org/kie/maven/plugin/helpers/ExecutorHelper.java
+++ kie-maven-plugin/src/main/java/org/kie/maven/plugin/helpers/ExecutorHelper.java
@@ -21,6 +21,7 @@ import java.util.stream.Collectors;
 
 import org.apache.maven.plugin.logging.Log;
 import org.drools.compiler.kie.builder.impl.InternalKieModule;
+import org.kie.memorycompiler.JavaConfiguration;
 
 public class ExecutorHelper {
 
@@ -32,6 +33,12 @@ public class ExecutorHelper {
         if (properties != null) {
             log.debug("Additional system properties: " + properties);
             for (Map.Entry<String, String> property : properties.entrySet()) {
+                if (property.getKey().equals(JavaConfiguration.JAVA_LANG_LEVEL_PROPERTY)) {
+                    log.warn("It seems you are setting `" + 
+                            JavaConfiguration.JAVA_LANG_LEVEL_PROPERTY + 
+                            "` while building a KJAR in a Maven-based build." +
+                            " It is recommended to properly set `maven.compiler.release` instead.");
+                }
                 System.setProperty(property.getKey(), property.getValue());
             }
             log.debug("Configured system properties were successfully set.");
