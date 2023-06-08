0d39fae63ac15ef13970a6af403a2eca9ea9b127
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2023-06-08 17:26:40 +0900

    Disable when no docker

--- drools-reliability/drools-reliability-infinispan/src/test/java/org/drools/reliability/infinispan/smoke/proto/ProtoSmokeTest.java
+++ drools-reliability/drools-reliability-infinispan/src/test/java/org/drools/reliability/infinispan/smoke/proto/ProtoSmokeTest.java
@@ -19,10 +19,12 @@ import org.drools.reliability.infinispan.BeforeAllMethodExtension;
 import org.drools.reliability.infinispan.smoke.BaseSmokeTest;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.condition.OS.WINDOWS;
 
 @DisabledOnOs(WINDOWS)
+@Testcontainers(disabledWithoutDocker = true)
 @ExtendWith(BeforeAllMethodExtension.class)
 class ProtoSmokeTest extends BaseSmokeTest {
 
