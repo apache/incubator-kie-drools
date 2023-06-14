2b4c82924ba3a38a4100dcc01d5c0620e3db388c
Cristiano Nicolai <570894+cristianonicolai@users.noreply.github.com>
2023-06-14 22:47:57 +1000

    Upgrade surefire and set test timeout (#3052)
    * Upgrade surefire and set test timeout
    
    * Disable devservice for codestart test
    
    * Disable devservices
    
    * Disable devservice
    
    * Use random port for testing

--- kogito-quarkus/extensions/kogito-quarkus-serverless-workflow-extension/kogito-quarkus-serverless-workflow-integration-test/src/test/java/org/kie/kogito/quarkus/ServerlessWorkflowCodestartTest.java
+++ kogito-quarkus/extensions/kogito-quarkus-serverless-workflow-extension/kogito-quarkus-serverless-workflow-integration-test/src/test/java/org/kie/kogito/quarkus/ServerlessWorkflowCodestartTest.java
@@ -16,9 +16,12 @@
 
 package org.kie.kogito.quarkus;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import io.quarkus.devtools.codestarts.quarkus.QuarkusCodestartData;
 import io.quarkus.devtools.testing.codestarts.QuarkusCodestartTest;
 import io.quarkus.maven.dependency.ArtifactKey;
 
@@ -30,6 +33,7 @@ public class ServerlessWorkflowCodestartTest {
     public static QuarkusCodestartTest codestartTest = QuarkusCodestartTest.builder()
             .setupStandaloneExtensionTest("org.kie.kogito:kogito-quarkus-serverless-workflow")
             .extension(ArtifactKey.fromString("io.quarkus:quarkus-config-yaml"))
+            .putData(QuarkusCodestartData.QuarkusDataKey.APP_CONFIG, Map.of("quarkus.devservices.enabled", "false"))
             .languages(JAVA)
             .build();
 
