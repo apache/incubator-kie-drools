23500beb485d2ef600a92f1da61e6bb9c837df2a
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2023-07-24 18:05:36 +0900

    - invalidate multiple named rule units test

--- drools-ruleunits/drools-ruleunits-dsl/src/test/java/org/drools/ruleunits/dsl/RuleUnitRebuildTest.java
+++ drools-ruleunits/drools-ruleunits-dsl/src/test/java/org/drools/ruleunits/dsl/RuleUnitRebuildTest.java
@@ -48,4 +48,34 @@ class RuleUnitRebuildTest {
             assertThat(newUnit.getResults()).containsExactly("it worked!");
         }
     }
+
+    @Test
+    void invalidateMultipleNamedRuleUnits() {
+        NamedHelloWorldUnit unit1 = new NamedHelloWorldUnit("Name-1");
+        unit1.getStrings().add("Name-1");
+
+        try (RuleUnitInstance<NamedHelloWorldUnit> unitInstance = RuleUnitProvider.get().createRuleUnitInstance(unit1)) {
+            assertThat(unitInstance.fire()).isEqualTo(1);
+            assertThat(unit1.getResults()).containsExactly("it worked!");
+        }
+
+        NamedHelloWorldUnit unit2 = new NamedHelloWorldUnit("Name-2");
+        unit2.getStrings().add("Name-2");
+
+        try (RuleUnitInstance<NamedHelloWorldUnit> unitInstance = RuleUnitProvider.get().createRuleUnitInstance(unit2)) {
+            assertThat(unitInstance.fire()).isEqualTo(1);
+            assertThat(unit2.getResults()).containsExactly("it worked!");
+        }
+
+        int invalidated = RuleUnitProvider.get().invalidateRuleUnits(NamedHelloWorldUnit.class);
+        assertThat(invalidated).as("Invalidate 2 rule units").isEqualTo(2);
+
+        NamedHelloWorldUnit unit3 = new NamedHelloWorldUnit("Name-3");
+        unit3.getStrings().add("Name-3");
+
+        try (RuleUnitInstance<NamedHelloWorldUnit> newUnitInstance = RuleUnitProvider.get().createRuleUnitInstance(unit3)) {
+            assertThat(newUnitInstance.fire()).isEqualTo(1);
+            assertThat(unit3.getResults()).containsExactly("it worked!");
+        }
+    }
 }
