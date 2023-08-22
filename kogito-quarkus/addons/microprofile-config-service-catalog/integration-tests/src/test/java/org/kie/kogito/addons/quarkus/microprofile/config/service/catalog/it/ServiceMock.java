bc1df2080fff16a9224c47b55a9e049fa58eeda2
Helber Belmiro <helber.belmiro@gmail.com>
2023-08-22 09:17:39 -0300

    KOGITO-9145 Create an implementation of the Service Discovery catalog based on MicroProfile Config (#3179)
    * [KOGITO-9145] Create an implementation of the Service Discovery catalog based on MicroProfile Config
    
    * [KOGITO-9145] - Create an implementation of the Service Discovery catalog based on MicroProfile Config
    
    * [KOGITO-9145] - Fixed sonar issues
    
    * [KOGITO-9145]- Updated as per comments
    
    * [KOGITO-9145] Removed extra space
    
    * [KOGITO-9145] formatted file
    
    * KOGITO-9145 Added prefix to config property and added integration test
    
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>
    
    * KOGITO-9145 Added dynamic port
    
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>
    
    * KOGITO-9145 Disabled dev services
    
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>
    
    ---------
    
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>
    Co-authored-by: Vani Haripriya Mudadla <vmudadla@redhat.com>

---
+++ kogito-quarkus/addons/microprofile-config-service-catalog/integration-tests/src/test/java/org/kie/kogito/addons/quarkus/microprofile/config/service/catalog/it/ServiceMock.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.kogito.addons.quarkus.microprofile.config.service.catalog.it;
+
+import java.net.HttpURLConnection;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+
+public class ServiceMock implements QuarkusTestResourceLifecycleManager {
+
+    private WireMockServer wireMockServer;
+
+    @Override
+    public Map<String, String> start() {
+        wireMockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort());
+        wireMockServer.start();
+
+        wireMockServer.stubFor(post(urlEqualTo("/emptyParamsKnativeFunction"))
+                .willReturn(aResponse()
+                        .withStatus(HttpURLConnection.HTTP_OK)
+                        .withHeader("Content-Type", "application/json")
+                        .withJsonBody(JsonNodeFactory.instance.objectNode()
+                                .put("org", "Acme")
+                                .put("project", "Kogito"))));
+
+        return Map.of("serverMockUrl", wireMockServer.baseUrl());
+    }
+
+    @Override
+    public void stop() {
+        if (wireMockServer != null) {
+            wireMockServer.stop();
+        }
+    }
+}
---
+++ kogito-quarkus/addons/microprofile-config-service-catalog/runtime/src/test/java/org/kie/kogito/addons/quarkus/microprofile/config/service/catalog/MicroProfileConfigServiceCatalogTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.kogito.addons.quarkus.microprofile.config.service.catalog;
+
+import javax.inject.Inject;
+
+import org.kie.kogito.addons.k8s.resource.catalog.KubernetesServiceCatalogTest;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class MicroProfileConfigServiceCatalogTest extends KubernetesServiceCatalogTest {
+
+    @Inject
+    MicroProfileConfigServiceCatalogTest(MicroProfileConfigServiceCatalog configServiceCatalog) {
+        super(configServiceCatalog);
+    }
+
+}
