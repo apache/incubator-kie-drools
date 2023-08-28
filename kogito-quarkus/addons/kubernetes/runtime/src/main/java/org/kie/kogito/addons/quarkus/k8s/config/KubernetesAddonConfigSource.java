7b0425d7f91001aedace98486be47feb18a7d411
Helber Belmiro <helber.belmiro@gmail.com>
2023-08-28 09:41:10 -0300

    KOGITO-8749 Disabled Kubernetes Dev Services by default (#3169)
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

---
+++ kogito-quarkus/addons/kubernetes/runtime/src/main/java/org/kie/kogito/addons/quarkus/k8s/config/KubernetesAddonConfigSource.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.kogito.addons.quarkus.k8s.config;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.microprofile.config.spi.ConfigSource;
+
+import io.quarkus.runtime.annotations.StaticInitSafe;
+
+@StaticInitSafe
+public class KubernetesAddonConfigSource implements ConfigSource {
+
+    private static final Map<String, String> configuration = Map.of(
+            "quarkus.kubernetes-client.devservices.enabled", "false");
+
+    @Override
+    public Set<String> getPropertyNames() {
+        return configuration.keySet();
+    }
+
+    @Override
+    public String getValue(String propertyName) {
+        return configuration.get(propertyName);
+    }
+
+    @Override
+    public String getName() {
+        return KubernetesAddonConfigSource.class.getSimpleName();
+    }
+}
