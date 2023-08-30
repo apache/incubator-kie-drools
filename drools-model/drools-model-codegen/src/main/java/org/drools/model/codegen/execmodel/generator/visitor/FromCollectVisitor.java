0382b9186966956d08e54ba3d4bb7f155ceb2292
Mario Fusco <mario.fusco@gmail.com>
2023-08-30 10:33:49 +0200

    [DROOLS-7534] implement collect on custom Collection classes in executable model (#5493)

--- drools-model/drools-model-codegen/src/main/java/org/drools/model/codegen/execmodel/generator/visitor/FromCollectVisitor.java
+++ drools-model/drools-model-codegen/src/main/java/org/drools/model/codegen/execmodel/generator/visitor/FromCollectVisitor.java
@@ -24,28 +24,46 @@ import org.drools.drl.ast.descr.PatternDescr;
 
 public class FromCollectVisitor {
 
+    public static final String GENERIC_COLLECT = "genericCollect";
+
     private final ModelGeneratorVisitor parentVisitor;
 
     public FromCollectVisitor(ModelGeneratorVisitor parentVisitor) {
         this.parentVisitor = parentVisitor;
     }
 
-    public void trasformFromCollectToCollectList(PatternDescr pattern, CollectDescr collectDescr) {
+    public void transformFromCollectToCollectList(PatternDescr pattern, CollectDescr collectDescr) {
         // The inner pattern of the "from collect" needs to be processed to have the binding
         final PatternDescr collectDescrInputPattern = collectDescr.getInputPattern();
         if (!parentVisitor.initPattern( collectDescrInputPattern )) {
             return;
         }
 
+        String collectTarget = pattern.getObjectType();
+
         final AccumulateDescr accumulateDescr = new AccumulateDescr();
         accumulateDescr.setInputPattern(collectDescrInputPattern);
-        accumulateDescr.addFunction("collectList", null, false, new String[]{collectDescrInputPattern.getIdentifier()});
+        accumulateDescr.addFunction(getCollectFunction(collectTarget), null, false, new String[]{collectDescrInputPattern.getIdentifier()});
 
-        final PatternDescr transformedPatternDescr = new PatternDescr(pattern.getObjectType(), pattern.getIdentifier());
+        final PatternDescr transformedPatternDescr = new PatternDescr(collectTarget, pattern.getIdentifier());
         for (BaseDescr o : pattern.getConstraint().getDescrs()) {
             transformedPatternDescr.addConstraint(o);
         }
         transformedPatternDescr.setSource(accumulateDescr);
         transformedPatternDescr.accept(parentVisitor);
+    }
+
+    private static String getCollectFunction(String collectTarget) {
+        switch (collectTarget) {
+            case "Collection":
+            case "java.util.Collection":
+            case "List":
+            case "java.util.List":
+                return "collectList";
+            case "Set":
+            case "java.util.Set":
+                return "collectSet";
+            default: return GENERIC_COLLECT;
+        }
     }
 }
