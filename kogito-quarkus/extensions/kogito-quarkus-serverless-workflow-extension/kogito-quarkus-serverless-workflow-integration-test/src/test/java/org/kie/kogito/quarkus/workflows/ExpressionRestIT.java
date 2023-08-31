278cd4c7a425b29293133781de9576869b178820
Francisco Javier Tirado Sarti <65240126+fjtirado@users.noreply.github.com>
2023-08-31 18:12:14 +0200

    [KOGITO-9650] Adding identity to SWF workflow (#3201)

--- kogito-quarkus/extensions/kogito-quarkus-serverless-workflow-extension/kogito-quarkus-serverless-workflow-integration-test/src/test/java/org/kie/kogito/quarkus/workflows/ExpressionRestIT.java
+++ kogito-quarkus/extensions/kogito-quarkus-serverless-workflow-extension/kogito-quarkus-serverless-workflow-integration-test/src/test/java/org/kie/kogito/quarkus/workflows/ExpressionRestIT.java
@@ -42,6 +42,7 @@ class ExpressionRestIT {
                 .body("workflowdata.result", is(4))
                 .body("workflowdata.number", nullValue())
                 .body("workflowdata.message", is("my name is javierito and in my native language dog is translated to perro and the header pepe is pepa"))
+                .body("workflowdata.user", is("anonymous"))
                 .body("workflowdata.discardedResult", nullValue());
     }
 
---
+++ kogito-quarkus/extensions/kogito-quarkus-serverless-workflow-extension/kogito-quarkus-serverless-workflow/src/main/java/org/kie/kogito/serverless/workflow/QuarkusKogitoProcessContextResolver.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.kogito.serverless.workflow;
+
+import java.util.Map;
+import java.util.function.Function;
+
+import org.kie.kogito.internal.process.runtime.KogitoProcessContext;
+import org.kie.kogito.serverless.workflow.utils.KogitoProcessContextResolverExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.quarkus.arc.Arc;
+import io.quarkus.security.identity.SecurityIdentity;
+
+public class QuarkusKogitoProcessContextResolver implements KogitoProcessContextResolverExtension {
+
+    private static final Logger logger = LoggerFactory.getLogger(QuarkusKogitoProcessContextResolver.class);
+
+    @Override
+    public Map<String, Function<KogitoProcessContext, Object>> getKogitoProcessContextResolver() {
+        return Map.of("identity", this::resolveInitiator);
+    }
+
+    private String resolveInitiator(KogitoProcessContext context) {
+        try {
+            SecurityIdentity identity = Arc.container().select(SecurityIdentity.class).get();
+            return identity.isAnonymous() ? "anonymous" : identity.getPrincipal().getName();
+        } catch (RuntimeException ex) {
+            logger.warn("Unable to resolve quarkus user identity", ex);
+            return null;
+        }
+    }
+
+}
