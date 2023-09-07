2aa0cd5f3c9c20550afc5d540568c66d24a4d191
Eder Ignatowicz <ignatowicz@gmail.com>
2023-09-07 08:13:25 -0400

    kie-issues#519: Replace copyright headers/comments on optaplanner as part of the migration to Apache #519 (#2949)
    * kie-issues#519: Replace copyright headers/comments on optaplanner as part of the migration to Apache #519
    
    * kie-issues#519: Replace copyright headers/commends fixing tests
    
    * kie-issues#519: Replace copyright headers/commends fixing tests

--- optaplanner-benchmark/src/main/resources/org/optaplanner/benchmark/impl/report/twitterbootstrap/js/jquery.js
+++ optaplanner-benchmark/src/main/resources/org/optaplanner/benchmark/impl/report/twitterbootstrap/js/jquery.js
@@ -1,15 +1,20 @@
-/*!
- * jQuery JavaScript Library v1.9.1
- * http://jquery.com/
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Includes Sizzle.js
- * http://sizzlejs.com/
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Copyright 2005, 2012 jQuery Foundation, Inc. and other contributors
- * Released under the MIT license
- * http://jquery.org/license
- *
- * Date: 2013-2-4
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 (function( window, undefined ) {
 
