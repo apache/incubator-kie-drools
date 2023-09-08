18047e93c81c3502cd759fbe3e8ce0c8f2e796f4
Eduardo Cerqueira <eduardomcerqueira@gmail.com>
2023-09-08 03:13:51 -0400

    kie-issues-531 updating header files for ASF (#5503)
    * kie-issues-531 prep for replacing file copyrights for moving to ASF
    
    * kie-issues-531 part-2 prep for replacing file copyrights for moving to ASF
    added comments back with ASF recommended header
    
    * kie-issues-531 part-3 prep for replacing file copyrights for moving to ASF
    removing mvn license plugin from pom files, restoring to the original version and deleting the static license-header.txt file, also used only during the header updates
    
    * fix https://github.com/kiegroup/kie-issues/issues/531
    
    * resolving merge conflict for PR https://github.com/kiegroup/drools/pull/5503
    fix https://github.com/kiegroup/kie-issues/issues/531
    
    * removed headers that were making tests unstable

--- drools-model/drools-mvel-parser/src/main/java/org/drools/mvel/parser/JavaToken.java
+++ drools-model/drools-mvel-parser/src/main/java/org/drools/mvel/parser/JavaToken.java
@@ -1,27 +1,21 @@
-/*
- * Copyright (C) 2007-2010 Júlio Vilmar Gesser.
- * Copyright (C) 2011, 2013-2016 The JavaParser Team.
- * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * This file is part of JavaParser.
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * JavaParser can be used either under the terms of
- * a) the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- * b) the terms of the Apache License
- *
- * You should have received a copy of both licenses in LICENCE.LGPL and
- * LICENCE.APACHE. Please refer to those files for details.
- *
- * JavaParser is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * Modified by Red Hat, Inc.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-
 package org.drools.mvel.parser;
 
 import java.util.Optional;
