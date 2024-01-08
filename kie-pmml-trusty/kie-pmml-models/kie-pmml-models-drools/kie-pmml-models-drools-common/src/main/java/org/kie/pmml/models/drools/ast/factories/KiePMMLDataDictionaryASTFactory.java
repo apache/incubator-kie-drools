5e567c105723919e4fdf0117faa5aa9f2d2890bb
BAMOE CI <bamoe.ci@ibm.com>
2024-01-08 14:52:44 +0100

    [incubator-kie-issues#758] Replace (T/t)upla with (T/t)uple

--- kie-pmml-trusty/kie-pmml-models/kie-pmml-models-drools/kie-pmml-models-drools-common/src/main/java/org/kie/pmml/models/drools/ast/factories/KiePMMLDataDictionaryASTFactory.java
+++ kie-pmml-trusty/kie-pmml-models/kie-pmml-models-drools/kie-pmml-models-drools-common/src/main/java/org/kie/pmml/models/drools/ast/factories/KiePMMLDataDictionaryASTFactory.java
@@ -42,7 +42,7 @@ public class KiePMMLDataDictionaryASTFactory {
 
     /**
      * @param fieldTypeMap the <code>Map&lt;String, KiePMMLOriginalTypeGeneratedType&gt;</code> to be populated with
-     * mapping between original field' name and <b>original type/generated type</b> tupla
+     * mapping between original field' name and <b>original type/generated type</b> tuple
      * @return
      */
     public static KiePMMLDataDictionaryASTFactory factory(final Map<String, KiePMMLOriginalTypeGeneratedType> fieldTypeMap) {
@@ -52,7 +52,7 @@ public class KiePMMLDataDictionaryASTFactory {
     /**
      * Create a <code>List&lt;KiePMMLDroolsType&gt;</code> out of original <code>Field</code>s,
      * and <b>populate</b> the <b>fieldNameTypeNameMap</b> with mapping between original field' name and <b>original
-     * type/generated type</b> tupla
+     * type/generated type</b> tuple
      * @param fields
      */
     public List<KiePMMLDroolsType> declareTypes(final List<Field<?>> fields) {
@@ -62,7 +62,7 @@ public class KiePMMLDataDictionaryASTFactory {
     /**
      * Create a <code>KiePMMLDroolsType</code> out of original <code>DataField</code>,
      * and <b>populate</b> the <b>fieldNameTypeNameMap</b> with mapping between original field' name and <b>original
-     * type/generated type</b> tupla
+     * type/generated type</b> tuple
      * @param field
      */
     public KiePMMLDroolsType declareType(Field field) {
