1a0507259dbd4b1da509e8f4be80b3e349eb70b6
Roberto Oliveira <robertoguimaraes8@gmail.com>
2024-01-24 15:58:26 +0100

    [SRVLOGIC-211] add script to remove modules that should not be in the productized build (#11)

---
+++ productized/remove_modules.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# This script expects a configuration file separated by semicolon, where the first part is the pom and the second the modules to be removed from that pom
+# It can be invoked from project root folder using: $ ./productized/remove_modules.sh ./productized/modules
+
+# fast fail
+set -e
+set -o pipefail
+
+if [ $# -lt 1 ];
+then
+  echo "$0: Missing arguments"
+  exit
+fi
+
+config_file="${1}"
+for line in `cat "$config_file"`
+do
+    pom=$(echo $line | cut -d ';' -f1)
+    modules=$(echo $line | cut -d ';' -f2)
+
+    modules_list=$(echo $modules | tr "," "\n")
+    for module in ${modules_list}
+    do
+        echo "Removing module ${module} from ${pom}"
+        sed -i "/<module>${module}<\/module/d" ${pom}
+    done
+done
