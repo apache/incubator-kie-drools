0a2e9f54de55acc505e0a3e8bfec4e7c5f91c207
Gabriele-Cardosi <gabriele.cardosi@ibm.com>
2024-02-16 12:16:02 +0100

    [incubator-kie-issues#926] Pull upward typeConstraint methods to provide default implementation

--- kie-dmn/kie-dmn-model/src/main/java/org/kie/dmn/model/v1_2/TItemDefinition.java
+++ kie-dmn/kie-dmn-model/src/main/java/org/kie/dmn/model/v1_2/TItemDefinition.java
@@ -60,12 +60,12 @@ public class TItemDefinition extends TNamedElement implements ItemDefinition {
 
     @Override
     public UnaryTests getTypeConstraint() {
-        throw new UnsupportedOperationException("Since DMNv1.5");
+        return null;
     }
 
     @Override
     public void setTypeConstraint(UnaryTests value) {
-        throw new UnsupportedOperationException("Since DMNv1.5");
+        // no op
     }
 
     @Override
