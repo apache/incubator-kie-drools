d84b6354ba7ec817b4177d4d4289c6e98c5f8444
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2024-02-27 10:48:44 +0100

    [incubator-kie-issues#968] Support ConditionalExpr in Drools executable model (#5746)
    Co-authored-by: Gabriele-Cardosi <gabriele.cardosi@ibm.com>
    (cherry picked from commit 7b8a70f67ab2cfad8eec731d864e5e5903ab6596)

--- drools-model/drools-model-codegen/src/main/java/org/drools/model/codegen/execmodel/generator/expressiontyper/ExpressionTyper.java
+++ drools-model/drools-model-codegen/src/main/java/org/drools/model/codegen/execmodel/generator/expressiontyper/ExpressionTyper.java
@@ -43,6 +43,7 @@ import com.github.javaparser.ast.expr.BinaryExpr.Operator;
 import com.github.javaparser.ast.expr.CastExpr;
 import com.github.javaparser.ast.expr.CharLiteralExpr;
 import com.github.javaparser.ast.expr.ClassExpr;
+import com.github.javaparser.ast.expr.ConditionalExpr;
 import com.github.javaparser.ast.expr.DoubleLiteralExpr;
 import com.github.javaparser.ast.expr.EnclosedExpr;
 import com.github.javaparser.ast.expr.Expression;
@@ -377,6 +378,10 @@ public class ExpressionTyper {
                 }
             }
             return of(new TypedExpression(drlxExpr, type));
+        }
+
+        if (drlxExpr instanceof ConditionalExpr) {
+            return of(new TypedExpression(drlxExpr, Boolean.class));
         }
 
         if (drlxExpr.isAssignExpr()) {
