347768fc3f0fa9f42aaf083944025723af36c700
Gabriele-Cardosi <gabriele.cardosi@ibm.com>
2024-04-05 15:03:09 +0200

    [incubator-kie-issues#1065] Fix ValidatorTest.validateAllValidSharedModels (attempt)

--- kie-dmn/kie-dmn-validation/src/test/java/org/kie/dmn/validation/ValidatorTest.java
+++ kie-dmn/kie-dmn-validation/src/test/java/org/kie/dmn/validation/ValidatorTest.java
@@ -28,13 +28,13 @@ import java.nio.file.DirectoryIteratorException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+import org.drools.io.ClassPathResource;
 import org.drools.io.FileSystemResource;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -535,12 +535,9 @@ public class ValidatorTest extends AbstractValidatorTest {
     @Test
     public void validateAllValidSharedModels() throws URISyntaxException, IOException {
         String modelFilesPath = "valid_models";
-        URL modelFilesUrl =
-                Collections.list(Thread.currentThread().getContextClassLoader().getResources(modelFilesPath))
-                .stream()
-                .filter(url -> url.getProtocol().equals("file"))
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("Failed to retrieve " + modelFilesPath));
+        ClassPathResource classPathResource = new ClassPathResource(modelFilesPath,
+                                                                    ValidatorTest.class.getClassLoader());
+        URL modelFilesUrl = classPathResource.getURL();
         Path modelsPath = Path.of(modelFilesUrl.toURI());
         testDirectory(modelsPath);
     }
