e368e23da75b4a83993081aa305897a1e6e278a1
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2024-04-16 15:49:15 +0200

    [dmn-range-time-test] Add after/before test with durations (#5849)
    Co-authored-by: Gabriele-Cardosi <gabriele.cardosi@ibm.com>

--- kie-dmn/kie-dmn-feel/src/test/java/org/kie/dmn/feel/runtime/functions/interval/FEELTemporalFunctionsTest.java
+++ kie-dmn/kie-dmn-feel/src/test/java/org/kie/dmn/feel/runtime/functions/interval/FEELTemporalFunctionsTest.java
@@ -40,6 +40,8 @@ public class FEELTemporalFunctionsTest extends BaseFEELTest {
                 { "before( [1..10], [10..20] )", false, null },
                 { "before( [1..10), [10..20] )", true, null },
                 { "before( [1..10], (10..20] )", true, null },
+                { "before( \"@2020-01-01\", [\"@2021-01-01\"..\"@2022-01-01\"])", true, null },
+                { "before( \"@2024-01-01\", [\"@2021-01-01\"..\"@2022-01-01\"])", false, null },
 
                 { "after( 10, 5 )", true, null },
                 { "after( 5, 10 )", false, null },
@@ -54,17 +56,20 @@ public class FEELTemporalFunctionsTest extends BaseFEELTest {
                 { "after( [1..10], [11..20] )", false, null },
                 { "after( [11..20], [1..11) )", true, null },
                 { "after( (11..20], [1..11] )", true, null },
+
+                { "after( \"@2020-01-01\", [\"@2021-01-01\"..\"@2022-01-01\"])", false, null },
+                { "after( \"@2024-01-01\", [\"@2021-01-01\"..\"@2022-01-01\"])", true, null },
                 
                 { "meets( [1..5], [5..10] )", true, null },
                 { "meets( [1..5), [5..10] )", false, null },
                 { "meets( [1..5], (5..10] )", false, null },
                 { "meets( [1..5], [6..10] )", false, null },
-                
+
                 { "met by( [5..10], [1..5] )", true, null },
                 { "met by( [5..10], [1..5) )", false, null },
                 { "met by( (5..10], [1..5] )", false, null },
                 { "met by( [6..10], [1..5] )", false, null },
-                
+
                 { "overlaps( [1..5], [3..8] )", true, null },
                 { "overlaps( [3..8], [1..5] )", true, null },
                 { "overlaps( [1..8], [3..5] )", true, null },
@@ -79,7 +84,7 @@ public class FEELTemporalFunctionsTest extends BaseFEELTest {
                 { "overlaps( (5..8], [1..5] )", false, null },
                 { "overlaps( [5..8], [1..5) )", false, null },
                 { "overlaps( (5..8], [1..5) )", false, null },
-                
+
                 { "overlaps before( [1..5], [3..8] )", true, null },
                 { "overlaps before( [1..5], [6..8] )", false, null },
                 { "overlaps before( [1..5], [5..8] )", true, null },
@@ -89,7 +94,7 @@ public class FEELTemporalFunctionsTest extends BaseFEELTest {
                 { "overlaps before( [1..5], (1..5] )", true, null },
                 { "overlaps before( [1..5), [1..5] )", false, null },
                 { "overlaps before( [1..5], [1..5] )", false, null },
-                
+
                 { "overlaps before( [1..5], (1..5) )", false, null }, // additional
                 { "overlaps before( [1..6], (1..5] )", false, null }, // additional
                 { "overlaps before( (1..5], (1..5] )", false, null }, // additional
@@ -108,7 +113,7 @@ public class FEELTemporalFunctionsTest extends BaseFEELTest {
                 { "overlaps after( (1..5], [1..6] )", false, null }, // additional
                 { "overlaps after( (1..5], (1..5] )", false, null }, // additional
                 { "overlaps after( (1..5], [2..5] )", false, null }, // additional
-                
+
                 { "finishes( 10, [1..10] )", true, null },
                 { "finishes( 10, [1..10) ) ", false, null },
                 { "finishes( [5..10], [1..10] )", true, null },
@@ -116,11 +121,11 @@ public class FEELTemporalFunctionsTest extends BaseFEELTest {
                 { "finishes( [5..10), [1..10) )", true, null },
                 { "finishes( [1..10], [1..10] )", true, null },
                 { "finishes( (1..10], [1..10] )", true, null },
-                
+
                 { "finishes( [5..11], [1..10] )", false, null }, // additional
                 { "finishes( [0..10], [1..10] )", false, null }, // additional
                 { "finishes( [1..10], (1..10] )", false, null }, // additional
-                
+
                 { "finished by( [1..10], 10 )", true, null },
                 { "finished by( [1..10), 10 )  ", false, null },
                 { "finished by( [1..10], [5..10] ) ", true, null },
@@ -128,12 +133,12 @@ public class FEELTemporalFunctionsTest extends BaseFEELTest {
                 { "finished by( [1..10), [5..10) ) ", true, null },
                 { "finished by( [1..10], [1..10] ) ", true, null },
                 { "finished by( [1..10], (1..10] ) ", true, null },
-                
+
                 { "finished by( [1..10], [5..11] )", false, null }, // additional
                 { "finished by( [1..10], [0..10] )", false, null }, // additional
                 { "finished by( (1..10], [1..10] )", false, null }, // additional
                 { "finished by( (1..10], (1..10] )", true, null }, // additional
-                
+
                 { "includes( [1..10], 5 )", true, null },
                 { "includes( [1..10], 12 )", false, null },
                 { "includes( [1..10], 1 )", true, null },
@@ -148,12 +153,12 @@ public class FEELTemporalFunctionsTest extends BaseFEELTest {
                 { "includes( [1..10], [1..10) )", true, null },
                 { "includes( [1..10], (1..10] )", true, null },
                 { "includes( [1..10], [1..10] )", true, null },
-                
+
                 { "includes( [4..6], [1..10] )", false, null }, // additional
                 { "includes( (1..5], [1..5] )", false, null }, // additional
                 { "includes( [1..5), [1..5] )", false, null }, // additional
                 { "includes( [1..4], [1..5] )", false, null }, // additional
-                
+
                 { "during( 5, [1..10] )", true, null },
                 { "during( 12, [1..10] )", false, null },
                 { "during( 1, [1..10] )", true, null },
@@ -168,12 +173,12 @@ public class FEELTemporalFunctionsTest extends BaseFEELTest {
                 { "during( [1..10), [1..10] )", true, null },
                 { "during( (1..10], [1..10] )", true, null },
                 { "during( [1..10], [1..10] )", true, null },
-                
+
                 { "during( [1..10], [4..6] )", false, null }, // additional
                 { "during( [1..5] , (1..5])", false, null }, // additional
                 { "during( [1..5] , [1..5))", false, null }, // additional
                 { "during( [1..5] , [1..4])", false, null }, // additional
-                
+
                 { "starts( 1, [1..10] )", true, null },
                 { "starts( 1, (1..10] )", false, null },
                 { "starts( 2, [1..10] )", false, null },
@@ -184,11 +189,11 @@ public class FEELTemporalFunctionsTest extends BaseFEELTest {
                 { "starts( [1..10], [1..10] )", true, null },
                 { "starts( [1..10), [1..10] )", true, null },
                 { "starts( (1..10), (1..10) )", true, null },
-                
+
                 { "starts( [1..9], [1..5] )", false, null }, // additional
                 { "starts( [1..5], [1..5) )", false, null }, // additional
                 { "starts( [2..9], [1..5] )", false, null }, // additional
-                
+
                 { "started by( [1..10], 1 )", true, null },
                 { "started by( (1..10], 1 )", false, null },
                 { "started by( [1..10], 2 )", false, null },
@@ -199,11 +204,11 @@ public class FEELTemporalFunctionsTest extends BaseFEELTest {
                 { "started by( [1..10], [1..10] )", true, null },
                 { "started by( [1..10], [1..10) )", true, null },
                 { "started by( (1..10), (1..10) )", true, null },
-                
+
                 { "started by( [1..5], [1..9] )", false, null }, // additional
                 { "started by( [1..5), [1..5] )", false, null }, // additional
                 { "started by( [1..5], [2..9] )", false, null }, // additional
-                
+
                 { "coincides( 5, 5 )", true, null },
                 { "coincides( 3, 4 )", false, null },
                 { "coincides( [1..5], [1..5] )", true, null },
