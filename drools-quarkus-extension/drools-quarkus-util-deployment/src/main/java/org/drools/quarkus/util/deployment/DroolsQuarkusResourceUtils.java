cd4410d0a868bc447c551577e52d964d9a29ba38
mariofusco <mario.fusco@gmail.com>
2024-04-22 17:56:38 +0200

    wip

--- drools-quarkus-extension/drools-quarkus-util-deployment/src/main/java/org/drools/quarkus/util/deployment/DroolsQuarkusResourceUtils.java
+++ drools-quarkus-extension/drools-quarkus-util-deployment/src/main/java/org/drools/quarkus/util/deployment/DroolsQuarkusResourceUtils.java
@@ -92,7 +92,6 @@ public class DroolsQuarkusResourceUtils {
                 .withApplicationProperties(appPaths.getResourceFiles())
                 .withClassAvailabilityResolver(className -> classAvailabilityResolver(index, className))
                 .withAppPaths(appPaths)
-                .withAppPaths(appPaths)
                 .build();
 
         return context;
