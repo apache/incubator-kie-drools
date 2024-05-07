efe8d7d41a68dc3f86e789ecf09b66ebdfea4a48
Jiří Locker <jiri.locker@gmail.com>
2024-05-07 04:41:39 +0200

    [new-parser] Allow OOPath as a LHS pattern (#5884)
    * Add OOPath support to lhsPattern
    
    * Set namespace and unit properties to PackageDescr children

--- drools-drl/drools-drl-parser-tests/src/test/java/org/drools/drl/parser/antlr4/MiscDRLParserTest.java
+++ drools-drl/drools-drl-parser-tests/src/test/java/org/drools/drl/parser/antlr4/MiscDRLParserTest.java
@@ -26,6 +26,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -4091,6 +4092,28 @@ class MiscDRLParserTest {
     }
 
     @Test
+    void ooPathLhsPattern() {
+        final String text = "package org.drools\n"
+                + "rule PlainNot when\n"
+                + "    not( /strings [ this == \"It Does Work\" ] )\n"
+                + "then\n"
+                + "end\n";
+        PackageDescr packageDescr = parseAndGetPackageDescr(text);
+        RuleDescr ruleDescr = packageDescr.getRules().get(0);
+        assertThat(ruleDescr.getLhs().getDescrs().get(0)).isInstanceOfSatisfying(NotDescr.class, notDescr -> {
+            assertThat(notDescr.getDescrs()).hasSize(1);
+            assertThat(notDescr.getDescrs().get(0)).isInstanceOfSatisfying(PatternDescr.class, patternDescr -> {
+               assertThat(patternDescr.getConstraint().getDescrs()).hasSize(1);
+               assertThat(patternDescr.getConstraint().getDescrs().get(0)).isInstanceOfSatisfying(ExprConstraintDescr.class, exprConstraintDescr -> {
+                   assertThat(exprConstraintDescr.getExpression()).isEqualTo("/strings [ this == \"It Does Work\" ]");
+                   assertThat(exprConstraintDescr.getType()).isEqualTo(ExprConstraintDescr.Type.NAMED);
+                   assertThat(exprConstraintDescr.getPosition()).isEqualTo(0);
+               });
+            });
+        });
+    }
+
+    @Test
     void inlineCast() {
         final String text = "package org.drools\n" +
                 "rule R1\n" +
@@ -4604,5 +4627,43 @@ class MiscDRLParserTest {
 
         TypeFieldDescr romanStr = enumDeclarationDescr.getFields().get("romanStr");
         assertThat(romanStr.getPattern().getObjectType()).isEqualTo("String");
+    }
+
+    @Test
+    void packageChildrenNamespaceAndUnitProperties() {
+        String namespace = "org.drools.compiler.test";
+        String source = readResource("package_children.drl");
+        PackageDescr pkg = parseAndGetPackageDescr(source);
+
+        // Package and Rule Unit's namespace.
+        assertThat(pkg.getName()).isEqualTo(namespace);
+        assertThat(pkg.getUnit().getNamespace()).isEqualTo(namespace);
+
+        // Children that are expected to have the package name as their namespace.
+        assertNamespace(pkg.getImports(), namespace);
+        assertNamespace(pkg.getFunctionImports(), namespace);
+        assertNamespace(pkg.getAccumulateImports(), namespace);
+        assertNamespace(pkg.getGlobals(), namespace);
+        assertNamespace(pkg.getFunctions(), namespace);
+        assertNamespace(pkg.getRules(), namespace);
+        assertNamespace(pkg.getAttributes(), namespace);
+
+        // Children that are expected to have no namespace.
+        assertNamespace(pkg.getTypeDeclarations(), "");
+        assertNamespace(pkg.getEnumDeclarations(), "");
+        assertNamespace(pkg.getEntryPointDeclarations(), "");
+        assertNamespace(pkg.getWindowDeclarations(), "");
+
+        assertThat(pkg.getRules())
+                .allSatisfy(ruleDescr -> assertThat(ruleDescr.getUnit()).isNotNull())
+                .allSatisfy(ruleDescr -> assertThat(ruleDescr.getUnit().getTarget()).isEqualTo("TestUnit"));
+
+        assertThat(pkg.getRules().get(0).getUnitQualifiedName()).isEqualTo("TestUnit.MyQuery");
+        assertThat(pkg.getRules().get(1).getUnitQualifiedName()).isEqualTo("TestUnit.My Rule");
+    }
+
+    static void assertNamespace(Collection<? extends BaseDescr> children, String namespace) {
+        assertThat(children).isNotEmpty(); // Make sure that every child type is represented.
+        assertThat(children).allSatisfy(baseDescr -> assertThat(baseDescr.getNamespace()).isEqualTo(namespace));
     }
 }
