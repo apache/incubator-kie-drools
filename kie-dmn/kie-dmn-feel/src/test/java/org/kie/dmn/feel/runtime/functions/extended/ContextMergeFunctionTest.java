bd21a1c26b66cc6f568999f1c438aebe0b2cfcbc
Yeser Amer <yeser.amer@ibm.com>
2024-06-18 17:12:37 +0200

    Fix + Tests

---
+++ kie-dmn/kie-dmn-feel/src/test/java/org/kie/dmn/feel/runtime/functions/extended/ContextMergeFunctionTest.java
@@ -0,0 +1,40 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.kie.dmn.feel.runtime.functions.extended;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.kie.dmn.feel.runtime.events.InvalidParametersEvent;
+import org.kie.dmn.feel.runtime.functions.FunctionTestUtil;
+
+class ContextMergeFunctionTest {
+
+    private ContextMergeFunction contextMergeFunction;
+
+    @BeforeEach
+    void setUp() {
+        contextMergeFunction = new ContextMergeFunction();
+    }
+
+    @Test
+    void invokeListNull() {
+        FunctionTestUtil.assertResultError(contextMergeFunction.invoke(null), InvalidParametersEvent.class);
+        FunctionTestUtil.assertResultError(contextMergeFunction.invoke((Object) null), InvalidParametersEvent.class);
+    }
+}
