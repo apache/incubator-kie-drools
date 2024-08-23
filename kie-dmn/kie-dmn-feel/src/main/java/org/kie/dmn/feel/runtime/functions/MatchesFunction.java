edc77d536e07a9c3cf879f51351bea28e7889897
bncriju <bincy.riju@ibm.com>
2024-08-23 12:39:55 +0530

    [incubator-kie-issues#1409]-Fixed matches function behavior (#6055)

--- kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/MatchesFunction.java
+++ kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/MatchesFunction.java
@@ -18,16 +18,21 @@
  */
 package org.kie.dmn.feel.runtime.functions;
 
+import java.security.InvalidParameterException;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
+import java.util.stream.Collectors;
 
 import org.kie.dmn.api.feel.runtime.events.FEELEvent.Severity;
 import org.kie.dmn.feel.runtime.events.InvalidParametersEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MatchesFunction
         extends BaseFEELFunction {
-
+    private static final Logger log = LoggerFactory.getLogger(MatchesFunction.class);
     public static final MatchesFunction INSTANCE = new MatchesFunction();
 
     private MatchesFunction() {
@@ -39,40 +44,58 @@ public class MatchesFunction
     }
 
     public FEELFnResult<Boolean> invoke(@ParameterName("input") String input, @ParameterName("pattern") String pattern, @ParameterName("flags") String flags) {
-        if ( input == null ) {
-            return FEELFnResult.ofError( new InvalidParametersEvent( Severity.ERROR, "input", "cannot be null" ) );
-        }
-        if ( pattern == null ) {
-            return FEELFnResult.ofError( new InvalidParametersEvent( Severity.ERROR, "pattern", "cannot be null" ) );
-        }
         try {
-            int f = processFlags( flags );
-            Pattern p = Pattern.compile( pattern, f );
-            Matcher m = p.matcher( input );
-            return FEELFnResult.ofResult( m.find() );
-        } catch ( PatternSyntaxException e ) {
-            return FEELFnResult.ofError( new InvalidParametersEvent( Severity.ERROR, "pattern", "is invalid and can not be compiled", e ) );
-        } catch ( IllegalArgumentException t ) {
+            return matchFunctionWithFlags(input,pattern,flags);
+        } catch ( PatternSyntaxException t ) {
+            return FEELFnResult.ofError( new InvalidParametersEvent( Severity.ERROR, "pattern", "is invalid and can not be compiled", t ) );
+        } catch (InvalidParameterException t ) {
+            return FEELFnResult.ofError( new InvalidParametersEvent( Severity.ERROR, t.getMessage(), "cannot be null", t ) );
+        } catch (IllegalArgumentException t ) {
             return FEELFnResult.ofError( new InvalidParametersEvent( Severity.ERROR, "flags", "contains unknown flags", t ) );
-        } catch ( Throwable t) {
+        } catch (Throwable t) {
             return FEELFnResult.ofError( new InvalidParametersEvent( Severity.ERROR, "pattern", "is invalid and can not be compiled", t ) );
         }
     }
 
-    private int processFlags(String flags) {
-        int f = 0;
-        if( flags != null ) {
-            if( flags.contains( "s" ) ) {
-                f |= Pattern.DOTALL;
-            }
-            if( flags.contains( "m" ) ) {
-                f |= Pattern.MULTILINE;
-            }
-            if( flags.contains( "i" ) ) {
-                f |= Pattern.CASE_INSENSITIVE;
-            }
+    static FEELFnResult<Boolean> matchFunctionWithFlags(String input, String pattern, String flags) {
+        log.debug("Input:  {} , Pattern: {}, Flags: {}", input, pattern, flags);
+        if ( input == null ) {
+            throw new InvalidParameterException("input");
         }
-        return f;
+        if ( pattern == null ) {
+            throw new InvalidParameterException("pattern");
+        }
+            final String flagsString;
+            if (flags != null && !flags.isEmpty()) {
+                checkFlags(flags);
+                if(!flags.contains("U")){
+                    flags += "U";
+                }
+                flagsString = String.format("(?%s)", flags);
+            } else {
+                flagsString = "";
+            }
+            log.debug("flagsString: {}", flagsString);
+            String stringToBeMatched = flagsString + pattern;
+            log.debug("stringToBeMatched: {}", stringToBeMatched);
+            Pattern p=Pattern.compile(stringToBeMatched);
+            Matcher m = p.matcher( input );
+            boolean matchFound=m.find();
+            log.debug("matchFound: {}", matchFound);
+            return FEELFnResult.ofResult(matchFound);
     }
 
+   static void checkFlags(String flags) {
+        Set<Character> allowedChars = Set.of('s','i','x','m');
+         boolean isValidFlag= flags.chars()
+                .mapToObj(c -> (char) c)
+                .allMatch(allowedChars::contains)
+                && flags.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toSet())
+                .size() == flags.length();
+         if(!isValidFlag){
+             throw new IllegalArgumentException("Not a valid flag parameter " +flags);
+         }
+    }
 }
