dd02203369a6924c9611366729b578a13531d0b5
Paolo Bizzarri <pibizza@gmail.com>
2024-09-09 15:05:04 +0200

    Migrating to JUnit5. Using good use of parametrized tests

--- drools-drlonyaml-parent/drools-drlonyaml-cli-tests/src/test/java/org/drools/drlonyaml/cli/tests/ConversionsUsingCliTest.java
+++ drools-drlonyaml-parent/drools-drlonyaml-cli-tests/src/test/java/org/drools/drlonyaml/cli/tests/ConversionsUsingCliTest.java
@@ -23,7 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.InputStream;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class ConversionsUsingCliTest {
 
