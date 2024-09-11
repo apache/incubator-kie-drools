175320931e8a5d7b3c23df981131c7023d34b4cf
Paolo Bizzarri <pibizza@gmail.com>
2024-09-11 10:46:52 +0200

    NO ISSUE: Migrating to JUnit5 (#6076)
    * Migrating to JUnit5. Using good use of parametrized tests
    
    * Reverted changes in pom

--- drools-drlonyaml-parent/drools-drlonyaml-model/src/test/java/org/drools/drlonyaml/model/SmokeTest.java
+++ drools-drlonyaml-parent/drools-drlonyaml-model/src/test/java/org/drools/drlonyaml/model/SmokeTest.java
@@ -26,11 +26,12 @@ import java.nio.file.Paths;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator.Feature;
-import org.assertj.core.api.Assertions;
 import org.drools.drl.ast.descr.PackageDescr;
 import org.drools.drl.parser.DrlParser;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -45,91 +46,40 @@ public class SmokeTest {
         YAMLFactory yamlFactory = YAMLFactory.builder().enable(Feature.MINIMIZE_QUOTES).build();
         mapper = new ObjectMapper(yamlFactory);
     }
-
-    private void assertDrlToYamlAndBack(String filename) {
-        try {
-            String content = Files.readString(Paths.get(this.getClass().getResource(filename).toURI()));
-            assertThat(content).as("Failed to read test resource").isNotNull();
-            
-            PackageDescr pkgDescr = drlParser.parse(new StringReader(content));
-            assertThat(pkgDescr).as("Failed to parse DRL as a PackageDescr").isNotNull();
-            
-            DrlPackage model = DrlPackage.from(pkgDescr);
-            assertThat(model).as("Failed to generate from a PackageDescr a valid model").isNotNull();
-            
-            StringWriter writer = new StringWriter();
-            mapper.writeValue(writer, model);
-            final String yaml = writer.toString();
-            writer.close();            
-            LOG.debug("{}", yaml);
-            assertThat(yaml).as("resulting YAML shall not be null nor empty").isNotNull().isNotEmpty();
-            
-            final DrlPackage deserPackage = mapper.readValue(yaml, DrlPackage.class);
-            assertThat(deserPackage).usingRecursiveComparison().isEqualTo(model);
-        } catch (Exception e) {
-            Assertions.fail("Failed to roundtrip from DRL to YAML and back to YAML", e);
-        }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {"/smoketests/smoke1.drl.txt",
+            "/smoketests/smoke2.drl.txt",
+            "/smoketests/smoke3.drl.txt",
+            "/smoketests/smoke4.drl.txt",
+            "/smoketests/smoke5.drl.txt",
+            "/smoketests/smoke6.drl.txt",
+            "/smoketests/smoke7.drl.txt",
+            "/smoketests/smoke8.drl.txt",
+            "/smoketests/smoke9.drl.txt",
+            "/smoketests/smoke10.drl.txt",
+            "/smoketests/ruleunit.drl.txt"})
+    public void smokeTestFromDrl(String filename ) throws Exception {
+        String content = Files.readString(Paths.get(this.getClass().getResource(filename).toURI()));
+        assertThat(content).as("Failed to read test resource").isNotNull();
+        
+        PackageDescr pkgDescr = drlParser.parse(new StringReader(content));
+        assertThat(pkgDescr).as("Failed to parse DRL as a PackageDescr").isNotNull();
+        
+        DrlPackage model = DrlPackage.from(pkgDescr);
+        assertThat(model).as("Failed to generate from a PackageDescr a valid model").isNotNull();
+        
+        StringWriter writer = new StringWriter();
+        mapper.writeValue(writer, model);
+        final String yaml = writer.toString();
+        writer.close();            
+        LOG.debug("{}", yaml);
+        assertThat(yaml).as("resulting YAML shall not be null nor empty").isNotNull().isNotEmpty();
+        
+        final DrlPackage deserPackage = mapper.readValue(yaml, DrlPackage.class);
+        assertThat(deserPackage).usingRecursiveComparison().isEqualTo(model);
     }
     
-    @Test
-    public void smokeTestFromDRL1() {
-        String filename = "/smoketests/smoke1.drl.txt";
-        assertDrlToYamlAndBack(filename);
-    }
-
-    @Test
-    public void smokeTestFromDRL2() {
-        String filename = "/smoketests/smoke2.drl.txt";
-        assertDrlToYamlAndBack(filename);
-    }
-    
-    @Test
-    public void smokeTestFromDRL3() {
-        String filename = "/smoketests/smoke3.drl.txt";
-        assertDrlToYamlAndBack(filename);
-    }
-    
-    @Test
-    public void smokeTestFromDRL4() {
-        String filename = "/smoketests/smoke4.drl.txt";
-        assertDrlToYamlAndBack(filename);
-    }
-    
-    @Test
-    public void smokeTestFromDRL5() {
-        String filename = "/smoketests/smoke5.drl.txt";
-        assertDrlToYamlAndBack(filename);
-    }
-    
-    @Test
-    public void smokeTestFromDRL6() {
-        String filename = "/smoketests/smoke6.drl.txt";
-        assertDrlToYamlAndBack(filename);
-    }
-    
-    @Test
-    public void smokeTestFromDRL7() {
-        String filename = "/smoketests/smoke7.drl.txt";
-        assertDrlToYamlAndBack(filename);
-    }
-    
-    @Test
-    public void smokeTestFromDRL8() {
-        String filename = "/smoketests/smoke8.drl.txt";
-        assertDrlToYamlAndBack(filename);
-    }
-    
-    @Test
-    public void smokeTestFromDRL9() {
-        String filename = "/smoketests/smoke9.drl.txt";
-        assertDrlToYamlAndBack(filename);
-    }
-    
-    @Test
-    public void smokeTestFromDRL10() {
-        String filename = "/smoketests/smoke10.drl.txt";
-        assertDrlToYamlAndBack(filename);
-    }
     
     @Test
     public void smokeTestFromYAML1() throws Exception {
@@ -138,17 +88,11 @@ public class SmokeTest {
         LOG.debug("{}", result);
     }
     
-    @Ignore("additional RHS types not supported at the moment.")
+    @Disabled("additional RHS types not supported at the moment.")
     @Test
     public void smokeTestFromYAML2() throws Exception {
         String content = Files.readString(Paths.get(this.getClass().getResource("/smoketests/yamlfirst_smoke2.yml").toURI()));
         DrlPackage result = mapper.readValue(content, DrlPackage.class);
         LOG.debug("{}", result);
-    }
-
-    @Test
-    public void smokeTestWithRuleUnit() {
-        String filename = "/smoketests/ruleunit.drl.txt";
-        assertDrlToYamlAndBack(filename);
     }
 }
