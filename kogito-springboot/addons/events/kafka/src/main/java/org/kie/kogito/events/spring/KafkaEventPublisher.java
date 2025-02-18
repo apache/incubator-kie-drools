8106281a47ccb1ff5d5fcbd84ea2920a9fea9789
Francisco Javier Tirado Sarti <65240126+fjtirado@users.noreply.github.com>
2025-02-18 19:32:26 +0100

    [incubator-kie-issues#1817] Check that topic is disabled (#3846)
    * [incubator-kie-issues#1817] Check that topic is disabled
    
    * [incubator-kie-issues#1817] Keep same enable properties than in quarkus
    
    * [incubator-kie-issues#1817] Gonzalo's comment
    
    Print log when disabled
    
    * Update springboot/addons/events/kafka/src/main/java/org/kie/kogito/events/spring/KafkaEventPublisher.java
    
    Co-authored-by: Gonzalo Muñoz <gonzalo51429@gmail.com>
    
    * Update springboot/addons/events/kafka/src/main/java/org/kie/kogito/events/spring/KafkaEventPublisher.java
    
    Co-authored-by: Gonzalo Muñoz <gonzalo51429@gmail.com>
    
    ---------
    
    Co-authored-by: Gonzalo Muñoz <gonzalo51429@gmail.com>

--- kogito-springboot/addons/events/kafka/src/main/java/org/kie/kogito/events/spring/KafkaEventPublisher.java
+++ kogito-springboot/addons/events/kafka/src/main/java/org/kie/kogito/events/spring/KafkaEventPublisher.java
@@ -25,7 +25,6 @@ import org.kie.kogito.event.EventPublisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -36,6 +35,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class KafkaEventPublisher implements EventPublisher {
 
     private static final Logger logger = LoggerFactory.getLogger(KafkaEventPublisher.class);
+    private static final String TOPIC_FORMAT = "kogito.addon.events.process.kafka.%s.%s";
+    private static final String ENABLED_FORMAT = "kogito.events.%s.enabled";
 
     @Autowired
     private ObjectMapper json;
@@ -46,15 +47,6 @@ public class KafkaEventPublisher implements EventPublisher {
     @Autowired
     private KafkaTemplate<String, String> eventsEmitter;
 
-    @Value("${kogito.events.processinstances.enabled:true}")
-    private boolean processInstancesEvents;
-
-    @Value("${kogito.events.processdefinitions.enabled:true}")
-    private boolean processDefinitionEvents;
-
-    @Value("${kogito.events.usertasks.enabled:true}")
-    private boolean userTasksEvents;
-
     @Override
     public void publish(DataEvent<?> event) {
 
@@ -64,7 +56,7 @@ public class KafkaEventPublisher implements EventPublisher {
             case "ProcessInstanceSLADataEvent":
             case "ProcessInstanceStateDataEvent":
             case "ProcessInstanceVariableDataEvent":
-                publishToTopic(event, PROCESS_INSTANCES_TOPIC_NAME);
+                publishToTopic(event, PROCESS_INSTANCES_TOPIC_NAME, "processinstances");
                 break;
             case "UserTaskInstanceAssignmentDataEvent":
             case "UserTaskInstanceAttachmentDataEvent":
@@ -72,10 +64,10 @@ public class KafkaEventPublisher implements EventPublisher {
             case "UserTaskInstanceDeadlineDataEvent":
             case "UserTaskInstanceStateDataEvent":
             case "UserTaskInstanceVariableDataEvent":
-                publishToTopic(event, USER_TASK_INSTANCES_TOPIC_NAME);
+                publishToTopic(event, USER_TASK_INSTANCES_TOPIC_NAME, "usertasks");
                 break;
             case "ProcessDefinitionEvent":
-                publishToTopic(event, PROCESS_DEFINITIONS_TOPIC_NAME);
+                publishToTopic(event, PROCESS_DEFINITIONS_TOPIC_NAME, "processdefinitions");
                 break;
             default:
                 logger.debug("Unknown type of event '{}', ignoring for this publisher", event.getType());
@@ -89,15 +81,19 @@ public class KafkaEventPublisher implements EventPublisher {
         }
     }
 
-    protected void publishToTopic(DataEvent<?> event, String topic) {
-        logger.debug("About to publish event {} to Kafka topic {}", event, topic);
-        try {
-            String eventString = json.writeValueAsString(event);
-            logger.debug("Event payload '{}'", eventString);
-            eventsEmitter.send(env.getProperty("kogito.addon.events.process.kafka." + topic + ".topic", topic), eventString);
-            logger.debug("Successfully published event {} to topic {}", event, topic);
-        } catch (Exception e) {
-            logger.error("Error while publishing event to Kafka topic {} for event {}", topic, event, e);
+    protected void publishToTopic(DataEvent<?> event, String topic, String enabled) {
+        if (env.getProperty(String.format(ENABLED_FORMAT, enabled), Boolean.class, true)) {
+            logger.debug("About to publish event {} to Kafka topic {}", event, topic);
+            try {
+                String eventString = json.writeValueAsString(event);
+                logger.debug("Event payload '{}'", eventString);
+                eventsEmitter.send(env.getProperty(String.format(TOPIC_FORMAT, topic, "topic"), topic), eventString);
+                logger.debug("Successfully published event {} to topic {}", event, topic);
+            } catch (Exception e) {
+                logger.error("Error while publishing event to Kafka topic {} for event {}", topic, event, e);
+            }
+        } else {
+            logger.debug("Event {} won't be published because topic {} is disabled", event, topic);
         }
     }
 }
