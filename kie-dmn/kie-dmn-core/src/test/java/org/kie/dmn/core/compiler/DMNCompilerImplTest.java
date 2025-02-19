6fbd51c279d137e34b38d13a1045f83c48a8e352
ChinchuAjith <Chinchu.P.Shaji@ibm.com>
2025-02-19 11:30:02 +0530

    TestCases for DMNCompilerImpl class

--- kie-dmn/kie-dmn-core/src/test/java/org/kie/dmn/core/compiler/DMNCompilerImplTest.java
+++ kie-dmn/kie-dmn-core/src/test/java/org/kie/dmn/core/compiler/DMNCompilerImplTest.java
@@ -18,29 +18,51 @@
  */
 package org.kie.dmn.core.compiler;
 
+
+import org.drools.io.ClassPathResource;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.kie.dmn.model.api.DMNElementReference;
-import org.kie.dmn.model.api.Definitions;
-import org.kie.dmn.model.api.InformationRequirement;
-import org.kie.dmn.model.v1_5.TDMNElementReference;
-import org.kie.dmn.model.v1_5.TDefinitions;
-import org.kie.dmn.model.v1_5.TInformationRequirement;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.kie.api.io.Resource;
+import org.kie.dmn.api.core.DMNCompiler;
+import org.kie.dmn.api.core.DMNModel;
+
+import org.kie.dmn.model.api.*;
+import org.kie.dmn.model.v1_5.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.*;
+import java.util.*;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
+@ExtendWith(MockitoExtension.class)
 class DMNCompilerImplTest {
+
+    private static DMNCompiler dMNCompiler;
+
+    private static List<DRGElementCompiler> drgCompilers = new ArrayList<>();
+    {
+        drgCompilers.add( new InputDataCompiler() );
+        drgCompilers.add( new BusinessKnowledgeModelCompiler() );
+        drgCompilers.add( new DecisionCompiler() );
+        drgCompilers.add( new DecisionServiceCompiler() );
+        drgCompilers.add( new KnowledgeSourceCompiler() ); // keep last as it's a void compiler
+    }
 
     private static final String nameSpace = "http://www.montera.com.au/spec/DMN/local-hrefs";
     private static Definitions parent;
 
     @BeforeAll
-    static void setup() {
+    static void setup() throws IOException {
         String modelName = "LocalHrefs";
         parent = new TDefinitions();
         parent.setName(modelName);
         parent.setNamespace(nameSpace);
+
+        dMNCompiler = new DMNCompilerImpl();
     }
 
     @Test
@@ -76,4 +98,84 @@ class DMNCompilerImplTest {
         retrieved = DMNCompilerImpl.getRootElement(elementReference);
         assertThat(retrieved).isNotNull().isEqualTo(parent);
     }
+
+    @Test
+    void compileTest()  {
+        List<DMNModel> dmnModels = new ArrayList<>();
+        Resource resource = new ClassPathResource( "valid_models/DMNv1_5/Sample.dmn",
+                this.getClass());
+        DMNModel model = dMNCompiler.compile( resource, dmnModels);
+        assertThat(model).isNotNull();
+    }
+
+    @Test
+    void compileTestWithInvalidModelImports() {
+        List<DMNModel> dmnModels = new ArrayList<>();
+        String nameSpace = "https://kie.apache.org/dmn/_857FE424-BEDA-4772-AB8E-2F4CDDB864AB";
+        String modelName = "loan_pre_qualification";
+        String importType = String.valueOf(ImportDMNResolverUtil.ImportType.DMN);
+        Resource resource = new ClassPathResource( "valid_models/DMNv1_5/Sample.dmn",
+                this.getClass());
+        DMNModel model = dMNCompiler.compile( resource, dmnModels);
+        assertThat(model).isNotNull();
+        Definitions dmnDefn = model.getDefinitions();
+        addImport(dmnDefn, importType, nameSpace, modelName);
+        dmnModels.add(model);
+        model = dMNCompiler.compile(dmnDefn, resource, dmnModels);
+        assertThat(model).isNotNull();
+
+    }
+
+    @Test
+    void compileTestWithDmnModelImports() {
+        List<DMNModel> dmnModels = new ArrayList<>();
+        String nameSpace = "https://kie.apache.org/dmn/_857FE424-BEDA-4772-AB8E-2F4CDDB864AB";
+        String modelName = "loan_pre_qualification";
+        String URI_DMN = "http://www.omg.org/spec/DMN/20180521/MODEL/";
+        Resource resource = new ClassPathResource( "valid_models/DMNv1_5/Sample.dmn",
+                this.getClass());
+        DMNModel model = dMNCompiler.compile( resource, dmnModels);
+        assertThat(model).isNotNull();
+        Definitions dmnDefn = model.getDefinitions();
+        addImport(dmnDefn, URI_DMN, nameSpace, modelName);
+
+        dmnModels.add(model);
+        model = dMNCompiler.compile(dmnDefn, resource, dmnModels);
+        assertThat(model).isNotNull();
+    }
+
+    @Test
+    void compileTestWithPmmlModelImports()  {
+        List<DMNModel> dmnModels = new ArrayList<>();
+        String nameSpace = "https://github.com/kiegroup/drools/kie-dmn/_A4BCA8B8-CF08-433F-93B2-A2598F19ECFF";
+        String modelName = "Traffic Violation";
+        String location_pmml = "http://www.dmg.org/PMML-4_3";
+        //String URI_DMN = "http://www.omg.org/spec/DMN/20180521/MODEL/";
+        Resource resource = new ClassPathResource( "valid_models/DMNv1_x/Traffic Violation.dmn",
+                this.getClass());
+        DMNModel model = dMNCompiler.compile( resource, dmnModels);
+        assertThat(model).isNotNull();
+        Definitions dmnDefs = model.getDefinitions();
+        addImport(dmnDefs, location_pmml, nameSpace, modelName);
+
+        dmnModels.add(model);
+        model = dMNCompiler.compile(dmnDefs, resource, dmnModels);
+        assertThat(model).isNotNull();
+    }
+
+    private void addImport(Definitions dmnDefs, String importType, String nameSpace, String modelName) {
+        dmnDefs.setName(modelName);
+        Import import1 = new TImport();
+        import1.setNamespace(nameSpace);
+        import1.setName(modelName);
+        import1.setImportType(importType);
+        import1.setParent(dmnDefs);
+        import1.setLocationURI(importType);
+        dmnDefs.getImport().add(import1);
+    }
+
 }
+
+
+
+
