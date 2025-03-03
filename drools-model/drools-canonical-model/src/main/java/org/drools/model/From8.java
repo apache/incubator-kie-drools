d60181d65723c0b968238141661ef1665e289ecb
Gabriel Piacenti <piacenti@users.noreply.github.com>
2025-03-03 06:55:43 -0800

    #6193 increasing the limits of number of bindings used in predicates and actions (#6260)
    * #6193 increasing the limits of bindings used in predicates and actions
    
    * #6193 adding headers and integration test
    
    * - move test to drools-model-codegen
    
    ---------
    
    Co-authored-by: GABRIEL PIACENTI <Gabriel.Piacenti@LibertyMutual.com>
    Co-authored-by: Toshiya Kobayashi <toshiyakobayashi@gmail.com>

---
+++ drools-model/drools-canonical-model/src/main/java/org/drools/model/From8.java
@@ -0,0 +1,33 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.drools.model;
+
+import org.drools.model.functions.Function8;
+
+public interface From8<A, B, C, D, E, F, G, H> extends From<A> {
+    Variable<B> getVariable2();
+    Variable<C> getVariable3();
+    Variable<D> getVariable4();
+    Variable<E> getVariable5();
+    Variable<F> getVariable6();
+    Variable<G> getVariable7();
+    Variable<H> getVariable8();
+    Function8<A, B, C, D, E, F, G, H, ?> getProvider();
+}
