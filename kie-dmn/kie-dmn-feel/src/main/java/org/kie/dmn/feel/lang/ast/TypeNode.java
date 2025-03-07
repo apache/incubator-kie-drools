5c44070c634575c28d81598918bfcce2811e3ae0
bncriju <bincy.riju@ibm.com>
2025-03-07 13:25:20 +0530

    license header corrected and managed unused imports

--- kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/lang/ast/TypeNode.java
+++ kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/lang/ast/TypeNode.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * <p>
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -21,7 +21,6 @@ package org.kie.dmn.feel.lang.ast;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.kie.dmn.feel.lang.EvaluationContext;
 import org.kie.dmn.feel.lang.Type;
-import org.kie.dmn.feel.lang.types.BuiltInType;
 import org.kie.dmn.feel.util.BuiltInTypeUtils;
 
 public abstract class TypeNode
