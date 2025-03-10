4cfec69492ec08b94816c4579a60295dd0823d6e
Francisco Javier Tirado Sarti <ftirados@redhat.com>
2025-03-10 18:34:57 +0100

    [Fix apache/incubator-kie-issues#1857] Adding isRetrigger

--- kie-api/src/main/java/org/kie/api/event/process/ProcessNodeTriggeredEvent.java
+++ kie-api/src/main/java/org/kie/api/event/process/ProcessNodeTriggeredEvent.java
@@ -22,8 +22,7 @@ package org.kie.api.event.process;
  * An event when a node inside a process instance has been triggered.
  */
 public interface ProcessNodeTriggeredEvent extends ProcessNodeEvent {
-
-    default int triggerCount() { 
-        return 1;
-    }
+	default boolean isRetrigger() {
+		return false;
+	}
 }
