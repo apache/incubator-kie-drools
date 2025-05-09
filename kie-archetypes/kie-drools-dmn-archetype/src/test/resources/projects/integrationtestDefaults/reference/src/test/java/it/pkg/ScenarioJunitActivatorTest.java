da2d1b8fdeff7f69dc0372cfe1ae52328b9f3b0b
Yeser Amer <yeser.amer@ibm.com>
2025-05-09 11:14:06 +0200

    Fix Tests

--- kie-archetypes/kie-drools-dmn-archetype/src/test/resources/projects/integrationtestDefaults/reference/src/test/java/it/pkg/ScenarioJunitActivatorTest.java
+++ kie-archetypes/kie-drools-dmn-archetype/src/test/resources/projects/integrationtestDefaults/reference/src/test/java/it/pkg/ScenarioJunitActivatorTest.java
@@ -19,14 +19,14 @@
 
 package it.pkg;
 
-import org.drools.scenariosimulation.backend.runner.ScenarioJunitActivator;
+import org.drools.scenariosimulation.backend.runner.TestScenarioActivator;
 
 /**
  * ScenarioJunitActivator is a custom JUnit runner that enables the execution of Test Scenario files (*.scesim).
  * This activator class, when executed, will load all scesim files available in the project and run them.
  * Each row of the scenario will generate a test JUnit result.
  */
-@org.junit.runner.RunWith(ScenarioJunitActivator.class)
+@TestScenarioActivator
 public class ScenarioJunitActivatorTest {
 
 }
