8d1499bf60e46247cb6cd2ad9b5faef95c94e346
ChinchuAjith <Chinchu.P.Shaji@ibm.com>
2025-06-27 19:45:08 +0530

    DMN Engine 1.6 changes

--- kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/impl/DMNModelImpl.java
+++ kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/impl/DMNModelImpl.java
@@ -58,13 +58,7 @@ import org.kie.dmn.core.api.DMNMessageManager;
 import org.kie.dmn.core.assembler.DMNAssemblerService;
 import org.kie.dmn.core.ast.BusinessKnowledgeModelNodeImpl;
 import org.kie.dmn.core.ast.DecisionNodeImpl;
-import org.kie.dmn.core.compiler.DMNCompilerImpl;
-import org.kie.dmn.core.compiler.DMNTypeRegistry;
-import org.kie.dmn.core.compiler.DMNTypeRegistryV11;
-import org.kie.dmn.core.compiler.DMNTypeRegistryV12;
-import org.kie.dmn.core.compiler.DMNTypeRegistryV13;
-import org.kie.dmn.core.compiler.DMNTypeRegistryV14;
-import org.kie.dmn.core.compiler.DMNTypeRegistryV15;
+import org.kie.dmn.core.compiler.*;
 import org.kie.dmn.core.pmml.DMNImportPMMLInfo;
 import org.kie.dmn.core.util.DefaultDMNMessagesManager;
 import org.kie.dmn.feel.lang.FEELDialect;
@@ -151,8 +145,10 @@ public class DMNModelImpl
             types = new DMNTypeRegistryV13(Collections.unmodifiableMap(importAliases));
         } else if (definitions instanceof org.kie.dmn.model.v1_4.TDefinitions) {
             types = new DMNTypeRegistryV14(Collections.unmodifiableMap(importAliases));
-        } else {
+        } else if (definitions instanceof org.kie.dmn.model.v1_5.TDefinitions) {
             types = new DMNTypeRegistryV15(Collections.unmodifiableMap(importAliases));
+        } else {
+            types = new DMNTypeRegistryV16(Collections.unmodifiableMap(importAliases));
         }
     }
     
