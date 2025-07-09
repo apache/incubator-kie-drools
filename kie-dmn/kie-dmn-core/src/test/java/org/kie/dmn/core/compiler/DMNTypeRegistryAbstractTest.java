9b755728a71ed53e05be06fa10042b6d311aba7e
ChinchuAjith <Chinchu.P.Shaji@ibm.com>
2025-07-09 17:04:45 +0530

    Adding more test cases

--- kie-dmn/kie-dmn-core/src/test/java/org/kie/dmn/core/compiler/DMNTypeRegistryAbstractTest.java
+++ kie-dmn/kie-dmn-core/src/test/java/org/kie/dmn/core/compiler/DMNTypeRegistryAbstractTest.java
@@ -1,19 +1,17 @@
 package org.kie.dmn.core.compiler;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.kie.dmn.api.core.DMNType;
 import org.kie.dmn.core.impl.SimpleTypeImpl;
 import org.kie.dmn.feel.lang.types.BuiltInType;
 import org.kie.dmn.feel.lang.types.ScopeImpl;
-import org.kie.dmn.model.v1_5.TDefinitions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class DMNTypeRegistryAbstractTest {
 
@@ -23,18 +21,45 @@ public class DMNTypeRegistryAbstractTest {
     void testGetFeelPrimitiveType_list() {
         DMNType result = DMNTypeRegistryAbstract.getFeelPrimitiveType("list", BuiltInType.LIST, "feel", null);
 
-        assertNotNull(result);
-        assertTrue(result.isCollection());
-        assertEquals("list", result.getName());
+        assertThat(result).isNotNull();
+        assertThat(result.isCollection()).isTrue();
+        assertThat(result.getName()).isEqualTo("list");
     }
 
     @Test
     void testGetFeelPrimitiveType_string() {
         DMNType result = DMNTypeRegistryAbstract.getFeelPrimitiveType("string", BuiltInType.STRING, "feel", null);
 
-        assertNotNull(result);
-        assertFalse(result.isCollection());
-        assertEquals("string", result.getName());
+        assertThat(result).isNotNull();
+        assertThat(result.isCollection()).isFalse();
+        assertThat(result.getName()).isEqualTo("string");
+    }
+
+    @Test
+    void testGetFeelPrimitiveType_number() {
+        DMNType result = DMNTypeRegistryAbstract.getFeelPrimitiveType("number", BuiltInType.NUMBER, "feel", null);
+
+        assertThat(result).isNotNull();
+        assertThat(result.isCollection()).isFalse();
+        assertThat(result.getName()).isEqualTo("number");
+    }
+
+    @Test
+    void testGetFeelPrimitiveType_context() {
+        DMNType result = DMNTypeRegistryAbstract.getFeelPrimitiveType("context", BuiltInType.CONTEXT, "feel", null);
+
+        assertThat(result).isNotNull();
+        assertThat(result.isCollection()).isFalse();
+        assertThat(result.getName()).isEqualTo("context");
+    }
+
+    @Test
+    void testGetFeelPrimitiveType_function() {
+        DMNType result = DMNTypeRegistryAbstract.getFeelPrimitiveType("function", BuiltInType.FUNCTION, "feel", null);
+
+        assertThat(result).isNotNull();
+        assertThat(result.isCollection()).isFalse();
+        assertThat(result.getName()).isEqualTo("function");
     }
 
     @Test
@@ -43,9 +68,10 @@ public class DMNTypeRegistryAbstractTest {
         DMNType unknownType = new SimpleTypeImpl(feelNamespace, "unknown", null, false, null, null, null, BuiltInType.UNKNOWN);
         Map<String, DMNType> feelTypes = new HashMap<>();
         ScopeImpl feelTypesScope = DMNTypeRegistryAbstract.getScopeImpl(feelNamespace, unknownType, feelTypes);
-        assertNotNull(feelTypesScope);
-        assertEquals("list", feelTypesScope.resolve("list").getId());
-        assertEquals(BuiltInType.LIST, feelTypesScope.resolve("list").getType());
+
+        assertThat(feelTypesScope).isNotNull();
+        assertThat(feelTypesScope.resolve("list").getId()).isEqualTo("list");
+        assertThat(feelTypesScope.resolve("list").getType()).isEqualTo(BuiltInType.LIST);
     }
 
 
