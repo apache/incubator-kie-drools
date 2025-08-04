1628a70aa3e89f55504a963d5f069a9392f07047
gabriel-farache <gfarache@redhat.com>
2025-08-04 09:55:49 +0000

    apache/incubator-kie-issues#1894: introduce OAUTH2 token exchange (#3928)
    * apache/incubator-kie-issues#1894: introduce OAUTH2 token exchange
    
    Signed-off-by: gabriel-farache <gfarache@redhat.com>
    
    * Add IT
    
    * Add license header
    
    * Fix default access token: get it from header
    
    * Scope quarkus-oidc-client to provided
    
    Signed-off-by: gabriel-farache <gfarache@redhat.com>
    
    * Fix Keycloak mock issue with body matching
    
    Signed-off-by: gabriel-farache <gfarache@redhat.com>
    
    * Update quarkus/extensions/kogito-quarkus-serverless-workflow-extension/kogito-quarkus-serverless-workflow/src/main/java/org/kie/kogito/serverless/workflow/openapi/OpenApiCustomCredentialProvider.java
    
    Co-authored-by: Gonzalo Muñoz <gonzalo51429@gmail.com>
    
    * Apply feedback
    
    Signed-off-by: gabriel-farache <gfarache@redhat.com>
    
    * Apply feedback for property naming
    
    Signed-off-by: gabriel-farache <gfarache@redhat.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Francisco Javier Tirado Sarti <65240126+fjtirado@users.noreply.github.com>
    
    * fix suggested changes
    
    Signed-off-by: gabriel-farache <gfarache@redhat.com>
    
    ---------
    
    Signed-off-by: gabriel-farache <gfarache@redhat.com>
    Co-authored-by: Gonzalo Muñoz <gonzalo51429@gmail.com>
    Co-authored-by: Francisco Javier Tirado Sarti <65240126+fjtirado@users.noreply.github.com>

--- kogito-quarkus/extensions/kogito-quarkus-serverless-workflow-extension/kogito-quarkus-serverless-workflow-integration-test/src/test/java/org/kie/kogito/quarkus/workflows/KeycloakServiceMock.java
+++ kogito-quarkus/extensions/kogito-quarkus-serverless-workflow-extension/kogito-quarkus-serverless-workflow-integration-test/src/test/java/org/kie/kogito/quarkus/workflows/KeycloakServiceMock.java
@@ -28,6 +28,7 @@ import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.matching;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
@@ -41,17 +42,26 @@ import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
  */
 public class KeycloakServiceMock implements QuarkusTestResourceLifecycleManager {
 
-    public static final String KEY_CLOAK_SERVICE_URL = "keycloak.mock.service.url";
-    public static final String KEY_CLOAK_SERVICE_TOKEN_PATH = "keycloak.mock.service.token-path";
+    public static final String KEYCLOAK_SERVICE_URL = "keycloak.mock.service.url";
+    public static final String KEYCLOAK_SERVICE_TOKEN_PATH = "keycloak.mock.service.token-path";
+
+    public static final String KEYCLOAK_EXCHANGE_SERVICE_TOKEN_PATH = "keycloak.mock.exchange-service.token-path";
     public static final String REALM = "kogito-tests";
-    public static final String KEY_CLOAK_SERVICE_TOKEN_PATH_VALUE = "/realms/" + REALM + "/protocol/openid-connect/token";
+    public static final String EXCHANGE_REALM = "kogito-exchange-tests";
+    public static final String KEYCLOAK_SERVICE_TOKEN_PATH_VALUE = "/realms/" + REALM + "/protocol/openid-connect/token";
+    public static final String KEYCLOAK_EXCHANGE_SERVICE_TOKEN_PATH_VALUE = "/realms/" + EXCHANGE_REALM + "/protocol/openid-connect/token";
     public static final String CLIENT_ID = "kogito-app";
     public static final String SECRET = "secret";
     public static final String KEYCLOAK_ACCESS_TOKEN = "KEYCLOAK_ACCESS_TOKEN";
+
+    public static final String KEYCLOAK_EXCHANGED_ACCESS_TOKEN = "KEYCLOAK_EXCHANGED_ACCESS_TOKEN";
+
     public static final String KEYCLOAK_REFRESH_TOKEN = "KEYCLOAK_REFRESH_TOKEN";
     public static final String KEYCLOAK_SESSION_STATE = "KEYCLOAK_SESSION_STATE";
 
     public static final String AUTH_REQUEST_BODY = "grant_type=client_credentials";
+
+    public static final String EXCHANGE_AUTH_REQUEST_BODY = "grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Atoken-exchange.*";
 
     private WireMockServer wireMockServer;
 
@@ -61,7 +71,7 @@ public class KeycloakServiceMock implements QuarkusTestResourceLifecycleManager 
         wireMockServer.start();
         configureFor(wireMockServer.port());
 
-        stubFor(post(KEY_CLOAK_SERVICE_TOKEN_PATH_VALUE)
+        stubFor(post(KEYCLOAK_SERVICE_TOKEN_PATH_VALUE)
                 .withHeader(CONTENT_TYPE, equalTo(APPLICATION_FORM_URLENCODED))
                 .withBasicAuth(CLIENT_ID, SECRET)
                 .withRequestBody(equalTo(AUTH_REQUEST_BODY))
@@ -69,23 +79,49 @@ public class KeycloakServiceMock implements QuarkusTestResourceLifecycleManager 
                         .withHeader(CONTENT_TYPE, APPLICATION_JSON)
                         .withBody(getTokenResult())));
 
+        stubFor(post(KEYCLOAK_EXCHANGE_SERVICE_TOKEN_PATH_VALUE)
+                .withHeader(CONTENT_TYPE, equalTo(APPLICATION_FORM_URLENCODED))
+                .withBasicAuth(CLIENT_ID, SECRET)
+                .withRequestBody(matching(EXCHANGE_AUTH_REQUEST_BODY))
+                .willReturn(aResponse()
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON)
+                        .withBody(exchangeTokenResult())));
+
         Map<String, String> properties = new HashMap<>();
-        properties.put(KEY_CLOAK_SERVICE_URL, wireMockServer.baseUrl());
-        properties.put(KEY_CLOAK_SERVICE_TOKEN_PATH, KEY_CLOAK_SERVICE_TOKEN_PATH_VALUE);
+        properties.put(KEYCLOAK_SERVICE_URL, wireMockServer.baseUrl());
+        properties.put(KEYCLOAK_SERVICE_TOKEN_PATH, KEYCLOAK_SERVICE_TOKEN_PATH_VALUE);
+        properties.put(KEYCLOAK_EXCHANGE_SERVICE_TOKEN_PATH, KEYCLOAK_EXCHANGE_SERVICE_TOKEN_PATH_VALUE);
         return properties;
     }
 
     private static String getTokenResult() {
-        return "{\n" +
-                "    \"access_token\": \"" + KEYCLOAK_ACCESS_TOKEN + "\",\n" +
-                "    \"expires_in\": 300,\n" +
-                "    \"refresh_expires_in\": 1800,\n" +
-                "    \"refresh_token\": \"" + KEYCLOAK_REFRESH_TOKEN + "\",\n" +
-                "    \"token_type\": \"bearer\",\n" +
-                "    \"not-before-policy\": 0,\n" +
-                "    \"session_state\": \"" + KEYCLOAK_SESSION_STATE + "\",\n" +
-                "    \"scope\": \"email profile\"\n" +
-                "}";
+        return """
+                {
+                    "access_token": "%s",
+                    "expires_in": 300,
+                    "refresh_expires_in": 1800,
+                    "refresh_token": "%s",
+                    "token_type": "bearer",
+                    "not-before-policy": 0,
+                    "session_state": "%s",
+                    "scope": "email profile"
+                }
+                """.formatted(KEYCLOAK_ACCESS_TOKEN, KEYCLOAK_REFRESH_TOKEN, KEYCLOAK_SESSION_STATE);
+    }
+
+    private static String exchangeTokenResult() {
+        return """
+                {
+                    "access_token": "%s",
+                    "expires_in": 300,
+                    "refresh_expires_in": 1800,
+                    "refresh_token": "%s",
+                    "token_type": "bearer",
+                    "not-before-policy": 0,
+                    "session_state": "%s",
+                    "scope": "email profile"
+                }
+                """.formatted(KEYCLOAK_EXCHANGED_ACCESS_TOKEN, KEYCLOAK_REFRESH_TOKEN, KEYCLOAK_SESSION_STATE);
     }
 
     @Override
---
+++ kogito-quarkus/extensions/kogito-quarkus-serverless-workflow-extension/kogito-quarkus-serverless-workflow-integration-test/src/test/java/org/kie/kogito/quarkus/workflows/TokenExchangeIT.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.kie.kogito.quarkus.workflows;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.restassured.path.json.JsonPath;
+
+import jakarta.ws.rs.core.HttpHeaders;
+
+import static org.kie.kogito.quarkus.workflows.ExternalServiceMock.SUCCESSFUL_QUERY;
+import static org.kie.kogito.quarkus.workflows.TokenExchangeExternalServicesMock.BASE_AND_PROPAGATED_AUTHORIZATION_TOKEN;
+import static org.kie.kogito.test.utils.ProcessInstancesRESTTestUtils.newProcessInstance;
+
+@QuarkusTestResource(TokenExchangeExternalServicesMock.class)
+@QuarkusTestResource(KeycloakServiceMock.class)
+@QuarkusIntegrationTest
+class TokenExchangeIT {
+
+    @Test
+    void tokenExchange() {
+        // start a new process instance by sending the post query and collect the process instance id.
+        String processInput = buildProcessInput(SUCCESSFUL_QUERY);
+        Map<String, String> headers = new HashMap<>();
+        // prepare the headers to pass to the token_propagation SW.
+        // service token-propagation-external-service1 and token-propagation-external-service2 will receive the AUTHORIZATION_TOKEN 
+        headers.put(HttpHeaders.AUTHORIZATION, BASE_AND_PROPAGATED_AUTHORIZATION_TOKEN);
+
+        JsonPath jsonPath = newProcessInstance("/token_exchange", processInput, headers);
+        Assertions.assertThat(jsonPath.getString("id")).isNotBlank();
+    }
+
+    protected static String buildProcessInput(String query) {
+        return "{\"workflowdata\": {\"query\": \"" + query + "\"} }";
+    }
+}
