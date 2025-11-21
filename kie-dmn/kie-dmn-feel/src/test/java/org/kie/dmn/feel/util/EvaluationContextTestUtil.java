f189a8430bc0e3897a4a19e614ab45c6ac97b019
Yeser Amer <yeser.amer@ibm.com>
2025-11-21 11:22:19 +0100

    minor

--- kie-dmn/kie-dmn-feel/src/test/java/org/kie/dmn/feel/util/EvaluationContextTestUtil.java
+++ kie-dmn/kie-dmn-feel/src/test/java/org/kie/dmn/feel/util/EvaluationContextTestUtil.java
@@ -18,7 +18,6 @@
  */
 package org.kie.dmn.feel.util;
 
-import org.kie.dmn.api.core.DMNVersion;
 import org.kie.dmn.feel.lang.EvaluationContext;
 import org.kie.dmn.feel.lang.FEELDialect;
 import org.kie.dmn.feel.lang.impl.EvaluationContextImpl;
