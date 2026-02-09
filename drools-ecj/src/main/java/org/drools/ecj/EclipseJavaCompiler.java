60106f121ea7db897422bc7bc28d494a0e8e7122
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2026-02-09 18:20:33 +0900

    temporal RuntimeException to detect EclipseJavaCompiler use

--- drools-ecj/src/main/java/org/drools/ecj/EclipseJavaCompiler.java
+++ drools-ecj/src/main/java/org/drools/ecj/EclipseJavaCompiler.java
@@ -179,6 +179,10 @@ public final class EclipseJavaCompiler extends AbstractJavaCompiler {
             final JavaCompilerSettings pSettings
             ) {
 
+        if (true) {
+            throw new RuntimeException("#### EclipseJavaCompiler is called!! ####");
+        }
+
 
         final Collection problems = new ArrayList();
 
