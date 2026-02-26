4b74f3c1c2dff0adcaa544cfb342089f737c716e
Gabriele-Cardosi <gabriele.cardosi@ibm.com>
2026-02-26 13:00:47 +0100

    [incubator-kie-issues#2261] Enforcing noDependencyManagementRule also inside profiles. Renamed kie-no-dependencymanagement-rule -> kie-no-dependencymanagement-enforcer-rule

--- kie-no-dependencymanagement-enforcer-rule/src/main/java/org/kie/nodependencymanagementrule/NoDependencyManagementRule.java
+++ kie-no-dependencymanagement-enforcer-rule/src/main/java/org/kie/nodependencymanagementrule/NoDependencyManagementRule.java
@@ -26,6 +26,8 @@ import javax.inject.Named;
 import java.util.Set;
 import org.apache.maven.enforcer.rule.api.AbstractEnforcerRule;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
+import org.apache.maven.model.DependencyManagement;
+import org.apache.maven.model.Profile;
 import org.apache.maven.project.MavenProject;
 
 /**
@@ -55,9 +57,32 @@ public class NoDependencyManagementRule extends AbstractEnforcerRule {
     }
 
     private void checkForDependencyManagement() throws EnforcerRuleException {
-        if (project.getDependencyManagement() != null && project.getDependencyManagement().getLocation("") != null) { // The getLocation("") retrieve the position, in the pom, of that specific dependencyManagement element; it is null when such an element is inherited
+        checkDependencyManagementInProject();
+        checkDependencyManagementInProfiles();
+    }
+
+    private void checkDependencyManagementInProject() throws EnforcerRuleException {
+        if (invalidDependencyManagement(project.getDependencyManagement())) { // The getLocation("") retrieve the position, in the pom, of that specific dependencyManagement element; it is null when such an element is inherited
             throw new EnforcerRuleException(String.format("The current pom %s:%s:%s has dependencyManagement tag!", project.getGroupId(), project.getArtifactId(), project.getVersion()));
         }
+    }
+
+    private void checkDependencyManagementInProfiles() throws EnforcerRuleException {
+        if (project.getModel().getProfiles() != null) {
+            for (Profile profile : project.getModel().getProfiles()) {
+                checkDependencyManagementInProfile(profile);
+            }
+        }
+    }
+
+    private void checkDependencyManagementInProfile(Profile profile) throws EnforcerRuleException {
+        if (invalidDependencyManagement(profile.getDependencyManagement())) { // The getLocation("") retrieve the position, in the pom, of that specific dependencyManagement element; it is null when such an element is inherited
+            throw new EnforcerRuleException(String.format("The profile %s in the current pom %s:%s:%s has dependencyManagement tag!", profile.getId(), project.getGroupId(), project.getArtifactId(), project.getVersion()));
+        }
+    }
+
+    private boolean invalidDependencyManagement(DependencyManagement dependencyManagement) {
+        return dependencyManagement != null && dependencyManagement.getLocation("") != null;
     }
 
     private boolean isAllowed(String groupId, String artifactId) {
