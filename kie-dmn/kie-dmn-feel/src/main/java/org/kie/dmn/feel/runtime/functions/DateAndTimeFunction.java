e74453264299cda7949864c07df8f1b0973d4b09
AthiraHari77 <athiraharidas77c@gmail.com>
2026-03-09 21:39:10 +0530

    [Incubator kie issues#2260]  Fix tests in tck due to failure in implicit conversion of Date Time (#6614)
    * [incubator-kie-issues#2260] fix date time issue
    
    * [incubator-kie-issues#2260] fix date time issue
    
    * [incubator-kie-issues#2260] fix date time issue
    
    * [incubator-kie-issues#2260] fix date time issue
    
    * [incubator-kie-issues#2260] fix review comments
    
    * [incubator-kie-issues#2260] update class name from CustomZonedDateTime to FormattedZonedDateTime
    
    * [incubator-kie-issues#2260] update test cases
    
    * [incubator-kie-issues#2260] update test cases
    
    * [incubator-kie-issues#2260] update test cases
    
    * [incubator-kie-issues#2260] update test cases
    
    ---------
    
    Co-authored-by: athira <athira77@ibm.com>

--- kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/DateAndTimeFunction.java
+++ kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/DateAndTimeFunction.java
@@ -25,7 +25,6 @@ import java.time.LocalTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.TemporalAccessor;
@@ -36,6 +35,7 @@ import java.util.TimeZone;
 
 import org.kie.dmn.api.feel.runtime.events.FEELEvent.Severity;
 import org.kie.dmn.feel.runtime.FEELDateTimeFunction;
+import org.kie.dmn.feel.runtime.custom.FormattedZonedDateTime;
 import org.kie.dmn.feel.runtime.events.InvalidParametersEvent;
 
 import static org.kie.dmn.feel.util.NumberEvalHelper.coerceIntegerNumber;
@@ -107,12 +107,12 @@ public class DateAndTimeFunction
             TemporalAccessor validatedTime = getValidTime(time);
             if (validatedDate instanceof LocalDate && validatedTime instanceof LocalTime) {
                 if (zoneId != null) {
-                    return FEELFnResult.ofResult(ZonedDateTime.of((LocalDate) validatedDate, (LocalTime) validatedTime, zoneId));
+                    return FEELFnResult.ofResult(FormattedZonedDateTime.of((LocalDate) validatedDate, (LocalTime) validatedTime, zoneId));
                 } else {
                     return FEELFnResult.ofResult(LocalDateTime.of((LocalDate) validatedDate, (LocalTime) validatedTime));
                 }
             } else if (validatedDate instanceof LocalDate && time.query(TemporalQueries.localTime()) != null && time.query(TemporalQueries.zone()) != null) {
-                return FEELFnResult.ofResult(ZonedDateTime.of((LocalDate) validatedDate, LocalTime.from(validatedTime), zoneId != null ? zoneId : ZoneId.from(validatedTime)));
+                return FEELFnResult.ofResult(FormattedZonedDateTime.of((LocalDate) validatedDate, LocalTime.from(validatedTime), zoneId != null ? zoneId : ZoneId.from(validatedTime)));
             }
             return FEELFnResult.ofError(new InvalidParametersEvent(Severity.ERROR, "cannot invoke function for the input parameters"));
         } catch (IllegalArgumentException e) {
@@ -136,10 +136,22 @@ public class DateAndTimeFunction
 
         try {
             if( val.contains( "T" ) ) {
-                return FEELFnResult.ofResult(FEEL_DATE_TIME.parseBest(val, ZonedDateTime::from, OffsetDateTime::from, LocalDateTime::from));
+                return FEELFnResult.ofResult(FEEL_DATE_TIME.parseBest(val, FormattedZonedDateTime::from, OffsetDateTime::from, LocalDateTime::from));
             } else {
-                LocalDate value = DateTimeFormatter.ISO_DATE.parse(val, LocalDate::from);
-                return FEELFnResult.ofResult( LocalDateTime.of(value, LocalTime.of(0, 0)));
+                TemporalAccessor parsed = DateTimeFormatter.ISO_DATE.parse(val);
+                LocalDate value = LocalDate.from(parsed);
+                ZoneId zoneId = parsed.query(TemporalQueries.zone());
+                if (zoneId == null) {
+                    ZoneOffset offset = parsed.query(TemporalQueries.offset());
+                    if (offset != null) {
+                        zoneId = ZoneId.ofOffset("UTC", offset);
+                    }
+                }
+                if (zoneId != null) {
+                    return FEELFnResult.ofResult(FormattedZonedDateTime.of(value, LocalTime.of(0, 0), zoneId));
+                } else {
+                    return FEELFnResult.ofResult(LocalDateTime.of(value, LocalTime.of(0, 0)));
+                }
             }
         } catch ( Exception e ) {
             return FEELFnResult.ofError(new InvalidParametersEvent(Severity.ERROR, "from", "date-parsing exception", e));
@@ -199,7 +211,7 @@ public class DateAndTimeFunction
             int coercedHour = coerceIntegerNumber(hour).orElseThrow(() -> new NoSuchElementException("hour"));
             int coercedMinute = coerceIntegerNumber(minute).orElseThrow(() -> new NoSuchElementException("minute"));
             int coercedSecond = coerceIntegerNumber(second).orElseThrow(() -> new NoSuchElementException("second"));
-            return FEELFnResult.ofResult(ZonedDateTime.of(coercedYear, coercedMonth, coercedDay,
+            return FEELFnResult.ofResult(FormattedZonedDateTime.of(coercedYear, coercedMonth, coercedDay,
                     coercedHour, coercedMinute, coercedSecond, 0, TimeZone.getTimeZone(timezone).toZoneId()));
         } catch (NoSuchElementException e) { // thrown by Optional.orElseThrow()
             return FEELFnResult.ofError(new InvalidParametersEvent(Severity.ERROR, e.getMessage(), "could not be coerced to Integer: either null or not a valid Number."));
