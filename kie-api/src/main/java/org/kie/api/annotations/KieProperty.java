2d4a9e9328b64ba4a603595946c38a15ba3a8b3a
Gabriele-Cardosi <gabriele.cardosi@ibm.com>
2026-06-12 12:08:20 +0200

    [kie_property] Implemented KieProperty

---
+++ kie-api/src/main/java/org/kie/api/annotations/KieProperty.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.kie.api.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.kie.api.annotations.KieProperty;
+
+/**
+ * Annotation used to mark <code>String</code>s that are used as <b>configurable properties</b> in running applications, e.g. inside <i>application.properties</i> files.
+ * Its retention is set as <code>RetentionPolicy.SOURCE</code> because, for the moment being, it is meant to be used only for documentation
+ * purpose.
+ * It has to be applied to <b>class properties</b>.
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.FIELD)
+public @interface KieProperty {
+}
