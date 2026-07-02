ac2df4639db6ffdd074ad46470b2f7f6ebfda4b1
athira <athira77@ibm.com>
2026-07-02 11:50:34 +0530

    [incubator-kie-issues#2260] handle null result

--- kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/ast/DMNDecisionServiceEvaluator.java
+++ kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/ast/DMNDecisionServiceEvaluator.java
@@ -78,7 +78,7 @@ public class DMNDecisionServiceEvaluator implements DMNExpressionEvaluator {
 
             DMNType expectedType = dsNode.getResultType();
             Object originalResult = decisionResultById.getResult();
-            Object coercedResult = CoerceUtil.coerceValue(expectedType, originalResult);
+            Object coercedResult = originalResult != null ? CoerceUtil.coerceValue(expectedType, originalResult) : null;
             if (coercedResult != originalResult) {
                 ((DMNDecisionResultImpl) decisionResultById).setResult(coercedResult);
             }
