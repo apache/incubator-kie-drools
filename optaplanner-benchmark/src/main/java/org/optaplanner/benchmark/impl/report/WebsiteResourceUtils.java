d17df484bf6734a9481e3dcd4755579d0c1f75b2
Kusuma <kusuma-latha@ibm.com>
2026-09-08 12:02:10 +0530

    updated bootstrap and jquery versions

--- optaplanner-benchmark/src/main/java/org/optaplanner/benchmark/impl/report/WebsiteResourceUtils.java
+++ optaplanner-benchmark/src/main/java/org/optaplanner/benchmark/impl/report/WebsiteResourceUtils.java
@@ -29,16 +29,14 @@ public class WebsiteResourceUtils {
     private static final String RESOURCE_NAMESPACE = "/org/optaplanner/benchmark/impl/report/";
 
     public static void copyResourcesTo(File benchmarkReportDirectory) {
-        // Twitter Bootstrap
-        copyResource(benchmarkReportDirectory, "twitterbootstrap/css/bootstrap-responsive.css");
-        // copyResource(benchmarkReportDirectory, "twitterbootstrap/css/bootstrap-responsive.min.css");
+        // Bootstrap 4.1.2 
         copyResource(benchmarkReportDirectory, "twitterbootstrap/css/bootstrap.css");
         // copyResource(benchmarkReportDirectory, "twitterbootstrap/css/bootstrap.min.css");
         copyResource(benchmarkReportDirectory, "twitterbootstrap/css/prettify.css");
-        copyResource(benchmarkReportDirectory, "twitterbootstrap/img/glyphicons-halflings-white.png");
-        copyResource(benchmarkReportDirectory, "twitterbootstrap/img/glyphicons-halflings.png");
+        // Bootstrap 4 bundles Popper.js; glyphicons removed (not part of Bootstrap 4)
         copyResource(benchmarkReportDirectory, "twitterbootstrap/js/bootstrap.js");
         // copyResource(benchmarkReportDirectory, "twitterbootstrap/js/bootstrap.min.js");
+        // jQuery 3.5.0 
         copyResource(benchmarkReportDirectory, "twitterbootstrap/js/jquery.js");
         // copyResource(benchmarkReportDirectory, "twitterbootstrap/js/jquery.min.js");
         copyResource(benchmarkReportDirectory, "twitterbootstrap/js/prettify.js");
